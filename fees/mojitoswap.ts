0eba7697b9fe3c3162dcc6190ffdbdcc972d9f78
0xtawa <0xtawanofi@gmail.com>
2022-10-06 10:45:54 +0200

    Add mojitoswap fees from old repo PR

---
+++ fees/mojitoswap.ts
@@ -0,0 +1,19 @@
+import { Adapter } from "../adapters/types";
+import volumeAdapter from "@defillama/adapters/volumes/adapters/mojitoswap";
+import { getDexChainFees } from "../helpers/getUniSubgraphFees";
+
+const TOTAL_FEES = 0.003;
+const PROTOCOL_FEES = 0.0012;
+
+const feeAdapter = getDexChainFees({
+  totalFees: TOTAL_FEES,
+  protocolFees: PROTOCOL_FEES,
+  volumeAdapter
+});
+
+const adapter: Adapter = {
+  adapter: feeAdapter
+};
+
+
+export default adapter;
