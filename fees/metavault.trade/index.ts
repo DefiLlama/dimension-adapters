041110a5c84d547b39d50cc7935795c49a824689
0xtawa <0xtawanofi@gmail.com>
2022-10-27 23:06:11 +0200

    Move file to folder + add methodology

--- fees/metavault.trade/index.ts
+++ fees/metavault.trade/index.ts
@@ -1,9 +1,9 @@
 import { Chain } from "@defillama/sdk/build/general";
 import { gql, request } from "graphql-request";
-import type { ChainEndpoints } from "../adapters/types";
-import { Adapter } from "../adapters/types";
-import { POLYGON } from "../helpers/chains";
-import { getTimestampAtStartOfDayUTC } from "../utils/date";
+import type { ChainEndpoints } from "../../adapters/types";
+import { Adapter } from "../../adapters/types";
+import { POLYGON } from "../../helpers/chains";
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 
 const endpoints = {
   [POLYGON]: "https://api.thegraph.com/subgraphs/name/sdcrypt0/metavault-mvx-subgraph",
@@ -49,6 +49,9 @@ const adapter: Adapter = {
     [POLYGON]: {
       fetch: graphs(endpoints)(POLYGON),
       start: async () => 1655784000,
+      meta: {
+        methodology: 'All mint, burn, marginAndLiquidation and swap fees are collected and the daily fee amount is determined. Daily revenue is calculated as 30% of the total fee.'
+      }
     },
   },
 };
