206b8f14955fa0c7eefce6cf1f83ffc5254c0d85
0xtawa <0xtawanofi@gmail.com>
2022-11-04 10:37:57 +0100

    Remove logs

--- .github/workflows/getFileList.js
+++ .github/workflows/getFileList.js
@@ -4,7 +4,6 @@ const ADDED = parse(process.env.ADDED)
 const fileSet = new Set();
 
 [...MODIFIED, ...ADDED].forEach(file => {
-  console.log("all files->", JSON.stringify([...fileSet]))
   const [root, adapter] = file.split('/')
   if (ALLOWED_ROOTS.includes(root) && adapter !== undefined)
     fileSet.add(`${root}@${adapter}`)
