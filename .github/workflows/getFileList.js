14b7bf018463657249cff8b241189e5bff716c2a
0xtawa <0xtawanofi@gmail.com>
2022-11-04 09:58:08 +0100

    Trigger llamatester

--- .github/workflows/getFileList.js
+++ .github/workflows/getFileList.js
@@ -1,15 +1,14 @@
-const ALLOWED_ROOTS = ['volumes', 'dexs', 'options', 'derivatives', 'incentives', 'fees', 'options']
+const ALLOWED_ROOTS = ['dexs', 'options', 'derivatives', 'incentives', 'fees', 'options']
 const MODIFIED = parse(process.env.MODIFIED)
 const ADDED = parse(process.env.ADDED)
 const fileSet = new Set();
 
 [...MODIFIED, ...ADDED].forEach(file => {
+  console.log("all files->", JSON.stringify([...fileSet]))
   const [root, adapter] = file.split('/')
   if (ALLOWED_ROOTS.includes(root) && adapter !== undefined)
     fileSet.add(`${root}@${adapter}`)
 })
-
-console.log(JSON.stringify([...fileSet]))
 
 function parse(data) {
   return data.replace('[', '').replace(']', '').split(',')
