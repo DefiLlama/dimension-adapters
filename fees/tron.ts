3d1264181db139b032b3db91c50982696a55ccfa
0xngmi <0xngmi@protonmail.com>
2022-11-05 22:12:11 +0000

    add tron fees

---
+++ fees/tron.ts
@@ -0,0 +1,32 @@
+import { Adapter, ProtocolType } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import axios from 'axios';
+import { getTimestampAtStartOfDayUTC } from "../utils/date";
+import { getPrices } from "../utils/prices";
+
+const adapter: Adapter = {
+  adapter: {
+    [CHAIN.TRON]: {
+        fetch:  async (timestamp: number) => {
+            const ts = getTimestampAtStartOfDayUTC(timestamp)
+            const today = new Date(ts * 1000).toISOString().substring(0, "2022-11-03".length)
+            const dailyFees = await axios.get(`https://apilist.tronscanapi.com/api/turnover?size=1000&start=1575158400000&end=${Date.now()}&type=0`);
+            const trxFeesToday = dailyFees.data.data.find((d:any)=>d.day===today).total_trx_burn // excludes trx burned for USDD
+            const pricesObj = await getPrices(["coingecko:tron"], ts);
+            const usdFees = (trxFeesToday*pricesObj["coingecko:tron"].price).toString()
+
+            return {
+                timestamp,
+                totalFees: "0",
+                dailyFees: usdFees, 
+                totalRevenue: "0",
+                dailyRevenue: usdFees,
+            };
+        },
+        start: async () => 1575158400
+    },
+},
+  protocolType: ProtocolType.CHAIN
+}
+
+export default adapter;
