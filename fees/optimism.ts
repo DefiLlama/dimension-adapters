aac71766089ef2d1030b5a9eef868abb79857a3a
0xngmi <0xngmi@protonmail.com>
2022-11-06 23:20:47 +0000

    fix optimism adapter

--- fees/optimism.ts
+++ fees/optimism.ts
@@ -1,5 +1,5 @@
 import { Adapter, ProtocolType } from "../adapters/types";
-import { OPTIMISM } from "../helpers/chains";
+import { OPTIMISM, ETHEREUM } from "../helpers/chains";
 import { request, gql } from "graphql-request";
 import { getBalance } from '@defillama/sdk/build/eth';
 import { getPrices } from "../utils/prices";
@@ -8,7 +8,9 @@ import { ChainBlocks } from "../adapters/types";
 import BigNumber from "bignumber.js";
 import { getTimestampAtStartOfDayUTC } from "../utils/date";
 
-async function totalSpent(todaysBlock:number, yesterdaysBlock:number){
+async function totalSpent(todaysTimestamp:number, yesterdaysTimestamp:number, chainBlocks: ChainBlocks){
+    const todaysBlock = (await getBlock(todaysTimestamp, ETHEREUM, chainBlocks));
+    const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, ETHEREUM, {}));
     const graphQuerySpent = gql
         `query txFees {
         yesterday: totalSequencerGas(id: "optimism", block: { number: ${yesterdaysBlock} }) {
@@ -22,7 +24,10 @@ async function totalSpent(todaysBlock:number, yesterdaysBlock:number){
     return (new BigNumber(graphRes["today"].totalETH).minus(graphRes["yesterday"].totalETH)).div(1e18)
 }
 
-async function getFees(todaysBlock:number, yesterdaysBlock:number){
+async function getFees(todaysTimestamp:number, yesterdaysTimestamp:number, chainBlocks: ChainBlocks){
+    const todaysBlock = (await getBlock(todaysTimestamp, OPTIMISM, chainBlocks));
+    const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, OPTIMISM, {}));
+
     const graphQuery = gql
         `query txFees {
         yesterday: withdrawns(id: "1", block: { number: ${yesterdaysBlock} }) {
@@ -49,25 +54,21 @@ async function getFees(todaysBlock:number, yesterdaysBlock:number){
         chain: "optimism"
     })
 
-    const diffBalance = (new BigNumber(endBalance.output).minus(startBalance.output)).div(1e18)
-    return diffBalance.plus(dailyFee)
+    return (new BigNumber(endBalance.output).plus(dailyFee).minus(startBalance.output)).div(1e18)
 }
 
 const feesAdapter = async (timestamp: number, chainBlocks: ChainBlocks) => {
     const todaysTimestamp = timestamp
     const yesterdaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
 
-    const todaysBlock = (await getBlock(todaysTimestamp, OPTIMISM, chainBlocks));
-    const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, OPTIMISM, {}));
-
     const ethAddress = "ethereum:0x0000000000000000000000000000000000000000";
     const pricesObj: any = await getPrices([ethAddress], todaysTimestamp);
     const latestPrice = pricesObj[ethAddress]["price"]
 
-    const totalFees = await getFees(todaysBlock, yesterdaysBlock)
+    const totalFees = await getFees(todaysTimestamp, yesterdaysTimestamp, chainBlocks)
     const finalDailyFee = totalFees.times(latestPrice)
 
-    const totalSpentBySequencer = await totalSpent(todaysBlock, yesterdaysBlock)
+    const totalSpentBySequencer = await totalSpent(todaysTimestamp, yesterdaysTimestamp, chainBlocks)
     const revenue = (totalFees.minus(totalSpentBySequencer)).times(latestPrice)
 
     return {
