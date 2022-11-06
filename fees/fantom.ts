5976856227f6483e8639848baab36709dae199aa
0xngmi <0xngmi@protonmail.com>
2022-11-06 23:14:36 +0000

    add some more chains

--- fees/fantom.ts
+++ fees/fantom.ts
@@ -1,18 +1,12 @@
 import { Adapter, ProtocolType } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import axios from 'axios';
-import { getTimestampAtStartOfDayUTC } from "../utils/date";
-import { getPrices } from "../utils/prices";
+import { getEtherscanFees } from "../helpers/etherscanFees";
 
 const adapter: Adapter = {
   adapter: {
     [CHAIN.FANTOM]: {
         fetch:  async (timestamp: number) => {
-            const ts = getTimestampAtStartOfDayUTC(timestamp)
-            const dailyFees = await axios.get(`https://ftmscan.com/chart/transactionfee?output=csv`);
-            const feesToday = dailyFees.data.split("\n").find((d:any)=>d.split(",")?.[1]?.slice(1, -1)==ts)
-            const pricesObj = await getPrices(["coingecko:fantom"], ts);
-            const usdFees = Number(feesToday.split(",")[2].slice(1, -2))/1e18*pricesObj["coingecko:fantom"].price
+            const usdFees = await getEtherscanFees(timestamp, `https://ftmscan.com/chart/transactionfee?output=csv`, "coingecko:fantom")
 
             return {
                 timestamp,
