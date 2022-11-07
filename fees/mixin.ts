40282ebffbc638f11cde2affe14b1fe9b186a90a
0xngmi <0xngmi@protonmail.com>
2022-11-07 19:53:57 +0000

    mixin

---
+++ fees/mixin.ts
@@ -0,0 +1,4 @@
+import { blockscoutFeeAdapter } from "../helpers/blockscoutFees";
+
+// DO NOT LIST, unsure if correct
+export default blockscoutFeeAdapter("mixin", "https://scan.mvm.dev/api?module=stats&action=totalfees", "coingecko:mixin")
