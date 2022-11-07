3c4981bc3a09593af956a1557d849b1f4902abf4
0xngmi <0xngmi@protonmail.com>
2022-11-07 19:36:52 +0000

    arbitrum upgrade

--- fees/arbitrum.ts
+++ fees/arbitrum.ts
@@ -1,56 +1,46 @@
 import { Adapter, ChainBlocks, ProtocolType } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
-import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../utils/date";
+import { getTimestampAtStartOfDayUTC } from "../utils/date";
 import { getPrices } from "../utils/prices";
-const { request, gql } = require("graphql-request");
+import postgres from 'postgres'
 
-
-const URL = 'https://api.thegraph.com/subgraphs/name/dmihal/arbitrum-fees-collected'
-interface IValue {
-  totalFeesETH: string;
-}
-interface IDailyResponse {
-  yesterday: IValue;
-  today: IValue;
-}
-
-interface ITx {
-  value: string;
-}
-
-const getUniswapDateId = (date?: Date) => getUniqStartOfTodayTimestamp(date) / 86400;
+const sql = postgres(process.env.INDEXER_DB!);
 
 const adapter: Adapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
         fetch:  async (timestamp: number, _: ChainBlocks) => {
-          const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
-          const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
+          const now = new Date(timestamp*1e3)
+          const dayAgo = new Date(getTimestampAtStartOfDayUTC(timestamp)*1e3)
 
-          const todaysId = getUniswapDateId(new Date(todaysTimestamp * 1000));
-          const yesterdaysId = getUniswapDateId(new Date(yesterdaysTimestamp * 1000));
+          const gasFees = await sql`
+          SELECT
+          SUM(arbitrum.transactions.gas_used*arbitrum.transactions.gas_price)
+      FROM arbitrum.transactions
+          INNER JOIN arbitrum.blocks ON arbitrum.transactions.block_number = arbitrum.blocks.number
+      WHERE timestamp BETWEEN ${dayAgo} AND ${now}
+      ;
+  `
+const fees = gasFees[0].sum/1e18
 
-          const graphQueryDaily = gql
-          `query fees {
-            yesterday: fee(id: ${yesterdaysId}) {
-              totalFeesETH
-            }
-            today: fee(id: ${todaysId}) {
-              totalFeesETH
-            }
-          }`;
+  const sequencerGas = await sql`
+SELECT
+   sum(ethereum.transactions.gas_used*ethereum.transactions.gas_price)
+FROM ethereum.transactions
+  INNER JOIN ethereum.blocks ON ethereum.transactions.block_number = ethereum.blocks.number
+WHERE ( to_address = '\\x1c479675ad559dc151f6ec7ed3fbf8cee79582b6'::bytea -- Current inbox
+  OR to_address = '\\x4c6f947Ae67F572afa4ae0730947DE7C874F95Ef'::bytea -- Arbitrum: Sequencer Inbox 
+  OR to_address = '\\x51de512aa5dfb02143a91c6f772261623ae64564'::bytea -- Arbitrum: Validator1
+) AND (timestamp BETWEEN ${dayAgo} AND ${now});
+`
+const seqGas = sequencerGas[0].sum/1e18
+          const ethAddress = "ethereum:0x0000000000000000000000000000000000000000";
+          const ethPrice = (await getPrices([ethAddress], timestamp))[ethAddress].price;
 
-          const graphResDaily: IDailyResponse = await request(URL, graphQueryDaily);
-          const pricesObj = await getPrices(["coingecko:ethereum"], todaysTimestamp);
-          const feesETH = Number(graphResDaily.yesterday.totalFeesETH) - Number(graphResDaily.today.totalFeesETH);
-          const dailyFees = feesETH * pricesObj["coingecko:ethereum"].price
           return {
               timestamp,
-              totalFees: undefined,
-              dailyFees: dailyFees.toString(),
-              totalRevenue: "0",
-              dailyRevenue: "0",
+              dailyFees: (fees*ethPrice).toString(),
+              dailyRevenue: ((fees-seqGas)*ethPrice).toString(),
           };
         },
         start: async () => 1575158400
