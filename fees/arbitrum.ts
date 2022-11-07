0b6c82a436fb57a94eb69f8079dc04791d20c3e7
dtmkeng <phumaisut@gmail.com>
2022-11-07 22:21:42 +0700

    delete not use

--- fees/arbitrum.ts
+++ fees/arbitrum.ts
@@ -1,9 +1,7 @@
-import axios from "axios";
 import { Adapter, ChainBlocks, ProtocolType } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getBlock } from "../helpers/getBlock";
 import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
-import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC, getTimestampAtStartOfPreviousDayUTC } from "../utils/date";
+import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../utils/date";
 import { getPrices } from "../utils/prices";
 const { request, gql } = require("graphql-request");
 
@@ -22,19 +20,6 @@ interface ITx {
 }
 
 const getUniswapDateId = (date?: Date) => getUniqStartOfTodayTimestamp(date) / 86400;
-
-const SEQUENCER_FEES = '0x18A08f3CA72DC4B5928c26648958655690b215ac'
-const NETWORK_INFRA_FEES = '0x582A62dB643BCFF3B0Bf1DA45f812e3a354d7518'
-const CONGESTION_FEES = '0xb04D2C62c0Cd8cec5691Cefd2E7CF041EBD26382'
-
-const getWithdrawalTxs = async (address: string, startblock: number, endblock: number) => {
-  const url = `https://api.arbiscan.io/api?module=account&action=txlist&address=${address}&startblock=${startblock}&endblock=${endblock}`;
-  const data: ITx[] = await axios.get(url).then((e: any) => e.data.result).catch((err: any) => {
-    console.log(`error get tx list: ${err}`);
-    return [];
-  });
-  return data.reduce((acc, { value }) => acc + Number(value) / 1e18, 0)
-};
 
 const adapter: Adapter = {
   adapter: {
