021dc64ef2440666762768bb2f66de2898375ba5
0xR <99395193+0xrmvdao@users.noreply.github.com>
2022-11-08 22:46:16 +0300

    Add Metavault.Trade Dexs Adapter (#17)

--- fees/metavault.trade/index.ts
+++ fees/metavault.trade/index.ts
@@ -48,7 +48,7 @@ const adapter: Adapter = {
   adapter: {
     [POLYGON]: {
       fetch: graphs(endpoints)(POLYGON),
-      start: async () => 1655784000,
+      start: async () => 1654041600,
       meta: {
         methodology: 'All mint, burn, marginAndLiquidation and swap fees are collected and the daily fee amount is determined. Daily revenue is calculated as 30% of the total fee.'
       }
