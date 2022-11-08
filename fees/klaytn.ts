de8eef3719448ff9803f4ddb5623f38dee699486
dtmkeng <phumaisut@gmail.com>
2022-11-09 00:07:10 +0700

    fix cal fee klaytn with price

--- fees/klaytn.ts
+++ fees/klaytn.ts
@@ -9,14 +9,16 @@ const fetch = async (timestamp: number): Promise<FetchResult> => {
   const form = new Date(startTime * 1000).toISOString().split('T')[0];
   const till = new Date(timestamp * 1000).toISOString().split('T')[0];
   const result: ITx[] = await adapterBitqueryFeesEthereumNetwork(form, till, "klaytn");
-  const totalFees = result.filter((a: ITx) => new Date(a.date.date) <= new Date(till)).reduce((a: number, b: ITx)=> a + b.gasValue, 0);
-  const dailyFees = result.find((a: ITx) => new Date(a.date.date) <= new Date(till))?.gasValue
+  const totalFees = result.filter((a: ITx) => new Date(a.date.date).getTime() <= new Date(till).getTime()).reduce((a: number, b: ITx)=> a + b.gasValue, 0);
+  const dailyFees = result.find((a: ITx) => new Date(a.date.date).getTime() == new Date(till).getTime())?.gasValue
   const price_id = 'coingecko:klay-token'
   const price = (await getPrices([price_id], timestamp))[price_id].price;
+  const dailyFeesUsd = (dailyFees || 0) * price
+  const totalFeesUsd = totalFees * price;
   return {
     timestamp,
-    totalFees: (totalFees * price).toString(),
-    dailyFees: (dailyFees || 0 * price).toString()
+    totalFees: totalFeesUsd.toString(),
+    dailyFees: dailyFeesUsd.toString()
   };
 };
 
