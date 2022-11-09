04e3301182c7c5ae09601b4bbcf0815b64ab5ae1
0xtawa <0xtawanofi@gmail.com>
2022-11-09 13:30:39 +0100

    Fix fees calc opensea adapter

--- fees/opensea/index.ts
+++ fees/opensea/index.ts
@@ -29,20 +29,15 @@ const getTotalFeesnRev = async (timestamp: number, graphUrl: string) => {
           marketplaceRevenueETH
         }
       }`;
-  const ethAddress = "ethereum:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
-  const pricesObj: any = await getPrices([ethAddress], timestamp);
-  const latestPrice = new BigNumber(pricesObj[ethAddress]["price"])
 
   const graphRes = await request(graphUrl, graphQuery)
   const data = graphRes['marketplaceDailySnapshot'];
   if (!data) return
-  const dailyFee = new BigNumber(data.totalRevenueETH).multipliedBy(latestPrice)
-  const dailyRev = new BigNumber(data.marketplaceRevenueETH).multipliedBy(latestPrice)
 
   return {
     timestamp,
-    totalFees: dailyFee.toString(),
-    totalRevenue: dailyRev.toString(),
+    totalFeesETH: data.totalRevenueETH,
+    totalRevenueETH: data.marketplaceRevenueETH,
   };
 }
 
@@ -53,12 +48,19 @@ const graphs = (graphUrls: ChainEndpoints) => {
       const snapshot24h = await getTotalFeesnRev(todaysTimestamp, graphUrls[chain])
       const snapshot48h = await getTotalFeesnRev(todaysTimestamp - 60 * 60 * 24, graphUrls[chain])
       if (!snapshot24h || !snapshot48h) return { timestamp: todaysTimestamp }
+      const ethAddress = "ethereum:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+      const pricesObj: any = await getPrices([ethAddress], todaysTimestamp);
+      const latestPrice = new BigNumber(pricesObj[ethAddress]["price"])
+
+      const dailyFeesETH = snapshot24h.totalFeesETH - snapshot48h.totalFeesETH
+      const dailyRevenueETH = snapshot24h.totalRevenueETH - snapshot48h.totalRevenueETH
+      
       return {
         timestamp: todaysTimestamp,
-        dailyFees: String(+snapshot24h?.totalFees - +snapshot48h?.totalFees),
-        dailyRevenue: String(+snapshot24h?.totalRevenue - +snapshot48h?.totalRevenue),
-        totalFees: snapshot24h?.totalFees,
-        totalRevenue: snapshot24h?.totalRevenue
+        dailyFees: (new BigNumber(dailyFeesETH).multipliedBy(latestPrice)).toString(),
+        dailyRevenue: (new BigNumber(dailyRevenueETH).multipliedBy(latestPrice)).toString(),
+        totalFees: (new BigNumber(snapshot24h.totalFeesETH).multipliedBy(latestPrice)).toString(),
+        totalRevenue: (new BigNumber(snapshot24h.totalRevenueETH).multipliedBy(latestPrice)).toString()
       }
     };
   };
