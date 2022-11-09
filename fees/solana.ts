85b8749248e96777c7aacf2eb88daa12e13b8a68
0xtawa <0xtawanofi@gmail.com>
2022-11-09 16:12:59 +0100

    Remove '0' totalRevenue on Solana

--- fees/solana.ts
+++ fees/solana.ts
@@ -13,26 +13,25 @@ interface IFees {
 const adapter: Adapter = {
   adapter: {
     [CHAIN.SOLANA]: {
-        fetch:  async (timestamp: number) => {
-            const ts = getTimestampAtStartOfDayUTC(timestamp)
-            const today = new Date(ts * 1000).toISOString().split('T')[0].split('-').reverse().join('-');
+      fetch: async (timestamp: number) => {
+        const ts = getTimestampAtStartOfDayUTC(timestamp)
+        const today = new Date(ts * 1000).toISOString().split('T')[0].split('-').reverse().join('-');
 
-            const dailyFees: IFees = (await axios.get(`https://hyper.solana.fm/v3/tx-fees?date=${today}`)).data;
-            const solanaFee = dailyFees.total_tx_fees
-            const pricesObj = await getPrices(["coingecko:solana"], ts);
-            const usdFees = (solanaFee/1e9 * pricesObj["coingecko:solana"].price);
-            const dailyRevenue = usdFees * 0.5;
+        const dailyFees: IFees = (await axios.get(`https://hyper.solana.fm/v3/tx-fees?date=${today}`)).data;
+        const solanaFee = dailyFees.total_tx_fees
+        const pricesObj = await getPrices(["coingecko:solana"], ts);
+        const usdFees = (solanaFee / 1e9 * pricesObj["coingecko:solana"].price);
+        const dailyRevenue = usdFees * 0.5;
 
-            return {
-                timestamp,
-                dailyFees: usdFees.toString(),
-                totalRevenue: "0",
-                dailyRevenue: dailyRevenue.toString(),
-            };
-        },
-        start: async () => 1610841600
+        return {
+          timestamp,
+          dailyFees: usdFees.toString(),
+          dailyRevenue: dailyRevenue.toString(),
+        };
+      },
+      start: async () => 1610841600
     },
-},
+  },
   protocolType: ProtocolType.CHAIN
 }
 
