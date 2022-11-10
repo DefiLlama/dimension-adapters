e9de8e853a91028935b7eb69e883a5dc40c998a8
0xtawa <0xtawanofi@gmail.com>
2022-11-10 18:19:35 +0100

    Add new dexs

--- dexs/cherryswap/index.ts
+++ dexs/cherryswap/index.ts
@@ -1,12 +1,67 @@
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { DEFAULT_DAILY_VOLUME_FIELD, DEFAULT_TOTAL_VOLUME_FIELD, getChainVolume } from "../../helpers/getUniSubgraphVolume";
 import { CHAIN } from "../../helpers/chains";
+import request, { gql } from "graphql-request";
+import { SimpleAdapter } from "../../adapters/types";
 
-const adapter = univ2Adapter({
+const blocksGraph =
+  "https://okinfo.cherryswap.net/subgraphs/name/cherryswap/block";
+
+const blockQuery = gql`
+  query blocks($timestampFrom: Int!, $timestampTo: Int!) {
+    blocks(
+      first: 1
+      orderBy: timestamp
+      orderDirection: asc
+      where: { timestamp_gt: $timestampFrom, timestamp_lt: $timestampTo }
+    ) {
+      id
+      number
+      timestamp
+      __typename
+    }
+  }
+`;
+
+
+const endpoints = {
   [CHAIN.OKEXCHAIN]: "https://okinfo.cherryswap.net/subgraphs/name/cherryswap/cherrysubgraph"
-}, {
-  factoriesName: "uniswapFactories",
-  dayData: "uniswapDayData",
+}
+const getCustomBlock = async (timestamp: number) => {
+  const block = Number(
+    (
+      await request(blocksGraph, blockQuery, {
+        timestampFrom: timestamp - 30,
+        timestampTo: timestamp + 30,
+      })
+    ).blocks[0].number
+  );
+
+  return block;
+};
+
+const DAILY_VOLUME_FACTORY = "uniswapDayData";
+
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: "uniswapFactories",
+    field: DEFAULT_TOTAL_VOLUME_FIELD,
+  },
+  dailyVolume: {
+    factory: DAILY_VOLUME_FACTORY,
+    field: DEFAULT_DAILY_VOLUME_FIELD,
+    dateField: 'date'
+  },
+  getCustomBlock,
 });
 
-adapter.adapter.okexchain.start = async () => 1627385129;
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.OKEXCHAIN]: {
+      fetch: graphs(CHAIN.OKEXCHAIN),
+      start: async () => 1627385129,
+    },
+  },
+};
+
 export default adapter;
