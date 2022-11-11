ea971cd4d1695532c54ba8092eff9191bb384dc9
dtmkeng <phumaisut@gmail.com>
2022-11-11 19:08:09 +0700

    add fee solarbeam

---
+++ fees/solarbeam.ts
@@ -0,0 +1,19 @@
+import { getDexChainFees } from "../helpers/getUniSubgraphFees";
+import volumeAdapter from "../dexs/solarbeam";
+import { Adapter, BaseAdapter } from "../adapters/types";
+
+const TOTAL_FEES = 0.0025;
+const PROTOCOL_FEES = 0.0005;
+
+const feeAdapter: BaseAdapter = getDexChainFees({
+  totalFees: TOTAL_FEES,
+  protocolFees: PROTOCOL_FEES,
+  volumeAdapter
+});
+
+const adapter: Adapter = {
+  adapter: feeAdapter
+};
+
+export default adapter;
+// ref https://docs.solarbeam.io/faq#is-there-a-transaction-fee-on-solarbeam
