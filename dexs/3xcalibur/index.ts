cf6a969329ec89844df0ad9c071851b8000bcd44
Leez <leez@3six9.io>
2022-11-15 13:40:29 +0100

    3xcalibur dex volume

---
+++ dexs/3xcalibur/index.ts
@@ -0,0 +1,36 @@
+import { SimpleAdapter } from "../../adapters/types";
+import { getStartTimestamp } from "../../helpers/getStartTimestamp";
+import { DEFAULT_DAILY_VOLUME_FIELD, DEFAULT_TOTAL_VOLUME_FIELD, getChainVolume } from "../../helpers/getUniSubgraphVolume";
+import { CHAIN } from "../../helpers/chains";
+
+const endpoints = {
+  [CHAIN.ARBITRUM]: "https://api.thegraph.com/subgraphs/name/0xleez/xcali-arbitrum",
+};
+
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: "swapFactories",
+    field: DEFAULT_TOTAL_VOLUME_FIELD,
+  },
+  dailyVolume: {
+    factory: "uniswapDayData",
+    field: DEFAULT_DAILY_VOLUME_FIELD,
+  },
+});
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.ARBITRUM]: {
+      fetch: graphs(CHAIN.ARBITRUM),
+      start: getStartTimestamp({
+        endpoints: endpoints,
+        chain: CHAIN.ARBITRUM,
+        volumeField: DEFAULT_DAILY_VOLUME_FIELD,
+        dailyDataField: "uniswapDayDatas",
+      }),
+    },
+  },
+};
+
+export default adapter;
