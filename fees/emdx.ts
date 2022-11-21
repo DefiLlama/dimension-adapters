89c5a11a80858d0989170e3622feb6afc8d841ea
0xgnek <0xgnek@gmail.com>
2022-11-21 17:43:37 +0000

    fix find by date

--- fees/emdx.ts
+++ fees/emdx.ts
@@ -1,11 +1,18 @@
 import { Adapter } from "../adapters/types";
-import { AVAX } from "../helpers/chains";
+import { CHAIN } from "../helpers/chains";
 import axios from "axios";
+import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
 
 const endpoint = "https://app-api.dune.com/v1/graphql";
 
-const getFees = async () => {
-  const response = await axios.post(endpoint,
+interface IData {
+  cumulative_fees: number;
+  day: string;
+  volume: number;
+}
+
+const getFees = async (): Promise<IData[]> => {
+  const response: IData[] = (await axios.post(endpoint,
     {
       operationName: "GetExecution",
       variables: {
@@ -15,27 +22,25 @@ const getFees = async () => {
       },
       query: "query\":\"query GetExecution($execution_id: String!, $query_id: Int!, $parameters: [Parameter!]!) {\\n  get_execution(\\n    execution_id: $execution_id\\n    query_id: $query_id\\n    parameters: $parameters\\n  ) {\\n    execution_queued {\\n      execution_id\\n      execution_user_id\\n      position\\n      execution_type\\n      created_at\\n      __typename\\n    }\\n    execution_running {\\n      execution_id\\n      execution_user_id\\n      execution_type\\n      started_at\\n      created_at\\n      __typename\\n    }\\n    execution_succeeded {\\n      execution_id\\n      runtime_seconds\\n      generated_at\\n      columns\\n      data\\n      __typename\\n    }\\n    execution_failed {\\n      execution_id\\n      type\\n      message\\n      metadata {\\n        line\\n        column\\n        hint\\n        __typename\\n      }\\n      runtime_seconds\\n      generated_at\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}"
     }
-  );
-
-  return response.data.data.get_execution.execution_succeeded?.data[0];
+  )).data.data.get_execution.execution_succeeded?.data;
+  return response
 }
 
 const fetch = async (timestamp: number) => {
-  const fees = await getFees();
-
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+  const info = await getFees();
+  const dailyFee: (IData | undefined) = info.find((e: IData) =>  (new Date(e.day.split('T')[0]).getTime() / 1000) === dayTimestamp);
   return {
     timestamp,
-    totalFees: fees?.cumulative_fees || 0,
-    dailyFees: fees?.volume || 0,
-    totalRevenue: '0',
-    dailyRevenue: '0',
+    totalFees: `${dailyFee?.cumulative_fees || 0}`,
+    dailyFees: `${dailyFee?.volume || 0}`,
   };
 };
 
 
 const adapter: Adapter = {
   adapter: {
-    [AVAX]: {
+    [CHAIN.AVAX]: {
       fetch: fetch,
       start: async () => 1653048000
     },
