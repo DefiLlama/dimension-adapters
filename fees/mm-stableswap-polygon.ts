9d3cd0b6d9ac16ad3b671df1aa811c3cd694f11e
0xgnek <0xgnek@gmail.com>
2022-11-22 14:40:58 +0000

    add mm-stableswap-polygon fees

---
+++ fees/mm-stableswap-polygon.ts
@@ -0,0 +1,18 @@
+import { getDexChainFees } from "../helpers/getUniSubgraphFees";
+import volumeAdapter from "../dexs/mm-stableswap-polygon";
+import { Adapter, BaseAdapter } from "../adapters/types";
+
+const TOTAL_FEES = 0.0017;
+const PROTOCOL_FEES = 0.0002;
+
+const feeAdapter: BaseAdapter = getDexChainFees({
+  totalFees: TOTAL_FEES,
+  protocolFees: PROTOCOL_FEES,
+  volumeAdapter
+});
+
+const adapter: Adapter = {
+  adapter: feeAdapter
+};
+
+export default adapter;
