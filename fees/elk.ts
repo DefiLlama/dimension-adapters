1a11aa6eb3b20c392c608679cb66f8fe1de9c024
0xgnek <0xgnek@gmail.com>
2022-11-24 03:31:24 +0000

    update elk fees adapter

--- fees/elk.ts
+++ fees/elk.ts
@@ -3,11 +3,9 @@ import volumeAdapter from "../dexs/elk";
 import { Adapter, BaseAdapter } from "../adapters/types";
 
 const TOTAL_FEES = 0.003;
-const PROTOCOL_FEES = 0;
 
 const feeAdapter: BaseAdapter = getDexChainFees({
   totalFees: TOTAL_FEES,
-  protocolFees: PROTOCOL_FEES,
   volumeAdapter
 });
 
