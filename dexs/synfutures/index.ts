3ebd1e5e24e15dd82b8d91af78bb9ddddcce4ef5
Eric Fan <yfanal@connect.ust.hk>
2022-11-24 16:44:38 +0800

    synfutures v1 volume

--- dexs/synfutures/index.ts
+++ dexs/synfutures/index.ts
@@ -1,27 +1,25 @@
+// SynFutures v1 volume
 import { Adapter, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 import axios from "axios";
 import { Chain } from '@defillama/sdk/build/general';
 import customBackfill from "../../helpers/customBackfill";
+import { getPrices } from "../../utils/prices";
 const { request, gql } = require("graphql-request");
 
-const info = {
+const info: {[key: string]: any} = {
+  polygon: {
+    subgraph: 'https://api.thegraph.com/subgraphs/name/synfutures/polygon-v1',
+  },
   ethereum: {
     subgraph: 'https://api.thegraph.com/subgraphs/name/synfutures/ethereum-v1',
-    startTimestamp: 1623168000,
   },
   bsc: {
     subgraph: 'https://api.thegraph.com/subgraphs/name/synfutures/bsc-v1',
-    startTimestamp: 1623168000,
-  },
-  polygon: {
-    subgraph: 'https://api.thegraph.com/subgraphs/name/synfutures/polygon-v1',
-    startTimestamp: 1623168000,
   },
   arbitrum: {
     subgraph: 'https://api.thegraph.com/subgraphs/name/synfutures/arbitrum-one-v1',
-    startTimestamp: 1623168000,
   },
 }
 
@@ -31,53 +29,63 @@ interface DailyVolume {
   volume: number;
 }
 
-const QUERY_DAILY_VOLUME = gql`{
-  query dailyVolumes(endDayId: Int!) {
-    quoteDataDailySnapshots(first: 1000, where: {dayId_lte: $endDayId}) {
-      id
-      dayId
-      quote{
-        id
-        symbol
-      }
-      dayTradeVolume
-    }
-  }`;
+export function dayIdFromTimestamp(timestamp: number): number {
+  return Math.floor(timestamp / 86400);
+}
 
 const fetch = (chain: Chain) => {
   return async (timestamp: number) => {
-    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-    console.info('xxxx', chain, timestamp, dayTimestamp);
-    // const data = await request(info[chain].subgraph, QUERY_DAILY_VOLUME, { endDayId: Math.floor(timestamp / 86400)});
-    // console.info('data:', data);
-    // const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint(chains[chain])))?.data;
-    // const totalVolume = historicalVolume
-    //   .filter(volItem =>Number(volItem.timestamp)/1000 <= dayTimestamp)
-    //   .reduce((acc, { dailyVolume }) => acc + Number(dailyVolume), 0)
+    const totdayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+    const endDayId = dayIdFromTimestamp(totdayTimestamp);
+  
+    const graphQL = `{
+      quoteDataDailySnapshots(first: 1000, where: {dayId: ${endDayId}}) {
+        id
+        dayId
+        quote{
+          id
+          symbol
+        }
+        dayTradeVolume
+      }
+    }`;
 
-    // const dailyVolume = historicalVolume
-    //   .find(dayItem =>Number(dayItem.timestamp)/1000 === dayTimestamp)?.dailyVolume
+    const data = await request(info[chain].subgraph, graphQL);
+
+    let sum = 0;
+    for (const dailyData of data.quoteDataDailySnapshots) {
+      const tokenId = chain+':'+dailyData.quote.id;
+      const prices = await getPrices([tokenId], totdayTimestamp);
+      sum += Number(dailyData.dayTradeVolume) / 10 ** 18 * prices[tokenId].price;
+    }
+
     return {
-      totalVolume: `12000`,
-      dailyVolume: `200`,
-      timestamp: dayTimestamp,
+      totalVolume: undefined,
+      dailyVolume: `${sum}`,
+      timestamp: totdayTimestamp,
     };
   }
 };
 
 const adapter: SimpleAdapter = {
-  adapter: Object.keys(info).reduce((acc, chain: any) => {
-    console.info(acc);
-    return {
-      ...acc,
-      [chain]: {
-        fetch: fetch(chain as Chain),
-        // start: () => 1623168000,
-        start: () => 1668873600,
-        customBackfill: customBackfill(chain as Chain, fetch)
-      }
-    }
-  }, {})
+  adapter: {
+    [CHAIN.POLYGON]: {
+      fetch: fetch(CHAIN.POLYGON),
+      start: async () => 1628128417,
+    },
+    [CHAIN.ARBITRUM]: {
+      fetch: fetch(CHAIN.ARBITRUM),
+      start: async () => 1659750817,
+    },
+    [CHAIN.ETHEREUM]: {
+      fetch: fetch(CHAIN.ETHEREUM),
+      start: async () => 1628128417,
+    },
+    [CHAIN.BSC]: {
+      fetch: fetch(CHAIN.BSC),
+      start: async () => 1628128417,
+    },    
+  },
 };
 
 export default adapter;
