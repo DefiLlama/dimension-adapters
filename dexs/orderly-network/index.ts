bcc877636ac2e684fe2519f7bb80c8dbcb25a9cb
Spring Chiu <yongchun@orderly.network>
2022-12-01 16:45:08 +0800

    feat: add trade volume of orderly-network

---
+++ dexs/orderly-network/index.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import type { SimpleAdapter } from "../../adapters/types";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+
+const dateToTs = (date: string) => new Date(date).getTime() / 1000
+
+const api = "https://api.orderly.org/md/volume/daily_stats"
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    "near":{
+      start: async()=>{
+        const data = await axios.get(api)
+        return dateToTs(data.data[0].date)
+      },
+      fetch: async(ts)=>{
+        const data = await axios.get(api)
+        const cleanTimestamp = getUniqStartOfTodayTimestamp(new Date(ts * 1000))
+        return {
+          timestamp: cleanTimestamp,
+          dailyVolume: data.data.find((t:any)=>dateToTs(t.date) === cleanTimestamp)?.volume
+        }
+      }
+    }
+  }
+};
+
+export default adapter;
