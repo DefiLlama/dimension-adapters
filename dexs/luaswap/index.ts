012c3f9cadf48bcca252a0bba3b75bbf1d99714e
0xtawa <0xtawanofi@gmail.com>
2022-12-05 16:41:57 +0100

    Fix luaswap

--- dexs/luaswap/index.ts
+++ dexs/luaswap/index.ts
@@ -1,7 +1,7 @@
 import { CHAIN } from "../../helpers/chains";
 import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
 export default univ2Adapter({
   [CHAIN.TOMOCHAIN]: "https://api.luaswap.org/subgraphs/name/phucngh/Luaswap3",
-  // [CHAIN.ETHEREUM]: "https://api.luaswap.org/subgraphs/name/luasubgraph/Luaswap3" // invalid url
+  [CHAIN.ETHEREUM]: "https://api.thegraph.com/subgraphs/name/phucngh/luaswap"
 }, {});
