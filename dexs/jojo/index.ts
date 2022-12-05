ab99239f62e33a6f017324f30dadf588e9306bc8
KittyFu307 <61280608+KittyFu307@users.noreply.github.com>
2022-12-05 23:40:11 +0800

    add jojo volume (#79)
    * add jojo volume
    
    * Simplify adapter with new helper function
    
    * add timestamp
    
    * Delete index.ts.bak
    
    Co-authored-by: KittyFu <fushikun307@outlook.com>
    Co-authored-by: 0xtawa <0xtawanofi@gmail.com>
    Co-authored-by: 0xtawa <106731946+0xtawa@users.noreply.github.com>

---
+++ dexs/jojo/index.ts
@@ -0,0 +1,26 @@
+import { CHAIN } from "../../helpers/chains";
+import { univ2DimensionAdapter } from "../../helpers/getUniSubgraph";
+
+const DEFAULT_DAILY_VOLUME_FACTORY = "dailyVolume";
+const DEFAULT_DAILY_DATE_FIELD = "timestamp";
+const DEFAULT_DAILY_VOLUME_FIELD = "dailyVolume";
+const DEFAULT_TOTAL_VOLUME_FACTORY = "jojodealers";
+const DEFAULT_TOTAL_VOLUME_FIELD = "totalVolumeUSD";
+
+const adapter = univ2DimensionAdapter({
+    graphUrls: {
+        [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/kittyfu307/jojov1",
+    },
+    dailyVolume: {
+        factory: DEFAULT_DAILY_VOLUME_FACTORY,
+        field: DEFAULT_DAILY_VOLUME_FIELD,
+        dateField: DEFAULT_DAILY_DATE_FIELD
+    },
+    totalVolume: {
+        factory: DEFAULT_TOTAL_VOLUME_FACTORY,
+        field: DEFAULT_TOTAL_VOLUME_FIELD
+    }
+}, {});
+
+adapter.adapter.bsc.start = async () => 1667260800;
+export default adapter;
