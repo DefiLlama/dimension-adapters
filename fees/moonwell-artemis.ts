e04c604241ff4f97acb66dc616948f33b5e7726a
0xgnek <0xgnek@gmail.com>
2022-12-06 16:38:40 +0000

    add fees moonwell artemis

---
+++ fees/moonwell-artemis.ts
@@ -0,0 +1,51 @@
+import { Adapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { request, gql } from "graphql-request";
+import type { ChainEndpoints } from "../adapters/types"
+import { Chain } from '@defillama/sdk/build/general';
+import BigNumber from "bignumber.js";
+import { getTimestampAtStartOfDayUTC } from "../utils/date";
+
+const endpoints = {
+  [CHAIN.MOONBEAN]: "https://api.thegraph.com/subgraphs/name/messari/moonwell-moonbeam"
+}
+
+
+const graphs = (graphUrls: ChainEndpoints) => {
+  return (chain: Chain) => {
+    return async (timestamp: number) => {
+      const dateId = Math.floor(getTimestampAtStartOfDayUTC(timestamp) / 86400)
+
+      const graphQuery = gql
+      `{
+        financialsDailySnapshot(id: ${dateId}) {
+            dailyTotalRevenueUSD
+            dailyProtocolSideRevenueUSD
+        }
+      }`;
+
+      const graphRes = await request(graphUrls[chain], graphQuery);
+
+      const dailyFee = new BigNumber(graphRes.financialsDailySnapshot.dailyTotalRevenueUSD);
+      const dailyRev = new BigNumber(graphRes.financialsDailySnapshot.dailyProtocolSideRevenueUSD);
+
+      return {
+        timestamp,
+        dailyFees: dailyFee.toString(),
+        dailyRevenue: dailyRev.toString(),
+      };
+    };
+  };
+};
+
+
+const adapter: Adapter = {
+  adapter: {
+    [CHAIN.MOONBEAN]: {
+        fetch: graphs(endpoints)(CHAIN.MOONBEAN),
+        start: async ()  => 1656115200,
+    },
+  }
+}
+
+export default adapter;
