ad1fbae3c893620c707b5d6cb2eb2e129727c167
0xgnek <0xgnek@gmail.com>
2022-12-07 18:31:43 +0000

    fix volume openbook

--- dexs/openbook/index.ts
+++ dexs/openbook/index.ts
@@ -1,42 +1,24 @@
-import { gql, GraphQLClient } from "graphql-request";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import fetchURL from "../../utils/fetchURL";
 
-const getDailyVolume = () => {
-  return gql`{
-    dailyStats {
-      volume {
-        interval
-        volumeUsd
-      }
-    }
-  }`
-}
 
-const graphQLClient = new GraphQLClient("http://51.159.109.243:8079");
-const getGQLClient = () => {
-  return graphQLClient
-}
-
-interface IGraphResponse {
-  interval: string;
-  volumeUsd: string;
+interface IVolume {
+  value: number;
+  humanTime: string;
 }
 
 const fetch = async (timestamp: number) => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  const historicalVolume: IGraphResponse[] = (await getGQLClient().request(getDailyVolume())).dailyStats.volume;
-  const totalVolume = historicalVolume
-  .filter(volItem => (new Date(volItem.interval.split('T')[0]).getTime() / 1000) <= dayTimestamp)
-  .reduce((acc, { volumeUsd }) => acc + Number(volumeUsd)/10**6, 0)
-
-const dailyVolume = historicalVolume
-  .find(dayItem => (new Date(dayItem.interval.split('T')[0]).getTime() / 1000) === dayTimestamp)?.volumeUsd
+  const url = `https://api.solscan.io/amm/chart?source=openbook&type=all&chart=total_volume24h`;
+  const historicalVolume: IVolume[] = (await fetchURL(url)).data.data.items;
+  const dailyVolume = historicalVolume
+    .find(dayItem => (new Date(dayItem.humanTime.split('T')[0]).getTime() / 1000) === dayTimestamp)?.value
 
   return {
     timestamp: dayTimestamp,
-    dailyVolume: `${dailyVolume ? Number(dailyVolume)/10**6 : 0}`,
+    dailyVolume: `${dailyVolume ? Number(dailyVolume) : 0}`,
   }
 }
 
