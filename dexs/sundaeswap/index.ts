1f3bd582799ff8df0fca634bd3f166e449afedd4
0xngmi <80541789+0xngmi@users.noreply.github.com>
2022-12-16 18:57:09 +0000

    remove totalVolume from sundaeswap

--- dexs/sundaeswap/index.ts
+++ dexs/sundaeswap/index.ts
@@ -14,9 +14,6 @@ interface IVolumeall {
 const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
   const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint))?.data.response;
-  const totalVolume = historicalVolume
-    .filter(volItem => (new Date(volItem.day).getTime() / 1000) <= dayTimestamp)
-    .reduce((acc, { volumeLovelace }) => acc + Number(volumeLovelace), 0)
 
   const dailyVolume = historicalVolume
     .find(dayItem => getUniqStartOfTodayTimestamp(new Date(dayItem.day)) === dayTimestamp)?.volumeLovelace
@@ -29,7 +26,6 @@ const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
   });
 
   return {
-    totalVolume: totalVolume ? String(Number(totalVolume) / 1e6 * prices.data.coins["coingecko:cardano"].price) : "0",
     dailyVolume: dailyVolume ? String(Number(dailyVolume) / 1e6 * prices.data.coins["coingecko:cardano"].price) : "0",
     timestamp: dayTimestamp,
   };
