c15b9848fd1d63e579e0a106e30ae31ee407fa4e
0xgnek <0xgnek@gmail.com>
2022-12-23 15:18:29 +0000

    add ampleswap dexs

---
+++ dexs/ampleswap/index.ts
@@ -0,0 +1,11 @@
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { CHAIN } from "../../helpers/chains";
+
+const endpoints = {
+  [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/shrinivasmani/ampleswapgraph",
+};
+const adapter = univ2Adapter(endpoints, {});
+
+adapter.adapter.bsc.start = async()=> 1631404800;
+
+export default adapter
