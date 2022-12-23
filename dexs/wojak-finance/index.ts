9a5c87f72cf291b9ab65a667672f33bf3e4d337b
0xgnek <0xgnek@gmail.com>
2022-12-23 13:10:36 +0000

    add wojak-finance dexs

---
+++ dexs/wojak-finance/index.ts
@@ -0,0 +1,71 @@
+import { CHAIN } from "../../helpers/chains";
+import { SimpleAdapter } from "../../adapters/types";
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24
+const { request, gql } = require("graphql-request");
+const {
+  getChainVolume,
+  DEFAULT_TOTAL_VOLUME_FIELD,
+  DEFAULT_DAILY_VOLUME_FIELD,
+} = require("../../helpers/getUniSubgraphVolume");
+
+const endpoints = {
+  [CHAIN.DOGECHAIN]:
+    "https://api.dogechainhealth.com/subgraphs/name/wojakswap/exchange",
+};
+
+const blocksGraph =
+  "https://api.dogechainhealth.com/subgraphs/name/wojakswap/blocks";
+
+const blockQuery = gql`
+  query blocks($timestampFrom: Int!, $timestampTo: Int!) {
+    blocks(
+      first: 1
+      orderBy: timestamp
+      orderDirection: asc
+      where: { timestamp_gt: $timestampFrom, timestamp_lt: $timestampTo }
+    ) {
+      id
+      number
+      timestamp
+      __typename
+    }
+  }
+`;
+
+const getCustomBlock = async (timestamp: number) => {
+  const block = Number(
+    (
+      await request(blocksGraph, blockQuery, {
+        timestampFrom: timestamp - ONE_DAY_IN_SECONDS,
+        timestampTo: timestamp + ONE_DAY_IN_SECONDS,
+      })
+    ).blocks[0].number
+  );
+
+  return block;
+};
+
+
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: "pancakeFactories",
+    field: DEFAULT_TOTAL_VOLUME_FIELD,
+  },
+  dailyVolume: {
+    factory: "pancakeDayData",
+    field: DEFAULT_DAILY_VOLUME_FIELD,
+  },
+  getCustomBlock,
+});
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.DOGECHAIN]: {
+      fetch: graphs(CHAIN.DOGECHAIN),
+      start: async () => 1661731200,
+    },
+  },
+};
+
+export default adapter;
