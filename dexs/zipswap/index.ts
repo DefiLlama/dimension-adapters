102b8ed056d16b9a04cb36336e49a8ddfad6a9de
0xgnek <0xgnek@gmail.com>
2022-12-24 16:21:13 +0000

    fix hardcode chain zipswap

--- dexs/zipswap/index.ts
+++ dexs/zipswap/index.ts
@@ -1,5 +1,6 @@
+import { CHAIN } from "../../helpers/chains";
 import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 
 export default univ2Adapter({
-    "optimism": "https://api.thegraph.com/subgraphs/name/nonamefits/zipswap"
+    [CHAIN.OPTIMISM]: "https://api.thegraph.com/subgraphs/name/nonamefits/zipswap"
 }, {});
