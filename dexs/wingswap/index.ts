46eecc9959603dabbd463f58a754912d194029de
0xgnek <0xgnek@gmail.com>
2022-12-26 13:33:08 +0000

    add wingswap dexs

---
+++ dexs/wingswap/index.ts
@@ -0,0 +1,15 @@
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { CHAIN } from "../../helpers/chains";
+
+const endpoints = {
+  [CHAIN.FANTOM]: "https://api.thegraph.com/subgraphs/name/wingswap/wingswap-exchange",
+};
+
+const adapter = univ2Adapter(endpoints, {
+  factoriesName: "wingSwapFactories",
+  dayData: "wingSwapDayData",
+});
+
+adapter.adapter.fantom.start = async () => 1637452800;
+
+export default adapter
