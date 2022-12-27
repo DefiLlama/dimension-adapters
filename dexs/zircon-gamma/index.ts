4e521d22e59d3bcc81d292f63b1941ee2f0fdf5b
0xgnek <0xgnek@gmail.com>
2022-12-27 17:55:33 +0000

    add zircon-gamma

---
+++ dexs/zircon-gamma/index.ts
@@ -0,0 +1,12 @@
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { CHAIN } from "../../helpers/chains";
+
+const endpoints = {
+  [CHAIN.MOONRIVER]: "https://api.thegraph.com/subgraphs/name/reshyresh/zircon-alpha",
+};
+
+const adapter = univ2Adapter(endpoints, {});
+
+adapter.adapter.moonriver.start = async()=> 1663200000;
+
+export default adapter
