4de1c1b5a7deb53a0eca856edb3e46b1eae53f15
0xgnek <0xgnek@gmail.com>
2022-12-28 16:17:10 +0000

    goosefx historical api does not work switch to runcurrent

--- dexs/goosefx/index.ts
+++ dexs/goosefx/index.ts
@@ -1,36 +1,20 @@
 
 
-import fetchURL, { postURL } from "../../utils/fetchURL"
-import { Chain } from "@defillama/sdk/build/general";
+import fetchURL from "../../utils/fetchURL"
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import customBackfill from "../../helpers/customBackfill";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 const dailyVolumeEndpoint = "https://nft-launchpad.goosefx.io/getTotalVolumeTrade";
-const historicalVolumeEndpoint = "https://nft-launchpad.goosefx.io/getDailyVolumeTrade"
 
 const fetch = async (timestamp: number) => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  const currentDayTimestamp = getUniqStartOfTodayTimestamp(new Date());
-  if (dayTimestamp === currentDayTimestamp) {
-    let res = await fetchURL(dailyVolumeEndpoint);
-    return {
-        dailyVolume: res?.data?.data?.totalVolumeTradeDay,
-        totalVolume: res?.data?.data?.totalVolumeTrade,
-        timestamp: dayTimestamp
-    };
-  } else {
-    const formattedDate = new Date(dayTimestamp * 1000).toISOString().substring(0, 10);
-    const dayVolume = await postURL(historicalVolumeEndpoint, {
-        "date": formattedDate
-    });
-    return {
-        dailyVolume: dayVolume?.data?.data[0]?.totalVolumeTradeDay,
-        totalVolume: dayVolume?.data?.data[0]?.totalVolumeTrade,
-        timestamp: dayTimestamp,
-    };
-  }
+  const res = await fetchURL(dailyVolumeEndpoint);
+  return {
+    dailyVolume: res?.data?.data?.totalVolumeTradeDay,
+    totalVolume: res?.data?.data?.totalVolumeTrade,
+    timestamp: dayTimestamp
+  };
 };
 
 const adapter: SimpleAdapter = {
@@ -38,9 +22,9 @@ const adapter: SimpleAdapter = {
       [CHAIN.SOLANA]: {
         fetch: fetch,
         start: async () => 1664360407,
-        customBackfill: customBackfill(CHAIN.SOLANA as Chain, () => fetch)
+        runAtCurrTime: true
       },
     },
   };
-  
+
   export default adapter;
