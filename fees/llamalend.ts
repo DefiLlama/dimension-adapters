fcc365978e869c7cf45a9229e7b644015bf1741c
0xtawa <0xtawanofi@gmail.com>
2023-01-01 16:47:19 +0100

    Llamalend breakdown fees + methodology

--- fees/llamalend.ts
+++ fees/llamalend.ts
@@ -20,7 +20,7 @@ const graphs = (graphUrls: ChainEndpoints) => {
     return async (timestamp: number) => {
       const todaysBlock = (await getBlock(timestamp, chain, {}));
       const graphQuery = gql
-      `
+        `
       {
         loans(where:{owner_not: "0x0000000000000000000000000000000000000000"}, block:{ number: ${todaysBlock}}) {
           interest
@@ -29,15 +29,20 @@ const graphs = (graphUrls: ChainEndpoints) => {
       }
       `;
 
-      const graphRes:IGraph[]  = (await request(graphUrls[chain], graphQuery)).loans;
+      const graphRes: IGraph[] = (await request(graphUrls[chain], graphQuery)).loans;
       const ethAddress = "ethereum:0x0000000000000000000000000000000000000000";
       const ethPrice = (await getPrices([ethAddress], timestamp))[ethAddress].price;
-      const dailyFeePerSec = graphRes.reduce((a: number, b: IGraph ) => a + (Number(b.interest) * Number(b.borrowed)), 0)
+      const dailyFeePerSec = graphRes.reduce((a: number, b: IGraph) => a + (Number(b.interest) * Number(b.borrowed)), 0)
       const dailyFee = dailyFeePerSec / 1e36 * ONE_DAY_IN_SECONDS * ethPrice;
 
       return {
         timestamp: timestamp,
         dailyFees: dailyFee.toString(),
+        dailyUserFees: dailyFee.toString(),
+        dailySupplySideRevenue: dailyFee.toString(),
+        dailyRevenue: "0",
+        dailyHoldersRevenue: "0",
+        dailyProtocolRevenue: "0"
       };
     };
   };
@@ -47,8 +52,18 @@ const graphs = (graphUrls: ChainEndpoints) => {
 const adapter: Adapter = {
   adapter: {
     [CHAIN.ETHEREUM]: {
-        fetch: graphs(endpoints)(CHAIN.ETHEREUM),
-        start: async ()  => 1667260800,
+      fetch: graphs(endpoints)(CHAIN.ETHEREUM),
+      start: async () => 1667260800,
+      meta: {
+        methodology: {
+          Fees: "Interest paid by borrowers",
+          UserFees: "Interest paid to borrow ETH",
+          SupplySideRevenue: "Interest paid to NFTs lenders",
+          Revenue: "Governance have no revenue",
+          HoldersRevenue: "Token holders have no revenue",
+          ProtocolRevenue: "Protocol have no revenue"
+        }
+      }
     },
   }
 }
