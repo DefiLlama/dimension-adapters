f3f91e476d1a230536f66e36983aefd2ed9fc7e8
0xtawa <0xtawanofi@gmail.com>
2023-01-01 21:38:17 +0100

    Breakdown babydoge fees + methodology

--- fees/babydogeswap.ts
+++ fees/babydogeswap.ts
@@ -1,19 +1,43 @@
 import { Adapter } from "../adapters/types";
 import volumeAdapter from "../dexs/babydogeswap";
-import { getDexChainFees } from "../helpers/getUniSubgraphFees";
+import { getGraphDimensions } from "../helpers/getUniSubgraph";
 
-const TOTAL_FEES = 0.003;
-const PROTOCOL_FEES = 0.001;
+import { CHAIN } from "../helpers/chains";
+import { univ2DimensionAdapter } from "../helpers/getUniSubgraph";
 
-const feeAdapter = getDexChainFees({
-  totalFees: TOTAL_FEES,
-  protocolFees: PROTOCOL_FEES,
-  volumeAdapter
+// 0.2% to LP providers
+// 0.05% to treasury
+// 0.05% to buy back and burn BabyDoge!
+
+const adapters = univ2DimensionAdapter({
+  graphUrls: {
+    [CHAIN.BSC]: "https://graph-bsc-mainnet.babydoge.com/subgraphs/name/babydoge/exchange"
+  },
+  dailyVolume: {
+    factory: "factoryDayData"
+  },
+  totalVolume: {
+    factory: "babyDogeFactories"
+  },
+  feesPercent: {
+    type: "volume",
+    Fees: 0.3,
+    UserFees: 0.3,
+    Revenue: 0.1,
+    ProtocolRevenue: 0.05,
+    HoldersRevenue: 0.05,
+    SupplySideRevenue: 0.2,
+  }
+}, {
+  methodology: {
+    Fees: "Fees collected from user trading fees",
+    UserFees: "Users pays 0.3% of each swap. Different user fee discounts depening on Baby Doge wallet balance (up to 70% off). Calculation made with base 0.3%",
+    Revenue: "A 0.1% of user fees are distributed 50/50 between treasury and BabyDoge buy back and burn",
+    ProtocolRevenue: "A 0.05% of user fees goes to treasure",
+    HoldersRevenue: "A 0.05% of user fees is used to buy back and burn BabyDoge tokens",
+    SupplySideRevenue: "A 0.2% user fees is distributed among LPs",
+  }
 });
 
-const adapter: Adapter = {
-  adapter: feeAdapter
-};
-
-
-export default adapter;
+adapters.adapter.bsc.start = async () => 1661780137;
+export default adapters;
