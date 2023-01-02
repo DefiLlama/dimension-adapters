793ba70f53ff4200d1c7aad89b342790e294fe3e
jjryu37 <somebody@else.com>
2023-01-02 23:22:36 +0100

    hegic: add fees adapter

---
+++ fees/hegic.ts
@@ -0,0 +1,39 @@
+import { Adapter, FetchResultFees } from "../adapters/types";
+import { ARBITRUM } from "../helpers/chains";
+
+import {
+  fetchArbitrumAnalyticsData,
+  HEGIC_HERGE_START,
+} from "../options/hegic";
+
+const adapter: Adapter = {
+  adapter: {
+    [ARBITRUM]: {
+      fetch: getHegicFees,
+      start: async () => HEGIC_HERGE_START,
+      meta: {
+        methodology: `
+The only thing anyone pays on Hegic is premiums. This goes into dailyFees.
+
+The paid premiums can be considered Revenue, but at the moment we don't include it in the dashboard, because:
+
+- payout is epoch-based (30 days), not daily
+- stakers get all premiums minus profits of traders (!!!), so in fact stakers may get a negative payout (slashed stake)
+
+Hegic Development fund also stakes ~170M Hegic tokens (16% of supply), so that could be considered ProtocolRevenue
+  `,
+      },
+    },
+  },
+};
+
+async function getHegicFees(timestamp: number): Promise<FetchResultFees> {
+  const optionsDashboardData = await fetchArbitrumAnalyticsData(timestamp);
+
+  return {
+    timestamp,
+    dailyFees: optionsDashboardData.dailyPremiumVolume,
+  };
+}
+
+export default adapter;
