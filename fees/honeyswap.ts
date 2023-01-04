4b0de6dc6ae01ba149a979d2328d6f9202a5651c
0xtawa <0xtawanofi@gmail.com>
2023-01-04 17:27:08 +0100

    Add honeyswap fees breakdown

--- fees/honeyswap.ts
+++ fees/honeyswap.ts
@@ -1,18 +1,37 @@
-import { getDexChainFees } from "../helpers/getUniSubgraphFees";
-import volumeAdapter from "../dexs/honeyswap";
-import { Adapter, BaseAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { univ2DimensionAdapter } from "../helpers/getUniSubgraph";
 
-const TOTAL_FEES = 0.003;
-const PROTOCOL_FEES = 0.0005;
-
-const feeAdapter: BaseAdapter = getDexChainFees({
-  totalFees: TOTAL_FEES,
-  protocolFees: PROTOCOL_FEES,
-  volumeAdapter
+const adapters = univ2DimensionAdapter({
+  graphUrls: {
+    [CHAIN.POLYGON]: " https://api.thegraph.com/subgraphs/name/1hive/honeyswap-polygon",
+    [CHAIN.XDAI]: "https://api.thegraph.com/subgraphs/name/1hive/honeyswap-xdai"
+  },
+  dailyVolume: {
+    factory: "honeyswapDayData"
+  },
+  totalVolume: {
+    factory: "honeyswapFactories"
+  },
+  feesPercent: {
+    type: 'volume',
+    Fees: 0.3,
+    UserFees: 0.3,
+    SupplySideRevenue: 0.25,
+    HoldersRevenue: 0,
+    ProtocolRevenue: 0.05,
+    Revenue: 0.05
+  }
+}, {
+  methodology: {
+    UserFees: "A 0.3% fee is charged for token swaps",
+    Fees: "Trading fees are 0.3% of each swap",
+    SupplySideRevenue: "A 0.25% of each swap is distributed to liquidity providers",
+    Revenue: "A 0.05% trading fee goes to treasury",
+    HoldersRevenue: "There's no revenue from trading fees for token holders",
+    ProtocolRevenue: "A 0.05% goes to the protocol treasury"
+  }
 });
+adapters.adapter.polygon.start = async () => 1622173831;
+adapters.adapter.xdai.start = async () => 1599191431;
 
-const adapter: Adapter = {
-  adapter: feeAdapter
-};
-
-export default adapter;
+export default adapters;
