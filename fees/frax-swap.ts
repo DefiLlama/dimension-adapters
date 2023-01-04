6f99f7ad54466da11871515a5dbcc722418d75e9
0xtawa <0xtawanofi@gmail.com>
2023-01-04 17:13:35 +0100

    Add frax swap user fees methodology

--- fees/frax-swap.ts
+++ fees/frax-swap.ts
@@ -45,7 +45,9 @@ const graphs = () => {
         .find(dayItem => (new Date(dayItem.intervalTimestamp).getTime() / 1000) === dayTimestamp)?.feeUsdAmount
       return {
         timestamp,
+        dailyUserFees: dailyFees?.toString(),
         totalFees: totalFees.toString(),
+        totalUserFees: totalFees.toString(),
         dailyFees: dailyFees?.toString(),
         totalRevenue: "0",
         dailyRevenue: "0",
@@ -59,51 +61,90 @@ const getStartTimestamp = async (chain: Chain) => {
   const historicalVolume = historical.filter(e => e.chain.toLowerCase() === chains[chain].toLowerCase());
   return (new Date(historicalVolume[historicalVolume.length - 1].intervalTimestamp).getTime()) / 1000
 }
+
+const methodology = {
+  UserFees: "Users pay 0.3% swap fees",
+  Fees: "A 0.3% fee is collected from each swap"
+}
+
 const adapter: Adapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
-        fetch: graphs()(CHAIN.ARBITRUM),
-        start: async () => getStartTimestamp(CHAIN.ARBITRUM),
+      fetch: graphs()(CHAIN.ARBITRUM),
+      start: async () => getStartTimestamp(CHAIN.ARBITRUM),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.AURORA]: {
       fetch: graphs()(CHAIN.AURORA),
       start: async () => getStartTimestamp(CHAIN.AURORA),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.AVAX]: {
       fetch: graphs()(CHAIN.AVAX),
       start: async () => getStartTimestamp(CHAIN.AVAX),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.BOBA]: {
       fetch: graphs()(CHAIN.BOBA),
       start: async () => getStartTimestamp(CHAIN.BOBA),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.BSC]: {
       fetch: graphs()(CHAIN.BSC),
       start: async () => getStartTimestamp(CHAIN.BSC),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.ETHEREUM]: {
       fetch: graphs()(CHAIN.ETHEREUM),
       start: async () => getStartTimestamp(CHAIN.ETHEREUM),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.FANTOM]: {
       fetch: graphs()(CHAIN.FANTOM),
       start: async () => getStartTimestamp(CHAIN.FANTOM),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.HARMONY]: {
       fetch: graphs()(CHAIN.HARMONY),
       start: async () => getStartTimestamp(CHAIN.HARMONY),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.MOONBEAN]: {
       fetch: graphs()(CHAIN.MOONBEAN),
       start: async () => getStartTimestamp(CHAIN.MOONBEAN),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.MOONRIVER]: {
       fetch: graphs()(CHAIN.MOONRIVER),
       start: async () => getStartTimestamp(CHAIN.MOONRIVER),
+      meta: {
+        methodology
+      }
     },
     [CHAIN.POLYGON]: {
       fetch: graphs()(CHAIN.POLYGON),
       start: async () => getStartTimestamp(CHAIN.POLYGON),
+      meta: {
+        methodology
+      }
     },
   }
 }
