fcd5b8e6fd6672308babeab0c1a626c9c2027a66
0xtawa <0xtawanofi@gmail.com>
2023-01-05 15:21:38 +0100

    Breakdown mimo fees + methodology

--- fees/mimo.ts
+++ fees/mimo.ts
@@ -1,17 +1,42 @@
 import { Adapter } from "../adapters/types";
-import volumeAdapter from "../dexs/mimo";
-import { getDexChainFees } from "../helpers/getUniSubgraphFees";
+import { CHAIN } from "../helpers/chains";
+import { univ2DimensionAdapter } from "../helpers/getUniSubgraph";
 
-const TOTAL_FEES = 0.003;
-
-const feeAdapter = getDexChainFees({
-  totalFees: TOTAL_FEES,
-  volumeAdapter
-});
-
-const adapter: Adapter = {
-  adapter: feeAdapter
+const endpoints = {
+  [CHAIN.IOTEX]: "https://graph-cache.mimo.exchange/subgraphs/name/mimo/mainnet"
 };
+
+
+const adapter = univ2DimensionAdapter({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: "uniswapFactories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "uniswapDayData",
+    field: "dailyVolumeUSD",
+    dateField: "date"
+  },
+  feesPercent: {
+    type: "volume",
+    UserFees: 0.3,
+    Fees: 0.3,
+    ProtocolRevenue: 0,
+    Revenue: 0,
+    SupplySideRevenue: 0.3,
+    HoldersRevenue: 0,
+  }
+}, {
+  methodology: {
+    UserFees: "User pays 0.3% fees on each swap",
+    Fees: "All fees are collected from trading fees",
+    ProtocolRevenue: "Protocol have no revenue.",
+    Revenue: "Protocol have no revenue.",
+    SupplySideRevenue: "All user fees are distributed among LPs.",
+    HoldersRevenue: "Holders have no revenue."
+  }
+});
 
 
 export default adapter;
