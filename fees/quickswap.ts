a589af4c1029680d821ab6a149a9b082f5800e7b
0xtawa <0xtawanofi@gmail.com>
2023-01-06 18:01:43 +0100

    Breakdown quickswap fees + methodology

--- fees/quickswap.ts
+++ fees/quickswap.ts
@@ -1,50 +1,88 @@
-import { getDexChainFeesRaw, getDexChainFees, getDexChainBreakdownFees } from "../helpers/getUniSubgraphFees";
-import { Adapter } from "../adapters/types";
-import volumeAdapter from "../dexs/quickswap";
-import { POLYGON } from "../helpers/chains";
 import { Chain } from "@defillama/sdk/build/general";
+import { BreakdownAdapter, BaseAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import customBackfill from "../helpers/customBackfill";
 
-// const endpoints = {
-//   [POLYGON]: "https://api.fura.org/subgraphs/name/quickswap",
-// };
+import {
+  getGraphDimensions
+} from "../helpers/getUniSubgraph"
 
-// const TOTAL_FEES = 0.003;
-// const PROTOCOL_FEES = 0.0005;
-
-// const graphs = getDexChainFeesRaw({
-//   graphUrls: {
-//     [POLYGON]: endpoints[POLYGON]
-//   },
-//   totalFees: TOTAL_FEES,
-//   protocolFees: PROTOCOL_FEES
-// });
-
-// const fees = Object.keys(endpoints).reduce(
-//   (acc, chain) => ({
-//     ...acc,
-//     [chain]: {
-//       fetch: graphs(chain as Chain),
-//       start: async ()  => 1602115200,
-//     },
-//   }),
-//   {}
-// );
-
-// const adapter: FeeAdapter = {
-//     fees,
-// };
-
-const TOTAL_FEES = 0.003;
-const PROTOCOL_FEES = 0.0005;
-
-const feeAdapter = getDexChainBreakdownFees({
-  totalFees: TOTAL_FEES,
-  protocolFees: PROTOCOL_FEES,
-  volumeAdapter
+const v2Endpoints = {
+  [CHAIN.POLYGON]: "https://api.fura.org/subgraphs/name/quickswap",
+}
+const v2Graph = getGraphDimensions({
+  graphUrls: v2Endpoints,
+  feesPercent: {
+    type: "volume",
+    UserFees: 0.3,
+    ProtocolRevenue: 0,
+    SupplySideRevenue: 0.3,
+    HoldersRevenue: 0,
+    Revenue: 0,
+    Fees: 0.3
+  }
 });
 
-const adapter: Adapter = {
-  breakdown: feeAdapter
-};
+const v3Endpoints = {
+  [CHAIN.POLYGON]: "https://api.thegraph.com/subgraphs/name/sameepsi/quickswap-v3"
+}
+const v3Graphs = getGraphDimensions({
+  graphUrls: v3Endpoints,
+  totalVolume: {
+    factory: "factories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "algebraDayData",
+    field: "volumeUSD",
+    dateField: "date"
+  },
+  dailyFees: {
+    factory: "algebraDayData",
+    field: "feesUSD",
+  },
+  feesPercent: {
+    type: "fees",
+    ProtocolRevenue: 0,
+    HoldersRevenue: 0,
+    Fees: 0,
+    UserFees: 100, // User fees are 100% of collected fees
+    SupplySideRevenue: 100, // 100% of fees are going to LPs
+    Revenue: 0 // Revenue is 100% of collected fees
+  }
+});
+
+const methodology = {
+  UserFees: "User pays 0.3% fees on each swap.",
+  Fees: "A 0.3% of each swap is collected as trading fees",
+  Revenue: "Protocol have no revenue",
+  ProtocolRevenue: "Protocol have no revenue.",
+  SupplySideRevenue: "All user fees are distributed among LPs.",
+  HoldersRevenue: "Holders have no revenue."
+}
+
+const adapter: BreakdownAdapter = {
+  breakdown: {
+    v2: {
+      [CHAIN.POLYGON]: {
+        fetch: v2Graph(CHAIN.POLYGON),
+        start: async () => 1602118043,
+        meta: {
+          methodology
+        },
+      },
+    },
+    v3: Object.keys(v3Endpoints).reduce((acc, chain) => {
+      acc[chain] = {
+        fetch: v3Graphs(chain as Chain),
+        start: async () => 1662425243,
+        meta: {
+          methodology
+        }
+      }
+      return acc
+    }, {} as BaseAdapter)
+  }
+}
 
 export default adapter;
