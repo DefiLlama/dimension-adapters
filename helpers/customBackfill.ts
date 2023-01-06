d647ec2cdc6232d720c299d8f4e8eb1906d41d56
0xtawa <0xtawanofi@gmail.com>
2023-01-06 17:59:09 +0100

    Improve custom backfill

--- helpers/customBackfill.ts
+++ helpers/customBackfill.ts
@@ -1,19 +1,15 @@
-import { ChainBlocks, Fetch } from "../adapters/types"
+import { ChainBlocks, Fetch, FetchResultGeneric } from "../adapters/types"
 import { getBlock } from "./getBlock"
 import { util } from '@defillama/sdk';
 import { Chain } from "@defillama/sdk/build/general";
+import { formatTimestampAsDate } from "../utils/date";
 
-const { blocks: {chainsForBlocks } } = util
+const { blocks: { chainsForBlocks } } = util
 const ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-export type IGraphs = (chain: Chain) => (timestamp: number, chainBlocks: ChainBlocks) => Promise<{
-    timestamp: number;
-    block?: number;
-    totalVolume: string;
-    dailyVolume?: string;
-}>
+export type IGraphs = (chain: Chain) => (timestamp: number, chainBlocks: ChainBlocks) => Promise<FetchResultGeneric>
 
-export default (chain: Chain, graphs: IGraphs): Fetch => async (timestamp: number, chainBlocks: ChainBlocks) => {
+export default (chain: Chain, graphs: IGraphs): Fetch => async (timestamp: number, chainBlocks: ChainBlocks): Promise<FetchResultGeneric> => {
     const fetchGetVolume = graphs(chain)
     const resultDayN = await fetchGetVolume(timestamp, chainBlocks)
     const timestampPreviousDay = timestamp - ONE_DAY_IN_SECONDS
@@ -21,10 +17,14 @@ export default (chain: Chain, graphs: IGraphs): Fetch => async (timestamp: numbe
     if (chainsForBlocks.includes(chain) || chain === "ethereum")
         chainBlocksPreviousDay = { [chain]: await getBlock(timestampPreviousDay, chain, {}).catch(() => { }) }
     const resultPreviousDayN = await fetchGetVolume(timestampPreviousDay, chainBlocksPreviousDay)
-    return {
-        block: resultDayN.block,
-        timestamp: resultDayN.timestamp,
-        totalVolume: resultDayN.totalVolume,
-        dailyVolume: `${Number(resultDayN.totalVolume) - Number(resultPreviousDayN.totalVolume)}`,
-    }
+    const response: FetchResultGeneric = { timestamp: resultDayN.timestamp, block: resultDayN.block }
+    Object.keys(resultPreviousDayN).filter((key) => key.includes('total')).forEach(key => {
+        const dimension = `daily${key.slice(5)}`
+        if (resultDayN[dimension] === undefined) {
+            if (resultPreviousDayN[key] !== undefined && resultDayN[key] !== undefined) {
+                response[dimension] = `${Number(resultDayN[key]) - Number(resultPreviousDayN[key])}`
+            }
+        }
+    })
+    return response
 }
