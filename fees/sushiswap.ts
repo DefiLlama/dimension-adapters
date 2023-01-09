5e8a190e8e0584b0b07ce8dd9b116bc4fc5f136a
dtmkeng <0xgnek@gmail.com>
2023-01-09 22:47:48 +0700

    fix fees breakdown sushiswap (#172)

--- fees/sushiswap.ts
+++ fees/sushiswap.ts
@@ -1,18 +1,27 @@
 import { getDexChainFees } from "../helpers/getUniSubgraphFees";
 import volumeAdapter from "../dexs/sushiswap";
-import { Adapter, BaseAdapter } from "../adapters/types";
+import { Adapter } from "../adapters/types";
 
 const TOTAL_FEES = 0.003;
 const PROTOCOL_FEES = 0.0005;
 
-const feeAdapter: BaseAdapter = getDexChainFees({
+const classic = getDexChainFees({
   totalFees: TOTAL_FEES,
   protocolFees: PROTOCOL_FEES,
-  volumeAdapter
+  volumeAdapter: {adapter: volumeAdapter.breakdown.classic}
 });
 
+const trident = getDexChainFees({
+  totalFees: TOTAL_FEES,
+  protocolFees: PROTOCOL_FEES,
+  volumeAdapter: {adapter: volumeAdapter.breakdown.trident}
+});
 const adapter: Adapter = {
-    adapter: feeAdapter
+  breakdown: {
+    classic: classic,
+    trident: trident
+  }
 };
+
 
 export default adapter;
