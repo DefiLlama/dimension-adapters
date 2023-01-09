3faffcf55aedd23bb2933c0d5ca4c507eb7ac45d
0xtawa <0xtawanofi@gmail.com>
2023-01-09 19:22:12 +0100

    Revert "fix fees breakdown sushiswap (#172)"
    This reverts commit 5e8a190e8e0584b0b07ce8dd9b116bc4fc5f136a.
    
    Revert "set timestamp sushiswap"
    
    This reverts commit 1b463b70d5de6717e0275a31ef9d3b9e2e3e52da.
    
    Revert "feat: add trident & more classic chains to dexs"
    
    This reverts commit 3fef3782c8e36b4fa60934a556ca8d6529b094f7.

--- fees/sushiswap.ts
+++ fees/sushiswap.ts
@@ -1,27 +1,18 @@
 import { getDexChainFees } from "../helpers/getUniSubgraphFees";
 import volumeAdapter from "../dexs/sushiswap";
-import { Adapter } from "../adapters/types";
+import { Adapter, BaseAdapter } from "../adapters/types";
 
 const TOTAL_FEES = 0.003;
 const PROTOCOL_FEES = 0.0005;
 
-const classic = getDexChainFees({
+const feeAdapter: BaseAdapter = getDexChainFees({
   totalFees: TOTAL_FEES,
   protocolFees: PROTOCOL_FEES,
-  volumeAdapter: {adapter: volumeAdapter.breakdown.classic}
+  volumeAdapter
 });
 
-const trident = getDexChainFees({
-  totalFees: TOTAL_FEES,
-  protocolFees: PROTOCOL_FEES,
-  volumeAdapter: {adapter: volumeAdapter.breakdown.trident}
-});
 const adapter: Adapter = {
-  breakdown: {
-    classic: classic,
-    trident: trident
-  }
+    adapter: feeAdapter
 };
-
 
 export default adapter;
