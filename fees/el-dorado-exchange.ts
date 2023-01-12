d7417be23560a6e8bf0bf8aaa9184e2c038d9423
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-01-12 12:03:33 +0100

    add El-Dorado-Exchange fees adapter (#182)
    * add El-Dorado-Exchange fees adapter
    
    * update
    
    * Fix user fees dimension
    
    * Fix filename typo
    
    Co-authored-by: Roy <hfers@msn.com>

---
+++ fees/el-dorado-exchange.ts
@@ -0,0 +1,65 @@
+import { Chain } from "@defillama/sdk/build/general";
+import { gql, request } from "graphql-request";
+import type { ChainEndpoints } from "../adapters/types";
+import { Adapter } from "../adapters/types";
+import { BSC } from "../helpers/chains";
+import { getTimestampAtStartOfDayUTC } from "../utils/date";
+
+const endpoints = {
+    [BSC]: "https://api.thegraph.com/subgraphs/name/aaronlux/ede-stats",
+};
+
+const graphs = (graphUrls: ChainEndpoints) => {
+    return (chain: Chain) => {
+        return async (timestamp: number) => {
+            const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+
+            const graphQuery = gql`{
+                feeStat(id: "${todaysTimestamp}",period: "daily") {
+                    mint
+                    burn
+                    marginAndLiquidation
+                    swap
+                }
+            }`;
+
+            const graphRes = await request(graphUrls[chain], graphQuery);
+
+            const dailyFee = (
+                parseInt(graphRes.feeStat.mint) +
+                parseInt(graphRes.feeStat.burn) +
+                parseInt(graphRes.feeStat.marginAndLiquidation) +
+                parseInt(graphRes.feeStat.swap)
+            ) / 1e30
+            const dailyUserFees = (
+                parseInt(graphRes.feeStat.marginAndLiquidation) +
+                parseInt(graphRes.feeStat.swap)
+            ) / 1e30;
+
+            return {
+                timestamp,
+                dailyFees: dailyFee.toString(),
+                dailyUserFees: dailyUserFees.toString(),
+                dailyRevenue: (dailyFee * 0.3).toString()
+            };
+        };
+    };
+};
+
+const adapter: Adapter = {
+    adapter: {
+        [BSC]: {
+            fetch: graphs(endpoints)(BSC),
+            start: async () => 1670659200,
+            meta: {
+                methodology: {
+                    Fees: "All mint, burn, margin and liquidation and swap fees are collected",
+                    UserFees: "Users pay swap fees and margin and liquidation fees",
+                    Revenue: "Revenue is calculated as 30% of the total fee.",
+                }
+            }
+        },
+    },
+};
+
+export default adapter;
