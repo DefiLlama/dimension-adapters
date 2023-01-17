ef251d3c58218b373f40e539a2cecce766d63107
0xgnek <0xgnek@gmail.com>
2023-01-17 16:21:36 +0000

    add gear box fees

--- fees/gearbox/index.ts
+++ fees/gearbox/index.ts
@@ -5,6 +5,7 @@ import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "..
 import { getBlock } from "../../helpers/getBlock";
 import BigNumber from "bignumber.js";
 import { getPrices } from "../../utils/prices";
+import postgres from 'postgres'
 
 const creditAccountFactoryAddress = "0x444cd42baeddeb707eed823f7177b9abcc779c04";
 const registyContract: any = {
@@ -39,21 +40,6 @@ const registyContract: any = {
   }
 };
 
-const PoolServiceABIs = {
-  dieselToken: {
-    "inputs": [],
-    "name": "dieselToken",
-    "outputs": [
-        {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  }
-}
 type IMapDieselToken = {
   [l: string]: string;
 }
@@ -63,6 +49,8 @@ const mapDieselToken: IMapDieselToken = {
   "0xF21fc650C1B34eb0FDE786D52d23dA99Db3D6278": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
   "0xe753260F1955e8678DCeA8887759e07aa57E8c54": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
   "0x2158034dB06f06dcB9A786D2F1F8c38781bA779d": "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+  '0xba3335588d9403515223f109edc4eb7269a9ab5d': '0xba3335588d9403515223f109edc4eb7269a9ab5d',
+  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
 };
 
 interface IAmount {
@@ -71,12 +59,10 @@ interface IAmount {
   transactionHash: string;
 }
 interface ITx {
+  token?: string;
   data: string;
   transactionHash: string;
 }
-
-const transfer_topic = "Transfer(index_topic_1 address from, index_topic_2 address to, uint256 value)";
-const transfer_topic0 = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
 const removeLpTopic = "RemoveLiquidity(index_topic_1 address sender, index_topic_2 address to, uint256 amount)";
 const removeLpTopic0 = "0xd8ae9b9ba89e637bcb66a69ac91e8f688018e81d6f92c57e02226425c8efbdf6";
@@ -94,6 +80,85 @@ const returnCreditAccountTopic = "ReturnCreditAccount(index_topic_1 address acco
 const returnCreditAccountTopic0 = "0xced6ab9afc868b3a088366f6631ae20752993b5cce5d5f0534ea5a59fcc57d56";
 
 const fetch = async (timestamp: number): Promise<FetchResultFees> => {
+  const sql = postgres(process.env.INDEXA_DB!);
+  const now = new Date(timestamp * 1e3);
+  const dayAgo = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+  const logEventTranferErc20ToTreasury = await sql`
+    SELECT
+      substr(encode(topic_1, 'hex'), 25) AS origin,
+      substr(encode(topic_2, 'hex'), 25) AS destination,
+      encode(data, 'hex') AS value,
+      encode(contract_address, 'hex') AS contract_address,
+      block_time AS evt_block_time,
+      encode(transaction_hash, 'hex') AS HASH
+    FROM
+      ethereum.event_logs
+    WHERE
+      block_number > 13733671 -- gearbox multisig creation block
+      AND topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- erc20 transfer event
+      AND topic_2 = '\\x0000000000000000000000007b065Fcb0760dF0CEA8CFd144e08554F3CeA73D1' -- erc20 transfer to gearbox multisig
+      AND block_time BETWEEN ${dayAgo.toISOString()} AND ${now.toISOString()};
+  `
+
+  const logEventTranferErc20FromTreasury = await sql`
+  SELECT
+    substr(encode(topic_1, 'hex'), 25) AS origin,
+    substr(encode(topic_2, 'hex'), 25) AS destination,
+    encode(data, 'hex') AS value,
+    encode(contract_address, 'hex') AS contract_address,
+    block_time AS evt_block_time,
+    encode(transaction_hash, 'hex') AS HASH
+  FROM
+    ethereum.event_logs
+  WHERE
+    block_number > 13733671 -- gearbox multisig creation block
+    AND topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- erc20 transfer event
+    AND topic_1 = '\\x0000000000000000000000007b065Fcb0760dF0CEA8CFd144e08554F3CeA73D1' -- erc20 transfer from gearbox multisig
+    AND block_time BETWEEN ${dayAgo.toISOString()} AND ${now.toISOString()};
+  `
+
+  const logEventTxToTreasury = await sql`
+    SELECT
+      encode(et.from_address, 'hex') AS origin,
+      encode(et.to_address, 'hex') AS destination,
+      value,
+      'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' AS contract_address,
+      block_time AS evt_block_time,
+      encode(et.hash, 'hex') AS HASH
+    FROM
+      ethereum.transactions et
+    WHERE
+      et.to_address = '\\x7b065Fcb0760dF0CEA8CFd144e08554F3CeA73D1'
+      AND block_time BETWEEN ${dayAgo.toISOString()} AND ${now.toISOString()};
+  `
+
+
+  const logEventTxFromTreasury = await sql`
+    SELECT
+      encode(et.from_address, 'hex') AS origin,
+      encode(et.to_address, 'hex') AS destination,
+      value,
+      'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' AS contract_address,
+      block_time AS evt_block_time,
+      encode(et.hash, 'hex') AS HASH
+    FROM
+      ethereum.transactions et
+    WHERE
+      et.from_address = '\\x7b065Fcb0760dF0CEA8CFd144e08554F3CeA73D1'
+      AND block_time BETWEEN ${dayAgo.toISOString()} AND ${now.toISOString()};
+  `
+  const logEventTranfer: ITx[] = logEventTranferErc20ToTreasury
+    .concat(logEventTranferErc20FromTreasury)
+    .concat(logEventTxToTreasury)
+    .concat(logEventTxFromTreasury)
+    .map((p: any) => {
+      return {
+        data: `0x${p.value}`,
+        transactionHash: `0x${p.hash}`.toLowerCase(),
+        token: `0x${p.contract_address}`.toLowerCase(),
+      } as ITx;
+    }) as ITx[];
+
   const creditManagersAddress: string[] = (await sdk.api.abi.call({
     target: registyContract.address,
     abi: registyContract.abis.getCreditManagers,
@@ -106,51 +171,28 @@ const fetch = async (timestamp: number): Promise<FetchResultFees> => {
     chain: 'ethereum'
   })).output;
 
-  const dieselTokenAddress: string[] = (await sdk.api.abi.multiCall({
-    abi: PoolServiceABIs.dieselToken,
-    calls: poolServiceAddress.map((address: string) => ({
-      target: address,
-    })),
-    chain: 'ethereum'
-  })).output.map((e: any) => e.output);
-
   const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
   const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
 
   const todaysBlock = (await getBlock(todaysTimestamp, 'ethereum', {}));
   const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, 'ethereum', {}));
 
-  const logEventTranfer: ITx[][] = (await Promise.all(
-    dieselTokenAddress.map((address: string) => sdk.api.util.getLogs({
+
+  const logEventRemoveLp: ITx[] = (await Promise.all(
+    poolServiceAddress.map((address: string) => sdk.api.util.getLogs({
       target: address,
-      topic: transfer_topic,
+      topic: removeLpTopic,
       toBlock: yesterdaysBlock,
       fromBlock: todaysBlock,
       keys: [],
       chain: 'ethereum',
-      topics: [transfer_topic0]
+      topics: [removeLpTopic0]
   })))).map((e: any) => e.output.map((p: any) => {
     return {
       data: p.data,
       transactionHash: p.transactionHash
     } as ITx
-  }));
-
-  // const logEventRemoveLp: ITx[] = (await Promise.all(
-  //   poolServiceAddress.map((address: string) => sdk.api.util.getLogs({
-  //     target: address,
-  //     topic: removeLpTopic,
-  //     toBlock: yesterdaysBlock,
-  //     fromBlock: todaysBlock,
-  //     keys: [],
-  //     chain: 'ethereum',
-  //     topics: [removeLpTopic0]
-  // })))).map((e: any) => e.output.map((p: any) => {
-  //   return {
-  //     data: p.data,
-  //     transactionHash: p.transactionHash
-  //   } as ITx
-  // })).flat();
+  })).flat();
 
   const logEventRepayCreditAccount: ITx[] = (await Promise.all(
     creditManagersAddress.map((address: string) => sdk.api.util.getLogs({
@@ -219,25 +261,24 @@ const fetch = async (timestamp: number): Promise<FetchResultFees> => {
   const prices = await getPrices(coins, timestamp);
 
   const logHashs: string[] = logEventRepayCreditAccount
-    // .concat(logEventRemoveLp)
+    .concat(logEventRemoveLp)
     .concat(logEventLiquidateCreditAccount)
     .concat(logEventCloseCreditAccount)
     .concat(logEventReturnCreditAccount).map((e: ITx) => e.transactionHash);
-  const hashEvent = [...new Set([...logHashs])];
-
-  const txAmountUSD: IAmount[] = logEventTranfer.map((transfer_events: ITx[], index: number) => {
-      const token = Object.values(mapDieselToken)[index];
+  const hashEvent = [...new Set([...logHashs])].map((e: string) => e.toLowerCase());
+  const txAmountUSD: IAmount[] = logEventTranfer.filter((e: ITx) => hashEvent.includes(e.transactionHash)).map((transfer_events: ITx, _: number) => {
+      const indexTokenMap = Object.keys(mapDieselToken).map((e: any) => e.toLowerCase()).findIndex((e: string) => e === transfer_events?.token);
+      const token = Object.values(mapDieselToken)[indexTokenMap];
       const { price, decimals } = prices[`ethereum:${token.toLocaleLowerCase()}`];
-      return transfer_events.filter(e => hashEvent.includes(e.transactionHash)).map((p: ITx) => {
-        const amount = new BigNumber(p.data).toNumber();
+        const amount = new BigNumber(transfer_events.data).toNumber();
         return {
           amount: amount / 10 ** decimals,
           amountUsd: (amount / (10 ** decimals)) * price,
-          transactionHash: p.transactionHash
+          transactionHash: transfer_events.transactionHash
         } as IAmount
-      });
-  }).flat();
+  });
   const dailyFees = [...new Set([...txAmountUSD.map(e => e.amountUsd)])].reduce((a: number, b: number) => a + b, 0);
+  await sql.end({ timeout: 5 })
   return {
     timestamp,
     dailyFees: dailyFees.toString(),
