6673e6f7548d185ecd3e774c2d168eb355101f8b
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-01-26 01:23:19 +0100

    Fix equalizer exchange volume calc (#210)
    * Fix equalizer exchange volume calc
    
    * Lint

--- dexs/equalizer-exchange/index.ts
+++ dexs/equalizer-exchange/index.ts
@@ -2,7 +2,6 @@ import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import * as sdk from "@defillama/sdk";
 import { getBlock } from "../../helpers/getBlock";
-import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../../utils/date";
 import BigNumber from "bignumber.js";
 import { getPrices } from "../../utils/prices";
 
@@ -29,11 +28,11 @@ const ABIs: TABI = {
     "type": "function",
     "stateMutability": "view",
     "outputs": [
-        {
-            "type": "uint256",
-            "name": "",
-            "internalType": "uint256"
-        }
+      {
+        "type": "uint256",
+        "name": "",
+        "internalType": "uint256"
+      }
     ],
     "name": "allPairsLength",
     "inputs": []
@@ -42,34 +41,34 @@ const ABIs: TABI = {
     "type": "function",
     "stateMutability": "view",
     "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
     "inputs": [
       {
-          "type": "uint256",
-          "name": "",
-          "internalType": "uint256"
+        "type": "uint256",
+        "name": "",
+        "internalType": "uint256"
       }
     ],
     "name": "allPairs",
   }
 };
 
-const PAIR_TOKEN_ABI  = (token: string): object => {
+const PAIR_TOKEN_ABI = (token: string): object => {
   return {
     "constant": true,
     "inputs": [],
     "name": token,
     "outputs": [
-        {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
     "payable": false,
     "stateMutability": "view",
@@ -79,8 +78,8 @@ const PAIR_TOKEN_ABI  = (token: string): object => {
 
 
 const fetch = async (timestamp: number) => {
-  const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
-  const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
+  const fromTimestamp = timestamp - 60 * 60 * 24
+  const toTimestamp = timestamp
 
   const poolLength = (await sdk.api.abi.call({
     target: FACTORY_ADDRESS,
@@ -114,45 +113,47 @@ const fetch = async (timestamp: number) => {
 
   const tokens0 = underlyingToken0.output.map((res) => res.output);
   const tokens1 = underlyingToken1.output.map((res) => res.output);
-  const todaysBlock = (await getBlock(todaysTimestamp, 'fantom', {}));
-  const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, 'fantom', {}));
+  const fromBlock = (await getBlock(fromTimestamp, 'fantom', {}));
+  const toBlock = (await getBlock(toTimestamp, 'fantom', {}));
   const logs: ILog[][] = (await Promise.all(lpTokens.map((address: string) => sdk.api.util.getLogs({
-      target: address,
-      topic: topic_name,
-      toBlock: yesterdaysBlock,
-      fromBlock: todaysBlock,
-      keys: [],
-      chain: 'fantom',
-      topics: [topic0]
+    target: address,
+    topic: topic_name,
+    toBlock: toBlock,
+    fromBlock: fromBlock,
+    keys: [],
+    chain: 'fantom',
+    topics: [topic0]
   }))))
     .map((p: any) => p)
     .map((a: any) => a.output);
-  const coins = [...tokens0, ...tokens1].map((e: string) => `fantom:${e}`);
+  const rawCoins = [...tokens0, ...tokens1].map((e: string) => `fantom:${e}`);
+  const coins = [...new Set(rawCoins)]
   const prices = await getPrices(coins, timestamp);
   const untrackVolumes: number[] = lpTokens.map((_: string, index: number) => {
     const log: IAmount[] = logs[index]
-    .map((e:ILog)  => {return  { ...e, data: e.data.replace('0x', '') }})
-    .map((p: ILog) => {
-      const amount0In = new BigNumber('0x'+p.data.slice(0, 64)).toString();
-      const amount1In = new BigNumber('0x'+p.data.slice(64, 128)).toString();
-      const amount0Out = new BigNumber('0x'+p.data.slice(128, 192)).toString();
-      const amount1Out = new BigNumber('0x'+p.data.slice(192, 256)).toString();
-      return {
-        amount0In,
-        amount1In,
-        amount0Out,
-        amount1Out,
-      } as IAmount
-    }) as IAmount [];
+      .map((e: ILog) => { return { ...e, data: e.data.replace('0x', '') } })
+      .map((p: ILog) => {
+        const amount0In = new BigNumber('0x' + p.data.slice(0, 64)).toString();
+        const amount1In = new BigNumber('0x' + p.data.slice(64, 128)).toString();
+        const amount0Out = new BigNumber('0x' + p.data.slice(128, 192)).toString();
+        const amount1Out = new BigNumber('0x' + p.data.slice(192, 256)).toString();
+        return {
+          amount0In,
+          amount1In,
+          amount0Out,
+          amount1Out,
+        } as IAmount
+      }) as IAmount[];
     const token0Price = (prices[`fantom:${tokens0[index].toLowerCase()}`]?.price || 0);
     const token1Price = (prices[`fantom:${tokens1[index].toLowerCase()}`]?.price || 0);
     const token0Decimals = (prices[`fantom:${tokens0[index].toLowerCase()}`]?.decimals || 0)
     const token1Decimals = (prices[`fantom:${tokens1[index].toLowerCase()}`]?.decimals || 0)
     const totalAmount0 = log
-      .reduce((a: number, b: IAmount) => Number(b.amount0In)+ Number(b.amount0Out) + a, 0) / 10 ** token0Decimals * token0Price;
+      .reduce((a: number, b: IAmount) => Number(b.amount0In) + Number(b.amount0Out) + a, 0) / 10 ** token0Decimals * token0Price;
     const totalAmount1 = log
-      .reduce((a: number, b: IAmount) => Number(b.amount1In)+ Number(b.amount1Out) + a, 0) / 10 ** token1Decimals * token1Price;
-    const untrackAmountUSD = (totalAmount1 !== 0 && totalAmount1 !== 0) ? (totalAmount0 + totalAmount1) : 0; // counted only we have price data
+      .reduce((a: number, b: IAmount) => Number(b.amount1In) + Number(b.amount1Out) + a, 0) / 10 ** token1Decimals * token1Price;
+    if (token0Price === 0 && token1Price === 0) console.log("Riiiiip", tokens0[index], tokens1[index])
+    const untrackAmountUSD = token0Price !== 0 ? totalAmount0 : token1Price !== 0 ? totalAmount1 : 0; // counted only we have price data
     return untrackAmountUSD;
   });
 
