c088427346542f3020e76419f6badc0991dead93
opar-xu <opar.xu@binance.com>
2023-02-10 10:37:19 +0400

    add onepunch

---
+++ dexs/onepunch/index.ts
@@ -0,0 +1,19 @@
+import { CHAIN } from "../../helpers/chains";
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+
+const adapters = univ2Adapter(
+  {
+    [CHAIN.BSC]:
+      "https://api.thegraph.com/subgraphs/name/one-punch-team/onepunch-subgraph-bsc",
+  },
+  {
+    factoriesName: "onePunchDatas",
+    dayData: "onePunchDayData",
+    dailyVolume: "dailyVolumnUSD",
+    totalVolume: "totalVolumeUSD",
+    hasTotalVolume: true,
+  }
+);
+adapters.adapter.bsc.start = async () => 1671580800;
+
+export default adapters;
