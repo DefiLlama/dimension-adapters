e64efbd4d7a1704018e796e3749d5e76ad1d538b
0xgnek <0xgnek@gmail.com>
2023-02-19 02:38:03 +0000

    update open book url

--- dexs/openbook/index.ts
+++ dexs/openbook/index.ts
@@ -5,16 +5,14 @@ import fetchURL from "../../utils/fetchURL";
 
 
 interface IVolume {
-  value: number;
-  humanTime: string;
+  volume_24h: number;
 }
 
 const fetch = async (timestamp: number) => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  const url = `https://api.solscan.io/amm/chart?source=openbook&type=all&chart=total_volume24h`;
-  const historicalVolume: IVolume[] = (await fetchURL(url)).data.data.items;
-  const dailyVolume = historicalVolume
-    .find(dayItem => (new Date(dayItem.humanTime.split('T')[0]).getTime() / 1000) === dayTimestamp)?.value
+  const url = `https://dry-ravine-67635.herokuapp.com/pairs`;
+  const historicalVolume: IVolume[] = (await fetchURL(url)).data;
+  const dailyVolume = historicalVolume.reduce((a: number, b: IVolume) => a + b.volume_24h, 0);
 
   return {
     timestamp: dayTimestamp,
@@ -26,6 +24,7 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.SOLANA]: {
       fetch: fetch,
+      runAtCurrTime: true,
       start: async () => 1668643200,
     },
   },
