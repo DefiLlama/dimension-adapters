0df5f7169cf48ec9383eadc4d81ddeb6f43ec148
0xgnek <0xgnek@gmail.com>
2023-02-23 14:03:25 +0000

    update dfx finance subgraph

--- dexs/dfx-finance/index.ts
+++ dexs/dfx-finance/index.ts
@@ -1,10 +1,10 @@
 import { CHAIN } from "../../helpers/chains";
 import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 const adapter = univ2Adapter({
-    [CHAIN.ETHEREUM]: "https://api.thegraph.com/subgraphs/name/dfx-finance/dfx-v1",
-    [CHAIN.POLYGON]: "https://api.thegraph.com/subgraphs/name/dfx-finance/dfx-v1-polygon",
+    [CHAIN.ETHEREUM]: "https://api.goldsky.com/api/public/project_clasdk93949ub0h10a9lf9pkq/subgraphs/dfx-v2/latest/gn",
+    [CHAIN.POLYGON]: "https://api.goldsky.com/api/public/project_clasdk93949ub0h10a9lf9pkq/subgraphs/dfx-v2-polygon/latest/gn",
 }, {
-    factoriesName: "dfxfactories",
+    factoriesName: "dfxfactoryV2S",
     totalVolume: "totalVolumeUSD",
     dayData: "dfxdayData",
     dailyVolume: "dailyVolumeUSD"
