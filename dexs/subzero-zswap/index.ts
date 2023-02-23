15efc6965cef5c0ea0ac8f68182331c60b70d863
0xgnek <0xgnek@gmail.com>
2023-02-23 15:57:44 +0000

    add subzero-zswap

---
+++ dexs/subzero-zswap/index.ts
@@ -0,0 +1,17 @@
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { CHAIN } from "../../helpers/chains";
+
+const endpoints = {
+  [CHAIN.AVAX]: "https://api.thegraph.com/subgraphs/name/mkrman/exchange",
+};
+
+const adapter = univ2Adapter(endpoints, {
+  factoriesName: "zswapFactories",
+  dayData: "zswapDayData",
+  dailyVolume: "dailyVolumeUSD",
+  totalVolume: "totalVolumeUSD"
+});
+
+adapter.adapter.avax.start = async()=> 1675814400;
+
+export default adapter
