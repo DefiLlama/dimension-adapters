ab6c3d459613d87d5ec81f1223d8db8498afd6e8
zyberswap <arbadex@protonmail.com>
2023-03-06 11:19:26 +0100

    feat: add zyberswap volume and fee data

--- dexs/zyberswap/index.ts
+++ dexs/zyberswap/index.ts
@@ -1,11 +1,79 @@
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { BreakdownAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { DEFAULT_DAILY_VOLUME_FACTORY, DEFAULT_DAILY_VOLUME_FIELD, DEFAULT_TOTAL_VOLUME_FACTORY, DEFAULT_TOTAL_VOLUME_FIELD, getChainVolume } from "../../helpers/getUniSubgraphVolume";
 
 const endpoints = {
   [CHAIN.ARBITRUM]: "https://api.thegraph.com/subgraphs/name/zyberswap-arbitrum/zyber-amm",
 };
-const adapter = univ2Adapter(endpoints, {});
 
-adapter.adapter.arbitrum.start = async () => 1674432000;
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: DEFAULT_TOTAL_VOLUME_FACTORY,
+    field: DEFAULT_TOTAL_VOLUME_FIELD,
+  },
+  dailyVolume: {
+    factory: DEFAULT_DAILY_VOLUME_FACTORY,
+    field: DEFAULT_DAILY_VOLUME_FIELD,
+    dateField: "date"
+  },
+});
 
-export default adapter
+const endpointsV3 = {
+  [CHAIN.ARBITRUM]: "https://api.thegraph.com/subgraphs/name/iliaazhel/zyberswap-info",
+};
+const graphsV3 = getChainVolume({
+  graphUrls: endpointsV3,
+  totalVolume: {
+    factory: "factories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "algebraDayData",
+    field: "volumeUSD",
+    dateField: "date"
+  },
+});
+
+const endpointsStable = {
+  [CHAIN.ARBITRUM]: "https://api.thegraph.com/subgraphs/name/zyberswap-arbitrum/zyber-stableamm"
+};
+
+const graphsStable = getChainVolume({
+  graphUrls: endpointsStable,
+  totalVolume: {
+    factory: "tradeVolumes",
+    field: "volume",
+  },
+  dailyVolume: {
+    factory: "dailyVolume",
+    field: "volume",
+    dateField: "timestamp"
+  },
+});
+
+
+const adapter: BreakdownAdapter = {
+  breakdown: {
+    v2: {
+      [CHAIN.ARBITRUM]: {
+        fetch: graphs(CHAIN.ARBITRUM),
+        start: async () => 1674432000
+      },
+    },
+    v3: {
+      [CHAIN.ARBITRUM]: {
+        fetch: graphsV3(CHAIN.ARBITRUM),
+        start: async () => 1676887200
+      },
+    },
+    stable: {
+      [CHAIN.ARBITRUM]: {
+        fetch: graphsStable(CHAIN.ARBITRUM),
+        start: async () => 1676113200,
+      },
+    }
+  },
+};
+
+export default adapter;
