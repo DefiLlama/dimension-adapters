1b2728d3d1ebb27a22907d7ebf130652d3651d0c
0xngmi <0xngmi@protonmail.com>
2023-03-25 21:31:04 +0000

    add tron unique addresses

--- users/chains.ts
+++ users/chains.ts
@@ -20,13 +20,6 @@ async function solanaUsers(start: number, end: number) {
     return query[0][0]
 }
 
-/*
-// https://tronscan.org/#/data/stats2/accounts/activeAccounts
-async function tronscan(start: number, end: number) {
-    // unsure about methodology being used so not included for now
-}
-*/
-
 async function osmosis(start: number, end: number) {
     const query = await queryFlipside(`select count(DISTINCT TX_FROM) from osmosis.core.fact_transactions where BLOCK_TIMESTAMP > TO_TIMESTAMP_NTZ(${start}) AND BLOCK_TIMESTAMP < TO_TIMESTAMP_NTZ(${end})`)
     return query[0][0]
@@ -37,26 +30,36 @@ async function near(start: number, end: number) {
     return query[0][0]
 }
 
-const toIso = (d:number) => new Date(d*1e3).toISOString()
 const timeDif = (d:string, t:number) => Math.abs(new Date(d).getTime() - new Date(t*1e3).getTime())
+function findClosestItem(results:any[], timestamp:number, getTimestamp:(x:any)=>string){
+    return results.reduce((acc:any, t:any)=>{
+        if(timeDif(getTimestamp(t), timestamp) < timeDif(getTimestamp(acc), timestamp)){
+            return t
+        } else {
+            return acc
+        }
+    }, results[0])
+}
+
+
+const toIso = (d:number) => new Date(d*1e3).toISOString()
 function coinmetricsData(assetID: string) {
     return async (start: number, end: number) => {
         const result = (await axios.get(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?page_size=10000&metrics=AdrActCnt&assets=${assetID}&start_time=${toIso(start - 24*3600)}&end_time=${toIso(end + 24*3600)}`)).data.data;
-        const closestDatapooint = result.reduce((acc:any, t:any)=>{
-            if(timeDif(t.time, start) < timeDif(acc.time, start)){
-                return t
-            } else {
-                return acc
-            }
-        }, result[0])
-        if (!closestDatapooint) {
+        const closestDatapoint = findClosestItem(result, start, t=>t.time)
+        if (!closestDatapoint) {
             throw new Error(`Failed to fetch CoinMetrics data for ${assetID} on ${end}, no data`);
         }
 
-        return parseFloat(closestDatapooint['AdrActCnt']);
+        return parseFloat(closestDatapoint['AdrActCnt']);
     }
 }
 
+// https://tronscan.org/#/data/stats2/accounts/activeAccounts
+async function tronscan(start: number, _end: number) {
+    const results = (await axios.get(`https://apilist.tronscanapi.com/api/account/active_statistic?type=day&start_timestamp=${(start - 2*24*3600)*1e3}`)).data.data;
+    return findClosestItem(results, start, t=>t.day_time).active_count
+}
 
 export const users = [
     "arbitrum", "avalanche", "bsc", "ethereum", "gnosis", "optimism", "polygon",
@@ -98,5 +101,10 @@ export const users = [
     {
         name: "bsv",
         getUsers: coinmetricsData("bsv")
+    },
+    // tronscan
+    {
+        name: "tron",
+        getUsers: tronscan
     },
 ])
