ba20bc77ee58812f0b3e4d7d2a3b549d74f8dde4
0xngmi <0xngmi@protonmail.com>
2023-03-25 00:18:12 +0000

    naming

--- users/chains.ts
+++ users/chains.ts
@@ -1,7 +1,7 @@
 import { queryFlipside } from "../helpers/flipsidecrypto";
 import { getBlocks } from "../helpers/getBlock";
 
-function getUsers(chain: string) {
+function getUsersChain(chain: string) {
     return async (start: number, end: number) => {
         const [startBlock, endBlock] = await getBlocks(chain, [start, end])
         const query = await queryFlipside(`select count(DISTINCT FROM_ADDRESS) from ${chain}.core.fact_transactions where BLOCK_NUMBER > ${startBlock} AND BLOCK_NUMBER < ${endBlock}`)
@@ -12,4 +12,4 @@ function getUsers(chain: string) {
 export const users = [
     "arbitrum", "avalanche", "bsc", "ethereum", "flow", "gnosis", "near", "optimism", "osmosis", "polygon", "solana",
     // "terra2"
-].map(c=>({name:c, users: getUsers(c)}))
+].map(c=>({name:c, getUsers: getUsersChain(c)}))
