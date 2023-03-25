7ba074bf94c8d7cd00ff9b1d9b5bc3df9e895349
0xngmi <0xngmi@protonmail.com>
2023-03-25 16:15:52 +0000

    fix some chains

--- users/chains.ts
+++ users/chains.ts
@@ -1,7 +1,7 @@
 import { queryFlipside } from "../helpers/flipsidecrypto";
 import { getBlocks } from "../helpers/getBlock";
 
-const convertChain = (chain:string)=>({
+const convertChain = (chain: string) => ({
     gnosis: "xdai",
     avalanche: "avax"
 }[chain] ?? chain)
@@ -14,7 +14,43 @@ function getUsersChain(chain: string) {
     }
 }
 
+async function solanaUsers(start: number, end: number) {
+    const query = await queryFlipside(`select count(DISTINCT SIGNERS[0]) from solana.core.fact_transactions where BLOCK_TIMESTAMP > TO_TIMESTAMP_NTZ(${start}) AND BLOCK_TIMESTAMP < TO_TIMESTAMP_NTZ(${end})`)
+    return query[0][0]
+}
+
+/*
+// https://tronscan.org/#/data/stats2/accounts/activeAccounts
+async function tronscan(start: number, end: number) {
+    // unsure about methodology being used so not included for now
+}
+*/
+
+async function osmosis(start: number, end: number) {
+    const query = await queryFlipside(`select count(DISTINCT TX_FROM) from osmosis.core.fact_transactions where BLOCK_TIMESTAMP > TO_TIMESTAMP_NTZ(${start}) AND BLOCK_TIMESTAMP < TO_TIMESTAMP_NTZ(${end})`)
+    return query[0][0]
+}
+
+async function near(start: number, end: number) {
+    const query = await queryFlipside(`select count(DISTINCT TX_SIGNER) from near.core.fact_transactions where BLOCK_TIMESTAMP > TO_TIMESTAMP_NTZ(${start}) AND BLOCK_TIMESTAMP < TO_TIMESTAMP_NTZ(${end})`)
+    return query[0][0]
+}
+
+
 export const users = [
-    "arbitrum", "avalanche", "bsc", "ethereum", "flow", "gnosis", "near", "optimism", "osmosis", "polygon", "solana",
-    // "terra2"
-].map(c=>({name:c, getUsers: getUsersChain(c)}))
+    "arbitrum", "avalanche", "bsc", "ethereum", "gnosis", "optimism", "polygon",
+    // "terra2", "flow"
+].map(c => ({ name: c, getUsers: getUsersChain(c) })).concat([
+    {
+        name: "solana",
+        getUsers: solanaUsers
+    },
+    {
+        name: "osmosis",
+        getUsers: osmosis
+    },
+    {
+        name: "near",
+        getUsers: near
+    }
+])
