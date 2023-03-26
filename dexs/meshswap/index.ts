f7af77af5c4c907d1e817714495804357cd1d53f
0xgnek <0xgnek@gmail.com>
2023-03-26 14:15:50 +0000

    update link volume Meshswap

--- dexs/meshswap/index.ts
+++ dexs/meshswap/index.ts
@@ -2,7 +2,7 @@ import { SimpleAdapter } from "../../adapters/types";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 import fetchURL from "../../utils/fetchURL"
 
-const historicalVolumeEndpoint = "https://s.meshswap.fi/stat/dashboardInfo.json"
+const historicalVolumeEndpoint = "https://ss.meshswap.fi/stat/dashboardInfo.json"
 
 interface IVolume {
   dateId: string;
