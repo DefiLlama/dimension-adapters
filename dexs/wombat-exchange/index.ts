3bc1f83d5db171eb961ea8be0c610cc7ad05d361
corey-wombat <corey@wombat.exchange>
2023-03-27 22:20:16 +0800

    fix double counted vol

--- dexs/wombat-exchange/index.ts
+++ dexs/wombat-exchange/index.ts
@@ -1,17 +1,43 @@
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { FetchResult, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { gql, request } from "graphql-request";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+interface IGraph {
+  dailyTradeVolumeUSD: string;
+  dayID: string;
+}
 
-const endpoints = {
-  [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/wombat-exchange/wombat-exchange",
+const URL =
+  "https://api.thegraph.com/subgraphs/name/wombat-exchange/wombat-exchange";
+
+const fetch = async (timestamp: number): Promise<FetchResult> => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+  const dayID = dayTimestamp / 86400;
+  const query = gql`
+    {
+        protocolDayData(id: "${dayID}") {
+            dayID
+            dailyTradeVolumeUSD
+        }
+    }
+    `;
+  const response: IGraph = (await request(URL, query)).protocolDayData;
+  console.log(response);
+  const dailyVolume = Number(response.dailyTradeVolumeUSD) / 2;
+
+  return {
+    dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
+    timestamp: dayTimestamp,
+  };
 };
 
-const adapter = univ2Adapter(endpoints, {
-  factoriesName: "protocols",
-  dayData: "protocolDayData",
-  dailyVolume: "dailyTradeVolumeUSD",
-  totalVolume: "totalTradeVolumeUSD"
-});
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.BSC]: {
+      fetch: fetch,
+      start: async () => 1650243600,
+    },
+  },
+};
 
-adapter.adapter.bsc.start = async()=>1650243600
-
-export default adapter
+export default adapter;
