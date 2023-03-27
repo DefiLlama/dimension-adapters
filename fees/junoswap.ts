335c0cd55404e755025e572937d216f9ea7964ed
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-03-27 20:20:06 +0200

    Fix + join junoswap adapters (#335)
    * Fix + join junoswap adapters
    
    * undefined is default

--- fees/junoswap.ts
+++ fees/junoswap.ts
@@ -3,8 +3,7 @@ import { CHAIN } from "../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
 import fetchURL from "../utils/fetchURL";
 
-
-const historicalVolumeEndpoint = "https://api-junoswap.enigma-validator.com/volumes/total/historical/999M/d"
+const historicalVolumeEndpoint = "https://api-junoswap.enigma-validator.com/volumes/total/historical/12M/d"
 
 interface IVolumeall {
   volume_total: string;
@@ -21,10 +20,12 @@ const fetch = async (timestamp: number) => {
   const dailyVolume = historicalVolume
     .find(dayItem => getUniqStartOfTodayTimestamp(new Date(dayItem.date)) === dayTimestamp)?.volume_total
   const totalFeesUsd = totalVolume * TOTAL_FEES;
-  const dailyFeesUsd = Number(dailyVolume || 0) * TOTAL_FEES
+  const dailyFeesUsd = dailyVolume ? Number(dailyVolume) * TOTAL_FEES : undefined
   return {
     totalFees: totalFeesUsd.toString(),
-    dailyFees: dailyFeesUsd.toString(),
+    dailyFees: dailyFeesUsd ? dailyFeesUsd.toString(): undefined,
+    dailyVolume: dailyVolume ? dailyVolume.toString() : undefined,
+    totalVolume: `${totalVolume}`,
     timestamp: dayTimestamp,
   };
 };
