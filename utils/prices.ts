9d549b0d692d823ff8f20b25072c12ac20c87823
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-03-28 16:48:21 +0200

    Improve chainlink adapter rebased (#338)
    * Improve getlogs flipside retry and getprices performance
    
    * Fix flipside bail condition
    
    * Split chainlink adapters
    
    * Revert "Split chainlink adapters"
    
    This reverts commit a63c50df77c6bf66b2c00081a6e16e5dd48554ea.
    
    * Split chainlink adapters
    
    This reverts commit c8fc290b5f2e3402e76351e87b9c052892c26da1.
    
    * Rename chainlink adapters

--- utils/prices.ts
+++ utils/prices.ts
@@ -1,16 +1,33 @@
 import axios from 'axios'
+import { IJSON } from '../adapters/types'
+interface Price { decimals: number, price: number, symbol: string, timestamp: number }
+const pricesStore: IJSON<IJSON<Price>> = {}
 
 export const getPrices = async (tokens: string[], timestamp?: number) => {
-    let body: any = {
-        "coins": Array.from(tokens),
-    }
+    let path = "/current"
     if (timestamp) {
-        body = {
-            "timestamp": timestamp,
-            ...body
+        path = `/historical/${timestamp}`
+    }
+    let tokens2Fetch = tokens
+    if (pricesStore[timestamp ?? '']) {
+        const storedtokens = Object.keys(pricesStore[timestamp ?? ''])
+        tokens2Fetch = tokens.filter(token => !storedtokens.includes(token))
+    }
+    let res = { data: { coins: {} } }
+    if (tokens2Fetch.length > 0)
+        res = await axios.get(`https://coins.llama.fi/prices${path}/${tokens2Fetch.join(",")}`)
+    if (timestamp)
+        pricesStore[timestamp] = {
+            ...pricesStore[timestamp],
+            ...res.data.coins
         }
-    }
-    return (await axios.post("https://coins.llama.fi/prices", body)).data.coins as {
-        [address:string]: { decimals: number, price: number, symbol: string, timestamp: number }
-    }
+    return {
+        ...getObjFromAttrs(pricesStore[timestamp ?? ''], tokens),
+        ...res.data.coins,
+    } as IJSON<Price>
 }
+
+const getObjFromAttrs = <T>(obj: IJSON<T>, list: string[]) => list.reduce((acc, curr) => {
+    acc[curr] = obj[curr]
+    return acc
+}, {} as IJSON<T>)
