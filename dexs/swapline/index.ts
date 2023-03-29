f1c8bf2341d00b40e1bfb2d13cd90216bb3e37b5
0xtawa <106731946+0xtawa@users.noreply.github.com>
2023-03-29 15:58:29 +0200

    Add swapline and disable metropolis (#343)

--- dexs/swapline/index.ts
+++ dexs/swapline/index.ts
@@ -1,7 +1,5 @@
 import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 import { CHAIN } from "../../helpers/chains";
-import disabledAdapter from "../../helpers/disabledAdapter";
-import { DISABLED_ADAPTER_KEY } from "../../adapters/types";
 
 const endpoints = {
   [CHAIN.FANTOM]: "https://api.thegraph.com/subgraphs/name/0xhans1/metropolis-v2",
@@ -16,12 +14,4 @@ const adapter = univ2Adapter(endpoints, {
 });
 
 adapter.adapter.fantom.start = async () => 1673827200;
-
-// Metropolis was acquired by Swapline.
-// Currently, both adapters (tvl) are the same code.
-// We have set Metropolis TVL to 0.
-// We counting volume for Metropolis,
-// and use the exact same code (with a new listing
-// since swapline recently launched)
-adapter.adapter[DISABLED_ADAPTER_KEY] = disabledAdapter
 export default adapter;
