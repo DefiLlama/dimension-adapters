b782fd292d68835036c477a5dffe1d762c87438c
0xgnek <0xgnek@gmail.com>
2023-03-31 06:58:22 +0000

    fix get block vision chain

--- helpers/getBlock.ts
+++ helpers/getBlock.ts
@@ -23,7 +23,7 @@ async function getBlock(timestamp: number, chain: Chain, chainBlocks: ChainBlock
             block = Number((await retry(async () => (await axios.get(`https://zksync2-mainnet.zkscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before`).catch((e) => {
                 throw new Error(`Error getting block: ${chain} ${timestamp} ${e.message}`)
             }))?.data?.result?.blockNumber)));
-        else if (chain as CHAIN === CHAIN.POLYGON_ZKEVM)
+        else if (chain as CHAIN === CHAIN.POLYGON_ZKEVM || chain === CHAIN.VISION)
             return sdk.api.util.lookupBlock(timestamp, { chain }).then(blockData => blockData.block) // TODO after get block support chain  polygon_zkevm then swith to use api https://coins.llama.fi/block
         else
             block = Number((await retry(async () => (await axios.get(`https://coins.llama.fi/block/${chain}/${timestamp}`).catch((e) => {
