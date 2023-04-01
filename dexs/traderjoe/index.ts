6fabee67934ef4e02f079acc2968e35f9ddc8406
0xgnek <0xgnek@gmail.com>
2023-04-01 13:48:23 +0000

    update joe api

--- dexs/traderjoe/index.ts
+++ dexs/traderjoe/index.ts
@@ -1,16 +1,44 @@
-import { BreakdownAdapter } from "../../adapters/types";
+import { Chain } from "@defillama/sdk/build/general";
+import { BreakdownAdapter, FetchResultVolume } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getChainVolume } from "../../helpers/getUniSubgraphVolume";
+import { getChainVolume, getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import fetchURL from "../../utils/fetchURL";
 
 const endpoints = {
   [CHAIN.AVAX]: "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/exchange",
+  [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v1-bnb",
+  [CHAIN.ARBITRUM]: "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v1-arbitrum",
 };
-const endpointsV2 = {
+type TEndpoint = {
+  [s: string | Chain]: string;
+}
+const endpointsV2: TEndpoint = {
   [CHAIN.AVAX]: "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2",
-  [CHAIN.ARBITRUM]: "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-arbitrum",
-  [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-bnb"
+  [CHAIN.ARBITRUM]: "https://barn.traderjoexyz.com/v1/dex/analytics/arbitrum?startTime=1672012800&aggregateBy=daily",
+  [CHAIN.BSC]: "https://barn.traderjoexyz.com/v1/dex/analytics/binance?startTime=1677801600&aggregateBy=daily"
 }
 
+interface IVolume {
+  timestamp: number;
+  volumeUsd: number;
+}
+const fetchV2 = (chain: Chain) => {
+  return async (timestamp: number): Promise<FetchResultVolume> => {
+    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+    const historicalVolume: IVolume[] = (await fetchURL(endpointsV2[chain]))?.data;
+    const totalVolume = historicalVolume
+      .filter(volItem => volItem.timestamp <= dayTimestamp)
+      .reduce((acc, { volumeUsd }) => acc + Number(volumeUsd), 0)
+
+    const dailyVolume = historicalVolume
+      .find(dayItem => dayItem.timestamp === dayTimestamp)?.volumeUsd
+    return {
+      totalVolume: `${totalVolume}`,
+      dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
+      timestamp: dayTimestamp,
+    }
+  }
+}
 
 const graphsV1 = getChainVolume({
   graphUrls: endpoints,
@@ -46,6 +74,14 @@ const adapter: BreakdownAdapter = {
         fetch: graphsV1(CHAIN.AVAX),
         start: async () => 1628467200,
       },
+      [CHAIN.BSC]: {
+        fetch: graphsV1(CHAIN.BSC),
+        start: async () => 1664841600,
+      },
+      [CHAIN.ARBITRUM]: {
+        fetch: graphsV1(CHAIN.ARBITRUM),
+        start: async () => 1664841600,
+      },
     },
     v2: {
       [CHAIN.AVAX]: {
@@ -53,12 +89,12 @@ const adapter: BreakdownAdapter = {
         start: async () => 1668556800
       },
       [CHAIN.ARBITRUM]: {
-        fetch: graphsV2(CHAIN.ARBITRUM),
-        start: async () => 1671926400
+        fetch: fetchV2(CHAIN.ARBITRUM),
+        start: async () => 1672012800
       },
       [CHAIN.BSC]: {
-        fetch: graphsV2(CHAIN.BSC),
-        start: async () => 1677715200
+        fetch: fetchV2(CHAIN.BSC),
+        start: async () => 1677801600
       },
     }
   },
