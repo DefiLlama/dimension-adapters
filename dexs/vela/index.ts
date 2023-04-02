6155878f9271431cf213f97b7904cbb3f97feddf
unknown <pogsmorc@gmail.com>
2023-04-02 16:58:08 +0300

    refactoring

--- dexs/vela/index.ts
+++ dexs/vela/index.ts
@@ -3,33 +3,38 @@ import { CHAIN } from "../../helpers/chains";
 import { request, gql } from "graphql-request";
 import type { ChainEndpoints } from "../../adapters/types";
 import { Chain } from "@defillama/sdk/build/general";
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 
 const endpoints = {
   [CHAIN.ARBITRUM]:
     "https://api.thegraph.com/subgraphs/name/tskweres/vela-exchange-v2",
 };
 
-const methodology = {
-  Volume: "Total trading volume",
-};
-
-const graphQuery = gql`
-  query {
-    globalInfos(where: { id: "all" }) {
-      volume
-    }
-  }
-`;
-
 const graphs = (graphUrls: ChainEndpoints) => {
   return (chain: Chain) => {
     return async (timestamp: number) => {
+      const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+      const graphQuery = gql`
+        query {
+          dailyGlobalInfos(where: { id: "global-day-${todaysTimestamp}" }) {
+            tradeVolume
+          }
+          globalInfos(where: { id: "all" }) {
+            volume
+          }
+        }
+      `;
+      console.log(todaysTimestamp);
+
       const graphRes = await request(graphUrls[chain], graphQuery);
       const totalVolume = parseInt(graphRes.globalInfos[0].volume) / 1e30;
+      const dailyVolume =
+        parseInt(graphRes.dailyGlobalInfos[0].tradeVolume) / 1e30;
 
       return {
         timestamp,
-        tradeVolume: totalVolume.toString(),
+        totalVolume: totalVolume.toString(),
+        dailyVolume: dailyVolume.toString(),
       };
     };
   };
@@ -40,9 +45,6 @@ const adapter: Adapter = {
     [CHAIN.ARBITRUM]: {
       fetch: graphs(endpoints)(CHAIN.ARBITRUM),
       start: async () => 1675288800,
-      meta: {
-        methodology,
-      },
     },
   },
 };
