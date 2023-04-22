52fe9bdd96183133772afb6ec2563564e662da43
CryptoSnowPrince <topdirector2017@gmail.com>
2023-04-22 22:09:28 +0200

    update merlin on dexes add merlin on fees

--- dexs/merlin/index.ts
+++ dexs/merlin/index.ts
@@ -5,5 +5,5 @@ const adapters = univ2Adapter({
     [CHAIN.ZKSYNC]: "https://api.studio.thegraph.com/query/45654/merlin-subgraph/v0.1.0"
 }, {});
 
-adapters.adapter.zksync.start = async () => 887190;
+adapters.adapter.zksync.start = async () => 1680274800;
 export default adapters;
