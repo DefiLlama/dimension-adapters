44b959e65913c017a3c60eb5156d3f8b35535430
0xgnek <0xgnek@gmail.com>
2023-04-24 12:55:08 +0000

    update jojo

--- dexs/jojo/index.ts
+++ dexs/jojo/index.ts
@@ -1,26 +1,52 @@
+import fetchURL from "../../utils/fetchURL"
+import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { univ2DimensionAdapter } from "../../helpers/getUniSubgraph";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { getPrices } from "../../utils/prices";
 
-const DEFAULT_DAILY_VOLUME_FACTORY = "dailyVolume";
-const DEFAULT_DAILY_DATE_FIELD = "timestamp";
-const DEFAULT_DAILY_VOLUME_FIELD = "dailyVolume";
-const DEFAULT_TOTAL_VOLUME_FACTORY = "jojodealers";
-const DEFAULT_TOTAL_VOLUME_FIELD = "totalVolumeUSD";
+const historicalVolumeEndpoint = (symbol: string) => `https://api.arbitrum.jojo.exchange/v1/klines?marketId=${symbol}&interval=1D&startTime=1681776000000&limit=500`
 
-const adapter = univ2DimensionAdapter({
-    graphUrls: {
-        [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/kittyfu307/jojov1",
+const coins = {
+    'ethusdc': 'coingecko:ethereum',
+    'btcusdc': 'coingecko:bitcoin',
+}
+
+interface IVolumeall {
+    id: string;
+    volume: string;
+    tokenPrice: string;
+    timestamp: number;
+}
+const fetch = async (timestamp: number) => {
+    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+    const historical = (await Promise.all(Object.keys(coins).map((coins: string) => fetchURL(historicalVolumeEndpoint(coins)))))
+        .map((a: any, index: number) => a.data.map((e: any) => { return { timestamp: e.time / 1000, volume: e.volume, id: Object.values(coins)[index] } })).flat()
+    const prices = (await getPrices(Object.values(coins), dayTimestamp))
+
+    const historicalUSD = historical.map((e: IVolumeall) => {
+        return {
+            ...e,
+            volumeUSD: Number(e.volume) * prices[e.id]?.price || 0
+        }
+    });
+    const dailyVolume = historicalUSD.filter((e: IVolumeall) => e.timestamp === dayTimestamp)
+        .reduce((a: number, { volumeUSD }) => a + volumeUSD, 0);
+    const totalVolume = historicalUSD.filter((e: IVolumeall) => e.timestamp <= dayTimestamp)
+        .reduce((a: number, { volumeUSD }) => a + volumeUSD, 0);
+    return {
+        totalVolume: `${totalVolume}`,
+        dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
+        timestamp: dayTimestamp,
+    };
+};
+
+const adapter: SimpleAdapter = {
+    adapter: {
+        [CHAIN.ARBITRUM]: {
+            fetch,
+            start: async () => 1682035200,
+        },
     },
-    dailyVolume: {
-        factory: DEFAULT_DAILY_VOLUME_FACTORY,
-        field: DEFAULT_DAILY_VOLUME_FIELD,
-        dateField: DEFAULT_DAILY_DATE_FIELD
-    },
-    totalVolume: {
-        factory: DEFAULT_TOTAL_VOLUME_FACTORY,
-        field: DEFAULT_TOTAL_VOLUME_FIELD
-    }
-}, {});
+};
 
-adapter.adapter.bsc.start = async () => 1667260800;
 export default adapter;
