4a108abc5499f66760fd8fad68cb348bcafbf98a
0xtawa <0xtawanofi@gmail.com>
2023-04-24 18:08:57 +0200

    Fix parsing advanced order

--- helpers/getOpenseaCollections/index.ts
+++ helpers/getOpenseaCollections/index.ts
@@ -108,17 +108,21 @@ const patientExtractFeesByPayoutFrmToken = async (iface: ethers.Interface, payou
                 clearInterval(intervalid)
                 const allTxs = rangeSeaportTxs.map(tx => {
                     try {
-                        return iface.parseTransaction({ data: `0x${tx.data.toString('hex')}` })
+                        return {
+                            ...iface.parseTransaction({ data: `0x${tx.data.toString('hex')}` }),
+                            tx_hash: tx.hash
+                        }
                     } catch (e) {
                         // @ts-ignore
                         console.error("parsetx", e.message)
                     }
-                }).filter((tx): tx is ethers.TransactionDescription => !!tx)
+                }).filter((tx): tx is ethers.TransactionDescription & { tx_hash: string } => !!tx && Object.keys(tx).length>1)
                 // extract fees based on payoutAddress and sum them
                 const allPayouts = [] as ReturnType<typeof extractFeesByPayoutAddress>
                 for (const tx of allTxs) {
                     allPayouts.push(...extractFeesByPayoutAddress(tx, payoutAddress, collectionAddress))
                 }
+                console.log(allPayouts.length)
                 resolve(allPayouts.reduce((acc, payout) => {
                     Object.entries(payout).forEach(([token, balance]) => {
                         let sum = balance
@@ -134,18 +138,36 @@ const patientExtractFeesByPayoutFrmToken = async (iface: ethers.Interface, payou
     });
 }
 
+const DEFAULT_OFFER_TOKEN = "0x0000000000000000000000000000000000000000"
 const extractFeesByPayoutAddress = (tx: ethers.TransactionDescription, payoutAddress: string, collectionAddress: string) => {
-    let considerationArr
-    let fee
+    let considerationArr: (string | BigInt)[] | undefined
+    let fee: BigNumber | undefined
     let response = [] as (IJSON<string> | undefined)[]
     try {
         switch (tx.fragment.name) {
-            case "fulfillAdvancedOrder":
+            /* case "fulfillAdvancedOrder":
+                // check collectionaddress
+                // @ts-ignore
+                // console.log("tx.tx_hash", tx.tx_hash)
+                // if no arg 5, then offer token is ETH
+                let offerToken = DEFAULT_OFFER_TOKEN
+                if (tx.args.length > 5) {
+                    offerToken = tx.args[5]
+                    console.log("offerToken", offerToken)
+                }
+                response = [processAdvancedOrder(tx?.args[0], payoutAddress, offerToken, collectionAddress)]
+                break */
             case "fulfillOrder":
                 if (tx.args[0][0][2][0][1].toLowerCase() !== collectionAddress.toLowerCase()) break
-                response = [processAdvancedOrder(tx?.args[0], payoutAddress, tx?.args[5])]
+                // @ts-ignore
+                console.log("tx.tx_hash", tx.tx_hash)
+                console.log(tx?.args[0][0])
+                console.log(tx?.args[0][0][2])
+                console.log(tx?.args[0][0][3])
+                console.log(tx?.args[0][0][4])
+                response = [processAdvancedOrder(tx?.args[0], payoutAddress, tx?.args[5], collectionAddress)]
                 break
-            case "fulfillAvailableOrders":
+            /* case "fulfillAvailableOrders":
             case "fulfillAvailableAdvancedOrders":
             case "matchAdvancedOrders":
             case "matchOrders":
@@ -162,9 +184,9 @@ const extractFeesByPayoutAddress = (tx: ethers.TransactionDescription, payoutAdd
                     consideration.filter((el): el is string => typeof el === 'string').map(el => el.toLowerCase()).includes(payoutAddress.toLowerCase())
                 )
                 if (considerationArr) {
-                    fee = BigNumber(considerationArr[0])
+                    fee = BigNumber(considerationArr[0] as string) // its BigInt but ts complains bc current target has no BigInt support
                     response = [{
-                        [tx?.args[5]]: fee.dividedBy(1e18).toString()
+                        [tx?.args[0][0]]: fee.dividedBy(1e18).toString()
                     }]
                 }
                 break
@@ -174,25 +196,26 @@ const extractFeesByPayoutAddress = (tx: ethers.TransactionDescription, payoutAdd
                     consideration.filter((el): el is string => typeof el === 'string').map(el => el.toLowerCase()).includes(payoutAddress.toLowerCase())
                 )
                 if (considerationArr) {
-                    fee = BigNumber(considerationArr[0])
+                    fee = BigNumber(considerationArr[0] as string) // its BigInt but ts complains bc current target has no BigInt support
                     response = [{
-                        [tx.args[5]]: fee.dividedBy(1e18).toString()
+                        [tx.args[0][0]]: fee.dividedBy(1e18).toString()
                     }]
                 }
-                break
+                break */
             default:
                 response = []
                 break
         }
     } catch (error) {
         // @ts-ignore
-        console.error("extracttx", tx.fragment.name, tx?.args, error)
+        console.error("Error processing tx with hash", tx.tx_hash, ", with error", error.message, "and body", tx)
     }
     return response.filter((el): el is IJSON<string> => el !== undefined)
 }
 
 
-const processAdvancedOrder = (order: any, payoutAddress: string, offerToken: string) => {
+const processAdvancedOrder = (order: any, payoutAddress: string, offerToken: string, collectionAddress: string) => {
+    if (order[0][2][0][1].toLowerCase() !== collectionAddress.toLowerCase()) return undefined
     const considerationArr = order[0][3].find((consideration: (string | BigInt)[]) =>
         consideration.filter((el): el is string => typeof el === 'string').map(el => el.toLowerCase()).includes(payoutAddress.toLowerCase())
     )
