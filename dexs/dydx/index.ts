b42e075fca5f8c13244d348090154aabdda98436
0xgnek <0xgnek@gmail.com>
2023-04-24 16:09:32 +0000

    add dydx

---
+++ dexs/dydx/index.ts
@@ -0,0 +1,45 @@
+import fetchURL from "../../utils/fetchURL"
+import { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+
+const historicalVolumeEndpoint = "https://dydx.metabaseapp.com/api/public/dashboard/5fa0ea31-27f7-4cd2-8bb0-bc24473ccaa3/dashcard/322/card/234?parameters=%5B%5D"
+
+interface IVolumeall {
+  totalVolume: number;
+  dailyVolume: number;
+  time: string;
+}
+
+const fetch = async (timestamp: number) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+  const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint))?.data.data.rows
+    .map(([time, totalVolume, dailyVolume]: [string, number, number]) => {
+      return {
+        totalVolume,
+        dailyVolume,
+        time,
+      }
+    });
+
+  const dateString = new Date(timestamp * 1000).toISOString().split("T")[0];
+  const volume = historicalVolume
+    .find(dayItem => dayItem.time.split('T')[0] === dateString)
+
+  return {
+    totalVolume: volume ? `${volume.totalVolume}` : undefined,
+    dailyVolume: volume ? `${volume.dailyVolume}` : undefined,
+    timestamp: dayTimestamp,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.ETHEREUM]: {
+      fetch,
+      start: async () => 1614211200,
+    },
+  },
+};
+
+export default adapter;
