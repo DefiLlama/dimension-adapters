601d85a6abee99de6d1871ad03871b72071a4ec3
0xgnek <0xgnek@gmail.com>
2023-04-24 12:58:08 +0000

    use close price

--- dexs/jojo/index.ts
+++ dexs/jojo/index.ts
@@ -2,7 +2,6 @@ import fetchURL from "../../utils/fetchURL"
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import { getPrices } from "../../utils/prices";
 
 const historicalVolumeEndpoint = (symbol: string) => `https://api.arbitrum.jojo.exchange/v1/klines?marketId=${symbol}&interval=1D&startTime=1681776000000&limit=500`
 
@@ -14,19 +13,18 @@ const coins = {
 interface IVolumeall {
     id: string;
     volume: string;
-    tokenPrice: string;
+    close: string;
     timestamp: number;
 }
 const fetch = async (timestamp: number) => {
     const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
     const historical = (await Promise.all(Object.keys(coins).map((coins: string) => fetchURL(historicalVolumeEndpoint(coins)))))
-        .map((a: any, index: number) => a.data.map((e: any) => { return { timestamp: e.time / 1000, volume: e.volume, id: Object.values(coins)[index] } })).flat()
-    const prices = (await getPrices(Object.values(coins), dayTimestamp))
+        .map((a: any, index: number) => a.data.map((e: any) => { return { timestamp: e.time / 1000, volume: e.volume, id: Object.values(coins)[index], close: e.close } })).flat()
 
     const historicalUSD = historical.map((e: IVolumeall) => {
         return {
             ...e,
-            volumeUSD: Number(e.volume) * prices[e.id]?.price || 0
+            volumeUSD: Number(e.volume) * Number(e.close)
         }
     });
     const dailyVolume = historicalUSD.filter((e: IVolumeall) => e.timestamp === dayTimestamp)
