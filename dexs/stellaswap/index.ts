58f140aa91371952f68322a1c1010533b3101c83
Atilla Birer <bireratilla@gmail.com>
2023-05-15 07:34:30 +0300

    2 AMMs under one project

--- dexs/stellaswap/index.ts
+++ dexs/stellaswap/index.ts
@@ -1,8 +1,27 @@
+import { BreakdownAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 
-const adapters = univ2Adapter({
-  [CHAIN.MOONBEAN]: "https://api.thegraph.com/subgraphs/name/stellaswap/stella-swap"
+const v2adapters = univ2Adapter({
+  [CHAIN.MOONBEAM]: "https://api.thegraph.com/subgraphs/name/stellaswap/stella-swap"
 }, {});
-adapters.adapter.moonbeam.start = async () => 1641960253;
-export default adapters;
+v2adapters.adapter.moonbeam.start = async () => 1641960253;
+
+const v3adapters = univ2Adapter({
+  [CHAIN.MOONBEAM]: "https://api.thegraph.com/subgraphs/name/stellaswap/pulsar"
+}, {
+  factoriesName: "factories",
+  dayData: "algebraDayData",
+  dailyVolume: "volumeUSD",
+  totalVolume: "totalVolumeUSD",
+});
+v3adapters.adapter.moonbeam.start = async () => 1672876800;
+
+const adapter: BreakdownAdapter = {
+  breakdown: {
+    v2: v2adapters.adapter,
+    v3: v3adapters.adapter
+  }
+}
+
+export default adapter;
