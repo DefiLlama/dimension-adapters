9551c911c785a58d565052ed211ec1edc7b00921
victorlaiyeeteng <vlai69863@gmail.com>
2023-05-16 11:12:53 +0800

    Updated historicalVolumeEndpoint

--- dexs/demex/index.ts
+++ dexs/demex/index.ts
@@ -1,38 +1,54 @@
-import fetchURL from "../../utils/fetchURL"
-import { SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+// import fetchURL from "../../utils/fetchURL"
+// import { SimpleAdapter } from "../../adapters/types";
+// import { CHAIN } from "../../helpers/chains";
+// import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
-const START_TIME = 1659312000;
-const historicalVolumeEndpoint = (until: number) => `https://api-insights.carbon.network/pool/volume?from=${START_TIME}&interval=day&until=${until}`
+// const START_TIME = 1659312000;
+// const historicalVolumeEndpoint = (until: number) => `https://api-insights.carbon.network/pool/volume?from=${START_TIME}&interval=day&until=${until}`
 
-interface IVolumeall {
-  volumeValue: string;
-  totalVolumeValue: string;
-  date: string;
-}
+// interface IVolumeall {
+//   volumeValue: string;
+//   totalVolumeValue: string;
+//   date: string;
+// }
 
-const fetch = async (timestamp: number) => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint(dayTimestamp)))?.data.result.entries;
+// const fetch = async (timestamp: number) => {
+//   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+//   const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint(dayTimestamp)))?.data.result.entries;
 
-  const volume = historicalVolume
-    .find(dayItem => (new Date(dayItem.date.split('T')[0]).getTime() / 1000) === dayTimestamp)
+//   const volume = historicalVolume
+//     .find(dayItem => (new Date(dayItem.date.split('T')[0]).getTime() / 1000) === dayTimestamp)
 
-  return {
-    totalVolume: `${volume?.totalVolumeValue}`,
-    dailyVolume: volume ? `${volume.volumeValue}` : undefined,
-    timestamp: dayTimestamp,
-  };
+//   return {
+//     totalVolume: `${volume?.totalVolumeValue}`,
+//     dailyVolume: volume ? `${volume.volumeValue}` : undefined,
+//     timestamp: dayTimestamp,
+//   };
+// };
+
+// const adapter: SimpleAdapter = {
+//   adapter: {
+//     [CHAIN.CARBON]: {
+//       fetch,
+//       start: async () => START_TIME,
+//     },
+//   },
+// };
+
+// export default adapter;
+
+const getUniqStartOfTodayTimestamp = (date = new Date()) => {
+  var date_utc = Date.UTC(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds()
+  );
+  var startOfDay = new Date(date_utc);
+  var timestamp = startOfDay.getTime() / 1000;
+  return Math.floor(timestamp / 86400) * 86400;
 };
-
-const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.CARBON]: {
-      fetch,
-      start: async () => START_TIME,
-    },
-  },
-};
-
-export default adapter;
+const exactDateTimestamp = getUniqStartOfTodayTimestamp(new Date(1691107200 * 1000))
+console.log(exactDateTimestamp)
