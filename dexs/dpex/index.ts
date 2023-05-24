d920f90f87a9f86abb46d9824ccf4b51d6097291
iulianivg <ivighenie@yahoo.com>
2023-05-24 12:41:40 +0100

    add dpex

---
+++ dexs/dpex/index.ts
@@ -0,0 +1,70 @@
+import request, { gql } from "graphql-request";
+import { Fetch, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+
+const endpoints: { [key: string]: string } = {
+  [CHAIN.POLYGON]: "https://api.thegraph.com/subgraphs/name/dpex-io/core",
+}
+
+const historicalDataSwap = gql`
+  query get_volume($period: String!, $id: String!) {
+    volumeStats(where: {period: $period, id: $id}) {
+        swap
+      }
+  }
+`
+
+interface IGraphResponse {
+  volumeStats: Array<{
+    burn: string,
+    liquidation: string,
+    margin: string,
+    mint: string,
+    swap: string,
+  }>
+}
+
+const getFetch = (query: string)=> (chain: string): Fetch => async (timestamp: number) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date((timestamp * 1000)))
+  const dailyData: IGraphResponse = await request(endpoints[chain], query, {
+    id: String(dayTimestamp),
+    period: 'daily',
+  })
+  const totalData: IGraphResponse = await request(endpoints[chain], query, {
+    id: 'total',
+    period: 'total',
+  })
+
+  return {
+    timestamp: dayTimestamp,
+    dailyVolume:
+      dailyData.volumeStats.length == 1
+        ? String(Number(Object.values(dailyData.volumeStats[0]).reduce((sum, element) => String(Number(sum) + Number(element)))) * 10 ** -30)
+        : undefined,
+    totalVolume:
+      totalData.volumeStats.length == 1
+        ? String(Number(Object.values(totalData.volumeStats[0]).reduce((sum, element) => String(Number(sum) + Number(element)))) * 10 ** -30)
+        : undefined,
+
+  }
+}
+
+const getStartTimestamp = async (chain: string) => {
+  const startTimestamps: { [chain: string]: number } = {
+    [CHAIN.POLYGON]: 1667520000,
+  }
+  return startTimestamps[chain]
+}
+
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.POLYGON]: {
+      fetch: getFetch(historicalDataSwap)(CHAIN.POLYGON),
+      start: async () => getStartTimestamp(CHAIN.POLYGON),
+    },
+  },
+};
+
+export default adapter;
