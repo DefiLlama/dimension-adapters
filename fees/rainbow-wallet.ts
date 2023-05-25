35be300bcdf0a583034aa57045b9721460ddb652
0xgnek <0xgnek@gmail.com>
2023-05-25 12:58:13 +0000

    fix switch use fli

--- fees/rainbow-wallet.ts
+++ fees/rainbow-wallet.ts
@@ -1,65 +1,65 @@
+import { Chain } from "@defillama/sdk/build/general";
 import { Adapter, FetchResultFees } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getTimestampAtStartOfDayUTC } from "../utils/date";
+import { getBlock } from "../helpers/getBlock";
 import { getPrices } from "../utils/prices";
-import postgres from "postgres";
+import { queryFlipside } from "../helpers/flipsidecrypto";
 
 interface IFee {
   contract_address: string;
   volume: number;
 }
 
-const fetch = () => {
+const fetch = (chain: Chain) => {
   return async (timestamp: number): Promise<FetchResultFees> => {
-      const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
-      const sql = postgres(process.env.INDEXA_DB!);
+
+    const fromTimestamp = timestamp - 60 * 60 * 24
+    const toTimestamp = timestamp
       try {
 
-      const now = new Date(timestamp * 1e3)
-      const dayAgo = new Date(now.getTime() - 1000 * 60 * 60 * 24)
+        const startblock = (await getBlock(fromTimestamp, chain, {}));
+        const endblock = (await getBlock(toTimestamp, chain, {}));
 
 
-      const logs = await sql`
-      SELECT
-        encode(transaction_hash, 'hex') AS HASH,
-        encode(data, 'hex') AS data,
-        encode(contract_address, 'hex') AS contract_address
-      FROM
-        ethereum.event_logs
-      WHERE
-        topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-        and topic_2 = '\\x00000000000000000000000000000000009726632680fb29d3f7a9734e3010e2'
-        AND block_time BETWEEN ${dayAgo.toISOString()} AND ${now.toISOString()};
+      const query =`
+        SELECT
+          data,
+          contract_address
+        from
+          ${chain}.core.fact_event_logs
+        WHERE
+          topics[0] = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+          and topics[2] = '0x00000000000000000000000000000000009726632680fb29d3f7a9734e3010e2'
+          and BLOCK_NUMBER > ${startblock} AND BLOCK_NUMBER < ${endblock}
       `;
 
 
-      const log = logs.map((p: any) => {
-          const volume =  Number('0x'+p.data)
+      const logs: [string, string][] = (await queryFlipside(query))
+      const log = logs.map(([data, contract_address]: [string, string]) => {
+          const volume =  Number(data)
         return {
           volume: volume,
-          contract_address: `0x${p.contract_address}`,
+          contract_address: contract_address,
         } as IFee
       });
-      const tokens = [...new Set(log.map((e: IFee) => `ethereum:${e.contract_address}`))]
-      const prices = await getPrices(tokens, todaysTimestamp);
+      const tokens = [...new Set(log.map((e: IFee) => `${chain}:${e.contract_address}`.toLowerCase()))]
+      const prices = await getPrices(tokens, timestamp);
       const amounts = log.map((p: IFee) => {
-        const price = prices[`ethereum:${p.contract_address}`.toLowerCase()]?.price || 0;
-        const decimals = prices[`ethereum:${p.contract_address}`.toLowerCase()]?.decimals || 0;
+        const price = prices[`${chain}:${p.contract_address}`.toLowerCase()]?.price || 0;
+        const decimals = prices[`${chain}:${p.contract_address}`.toLowerCase()]?.decimals || 0;
         return (p.volume / 10 ** decimals) * price;
       })
       const volume = amounts.reduce((a: number, b: number) => a+b, 0);
       const dailyFees = volume * .0085;
 
-      await sql.end({ timeout: 3 })
       return {
-        timestamp: todaysTimestamp,
+        timestamp: timestamp,
         dailyFees: dailyFees.toString(),
         dailyRevenue: dailyFees.toString(),
         dailyProtocolRevenue: dailyFees.toString(),
       } as FetchResultFees
 
       } catch (error) {
-        await sql.end({ timeout: 3 })
         throw error
       }
   }
@@ -73,12 +73,19 @@ const methodology = {
 const adapter: Adapter = {
   adapter: {
     [CHAIN.ETHEREUM]: {
-        fetch: fetch(),
+        fetch: fetch(CHAIN.ETHEREUM),
         start: async ()  => 1669852800,
         meta: {
           methodology
         }
     },
+    [CHAIN.OPTIMISM]: {
+      fetch: fetch(CHAIN.OPTIMISM),
+      start: async ()  => 1669852800,
+      meta: {
+        methodology
+      }
+  },
   },
 
 }
