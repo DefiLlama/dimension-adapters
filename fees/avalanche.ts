904dd431f24060273d8934c45e6799f701760074
0xgnek <0xgnek@gmail.com>
2023-05-30 17:59:27 +0000

    fix fees avax

--- fees/avalanche.ts
+++ fees/avalanche.ts
@@ -7,7 +7,8 @@ import { getBlock } from "../helpers/getBlock";
 
 
 interface IFees {
-  txfee: number;
+  timestamp: number;
+  value: number;
 }
 
 interface IResult {
@@ -33,17 +34,15 @@ const adapter: Adapter = {
         fetch:  async (timestamp: number) => {
             const ts = getTimestampAtStartOfDayUTC(timestamp)
             const endDatets = getTimestampAtStartOfNextDayUTC(timestamp)
-            const today = new Date(ts * 1000).toISOString();
-            const endDate = new Date(endDatets * 1000).toISOString();
             const [chainBlockToday, chainBlockNextday] = await Promise.all([getBlock(ts, "avax", {}), getBlock(endDatets, "avax", {})]);
             const [balanceToday, balanceNextday] = await Promise.all([getBalance(`0x${chainBlockToday.toString(16)}`), getBalance(`0x${chainBlockNextday.toString(16)}`)]);
-
-            const url = `https://explorerapi.avax.network/v2/txfeeAggregates?startTime=${today}&endTime=${endDate}&chainID=2q9e4r6Mu3U68nU1fYjgbR6JvwrRx36CohpAX5UQxse55x1Q5`;
-            const dailyFees: IFees = (await axios.get(url)).data.aggregates;
-            const avaxChainFee = dailyFees.txfee/1e9;
+            // const a = `https://metrics.avax.network/v1/gas_used/mainnet?from=${ts}&to=${endDatets}&interval=day`
+            // const data: any[] = (await axios.get(a)).data.results
+            // const dailyFees: IFees = data.find(e => e.timestamp === ts)
+            // const avaxChainFee = dailyFees.value/1e9;
             const txFees = balanceNextday - balanceToday;
             const pricesObj = await getPrices(["coingecko:avalanche-2"], ts);
-            const dailyFee = ((avaxChainFee  + txFees) * pricesObj["coingecko:avalanche-2"].price);
+            const dailyFee = ((txFees) * pricesObj["coingecko:avalanche-2"].price);
 
             return {
                 timestamp,
