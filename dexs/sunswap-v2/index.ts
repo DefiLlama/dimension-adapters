a2e6c2ef03b8a13730e0ee315c89297101e9cea5
0xgnek <0xgnek@gmail.com>
2023-05-31 12:32:58 +0000

    add sunswap v2

---
+++ dexs/sunswap-v2/index.ts
@@ -0,0 +1,42 @@
+import fetchURL from "../../utils/fetchURL"
+import { Chain } from "@defillama/sdk/build/general";
+import { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import customBackfill from "../../helpers/customBackfill";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+
+const historicalVolumeEndpoint = "https://pabc.endjgfsv.link/swapv2/scan/getAllLiquidityVolume"
+
+interface IVolumeall {
+  volume: string;
+  time: number;
+}
+
+const fetch = async (timestamp: number) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+  const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint))?.data.data;
+  const totalVolume = historicalVolume
+    .filter(volItem => volItem.time <= dayTimestamp)
+    .reduce((acc, { volume }) => acc + Number(volume), 0)
+
+  const dailyVolume = historicalVolume
+    .find(dayItem =>dayItem.time === dayTimestamp)?.volume
+
+  return {
+    totalVolume: `${totalVolume}`,
+    dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
+    timestamp: dayTimestamp,
+  };
+};
+
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.TRON]: {
+      fetch,
+      start: async () => 1639440000,
+    },
+  },
+};
+
+export default adapter;
