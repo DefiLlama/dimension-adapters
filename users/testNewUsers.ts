ab9961ab3af199f2807ec1de6e299d665a44b8c6
0xngmi <0xngmi@protonmail.com>
2023-06-03 02:03:00 +0100

    new test script

---
+++ users/testNewUsers.ts
@@ -0,0 +1,23 @@
+require("dotenv").config();
+import { humanizeNumber } from "@defillama/sdk"
+
+async function main(){
+    const file = process.argv[2]
+    const adapterName = process.argv[3]
+    const adapter = await import(`../${file}`)
+    await Promise.all((adapter.default as {name:string, getNewUsers:any}[]).filter(({name, getNewUsers})=>getNewUsers && (adapterName === undefined || adapterName.toLowerCase() === name.toLowerCase()))
+    .map(async ({name, getNewUsers})=>{
+        try{
+            const end = Math.floor(Date.now()/1e3)
+            const start = end - 24*3600
+            const users = await getNewUsers(start, end)
+            console.log(name, humanizeNumber(users))
+        } catch(e){
+            console.log(`Adapter for ${name} failed with error`, e)
+        }
+    }))
+}
+main()
+
+// eg: npx ts-node users/testNewUsers.ts users/chains.ts
+// or npx ts-node users/testNewUsers.ts users/chains.ts ethereum
