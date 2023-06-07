ee5bc6923df22e7030d42f551f6334b3ea434cca
0xgnek <0xgnek@gmail.com>
2023-06-07 12:34:23 +0000

    add get volult

--- fees/y2k-finance.ts
+++ fees/y2k-finance.ts
@@ -15,6 +15,43 @@ const address: TAddress = {
   [CHAIN.ARBITRUM]: '0x225acf1d32f0928a96e49e6110aba1fdf777c85f',
 }
 
+const vault_factory = '0x984e0eb8fb687afa53fc8b33e12e04967560e092';
+
+const abis: any = {
+  getVaults: {
+    "inputs": [
+        {
+            "internalType": "uint256",
+            "name": "index",
+            "type": "uint256"
+        }
+    ],
+    "name": "getVaults",
+    "outputs": [
+        {
+            "internalType": "address[]",
+            "name": "vaults",
+            "type": "address[]"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  marketIndex: {
+    "inputs": [],
+    "name": "marketIndex",
+    "outputs": [
+        {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+}
+
 interface ITx {
   data: string;
   transactionHash: string;
@@ -37,6 +74,25 @@ const fetch = (chain: Chain) => {
       keys: [],
       chain: chain
     })).output.map((e: any) => { return { data: e.data.replace('0x', ''), transactionHash: e.transactionHash } as ITx});
+
+    const poolLength = (await sdk.api.abi.call({
+      target: vault_factory,
+      chain: chain,
+      abi: abis.marketIndex,
+    })).output;
+
+    const vaultRes = (await sdk.api.abi.multiCall({
+      abi: abis.getVaults,
+      calls: Array.from(Array(Number(poolLength)).keys()).map((i) => ({
+        target: vault_factory,
+        params: i,
+      })),
+      chain: chain
+    }))
+
+    const vaults_address = vaultRes.output
+      .map(({ output }) => output).flat().map((e: string) => e.toLowerCase());
+
 
     const rawLogsData: number[] = logs.map((tx: ITx) => {
       const insrFinalTvl = Number('0x' + tx.data.slice(256, 320)) / 10 **  18; // 4
