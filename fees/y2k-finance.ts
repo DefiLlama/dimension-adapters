b0d49cb68ba52a0043f04517f5e5154fc60bd96c
0xgnek <0xgnek@gmail.com>
2023-06-07 13:18:04 +0000

    include withdraw fees

--- fees/y2k-finance.ts
+++ fees/y2k-finance.ts
@@ -1,10 +1,10 @@
 import { Adapter, FetchResultFees } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../utils/date";
 import * as sdk from "@defillama/sdk";
 import { getPrices } from "../utils/prices";
 import { getBlock } from "../helpers/getBlock";
 import { Chain } from "@defillama/sdk/build/general";
+import { queryFlipside } from "../helpers/flipsidecrypto";
 
 const topic0 = '0x4c48fdcd7e3cb84b81aa54aa5dd04105736ae1bc179d84611c6fa5a642e803f2';
 
@@ -60,16 +60,15 @@ interface ITx {
 
 const fetch = (chain: Chain) => {
   return async (timestamp: number): Promise<FetchResultFees> => {
-    const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
-    const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
-
-    const fromBlock = (await getBlock(todaysTimestamp, chain, {}));
-    const toBlock = (await getBlock(yesterdaysTimestamp, chain, {}));
+    const fromTimestamp = timestamp - 60 * 60 * 24
+    const toTimestamp = timestamp
+    const startblock = (await getBlock(fromTimestamp, chain, {}));
+    const endblock = (await getBlock(toTimestamp, chain, {}));
     const logs: ITx[] = (await sdk.api.util.getLogs({
       target: address[chain],
       topic: '',
-      fromBlock: fromBlock,
-      toBlock: toBlock,
+      fromBlock: startblock,
+      toBlock: endblock,
       topics: [topic0],
       keys: [],
       chain: chain
@@ -94,28 +93,46 @@ const fetch = (chain: Chain) => {
       .map(({ output }) => output).flat().map((e: string) => e.toLowerCase());
 
 
+    const query = `
+      SELECT tx_hash from arbitrum.core.fact_event_logs
+      WHERE topics[0] = '0xbbbdee62287b5bf3bee13cab60a29ad729cf38109bccbd2a986a11c99b8ca704'
+      and contract_address in (${vaults_address.map(a => `'${a.toLowerCase()}'`).join(',')})
+      and BLOCK_NUMBER > ${startblock} AND BLOCK_NUMBER < ${endblock}
+    `
+
+    const value: string[] = (await queryFlipside(query)).flat();
+    const query_tx_ether = `
+      SELECT data from arbitrum.core.fact_event_logs
+      WHERE topics[0] = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+      and topics[2] = '0x0000000000000000000000005c84cf4d91dc0acde638363ec804792bb2108258'
+      and contract_address = '0x82af49447d8a07e3bd95bd0d56f35241523fbab1'
+      and tx_hash in (${value.map(a => `'${a.toLowerCase()}'`).join(',')})
+      and BLOCK_NUMBER > ${startblock} AND BLOCK_NUMBER < ${endblock}
+    `
+    const ether_tx_value: string[] = (await queryFlipside(query_tx_ether)).flat();
+    const withdrawFeesAmount: number = ether_tx_value.map((e: string) => Number(e) / 1e18).reduce((a: number, b: number) => a + b, 0)
+
+
     const rawLogsData: number[] = logs.map((tx: ITx) => {
       const insrFinalTvl = Number('0x' + tx.data.slice(256, 320)) / 10 **  18; // 4
       const riskFinalTvl = Number('0x' + tx.data.slice(128, 192)) / 10 **  18; // 2
       const isDisaster =  Number('0x' + tx.data.slice(320, 384)); // 5
       return isDisaster === 1 ? riskFinalTvl : insrFinalTvl
     });
-    const dailyFees = rawLogsData.reduce((a: number, b: number) => a+b,0)
+    const dailyFee = rawLogsData.reduce((a: number, b: number) => a+b,0)
     const ethAddress = "ethereum:0x0000000000000000000000000000000000000000";
     const ethPrice = (await getPrices([ethAddress], timestamp))[ethAddress].price;
-    const dailyFeesUSD = (dailyFees * ethPrice) * 0.05;
+    const withdrawFeesAmountUSD = withdrawFeesAmount * ethPrice;
+    const dailyFeesUSD = (dailyFee * ethPrice) * 0.05;
+    const dailyFees = dailyFeesUSD+withdrawFeesAmountUSD;
     return {
-      dailyFees: dailyFeesUSD.toString(),
-      dailyRevenue: dailyFeesUSD.toString(),
+      dailyFees: dailyFees.toString(),
+      dailyRevenue: dailyFees.toString(),
       timestamp
     }
   }
 }
 
-const methodology = {
-  Fees: "5% of Hedge Vault deposits, 5% of Risk Vault deposits upon a depeg event",
-  Revenue: "5% of Hedge Vault deposits, 5% of Risk Vault deposits upon a depeg event",
-}
 const adapter: Adapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
