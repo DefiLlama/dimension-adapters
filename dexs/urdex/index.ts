1fa32dca0ddcd28b3e619056be646c6bbcf050fc
inti25 <inti25@upfi.network>
2023-06-08 15:24:56 +0700

    add urdex

---
+++ dexs/urdex/index.ts
@@ -0,0 +1,34 @@
+import { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import fetchURL from "../../utils/fetchURL";
+
+const volumeEndpoint = "https://api.urdex.finance/kol/getVolumeData"
+
+const fetch = async (timestamp: number) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+  const volumeData = (await fetchURL(`${volumeEndpoint}?date=${dayTimestamp}`))?.data.data;
+
+  return {
+    totalVolume: `${volumeData.total.TotalTradingVolume}`,
+    totalFees: `${volumeData.total.AccruedFees}`,
+    dailyVolume: volumeData.daily.TotalTradingVolume ? `${volumeData.daily.TotalTradingVolume}` : undefined,
+    dailyFees: volumeData.daily.AccruedFees ? `${volumeData.daily.AccruedFees}` : undefined,
+    timestamp: dayTimestamp,
+  };
+};
+
+const getStartTimestamp = async () => {
+  return 1686009600
+}
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.ARBITRUM]: {
+      fetch,
+      start: getStartTimestamp
+    },
+  },
+};
+
+export default adapter;
