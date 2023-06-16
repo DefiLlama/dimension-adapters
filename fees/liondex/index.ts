6a677d205ebed3479aa6cf19d1d0d1ac581c95b8
LionDEX <131145154+LionDEXDev@users.noreply.github.com>
2023-06-16 05:32:57 +0000

    add liondex fees

---
+++ fees/liondex/index.ts
@@ -0,0 +1,75 @@
+import { Adapter } from "../../adapters/types";
+import { ARBITRUM } from "../../helpers/chains";
+import { request, gql } from "graphql-request";
+import type { ChainEndpoints } from "../../adapters/types";
+import { Chain } from "@defillama/sdk/build/general";
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
+import * as sdk from "@defillama/sdk";
+
+const endpoints = {
+  [ARBITRUM]:
+    "https://api.thegraph.com/subgraphs/name/liondextrade/finance",
+};
+
+const methodology = {
+  Fees: "Daily fees collected from user trading fees",
+};
+
+const VAULT = "0x8eF99304eb88Af9BDe85d58a35339Cb0e2a557B6";
+
+const abis = {
+  "getLPPrice": {
+    "inputs": [],
+    "name": "getLPPrice",
+    "outputs": [
+        {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+}
+
+async function lpPrice() { 
+  return (await sdk.api.abi.call({ target: VAULT, abi: abis['getLPPrice'], chain: 'arbitrum' })).output; 
+}
+
+const graphs = (graphUrls: ChainEndpoints) => {
+  return (chain: Chain) => {
+    return async (timestamp: number) => {
+      const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+
+      const graphQuery = gql`
+        {
+          dailyGlobalInfo(id: "global-fee-${todaysTimestamp}" ) {
+            fees
+          }
+        }
+      `;
+      const graphRes = await request(graphUrls[chain], graphQuery);
+      const fees = graphRes.dailyGlobalInfo.fees * (await lpPrice()) / 1e18;
+
+      return {
+        timestamp,
+        dailyFees: fees.toString()
+      };
+    };
+  };
+};
+
+const adapter: Adapter = {
+  adapter: {
+    [ARBITRUM]: {
+      fetch: graphs(endpoints)(ARBITRUM),
+      start: async () => 1686614400,
+      meta: {
+        methodology,
+      },
+    },
+  },
+};
+
+export default adapter;
