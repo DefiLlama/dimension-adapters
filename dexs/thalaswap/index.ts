798f4a6ffae0047d77b5688b000be7bce2238bad
0xgnek <0xgnek@gmail.com>
2023-06-19 10:48:14 +0000

    fix query daily timestamp

--- dexs/thalaswap/index.ts
+++ dexs/thalaswap/index.ts
@@ -1,21 +1,24 @@
 import fetchURL from "../../utils/fetchURL";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 const thalaDappURL = 'https://app.thala.fi';
 const baseQueryURL = `${thalaDappURL}/api/trading-volume-chart?timeframe=`;
 // if we include a startTimestamp, then pass this in, else don't
-const volumeEndpoint = (startTimestamp: number, timeframe: string) => 
+const volumeEndpoint = (startTimestamp: number, timeframe: string) =>
     startTimestamp ? baseQueryURL + timeframe + `&startTimestamp=${startTimestamp}` : baseQueryURL + timeframe;
 
+const historicalEndpoint = "https://app.thala.fi/api/trading-volume-chart?startTimestamp=1680480000";
 interface IVolumeall {
   value: number;
   timestamp: string;
 }
 
 const fetch = async (timestamp: number) => {
-    const dayQuery = (await fetchURL(volumeEndpoint(timestamp, "1D")))?.data.data;
-    const dailyVolume = dayQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+    const dayQuery = (await fetchURL(historicalEndpoint))?.data.data;
+    const dailyVolume = dayQuery.find((e: IVolumeall) => Number(e.timestamp) === dayTimestamp).value
 
     const totalQuery = (await fetchURL(volumeEndpoint(0, "ALL")))?.data.data;
     const totalVolume = totalQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
@@ -23,7 +26,7 @@ const fetch = async (timestamp: number) => {
   return {
     totalVolume: `${totalVolume}`,
     dailyVolume: `${dailyVolume}`,
-    timestamp,
+    timestamp: dayTimestamp,
   };
 };
 
@@ -32,7 +35,7 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.APTOS]: {
       fetch,
-      start: async () => 1680652406 
+      start: async () => 1680480000
     },
   },
 };
