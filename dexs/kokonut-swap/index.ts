def4ee4823c8ebda7526db01f3bfbc255de8be9e
dev0kokoa <dev0@kokoa.finance>
2023-06-21 17:21:13 +0900

    feat: add zkevm 24 volume

--- dexs/kokonut-swap/index.ts
+++ dexs/kokonut-swap/index.ts
@@ -18,12 +18,27 @@ const fetch = async (timestamp: number) => {
   };
 };
 
+const fetchZKEVM = async (timestamp: number) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+  const volume24hrOnlySwap = (await fetchURL('https://prod.kokonut-api.com/zkevm-24hr-volume'))?.data;
+  console.log(volume24hrOnlySwap);
+  return {
+    dailyVolume: volume24hrOnlySwap,
+    timestamp: dayTimestamp,
+  };
+};
+
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.KLAYTN]: {
       fetch,
       start: async () => 1672358400,
+      runAtCurrTime: true
+    },
+    [CHAIN.POLYGON_ZKEVM]: {
+      fetch: fetchZKEVM,
+      start: async () => 1687335466,
       runAtCurrTime: true
     },
   },
