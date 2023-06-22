017c58789988f0209e1fd7e299817db40e5c009e
0xgnek <0xgnek@gmail.com>
2023-06-22 15:50:19 +0000

    fix total data

--- fees/azuro/index.ts
+++ fees/azuro/index.ts
@@ -1,23 +1,33 @@
-import {Adapter, ChainEndpoints} from "../../adapters/types";
+import {Adapter, ChainEndpoints, FetchResultFees} from "../../adapters/types";
 import {CHAIN} from "../../helpers/chains";
 import {Bet, BetResult} from "./types";
 import {Chain} from "@defillama/sdk/build/general";
 import {request, gql} from "graphql-request";
 import {getTimestampAtStartOfDayUTC} from "../../utils/date";
+import { type } from "os";
 
 const endpoints = {
     [CHAIN.POLYGON]: "https://thegraph.azuro.org/subgraphs/name/azuro-protocol/azuro-api-polygon-v2",
     [CHAIN.XDAI]: "https://thegraph.azuro.org/subgraphs/name/azuro-protocol/azuro-api-gnosis-v2",
     [CHAIN.ARBITRUM]: "https://thegraph.azuro.org/subgraphs/name/azuro-protocol/azuro-api-arbitrum-one-v2"
 }
+type IStart = {
+    [s: string | Chain]: number
+}
+const getStartTimestamp: IStart = {
+    [CHAIN.POLYGON]: 1675209600,
+    [CHAIN.XDAI]: 1654646400,
+    [CHAIN.ARBITRUM]: 1686009600
+}
 
 const graphs = (graphUrls: ChainEndpoints) => {
     return (chain: Chain) => {
-        return async (timestamp: number) => {
+        return async (timestamp: number): Promise<FetchResultFees> => {
             const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
             const fromTimestamp = todaysTimestamp - 60 * 60 * 24
             const toTimestamp = todaysTimestamp
             const bets: Bet[] = []
+            const total_bets: Bet[] = []
             let skip = 0
 
             while (true) {
@@ -47,11 +57,42 @@ const graphs = (graphUrls: ChainEndpoints) => {
                 if (graphRes.bets.length < 1000) break
             }
 
-            const totalBetsAmount = bets.reduce((e: number, {amount}) => e+Number(amount), 0)
-            const wonAmount = bets.filter(({result}) => result === BetResult.Won)
+            let skip_total = 0
+            while (true) {
+                const graphQuery = gql`
+                    {
+                        bets(
+                            where: {
+                            status: Resolved,
+                            _isFreebet: false
+                            resolvedBlockTimestamp_gte: ${getStartTimestamp[chain]},
+                            resolvedBlockTimestamp_lte: ${toTimestamp},
+                            }
+                            first: 1000,
+                            skip: ${skip_total}
+                        ) {
+                            amount
+                            odds
+                            result
+                        }
+                    }
+                    `;
+                const graphRes = await request(graphUrls[chain], graphQuery);
+
+                total_bets.push(...graphRes.bets)
+                skip_total += 1000
+                if (graphRes.bets.length < 1000) break
+            }
+            const totalBetAmount = total_bets.reduce((e: number, {amount}) => e+Number(amount), 0);
+            const totalWonAmount = total_bets.filter(({result}) => result === BetResult.Won)
+                .reduce((e: number, {amount, odds}) => e+Number(amount) * Number(odds), 0);
+            const totalFees = totalBetAmount - totalWonAmount;
+
+            const dailyBetAmount = bets.reduce((e: number, {amount}) => e+Number(amount), 0)
+            const dailyWonAmount = bets.filter(({result}) => result === BetResult.Won)
                                  .reduce((e: number, {amount, odds}) => e+Number(amount) * Number(odds), 0)
 
-            const totalPoolProfit = totalBetsAmount - wonAmount;
+            const totalPoolProfit = dailyBetAmount - dailyWonAmount;
             const dailyFees = totalPoolProfit;
             const dailyRevenue = totalPoolProfit;
 
@@ -59,6 +100,8 @@ const graphs = (graphUrls: ChainEndpoints) => {
                 timestamp,
                 dailyFees: dailyFees.toString(),
                 dailyRevenue: dailyRevenue.toString(),
+                totalFees: totalFees.toString(),
+                totalRevenue: totalFees.toString(),
             };
         }
     }
