9a6284f9517293d3421f419a1e15fc09a1f71ac9
0xgnek <0xgnek@gmail.com>
2023-06-27 13:03:23 +0000

    add litx

---
+++ dexs/litx/index.ts
@@ -0,0 +1,14 @@
+import { CHAIN } from "../../helpers/chains";
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+
+const adapters = univ2Adapter({
+  [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/iliaazhel/litx-v1-1"
+}, {
+  factoriesName: "factories",
+  dayData: "algebraDayData",
+  dailyVolume: "volumeUSD",
+  totalVolume: "totalVolumeUSD",
+});
+
+adapters.adapter.bsc.start = async () => 1687305600;
+export default adapters;
