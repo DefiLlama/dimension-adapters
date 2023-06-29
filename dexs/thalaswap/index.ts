cf22c47848860caed804e39f568bbd16b72d1ef4
Lawson Graham <lawson.graham8@gmail.com>
2023-06-28 23:34:34 -0700

    added fees and revenue

--- dexs/thalaswap/index.ts
+++ dexs/thalaswap/index.ts
@@ -2,11 +2,17 @@ import fetchURL from "../../utils/fetchURL";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const thalaDappURL = 'https://app.thala.fi';
-const baseQueryURL = `${thalaDappURL}/api/trading-volume-chart?timeframe=`;
+const thalaDappURL = 'https://app.thala.fi/';
+const volumeQueryURL = `${thalaDappURL}/api/trading-volume-chart?timeframe=`;
+const feesQueryURL = `${thalaDappURL}/api/trading-fee-chart?timeframe=`;
+const protocolRatioQueryURL = `${thalaDappURL}/api/protocol-revenue-ratio`;
+
 // if we include a startTimestamp, then pass this in, else don't
 const volumeEndpoint = (startTimestamp: number, timeframe: string) => 
-    startTimestamp ? baseQueryURL + timeframe + `&startTimestamp=${startTimestamp}` : baseQueryURL + timeframe;
+    startTimestamp ? volumeQueryURL + timeframe + `&startTimestamp=${startTimestamp}` : volumeQueryURL + timeframe;
+
+const feesEndpoint = (startTimestamp: number, timeframe: string) => 
+startTimestamp ? feesQueryURL + timeframe + `&startTimestamp=${startTimestamp}` : feesQueryURL + timeframe;
 
 interface IVolumeall {
   value: number;
@@ -14,15 +20,29 @@ interface IVolumeall {
 }
 
 const fetch = async (timestamp: number) => {
-    const dayQuery = (await fetchURL(volumeEndpoint(timestamp, "1D")))?.data.data;
-    const dailyVolume = dayQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+    const dayVolumeQuery = (await fetchURL(volumeEndpoint(timestamp, "1D")))?.data.data;
+    const dailyVolume = dayVolumeQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
 
-    const totalQuery = (await fetchURL(volumeEndpoint(0, "ALL")))?.data.data;
-    const totalVolume = totalQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+    const totalVolumeQuery = (await fetchURL(volumeEndpoint(0, "ALL")))?.data.data;
+    const totalVolume = totalVolumeQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+
+    const dayFeesQuery = (await fetchURL(feesEndpoint(timestamp, "1D")))?.data.data;
+    const dailyFees = dayFeesQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+
+    const totalFeesQuery = (await fetchURL(feesEndpoint(0, "ALL")))?.data.data;
+    const totalFees = totalFeesQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+    
+    const protocolFeeRatio = (await fetchURL(protocolRatioQueryURL))?.data.data;
+    const dailyProtocolRevenue = dailyFees * protocolFeeRatio;
+    const totalProtocolRevenue = totalFees * protocolFeeRatio;
 
   return {
     totalVolume: `${totalVolume}`,
     dailyVolume: `${dailyVolume}`,
+    totalFees: `${totalFees}`,
+    dailyFees: `${dailyFees}`,
+    totalProtocolRevenue: `${totalProtocolRevenue}`,
+    dailyProtocolRevenue: `${dailyProtocolRevenue}`,
     timestamp,
   };
 };
