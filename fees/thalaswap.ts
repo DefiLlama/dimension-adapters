92bdbf53beebdaed2327e99cd6e5185afc49f196
0xgnek <0xgnek@gmail.com>
2023-06-30 05:24:22 +0000

    fix backfill data

--- fees/thalaswap.ts
+++ fees/thalaswap.ts
@@ -11,6 +11,7 @@ const protocolRatioQueryURL = `${thalaDappURL}/api/protocol-revenue-ratio`;
 const feesEndpoint = (startTimestamp: number, timeframe: string) =>
 startTimestamp ? feesQueryURL + timeframe + `&startTimestamp=${startTimestamp}` : feesQueryURL + timeframe;
 
+const historicalEndpoint = "https://app.thala.fi/api/trading-fee-chart";
 interface IVolumeall {
   value: number;
   timestamp: string;
@@ -18,8 +19,8 @@ interface IVolumeall {
 
 const fetch = async (timestamp: number) => {
     const dayTime = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-    const dayFeesQuery = (await fetchURL(feesEndpoint(dayTime, "1D")))?.data.data;
-    const dailyFees = dayFeesQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+    const dayFeesQuery = (await fetchURL(historicalEndpoint))?.data.data;
+    const dailyFees = dayFeesQuery.find((a:IVolumeall) => Number(a.timestamp) === dayTime)?.value;
 
     const totalFeesQuery = (await fetchURL(feesEndpoint(0, "ALL")))?.data.data;
     const totalFees = totalFeesQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
