725637d281299976d12df4dbc1c8fef0987f11b1
0xngmi <0xngmi@protonmail.com>
2023-07-03 23:10:59 +0100

    fix

--- helpers/flipsidecrypto.ts
+++ helpers/flipsidecrypto.ts
@@ -40,13 +40,18 @@ export async function queryFlipside(sqlQuery: string) {
         } catch(e:any){
           if(e.response.statusText === 'Payment Required'){
             if(API_KEY_INDEX < (FLIPSIDE_API_KEYS.length-1)){
-              API_KEY_INDEX++;
+              const nextIndex = FLIPSIDE_API_KEYS.findIndex(k=>k===FLIPSIDE_API_KEY) + 1
+              if(API_KEY_INDEX < nextIndex){
+                API_KEY_INDEX = nextIndex;
+              }
               throw "Increasing API_KEY_INDEX";
             } else {
-              bail(new Error(`Payment Required`))
+              const error = new Error(`Payment Required`)
+              bail(error)
+              throw error
             }
           }
-          console.log("make query flipside", e)
+          console.log("make query flipside", e.response)
           throw e
         }
       }
