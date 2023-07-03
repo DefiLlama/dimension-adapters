1851950e336e35b5967f2f40826bae67014575ea
0xgnek <0xgnek@gmail.com>
2023-07-03 17:47:18 +0000

    set 0 wen data not found

--- dexs/equity-spot/index.ts
+++ dexs/equity-spot/index.ts
@@ -41,7 +41,7 @@ const getFetch = (chain: string): Fetch => async (timestamp: number) => {
     dailyVolume:
       dailyData.volumeStats.length == 1
         ? String(Number(Object.values(dailyData.volumeStats[0]).reduce((sum, element) => String(Number(sum) + Number(element)))) * 10 ** -30)
-        : undefined,
+        : '0',
     totalVolume:
       totalData.volumeStats.length == 1
         ? String(Number(Object.values(totalData.volumeStats[0]).reduce((sum, element) => String(Number(sum) + Number(element)))) * 10 ** -30)
