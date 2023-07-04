8a6c286f0e81ec084e02781f40572a7bf1e79d60
not found <notfound@gmail.com>
2023-07-04 09:39:17 +0700

    volume caculate

---
+++ dexs/zkSwap_Finance/index.ts
@@ -0,0 +1,18 @@
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { CHAIN } from "../../helpers/chains";
+
+const endpoints = {
+  [CHAIN.ERA]: "https://api.studio.thegraph.com/query/12332/muteswitch---zksync-era/v0.0.5"
+};
+
+const adapter = univ2Adapter(endpoints, {
+  factoriesName: "muteSwitchFactories",
+  dayData: "muteSwitchDayData",
+  dailyVolume: "dailyVolumeUSD",
+  totalVolume: "totalVolumeUSD",
+  dailyVolumeTimestampField: "date",
+});
+
+adapter.adapter.era.start = async () => 1679529600
+
+export default adapter
