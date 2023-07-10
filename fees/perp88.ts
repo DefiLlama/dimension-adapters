57257a47f3cff1510c0050e6ecb938211c63c410
shutsuwei <shu.tsu.wei88@gmail.com>
2023-07-10 19:07:13 +0700

    feat: add arbitrum fees

--- fees/perp88.ts
+++ fees/perp88.ts
@@ -1,5 +1,5 @@
 import { Adapter } from "../adapters/types";
-import { CHAIN, POLYGON } from "../helpers/chains";
+import { ARBITRUM, CHAIN, POLYGON } from "../helpers/chains";
 import { request, gql } from "graphql-request";
 import type { ChainEndpoints } from "../adapters/types";
 import { Chain } from "@defillama/sdk/build/general";
@@ -8,44 +8,78 @@ import { getBlock } from "../helpers/getBlock";
 
 const endpoints = {
   [POLYGON]: "https://api.thegraph.com/subgraphs/name/perp88/plp-pool",
+  [ARBITRUM]: "https://subgraph.satsuma-prod.com/6350b8b3ceb3/92d146b1e22261b5990c85a8b277ed8804ce4906c5e095f5311b4e4ce8ce4bf8/arbitrum-one-stats/version/v.0.0.4/api",
 };
 
 const graphs = (graphUrls: ChainEndpoints) => {
   return (chain: Chain) => {
     return async (timestamp: number) => {
-      const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
-      const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
+      if (chain === CHAIN.POLYGON) {
+        const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
+        const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
 
-      const todaysBlock = (await getBlock(todaysTimestamp, "polygon", {}));
-      const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, "polygon", {}));
-      const graphQuery = gql`
-        {
-          statistic(id: 0, block: {number: ${yesterdaysBlock}}) {
-            totalFees
+        const todaysBlock = (await getBlock(todaysTimestamp, chain, {}));
+        const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, chain, {}));
+        const graphQuery = gql`
+          {
+            statistic(id: 0, block: {number: ${yesterdaysBlock}}) {
+              totalFees
+            }
           }
+        `;
+        const queryDaily = gql`
+          query fees {
+              yesterday: statistic(id: "0", block: {number: ${yesterdaysBlock}}){
+                totalFees
+              }
+              today: statistic(id: "0", block: {number: ${todaysBlock}}) {
+                totalFees
+              }
+            }
+        `
+
+        const graphRes = await request(graphUrls[chain], graphQuery);
+        const graphResDaily = await request(graphUrls[chain], queryDaily);
+        const dailyFee = (Number(graphResDaily.yesterday.totalFees) - Number(graphResDaily.today.totalFees))/1e30;
+        const totalFees = parseInt(graphRes.statistic.totalFees) / 1e30;
+
+        return {
+          timestamp,
+          dailyFees: dailyFee.toString(),
+          totalFees: totalFees.toString(),
+        };
+      } else if (chain === CHAIN.ARBITRUM) {
+        const floorDayTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+        const totalFeeQuery = gql`
+          {
+            globalFeesStat(id: "global") {
+              totalFeePaid
+            }
+          }
+        `
+        const dailyFeeQuery = gql`
+          {
+            dailyFeesStat(id: "${floorDayTimestamp}") {
+              totalFeePaid
+            }
+          }
+        `
+
+        const totalFeeResp = await request(graphUrls[chain], totalFeeQuery);
+        const dailyFeeResp = await request(graphUrls[chain], dailyFeeQuery);
+
+        return {
+          timestamp,
+          dailyFees: (Number(dailyFeeResp.dailyFeesStat.totalFeePaid) / 1e30).toString(),
+          totalFees: (Number(totalFeeResp.globalFeesStat.totalFeePaid) / 1e30).toString(),
         }
-      `;
-      const queryDaily = gql`
-        query fees {
-            yesterday: statistic(id: "0", block: {number: ${yesterdaysBlock}}){
-              totalFees
-            }
-            today: statistic(id: "0", block: {number: ${todaysBlock}}) {
-              totalFees
-            }
-          }
-      `
-
-      const graphRes = await request(graphUrls[chain], graphQuery);
-      const graphResDaily = await request(graphUrls[chain], queryDaily);
-      const dailyFee = (Number(graphResDaily.yesterday.totalFees) - Number(graphResDaily.today.totalFees))/1e30;
-      const totalFees = parseInt(graphRes.statistic.totalFees) / 1e30;
+      }
 
       return {
         timestamp,
-        dailyFees: dailyFee.toString(),
-        totalFees: totalFees.toString(),
-      };
+        dailyFees: "0",
+        totalFees: "0",
+      }
     };
   };
 };
@@ -56,6 +90,10 @@ const adapter: Adapter = {
       fetch: graphs(endpoints)(CHAIN.POLYGON),
       start: async () => 1668643200,
     },
+    [CHAIN.ARBITRUM]: {
+      fetch: graphs(endpoints)(CHAIN.ARBITRUM),
+      start: async () => 1687392000,
+    }
   },
 };
 
