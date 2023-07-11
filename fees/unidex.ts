3f93799ce0850c4a81f9830bb5d7f78c4156d2a2
0xgnek <0xgnek@gmail.com>
2023-07-11 12:54:04 +0000

    fix unidex

--- fees/unidex.ts
+++ fees/unidex.ts
@@ -8,12 +8,10 @@ type TUrl = {
   [l: string | Chain]: string;
 }
 const endpoints: TUrl = {
-  [CHAIN.OPTIMISM]: 'https://api.thegraph.com/subgraphs/name/unidex-finance/optimismleverage',
-  [CHAIN.ERA]: 'https://zksync.tempsubgraph.xyz/subgraphs/name/unidex-finance/zkssyncleverage',
-  [CHAIN.FANTOM]: 'https://api.thegraph.com/subgraphs/name/unidex-finance/unidexleverage',
-  [CHAIN.BOBA]: 'https://api.thegraph.com/subgraphs/name/unidex-finance/bobaleverage',
-  [CHAIN.METIS]: 'https://unidexcronos.xyz/subgraphs/name/unidex-finance/leverage',
-  [CHAIN.BSC]: 'https://api.thegraph.com/subgraphs/name/unidex-finance/bscleverage'
+  [CHAIN.OPTIMISM]: 'https://api.thegraph.com/subgraphs/name/unidex-finance/optimismleveragev2',
+  [CHAIN.ERA]: 'https://zksync.tempsubgraph.xyz/subgraphs/name/unidex-finance/zkssyncleveragev2',
+  [CHAIN.FANTOM]: 'https://api.thegraph.com/subgraphs/name/unidex-finance/fantomleveragev2',
+  [CHAIN.METIS]: 'https://unidexcronos.xyz/subgraphs/name/unidex-finance/leveragev2',
 }
 
 interface IDTrade {
@@ -34,9 +32,9 @@ const fetch = (chain: Chain) => {
 
     const graphRes: IDTrade[] = (await request(endpoints[chain], graphQuery)).dayDatas;
     const dailyFee = Number(graphRes[0]?.cumulativeFees || 0) / 10 ** 8;
-    const dailyHoldersRevenue = dailyFee * 0.3;
-    const dailyProtocolRevenue = dailyFee * 0.4;
-    const dailySupplySideRevenue = dailyFee * 0.6;
+    const dailyHoldersRevenue = dailyFee * 0.35;
+    const dailyProtocolRevenue = dailyFee * 0.35;
+    const dailySupplySideRevenue = dailyFee * 0.3;
     return {
       dailyFees: dailyFee.toString(),
       dailyHoldersRevenue: dailyHoldersRevenue.toString(),
@@ -52,27 +50,19 @@ const adapter: Adapter = {
   adapter: {
     [CHAIN.OPTIMISM]: {
         fetch: fetch(CHAIN.OPTIMISM),
-        start: async ()  => 1675382400,
+        start: async ()  => 1687422746,
     },
     [CHAIN.ERA]: {
       fetch: fetch(CHAIN.ERA),
-      start: async ()  => 1675382400,
+      start: async ()  => 1687422746,
     },
     [CHAIN.FANTOM]: {
       fetch: fetch(CHAIN.FANTOM),
-      start: async ()  => 1675382400,
-    },
-    [CHAIN.BOBA]: {
-      fetch: fetch(CHAIN.BOBA),
-      start: async ()  => 1675382400,
+      start: async ()  => 1687422746,
     },
     [CHAIN.METIS]: {
       fetch: fetch(CHAIN.METIS),
-      start: async ()  => 1675382400,
-    },
-    [CHAIN.BSC]: {
-      fetch: fetch(CHAIN.BSC),
-      start: async ()  => 1675382400,
+      start: async ()  => 1687898060,
     },
   }
 }
