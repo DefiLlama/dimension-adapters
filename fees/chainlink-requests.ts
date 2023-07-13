376e2d790089f5fb67db433b5403d53c37e78d5e
0xgnek <0xgnek@gmail.com>
2023-07-13 16:19:55 +0000

    fix chain link request fees

--- fees/chainlink-requests.ts
+++ fees/chainlink-requests.ts
@@ -1,19 +1,9 @@
-import { SimpleAdapter, ChainBlocks, FetchResultFees, IJSON } from "../adapters/types";
+import { SimpleAdapter, ChainBlocks, FetchResultFees } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../utils/date";
 import { getPrices } from "../utils/prices";
 import { getBlock } from "../helpers/getBlock";
 import { queryFlipside } from "../helpers/flipsidecrypto";
 import { Chain } from "@defillama/sdk/build/general";
-
-type TAddrress = {
-  [l: string | Chain]: string;
-}
-const address_v1: TAddrress = {
-  [CHAIN.ETHEREUM]: '0xf0d54349addcf704f77ae15b96510dea15cb7952',
-  [CHAIN.BSC]: '0x747973a5A2a4Ae1D3a8fDF5479f1514F65Db9C31',
-  [CHAIN.POLYGON]: '0x3d2341ADb2D31f1c5530cDC622016af293177AE0'
-}
 
 
 type IGasTokenId = {
@@ -29,42 +19,63 @@ const gasTokenId: IGasTokenId = {
   [CHAIN.OPTIMISM]: "coingecko:ethereum"
 }
 
-let chainBlocksStore: IJSON<ChainBlocks> | undefined = undefined
+interface ILog {
+  data: string;
+  transactionHash: string;
+  topics: string[];
+}
 
 const fetchRequests = (chain: Chain) => {
-  return async (timestamp: number, chainBlocks: ChainBlocks): Promise<FetchResultFees> => {
-    if (!chainBlocksStore)
-      chainBlocksStore = {
-        [timestamp]: chainBlocks
-      }
-    const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
-    const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
-
-    if (!chainBlocksStore[todaysTimestamp])
-      chainBlocksStore[todaysTimestamp] = {}
-    const fromBlock = (await getBlock(todaysTimestamp, chain, chainBlocksStore[todaysTimestamp]));
-    if (!chainBlocksStore[yesterdaysTimestamp])
-      chainBlocksStore[yesterdaysTimestamp] = {}
-    const toBlock = (await getBlock(yesterdaysTimestamp, chain, chainBlocksStore[yesterdaysTimestamp]));
+  return async (timestamp: number, _: ChainBlocks): Promise<FetchResultFees> => {
+    const fromTimestamp = timestamp - 60 * 60 * 24
+    const toTimestamp = timestamp
+    const fromBlock = (await getBlock(fromTimestamp, chain, {}));
+    const toBlock = (await getBlock(toTimestamp, chain, {}));
 
     const flipsideChain = chain === "avax" ? "avalanche" : chain
-    const linkPaid = await queryFlipside(`SELECT SUM(EVENT_INPUTS['payment']) / 1e18 as payments, COUNT(*) from ${flipsideChain}.core.fact_event_logs WHERE EVENT_NAME = 'OracleRequest'
-      AND BLOCK_NUMBER > ${fromBlock} AND BLOCK_NUMBER < ${toBlock}`)
-    const ethGas = await queryFlipside(`
-  SELECT
-    SUM(TX_FEE)
-  from
-    ${flipsideChain}.core.fact_event_logs logs
-    JOIN ${flipsideChain}.core.fact_transactions txs ON txs.tx_hash=logs.tx_hash
-  WHERE
-    logs.TOPICS[0] = '0x9e9bc7616d42c2835d05ae617e508454e63b30b934be8aa932ebc125e0e58a64'
-    AND logs.BLOCK_NUMBER > ${fromBlock} AND logs.BLOCK_NUMBER < ${toBlock}`)
+
+    const query_paid = `
+        SELECT
+          data,
+          topics,
+          tx_hash as transactionHash
+        from
+          ${flipsideChain}.core.fact_event_logs
+        WHERE
+          topics[0] = '0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65'
+          AND BLOCK_NUMBER > ${fromBlock} AND BLOCK_NUMBER < ${toBlock}`
+
+    const gas_query = `
+        SELECT
+        SUM(TX_FEE)
+        from
+        ${flipsideChain}.core.fact_event_logs logs
+        JOIN ${flipsideChain}.core.fact_transactions txs ON txs.tx_hash=logs.tx_hash
+        WHERE
+        logs.TOPICS[0] = '0x9e9bc7616d42c2835d05ae617e508454e63b30b934be8aa932ebc125e0e58a64'
+        AND logs.BLOCK_NUMBER > ${fromBlock} AND logs.BLOCK_NUMBER < ${toBlock}`
+
+    const linkPaid_logs: ILog[] = (await queryFlipside(query_paid))
+      .map(([data, topics, transactionHash]: [string, string[], string]) => {
+        return {
+          data,
+          topics,
+          transactionHash
+        } as ILog
+      })
+
+    const link_amount: number = linkPaid_logs.map((e: ILog) => {
+      const data = e.data.replace('0x', '');
+      const payments = Number('0x'+data.slice(128, 192)) / 10 ** 18;
+      return payments;
+    }).reduce((a: number, b: number) => a + b, 0);
+    const ethGas = await queryFlipside(gas_query)
 
     const linkAddress = "coingecko:chainlink";
     const gasToken = gasTokenId[chain];
     const prices = (await getPrices([linkAddress, gasToken], timestamp))
     const linkPrice = prices[linkAddress].price
-    const dailyFeesUsd = (linkPaid[0][0] ?? 0) * linkPrice;
+    const dailyFeesUsd = link_amount * linkPrice;
     const dailyGasUsd = ethGas[0][0] * prices[gasToken].price;
     return {
       dailyFees: dailyFeesUsd.toString(),
