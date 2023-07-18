cc0d0ba011d93592ef6d98bd6249de91a0ce349c
0xgnek <0xgnek@gmail.com>
2023-07-18 15:42:05 +0000

    fix start time wigoswap

--- dexs/wigoswap/index.ts
+++ dexs/wigoswap/index.ts
@@ -1,9 +1,12 @@
 import { CHAIN } from "../../helpers/chains";
 import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 
-export default univ2Adapter({
+const adapters = univ2Adapter({
   [CHAIN.FANTOM]: "https://api.thegraph.com/subgraphs/name/wigoswap/exchange2"
 }, {
   factoriesName: "wigoswapFactories",
   dayData: "wigoDayData",
 });
+
+adapters.adapter.fantom.start = async () => 1642982400;
+export default adapters;
