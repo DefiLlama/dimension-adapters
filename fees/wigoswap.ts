86ce936f552b2aaf76ed8641466cb3f68eb3c1ee
0xgnek <0xgnek@gmail.com>
2023-07-18 15:45:03 +0000

    add wigoswap

---
+++ fees/wigoswap.ts
@@ -0,0 +1,26 @@
+import { CHAIN } from "../helpers/chains";
+import { univ2DimensionAdapter } from "../helpers/getUniSubgraph";
+
+const adapters = univ2DimensionAdapter({
+  graphUrls: {
+    [CHAIN.FANTOM]: "https://api.thegraph.com/subgraphs/name/wigoswap/exchange2"
+  },
+  dailyVolume: {
+    factory: "wigoDayData"
+  },
+  totalVolume: {
+    factory: "wigoswapFactories"
+  },
+  feesPercent: {
+    type: "volume",
+    Fees: 0.19,
+    UserFees: 0.19,
+    Revenue: 0.01,
+    ProtocolRevenue: 0,
+    HoldersRevenue: 0.01,
+    SupplySideRevenue: 0.18,
+  }
+}, {
+});
+adapters.adapter.fantom.start = async () => 1642982400;
+export default adapters;
