4cc20245f034176463f972ed257d5bda6a793169
vladjito <95035799+vladjito@users.noreply.github.com>
2023-07-19 13:47:37 +0300

    Create index.ts

---
+++ dexs/concordex-io/index.ts
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import type { SimpleAdapter } from '../../adapters/types'
+
+const POOLS_SERVICE_URL = 'https://cdex-liquidity-pool.concordex.io/v1/rpc'
+
+const rpc = (url: string, method: string, params: any) =>
+  axios.post(
+    url,
+    {
+      jsonrpc: '2.0',
+      method,
+      params,
+      id: '0',
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+  )
+    .then(res => {
+      if (res.data.error) {
+        throw new Error(res.data.error.message)
+      }
+      return res.data.result
+    });
+
+
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    concordium: {
+      start: async () => 1682607600,
+      fetch: async (ts) => {
+        const data = await rpc(POOLS_SERVICE_URL, 'volumes_statistic', {
+          timestamp: ts,
+        })
+        return {
+          timestamp: ts,
+          dailyVolume: data.daily_volume,
+          totalVolume: data.total_volume,
+          dailyFees: data.daily_fees,
+          totalFees: data.total_fees,
+        }
+      }
+    }
+  }
+};
