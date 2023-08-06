e88f1adb65e5a4b0d15c824aa6b697c6fffa1e2f
0xgnek <0xgnek@gmail.com>
2023-08-06 14:49:04 +0000

    get start time

--- dexs/dydx/index.ts
+++ dexs/dydx/index.ts
@@ -1,20 +1,27 @@
 import fetchURL from "../../utils/fetchURL"
-import { SimpleAdapter } from "../../adapters/types";
+import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 const historicalVolumeEndpoint = "https://api.dydx.exchange/v3/stats?days=1"
+const candles = (market: string, fromISO: string) => `https://api.dydx.exchange/v3/candles/${market}?resolution=1DAY&fromISO=${fromISO}`
 
 interface IVolumeall {
-  quoteVolume: string;
+  usdVolume: string;
+  startedAt: string;
 }
 
-const fetch = async (timestamp: number) => {
+const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const historicalVolume: IVolumeall[] = Object.values((await fetchURL(historicalVolumeEndpoint))?.data.markets);
-  const dailyVolume = historicalVolume.reduce((a: number, b: IVolumeall) => a+Number(b.quoteVolume), 0)
+  const fromTimestamp = dayTimestamp - 60 * 60 * 24
+  const fromISO = new Date(fromTimestamp * 1000).toISOString();
+  const dateString = new Date(dayTimestamp * 1000).toISOString();
+  const markets: string[] = Object.keys((await fetchURL(historicalVolumeEndpoint))?.data.markets);
+  const historical: IVolumeall[] = (await Promise.all(markets.map((market: string) => fetchURL(candles(market, fromISO))))).map((e: any) => e.data.candles).flat()
+  const dailyolume = historical.filter((e: IVolumeall) => e.startedAt === dateString)
+    .reduce((a: number, b: IVolumeall) => a+Number(b.usdVolume), 0)
   return {
-    dailyVolume: dailyVolume && dailyVolume > 0 ? `${dailyVolume}` : undefined,
+    dailyVolume: dailyolume ? `${dailyolume}` : undefined,
     timestamp: dayTimestamp,
   };
 };
@@ -23,7 +30,6 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.ETHEREUM]: {
       fetch,
-      runAtCurrTime: true,
       start: async () => 1614211200,
     },
   },
