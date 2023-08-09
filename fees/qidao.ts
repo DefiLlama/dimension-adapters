f8d6cc5621bea5af05bd3f43d10c282b23c53e75
0xgnek <0xgnek@gmail.com>
2023-08-09 12:23:41 +0000

    add fees

--- fees/qidao.ts
+++ fees/qidao.ts
@@ -20,19 +20,19 @@ interface ILog {
 const Vault_Fee_Manager_Contracts: TAddress = {
   [CHAIN.ARBITRUM]: '0xdCC1c692110E0e53Bd57D5B2234867E9C5B98158',
   [CHAIN.POLYGON]: '0x11606d99AD8aAC49E033B14c89552F585028bA7d',
-  [CHAIN.AVAX]: '0xca3eb45fb186ed4e75b9b22a514ff1d4abadd123',
-  [CHAIN.OPTIMISM]: '0xbdef6DAD6841aA60Caf462baAee0AA912EeF817A'
+  [CHAIN.OPTIMISM]: '0xbdef6DAD6841aA60Caf462baAee0AA912EeF817A',
+  // [CHAIN.AVAX]: '0xca3eb45fb186ed4e75b9b22a514ff1d4abadd123',
 }
 
 const Performance_Fee_Management_Contracts: TAddress = {
   [CHAIN.ARBITRUM]: '0x580d0B0ed579c22635AdE9C91Bb7A1f0755F9C85',
   [CHAIN.POLYGON]: '0x232627F88a84A657b8A009AC17ffa226a34c9a87',
   [CHAIN.OPTIMISM]: '0x954aC12C339C60EAFBB32213B15af3F7c7a0dEc2',
-  [CHAIN.ETHEREUM]: '0xEd8a2759B0f8ea0f33225C86cB726fa9C6E030A4'
+  // [CHAIN.ETHEREUM]: '0xEd8a2759B0f8ea0f33225C86cB726fa9C6E030A4'
 }
 
-const event_fees_withdraw = 'event FeeWithdrawn( address token,uint256 amount)';
-const event_token_earned = 'event TokensEarned( address indexed perfToken,address indexed recipient,uint256 amount)';
+const event_fees_withdraw = 'event FeeWithdrawn(address token,uint256 amount)';
+const event_token_earned = 'event TokensEarned(address indexed perfToken,address indexed recipient,uint256 amount)';
 
 const topic0_fees_withdraw = '0x78473f3f373f7673597f4f0fa5873cb4d375fea6d4339ad6b56dbd411513cb3f';
 const topic0_token_earned = '0x7750c36249a9e8cdc4a6fb8a68035d55429ff67a4db8b110026b1375dd9d380c';
@@ -42,9 +42,14 @@ const contract_interface = new ethers.utils.Interface([
   event_token_earned
 ]);
 
+interface IWithdraw {
+  token: string;
+  amount: number;
+}
+
 
 const fetch = (chain: Chain) => {
-  return async (timestamp: number) => {
+  return async (timestamp: number): Promise<FetchResultFees> => {
     const fromTimestamp = timestamp - 60 * 60 * 24
     const toTimestamp = timestamp
   try {
@@ -60,8 +65,52 @@ const fetch = (chain: Chain) => {
         chain: chain,
         topics: [topic0_fees_withdraw]
       })).output as ILog[]
-      console.log(log_withdraw_fees)
+
+      const log_token_earned: ILog[] = (await sdk.api.util.getLogs({
+        target: Performance_Fee_Management_Contracts[chain],
+        topic: '',
+        toBlock: toBlock,
+        fromBlock: fromBlock,
+        keys: [],
+        chain: chain,
+        topics: [topic0_token_earned]
+      })).output as ILog[]
+
+      const raw_withdraw: IWithdraw[] = log_withdraw_fees.map((e: ILog) => {
+        const value = contract_interface.parseLog(e);
+        const token = value.args.token;
+        const amount = Number(value.args.amount._hex);
+        return {
+          token: token,
+          amount: amount,
+        } as IWithdraw
+      })
+
+      const raw_token_earned: IWithdraw[] = log_token_earned.map((e: ILog) => {
+        const value = contract_interface.parseLog(e);
+        const token = value.args.perfToken;
+        const amount = Number(value.args.amount._hex);
+        return {
+          token: token,
+          amount: amount,
+        } as IWithdraw
+      })
+
+      const coins = [...new Set([...raw_withdraw,...raw_token_earned].map((e: IWithdraw) => `${chain}:${e.token}`.toLowerCase()))]
+      const prices = await getPrices(coins, timestamp);
+      const dailyFeesUSD = [...raw_withdraw, ...raw_token_earned].map((e: IWithdraw) => {
+        const price = (prices[`${chain}:${e.token}`.toLowerCase()]?.price || 0);
+        const decimals = (prices[`${chain}:${e.token}`.toLowerCase()]?.decimals || 0);
+        return (Number(e.amount) / 10 ** decimals) * price;
+      }).reduce((a: number, b: number) => a+b, 0)
+      const dailyFees = dailyFeesUSD;
+      const dailyRevenue = dailyFees * .5;
+      const totalSupplySideRevenue = dailyFees * .5;
       return {
+        dailyFees: `${dailyFees}`,
+        dailyRevenue: `${dailyRevenue}`,
+        dailyHoldersRevenue: `${dailyRevenue}`,
+        dailySupplySideRevenue: `${totalSupplySideRevenue}`,
         timestamp
       }
     } catch (error) {
@@ -74,13 +123,17 @@ const fetch = (chain: Chain) => {
 
 const adapter: SimpleAdapter = {
   adapter: {
-    // [CHAIN.ARBITRUM]: {
-    //   fetch: fetch(CHAIN.ARBITRUM),
-    //   start: async () => 1682121600,
-    // },
+    [CHAIN.ARBITRUM]: {
+      fetch: fetch(CHAIN.ARBITRUM),
+      start: async () => 1691193600,
+    },
     [CHAIN.POLYGON]: {
       fetch: fetch(CHAIN.POLYGON),
-      start: async () => 1682121600,
+      start: async () => 1691193600,
+    },
+    [CHAIN.OPTIMISM]: {
+      fetch: fetch(CHAIN.OPTIMISM),
+      start: async () => 1691193600,
     },
   }
 };
