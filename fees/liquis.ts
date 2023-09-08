5c960131f7acce5b5814f92a00c329d95a627e3a
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-09-08 04:12:11 -0400

    feat: liquis now use only sdk and lit price / 2 for oLit

--- fees/liquis.ts
+++ fees/liquis.ts
@@ -1,51 +1,54 @@
+import { getBlock } from "../helpers/getBlock";
 import { Adapter, FetchResultFees } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { getTimestampAtStartOfDayUTC } from "../utils/date";
 import { getPrices } from "../utils/prices";
-import postgres from "postgres";
+import * as sdk from "@defillama/sdk";
 
-const OLIT_TOKEN = '0xba100000625a3754423978a60c9317c58a424e3D';
+const LIT = '0xfd0205066521550d7d7ab19da8f72bb004b4c341';
+const OLIT_TOKEN = '0x627fee87d0D9D2c55098A06ac805Db8F98B158Aa';
+const topic = 'event Transfer (address indexed from, address indexed to, uint256 amount)';
+const topic0 = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
+const topic1 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+const topic2 = '0x00000000000000000000000037aeB332D6E57112f1BFE36923a7ee670Ee9278b';
+
+interface ILog {
+  topics: string[];
+  data: string;
+  transactionHash: string;
+}
 
 const fetch = () => {
   return async (timestamp: number): Promise<FetchResultFees> => {
+    try {
       const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
-      const sql = postgres(process.env.INDEXA_DB!);
-      try {
+      const dayAgo = todaysTimestamp - 60 * 60 * 24;
+      const fromBlock = await getBlock(dayAgo, CHAIN.ETHEREUM, {});
+			const toBlock = await getBlock(todaysTimestamp, CHAIN.ETHEREUM, {});
 
-      const now = new Date(timestamp * 1e3)
-      const dayAgo = new Date(now.getTime() - 1000 * 60 * 60 * 24)
+      const logs: ILog[] = (await sdk.api.util.getLogs({
+        target: OLIT_TOKEN,
+        topic: topic,
+        toBlock,
+        fromBlock,
+        keys: [],
+        chain: CHAIN.ETHEREUM,
+        topics: [topic0, topic1, topic2]
+      })).output as ILog[];
 
-      const olit_transfer_logs = await sql`
-        SELECT
-          substr(encode(topic_1, 'hex'), 25) AS origin,
-          substr(encode(topic_2, 'hex'), 25) AS destination,
-          encode(transaction_hash, 'hex') AS HASH,
-          encode(data, 'hex') AS data
-        FROM
-          ethereum.event_logs
-        WHERE
-          block_number > 18025939
-          AND contract_address = '\\x627fee87d0D9D2c55098A06ac805Db8F98B158Aa'
-          AND topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-          AND topic_1 = '\\x0000000000000000000000000000000000000000000000000000000000000000'
-          AND topic_2 = '\\x00000000000000000000000037aeB332D6E57112f1BFE36923a7ee670Ee9278b'
-          AND block_time BETWEEN ${dayAgo.toISOString()} AND ${now.toISOString()};
-      `;
-
-    const olit_transfer_amounts: number[] = olit_transfer_logs.map((e:any) => {
-      return Number('0x'+e.data) / 10 ** 18;
+    const olit_transfer_amounts: number[] = logs.map((e:any) => {
+      return Number(e.data) / 10 ** 18;
     });
 
-    const olitAddress = `ethereum:${OLIT_TOKEN.toLowerCase()}`;
-    const olitPrice = (await getPrices([olitAddress], todaysTimestamp))[olitAddress].price;
+    const litAddress = `ethereum:${LIT.toLowerCase()}`;
+    const litPrice = (await getPrices([litAddress], todaysTimestamp))[litAddress].price;
 
     const olit_transfer_amount = olit_transfer_amounts.reduce((a: number, b: number) => a+b,0);
-    const dailyFee = olit_transfer_amount * olitPrice;
+    const dailyFee = olit_transfer_amount * (litPrice / 2);
     const dailySupplySideRevenue = dailyFee * .75
     const dailyRevenue =  dailyFee * .25;
     const dailyHoldersRevenue = dailyFee * .03;
 
-    await sql.end({ timeout: 3 })
     return {
       timestamp: todaysTimestamp,
       dailyFees: dailyFee.toString(),
@@ -54,7 +57,6 @@ const fetch = () => {
       dailyHoldersRevenue: dailyHoldersRevenue.toString(),
     } as FetchResultFees
     } catch (error) {
-      await sql.end({ timeout: 3 })
       throw error
     }
   }
