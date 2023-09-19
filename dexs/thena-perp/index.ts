d79fd9cd967d0382b2ae881ec29c4737c600c4d8
0xgnek <0xgnek@gmail.com>
2023-09-19 12:47:55 +0000

    add thena-perp

---
+++ dexs/thena-perp/index.ts
@@ -0,0 +1,40 @@
+
+import request from "graphql-request";
+import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+
+interface IGraphResponse {
+  id: string;
+  closeTradeVolume: string;
+  platformFee: string;
+}
+const url = 'https://api.thegraph.com/subgraphs/name/navid-fkh/symmetrical_bsc';
+const fetchVolume = async (timestamp: number): Promise<FetchResultVolume> => {
+  const fromTimestamp = timestamp - 60 * 60 * 24
+  const toTimestamp = timestamp
+  const query = `
+    {
+      dailyHistories(orderBy: timestamp, orderDirection:desc, where: {timestamp_gte: ${fromTimestamp}, timestamp_lte: ${toTimestamp}}) {
+        id
+        closeTradeVolume
+        platformFee
+      }
+    }
+  `;
+  const response: IGraphResponse[] = (await request(url, query)).dailyHistories as IGraphResponse[]
+  const value = response.reduce((acc, curr) => acc + Number(curr.closeTradeVolume)/10**18, 0);
+  return {
+    dailyVolume: `${value}`,
+    timestamp
+  }
+}
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.BSC]: {
+      fetch: fetchVolume,
+      start: async () => 1687877205
+    }
+  }
+}
+export default adapter;
