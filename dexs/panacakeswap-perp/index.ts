92348e2232158f0913a3ba77155c2ed4afcca151
0xgnek <0xgnek@gmail.com>
2023-09-20 11:55:29 +0000

    load data

--- dexs/panacakeswap-perp/index.ts
+++ dexs/panacakeswap-perp/index.ts
@@ -3,8 +3,10 @@ import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getBlock } from "../../helpers/getBlock";
 import * as sdk from "@defillama/sdk";
+import { queryDune } from "../../helpers/dune";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
-type TContractAddress = {
+type TID = {
   [key: string | Chain]: string;
 }
 type TBrokerID = {
@@ -14,9 +16,9 @@ const brokerID: TBrokerID = {
   [CHAIN.BSC]: [2],
   [CHAIN.ARBITRUM]: [1,2]
 }
-const contract_address: TContractAddress = {
-  [CHAIN.BSC]: '0x1b6f2d3844c6ae7d56ceb3c3643b9060ba28feb0',
-  [CHAIN.ARBITRUM]: '0xb3879e95a4b8e3ee570c232b19d520821f540e48'
+const contract_address: TID = {
+  [CHAIN.BSC]: '2826941',
+  [CHAIN.ARBITRUM]: '2982352'
 }
 interface ILog {
   data: string;
@@ -25,41 +27,21 @@ interface ILog {
 }
 const topic_0_open = '0xa858fcdefab65cbd1997932d8ac8aa1a9a8c46c90b20947575525d9a2a437f8c'
 
+interface IData {
+  dt: string;
+  volume: number;
+  fees: number;
+}
 
 const fetchVolume = (chain: Chain) => {
   return async (timestamp: number) => {
-    const fromTimestamp = timestamp - 86400;
-    const toTimestamp = timestamp;
     try {
+      const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+      const dateString = new Date(dayTimestamp * 1000).toISOString().split("T")[0];
+      // const query: IData[] = (await queryDune(contract_address[chain]))
+      const query: IData[] = require(`./${chain}.json`);
+      const dailyVolume = query.find((e: IData) => e.dt.split(' ')[0] === dateString)?.volume;
 
-      const fromBlock = await getBlock(fromTimestamp, chain, {});
-      const toBlock = await getBlock(toTimestamp, chain, {});
-      const logs_open: ILog[] = (await sdk.api.util.getLogs({
-        target: contract_address[chain],
-        topic: '',
-        toBlock: toBlock,
-        fromBlock: fromBlock,
-        keys: [],
-        chain: chain,
-        topics: [topic_0_open]
-      })).output as ILog[];
-
-      // calculate volume when open use entry price and qty
-      const value = logs_open.map((e: ILog) => {
-        // 14 -> qty
-        // 2 -> price
-        // 8 -> brokerID
-        const data = e.data.replace('0x', '');
-        const price = Number('0x' + data.slice(128, 192)) / 10 ** 8;
-        const qty = Number('0x' + data.slice(64 * 14, (64 * 14) + 64)) / 10 ** 10;
-        const _brokerID = Number('0x' + data.slice(64 * 8, (64 * 8) + 64));
-        if (!brokerID[chain].includes(_brokerID)) {
-          return 0;
-        }
-        return price * qty;
-      }).reduce((a: number, b: number) => a + b, 0);
-
-      const dailyVolume = value;
       return {
         dailyVolume: `${dailyVolume}`,
         timestamp,
@@ -75,11 +57,11 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.BSC]: {
       fetch: fetchVolume(CHAIN.BSC),
-      start: async () => 1688688000,
+      start: async () => 1682035200,
     },
     [CHAIN.ARBITRUM]: {
       fetch: fetchVolume(CHAIN.ARBITRUM),
-      start: async () => 1692576000,
+      start: async () => 1692662400,
     },
   },
 };
