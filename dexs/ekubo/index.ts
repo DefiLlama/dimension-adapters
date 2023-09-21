221494b61a861df5a07123f0c09ce024de814dc5
0xngmi <0xngmi@protonmail.com>
2023-09-21 20:50:27 +0100

    ekubo

--- dexs/ekubo/index.ts
+++ dexs/ekubo/index.ts
@@ -6,8 +6,8 @@ import { getPrices } from "../../utils/prices";
 const toki = (n:any) =>"starknet:0x"+BigInt(n).toString(16).padStart("049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7".length, "0")
 
 const fetch = async (timestamp: number) => {
-  const response =  ((await fetchURL("https://mainnet-api.ekubo.org/overview")).data.topPairs as any[])
-    .map(t=>([{token: toki(t.token0), vol: t.volume0_24h}, {token: toki(t.token1), vol: t.volume1_24h}])).flat()
+  const response =  ((await fetchURL("https://mainnet-api.ekubo.org/overview")).data.volumeByToken_24h as any[])
+    .map(t=>({token: toki(t.token), vol: t.volume}))
   const prices = await getPrices(response.map(t=>t.token), timestamp)
   const dailyVolume = response.map((token) => {
     const price = prices[token.token]
