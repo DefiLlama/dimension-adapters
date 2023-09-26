c046a60b070dd5e6522a3586b0bc650d21555a55
0xgnek <0xgnek@gmail.com>
2023-09-26 14:38:04 +0000

    fix start time

--- dexs/tegro/index.ts
+++ dexs/tegro/index.ts
@@ -1,92 +1,22 @@
-import { Adapter } from '../../adapters/types'
-import { gql, request } from 'graphql-request'
+import { CHAIN } from "../../helpers/chains";
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 
-const endpoints = {
-  ethereum: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro',
-  polygon: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-polygon',
-  avax: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-avax',
-  bsc: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-bnb',
-  arbitrum: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-arbitrum',
-}
+const adapters = univ2Adapter({
+  [CHAIN.ETHEREUM]: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro',
+  [CHAIN.POLYGON]: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-polygon',
+  [CHAIN.AVAX]: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-avax',
+  [CHAIN.BSC]: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-bnb',
+  [CHAIN.ARBITRUM]: 'https://api.thegraph.com/subgraphs/name/ashtegro/tegro-arbitrum',
+}, {
+  factoriesName: "totalVolumes",
+  dailyVolume: "volume",
+  totalVolume: "volume",
+  dayData: "dailyVolume",
+});
 
-const getNumberOfDay = (timestamp: number) => {
-  return Math.floor(timestamp / (24 * 60 * 60))
-}
-
-const getVolume = async (queryUrl: string, timestamp: number) => {
-  const todayDayNumber = getNumberOfDay(timestamp)
-
-  const volumeQuery = gql`
-    query TotalAndDailyVolumes {
-      dailyVolume(id: ${todayDayNumber}) {
-        volume
-      }
-      totalVolumes {
-        volume
-        id
-      }
-    }`
-  
-  const res = await request(queryUrl, volumeQuery)
-  return {
-    dailyVolume: res.dailyVolume?.volume || '0',
-    totalVolume: res.totalVolumes.reduce((acc: number, item: { volume: number; id: string }) => acc + Number(item.volume), 0)
-  }
-}
-
-const adapter: Adapter = {
-  adapter: {
-    ethereum: {
-      fetch: async (timestamp: number) => {
-        const res = await getVolume(endpoints.ethereum, timestamp)
-        return {
-          timestamp,
-          ...res,
-        }
-      },
-      start: async () => 0
-    },
-    polygon: {
-      fetch: async (timestamp: number) => {
-        const res = await getVolume(endpoints.polygon, timestamp)
-        return {
-          timestamp,
-          ...res,
-        }
-      },
-      start: async () => 0
-    },
-    avax: {
-      fetch: async (timestamp: number) => {
-        const res = await getVolume(endpoints.avax, timestamp)
-        return {
-          timestamp,
-          ...res,
-        }
-      },
-      start: async () => 0
-    },
-    bsc: {
-      fetch: async (timestamp: number) => {
-        const res = await getVolume(endpoints.bsc, timestamp)
-        return {
-          timestamp,
-          ...res,
-        }
-      },
-      start: async () => 0
-    },
-    arbitrum: {
-      fetch: async (timestamp: number) => {
-        const res = await getVolume(endpoints.arbitrum, timestamp)
-        return {
-          timestamp,
-          ...res,
-        }
-      },
-      start: async () => 0
-    },
-  },
-}
-
-export default adapter
+adapters.adapter.ethereum.start = async () => (24 * 60 * 60) * 19600;
+adapters.adapter.polygon.start = async () => (24 * 60 * 60) * 19600;
+adapters.adapter.avax.start = async () => (24 * 60 * 60) * 19619;
+adapters.adapter.bsc.start = async () => (24 * 60 * 60) * 19619;
+adapters.adapter.arbitrum.start = async () => (24 * 60 * 60) * 19619;
+export default adapters;
