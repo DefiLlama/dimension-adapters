2f46b17760d1b0ecdbc3db7510b08f8845e4ef10
Hemant Singhal <hemant@seed.im>
2023-10-11 22:44:54 +0100

    Fixing config variables and comments

--- dexs/bluefin/index.ts
+++ dexs/bluefin/index.ts
@@ -3,12 +3,12 @@ import { CHAIN } from "../../helpers/chains";
 import axios from "axios";
 import BigNumber from "bignumber.js";
 
-const url_config = {
+const URL_CONFIG = {
   "arb": "https://dapi.api.arbitrum-prod.firefly.exchange/marketData",
   "sui": "https://dapi.api.sui-prod.bluefin.io/marketData",
 }
 
-const product_config = {
+const PRODUCT_CONFIG = {
   "arb": ["ETH-PERP", "BTC-PERP"],
   "sui": ["ETH-PERP", "BTC-PERP"],
 }
@@ -39,14 +39,14 @@ const computeVolume = async (timestamp: number, baseUrl: string, productIds: str
 };
 
 const fetchSUI = async (timeStamp: number) => {
-  return await computeVolume(timeStamp, url_config.sui, product_config.sui);
+  return await computeVolume(timeStamp, URL_CONFIG.sui, PRODUCT_CONFIG.sui);
 };
 
 const fetchArb = async (timeStamp: number) => {
-  return await computeVolume(timeStamp, url_config.arb, product_config.arb);
+  return await computeVolume(timeStamp, URL_CONFIG.arb, PRODUCT_CONFIG.arb);
 };
 
-const startTime = 1695857191;
+const startTime = 1695600000; // 25th September when SUI trading starts
 
 const adapter: BreakdownAdapter = {
   breakdown: {
