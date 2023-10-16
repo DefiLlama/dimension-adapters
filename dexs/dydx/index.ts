d85e9abc95b5d6facf68ec1a2d503a367131fd3a
0xgnek <0xgnek@gmail.com>
2023-10-16 16:59:17 +0000

    fix data wrong data

--- dexs/dydx/index.ts
+++ dexs/dydx/index.ts
@@ -18,23 +18,26 @@ const url = 'https://dydx.metabaseapp.com/api/public/dashboard/5fa0ea31-27f7-4cd
 
 const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const fromTimestamp = dayTimestamp - 60 * 60 * 24
-  const fromISO = new Date(fromTimestamp * 1000).toISOString();
+  // const fromTimestamp = dayTimestamp - 60 * 60 * 24
+  // const fromISO = new Date(fromTimestamp * 1000).toISOString();
   const dateString = new Date(dayTimestamp * 1000).toISOString();
-  const markets: string[] = Object.keys((await fetchURL(historicalVolumeEndpoint))?.data.markets);
-  const historical: IVolumeall[] = (await Promise.all(markets.map((market: string) => fetchURL(candles(market, fromISO))))).map((e: any) => e.data.candles).flat()
-  const dailyolume = historical.filter((e: IVolumeall) => e.startedAt === dateString)
-    .reduce((a: number, b: IVolumeall) => a+Number(b.usdVolume), 0)
+  // const markets: string[] = Object.keys((await fetchURL(historicalVolumeEndpoint))?.data.markets);
+  // const historical: IVolumeall[] = (await Promise.all(markets.map((market: string) => fetchURL(candles(market, fromISO))))).map((e: any) => e.data.candles).flat()
+  // const dailyolume = historical.filter((e: IVolumeall) => e.startedAt === dateString)
+  //   .reduce((a: number, b: IVolumeall) => a+Number(b.usdVolume), 0)
   const timeString = dateString.split("T")[0];
-  const data: OpenInterest[] = (await fetchURL(url))?.data.data.rows.map((e: any) => {
-    return {
-      date: e[0].split('T')[0],
-      openInterest: e[2],
-    }
-  })
+  // const data: OpenInterest[] = (await fetchURL(url))?.data.data.rows.map((e: any) => {
+  //   return {
+  //     date: e[0].split('T')[0],
+  //     openInterest: e[1],
+  //   }
+  // })
+  // const fs = require('fs');
+  // fs.writeFileSync('dydx.json', JSON.stringify(data))
+  const data = require('./dydx.json')
   const dailyOpenInterest = data.find((e: OpenInterest) => e.date === timeString)?.openInterest;
   return {
-    dailyVolume: dailyolume ? `${dailyolume}` : undefined,
+    // dailyVolume: dailyolume ? `${dailyolume}` : undefined,
     dailyOpenInterest: dailyOpenInterest ? `${dailyOpenInterest}` : undefined,
     timestamp: timestamp,
   };
