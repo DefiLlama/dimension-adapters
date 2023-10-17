2ed2bc151d436e73cc20d5fdd03856aadc32578f
0xgnek <0xgnek@gmail.com>
2023-10-17 05:13:26 +0000

    remove set env

--- fees/unidex.ts
+++ fees/unidex.ts
@@ -16,7 +16,7 @@ const endpoints: TUrl = {
   [CHAIN.ARBITRUM]: 'https://api.thegraph.com/subgraphs/name/unidex-finance/arbitrumleveragev2',
 }
 // hot fix era CERT_HAS_EXPIRED
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 interface IDTrade {
   id: string; // Add this line
