cc9d33a98aaf3f8d44994b25aaf8fa45d1ef6cc6
0xgnek <0xgnek@gmail.com>
2023-10-17 14:06:33 +0000

    fix gmx volume

--- dexs/gmx-v2-trade/index.ts
+++ dexs/gmx-v2-trade/index.ts
@@ -16,6 +16,9 @@ const topic1_ins = '0xf94196ccb31f81a3e67df18f2a62cbfb50009c80a7d3c728a3f542e3ab
 const topic0_des = '0x137a44067c8961cd7e1d876f4754a5a3a75989b4552f1843fc69c3b372def160';
 const topic1_des = '0x07d51b51b408d7c62dcc47cc558da5ce6a6e0fd129a427ebce150f52b0e5171a';
 
+const topic0_fees = '0x137a44067c8961cd7e1d876f4754a5a3a75989b4552f1843fc69c3b372def160';
+const topic1_fees = '0xe096982abd597114bdaa4a60612f87fabfcc7206aa12d61c50e7ba1e6c291100';
+
 type TChain  = {
   [s: Chain | string]: string;
 }
@@ -53,17 +56,34 @@ const fetch = (chain: Chain) => {
           topics: [topic0_des, topic1_des]
         })).output as ILog[];
 
-        const raw_in = posistion_logs.map((e: ILog) => {
-          const data = e.data.replace('0x', '');
-          const volume = data.slice(50 * 64, (50 * 64) + 64);
-          return Number('0x'+volume) / 1e30;
-        })
+        // const fees_logs: ILog[] = (await sdk.api.util.getLogs({
+        //   target: contract[chain],
+        //   topic: '',
+        //   toBlock: toBlock,
+        //   fromBlock: fromBlock,
+        //   keys: [],
+        //   chain: chain,
+        //   topics: [topic0_fees, topic1_fees]
+        // })).output as ILog[];
 
+        let hash: string[] = [];
         const raw_des = decress_logs.map((e: ILog) => {
           const data = e.data.replace('0x', '');
           const volume = data.slice(102 * 64, (102 * 64) + 64);
+          const key =  Number('0x'+data.slice(118 * 64, (118 * 64) + 64));
+          if (key === 7)  return 0;
+          hash.push(e.transactionHash);
+          // 156
           return Number('0x'+volume) / 1e30;
         })
+
+        const raw_in = posistion_logs.filter(e => !hash.includes(e.transactionHash)).map((e: ILog) => {
+          const data = e.data.replace('0x', '');
+          const volume = data.slice(100 * 64, (100 * 64) + 64);
+          return Number('0x'+volume) / 1e30;
+        })
+
+
 
         const dailyVolume: number = [...raw_des, ...raw_in]
           .reduce((a: number, b: number) => a+b, 0);
