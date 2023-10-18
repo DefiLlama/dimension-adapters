b6b2b22bd33e59b9058ee248a5d1827ba83551db
vladjito <95035799+vladjito@users.noreply.github.com>
2023-10-18 13:30:58 +0200

    Update index.ts

--- dexs/dx25/index.ts
+++ dexs/dx25/index.ts
@@ -30,7 +30,7 @@ const rpc = (url: string, method: string, params: any) =>
 const adapter: SimpleAdapter = {
   adapter: {
     concordium: {
-      start: async () => 1697544000,
+      start: async () => 1697628619,
       fetch: async (ts) => {
         const data = await rpc(POOLS_SERVICE_URL, 'volumes_statistic', {
           timestamp: ts,
