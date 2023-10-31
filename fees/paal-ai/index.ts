3b0f76d108a6f993e84b754bc81b352b18c76f61
0xgnek <0xgnek@gmail.com>
2023-10-31 14:42:56 +0000

    fix get fees

--- fees/paal-ai/index.ts
+++ fees/paal-ai/index.ts
@@ -1,60 +1,67 @@
 import * as sdk from "@defillama/sdk";
 import { getBalance } from "@defillama/sdk/build/eth";
-import { Adapter, FetchResultFees } from "../../adapters/types";
+import { Adapter, ChainBlocks, FetchResultFees } from "../../adapters/types";
 import { getBlock } from "../../helpers/getBlock";
 import { getPrices } from "../../utils/prices";
-import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../../utils/date";
 import { CHAIN } from "../../helpers/chains";
 
 /** Address to check = paalecosystemfund.eth */
 const CONTRACT_ADDRESS = "0x54821d1B461aa887D37c449F3ace8dddDFCb8C0a";
 
 /** Check ether balance at a given block number*/
-async function checkBalance(timeStamp: number) {
-    const currentBlock = await getBlock(timeStamp, "ethereum", {});
-    const yesterdayBlock = await getBlock(timeStamp - 86400, "ethereum", {});
+interface IBalance {
+    daily: string;
+    total: string;
+}
 
-    //const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp) // Not used by now but would be useful in the future
-    //const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp) // Not used by now but would be useful in the future
-     
+const checkBalance = async (timestamp: number, chainBlocks: ChainBlocks): Promise<IBalance> => {
+    const fromTimestamp = timestamp - 60 * 60 * 24
+    const toTimestamp = timestamp
+    const currentBlock = await getBlock(toTimestamp, CHAIN.ETHEREUM, chainBlocks);
+    const yesterdayBlock = await getBlock(fromTimestamp, CHAIN.ETHEREUM, {});
+
     const paramsCurrent = {
         target: CONTRACT_ADDRESS,
         block: currentBlock,
-        chain: [CHAIN.ETHEREUM].toString(),
+        chain: CHAIN.ETHEREUM,
     }
 
     const paramsYesterday = {
         target: CONTRACT_ADDRESS,
         block: yesterdayBlock,
-        chain: [CHAIN.ETHEREUM].toString(),
+        chain: CHAIN.ETHEREUM,
     }
 
     const currentBalance = (await getBalance(paramsCurrent)).output;
     const yesterdayBalance = (await getBalance(paramsYesterday)).output;
 
-    return (Number(currentBalance) - Number(yesterdayBalance)).toString();
+    return {
+        daily: (Number(currentBalance) - Number(yesterdayBalance)).toString(),
+        total: Number(currentBalance).toString()
+    } as IBalance
 }
 
 /** Calculate USD equivalent for a given ether amount */
 async function usdEquivalent(ethBalance: string, timeStamp: number) {
     const etherAddress = "ethereum:0x0000000000000000000000000000000000000000";
     const etherPrice = (await getPrices([etherAddress], timeStamp))[etherAddress].price;
-    return ((Number(ethBalance) / 1e18) * Number(etherPrice)).toString();    
+    const dailyFees = ((Number(ethBalance) / 1e18) * Number(etherPrice)).toString()
+    return dailyFees;
 }
 
 /** Adapter */
 const adapter: Adapter = {
     adapter: {
         ethereum: {
-            fetch: async (timestamp: number) => {
-                const balance = await checkBalance(timestamp);
+            fetch: async (timestamp: number, chainBlocks: ChainBlocks): Promise<FetchResultFees> => {
+                const { daily } = await checkBalance(timestamp, chainBlocks);
+                const dailyFees = await usdEquivalent(daily, timestamp)
                 return {
                     timestamp,
-                    totalFees: balance.toString(),
-                    totalFeesUsd: await usdEquivalent(balance, timestamp),
+                    dailyFees: dailyFees.toString(),
                 };
             },
-            start: async () => 1633478400, //Wed Oct 06 2021 00:00:00 GMT+0000            
+            start: async () => 1633478400, //Wed Oct 06 2021 00:00:00 GMT+0000
         },
     },
 }
