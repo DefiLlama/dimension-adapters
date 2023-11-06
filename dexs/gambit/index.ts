d6290eb69ff6285725a58aecb04507c5af9a1cf8
ALPHADA <48852104+ALPHADA@users.noreply.github.com>
2023-11-06 16:29:36 +0900

    Update arbitrum chain
    - gambit trade

--- dexs/gambit/index.ts
+++ dexs/gambit/index.ts
@@ -1,4 +1,4 @@
-import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import { BreakdownAdapter, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import * as sdk from "@defillama/sdk";
 import { getBlock } from "../../helpers/getBlock";
@@ -7,7 +7,8 @@ import { Chain } from "@defillama/sdk/build/general";
 const topic0_limit_ex = '0x165b0f8d6347f7ebe92729625b03ace41aeea8fd7ebf640f89f2593ab0db63d1';
 const topic0_market_ex = '0x2739a12dffae5d66bd9e126a286078ed771840f2288f0afa5709ce38c3330997';
 
-const USDC_DECIMAL = 6;
+const USDC_DECIMAL = 6
+const LEVERAGE_DECIMAL = 18
 
 type IAddress = {
   [s: string | Chain]: string
@@ -19,11 +20,16 @@ interface ILog {
   topics: string[];
 }
 
-const contract_address: IAddress = {
-  [CHAIN.ERA]: '0xE95a6FCC476Dc306749c2Ac62fB4637c27ac578d',
+const contract_address_v1: IAddress = {
+  [CHAIN.ERA]: '0xE95a6FCC476Dc306749c2Ac62fB4637c27ac578d'
 }
 
-const fetch = (chain: Chain) => {
+const contract_address: IAddress = {
+  [CHAIN.ERA]: '0x50853A14cD14CC6A891BF034A204A15d294AF056',
+  [CHAIN.ARBITRUM]: '0x8d85f4615ea5F2Ea8D91C196aaD4C04D8416865C',
+}
+
+const fetch = (chain: Chain, contractAddress: string = contract_address[chain], decimal = LEVERAGE_DECIMAL) => {
   return async (timestamp: number): Promise<FetchResultVolume> => {
     const fromTimestamp = timestamp - 60 * 60 * 24
     const toTimestamp = timestamp
@@ -32,7 +38,7 @@ const fetch = (chain: Chain) => {
     const toBlock = (await getBlock(toTimestamp, chain, {}));
     try {
       const logs_limit_ex: ILog[] = (await sdk.api.util.getLogs({
-        target: contract_address[chain],
+        target: contractAddress,
         topic: '',
         toBlock: toBlock,
         fromBlock: fromBlock,
@@ -42,7 +48,7 @@ const fetch = (chain: Chain) => {
       })).output as ILog[];
 
       const logs_market_ex: ILog[] = (await sdk.api.util.getLogs({
-        target: contract_address[chain],
+        target: contractAddress,
         topic: '',
         toBlock: toBlock,
         fromBlock: fromBlock,
@@ -53,14 +59,14 @@ const fetch = (chain: Chain) => {
 
       const limit_volume = logs_limit_ex.map((e: ILog) => {
         const data = e.data.replace('0x', '');
-        const leverage = Number('0x' + data.slice(512, 576));
+        const leverage = Number('0x' + data.slice(512, 576)) / 10 ** decimal;
         const positionSizeUsdc = Number('0x' + data.slice(896, 960)) / 10 ** USDC_DECIMAL;
         return (leverage * positionSizeUsdc)
       }).reduce((a: number, b: number) => a + b, 0);
 
       const market_volume = logs_market_ex.map((e: ILog) => {
         const data = e.data.replace('0x', '');
-        const leverage = Number('0x' + data.slice(448, 512));
+        const leverage = decimal != 0 ? Number('0x' + data.slice(448, 512)) / 10 ** decimal : Number('0x' + data.slice(448, 512));
         const positionSizeUsdc = Number('0x' + data.slice(832, 896)) / 10 ** USDC_DECIMAL;
         return (leverage * positionSizeUsdc)
       }).reduce((a: number, b: number) => a + b, 0);
@@ -77,14 +83,25 @@ const fetch = (chain: Chain) => {
   }
 }
 
-
-const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.ERA]: {
-      fetch: fetch(CHAIN.ERA),
-      start: async () => 1684324400,
-    }
-  }
+const adapter: BreakdownAdapter = {
+  breakdown: {
+    v1: {
+      [CHAIN.ERA]: {
+        fetch: fetch(CHAIN.ERA, contract_address_v1[CHAIN.ERA], 0),
+        start: async () => 1690761600, // 2023/07/31 00:00:00
+      }
+    },
+    v2: {
+      [CHAIN.ERA]: {
+        fetch: fetch(CHAIN.ERA),
+        start: async () => 1698883200, // 2023/11/02 00:00:00
+      },
+      [CHAIN.ARBITRUM]: {
+        fetch: fetch(CHAIN.ARBITRUM),
+        start: async () => 1698883200, // 2023/11/02 00:00:00
+      }
+    },
+  },
 };
 
 export default adapter;
