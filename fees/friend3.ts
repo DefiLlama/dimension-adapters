2bdc910499773991911d6331e1250b9c6636c508
0xgnek <0xgnek@gmail.com>
2023-11-06 12:11:23 +0000

    fix start time

--- fees/friend3.ts
+++ fees/friend3.ts
@@ -4,42 +4,42 @@ import * as sdk from "@defillama/sdk";
 import { getBlock } from "../helpers/getBlock";
 import { getPrices } from "../utils/prices";
 import { ethers } from "ethers";
-​
+
 const FriendV1Address = '0x1e70972ec6c8a3fae3ac34c9f3818ec46eb3bd5d';
 const topic0_trade = '0x2c76e7a47fd53e2854856ac3f0a5f3ee40d15cfaa82266357ea9779c486ab9c3';
 const event_trade = 'event Trade(address trader, address subject, bool isBuy, uint256 ticketAmount, uint256 ethAmount, uint256 protocolEthAmount, uint256 subjectEthAmount, uint256 supply)'
 const contract_interface = new ethers.utils.Interface([
   event_trade
 ]);
-​
-​
+
+
 const FriendV2Address = '0x2C5bF6f0953ffcDE678A35AB7d6CaEBC8B6b29F0';
 const topic0_trade_V2 = '0x5acb97638a46771a62aed2578c5c5c260108ffe0c606b887d61a9057af4034c9';
 const event_trade_V2 = 'event Trade (address trader , bytes32 subjectId , bool isBuy , uint256 ticketAmount , uint256 tokenAmount , uint256 protocolAmount , uint256 subjectAmount , uint256 holderAmount , uint256 referralAmount , uint256 supply)'
 const contract_interface_V2 = new ethers.utils.Interface([
   event_trade_V2
 ]);
-​
+
 interface ILog {
   data: string;
   transactionHash: string;
   topics: string[];
 }
-​
+
 interface IFee {
   fees: number;
   rev: number;
 }
-​
+
 const fetch = async (timestamp: number): Promise<FetchResultFees> => {
   const fromTimestamp = timestamp - 60 * 60 * 24
   const toTimestamp = timestamp
-​
+
   try {
     const fromBlock = await getBlock(fromTimestamp, CHAIN.BSC, {});
     const toBlock = await getBlock(toTimestamp, CHAIN.BSC, {});
-​
-​
+
+
     let _logs: ILog[] = [];
     for(let i = fromBlock; i < toBlock; i += 5000) {
       const logs: ILog[] = (await sdk.api.util.getLogs({
@@ -53,7 +53,7 @@ const fetch = async (timestamp: number): Promise<FetchResultFees> => {
       })).output as ILog[];
       _logs = _logs.concat(logs);
     }
-​
+
     const fees_details: IFee[] = _logs.map((e: ILog) => {
       const value = contract_interface.parseLog(e);
       const protocolEthAmount = Number(value.args.protocolEthAmount._hex) / 10 ** 18;
@@ -78,18 +78,18 @@ const fetch = async (timestamp: number): Promise<FetchResultFees> => {
     console.error(error)
     throw error;
   }
-​
+
 }
-​
+
 const fetchOpbnb = async (timestamp: number): Promise<FetchResultFees> => {
   const fromTimestamp = timestamp - 60 * 60 * 24
   const toTimestamp = timestamp
-​
+
   try {
     const fromBlock = await getBlock(fromTimestamp, CHAIN.OP_BNB, {});
     const toBlock = await getBlock(toTimestamp, CHAIN.OP_BNB, {});
-​
-​
+
+
     let _logs: ILog[] = [];
     for(let i = fromBlock; i < toBlock; i += 5000) {
       const logs: ILog[] = (await sdk.api.util.getLogs({
@@ -103,7 +103,7 @@ const fetchOpbnb = async (timestamp: number): Promise<FetchResultFees> => {
       })).output as ILog[];
       _logs = _logs.concat(logs);
     }
-​
+
     const fees_details: IFee[] = _logs.map((e: ILog) => {
       const value = contract_interface_V2.parseLog(e);
       const protocolAmount = Number(value.args.protocolAmount._hex) / 10 ** 18;
@@ -130,21 +130,21 @@ const fetchOpbnb = async (timestamp: number): Promise<FetchResultFees> => {
     console.error(error)
     throw error;
   }
-​
+
 }
-​
-​
+
+
 const adapter: Adapter = {
   adapter: {
     [CHAIN.BSC]: {
         fetch: fetch,
-        start: async ()  => 31133042,
+        start: async ()  => 1692835200,
     },
     [CHAIN.OP_BNB]: {
       fetch: fetchOpbnb,
-      start: async ()  => 7023367,
+      start: async ()  => 1698710400,
   },
   }
 }
-​
+
 export default adapter;
