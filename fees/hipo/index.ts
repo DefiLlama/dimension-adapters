5c03102fa98db5668decee62f343b4c8e393cb2d
0xgnek <0xgnek@gmail.com>
2023-11-07 14:01:09 +0000

    Revert "disable run current"
    This reverts commit 269da3486da09b2bf0643057c41d9358f4f28f12.

--- fees/hipo/index.ts
+++ fees/hipo/index.ts
@@ -6,7 +6,7 @@ const address = 'EQBNo5qAG8I8J6IxGaz15SfQVB-kX98YhKV_mT36Xo5vYxUa'
 export default {
     adapter: {
         [CHAIN.TON]: {
-            runAtCurrTime: false,
+            runAtCurrTime: true,
             start: async () => 1698685200,
             meta: {
                 hallmarks: [
