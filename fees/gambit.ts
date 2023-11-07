8777fbeed4b63e416aa141f85db2e58de74542f4
ALPHADA <48852104+ALPHADA@users.noreply.github.com>
2023-11-07 14:19:56 +0900

    remove gambit v2

--- fees/gambit.ts
+++ fees/gambit.ts
@@ -1,4 +1,4 @@
-import { Adapter, BreakdownAdapter, FetchResultFees } from "../adapters/types";
+import { Adapter, FetchResultFees } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import * as sdk from "@defillama/sdk";
 import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../utils/date";
@@ -35,35 +35,40 @@ interface ITx {
   data: string;
 }
 
-const FEE_ADDRESS_V1 = {
-  [CHAIN.ERA]: "0xE95a6FCC476Dc306749c2Ac62fB4637c27ac578d",
-};
 const FEE_ADDRESS = {
-  [CHAIN.ERA]: "0x50853A14cD14CC6A891BF034A204A15d294AF056",
-  [CHAIN.ARBITRUM]: "0x8d85f4615ea5F2Ea8D91C196aaD4C04D8416865C",
+  [CHAIN.ERA]: ["0xE95a6FCC476Dc306749c2Ac62fB4637c27ac578d", "0x6cf71FaeA3771D56e72c72501e7172e79116E2A3", "0x50853A14cD14CC6A891BF034A204A15d294AF056"],
+  [CHAIN.ARBITRUM]: ["0x8d85f4615ea5F2Ea8D91C196aaD4C04D8416865C"],
 };
 
 const BIG_TEN = new BigNumber('10');
 
 const USDC_DECIMAL = 6
 
-const fetch = (address: string, chain: Chain) => {
+const fetch = (addressList: string[], chain: Chain) => {
   return async (timestamp: number): Promise<FetchResultFees> => {
     const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp)
     const yesterdaysTimestamp = getTimestampAtStartOfNextDayUTC(timestamp)
 
     const todaysBlock = (await getBlock(todaysTimestamp, chain, {}));
     const yesterdaysBlock = (await getBlock(yesterdaysTimestamp, chain, {}));
-    const [devFeeCall, ssFeeCall, referralFeeCall, nftBotFeeCall]: any = await Promise.all(
-      event.map((e: IEvent) => sdk.api.util.getLogs({
-        target: address,
-        topic: e.name,
-        toBlock: yesterdaysBlock,
-        fromBlock: todaysBlock,
-        keys: [],
-        chain: chain,
-        topics: [e.topic]
-      })));
+
+    const [devFeeCall, ssFeeCall, referralFeeCall, nftBotFeeCall]: any = (await Promise.all(
+      event.map((e: IEvent) => {
+        return Promise.all(
+          addressList.map(async (address) => {
+            return sdk.api.util.getLogs({
+              target: address,
+              topic: e.name,
+              toBlock: yesterdaysBlock,
+              fromBlock: todaysBlock,
+              keys: [],
+              chain: chain,
+              topics: [e.topic],
+            });
+          })
+        );
+      })
+    )).flat();
     const devFeeValume = devFeeCall.output.map((p: ITx) => new BigNumber(p.data)).reduce((a: BigNumber, c: BigNumber) => a.plus(c), new BigNumber('0'));
     const ssFeeVol = ssFeeCall.output.map((p: ITx) => new BigNumber(p.data)).reduce((a: BigNumber, c: BigNumber) => a.plus(c), new BigNumber('0'));
     const referralFeeVol = referralFeeCall.output.map((p: ITx) => new BigNumber(p.data)).reduce((a: BigNumber, c: BigNumber) => a.plus(c), new BigNumber('0'));
@@ -81,23 +86,15 @@ const fetch = (address: string, chain: Chain) => {
   }
 }
 
-const adapter: BreakdownAdapter = {
-  breakdown: {
-    v1: {
-      [CHAIN.ERA]: {
-        fetch: fetch(FEE_ADDRESS_V1[CHAIN.ERA], CHAIN.ERA),
-        start: async () => 1690761600, // 2023/07/31 00:00:00
-      }
+const adapter: Adapter = {
+  adapter: {
+    [CHAIN.ERA]: {
+      fetch: fetch(FEE_ADDRESS[CHAIN.ERA], CHAIN.ERA),
+      start: async () => 1690848000, // 2023/08/01 00:00:00
     },
-    v2: {
-      [CHAIN.ERA]: {
-        fetch: fetch(FEE_ADDRESS[CHAIN.ERA], CHAIN.ERA),
-        start: async () => 1698883200, // 2023/11/02 00:00:00
-      },
-      [CHAIN.ARBITRUM]: {
-        fetch: fetch(FEE_ADDRESS[CHAIN.ARBITRUM], CHAIN.ARBITRUM),
-        start: async () => 1698883200, // 2023/11/02 00:00:00
-      },
+    [CHAIN.ARBITRUM]: {
+      fetch: fetch(FEE_ADDRESS[CHAIN.ARBITRUM], CHAIN.ARBITRUM),
+      start: async () => 1698883200, // 2023/11/02 00:00:00
     },
   },
 };
