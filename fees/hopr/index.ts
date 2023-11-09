f05178b0cc10e6fe6627acae09d0782990e32691
Michał Jadach <michal.jadach@hoprnet.org>
2023-11-09 17:52:35 +0100

    update revenue to usd

--- fees/hopr/index.ts
+++ fees/hopr/index.ts
@@ -2,9 +2,13 @@ import { Adapter, FetchResultFees } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import * as sdk from "@defillama/sdk";
 import { getBlock } from "../../helpers/getBlock";
+import { getPrices } from "../../utils/prices";
+import BigNumber from "bignumber.js";
 
 const channels_address = '0x693Bac5ce61c720dDC68533991Ceb41199D8F8ae';
 const wxHOPR_address = '0xd4fdec44db9d44b8f2b6d529620f9c0c7066a2c1';
+const xHOPR_address = '0xd057604a14982fe8d88c5fc25aac3267ea142a08';
+const chain = 'xdai';
 const topic0 = '0x7165e2ebc7ce35cc98cb7666f9945b3617f3f36326b76d18937ba5fecf18739a'; //TicketRedeemed
 const topic1 = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'; //Sent 
 
@@ -16,6 +20,11 @@ const methodology = {
 interface ITx {
   data: string;
   transactionHash: string;
+}
+
+const toString = (x: BigNumber) => {
+  if (x.isEqualTo(0)) return undefined
+  return x.toString()
 }
 
 const fetch = async (timestamp: number): Promise<FetchResultFees> => {
@@ -53,14 +62,18 @@ const fetch = async (timestamp: number): Promise<FetchResultFees> => {
   }).filter(elem => elem !== undefined) as string[];
 
   const dailyRevenue = dailyRevenueArray.map((data: string) => {
-    const amount = Number(data) / 10 ** 6;
+    const amount = Number(data) / 10 ** 18;
     return amount;
   }).reduce((a: number, b: number) => a+b,0);
+
+  const prices: any = (await getPrices([`${chain}:${xHOPR_address}`], toTimestamp));
+  const price = prices[`${chain}:${xHOPR_address}`]?.price || 0;
+  const dailyRevenueUSD = dailyRevenue * price;
 
   return {
     timestamp: timestamp,
     dailyFees: `0`,
-    dailyRevenue: `${dailyRevenue}`
+    dailyRevenue: `${dailyRevenueUSD}`
   };
 }
 
