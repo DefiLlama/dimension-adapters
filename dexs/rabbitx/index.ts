3f215891e5f6deb087f0054d72e25878f03e85de
0xgnek <0xgnek@gmail.com>
2023-11-22 13:30:38 +0000

    fix logic get volume

--- dexs/rabbitx/index.ts
+++ dexs/rabbitx/index.ts
@@ -1,11 +1,13 @@
 import fetchURL from "../../utils/fetchURL"
 import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 const historicalVolumeEndpoint = "https://api.prod.rabbitx.io/markets"
-const candles = (market: string, timestampFrom: number, timestampTo: number) => 
-    `https://api.prod.rabbitx.io/candles?market_id=${market}&timestamp_from=${timestampFrom}&timestamp_to=${timestampTo}&period=60`;
+const candles = (market: string, timestampFrom: number, timestampTo: number) => {
+  const url = `https://api.prod.rabbitx.io/candles?market_id=${market}&timestamp_from=${timestampFrom}&timestamp_to=${timestampTo}&period=1440`;
+  return url;
+
+}
 
 interface IVolumeall {
   volume: string;
@@ -14,21 +16,22 @@ interface IVolumeall {
 }
 
 const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
-  const dayTimestamp = Math.floor(getUniqStartOfTodayTimestamp(new Date(timestamp * 1000)) / 1000)
-  const fromTimestamp = dayTimestamp - 60 * 60 * 24;  // 24 hours back
-  const toTimestamp = dayTimestamp;
+  const fromTimestamp = timestamp - 60 * 60 * 24
+  const toTimestamp = timestamp
 
   // Get market data
   const response = await fetchURL(historicalVolumeEndpoint);
   const marketsData = response.data.result;
-  
-  // Fetch candles for each USD market  
+
+  // Fetch candles for each USD market
   const historical: IVolumeall[] = (await Promise.all(marketsData.map((market: any) => fetchURL(candles(market.id, fromTimestamp, toTimestamp)))))
     .map((e: any) => e.data.result)
     .flat();
 
   // Calculate daily volume
-  const dailyVolume = historical.filter((e: IVolumeall) => Number(e.time) < dayTimestamp)
+  const dailyVolume = historical
+    .filter((e: IVolumeall) => Number(e.time) >= fromTimestamp )
+    .filter((e: IVolumeall) => Number(e.time) <= toTimestamp)
     .reduce((a: number, b: IVolumeall) => a + Number(b.volume), 0)
 
   return {
@@ -41,7 +44,7 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.STARKNET]: {
       fetch,
-      start: async () => 1700179200000, // Replace with actual start timestamp
+      start: async () => 1700179200, // Replace with actual start timestamp
     },
   },
 };
