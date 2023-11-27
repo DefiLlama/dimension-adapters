03cffcba392dad0f0bc5be2df756532b6f9a51a5
0xgnek <0xgnek@gmail.com>
2023-11-27 16:51:44 +0000

    set more header

--- fees/near.ts
+++ fees/near.ts
@@ -10,13 +10,18 @@ interface IChart {
 const fetchFees =  async (timestamp: number): Promise<FetchResultFees> => {
   const todayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
   const url  = 'https://api.nearblocks.io/v1/charts'
-  const value: IChart[] = (await axios.get(url, { headers: {
-    'origin': 'https://nearblocks.io' } })).data.charts;
+  const value: IChart[] = (await axios.get(url, {
+    headers: {
+    'origin': 'https://nearblocks.io',
+    'referer': 'https://nearblocks.io/charts' ,
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+  }})).data.charts;
   const dateStr = new Date(todayTimestamp * 1000).toISOString().split('T')[0];
   const dailyFees = value.find((item) => item.date.split('T')[0] === dateStr)?.txn_fee_usd;
   return {
     timestamp,
     dailyFees: dailyFees ? `${dailyFees}` : undefined,
+    dailyRevenue: dailyFees ? `${dailyFees}` : undefined,
   }
 }
 
