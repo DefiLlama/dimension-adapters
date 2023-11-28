5fb977333692085b74ca68141929b1400084756d
Ahmet Avcı <ahmetabdullahavci07@gmail.com>
2023-11-28 16:07:08 +0300

    add lighter v2 in seperate folder

--- dexs/lighterv2/index.ts
+++ dexs/lighterv2/index.ts
@@ -3,21 +3,21 @@ import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
-var lighterVolumeEndpoint =
-  "https://api.lighter.xyz/volume?blockchain_id=42161";
+var lighterV2VolumeEndpoint =
+  "https://api.lighter.xyz/v2/volume?blockchain_id=42161";
 
 interface IVolumeall {
   totalVolume: number;
   dailyVolume: number;
 }
 
-const fetch = async (timestamp: number) => {
+const fetchV2 = async (timestamp: number) => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  lighterVolumeEndpoint = lighterVolumeEndpoint.concat(
+  lighterV2VolumeEndpoint = lighterV2VolumeEndpoint.concat(
     `&timestamp=${dayTimestamp}`
   );
 
-  const result: IVolumeall = (await fetchURL(lighterVolumeEndpoint)).data;
+  const result: IVolumeall = (await fetchURL(lighterV2VolumeEndpoint)).data;
 
   return {
     totalVolume: result ? `${result.totalVolume}` : undefined,
@@ -29,9 +29,10 @@ const fetch = async (timestamp: number) => {
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
-      fetch,
-      start: async () => 1677934513,
+      fetch: fetchV2,
+      start: async () => 1697144400,
     },
   },
 };
+
 export default adapter;
