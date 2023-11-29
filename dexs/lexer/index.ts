139b88ca56577ec9700bb37442bf959b7852998f
vincracker <vincentchen0930@gmail.com>
2023-11-29 12:49:08 +0800

    Added fees and volume query

---
+++ dexs/lexer/index.ts
@@ -0,0 +1,59 @@
+import { Fetch, SimpleAdapter } from "../../adapters/types";
+import { ARBITRUM } from "../../helpers/chains";
+import {request} from 'graphql-request';
+
+// TODO: change these endpoints 
+const apiEndPoints = [
+    'https://api.studio.thegraph.com/query/48038/lexer-synstats-staging/version/latest',
+    'https://api.studio.thegraph.com/query/48038/lexer-xlpstats2-staging/version/latest',
+    'https://api.studio.thegraph.com/query/48038/lexer-xlpstats-staging/version/latest',
+]
+
+type VolumeStatsQuery = {
+    volumeStats: [
+        {
+            swap: string,
+            mint: string,
+            burn: string,
+            margin: string,
+            liquidation: string,
+        }
+    ]
+}
+
+const queryString = `{
+    volumeStats(where: {period: total}) {
+      swap
+      mint
+      burn
+      margin
+      liquidation
+    }
+  }`
+
+const fetch: Fetch = async(timestamp) => {
+    // TODO: get result from fetching api call
+    let totalVolume = 0;
+    for (const api of apiEndPoints){
+        const {swap, mint, burn, liquidation, margin} = (await request<VolumeStatsQuery>(api, queryString)).volumeStats[0];
+        const endPointVolume = Number(swap) + Number(mint) + Number(burn) + Number(liquidation) + Number(margin);
+        totalVolume += endPointVolume;
+    }
+    totalVolume /= 1e30
+    return {
+        timestamp,
+        totalVolume: String(totalVolume)
+    }
+}
+
+const adapter: SimpleAdapter = {
+    adapter: {
+        [ARBITRUM]: {
+            runAtCurrTime: true,
+            start: async () => 1000235,
+            fetch
+        }
+    }
+}
+
+export default adapter;
---
+++ fees/lexer/index.ts
@@ -0,0 +1,57 @@
+import { Fetch, SimpleAdapter } from "../../adapters/types";
+import { ARBITRUM } from "../../helpers/chains";
+import {request} from 'graphql-request';
+
+// TODO: change these endpoints 
+const apiEndPoints = [
+    'https://api.studio.thegraph.com/query/48038/lexer-synstats-staging/version/latest',
+    'https://api.studio.thegraph.com/query/48038/lexer-xlpstats2-staging/version/latest',
+    'https://api.studio.thegraph.com/query/48038/lexer-xlpstats-staging/version/latest',
+]
+
+type FeeStatsQuery = {
+    feeStats: [
+        {
+            swap: string,
+            mint: string,
+            burn: string,
+            marginAndLiquidation: string,
+        }
+    ]
+}
+
+const queryString = `{
+    feeStats(where: {period: total}) {
+      marginAndLiquidation
+      swap
+      mint
+      burn
+    }
+  }`
+
+const fetch: Fetch = async(timestamp) => {
+    // TODO: get result from fetching api call
+    let totalFee = 0;
+    for (const api of apiEndPoints){
+        const {swap, mint, burn, marginAndLiquidation} = (await request<FeeStatsQuery>(api, queryString)).feeStats[0];
+        const endPointFee = Number(swap) + Number(mint) + Number(burn) + Number(marginAndLiquidation);
+        totalFee += endPointFee;
+    }
+    totalFee /= 1e30
+    return {
+        timestamp,
+        totalFees: String(totalFee)
+    }
+}
+
+const adapter: SimpleAdapter = {
+    adapter: {
+        [ARBITRUM]: {
+            runAtCurrTime: true,
+            start: async () => 1000235,
+            fetch
+        }
+    }
+}
+
+export default adapter;
