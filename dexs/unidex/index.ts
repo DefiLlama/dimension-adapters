64e16cf15ce390f212bba0866491cb9875fcab0f
Krunal Amin <kishanamin97@gmail.com>
2023-11-28 18:21:35 -0600

    add mux ref volume

--- dexs/unidex/index.ts
+++ dexs/unidex/index.ts
@@ -4,6 +4,11 @@ import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 import { Chain } from "@defillama/sdk/build/general";
 import request, { gql } from "graphql-request";
 
+interface IReferralRecord {
+  volume: string; // Assuming volume is a string that represents a number
+  timestamp: number;
+}
+
 type TChainIDs = {
   [key in Chain]?: number;
 };
@@ -20,6 +25,79 @@ interface IDayProduct {
   cumulativeVolumeUsd: number;
   _id: string;
 }
+
+const fetchReferralVolume = async (timestamp: number): Promise<number> => {
+  const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+
+  const referralQuery = gql`
+    {
+      referrerStats(
+        where: {referrer: "0x8c128f336b479b142429a5f351af225457a987fa", timestamp_gt: "${todaysTimestamp}"}
+      ) {
+        volume
+      }
+    }
+  `;
+
+  const referralEndpoint = 'https://api.thegraph.com/subgraphs/name/gmx-io/gmx-arbitrum-referrals';
+  const referralRes = await request(referralEndpoint, referralQuery);
+
+  // If there's no volume data, return 0
+  if (!referralRes.referrerStats || referralRes.referrerStats.length === 0) {
+    return 0;
+  }
+
+  return Number(referralRes.referrerStats[0].volume) / 10 ** 30;
+};
+
+
+const fetchMuxReferralVolume = async (timestamp: number): Promise<number> => {
+  const startOfDayTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+  const endOfDayTimestamp = startOfDayTimestamp + 86400; // Add one day's worth of seconds for the end of the day
+
+  const referralQuery = gql`
+    query MyQuery($timestamp_gte: BigInt = "", $timestamp_lte: BigInt = "") {
+      referralRecords(
+        where: {
+          referralCode: "0x556e694465780000000000000000000000000000000000000000000000000000",
+          timestamp_gte: $timestamp_gte,
+          timestamp_lte: $timestamp_lte
+        }
+      ) {
+        volume
+        timestamp
+      }
+    }
+  `;
+
+  const variables = {
+    timestamp_gte: startOfDayTimestamp.toString(),
+    timestamp_lte: endOfDayTimestamp.toString()
+  };
+
+  const referralEndpoint = 'https://api.thegraph.com/subgraphs/name/mux-world/mux-referral-arb';
+  const referralRes = await request(referralEndpoint, referralQuery, variables);
+
+  // Sum up the volumes
+  let totalVolume = 0;
+
+  if (referralRes.referralRecords && Array.isArray(referralRes.referralRecords)) {
+    referralRes.referralRecords.forEach((record: IReferralRecord) => {
+      const volume = parseFloat(record.volume);
+      if (!isNaN(volume)) {
+        totalVolume += volume / 10 ** 18; // Adjust the unit conversion as needed
+      }
+    });
+  }
+  
+  return totalVolume;
+};
+
+
+
+
+
+
 
 const fetch = (chain: Chain) => {
   return async (timestamp: number): Promise<FetchResultVolume> => {
@@ -47,6 +125,12 @@ const fetch = (chain: Chain) => {
         dailyVolumeUSD += product.cumulativeVolumeUsd;
       }
     });
+
+    if (chain === CHAIN.ARBITRUM) {
+      const referralVolumeUSD = await fetchReferralVolume(timestamp);
+      const muxReferralVolumeUSD = await fetchMuxReferralVolume(timestamp);
+      dailyVolumeUSD += referralVolumeUSD + muxReferralVolumeUSD;
+    }
 
     return {
       dailyVolume: dailyVolumeUSD.toString(),
