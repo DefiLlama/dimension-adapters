b9403fb0077dd4cc00ba4e5c25e72086ad8d745d
0xgnek <0xgnek@gmail.com>
2023-12-01 16:02:15 +0000

    disable litx and fix get block

--- helpers/getBlock.ts
+++ helpers/getBlock.ts
@@ -19,10 +19,6 @@ async function getBlock(timestamp: number, chain: Chain, chainBlocks: ChainBlock
             block = Number((await retry(async () => (await axios.get(`https://explorer.kava.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before`).catch((e) => {
                 throw new Error(`Error getting block: ${chain} ${timestamp} ${e.message}`)
             }))?.data?.result?.blockNumber)));
-        else if (chain === CHAIN.PULSECHAIN)
-            block = Number((await retry(async () => (await axios.get(`https://scan.pulsechain.com/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before`).catch((e) => {
-                throw new Error(`Error getting block: ${chain} ${timestamp} ${e.message}`)
-            }))?.data?.result?.blockNumber)));
         else if (chain === CHAIN.ONUS)
             block = Number((await retry(async () => (await axios.get(`https://explorer.onuschain.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before`).catch((e) => {
                 throw new Error(`Error getting block: ${chain} ${timestamp} ${e.message}`)
