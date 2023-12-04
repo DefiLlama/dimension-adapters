034ed0adad7ca807c41799d1a7342b16e1670eae
0xgnek <0xgnek@gmail.com>
2023-12-04 13:05:42 +0000

    fix hopr get logs with batch size

--- fees/hopr/index.ts
+++ fees/hopr/index.ts
@@ -11,7 +11,8 @@ const wxHOPR_address = '0xd4fdec44db9d44b8f2b6d529620f9c0c7066a2c1';
 const xHOPR_address = '0xd057604a14982fe8d88c5fc25aac3267ea142a08';
 const chain = 'xdai';
 const topic0 = '0x7165e2ebc7ce35cc98cb7666f9945b3617f3f36326b76d18937ba5fecf18739a'; //TicketRedeemed
-const topic1 = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'; //Transfer 
+const topic1 = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'; //Transfer
+const topic2 = '0x000000000000000000000000693bac5ce61c720ddc68533991ceb41199d8f8ae';
 
 const methodology = {
   Fees: "Protocol has no supply-side fees, only user fees which are Sum of all ticket values redeemed in wxHOPR internally in the channels contract and also to the HOPR safe address",
@@ -42,15 +43,22 @@ const fetch = async (timestamp: number): Promise<FetchResultFees> => {
     chain: CHAIN.XDAI
   })).output as ITx[];
 
-  const erc20transferLog: ITx[] = (await sdk.api.util.getLogs({
-    target: wxHOPR_address,
-    topic: topic1,
-    fromBlock: fromBlock,
-    toBlock: toBlock,
-    topics: [topic1],
-    keys: [],
-    chain: CHAIN.XDAI
-  })).output as ITx[];
+  const batchSize = 4500;
+  const batches = Math.ceil((toBlock - fromBlock) / batchSize);
+
+  const erc20transferLog: ITx[] = await Promise.all(
+    Array.from({ length: batches }, (_, __) =>
+    sdk.api.util.getLogs({
+      target: wxHOPR_address,
+      topic: topic1,
+      fromBlock: fromBlock,
+      toBlock: toBlock,
+      topics: [topic1, topic2],
+      keys: [],
+      chain: CHAIN.XDAI
+    })
+    )
+  ).then((responses) => responses.flatMap((response) => response.output as ITx[]));
 
   let dailyRevenueStayedInChannelsTXs: string[] = [];
   const dailyRevenueArrayPaidToSafe = ticketRedeemedLogs.map(ticket => {
