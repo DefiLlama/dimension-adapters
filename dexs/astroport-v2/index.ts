53434569f9d74d34bc47ee7440bf24458997bf42
0xgnek <0xgnek@gmail.com>
2023-12-12 17:05:05 +0000

    updateastroport-v2

--- dexs/astroport-v2/index.ts
+++ dexs/astroport-v2/index.ts
@@ -1,41 +1,54 @@
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import { FetchResult, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { request } from "graphql-request";
 
+const API_ENDPOINT = "https://multichain-api.astroport.fi/graphql";
 
-const fetchVolume = (chainId: string) => {
-  return async (timestamp: number): Promise<FetchResultVolume> => {
+const statsQuery = `
+query Stats($chains: [String]!) {
+  stats(chains: $chains, sortDirection: DESC) {
+    chains {
+      chainId
+      totalVolume24h
+    }
+  }
+}
+`;
+
+const fetch = (chainId: string) => {
+  return async (timestamp: number): Promise<FetchResult> => {
     const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-    const raw = require('./astroport.json');
-    const dailyVolume = raw.find((e: any) => e.timestamp === dayTimestamp)[chainId]['astroport'];
-
+    const results = await request(API_ENDPOINT, statsQuery, { chains: [chainId] });
+    const totalVolume24h = results?.stats?.chains[0]?.totalVolume24h;
     return {
       timestamp: dayTimestamp,
-      dailyVolume: dailyVolume,
+      dailyVolume: totalVolume24h ? String(totalVolume24h) : undefined,
     };
   };
 };
 
-const adapters: SimpleAdapter = {
+const adapter: SimpleAdapter = {
   adapter: {
+    "terra2": {
+      fetch: fetch("phoenix-1"),
+      runAtCurrTime: true,
+      customBackfill: undefined,
+      start: async () => 0,
+    },
     [CHAIN.INJECTIVE]: {
-      fetch: fetchVolume(CHAIN.INJECTIVE),
-      // runAtCurrTime: true,
-      // customBackfill: undefined,
-      start: async () => 1688860800,
+      fetch: fetch("injective-1"),
+      runAtCurrTime: true,
+      customBackfill: undefined,
+      start: async () => 0,
     },
     neutron: {
-      fetch: fetchVolume("neutron"),
-      // runAtCurrTime: true,
-      // customBackfill: undefined,
-      start: async () => 1688860800,
-    },
-    terra2: {
-      fetch: fetchVolume("terra"),
-      // runAtCurrTime: true,
-      // customBackfill: undefined,
-      start: async () => 1688860800,
+      fetch: fetch("neutron-1"),
+      runAtCurrTime: true,
+      customBackfill: undefined,
+      start: async () => 0,
     },
   },
-}
-export default adapters;
+};
+
+export default adapter;
