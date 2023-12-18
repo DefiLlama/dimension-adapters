0757154c17a581972bce5b08392312234909871f
vrtnd <vrotend@proton.me>
2023-12-18 15:13:25 +0300

    rename dodo and tokenlon

--- aggregators/dodo-agg/index.ts
+++ aggregators/dodo-agg/index.ts
@@ -1,7 +1,7 @@
 import { getAdapter } from "../../helpers/aggregators/duneAdapter";
 import { CHAIN } from "../../helpers/chains";
 
-const chains = ["arbitrum", "bnb", "ethereum", "optimism", "polygon", "base"];
+const chains = ["arbitrum", "bsc", "ethereum", "optimism", "polygon", "base"];
 const chainMap = { [CHAIN.BSC]: "bnb" };
 const adapter = getAdapter(chains, chainMap, "DODO X", 1676592000);
 
