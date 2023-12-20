553eb1e74dc0a69c82e402d161a314aa06b1fac7
Hemant Singhal <hemant@seed.im>
2023-12-20 10:38:48 +0000

    Removing Arbitrum chain from Bluefin Derivatives

--- dexs/bluefin/index.ts
+++ dexs/bluefin/index.ts
@@ -4,12 +4,10 @@ import axios from "axios";
 import BigNumber from "bignumber.js";
 
 const URL_CONFIG = {
-  "arb": "https://dapi.api.arbitrum-prod.firefly.exchange/marketData",
   "sui": "https://dapi.api.sui-prod.bluefin.io/marketData",
 }
 
 const PRODUCT_CONFIG = {
-  "arb": ["ETH-PERP", "BTC-PERP"],
   "sui": ["ETH-PERP", "BTC-PERP"],
 }
 
@@ -41,19 +39,11 @@ const fetchSUI = async (timeStamp: number) => {
   return await computeVolume(timeStamp, URL_CONFIG.sui, PRODUCT_CONFIG.sui);
 };
 
-const fetchArb = async (timeStamp: number) => {
-  return await computeVolume(timeStamp, URL_CONFIG.arb, PRODUCT_CONFIG.arb);
-};
-
 const startTime = 1695600000; // 25th September when SUI trading starts
 
 const adapter: BreakdownAdapter = {
   breakdown: {
     derivatives: {
-      [CHAIN.ARBITRUM]: {
-        fetch: fetchArb,
-        start: async () => startTime,
-      },
       [CHAIN.SUI]: {
         fetch: fetchSUI,
         start: async () => startTime,
