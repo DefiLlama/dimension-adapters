f0a2f78f0021bb97c82cee1e06cfa832fac2b90b
0xgnek <0xgnek@gmail.com>
2023-12-25 15:37:57 +0000

    fix cache time

--- fees/rainbow-wallet.ts
+++ fees/rainbow-wallet.ts
@@ -41,7 +41,7 @@ const fetch = (chain: Chain) => {
       `;
 
 
-      const logs: [string, string][] = (await queryFlipside(query, 210))
+      const logs: [string, string][] = (await queryFlipside(query, 260))
       const log = logs.map(([data, contract_address]: [string, string]) => {
           const volume =  Number(data)
         return {
