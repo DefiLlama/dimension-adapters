54a11f1222a9e9c3f50000bf500346cad9a8f14f
0xgnek <0xgnek@gmail.com>
2024-01-15 15:24:47 +0000

    fix equilibre dexs

--- dexs/equilibre/index.ts
+++ dexs/equilibre/index.ts
@@ -2,6 +2,7 @@ import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import * as sdk from "@defillama/sdk";
 import { getBlock } from "../../helpers/getBlock";
+import { IYield, getTopPool } from "../../helpers/pool";
 import { getPrices } from "../../utils/prices";
 import { Chain } from "@defillama/sdk/build/general";
 
@@ -12,121 +13,41 @@ interface ILog {
 }
 
 const topic0 = '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
-const FACTORY_ADDRESS = '0xA138FAFc30f6Ec6980aAd22656F2F11C38B56a95';
-
-type TABI = {
-  [k: string]: object;
-}
-const ABIs: TABI = {
-  allPairsLength: {
-    "type": "function",
-    "stateMutability": "view",
-    "outputs": [
-      {
-        "type": "uint256",
-        "name": "",
-        "internalType": "uint256"
-      }
-    ],
-    "name": "allPairsLength",
-    "inputs": []
-  },
-  allPairs: {
-    "type": "function",
-    "stateMutability": "view",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "inputs": [
-      {
-        "type": "uint256",
-        "name": "",
-        "internalType": "uint256"
-      }
-    ],
-    "name": "allPairs",
-  }
-};
-
-const PAIR_TOKEN_ABI = (token: string): object => {
-  return {
-    "constant": true,
-    "inputs": [],
-    "name": token,
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
-};
-
 
 const fetch = async (timestamp: number) => {
   const fromTimestamp = timestamp - 60 * 60 * 24
   const toTimestamp = timestamp
   try {
-    const poolLength = (await sdk.api2.abi.call({
-      target: FACTORY_ADDRESS,
-      chain: 'kava',
-      abi: ABIs.allPairsLength,
-    }));
+    const poolData = (await getTopPool('equilibre', CHAIN.KAVA))
+    const pools = poolData.map((e: IYield) => e.pool_old);
 
-    const poolsRes = await sdk.api2.abi.multiCall({
-      abi: ABIs.allPairs,
-      calls: Array.from(Array(Number(poolLength)).keys()).map((i) => ({
-        target: FACTORY_ADDRESS,
-        params: i,
-      })),
-      chain: 'kava',
-      permitFailure: true
-    });
-
-    const lpTokens = poolsRes
+    const lpTokens = pools
       .filter((e: string)  => e.toLowerCase() !== '0xE6c4B59C291562Fa7D9FF5b39C38e2a28294ec49'.toLowerCase());
 
-    const [underlyingToken0, underlyingToken1] = await Promise.all(
-      ['token0', 'token1'].map((method) =>
-        sdk.api2.abi.multiCall({
-          abi: PAIR_TOKEN_ABI(method),
-          calls: lpTokens.map((address: string) => ({
-            target: address,
-          })),
-          chain: 'kava',
-          permitFailure: true
-        })
-      )
-    );
+      const underlyingToken = poolData.map((e: IYield) => {
+        return {
+          underlyingToken0: e.underlyingTokens[0],
+          underlyingToken1: e.underlyingTokens[1],
+        }
+      });
 
-    const tokens0 = underlyingToken0;
-    const tokens1 = underlyingToken1;
+    const tokens0 = underlyingToken.map((e: any) => e.underlyingToken0);
+    const tokens1 = underlyingToken.map((e: any) => e.underlyingToken1);
     const fromBlock = await getBlock(fromTimestamp, 'kava' as Chain, {});
     const toBlock = await getBlock(toTimestamp, 'kava' as Chain, {});
-    const _logs: ILog[] = [];
-    const split_size: number = 55;
-    for(let i = 0; i < lpTokens.length; i+=split_size) {
-      const logs: ILog[] = (await Promise.all(lpTokens.slice(i, i + split_size).map((address: string) => sdk.getEventLogs({
-        target: address,
-        toBlock: toBlock,
-        fromBlock: fromBlock,
-        chain: 'kava',
-        topics: [topic0]
-      })))).flat();
-      _logs.push(...logs)
-    }
+    const logs: ILog[] = (await Promise.all(lpTokens.map((address: string) => sdk.getEventLogs({
+      target: address,
+      toBlock: toBlock,
+      fromBlock: fromBlock,
+      topic: '',
+      chain: 'kava',
+      topics: [topic0]
+    })))).flat();
+
     const rawCoins = [...tokens0, ...tokens1].map((e: string) => `kava:${e}`);
     const coins = [...new Set(rawCoins)]
     const prices = await getPrices(coins, timestamp);
-    const untrackVolumes = _logs.map((e: ILog) => {
+    const untrackVolumes = logs.map((e: ILog) => {
       const data =  e.data.replace('0x', '');
       const amount0In = Number('0x' + data.slice(0, 64));
       const amount1In = Number('0x' + data.slice(64, 128));
