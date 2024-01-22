ed55f5fb690d20f4d3b887c9b00edfd0dcf4fc6a
0xshad0w <hugomncs@gmail.com>
2024-01-22 04:14:43 +0000

    fix-2

--- fees/ramses-exchange/ramses-exchange.ts
+++ fees/ramses-exchange/ramses-exchange.ts
@@ -1,5 +1,5 @@
-import { SimpleAdapter } from "../adapters/types";
-import { CHAIN } from "../helpers/chains";
+import { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
 import { getDexFeesExports } from "../helpers/dexVolumeLogs";
 
 const FACTORY_ADDRESS = '0xaaa20d08e59f6561f242b08513d36266c5a29415';
