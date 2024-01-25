ee751a1e438c6c83d0820630c6dd4874f4147002
0xngmi <0xngmi@protonmail.com>
2024-01-25 04:03:23 +0000

    dydx v4

---
+++ dexs/dydx/v4.ts
@@ -0,0 +1,25 @@
+import fetchURL from "../../utils/fetchURL"
+import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+
+const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
+  const markets = (await fetchURL("https://indexer.dydx.trade/v4/perpetualMarkets")).data.markets;
+  const dailyolume = Object.values(markets).reduce((a: number, b: any) => a+Number(b.volume24H), 0)
+  const dailyOpenInterest = Object.values(markets).reduce((a: number, b: any) => a+(Number(b.openInterest)*Number(b.oraclePrice)), 0)
+  return {
+    dailyVolume: dailyolume ? `${dailyolume}` : undefined,
+    dailyOpenInterest: dailyOpenInterest ? `${dailyOpenInterest}` : undefined,
+    timestamp: timestamp,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    "dydx": {
+      runAtCurrTime:true,
+      fetch,
+      start: async () => 1614211200,
+    },
+  },
+};
+
+export default adapter;
