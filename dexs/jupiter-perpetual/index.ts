10567e9fda0b78f6cbf95640a380edae30e2cce8
0xgnek <0xgnek@gmail.com>
2024-01-30 12:42:41 +0000

    set comment

--- dexs/jupiter-perpetual/index.ts
+++ dexs/jupiter-perpetual/index.ts
@@ -6,6 +6,7 @@ import { queryDune } from "../../helpers/dune";
 const fetch = async (timestamp: number): Promise<FetchResult> => {
   const unixTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
   try {
+    // 3385640 old query id
     const data = await queryDune("3391484", { endTime: unixTimestamp + 86400 });
     return {
       dailyVolume:  data?.volume || "0",
