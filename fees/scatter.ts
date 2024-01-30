04cb639701c7603137f2b004b84195409aa03e9a
0xgnek <0xgnek@gmail.com>
2024-01-30 16:15:56 +0000

    create helper

--- fees/scatter.ts
+++ fees/scatter.ts
@@ -4,32 +4,8 @@ import { getBlock } from '../helpers/getBlock';
 import { CHAIN } from '../helpers/chains';
 import { getPrices } from '../utils/prices';
 import fetchURL from '../utils/fetchURL';
-import postgres from 'postgres';
+import { indexa, toBytea } from '../helpers/db';
 
-export const createInstance = (connectionString: string) =>
-  postgres(connectionString, {
-    transform: {
-      undefined: null,
-    },
-    types: {
-      bytea: {
-        to: 17,
-        from: [17],
-        serialize: (x: any) => "\\x" + Buffer.from(x).toString("hex"),
-        parse: (x: any) => Buffer.from(x.slice(2), "hex"),
-      },
-    },
-  });
-
-export const indexa = createInstance(process.env.INDEXA_DB!);
-
-export const toBytea = (address: string | Buffer) => {
-  if (typeof address === "string") {
-    return indexa.typed.bytea(Buffer.from(address.slice(2), "hex"));
-  } else {
-    return indexa.typed.bytea(address);
-  }
-};
 
 const graph = (chain: Chain) => {
   return async (timestamp: number): Promise<FetchResultFees> => {
