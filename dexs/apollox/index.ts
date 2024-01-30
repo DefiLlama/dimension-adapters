b2db5a47e5626517a23c9827e78da4973a95fbe5
AQVYmeCkJsFTFWpMAHmA <105714995+AQVYmeCkJsFTFWpMAHmA@users.noreply.github.com>
2024-01-30 17:32:32 +0800

    feat: update api exclude cake volumne

--- dexs/apollox/index.ts
+++ dexs/apollox/index.ts
@@ -1,29 +1,36 @@
 import axios from "axios";
-import BigNumber from 'bignumber.js'
+import BigNumber from "bignumber.js";
 import { Chain } from "@defillama/sdk/build/general";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 type ResponseItem = {
-  symbol: string
-  baseAsset: string
-  qouteAsset: string
-  productType: string
-  lastPrice: number
-  low: number
-  high: number
-  baseVol: number
-  qutoVol: number
-  openInterest: number
-}
+  symbol: string;
+  baseAsset: string;
+  qouteAsset: string;
+  productType: string;
+  lastPrice: number;
+  low: number;
+  high: number;
+  baseVol: number;
+  qutoVol: number;
+  openInterest: number;
+};
 
-const VolumeAPI = 'https://www.apollox.finance/bapi/future/v1/public/future/apx/pair'
+const VolumeAPI =
+  "https://www.apollox.finance/bapi/future/v1/public/future/apx/pair";
 
 const fetchVolume = (chain: Chain) => {
   return async (timestamp: number) => {
     try {
-      const { data = [] } = (await axios.get(VolumeAPI, { params: { chain } })).data as { data: ResponseItem[]};
-      const dailyVolume = data.reduce((p, c) => p.plus(c.qutoVol), new BigNumber(0))
+      const { data = [] } = (
+        await axios.get(VolumeAPI, { params: { chain, excludeCake: true } })
+      ).data as { data: ResponseItem[] };
+
+      const dailyVolume = data.reduce(
+        (p, c) => p.plus(c.qutoVol),
+        new BigNumber(0)
+      );
 
       return {
         dailyVolume: dailyVolume.toString(),
