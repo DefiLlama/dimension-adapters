5190087f56e705bf77f332e3ba8b165ca0e99936
hyperionX2024 <157451025+hyperionX2024@users.noreply.github.com>
2024-02-02 17:51:09 +0800

    add header

--- dexs/hyperionx/index.ts
+++ dexs/hyperionx/index.ts
@@ -4,6 +4,7 @@ import { request, gql, GraphQLClient } from "graphql-request";
 import type { ChainEndpoints } from "../../adapters/types";
 import { Chain } from "@defillama/sdk/build/general";
 
+const headers = { 'sex-dev': 'ServerDev'}
 
 const endpoints = {
   [CHAIN.ZKFAIR]: 'https://gql.hyperionx.xyz/subgraphs/name/hyperionx/zkfair',
@@ -46,8 +47,12 @@ const graphs = (graphUrls: ChainEndpoints) => {
           }
         `;
 
-        const blockNumberGraphQLClient = new GraphQLClient(blockNumberGraph[chain]);
-        const graphQLClient = new GraphQLClient(graphUrls[chain]);
+        const blockNumberGraphQLClient = new GraphQLClient(blockNumberGraph[chain], {
+          headers: headers,
+        });
+        const graphQLClient = new GraphQLClient(graphUrls[chain], {
+          headers: headers,
+        });
 
 
         const blockNumber = (
