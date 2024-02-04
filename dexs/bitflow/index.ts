b130d05bdcd2b739f2e36a309ea0a239969a8513
Rodrigo Eduardo <rodrigoeduardodb1@gmail.com>
2024-02-04 14:37:33 -0300

    fix: bitflow adapter

--- dexs/bitflow/index.ts
+++ dexs/bitflow/index.ts
@@ -3,55 +3,49 @@ import { Chain } from "@defillama/sdk/build/general";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import customBackfill from "../../helpers/customBackfill";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 const historicalVolumeEndpoint = "https://app.bitflow.finance/api/totalVolume";
+
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
 
 interface Timestamp {
   _seconds: number;
   _nanoseconds: number;
 }
 
+type LPTokenTVL = {
+  totalShares: number;
+  dollarPrice: number;
+  totalSharesInDollars: number;
+};
+
+type TVL = {
+  pairs: Record<string, LPTokenTVL>;
+  total: number;
+};
+
 interface HistoricalTotalVolume {
   date: Timestamp;
-  tvl: number;
+  tvl: TVL;
 }
 
-const getHistoricalTVL = async (): Promise<HistoricalTotalVolume[]> => {
-  return (await fetchURL(historicalVolumeEndpoint)).data.data;
-};
-
-const getDateFromTimestamp = (timestamp: Timestamp) => {
-  return new Date(timestamp._seconds * 1000);
-};
-
 const fetch = async (timestamp: number) => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-
   const historicalVolume: HistoricalTotalVolume[] = await getHistoricalTVL();
 
-  const totalVolume = historicalVolume
-    .filter(
-      (volItem) =>
-        getDateFromTimestamp(volItem.date).getTime() / 1000 <= dayTimestamp
-    )
-    .reduce((acc, { tvl }) => acc + tvl, 0);
+  const totalVolume = historicalVolume[historicalVolume.length - 1].tvl.total;
 
-  const dailyVolume = historicalVolume.find(
-    (dayItem) =>
-      getDateFromTimestamp(dayItem.date).getTime() / 1000 === dayTimestamp
-  )?.tvl;
+  const dailyVolume = getDailyVolume(timestamp, historicalVolume);
 
   return {
     totalVolume: `${totalVolume}`,
-    dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
-    timestamp: dayTimestamp,
+    dailyVolume: `${dailyVolume ?? 0}`,
+    timestamp,
   };
 };
 
 const getStartTimestamp = async () => {
   const historicalVolume: HistoricalTotalVolume[] = await getHistoricalTVL();
-  return getDateFromTimestamp(historicalVolume[0].date).getTime() / 1000;
+  return historicalVolume[0].date._seconds;
 };
 
 const adapter: SimpleAdapter = {
@@ -68,3 +62,28 @@ const adapter: SimpleAdapter = {
 };
 
 export default adapter;
+
+const getHistoricalTVL = async (): Promise<HistoricalTotalVolume[]> => {
+  return (await fetchURL(historicalVolumeEndpoint)).data.data;
+};
+
+const getDailyVolume = async (
+  timestamp: number,
+  historicalVolume: HistoricalTotalVolume[]
+) => {
+  const startOfDay = timestamp - ONE_DAY_IN_SECONDS;
+  const endOfDay = timestamp;
+
+  const dayTVL = historicalVolume.find(
+    (dayItem) =>
+      dayItem.date._seconds > startOfDay && dayItem.date._seconds <= endOfDay
+  );
+
+  if (!dayTVL) return;
+
+  const dayTVLIndex = historicalVolume.indexOf(dayTVL) - 1;
+
+  const dayBeforeTVL = historicalVolume[dayTVLIndex >= 0 ? dayTVLIndex : 0];
+
+  return dayTVL.tvl.total - dayBeforeTVL.tvl.total;
+};
