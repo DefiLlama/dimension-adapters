cc5826bb90cae0567d33365af2246efb12519578
rohith <rohithkumar0369@gmail.com>
2024-02-05 17:14:43 +0530

    status code error fixed

--- aggregators/kanalabs/index.ts
+++ aggregators/kanalabs/index.ts
@@ -2,24 +2,76 @@ import fetchURL from "../../utils/fetchURL";
 import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const URL = "https://stats.kanalabs.io/volume";
+const URL = "https://stats.kanalabs.io/transaction/volume";
+// const URL = "https://stats.kanalabs.io/volume";
 
-const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
-  const dailyVolume = (await fetchURL(`${URL}?chainId=1`)).data.data;
-  return {
-    timestamp,
-    dailyVolume: dailyVolume.today.volume,
-  };
+export enum KanaChainID {
+  "solana" = 1,
+  "aptos" = 2,
+  "polygon" = 3,
+  "ethereum" = 4,
+  "bsc" = 5,
+  "klaytn" = 6,
+  "sui" = 8,
+  "Arbitrum" = 9,
+  "Avalanche" = 10,
+  "zkSync" = 11,
+  "base" = 12,
+}
+
+const fetch = (chain: KanaChainID) => async (timestamp: number) => {
+  try {
+    const res = (await fetchURL(`${URL}?chainId=${chain}`)).data;
+    const data = res.data
+    console.log(res);
+    return {
+      timestamp,
+      dailyVolume: data.today.volume,
+      totalVolume: data.totalVolume.volume,
+    };
+  } catch (err) {
+    console.log(err)
+    return {
+      timestamp,
+      dailyVolume: "0",
+      totalVolume: "0",
+    };
+  }
 };
 
 // Define the adapter
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.ETHEREUM]: {
-      fetch,
-      runAtCurrTime: true,
+      fetch: fetch(KanaChainID.ethereum),
+      runAtCurrTime: false,
       start: async () => 1695897839,
     },
+    // [CHAIN.BSC]: {
+    //   fetch: fetch(KanaChainID.bsc),
+    //   runAtCurrTime: true,
+    //   start: async () => 1695897839,
+    // },
+    // [CHAIN.AVAX]: {
+    //   fetch: fetch(KanaChainID.Avalanche),
+    //   runAtCurrTime: true,
+    //   start: async () => 1695897839,
+    // },
+    // [CHAIN.ARBITRUM]: {
+    //   fetch: fetch(KanaChainID.Arbitrum),
+    //   runAtCurrTime: true,
+    //   start: async () => 1695897839,
+    // },
+    // [CHAIN.POLYGON]: {
+    //   fetch: fetch(KanaChainID.polygon),
+    //   runAtCurrTime: true,
+    //   start: async () => 1695897839,
+    // },
+    // [CHAIN.ZKSYNC]: {
+    //   fetch: fetch(KanaChainID.zkSync),
+    //   runAtCurrTime: true,
+    //   start: async () => 1695897839,
+    // },
   },
 };
 
