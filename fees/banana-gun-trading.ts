1c2477f6e1f3fa6ad5db94b2d2d3945c04abfdb5
0xgnek <0xgnek@gmail.com>
2024-02-08 17:11:44 +0000

    remove runcurrent

--- fees/banana-gun-trading.ts
+++ fees/banana-gun-trading.ts
@@ -89,7 +89,7 @@ const adapter: SimpleAdapter = {
     },
     [CHAIN.SOLANA]: {
       fetch: fethcFeesSolana,
-      runAtCurrTime: true,
+      // runAtCurrTime: true,
       start: async () => 1685577600,
     },
   },
