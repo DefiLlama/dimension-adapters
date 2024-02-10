d3598cd5719a951570eee5dad8921642fc7de098
0xgnek <0xgnek@gmail.com>
2024-02-10 17:36:00 +0000

    fix pass api

--- fees/aerodrome/index.ts
+++ fees/aerodrome/index.ts
@@ -10,13 +10,13 @@ const abis: any = {
   "forSwaps": "function forSwaps() view returns ((address lp, bool stable, address token0, address token1, address factory)[])"
 }
 
-const fetch = async (timestamp: number): Promise<FetchResultFees> => {
+const fetch = async (timestamp: number, _, fetchOptions): Promise<FetchResultFees> => {
   const fromTimestamp = timestamp - 60 * 60 * 24
   const toTimestamp = timestamp
   const forSwaps = await sdk.api2.abi.call({ target: gurar, abi: abis.forSwaps, chain: CHAIN.BASE, })
   const pools = forSwaps.map((e: any) => e.lp)
 
-  const res: any = await getDexFees({ chain: CHAIN.BASE, fromTimestamp, toTimestamp, pools, timestamp, })
+  const res: any = await getDexFees({ chain: CHAIN.BASE, fromTimestamp, toTimestamp, pools, timestamp, fetchOptions })
   const fromBlock = (await getBlock(fromTimestamp, CHAIN.BASE, {}));
   const toBlock = (await getBlock(toTimestamp, CHAIN.BASE, {}));
   const dailyBribesRevenue = await fees_bribes(fromBlock, toBlock, timestamp);
