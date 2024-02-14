4b07954e20b827125af8ab68e2071808bc57b82d
0xgnek <0xgnek@gmail.com>
2024-02-14 16:01:52 +0000

    fix duplicate

--- dexs/empiredex/index.ts
+++ dexs/empiredex/index.ts
@@ -5,7 +5,6 @@ export default univ2Adapter({
   [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/trnhgquan/empiredexbsc",
   [CHAIN.XDAI]: "https://api.thegraph.com/subgraphs/name/zikyfranky/empire-xdai",
   [CHAIN.POLYGON]: "https://api.thegraph.com/subgraphs/name/zikyfranky/empire-polygon",
-  [CHAIN.POLYGON]: "https://api.thegraph.com/subgraphs/name/zikyfranky/empire-exchange",
   [CHAIN.AVAX]: "https://api.thegraph.com/subgraphs/name/zikyfranky/empire-subgraph-avax",
 }, {
 });
