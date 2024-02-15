17a7af426913ddc8240c2b2496ef8e6ebb7142e5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-15 08:57:59 +0100

    Refactor fetch function in swapline/index.ts

--- dexs/swapline/index.ts
+++ dexs/swapline/index.ts
@@ -1,55 +1,40 @@
 import fetchURL from "../../utils/fetchURL"
-import { SimpleAdapter } from "../../adapters/types";
+import { ChainBlocks, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { Chain } from "@defillama/sdk/build/general";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 //const historicalVolumeEndpoint = "https://api.swapline.com/api/v1/protocol-chartdata?aggregate=true"
-const historicalVolumeEndpoint = "https://api.swapline.com/api/v1/protocol-chartdata?chainId=";
+const historicalVolumeEndpoint = "https://api-c.swapline.com/api/v1/protocol-chartdata?chainId=";
 
 interface IVolumeall {
   volumeUSD: number;
   date: number;
 }
 
-const graph = (_chain: number) => {
-	return async (timestamp: number) => {
-  		const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  		const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint+_chain))[0]?.chainEntries;
-  		const totalVolume = historicalVolume
-    		.filter(volItem => volItem.date <= dayTimestamp)
-    		.reduce((acc, { volumeUSD }) => acc + Number(volumeUSD), 0)
+const fetch = async (_timestamp: number , _: ChainBlocks, { startOfDay,api }: FetchOptions) => {
+  const dayTimestamp = startOfDay
+  const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint + api.getChainId()))[0]?.chainEntries;
+  const totalVolume = historicalVolume
+    .filter(volItem => volItem.date <= dayTimestamp)
+    .reduce((acc, { volumeUSD }) => acc + Number(volumeUSD), 0)
 
-  		const dailyVolume = historicalVolume
-    		.find(dayItem =>  dayItem.date  === dayTimestamp)?.volumeUSD
+  const dailyVolume = historicalVolume
+    .find(dayItem => dayItem.date === dayTimestamp)?.volumeUSD
 
-  		return {
-    		totalVolume: `${totalVolume}`,
-    		dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
-    		timestamp: dayTimestamp,
-  		}
-	}
+  return {
+    totalVolume: totalVolume,
+    dailyVolume: dailyVolume,
+    timestamp: dayTimestamp,
+  }
 }
 
+const fetchObject = { fetch, start:1680048000 }
 
 const adapter: SimpleAdapter = {
   adapter: {
-    [CHAIN.FANTOM]: {
-      fetch: graph(250),
-      start: 1680048000,
-    },
-    [CHAIN.OPTIMISM]: {
-      fetch: graph(10),
-      start: 1680048000,
-    },
-    [CHAIN.ARBITRUM]: {
-      fetch: graph(42161),
-      start: 1680048000,
-    },
-    [CHAIN.SHIMMER_EVM]: {
-      fetch: graph(148),
-      start: 1680048000,
-    },
+    [CHAIN.FANTOM]: fetchObject,
+    [CHAIN.OPTIMISM]: fetchObject,
+    [CHAIN.ARBITRUM]: fetchObject,
+    [CHAIN.SHIMMER_EVM]: fetchObject,
   },
 };
 
