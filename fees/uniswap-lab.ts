4c1d2a14ebdfef3c57c1b7c2193e2e12e8f4b3e7
0xngmi <80541789+0xngmi@users.noreply.github.com>
2024-02-19 16:10:50 +0000

    add missing uniswap address

--- fees/uniswap-lab.ts
+++ fees/uniswap-lab.ts
@@ -4,7 +4,7 @@ import { CHAIN } from "../helpers/chains";
 import { addTokensReceived } from '../helpers/token';
 
 const config: any = {
-  ethereum: { tokens: [ADDRESSES.ethereum.USDC], targets: ['0x163c5e051049e92915017fe7bb9b8ce6182bcbb1', '0x6460d14dbaeb27aefec8ebef85db35defa31c3b9'] },
+  ethereum: { tokens: [ADDRESSES.ethereum.USDC], targets: ['0x163c5e051049e92915017fe7bb9b8ce6182bcbb1', '0x6460d14dbaeb27aefec8ebef85db35defa31c3b9', '0x27213E28D7fDA5c57Fe9e5dD923818DBCcf71c47'] },
   optimism: { tokens: [ADDRESSES.optimism.USDC_CIRCLE], targets: ['0xd4ce1f1b8640c1988360a6729d9a73c85a0c80a3'] },
   polygon: { tokens: [ADDRESSES.polygon.USDC, ADDRESSES.polygon.USDC_CIRCLE], targets: ['0xce946931adf7afc0797de2a76270a28458f487ed'] },
   arbitrum: { tokens: [ADDRESSES.arbitrum.USDC_CIRCLE], targets: ['0xd4ce1f1b8640c1988360a6729d9a73c85a0c80a3'] },
