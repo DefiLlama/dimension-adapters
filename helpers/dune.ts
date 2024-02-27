e37370f08a673c3fd934c5920ddf009bf0dada2b
0xgnek <0xgnek@gmail.com>
2024-02-27 18:38:53 +0000

    fix dune quer

--- helpers/dune.ts
+++ helpers/dune.ts
@@ -10,98 +10,137 @@ const API_KEYS =getEnv('DUNE_API_KEYS')?.split(',') ?? ["L0URsn5vwgyrWbBpQo9yS1E
 let API_KEY_INDEX = 0;
 
 const MAX_RETRIES = 6 * API_KEYS.length + 3;
+const NOW_TIMESTAMP = Math.trunc((Date.now()) / 1000)
 
-export async function queryDune(queryId: string, query_parameters = {}) {
-  /* const error = new Error("Dune: queryId is required")
-  delete error.stack
-  throw error */
-  return await retry(
-    async (bail, attempt: number) => {
-      const API_KEY = API_KEYS[API_KEY_INDEX]
-      let query: undefined | any = undefined
-      if (!token[queryId]) {
-        try {
-          query = await limit(() => httpPost(`https://api.dune.com/api/v1/query/${queryId}/execute`, { query_parameters }, {
-            headers: {
-              "x-dune-api-key": API_KEY,
-              'Content-Type': 'application/json'
-            }
-          }))
-          if (query?.execution_id) {
-            token[queryId] = query?.execution_id
-          } else {
-            console.log("error query data", query)
-            throw query
-          }
-        } catch (e: any) {
+const getLatestData = async (queryId: string) => {
+  const url = `https://api.dune.com/api/v1/query/${queryId}/results`
+  try {
+    const latest_result = (await limit(() => httpGet(url, {
+      headers: {
+        "x-dune-api-key": API_KEYS[API_KEY_INDEX]
+      }
+    })))
+    const submitted_at = latest_result.submitted_at
+    const submitted_at_timestamp = Math.trunc(new Date(submitted_at).getTime() / 1000)
+    const diff = NOW_TIMESTAMP - submitted_at_timestamp
+    if (diff < 60 * 60 * 3) {
+      return latest_result.result.rows
+    }
+    return undefined
+  } catch (e: any) {
+    return null
+  }
+}
+
+const inquiryStatus = async (queryId: string) => {
+  let _status = undefined;
+  do {
+    try {
+      _status = (await limit(() => httpGet(`https://api.dune.com/api/v1/execution/${token[queryId]}/status`, {
+        headers: {
+          "x-dune-api-key": API_KEYS[API_KEY_INDEX]
+        }
+      }))).state
+    } catch (e: any) {
+      return 'QUERY_STATE_FAILED';
+    }
+
+  } while (_status !== 'QUERY_STATE_COMPLETED' && _status !== 'QUERY_STATE_FAILED');
+  return _status
+}
+
+const submitQuery = async (queryId: string, query_parameters = {}) => {
+    let query: undefined | any = undefined
+    if (!token[queryId]) {
+    try {
+      query = await limit(() => httpPost(`https://api.dune.com/api/v1/query/${queryId}/execute`, { query_parameters }, {
+        headers: {
+          "x-dune-api-key": API_KEYS[API_KEY_INDEX],
+          'Content-Type': 'application/json'
+        }
+      }))
+      if (query?.execution_id) {
+        token[queryId] = query?.execution_id
+      } else {
+        console.log("error query data", query)
+        return null
+      }
+    } catch (e: any) {
+      return null;
+    }
+  }
+}
+
+
+export const queryDune = async (queryId: string, query_parameters = {}) => {
+  return await retry(async (bail: any, attempt: number) => {
+    if (Object.keys(query_parameters).length === 0) {
+      let latest_result = undefined
+      while (latest_result === undefined) {
+        latest_result = await getLatestData(queryId)
+        if (latest_result === undefined) break;
+        if (latest_result === null) {
           if (API_KEY_INDEX < API_KEYS.length - 1) {
             console.error('api key out of limit waiting retry next key')
             API_KEY_INDEX = API_KEY_INDEX + 1
+            latest_result = undefined
           } else {
-            const error = new Error(`Dune: there is no more api key`)
-            console.error(error.message, e?.message)
-            bail(error)
-            throw error
+            bail(new Error("GetLatestData Dune: there is no more api key"))
           }
         }
       }
+      return latest_result
+    }
 
-      if (!token[queryId]) {
-        const error = new Error("Couldn't get a token from dune")
-        delete error.stack
-        throw error
+    let execute = undefined;
+    while (execute === undefined) {
+      execute = await submitQuery(queryId, query_parameters)
+      if (execute === null) {
+        if (API_KEY_INDEX < API_KEYS.length - 1) {
+          console.error('api key out of limit waiting retry next key')
+          API_KEY_INDEX = API_KEY_INDEX + 1
+          execute = undefined
+        } else {
+          bail(new Error("submitQuery Dune: there is no more api key"))
+        }
       }
+    }
 
-      let queryStatus = undefined
+    let _status = undefined
+    while (_status === undefined) {
+      _status = await inquiryStatus(queryId)
+      if (_status === 'QUERY_STATE_FAILED') {
+        if (API_KEY_INDEX < API_KEYS.length - 1) {
+          console.error('api key out of limit waiting retry next key')
+          API_KEY_INDEX = API_KEY_INDEX + 1
+          _status = undefined
+        } else {
+          bail(new Error("inquiryStatus Dune: there is no more api key"))
+        }
+      }
+      if (_status === 'QUERY_STATE_COMPLETED') {
+        break;
+      }
+      if (MAX_RETRIES === attempt) {
+        bail(new Error("Dune query cancelled"))
+      }
+    }
+
+    if (_status === 'QUERY_STATE_COMPLETED') {
+      const API_KEY = API_KEYS[API_KEY_INDEX]
       try {
-        queryStatus = await limit(() => httpGet(`https://api.dune.com/api/v1/execution/${token[queryId]}/results?limit=5&offset=0`, {
+        const queryStatus = await limit(() => httpGet(`https://api.dune.com/api/v1/execution/${token[queryId]}/results?limit=5&offset=0`, {
           headers: {
             "x-dune-api-key": API_KEY
           }
         }))
-      } catch (e: any) {
-        if (API_KEY_INDEX < API_KEYS.length - 1) {
-          API_KEY_INDEX = API_KEY_INDEX + 1
-        } else {
-          const error = new Error(`there is no more api key`)
-          console.error('there is no more api key')
-          bail(error)
-          throw e
-        }
-      }
-
-      if (!queryStatus) {
-        const error_query_status_undefined = new Error("Query status is undefined")
-        throw error_query_status_undefined;
-      }
-
-
-      const status = queryStatus.state
-      if (["QUERY_STATE_PENDING", "QUERY_STATE_EXECUTING"].includes(status) && MAX_RETRIES === attempt) {
-        const url = `https://api.dune.com/api/v1/execution/${token[queryId]}/cancel`
-        await httpPost(url, {}, {
-          headers: {
-            "x-dune-api-key": API_KEY
-          }
-        })
-        console.error('Dune query cancelled', token[queryId])
-        bail(new Error("Dune query cancelled"))
-        throw new Error("Dune query cancelled")
-      }
-      if (status === "QUERY_STATE_COMPLETED") {
         return queryStatus.result.rows
-      } else if (status === "QUERY_STATE_FAILED") {
-        console.log(queryStatus)
-        const error = new Error("Dune query failed")
-        bail(error)
-        throw error
+      } catch (e: any) {
+        bail(new Error("ex Dune: error query data: " + e.message))
       }
-      throw new Error("Still running")
-    },
-    {
-      retries: MAX_RETRIES,
-      minTimeout: 1000 * 2,
-      maxTimeout: 1000 * 60 * 5
     }
-  );
+  }, {
+    retries: MAX_RETRIES,
+    maxTimeout: 1000 * 60 * 5
+  })
 }
