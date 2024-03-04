356db0be4fc670be6e73998e7bdbf6bef8016a97
0xgnek <0xgnek@gmail.com>
2024-03-04 11:32:21 +0000

    fix url

--- dexs/metavault-v3/index.ts
+++ dexs/metavault-v3/index.ts
@@ -6,8 +6,7 @@ import { BreakdownAdapter } from "../../adapters/types";
 const endpointsV3 = {
   [CHAIN.LINEA]:
     "https://linea-graph-node.metavault.trade/subgraphs/name/metavault/v3",
-  [CHAIN.SCROLL]:
-    "https://api.studio.thegraph.com/query/55804/metavault-v3/version/latest",
+  [CHAIN.SCROLL]: "https://api.studio.thegraph.com/query/55804/metavault-v3/version/latest",
 };
 
 const v3Graphs = getGraphDimensions({
