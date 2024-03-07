862194f775ccff8943feda09ba9d72a8dbb81e0a
0xngmi <0xngmi@protonmail.com>
2024-03-07 02:09:51 +0000

    fix

--- fees/stars-arena.ts
+++ fees/stars-arena.ts
@@ -6,7 +6,7 @@ const fetch = async ({createBalances, getLogs}: FetchOptions) => {
   const dailyRevenue = createBalances()
   const logs = await getLogs({
     topic: '0xc9d4f93ded9b42fa24561e02b2a40f720f71601eb1b3f7b3fd4eff20877639ee',
-    target: '0xC605C2cf66ee98eA925B1bb4FeA584b71C00cC4C',
+    targets: ['0xC605C2cf66ee98eA925B1bb4FeA584b71C00cC4C', '0x69B7F08B2952e2EE3CA4222190BCF07831f1096f'],
     eventAbi: 'event Trade (address trader, address subject, bool isBuy, uint256 shareAmount, uint256 amount, uint256 protocolAmount, uint256 subjectAmount, uint256 referralAmount, uint256 supply, uint256 buyPrice, uint256 myShares)'
   });
   logs.map((log: any) => {
