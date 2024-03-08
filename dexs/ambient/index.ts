f7acd43d5e78158f2de1fb79a494338b4faba9fb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-08 21:32:24 +0100

    ambient: use indexer

--- dexs/ambient/index.ts
+++ dexs/ambient/index.ts
@@ -1,84 +1,51 @@
-import { Adapter, ChainBlocks, FetchOptions, FetchResultVolume } from "../../adapters/types";
+import PromisePool from "@supercharge/promise-pool";
+import { FetchV2 } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { Chain } from "@defillama/sdk/build/general";
-import { request, } from "graphql-request";
+import { httpGet } from "../../utils/fetchURL";
 
-type TEndpoint = {
-  [s: Chain | string]: string;
-}
-const endpoints: TEndpoint = {
-  [CHAIN.ETHEREUM]: "https://api.thegraph.com/subgraphs/name/crocswap/croc-mainnet",
-}
-interface IPool {
-  quote: string;
-}
-interface ISwap {
-  quoteFlow: string;
-  pool: IPool;
-  dex: string;
+const config = {
+  scroll: { endpoint: 'https://ambindexer.net/scroll-gcgo/', chainId: '0x82750', poolIdx: '420' },
+  blast: { endpoint: 'https://ambindexer.net/blast-gcgo/', chainId: '0x13e31', poolIdx: '420' },
+  ethereum: { endpoint: 'https://ambindexer.net/gcgo/', chainId: '0x1', poolIdx: '420' },
+  canto: { endpoint: 'https://ambient-graphcache.fly.dev/gcgo/', chainId: '0x1e14', poolIdx: '420' },
 }
 
-const toPositive = (n: any) => +n > 0 ? +n : n * -1
+const fetch: FetchV2 = async ({ startTimestamp, endTimestamp, createBalances, chain }) => {
+  const dailyVolume = createBalances()
+  const dailyFees = createBalances()
+  const { poolIdx, chainId, endpoint, } = config[chain]
+  const { data } = await httpGet(endpoint + 'pool_list', { params: { poolIdx, chainId } })
 
-const graphs = (chain: Chain) => {
-  return async (timestamp: number, _: ChainBlocks, { fromTimestamp, toTimestamp, createBalances, }: FetchOptions): Promise<FetchResultVolume> => {
-    const query = `
-          {
-            swaps(where: {
-              time_gte: ${fromTimestamp}
-              time_lte: ${toTimestamp}
-              dex: "croc"
-            }, orderBy:time, orderDirection: desc) {
-              quoteFlow
-              pool {
-                quote
-              }
-              dex
-            }
-          }
-        `
-    const graphRes: ISwap[] = (await request(endpoints[chain], query)).swaps;
-    const dailyVolume = createBalances()
-    graphRes.map((e: ISwap) => {
-      dailyVolume.add(e.pool.quote, toPositive(e.quoteFlow))
+  const { errors } = await PromisePool
+    .withConcurrency(10)
+    .for(data)
+    .process(async ({ base, quote }: any) => {
+      const { data, } = await httpGet(endpoint + 'pool_stats', { params: { poolIdx, chainId, base, quote, histTime: endTimestamp, } })
+      const { data: dataOld } = await httpGet(endpoint + 'pool_stats', { params: { poolIdx, chainId, base, quote, histTime: startTimestamp, } })
+
+      dailyVolume.add(base, data.baseVolume)
+      dailyVolume.add(quote, data.quoteVolume)
+      dailyVolume.subtractToken(base, dataOld.baseVolume)
+      dailyVolume.subtractToken(quote, dataOld.quoteVolume)
+
+      dailyFees.add(base, data.baseVolume * data.feeRate)
+      dailyFees.add(quote, data.quoteVolume * data.feeRate)
+      dailyFees.subtractToken(base, dataOld.baseVolume * data.feeRate)
+      dailyFees.subtractToken(quote, dataOld.quoteVolume * data.feeRate)
     })
-    return { dailyVolume, timestamp, }
-  }
+  if (errors?.length) throw errors
+  return { dailyVolume, dailyFees, }
 }
 
-const swapEvent = 'event CrocSwap (address indexed base, address indexed quote, uint256 poolIdx, bool isBuy, bool inBaseQty, uint128 qty, uint16 tip, uint128 limitPrice, uint128 minOut, uint8 reserveFlags, int128 baseFlow, int128 quoteFlow)';
+const adapter = { fetch, runAtCurrTime: true, start: 1685232000, }
 
-type TContractAddress = {
-  [s: Chain]: string;
-}
-interface ILog {
-  quote: string;
-  quoteFlow: string;
-}
-const contract_address: TContractAddress = {
-  [CHAIN.SCROLL]: '0xaaaaaaaacb71bf2c8cae522ea5fa455571a74106',
-}
 
-const fetchVolume = (chain: Chain) => {
-  return async (timestamp: number, _: ChainBlocks, { getLogs, createBalances, }: FetchOptions): Promise<FetchResultVolume> => {
-    const dailyVolume = createBalances()
-    const logs: ILog[] = await getLogs({ target: contract_address[chain], eventAbi: swapEvent, })
-    logs.forEach((log: ILog) => dailyVolume.add(log.quote, Number(log.quoteFlow) < 0 ? 0 : log.quoteFlow));
-    return { dailyVolume, timestamp, }
-  }
-}
-
-const adapter: Adapter = {
+export default {
   adapter: {
-    [CHAIN.ETHEREUM]: {
-      fetch: graphs(CHAIN.ETHEREUM),
-      start: 1685232000,
-    },
-    [CHAIN.SCROLL]: {
-      fetch: fetchVolume(CHAIN.SCROLL),
-      start: 1685232000,
-    },
-  }
-}
-
-export default adapter;
+    [CHAIN.ETHEREUM]: adapter,
+    [CHAIN.SCROLL]: adapter,
+    [CHAIN.CANTO]: adapter,
+    [CHAIN.BLAST]: adapter,
+  },
+  version: 2,
+};
