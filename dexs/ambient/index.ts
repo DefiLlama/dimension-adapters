46822bbd1b5ec6b33ceb1e0354a3970f7acf91cb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-09 11:53:33 +0100

    Remove unnecessary property from adapter object

--- dexs/ambient/index.ts
+++ dexs/ambient/index.ts
@@ -37,7 +37,7 @@ const fetch: FetchV2 = async ({ startTimestamp, endTimestamp, createBalances, ch
   return { dailyVolume, dailyFees, }
 }
 
-const adapter = { fetch, runAtCurrTime: true, start: 1685232000, }
+const adapter = { fetch, start: 1685232000, }
 
 
 export default {
