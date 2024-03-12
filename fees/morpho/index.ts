64b82763b985340632ad1d30e1d9a95f4bb8daa4
waynebruce0x <waynebruce0x@gmail.com>
2024-03-12 16:56:19 +0000

    update

--- fees/morpho/index.ts
+++ fees/morpho/index.ts
@@ -1,6 +1,6 @@
 const { request, gql } = require("graphql-request");
+import { Balances } from "@defillama/sdk";
 import { FetchV2, SimpleAdapter } from "../../adapters/types";
-import { getPrices } from "../../utils/prices";
 
 type Market = {
   loanAsset: { address: string; decimals: number };
@@ -40,61 +40,35 @@ const apyToDaily = (apy: number): number =>
   (((apy / 100 + 1) ** (1 / 365) - 1) * 365) / (0.01 * 365);
 
 const fetch: FetchV2 = async (options) => {
+  const chain = "ethereum";
   const res = await request("https://blue-api.morpho.org/graphql", query);
 
   let marketData: {
-    [asset: string]: {
-      dailyLendFee: number;
-      dailyBorrowFee: number;
-    };
-  } = {};
+    dailySupplySideRevenue: Balances;
+    dailyUserFees: Balances;
+    dailyFees?: Balances;
+    dailyRevenue: Balances;
+  } = {
+    dailySupplySideRevenue: new Balances({ chain }),
+    dailyUserFees: new Balances({ chain }),
+    dailyRevenue: new Balances({ chain }),
+  };
 
   res.markets.items.map((m: Market) => {
     const { address } = m.loanAsset;
     const { supplyApy, borrowApy, supplyAssets, borrowAssets } = m.state;
-    if (!(address in marketData))
-      marketData[address] = {
-        dailyLendFee: 0,
-        dailyBorrowFee: 0,
-      };
 
-    marketData[address].dailyLendFee += supplyAssets * apyToDaily(supplyApy);
-    marketData[address].dailyBorrowFee += borrowAssets * apyToDaily(borrowApy);
+    const supply = supplyAssets * apyToDaily(supplyApy);
+    const borrow = borrowAssets * apyToDaily(borrowApy);
+
+    marketData.dailySupplySideRevenue.add(address, supply);
+    marketData.dailyUserFees.add(address, borrow);
+    marketData.dailyRevenue.add(address, borrow - supply);
   });
 
-  const prices = await getPrices(
-    Object.keys(marketData).map((t: string) => `ethereum:${t}`),
-    options.endTimestamp,
-  );
+  marketData.dailyFees = marketData.dailyUserFees;
 
-  let aggregateData: {
-    dailyRevenue: number;
-    dailySupplySideRevenue: number;
-    dailyUserFees: number;
-    dailyFees?: number;
-  } = {
-    dailyRevenue: 0,
-    dailySupplySideRevenue: 0,
-    dailyUserFees: 0,
-  };
-
-  Object.keys(marketData).map((token: string) => {
-    const priceData = prices[`ethereum:${token}`];
-    const supplySide =
-      (priceData.price * marketData[token].dailyLendFee) /
-      10 ** priceData.decimals;
-    const userFees =
-      (priceData.price * marketData[token].dailyBorrowFee) /
-      10 ** priceData.decimals;
-
-    aggregateData.dailySupplySideRevenue += supplySide;
-    aggregateData.dailyUserFees += userFees;
-    aggregateData.dailyRevenue += userFees - supplySide;
-  });
-
-  aggregateData.dailyFees = aggregateData.dailyUserFees;
-
-  return aggregateData;
+  return marketData;
 };
 
 const adapter: SimpleAdapter = {
