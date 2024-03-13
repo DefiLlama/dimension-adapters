dc964f3bdb82f74862daaf8eacbc0903988fdad9
slasher125 <slasher125@protonmail.com>
2024-03-13 16:47:51 +0400

    combine projects

--- users/list.ts
+++ users/list.ts
@@ -1,11 +1,9 @@
 import chains from "./chains"
-import routers from "./routers/uniswap-v2"
+import routers from "./routers/index"
 import protocolAddresses from "./routers/routerAddresses";
 import compoundV2, {addresses as compoundAddresses} from "./compound-v2";
-import other from "./other/index"
-import protocolAddressesOther from "./other/addresses"
 import { ExtraProtocolAddresses } from "./utils/types";
 
-export default routers.concat(chains as any[]).concat(compoundV2 as any[]).concat(other as any[])
+export default routers.concat(chains as any[]).concat(compoundV2 as any[])
 
-export const addressList = (protocolAddresses as ExtraProtocolAddresses[]).concat(compoundAddresses).concat(protocolAddressesOther)
+export const addressList = (protocolAddresses as ExtraProtocolAddresses[]).concat(compoundAddresses)
