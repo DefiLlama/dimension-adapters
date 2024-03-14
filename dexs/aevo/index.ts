8a93db5244bed66c8bd86f11d08089e97db3fcc4
Timothy <timongll@hotmail.com>
2024-03-15 01:31:46 +0800

    aevo endpoint update timestamp

--- dexs/aevo/index.ts
+++ dexs/aevo/index.ts
@@ -1,7 +1,7 @@
 import { SimpleAdapter } from "../../adapters/types";
 import fetchURL from "../../utils/fetchURL";
 import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphFees";
+import { getTimestampAtStartOfDayUTC, getTimestampAtStartOfNextDayUTC } from "../../utils/date";
 
 interface IAevoVolumeResponse {
   daily_volume: string;
@@ -10,14 +10,17 @@ interface IAevoVolumeResponse {
 
 // endTime is in nanoseconds
 export const aevoVolumeEndpoint = (endTime: number) => {
-  return "https://api.aevo.xyz/statistics?instrument_type=PERPETUAL&end_time=" + endTime;
-}
+  return (
+    "https://api.aevo.xyz/statistics?instrument_type=PERPETUAL&end_time=" +
+    endTime
+  );
+};
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.ETHEREUM]: {
       fetch: fetchAevoVolumeData,
-      start: 1681430400
+      start: 1681430400,
     },
   },
 };
@@ -26,8 +29,8 @@ export async function fetchAevoVolumeData(
   /** Timestamp representing the end of the 24 hour period */
   timestamp: number
 ) {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const url = aevoVolumeEndpoint(dayTimestamp * 1e9)
+  const dayTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+  const url = aevoVolumeEndpoint(dayTimestamp * 1e9);
   const aevoVolumeData = await getAevoVolumeData(url);
   const dailyVolume = Number(aevoVolumeData.daily_volume).toFixed(2);
   const totalVolume = Number(aevoVolumeData.total_volume).toFixed(2);
@@ -39,8 +42,10 @@ export async function fetchAevoVolumeData(
   };
 }
 
-async function getAevoVolumeData(endpoint: string): Promise<IAevoVolumeResponse> {
-  return (await fetchURL(endpoint));
+async function getAevoVolumeData(
+  endpoint: string
+): Promise<IAevoVolumeResponse> {
+  return await fetchURL(endpoint);
 }
 
 export default adapter;
