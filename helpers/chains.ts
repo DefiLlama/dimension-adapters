6096990ac89224ad819e58db8820346b331ffc58
0xgnek <0xgnek@gmail.com>
2024-03-15 15:15:58 +0000

    fix chain

--- helpers/chains.ts
+++ helpers/chains.ts
@@ -26,7 +26,7 @@ export enum CHAIN {
   CARDANO = "cardano",
   LITECOIN = "litecoin",
   BITCOIN = "bitcoin",
-  BITCOIN_CASH = "bitcoin_cash",
+  BITCOIN_CASH = "bitcoincashs",
   DOGECHAIN = "dogechain",
   ENERGI = "energi",
   VELAS = "velas",
