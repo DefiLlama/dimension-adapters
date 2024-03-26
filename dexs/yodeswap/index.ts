8b3b3bbd1842c1343481fc067ec2297d1ae6a364
0xgnek <0xgnek@gmail.com>
2024-03-26 18:49:29 +0000

    fix adapter

--- dexs/yodeswap/index.ts
+++ dexs/yodeswap/index.ts
@@ -1,6 +1,10 @@
 import { CHAIN } from "../../helpers/chains";
 import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 
-export default univ2Adapter({
+const adapter = univ2Adapter({
   [CHAIN.DOGECHAIN]: "https://graph.yodeswap.dog/subgraphs/name/yodeswap"
 }, {});
+
+adapter.adapter[CHAIN.DOGECHAIN].start = 1630000000;
+adapter.adapter[CHAIN.DOGECHAIN].fetch = async (timestamp: number) => { return { timestamp}};
+export default adapter;
