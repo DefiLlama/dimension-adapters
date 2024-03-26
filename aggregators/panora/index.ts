5fedda67708555f7f9acee727d110fde47778830
Nileshgarg13 <ng5787@srmist.edu.in>
2024-03-26 17:44:35 +0530

    changing the name from swapgpt to panora and updating the links

--- aggregators/panora/index.ts
+++ aggregators/panora/index.ts
@@ -1,28 +1,27 @@
-import fetchURL from "../../utils/fetchURL"
+import fetchURL from "../../utils/fetchURL";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-
-
 
 interface IVolumeall {
   grouptimestamp: string;
   amount: string;
 }
 
-const baseUrl = "https://stats.swapgpt.ai"
-const endpoint = "stats/getDefiLamaStats"
+const baseUrl = "https://stats.panora.exchange";
+const endpoint = "stats/getDefiLamaStats";
 
 
 const fetch = async (timestamp: number) => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const historicalVolume: IVolumeall[] = (await fetchURL(`${baseUrl}/${endpoint}`))?.volumeInUsd
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+  
+  const historicalVolume: IVolumeall[] = (await fetchURL(`${baseUrl}/${endpoint}`)).volumeInUsd;
   const totalVolume = historicalVolume
     .filter(volItem => (new Date(volItem.grouptimestamp).getTime() / 1000) <= dayTimestamp)
-    .reduce((acc, { amount }) => acc + Number(amount), 0)
+    .reduce((acc, { amount }) => acc + Number(amount), 0);
 
   const dailyVolume = historicalVolume
-    .find(dayItem => (new Date(dayItem.grouptimestamp).getTime() / 1000) === dayTimestamp)?.amount
+    .find(dayItem => (new Date(dayItem.grouptimestamp).getTime() / 1000) === dayTimestamp)?.amount;
 
   return {
     totalVolume: `${totalVolume}`,
@@ -31,11 +30,15 @@ const fetch = async (timestamp: number) => {
   };
 };
 
+const getStartTimestamp =  () => {
+  return (new Date('2023-11-28T00:00:00.000Z').getTime() / 1000);
+};
+
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.APTOS]: {
       fetch: fetch,
-      start: (new Date('2023-11-28T00:00:00.000Z').getTime() / 1000),
+      start: getStartTimestamp(),
     }
   },
 };
