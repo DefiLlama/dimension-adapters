ef117770c2f41bf35da0b8a0102ac861f2c9930b
mattt21 <matttawil@protonmail.com>
2024-04-01 20:03:14 -0500

    add koi-finance fees

--- dexs/mute.io/index.ts
+++ dexs/mute.io/index.ts
@@ -2,7 +2,7 @@ import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 import { CHAIN } from "../../helpers/chains";
 
 const endpoints = {
-  [CHAIN.ERA]: "https://api.studio.thegraph.com/query/12332/muteswitch---zksync-era/v0.0.5",
+  [CHAIN.ERA]: "https://api.goldsky.com/api/public/project_clmtie4nnezuh2nw6hhjg6mo7/subgraphs/mute_switch/v0.0.7/gn",
 };
 
 const adapter = univ2Adapter(endpoints, {
