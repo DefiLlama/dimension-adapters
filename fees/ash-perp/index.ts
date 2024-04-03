845422e1c014e7a8ac3ce802c9f9c5bc2ed617e1
htanh9320 <htanh9320@gmail.com>
2024-04-03 16:59:51 +0700

    add ash-perp

---
+++ fees/ash-perp/index.ts
@@ -0,0 +1,50 @@
+import { Chain } from "@defillama/sdk/build/general";
+import { Adapter, FetchResultFees } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { queryDune } from "../../helpers/dune";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import request from "graphql-request";
+
+const API_URL = 'http://localhost:3000/graphql';
+
+interface IFee {
+  time: string;
+  v2_fees: number;
+  total_fees: number;
+}
+
+const fetch = (chain: Chain) => {
+  return async (timestamp: number): Promise<FetchResultFees> => {
+    const startTs = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+    const endTs = startTs + 86400;
+    const feeQuery =`query Trading {
+      trading {
+        getDailyFee(from: ${startTs}, to: ${endTs}){
+          daily_fees
+          daily_holders_revenue
+          daily_protocol_revenue
+        }
+      }
+    }`;
+
+    const dailyFee = (await request(API_URL, feeQuery));
+    return {
+      dailyFees: `${dailyFee.trading.getDailyFee.daily_fees}`,
+      dailyHoldersRevenue: `${dailyFee.trading.getDailyFee.daily_holders_revenue}`,
+      dailyProtocolRevenue: `${dailyFee.trading.getDailyFee.daily_protocol_revenue}`,
+      timestamp,
+    };
+  };
+};
+
+const adapter: Adapter = {
+  adapter: {
+    [CHAIN.ELROND]: {
+      fetch: fetch(CHAIN.ARBITRUM),
+      start: 1706745600,
+      runAtCurrTime: true,
+    }
+  },
+  isExpensiveAdapter: true,
+};
+export default adapter;
