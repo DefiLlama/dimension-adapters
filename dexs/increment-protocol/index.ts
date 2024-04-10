ce4cd2532cbb2a9f2f232929d94395c99e8e4903
0xgnek <0xgnek@gmail.com>
2024-04-10 14:23:24 +0000

    fix adapter

--- dexs/increment-protocol/index.ts
+++ dexs/increment-protocol/index.ts
@@ -5,23 +5,13 @@ const subgraphUrl =
 
 const volumeQuery = gql`
   query volumeQuery($endTimestamp: Int!) {
-    markets {
-      hourlyCandles(
-        orderBy: openTimestamp
-        orderDirection: desc
-        first: 24
-        where: { openTimestamp_lte: $endTimestamp }
-      ) {
-        volume
-      }
-      dailyCandles(
-        orderBy: openTimestamp
-        orderDirection: desc
-        first: 1000
-        where: { openTimestamp_lte: $endTimestamp }
-      ) {
-        volume
-      }
+    dailyCandles(
+      orderBy: openTimestamp
+      orderDirection: desc
+      first: 24
+      where: { openTimestamp: $endTimestamp }
+    ) {
+      volume
     }
   }
 `;
@@ -29,23 +19,16 @@ const volumeQuery = gql`
 export default {
   adapter: {
     era: {
-      fetch: async ({ endTimestamp }) => {
+      fetch: async (_t: any, _c: any, { endTimestamp }) => {
         const volumeData = await request(subgraphUrl, volumeQuery, {
           endTimestamp: endTimestamp ?? Math.floor(Date.now() / 1000)
         });
         let dailyVolume = 0;
-        let totalVolume = 0;
-        for (const market of volumeData.markets) {
-          dailyVolume += market.hourlyCandles.reduce(
-            (acc, { volume }) => acc + volume * 10 ** -18,
-            0
-          );
-          totalVolume += market.dailyCandles.reduce(
-            (acc, { volume }) => acc + volume * 10 ** -18,
-            0
-          );
-        }
-        return { dailyVolume, totalVolume };
+        dailyVolume += volumeData.dailyCandles.reduce(
+          (acc, { volume }) => acc + volume * 10 ** -18,
+          0
+        );
+        return { dailyVolume };
       },
       start: 1710004200 // 2024-03-09 09:10
     }
