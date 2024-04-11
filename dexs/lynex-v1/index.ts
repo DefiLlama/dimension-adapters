6b1ec445503c7c224c4f97a8b8dd81edf707de1f
0xgnek <0xgnek@gmail.com>
2024-04-11 12:34:42 +0000

    fix function get volume

--- dexs/lynex-v1/index.ts
+++ dexs/lynex-v1/index.ts
@@ -1,19 +1,32 @@
+import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { getChainVolume } from "../../helpers/getUniSubgraphVolume";
 
-const adapters = univ2Adapter(
-  {
-    [CHAIN.LINEA]:
-      "https://api.studio.thegraph.com/query/59052/lynex-v1/v0.1.0",
+const graphqlV3 = getChainVolume({
+  graphUrls: {
+    [CHAIN.LINEA]: "https://api.studio.thegraph.com/query/59052/lynex-v1/v0.0.4",
   },
-  {
-    factoriesName: "factories",
-    dayData: "dayData",
-    dailyVolume: "dailyVolumeUSD",
-    totalVolume: "totalVolumeUSD",
-  }
-);
+  totalVolume: {
+    factory: "factories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "dayData",
+    field: "dailyVolumeUSD",
+    dateField: "date"
+  },
+  hasDailyVolume: true,
+});
 
-// New v1 factory contract created at block 2202427 = UNIX timestamp 1707620640
-adapters.adapter.linea.start = 1707620640;
+
+const adapters: SimpleAdapter = {
+  adapter: {
+    [CHAIN.LINEA]: {
+      fetch: graphqlV3(CHAIN.LINEA),
+      start: 1707620640,
+    },
+  },
+  version: 2,
+}
+
 export default adapters;
