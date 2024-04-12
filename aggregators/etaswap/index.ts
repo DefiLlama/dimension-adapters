cc3c1d92d3ce5fbbe86fe66181432020173d3c31
0xgnek <0xgnek@gmail.com>
2024-04-12 15:48:21 +0000

    fix use wrap function

--- aggregators/etaswap/index.ts
+++ aggregators/etaswap/index.ts
@@ -1,14 +1,14 @@
-import axios from "axios"
 import BigNumber from "bignumber.js";
 import { CHAIN } from "../../helpers/chains";
+import { httpGet } from "../../utils/fetchURL";
 
 const fetchLogs = async (timestamp: number) => {
-  const res = await axios.get(`https://api.etaswap.com/v1/statistics/volume/total?timestamp=${timestamp}`);
+  const res = await httpGet(`https://api.etaswap.com/v1/statistics/volume/total?timestamp=${timestamp}`);
   return {
-    dailyVolume: new BigNumber(res.data.volume_USD_24h).div(100).toFixed(2),
-    totalVolume: new BigNumber(res.data.volume_USD_total).div(100).toFixed(2),
-    dailyFees: new BigNumber(res.data.fee_USD_24h).div(100).toFixed(2),
-    totalFees: new BigNumber(res.data.fee_USD_total).div(100).toFixed(2),
+    dailyVolume: new BigNumber(res.volume_USD_24h).div(100).toFixed(2),
+    totalVolume: new BigNumber(res.volume_USD_total).div(100).toFixed(2),
+    dailyFees: new BigNumber(res.fee_USD_24h).div(100).toFixed(2),
+    totalFees: new BigNumber(res.fee_USD_total).div(100).toFixed(2),
     timestamp: timestamp,
   };
 };
