5c53c4e90d5ba8ec4608c5ad1bab6e6f054f5bdf
Tomachi Anura <info@hbarsuite.network>
2024-04-12 11:55:16 +0200

    completed dimension adapter for hsuite-dex

--- dexs/hbarsuite-dex/index.ts
+++ dexs/hbarsuite-dex/index.ts
@@ -3,6 +3,7 @@ import { CHAIN } from "../../helpers/chains";
 import axios from "axios";
 import asyncRetry from "async-retry";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+const BigNumber = require("bignumber.js");
 
 /*
  * HbarSuite is a decentralized network of features built on Hedera Hashgraph.
@@ -29,16 +30,17 @@ const nodes = [
 ]
 
 interface IHbarSuiteVolumes {
-  allTimeVolume: number
-  allTimeVolumesByPools: Array<{
-    pool_id: string
-    pool_name: string
-    volume: number
-  }>
-  dailyVolumes: Array<{
-    timestamp: number
-    volume: number
-  }>
+  ticker: string
+  pool: string
+  daily: string
+  total: string
+}
+
+const methodology = {
+  UserFees: "User pays 0.3% fees on each swap.",
+  ProtocolRevenue: "Liquidity Providers earn a fixed APR in HSUITE tokens.",
+  Revenue: "All revenue generated comes from user fees.",
+  Fees: "All fees comes from the user."
 }
 
 const fetch = async (timestamp: number) => {
@@ -47,30 +49,35 @@ const fetch = async (timestamp: number) => {
     // generating a random number, so to grab a random smart-node from the network..
     let randomNode = nodes[Math.floor(Math.random() * nodes.length)];
 
-    // fetching the HBAR price from coingecko..
-    let price_feed = await asyncRetry(async() => await axios.get(
-        'https://api.coingecko.com/api/v3/simple/price?ids=hedera-hashgraph&vs_currencies=usd'
-    ));
-
-    let volumes: IHbarSuiteVolumes = (await asyncRetry(async() => await axios.get(
-        `${randomNode}/pools/volumes`
+    let volumesForPools: Array<IHbarSuiteVolumes> = (await asyncRetry(async() => await axios.get(
+        `${randomNode}/dex/analytics/volumes`
     ))).data;
 
-    let dailyVolume = volumes.dailyVolumes.find((volume) => volume.timestamp >= dayTimestamp);
+    let dailyVolumes = volumesForPools.reduce((acc, pool) => {
+      acc = new BigNumber(acc).plus(pool.daily);
+      return acc;
+    }, "0");
+
+    let allTimeVolumes = volumesForPools.reduce((acc, pool) => {
+      acc = new BigNumber(acc).plus(pool.total);
+      return acc;
+    }, "0");
 
     return {
-      totalVolume: (volumes.allTimeVolume * price_feed.data['hedera-hashgraph'].usd).toString(),
-      dailyVolume: dailyVolume ? (dailyVolume.volume * price_feed.data['hedera-hashgraph'].usd).toString() : "0",
+      totalVolume: allTimeVolumes.toString(),
+      dailyVolume: dailyVolumes.toString(),
       timestamp: dayTimestamp
     };
 };
-
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.HEDERA]: {
       fetch,
       start: async () => 1672531200,
+      meta: {
+        methodology
+      }
     },
   },
 };
