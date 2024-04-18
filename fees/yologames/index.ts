4a1b6706531e0f79d7677c62c2a254d32273d90f
george-lr <george@looksrare.org>
2024-04-18 14:28:00 +0200

    Remove volume from the adapter

--- fees/yologames/index.ts
+++ fees/yologames/index.ts
@@ -7,30 +7,27 @@ const nextDayTimestamp = (timestamp: number) => timestamp + 86_400_000;
 
 const fetchDailyStats = async (
   timestampSeconds: number
-): Promise<{ volumeETH: number; feesETH: number }> => {
+): Promise<{ feesETH: number }> => {
   const timestampMs = timestampSeconds * 1000;
   const url = `https://stats.yologames.io/stats?from=${timestampMs}&to=${nextDayTimestamp(
     timestampMs
   )}`;
   return fetchURL(url)
     .then((res) => {
-      return { volumeETH: Number(res.volumeETH), feesETH: Number(res.feesETH) };
+      return { feesETH: Number(res.feesETH) };
     })
     .catch(() => {
-      return { volumeETH: 0, feesETH: 0 };
+      return {feesETH: 0 };
     });
 };
 
 const fetch: any = async (timestampSeconds: number, _: any, options: FetchOptions) => {
   const dailyFees = options.createBalances();
-  const dailyVolume = options.createBalances();
   const statsApiResponse = await fetchDailyStats(timestampSeconds);
   dailyFees.add(ETHER_ADDRESS, statsApiResponse.feesETH * 1e18);
-  dailyVolume.add(ETHER_ADDRESS, statsApiResponse.volumeETH * 1e18);
   return {
     timestamp: timestampSeconds,
     dailyFees,
-    dailyVolume,
   };
 };
 
@@ -42,8 +39,6 @@ const adapter: Adapter = {
       meta: {
         methodology: {
           Fees: "YOLO Games collects a 1% fee for Moon Or Doom and YOLO winnings, and a 3% fee on Poke The Bear winnings.",
-          Volume:
-            "YOLO Games derives volume data from the deposit transactions into all the games.",
         },
       },
     },
