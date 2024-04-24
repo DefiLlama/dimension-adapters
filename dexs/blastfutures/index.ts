2cf9d2f1237fe0156b35040de96bd372138d908d
Sinan BALCIN <sbalcin@gmail.com>
2024-04-24 14:58:47 +0100

    update on bfx volume adapter

--- dexs/blastfutures/index.ts
+++ dexs/blastfutures/index.ts
@@ -1,51 +1,39 @@
 import fetchURL from "../../utils/fetchURL"
-import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import {FetchResultVolume, SimpleAdapter} from "../../adapters/types";
+import {getUniqStartOfTodayTimestamp} from "../../helpers/getUniSubgraphVolume";
 
-const historicalVolumeEndpoint = "https://api.bfx.trade/markets"
-const candles = (market: string, timestampFrom: number, timestampTo: number) => {
-  const url = `https://api.bfx.trade/candles?market_id=${market}&timestamp_from=${timestampFrom}&timestamp_to=${timestampTo}&period=1440`;
-  return url;
-
+const historicalVolumeEndpoint = "https://api.prod.rabbitx.io/bfx/volume"
+const volumeByTime = (timestampFrom: number, timestampTo: number) => {
+    const url = `${historicalVolumeEndpoint}?start_date=${timestampFrom}&end_date=${timestampTo}`;
+    return url;
 }
 
 interface IVolumeall {
-  volume: string;
-  time: string;
-  close: string;
+    volume: string;
 }
 
 const fetchVolume = async (timestamp: number): Promise<FetchResultVolume> => {
-  const fromTimestamp = timestamp - 60 * 60 * 24
-  const toTimestamp = timestamp
+    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+    const fromMS = dayTimestamp * 1000 * 1000;
+    const toMS = (dayTimestamp + 60 * 60 * 24) * 1000 * 1000;
 
-  // Get market data
-  const response = await fetchURL(historicalVolumeEndpoint);
-  const marketsData = response.result;
+    const response = await fetchURL(volumeByTime(fromMS, toMS));
+    const marketsData: IVolumeall[] = response.result;
+    const dailyVolume = marketsData.reduce((acc, {volume}) => acc + Number(volume), 0);
 
-  // Fetch candles for each USD market
-  const historical: IVolumeall[] = (await Promise.all(marketsData.map((market: any) => fetchURL(candles(market.id, fromTimestamp, toTimestamp)))))
-    .map((e: any) => e.result)
-    .flat();
-
-  // Calculate daily volume
-  const dailyVolume = historical
-    .filter((e: IVolumeall) => Number(e.time) >= fromTimestamp )
-    .filter((e: IVolumeall) => Number(e.time) <= toTimestamp)
-    .reduce((a: number, b: IVolumeall) => a + Number(b.volume), 0)
-
-  return {
-    dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
-    timestamp: timestamp,
-  };
+    return {
+        dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
+        timestamp: timestamp,
+    };
 };
 
 const adapter: SimpleAdapter = {
-  adapter: {
-    "blast": {
-      fetch: fetchVolume,
-      start: 1700179200, // Replace with actual start timestamp
+    adapter: {
+        "blast": {
+            fetch: fetchVolume,
+            start: 1700179200, // Replace with actual start timestamp
+        },
     },
-  },
 };
 
 export default adapter;
