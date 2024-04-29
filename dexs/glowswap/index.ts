10e1336392612b09c15b5f0d43a44724796d9a7f
0xgnek <0xgnek@gmail.com>
2024-04-29 15:28:09 +0000

    update

--- dexs/glowswap/index.ts
+++ dexs/glowswap/index.ts
@@ -1,17 +1,16 @@
 import { SimpleAdapter } from "../../adapters/types"
-import { CHAIN } from "../../helpers/chains"
 import { httpGet } from "../../utils/fetchURL"
 
-const fetch = async () => {
+const fetch = async (timestamp: number) => {
   const url = "https://api.glowswap.io/v1/analytics"
   const response = await httpGet(url)
   const dailyVolume = response.data.volUSD.day;
   return {
-    dailyVolume
+    dailyVolume,
+    timestamp
   }
 }
 const adapter: SimpleAdapter = {
-  version: 2,
   adapter: {
     bsquared: {
       fetch,
