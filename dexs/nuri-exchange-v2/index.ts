0ee014f6a3273d5ca84d68a9995c8ac797221055
0xshad0w <hugomncs@gmail.com>
2024-05-09 19:23:11 +0100

    add nuri-v1 and nuri-v2

---
+++ dexs/nuri-exchange-v2/index.ts
@@ -0,0 +1,14 @@
+import { CHAIN } from "../../helpers/chains";
+import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+
+const adapters = univ2Adapter({
+  [CHAIN.SCROLL]: "https://api.studio.thegraph.com/query/66247/nuri-cl/version/latest"
+}, {
+  factoriesName: "factories",
+  dayData: "uniswapDayData",
+  dailyVolume: "volumeUSD",
+  totalVolume: "totalVolumeUSD",
+});
+
+adapters.adapter.scroll.start = 1714608000;
+export default adapters;
