c5840efa65e3475290ba932fa84d50e552f35c00
0xgnek <0xgnek@gmail.com>
2024-05-09 14:32:45 +0000

    update code

--- fees/bitlayer/index.ts
+++ fees/bitlayer/index.ts
@@ -1,59 +1,46 @@
 import {
   Adapter,
-  ChainBlocks,
   FetchOptions,
   ProtocolType,
 } from "../../adapters/types";
 import { httpPost } from "../../utils/fetchURL";
 import { CHAIN } from "../../helpers/chains";
-import { getPrices } from "../../utils/prices";
+import { time } from "console";
 
-export async function getFeeUSD({ startOfDay }: FetchOptions, url: string) {
-  startOfDay -= 86400;
-  const dailyFees = await httpPost(url, {
-    responseType: "blob",
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-      "Content-Type": "text/csv; charset=utf-8",
-      Accept: "text/csv; charset=utf-8",
-      origin: url,
-    },
-  });
-  const feesToday = dailyFees
-    .split("\n")
-    .filter((d: any) => d?.split(",")?.[1]?.slice(1, -1) == startOfDay);
+export async function getFeeUSD({ startOfDay, createBalances }: FetchOptions, url: string) {
+    const dailyFees = createBalances()
+    const response = await httpPost(url, {
+        responseType: 'blob', headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+            "Content-Type": "text/csv; charset=utf-8",
+            "Accept": "text/csv; charset=utf-8",
+            "origin": url,
+        }
+    });
+    const feesToday = response
+      .split("\n")
+      .filter((d: any) => d?.split(",")?.[1]?.slice(1, -1) == startOfDay);
 
   const gasToken = "coingecko:bitcoin";
-
-  return Promise.all(
-    feesToday.map(async (fee: any) => {
-      const timestamp = Number(fee.split(",")[1].slice(1, -1));
-      const prices = await getPrices([gasToken], timestamp);
-      const value = Number(fee.split(",")[2].slice(1, -2));
-      const amountReal = value / 1e18;
-      return (amountReal * prices[gasToken].price).toString();
-    })
-  );
+  feesToday.forEach((fee: any) => {
+    const value = Number(fee.split(",")[2].slice(1, -2));
+    const amountReal = value / 1e18;
+    dailyFees.addCGToken(gasToken, amountReal);
+  });
+  return dailyFees;
 }
 
 const adapter: Adapter = {
-  version: 2,
+  version: 1,
   adapter: {
     [CHAIN.BITLAYER]: {
-      fetch: async (options: FetchOptions) => {
-        const feeUSD = await getFeeUSD(
-          options,
-          "https://api.btrscan.com/scan/v1/chain/txForDefillama"
-        );
-        const dailyFees = feeUSD.toString();
-
+      fetch: async (_t: any, _b: any, options: FetchOptions) => {
+        const url = "https://api.btrscan.com/scan/v1/chain/txForDefillama";
         return {
           timestamp: options.startOfDay,
-          dailyFees,
+          dailyFees: (await getFeeUSD(options,url)),
         };
       },
-      runAtCurrTime: true,
       start: 1713089236,
     },
   },
