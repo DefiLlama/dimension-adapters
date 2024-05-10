42d7d8ec08432a8873e0af6f20e4bb9ae22d92aa
mrben1 <ben@gnoli.co.uk>
2024-05-10 15:06:13 +1200

    clean up

--- dexs/apestore/index.ts
+++ dexs/apestore/index.ts
@@ -1,7 +1,5 @@
 import { httpPost } from "../../utils/fetchURL"
-import { SimpleAdapter, FetchResultVolume, FetchOptions, FetchResultV2, Adapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { FetchOptions, FetchResultV2, Adapter } from "../../adapters/types";
 
 const URL = 'https://ape.store/api/public/base/volume'
 
@@ -10,20 +8,6 @@ interface VolumeInfo {
 	totalVolume: string;
 	timeStamp: number;
 }
-
-const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
-	const volumeData: VolumeInfo = await httpPost(URL, { date: options.startOfDay }, {
-		headers: {
-			"Authorization": "8690be69-3c53-4bc1-8e99-e4fe0472b757"
-		},
-	});
-
-	return {
-		totalVolume: volumeData.totalVolume,
-		dailyVolume: volumeData.dailyVolume,
-		timestamp: volumeData.timeStamp,
-	};
-};
 
 const adapter: Adapter = {
 	version: 2,
