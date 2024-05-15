00b80260112b14fed3bf7a76d73b95c8d4aaca96
0xgnek <0xgnek@gmail.com>
2024-05-15 14:30:01 +0000

    count only one side

--- dexs/mangrove/index.ts
+++ dexs/mangrove/index.ts
@@ -37,7 +37,7 @@ const adapter: Adapter = {
         for (const market of markets) {
           const { base, quote, totalValueInBase, totalValueInQuote } = market;
           dailyVolume.add(base, totalValueInBase);
-          dailyVolume.add(quote, totalValueInQuote);
+          // dailyVolume.add(quote, totalValueInQuote);
         }
 
         return { dailyVolume };
