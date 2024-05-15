068276bce5d10f95d8067d581b0699bdfd3ac8db
stepollo2 <stepolloz@protonmail.com>
2024-05-15 17:43:08 +0200

    fees

--- fees/vfat/index.ts
+++ fees/vfat/index.ts
@@ -1,68 +1,69 @@
-import { Adapter, FetchOptions } from "../../adapters/types";
+import { Adapter, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const chainSettings = {
-  base: {
-    factory: '0x71D234A3e1dfC161cc1d081E6496e76627baAc31',
-    fromBlock: 12116234,
-    chainName: 'base',
-  },
-  optimism: {
-    factory: '0xB4C31b0f0B76b351395D4aCC94A54dD4e6fbA1E8',
-    fromBlock: 117753454,
-    chainName: 'optimism',
-  },
-};
-
-const fetchFees = (chain: string) => async ({ createBalances, getLogs }: FetchOptions) => {
-  const dailyFees = createBalances();
-  const settings = chainSettings[chain];
+const chainSettings: any = {
+    base: {
+      factory: '0x71D234A3e1dfC161cc1d081E6496e76627baAc31',
+      fromBlock: 12116234,
+      chainName: 'base',
+    },
+    optimism: {
+      factory: '0xB4C31b0f0B76b351395D4aCC94A54dD4e6fbA1E8',
+      fromBlock: 117753454,
+      chainName: 'optimism',
+    },
+  };
   
-
-  // Fetch Deploy events to get all Sickle contract addresses
-  const deployLogs = await getLogs({
-    target: settings.factory,
-    fromBlock: settings.fromBlock,
-    eventAbi: 'event Deploy(address indexed admin, address sickle)',
-  });
-
-
-  const sickleContracts = deployLogs.map((log: any) => log.sickle);
-
-
-  // Fetch FeeCharged events from all Sickle contracts
-  for (const sickle of sickleContracts) {
-    const logs = await getLogs({
-      target: sickle,
+  const fetchFees = async ({ createBalances, getLogs, chain }: FetchOptions) => {
+    const dailyFees = createBalances();
+    const settings = chainSettings[chain];
+  
+  
+    // Fetch Deploy events to get all Sickle contract addresses
+    const deployLogs = await getLogs({
+      target: settings.factory,
       fromBlock: settings.fromBlock,
-      eventAbi: 'event FeeCharged(bytes32 feesHash, uint256 amount, address token)',
+      eventAbi: 'event Deploy(address indexed admin, address sickle)',
     });
-
-    logs.forEach((log: any) => {
-      dailyFees.add(log.token, log.amount);
-    });
-
-    console.log(dailyFees)
+  
+  
+    const sickleContracts = deployLogs.map((log: any) => log.sickle);
+  
+  
+    // Fetch FeeCharged events from all Sickle contracts
+    for (const sickle of sickleContracts) {
+      const logs = await getLogs({
+        target: sickle,
+        fromBlock: settings.fromBlock,
+        eventAbi: 'event FeeCharged(bytes32 feesHash, uint256 amount, address token)',
+      });
+      
+      logs.forEach((log: any) => {
+        dailyFees.add(log.token, log.amount);
+      });
+      console.log(dailyFees)
+    }
+  
+    return {
+      dailyFees,
+      dailyRevenue: dailyFees,
+  };
+  };
+  
+  const adapter: SimpleAdapter = {
+    version: 2,
+    adapter: {
+      [CHAIN.OPTIMISM]: {
+        fetch: fetchFees,
+        start: chainSettings.optimism.fromBlock,
+      },
+      [CHAIN.BASE]: {
+        fetch: fetchFees,
+        start: chainSettings.base.fromBlock,
+      },
+    }
   }
-
-  return {
-    dailyFees,
-    dailyRevenue: dailyFees,
-};
-};
-
-const adapter: Adapter = {
-  version: 2,
-  adapter: {
-    [CHAIN.BASE]: {
-      fetch: fetchFees('base'),
-      start: 1713225241,
-    },
-    [CHAIN.OPTIMISM]: {
-      fetch: fetchFees('optimism'),
-      start: 117753454,
-    },
-  },
-};
-
-export default adapter;
+  
+  export default adapter;
+  
+  
