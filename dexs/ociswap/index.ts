53579b0bbf49e6bb30bbb61a206341191f8dc285
ChrisPantostin <chris-man@gmx.de>
2024-05-16 14:12:53 +0200

    ociswap volume and fee dimensions

---
+++ dexs/ociswap/index.ts
@@ -0,0 +1,54 @@
+import { BreakdownAdapter, FetchResultVolume, SimpleAdapter } from "../../adapters/types"
+import { CHAIN } from "../../helpers/chains"
+import fetchURL from "../../utils/fetchURL"
+
+interface PoolStatistics {
+    pool_type: string;
+    volume: {
+        xrd: {
+            '24h': string;
+            total: string;
+        };
+        usd: {
+            '24h': string;
+            total: string;
+        };
+    };
+}
+
+const fetchVolume = (poolType: 'basic' | 'precision') => {
+    return async (timestamp: number): Promise<FetchResultVolume> => {
+        const response: Array<PoolStatistics> = await fetchURL('http://api.ociswap.com/statistics/pool-types');
+
+        const index = response.findIndex(pool => pool.pool_type === poolType);
+
+        const dailyVolume = Number(response[index].volume.usd["24h"]);
+        const totalVolume = Number(response[index].volume.usd.total);
+
+        return {
+            dailyVolume: `${dailyVolume}`,
+            totalVolume: `${totalVolume}`,
+            timestamp
+        };
+    };
+};
+
+const adapters: BreakdownAdapter = {
+    breakdown: {
+        basic: {
+            [CHAIN.RADIXDLT]: {
+                fetch: fetchVolume('basic'),
+                start: 1696118400,
+                // runAtCurrTime: true
+            }
+        },
+        precision: {
+            [CHAIN.RADIXDLT]: {
+                fetch: fetchVolume('precision'),
+                start: 1696118400,
+                // runAtCurrTime: true
+            }
+        }
+    }
+}
+export default adapters;
