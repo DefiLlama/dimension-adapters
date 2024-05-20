44e04c6bcf769843912f168fb17eda8968ed3a30
0xgnek <0xgnek@gmail.com>
2024-05-20 16:48:50 +0000

    fix addapter

--- dexs/rollie-finance/index.ts
+++ dexs/rollie-finance/index.ts
@@ -31,7 +31,7 @@ const getFetch = (chain: string): FetchV2 => async (options: FetchOptions): Prom
   const dayTimestamp = getUniqStartOfTodayTimestamp(
       new Date(startTimestamp * 1000)
   );
-  const dayIndex = Math.floor(dayTimestamp / 86400);
+  const dayIndex = Math.floor(options.startOfDay / 86400);
   const { market: response } = await request(ENDPOINTS[chain],
     getVolume, {
       id: String(dayIndex),
@@ -43,7 +43,7 @@ const getFetch = (chain: string): FetchV2 => async (options: FetchOptions): Prom
       response.marketDayDatas.length === 1
         ? (BigInt(response.marketDayDatas[0].tradeVolume) /
           BigInt(10 ** USDC_DECIMALS[chain])).toString()
-        : undefined,
+        : '0',
     totalVolume: (BigInt(response.tradeVolume) /
       BigInt(10 ** USDC_DECIMALS[chain])).toString(),
 
@@ -55,7 +55,7 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.SCROLL]: {
       fetch: getFetch(CHAIN.SCROLL),
-      start: 5423335,
+      start: 1715212800,
     },
   },
 };
