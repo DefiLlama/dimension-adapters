e18fc344cdd48695405759dbe14ee2e80d271e67
kmax3ever <kmax3ever2@gmail.com>
2024-05-20 23:18:16 +0700

    update calculate fee goat-tech

--- fees/goat-tech/index.ts
+++ fees/goat-tech/index.ts
@@ -1,38 +1,103 @@
-import { Adapter, FetchOptions, FetchResultFees } from "../../adapters/types";
+import { Adapter, FetchResultFees } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { queryIndexer } from "../../helpers/indexer";
-const fetch = async (
-  timestamp: number,
-  _: any,
-  options: FetchOptions
-): Promise<FetchResultFees> => {
-  const dailyFees = options.createBalances();
-  const logsTranferERC20: any[] = await queryIndexer(
-    `
-        SELECT
-          '0x' || encode(data, 'hex') AS value,
-          '0x' || encode(contract_address, 'hex') AS contract_address
-        FROM
-          arbitrum.event_logs
-        WHERE
-          block_number > 210487219
-          AND topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-          AND topic_2 = '\\x0000000000000000000000007781597dd4f782f6d11840c34160227204f7afa9'
-          AND block_time BETWEEN llama_replace_date_range;
-          `,
-    options
-  );
-  logsTranferERC20.map((p: any) => dailyFees.add(p.contract_address, p.value));
-  return { dailyFees, dailyRevenue: dailyFees, timestamp };
+import {
+  getTimestampAtStartOfDayUTC,
+  getTimestampAtStartOfNextDayUTC,
+} from "../../utils/date";
+import * as sdk from "@defillama/sdk";
+import { getBlock } from "../../helpers/getBlock";
+import { Chain } from "@defillama/sdk/build/general";
+
+const topic0NewTransferAdded =
+  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+
+type TAddress = {
+  [l: string | Chain]: string;
+};
+
+const address: TAddress = {
+  [CHAIN.ARBITRUM]: "0x282e51D8E5812adbff5751DA50a36A9A6a0eC6F2",
+};
+
+interface ITx {
+  data: string;
+  transactionHash: string;
+  topics: string[];
+}
+
+interface IData {
+  contract_address: string;
+  amount: number;
+}
+
+const fetch = (chain: Chain) => {
+  return async (timestamp: any): Promise<FetchResultFees> => {
+    const fromTimestamp = timestamp.fromTimestamp;
+    const toTimestamp = timestamp.toTimestamp;
+
+    const fromBlock = await getBlock(fromTimestamp, CHAIN.ARBITRUM, {});
+    const toBlock = await getBlock(toTimestamp, CHAIN.ARBITRUM, {});
+
+    console.log({ fromBlock, toBlock });
+    const logs: ITx[] = (
+      await sdk.getEventLogs({
+        target: address[chain],
+        fromBlock: 210487219,
+        toBlock: 210487219 + 100000,
+        topics: [topic0NewTransferAdded],
+        chain: chain,
+      })
+    ).map((e: any) => {
+      return {
+        data: e.data.replace("0x", ""),
+        transactionHash: e.transactionHash,
+        topics: e.topics,
+      } as ITx;
+    });
+    console.log(logs);
+
+    const raw_data_logs: IData[] = logs.map((tx: ITx) => {
+      const amount = Number("0x" + tx.data);
+      const address = tx.topics[1];
+      const contract_address = "0x" + address.slice(26, address.length);
+      return {
+        amount,
+        contract_address,
+        tx: tx.transactionHash,
+      };
+    });
+    console.log({ raw_data_logs });
+    const feesAmuntsUSD: any[] = raw_data_logs.map((d: any) => {
+      return { amount: d.amount / 10 ** 18, tx: d.tx, a: d.contract_address }; // debug
+    });
+    const dailyFee = feesAmuntsUSD.reduce(
+      (a: number, b: any) => a + b.amount,
+      0
+    );
+    const supplySideRev = dailyFee;
+    const dailyHoldersRevenue = dailyFee;
+    const protocolRev = dailyFee;
+    console.log({ dailyFee });
+
+    return {
+      dailyFees: dailyFee.toString(),
+      dailySupplySideRevenue: supplySideRev.toString(),
+      dailyHoldersRevenue: dailyHoldersRevenue.toString(),
+      dailyProtocolRevenue: protocolRev.toString(),
+      dailyRevenue: (protocolRev + dailyHoldersRevenue).toString(),
+      timestamp,
+    };
+  };
 };
 
 const adapter: Adapter = {
   version: 2,
   adapter: {
     [CHAIN.ARBITRUM]: {
-      fetch: fetch as any,
+      fetch: fetch(CHAIN.ARBITRUM),
       start: 210487219,
     },
   },
 };
+
 export default adapter;
