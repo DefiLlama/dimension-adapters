8da89ded947526218ff3c50a9c74c6aa9d5be9d4
mmd <22621120+mm-ghost@users.noreply.github.com>
2024-05-20 10:43:31 +0330

    Add BACKED

---
+++ fees/backed/index.ts
@@ -0,0 +1,58 @@
+import { Chain } from "@defillama/sdk/build/general";
+import { CHAIN } from "../../helpers/chains";
+import * as sdk from "@defillama/sdk";
+import { Address } from "@defillama/sdk/build/types";
+import { Adapter, SimpleAdapter } from "../../adapters/types";
+
+const expansionABI = {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+        {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+}
+
+const getExpansionAddress = (chain: Chain): Address => {
+    if (chain == CHAIN.MANTLE) return '0x2139788f64bA30Ee73b6b2549e62E42F3c6Da1C5'
+    if (chain == CHAIN.BLAST) return '0xf06DB250003C9aE2b7a6D21b7b5b468C8325D690'
+    if (chain == CHAIN.BASE) return '0x2139788f64bA30Ee73b6b2549e62E42F3c6Da1C5'
+
+    throw 'NOT SUPPORTED CHAIN'
+}
+
+
+const getTVL = async (chain: Chain) => {
+    return (await sdk.api.abi.call(
+        {
+            target: getExpansionAddress(chain),
+            chain: chain,
+            abi: expansionABI,
+        }
+    )).output
+}
+
+const adapter: Adapter = {
+    version: 2,
+    adapter: {
+        [CHAIN.MANTLE]: {
+            fetch: async () => getTVL(CHAIN.MANTLE),
+            start: async () => 0
+        },
+        [CHAIN.BLAST]: {
+            fetch: async () => getTVL(CHAIN.BLAST),
+            start: async () => 0
+        },
+        [CHAIN.BASE]: {
+            fetch: async () => getTVL(CHAIN.BASE),
+            start: async () => 0
+        },
+    }
+}
+
+export default adapter;
