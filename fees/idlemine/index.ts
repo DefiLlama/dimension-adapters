e3857cf75559d045430751113d45bedff7d1bfe7
abhinavabhi1218 <86964670+abhinavabhi1218@users.noreply.github.com>
2024-05-21 12:45:39 +0530

    changed successfully

--- fees/idlemine/index.ts
+++ fees/idlemine/index.ts
@@ -11,33 +11,21 @@ interface DailyStats {
   revenueUSDT:number
 }
 
-/**
- * Fetches and decrypts daily stats from a specific endpoint.
- * @param timestampSeconds The timestamp at the start of the day in seconds.
- * @returns A promise that resolves to the fees in USDT.
- */
-
-
-/**
- * Fetches the daily fees and adds them to the balances.
- * @param timestampSeconds The current timestamp in seconds.
- * @param options FetchOptions containing utility functions and additional info.
- * @returns An object containing the timestamp and daily fees.
- */
 const fetch = async (timestampSeconds: number, _: any, options: FetchOptions) => {
   
   const url = "https://api.idlemine.io/api/admin/user/revenue";
   const response = await fetchURL(url);
   const responsedata = response.data;
-  const totalRevenue = options.createBalances();
- 
-  totalRevenue.add('0x55d398326f99059fF775485246999027B3197955', responsedata.Totalrevenue * 1e18);
- 
+  console.log(responsedata, 'responsedata');
+  // const totalRevenue = options.createBalances();
+  // const totalFees = options.createBalances();
+  // totalRevenue.add('0x55d398326f99059fF775485246999027B3197955', responsedata.Totalrevenue * 1e18);
+  // totalFees.add('0x55d398326f99059fF775485246999027B3197955', responsedata.Fee * 1e18);
   
   return {
     timestamp: timestampSeconds,
-    totalRevenue
-  
+    totalFees: responsedata.Fee   || 0,
+    totalRevenue: responsedata.Totalrevenue  || 0,
   };
 };
 
@@ -46,7 +34,7 @@ const adapter: Adapter = {
   adapter: {
     [CHAIN.BSC]: {
       fetch,
-      start: 1715986805152, // Start timestamp in seconds.
+      start: 1709251200, // Start timestamp in seconds.
       meta: {
         methodology: "idlemine revenue from idlemine thumb game and idlemine battle games",
       },
