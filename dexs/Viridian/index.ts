32fce28191deec6be31eaa3508213d035ad4a786
GabrielReFintech <gabriel.re@actionfintech.com>
2024-05-23 19:50:45 -0300

    Fix: Adapter V2 for dex

--- dexs/Viridian/index.ts
+++ dexs/Viridian/index.ts
@@ -5,6 +5,7 @@ import { getDexVolumeExports } from "../../helpers/dexVolumeLogs";
 const FACTORY_ADDRESS = '0xb54a83cfEc6052E05BB2925097FAff0EC22893F3'
 
 const adapter: SimpleAdapter = {
+  version: 2,
   adapter: {
     [CHAIN.CORE]: {
       fetch: getDexVolumeExports({ chain: CHAIN.CORE, factory: FACTORY_ADDRESS }),
