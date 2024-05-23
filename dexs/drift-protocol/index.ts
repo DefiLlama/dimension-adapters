72ca7d47ed58261b74fa20a573ad38300c1ba851
Luke Steyn <lukecaan@gmail.com>
2024-05-23 15:31:16 +0800

    Added new dimensions for Drift Protocol

--- dexs/drift-protocol/index.ts
+++ dexs/drift-protocol/index.ts
@@ -1,28 +1,75 @@
 import { CHAIN } from "../../helpers/chains";
 import { httpGet } from "../../utils/fetchURL";
+import { queryDune } from "../../helpers/dune";
 
-const dailyVolEndpoint =
+const DAILY_VOL_ENDPOINT =
   "https://mainnet-beta.api.drift.trade/stats/24HourVolume";
 
-async function fetch(type: "perp" | "spot") {
+const DUNE_QUERY_ID = "3756979";
+
+type DimentionResult = {
+  dailyVolume?: number;
+  dailyFees?: number;
+  dailyUserFees?: number;
+  dailyRevenue?: number;
+};
+
+async function getPerpDimensions(): Promise<DimentionResult> {
+  const resultRows = await queryDune(DUNE_QUERY_ID);
+
+  const summaryRow = resultRows.find((row) => row.market_index === null);
+
+  // Perp Volume
+  const dailyVolume = summaryRow.total_volume as number;
+
+  // All taker fees paid
+  const dailyFees = summaryRow.total_taker_fee as number;
+
+  // All taker fees paid, minus maker rebates paid - not sure if this should be used as the "dailyFees" number instead.
+  const dailyRevenue = summaryRow.total_revenue as number;
+
+  return {
+    dailyVolume,
+    dailyFees,
+    dailyUserFees: dailyFees,
+    dailyRevenue,
+  };
+}
+
+async function getSpotDimensions(): Promise<DimentionResult> {
   const volumeResponse = await httpGet(
-    `${dailyVolEndpoint}?${
-      type === "perp" ? "perpMarkets" : "spotMarkets"
-    }=true`
+    `${DAILY_VOL_ENDPOINT}?spotMarkets=true`
   );
 
   const rawVolumeQuotePrecision = volumeResponse.data.volume;
 
   // Volume will be returned in 10^6 precision
-  const volumeNumber =
+  const dailyVolume =
     rawVolumeQuotePrecision.length >= 6
       ? Number(rawVolumeQuotePrecision.slice(0, -6))
       : 0;
 
-  return {
-    dailyVolume: volumeNumber,
-    timestamp: Date.now() / 1e3,
-  };
+  return { dailyVolume };
+}
+
+async function fetch(type: "perp" | "spot") {
+  const timestamp = Date.now() / 1e3;
+
+  if (type === "perp") {
+    const results = await getPerpDimensions();
+
+    return {
+      ...results,
+      timestamp,
+    };
+  } else {
+    const results = await getSpotDimensions();
+
+    return {
+      ...results,
+      timestamp: Date.now() / 1e3,
+    };
+  }
 }
 
 const adapter = {
