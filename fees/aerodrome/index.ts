7d3118ecc9e7c10605b37705eb3d9eec8122a96c
0xgnek <0xgnek@gmail.com>
2024-05-23 14:33:58 +0000

    fix aerodrome version 2

--- fees/aerodrome/index.ts
+++ fees/aerodrome/index.ts
@@ -1,24 +1,11 @@
-import { ChainBlocks, FetchOptions, FetchResultFees, SimpleAdapter } from "../../adapters/types"
+import { FetchOptions, FetchResultFees, SimpleAdapter } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains"
-import * as sdk from "@defillama/sdk";
 import { fees_bribes } from "./bribes";
 import { getDexFees } from "../../helpers/dexVolumeLogs";
 
 const sugar = '0xe521fc2C55AF632cdcC3D69E7EFEd93d56c89015';
 const abis: any = {
   "forSwaps": "function forSwaps(uint256 _limit, uint256 _offset) view returns ((address lp, int24 type, address token0, address token1, address factory, uint256 pool_fee)[])"
-}
-
-// defualt abi for multiCall is error some pools
-const multiCall = async (callN: any) => {
-  return (await sdk.api.abi.multiCall({
-    abi: callN.abi,
-    calls: callN.calls.map((pool: any) => ({
-      target: pool,
-    })),
-    chain: CHAIN.BASE,
-    permitFailure: true,
-  })).output.map((r: any) => r.output).flat()
 }
 
 const fetch = async (fetchOptions: FetchOptions): Promise<FetchResultFees> => {
