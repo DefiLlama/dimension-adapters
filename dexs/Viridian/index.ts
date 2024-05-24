043c9d406fcb1eb91a20c6a211e773e8fabb5c5b
GabrielReFintech <gabriel.re@actionfintech.com>
2024-05-24 13:52:00 -0300

    Feat: Viri fees & volume adapter

---
+++ dexs/Viridian/index.ts
@@ -0,0 +1,17 @@
+import { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getDexVolumeExports } from "../../helpers/dexVolumeLogs";
+
+const FACTORY_ADDRESS = '0xb54a83cfEc6052E05BB2925097FAff0EC22893F3'
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.CORE]: {
+      fetch: async () => await getDexVolumeExports({ chain: CHAIN.CORE, factory: FACTORY_ADDRESS }),
+      start: 1715904000,
+    },
+  }
+};
+
+export default adapter;
