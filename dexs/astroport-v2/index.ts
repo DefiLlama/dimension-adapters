03a7ac0b01b646d3dcc4e3c4be3f6fea10fcf019
JohnnyWyles <97029546+JohnnyWyles@users.noreply.github.com>
2024-05-27 18:30:57 +0100

    Add Astroport Osmosis Deployment

--- dexs/astroport-v2/index.ts
+++ dexs/astroport-v2/index.ts
@@ -16,7 +16,7 @@ query Stats($chains: [String]!) {
   }
 }
 `;
-const url = 'https://app.astroport.fi/api/trpc/protocol.stats?input={"json":{"chains":["phoenix-1","injective-1","neutron-1","pacific-1"]}}'
+const url = 'https://app.astroport.fi/api/trpc/protocol.stats?input={"json":{"chains":["phoenix-1","injective-1","neutron-1","pacific-1","osmosis-1"]}}'
 const fetch = (chainId: string) => {
   return async (timestamp: number): Promise<FetchResult> => {
     const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
@@ -51,6 +51,12 @@ const adapter: SimpleAdapter = {
     },
     [CHAIN.SEI]: {
       fetch: fetch("pacific-1"),
+      runAtCurrTime: true,
+      customBackfill: undefined,
+      start: 0,
+    },
+    [CHAIN.OSMOSIS]: {
+      fetch: fetch("osmosis-1"),
       runAtCurrTime: true,
       customBackfill: undefined,
       start: 0,
