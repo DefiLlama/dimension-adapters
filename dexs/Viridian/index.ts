03f203d4974116da2b9e220e6d0a5edf7f1e5dcd
0xgnek <0xgnek@gmail.com>
2024-05-28 12:51:33 +0000

    fix adapter version

--- dexs/Viridian/index.ts
+++ dexs/Viridian/index.ts
@@ -1,4 +1,4 @@
-import { SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getDexVolumeExports } from "../../helpers/dexVolumeLogs";
 
@@ -8,7 +8,7 @@ const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
     [CHAIN.CORE]: {
-      fetch: async () => await getDexVolumeExports({ chain: CHAIN.CORE, factory: FACTORY_ADDRESS }),
+      fetch: async (options: FetchOptions) => getDexVolumeExports({ chain: CHAIN.CORE, factory: FACTORY_ADDRESS, })(options.startOfDay, null, options),
       start: 1715904000,
     },
   }
