488076ccf452a7bd510a9371df9a9142b12cea31
vulntor <22735155+algtm@users.noreply.github.com>
2024-06-02 22:48:36 +0800

    feat: change to getLog

--- aggregators/udex-agg/index.ts
+++ aggregators/udex-agg/index.ts
@@ -1,63 +1,40 @@
-import { FetchOptions, FetchResult, FetchV2, } from "../../adapters/types";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import { fetchURLWithRetry } from "../../helpers/duneRequest";
-import customBackfill from "../../helpers/customBackfill";
-import { ChainApi, api2 } from "@defillama/sdk";
+import { Chain } from "@defillama/sdk/build/general";
+import { FetchResult, FetchResultV2, FetchV2, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
 
-const chainsMap: Record<string, string> = {
-  BNB: "bsc",
-  //comming soon
-  // ETHEREUM: "ethereum",
-  // POLYGON: "polygon",
-  // BASE: "base",
-};
-function convertTimestampToUTCString(timestamp) {
-  const date = new Date(timestamp);
-  const year = date.getUTCFullYear();
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(date.getUTCDate()).padStart(2, '0');
-  const hours = String(date.getUTCHours()).padStart(2, '0');
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-  const milliseconds = String(date.getUTCMilliseconds()).padStart(3, '0');
-  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds} UTC`;
+let abi = ["event Swap(address indexed payer,address indexed payee,address fromToken,address toToken,uint fromAmount,uint receivedAmount)"];
 
-  return formattedDate;
+type IContract = {
+    [c: string | Chain]: string;
 }
 
-const fetch =
-  (chain: string) =>
-    async (opt:FetchOptions): Promise<FetchResult> => {
-      const utcString=convertTimestampToUTCString(opt.startOfDay*1000)//like 2024-05-31 00:00:00.000 UTC
-      const data = await fetchURLWithRetry(`https://api.dune.com/api/v1/query/3781797/results`)
-      const chainData = data.result.rows.find(
-        (row: any) => row.block_date === utcString
-      );
-      return {
-        dailyVolume: chainData.bscVolumeUSD,
-        dailyUserFees:chainData.feesUSD,
-        timestamp:opt.endTimestamp,
-      }
-    };
+const contract: IContract = {
+    [CHAIN.BSC]: '0x15Fa1b705D85Cf35b625d0Afa3Ab4360b92D019a',
+    [CHAIN.POLYGON]:'0xc6C4013D6f7D4313634798D6E516ef72D1912915',
+    [CHAIN.ETHEREUM]:'0x94aa97b013b9b1cc7915199e8621ddf664646339',
+    [CHAIN.BASE]:'0x94aa97b013b9b1cc7915199e8621ddf664646339'
 
-const adapter: any = {
-  timetravel: false,
-  version: 2,
-  adapter: {
-    ...Object.values(chainsMap).reduce((acc, chain) => {
-      return {
-        ...acc,
-        [(chainsMap as any)[chain] || chain]: {
-          fetch: fetch(chain),
-          runAtCurrTime: false,
-          start: 1703376000,
-          customBackfill: customBackfill(chain, () => fetch(chain))
+}
 
-        },
-      };
-    }, {}),
-  },
-  isExpensiveAdapter: true,
+const fetch: FetchV2 = async ({ getLogs, createBalances, chain, }): Promise<FetchResultV2> => {
+    const dailyVolume = createBalances();
+    const logs = (await getLogs({ target: contract[chain], eventAbi: abi[0], }))
+
+    logs.map((log: any) => dailyVolume.add(log.toToken, log.receivedAmount));
+
+    return { dailyVolume };
 };
 
+const adapter: SimpleAdapter = {
+    adapter: Object.keys(contract).reduce((acc, chain) => {
+        return {
+            ...acc,
+            [chain]: {
+                fetch,
+                start: 1703376000,
+            }
+        }
+    }, {}),
+    version: 2,
+};
 export default adapter;
