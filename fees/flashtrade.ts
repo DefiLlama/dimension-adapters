12a96fc05832e527eeea12f52b1e66c84afcab7f
unknown <ujjwalg056@gmail.com>
2024-06-04 17:11:19 +0530

    updated fees and revnue adapter

--- fees/flashtrade.ts
+++ fees/flashtrade.ts
@@ -8,39 +8,55 @@ interface Pool {
     paid: string;
 }
 
-
 const methodology = {
-    ProtocolReveneue: 'Sum of all fees accrued from LP pools.'
+    Fees: 'Sum of all fees accrued from LP pools.',
+    ProtocolReveneue: '30% of all the fees accrued excluding Community pool.'
 }
 
 const urlTotalStats = "https://api.prod.flash.trade/market-stat/revenue-all-time";
 const urlDailyStats = "https://api.prod.flash.trade/market-stat/revenue-24hr";
 
-const fetchFlashStats = async (timestamp: number): Promise<FetchResultFees> => {
-    const totalStats:Pool[]  =  (await fetchURL(urlTotalStats));
-    const dailyStats:Pool[]  = (await fetchURL(urlDailyStats));
-    const dailyAccured = dailyStats.reduce((sum, item) => sum + parseFloat(item.accured), 0);
-    const totalAccured = totalStats.reduce((sum, item) => sum + parseFloat(item.accured), 0);
-    return {
-      timestamp,
-      dailyProtocolRevenue: dailyAccured.toString(),
-      totalProtocolRevenue: totalAccured.toString(),
-    };
-  };
+const calculateProtocolRevenue = (stats: Pool[]) => {
+    return stats
+        .filter(item => item.pool !== "Community.1")
+        .reduce((sum, item) => sum + 0.3 * parseFloat(item.accured), 0);
+}
 
-  const adapter: Adapter = {
+const fetchFlashStats = async (timestamp: number): Promise<FetchResultFees> => {
+    const totalStatsResponse = await fetchURL(urlTotalStats);
+    const dailyStatsResponse = await fetchURL(urlDailyStats);
+
+    const totalStats: Pool[] = totalStatsResponse.data;
+    const dailyStats: Pool[] = dailyStatsResponse.data;
+
+    const dailyAccrued = dailyStats.reduce((sum, item) => sum + parseFloat(item.accured), 0);
+    const totalAccrued = totalStats.reduce((sum, item) => sum + parseFloat(item.accured), 0);
+
+    const dailyProtocolRevenue = calculateProtocolRevenue(dailyStats);
+    const totalProtocolRevenue = calculateProtocolRevenue(totalStats);
+
+    return {
+        timestamp,
+        dailyFees: dailyAccrued.toString(),
+        totalFees: totalAccrued.toString(),
+        dailyProtocolRevenue: dailyProtocolRevenue.toString(),
+        totalProtocolRevenue: totalProtocolRevenue.toString(),
+    };
+};
+
+const adapter: Adapter = {
     version: 2,
     adapter: {
-      [CHAIN.SOLANA]: {
-        runAtCurrTime: true,
-        customBackfill: undefined,
-        fetch: fetchFlashStats,
-        start: 0,
-        meta: {
-          methodology,
+        [CHAIN.SOLANA]: {
+            runAtCurrTime: true,
+            customBackfill: undefined,
+            fetch: fetchFlashStats,
+            start: 0,
+            meta: {
+                methodology,
+            },
         },
-      },
     },
-  };
-  
-  export default adapter;
+};
+
+export default adapter;
