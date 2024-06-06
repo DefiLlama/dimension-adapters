a580e81a204061fdc1241a3c8424b07d34b3b53e
stabble-devops <keisuke@stabble.org>
2024-06-06 16:43:06 -0400

    add stabble DEX

---
+++ dexs/stabble/index.ts
@@ -0,0 +1,25 @@
+import { CHAIN } from '../../helpers/chains';
+import { httpGet } from '../../utils/fetchURL';
+
+const REGISTRY_API = 'https://app.stabble.org/api/registry';
+
+async function fetch(timestamp: number) {
+    const registry = await httpGet(REGISTRY_API);
+    const vol24h = registry.pools
+        .filter((pool: any) => pool.chainId === 101)
+        .reduce((sum: number, pool: any) => sum + (pool?.stats?.volume_24h || 0), 0);
+    return {
+        dailyVolume: vol24h,
+        timestamp: timestamp
+    }
+}
+
+export default {
+    adapter: {
+        [CHAIN.SOLANA]: {
+            fetch: fetch,
+            runAtCurrTime: true,
+            start: 1719579600,
+        }
+    }
+}
