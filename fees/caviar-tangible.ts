247cd3e3a3a27bea976f2b32874ac8a0f354a9c7
waynebruce0x <waynebruce0x@gmail.com>
2024-06-06 13:21:21 +0100

    v2

--- fees/caviar-tangible.ts
+++ fees/caviar-tangible.ts
@@ -1,20 +1,20 @@
 import ADDRESSES from '../helpers/coreAssets.json'
-import { ChainBlocks, FetchOptions, FetchResultFees, SimpleAdapter } from "../adapters/types"
+import { FetchOptions, SimpleAdapter } from "../adapters/types"
 import { CHAIN } from "../helpers/chains"
 import { addTokensReceived } from '../helpers/token';
 
-const fetchFees = async (timestamp: number , _: ChainBlocks, options: FetchOptions): Promise<FetchResultFees> => {
+const fetchFees = async (options: FetchOptions) => {
   const dailyFees = await addTokensReceived({ options, tokens: [ADDRESSES.polygon.USDC], fromAddressFilter: '0xbbc843dcb1009bc7dc988bceb5bb1b50299d9a6d' , target: '0x6ced48efbb581a141667d7487222e42a3fa17cf7' })
 
   return {
     dailyFees: dailyFees,
     dailyHoldersRevenue: dailyFees,
     dailyRevenue: dailyFees,
-    timestamp
   }
 }
 
 const adapter: SimpleAdapter = {
+  version: 2,
   adapter: {
     [CHAIN.POLYGON]: {
       fetch: fetchFees,
