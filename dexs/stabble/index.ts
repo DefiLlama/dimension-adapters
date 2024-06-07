fa45aebd4753a79a9dde15222ec2941a20a2b516
stabble-devops <keisuke@stabble.org>
2024-06-07 17:54:32 -0400

    chore: fetch registry by mainnet only

--- dexs/stabble/index.ts
+++ dexs/stabble/index.ts
@@ -1,13 +1,11 @@
 import { CHAIN } from '../../helpers/chains';
 import { httpGet } from '../../utils/fetchURL';
 
-const REGISTRY_API = 'https://app.stabble.org/api/registry';
+const REGISTRY_API = 'https://app.stabble.org/api/registry?cluster=mainnet-beta';
 
 async function fetch(timestamp: number) {
     const registry = await httpGet(REGISTRY_API);
-    const vol24h = registry.pools
-        .filter((pool: any) => pool.chainId === 101)
-        .reduce((sum: number, pool: any) => sum + (pool?.stats?.volume_24h || 0), 0);
+    const vol24h = registry.pools.reduce((sum: number, pool: any) => sum + (pool?.stats?.volume_24h || 0), 0);
     return {
         dailyVolume: vol24h,
         timestamp: timestamp
