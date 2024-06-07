a25c6cbd0affcc302cc57ea9bd45d7e2e1bacde9
0xngmi <0xngmi@protonmail.com>
2024-06-07 01:49:01 +0100

    add sui + pumpup

---
+++ fees/pumpup.ts
@@ -0,0 +1,22 @@
+import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import {queryEvents} from "../helpers/sui"
+
+const fetchFees = async (options: FetchOptions) => {
+        const dailyFees = options.createBalances()
+        const events = await queryEvents({eventType:"0x3f2a0baf78f98087a04431f848008bad050cb5f4427059fa08eeefaa94d56cca::curve::Points", options})
+        events.map(ev=>dailyFees.addCGToken("sui", ev.amount/1e9))
+
+        return { dailyFees, dailyRevenue: dailyFees }
+    }
+
+const adapters: SimpleAdapter = {
+    adapter: {
+        [CHAIN.SUI]: {
+            fetch: fetchFees,
+            start: 0
+        },
+    },
+    version: 2
+}
+export default adapters;
