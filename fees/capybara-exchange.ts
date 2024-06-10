cca81c3f2a89224d028c6793a6adf8578f3fea25
0xgnek <0xgnek@gmail.com>
2024-06-10 14:34:54 +0000

    remove not use

--- fees/capybara-exchange.ts
+++
@@ -1,5 +0,0 @@
-import { Adapter } from "../adapters/types";
-import volumeAdapter from "../dexs/capybara-exchange";
-import { getDexChainFees } from "../helpers/getUniSubgraphFees";
-
-export default volumeAdapter;
