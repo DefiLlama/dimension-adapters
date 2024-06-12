db1975f0d60edcabde4159c9e54b31ce3aa7fbdd
mul53 <mulengabowa53@gmail.com>
2024-06-12 20:00:04 +0200

    add voltage-v3 volume

---
+++ dexs/voltage-v3/index.ts
@@ -0,0 +1,40 @@
+import { SimpleAdapter } from "../../adapters/types";
+import {CHAIN} from "../../helpers/chains";
+import { getGraphDimensions } from "../../helpers/getUniSubgraph";
+
+const endpoint = {
+    [CHAIN.FUSE]: "https://api.studio.thegraph.com/query/78455/exchange-v3/version/latest",
+};
+
+const v3Graph = getGraphDimensions({
+    graphUrls: endpoint,
+    totalVolume: {
+        factory: "factories"
+    },
+    dailyVolume: {
+        factory: "pancakeDayData",
+        field: 'volumeUSD'
+    },
+    totalFees: {
+        factory: "factories"
+    },
+    dailyFees: {
+        factory: "pancakeDayData",
+        field: "feesUSD"
+    }
+})
+
+const v3StartTimes = {
+    [CHAIN.FUSE]: 1703725380,
+}
+
+const adapter: SimpleAdapter = {
+    adapter: {
+        [CHAIN.FUSE]: {
+            fetch: v3Graph(CHAIN.FUSE),
+            start: v3StartTimes[CHAIN.FUSE]
+        }
+    }
+}
+
+export default adapter
