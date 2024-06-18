baf661ee7f023d0d78e6cd82f3a2e6c7c35c8b78
0xgnek <0xgnek@gmail.com>
2024-06-18 12:00:29 +0000

    fix adapter

--- dexs/sundaeswap/index.ts
+++ dexs/sundaeswap/index.ts
@@ -1,8 +1,7 @@
-import fetchURL from "../../utils/fetchURL"
-import { ChainBlocks, DISABLED_ADAPTER_KEY, FetchOptions, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import fetchURL from "../../utils/fetchURL";
+import { FetchOptions, FetchResultV2, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import disabledAdapter from "../../helpers/disabledAdapter";
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 
 const historicalVolumeEndpoint = "https://stats.sundaeswap.finance/api/defillama/v0/global-stats/2100"
 
@@ -11,21 +10,24 @@ interface IVolumeall {
   day: string;
 }
 
-const fetch = async (timestamp: number, _: ChainBlocks, { createBalances, startOfDay }: FetchOptions): Promise<FetchResultVolume> => {
+const fetch = async ({ createBalances, startOfDay }: FetchOptions): Promise<FetchResultV2> => {
   const dailyVolume = createBalances()
+  const dayTimestamp = getTimestampAtStartOfDayUTC(startOfDay);
+  const dateStr = new Date(dayTimestamp * 1000).toISOString().split('T')[0];
   const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint)).response;
-
-  dailyVolume.addGasToken(historicalVolume
-    .find(dayItem => getUniqStartOfTodayTimestamp(new Date(dayItem.day)) === startOfDay)?.volumeLovelace as any)
+  const volume = historicalVolume.find(dayItem => dayItem.day === dateStr)?.volumeLovelace as any
+  if (!volume) {
+    return {}
+  }
+  dailyVolume.addGasToken(volume)
   return {
     dailyVolume,
-    timestamp: startOfDay,
   };
 };
 
 const adapter: SimpleAdapter = {
+  version: 2,
   adapter: {
-    [DISABLED_ADAPTER_KEY]: disabledAdapter,
     [CHAIN.CARDANO]: {
       fetch,
       start: 1643673600,
