b0b4fd82876dda035972f7b079eccc6989f4c764
aya <aya.belarbi16@gmail.com>
2024-06-19 14:37:37 +0100

    fix: conflicts chains

--- helpers/chains.ts
+++ helpers/chains.ts
@@ -141,7 +141,7 @@ export enum CHAIN {
   FILECOIN = "filecoin",
   FRAXTAL = "fraxtal",
   IMX = "imx",
-  KROMA = "kroma", 
+  KROMA = "kroma",
   ZORA = "zora",
   SVM = "svm",
   ASTRZK = "astrzk",
