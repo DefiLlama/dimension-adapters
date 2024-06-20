9fd62765330cf0abd01a6edaad54e63f4e44a926
0xngmi <80541789+0xngmi@users.noreply.github.com>
2024-06-20 23:32:14 +0100

    Update index.ts

--- aggregators/rubic/index.ts
+++ aggregators/rubic/index.ts
@@ -35,7 +35,7 @@ const chains: Record<string, string> = {
   [CHAIN.OKEXCHAIN]: 'oke-x-chain',
   [CHAIN.CRONOS]: 'cronos',
   [CHAIN.MODE]: 'mode',
-  [CHAIN.MERLIN]: 'merlin'
+  [CHAIN.MERLIN]: 'merlin',
   [CHAIN.CORE]: 'core'
 };
 
