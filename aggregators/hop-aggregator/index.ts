23746f3ac713362305917e538f1c87590f4d9790
0xgnek <0xgnek@gmail.com>
2024-06-21 16:28:30 +0000

    fix adapter does not support v2

--- aggregators/hop-aggregator/index.ts
+++ aggregators/hop-aggregator/index.ts
@@ -1,9 +1,10 @@
 import { CHAIN } from "../../helpers/chains";
 import { httpPost } from "../../utils/fetchURL";
 import { FetchOptions } from "../../adapters/types";
+import { getTimestampAtStartOfDayUTC } from "../../utils/date"
 
-const fetchVolume = async (options: FetchOptions) => {
-  let timestamp = options.startOfDay * 1_000;
+const fetchVolume = async (_t: any, _b: any ,options: FetchOptions) => {
+  const timestamp = getTimestampAtStartOfDayUTC(options.startOfDay) * 1_000;
 
   const res = await httpPost("https://d3axhvc6i89jmo.cloudfront.net/api/volume", { timestamp });
   const record = res.record || {};
@@ -11,14 +12,11 @@ const fetchVolume = async (options: FetchOptions) => {
   return {
     dailyVolume: record.total_volume_usd ? record.total_volume_usd.toFixed(2) : 0,
     totalVolume: res.total_volume_usd ? res.total_volume_usd.toFixed(2) : 0,
-    dailyFees: 0,
-    totalFees: 0,
     timestamp: timestamp,
   };
 };
 
 const adapter: any = {
-  version: 2,
   adapter: {
     [CHAIN.SUI]: {
       fetch: fetchVolume,
