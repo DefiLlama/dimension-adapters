71c077218f103246a507fa0baf29756ad2cfc62b
Vinay Sagar <vinaysagar@Vinays-MacBook-Pro.local>
2024-06-25 13:38:38 +0530

    add boba chain to wefi

--- dexs/wefi/index.ts
+++ dexs/wefi/index.ts
@@ -5,6 +5,7 @@ const address: any = {
   [CHAIN.LINEA]: '0x7e0da0deccac2e7b9ad06e378ee09c15b5bdeefa',
   [CHAIN.XDC]: '0x7e0da0deccac2e7b9ad06e378ee09c15b5bdeefa',
   [CHAIN.POLYGON]: '0xA42e5d2A738F83a1e1a907eB3aE031e5A768C085',
+  [CHAIN.BOBA]: '0x7E0DA0DECCAc2E7B9AD06E378ee09c15B5BDeefa'
 }
 
 const fetchVolume: FetchV2 = async (options: FetchOptions) => {
@@ -36,6 +37,10 @@ const adapter: SimpleAdapter = {
       start: 1704067200,
     },
     [CHAIN.POLYGON]: {
+      fetch: fetchVolume,
+      start: 1704067200,
+    },
+    [CHAIN.BOBA]: {
       fetch: fetchVolume,
       start: 1704067200,
     },
