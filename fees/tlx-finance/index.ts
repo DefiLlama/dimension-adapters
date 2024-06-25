5b1eef838abeb1ce3966b5c28dac7117a0b2ad75
Chase Manning <chase@manning.dev>
2024-06-25 15:52:37 +0100

    add tlx finance fees

---
+++ fees/tlx-finance/index.ts
@@ -0,0 +1,37 @@
+import { Adapter, FetchOptions } from "../../adapters/types";
+import { OPTIMISM } from "../../helpers/chains";
+
+const STAKER = "0xc30877315f3b621a8f7bcda27819ec29429f3817";
+const DONATE_EVENT =
+  "event DonatedRewards(address indexed account, uint256 amount)";
+
+const getFees = async (options: FetchOptions) => {
+  const { getToBlock, getFromBlock, getLogs } = options;
+  const [fromBlock, toBlock] = await Promise.all([
+    getFromBlock(),
+    getToBlock(),
+  ]);
+  const logs = await getLogs({
+    targets: [STAKER],
+    eventAbi: DONATE_EVENT,
+    fromBlock,
+    toBlock,
+  });
+  const fees = logs.reduce((acc: any, log: any) => acc + Number(log.amount), 0);
+  return {
+    dailyFees: fees,
+    dailyRevenue: fees,
+    dailyHoldersRevenue: fees,
+  };
+};
+
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+    [OPTIMISM]: {
+      fetch: getFees,
+      start: 1712727843,
+    },
+  },
+};
+export default adapter;
