cad58afc2c08a385b9179f226a092bc522c9f11d
0xgnek <0xgnek@gmail.com>
2024-06-25 16:31:50 +0000

    fixtlx-finance

--- fees/tlx-finance/index.ts
+++ fees/tlx-finance/index.ts
@@ -1,25 +1,9 @@
-import BigNumber from "bignumber.js";
 import { Adapter, FetchOptions } from "../../adapters/types";
 import { OPTIMISM } from "../../helpers/chains";
 
 const STAKER = "0xc30877315f3b621a8f7bcda27819ec29429f3817";
-const DONATE_EVENT =
-  "event DonatedRewards(address indexed account, uint256 amount)";
-
-const sUsdPrice = async (): Promise<number> => {
-  const ID = "optimism:0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9";
-  const ENDPOINT = `https://coins.llama.fi/prices/current/${ID}`;
-  const response = await fetch(ENDPOINT);
-  const data = await response.json();
-  if (!data) throw new Error("no data");
-  if (!data.coins) throw new Error("no data.coins");
-  const priceData = data.coins[ID];
-  if (!priceData) throw new Error("no priceData");
-  const price = priceData.price;
-  if (!price) throw new Error("no price");
-  if (price === 0) throw new Error("price is 0");
-  return price;
-};
+const DONATE_EVENT = "event DonatedRewards(address indexed account, uint256 amount)";
+const token = "0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9"
 
 const getFees = async (options: FetchOptions) => {
   const { getLogs } = options;
@@ -27,17 +11,15 @@ const getFees = async (options: FetchOptions) => {
     targets: [STAKER],
     eventAbi: DONATE_EVENT,
   });
-  const sUsdFees = logs
-    .reduce((acc: any, log: any) => acc.plus(log.amount), new BigNumber(0))
-    .div(1e18)
-    .toNumber();
-  const susdPrice = await sUsdPrice();
-  const fees = sUsdFees * susdPrice;
+  const dailyFees = options.createBalances();
+  logs.forEach((log) => {
+    dailyFees.add(token, log.amount);
+  })
 
   return {
-    dailyFees: `${fees}`,
-    dailyRevenue: `${fees}`,
-    dailyHoldersRevenue: `${fees}`,
+    dailyFees: dailyFees,
+    dailyRevenue: dailyFees,
+    dailyHoldersRevenue: dailyFees,
   };
 };
 
