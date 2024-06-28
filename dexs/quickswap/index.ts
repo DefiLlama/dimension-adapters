b3e40d5adadb6c01f6f413b22c92d0cac50780e7
0xoscario <0xoscario@gmail.com>
2024-06-28 13:35:44 -0400

    Update Volume, Fees & Revenue Sources

--- dexs/quickswap/index.ts
+++ dexs/quickswap/index.ts
@@ -15,22 +15,13 @@ const graphs = getChainVolume({
     factory: DEFAULT_TOTAL_VOLUME_FACTORY,
     field: DEFAULT_TOTAL_VOLUME_FIELD,
   },
-  dailyVolume: {
-    factory: DEFAULT_DAILY_VOLUME_FACTORY,
-    field: DEFAULT_DAILY_VOLUME_FIELD,
-    dateField: "date"
-  },
 });
 
 const endpointsAlgebraV3 = {
   [CHAIN.POLYGON]: sdk.graph.modifyEndpoint('CCFSaj7uS128wazXMdxdnbGA3YQnND9yBdHjPtvH7Bc7'),
   // [CHAIN.DOGECHAIN]: "https://graph-node.dogechain.dog/subgraphs/name/quickswap/dogechain-info",
-  [CHAIN.POLYGON_ZKEVM]:"https://api.studio.thegraph.com/query/44554/quickswap-v3-02/0.0.7",
-  [CHAIN.MANTA]:"https://api.goldsky.com/api/public/project_clo2p14by0j082owzfjn47bag/subgraphs/quickswap/prod/gn",
-  [CHAIN.XLAYER]: "https://api.studio.thegraph.com/query/44554/qs-xlayer-v3/version/latest"
-};
-
-const endpointsUniV3 = {
+  [CHAIN.POLYGON_ZKEVM]: "https://api.studio.thegraph.com/query/44554/quickswap-v3-02/version/latest",
+  [CHAIN.XLAYER]: "https://api.studio.thegraph.com/query/44554/qs-xlayer-v3/version/latest",
   [CHAIN.MANTA]:"https://api.goldsky.com/api/public/project_clo2p14by0j082owzfjn47bag/subgraphs/quickswap/prod/gn",
   [CHAIN.ASTAR_ZKEVM]:"https://api.studio.thegraph.com/query/44554/astar-quickswap/version/latest",
   [CHAIN.IMX]: "https://api.goldsky.com/api/public/project_clo2p14by0j082owzfjn47bag/subgraphs/quickswap-IMX/prod/gn",
@@ -47,10 +38,11 @@ const graphsAlgebraV3 = getChainVolume({
     field: "volumeUSD",
     dateField: "date"
   },
+  hasDailyVolume: true
 });
 
-const v3GraphsUni = getGraphDimensions({
-  graphUrls: endpointsUniV3,
+const graphsAlgebraV3_1 = getChainVolume({
+  graphUrls: endpointsAlgebraV3,
   totalVolume: {
     factory: "factories",
     field: "totalVolumeUSD",
@@ -59,20 +51,25 @@ const v3GraphsUni = getGraphDimensions({
     factory: "uniswapDayData",
     field: "volumeUSD",
   },
-  feesPercent: {
-    type: "fees",
-    ProtocolRevenue: 0,
-    HoldersRevenue: 0,
-    UserFees: 100, // User fees are 100% of collected fees
-    SupplySideRevenue: 100, // 100% of fees are going to LPs
-    Revenue: 0, // Set revenue to 0 as protocol fee is not set for all pools for now
-  },
+  hasDailyVolume: true
 });
 
+type TStartTime = {
+  [s: string | CHAIN]: number;
+}
+
+const startTimeV3: TStartTime = {
+  [CHAIN.POLYGON]: 1662425243,
+  [CHAIN.POLYGON_ZKEVM]: 1679875200,
+  [CHAIN.XLAYER]: 1712657439,
+  [CHAIN.MANTA]: 1697690974,
+  [CHAIN.ASTAR_ZKEVM]: 1709284529,
+  [CHAIN.IMX]: 1702977793,
+  // [CHAIN.DOGECHAIN]: 1660694400,
+}
 
 const fetchLiquidityHub = async (timestamp: number) => {
     let dailyResult = (await fetchURL('https://hub.orbs.network/analytics-daily/v1'));
-
     let rows = dailyResult.result.rows;
     let lastDay = rows[rows.length - 1];
     let dailyVolume = lastDay.daily_total_calculated_value;
@@ -83,9 +80,7 @@ const fetchLiquidityHub = async (timestamp: number) => {
         totalVolume: `${totalVolume}`,
         timestamp: timestamp,
     };
-
 }
-
 
 const adapter: BreakdownAdapter = {
   version: 2,
@@ -99,20 +94,32 @@ const adapter: BreakdownAdapter = {
     v3: {
       [CHAIN.POLYGON]: {
         fetch: graphsAlgebraV3(CHAIN.POLYGON),
-        start: 1662425243
+        start: startTimeV3[CHAIN.POLYGON]
       },
-      // [CHAIN.DOGECHAIN]: {
-      //   fetch: graphsV3(CHAIN.DOGECHAIN),
-      //   start: 1660694400
-      // },
+    //   // [CHAIN.DOGECHAIN]: {
+    //   //   fetch: graphsV3(CHAIN.DOGECHAIN),
+    //   //   start: 1660694400
+    //   // },
       [CHAIN.POLYGON_ZKEVM]: {
         fetch: graphsAlgebraV3(CHAIN.POLYGON_ZKEVM),
-        start: 1679875200
+        start: startTimeV3[CHAIN.POLYGON_ZKEVM]
       },
       [CHAIN.MANTA]: {
-        fetch: v3GraphsUni(CHAIN.MANTA),
-        start: 1697690974
-      }
+        fetch: graphsAlgebraV3_1(CHAIN.MANTA),
+        start: startTimeV3[CHAIN.MANTA]
+      },
+      [CHAIN.ASTAR_ZKEVM]: {
+        fetch: graphsAlgebraV3_1(CHAIN.ASTAR_ZKEVM),
+        start: startTimeV3[CHAIN.ASTAR_ZKEVM]
+      },
+      [CHAIN.IMX]: {
+        fetch: graphsAlgebraV3_1(CHAIN.IMX),
+        start: startTimeV3[CHAIN.IMX]
+      },
+      [CHAIN.XLAYER]: {
+        fetch: graphsAlgebraV3(CHAIN.XLAYER),
+        start: startTimeV3[CHAIN.XLAYER]
+      },
     },
     liquidityHub: {
       [CHAIN.POLYGON]: {
@@ -121,6 +128,7 @@ const adapter: BreakdownAdapter = {
       },
     },
   },
+
 };
 
 export default adapter;
--- fees/quickswap.ts
+++ fees/quickswap.ts
@@ -26,9 +26,12 @@ const v2Graph = getGraphDimensions({
 
 const v3Endpoints = {
   [CHAIN.POLYGON]: sdk.graph.modifyEndpoint('CCFSaj7uS128wazXMdxdnbGA3YQnND9yBdHjPtvH7Bc7'),
-  // [CHAIN.DOGECHAIN]: "https://graph-node.dogechain.dog/subgraphs/name/quickswap/dogechain-info",
-  [CHAIN.POLYGON_ZKEVM]:"https://api.studio.thegraph.com/query/44554/quickswap-v3-02/0.0.7",
+  // // [CHAIN.DOGECHAIN]: "https://graph-node.dogechain.dog/subgraphs/name/quickswap/dogechain-info",
+  [CHAIN.POLYGON_ZKEVM]: "https://api.studio.thegraph.com/query/44554/quickswap-v3-02/version/latest",
+  [CHAIN.XLAYER]: "https://api.studio.thegraph.com/query/44554/qs-xlayer-v3/version/latest",
   [CHAIN.MANTA]:"https://api.goldsky.com/api/public/project_clo2p14by0j082owzfjn47bag/subgraphs/quickswap/prod/gn",
+  [CHAIN.ASTAR_ZKEVM]:"https://api.studio.thegraph.com/query/44554/astar-quickswap/version/latest",
+  [CHAIN.IMX]: "https://api.goldsky.com/api/public/project_clo2p14by0j082owzfjn47bag/subgraphs/quickswap-IMX/prod/gn",
 }
 
 type TStartTime = {
@@ -38,7 +41,11 @@ type TStartTime = {
 const startTimeV3: TStartTime = {
   [CHAIN.POLYGON]: 1662425243,
   [CHAIN.POLYGON_ZKEVM]: 1679875200,
-  [CHAIN.DOGECHAIN]: 1660694400,
+  [CHAIN.XLAYER]: 1712657439,
+  [CHAIN.MANTA]: 1697690974,
+  [CHAIN.ASTAR_ZKEVM]: 1709284529,
+  [CHAIN.IMX]: 1702977793,
+  // [CHAIN.DOGECHAIN]: 1660694400,
 }
 
 const v3Graphs = getGraphDimensions({
@@ -64,8 +71,35 @@ const v3Graphs = getGraphDimensions({
     UserFees: 100, // User fees are 100% of collected fees
     SupplySideRevenue: 100, // 100% of fees are going to LPs
     Revenue: 0 // Revenue is 100% of collected fees
-  }
+  },
 });
+
+const v3Graphs1 = getGraphDimensions({
+  graphUrls: v3Endpoints,
+  totalVolume: {
+    factory: "factories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "uniswapDayData",
+    field: "volumeUSD",
+    dateField: "date"
+  },
+  dailyFees: {
+    factory: "uniswapDayData",
+    field: "feesUSD",
+  },
+  feesPercent: {
+    type: "fees",
+    ProtocolRevenue: 0,
+    HoldersRevenue: 0,
+    Fees: 0,
+    UserFees: 100, // User fees are 100% of collected fees
+    SupplySideRevenue: 100, // 100% of fees are going to LPs
+    Revenue: 0, // Revenue is 100% of collected fees
+  },
+});
+
 
 const methodology = {
   UserFees: "User pays 0.3% fees on each swap.",
@@ -73,7 +107,7 @@ const methodology = {
   Revenue: "Protocol have no revenue",
   ProtocolRevenue: "Protocol have no revenue.",
   SupplySideRevenue: "All user fees are distributed among LPs.",
-  HoldersRevenue: "Holders have no revenue."
+  HoldersRevenue: "Holders have no revenue.",
 }
 
 const adapter: BreakdownAdapter = {
@@ -88,16 +122,32 @@ const adapter: BreakdownAdapter = {
         },
       },
     },
-    v3: Object.keys(v3Endpoints).reduce((acc, chain) => {
-      acc[chain] = {
-        fetch: v3Graphs(chain as Chain),
-        start: startTimeV3[chain],
-        meta: {
-          methodology
-        }
-      }
-      return acc
-    }, {} as BaseAdapter)
+    v3: {
+      [CHAIN.POLYGON]: {
+        fetch: v3Graphs(CHAIN.POLYGON),
+        start: startTimeV3[CHAIN.POLYGON],
+      },
+      [CHAIN.POLYGON_ZKEVM]: {
+        fetch: v3Graphs(CHAIN.POLYGON_ZKEVM),
+        start: startTimeV3[CHAIN.POLYGON_ZKEVM],
+      },
+      [CHAIN.XLAYER]: {
+        fetch: v3Graphs(CHAIN.XLAYER),
+        start: startTimeV3[CHAIN.XLAYER],
+      },
+      [CHAIN.MANTA]: {
+        fetch: v3Graphs1(CHAIN.MANTA),
+        start: startTimeV3[CHAIN.MANTA],
+      },
+      [CHAIN.ASTAR_ZKEVM]: {
+        fetch: v3Graphs1(CHAIN.ASTAR_ZKEVM),
+        start: startTimeV3[CHAIN.ASTAR_ZKEVM],
+      },
+      [CHAIN.IMX]: {
+        fetch: v3Graphs1(CHAIN.IMX),
+        start: startTimeV3[CHAIN.IMX],
+      },
+    },
   }
 }
 
