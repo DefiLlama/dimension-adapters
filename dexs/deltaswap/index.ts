3b68b5679a5910399643dc86b8e3d9c60d0d1808
Daniel Alcarraz <dam417@nyu.edu>
2024-06-28 12:27:42 -0400

    updated chain in options to correct chain for Arbitrum, Base, and Ethereum. Also updated start time in seconds for all chains

--- dexs/deltaswap/index.ts
+++ dexs/deltaswap/index.ts
@@ -5,16 +5,16 @@ import { getDexVolumeExports } from "../../helpers/dexVolumeLogs";
 const adapters: SimpleAdapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
-      fetch: getDexVolumeExports({ chain: CHAIN.MANTLE, factory: '0xcb85e1222f715a81b8edaeb73b28182fa37cffa8' }),
-      start: 1706745600,
+      fetch: getDexVolumeExports({ chain: CHAIN.ARBITRUM, factory: '0xcb85e1222f715a81b8edaeb73b28182fa37cffa8' }),
+      start: 1705968000,
     },
     [CHAIN.BASE]: {
-      fetch: getDexVolumeExports({ chain: CHAIN.MANTLE, factory: '0x9a9a171c69cc811dc6b59bb2f9990e34a22fc971' }),
-      start: 14455515,
+      fetch: getDexVolumeExports({ chain: CHAIN.BASE, factory: '0x9a9a171c69cc811dc6b59bb2f9990e34a22fc971' }),
+      start: 1715644800,
     },
     [CHAIN.ETHEREUM]: {
-      fetch: getDexVolumeExports({ chain: CHAIN.MANTLE, factory: '0x5fbe219e88f6c6f214ce6f5b1fcaa0294f31ae1b' }),
-      start: 19964909,
+      fetch: getDexVolumeExports({ chain: CHAIN.ETHEREUM, factory: '0x5fbe219e88f6c6f214ce6f5b1fcaa0294f31ae1b' }),
+      start: 1716854400,
     }
   }
 }
