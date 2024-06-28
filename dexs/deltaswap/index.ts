78b74f8f3c21422f48c1ca4261dc7487b6f74443
Daniel Alcarraz <dam417@nyu.edu>
2024-06-28 12:08:02 -0400

    adding DeltaSwapFactory for Base and Ethereum in dexs/deltaswap

--- dexs/deltaswap/index.ts
+++ dexs/deltaswap/index.ts
@@ -2,12 +2,19 @@ import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getDexVolumeExports } from "../../helpers/dexVolumeLogs";
 
-const FACTORY_ADDRESS = '0xcb85e1222f715a81b8edaeb73b28182fa37cffa8'
 const adapters: SimpleAdapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
-      fetch: getDexVolumeExports({ chain: CHAIN.MANTLE, factory: FACTORY_ADDRESS }),
+      fetch: getDexVolumeExports({ chain: CHAIN.MANTLE, factory: '0xcb85e1222f715a81b8edaeb73b28182fa37cffa8' }),
       start: 1706745600,
+    },
+    [CHAIN.BASE]: {
+      fetch: getDexVolumeExports({ chain: CHAIN.MANTLE, factory: '0x9a9a171c69cc811dc6b59bb2f9990e34a22fc971' }),
+      start: 14455515,
+    },
+    [CHAIN.ETHEREUM]: {
+      fetch: getDexVolumeExports({ chain: CHAIN.MANTLE, factory: '0x5fbe219e88f6c6f214ce6f5b1fcaa0294f31ae1b' }),
+      start: 19964909,
     }
   }
 }
