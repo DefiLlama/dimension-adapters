519fdc6546d1c743d48aed1217780e1a284fc8ea
0xgnek <0xgnek@gmail.com>
2024-07-03 17:35:39 +0000

    fix adapter

--- fees/lynex-v1.ts
+++ fees/lynex-v1.ts
@@ -1,7 +1,7 @@
 import { Chain } from "@defillama/sdk/build/general";
 import BigNumber from "bignumber.js";
 import request, { gql } from "graphql-request";
-import { Adapter, FetchResultFees } from "../adapters/types";
+import { Adapter, FetchOptions, FetchResultFees, FetchResultV2 } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
 import { getTimestampAtStartOfDayUTC } from "../utils/date";
@@ -19,96 +19,73 @@ interface IPair {
 type IURL = {
   [l: string | Chain]: string;
 };
+interface IPair {
+  id: string;
+  volumeUSD: string;
+  isStable: boolean;
+}
 
-const STABLE_FEE = 0.0001;
-const VOLATILE_FEE = 0.0025;
+interface IGraphResponse {
+  today: IPair[];
+  yesterday: IPair[];
+}
+const STABLE_FEES = 0.0001;
+const VOLATILE_FEES = 0.0025;
 
 const endpoints: IURL = {
   [CHAIN.LINEA]:
     "https://api.studio.thegraph.com/query/59052/lynex-v1/version/latest",
 };
 
-const fetch = (chain: Chain) => {
-  return async (timestamp: number): Promise<FetchResultFees> => {
-    console.log("Starting fetch for timestamp:", timestamp);
-    const todayTimestamp = getUniqStartOfTodayTimestamp(
-      new Date(timestamp * 1000)
-    );
-    const dateId = Math.floor(
-      getTimestampAtStartOfDayUTC(todayTimestamp) / 86400
-    ).toString();
+const fetch = async ({ getFromBlock, getToBlock, chain}: FetchOptions): Promise<FetchResultV2> => {
+    const [fromBlock, toBlock] = await Promise.all([getFromBlock(), getToBlock()])
 
-    console.log("Calculated dateId:", dateId);
-
-    const pairsQuery = gql`
-      query getPairs {
-        pairs(first: 500, orderBy: trackedReserveETH, orderDirection: desc) {
+    const query = gql`
+      query fees {
+        yesterday: pairs(block: {number: ${fromBlock}}, where: {volumeUSD_gt: "0"}, first: 1000) {
           id
           isStable
+          volumeUSD
         }
-      }
-    `;
-
-    const pairDayDataQuery = gql`
-      query getPairDayData($id: String!) {
-        pairDayData(id: $id) {
-          dailyVolumeUSD
+        today: pairs(block: {number: ${toBlock}}, where: {volumeUSD_gt: "0"}, first: 1000) {
           id
+          isStable
+          volumeUSD
         }
       }
     `;
 
-    let pairsResult;
-    try {
-      pairsResult = await request(endpoints[chain], pairsQuery);
-      console.log("Pairs result:", pairsResult);
-    } catch (error) {
-      console.error("Error fetching pairs:", error);
-      return { timestamp };
-    }
-
-    const pairs: IPair[] = pairsResult.pairs;
-    let totalFeesUSD = new BigNumber(0);
-
-    for (const pair of pairs) {
-      try {
-        const pairDayDataResult = await request(
-          endpoints[chain],
-          pairDayDataQuery,
-          { id: `${pair.id}-${dateId}` }
-        );
-        const pairDayData: IPairDayData = pairDayDataResult.pairDayData;
-
-        if (pairDayData) {
-          const volumeUSD = new BigNumber(pairDayData.dailyVolumeUSD);
-          const feeRate = pair.isStable ? STABLE_FEE : VOLATILE_FEE;
-          const dailyFeesUSD = volumeUSD.multipliedBy(feeRate);
-
-          totalFeesUSD = totalFeesUSD.plus(dailyFeesUSD);
-        } else {
-          console.log(`No data for pair: ${pair.id} on date: ${dateId}`);
-        }
-      } catch (error) {
-        console.error(`Error fetching pairDayData for pair ${pair.id}:`, error);
+    const graphRes: IGraphResponse = await request(endpoints[chain], query);
+    const totalFeesToday = graphRes.today.reduce((acc, pool) => {
+      if (pool.isStable) {
+        return acc.plus(new BigNumber(pool.volumeUSD).times(STABLE_FEES));
+      } else {
+        return acc.plus(new BigNumber(pool.volumeUSD).times(VOLATILE_FEES));
       }
-    }
+    }, new BigNumber(0));
 
-    if (totalFeesUSD.isZero()) return { timestamp };
+    const totalFeesYesterday = graphRes.yesterday.reduce((acc, pool) => {
+      if (pool.isStable) {
+        return acc.plus(new BigNumber(pool.volumeUSD).times(STABLE_FEES));
+      } else {
+        return acc.plus(new BigNumber(pool.volumeUSD).times(VOLATILE_FEES));
+      }
+    }, new BigNumber(0));
+    const dailyFee = totalFeesToday.minus(totalFeesYesterday);
 
     return {
-      timestamp,
-      dailyFees: totalFeesUSD.toString(),
-      dailyUserFees: totalFeesUSD.toString(),
-      dailyRevenue: totalFeesUSD.toString(),
-      dailyHoldersRevenue: totalFeesUSD.toString(),
+      dailyFees: dailyFee.toString(),
+      dailyUserFees: dailyFee.toString(),
+      dailyRevenue: dailyFee.toString(),
+      dailyHoldersRevenue: dailyFee.toString(),
     };
-  };
 };
 
 const adapter: Adapter = {
+  version: 2,
   adapter: {
     [CHAIN.LINEA]: {
-      fetch: fetch(CHAIN.LINEA),
+      fetch: fetch,
       start: 1691394680,
     },
   },
