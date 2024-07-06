e1609432c0b9c55cbfdc781a3b112db94cda0263
0xpeluche <0xpeluche@protonmail.com>
2024-07-06 15:41:28 +0200

    use FetchOptions instead of sdk.ChainApi

--- fees/ether-fi/index.ts
+++ fees/ether-fi/index.ts
@@ -1,40 +1,34 @@
 // https://etherfi.gitbook.io/etherfi/liquid/technical-documentation#fees
-import * as sdk from "@defillama/sdk";
-import { Adapter } from "../../adapters/types";
+import { Adapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const LIQUID_VAULT = "0xeA1A6307D9b18F8d1cbf1c3Dd6aad8416C06a221";
 const YEAR = 365;
 const ANNUAL_FEE_RATE = 0.02;
 
-const getDailyFees = async (api: sdk.ChainApi): Promise<number> => {
+const fetch = async (options: FetchOptions) => {
   const dailyFeeRate = ANNUAL_FEE_RATE / YEAR;
+  const dailyFees = options.createBalances();
   const [asset, totalSupply] = await Promise.all([
-    await api.call({
+    options.api.call({
       target: LIQUID_VAULT,
       abi: "function asset() external view returns (address)",
     }),
-    await api.call({
+    options.api.call({
       target: LIQUID_VAULT,
       abi: "function totalAssets() external view returns (uint256 assets)",
     }),
   ]);
-
-  api.addTokens(asset, totalSupply);
-  return (await api.getUSDValue()) * dailyFeeRate;
+  dailyFees.add(asset, totalSupply);
+  dailyFees.resizeBy(dailyFeeRate);
+  return { dailyFees };
 };
 
 const adapter: Adapter = {
   version: 2,
   adapter: {
     [CHAIN.ETHEREUM]: {
-      fetch: async () => {
-        const api = new sdk.ChainApi({ chain: CHAIN.ETHEREUM });
-        const dailyFees = await getDailyFees(api);
-        return {
-          dailyFees: Number(dailyFees.toFixed(0)),
-        };
-      },
+      fetch,
       meta: {
         methodology: {
           totalFees:
