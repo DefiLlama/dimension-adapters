697313d25cee5a39fe8fd1324f921cf425f69bee
htanh9320 <htanh9320@gmail.com>
2024-07-08 14:14:30 +0700

    7k aggregator volume statistic

---
+++ aggregators/7k-aggregator/index.ts
@@ -0,0 +1,27 @@
+import fetchURL from '../../utils/fetchURL';
+import { FetchResult, SimpleAdapter } from '../../adapters/types';
+import { CHAIN } from '../../helpers/chains';
+
+const URL = 'https://statistic.7k.ag';
+
+const fetch = async (timestamp: number): Promise<FetchResult> => {
+	const dailyVolume = await fetchURL(
+		`${URL}/daily-volume-with-ts?timestamp=${timestamp}`,
+	);
+
+	return {
+		dailyVolume,
+		timestamp,
+	};
+};
+
+const adapter: SimpleAdapter = {
+	adapter: {
+		[CHAIN.SUI]: {
+			fetch,
+			start: 1719563120,
+		},
+	},
+};
+
+export default adapter;
