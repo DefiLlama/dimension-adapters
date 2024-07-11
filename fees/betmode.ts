139d9ebb316f525925f6aaf44bd4f67d2758c5a5
0xgnek <0xgnek@gmail.com>
2024-07-11 17:14:46 +0000

    add betmode

---
+++ fees/betmode.ts
@@ -0,0 +1,24 @@
+import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+
+const address = '0x79b4be7eD13Eef58Bd15ABd6ed79569f21D6c3AF'
+const fetch = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances();
+  const feesStart = await options.fromApi.call({target: address, abi: "uint:totalWagered"})
+  const feesEnd = await options.toApi.call({target: address, abi: "uint:totalWagered"})
+  dailyFees.add("0xDfc7C877a950e49D2610114102175A06C2e3167a", feesEnd-feesStart)
+  dailyFees.resizeBy(1/100)
+  return {dailyFees}
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.MODE]: {
+      fetch: fetch,
+      start: 0,
+    }
+  }
+}
+
+export default adapter;
