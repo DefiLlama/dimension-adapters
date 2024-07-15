2649861e392f0b69bd8a0dce34da69f5b22ade8f
stabble-devops <keisuke@stabble.org>
2024-07-15 03:21:55 -0400

    Update daily volume API end point for stabble

--- dexs/stabble/index.ts
+++ dexs/stabble/index.ts
@@ -1,15 +1,8 @@
 import { CHAIN } from '../../helpers/chains';
 import { httpGet } from '../../utils/fetchURL';
 
-const POOLS_API_ENDPOINT = 'https://pools-spn5wgvtfq-uc.a.run.app/';
-
-async function fetch(timestamp: number) {
-    const pools = await httpGet(POOLS_API_ENDPOINT);
-    const vol24h = pools.reduce((sum: number, pool: any) => sum + (pool?.stats?.volume_24h || 0), 0);
-    return {
-        dailyVolume: vol24h,
-        timestamp: timestamp
-    }
+async function fetch() {
+    return httpGet('https://api.stabble.org/stats/volume?type=daily');
 }
 
 export default {
