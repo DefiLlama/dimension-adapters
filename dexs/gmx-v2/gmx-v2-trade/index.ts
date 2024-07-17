ef20ca63d7fc9d709c6597bacae2b5d59a52286f
K-Lascar <36436810+K-Lascar@users.noreply.github.com>
2024-07-17 22:53:38 +1000

    Update Volume Tracking
    Since the launch of V2.1 Contracts, contract events needs to track the fees collected event to receive the volume for GMX.

--- dexs/gmx-v2/gmx-v2-trade/index.ts
+++ dexs/gmx-v2/gmx-v2-trade/index.ts
@@ -8,14 +8,8 @@ interface ILog {
   topics: string[];
 }
 
-const topic0_ins = '0x137a44067c8961cd7e1d876f4754a5a3a75989b4552f1843fc69c3b372def160';
-const topic1_ins = '0xf94196ccb31f81a3e67df18f2a62cbfb50009c80a7d3c728a3f542e3abc5cb63';
-
-const topic0_des = '0x137a44067c8961cd7e1d876f4754a5a3a75989b4552f1843fc69c3b372def160';
-const topic1_des = '0x07d51b51b408d7c62dcc47cc558da5ce6a6e0fd129a427ebce150f52b0e5171a';
-
-const topic0_fees = '0x137a44067c8961cd7e1d876f4754a5a3a75989b4552f1843fc69c3b372def160';
-const topic1_fees = '0xe096982abd597114bdaa4a60612f87fabfcc7206aa12d61c50e7ba1e6c291100';
+const topic0_trades = '0x137a44067c8961cd7e1d876f4754a5a3a75989b4552f1843fc69c3b372def160';
+const topic1_trades = '0xe096982abd597114bdaa4a60612f87fabfcc7206aa12d61c50e7ba1e6c291100';
 
 type TChain = {
   [s: Chain | string]: string;
@@ -27,34 +21,19 @@ const contract: TChain = {
 }
 
 const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
-    const posistion_logs = await options.getLogs({
+    const trade_logs = await options.getLogs({
       target: contract[options.chain],
-      topics: [topic0_ins, topic1_ins]
+      topics: [topic0_trades, topic1_trades]
     });
-
-    const decress_logs = await options.getLogs({
-      target: contract[options.chain],
-      topics: [topic0_des, topic1_des]
-    });
-
+  
     let hash: string[] = [];
-    const raw_des = decress_logs.map((e: ILog) => {
+    const raw_trades = trade_logs.map((e: ILog) => {
       const data = e.data.replace('0x', '');
-      const volume = data.slice(102 * 64, (102 * 64) + 64);
-      const key = Number('0x' + data.slice(118 * 64, (118 * 64) + 64));
-      if (key === 7) return 0;
-      hash.push(e.transactionHash);
-      // 156
+      const volume = data.slice(81 * 64, (81 * 64) + 64);
       return Number('0x' + volume) / 1e30;
     })
 
-    const raw_in = posistion_logs.filter(e => !hash.includes(e.transactionHash)).map((e: ILog) => {
-      const data = e.data.replace('0x', '');
-      const volume = data.slice(100 * 64, (100 * 64) + 64);
-      return Number('0x' + volume) / 1e30;
-    })
-
-    const dailyVolume: number = [...raw_des, ...raw_in]
+    const dailyVolume: number = [...raw_trades]
       .reduce((a: number, b: number) => a + b, 0);
 
     return {
