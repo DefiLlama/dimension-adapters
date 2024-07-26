28d0fc5ba64ea2c7b7261931365f4730d3758eee
Define101 <Lovelovenow@protonmail.com>
2024-07-26 14:39:09 +0100

    fix betmode

--- fees/betmode.ts
+++ fees/betmode.ts
@@ -1,14 +1,14 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
-const address = '0x79b4be7eD13Eef58Bd15ABd6ed79569f21D6c3AF'
+const address = '0xeb5D5af6a0ac3B64243858094d6b3b379B8772Aa'
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
-  const feesStart = await options.fromApi.call({target: address, abi: "uint:totalWagered"})
-  const feesEnd = await options.toApi.call({target: address, abi: "uint:totalWagered"})
-  dailyFees.add("0xDfc7C877a950e49D2610114102175A06C2e3167a", feesEnd-feesStart)
-  dailyFees.resizeBy(0.01)
-  return {dailyFees, dailyRevenue: dailyFees}
+  const feesStart = await options.fromApi.call({ target: address, abi: "uint:GGR" })
+  const feesEnd = await options.toApi.call({ target: address, abi: "uint:GGR" })
+  dailyFees.add("0xd988097fb8612cc24eeC14542bC03424c656005f", feesEnd - feesStart)
+  dailyFees.resizeBy(0.065)
+  return { dailyFees, dailyRevenue: dailyFees }
 }
 
 const adapter: SimpleAdapter = {
