57bc92b56987d174b6b58c805f4b2373546964d5
Grzegorz Łyskawa <grzegorz.lyskawa@gmail.com>
2024-07-26 13:37:52 +0200

    colony dimension adapter update

---
+++ fees/colony/masterChef.ts
@@ -0,0 +1,132 @@
+import { Balances } from "@defillama/sdk";
+import { FetchOptions } from "../../adapters/types";
+import { request, gql } from "graphql-request";
+import BigNumber from "bignumber.js";
+
+const usdcToken = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E'
+
+export interface Airdrops {
+  dailySupplySideRevenue: Balances;
+  totalSupplySideRevenue: Balances;
+}
+
+interface IGraphAirdropsResponse {
+  rewarders: {
+    rewardToken: string
+    name: string
+    decimals: number
+    schedule: {
+      rangeAmount: string
+    }[]
+    startTimestamp: number
+  }[]
+}
+
+interface ICeTokensResponse {
+  projects: {
+    ceToken: {
+      id: string
+    }
+    tokenPrice: string
+  }[]
+}
+
+const rewardersQuery = gql
+  `{
+    rewarders {
+      rewardToken
+      name
+      decimals
+      schedule {
+        rangeAmount
+      }
+      startTimestamp
+    }
+  }`;
+
+const ceTokensQuery = gql
+  `{
+    projects(where: {ceToken_not: null}) {
+      ceToken {
+        id
+      }
+      tokenPrice
+    }
+  }`;
+
+function addTokenBalance(
+  balance: Balances,
+  prices: Map<string, string>,
+  tokenAddress: string,
+  tokenDecimals: number,
+  rewarderSchedule: Array<{ rangeAmount: string }>,
+  isCeToken: boolean
+) {
+  if (isCeToken) {
+    const price = prices.get(tokenAddress.toLowerCase()) ?? 0
+    const totalCeToken = rewarderSchedule.reduce((acc, x) => {
+      return acc.plus(x.rangeAmount)
+    }, new BigNumber(0))
+
+    balance.add(usdcToken, totalCeToken.multipliedBy(price).div(new BigNumber(10).pow(tokenDecimals)).toFixed(0))
+  }
+  else {
+    balance.add(tokenAddress, rewarderSchedule.reduce((acc, x) => {
+      return acc.plus(x.rangeAmount)
+    }, new BigNumber(0)).toFixed(0))
+  }
+
+  return balance
+}
+
+export async function masterChef(
+  options: FetchOptions,
+  masterChefSubgraphEndpoint: string,
+  earlyStageSubgraphEndpoint: string,
+): Promise<Airdrops> {
+  const { createBalances, startTimestamp } = options;
+
+  let dailySupplySideRevenue = createBalances()
+  let totalSupplySideRevenue = createBalances()
+
+  const day = Math.floor(startTimestamp / 86400)
+  const date = day * 86400
+
+  try {
+    const ceTokenRes: ICeTokensResponse = await request(earlyStageSubgraphEndpoint, ceTokensQuery);
+    const priceMap = new Map()
+    for (const ceToken of ceTokenRes.projects) {
+      priceMap.set(ceToken.ceToken.id.toLowerCase(), ceToken.tokenPrice)
+    }
+
+    const rewardersRes: IGraphAirdropsResponse = await request(masterChefSubgraphEndpoint, rewardersQuery);
+    for (const rewarder of rewardersRes.rewarders) {
+      if (rewarder.startTimestamp >= date && rewarder.startTimestamp < date + 86400) {
+        addTokenBalance(
+          dailySupplySideRevenue,
+          priceMap,
+          rewarder.rewardToken,
+          rewarder.decimals,
+          rewarder.schedule,
+          rewarder.name.includes(' ceToken')
+        )
+      }
+      addTokenBalance(
+        totalSupplySideRevenue,
+        priceMap,
+        rewarder.rewardToken,
+        rewarder.decimals,
+        rewarder.schedule,
+        rewarder.name.includes(' ceToken')
+      )
+    }
+
+  } catch (e) {
+    console.error(e);
+  }
+
+  return {
+    dailySupplySideRevenue,
+    totalSupplySideRevenue
+  }
+}
--- fees/colony/staking.ts
+++ fees/colony/staking.ts
@@ -1,13 +1,10 @@
 import { Balances } from "@defillama/sdk";
 import { FetchOptions } from "../../adapters/types";
+import { request, gql } from "graphql-request";
 
-const { request, gql } = require("graphql-request");
-
-export interface StakingRevenue {
-  totalFees: number;
-  dailyFees: number;
-  totalHoldersRevenue: number;
-  dailyHoldersRevenue: number;
+export interface StakingFees {
+  dailyHoldersRevenue: Balances;
+  totalHoldersRevenue: Balances;
 }
 
 interface ITotalStakeFees {
@@ -37,40 +34,40 @@ const queryStakingFeesMetrics = gql
   }
 }`;
 
-export async function stakingRevenue(
+export async function stakingFees(
   options: FetchOptions,
   stakingSubgraphEndpoint: string,
-  ColonyGovernanceToken: string,
-  StakingV3Contract: string,
-): Promise<StakingRevenue> {
+  ColonyGovernanceToken: string
+): Promise<StakingFees> {
   const { createBalances, startTimestamp } = options;
 
-  let totalHoldersRevenue = createBalances();
-  let dailyHoldersRevenue = createBalances();
+  let dailyHoldersRevenue = createBalances()
+  let totalHoldersRevenue = createBalances()
 
   const day = Math.floor(startTimestamp / 86400)
   const date = day * 86400
 
+  // add staking airdrops to be considered as holders revenue
+
   try {
     const res: IGraphStakeResponse = await request(stakingSubgraphEndpoint, queryStakingFeesMetrics, { date });
 
+    if (res.dailyMetrics && res.dailyMetrics.length) {
+      dailyHoldersRevenue.add(ColonyGovernanceToken, res.dailyMetrics[0].stakeFees);
+      totalHoldersRevenue.add(ColonyGovernanceToken, res.dailyMetrics[0].unstakeFees);
+    }
+
     if (res.metrics && res.metrics.length) {
-      totalHoldersRevenue.add(ColonyGovernanceToken, res.metrics[0].totalStakeFees);
+      dailyHoldersRevenue.add(ColonyGovernanceToken, res.metrics[0].totalStakeFees);
       totalHoldersRevenue.add(ColonyGovernanceToken, res.metrics[0].totalUnstakeFees);
     }
 
-    if (res.dailyMetrics && res.dailyMetrics.length) {
-      dailyHoldersRevenue.add(ColonyGovernanceToken, res.dailyMetrics[0].stakeFees);
-      dailyHoldersRevenue.add(ColonyGovernanceToken, res.dailyMetrics[0].unstakeFees);
-    }
   } catch (e) {
     console.error(e);
   }
 
   return {
-    totalFees: await totalHoldersRevenue.getUSDValue(),
-    dailyFees: await dailyHoldersRevenue.getUSDValue(),
-    totalHoldersRevenue: await totalHoldersRevenue.getUSDValue(),
-    dailyHoldersRevenue: await dailyHoldersRevenue.getUSDValue(),
+    dailyHoldersRevenue,
+    totalHoldersRevenue
   }
 }
