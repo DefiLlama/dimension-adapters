8a457d5dd0220dabcf789eb7bf511c8d8b82841c
tuco-gordian <tuco@gordian.agency>
2024-07-29 16:36:52 +0300

    Subgraph on dexswap

--- dexs/dexswap/index.ts
+++ dexs/dexswap/index.ts
@@ -1,16 +1,60 @@
+import * as sdk from "@defillama/sdk";
 import { FetchOptions } from "../../adapters/types"
 import { exportDexVolumeAndFees } from "../../helpers/dexVolumeLogs";
+import request, { gql } from "graphql-request";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import BigNumber from "bignumber.js";
 
 const FACTORY_ADDRESS = '0x3e40739d8478c58f9b973266974c58998d4f9e8b';
+
+const endpoints = {
+	[CHAIN.ARBITRUM]: sdk.graph.modifyEndpoint('AaAYom6ATrw15xZq3LJijLHsBj8xWdcL11Xg6sCxEBqZ'),
+  };
+
 const startDate = 1684702800;
+
+const fetch = async (options: FetchOptions) => {
+	const dataFactory = await request(endpoints['arbitrum'], gql
+		`{
+			dexSwapFactories {
+				totalVolumeETH
+			}
+		}`
+	)
+	const totalVolume = dataFactory.dexSwapFactories[0].totalVolumeETH;
+	let totalFees = new BigNumber(0);
+	let date = startDate;
+	while (true) {
+		const dataFees = await request(endpoints['arbitrum'], gql
+			`query DexSwapFees {
+				dexSwapFees(first: 900, orderBy: timestamp, where: { timestamp_gt: ${date} }) {
+					volume,
+					timestamp
+				}
+			}`
+		)
+		if (!dataFees.dexSwapFees.length) break;
+		dataFees.dexSwapFees.forEach((data) => {
+			totalFees = totalFees.plus(data.volume);
+		})
+		const last = dataFees.dexSwapFees[dataFees.dexSwapFees.length - 1].timestamp;
+		if (last === date) break;
+		date = last;
+	}
+	const dailyData = await exportDexVolumeAndFees({ chain: CHAIN.ARBITRUM, factory: FACTORY_ADDRESS })(options.endTimestamp, {}, options);
+	return {
+		...dailyData,
+		totalVolume,
+		totalFees: totalFees.div(10 ** 18).toString(),
+	}
+} 
 
 const adapter: SimpleAdapter = {
 	version: 2,
 	adapter: {
 		[CHAIN.ARBITRUM]: {
-			fetch: (options: FetchOptions) => exportDexVolumeAndFees({ chain: CHAIN.ARBITRUM, factory: FACTORY_ADDRESS })(options.endTimestamp, {}, options),
+			fetch,
 			start: startDate,
 		},
 	}
