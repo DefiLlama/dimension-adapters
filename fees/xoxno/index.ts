6b17dc018069ebcdc66542270dc6c573ec0d74a9
0xgnek <0xgnek@gmail.com>
2024-07-31 16:40:28 +0000

    fix set 0 fees

--- fees/xoxno/index.ts
+++ fees/xoxno/index.ts
@@ -9,24 +9,24 @@ interface IRaw  {
 }
 
 const fetchFees = async ({ fromTimestamp, toTimestamp }: FetchOptions) => {
-  const startTimeStr = new Date(fromTimestamp * 1000).toISOString().split("T")[0];
-  const toDayTimeStrNext = new Date((toTimestamp + 84000) * 1000).toISOString().split("T")[0];
-  const toDayTimeStr = new Date((toTimestamp) * 1000).toISOString().split("T")[0];
-  const url = `https://proxy-api.xoxno.com/getMarketplaceVolume?after=${startTimeStr}&before=${toDayTimeStrNext}&bin=1d`;
-  const response: IRaw[] = (await httpGet(url, {
-    headers: {
-      origin: 'https://xoxno.com',
-      referer: 'https://xoxno.com/'
-    }
-  }));
+  // const startTimeStr = new Date(fromTimestamp * 1000).toISOString().split("T")[0];
+  // const toDayTimeStrNext = new Date((toTimestamp + 84000) * 1000).toISOString().split("T")[0];
+  // const toDayTimeStr = new Date((toTimestamp) * 1000).toISOString().split("T")[0];
+  // const url = `https://proxy-api.xoxno.com/getMarketplaceVolume?after=${startTimeStr}&before=${toDayTimeStrNext}&bin=1d`;
+  // const response: IRaw[] = (await httpGet(url, {
+  //   headers: {
+  //     origin: 'https://xoxno.com',
+  //     referer: 'https://xoxno.com/'
+  //   }
+  // }));
 
-  const toDayStr = toDayTimeStr.split('-')[1] +'-'+ toDayTimeStr.split('-')[2];
-  const dayData = response.find((item) => item.Day === toDayStr);
-  const dailyFees = (dayData?.XO_FeesPaidUSD || 0) + (dayData?.XO_RoyaltiesPaidUSD || 0);
-  const dailyRevenue = dayData?.XO_FeesPaidUSD || 0;
+  // const toDayStr = toDayTimeStr.split('-')[1] +'-'+ toDayTimeStr.split('-')[2];
+  // const dayData = response.find((item) => item.Day === toDayStr);
+  // const dailyFees = (dayData?.XO_FeesPaidUSD || 0) + (dayData?.XO_RoyaltiesPaidUSD || 0);
+  // const dailyRevenue = dayData?.XO_FeesPaidUSD || 0;
   return {
-    dailyFees: `${dailyFees}`,
-    dailyRevenue: `${dailyRevenue}`,
+    dailyFees: `${0}`,
+    dailyRevenue: `${0}`,
   }
 }
 const adapters: SimpleAdapter = {
