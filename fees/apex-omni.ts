24e50a7aba08fd5db55109121c1a34735e5c4927
roderick <roderick_lu@163.com>
2024-08-01 17:40:59 +0800

    feat: apex-omni

---
+++ fees/apex-omni.ts
@@ -0,0 +1,31 @@
+import { FetchResultFees, SimpleAdapter } from "../adapters/types"
+import { CHAIN } from "../helpers/chains";
+import { getTimestampAtStartOfDayUTC } from "../utils/date"
+import fetchURL from "../utils/fetchURL"
+
+
+interface IFees {
+  feeOfDate: string;
+}
+const fees = async (timestamp: number): Promise<FetchResultFees> => {
+  const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp) * 1000;
+  console.log('todaysTimestamp', todaysTimestamp)
+  const url = `https://omni.apex.exchange/api/v3/data/fee-by-date?time=${todaysTimestamp}`;
+  const feesData: IFees = (await fetchURL(url)).data;
+  const dailyFees = feesData?.feeOfDate || '0';
+  return {
+    dailyFees: dailyFees,
+    dailyUserFees: dailyFees,
+    timestamp
+  }
+}
+const adapter: SimpleAdapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.ETHEREUM]: {
+      fetch: fees,
+      start: 1693440000,
+    }
+  }
+}
+export default adapter;
