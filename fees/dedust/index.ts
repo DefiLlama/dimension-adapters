7e8fb8892272377777b3e737f0f4c2885ee3ddba
0xgnek <0xgnek@gmail.com>
2024-08-04 14:36:38 +0000

    allow run current

--- fees/dedust/index.ts
+++ fees/dedust/index.ts
@@ -1,3 +1,4 @@
+import { FetchResultV2 } from '../../adapters/types';
 import { CHAIN } from '../../helpers/chains'
 import { postURL } from "../../utils/fetchURL"
 
@@ -32,12 +33,56 @@ query GetAssets {
 
 // LPs get 80% of fees
 const FEES_PERCENT_TO_LP = 0.8;
+const fetchFees = async (): Promise<FetchResultV2> => {
+    const assetsList = (await postURL(GRAPHQL_ENDPOINT, {
+        query: ASSETS_QUERY,
+        operationName: 'GetAssets'
+    })).data.assets;
+
+    const assetInfo = {};
+    for (const asset of assetsList) {
+        const address = asset.type == 'native' ? 'native' : 'jetton:' + asset.address;
+        assetInfo[address] = {
+            decimals: asset.decimals,
+            price: Number(asset.price),
+            symbol: asset.symbol
+        }
+    }
+
+    const poolsList = (await postURL(GRAPHQL_ENDPOINT, {
+        query: POOLS_QUERY,
+        operationName: 'GetPools'
+    })).data.pools;
+
+    let dailyFees = 0;
+    for (const pool of poolsList) {
+        const address = pool.address;
+        const leftAddr = pool.assets[0];
+        const rightAddr = pool.assets[1];
+        if (!(leftAddr in assetInfo && rightAddr in assetInfo)) {
+            console.warn("No assets info for pool", pool);
+            continue;
+        }
+        const left = assetInfo[leftAddr];
+        const right = assetInfo[rightAddr];
+
+        dailyFees += (left.price * Number(pool.fees[0]) / Math.pow(10, left.decimals)
+            + right.price * Number(pool.fees[1]) / Math.pow(10, right.decimals)) / 2;
+    }
+
+
+    return {
+        dailyUserFees: dailyFees,
+        dailyFees: dailyFees,
+        dailySupplySideRevenue: dailyFees * FEES_PERCENT_TO_LP,
+        dailyRevenue: dailyFees * (1 - FEES_PERCENT_TO_LP)
+    }
+}
 
 export default {
     version: 2,
     adapter: {
         [CHAIN.TON]: {
-            runAtCurrTime: true,
             start: 1700000000,
             meta: {
                 methodology: {
@@ -46,54 +91,7 @@ export default {
                     SupplySideRevenue: "80% of user fees are distributed among LPs.",
                 },
             },
-            fetch: async () => {
-                console.log("Requesting assets list")
-                const assetsList = (await postURL(GRAPHQL_ENDPOINT, {
-                    query: ASSETS_QUERY,
-                    operationName: 'GetAssets'
-                })).data.assets;
-
-                const assetInfo = {};
-                for (const asset of assetsList) {
-                    const address = asset.type == 'native' ? 'native' : 'jetton:' + asset.address;
-                    assetInfo[address] = {
-                        decimals: asset.decimals,
-                        price: Number(asset.price),
-                        symbol: asset.symbol
-                    }
-                }
-                console.log(`Inited ${Object.keys(assetInfo).length} assets`);
-
-                console.log("Requesting pools list")
-                const poolsList = (await postURL(GRAPHQL_ENDPOINT, {
-                    query: POOLS_QUERY,
-                    operationName: 'GetPools'
-                })).data.pools;
-
-                let dailyFees = 0;
-                for (const pool of poolsList) {
-                    const address = pool.address;
-                    const leftAddr = pool.assets[0];
-                    const rightAddr = pool.assets[1];
-                    if (!(leftAddr in assetInfo && rightAddr in assetInfo)) {
-                        console.warn("No assets info for pool", pool);
-                        continue;
-                    }
-                    const left = assetInfo[leftAddr];
-                    const right = assetInfo[rightAddr];
-
-                    dailyFees += (left.price * Number(pool.fees[0]) / Math.pow(10, left.decimals)
-                        + right.price * Number(pool.fees[1]) / Math.pow(10, right.decimals)) / 2;
-                }
-
-
-                return {
-                    dailyUserFees: dailyFees,
-                    dailyFees: dailyFees,
-                    dailySupplySideRevenue: dailyFees * FEES_PERCENT_TO_LP,
-                    dailyRevenue: dailyFees * (1 - FEES_PERCENT_TO_LP)
-                }
-            },
+            fetch: fetchFees,
         },
     },
 }
