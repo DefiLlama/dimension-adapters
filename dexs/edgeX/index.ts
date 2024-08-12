5516c53e7ad24192709811f338e88b3cd177c447
0xgnek <0xgnek@gmail.com>
2024-08-12 08:16:12 +0000

    fix contract id

--- dexs/edgeX/index.ts
+++ dexs/edgeX/index.ts
@@ -36,12 +36,15 @@ function parseContractIds(response: any): string[] {
 const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000)) * 1000
   const toTimestamp = dayTimestamp + 60 * 60 * 24 * 1000;
-  const contractIds: string[] = parseContractIds(await fetchURL(metaDataEndpoint));
-  const klines = await Promise.all(contractIds.map(async (contractId) => {
-    const response: ApiResponse = await fetchURL(klineDailyEndpoint(contractId, dayTimestamp, toTimestamp));
-    return response.data.dataList;
-  }
-  ));
+  const contractIds: string[] = [
+    '10000001', '10000002',
+    '10000003', '10000004',
+    '10000005', '10000006',
+    '10000007', '10000008',
+    '10000009', '10000010'
+  ]
+  const klines = (await Promise.all(contractIds.map(async (contractId) => fetchURL(klineDailyEndpoint(contractId, dayTimestamp, toTimestamp)))))
+    .map((response: ApiResponse) => response.data.dataList);
   const volumes = klines
     .flat()
     .map(kline => parseFloat(kline.value))
