959b9bf48b0505164a48953771633ee074b1b1b2
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-08-13 10:46:05 -0300

    Add demented games to fees dashboard

---
+++ fees/demented-games/index.ts
@@ -0,0 +1,44 @@
+import { ETHER_ADDRESS } from '@defillama/sdk/build/general';
+import { Adapter, FetchOptions } from '../../adapters/types';
+import { CHAIN } from '../../helpers/chains';
+import { commonAbi } from './abis';
+
+const ROULETTE_ADDRESS = '0x94ba26ee118ef6c407c75dbb23385b1ad71a4547';
+const PUMP_OR_REKT_ADDRESS = '0xcbc003cb76c5d218cba2dfb3a2b2f101950ed7e7';
+
+async function fetch({ createBalances, api }: FetchOptions) {
+  const totalFees = createBalances();
+
+  const pumpOrRektFees = await api.call({
+    abi: commonAbi[0],
+    target: ROULETTE_ADDRESS,
+  });
+
+  const roulletteFees = await api.call({
+    abi: commonAbi[0],
+    target: PUMP_OR_REKT_ADDRESS,
+  });
+
+  totalFees.add(ETHER_ADDRESS, pumpOrRektFees);
+  totalFees.add(ETHER_ADDRESS, roulletteFees);
+
+  return { totalFees };
+}
+
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.FUSE]: {
+      fetch,
+      runAtCurrTime: true,
+      start: 1720396800,
+      meta: {
+        methodology: {
+          Fees: 'Sum of all fees from each game in the demended games platform.',
+        },
+      },
+    },
+  },
+};
+
+export default adapter;
