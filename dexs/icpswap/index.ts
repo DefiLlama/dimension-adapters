4f9cd543e7110150a5eb1d453ebda907ce9128cc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-19 17:31:06 +0200

    check metric against whitelist

--- dexs/icpswap/index.ts
+++ dexs/icpswap/index.ts
@@ -1,9 +1,12 @@
 import { Adapter, FetchResultVolume } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraph/utils";
 import fetchURL from "../../utils/fetchURL";
 
 const fetch  = async (timestamp: number): Promise<FetchResultVolume> => {
   const { volumeUSD } = await fetchURL('https://uvevg-iyaaa-aaaak-ac27q-cai.raw.ic0.app/overview')
+  
+  timestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
   return {
     dailyVolume: volumeUSD,
     timestamp
@@ -15,6 +18,7 @@ const adapter: Adapter = {
   adapter: {
     [CHAIN.ICP]: {
       fetch: fetch,
+      runAtCurrTime: true,
       start: 1689465600,
     },
   }
