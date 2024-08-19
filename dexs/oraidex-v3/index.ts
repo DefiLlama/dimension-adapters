0dbc0aaff2977f3c566c21f228114b6f6156eb41
0xgnek <0xgnek@gmail.com>
2024-08-19 13:18:35 +0000

    fix date

--- dexs/oraidex-v3/index.ts
+++ dexs/oraidex-v3/index.ts
@@ -1,11 +1,11 @@
 import { gql, GraphQLClient } from "graphql-request";
-import { SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 const historicalVolumeEndpoint = "https://staging-ammv3-indexer.oraidex.io/";
-const fetch = async (timestamp: number) => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+const fetch = async (timestamp: number, _t: any, options: FetchOptions) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(options.startOfDay * 1000));
   const dayIndex = Math.floor(dayTimestamp / 86400);
   const query = gql`
       query PoolDayData {
