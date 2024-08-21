8276e2f242f26e667638017ea6a2c11395c4b920
Denys Proskura <57964380+MrJeleika@users.noreply.github.com>
2024-08-21 17:06:56 +0300

    Update api url

--- aggregators/injex/index.ts
+++ aggregators/injex/index.ts
@@ -1,7 +1,7 @@
 import { httpGet } from '../../utils/fetchURL';
 
 const fetchLogs = async (timestamp: any) => {
-  const res = await httpGet(`https://injex-api-cb4a9db30875.herokuapp.com/api/volume-stats/usd?timestamp=${timestamp.startOfDay}`);
+  const res = await httpGet(`https://inj-api-78847b1b16a1.herokuapp.com/api/volume-stats/usd?timestamp=${timestamp.startOfDay}`);
   return {
     dailyVolume: res.dailyVolume,
     totalVolume: res.totalVolume,
