5acdd0d3afdf990464828d1896ff99a5bb746b56
0xgnek <0xgnek@gmail.com>
2024-08-24 12:44:53 +0000

    fix split block ragg for get fees

--- fees/sunpump.ts
+++ fees/sunpump.ts
@@ -7,10 +7,18 @@ const adapter: Adapter = {
     [CHAIN.TRON]: {
       fetch: (async (options: FetchOptions) => {
         const dailyFees = options.createBalances();
-        const logs = await options.getLogs({
-          targets: ['TG9nDZMUtC4LBmrWSdNXNi8xrKzXTMMSKT'],
-          eventAbi: "event TRXReceived(address indexed from,uint256 amount)",
-        })
+        const start = await options.getFromBlock();
+        const end = await options.getToBlock();
+        const logs = []
+        for(let i = start; i <= end; i+=2000) {
+          const _logs = await options.getLogs({
+            target: 'TG9nDZMUtC4LBmrWSdNXNi8xrKzXTMMSKT',
+            eventAbi: "event TRXReceived(address indexed from,uint256 amount)",
+            fromBlock: start,
+            toBlock: start + 2000,
+          })
+          logs.push(..._logs)
+        }
         logs.map((tx: any) => {
           dailyFees.addGasToken(tx.amount)
         })
