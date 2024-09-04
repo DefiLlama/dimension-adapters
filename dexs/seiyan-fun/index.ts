8eec0950b3d9e02d6db091a8189384310ef55690
0xgnek <0xgnek@gmail.com>
2024-09-04 16:43:47 +0000

    fix adapter

--- dexs/seiyan-fun/index.ts
+++ dexs/seiyan-fun/index.ts
@@ -1,9 +1,6 @@
 import fetchURL from "../../utils/fetchURL";
 import { CHAIN } from "../../helpers/chains";
-import {
-  getTimestampAtStartOfDayUTC,
-  getTimestampAtStartOfNextDayUTC,
-} from "../../utils/date";
+import { FetchOptions } from "../../adapters/types";
 
 const SEIYAN_FUN_BASE_URL = "https://seiyan.fun/api/public/v1";
 const SEI_PACIFIC_CAIP_CHAIN_ID = "cosmos:sei-pacific-1";
@@ -12,13 +9,12 @@ const SEIYAN_FUN_INITIAL_TIMESTAMP = 1722470400; // 2024-08-01 00:00:00 UTC
 const buildTradingVolumeUrl = (startAt: number, endAt: number) =>
   `${SEIYAN_FUN_BASE_URL}/trading-volume?caipChainID=${SEI_PACIFIC_CAIP_CHAIN_ID}&startAt=${startAt}&endAt=${endAt}`;
 
-const fetch = async (timestamp: number) => {
-  const dayStart = getTimestampAtStartOfDayUTC(timestamp);
-  const nextDayStart = getTimestampAtStartOfNextDayUTC(timestamp);
+const fetch = async (timestamp: number, _t: any, options: FetchOptions) => {
+  const dayStart = options.startOfDay;
+  const nextDayStart = dayStart + 86400;
   const url = buildTradingVolumeUrl(dayStart, nextDayStart);
-
-  const { volume } = await fetchURL(url);
-
+  console.log("fetching seiyan fun", url);
+  const { volume }= await fetchURL(url);
   return {
     dailyVolume: volume,
     timestamp,
