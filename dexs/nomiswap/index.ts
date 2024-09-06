f606925203d586879ae2f470fa9758898ed50af4
waynebruce0x <waynebruce0x@gmail.com>
2024-09-06 14:44:56 +0100

    quickswap dex

--- dexs/nomiswap/index.ts
+++ dexs/nomiswap/index.ts
@@ -1,9 +1,7 @@
 import * as sdk from "@defillama/sdk";
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 import { CHAIN } from "../../helpers/chains";
-import { getGraphDimensions } from "../../helpers/getUniSubgraph";
+import { getGraphDimensions2 } from "../../helpers/getUniSubgraph";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
-import { time } from "console";
 
 const endpoints = {
   [CHAIN.BSC]: sdk.graph.modifyEndpoint('9ggB4DiKGyXfiS4vh1xqQJMcTQEvxxt715HVm8S3r27G'),
@@ -15,15 +13,11 @@ const blacklistTokens = {
     "0x7f9ad7a5854658d984924e868187b2135514fb88"
   ]
 }
-const graphsClassic = getGraphDimensions({
+const graphsClassic = getGraphDimensions2({
   graphUrls: endpoints,
   totalVolume: {
     factory: "nomiswapFactories",
     field: "totalVolumeUSD",
-  },
-  dailyVolume: {
-    factory: "nomiswapDayData",
-    field: VOLUME_FIELD,
   },
   blacklistTokens
 });
