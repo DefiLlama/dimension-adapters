066dfea626f5614d03e6cfea0f9bda9ba8e1d4df
0xgnek <0xgnek@gmail.com>
2024-09-16 16:12:29 +0000

    fix dragonswap-sei

--- dexs/dragonswap-sei/index.ts
+++ dexs/dragonswap-sei/index.ts
@@ -1,10 +1,35 @@
+import request, { gql } from "graphql-request";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { univ2Adapter2 } from "../../helpers/getUniSubgraphVolume";
 
-const adapters = univ2Adapter2({
-    [CHAIN.SEI]: "https://api.goldsky.com/api/public/project_clu1fg6ajhsho01x7ajld3f5a/subgraphs/dragonswap-prod/1.0.0/gn"
-}, {});
+const fetch = async (timestamp: number, _:any, options: FetchOptions): Promise<any> => {
+    const dayID = Math.floor(options.startOfDay / 86400);
+    const query =gql`
+    {
+        uniswapDayData(id:${dayID}) {
+            id
+            dailyVolumeUSD
+        }
+        uniswapFactories {
+            totalVolumeUSD
+        }
+    }`;
+    const url = "https://api.goldsky.com/api/public/project_clu1fg6ajhsho01x7ajld3f5a/subgraphs/dragonswap-prod/1.0.0/gn";
+    const req = await request(url, query);
+    return {
+        dailyVolume: req.uniswapDayData.dailyVolumeUSD,
+        totalVolume: req.uniswapFactories[0].totalVolumeUSD,
+        timestamp: timestamp,
+    }
+}
 
-adapters.adapter.sei.start = 1716854400;
+const adapter: SimpleAdapter = {
+    adapter: {
+        [CHAIN.SEI]: {
+            fetch,
+            start: 1716854400,
+        },
+    }
+}
 
-export default adapters;
+export default adapter;
