1afd285e37b16691564035f95bbd963a9de47210
waynebruce0x <waynebruce0x@gmail.com>
2024-09-16 14:49:23 +0100

    syncswap

--- dexs/syncswap/index.ts
+++ dexs/syncswap/index.ts
@@ -1,18 +1,16 @@
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { univ2Adapter2 } from "../../helpers/getUniSubgraphVolume";
 import { CHAIN } from "../../helpers/chains";
 
 const endpoints = {
-  [CHAIN.ERA]: "https://api.studio.thegraph.com/query/30365/syncswap-graph/1.4.0",
+  [CHAIN.ERA]:
+    "https://api.studio.thegraph.com/query/30365/syncswap-graph/1.4.0",
 };
 
-const adapter = univ2Adapter(endpoints, {
+const adapter = univ2Adapter2(endpoints, {
   factoriesName: "syncSwapFactories",
-  dayData: "dayData",
-  dailyVolume: "dailyVolumeUSD",
   totalVolume: "totalVolumeUSD",
-  dailyVolumeTimestampField: "date",
 });
 
-adapter.adapter.era.start = 1679529600
+adapter.adapter.era.start = 1679529600;
 
-export default adapter
+export default adapter;
