c38516359f8db7b1d8f407135d61f8e1fc130e77
literallymarvellous <ahiaraikechukwu@gmail.com>
2024-09-18 06:07:19 +0100

    fix timestamp

--- dexs/pear-protocol/index.ts
+++ dexs/pear-protocol/index.ts
@@ -3,12 +3,8 @@ import { Adapter, BreakdownAdapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { httpGet } from "../../utils/fetchURL";
 
-const fetch = async (
-  timestamp: number,
-  chainBlocks: any,
-  options: FetchOptions
-) => {
-  const url = "https://www.api.pearprotocol.io/v1/metric";
+const fetch = async (timestamp: number) => {
+  const url = `https://www.api.pearprotocol.io/v1/metric?timestamp=${timestamp}`;
   const response = await httpGet(url);
 
   const totalVolume = response.payload.totalVolume;
