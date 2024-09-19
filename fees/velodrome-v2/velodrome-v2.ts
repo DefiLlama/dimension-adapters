2db43d5a5fe0145e5887be17955099170a2fedea
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-19 17:30:11 +0200

    refactor velodrome/aerodrome (#1930)

--- fees/velodrome-v2/velodrome-v2.ts
+++
@@ -1,9 +0,0 @@
-import { FetchOptions } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import { getDexFees } from "../../helpers/dexVolumeLogs";
-
-const FACTORY_ADDRESS = '0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a';
-
-export const fetchV2 = async (fromBlock: number, toBlock: number, fetchOptions: FetchOptions): Promise<any> => {
-  return getDexFees({ chain: CHAIN.OPTIMISM, fromBlock, toBlock, factory: FACTORY_ADDRESS, timestamp: fetchOptions.toTimestamp, lengthAbi: 'allPoolsLength', itemAbi: 'allPools', fetchOptions })
-}
