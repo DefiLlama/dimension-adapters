fbf7cff044207fab8c24b20207f38584227da42c
Dav0808 <d.khantadze@gmail.com>
2024-10-03 18:28:32 +0400

    cleaning code

--- dexs/hinkal/index.ts
+++ dexs/hinkal/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Adapter, FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { Adapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const fetchEthereum = async (options: FetchOptions) => {
