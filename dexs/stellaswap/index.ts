1bbba947f9eeb116a75023ba8411ed15da8ca389
waynebruce0x <waynebruce0x@gmail.com>
2024-10-03 10:13:29 +0100

    v2

--- dexs/stellaswap/index.ts
+++ dexs/stellaswap/index.ts
@@ -1,8 +1,8 @@
 import * as sdk from "@defillama/sdk";
 import { CHAIN } from "../../helpers/chains";
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { univ2Adapter2 } from "../../helpers/getUniSubgraphVolume";
 
-const adapters = univ2Adapter({
+const adapters = univ2Adapter2({
   [CHAIN.MOONBEAN]: sdk.graph.modifyEndpoint('HgSAfZvHEDbAVuZciPUYEqFzhAUnjJWmyix5C1R2tmTp')
 }, {});
 adapters.adapter.moonbeam.start = 1641960253;
