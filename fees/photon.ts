7ef828c06c1cab158bbd83fac3fd208e88455b5b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-10 17:58:33 +0200

    SOL transfer (#1984)
    * use allium
    
    * minor fix
    
    * bugfix
    
    * update adapters
    
    * bugfix

--- fees/photon.ts
+++ fees/photon.ts
@@ -1,18 +1,10 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { queryDune } from "../helpers/dune";
+import { getSolanaReceived } from "../helpers/token";
 
 const fetch: any = async (options: FetchOptions) => {
-  const dailyFees = options.createBalances();
-  const value = (await queryDune("3521814", {
-    start: options.startTimestamp,
-    end: options.endTimestamp,
-    receiver: 'AVUCZyuT35YSuj4RH7fwiyPu82Djn2Hfg7y2ND2XcnZH'
-  }));
-  dailyFees.add('So11111111111111111111111111111111111111112', value[0].fee_token_amount);
-
-  return { dailyFees, dailyRevenue: dailyFees }
-
+  const dailyFees = await getSolanaReceived({ options, target: 'AVUCZyuT35YSuj4RH7fwiyPu82Djn2Hfg7y2ND2XcnZH' })
+  return { dailyFees, dailyRevenue: dailyFees, }
 }
 
 const adapter: SimpleAdapter = {
--- helpers/token.ts
+++ helpers/token.ts
@@ -7,6 +7,7 @@ import { ethers } from "ethers";
 import { getUniqueAddresses } from '@defillama/sdk/build/generalUtil';
 import { getEnv } from './env';
 import { queryDuneSql } from './dune';
+import { queryAllium } from './allium';
 
 export const nullAddress = ADDRESSES.null
 
@@ -307,3 +308,18 @@ export const evmReceivedGasAndTokens = (receiverWallet: string, tokens: string[]
       dailyRevenue: dailyFees,
     }
   }
+
+export async function getSolanaReceived({ options, balances, target }: { options: FetchOptions, balances?: sdk.Balances, target: string }) {
+  if (!balances) balances = options.createBalances()
+  const query = `
+    SELECT SUM(usd_amount) as usd_value,  SUM(amount) as amount
+    FROM solana.assets.transfers
+    WHERE to_address = '${target}' 
+    AND block_timestamp BETWEEN TO_TIMESTAMP_NTZ(${options.startTimestamp}) AND TO_TIMESTAMP_NTZ(${options.endTimestamp})
+    `
+    // AND transfer_type = 'sol_transfer'`  // enable this if you want to track only SOL transfers
+    
+  const res = await queryAllium(query)
+  balances.addUSDValue(res[0].usd_value)
+  return balances
+}
