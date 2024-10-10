f66a03a27dc6fae8625611e5c65eeeb765dcf398
g1ntoki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-10 16:36:09 +0200

    revert perpie change

--- aggregator-derivatives/perpie/index.ts
+++ aggregator-derivatives/perpie/index.ts
@@ -31,7 +31,7 @@ const fetchVolumeAndFees: (chain: string) => FetchV2 =
 
     const dayStartOfDayTimestamp = getUniqStartOfTodayTimestamp(date);
 
-    throw new Error('Dune query is broken, fix it by turning adapter on chain')
+    // throw new Error('Dune query is broken, fix it by turning adapter on chain')
 
     let data = (
       await queryDune("3855069", {
