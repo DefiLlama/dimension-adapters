ee9432a88a7e51ca6a15d1d1c1419d05723e50da
Jenelyn Castil Contillo <jenelyncontillo02141995@gmail.com>
2024-10-10 21:35:15 +0800

    Updated logic for calculating fees of wise-lending-v2

--- fees/wise-lending-v2/index.ts
+++ fees/wise-lending-v2/index.ts
@@ -1,35 +1,40 @@
-import { SimpleAdapter } from "../../adapters/types";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import { httpGet } from "../../utils/fetchURL";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { ARBITRUM } from "../../helpers/chains";
+const feeMaangerContract = "0x90a022796798f9dbA1Da0f8645234B284d4E8EC6";
 
-const feesEndpoint = "https://data.wisetoken.net/WISE/globals/fees.json";
+const arbitrumPools = [
+    //aave
+    "0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8",
+    "0x6ab707Aca953eDAeFBc4fD23bA73294241490620",
+    "0x724dc807b04555b71ed48a6896b6F41593b8C637",
+    "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE",
+    //native
+    "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+    "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+    "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+    "0x5979D7b546E38E414F7E9822514be443A4800529",
+];
 
-interface IFeeResponse {
-    totals: {
-        totalFeeTokensUSD: number;
-    };
-    tokens: {
-        [key: string]: { feeTokensUSD: number };
-    };
-}
+const fetch: any = async ({ api }: FetchOptions) => {
+    const feeAmounts = await api.multiCall({
+        abi: "function feeTokens(address) view returns (uint256)",
+        target: feeMaangerContract,
+        calls: arbitrumPools,
+    });
 
-const fetch = () => {
-    return async (timestamp: number) => {
-        const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-        const fees: IFeeResponse = await httpGet(feesEndpoint);
+    api.add(arbitrumPools, feeAmounts);
 
-        return {
-            dailyFees: `${fees?.totals.totalFeeTokensUSD || undefined}`,
-            timestamp: dayTimestamp,
-        };
+    return {
+        dailyFees: await api.getUSDValue(),
     };
 };
 
 const adapter: SimpleAdapter = {
     version: 2,
     adapter: {
-        ["arbitrum"]: {
-            fetch: fetch(),
+        [ARBITRUM]: {
+            fetch,
             runAtCurrTime: true,
             start: 1727740800,
         },
