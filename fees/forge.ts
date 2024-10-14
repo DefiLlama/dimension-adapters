dcdd38e94f332f7997534127eb7db6eed5421433
waynebruce0x <waynebruce0x@gmail.com>
2024-10-14 14:15:46 +0100

    forge

--- fees/forge.ts
+++ fees/forge.ts
@@ -1,55 +1,12 @@
-import { Chain } from "@defillama/sdk/build/general";
-import BigNumber from "bignumber.js";
-import request, { gql } from "graphql-request";
-import { Adapter, FetchResultFees } from "../adapters/types";
+import { Adapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
-import { getTimestampAtStartOfDayUTC } from "../utils/date";
-
-interface IPoolData {
-  date: number;
-  feesUSD: string;
-}
-
-type IURL = {
-  [l: string | Chain]: string;
-}
-
-const endpoints: IURL = {
-  [CHAIN.EVMOS]: "https://subgraph.satsuma-prod.com/09c9cf3574cc/orbital-apes/v3-subgraph/api",
-}
-
-const fetch = (chain: Chain) => {
-  return async (timestamp: number): Promise<FetchResultFees> => {
-    const todayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-    const dateId = Math.floor(getTimestampAtStartOfDayUTC(todayTimestamp) / 86400)
-    const graphQuery = gql
-      `
-      {
-        uniswapDayData(id: ${dateId}) {
-          id
-          feesUSD
-        }
-      }
-    `;
-
-    const graphRes: IPoolData = (await request(endpoints[chain], graphQuery)).uniswapDayData;
-    const dailyFeeUSD = graphRes;
-    const dailyFee = dailyFeeUSD?.feesUSD ? new BigNumber(dailyFeeUSD.feesUSD) : undefined
-    if (dailyFee === undefined) return { timestamp }
-    return {
-      timestamp,
-      dailyFees: dailyFee.toString(),
-      dailyUserFees: dailyFee.toString(),
-    };
-  };
-}
+import { getUniV3LogAdapter } from "../helpers/uniswap";
 
 const adapter: Adapter = {
   version: 1,
   adapter: {
     [CHAIN.EVMOS]: {
-      fetch: fetch(CHAIN.EVMOS),
+      fetch: getUniV3LogAdapter({ factory: '0xf544365e7065966f190155F629cE0182fC68Eaa2' }),
       start: 1680480000,
     },
   },
