ba12e31886d2baedf0610d9c879b412dabaef1e9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-14 15:52:17 +0200

    fix pool address

--- fees/avalon-finance-cedefi/index.ts
+++ fees/avalon-finance-cedefi/index.ts
@@ -8,21 +8,20 @@ const abi = {
 		'function getPoolManagerConfig() view returns (tuple(uint256 DEFAULT_LTV, uint256 DEFAULT_LIQUIDATION_THRESHOLD, uint256 DEFAULT_POOL_INTEREST_RATE, uint256 DEFAULT_PROTOCOL_INTEREST_RATE, address USDT, address FBTC0, address FBTC1, address FBTCOracle, address AvalonUSDTVault, address AntaphaUSDTVault))',
 }
 
-const poolAddress = '0xF95308B15E45BFF3d0811B5732d17Eb2A555afB2'
+const poolAddress = '0x02feDCff97942fe28e8936Cdc3D7A480fdD248f0'
 
 export default {
 	adapter: {
 		[CHAIN.ETHEREUM]: {
 			fetch: (async ({ api, createBalances }) => {
-				const fees = createBalances()
-				const revenue = createBalances()
+				const dailyFees = createBalances()
+				const dailyRevenue = createBalances()
 
 				// Get the pool manager configuration
 				const poolManagerConfig = await api.call({
 					target: poolAddress,
 					abi: abi.getPoolManagerConfig,
 				})
-
 				const poolManagerReserveInformation = await api.call({
 					target: poolAddress,
 					abi: abi.getPoolManagerReserveInformation,
@@ -30,14 +29,15 @@ export default {
 
 				// Protocol Fee = PoolManagerReserveInformation.debt * PoolManagerConfig.DEFAULT_PROTOCOL_INTEREST_RATE
 				// DEFAULT_PROTOCOL_INTEREST_RATE decimals = 4
-				const amount = (poolManagerReserveInformation.debt * poolManagerConfig.DEFAULT_PROTOCOL_INTEREST_RATE) / 10000
+				const amount = poolManagerReserveInformation.debt * poolManagerConfig.DEFAULT_PROTOCOL_INTEREST_RATE / 1e4
 
-				fees.addToken('USDT', amount)
-				revenue.addToken('USDT', amount)
 
-				return { fees, revenue }
+				dailyFees.addUSDValue(amount)
+				dailyRevenue.addUSDValue(amount)
+
+				return { dailyFees, dailyRevenue }
 			}) as FetchV2,
-			start: 20398625,
+			start: 1722088222,
 		},
 	},
 	version: 2,
