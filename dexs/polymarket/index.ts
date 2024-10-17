c7c855fb5ce2aa69cda35dceba13fc7d0f40bb0c
0xgnek <0xgnek@gmail.com>
2024-10-17 18:24:42 +0000

    fix polymarket

--- dexs/polymarket/index.ts
+++ dexs/polymarket/index.ts
@@ -1,26 +1,29 @@
 
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { queryDune } from "../../helpers/dune";
 
-const fetchFees = async (options: FetchOptions) => {
-  const data = require('./data.json');
-  const dateStr = new Date(options.startOfDay * 1000).toISOString().split('T')[0];
+const fetchFees = async (timestamp: number, _t: any, options: FetchOptions) => {
+  const data: any[] = await queryDune("4172945")
+  const dateStr = new Date(timestamp * 1000).toISOString().split('T')[0];
   const daily = data.find(e => e.day.split(' ')[0] === dateStr);
   const dailyVolume = options.createBalances();
   dailyVolume.addUSDValue(daily.total_volume_usd);
   return {
+    timestamp: timestamp,
     dailyVolume: dailyVolume
   }
 }
 
 const adapters: SimpleAdapter = {
-  version: 2,
+  version: 1,
   adapter: {
     [CHAIN.POLYGON]: {
       fetch: fetchFees,
       start: 1601424000,
     }
-  }
+  },
+  isExpensiveAdapter: true,
 }
 
 export default adapters
