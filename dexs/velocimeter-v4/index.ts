2d61f52a06f963dbb276e12b1420653412ad4355
oxSaturn <oxSaturn@proton.me>
2024-10-18 17:11:35 +0800

    add dex for velocimeter v4 on iotaevm

---
+++ dexs/velocimeter-v4/index.ts
@@ -0,0 +1,6 @@
+import { CHAIN } from "../../helpers/chains";
+import { uniV2Exports } from "../../helpers/uniswap";
+
+export default uniV2Exports({
+  [CHAIN.IOTAEVM]: { factory: '0x10A288eF87586BE54ea690998cAC82F7Cc90BC50', },
+})
