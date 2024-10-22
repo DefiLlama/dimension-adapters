86720cf9e0cd44db8fdcb996691a30929d9eed3c
starketh25 <starketh25@gmail.com>
2024-10-22 23:05:21 +0530

    fix: add volume dimension for v2 and v3 pools

--- dexs/fenix-finance/index.ts
+++ dexs/fenix-finance/index.ts
@@ -1,18 +1,67 @@
+import { BreakdownAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import {
+  DEFAULT_DAILY_VOLUME_FACTORY,
+  DEFAULT_DAILY_VOLUME_FIELD,
+  DEFAULT_TOTAL_VOLUME_FACTORY,
+  DEFAULT_TOTAL_VOLUME_FIELD,
+  getChainVolume,
+} from "../../helpers/getUniSubgraphVolume";
 
-const adapters = univ2Adapter(
-  {
-    [CHAIN.BLAST]:
-      "https://api.studio.thegraph.com/query/67572/mainnet-algebra-fenix/version/latest",
+const endpoints = {
+  [CHAIN.BLAST]:
+  "https://api.goldsky.com/api/public/project_clxadvm41bujy01ui2qalezdn/subgraphs/fenix-v2-subgraph/0.0.1/gn",
+};
+const endpointsAlgebraV3 = {
+  [CHAIN.BLAST]:
+    "https://api.goldsky.com/api/public/project_clxadvm41bujy01ui2qalezdn/subgraphs/fenix-v3-dex/ce3738b/gn",
+};
+
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: DEFAULT_TOTAL_VOLUME_FACTORY,
+    field: DEFAULT_TOTAL_VOLUME_FIELD,
   },
-  {
-    factoriesName: "factories",
-    dayData: "algebraDayData",
-    dailyVolume: "volumeUSD",
-    totalVolume: "totalVolumeUSD",
-  }
-);
+  dailyVolume: {
+    factory: DEFAULT_DAILY_VOLUME_FACTORY,
+    field: DEFAULT_DAILY_VOLUME_FIELD,
+    dateField: "date",
+  },
+  hasDailyVolume: true,
+});
+const graphsAlgebraV3 = getChainVolume({
+  graphUrls: endpointsAlgebraV3,
+  totalVolume: {
+    factory: "factories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "algebraDayData",
+    field: "volumeUSD",
+    dateField: "date",
+  },
+});
 
-adapters.adapter.blast.start = 1714590000;
-export default adapters;
+const adapter: BreakdownAdapter = {
+  version: 1,
+  breakdown: {
+    v2: {
+      [CHAIN.BLAST]: {
+        fetch: graphs(CHAIN.BLAST),
+        start: 1596021,
+      },
+    },
+    v3: {
+      [CHAIN.BLAST]: {
+        fetch: graphsAlgebraV3(CHAIN.BLAST),
+        start: 1596025,
+      },
+     
+    },
+   
+  },
+};
+
+export default adapter;
+
