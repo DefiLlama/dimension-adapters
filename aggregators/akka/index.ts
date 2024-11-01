7725081871fd9398be3ab89028b146ffe9e6e701
chef-plankton <ghadirimohammadsaeed@gmail.com>
2024-11-01 07:41:42 +0400

    update fork and add new chains for akka

--- aggregators/akka/index.ts
+++ aggregators/akka/index.ts
@@ -1,36 +1,58 @@
-import fetchURL from "../../utils/fetchURL"
-import { FetchResult, SimpleAdapter } from "../../adapters/types";
+import fetchURL from "../../utils/fetchURL";
+import { FetchOptions, FetchResult, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 const URL = 'https://routerv2.akka.finance';
-const endpoint = '/v2/1116/statistics/dappradar';
-const startTimestamp = 1717200000;// 6/1/2024
 
 interface IAPIResponse {
-    dailyVolume: string;
-    totalVolume: string;
+  dailyVolume: string;
+  totalVolume: string;
 }
 
-const fetch = async (timestamp: number): Promise<FetchResult> => {
-    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-    const { dailyVolume, totalVolume }: IAPIResponse = (await fetchURL(`${URL}${endpoint}`));
-    return {
-        dailyVolume,
-        totalVolume,
-        timestamp: dayTimestamp,
-    };
-}
+const chainIds = {
+  [CHAIN.CORE]: 1116,
+  [CHAIN.XDC]: 50,
+  [CHAIN.BITLAYER]: 200901,
+  [CHAIN.BSQUARED]: 223,
+};
+
+const startTimestamps = {
+  [CHAIN.CORE]: 1717200000,// 6/1/2024
+  [CHAIN.XDC]: 1730160000,// 29/10/2024
+  [CHAIN.BITLAYER]: 1730160000,// 29/10/2024
+  [CHAIN.BSQUARED]: 1730160000,// 29/10/2024
+};
+
+const fetch = async (timestamp: number, _: any, { chain }: FetchOptions): Promise<FetchResult> => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+  const chainId = chainIds[chain];
+
+  const endpoint = `/v2/${ chainId }/statistics/dappradar`;
+  const response = await fetchURL(`${ URL }${ endpoint }`);
+
+  const { dailyVolume, totalVolume }: IAPIResponse = response;
+
+  return {
+    dailyVolume,
+    totalVolume,
+    timestamp: dayTimestamp,
+  };
+};
 
 const adapter: SimpleAdapter = {
-    version: 1,
-    adapter: {
-        [CHAIN.CORE]: {
-            fetch,
-            runAtCurrTime: true,
-            start: startTimestamp,
-        },
-    },
+  version: 1,
+  adapter: Object.keys(chainIds).reduce((acc, chain) => {
+    const startTimestamp = startTimestamps[chain];
+    return {
+      ...acc,
+      [chain]: {
+        fetch,
+        runAtCurrTime: true,
+        start: startTimestamp,
+      },
+    };
+  }, {}),
 };
 
 export default adapter;
