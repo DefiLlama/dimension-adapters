f51f844798a72f867e907e6815abbafb9599025e
yellow <8539006+yellowBirdy@users.noreply.github.com>
2024-11-03 21:51:28 +0100

    volume arb

--- dexs/nabla/index.ts
+++ dexs/nabla/index.ts
@@ -19,6 +19,7 @@ const abis = {
     },
     router: {
         poolByAsset: "function poolByAsset(address asset) external view returns (address)",
+        swapEvent: "event Swap(address indexed,uint256,uint256 amountOut,address tokenIn,address tokenOut,address indexed to)",
     },
     swapPool: {
         chargedSwapFeesEvent: 'event ChargedSwapFees(uint256 lpFees, uint256 backstopFees, uint256 protocolFees)'
@@ -61,56 +62,68 @@ export default {
             calls: poolsCalls
         });
         
-      
+        const dailyVolume = createBalances()
+        const volumeLogs = await Promise.all(routers.map(router => getLogs({
+                target: router,
+                eventAbi: abis.router.swapEvent
+            })
+        ));
+
+        volumeLogs.forEach((logs, i) => {
+            logs.map((e: any) => {
+                dailyVolume.add(e.tokenIn, e.amountIn)
+                dailyVolume.add(e.tokenOut, e.amountOut)
+            })
+        })
  
         const dailyFees = createBalances()
         const dailyUserFees = createBalances()
         const dailyProtocolRevenue = createBalances()
-        const logs = await Promise.all(pools.map(pool => getLogs({
-                target: pool,
-                eventAbi: abis.swapPool.chargedSwapFeesEvent
-            })
-        ));
-        logs.forEach((log, i) => {
-            log.map((e: any) => {
-                dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
-                dailyUserFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
-                dailyProtocolRevenue.add(assets[i], e.protocolFees) 
-            })
-        })
-        return { dailyFees, dailyProtocolRevenue, }
+        // const logs = await Promise.all(pools.map(pool => getLogs({
+        //         target: pool,
+        //         eventAbi: abis.swapPool.chargedSwapFeesEvent
+        //     })
+        // ));
+        // logs.forEach((log, i) => {
+        //     log.map((e: any) => {
+        //         dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
+        //         dailyUserFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
+        //         dailyProtocolRevenue.add(assets[i], e.protocolFees) 
+        //     })
+        // })
+        return { dailyFees, dailyProtocolRevenue, dailyUserFees, dailyVolume, }
       }) as FetchV2,
       meta: {
         methodology 
       },
       start: 1723690984,
     },
-    [CHAIN.BASE]: {
-        fetch: (async ({ getLogs, createBalances}) => {
+    // [CHAIN.BASE]: {
+    //     fetch: (async ({ getLogs, createBalances}) => {
 
-            const pools = ["0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85", "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB"];
-            const assets = ["0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf", "0x4200000000000000000000000000000000000006"];
+    //         const pools = ["0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85", "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB"];
+    //         const assets = ["0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf", "0x4200000000000000000000000000000000000006"];
             
-            const dailyFees = createBalances()
-            const dailyRevenue = createBalances()
-            const logs = await Promise.all(pools.map(pool => getLogs({
-                    target: pool,
-                    eventAbi: abis.swapPool.chargedSwapFeesEvent
-                })
-            ));
-            logs.forEach((log, i) => {
-                log.map((e: any) => {
-                    dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
-                    dailyRevenue.add(assets[i], e.protocolFees) 
-                })
-            })
-            return { dailyFees, dailyRevenue, }
-        }) as FetchV2,
-        start: 1726157219,
-        meta: {
-            methodology 
-        }
-      },
+    //         const dailyFees = createBalances()
+    //         const dailyRevenue = createBalances()
+    //         const logs = await Promise.all(pools.map(pool => getLogs({
+    //                 target: pool,
+    //                 eventAbi: abis.swapPool.chargedSwapFeesEvent
+    //             })
+    //         ));
+    //         logs.forEach((log, i) => {
+    //             log.map((e: any) => {
+    //                 dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
+    //                 dailyRevenue.add(assets[i], e.protocolFees) 
+    //             })
+    //         })
+    //         return { dailyFees, dailyRevenue, }
+    //     }) as FetchV2,
+    //     start: 1726157219,
+    //     meta: {
+    //         methodology 
+    //     }
+    //   },
   },
   version: 2,
 } as Adapter
