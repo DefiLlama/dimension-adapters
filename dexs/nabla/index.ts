e0b5d0e9c59d31c2fd6d5a42b9ff75e2279fe0c5
yellow <8539006+yellowBirdy@users.noreply.github.com>
2024-11-04 02:10:00 +0100

    all

--- dexs/nabla/index.ts
+++ dexs/nabla/index.ts
@@ -29,11 +29,16 @@ const abis = {
 const methodology = {
     UserFees: "User pays between 0.01% and 0.1% fees on each swap.",
     ProtocolRevenue: "Currently no fees are taken by the protocl.",
-    // SupplySideRevenue: "LPs receive 0.17% of the fees.",
-    // HoldersRevenue: "0.0575% is used to facilitate CAKE buyback and burn.",
-    // Revenue: "All revenue generated comes from user fees.",
     Fees: "All fees comes from the user."
 }
+
+function addToBalances(balances: any, token: any, amount: any) {
+    if (!balances[token]) {
+        balances[token] = 0
+    }
+    balances[token] += amount
+}
+
 export default {
   adapter: {
     [CHAIN.ARBITRUM]: {
@@ -61,6 +66,8 @@ export default {
             abi: abis.router.poolByAsset,
             calls: poolsCalls
         });
+
+        
         
         const dailyVolume = createBalances()
         const volumeLogs = await Promise.all(routers.map(router => getLogs({
@@ -70,7 +77,7 @@ export default {
         ));
 
         volumeLogs.forEach((logs, i) => {
-            logs.map((e: any) => {
+            logs.forEach((e: any) => {
                 dailyVolume.add(e.tokenIn, e.amountIn)
                 dailyVolume.add(e.tokenOut, e.amountOut)
             })
@@ -79,18 +86,18 @@ export default {
         const dailyFees = createBalances()
         const dailyUserFees = createBalances()
         const dailyProtocolRevenue = createBalances()
-        // const logs = await Promise.all(pools.map(pool => getLogs({
-        //         target: pool,
-        //         eventAbi: abis.swapPool.chargedSwapFeesEvent
-        //     })
-        // ));
-        // logs.forEach((log, i) => {
-        //     log.map((e: any) => {
-        //         dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
-        //         dailyUserFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
-        //         dailyProtocolRevenue.add(assets[i], e.protocolFees) 
-        //     })
-        // })
+        const logs = await Promise.all(pools.map(pool => getLogs({
+                target: pool,
+                eventAbi: abis.swapPool.chargedSwapFeesEvent
+            })
+        ));
+        logs.forEach((log, i) => {
+            log.forEach((e: any) => {
+                dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
+                dailyUserFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
+                dailyProtocolRevenue.add(assets[i], e.protocolFees) 
+            })
+        })
         return { dailyFees, dailyProtocolRevenue, dailyUserFees, dailyVolume, }
       }) as FetchV2,
       meta: {
@@ -98,32 +105,46 @@ export default {
       },
       start: 1723690984,
     },
-    // [CHAIN.BASE]: {
-    //     fetch: (async ({ getLogs, createBalances}) => {
+    [CHAIN.BASE]: {
+        fetch: (async ({ getLogs, createBalances}) => {
 
-    //         const pools = ["0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85", "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB"];
-    //         const assets = ["0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf", "0x4200000000000000000000000000000000000006"];
+            const router = "0x791Fee7b66ABeF59630943194aF17B029c6F487B";
+            const pools = ["0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85", "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB"];
+            const assets = ["0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf", "0x4200000000000000000000000000000000000006"];
             
-    //         const dailyFees = createBalances()
-    //         const dailyRevenue = createBalances()
-    //         const logs = await Promise.all(pools.map(pool => getLogs({
-    //                 target: pool,
-    //                 eventAbi: abis.swapPool.chargedSwapFeesEvent
-    //             })
-    //         ));
-    //         logs.forEach((log, i) => {
-    //             log.map((e: any) => {
-    //                 dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
-    //                 dailyRevenue.add(assets[i], e.protocolFees) 
-    //             })
-    //         })
-    //         return { dailyFees, dailyRevenue, }
-    //     }) as FetchV2,
-    //     start: 1726157219,
-    //     meta: {
-    //         methodology 
-    //     }
-    //   },
+            const dailyVolume = createBalances()
+            const volumeLogs = await getLogs({
+                target: router,
+                eventAbi: abis.router.swapEvent
+            });
+    
+            volumeLogs.forEach((e, i) => {
+                dailyVolume.add(e.tokenIn, e.amountIn)
+                dailyVolume.add(e.tokenOut, e.amountOut)
+            })
+
+            const dailyFees = createBalances()
+            const dailyUserFees = createBalances()
+            const dailyRevenue = createBalances()
+            const logs = await Promise.all(pools.map(pool => getLogs({
+                    target: pool,
+                    eventAbi: abis.swapPool.chargedSwapFeesEvent
+                })
+            ));
+            logs.forEach((log, i) => {
+                log.forEach((e: any) => {
+                    dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
+                    dailyUserFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
+                    dailyRevenue.add(assets[i], e.protocolFees) 
+                })
+            })
+            return { dailyFees, dailyRevenue, dailyUserFees, dailyVolume }
+        }) as FetchV2,
+        start: 1726157219,
+        meta: {
+            methodology 
+        }
+      },
   },
   version: 2,
 } as Adapter
