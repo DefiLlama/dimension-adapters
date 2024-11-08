f161dde4851273b43b8788737a2a50233fed21c7
0xgnek <0xgnek@gmail.com>
2024-11-08 18:03:14 +0000

    fix use axios

--- aggregators/navi/index.ts
+++ aggregators/navi/index.ts
@@ -1,25 +1,26 @@
 import { CHAIN } from "../../helpers/chains";
 import { FetchOptions } from "../../adapters/types";
+import axios from "axios";
 
 const sentioApiKey = '3taTsrTS3cZq4tcKkKVSajWSJxkjZmcet'; //Read Only
 
 const fetchDailyVolume = async ({ fromTimestamp, toTimestamp, startOfDay }: FetchOptions) => {
   const url = 'https://app.sentio.xyz/api/v1/analytics/navi/dex-aggregator/sql/execute';
-  const res = await fetch(url, {
+  const res = await axios(url, {
     method: 'POST',
     headers: {
       'api-key': sentioApiKey,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
+    data: JSON.stringify({
       sqlQuery: {
         sql: `SELECT SUM(GREATEST(amount_in_usd, amount_out_usd)) AS usdValue
-              FROM \`swapEvent\`
+              FROM \'swapEvent\'
               WHERE timestamp >= ${fromTimestamp} AND timestamp <= ${toTimestamp};`
       },
       version: 15
     })
-  }).then(response => response.json());
+  }).then(response => response.data);
 
   return {
     dailyVolume: res.result.rows[0].usdValue,
