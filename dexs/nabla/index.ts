5fb0f6ab5c9cbc580bfd33f5ec58cd85bccf62e5
yellow <8539006+yellowBirdy@users.noreply.github.com>
2024-11-10 02:14:09 +0100

    fix for base vol, backfill fix

--- dexs/nabla/index.ts
+++ dexs/nabla/index.ts
@@ -7,9 +7,9 @@ const config = {
     [CHAIN.ARBITRUM]: {
         registry: "0xcB94Eee869a2041F3B44da423F78134aFb6b676B",
     },
-    // [CHAIN.BASE]: {
-    //     registry: "0xcB94Eee869a2041F3B44da423F78134aFb6b676B",
-    // }
+    [CHAIN.BASE]: {
+        registry: "0xd24d145f5E351de52934A7E1f8cF55b907E67fFF",
+    }
 }
 
 const abis = {
@@ -32,19 +32,13 @@ const methodology = {
     Fees: "All fees comes from the user."
 }
 
-function addToBalances(balances: any, token: any, amount: any) {
-    if (!balances[token]) {
-        balances[token] = 0
-    }
-    balances[token] += amount
-}
-
-export default {
-  adapter: {
-    [CHAIN.ARBITRUM]: {
-      fetch: (async ({ getLogs, createBalances, api}) => {
-        const {registry} = config[CHAIN.ARBITRUM];
-        const routers = await api.call({abi: abis.portal.routers, target: registry, block: "latest"})
+async function getAddresses(chain, api) {
+    const {registry} = config[chain];
+    let routers;
+    let pools;
+    let assets;
+    try {
+        routers = await api.call({abi: abis.portal.routers, target: registry, block: "latest"});
         const assetsResponse = await api.multiCall({
             abi: abis.portal.routerAssets,
             calls: routers.map((router: any) => ({
@@ -52,7 +46,7 @@ export default {
                 params: router
             }))
         });
-        const assets = assetsResponse.reduce((acc, a: any[]) => [...acc,...a], [])
+        assets = assetsResponse.reduce((acc, a: any[]) => [...acc,...a], [])
         const poolsCalls: any[] = [];
         routers.forEach((router: any, i) => {
             assetsResponse[i].forEach((asset: any) => {
@@ -62,12 +56,25 @@ export default {
                 })
             });
         });
-        const pools = await api.multiCall({
+        pools = await api.multiCall({
             abi: abis.router.poolByAsset,
             calls: poolsCalls
         });
 
+    } catch (e) {
+        routers = ["0x791Fee7b66ABeF59630943194aF17B029c6F487B"];
+        pools = ["0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85", "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB"];
+        assets = ["0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf", "0x4200000000000000000000000000000000000006"];
         
+    }
+    return {routers, pools, assets}
+}
+
+export default {
+  adapter: {
+    [CHAIN.ARBITRUM]: {
+      fetch: (async ({ getLogs, createBalances, api}) => {
+        const {routers, pools, assets} = await getAddresses(CHAIN.ARBITRUM, api)
         
         const dailyVolume = createBalances()
         const volumeLogs = await Promise.all(routers.map(router => getLogs({
@@ -105,35 +112,37 @@ export default {
       start: 1723690984,
     },
     [CHAIN.BASE]: {
-        fetch: (async ({ getLogs, createBalances}) => {
+        fetch: (async ({ getLogs, createBalances, api}) => {
 
-            const router = "0x791Fee7b66ABeF59630943194aF17B029c6F487B";
-            const pools = ["0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85", "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB"];
-            const assets = ["0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf", "0x4200000000000000000000000000000000000006"];
-            
+            const {routers, pools, assets} = await getAddresses(CHAIN.BASE, api)
+
             const dailyVolume = createBalances()
-            const volumeLogs = await getLogs({
+            const volumeLogs = await Promise.all(routers.map(router => getLogs({
                 target: router,
                 eventAbi: abis.router.swapEvent
-            });
-    
-            volumeLogs.forEach((e, i) => {
-                dailyVolume.add(e.tokenIn, e.amountIn)
+            })
+            ));
+
+            volumeLogs.forEach((logs, i) => {
+                logs.forEach((e: any) => {
+                    dailyVolume.add(e.tokenOut, e.amountOut)
+                })
             })
 
             const dailyFees = createBalances()
             const dailyUserFees = createBalances()
             const dailyRevenue = createBalances()
+            const dailyProtocolRevenue = createBalances()
             const logs = await Promise.all(pools.map(pool => getLogs({
-                    target: pool,
-                    eventAbi: abis.swapPool.chargedSwapFeesEvent
-                })
+                target: pool,
+                eventAbi: abis.swapPool.chargedSwapFeesEvent
+            })
             ));
             logs.forEach((log, i) => {
                 log.forEach((e: any) => {
                     dailyFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
                     dailyUserFees.add(assets[i], e.lpFees+e.backstopFees+e.protocolFees) 
-                    dailyRevenue.add(assets[i], e.protocolFees) 
+                    dailyProtocolRevenue.add(assets[i], e.protocolFees) 
                 })
             })
             return { dailyFees, dailyRevenue, dailyUserFees, dailyVolume }
