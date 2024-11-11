114ad1419a9cf9933e605e948ca4ab51d18c4608
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-11 13:21:48 +0100

    Morfi (#2085)
    * init
    
    * init
    
    * add fee
    
    * revoke package-lock.json change
    
    * update defillama-sdk
    
    * revert formatter
    
    * track fees
    
    ---------
    
    Co-authored-by: gWhy-j <gwanghyunjeon@gmail.com>

---
+++ fees/morFi/index.ts
@@ -0,0 +1,36 @@
+import { CHAIN } from "../../helpers/chains";
+import { Adapter } from "../../adapters/types";
+import { getGraphDimensions } from "../../helpers/getUniSubgraph";
+
+const fetch = getGraphDimensions({
+  graphUrls: {
+    [CHAIN.MORPH]: "https://subgraph.morfi.io/subgraphs/name/morfi/core",
+  },
+  totalVolume: {
+    factory: "factories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "algebraDayData",
+    field: "volumeUSD",
+  },
+  totalFees: {
+    factory: "factories",
+    field: "totalFeesUSD",
+  },
+  dailyFees: {
+    factory: "algebraDayData",
+    field: "feesUSD",
+  },
+});
+
+const adapters: Adapter = {
+  adapter: {
+    [CHAIN.MORPH]: {
+      fetch: fetch(CHAIN.MORPH),
+      start: 1730177105,
+    },
+  },
+};
+
+export default adapters;
