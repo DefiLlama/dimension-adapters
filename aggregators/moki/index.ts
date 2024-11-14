a4a20caa9630d5deb3b032cb48138d4f823a9370
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-14 11:33:08 +0100

    minor fix

--- aggregators/moki/index.ts
+++ aggregators/moki/index.ts
@@ -1,31 +1,30 @@
-import {Fetch, SimpleAdapter} from "../../adapters/types";
-import {CHAIN} from "../../helpers/chains";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
 import fetchURL from "../../utils/fetchURL";
 
 const URL = 'https://api.leapwallet.io/';
 const endpoint = 'ton-sor/api/v1/analytics/volumes';
-const start = 1727711103;// 30.09.2024
 
 interface IAPIResponse {
-    dailyVolume: string;
-    totalVolume: string;
+  dailyVolume: string;
+  totalVolume: string;
 }
 
-const fetch: Fetch = async (timestamp) => {
-    const {dailyVolume, totalVolume}: IAPIResponse = await fetchURL(`${URL}${endpoint}?timestamp=${timestamp * 1000}`);
-    return {
-        timestamp,
-        dailyVolume,
-        totalVolume
-    };
+const fetch: any = async ({ endTimestamp }: FetchOptions) => {
+  const { dailyVolume, totalVolume }: IAPIResponse = await fetchURL(`${URL}${endpoint}?timestamp=${endTimestamp * 1000}`);
+  return {
+    dailyVolume,
+    totalVolume
+  };
 };
 
 const adapters: SimpleAdapter = {
-    adapter: {
-        [CHAIN.TON]: {
-            start,
-            fetch
-        }
+  version: 2,
+  adapter: {
+    [CHAIN.TON]: {
+      start: "2024-09-30",
+      fetch
     }
+  }
 }
 export default adapters
