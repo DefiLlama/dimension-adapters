cfef70824f68f5f0c721458c2efb84773d69266e
stabble-devops <keisuke@stabble.org>
2024-11-17 12:52:10 -0500

    chore: reformat code styles

--- dexs/stabble/index.ts
+++ dexs/stabble/index.ts
@@ -1,28 +1,32 @@
-import { CHAIN } from '../../helpers/chains';
-import { FetchOptions, FetchV2 } from '../../adapters/types';
+import { CHAIN } from "../../helpers/chains";
+import { Adapter, FetchOptions, FetchV2 } from "../../adapters/types";
 import { getTimestampAtStartOfPreviousDayUTC } from "../../utils/date";
-import fetchURL from '../../utils/fetchURL';
+import fetchURL from "../../utils/fetchURL";
 
-const volumeURL = 'https://api.stabble.org/stats/volume';
+const volumeURL = "https://api.stabble.org/stats/volume";
 
 const fetch: FetchV2 = async (options: FetchOptions) => {
-    const dayTimestamp = getTimestampAtStartOfPreviousDayUTC(options.endTimestamp);
+  const dayTimestamp = getTimestampAtStartOfPreviousDayUTC(
+    options.endTimestamp
+  );
 
-    const url = `${volumeURL}?startTimestamp=${options.startTimestamp}&endTimestamp=${options.endTimestamp}`;
-    const dailyVolume = await fetchURL(url);
+  const url = `${volumeURL}?startTimestamp=${options.startTimestamp}&endTimestamp=${options.endTimestamp}`;
+  const dailyVolume = await fetchURL(url);
 
-    return {
-        timestamp: dayTimestamp,
-        dailyVolume: dailyVolume,
-    }
+  return {
+    timestamp: dayTimestamp,
+    dailyVolume: dailyVolume,
+  };
 };
 
-export default {
-    version: 2,
-    adapter: {
-        [CHAIN.SOLANA]: {
-            fetch: fetch,
-            start: 1717563162,
-        }
-    }
-}
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.SOLANA]: {
+      fetch: fetch,
+      start: 1717563162,
+    },
+  },
+};
+
+export default adapter;
