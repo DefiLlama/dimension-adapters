801a6a2525f1d6a6cb4ee479c9e2d138de64e42a
matt cole <mattcole1093@gmail.com>
2024-11-17 23:57:53 +0700

    adding kamino-liquidity fees

--- fees/kamino-liquidity/index.ts
+++ fees/kamino-liquidity/index.ts
@@ -11,21 +11,33 @@ const fetch: FetchV2 = async ({ endTimestamp }) =>  {
     const dayTimestamp = getTimestampAtStartOfPreviousDayUTC(endTimestamp);
     const historicalFeesRes = (await fetchURL(AllezLabsKaminoFeeEndpoint));
 
+    // Calculate total and daily revenue
     const totalRevenue = historicalFeesRes['data']
     .filter(row => row.timestamp <= dayTimestamp)
     .reduce((acc, {KaminoLiquidityRevenueUsd}) => acc + KaminoLiquidityRevenueUsd, 0);
     
     const dailyRevenue = historicalFeesRes['data']
     .find(row => Math.abs(row.timestamp - dayTimestamp) < 3600*24 - 1)?.KaminoLiquidityRevenueUsd;
+
+    // Calculate total and daily fees
+    const totalFees = historicalFeesRes['data']
+    .filter(row => row.timestamp <= dayTimestamp)
+    .reduce((acc, {KaminoLiquidityFeesUsd}) => acc + KaminoLiquidityFeesUsd, 0);
+
+    const dailyFees = historicalFeesRes['data']
+    .find(row => Math.abs(row.timestamp - dayTimestamp) < 3600*24 - 1)?.KaminoLiquidityFeesUsd;
     
     return {
         timestamp: dayTimestamp,
         totalRevenue: `${totalRevenue}`,
-        dailyRevenue: `${dailyRevenue}`
+        dailyRevenue: `${dailyRevenue}`,
+        totalFees: `${totalFees}`,
+        dailyFees: `${dailyFees}`
     };
 };
 const methodology = {
-    Revenue: "Revenues are aggregated by Allez Labs using the following flipside crypto: https://flipsidecrypto.xyz/kakagri/kamino-vault-fees-4cDBfs"
+    Revenue: "Revenues are aggregated by Allez Labs using Flipside Crypto data.",
+    Fees: "Fees are aggregated by Allez Labs using the Kamino API."
 }
 
 const adapter: Adapter = {
@@ -42,5 +54,3 @@ const adapter: Adapter = {
     }
 }
 export default adapter;
-
-
