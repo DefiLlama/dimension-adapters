08054f95682cbedfea27b9b0fb5f97ed159303bc
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-11-18 13:31:44 -0300

    Remove v1.2 logic

--- fees/size-credit.ts
+++ fees/size-credit.ts
@@ -1,23 +1,10 @@
 import { Adapter, FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
-const SIZE_REGISTRY = '0x'
-
-const SizeV1Addresses = [
-  {
-    name: 'Size_WETH_USDC',
-    Size: '0xC2a429681CAd7C1ce36442fbf7A4a68B11eFF940',
-    BorrowAToken: '0x38978038a06a21602a4202dfa66968e7f525bf3e',
-  },
-  {
-    name: 'Size_cbBTC_USDC',
-    Size: '0xB21Bbe052F5cE9ae681c59725f0A313765Fd016c',
-    BorrowAToken: '0x539cB6BB9bee5aaBdFd98eE3ad36849Ac81a2b07'
-  }
-]
+const SIZE_FACTORY = '0x330Dc31dB45672c1F565cf3EC91F9a01f8f3DF0b'
 
 const abis = {
-  SizeRegistry: {
+  SizeFactory: {
     getMarkets: 'function getMarkets() view returns (address[])',
   },
   Size: {
@@ -27,22 +14,18 @@ const abis = {
   ERC20: {
     Transfer: "event Transfer(address indexed from,address indexed to,uint256 value)",
   },
-  BorrowATokenV1: {
-    TransferUnscaled: "event(address indexed from,address indexed to,uint256 value)"
-  }
 }
 
 const fetch: any = async ({ createBalances, getLogs, api, }: FetchOptions) => {
   const fees = createBalances()
 
-  const markets = await api.call({ abi: abis.SizeRegistry.getMarkets, target: SIZE_REGISTRY })
+  const markets = await api.call({ abi: abis.SizeFactory.getMarkets, target: SIZE_FACTORY })
   const [datas, feeConfigs] = await Promise.all([
     api.multiCall({ abi: abis.Size.data, calls: markets }),
     api.multiCall({ abi: abis.Size.feeConfig, calls: markets }),
   ])
 
   for (let i = 0; i < markets.length; i++) {
-    const market = markets[i]
     const data = datas[i]
     const feeConfig = feeConfigs[i]
 
@@ -57,7 +40,7 @@ const fetch: any = async ({ createBalances, getLogs, api, }: FetchOptions) => {
       underlyingBorrowToken,
     ]
 
-    const getLogsArray = [
+    const logsArray = await Promise.all([
       getLogs({
         target: collateralToken,
         eventAbi: abis.ERC20.Transfer,
@@ -66,22 +49,7 @@ const fetch: any = async ({ createBalances, getLogs, api, }: FetchOptions) => {
         target: borrowAToken,
         eventAbi: abis.ERC20.Transfer,
       }),
-    ]
-
-    const v1Markets = SizeV1Addresses.map((v1) => v1.Size.toLowerCase())
-
-    if (v1Markets.includes(market.toLowerCase())) {
-      // also track old (v1) events that happened before the migration
-      FEE_MAPPING.push(underlyingBorrowToken)
-      getLogsArray.push(
-        getLogs({
-          target: SizeV1Addresses.find((v1) => v1.Size.toLowerCase() === market.toLowerCase())!.BorrowAToken,
-          eventAbi: abis.BorrowATokenV1.TransferUnscaled,
-        }),
-      )
-    }
-
-    const logsArray = await Promise.all(getLogsArray)
+    ])
 
     logsArray.forEach((logs, j) => {
       logs.forEach((log) => {
