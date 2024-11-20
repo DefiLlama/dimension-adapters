34a817783512f2a7ae2ee11f21f41aa0cf85131d
Paul <zhpv96@gmail.com>
2024-11-20 16:21:19 +0700

    add totalVolume spark

---
+++ dexs/spark/index.ts
@@ -0,0 +1,61 @@
+import type { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import fetch from "node-fetch";
+
+const url = 'https://app.sentio.xyz/api/v1/analytics/zhpv96/spark-processor/sql/execute';
+const apiKey = 'TLjw41s3DYbWALbwmvwLDM9vbVEDrD9BP';
+const data = {
+  "sqlQuery": {
+    "sql": "SELECT tradeVolume, timestamp FROM `TradeVolume_raw` ORDER BY `timestamp` DESC LIMIT 1"
+  }
+};
+
+const fetchTradeVolume = () =>
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'api-key': apiKey,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((result) => {
+      const rows = result.result?.rows || [];
+
+      if (rows.length === 0) {
+        console.warn('No trade volume data available.');
+        return {
+          totalVolume: null,
+          timestamp: null,
+        };
+      }
+
+      const totalVolume = rows[0]?.tradeVolume;
+      const timestamp = rows[0]?.timestamp;
+
+      console.log('Trade Volume:', totalVolume);
+      console.log('Timestamp:', timestamp);
+
+      return {
+        totalVolume: totalVolume,
+        timestamp: timestamp,
+      };
+    });
+
+const adapters: SimpleAdapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.FUEL]: {
+      fetch: fetchTradeVolume,
+      start: 1601424000,
+    },
+  },
+};
+
+export default adapters;
