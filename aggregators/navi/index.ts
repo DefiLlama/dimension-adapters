7ae4cf4404e7d42e2eaa0a5b87e73ff83e6ae38e
Luren L. <haoran.sde@gmail.com>
2024-11-22 22:25:12 -0500

    Revert "Update index.ts"
    This reverts commit f60ef7cbfef8fc6e26e317e67ad26aad39b3cf00.

--- aggregators/navi/index.ts
+++ aggregators/navi/index.ts
@@ -18,7 +18,7 @@ const fetchDailyVolume = async ({ fromTimestamp, toTimestamp, startOfDay }: Fetc
               FROM \'swapEvent\'
               WHERE timestamp >= ${fromTimestamp} AND timestamp <= ${toTimestamp};`
       },
-      version: 22
+      version: 16
     })
   }).then(response => response.data);
 
