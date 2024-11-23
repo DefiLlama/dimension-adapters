a28000d49758a64a8e3f192efa940d3cd36a5386
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-23 16:53:30 +0100

    Revert "fix opensea"
    This reverts commit 2fb55c1cc1d8608739e3522abd9f41b65097cea0.

--- fees/opensea/index.ts
+++ fees/opensea/index.ts
@@ -1,10 +1,31 @@
+import * as sdk from "@defillama/sdk";
 import { Adapter, DISABLED_ADAPTER_KEY } from "../../adapters/types";
+import type { ChainEndpoints } from "../../adapters/types"
+import { Chain } from '@defillama/sdk/build/general';
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 import { CHAIN } from "../../helpers/chains";
 import { fetch } from "./seaport";
 import disabledAdapter from "../../helpers/disabledAdapter";
 
+const seaportEndpoints = {
+  [CHAIN.ETHEREUM]: sdk.graph.modifyEndpoint('kGCuz7xhxMuyRSk8QdnUgijUEqgvhGwkzAVHkbYedCk'),
+}
+
+const graphs = (_: ChainEndpoints) => {
+  return (_: Chain) => {
+    return async (timestamp: number) => {
+      const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
+      const fees = await fetch(timestamp);
+      return {
+        ...fees,
+        timestamp: todaysTimestamp,
+      }
+    };
+  };
+};
+
 const adapter: Adapter = {
-  version: 2,
+  version: 1,
   breakdown: {
     v1: {
       [DISABLED_ADAPTER_KEY]: disabledAdapter,
@@ -16,7 +37,7 @@ const adapter: Adapter = {
     },
     seaport: {
       [CHAIN.ETHEREUM]: {
-        fetch: fetch,
+        fetch: graphs(seaportEndpoints)(CHAIN.ETHEREUM),
         start: '2022-06-12',
       },
     }
