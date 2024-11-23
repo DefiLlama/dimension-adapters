4772fd0fd8a5436b1d72bb29d6ce48c6ab109b34
Catton <166699866+Cattontech@users.noreply.github.com>
2024-11-23 07:57:20 +0700

    fix bug

--- dexs/catton/index.ts
+++ dexs/catton/index.ts
@@ -5,9 +5,8 @@ import { FetchOptions } from "../../adapters/types";
 const endpoint = "https://datagrab.catton.tech/getVolumeData?"
 
 const fetch = async (options: FetchOptions) => {
-    console.log('options', options)
-  const startTime = new Date(options.startTimestamp * 1000).toISOString()
-  const endTime = new Date(options.endTimestamp * 1000).toISOString()
+  const startTime = new Date(options.startTimestamp * 1000).toISOString().split("T")[0]
+  const endTime = new Date(options.endTimestamp * 1000).toISOString().split("T")[0]
   const res = await fetchURL(`${endpoint}startTime=${startTime}&endTime=${endTime}`)
   return {
     dailyVolume: parseInt(res['dailyVolume']),
@@ -22,7 +21,7 @@ const adapter: any = {
   adapter: {
     [CHAIN.TON]: {
       fetch,
-      start: '2024-11-22',
+      start: '2024-11-21',
     },
   },
 };
