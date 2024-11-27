ca2231670fa8caf82d162556e44355678d08c568
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-11-28 03:15:32 +0800

    feat: add IoTeX fees and revenue

---
+++ fees/iotex/index.ts
@@ -0,0 +1,60 @@
+import { Adapter, FetchOptions, ProtocolType } from "../../adapters/types";
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
+import { httpGet } from "../../utils/fetchURL";
+import { CHAIN } from "../../helpers/chains";
+
+const IOTEX_SUM_GAS_URL = "https://gateway1.iotex.me/analyzer/sumGasFeeIotx";
+
+const fetch = async (options: FetchOptions) => {
+  const dailyFees = await getDailyFees(options.startTimestamp);
+  const totalFees = await getTotalFees();
+
+  return {
+    dailyFees,
+    dailyRevenue: dailyFees,
+    totalFees,
+    totalRevenue: totalFees,
+  };
+};
+
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.IOTEX]: {
+      fetch,
+      start: "2021-06-22",
+      meta: {
+        hallmarks: [],
+      },
+    },
+  },
+  protocolType: ProtocolType.CHAIN,
+};
+
+export default adapter;
+
+const getDailyFees = async (timestamp: number) => {
+  const today = new Date(getTimestampAtStartOfDayUTC(timestamp) * 1000)
+    .toISOString()
+    .split("T")[0];
+
+  const result = await httpGet(
+    `${IOTEX_SUM_GAS_URL}?start_date=${today}&end_date=${today}`
+  );
+
+  if (!result) {
+    throw new Error(`Failed to fetch IoTeX Sum Gas data for ${today}`);
+  }
+
+  return parseFloat(result);
+};
+
+const getTotalFees = async () => {
+  const result = await httpGet(IOTEX_SUM_GAS_URL);
+
+  if (!result) {
+    throw new Error(`Failed to fetch IoTeX Total Sum Gas data`);
+  }
+
+  return parseFloat(result);
+};
