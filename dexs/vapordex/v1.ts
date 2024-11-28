53b7f32732b77f4d281703479338f36caa9ad4dd
royvardhan <royvardhan.official@gmail.com>
2024-11-28 11:16:11 +0530

    feat: add apechain support vapordex

--- dexs/vapordex/v1.ts
+++ dexs/vapordex/v1.ts
@@ -2,10 +2,11 @@ import * as sdk from "@defillama/sdk";
 import { CHAIN } from "../../helpers/chains";
 import { univ2Adapter2 } from "../../helpers/getUniSubgraphVolume";
 
-const v1 = univ2Adapter2(
+// Create AVAX adapter with custom fields
+const avaxAdapter = univ2Adapter2(
   {
     [CHAIN.AVAX]: sdk.graph.modifyEndpoint(
-      "B6Tur5gXGCcswG8rEtmwfjBqeyDXCDUQSwM9wUXHoui5",
+      "B6Tur5gXGCcswG8rEtmwfjBqeyDXCDUQSwM9wUXHoui5"
     ),
   },
   {
@@ -14,8 +15,23 @@ const v1 = univ2Adapter2(
     dayData: "financialsDailySnapshot",
     dailyVolume: "dailyVolumeUSD",
     dailyVolumeTimestampField: "timestamp",
-  },
+  }
 );
 
-v1.adapter.avax.start = 1663545600;
-export default v1.adapter;
+const apechainAdapter = univ2Adapter2(
+  {
+    [CHAIN.APECHAIN]: sdk.graph.modifyEndpoint(
+      "QmUpkSrGVym7Qv2akaJkCm8HZzH2wyfdzuFoYW9mrX83eA"
+    ),
+  },
+  {} // Use default values
+);
+
+const adapter = {
+  ...avaxAdapter.adapter,
+  ...apechainAdapter.adapter,
+};
+
+adapter.avax.start = 1663545600;
+adapter.apechain.start = 2519744;
+export default adapter;
