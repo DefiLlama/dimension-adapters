c5d66850f1a76e5c1a0fa90623ce118136f48f5c
octavionotpunk <octave.mesnard@gmail.com>
2024-12-01 19:01:18 +0100

    add new chains

--- bridge-aggregators/jumper.exchange/index.ts
+++ bridge-aggregators/jumper.exchange/index.ts
@@ -24,6 +24,14 @@ const contract: IContract = {
   [CHAIN.ERA]: '0x341e94069f53234fe6dabef707ad424830525715',
   [CHAIN.METIS]: '0x24ca98fb6972f5ee05f0db00595c7f68d9fafd68',
   [CHAIN.XDAI]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.TAIKO]: '0x3a9a5dba8fe1c4da98187ce4755701bca182f63b',
+  [CHAIN.BLAST]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.BOBA]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.FUSE]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.CRONOS]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.GRAVITY]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  // [CHAIN.SEI]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  // [CHAIN.ROOTSTOCK]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
 }
 
 const fetch: any = async (timestamp: number, _, { chain, getLogs, createBalances, }: FetchOptions): Promise<FetchResultVolume> => {
