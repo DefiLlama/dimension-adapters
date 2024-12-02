c2f918c06f1e87dc5d198e07428c0aa77e0dac7a
octavionotpunk <83534113+Octave-byte@users.noreply.github.com>
2024-12-02 11:31:27 +0100

    Add new chains (#2164)
    * add new chains
    
    * add chains to swap agg, change manta to mantle on bridge

--- bridge-aggregators/jumper.exchange/index.ts
+++ bridge-aggregators/jumper.exchange/index.ts
@@ -15,7 +15,7 @@ const contract: IContract = {
   [CHAIN.AVAX]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
   [CHAIN.BSC]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
   [CHAIN.LINEA]: '0xde1e598b81620773454588b85d6b5d4eec32573e',
-  [CHAIN.MANTA]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.MANTLE]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
   [CHAIN.POLYGON]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
   [CHAIN.POLYGON_ZKEVM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
   [CHAIN.FANTOM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
@@ -24,6 +24,14 @@ const contract: IContract = {
   [CHAIN.ERA]: '0x341e94069f53234fe6dabef707ad424830525715',
   [CHAIN.METIS]: '0x24ca98fb6972f5ee05f0db00595c7f68d9fafd68',
   [CHAIN.XDAI]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.TAIKO]: '0x3a9a5dba8fe1c4da98187ce4755701bca182f63b',
+  [CHAIN.BLAST]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.BOBA]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.FUSE]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.CRONOS]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  [CHAIN.GRAVITY]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  // [CHAIN.SEI]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+  // [CHAIN.ROOTSTOCK]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
 }
 
 const fetch: any = async (timestamp: number, _, { chain, getLogs, createBalances, }: FetchOptions): Promise<FetchResultVolume> => {
