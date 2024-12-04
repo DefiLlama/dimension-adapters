b116812239278b7b7392cb274ebbbd40c0caef19
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-04 17:25:49 +0100

    refactor

--- dexs/blum/index.ts
+++ dexs/blum/index.ts
@@ -1,18 +1,17 @@
 import fetchURL from "../../utils/fetchURL"
 import { CHAIN } from "../../helpers/chains"
 import { FetchOptions } from "../../adapters/types"
-import { getPrices } from "../../utils/prices"
 
 const endpoint = "https://tonfunstats-eqnd7.ondigitalocean.app/api/v1/getVolume"
 
-const fetch = async (options: FetchOptions) => {
-  const res = await fetchURL(`${endpoint}?from=${options.startTimestamp}&to=${options.endTimestamp}&service=blum`)
+const fetch = async ({ startTimestamp, endTimestamp, createBalances, }: FetchOptions) => {
+  const res = await fetchURL(`${endpoint}?from=${startTimestamp}&to=${endTimestamp}&service=blum`)
   const TON = "coingecko:the-open-network"
-  const ton_price = await getPrices([TON], options.startTimestamp);
 
+  const dailyVolume = createBalances()
+  dailyVolume.addCGToken('the-open-network', res.volume / 1e9)
   return {
-    dailyVolume: Number(BigInt(res.volume) / 1000000000n) * ton_price[TON].price,
-    timestamp: options.startTimestamp,
+    dailyVolume,
   }
 }
 
