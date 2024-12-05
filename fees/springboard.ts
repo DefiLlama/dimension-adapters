732c34848958b39811eb3752dea3fd15029a13b9
0xgnek <0xgnek@gmail.com>
2024-12-05 05:54:06 +0000

    springboard fees

---
+++ fees/springboard.ts
@@ -0,0 +1,26 @@
+import { FetchOptions, SimpleAdapter } from "../adapters/types"
+import { CHAIN } from "../helpers/chains"
+
+const address = '0x5c952063c7fc8610FFDB798152D69F0B9550762b'
+const fetchFees = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances();
+  await options.api.sumTokens({ owners: [address], tokens: ["0x0000000000000000000000000000000000000000"] })
+  await options.fromApi.sumTokens({ owners: [address], tokens: ["0x0000000000000000000000000000000000000000"] })
+  dailyFees.addBalances(options.api.getBalancesV2())
+  dailyFees.subtract(options.fromApi.getBalancesV2())
+  return {
+    dailyFees,
+    dailyRevenue: dailyFees.clone(),
+  }
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.BSC]: {
+      fetch: fetchFees,
+      start: '2024-11-04',
+    }
+  }
+}
+export default adapter
