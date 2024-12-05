a23fe06cdb5d7db37f14838c5abb148720b2b910
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-05 10:07:15 +0100

    minor refactor

--- fees/springboard.ts
+++ fees/springboard.ts
@@ -2,15 +2,15 @@ import { FetchOptions, SimpleAdapter } from "../adapters/types"
 import { CHAIN } from "../helpers/chains"
 
 const address = '0x5c952063c7fc8610FFDB798152D69F0B9550762b'
-const fetchFees = async (options: FetchOptions) => {
-  const dailyFees = options.createBalances();
-  await options.api.sumTokens({ owners: [address], tokens: ["0x0000000000000000000000000000000000000000"] })
-  await options.fromApi.sumTokens({ owners: [address], tokens: ["0x0000000000000000000000000000000000000000"] })
-  dailyFees.addBalances(options.api.getBalancesV2())
-  dailyFees.subtract(options.fromApi.getBalancesV2())
+const fetchFees = async ({ createBalances, fromApi, api }: FetchOptions) => {
+  const dailyFees = createBalances();
+  await api.sumTokens({ owners: [address], tokens: ["0x0000000000000000000000000000000000000000"] })
+  await fromApi.sumTokens({ owners: [address], tokens: ["0x0000000000000000000000000000000000000000"] })
+  dailyFees.addBalances(api.getBalances())
+  dailyFees.subtract(fromApi.getBalances())
   return {
     dailyFees,
-    dailyRevenue: dailyFees.clone(),
+    dailyRevenue: dailyFees,
   }
 }
 
