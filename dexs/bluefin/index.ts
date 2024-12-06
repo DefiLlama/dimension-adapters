adf925539073705bbd7b7af6d72263deca9b25f9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-06 16:37:03 +0100

    minor refactor

--- dexs/bluefin/index.ts
+++ dexs/bluefin/index.ts
@@ -1,43 +1,23 @@
-import { start } from "repl";
-import { BreakdownAdapter } from "../../adapters/types";
+import { BreakdownAdapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { httpGet } from "../../utils/fetchURL";
-interface Volume {
-  totalVolume: string | undefined;
-  dailyVolume: string | undefined;
-  timestamp: number;
-}
 
-const computeVolume = async (
-  startTime: number,
-  endTime: number
-): Promise<Volume> => {
-  const dailyVolume = (
-    await httpGet(
-      `https://dapi.api.sui-prod.bluefin.io/marketData/volume?startTime=${startTime}&&endTime=${endTime}`
-    )
-  ).volume;
+const fetch = async ({ startTimestamp, endTimestamp }: FetchOptions) => {
+  const { volume, } = (await httpGet(`https://dapi.api.sui-prod.bluefin.io/marketData/volume?startTime=${startTimestamp * 1000}&&endTime=${endTimestamp * 1000}`))
 
   return {
-    totalVolume: undefined,
-    dailyVolume: dailyVolume ? `${dailyVolume}` : undefined,
-    timestamp: endTime/1000,
+    dailyVolume: volume,
   };
 };
 
-const fetchSUI = async (timeStamp: number) => {
-  return await computeVolume((timeStamp - 86399) * 1000, timeStamp * 1000); //endpoint expects time in ms
-};
-
-const startTime = 1695600000; // 25th September when SUI trading starts
 
 const adapter: BreakdownAdapter = {
+  version: 2,
   breakdown: {
     derivatives: {
       [CHAIN.SUI]: {
-        fetch: fetchSUI,
-        start: startTime,
-        runAtCurrTime: false,
+        fetch,
+        start: 1695600000,
       },
     },
   },
