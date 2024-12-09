91282a0e18d301cd648c54f2b7fb0865832e1624
0xngmi <80541789+0xngmi@users.noreply.github.com>
2024-12-09 09:35:50 +0000

    Update lyra-v2.ts

--- fees/lyra-v2.ts
+++ fees/lyra-v2.ts
@@ -1,28 +1,21 @@
 import { Adapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
 import axios from "axios";
 
-async function getLyraV2Fees(timestamp: number) {
-  const cleanTimestamp = getUniqStartOfTodayTimestamp(
-    new Date(timestamp * 1000)
-  );
-  const cleanTimestampMs = cleanTimestamp * 1000;
-
-  const data = await axios.get(`https://api.lyra.finance/public/statistics?instrument_name=ALL&end_time=${cleanTimestampMs}`, {
+async function getLyraV2Fees({endTimestamp}) {
+  const data = await axios.get(`https://api.lyra.finance/public/statistics?instrument_name=ALL&end_time=${endTimestamp*1e3}`, {
     headers: {
       'Content-Type': 'application/json',
     },
   });
 
-
   return {
-    timestamp,
     dailyFees: data.data.result.daily_fees.toString(),
   };
 }
 
 const adapter: Adapter = {
+  version: 2,
   adapter: {
     [CHAIN.LYRA]: {
       fetch: getLyraV2Fees,
