9868a862a3d551a720577e1560444d9f22c067d5
Bogdan Crisan <bogdan@crbl.studio>
2024-12-09 18:43:38 +0100

    Rework the emojicoin adapter and Aptos helper

--- fees/emojicoin.ts
+++ fees/emojicoin.ts
@@ -1,56 +1,15 @@
-import {SimpleAdapter} from "../adapters/types";
+import {FetchOptions} from "../adapters/types";
 import {CHAIN} from "../helpers/chains";
-import {GraphQLClient} from "graphql-request";
-import {view} from "../helpers/aptops";
-import {getPrices} from "../utils/prices";
+import {getVersionFromTimestamp, octasToApt, view} from "../helpers/aptops";
 
-const VERSION_GROUPING = BigInt(1000000)
+// The emojicoin package address.
+const MAINNET_PACKAGE_ADDRESS = "0xface729284ae5729100b3a9ad7f7cc025ea09739cd6e7252aff0beb53619cafe";
 
-// If I can get this timestampQuery to work... everything will work seamlessly
-async function timestampToVersion(timestamp: number, start_version: bigint = BigInt(1962588495), end_version: bigint = BigInt(1962588495) + VERSION_GROUPING): Promise<string> {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        let closestTransactions = await findClosestTransaction(timestamp, start_version, end_version)
-        if (closestTransactions.length < 1) {
-            start_version += VERSION_GROUPING
-            end_version += VERSION_GROUPING
-        } else {
-            return closestTransactions[0].version
-        }
-    }
-}
+// The emojicoin module.
+const MODULE = "emojicoin_dot_fun";
 
-const graphQLClient = new GraphQLClient("https://api.mainnet.aptoslabs.com/v1/graphql")
-const timestampQuery = `query TimestampToVersion($timestamp: timestamp, $start_version: bigint, $end_version: bigint) {
-block_metadata_transactions(
-  where: {timestamp: {_gte: $timestamp }, version: {_gte: $start_version, _lte: $end_version}}
-  limit: 1
-  order_by: {version: asc}
-) {
-    timestamp
-    version
-  }
-}`;
-
-async function findClosestTransaction(timestamp: number, start_version: bigint, end_version: bigint): Promise<{
-    version: string
-}[]> {
-    let date = new Date(timestamp * 1000).toISOString()
-
-    const results = await graphQLClient.request(
-        timestampQuery,
-        {
-            timestamp: date,
-            start_version: start_version.toString(),
-            end_version: end_version.toString(),
-        }
-    )
-
-    return results.block_metadata_transactions as { version: string }[]
-}
-
-
-async function registryView(version?: bigint): Promise<{
+// Return type of the `registry_view` view function.
+type RegistryView = {
     cumulative_chat_messages: { value: string },
     cumulative_integrator_fees: { value: string },
     cumulative_quote_volume: { value: string },
@@ -63,65 +22,60 @@ async function registryView(version?: bigint): Promise<{
     registry_address: string,
     total_quote_locked: { value: string },
     total_value_locked: { value: string }
-}> {
-    const [result] = await view<[{
-        cumulative_chat_messages: { value: string },
-        cumulative_integrator_fees: { value: string },
-        cumulative_quote_volume: { value: string },
-        cumulative_swaps: { value: string },
-        fully_diluted_value: { value: string },
-        last_bump_time: string,
-        market_cap: { value: string },
-        n_markets: string,
-        nonce: { value: string },
-        registry_address: string,
-        total_quote_locked: { value: string },
-        total_value_locked: { value: string }
-    }]>(
-        "0xface729284ae5729100b3a9ad7f7cc025ea09739cd6e7252aff0beb53619cafe::emojicoin_dot_fun::registry_view",
-        [],
-        [],
-        version);
+};
+
+// Query the `registry_view` view function at the given version.
+async function registryView(version?: number) {
+    const viewFunction = `${MAINNET_PACKAGE_ADDRESS}::${MODULE}::registry_view`;
+    const [result] =
+        await view<[RegistryView]>(viewFunction, [], [], version);
     return result
 }
 
-const ONE_DAY: number = (24 * 60 * 60 * 1000);
+const fetch = async (options: FetchOptions) => {
+    // Convert timestamps to versions.
+    const startVersion = await getVersionFromTimestamp(new Date(options.startTimestamp * 1000));
+    const endVersion = await getVersionFromTimestamp(new Date(options.endTimestamp * 1000));
 
-const fetch = async (timestamp: number) => {
-    // Find the timestamp
-    const date = new Date(timestamp * 1000)
-    const closestToDate = await timestampToVersion(timestamp)
-    const previousDayTimestamp = new Date(date.getTime() - ONE_DAY).getTime() / 1000
-    const closestToPreviousDate = await timestampToVersion(previousDayTimestamp)
+    // Get the view results at the start and end bounds.
+    const viewStart = await registryView(startVersion);
+    const viewEnd = await registryView(endVersion);
 
-    const yesterdayRegistry = await registryView(BigInt(closestToPreviousDate))
-    const todayRegistry = await registryView(BigInt(closestToDate))
+    // Extract fees and volume from `registry_view` results.
+    const feesEnd = BigInt(viewEnd.cumulative_integrator_fees.value);
+    const feesStart = BigInt(viewStart.cumulative_integrator_fees.value);
+    const volumeEnd = BigInt(viewEnd.cumulative_quote_volume.value);
+    const volumeStart = BigInt(viewStart.cumulative_quote_volume.value);
 
-    const prices = await getPrices(["coingecko:aptos"], timestamp)
-    const apt_price = prices["coingecko:aptos"].price;
+    // Calculate daily and total values
 
-    function parseFees(fees: bigint) {
-        return parseInt((BigInt(fees) / BigInt(10 ** 8)).toString(10)) * apt_price
-    }
+    const dailyFees = options.createBalances();
+    dailyFees.addCGToken('aptos', octasToApt(feesEnd - feesStart));
 
-    const totalFees = parseFees(BigInt(todayRegistry.cumulative_integrator_fees.value));
-    const dailyFees = parseFees(BigInt(todayRegistry.cumulative_integrator_fees.value) - BigInt(yesterdayRegistry.cumulative_integrator_fees.value));
+    const totalFees = options.createBalances();
+    totalFees.addCGToken('aptos', octasToApt(feesEnd));
+
+    const dailyVolume = options.createBalances();
+    dailyVolume.addCGToken('aptos', octasToApt(volumeEnd - volumeStart));
+
+    const totalVolume = options.createBalances();
+    totalVolume.addCGToken('aptos', octasToApt(volumeEnd));
 
     return {
-        totalFees: `${totalFees}`,
-        dailyFees: `${dailyFees}`,
-        // TODO: revenue
-        timestamp,
+        dailyFees,
+        totalFees,
+        dailyRevenue: dailyFees,
+        dailyVolume,
+        totalVolume,
     };
 };
 
-const adapter: SimpleAdapter = {
+export default {
+    version: 2,
     adapter: {
         [CHAIN.APTOS]: {
             fetch,
-            start: '2023-04-03',
+            start: '2024-11-19',
         },
-    },
+    }
 };
-
-export default adapter;
--- helpers/aptops.ts
+++ helpers/aptops.ts
@@ -1,6 +1,71 @@
 import {httpGet, httpPost} from "../utils/fetchURL";
+import {GraphQLClient} from "graphql-request";
 
 export const APTOS_PRC = 'https://aptos-mainnet.pontem.network';
+
+// Number of decimals for the APT token.
+const APT_DECIMALS = 8;
+
+// Number to multiply and APT vaule to get the amount in Octas.
+const APT_TO_OCTAS_MUTLIPLIER = Math.pow(10, APT_DECIMALS);
+
+// Takes an amount in Octas as input and returns the same amount in APT.
+const octasToApt = (octas: number | bigint) => {
+    if (typeof octas === "number") {
+        return octas / APT_TO_OCTAS_MUTLIPLIER;
+    } else {
+        return Number(octas) / APT_TO_OCTAS_MUTLIPLIER;
+    }
+}
+
+const graphQLClient = new GraphQLClient("https://api.mainnet.aptoslabs.com/v1/graphql");
+
+// Query to get the latest block.
+const latestBlockQuery = `query LatestBlock {
+  block_metadata_transactions(order_by: {version: desc}, limit: 1) {
+    block_height
+  }
+}`;
+
+// Query to get a block.
+const blockQuery = `query Block($block: bigint) {
+  block_metadata_transactions(limit: 1, where: {block_height: {_eq: $block}}) {
+    timestamp
+    version
+  }
+}`;
+
+// Query to get a block range.
+const blockRangeQuery = `query Block($firstBlock: bigint, $limit: Int) {
+  block_metadata_transactions(limit: $limit, where: {block_height: {_gte: $firstBlock}}, order_by: {block_height: asc}) {
+    timestamp
+    version
+  }
+}`;
+
+// Given a timestamp, returns the transaction version that is closest to that timestamp.
+const getVersionFromTimestamp = async (timestamp: Date) => {
+    let left = 0;
+    let right = await graphQLClient.request(latestBlockQuery).then(r => Number(r.block_metadata_transactions[0].block_height));
+    let middle = Math.round(right / 2);
+    while (left + 100 < right) {
+        const middleBlock = await graphQLClient.request(blockQuery, {block: middle}).then(r => r.block_metadata_transactions[0]);
+        const middleBlockDate = new Date(middleBlock.timestamp);
+        if (middleBlockDate === timestamp) {
+            return Number(middleBlock.version);
+        }
+        if (timestamp < middleBlockDate) {
+            right = middle;
+        } else {
+            left = middle + 1;
+        }
+        middle = Math.round((left + right) / 2);
+    }
+    const blocks: {timestamp: string, version: string}[] = await graphQLClient.request(blockRangeQuery, {firstBlock: left, limit: right - left}).then(r => r.block_metadata_transactions);
+    const mappedBlocks = blocks.map((e) => ({version: Number(e.version), delta: Math.abs(timestamp.getTime() - new Date(e.timestamp).getTime())}));
+    mappedBlocks.sort((a, b) => a.delta - b.delta);
+    return mappedBlocks[0].version;
+}
 
 const getResources = async (account: string): Promise<any[]> => {
     const data: any = []
@@ -25,5 +90,7 @@ async function view<T extends any[]>(functionStr: string, type_arguments: string
 
 export {
     getResources,
+    getVersionFromTimestamp,
+    octasToApt,
     view
 }
