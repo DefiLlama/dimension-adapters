54ade50c62179bf0274d2fdf46e7de68b7690190
Igor Shelkovenkov <is@topiet.de>
2024-12-10 17:35:51 +0100

    migrate jav to base

--- dexs/javsphere/index.ts
+++ dexs/javsphere/index.ts
@@ -1,40 +1,20 @@
-
 import fetchURL from "../../utils/fetchURL";
 import type { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 type DexData = {
-    countTotal: number,
     volumeTotal: number,
-    feeTotal: number,
-    tradesBuy: number,
-    volumeBuy: number,
-    volumeBuyFees: number,
-    countSell: number,
-    volumeSell: number,
-    volumeSellFees: number,
     volume24: number,
-    fee24: number
 
-};
-
-type StakingData = {
-    totalFees: number,
-    dailyFees: number,
 };
 
 const methodology = {
-    Fees: "User pays 0.1% fees on each trade. User pays 10% fee of rewards in dusdstaking.",
-    Volume: "User buys and sell RWA tokens.",
+    Volume: "User buys and sell JAV token on CEXes and DEXes.",
 }
 
 const fetch = async (timestamp: number) => {
-    const stats: DexData = (await fetchURL(`https://aws-api.javlis.com/api/dtoken/stats`)).data;
-    const statsStaking: StakingData = (await fetchURL(`https://aws-api.javlis.com/api/dusdStaking/stats`)).data;
+    const stats: DexData = (await fetchURL(`https://aws-api.javlis.com/api/javsphere/coin-volume`)).data;
     return {
-        totalVolume: `${stats.volumeTotal}`,
-        totalFees: `${stats.feeTotal + statsStaking.totalFees}`,
-        dailyFees: `${stats.fee24 + statsStaking.dailyFees}`,
         dailyVolume: `${stats.volume24}`,
         timestamp,
     };
@@ -42,9 +22,9 @@ const fetch = async (timestamp: number) => {
 
 const adapter: SimpleAdapter = {
     adapter: {
-        [CHAIN.DEFICHAIN]: {
+        [CHAIN.BASE]: {
             fetch,
-                        runAtCurrTime: true,
+            runAtCurrTime: true,
             meta: {
                 methodology
             },
