4db49438fe24e8f0b135894815f5da68ace7bb00
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-11 11:06:59 +0100

    update existing adapter

--- protocols/thruster-v3.ts
+++ protocols/thruster-v3.ts
@@ -3,5 +3,16 @@ import { uniV3Exports } from "../helpers/uniswap";
 export default uniV3Exports({ 
   blast: {
     factory: '0x71b08f13B3c3aF35aAdEb3949AFEb1ded1016127',
+    customLogic: async ({ dailyVolume, dailyFees, fetchOptions }: any) => {
+      // Add fees from fee distributor
+      const logs = await fetchOptions.getLogs({
+        target: "0xaafa3db42ea9c114c36a2a033e04c8bc0813c65c",
+        eventAbi: "event CheckpointToken(uint256 time, uint256 tokens)",
+      });
+      logs.map((e: any) => {
+        dailyFees.add('0x4300000000000000000000000000000000000003', e.tokens)
+      });
+      return { dailyVolume, dailyFees }
+    }
   }
 })
