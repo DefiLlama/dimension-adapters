2353051ccd5e2cb2f6a8d92cbcbdf838de9108b6
jooleseth <jooleseth@gmail.com>
2024-12-11 15:45:47 +0800

    return string for fees directly

--- fees/thruster/index.ts
+++ fees/thruster/index.ts
@@ -9,15 +9,15 @@ export default {
   adapter: {
     [CHAIN.BLAST]: {
       fetch: (async ({ getLogs, createBalances }) => {
-        const dailyFees = createBalances();
+        let dailyFees = BigInt(0);
         const logs = await getLogs({
           target: "0xaafa3db42ea9c114c36a2a033e04c8bc0813c65c",
           eventAbi: "event CheckpointToken(uint256 time, uint256 tokens)",
         });
         logs.map((e: any) => {
-          dailyFees.add("USDB", e.tokens);
+          dailyFees += e.tokens / BigInt(1e18); // assume USDB price is $1
         });
-        return { dailyFees };
+        return { dailyFees: dailyFees.toString() };
       }) as FetchV2,
       start: 9220025,
       meta: {
