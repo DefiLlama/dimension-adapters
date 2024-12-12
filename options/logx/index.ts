d5e09a9b5a0ba61ff0ac44c4cea0a8bc26b93b7a
Arpit Singh <arpit140940@gmail.com>
2024-12-12 17:49:04 +0530

    logx options volume adapter

---
+++ options/logx/index.ts
@@ -0,0 +1,36 @@
+import fetchURL from "../../utils/fetchURL";
+import { FetchResult, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+
+
+const URLEndpoint = "https://mainnetapiserver.logx.network/api/v1/stats/defillama/options?endTime=";
+const startTimestamp = 1733961600; // 12.12.2024
+
+interface IAPIResponse {
+  last24HourVolume: string;
+  totalVolume: string;
+}
+const fetch = async (timestamp: number): Promise<FetchResult> => {
+  const { last24HourVolume, totalVolume }: IAPIResponse = (
+    await fetchURL(`${URLEndpoint}${timestamp}`)
+  );
+
+  return {
+    totalPremiumVolume: totalVolume,
+    dailyPremiumVolume: last24HourVolume,
+    timestamp: timestamp,
+    dailyNotionalVolume: 0,
+    totalNotionalVolume: 0,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.LOGX]: {
+      fetch,
+      start: startTimestamp,
+    },
+  }
+};
+
+export default adapter;
