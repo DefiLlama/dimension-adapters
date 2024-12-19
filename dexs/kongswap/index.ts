1eedc7cf292398c8908d8bc0a3e81daad6c1e22d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-19 15:56:38 +0100

    Update index.ts

--- dexs/kongswap/index.ts
+++ dexs/kongswap/index.ts
@@ -1,15 +1,11 @@
 import { Adapter, FetchResultVolume } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraph/utils";
 import fetchURL from "../../utils/fetchURL";
 
 const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
   const response = await fetchURL('https://api.kongswap.io/api/defillama/total_volume');
-  const { '24h_volume': dailyVolume } = response;
-  
-  timestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
   return {
-    dailyVolume, // Assign correctly fetched value
+    dailyVolume: response['24h_volume'],
     timestamp
   }
 };
@@ -18,7 +14,7 @@ const adapter: Adapter = {
   adapter: {
     [CHAIN.ICP]: {
       fetch: fetch,
-      runAtCurrTime: false,
+      runAtCurrTime: true,
       start: '2024-11-01',
     },
   }
