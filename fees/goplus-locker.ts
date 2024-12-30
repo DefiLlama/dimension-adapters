eac7a14b3b09d35c5bbe3e82022b26f56367900f
Mr-Lucky <jeff-hall@foxmail.com>
2024-12-30 11:55:15 +0800

    fix conflict

--- fees/goplus-locker.ts
+++ fees/goplus-locker.ts
@@ -1,30 +1,38 @@
 import { Adapter, FetchOptions, } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { addTokensReceived } from "../helpers/token";
 import ADDRESSES from '../helpers/coreAssets.json';
+import { queryDune } from "../helpers/dune";
+import moment from "moment";
 
-const CHAIN_CONFIG = {
-  [CHAIN.ETHEREUM]: { start: 20790869 },
-  [CHAIN.BSC]: { start: 42387186 },
-  [CHAIN.BASE]: { start: 20014325 },
-  [CHAIN.ARBITRUM]: { start: 279127453 },
-  // [CHAIN.GRAVITY]: { start: 23719062 },
-  // [CHAIN.MORPH]: { start: 1125634 }
-}
 
 const fetch: any = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
 
+  let feeTo = "0x521faacdfa097ad35a32387727e468f7fd032fd6";
+  
   // add native fee
-  let feeTo = "0x521faAcDFA097ad35a32387727e468F7fD032fD6";
-
   await options.api.sumTokens({ owner: feeTo, token: ADDRESSES.null });
   await options.fromApi.sumTokens({ owner: feeTo, token: ADDRESSES.null });
   dailyFees.addBalances(options.api.getBalancesV2());
   dailyFees.subtract(options.fromApi.getBalancesV2());
 
-  await addTokensReceived({ balances: dailyFees, target: feeTo, options, })
+  let startDate = new Date();
+  startDate.setTime(options.startTimestamp * 1000);
+  let start = moment(startDate).format("YYYY-MM-DD hh:mm:ss");
+  let endDate = new Date();
+  endDate.setTime(options.endTimestamp * 1000);
+  let end = moment(endDate).format("YYYY-MM-DD hh:mm:ss");
 
+  const values = await queryDune('4444430', {
+    chain: options.chain == 'bsc' ? 'bnb' : options.chain,
+    start,
+    end,
+    to: feeTo
+  })
+
+  values.forEach((e: { contract_address: string; amount_raw: string; }) => {
+    dailyFees.add(e.contract_address, e.amount_raw);
+  });
 
   return { dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, }
 }
@@ -36,8 +44,8 @@ const adapter: Adapter = {
     [CHAIN.BSC]: { fetch, start: '2024-09-19', },
     [CHAIN.BASE]: { fetch, start: '2024-09-20', },
     [CHAIN.ARBITRUM]: { fetch, start: '2024-11-28', },
-    [CHAIN.GRAVITY]: { fetch, start: '2024-12-11', },
-    [CHAIN.MORPH]: { fetch, start: '2024-12-11', },
+    // [CHAIN.GRAVITY]: { fetch, start: '2024-12-11', },
+    // [CHAIN.MORPH]: { fetch, start: '2024-12-11', },
   },
 
 }
