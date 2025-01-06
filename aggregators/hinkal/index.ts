e895d69d9e78ee0cb544dc082e4db41b3860bb72
Dav0808 <d.khantadze@gmail.com>
2025-01-06 18:16:42 +0400

    blast-remove

--- aggregators/hinkal/index.ts
+++ aggregators/hinkal/index.ts
@@ -98,20 +98,6 @@ const fetchOPTIMISM = async (options: FetchOptions) => {
   };
 };
 
-const fetchBLAST = async (options: FetchOptions) => {
-  const timestampStart = options.startTimestamp;
-  const timestampEnd = options.endTimestamp;
-  const urlVolume = `https://blast.server.hinkal.pro/totalVolume/${timestampStart}/${timestampEnd}/81457`;
-
-  const responseVolume = await httpGet(urlVolume);
-  const dataTotal = responseVolume;
-  const dailyVolume = dataTotal.internal_volume + dataTotal.external_volume;
-  return {
-    timestamp: options.startTimestamp,
-    dailyVolume,
-  };
-};
-
 const adapter: Adapter = {
   version: 2,
   adapter: {
@@ -125,7 +111,6 @@ const adapter: Adapter = {
     [CHAIN.BSC]: { fetch: fetchBNB, start: '2023-11-08' },
     [CHAIN.AVAX]: { fetch: fetchAVALANCHE, start: '2023-11-08' },
     [CHAIN.OPTIMISM]: { fetch: fetchOPTIMISM, start: '2023-11-07' },
-    [CHAIN.BLAST]: { fetch: fetchBLAST, start: '2024-05-23' },
   },
 };
 
