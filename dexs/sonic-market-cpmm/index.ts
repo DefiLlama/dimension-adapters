6089367ae627372e71dff3a268d6fb42b61f1278
0xgnek <0xgnek@gmail.com>
2025-01-10 09:11:10 +0000

    fix query

--- dexs/sonic-market-cpmm/index.ts
+++ dexs/sonic-market-cpmm/index.ts
@@ -3,9 +3,4 @@ import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 
 export default univ2Adapter({
     [CHAIN.SONIC]: "https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/cpmm-v2-subgraph-sonic-mainnet/api"
-}, {
-    factoriesName: "sonicmarketFactories",
-    totalVolume: "totalVolumeUSD",
-    dayData: "dailySonicmarketStatistic",
-    dailyVolume: "dailyVolumeUSD"
-});
+}, {});
