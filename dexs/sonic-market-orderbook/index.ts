05f5370bcbf66fe1491251563de0859a64acef5c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-10 08:18:12 +0100

    minor fix

--- dexs/sonic-market-orderbook/index.ts
+++ dexs/sonic-market-orderbook/index.ts
@@ -1,73 +1,38 @@
-import type {SimpleAdapter} from "../../adapters/types";
-import {FetchOptions, FetchResultV2, FetchV2} from "../../adapters/types";
-import {getTimestampAtStartOfDayUTC} from "../../utils/date"
-import {CHAIN} from "../../helpers/chains";
-import {CONTRACT_INFOS} from "./const";
+import type { SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, FetchResultV2, FetchV2 } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+const abi = {
+  getBookKey: "function getBookKey(uint192 id) view returns ((address base, uint64 unitSize, address quote, uint24 makerPolicy, address hooks, uint24 takerPolicy))",
+  take: 'event Take(uint192 indexed bookId, address indexed user, int24 tick, uint64 unit)'
+}
+const bookManagerContract = {
+  [CHAIN.SONIC]: '0xD4aD5Ed9E1436904624b6dB8B1BE31f36317C636',
+}
 
-const fetch: FetchV2 = async (options: FetchOptions): Promise<FetchResultV2> => {
-    const dayTimestamp = getTimestampAtStartOfDayUTC(options.toTimestamp)
+const fetch: FetchV2 = async ({ getLogs, createBalances, chain, api }: FetchOptions): Promise<FetchResultV2> => {
+  const dailyVolume = createBalances()
 
-    const dailyVolume = options.createBalances()
+  const target = bookManagerContract[chain]
+  const takeEvents = await getLogs({ target, eventAbi: abi.take, })
 
-    const bookInfos: any = {}
-    const CONTRACT_INFO = CONTRACT_INFOS[options.chain];
+  const bookKeys = takeEvents.map(i => i.bookId.toString())
+  const tokens = await api.multiCall({ abi: abi.getBookKey, calls: bookKeys, target })
+  takeEvents.forEach((i, idx) => dailyVolume.add(tokens[idx].quote, i.unit))
 
-    const openEvents = await options.getLogs({
-        target: CONTRACT_INFO.bookManagerContract.address,
-        fromBlock: CONTRACT_INFO.bookManagerContract.fromBlock,
-        eventAbi: CONTRACT_INFO.bookManagerContract.abi.openEvent,
-        onlyArgs: true,
-    })
-    for (const open of openEvents) {
-        const bookId = open.id;
-        bookInfos[bookId] = {
-            quote: open.quote,
-            unitSize: open.unitSize,
-        }
-    }
-
-    const dailyTakeEvents = await options.getLogs({
-        target: CONTRACT_INFO.bookManagerContract.address,
-        eventAbi: CONTRACT_INFO.bookManagerContract.abi.takeEvent,
-        onlyArgs: true,
-    })
-    const takeEvents = [
-        {
-            events: dailyTakeEvents,
-            volume: dailyVolume,
-        },
-    ]
-    for (const {events, volume} of takeEvents) {
-        for (const take of events) {
-            const bookId = take.bookId;
-            const bookInfo = bookInfos[bookId];
-            if (!bookInfo) {
-                continue;
-            }
-            const quote = bookInfo.quote;
-            const quoteVolume = take.unit * bookInfo.unitSize;
-            volume.add(quote, quoteVolume);
-        }
-    }
-
-    return {
-        dailyVolume: dailyVolume,
-        timestamp: dayTimestamp,
-    };
+  return { dailyVolume, };
 };
 
 const adapter: SimpleAdapter = {
-    version: 2,
-    adapter: {
-        [CHAIN.SONIC]: {
-            fetch: fetch,
-            // runAtCurrTime: true,
-            start: '2024-12-09',
-            meta: {
-                methodology: 'Volume is calculated by summing the quote token volume of all trades on the protocol.',
-            }
-        },
-    }
+  version: 2,
+  adapter: {
+    [CHAIN.SONIC]: {
+      fetch: fetch,
+      start: '2024-12-09',
+      meta: {
+        methodology: 'Volume is calculated by summing the quote token volume of all trades on the protocol.',
+      }
+    },
+  }
 };
 
 export default adapter;
