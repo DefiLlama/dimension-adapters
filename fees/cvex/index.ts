3a2773f07a8d2ab705c125d0f1a1e96c84f63ce5
vladjito <95035799+vladjito@users.noreply.github.com>
2025-01-10 11:13:49 +0100

    Create index.ts

---
+++ fees/cvex/index.ts
@@ -0,0 +1,34 @@
+import type { SimpleAdapter } from '../../adapters/types'
+import { httpPost } from '../../utils/fetchURL';
+import {CHAIN} from "../../helpers/chains";
+
+const API_SERVICE_URL = 'https://api.cvex.trade/v1/statistics/fee'
+
+const api = (url: string, ts: any) =>
+  httpPost(url, { timestamp: ts },
+    { headers: { 'Content-Type': 'application/json', } }
+  ).then(res => {
+    if (res.error)
+      throw new Error(res.error.message)
+
+    return res
+  });
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.ARBITRUM]: {
+      start: 1736328600,
+      fetch: async (ts) => {
+        const data = await api(API_SERVICE_URL, ts)
+
+        return {
+          timestamp: ts,
+          dailyFees: data.daily_fee,
+          totalFees: data.total_fee,
+        }
+      }
+    }
+  }
+};
+
+export default adapter;
