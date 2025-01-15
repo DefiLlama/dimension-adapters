de0c8569e90e944ca197012c00f4f87c2c6daa2c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-15 12:33:58 +0100

    rever renaming bitlayer -> btr

--- helpers/chains.ts
+++ helpers/chains.ts
@@ -156,7 +156,7 @@ export enum CHAIN {
   SVM = "svm",
   ASTRZK = "astrzk",
   LYRA = "lyra",
-  BITLAYER = "btr",
+  BITLAYER = "bitlayer",
   XLAYER = "xlayer",
   MERLIN = "merlin",
   CHILIZ = "chiliz",
