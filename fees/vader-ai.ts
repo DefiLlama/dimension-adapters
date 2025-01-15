9e4e4b4e1dfef704cf2936a3ab4e7c2b307ead34
0xgnek <0xgnek@gmail.com>
2025-01-15 15:21:46 +0000

    fix vader id

--- fees/vader-ai.ts
+++ fees/vader-ai.ts
@@ -8,6 +8,7 @@ const contract: any = {
     '0x8bE2c661b35161A138A35C84F77895c4cc23900D',
   ]
 }
+const varder = '0x731814e491571A2e9eE3c5b1F7f3b962eE8f4870';
 
 const fetchFees = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
@@ -15,6 +16,14 @@ const fetchFees = async (options: FetchOptions) => {
     options,
     targets: contract[options.chain],
     token: '0x0b3e328455c4059eeb9e3f84b5543f74e24e7e1b',
+    balances: dailyFees,
+  })
+
+   // fees when swap varder-ai
+  await addTokensReceived({
+    options,
+    target: varder,
+    token: varder,
     balances: dailyFees,
   })
   return {
