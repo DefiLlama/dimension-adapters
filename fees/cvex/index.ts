70dd24f53097e503d703ccb1856952511c2578dd
vladjito <95035799+vladjito@users.noreply.github.com>
2025-01-23 16:29:05 +0100

    Update volume and fees CVEX (#2345)
    * Update index.ts
    
    * Update index.ts
    
    * minor refactor
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- fees/cvex/index.ts
+++ fees/cvex/index.ts
@@ -4,28 +4,22 @@ import {CHAIN} from "../../helpers/chains";
 
 const API_SERVICE_URL = 'https://api.cvex.trade/v1/statistics/fee'
 
-const buildUrl = (baseUrl: string, params: Record<string, any>) => {
-  const query = new URLSearchParams(params).toString();
-  return `${baseUrl}?${query}`;
-};
-
-const api = (url: string, ts: any) => {
-  const fullUrl = buildUrl(url, { timestamp: ts });
-  return httpGet(fullUrl).then(res => {
-    if (res.error) throw new Error(res.error.message);
-    return res;
-  });
+const api = async (url: string) => {
+  const res = await httpGet(url);
+  if (res.error) throw new Error(res.error.message);
+  return res;
 };
 
 const adapter: SimpleAdapter = {
+  version: 2,
   adapter: {
     [CHAIN.ARBITRUM]: {
+      runAtCurrTime: true,
       start: 1736328600,
-      fetch: async (ts) => {
-        const data = await api(API_SERVICE_URL, ts)
+      fetch: async () => {
+        const data = await api(API_SERVICE_URL)
 
         return {
-          timestamp: ts,
           dailyFees: data.daily_fee,
           totalFees: data.total_fee,
         }
