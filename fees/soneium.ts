977d1962e02724f46c950fc72d876d2f75e4c29a
0xgnek <0xgnek@gmail.com>
2025-01-23 14:56:08 +0000

    add fees

---
+++ fees/soneium.ts
@@ -0,0 +1,22 @@
+import { CHAIN } from "../helpers/chains";
+import { Adapter, ProtocolType } from "../adapters/types";
+import { L2FeesFetcher } from "../helpers/ethereum-l2";
+
+const ethereumWallets = [
+  '0x4200000000000000000000000000000000000019',
+  '0x420000000000000000000000000000000000001A',
+  '0x4200000000000000000000000000000000000011'
+]
+
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.SONEIUM]: {
+      fetch: L2FeesFetcher({ ethereumWallets }),
+      start: '2020-08-29',
+    },
+  },
+  protocolType: ProtocolType.CHAIN
+}
+
+export default adapter;
