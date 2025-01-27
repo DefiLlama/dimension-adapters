4dbbc01bbce9b7a70bc066aa92fc49dd729c244a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-27 11:38:59 +0100

    refactor: remove unused URL constants in metastable musd adapter

--- dexs/metastable-musd/index.ts
+++ dexs/metastable-musd/index.ts
@@ -4,15 +4,13 @@ import { CHAIN } from "../../helpers/chains";
 
 const M_USD_VAULT_ID =
 	"0xb950819c5eba1bb5980f714f2a3b1d8738e3da58a4d9daf5fa21b6c2a7dd1e12";
-const URL = `https://aftermath.finance/api/metastable/${M_USD_VAULT_ID}/24hr-volume`;
 const M_ETH_VAULT_ID =
 	"0x2d6e81126336685a28ea0637109b570510f988bba2b589877c9b579d3cb8cad8";
-const URL2 = `https://aftermath.finance/api/metastable/${M_ETH_VAULT_ID}/24hr-volume`;
 
 
 const fetch = async (timestamp: number): Promise<FetchResult> => {
 	let dailyVolume = 0
-	const vaults = [M_USD_VAULT_ID, ]
+	const vaults = [M_USD_VAULT_ID, M_ETH_VAULT_ID, ]
 	for (const vault of vaults) {
 		const data = await fetchURL(`https://aftermath.finance/api/metastable/${vault}/24hr-volume`);
 		dailyVolume += data
