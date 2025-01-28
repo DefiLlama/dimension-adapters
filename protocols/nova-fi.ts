ca6b56be418f4e417f24ff76240fd54bde08bad0
0xgnek <0xgnek@gmail.com>
2025-01-28 10:48:13 +0000

    fix pair

--- protocols/nova-fi.ts
+++ protocols/nova-fi.ts
@@ -2,6 +2,6 @@ import { uniV2Exports } from "../helpers/uniswap";
 
 export default uniV2Exports({
   abstract: {
-    factory: '0x441b9333D1D1ccAd27f2755e69d24E60c9d8F9CF',
+    factory: '0xE1e98623082f662BCA1009a05382758f86F133b3',
   }
 })
