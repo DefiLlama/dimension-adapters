6a02331bf3fde70455aa64c440f8f94730885ba9
Rudy <lengxin5348@gmail.com>
2025-02-04 19:00:06 +0800

    add raydium perps info more

--- dexs/raydium-perps/index.ts
+++ dexs/raydium-perps/index.ts
@@ -1,11 +1,27 @@
 import { FetchResultFees, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import fetchURL from "../../utils/fetchURL";
 
+interface PerpInfo {
+  volume: {
+    '24h': number,
+    '7d': number,
+    '30d': number,
+  },
+  openInterest: {
+    long: number,
+    short: number,
+    all: number,
+  }
+}
+
 const graphs = async (timestamp: number): Promise<FetchResultVolume & FetchResultFees> => {
-  const info: { "volume24h": number, "volume7d": number, "volume30d": number, } = (await fetchURL('https://api-perps-v1.raydium.io/main/info')).data
+  const info: PerpInfo = (await fetchURL('https://api-perps-v1.raydium.io/main/info')).data
 
   return {
-    dailyVolume: info.volume24h,
+    dailyVolume: info.volume["24h"],
+    dailyShortOpenInterest: info.openInterest.short,
+    dailyLongOpenInterest: info.openInterest.long,
+    dailyOpenInterest: info.openInterest.all,
     timestamp,
   };
 };
