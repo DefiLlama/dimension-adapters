b2938e0a4408dcf9e299793f37bee6ae80f78068
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-05 10:37:27 +0100

    minro fix

--- fees/levvy-fi/index.ts
+++ fees/levvy-fi/index.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 const fetch: any = async (options: FetchOptions) => {
   const startDateTime = new Date(options.startTimestamp * 1000).toISOString();
   const { data } = await axios.post(
-    `https://8080-truthful-birthday-xc2vhr.us1.demeter.run//api/v1/nft/platform/stats?from=${startDateTime}&timeFrame=${24}`,
+    `https://8080-truthful-birthday-xc2vhr.us1.demeter.run/api/v1/nft/platform/stats?from=${startDateTime}&timeFrame=${24}`,
   );
 
   return {
