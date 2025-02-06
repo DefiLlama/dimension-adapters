a7953f85f3e3ad6f525e5610cf0e697752957523
donguks <donguk.seo@modori.io>
2025-02-06 11:37:26 +0900

    add dex/wavex

---
+++ dexs/wavex/index.ts
@@ -0,0 +1,161 @@
+import request, { gql } from "graphql-request";
+import { BreakdownAdapter, Fetch, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+
+const endpoints: { [key: string]: string } = {
+  [CHAIN.SONEIUM]: "https://wavex-indexer-serve-mainnet.up.railway.app/",
+};
+
+const historicalDataSwap = gql`
+  query get_volume($period: String!, $id: String!) {
+    volumeStats(where: { period: $period, id: $id }) {
+      swap
+    }
+  }
+`;
+
+const historicalDataDerivatives = gql`
+  query get_volume($period: String!, $id: String!) {
+    volumeStats(where: { period: $period, id: $id }) {
+      liquidation
+      margin
+    }
+  }
+`;
+const historicalOI = gql`
+  query get_trade_stats($period: String!, $id: String!) {
+    tradingStats(where: { period: $period, id: $id }) {
+      id
+      longOpenInterest
+      shortOpenInterest
+    }
+  }
+`;
+
+interface IGraphResponse {
+  volumeStats: Array<{
+    burn: string;
+    liquidation: string;
+    margin: string;
+    mint: string;
+    swap: string;
+  }>;
+}
+interface IGraphResponseOI {
+  tradingStats: Array<{
+    id: string;
+    longOpenInterest: string;
+    shortOpenInterest: string;
+  }>;
+}
+
+const getFetch =
+  (query: string) =>
+  (chain: string): Fetch =>
+  async (timestamp: number) => {
+    const dayTimestamp = getUniqStartOfTodayTimestamp(
+      new Date(timestamp * 1000)
+    );
+    const dailyData: IGraphResponse = await request(endpoints[chain], query, {
+      id:
+        chain === CHAIN.ARBITRUM
+          ? String(dayTimestamp)
+          : String(dayTimestamp) + ":daily",
+      period: "daily",
+    });
+    const totalData: IGraphResponse = await request(endpoints[chain], query, {
+      id: "total",
+      period: "total",
+    });
+    let dailyOpenInterest = 0;
+    let dailyLongOpenInterest = 0;
+    let dailyShortOpenInterest = 0;
+
+    if (query === historicalDataDerivatives) {
+      const tradingStats: IGraphResponseOI = await request(
+        endpoints[chain],
+        historicalOI,
+        {
+          id:
+            chain === CHAIN.ARBITRUM
+              ? String(dayTimestamp)
+              : String(dayTimestamp) + ":daily",
+          period: "daily",
+        }
+      );
+      dailyOpenInterest =
+        Number(tradingStats.tradingStats[0].longOpenInterest) +
+        Number(tradingStats.tradingStats[0].shortOpenInterest);
+      dailyLongOpenInterest = Number(
+        tradingStats.tradingStats[0].longOpenInterest
+      );
+      dailyShortOpenInterest = Number(
+        tradingStats.tradingStats[0].shortOpenInterest
+      );
+    }
+
+    return {
+      timestamp: dayTimestamp,
+      dailyLongOpenInterest: dailyLongOpenInterest
+        ? String(dailyLongOpenInterest * 10 ** -30)
+        : undefined,
+      dailyShortOpenInterest: dailyShortOpenInterest
+        ? String(dailyShortOpenInterest * 10 ** -30)
+        : undefined,
+      dailyOpenInterest: dailyOpenInterest
+        ? String(dailyOpenInterest * 10 ** -30)
+        : undefined,
+      dailyVolume:
+        dailyData.volumeStats.length == 1
+          ? String(
+              Number(
+                Object.values(dailyData.volumeStats[0]).reduce((sum, element) =>
+                  String(Number(sum) + Number(element))
+                )
+              ) *
+                10 ** -30
+            )
+          : undefined,
+      totalVolume:
+        totalData.volumeStats.length == 1
+          ? String(
+              Number(
+                Object.values(totalData.volumeStats[0]).reduce((sum, element) =>
+                  String(Number(sum) + Number(element))
+                )
+              ) *
+                10 ** -30
+            )
+          : undefined,
+    };
+  };
+
+const startTimestamps: { [chain: string]: number } = {
+  [CHAIN.ARBITRUM]: 1630368000,
+  [CHAIN.AVAX]: 1640131200,
+};
+const adapter: BreakdownAdapter = {
+  breakdown: {
+    swap: Object.keys(endpoints).reduce((acc, chain) => {
+      return {
+        ...acc,
+        [chain]: {
+          fetch: getFetch(historicalDataSwap)(chain),
+          start: startTimestamps[chain],
+        },
+      };
+    }, {}),
+    derivatives: Object.keys(endpoints).reduce((acc, chain) => {
+      return {
+        ...acc,
+        [chain]: {
+          fetch: getFetch(historicalDataDerivatives)(chain),
+          start: startTimestamps[chain],
+        },
+      };
+    }, {}),
+  },
+};
+
+export default adapter;
