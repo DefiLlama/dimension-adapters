153768df0bbdd94aa72513bebbbb88ec521fe292
Piotr Smreczyński <69101062+Smrecz@users.noreply.github.com>
2025-02-10 12:51:02 +0100

    Improve underlying queries

--- aggregators/enso/index.ts
+++ aggregators/enso/index.ts
@@ -5,7 +5,7 @@ import { queryDune } from "../../helpers/dune";
 const chains: Record<string, { duneChain: string; start: string }> = {
   [CHAIN.ETHEREUM]: { duneChain: "ethereum", start: "2023-06-22" },
   [CHAIN.OPTIMISM]: { duneChain: "optimism", start: "2023-09-19" },
-  [CHAIN.BSC]: { duneChain: "bnb", start: "2023-09-20" },
+  [CHAIN.BSC]: { duneChain: "binance", start: "2023-09-20" },
   [CHAIN.POLYGON]: { duneChain: "polygon", start: "2023-09-05" },
   [CHAIN.BASE]: { duneChain: "base", start: "2023-12-24" },
   [CHAIN.ARBITRUM]: { duneChain: "arbitrum", start: "2023-09-11" },
@@ -25,7 +25,7 @@ const fetchVolume = (chain: string): FetchV2 => async ({ startTimestamp, endTime
   if (!chainData) throw new Error(`Dune query failed: ${JSON.stringify(data)}`);
 
   return {
-    dailyVolume: chainData.volume_24h,
+    dailyVolume: chainData.volume_timerange,
     totalVolume: chainData.total_volume,
     timestamp: endTimestamp,
   };
