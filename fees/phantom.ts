d30414f5d7fb4424b43ff49368ab85422d0bd6f3
0xgnek <0xgnek@gmail.com>
2025-02-12 17:30:28 +0000

    update phantom

--- fees/phantom.ts
+++ fees/phantom.ts
@@ -1,19 +1,19 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { queryDune } from "../helpers/dune";
+import { getSolanaReceived } from "../helpers/token";
 
 const fetch: any = async (options: FetchOptions) => {
-  const dailyFees = options.createBalances();
-  const value = (await queryDune("4313632", {
-    start: options.startTimestamp,
-    end: options.endTimestamp,
-  }));
-  value.forEach((v: any) => {
-    dailyFees.add(v.token_mint_address, v.total_balance_change);
-  });
-
-  return { dailyFees, dailyRevenue: dailyFees }
-
+    const dailyFees = await getSolanaReceived({ options, targets: [
+      '25mYnjJ2MXHZH6NvTTdA63JvjgRVcuiaj6MRiEQNs1Dq'
+      ,'9yj3zvLS3fDMqi1F8zhkaWfq8TZpZWHe6cz1Sgt7djXf',
+      '8psNvWTrdNTiVRNzAgsou9kETXNJm2SXZyaKuJraVRtf',
+      'CnmA6Zb8hLrG33AT4RTzKdGv1vKwRBKQQr8iNckvv8Yg',
+      '2rQZb9xqQGwoCMDkpabbzDB9wyPTjSPj9WNhJodTaRHm',
+      '9gnLg6NtVxaASvxtADLFKZ9s8yHft1jXb1Vu6gVKvh1J',
+      'wtpXRqKLdGc7vpReogsRugv6EFCw4HBHcxm8pFcR84a',
+      'D1NJy3Qq3RKBG29EDRj28ozbGwnhmM5yBUp8PonSYUnm',
+    ]})
+    return { dailyFees, dailyRevenue: dailyFees }
 }
 
 const adapter: SimpleAdapter = {
