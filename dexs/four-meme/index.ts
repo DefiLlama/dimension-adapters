0166f60aff7c5feb7f626df1b16c7362beb447bd
0xgnek <0xgnek@gmail.com>
2025-02-15 09:28:58 +0000

    update four meme

--- dexs/four-meme/index.ts
+++ dexs/four-meme/index.ts
@@ -2,18 +2,27 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains"
 
 const address = '0x5c952063c7fc8610ffdb798152d69f0b9550762b'
-const topics0 = '0x7db52723a3b2cdd6164364b3b766e65e540d7be48ffa89582956d8eaebe62942';
+const topics0_buy = '0x7db52723a3b2cdd6164364b3b766e65e540d7be48ffa89582956d8eaebe62942';
+const topics0_sell_1 = '0x0a5575b3648bae2210cee56bf33254cc1ddfbc7bf637c0af2ac18b14fb1bae19';
+
+
 
 const fetchVolume = async (options: FetchOptions) => {
-  const logs = await options.getLogs({
+  const buy_logs: any[] = await options.getLogs({
     target: address,
-    topics: [topics0],
+    topics: [topics0_buy],
   })
+
+  const sell_logs_1: any[] = await options.getLogs({
+    target: address,
+    topics: [topics0_sell_1],
+  })
+
   const dailyVolume = options.createBalances()
-  logs.forEach((log) => {
+  buy_logs.concat(sell_logs_1).forEach((log) => {
     const data = log.data.replace('0x', '');
     const amount = Number('0x' + data.slice(4 * 64, 5 * 64))
-    dailyVolume.addGasToken(amount)
+    if (amount/1e18 < 100) dailyVolume.addGasToken(amount)
   });
   return {
     dailyVolume: dailyVolume
