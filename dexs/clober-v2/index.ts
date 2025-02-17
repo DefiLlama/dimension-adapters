915d105a2e3ea58c72f67871d1eee1e472e79f9d
Tae Hwan Jung <nlkey2022@gmail.com>
2025-02-17 12:11:29 +0900

    feat: update clober volume tracker after launching new vault

--- dexs/clober-v2/index.ts
+++ dexs/clober-v2/index.ts
@@ -24,7 +24,7 @@ const fetch: FetchV2 = async ({ getLogs, createBalances, chain, api }: FetchOpti
 };
 
 const adapter: SimpleAdapter = {
-    version: 2,
+    version: 2.1,
     adapter: {
         [CHAIN.BASE]: {
             fetch,
