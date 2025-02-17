6b03790cbc7242ff23569cbaef9d225eaabaf46c
0xgnek <0xgnek@gmail.com>
2025-02-17 16:28:04 +0000

    fix cow swap

--- fees/cow-protocol.ts
+++ fees/cow-protocol.ts
@@ -3,23 +3,13 @@ import { CHAIN } from "../helpers/chains";
 import { Chain, } from "@defillama/sdk/build/general";
 import { queryDune } from "../helpers/dune";
 
-type TAddress = {
-  [l: string | Chain]: string;
-}
-const address: TAddress = {
-  [CHAIN.ETHEREUM]: '0x9008d19f58aabd9ed0d60971565aa8510560ab41',
-  [CHAIN.XDAI]: '0x9008d19f58aabd9ed0d60971565aa8510560ab41'
-}
-
-
 const fetch = (_: Chain) => {
   return async (options: FetchOptions) => {
     const dailyFees = options.createBalances();
     try {
-      const value = (await queryDune("3968762"));
-      const dateStr = new Date(options.endTimestamp * 1000).toISOString().split("T")[0];
-      const dayItem = value.find((item: any) => item.time.split(' ')[0] === dateStr);
-      dailyFees.addGasToken((dayItem?.total_revenue) * 1e18 || 0)
+      const value = (await queryDune("4736286", { start: options.startOfDay }));
+      const dayItem = value[0]
+      dailyFees.addGasToken((dayItem?.eth_value) * 1e18 || 0)
       return {
         dailyFees: dailyFees,
         dailyRevenue: dailyFees,
