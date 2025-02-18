9ff9a94ce7f8a68512f970821a44faf3eb17560b
alexweb30 <alexlweb30@gmail.com>
2025-02-17 23:07:35 -0800

    update daily volume

--- dexs/piperx/index.ts
+++ dexs/piperx/index.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient, gql } from 'graphql-request'
 import { CHAIN } from "../../helpers/chains";
 
-const PIPERX_GRAPHQL_URL = 'https://api.goldsky.com/api/public/project_clzxbl27v2ce101zr2s7sfo05/subgraphs/story-dex-swaps-mainnet/1.0.1/gn'
+const PIPERX_GRAPHQL_URL = 'https://api.goldsky.com/api/public/project_clzxbl27v2ce101zr2s7sfo05/subgraphs/story-dex-swaps-mainnet/1.0.3/gn'
 
 const graphQLClient = new GraphQLClient(PIPERX_GRAPHQL_URL)
 
@@ -12,14 +12,25 @@ const METRICS_QUERY = gql`
     }
   }
 `
-
+const DAILY_VOLUME_QUERY = gql`
+  query {
+        totalVolumeAggregates(
+          interval: "day"
+        ) {
+          timestamp
+          volumeUSD
+          volumeNative
+        }
+  }
+`
 async function volume(api) {
   const { dex } = await graphQLClient.request(METRICS_QUERY)
-  
+  const { totalVolumeAggregates } = await graphQLClient.request(DAILY_VOLUME_QUERY)
   // The value has 6 decimal places
   const totalVolumeUSD = Number(dex.totalVolumeUSD) / 1e6
-  
+  const dailyVolumeUSD = totalVolumeAggregates[0].volumeUSD / 1e6
   return {
+    dailyVolume: dailyVolumeUSD,
     totalVolume: totalVolumeUSD
   }
 }
