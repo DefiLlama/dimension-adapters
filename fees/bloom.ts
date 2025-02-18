1576015734612305dce56578baa66f49bb63a79c
0xgnek <0xgnek@gmail.com>
2025-02-18 13:39:03 +0000

    add bloom trading bot

---
+++ fees/bloom.ts
@@ -0,0 +1,34 @@
+import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { queryDune } from "../helpers/dune";
+
+const fetchFees = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances()
+  const res = await queryDune("4740596", {
+    start: options.startTimestamp,
+    end: options.endTimestamp,
+    chain: options.chain
+  });
+
+  const dayItem = res[0]
+  dailyFees.addUSDValue((dayItem?.fee_usd) || 0)
+
+  return {
+    dailyFees,
+    dailyRevenue: dailyFees,
+  }
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.BASE]: {
+      fetch: fetchFees,
+    },
+    [CHAIN.SOLANA]: {
+      fetch: fetchFees,
+    }
+  },
+}
+
+export default adapter;
