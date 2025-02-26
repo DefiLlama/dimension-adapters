7e47a1d9dcc5b3072f62b0821a47f1a03e66a55f
0xgnek <0xgnek@gmail.com>
2025-02-26 10:52:43 +0000

    fix bsc fees

--- fees/bsc.ts
+++ fees/bsc.ts
@@ -1,40 +1,26 @@
 import * as sdk from "@defillama/sdk";
 import { Adapter, ProtocolType } from "../adapters/types";
 import { BSC } from "../helpers/chains";
-import { request, } from "graphql-request";
 import type { ChainEndpoints, FetchOptions } from "../adapters/types"
 import { Chain } from '@defillama/sdk/build/general';
+import { fetchTransactionFees } from "../helpers/getChainFees";
 
 const endpoints = {
-  [BSC]:
-    sdk.graph.modifyEndpoint('3a3f5kp31kutZzjmQoE2NKBSr6Ady5rgxRxD2nygYcQo')
+  [BSC]: sdk.graph.modifyEndpoint('3a3f5kp31kutZzjmQoE2NKBSr6Ady5rgxRxD2nygYcQo')
 }
 
 
-const graphs = (graphUrls: ChainEndpoints) => {
-  return (chain: Chain) => {
-    return async ({ createBalances, getFromBlock, getToBlock, toTimestamp }: FetchOptions) => {
-      const dailyFees = createBalances()
+const graphs = (_t: ChainEndpoints) => {
+  return (_t: Chain) => {
+    return async (options: FetchOptions) => {
 
-      const graphQuery = `query txFees {
-        yesterday: fee(id: "1", block: { number: ${await getFromBlock()} }) {
-          totalFees
-        }
-        today: fee(id: "1", block: { number: ${await getToBlock()} }) {
-          totalFees
-        }
-      }`;
 
-      const graphRes = await request(graphUrls[chain], graphQuery);
-
-      const dailyFee = graphRes["today"]["totalFees"] - graphRes["yesterday"]["totalFees"]
-      console.log(dailyFee, graphRes)
-      dailyFees.addGasToken(dailyFee * 1e18)
+      const dailyFees = await fetchTransactionFees(options)
 
       return {
         dailyFees,
         // totalFees: finalTotalFee.toString(),
-        dailyRevenue: toTimestamp < 1638234000 ? 0: dailyFees.clone(0.1), // https://github.com/bnb-chain/BEPs/blob/master/BEP95.md
+        dailyRevenue: options.toTimestamp < 1638234000 ? 0: dailyFees.clone(0.1), // https://github.com/bnb-chain/BEPs/blob/master/BEP95.md
       };
     };
   };
