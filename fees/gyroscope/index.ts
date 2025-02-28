9a7a8a34e37a82689252192bac7bf2ac1b8cea5e
Lor3mipsvm <52577544+Lor3mipsvm@users.noreply.github.com>
2025-02-28 12:14:54 +0000

    Update index.ts for a fee split by chain

--- fees/gyroscope/index.ts
+++ fees/gyroscope/index.ts
@@ -1,18 +1,17 @@
 import { Adapter, FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { Chain } from "@defillama/sdk/build/general";
 import { queryDune } from "../helpers/dune";
 
-const fetch = (_: Chain) => {
+const fetch = () => {
   return async (options: FetchOptions) => {
     const dailyFees = options.createBalances();
     try {
-      const value = await queryDune("4776022", {});
-      const lastRecord = value[0];
-      
-      const feeValue = (lastRecord?.daily_diff_eth_value || 0) * 1e18;
-      dailyFees.add("eth", feeValue);
-      
+      const rows = await queryDune("4786708", {});
+      rows.forEach(row => {
+        const chainKey = row.blockchain;
+        const feeValue = (row.fees_by_chain || 0) * 1e18;
+        dailyFees.add(chainKey, feeValue);
+     }); 
       return { dailyFees };
     } catch (e) {
       return { dailyFees };
@@ -24,17 +23,29 @@ const methodology = {
   dailyFees: "Combines reserve asset yield and protocol fees on swap fee revenue.",
 };
 
+const supportedChains = [
+  CHAIN.ETHEREUM,
+  CHAIN.POLYGON,
+  CHAIN.POLYGON_ZKEVM,
+  CHAIN.OPTIMISM,
+  CHAIN.ARBITRUM,
+  CHAIN.BASE,
+  CHAIN.AVAX,
+  CHAIN.SONIC,
+  CHAIN.SEI,
+  CHAIN.XDAI,
+];
+
+const adapterChains = supportedChains.reduce((acc, chain) => {
+  acc[chain] = { fetch: fetch() as any, meta: { methodology } };
+  return acc;
+}, {});
+
 const adapter: Adapter = {
   version: 2,
-  adapter: {
-    [CHAIN.ETHEREUM]: {
-      fetch: fetch(CHAIN.ETHEREUM) as any,
-      meta: {
-        methodology,
-      },
-    },
-  },
+  adapter: adapterChains,
   isExpensiveAdapter: true,
 };
+
 
 export default adapter;
