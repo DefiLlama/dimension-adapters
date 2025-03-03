bba7e5fa48b6d0cb3de62a47d27901358954e7a7
tronsave <121288350+tronsave@users.noreply.github.com>
2025-03-03 22:31:46 +0700

    add Tronsave Fee

---
+++ fees/tronsave/index.ts
@@ -0,0 +1,50 @@
+import { FetchOptions } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { httpGet } from "../../utils/fetchURL";
+
+const TRON_SAVE_ADDRESS = "TWZEhq5JuUVvGtutNgnRBATbF8BnHGyn4S";
+const LIMIT = 50;
+
+const getDailyFees = async (fromTimestamp: number, endTimestamp: number) => {
+    let start = 0;
+    let totalFees = 0;
+    let hasMoreData = true;
+
+    while (hasMoreData) {
+        const trxTransactions = await httpGet(`https://apilist.tronscanapi.com/api/transfer/trx?address=${TRON_SAVE_ADDRESS}&start=${start}&limit=${LIMIT}&direction=2&&reverse=false&fee=true&db_version=1&start_timestamp=${fromTimestamp}&end_timestamp=${endTimestamp}`);
+
+        if (!trxTransactions.data || trxTransactions.data.length === 0) {
+            break;
+        }
+
+        totalFees += trxTransactions.data.reduce((acc: number, curr: any) => acc + Number(curr.amount) / 1000000, 0);
+        // If we got fewer results than the limit, we've reached the end
+        if (trxTransactions.data.length < LIMIT) {
+            hasMoreData = false;
+        } else {
+            // Move to the next page
+            start += LIMIT;
+        }
+    }
+    return totalFees;
+}
+
+const fetch = async ({ createBalances, endTimestamp, fromTimestamp }: FetchOptions) => {
+    const dailyRevenue = createBalances()
+    const totalRevenue = await getDailyFees(fromTimestamp, endTimestamp)
+    dailyRevenue.addCGToken('tron', totalRevenue)
+    return {
+        dailyFees: dailyRevenue,
+        dailyRevenue: dailyRevenue
+    };
+}
+
+export default {
+    version: 2,
+    adapter: {
+        [CHAIN.TRON]: {
+            fetch: fetch,
+            start: 1738368000,
+        },
+    },
+};
