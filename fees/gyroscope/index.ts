dae045b0f02762692931d4eac672657db52e6dd7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-05 23:00:05 +0100

    track gyroscope fees

---
+++ fees/gyroscope/index.ts
@@ -0,0 +1,67 @@
+import { Adapter, FetchOptions } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { addTokensReceived } from '../../helpers/token';
+
+const config = {
+  [CHAIN.BASE]: ['0xce88686553686DA562CE7Cea497CE749DA109f9F', '0xA01ba17778A860EC92053325d0de4022240ceeA4',],
+  [CHAIN.ETHEREUM]: ['0xce88686553686DA562CE7Cea497CE749DA109f9F', '0xA01ba17778A860EC92053325d0de4022240ceeA4',],
+  [CHAIN.AVAX]: ['0xce88686553686DA562CE7Cea497CE749DA109f9F', '0xA01ba17778A860EC92053325d0de4022240ceeA4',],
+  [CHAIN.ARBITRUM]: ['0xce88686553686DA562CE7Cea497CE749DA109f9F', '0xA01ba17778A860EC92053325d0de4022240ceeA4',],
+  [CHAIN.XDAI]: ['0xce88686553686DA562CE7Cea497CE749DA109f9F',],
+  [CHAIN.POLYGON]: ['0xce88686553686DA562CE7Cea497CE749DA109f9F',],
+  // [CHAIN.SEI]: ['0xDcC628c4C9f3840EA70f95798Ec4C16B0DA23ef9',],
+}
+async function fetch(options: FetchOptions) {
+  const { chain, api } = options;
+
+  const dailyFees = await addTokensReceived({ options, targets: config[chain] })
+
+  /* 
+  const keys = Object.keys(dailyFees.getBalances())
+  const tokens = keys.map((key) => key.split(':')[1])
+  const allVaults = await api.multiCall({ abi: 'address:getVault', calls: tokens, permitFailure: true})
+  const balancerVaultTokens = tokens.filter((_, i) => allVaults[i])
+  const vaults = allVaults.filter((_, i) => allVaults[i])
+  const actualSupplies = await api.multiCall({  abi: 'uint256:getActualSupply', calls: balancerVaultTokens})
+  const poolIds  = await api.multiCall({  abi: 'function getPoolId() view returns (bytes32)', calls: balancerVaultTokens })
+  const calls = poolIds.map((poolId, i) => ({ target: vaults[i], params: [poolId] }))
+  const poolBalances = await api.multiCall({  abi: 'function getPoolTokens(bytes32) view returns (address[] tokens, uint256[] bals, uint256)', calls})
+
+  const balancesObject = dailyFees.getBalances()
+  api.log(balancesObject)
+  api.log(await dailyFees.getUSDJSONs())
+  
+  poolBalances.forEach((poolBalance, i) => {
+    const balancerVaultToken = balancerVaultTokens[i]
+    const actualSupply = actualSupplies[i]
+    const lpBalance: any = balancesObject[`ethereum:${balancerVaultToken}`]
+    if (!lpBalance) {
+      api.log(`No balance for ${balancerVaultToken}`)
+      return
+    }
+    delete balancesObject[`ethereum:${balancerVaultToken}`]
+    const ratio = lpBalance / actualSupply
+    api.log(balancerVaultToken, ratio)
+    poolBalance.tokens.forEach((token, i) => dailyFees.add(token, poolBalance.bals[i]*ratio))
+  })
+
+  api.log(balancesObject)
+  api.log(await dailyFees.getUSDJSONs())
+*/
+  return {
+    dailyFees
+  };
+};
+
+
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+  }
+}
+
+Object.keys(config).forEach((chain) => {
+  adapter.adapter[chain] = { fetch }
+})
+
+export default adapter;
