4c7399313855f3bde861a5a77720135f0526b015
batu4404 <batu4404@gmail.com>
2025-03-12 11:09:25 +0700

    chore: use movement constanst from helpers

--- aggregators/mosaic/index.ts
+++ aggregators/mosaic/index.ts
@@ -1,29 +1,32 @@
-import {FetchResult,} from "../../adapters/types";
-import {getUniqStartOfTodayTimestamp} from "../../helpers/getUniSubgraphVolume";
-import {fetchURLWithRetry} from "../../helpers/duneRequest";
+import { FetchResult } from "../../adapters/types";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { fetchURLWithRetry } from "../../helpers/duneRequest";
+import { CHAIN } from "../../helpers/chains";
 
-const fetch = async (_: number): Promise<FetchResult> => {
-    const unixTimestamp = getUniqStartOfTodayTimestamp();
-    const data = await fetchURLWithRetry(`https://analytics.mosaic.ag/report/volume`)
-    const volumeData = data.data;
-    if (!volumeData) throw new Error(`Fail to query volume report`)
-    return {
-        dailyVolume: volumeData.volumeByDate[0]?.volume,
-        totalVolume: volumeData.totalVolume,
-        timestamp: unixTimestamp,
-    };
+const fetch = async (option): Promise<FetchResult> => {
+  const unixTimestamp = getUniqStartOfTodayTimestamp();
+  const data = await fetchURLWithRetry(
+    `https://analytics.mosaic.ag/report/volume`
+  );
+  const volumeData = data.data;
+  if (!volumeData) throw new Error(`Fail to query volume report`);
+  return {
+    dailyVolume: volumeData.volumeByDate[0]?.volume,
+    totalVolume: volumeData.totalVolume,
+    timestamp: unixTimestamp,
+  };
 };
 
 const adapter: any = {
-    timetravel: false,
-    adapter: {
-        "movement": {
-            fetch: fetch,
-            runAtCurrTime: true,
-            start: '2025-02-24',
-        }
+  timetravel: false,
+  adapter: {
+    [CHAIN.MOVEMENT]: {
+      fetch: fetch,
+      runAtCurrTime: true,
+      start: "2025-03-10",
     },
-    isExpensiveAdapter: true,
+  },
+  isExpensiveAdapter: true,
 };
 
 export default adapter;
