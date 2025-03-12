abd4d17afbb327822120e3e36b938572cf68a0d2
batu4404 <batu4404@gmail.com>
2025-03-12 11:59:07 +0700

    Add volume adapter for Mosaic aggregator

---
+++ aggregators/mosaic/index.ts
@@ -0,0 +1,32 @@
+import { FetchResult } from "../../adapters/types";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { fetchURLWithRetry } from "../../helpers/duneRequest";
+import { CHAIN } from "../../helpers/chains";
+
+const fetch = async (option): Promise<FetchResult> => {
+  const unixTimestamp = getUniqStartOfTodayTimestamp();
+  const data = await fetchURLWithRetry(
+    `https://analytics.mosaic.ag/report/volume`
+  );
+  const volumeData = data.data;
+  if (!volumeData) throw new Error(`Fail to query volume report`);
+  return {
+    dailyVolume: volumeData.volumeByDate[0]?.volume,
+    totalVolume: volumeData.totalVolume,
+    timestamp: unixTimestamp,
+  };
+};
+
+const adapter: any = {
+  timetravel: false,
+  adapter: {
+    [CHAIN.MOVEMENT]: {
+      fetch: fetch,
+      runAtCurrTime: true,
+      start: "2025-03-10",
+    },
+  },
+  isExpensiveAdapter: true,
+};
+
+export default adapter;
