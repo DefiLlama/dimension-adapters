0ffc7a5bb7b87f62ed811e02178fee043bb8b506
jeebx <jeeb24@gmail.com>
2025-03-14 00:44:45 -0500

    update clanker query

--- fees/clanker.ts
+++ fees/clanker.ts
@@ -4,16 +4,16 @@ import { queryDune } from "../helpers/dune";
 
 const fetchFees = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
-  const res = await queryDune("4343160", {
+  const res = await queryDune("4850065", {
     start: options.startTimestamp,
     end: options.endTimestamp,
-  })
+  });
   dailyFees.addUSDValue(res[0].daily_fees);
   return {
     dailyFees: dailyFees,
     dailyRevenue: dailyFees,
-  }
-}
+  };
+};
 
 const adapter: SimpleAdapter = {
   version: 2,
@@ -23,6 +23,6 @@ const adapter: SimpleAdapter = {
       start: "2024-11-22",
     },
   },
-}
+};
 
 export default adapter;
