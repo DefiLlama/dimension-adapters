f98fa09194de83846ced591354103ac708c6eff6
0xgnek <0xgnek@gmail.com>
2025-03-15 14:37:54 +0000

    group protocol

--- protocols/puppyfun/index.ts
+++ protocols/puppyfun/index.ts
@@ -1,42 +1,38 @@
 import { FetchOptions } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains"
-import axios from "axios"
+import { httpGet } from "../../utils/fetchURL"
 
 const BNB_ADDRESS = "0x0000000000000000000000000000000000000000"
-
 const apiBaseURL = "https://bd-fun-defilama-ts-backend-main.puppy.fun/lama-api"
 const volumeMethod = "/volume"
 
-const maxRetriesCount = 3
+const fetch = async (options: FetchOptions) => {
+    const response = await httpGet(apiBaseURL + volumeMethod)
+    const feesMethod = "/fees"
+    const feesResponse = await httpGet(apiBaseURL + feesMethod)
+    
+    const volume = options.createBalances()
+    const dailyVolume = options.createBalances()
+    volume.add(BNB_ADDRESS, response.volume)
+    dailyVolume.add(BNB_ADDRESS, response.dailyVolume)
+    const fees = options.createBalances()
+    const dailyFees = options.createBalances()
+    const revenue = options.createBalances()
+    const dailyRevenue = options.createBalances()
 
-const fetch: any = async (options: FetchOptions) => {
-    for (let tryCount = 0; tryCount < maxRetriesCount; ++tryCount) {
-        try {
-            console.log(`Query Volume ...`)
-            const response = await axios.get(apiBaseURL + volumeMethod)
-
-            const volume = options.createBalances()
-            const dailyVolume = options.createBalances()
-            volume.add(BNB_ADDRESS, response.data.volume)
-            dailyVolume.add(BNB_ADDRESS, response.data.dailyVolume)
-
-            const totalVolume = Number(response.data.volume) / 10 ** 18
-            console.log(`Got Volume: ${totalVolume} BNB`, response.data.volume)
-            console.log(`Got Daily Volume: ${Number(response.data.dailyVolume) / 10 ** 18} BNB`, response.data.dailyVolume)
-            return { totalVolume: volume, dailyVolume }
-        } catch (err) {
-            console.log(`Error getting Volume`, err)
-            continue
-        }
-    }
+    fees.add(BNB_ADDRESS, feesResponse.totalFee)
+    dailyFees.add(BNB_ADDRESS, feesResponse.totalDailyFee)
+    revenue.add(BNB_ADDRESS, feesResponse.totalFee)
+    dailyRevenue.add(BNB_ADDRESS, feesResponse.totalDailyFee)
+    
+    return { totalVolume: volume, dailyVolume, totalFees: fees, dailyFees, totalRevenue: revenue, dailyRevenue }
 }
 
 export default {
     version: 2,
     adapter: {
         [CHAIN.BSC]: {
-            fetch: fetch,
-            // start: '2024-12-01'
+            fetch: fetch
         },
     },
 }
