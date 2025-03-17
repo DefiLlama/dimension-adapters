602a64bdf4b196c79e0525c7b764ae513295483b
Michael Chan <michaelchan@Michaels-MacBook-Pro-2.local>
2025-03-17 19:51:57 +0800

    added daily sale fees for paint-swap

--- dexs/paint-swap/index.ts
+++ dexs/paint-swap/index.ts
@@ -1,31 +1,63 @@
 import fetchURL from "../../utils/fetchURL"
 import { ChainBlocks, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { start } from "repl";
+import { group } from "console";
 
 const dateFrom = 1630584906;
 const historicalVolumeEndpoint = (dateTo: number) => `https://api.paintswap.finance/v2/marketplaceDayDatas?numToSkip=0&numToFetch=1000&orderDirection=asc&dateFrom=${dateFrom}&dateTo=${dateTo}`;
+const recentSalesEndpoint = `https://api.paintswap.finance/v2/sales?numToFetch=1000&sold=true&orderDirection=desc`;
+//NFT sales fee is 2.5%
+const salesFee = 2.5; 
 
 interface IVolumeall {
   dailyVolume: string;
   date: number;
 }
 
+
 const fetch = async (timestamp: number, _: ChainBlocks, { startOfDay, createBalances, }: FetchOptions) => {
   const dailyVolume = createBalances();
+  const dailyUserFees = createBalances();
   const totalVolume = createBalances();
   const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint(startOfDay))).marketPlaceDayDatas;
+  const pastSales = (await fetchURL(recentSalesEndpoint)).sales;
   historicalVolume
     .filter(volItem => (new Date(volItem.date).getTime()) <= startOfDay)
     .map(({ dailyVolume }) => totalVolume.addGasToken(dailyVolume))
 
+    //helper function to group by days
+    const formatDate = (timestamp: number): string => {
+      return new Date(timestamp).toISOString().split("T")[0]; // Convert to YYYY-MM-DD
+    };
+
+
+    const groupedSales = pastSales.reduce((feeSum, sale) => {
+      const day = formatDate(Number(sale.endTime*1000)); // Convert timestamp to date for grouping
+      const dayTimestamp = new Date(day).getTime()/1000; 
+      feeSum[dayTimestamp] = (feeSum[dayTimestamp] || 0) + Number(sale.price) * (salesFee/100); // Sum up sale fees per day
+      
+      return feeSum;
+    }, {} as Record<string, number>);
+    
+
+  
+
+
+
   dailyVolume.addGasToken(historicalVolume
     .find(dayItem => (new Date(dayItem.date).getTime()) === startOfDay)?.dailyVolume)
+  
+    dailyUserFees.addGasToken(groupedSales[startOfDay] ?? 0);
 
+
+  
 
   return {
     timestamp: startOfDay,
     // totalVolume, 
     dailyVolume,
+    dailyUserFees
   };
 };
 
@@ -35,6 +67,11 @@ const adapter: SimpleAdapter = {
     [CHAIN.FANTOM]: {
       fetch,
       start: '2021-09-02',
+      meta: {
+        methodology: {
+          Fees: `${salesFee}% of each successful NFT sale is charged as a platform fee.`
+        }
+      }
     },
   },
 };
