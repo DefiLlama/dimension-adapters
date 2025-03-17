2533d16ff655f0d091c64fd11f9956342eb501bd
WHYTEWYLL <juanmiblanco1@gmail.com>
2025-03-17 21:18:15 +0200

    fix: eth address

--- fees/moonbot/index.ts
+++ fees/moonbot/index.ts
@@ -1,3 +1,4 @@
+import { ETHER_ADDRESS } from "@defillama/sdk/build/general";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { queryDune } from "../../helpers/dune";
@@ -12,15 +13,9 @@ const fetchFees = async (options: FetchOptions) => {
     receiver: "0x61131513C4fF67Bcd3318eb309834D26A3509Cdb",
   });
 
-  dailyFees.add(
-    "0x0000000000000000000000000000000000000000",
-    value[0]["Revenue"]
-  );
+  dailyFees.add(ETHER_ADDRESS, value[0]["Revenue"]);
 
-  dailyVolume.add(
-    "0x0000000000000000000000000000000000000000",
-    value[0]["Revenue"] * 100
-  );
+  dailyVolume.add(ETHER_ADDRESS, value[0]["Revenue"] * 100);
 
   return {
     dailyFees,
