443e173b905b7fd78afb09d43660613ece0629ef
Rouan van der Ende <rouan@linumlabs.com>
2025-03-18 07:22:10 +0200

    update for prod

--- dexs/hibachi/index.ts
+++ dexs/hibachi/index.ts
@@ -1,24 +1,26 @@
 import type { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 import { httpGet, httpPost } from "../../utils/fetchURL";
 
-
 type FutureContracts = {
   id: number;
-  symbol: string,
-  volume24h: string | null,
-  low24h: string | null,
-  high24h: string | null
-}
+  symbol: string;
+  volume24h: string | null;
+  low24h: string | null;
+  high24h: string | null;
+};
 
 interface Response {
   future_contracts: FutureContracts[];
-  timestamp: string
+  timestamp: string;
 }
 
 const fetch = async (timestamp: number) => {
-  const response: Response = (await httpGet("https://data-api.hibachi.xyz/exchange/stats/volumes"));
-  
+  const response: Response = await httpGet(
+    "https://data-api.hibachi.xyz/exchange/stats/volumes"
+  );
+
   const dailyVolume = response.future_contracts.reduce((acc, item) => {
     return acc + Number(item.volume24h ?? 0);
   }, 0);
@@ -26,19 +28,18 @@ const fetch = async (timestamp: number) => {
   const output = {
     dailyVolume: dailyVolume?.toString(),
     timestamp: new Date(response.timestamp).getTime() / 1000,
-  }
+  };
 
-
-  return output
+  return output;
 };
 
 const adapter: SimpleAdapter = {
   adapter: {
-    "hibachi": {
+    [CHAIN.ARBITRUM]: {
       fetch,
-      start: '2025-03-01',
+      start: "2025-03-18",
     },
-  }
+  },
 };
 
 export default adapter;
