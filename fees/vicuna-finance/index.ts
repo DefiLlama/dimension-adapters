057a8ff4dbedc7065ba9534a90d8dd2024f41460
Vicuna Finance <Vicunafinance@gmail.com>
2025-03-19 21:53:06 +0100

    Update index.ts
    Added the 50% of Fees that goes to the Governance stakers of Vicuna Finance Governance Vault.

--- fees/vicuna-finance/index.ts
+++ fees/vicuna-finance/index.ts
@@ -1,13 +1,15 @@
-import { Adapter, FetchOptions, } from '../../adapters/types';
+import { Adapter, FetchOptions } from '../../adapters/types';
 import { CHAIN } from '../../helpers/chains';
 import { addTokensReceived } from '../../helpers/token';
 
 // total harvest strategy fee is 7%
 const totalFee = 7;
-//call fee is 0.05%
+// call fee is 0.05%
 const callFee = 0.05;
 // 6.95% of the total fee goes to the vault
 const vaultFee = totalFee - callFee;
+// 50% of the harvest fee goes to Governance users
+const governanceShare = totalFee / 2;
 
 const adapter: Adapter = {
     adapter: {
@@ -17,16 +19,17 @@ const adapter: Adapter = {
                 const targets = ['0xad1bB693975C16eC2cEEF65edD540BC735F8608B'];
     
                 const dailyRevenue = await addTokensReceived({ options, targets, tokens });
-                const dailyFees = dailyRevenue.clone(totalFee / vaultFee);  // Total fees = protocol revenue * (7/6.95)
-                const dailyProtocolRevenue = dailyRevenue 
+                const dailyFees = dailyRevenue.clone(totalFee / vaultFee); // Total fees = protocol revenue * (7/6.95)
+                const dailyProtocolRevenue = dailyRevenue.clone(vaultFee / totalFee); // Vault share of the revenue
+                const governanceRevenue = dailyRevenue.clone(governanceShare / totalFee); // Governance share of the revenue
     
-                return { dailyFees, dailyRevenue, dailyProtocolRevenue }
+                return { dailyFees, dailyRevenue, dailyProtocolRevenue, governanceRevenue };
             },
             start: '2025-01-02',
             meta: {
                 methodology: {
-                    Fees: `${totalFee}% of each harvest is charged as a performance fee`,
-                    Revenue: `All fees except for ${callFee}% to call fee are revenue`,
+                    Fees: `${totalFee}% of each harvest is charged as a performance fee, with 50% of the fee going to Governance users.`,
+                    Revenue: `All fees except for ${callFee}% to call fee are considered revenue. 50% of the fee goes to Governance users and is excluded from protocol revenue.`,
                 }
             }
         }
@@ -34,4 +37,4 @@ const adapter: Adapter = {
     version: 2,
 };
 
-export default adapter
+export default adapter;
