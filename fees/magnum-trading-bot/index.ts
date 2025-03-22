649a30d021c1a56944a28d8e1e3df3c5c40dd9d4
treeoflife2 <solanalyzoor@gmail.com>
2025-03-22 19:37:50 +0530

    Add Solana trading bot adapter with daily fees fetching

---
+++ fees/magnum-trading-bot/index.ts
@@ -0,0 +1,26 @@
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getSolanaReceived } from "../../helpers/token";
+
+const fetch: any = async (options: FetchOptions) => {
+  const dailyFees = await getSolanaReceived({
+    options,
+    targets:
+      [
+        'CPixcsP8LEMeUoavaHG3bdkywR8s4mZXNN3mYUgbXFev', '8dEe5BM7irAnHtJ6SSWwCRf7njgnyczS3jPrvJJs88U5'
+      ]
+  })
+  return { dailyFees, dailyRevenue: dailyFees, }
+}
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.SOLANA]: {
+      fetch: fetch,
+    },
+  },
+  isExpensiveAdapter: true
+};
+
+export default adapter;
