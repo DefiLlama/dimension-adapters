16335001b2e9f062c939af2efeac91f5429e0a3f
0xgnek <0xgnek@gmail.com>
2025-03-24 10:05:56 +0000

    add more chain

--- fees/hop-protocol.ts
+++ fees/hop-protocol.ts
@@ -5,6 +5,7 @@ import { httpGet } from "../utils/fetchURL";
 const event_ccpt = 'event CCTPTransferSent(uint64 indexed cctpNonce,uint256 indexed chainId,address indexed recipient,uint256 amount,uint256 bonderFee)'
 const event_bond = 'event TransferSent(bytes32 indexed transferId,uint256 indexed chainId,address indexed recipient,uint256 amount,bytes32 transferNonce,uint256 bonderFee,uint256 index,uint256 amountOutMin,uint256 deadline)'
 const event_l1 = 'event TransferSentToL2(uint256 indexed chainId,address indexed recipient,uint256 amount,uint256 amountOutMin,uint256 deadline,address indexed relayer,uint256 relayerFee)'
+const event_l1_com ='event TransferFromL1Completed(address indexed recipient,uint256 amount,uint256 amountOutMin,uint256 deadline,address indexed relayer,uint256 relayerFee)'
 type IRequest = {
     [key: string]: Promise<any>;
 }
@@ -62,12 +63,20 @@ const fetchFeesL2 = async (options: FetchOptions): Promise<FetchResultV2> => {
   const dailyFees = options.createBalances();
   const config = await fetchCacheURL('https://s3.us-west-1.amazonaws.com/assets.hop.exchange/mainnet/v1-core-config.json')
   const l2_bridges = Object.values(config.bridges).map((e: any) => e[options.chain]).filter(Boolean)
-  const contract_bond: string[] = l2_bridges.map((e: any) => e.l2Bridge || e.cctpL2Bridge).filter(Boolean)
+  const contract_bond: string[] = l2_bridges.map((e: any) => e.l2Bridge).filter(Boolean)
   const mapping_token = l2_bridges.map((e: any) => {
     return {
-      [e.l2Bridge || e.cctpL2Bridge]: e.l2CanonicalToken
+      [e.l2Bridge]: e.l2CanonicalToken
     }
   }).filter(Boolean)
+  const contract_ccpt: string[] = l2_bridges.map((e: any) => e.cctpL2Bridge).filter(Boolean)
+  const mapping_token_ccp = l2_bridges.map((e: any) => {
+    return {
+      [e.cctpL2Bridge]: e.l2CanonicalToken
+    }
+  }).filter(Boolean)
+  contract_bond.concat(contract_ccpt)
+  mapping_token.concat(mapping_token_ccp)
 
   const logs_ccpt = await options.getLogs({
     eventAbi: event_ccpt,
@@ -80,6 +89,21 @@ const fetchFeesL2 = async (options: FetchOptions): Promise<FetchResultV2> => {
     targets: contract_bond,
     flatten: false
   })
+
+  const logs_tran_l1_com = await options.getLogs({
+    eventAbi: event_l1_com,
+    targets: contract_bond,
+    flatten: false
+  })
+
+  logs_tran_l1_com.forEach((logs, index) => {
+    logs.forEach((log: any) => {
+      const hop_contract = contract_bond[index]
+      const token_l2: any = mapping_token.find((e: any) => e[hop_contract])
+      dailyFees.add(token_l2[hop_contract], log.relayerFee)
+    })
+  })
+  
 
   logs_ccpt.forEach((logs, index) => {
     logs.forEach((log: any) => {
