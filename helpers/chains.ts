6c5b2cec6e84419adc5c16f3ad7d269eeb1f2e75
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-24 19:16:31 +0100

    track more chains

--- helpers/chains.ts
+++ helpers/chains.ts
@@ -222,7 +222,9 @@ export enum CHAIN {
   FORMNETWORK = "formnetwork",
   HEMI = "hemi",
   MOVE = "move",
-  SUPERPOSITION = "superposition"
+  SUPERPOSITION = "superposition",
+  SWELLCHAIN = "swellchain",
+  ANCIENT8 = "ancient8",
 }
 
 // Don´t use
