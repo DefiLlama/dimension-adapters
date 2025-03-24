a2d544cafe0d8f14a37c3cc479ac0264d453e7db
treeoflife2 <solanalyzoor@gmail.com>
2025-03-24 16:41:06 +0530

    Change adapter version to v1

--- fees/dextoro/index.ts
+++ fees/dextoro/index.ts
@@ -2,22 +2,23 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { queryDune } from "../../helpers/dune";
 
-const fetch: any = async (options: FetchOptions) => {
+const fetch = async (timestamp: number, chainBlocks: any, options: FetchOptions) => {
+    // source: https://dune.com/0xvitalii/dextoro-stats - created Optimized version of the official stats query.
     const start = new Date(options.startTimestamp * 1000).toISOString().slice(0, 16).replace('T', ' ');
     const end = new Date(options.endTimestamp * 1000).toISOString().slice(0, 16).replace('T', ' ');
-    console.log(start, end);
+    // console.log(start, end);
     const value = (await queryDune("4888269", {
         start,
         end,
     }));
-
-    const dailyFees = value[0].fees;
+    // console.log(value);
+    const dailyFees = value[0].fees ? value[0].fees : 0;
 
     return { dailyFees, dailyRevenue: dailyFees }
 }
 
 const adapter: SimpleAdapter = {
-    version: 2,
+    version: 1,
     adapter: {
         [CHAIN.OPTIMISM]: {
             fetch: fetch,
