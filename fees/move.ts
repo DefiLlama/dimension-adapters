e25f82dc6c3525caa2de1192a4fdce85b6f6a31a
treeoflife2 <solanalyzoor@gmail.com>
2025-03-26 01:55:27 +0530

    [Chain] Movement Fees

---
+++ fees/move.ts
@@ -0,0 +1,36 @@
+import { Adapter, FetchOptions, ProtocolType } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { queryFlipside } from "../helpers/flipsidecrypto";
+
+const adapter: Adapter = {
+  adapter: {
+    [CHAIN.MOVE]: {
+      fetch: (async (options: FetchOptions) => {
+        const { startTimestamp, endTimestamp, createBalances, } = options
+
+        const dailyFees = createBalances()
+
+        const feeQuery = await queryFlipside(`
+          SELECT 
+            SUM(gas_used * gas_unit_price)/pow(10,8)
+          FROM movement.core.fact_transactions
+          WHERE SUCCESS AND 
+          block_timestamp BETWEEN TO_TIMESTAMP_NTZ(${startTimestamp}) AND TO_TIMESTAMP_NTZ(${endTimestamp})
+        `, 260)
+
+        const fees = Number(feeQuery[0][0])
+
+        dailyFees.addGasToken(fees)
+
+        return { dailyFees, dailyHoldersRevenue: dailyFees };
+
+      }) as any,
+      start: '2024-12-06',
+    },
+  },
+  isExpensiveAdapter: true,
+  protocolType: ProtocolType.CHAIN,
+  version: 2
+}
+
+export default adapter;
