9bdb19e5fb014d7b6eaf07c2defbdfb79ef9c1b6
treeoflife2 <solanalyzoor@gmail.com>
2025-03-26 23:37:08 +0530

    [chain] hedera fees

---
+++ fees/hedera.ts
@@ -0,0 +1,56 @@
+import { gql, request } from "graphql-request";
+import type { FetchOptions } from "../adapters/types";
+import { Adapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+
+const graphqlEndpoint = "https://mainnet.hedera.api.hgraph.dev/v1/graphql";
+
+const fetch = async (options: FetchOptions) => {
+  const { startOfDay } = options;
+  const endOfDay = startOfDay + 24 * 60 * 60;
+  const startDate = new Date(startOfDay * 1000).toISOString();
+  const endDate = new Date(endOfDay * 1000).toISOString();
+
+  const graphQuery = gql`
+    {
+          all_metrics: ecosystem_metric(
+            where: {
+              name: {_eq: "network_fee"},
+              period: {_eq: "hour"},
+              start_date: {_gte: "${startDate}"},
+              end_date: {_lte: "${endDate}"},
+            }
+            order_by: {end_date: asc}
+          ) {
+            start_date
+            end_date
+            total
+          }
+        }
+    `;
+
+  const graphRes = await request(graphqlEndpoint, graphQuery);
+
+  const tokenAmount = graphRes.all_metrics.reduce((acc: number, curr: any) => acc + Number(curr.total), 0);
+  const finalDailyFee = tokenAmount / 1e8;
+
+  const dailyFees = options.createBalances();
+  dailyFees.addCGToken('hedera', tokenAmount);
+
+  return {
+    dailyFees: finalDailyFee.toString(),
+  };
+};
+
+
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.HEDERA]: {
+      fetch,
+      start: '2019-09-14'
+    },
+  },
+};
+
+export default adapter;
