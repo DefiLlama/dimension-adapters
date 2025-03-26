4c29b6bc63a0022c15b48a9fcc09d5b7de0ddc31
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-26 23:20:00 +0100

    minor fix

--- fees/pinksale/index.ts
+++ fees/pinksale/index.ts
@@ -20,28 +20,9 @@ const FEE_WALLETS = {
   [CHAIN.UNICHAIN]: ["0x2e6C8927285353F24A00fcBAF605C54E2E18ea83", "0x4b04213c2774f77e60702880654206b116d00508"]
 };
 
-const SOLANA_PINKSALE_PROGRAM = "Pinks8fXRcJ8EpNjK4vCf7saJ5hFeP9zRXGPwZrTukQ"
 
 const fetchSolanaFees: any = async (options: FetchOptions) => {
   const dailyFees = await getSolanaReceived({ options, targets: FEE_WALLETS[CHAIN.SOLANA] })
-  // const dailyFees = options.createBalances();
-
-  // const wsolQuery = `
-  //   SELECT
-  //       SUM(amount/10e9) as total_wsol
-  //   FROM tokens_solana.transfers
-  //   WHERE
-  //       outer_executing_account = '${SOLANA_PINKSALE_PROGRAM}'
-  //       AND token_mint_address = 'So11111111111111111111111111111111111111112'
-  //       AND to_owner = '${FEE_WALLETS[CHAIN.SOLANA][0]}'
-  //       AND TIME_RANGE
-  // `;
-
-  // const wsolResults = await queryDuneSql(options, wsolQuery);
-  // if (wsolResults[0]?.total_wsol) {
-  //   dailyFees.add('So11111111111111111111111111111111111111112', wsolResults[0].total_wsol * 1e9);
-  // }
-  // console.log(dailyFees);
   return { dailyFees, dailyRevenue: dailyFees, }
 }
 
@@ -49,7 +30,7 @@ const fetch = async (options: FetchOptions) => {
   // https://docs.pinksale.finance/service-fees
 
   const feeWallet = FEE_WALLETS[options.chain];
-  const dailyFees = await  getETHReceived({ options, targets: feeWallet,});
+  const dailyFees = await getETHReceived({ options, targets: feeWallet, });
 
   return { dailyFees, dailyRevenue: dailyFees };
 };
@@ -57,50 +38,17 @@ const fetch = async (options: FetchOptions) => {
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
-    [CHAIN.ETHEREUM]: {
-      fetch,
-      start: '2021-10-30',
-    },
-    [CHAIN.BSC]: {
-      fetch,
-      start: '2021-06-01',
-    },
-    [CHAIN.POLYGON]: {
-      fetch,
-      start: '2024-12-06',
-    },
-    [CHAIN.ARBITRUM]: {
-      fetch,
-      start: '2023-02-01',
-    },
-    [CHAIN.AVAX]: {
-      fetch,
-      start: '2021-09-18',
-    },
-    // [CHAIN.CRONOS]: {
-    //   fetch,
-    //   start: '2022-04-01',
-    // },
-    // [CHAIN.CORE]: {
-    //   fetch,
-    //   start: '2023-10-19',
-    // },
-    // [CHAIN.ZETA]: {
-    //   fetch,
-    //   start: '2025-02-07',
-    // },
-    [CHAIN.BASE]: {
-      fetch,
-      start: '2024-04-05',
-    },
-    [CHAIN.UNICHAIN]: {
-      fetch,
-      start: '2025-02-21',
-    },
-    [CHAIN.SOLANA]: {
-      fetch: fetchSolanaFees,
-      start: '2024-02-04',
-    },
+    [CHAIN.ETHEREUM]: { fetch, start: '2021-10-30', },
+    [CHAIN.BSC]: { fetch, start: '2021-06-01', },
+    [CHAIN.POLYGON]: { fetch, start: '2024-12-06', },
+    [CHAIN.ARBITRUM]: { fetch, start: '2023-02-01', },
+    [CHAIN.AVAX]: { fetch, start: '2021-09-18', },
+    // [CHAIN.CRONOS]: {  fetch,  start: '2022-04-01',},
+    // [CHAIN.CORE]: {  fetch,  start: '2023-10-19',},
+    // [CHAIN.ZETA]: {  fetch,  start: '2025-02-07',},
+    [CHAIN.BASE]: { fetch, start: '2024-04-05', },
+    [CHAIN.UNICHAIN]: { fetch, start: '2025-02-21', },
+    [CHAIN.SOLANA]: { fetch: fetchSolanaFees, start: '2024-02-04', },
   },
 };
 
