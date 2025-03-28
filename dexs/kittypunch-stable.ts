e5531092114e3e288b79df7caaa2a5e98eca4a2d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-28 11:17:29 +0100

    refactor

--- dexs/kittypunch-stable.ts
+++ dexs/kittypunch-stable.ts
@@ -1,74 +1,57 @@
 import request from "graphql-request";
-import type { ChainBlocks, FetchOptions, FetchResponseValue, FetchResult, SimpleAdapter } from "../adapters/types";
+import type { FetchOptions, FetchResult, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
-
-interface Exchange {
-  tokenInAmount: string;
-  tokenOutAmount: string;
-  tokenIn: {
-    token: {
-      id: string;
-      decimals: string;
-    };
-  };
-  tokenOut: {
-    token: {
-      id: string;
-      decimals: string;
-    };
-  };
-}
+import { getConfig } from "../helpers/cache";
+import { addOneToken } from "../helpers/prices";
 
 const subgraphUrl = "https://api.goldsky.com/api/public/project_cm33d1338c1jc010e715n1z6n/subgraphs/stable-swap-factory-ng-contracts-subgraph-flow-mainnet/2.2.0/gn"
 
 const fetch = async (
-  timestamp: number,
-  _chainBlocks: ChainBlocks,
-  options: FetchOptions
+  { createBalances, chain, getLogs, }: FetchOptions
 ): Promise<FetchResult> => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const query = `{
-    poolTokensExchanges(
-      where: {
-        timestamp_gte: "${dayTimestamp}",
-        timestamp_lt: "${dayTimestamp + 86400}"
-      }
-      orderBy: timestamp
-      orderDirection: asc
-    ) {
-      tokenInAmount
-      tokenOutAmount
-      tokenIn {
-        token {
-          id
-          decimals
-        }
-      }
-      tokenOut {
-        token {
-          id
-          decimals
-        }
+  const {pools, tokenMap} = await getConfig('kittpunch/stable-' + chain, '', {
+    fetcher: async () => {
+      const query = `{  pools {
+    address
+    tokens {
+      id
+      token {
+        id
       }
     }
-  }`
-  const res = await request(subgraphUrl, query);
+  }}`
+      const res = await request(subgraphUrl, query);
+      const tokenMap: {[id: string]: string} = {}
+      res.pools.forEach((pool: { tokens: { id: string, token: { id: string } }[] }) => {
+        pool.tokens.forEach((token) => {
+          tokenMap[token.id] = token.token.id
+        })
+      })
+      const pools = res.pools.map((pool: { address: string }) => pool.address)
+      return {pools, tokenMap}
+    }
+  })
 
-  const volumeData = options.createBalances();
+  const logs = await getLogs({
+    targets: pools,
+    eventAbi: 'event TokenExchange(address indexed buyer, int128 sold_id, uint256 tokens_sold, int128 bought_id, uint256 tokens_bought)',
+    flatten: false,
+  })
 
-  for (const curr of res.poolTokensExchanges as Exchange[]) {
-    volumeData.add(curr.tokenIn.token.id, BigInt(curr.tokenInAmount));
-    volumeData.add(curr.tokenOut.token.id, BigInt(curr.tokenOutAmount));
-  }
-
-  return {
-    timestamp: dayTimestamp,
-    dailyVolume: volumeData,
-  }
+  const dailyVolume = createBalances()
+  logs.forEach((_logs, idx) => {
+    const pool = pools[idx]
+    _logs.forEach((log: any) => {
+      const token0 = tokenMap[pool+'-'+log.sold_id]
+      const token1 = tokenMap[pool+'-'+log.bought_id]
+      addOneToken({ chain, balances: dailyVolume, token0, amount0: log.tokens_sold, token1, amount1: log.tokens_bought })
+    })
+  })
+  return { dailyVolume }
 };
 
 const adapter: SimpleAdapter = {
+  version: 2,
   adapter: {
     [CHAIN.FLOW]: {
       fetch,
