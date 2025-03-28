c2a9a91bb37cc00241e189d67f824cc4f52710a7
Tang Bo Hao <tech@btang.cn>
2025-03-28 16:28:43 +0800

    feat: impl volume adapter for stable kitty

--- dexs/kittypunch-stable.ts
+++ dexs/kittypunch-stable.ts
@@ -1,35 +1,72 @@
 import request from "graphql-request";
-import { SimpleAdapter } from "../adapters/types";
+import type { ChainBlocks, FetchOptions, FetchResponseValue, FetchResult, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume";
 
+interface Exchange {
+  tokenInAmount: string;
+  tokenOutAmount: string;
+  tokenIn: {
+    token: {
+      id: string;
+      decimals: string;
+    };
+  };
+  tokenOut: {
+    token: {
+      id: string;
+      decimals: string;
+    };
+  };
+}
+
 const subgraphUrl = "https://api.goldsky.com/api/public/project_cm33d1338c1jc010e715n1z6n/subgraphs/stable-swap-factory-ng-contracts-subgraph-flow-mainnet/2.2.0/gn"
 
-const fetch: any = async (timestamp: number) => {
+const fetch = async (
+  timestamp: number,
+  _chainBlocks: ChainBlocks,
+  options: FetchOptions
+): Promise<FetchResult> => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
   const query = `{
-    pools {
-      id
-      name
-      dailyVolumes(
-        where: {
-          timestamp: ${dayTimestamp}
+    poolTokensExchanges(
+      where: {
+        timestamp_gte: "${dayTimestamp}",
+        timestamp_lt: "${dayTimestamp + 86400}"
+      }
+      orderBy: timestamp
+      orderDirection: asc
+    ) {
+      tokenInAmount
+      tokenOutAmount
+      tokenIn {
+        token {
+          id
+          decimals
         }
-      ) {
-        volume
+      }
+      tokenOut {
+        token {
+          id
+          decimals
+        }
       }
     }
   }`
   const res = await request(subgraphUrl, query);
-  const dailyVolume = res.pools.reduce((acc: number, pool: any) => {
-    return acc + pool.dailyVolumes.reduce((poolAcc: number, volume: any) => poolAcc + Number(volume.volume), 0);
-  }, 0);
+
+  const volumeData = options.createBalances();
+
+  for (const curr of res.poolTokensExchanges as Exchange[]) {
+    volumeData.add(curr.tokenIn.token.id, BigInt(curr.tokenInAmount));
+    volumeData.add(curr.tokenOut.token.id, BigInt(curr.tokenOutAmount));
+  }
+
   return {
-    dailyVolume,
     timestamp: dayTimestamp,
+    dailyVolume: volumeData,
   }
 };
-
 
 const adapter: SimpleAdapter = {
   adapter: {
