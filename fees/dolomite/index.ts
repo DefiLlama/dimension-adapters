fb628b6780ae418ecb2525daca125b143cbc49c8
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-03-28 11:09:23 +0700

    dolomite single call

--- fees/dolomite/index.ts
+++ fees/dolomite/index.ts
@@ -4,15 +4,24 @@ import { CHAIN } from "../../helpers/chains"
 const dolomiteMarginAddresses = {
     [CHAIN.ARBITRUM]: "0x6Bd780E7fDf01D77e4d475c821f1e7AE05409072",
     [CHAIN.BERACHAIN]: "0x003Ca23Fd5F0ca87D01F6eC6CD14A8AE60c2b97D",
-    [CHAIN.BASE]: "0x003Ca23Fd5F0ca87D01F6eC6CD14A8AE60c2b97D"
+    [CHAIN.MANTLE]: "0xE6Ef4f0B2455bAB92ce7cC78E35324ab58917De8",
+    [CHAIN.POLYGON_ZKEVM]: "0x836b557Cf9eF29fcF49C776841191782df34e4e5",
+    [CHAIN.XLAYER]: "0x836b557Cf9eF29fcF49C776841191782df34e4e5"
 }
 
 const dolomiteMarginABI = {
     getNumMarkets: "function getNumMarkets() view returns (uint256)",
-    getMarketBorrowInterestRateApr: "function getMarketBorrowInterestRateApr(uint256 marketId) view returns (tuple(uint256 value))",
     getMarketTokenAddress: "function getMarketTokenAddress(uint256 marketId) view returns (address)",
-    getMarketTotalWei: "function getMarketTotalWei(uint256 marketId) view returns (tuple(uint128 borrow, uint128 supply))",
-    getEarningsRate: "function getEarningsRate() view returns (uint256)"
+    getMarketTotalPar: "function getMarketTotalPar(uint256 marketId) view returns (tuple(uint128 borrow, uint128 supply))",
+    getMarketCurrentIndex: "function getMarketCurrentIndex(uint256 marketId) view returns (tuple(uint112 borrow, uint112 supply, uint32 lastUpdate))",
+    getMarketBorrowInterestRatePerSecond: "function getMarketBorrowInterestRatePerSecond(uint256 marketId) view returns (tuple(uint256 value))",
+    getEarningsRate: "function getEarningsRate() view returns (uint256)",
+    getMarketInterestSetter: "function getMarketInterestSetter(uint256 marketId) view returns (address)"
+
+}
+
+const interestRateSetterABI = {
+    "getInterestRate": "function getInterestRate(address token, uint256 borrowWei, uint256 supplyWei) view returns (tuple(uint256 value))"
 }
 
 const fetch = async ({ createBalances, api, getLogs, chain }: FetchOptions) => {
@@ -21,20 +30,12 @@ const fetch = async ({ createBalances, api, getLogs, chain }: FetchOptions) => {
     const marketLength = await api.call({ target: dolomiteMarginAddresses[chain], abi: dolomiteMarginABI.getNumMarkets })
     const earningRate = await api.call({ target: dolomiteMarginAddresses[chain], abi: dolomiteMarginABI.getEarningsRate })
     if (marketLength === 0) return {}
-    //dolomite doesn't have a way to get all markets in one go
+
+    //dolomite doesn't have a way to get all markets info in one go
     const markets = Array.from({ length: marketLength }, (_, i) => ({
         target: dolomiteMarginAddresses[chain],
         params: i 
     }));
-
-    //interest rate is in apr 18 decimals
-    const marketsBorrowInterestRateApr = await api.multiCall({
-        abi: dolomiteMarginABI.getMarketBorrowInterestRateApr,
-        calls: markets.map((market) => ({
-            target: market.target,
-            params: [market.params]
-        }))
-    })
 
     const marketsTokenAddress = await api.multiCall({
         abi: dolomiteMarginABI.getMarketTokenAddress,
@@ -44,18 +45,50 @@ const fetch = async ({ createBalances, api, getLogs, chain }: FetchOptions) => {
         }))
     })
 
-    const marketsTotalWei = await api.multiCall({
-        abi: dolomiteMarginABI.getMarketTotalWei,
+    const marketsInterestSetter = await api.multiCall({
+        abi: dolomiteMarginABI.getMarketInterestSetter,
         calls: markets.map((market) => ({
             target: market.target,
             params: [market.params]
         }))
     })
 
+    const marketsTotalPar = await api.multiCall({
+        abi: dolomiteMarginABI.getMarketTotalPar,
+        calls: markets.map((market) => ({
+            target: market.target,
+            params: [market.params]
+        }))
+    })
+
+    const marketsCurrentIndex = await api.multiCall({
+        abi: dolomiteMarginABI.getMarketCurrentIndex,
+        calls: markets.map((market) => ({
+            target: market.target,
+            params: [market.params]
+        }))
+    })
+    const marketsTotalWei = marketsTotalPar.map((par, i) => [
+        BigInt(par.borrow) * BigInt(marketsCurrentIndex[i].borrow) / BigInt(1e18),
+        BigInt(par.supply) * BigInt(marketsCurrentIndex[i].supply) / BigInt(1e18)
+    ].map(val => val.toString()))
+
+
+    const marketsBorrowInterestRatePerSecond = await api.multiCall({
+        abi: interestRateSetterABI.getInterestRate,
+        calls: markets.map((market, i) => ({
+            target: marketsInterestSetter[i],
+            params: [marketsTokenAddress[i], marketsTotalWei[i][0], marketsTotalWei[i][1]]
+        }))
+    })
+
+    const marketsBorrowInterestRateApr = marketsBorrowInterestRatePerSecond.map((rate) => rate * 31536000)
+
+
     const earningsRate = 1 - (earningRate / 1e18)
     marketsBorrowInterestRateApr.map((rate, i) => {
         const token = marketsTokenAddress[i]
-        const totalWei = marketsTotalWei[i] //0 is borrow, 1 is supply
+        const totalWei = marketsTotalWei[i]
         const borrowInterestRate = rate / 1e18 / 365
         const dailyBorrowInterest = totalWei[0] * borrowInterestRate
         dailyFees.add(token, dailyBorrowInterest)
@@ -69,33 +102,48 @@ const fetch = async ({ createBalances, api, getLogs, chain }: FetchOptions) => {
 }
 
 const methodology = {
-    dailyFees: "Interest that are paid by the borrowers to the vaults",
-    dailyRevenue: "Protocol & Governor fees share"
+    dailyFees: "Interest paid by the borrowers",
+    dailyRevenue: "Portion of fees that goes to the protocol",
+    dailySupplySideRevenue: "Portion of fees that goes to the lenders"
 }
 
 const adapters: Adapter = {
     adapter: {
-        [CHAIN.BERACHAIN]: {
+        [CHAIN.ARBITRUM]: {
             fetch: fetch,
-            start: '2024-08-18',
+            start: '2022-10-03',
             meta: {
                 methodology
             }
         },
-        // [CHAIN.SONIC]: {
-        //     fetch: fetch,
-        //     start: '2025-01-31',
-        //     meta: {
-        //         methodology
-        //     }
-        // },
-        // [CHAIN.BASE]: {
-        //     fetch: fetch,
-        //     start: '2024-11-27',
-        //     meta: {
-        //         methodology
-        //     }
-        // }
+        [CHAIN.BERACHAIN]: {
+            fetch: fetch,
+            start: '2024-01-24',
+            meta: {
+                methodology
+            }
+        },
+        [CHAIN.MANTLE]: {
+            fetch: fetch,
+            start: '2024-04-28',
+            meta: {
+                methodology
+            }
+        },
+        [CHAIN.POLYGON_ZKEVM]: {
+            fetch: fetch,
+            start: '2024-02-01',
+            meta: {
+                methodology
+            }
+        },
+        [CHAIN.XLAYER]: {
+            fetch: fetch,
+            start: '2024-04-28',
+            meta: {
+                methodology
+            }
+        }
     },
 
     version: 2
