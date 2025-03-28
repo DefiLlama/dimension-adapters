aeff73d243632ed5768aef5e96868d31879fe26d
caglavol <volkan@1delta.io>
2025-03-28 15:49:10 +0100

    use delta api to fetch evm volume

--- aggregators/1delta/index.ts
+++ aggregators/1delta/index.ts
@@ -1,4 +1,4 @@
-import { ethers } from "ethers";
+import { Chain } from "@defillama/sdk/build/types";
 import { Adapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
@@ -36,65 +36,70 @@ const createFuelVolumeFetcher = () => {
   }
 }
 
-const transferEventSig = ethers.id("Transfer(address,address,uint256)");
-
-const META_AGGREGATOR_ADDRESS = {
-  [CHAIN.MANTLE]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.OPTIMISM]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.POLYGON]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.LINEA]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.BSC]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.AVAX]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.TAIKO]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.BASE]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.ARBITRUM]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.BLAST]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.METIS]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.XDAI]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.MODE]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.HEMI]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.SCROLL]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.CORE]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.SONIC]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400',
-  [CHAIN.FANTOM]: '0xDDDD191F453A387E3D2bB594E657A6D8c8c6D400'
+const SUPPORTED_CHAIN_MAPPING: { [chain: Chain]: number } = {
+  [CHAIN.MANTLE]: 5000,
+  [CHAIN.OPTIMISM]: 10,
+  [CHAIN.POLYGON]: 137,
+  [CHAIN.LINEA]: 59144,
+  [CHAIN.BSC]: 56,
+  [CHAIN.AVAX]: 43114,
+  [CHAIN.TAIKO]: 167000,
+  [CHAIN.BASE]: 8453,
+  [CHAIN.ARBITRUM]: 42161,
+  [CHAIN.BLAST]: 81457,
+  [CHAIN.METIS]: 1088,
+  [CHAIN.XDAI]: 100,
+  [CHAIN.MODE]: 34443,
+  [CHAIN.HEMI]: 43111,
+  [CHAIN.SCROLL]: 534352,
+  [CHAIN.CORE]: 1116,
+  [CHAIN.SONIC]: 146,
+  [CHAIN.FANTOM]: 250
 }
 
+const TIMESTAMP_TO_BLOCK_FUNCTION_MAPPING: { [chain: Chain]: ((blockTimestamp: number) => number) | undefined } = {
+  [CHAIN.HEMI]: (blockTimestamp: number): number => {
+    const genesisBlockTimestamp = 1725866711
+    const averageSecPerBlock = 12
+    return Math.max(0, Math.floor((blockTimestamp - genesisBlockTimestamp) / averageSecPerBlock))
+  }
+}
+
+const getEVMVolumeAPI = (chainId: number, fromBlock: number, toBlock: number) =>
+  `https://volume.1delta.io/volume?chainId=${chainId}&fromBlock=${fromBlock}&toBlock=${toBlock}`
+
 const createEVMVolumeFetcher = (chain: string) => {
-  return async ({ getFromBlock, getToBlock, createBalances, api }: FetchOptions) => {
+  return async ({ getFromBlock, getToBlock, startTimestamp, endTimestamp, createBalances }: FetchOptions) => {
     const dailyVolume = createBalances()
 
-    const toTopic = ethers.zeroPadValue(META_AGGREGATOR_ADDRESS[chain], 32)
+    const chainId: number | undefined = SUPPORTED_CHAIN_MAPPING[chain]
+    if (!chainId) return
 
-    console.log("fromBlock", await getFromBlock())
-    console.log("toBlock", await getToBlock())
-    console.log("chain", chain)
+    const convertTsToBlock = TIMESTAMP_TO_BLOCK_FUNCTION_MAPPING[chain]
+    const fromBlock = convertTsToBlock ? convertTsToBlock(startTimestamp) : await getFromBlock()
+    const toBlock = convertTsToBlock ? convertTsToBlock(endTimestamp) : await getToBlock()
 
-    const transferLogs = (await api.provider.getLogs({
-      fromBlock: await getFromBlock(),
-      toBlock: await getToBlock(),
-      topics: [transferEventSig, null, toTopic],
-    }))
+    const response = await fetch(getEVMVolumeAPI(chainId, fromBlock, toBlock), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
 
-    const erc20Transfers = transferLogs.map((log) => {
-      try {
-        const decoded = ethers.AbiCoder.defaultAbiCoder().decode(
-          ["uint256"],
-          log.data
-        )
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const volumeByAsset = await response.json()
+
+    const asssets = Object.keys(volumeByAsset)
+    const volumes = Object.values(volumeByAsset)
     
-        return {
-          asset: log.address,
-          value: decoded[0].toString(),
-        }
-      } catch (error) {
-        console.error("Error decoding log:", error)
-        return null
-      }
-    }).filter(t => !!t)
+    dailyVolume.add(asssets, volumes)
 
-    dailyVolume.add(erc20Transfers.map(t => t.asset), erc20Transfers.map(t => t.value))
-
-    return { dailyVolume }
+    return {
+      dailyVolume,
+    }
   }
 }
 
@@ -102,7 +107,7 @@ const adapter: Adapter = {
   version: 2,
   adapter: {
     ...Object.fromEntries(
-      Object.keys(META_AGGREGATOR_ADDRESS).map(chain => [
+      Object.keys(SUPPORTED_CHAIN_MAPPING).map(chain => [
         chain, 
         { fetch: createEVMVolumeFetcher(chain), start: '2025-03-01' }
       ])
