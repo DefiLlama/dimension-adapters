abacf3f55095d4f160cd2cd07153ddd30a5abd2d
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-03-29 11:16:43 +0700

    use event for fee tracking instead

--- fees/cian-yieldlayer/index.ts
+++ fees/cian-yieldlayer/index.ts
@@ -18,9 +18,12 @@ const cianVaults = {
         // "LRT Yield Layer - rstETH": "0x7CB6Bb4320622B85b265f596a4AD9f7Cc85F8797", these two doesn't have verified contract so we skip them
         "fBTC Yield Layer Ⅲ": "0x9fdDAD44eD6b77e6777dC1b16ee4FCcCBaF0A019"
     },
-    // [CHAIN.ARBITRUM]: {
-    //     "rsETH Yield Layer": "0x15cbFF12d53e7BdE3f1618844CaaEf99b2836d2A",
-    // },
+    [CHAIN.ARBITRUM]: {
+        "rsETH Yield Layer": "0x15cbFF12d53e7BdE3f1618844CaaEf99b2836d2A",
+    },
+    [CHAIN.BSC]: {
+        "slisBNB Yield Layer": "0x406e1e0e3cb4201B4AEe409Ad2f6Cd56d3242De7"
+    }
     // [CHAIN.SEI]: {
     //     "redBTC Yield Layer": "0xff6771a9565F18638faB2972BA7Fc798ad8bCad0"
     // }
@@ -28,10 +31,12 @@ const cianVaults = {
 
 const cianVaultABI = {
     getVaultParams: "function getVaultParams() view returns (tuple(address underlyingToken, string name, string symbol, uint256 marketCapacity, uint256 managementFeeRate, uint256 managementFeeClaimPeriod, uint256 maxPriceUpdatePeriod, uint256 revenueRate, uint256 exitFeeRate, address admin, address rebalancer, address feeReceiver, address redeemOperator))",
-    totalAssets: "function totalAssets() view returns (uint256)"
+    totalAssets: "function totalAssets() view returns (uint256)",
+    updateExchangePrice: "event UpdateExchangePrice(uint256 newExchangePrice, uint256 newRevenue)",
+    collectManagementFee: "event CollectManagementFee(uint256 managementFee)",
 }
 
-const fetch = async ({createBalances, api, fromApi, toApi, chain}: FetchOptions) => {
+const fetch = async ({createBalances, api, getLogs, chain}: FetchOptions) => {
     const dailyFees = createBalances()
     // call the vaults
     const vaults = Object.values(cianVaults[chain]) as string[]
@@ -42,23 +47,30 @@ const fetch = async ({createBalances, api, fromApi, toApi, chain}: FetchOptions)
         permitFailure: true
     })).map((params) => ({
         token: params.underlyingToken,
-        managementFeeRate: params.managementFeeRate,
         revenueRate: params.revenueRate
     }))
 
-    const vaultsTotalAssets = await fromApi.multiCall({
-        abi: cianVaultABI.totalAssets,
-        calls: vaults
-    })
+    const vaultsUpdateExchangePrice = (await getLogs({
+        targets: vaults,
+        eventAbi: cianVaultABI.updateExchangePrice,
+        flatten: false
+    })).map((logs) => {
+        return logs.reduce((totalRevenue, log) => totalRevenue + log.newRevenue, 0n);
+    });
 
-    const vaultsTotalAssetsEnd = await toApi.multiCall({
-        abi: cianVaultABI.totalAssets,
-        calls: vaults
-    })
+    const vaultsManagementFee = (await getLogs({
+        targets: vaults,
+        eventAbi: cianVaultABI.collectManagementFee,
+        flatten: false
+    })).map((logs) => {
+        return logs.reduce((totalManagement, log) => totalManagement + log.managementFee, 0n);
+    });
 
-    vaultsTotalAssetsEnd.forEach((totalAssets, i) => {
-        dailyFees.add(vaultsParams[i].token, (totalAssets - vaultsTotalAssets[i]) * vaultsParams[i].managementFeeRate / 1e4)
-        dailyFees.add(vaultsParams[i].token, (totalAssets - vaultsTotalAssets[i]) * vaultsParams[i].revenueRate / 1e4)
+    vaultsUpdateExchangePrice.forEach((data, i) => {
+        dailyFees.add(vaultsParams[i].token, data)
+    })
+    vaultsManagementFee.forEach((data, i) => {
+        dailyFees.add(vaultsParams[i].token, data)
     })
 
     return {dailyFees, dailyRevenue: dailyFees}
@@ -77,13 +89,18 @@ const adapters: Adapter = {
                 methodology
             }
         },
-        // [CHAIN.ARBITRUM]: {
-        //     fetch: fetch,
-        //     start: '2025-01-31',
-        //     meta: {
-        //         methodology
-        //     }
-        // },
+        [CHAIN.BSC]: {
+            fetch: fetch,
+            meta: {
+                methodology
+            }
+        },
+        [CHAIN.ARBITRUM]: {
+            fetch: fetch,
+            meta: {
+                methodology
+            }
+        },
         // [CHAIN.SEI]: {
         //     fetch: fetch,
         //     start: '2025-01-31',
