0b26c8dbfc5c516d97e4610a56e8c66da3262655
Yagami <103039735+yagamipro@users.noreply.github.com>
2025-03-31 23:34:39 +0400

    fix: BullX Fees Aggregation

--- fees/bullx.ts
+++ fees/bullx.ts
@@ -3,34 +3,12 @@ import { CHAIN } from "../helpers/chains";
 import { queryDuneSql } from "../helpers/dune";
 
 const fetch: any = async (options: FetchOptions) => {
-  const fees = await queryDuneSql(options, `
-WITH
-allFeePayments AS (
-    SELECT
-      tx_id,
-      'SOL' AS feeTokenType,
-      balance_change / 1e9 AS fee_token_amount,
-      'So11111111111111111111111111111111111111112' AS fee_token_mint_address 
-    FROM
-      solana.account_activity
-    WHERE
-      tx_success
-      AND address = 'F4hJ3Ee3c5UuaorKAMfELBjYCjiiLH75haZTKqTywRP3' 
-      OR address = '9RYJ3qr5eU5xAooqVcbmdeusjcViL5Nkiq7Gske3tiKq'
-      AND balance_change > 0 
-      AND TIME_RANGE
-)
-    SELECT
-      SUM(fee_token_amount) AS fee
-    FROM
-      dex_solana.trades AS trades
-      JOIN allFeePayments AS feePayments ON trades.tx_id = feePayments.tx_id
-    WHERE
-      trades.trader_id != 'F4hJ3Ee3c5UuaorKAMfELBjYCjiiLH75haZTKqTywRP3'
-      AND TIME_RANGE
-`)
-  const dailyFees = options.createBalances()
-  dailyFees.add('So11111111111111111111111111111111111111112', fees[0].fee*1e9);
+  const targets = [
+    'F4hJ3Ee3c5UuaorKAMfELBjYCjiiLH75haZTKqTywRP3',
+    '9RYJ3qr5eU5xAooqVcbmdeusjcViL5Nkiq7Gske3tiKq',
+  ]
+
+  const dailyFees = await getSolanaReceived({ options, targets, });
   return { dailyFees, dailyRevenue: dailyFees, }
 }
 
