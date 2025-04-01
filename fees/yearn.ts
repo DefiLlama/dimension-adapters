7b9c02880df69c64ba0f0b5f6bb2b3f8cb55536a
Michael Chan <michaelchan@Michaels-MacBook-Pro-2.local>
2025-04-01 13:25:00 +0800

    added fees and revenue for yearn on ethereum

---
+++ fees/yearn.ts
@@ -0,0 +1,54 @@
+import * as sdk from "@defillama/sdk";
+import { Adapter } from "../adapters/types";
+import { ETHEREUM } from "../helpers/chains";
+import { request, gql } from "graphql-request";
+import type { ChainEndpoints } from "../adapters/types"
+import { Chain } from '@defillama/sdk/build/general';
+import { getTimestampAtStartOfDayUTC } from "../utils/date";
+import BigNumber from "bignumber.js";
+
+const endpoints = {
+  [ETHEREUM]: sdk.graph.modifyEndpoint('FDLuaz69DbMADuBjJDEcLnTuPnjhZqNbFVrkNiBLGkEg'),
+}
+
+const graphs = (graphUrls: ChainEndpoints) => {
+  return (chain: Chain) => {
+    return async (timestamp: number) => {
+      const dateId = Math.floor(getTimestampAtStartOfDayUTC(timestamp) / 86400)
+
+      const graphQuery = gql
+      `{
+        financialsDailySnapshot(id: ${dateId}) {
+            dailyTotalRevenueUSD
+            dailyProtocolSideRevenueUSD
+            dailySupplySideRevenueUSD
+        }
+      }`;
+
+      const graphRes = await request(graphUrls[chain], graphQuery);
+
+      const dailyFee = new BigNumber(graphRes.financialsDailySnapshot.dailyTotalRevenueUSD);
+      const dailyRevenue = new BigNumber(graphRes.financialsDailySnapshot.dailyProtocolSideRevenueUSD);
+      const dailySSRev = new BigNumber(graphRes.financialsDailySnapshot.dailySupplySideRevenueUSD);
+
+      return {
+        timestamp,
+        dailyFees: dailyFee.toString(),
+        dailyRevenue: dailyRevenue.toString(),
+        dailySupplySideRevenue: dailySSRev.toString(),
+      };
+    };
+  };
+};
+
+
+const adapter: Adapter = {
+  adapter: {
+    [ETHEREUM]: {
+        fetch: graphs(endpoints)(ETHEREUM),
+        start: '2023-01-01',
+    },
+  }
+}
+
+export default adapter;
