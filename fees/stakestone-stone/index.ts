1322b2e892b599db820a137ac058f7f9b152bbe4
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-04-01 20:12:44 +0700

    use sharePrice at start and end instead because round is not rerolled every day

--- fees/stakestone-stone/index.ts
+++ fees/stakestone-stone/index.ts
@@ -1,72 +1,73 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { ChainApi } from "@defillama/sdk";
 
 const STONE_VAULT = "0xa62f9c5af106feee069f38de51098d9d81b90572"
 const STONE_TOKEN = "0x7122985656e38bdc0302db86685bb972b145bd3c"
+
+const DECIMALS = 10n ** 18n;
+
 const abi = {
-    roundPricePerShare: "function roundPricePerShare(uint256) external view returns (uint256)",
-    settlementTime:"function settlementTime(uint256) external view returns (uint256)",
+  currentSharePrice: "function currentSharePrice() external view returns (uint256)",
+  withdrawingSharesInPast: "function withdrawingSharesInPast() external view returns (uint256)",
+  totalSupply: "function totalSupply() external view returns (uint256)",
 }
 
-const fetch = async ({createBalances, api,}: FetchOptions) => {
-    const roundID = await api.call({
-        target: STONE_VAULT,
-        abi: "function latestRoundID() external view returns (uint256)",
-    })
+async function getVaultState(api: ChainApi): Promise<{ sharePrice: string, totalSupply: string, withdrawingShares: string }> {
+  const [sharePrice, totalSupply, withdrawingShares] = await Promise.all([
+    api.call({
+      target: STONE_VAULT,
+      abi: abi.currentSharePrice,
+    }),
+    api.call({
+      target: STONE_TOKEN,
+      abi: abi.totalSupply,
+    }),
+    api.call({
+      target: STONE_VAULT,
+      abi: abi.withdrawingSharesInPast,
+    }),
+  ]);
+  return { sharePrice, totalSupply, withdrawingShares };
+}
 
-    const [priceStart, priceEnd, timeStart, timeEnd, totalSupply] = await Promise.all([
-        api.call({
-            target: STONE_VAULT,
-            abi: abi.roundPricePerShare,
-            params: [roundID - 2]
-        }),
-        api.call({
-            target: STONE_VAULT,
-            abi: abi.roundPricePerShare,
-            params: [roundID - 1] 
-        }),
-        api.call({
-            target: STONE_VAULT,
-            abi: abi.settlementTime,
-            params: [roundID - 2] 
-        }),
-        api.call({
-            target: STONE_VAULT,
-            abi: abi.settlementTime,
-            params: [roundID - 1] 
-        }),
-        api.call({
-            target: STONE_TOKEN,
-            abi: "function totalSupply() external view returns (uint256)",
-        })
-    ])
-    
+const fetch = async (options: FetchOptions) => {
+    const { createBalances, api, fromApi } = options;
+  
+    const startState = await getVaultState(fromApi);
+    const endState = await getVaultState(api);
 
-    const priceIncrease = priceEnd - priceStart
-    const ethRewards = (priceIncrease * totalSupply) / 1e18
-    const timeDiff = timeEnd - timeStart
-    const dailyEthRewards = (ethRewards * 86400) / timeDiff
-
+    const sharePriceStart = BigInt(startState.sharePrice);
+    const sharePriceEnd = BigInt(endState.sharePrice);
+  
+    const totalSupplyStart = BigInt(startState.totalSupply);
+    const withdrawingSharesStart = BigInt(startState.withdrawingShares);
+    const activeSharesStart = totalSupplyStart - withdrawingSharesStart;
+  
+    const priceIncrease = sharePriceEnd - sharePriceStart;
+  
+    const dailyEthRewards = (priceIncrease * activeSharesStart) / DECIMALS;
+  
     const dailyFees = createBalances()
-    dailyFees.addGasToken(dailyEthRewards)
-
+    dailyFees.addGasToken(dailyEthRewards);
+  
     return {
-        dailyFees
+      dailyFees
     }
-}
+  }
 
 const adapter: SimpleAdapter = {
-    version: 2,
-    adapter: {
-      [CHAIN.ETHEREUM]: {
-          fetch: fetch,
-          meta: {
-              methodology: {
-                  dailyFees: "Staking rewards earned by all staked ETH"
-              }
-          }
+  version: 2,
+  adapter: {
+    [CHAIN.ETHEREUM]: {
+      fetch: fetch,
+      meta: {
+        methodology: {
+          dailyFees: "Staking rewards earned by all staked ETH",
+        }
       }
     }
-  };
-  
-  export default adapter;
+  }
+};
+
+export default adapter;
