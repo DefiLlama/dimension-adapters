01058d5636773b1895f24139ba8e4d83ad5b5886
treeoflife2 <solanalyzoor@gmail.com>
2025-04-02 20:13:47 +0530

    [chain] near fees (#2712)
    * [chain] near fees
    
    * Fix: start date
    
    * change to v1 adapter
    
    * used official api and docs for fees calc
    
    * reuse existing near chain file
    
    * refactor

--- fees/near/index.ts
+++ fees/near/index.ts
@@ -1,39 +1,44 @@
-import { FetchResultFees, ProtocolType, SimpleAdapter } from "../../adapters/types";
+import { Adapter, ProtocolType } from "../../adapters/types";
+import { httpGet } from "../../utils/fetchURL";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import { httpGet } from "../../utils/fetchURL";
-interface IChart {
+
+interface ChartData {
   date: string;
   txn_fee_usd: string;
 }
-const fetchFees =  async (timestamp: number): Promise<FetchResultFees> => {
-  const todayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  const url  = 'https://api.nearblocks.io/v1/charts'
-  const value: IChart[] = (await httpGet(url, {
-    headers: {
-    'origin': 'https://nearblocks.io',
-    'referer': 'https://nearblocks.io/charts' ,
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
-  }})).charts;
-  // const value: IChart[] = require('./near.json');
 
+const feesAPI = 'https://api.nearblocks.io/v1/charts';
+
+const fetch = async (timestamp: number) => {
+  const todayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
   const dateStr = new Date(todayTimestamp * 1000).toISOString().split('T')[0];
-  const dailyFees = value.find((item) => item.date.split('T')[0] === dateStr)?.txn_fee_usd;
+
+  const feesData = await httpGet(feesAPI);
+
+  const dailyFees = feesData.charts.find((chart: ChartData) =>
+    chart.date.split('T')[0] === dateStr
+  )?.txn_fee_usd;
+
   return {
     timestamp,
     dailyFees: dailyFees ? `${dailyFees}` : undefined,
-    dailyRevenue: dailyFees ? `${dailyFees}` : undefined,
-  }
-}
+    dailyRevenue: dailyFees ? `${dailyFees}` : undefined
+  };
+};
 
-const adapter: SimpleAdapter = {
+const adapter: Adapter = {
   version: 1,
   adapter: {
     [CHAIN.NEAR]: {
-      fetch: fetchFees,
-      start: '2020-07-21'
-    }
+      fetch,
+      start: '2020-07-21',
+      meta: {
+        methodology: "We fetch daily transaction fees from NearBlocks API. The data is aggregated daily and includes all transaction fees paid on the NEAR blockchain. 70% of transaction fees are burned, while 30% can optionally be allocated to smart contract developers as rewards if they specify a fee percentage for their contracts (otherwise 100% is burned). Note that validators do not earn transaction fees - their rewards come from protocol-level inflation."
+      }
+    },
   },
   protocolType: ProtocolType.CHAIN
-}
+};
+
 export default adapter;
