8910a3e9b1dca6fe3ea9660aa0069ca472ea82b3
bilenko-ol <bilenko.le@gmail.com>
2025-04-03 18:53:57 +0300

    add vest adapter

---
+++ dexs/vest/index.ts
@@ -0,0 +1,25 @@
+import {CHAIN} from "../../helpers/chains";
+import {FetchResultVolume, SimpleAdapter} from "../../adapters/types";
+import {getUniqStartOfTodayTimestamp} from "../../helpers/getUniSubgraphVolume";
+import fetchURL from "../../utils/fetchURL"
+
+const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
+    const toDayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+    const fromDayTimestamp = toDayTimestamp - 86400; // 60*60*24
+
+    const data = (await fetchURL(`https://serverprod.vest.exchange/v2/exchangeInfo/volume?from_date=${fromDayTimestamp * 1000}&to_date=${toDayTimestamp * 1000}`));
+    return {
+        dailyVolume: data.total,
+        timestamp: timestamp,
+    };
+};
+
+const adapter: SimpleAdapter = {
+    adapter: {
+        [CHAIN.BASE]: {
+            fetch,
+            start: '2025-01-01',
+        },
+    },
+};
+export default adapter;
