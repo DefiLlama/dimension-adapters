341ee6d7d7d71516a2da85e2bff61ef4e62112a1
treeoflife2 <solanalyzoor@gmail.com>
2025-04-04 14:00:33 +0530

    fix: syntax error

--- fees/jup-ape.ts
+++ fees/jup-ape.ts
@@ -2,7 +2,7 @@ import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { getSolanaReceived } from "../helpers/token";
 
-const fetchFeesSolana = async (options: FetchOptions) => {
+const fetchFeesSolana = async (_as:any, _b:any, options: FetchOptions) => {
   const dailyFees = await getSolanaReceived({ options, target: '5YET3YapxD6to6rqPqTWB3R9pSbURy6yduuUtoZkzoPX' })
   return { dailyFees, dailyRevenue: dailyFees, }
 }
