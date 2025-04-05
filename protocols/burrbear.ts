d00d38012780c59da0635ff554f951db83beb4d1
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-04-05 13:51:48 +0700

    add type

--- protocols/burrbear.ts
+++ protocols/burrbear.ts
@@ -2,6 +2,11 @@ import request, { gql } from "graphql-request";
 import { FetchOptions, FetchResultV2, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
+interface Balancer {
+  totalSwapFee: number;
+  totalSwapVolume: number;
+  totalProtocolFee: number;
+}
 
 const fetch = async ({ getFromBlock, getToBlock }: FetchOptions): Promise<FetchResultV2> => {
   const [fromBlock, toBlock] = await Promise.all([getFromBlock(), getToBlock()]);
@@ -13,12 +18,13 @@ const fetch = async ({ getFromBlock, getToBlock }: FetchOptions): Promise<FetchR
 
   const graphRes: any = await request('https://api.goldsky.com/api/public/project_cluukfpdrw61a01xag6yihcuy/subgraphs/berachain/prod/gn', graphQuery);
 
-  const totalVolume = graphRes.today.reduce((p, c) => p + c.totalSwapVolume, 0);
-  const totalFees = graphRes.today.reduce((p, c) => p + c.totalSwapFee, 0);
-  const totalProtocolFees = graphRes.today.reduce((p, c) => p + c.totalProtocolFee, 0);
-  const previousVolume = graphRes.yesterday.reduce((p, c) => p + c.totalSwapVolume, 0);
-  const previousFees = graphRes.yesterday.reduce((p, c) => p + c.totalSwapFee, 0);
-  const previousProtocolFees = graphRes.yesterday.reduce((p, c) => p + c.totalProtocolFee, 0);
+
+  const totalVolume: number = graphRes.today.reduce((p: number, c: Balancer) => p + c.totalSwapVolume, 0);
+  const totalFees = graphRes.today.reduce((p: number, c: Balancer) => p + c.totalSwapFee, 0);
+  const totalProtocolFees = graphRes.today.reduce((p: number, c: Balancer) => p + c.totalProtocolFee, 0);
+  const previousVolume = graphRes.yesterday.reduce((p: number, c: Balancer) => p + c.totalSwapVolume, 0);
+  const previousFees = graphRes.yesterday.reduce((p: number, c: Balancer) => p + c.totalSwapFee, 0);
+  const previousProtocolFees = graphRes.yesterday.reduce((p: number, c: Balancer) => p + c.totalProtocolFee, 0);
 
   const dailyVolume = totalVolume - previousVolume;
   const dailyFees = totalFees - previousFees;
