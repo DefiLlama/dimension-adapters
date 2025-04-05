b6f38c2484e4bbf42c1375142578466c088a640c
0xgnek <0xgnek@gmail.com>
2025-04-05 12:24:27 +0000

    fix query by timestamp

--- dexs/vest/index.ts
+++ dexs/vest/index.ts
@@ -2,13 +2,13 @@ import {CHAIN} from "../../helpers/chains";
 import {FetchResultVolume, SimpleAdapter} from "../../adapters/types";
 import {getUniqStartOfTodayTimestamp} from "../../helpers/getUniSubgraphVolume";
 import fetchURL from "../../utils/fetchURL"
+import { FetchOptions } from "../../adapters/types";
 
-const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
-
-    const toDayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-    const fromDayTimestamp = toDayTimestamp - 86400; // 60*60*24
-
-    const data = (await fetchURL(`https://serverprod.vest.exchange/v2/exchangeInfo/volume?from_date=${fromDayTimestamp * 1000}&to_date=${toDayTimestamp * 1000}`));
+const fetch = async (timestamp: number, _: any, options: FetchOptions): Promise<FetchResultVolume> => {
+    const from_date = getUniqStartOfTodayTimestamp(new Date(options.startOfDay * 1000));
+    const to_date = from_date + 86400;
+    const data = (await fetchURL(`https://serverprod.vest.exchange/v2/exchangeInfo/volume?from_date=${from_date * 1000}&to_date=${to_date * 1000}`));
+    console.log(data)
     return {
         dailyVolume: data.total,
         timestamp: timestamp,
