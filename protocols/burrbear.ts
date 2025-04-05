a0f068d026b1456ba7e26b14e978e4c1f003da5d
treeoflife2 <solanalyzoor@gmail.com>
2025-04-05 16:51:11 +0530

    Fix: burrbear new subgraph and total volume and total fees

--- protocols/burrbear.ts
+++ protocols/burrbear.ts
@@ -1,33 +1,73 @@
-import request from "graphql-request"
+import { graph } from "@defillama/sdk";
 import { FetchOptions, SimpleAdapter } from "../adapters/types"
 import { CHAIN } from "../helpers/chains"
+import { getBlock } from "../helpers/getBlock"
 
-const url = 'https://api.goldsky.com/api/public/project_clq1h5ct0g4a201x18tfte5iv/subgraphs/bgt-subgraph/v1000000/gn'
+const url = 'https://api.goldsky.com/api/public/project_cluukfpdrw61a01xag6yihcuy/subgraphs/berachain/prod/gn'
 
-
-interface IProtocolData {
-    volumeUsd: number
-    feesUsd: number
+interface IPool {
+  address: string
+  symbol: string
+  totalSwapVolume: number
+  totalSwapFee: number
 }
+
+interface GraphQLResponse {
+  yesterday: { pool0: IPool[] }
+  today: { pool0: IPool[] }
+}
+
 const fetchVolume = async (options: FetchOptions) => {
-  const querry = `
-    {
-        poolDayDatas(where: {date_gte: ${options.startTimestamp}, date_lt: ${options.endTimestamp}}) {
-            date
-            volumeUsd
-            feesUsd
-        }
-    }`
+  const [todaysBlock, yesterdaysBlock] = await Promise.all([
+    getBlock(options.endTimestamp, options.chain, {}),
+    getBlock(options.startTimestamp, options.chain, {}),
+  ]);
 
-  const respose = (await request(url, querry)).poolDayDatas as IProtocolData[]
+  const query = `{
+    yesterday: pools(
+      first: 1000
+      where: { totalSwapVolume_gt: "0" }
+      block: { number: ${yesterdaysBlock} }
+    ) {
+      address
+      symbol
+      totalSwapVolume
+      totalSwapFee
+    }
+    today: pools(
+      first: 1000
+      where: { totalSwapVolume_gt: "0" }
+      block: { number: ${todaysBlock} }
+    ) {
+      address
+      symbol
+      totalSwapVolume
+      totalSwapFee
+    }
+  }`
 
-  const dailyVolume = options.createBalances();
-  dailyVolume.addUSDValue(Number(respose.reduce((acc, item) => acc + Number(item.volumeUsd), 0)))
-  const dailyFees = dailyVolume.clone();
-  dailyFees.addUSDValue(Number(respose.reduce((acc, item) => acc + Number(item.feesUsd), 0)))
+  const response = await graph.request(url, query) as {
+    yesterday: IPool[]
+    today: IPool[]
+  }
+
+  const yesterdayTotalVolume = response.yesterday.reduce((acc, pool) => acc + Number(pool.totalSwapVolume), 0);
+  const yesterdayTotalFees = response.yesterday.reduce((acc, pool) => acc + Number(pool.totalSwapFee), 0);
+
+  const todayTotalVolume = response.today.reduce((acc, pool) => acc + Number(pool.totalSwapVolume), 0);
+  const todayTotalFees = response.today.reduce((acc, pool) => acc + Number(pool.totalSwapFee), 0);
+
+  const volumeDiff = todayTotalVolume - yesterdayTotalVolume;
+  const feesDiff = todayTotalFees - yesterdayTotalFees;
+
+  const dailyVolume = (volumeDiff > 0 ? volumeDiff : 0);
+  const dailyFees = (feesDiff > 0 ? feesDiff : 0);
+
   return {
     dailyVolume: dailyVolume,
+    totalVolume: todayTotalVolume,
     dailyFees: dailyFees,
+    totalFees: todayTotalFees,
   }
 }
 
@@ -35,7 +75,8 @@ const adapters: SimpleAdapter = {
   version: 2,
   adapter: {
     [CHAIN.BERACHAIN]: {
-      fetch: fetchVolume
+      fetch: fetchVolume,
+      start: '2025-01-25'
     }
   }
 }
