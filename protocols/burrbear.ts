fef8d1680bf2bbf6079bf25623cf297b03f8eabc
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-04-05 20:00:24 +0700

    fix burrbear fees & revenue (#2803)
    * fix burrbear fees & revenue
    
    * add type

--- protocols/burrbear.ts
+++ protocols/burrbear.ts
@@ -1,43 +1,51 @@
-import request from "graphql-request"
-import { FetchOptions, SimpleAdapter } from "../adapters/types"
-import { CHAIN } from "../helpers/chains"
+import request, { gql } from "graphql-request";
+import { FetchOptions, FetchResultV2, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
 
-const url = 'https://api.goldsky.com/api/public/project_clq1h5ct0g4a201x18tfte5iv/subgraphs/bgt-subgraph/v1000000/gn'
-
-
-interface IProtocolData {
-    volumeUsd: number
-    feesUsd: number
+interface Balancer {
+  totalSwapFee: number;
+  totalSwapVolume: number;
+  totalProtocolFee: number;
 }
-const fetchVolume = async (options: FetchOptions) => {
-  const querry = `
-    {
-        poolDayDatas(where: {date_gte: ${options.startTimestamp}, date_lt: ${options.endTimestamp}}) {
-            date
-            volumeUsd
-            feesUsd
-        }
-    }`
 
-  const respose = (await request(url, querry)).poolDayDatas as IProtocolData[]
+const fetch = async ({ getFromBlock, getToBlock }: FetchOptions): Promise<FetchResultV2> => {
+  const [fromBlock, toBlock] = await Promise.all([getFromBlock(), getToBlock()]);
 
-  const dailyVolume = options.createBalances();
-  dailyVolume.addUSDValue(Number(respose.reduce((acc, item) => acc + Number(item.volumeUsd), 0)))
-  const dailyFees = dailyVolume.clone();
-  dailyFees.addUSDValue(Number(respose.reduce((acc, item) => acc + Number(item.feesUsd), 0)))
+  const graphQuery = gql`query fees {
+        today:balancers(block: { number: ${toBlock}}) { totalSwapFee totalSwapVolume totalProtocolFee }
+        yesterday:balancers(block: { number: ${fromBlock}}) { totalSwapFee totalSwapVolume totalProtocolFee }
+      }`;
+
+  const graphRes: any = await request('https://api.goldsky.com/api/public/project_cluukfpdrw61a01xag6yihcuy/subgraphs/berachain/prod/gn', graphQuery);
+
+
+  const totalVolume: number = graphRes.today.reduce((p: number, c: Balancer) => p + c.totalSwapVolume, 0);
+  const totalFees = graphRes.today.reduce((p: number, c: Balancer) => p + c.totalSwapFee, 0);
+  const totalProtocolFees = graphRes.today.reduce((p: number, c: Balancer) => p + c.totalProtocolFee, 0);
+  const previousVolume = graphRes.yesterday.reduce((p: number, c: Balancer) => p + c.totalSwapVolume, 0);
+  const previousFees = graphRes.yesterday.reduce((p: number, c: Balancer) => p + c.totalSwapFee, 0);
+  const previousProtocolFees = graphRes.yesterday.reduce((p: number, c: Balancer) => p + c.totalProtocolFee, 0);
+
+  const dailyVolume = totalVolume - previousVolume;
+  const dailyFees = totalFees - previousFees;
+  const dailyRevenue = totalProtocolFees - previousProtocolFees;
+
   return {
-    dailyVolume: dailyVolume,
-    dailyFees: dailyFees,
-  }
-}
+    dailyVolume,
+    totalVolume,
+    dailyFees,
+    totalFees,
+    dailyRevenue,
+    totalRevenue: totalProtocolFees,
+  };
+};
 
-const adapters: SimpleAdapter = {
+
+const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
-    [CHAIN.BERACHAIN]: {
-      fetch: fetchVolume
-    }
-  }
-}
+    [CHAIN.BERACHAIN]: { fetch, }
+  },
+};
 
-export default adapters
+export default adapter;
