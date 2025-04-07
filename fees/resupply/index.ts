1e789e97aa1ca4f628cc535f6134dc981158d440
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-07 13:46:30 +0200

    add redemption fee tracking and update revenue calculations

--- fees/resupply/index.ts
+++ fees/resupply/index.ts
@@ -6,43 +6,25 @@ const feeDepostController = "0x7E3D2F480AbbA95863040D763DDe8F30D100C6F5"
 
 const abi = {
   addInterest: "event AddInterest(uint256 interestEarned, uint256 rate)",
+  redeemed: "event Redeemed(address indexed _caller, uint256 _amount, uint256 _collateralFreed, uint256 _protocolFee, uint256 _debtReduction)",
   splits: "function splits() external view returns (tuple(uint80 insurance, uint80 treasury, uint80 platform))"
 }
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances()
-  const dailyProtocolRevenue = options.createBalances()
-  const dailyHoldersRevenue = options.createBalances()
-  const dailyRevenue = options.createBalances()
   const pairContracts = await options.api.call({ abi: 'address[]:getAllPairAddresses', target: '0x10101010E0C3171D894B71B3400668aF311e7D94' })
   const splits = await options.api.call({ target: feeDepostController, abi: abi.splits })
 
-  const treasuryRatio = BigInt(splits.treasury)
-  const platformRatio = BigInt(splits.platform)
+  const addInterestLogs = (await options.getLogs({ targets: pairContracts as any, eventAbi: abi.addInterest, }))
+  const redeemedLogs = (await options.getLogs({ targets: pairContracts as any, eventAbi: abi.redeemed, }))
 
-  const addInterestLogs = (await options.getLogs({
-    targets: pairContracts as any,
-    eventAbi: abi.addInterest,
-    flatten: false
-  }))
+  addInterestLogs.forEach((log) => dailyFees.add(reUSD, log.interestEarned))
+  redeemedLogs.forEach((log) => dailyFees.add(reUSD, log._protocolFee))
 
-  addInterestLogs.forEach((logs) => {
-    if (!logs.length) return
-    let totalFees = 0n
-    for (const log of logs) {
-      const fees = BigInt(log.interestEarned.toString())
-      totalFees += fees
-    }
-    dailyFees.add(reUSD, totalFees)
-    const treasuryFees = (totalFees * treasuryRatio) / 10_000n
-    const holdersFees = (totalFees * platformRatio) / 10_000n
-    dailyHoldersRevenue.add(reUSD, holdersFees)
-    dailyProtocolRevenue.add(reUSD, treasuryFees)
-    dailyRevenue.add(reUSD, treasuryFees + holdersFees)
-  })
-
-  const dailySupplySideRevenue = dailyFees.clone()
-  dailySupplySideRevenue.subtract(dailyRevenue)
+  const dailySupplySideRevenue = dailyFees.clone(splits.insurance / 1e4)
+  const dailyRevenue = dailyFees.clone((+splits.treasury + +splits.platform) / 1e4)
+  const dailyHoldersRevenue = dailyFees.clone(splits.platform / 1e4)
+  const dailyProtocolRevenue = dailyFees.clone(splits.treasury / 1e4)
 
   return {
     dailyFees,
@@ -51,16 +33,14 @@ const fetch = async (options: FetchOptions) => {
     dailyProtocolRevenue,
     dailyHoldersRevenue
   }
-
 }
 
-
 const methodology = {
-  dailyFees: "Total interest paid by borrowers",
+  dailyFees: "Total interest paid by borrowers + redemption fees",
   dailyRevenue: "Protocol's share of interest (treasury + RSUP stakers)",
   dailyProtocolRevenue: "Treasury's portion of interest",
   dailyHoldersRevenue: "Platform fees distributed to RSUP stakers",
-  dailySupplySideRevenue: "Interest paid to lenders"
+  dailySupplySideRevenue: "Interest paid to lenders in the insurance pool"
 }
 
 const adapters: Adapter = {
