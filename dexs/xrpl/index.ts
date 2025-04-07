ff3117b8319cc82f2f4b31710462eac17501a2d9
WHYTEWYLL <juanmiblanco1@gmail.com>
2025-04-07 18:32:37 +0300

    add: sleep limit

--- dexs/xrpl/index.ts
+++ dexs/xrpl/index.ts
@@ -1,11 +1,17 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { httpPost, httpGet } from "../../utils/fetchURL";
+import { getCache, setCache } from "../../helpers/cache";
+import { cache } from "@defillama/sdk";
+
 
 const RPC = "https://xrplcluster.com";
 const XRPSCAN_POOLS_API = "https://api.xrpscan.com/api/v1/amm/pools";
-const LEDGER_SAMPLE_SIZE = 1000;
 const LEDGER_STEP = 10;
+
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 
 const fetchAllAMMPools = async () => {
   let offset = 0;
@@ -21,25 +27,44 @@ const fetchAllAMMPools = async () => {
 };
 
 const fetch = async (options: FetchOptions) => {
+
+
+  
   const dailyFees = options.createBalances();
   const dailyVolume = options.createBalances();
-  // Precargar todos los AMMs
+  
+
   const pools = await fetchAllAMMPools();
   const ammMap = new Map<string, number>();
   for (const pool of pools) {
     if (pool.index && pool.TradingFee !== undefined) {
-      ammMap.set(pool.index, pool.TradingFee / 1_000_000); // convertir a porcentaje
+      ammMap.set(pool.index, pool.TradingFee / 1_000_000);
     }
   }
 
+  const cachedLedger  = await getCache('xrpl-dex', 'lastLedger') || 95249409;
+  const currentLedgerInfo = await httpPost(RPC, {
+    method: "ledger",
+    params: [{
+      ledger_index: "validated",
+      expand: false,
+    }],
+  });
+  
+  const currentLedger = currentLedgerInfo.result.ledger.ledger_index;
+  
+  const LEDGER_PER_DAY = 21600;
+  const startLedger = cachedLedger.currentLedger ? currentLedger - LEDGER_PER_DAY : currentLedger - LEDGER_PER_DAY;
+    
+  // Get a call to get current ledger index substract 1 to get the last ledger index
+  console.log(startLedger, currentLedger);
 
-  // Volume
-  // adjust for a daily run
-  const currentLedger = 95249409;
-  const startLedger = currentLedger; // For daily fetch
+  const ledgersToFetch = Math.ceil((currentLedger - startLedger) / LEDGER_STEP);
+  const sleepTime = ledgersToFetch > 300 ? 150 : 50;
 
   for (let i = startLedger; i <= currentLedger; i += LEDGER_STEP) {
     try {
+      await sleep(sleepTime)
       const ledger = await httpPost(RPC, {
         method: "ledger",
         params: [{
@@ -51,7 +76,6 @@ const fetch = async (options: FetchOptions) => {
 
       for (const tx of ledger.result.ledger.transactions ?? []) {
         if (!tx.metaData?.AffectedNodes) continue;
-
         if (
           (tx.TransactionType === "OfferCreate" || tx.TransactionType === "Payment") &&
           tx.metaData.TransactionResult === "tesSUCCESS"
@@ -60,24 +84,25 @@ const fetch = async (options: FetchOptions) => {
             const flags = node.ModifiedNode?.FinalFields?.Flags;
             return (
               node.ModifiedNode?.LedgerEntryType === "RippleState" &&
-              (flags & 0x01000000) !== 0
+              ((flags & 0x01000000) !== 0)
             );
           });
             if (tx.TransactionType === "OfferCreate") {
-              const sender = tx.Account;
               // using TakerPays and TakerGets to get the asset sent
               const sentRaw = tx.TakerPays?.value || tx.TakerPays || tx.TakerGets?.value || tx.TakerGets || "0";
               const sent = parseFloat(typeof sentRaw === "string" ? sentRaw : sentRaw.value || "0");
-              const assetSent = tx.TakerPays?.currency || tx.TakerPays?.value?.currency || tx.TakerGets?.currency || tx.TakerGets?.value?.currency || "XRP";
+              let assetSent = tx.TakerPays?.currency || tx.TakerPays?.value?.currency || tx.TakerGets?.currency || tx.TakerGets?.value?.currency || "XRP";
+              if (assetSent !== "XRP") {
+                const issuer = tx.SendMax?.issuer || tx.Amount?.issuer ;
+                if (issuer) {
+                  assetSent = assetSent + "." + issuer; 
+                }
+              }
               if (interactedWithAMM) { // fees are only charged if the transaction interacts with an AMM
               const ammNode = tx.metaData.AffectedNodes.find((node: any) =>
                 node.ModifiedNode?.FinalFields?.AMMID
               );
               const ammID = ammNode?.ModifiedNode?.FinalFields?.AMMID;
-              if (!ammID || !ammMap.has(ammID)) {
-                console.warn("No AMMID or AMMID not in pool map for tx", tx.hash);
-                continue;
-              }
               const tradingFee = ammMap.get(ammID) || 0;
               const ammFee = tradingFee * sent;
               dailyFees.add(assetSent, ammFee);
@@ -85,16 +110,18 @@ const fetch = async (options: FetchOptions) => {
           } else if (tx.TransactionType === "Payment") {
             const sentRaw = tx.SendMax?.value || tx.SendMax || tx.Amount?.value || tx.Amount || "0";
             const sent = parseFloat(typeof sentRaw === "string" ? sentRaw : sentRaw.value || "0");
-            const assetSent = tx.SendMax?.currency || tx.SendMax?.value?.currency || tx.Amount?.currency || tx.Amount?.value?.currency || "XRP";
+            let assetSent = tx.SendMax?.currency || tx.SendMax?.value?.currency || tx.Amount?.currency || tx.Amount?.value?.currency || "XRP";
+            if (assetSent !== "XRP") {
+              const issuer = tx.SendMax?.issuer || tx.Amount?.issuer ;
+              if (issuer) {
+                assetSent = assetSent + "." + issuer; 
+              }
+            }
             if (interactedWithAMM) { 
                 const ammNode = tx.metaData.AffectedNodes.find((node: any) =>
                   node.ModifiedNode?.FinalFields?.AMMID
                 );
                 const ammID = ammNode?.ModifiedNode?.FinalFields?.AMMID;
-                if (!ammID || !ammMap.has(ammID)) {
-                  console.warn("No AMMID or AMMID not in pool map for tx", tx.hash);
-                  continue;
-                }
                 const tradingFee = ammMap.get(ammID) || 0;
                 const ammFee = tradingFee * sent;
                 dailyFees.add(assetSent, ammFee);
@@ -110,6 +137,7 @@ const fetch = async (options: FetchOptions) => {
 
   console.log(dailyFees)
   console.log(dailyVolume)
+  await setCache('xrpl-dex', 'lastLedger', { lastLedger: currentLedger, lastUpdate: Date.now() });
   return { dailyFees, dailyVolume };
 };
 
