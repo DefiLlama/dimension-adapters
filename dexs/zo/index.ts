1b418a307f61430e53f5a2013beb6e57a3d42d81
Theo Legend <theo@sudofinance.xyz>
2025-04-08 15:22:12 -0700

    Add ZO in volume and fee

---
+++ dexs/zo/index.ts
@@ -0,0 +1,45 @@
+import fetchURL from '../../utils/fetchURL';
+import { Chain } from '@defillama/sdk/build/general';
+import { SimpleAdapter } from '../../adapters/types';
+import { CHAIN } from '../../helpers/chains';
+import { getUniqStartOfTodayTimestamp } from '../../helpers/getUniSubgraphVolume';
+
+type IUrl = {
+  [s: string]: string;
+};
+
+const url: IUrl = {
+  [CHAIN.SUI]: 'https://api.zofinance.io/volume',
+};
+
+interface IVolume {
+  totalVolume: number;
+  dailyVolume: number;
+}
+
+const fetch = (chain: Chain) => {
+  return async (timestamp: number) => {
+    const dayTimestamp = getUniqStartOfTodayTimestamp(
+      new Date(timestamp * 1000)
+    );
+    const volume: IVolume = await fetchURL(
+      `${url[chain]}?timestamp=${timestamp}`
+    );
+    return {
+      totalVolume: `${volume?.totalVolume}`,
+      dailyVolume: `${volume?.dailyVolume}`,
+      timestamp: dayTimestamp,
+    };
+  };
+};
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.SUI]: {
+      fetch: fetch(CHAIN.SUI),
+      start: '2025-04-06',
+    },
+  },
+};
+
+export default adapter;
