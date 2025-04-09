31ce933762a295657954014aaf9028a2e0ba017e
WHYTEWYLL <juanmiblanco1@gmail.com>
2025-04-09 14:51:10 +0300

    add: fees

--- dexs/bancor/index.ts
+++ dexs/bancor/index.ts
@@ -3,6 +3,8 @@ import { CHAIN } from "../../helpers/chains";
 
 import fetchURL from "../../utils/fetchURL"
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { queryDune } from "../../helpers/dune";
+
 
 interface BancorV2Response {
   data: Array<{
@@ -28,31 +30,41 @@ const endpoints = {
 };
 
 const fetchV3 = async (timestamp: number): Promise<FetchResult> => {
-  const res: BancorV3Response = (await fetchURL(v3Url)).data.totalVolume24h;
+  const res = (await fetchURL(v3Url))
+  const totalVolume : BancorV3Response = res?.data.totalVolume24h;
+  const totalFee: BancorV3Response = res?.data.totalFees24h; 
   return {
     timestamp,
-    dailyVolume: res.usd
+    dailyVolume: totalVolume.usd,
+    dailyFees: totalFee.usd,
   }
 }
 
 const graphs = (chain: string) =>
   async (timestamp: number, _chainBlocks: ChainBlocks): Promise<FetchResult> => {
     const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+
     switch (chain) {
       case "ethereum":
-        return fetchURL(endpoints.ethereum(dayTimestamp))
-          .then(({ data }: BancorV2Response) => {
-            const volume = data.find(item => (item.timestamp / 1000) === dayTimestamp)
-            if (!volume) throw new Error(`Unexpected error: No volume found for ${dayTimestamp}`)
-            return {
-              timestamp: dayTimestamp,
-              dailyVolume: volume?.usd || "0"
-            }
-          })
+        const { data }: BancorV2Response = await fetchURL(endpoints.ethereum(dayTimestamp))
+        const volume = data.find(item => (item.timestamp / 1000) === dayTimestamp)
+        if (!volume) throw new Error(`Unexpected error: No volume found for ${dayTimestamp}`)
+        // convert timestamp to date format
+        const date = new Date(dayTimestamp * 1000)
+        const formattedDate = date.toISOString().split('T')[0]
+        const fees = await queryDune("4954777", { daytime: formattedDate })
+        console.log(fees)
+        return {
+          timestamp: dayTimestamp,
+          dailyVolume: volume?.usd || "0",
+          dailyFees: fees[0]['conversion_fees_usd']?.usd || "0",
+        }
+
       default:
         throw new Error(`No adapter found for ${chain}`)
     }
   }
+
 
 
 
