27e8fdabc04662a0625995ae64c721b13738a3c5
theosudofi <theo@sudofinance.xyz>
2025-04-10 08:43:06 -0700

    Add ZO volume and fee calculation (#2838)
    * Add ZO in volume and fee
    
    * refactor
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ dexs/zo/index.ts
@@ -0,0 +1,27 @@
+import fetchURL from '../../utils/fetchURL'
+import { FetchOptions, SimpleAdapter } from '../../adapters/types'
+import { CHAIN } from '../../helpers/chains'
+
+const url = {
+  [CHAIN.SUI]: 'https://api.zofinance.io/volume',
+}
+
+const fetch = async (_1: number, _: any, { startOfDay, chain, }: FetchOptions) => {
+  const volume = await fetchURL(`${url[chain]}?timestamp=${startOfDay}`)
+  return {
+    totalVolume: volume?.totalVolume,
+    dailyVolume: volume?.dailyVolume,
+    timestamp: startOfDay,
+  }
+}
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.SUI]: {
+      fetch,
+      start: '2025-04-06',
+    },
+  },
+}
+
+export default adapter
