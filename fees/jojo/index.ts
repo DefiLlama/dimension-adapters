c09dcc3899359c9600d40a29aa7a51e98298fe1c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-10 17:06:26 +0200

    jojo: track revenue

--- fees/jojo/index.ts
+++ fees/jojo/index.ts
@@ -1,8 +1,8 @@
 import { FetchOptions, SimpleAdapter, FetchV2, FetchResultV2 } from '../../adapters/types'
 import { CHAIN } from '../../helpers/chains'
 
-const OrderFilledEvent =  "event OrderFilled(bytes32 indexed orderHash,address indexed trader,address indexed perp,int256 orderFilledPaperAmount,int256 filledCreditAmount,uint256 positionSerialNum,int256 fee)";
-const PositionFinalizeLogEvent =  "event PositionFinalizeLog(address indexed trader, int256 paperAmount, int256 creditAmount, int256 fee, int256 pnl, string perp)"
+const OrderFilledEvent = "event OrderFilled(bytes32 indexed orderHash,address indexed trader,address indexed perp,int256 orderFilledPaperAmount,int256 filledCreditAmount,uint256 positionSerialNum,int256 fee)";
+const PositionFinalizeLogEvent = "event PositionFinalizeLog(address indexed trader, int256 paperAmount, int256 creditAmount, int256 fee, int256 pnl, string perp)"
 
 const degenDealerAddress = '0xb7ffeaf4af97aece3c9ae7e5f68b9cd66d02f8ac';
 const perpAddress = '0x2f7c3cF9D9280B165981311B822BecC4E05Fe635';
@@ -17,10 +17,11 @@ const getFetch: FetchV2 = async (options: FetchOptions): Promise<FetchResultV2> 
         target: degenDealerAddress,
         eventAbi: PositionFinalizeLogEvent,
     })
-    orderLogs.forEach(log => dailyFees.addUSDValue(Math.abs(Number(log.fee)/Number(1e6))))
-    positionFinalizeLog.forEach(log => dailyFees.addUSDValue(Math.abs(Number(log.fee)/Number(1e6))))
+    orderLogs.forEach(log => dailyFees.addUSDValue(Math.abs(Number(log.fee) / Number(1e6))))
+    positionFinalizeLog.forEach(log => dailyFees.addUSDValue(Math.abs(Number(log.fee) / Number(1e6))))
     return {
         dailyFees: dailyFees,
+        dailyRevenue: dailyFees,
     }
 }
 
