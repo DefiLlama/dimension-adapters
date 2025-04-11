cd099325ca7589c10d5ab70ab119289d01f15a8b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-11 14:39:25 +0200

    fetch only daily data

--- dexs/traderjoe-lb-v2-2/index.ts
+++ dexs/traderjoe-lb-v2-2/index.ts
@@ -16,34 +16,18 @@ const chainMap: any = {
 
 const fetchV22Volume = async (_t: any, _tt: any, options: FetchOptions) => {
   const dayTimestamp = options.startOfDay
-  let { start, chainKey } = chainMap[options.chain];
-  start = Math.floor(+new Date(start).getTime() / 1000)
+  let { chainKey } = chainMap[options.chain];
   const end = dayTimestamp + 24 * 60 * 60;
-  const url = `https://api.lfj.dev/v1/dex/analytics/${chainKey}?startTime=${start}&endTime=${end}&version=v2.2`
+  const url = `https://api.lfj.dev/v1/dex/analytics/${chainKey}?startTime=${dayTimestamp - 86400}&endTime=${end}&version=v2.2`
   const historicalVolumeAndFees: IVolume[] = (await httpGet(url, {
     headers: {
       'x-traderjoe-api-key': process.env.TRADERJOE_API_KEY
     }
   }));
 
-  const totalVolume = historicalVolumeAndFees
-    .filter(volItem => volItem.timestamp <= dayTimestamp)
-    .reduce((acc, { volumeUsd }) => acc + Number(volumeUsd), 0)
-  const totalFees = historicalVolumeAndFees
-    .filter(volItem => volItem.timestamp <= dayTimestamp)
-    .reduce((acc, { feesUsd }) => acc + Number(feesUsd), 0)
-  const totalHoldersRevenue = historicalVolumeAndFees
-    .filter(volItem => volItem.timestamp <= dayTimestamp)
-    .reduce((acc, { protocolFeesUsd }) => acc + Number(protocolFeesUsd), 0)
-
   const { feesUsd: dailyFees, protocolFeesUsd: dailyHoldersRevenue, volumeUsd: dailyVolume, } = historicalVolumeAndFees.find(item => item.timestamp === dayTimestamp) ?? {}
 
   return {
-    totalVolume,
-    totalFees,
-    totalUserFees: totalFees,
-    totalRevenue: totalHoldersRevenue,
-    totalHoldersRevenue,
     dailyVolume,
     timestamp: dayTimestamp,
     dailyFees,
