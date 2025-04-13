765c9b7ebb7d5e96fcc00655df2e60a6040fc030
0xd1esel <0xdiesel123@gmail.com>
2025-04-12 20:05:52 -0700

    add revenue endpoint

--- dexs/thalaswap-v2/index.ts
+++ dexs/thalaswap-v2/index.ts
@@ -5,6 +5,7 @@ import { CHAIN } from "../../helpers/chains";
 const thalaDappURL = "https://app.thala.fi/";
 const volumeQueryURL = `${thalaDappURL}/api/defillama/trading-volume-chart?project=thalaswap-v2&timeframe=`;
 const feesQueryURL = `${thalaDappURL}/api/defillama/trading-fee-chart?project=thalaswap-v2&timeframe=`;
+const revenueQueryURL = `${thalaDappURL}/api/defillama/protocol-revenue-chart?project=thalaswap-v2&timeframe=`;
 const protocolRatioQueryURL = `${thalaDappURL}/api/defillama/protocol-revenue-ratio`;
 
 const volumeEndpoint = (endTimestamp: number, timeframe: string) =>
@@ -16,6 +17,11 @@ const feesEndpoint = (endTimestamp: number, timeframe: string) =>
   endTimestamp
     ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}`
     : feesQueryURL + timeframe;
+
+const revenueEndpoint = (endTimestamp: number, timeframe: string) =>
+  endTimestamp
+    ? revenueQueryURL + timeframe + `&endTimestamp=${endTimestamp}`
+    : revenueQueryURL + timeframe;
 
 interface IVolumeall {
   value: number;
@@ -48,18 +54,26 @@ const fetch = async (timestamp: number) => {
     0
   );
 
-  const protocolFeeRatio = (await fetchURL(protocolRatioQueryURL))?.data;
-  const dailyProtocolRevenue = dailyFees * protocolFeeRatio;
-  const totalProtocolRevenue = totalFees * protocolFeeRatio;
+  const dayRevenueQuery = (await fetchURL(revenueEndpoint(timestamp, "1D")))
+    ?.data;
+  const dailyRevenue = dayRevenueQuery.reduce(
+    (partialSum: number, a: IVolumeall) => partialSum + a.value,
+    0
+  );
+
+  const totalRevenueQuery = (await fetchURL(revenueEndpoint(0, "ALL")))?.data;
+  const totalRevenue = totalRevenueQuery.reduce(
+    (partialSum: number, a: IVolumeall) => partialSum + a.value,
+    0
+  );
 
   return {
     totalVolume: totalVolume,
     dailyVolume: dailyVolume,
     totalFees,
     dailyFees,
-    totalProtocolRevenue: totalProtocolRevenue,
-    dailyProtocolRevenue: dailyProtocolRevenue,
-    dailyRevenue: dailyProtocolRevenue,
+    totalRevenue,
+    dailyRevenue,
     timestamp,
   };
 };
