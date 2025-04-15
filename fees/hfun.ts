93028d48b528a73ecae4b23b6bb6f4c5e98fa943
treeoflife2 <sol.analyzoor@gmail.com>
2025-04-15 15:52:03 +0530

    hfun: remove referral fee from the revenue (#2895)

--- fees/hfun.ts
+++ fees/hfun.ts
@@ -1,3 +1,6 @@
+// 0xaB4AdA40112e0051a5add07f2304D749Bb8944fA - referral rewards
+// 0x501a76325a353a4249740ada1d4bce46dbdd67d6 - HFUN Fees Receiver
+
 import { Balances } from "@defillama/sdk";
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
@@ -8,7 +11,7 @@ async function addReceivedUSDC(options:FetchOptions, balances: Balances, address
     const txs:any[] = await postURL("https://api.hyperliquid.xyz/info", { "type": "userNonFundingLedgerUpdates", "user": address })
     txs.forEach(tx=>{
         const ts = tx.time/1e3
-        if(options.startTimestamp < ts && ts < options.endTimestamp && tx.delta.type === "spotTransfer" && tx.delta.token === "USDC" && tx.delta.destination === address.toLowerCase()){
+        if(options.startTimestamp < ts && ts < options.endTimestamp && (tx.delta.type === "spotTransfer" || tx.delta.type === "spotSend") && tx.delta.token === "USDC" && tx.delta.destination === address.toLowerCase()){
             balances.addCGToken("usd-coin", Number(tx.delta.amount))
         }
     })
@@ -16,9 +19,11 @@ async function addReceivedUSDC(options:FetchOptions, balances: Balances, address
 
 const fetch: any = async (options: FetchOptions) => {
     const dailyFees = options.createBalances()
-    await addReceivedUSDC(options, dailyFees, "0x501a76325a353a4249740ada1d4bce46dbdd67d6")
+    const dailyRevenue = options.createBalances()
+    await addReceivedUSDC(options, dailyRevenue, "0x501a76325a353a4249740ada1d4bce46dbdd67d6")
     await addReceivedUSDC(options, dailyFees, "0xaB4AdA40112e0051a5add07f2304D749Bb8944fA")
-    return { dailyFees, dailyRevenue: dailyFees, }
+    dailyFees.addBalances(dailyRevenue);
+    return { dailyFees, dailyRevenue }
 }
 
 const adapter: SimpleAdapter = {
