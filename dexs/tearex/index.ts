82f722369200ac5a4021251a87c883170b857fec
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-16 21:59:47 +0200

    minor fix

--- dexs/tearex/index.ts
+++ dexs/tearex/index.ts
@@ -4,18 +4,10 @@ import { httpGet } from "../../utils/fetchURL";
 const fetch = async () => {
   const api = "https://alpha-api.trex.trade/trade";
   const res = await httpGet(api);
-  const decimals = 10n ** 6n;
 
-  if (!res || typeof res !== "object" || !("trading" in res)) {
-    throw new Error("Invalid response");
-  }
-
-  const vol24h = Number(BigInt(res["trading"]["borrowAmount24h"]) / decimals);
-  const volAll = Number(BigInt(res["trading"]["totalBorrowAmount"]) / decimals);
-  
   return {
-    dailyVolume: vol24h,
-    totalVolume: volAll,
+    dailyVolume: res.trading.borrowAmount24h / 1e6,
+    totalVolume: res.trading.totalBorrowAmount / 1e6,
   };
 };
 
