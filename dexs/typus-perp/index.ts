db663a566c1036d3411dbb478e3d5cc19c488f3c
Wayne Kuo <95527849+WayneAl@users.noreply.github.com>
2025-04-17 02:57:25 +0800

    add typus-perp (#2905)
    * add typus-perp
    
    * minor fix
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ dexs/typus-perp/index.ts
@@ -0,0 +1,35 @@
+import { FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { queryEvents } from "../../helpers/sui";
+
+async function getChainData(options: FetchOptions): Promise<FetchResultV2> {
+  const events = await queryEvents({
+    eventType:
+      "0xe27969a70f93034de9ce16e6ad661b480324574e68d15a64b513fd90eb2423e5::position::OrderFilledEvent",
+    options,
+  });
+  const dailyVolume = options.createBalances();
+
+  for (const curr of events) {
+    const parsedJson = curr;
+
+    const trading_token_name = "0x" + parsedJson.symbol.base_token.name;
+    dailyVolume.add(trading_token_name, Number(parsedJson.position_size));
+  }
+
+  return {
+    dailyVolume,
+  };
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.SUI]: {
+      fetch: getChainData,
+      start: "2025-4-1",
+    },
+  },
+};
+
+export default adapter;
