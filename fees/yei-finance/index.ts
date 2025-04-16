f20140f7736b461d205a22d02c48259611ef5bf5
santai <dev@penpad.io>
2025-04-16 14:28:03 +0800

    fix: Remote unnecessary configs

--- fees/yei-finance/index.ts
+++ fees/yei-finance/index.ts
@@ -5,23 +5,6 @@ import { V3Reserve } from "./types";
 import { Chain } from "@defillama/sdk/build/general";
 import axios from "axios";
 
-//remember to lowercase
-const poolIDs = {
-    WSEI: "0xe30fedd158a2e3b13e9badaeabafc5516e95e8c70x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    USDC: "0x3894085ef7ff0f0aedf52e2a2704928d1ec074f10x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    ISEI: "0x5cf6826140c1c56ff49c808a1a75407cd1df94230x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    WETH: "0x160345fc359604fc6e70e3c5facbde5f7a9342d80x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    FASTUSD: "0x37a4dd9ced2b19cfe8fac251cd727b5787e452690x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    WBTC: "0x0555e30da8f98308edb960aa94c0db47230d2b9c0x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    FRAX: "0x80eede496655fb9047dd39d9f418d5483ed600df0x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    USDT: "0xb75d0b03c06a926e488e2659df1a861f860bd3d10x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    SFRXETH: "0x3ec3849c33291a9ef4c5db86de593eb4a37fde450x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    SFRAX: "0x5bff88ca1442c2496f7e475e9e7786383bc070c00x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    FRXETH: "0x43edd7f3831b08fe70b7555ddd373c8bf65a90500x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    SFASTUSD: "0xdf77686d99667ae56bc18f539b777dbc2bbe3e9f0x5c57266688a4ad1d3ab61209ebcb967b84227642",
-    SOLVBTC: "0x541fd749419ca806a8bc7da8ac23d346f2df8b770x5c57266688a4ad1d3ab61209ebcb967b84227642",
-};
-
 const ONE_DAY = 24 * 60 * 60;
 
 const v3Endpoints = {
@@ -31,7 +14,6 @@ const v3Endpoints = {
 const v3Reserves = async (graphUrls: ChainEndpoints, chain: string, timestamp: number) => {
     const graphQuery = `{
     reserves {
-        id
         paramsHistory(
           where: { timestamp_lte: ${timestamp}, timestamp_gte: ${timestamp - ONE_DAY} },
           orderBy: "timestamp",
