152507f1a80f8af3c8f9d5e2013bc29d3dea54e5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-16 21:53:25 +0200

    code refactor

--- fees/yei-finance/index.ts
+++ fees/yei-finance/index.ts
@@ -1,18 +1,16 @@
 import { CHAIN } from "../../helpers/chains";
 import { request } from "graphql-request";
-import type { ChainEndpoints, FetchOptions } from "../../adapters/types";
-import { V3Reserve } from "./types";
-import { Chain } from "@defillama/sdk/build/general";
-import axios from "axios";
+import type { FetchOptions } from "../../adapters/types";
+
 
 const ONE_DAY = 24 * 60 * 60;
 
 const v3Endpoints = {
-    [CHAIN.SEI]: "https://api.studio.thegraph.com/query/82370/yei-finance/version/latest",
+  [CHAIN.SEI]: "https://api.studio.thegraph.com/query/82370/yei-finance/version/latest",
 };
 
-const v3Reserves = async (graphUrls: ChainEndpoints, chain: string, timestamp: number) => {
-    const graphQuery = `{
+const v3Reserves = async (endpoint: string, timestamp: number) => {
+  const graphQuery = `{
     reserves {
         paramsHistory(
           where: { timestamp_lte: ${timestamp}, timestamp_gte: ${timestamp - ONE_DAY} },
@@ -38,142 +36,53 @@ const v3Reserves = async (graphUrls: ChainEndpoints, chain: string, timestamp: n
         }
       }
     }`;
-    const graphRes = await request(graphUrls[chain], graphQuery);
-    const reserves = await Promise.all(
-        graphRes.reserves
-            .map((r: any) => r.paramsHistory[0])
-            .filter((r: any) => r)
-            .map(async (r: any) => {
-                const capitalSymbol = r.reserve.symbol.toUpperCase(); // Capitalize the symbol
-                if (capitalSymbol !== "USD₮0" && capitalSymbol !== "WSTETH") {
-                    const priceRes = await axios.get(
-                        `https://api.yei.finance/api/token_price?symbol=${capitalSymbol}&timestamp=${timestamp}`
-                    );
-                    return {
-                        ...r,
-                        priceInUsd: priceRes.data.price.toString(),
-                    };
-                } else {
-                    return {
-                        ...r,
-                        priceInUsd: "0",
-                    };
-                }
-            })
-    );
-    return reserves;
+  const graphRes = await request(endpoint, graphQuery);
+  const res = {}
+  graphRes.reserves.map((r: any) => {
+    if (!r.paramsHistory.length) return;
+    const a = r.paramsHistory[0]
+    res[a.reserve.underlyingAsset] = a
+  });
+  return res
 };
 
-const v3Graphs = (graphUrls: ChainEndpoints) => {
-    return (chain: Chain) => {
-        return async ({ endTimestamp }: FetchOptions) => {
-            const todaysTimestamp = endTimestamp;
-            const yesterdaysTimestamp = todaysTimestamp - 60 * 60 * 24;
+const fetch = async ({ endTimestamp, chain, createBalances, }: FetchOptions) => {
+  const todaysTimestamp = endTimestamp;
+  const yesterdaysTimestamp = todaysTimestamp - 60 * 60 * 24;
+  const dailyFees = createBalances()
+  const dailyRevenue = createBalances()
 
-            const todaysReserves: V3Reserve[] = await v3Reserves(graphUrls, chain, todaysTimestamp);
-            const yesterdaysReserves: V3Reserve[] = await v3Reserves(graphUrls, chain, yesterdaysTimestamp);
+  const todaysReserves = await v3Reserves(v3Endpoints[chain], todaysTimestamp);
+  const yesterdaysReserves = await v3Reserves(v3Endpoints[chain], yesterdaysTimestamp);
 
-            const feeBreakdown: any = todaysReserves.reduce(
-                (acc, reserve: V3Reserve) => {
-                    const yesterdaysReserve = yesterdaysReserves.find(
-                        (r: any) => r.reserve.underlyingAsset === reserve.reserve.underlyingAsset
-                    );
+  Object.entries(todaysReserves).forEach(([token, reserve]: any) => {
+    const yReserve = yesterdaysReserves[token]
+    if (!yReserve) return;
+    const depositorInterest = +reserve.lifetimeDepositorsInterestEarned - +yReserve.lifetimeDepositorsInterestEarned;
+    const treasuryIncome = +reserve.lifetimeReserveFactorAccrued - +yReserve.lifetimeReserveFactorAccrued;
+    /*  const flashloanLPPremium = +reserve.lifetimeFlashLoanLPPremium - +yReserve.lifetimeFlashLoanLPPremium;
+     const flashloanProtocolPremium = +reserve.lifetimeFlashLoanProtocolPremium - +yReserve.lifetimeFlashLoanProtocolPremium;
+     const portalLPFee = +reserve.lifetimePortalLPFee - +yReserve.lifetimePortalLPFee;
+     const portalProtocolFee = +reserve.lifetimePortalProtocolFee - +yReserve.lifetimePortalProtocolFee;
+     const outstandingTreasuryIncome = +reserve.accruedToTreasury - +yReserve.accruedToTreasury; */
 
-                    if (!yesterdaysReserve) {
-                        return acc;
-                    }
+    dailyFees.add(token, depositorInterest);
+    dailyFees.add(token, treasuryIncome);
+    dailyRevenue.add(token, treasuryIncome);
 
-                    const priceInUsd = parseFloat(reserve.priceInUsd);
+  })
 
-                    const depositorInterest =
-                        parseFloat(reserve.lifetimeDepositorsInterestEarned) -
-                        parseFloat(yesterdaysReserve?.lifetimeDepositorsInterestEarned);
-                    const depositorInterestUSD = (depositorInterest * priceInUsd) / 10 ** reserve.reserve.decimals;
-
-                    const flashloanLPPremium =
-                        parseFloat(reserve.lifetimeFlashLoanLPPremium) -
-                        parseFloat(yesterdaysReserve.lifetimeFlashLoanLPPremium);
-                    const flashloanLPPremiumUSD = (flashloanLPPremium * priceInUsd) / 10 ** reserve.reserve.decimals;
-
-                    const flashloanProtocolPremium =
-                        parseFloat(reserve.lifetimeFlashLoanProtocolPremium) -
-                        parseFloat(yesterdaysReserve.lifetimeFlashLoanProtocolPremium);
-                    const flashloanProtocolPremiumUSD =
-                        (flashloanProtocolPremium * priceInUsd) / 10 ** reserve.reserve.decimals;
-
-                    const portalLPFee =
-                        parseFloat(reserve.lifetimePortalLPFee) - parseFloat(yesterdaysReserve?.lifetimePortalLPFee);
-                    const portalLPFeeUSD = (portalLPFee * priceInUsd) / 10 ** reserve.reserve.decimals;
-
-                    const portalProtocolFee =
-                        parseFloat(reserve.lifetimePortalProtocolFee) -
-                        parseFloat(yesterdaysReserve?.lifetimePortalProtocolFee);
-                    const portalProtocolFeeUSD = (portalProtocolFee * priceInUsd) / 10 ** reserve.reserve.decimals;
-
-                    const treasuryIncome =
-                        parseFloat(reserve.lifetimeReserveFactorAccrued) -
-                        parseFloat(yesterdaysReserve?.lifetimeReserveFactorAccrued);
-
-                    const outstandingTreasuryIncome =
-                        parseFloat(reserve.accruedToTreasury) - parseFloat(yesterdaysReserve?.accruedToTreasury);
-
-                    const treasuryIncomeUSD = (treasuryIncome * priceInUsd) / 10 ** reserve.reserve.decimals;
-
-                    const outstandingTreasuryIncomeUSD =
-                        (outstandingTreasuryIncome * priceInUsd) / 10 ** reserve.reserve.decimals;
-
-                    if (depositorInterestUSD < 0 || depositorInterestUSD > 1_000_000) {
-                        return acc;
-                    }
-
-                    if (treasuryIncomeUSD < 0 || treasuryIncomeUSD > 1_000_000) {
-                        return acc;
-                    }
-
-                    if (treasuryIncomeUSD < 0 || treasuryIncomeUSD > 1_000_000) {
-                        return acc;
-                    }
-
-                    acc.outstandingTreasuryIncomeUSD += outstandingTreasuryIncomeUSD;
-                    acc.treasuryIncomeUSD += treasuryIncomeUSD;
-                    acc.depositorInterestUSD += depositorInterestUSD;
-                    acc.flashloanLPPremiumUSD += flashloanLPPremiumUSD;
-                    acc.flashloanProtocolPremiumUSD += flashloanProtocolPremiumUSD;
-                    acc.portalLPFeeUSD += portalLPFeeUSD;
-                    acc.portalProtocolFeeUSD += portalProtocolFeeUSD;
-                    return acc;
-                },
-                {
-                    depositorInterestUSD: 0,
-                    flashloanLPPremiumUSD: 0,
-                    flashloanProtocolPremiumUSD: 0,
-                    portalLPFeeUSD: 0,
-                    portalProtocolFeeUSD: 0,
-                    treasuryIncomeUSD: 0,
-                    outstandingTreasuryIncomeUSD: 0,
-                }
-            );
-            const dailyFee = feeBreakdown.depositorInterestUSD + feeBreakdown.treasuryIncomeUSD;
-            const dailyRev = feeBreakdown.treasuryIncomeUSD;
-
-            return {
-                dailyFees: dailyFee.toString(),
-                dailyRevenue: dailyRev.toString(),
-            };
-        };
-    };
+  return { dailyFees, dailyRevenue };
 };
 
 const adapter = {
-    breakdown: {
-        v3: {
-            [CHAIN.SEI]: {
-                fetch: v3Graphs(v3Endpoints)(CHAIN.SEI),
-                start: "2025-01-01",
-            },
-        },
+  adapter: {
+    [CHAIN.SEI]: {
+      fetch,
+      start: "2025-01-01",
     },
-    version: 2,
+  },
+  version: 2,
 };
 
 export default adapter;
