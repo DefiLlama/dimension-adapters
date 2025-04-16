1e759fc3a94b9a31a2aba53614e29bf4e7ab4202
Eden <noat.eth@gmail.com>
2025-04-17 03:13:54 +0700

    Update Morpho adapter to get fees from blue contracts (#2903)
    * update: get fees from blue contracts
    
    * morpho: cache graph result
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- fees/morpho/index.ts
+++ fees/morpho/index.ts
@@ -1,87 +1,123 @@
-const { request, gql } = require("graphql-request");
-import { Balances } from "@defillama/sdk";
-import { FetchV2, SimpleAdapter } from "../../adapters/types";
+import { request, } from 'graphql-request';
+import { FetchOptions, FetchV2, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getConfig } from "../../helpers/cache";
 
-type Market = {
-  loanAsset: { address: string; decimals: number };
-  state: {
-    supplyApy: number;
-    borrowApy: number;
-    supplyAssets: number;
-    borrowAssets: number;
-  };
+const methodology = {
+  Fees: 'Total borrow interest paid by borrowers.',
+  SupplySideRevenue: 'Total interests are distributed to suppliers/lenders.',
+  ProtocolRevenue: 'Total interests are distributed to Morpho.',
+}
+
+type MorphoMarket = {
+  marketId: string;
+  loanAsset: string;
 };
+
+type MorphoBlueAccrueInterestEvent = {
+  token: string | undefined | null;
+  interest: bigint;
+}
 
 const BLUE_API_ENDPOINT = "https://blue-api.morpho.org/graphql";
 
-const query = gql`
+const query = `
   query GetMarketsData($chainId: Int!) {
     markets(where: { chainId_in: [$chainId] }) {
       items {
+        uniqueKey
         loanAsset {
           address
-        }
-        state {
-          borrowAssets
-          supplyAssets
-          supplyApy
-          borrowApy
         }
       }
     }
   }
 `;
 
-const apyToDaily = (apy: number): number =>
-  (((apy / 100 + 1) ** (1 / 365) - 1) * 365) / (0.01 * 365);
+const MorphoBlues = {
+  [CHAIN.ETHEREUM] : {
+    chainId: 1,
+    blue: '0xbbbbbbbbbb9cc5e90e3b3af64bdaf62c37eeffcb',
+  },
+  [CHAIN.BASE] : {
+    chainId: 8453,
+    blue: '0xbbbbbbbbbb9cc5e90e3b3af64bdaf62c37eeffcb',
+  },
+}
 
-const fetchMarkets = async (chainId: number, url: string) => {
+const MorphoBlueAbis = {
+  AccrueInterest: 'event AccrueInterest(bytes32 indexed id, uint256 prevBorrowRate, uint256 interest, uint256 feeShares)',
+}
+
+const _fetchMarkets = async (chainId: number, url: string): Promise<Array<MorphoMarket>> => {
   const res = await request(url, query, { chainId });
 
-  let marketData: {
-    dailySupplySideRevenue: Balances;
-    dailyUserFees: Balances;
-    dailyFees?: Balances;
-  } = {
-    dailySupplySideRevenue: new Balances({
-      chain: chainId === 1 ? "ethereum" : "base",
-    }),
-    dailyUserFees: new Balances({ chain: chainId === 1 ? "ethereum" : "base" }),
-  };
-
-  res.markets.items.map((m: Market) => {
-    const { address } = m.loanAsset;
-    const { supplyApy, borrowApy, supplyAssets, borrowAssets } = m.state;
-
-    const supply = supplyAssets * apyToDaily(supplyApy);
-    const borrow = borrowAssets * apyToDaily(borrowApy);
-
-    marketData.dailySupplySideRevenue.add(address, supply);
-    marketData.dailyUserFees.add(address, borrow);
+  return res.markets.items.map((item: any) => {
+    return {
+      marketId: item.uniqueKey,
+      loanAsset: item.loanAsset.address,
+    }
   });
-
-  marketData.dailyFees = marketData.dailyUserFees;
-
-  return marketData;
 };
 
-const fetchEthereum: FetchV2 = async (options) => {
-  return fetchMarkets(1, BLUE_API_ENDPOINT);
-};
+async function fetchMarkets(chainId: number, url: string): Promise<Array<MorphoMarket>> {
+  return getConfig('morpho-blue/markets-'+chainId, '', {
+    fetcher: async () => _fetchMarkets(chainId, url),
+  })
+}
 
-const fetchBase: FetchV2 = async (options) => {
-  return fetchMarkets(8453, BLUE_API_ENDPOINT);
-};
+const fetchEvents = async (options: FetchOptions): Promise<Array<MorphoBlueAccrueInterestEvent>> => {
+  const markets = await fetchMarkets(MorphoBlues[options.chain].chainId, BLUE_API_ENDPOINT)
+  const marketMap = {} as any
+  markets.forEach((item) => {
+    marketMap[item.marketId.toLowerCase()] = item.loanAsset
+  })
+
+  return (await options.getLogs({
+    eventAbi: MorphoBlueAbis.AccrueInterest,
+    target: MorphoBlues[options.chain].blue,
+  })).map((log: any) => {
+    return {
+      token: marketMap[String(log.id).toLowerCase()],
+      interest: BigInt(log.interest),
+    }
+  })
+}
+
+const fetch: FetchV2 = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances()
+
+  const events = await fetchEvents(options)
+  for (const event of events) {
+    if (event.token) {
+      dailyFees.add(event.token, event.interest)
+    }
+  }
+
+  return {
+    dailyFees: dailyFees,
+    dailySupplySideRevenue: dailyFees,
+
+    // Morpho gets no fees
+    dailyProtocolRevenue: 0,
+  }
+}
 
 const adapter: SimpleAdapter = {
   adapter: {
     ethereum: {
-      fetch: fetchEthereum,
+      fetch: fetch,
       start: '2024-01-02',
+      meta: {
+        methodology,
+      }
     },
     base: {
-      fetch: fetchBase,
+      fetch: fetch,
       start: '2024-05-03',
+      meta: {
+        methodology,
+      }
     },
   },
   version: 2,
