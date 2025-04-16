d6cc2ae1b7d80eaff775fa7174f6a80c82f3c92a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-16 22:42:59 +0200

    minor fix

--- dexs/tonpump/index.ts
+++ dexs/tonpump/index.ts
@@ -1,21 +1,15 @@
 import fetchURL from "../../utils/fetchURL"
 import { CHAIN } from "../../helpers/chains";
 import { FetchOptions } from "../../adapters/types";
-import { getPrices } from "../../utils/prices";
-
 
 const endpoint = "https://tonfunstats-eqnd7.ondigitalocean.app/api/v1/getVolume"
 
-
 const fetch = async (options: FetchOptions) => {
   const res = await fetchURL(`${endpoint}?from=${options.startTimestamp}&to=${options.endTimestamp}&service=hot`)
-  const TON = "coingecko:the-open-network"
-  const ton_price = await getPrices([TON], options.startTimestamp);
+  const dailyVolume = options.createBalances()
+  dailyVolume.addCGToken('the-open-network', res.volume / 1e9)
 
-  return {
-    dailyVolume: Number(BigInt(res.volume) / 1000000000n) * ton_price[TON].price,
-    timestamp: options.startTimestamp,
-  };
+  return { dailyVolume, }
 };
 
 
