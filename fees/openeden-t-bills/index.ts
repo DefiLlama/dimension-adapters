cdbea6d9e74b2f1ac62d8c23cb880ef7cfef3d09
Eden <noat.eth@gmail.com>
2025-04-16 15:36:18 +0700

    add tbill balance on ripple, sol

--- fees/openeden-t-bills/index.ts
+++ fees/openeden-t-bills/index.ts
@@ -4,6 +4,8 @@ import { Chain } from "@defillama/sdk/build/general";
 import { Adapter, FetchOptions, FetchResultV2 } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import ADDRESSES from "../../helpers/coreAssets.json";
+import { getTokenSupply } from '../../helpers/solana';
+import { rpcCall } from '../../helpers/ripple';
 
 const eventAbi = `event ProcessDeposit(
   address sender,
@@ -22,19 +24,45 @@ const VAULT_ADDRESSES: Record<Chain, string> = {
   [CHAIN.ARBITRUM]: "0xF84D28A8D28292842dD73D1c5F99476A80b6666A",
 };
 
+const SOL_TBILL = '4MmJVdwYN8LwvbGeCowYjSx7KoEi6BJWg8XXnW4fDDp6';
+const RIPPLE_TBILL = {
+  ACCOUNT: 'rJNE2NNz83GJYtWVLwMvchDWEon3huWnFn',
+  HOT_WALLET: 'rB56JZWRKvpWNeyqM3QYfZwW4fS9YEyPWM',
+};
+
 const MANAGEMENT_FEES: number = 0.003;
 const DAILY_MANAGEMENT_FEES: number = MANAGEMENT_FEES / 365;
 
 const fetch = async (
   vault: string,
-  { api, getLogs, createBalances }: FetchOptions
+  { chain, api, getLogs, createBalances }: FetchOptions
 ): Promise<FetchResultV2> => {
   const dailyFees = createBalances();
 
-  const [logs, totalUSDC] = await Promise.all([
+  let [logs, totalUSDC] = await Promise.all([
     getLogs({ target: vault, eventAbi }),
     api.call({ target: vault, abi: "uint256:totalAssets" }),
   ]);
+
+  if (chain === CHAIN.ETHEREUM) {
+    console.log({before: totalUSDC})
+    
+    const balanceOnSol = await getTokenSupply(SOL_TBILL)
+    const rippleCallRes = await rpcCall('gateway_balances', [
+      {
+        account: RIPPLE_TBILL.ACCOUNT,
+        hotwallet: [
+          RIPPLE_TBILL.HOT_WALLET,
+        ],
+        ledger_index: "validated",
+        strict: true
+      }
+    ])
+    const balanceOnRipple = rippleCallRes.result && rippleCallRes.result.obligations ? Number(rippleCallRes.result.obligations.TBL) : 0
+    
+    // because USDC has 6 decimals on eth
+    totalUSDC = Number(totalUSDC) + balanceOnSol * 1e6 + balanceOnRipple * 1e6
+  }
 
   dailyFees.add(ADDRESSES[api.chain].USDC, totalUSDC * DAILY_MANAGEMENT_FEES);
 
