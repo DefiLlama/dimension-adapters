83c7d3d05ba2497e5ba54e605d823ded0568f6ec
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-17 13:30:03 +0200

    code refactor

--- aggregators/kame-aggregator/index.ts
+++ aggregators/kame-aggregator/index.ts
@@ -1,25 +1,20 @@
+
 import fetchURL from "../../utils/fetchURL";
-import { FetchV2, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const URL = "https://api.kame.ag/api/statistics";
 
-const fetch: FetchV2 = async ({ fromTimestamp, toTimestamp }) => {
-  const response = await fetchURL(
-    `${URL}/?startTime=${fromTimestamp}&endTime=${toTimestamp}`
-  );
+const fetch = async ({ fromTimestamp, toTimestamp }) => {
+  const response = await fetchURL(`${URL}/?startTime=${fromTimestamp}&endTime=${toTimestamp}`);
   return { dailyVolume: response.volume };
 };
 
-const adapter: SimpleAdapter = {
+export default {
   version: 2,
   adapter: {
     [CHAIN.SEI]: {
       fetch,
-      start: "2025-04-17",
-      runAtCurrTime: true,
+      start: "2025-04-14",
     },
   },
-};
-
-export default adapter;
+}
