6c21dd98af9887ed2bbf91ce78d6477de7773c40
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-18 12:04:43 +0200

    minor fix

--- fees/lista-lending/index.ts
+++ fees/lista-lending/index.ts
@@ -1,5 +1,5 @@
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types"
+import { CHAIN } from "../../helpers/chains"
 
 /**
  * Fetches data from Lista DAO
@@ -7,52 +7,41 @@ import { CHAIN } from "../../helpers/chains";
  * @test npx ts-node --transpile-only cli/testAdapter.ts fees lista-lending
  */
 
-const eventContract = "0x2E2Eed557FAb1d2E11fEA1E1a23FF8f1b23551f3";
+const eventContract = "0x2E2Eed557FAb1d2E11fEA1E1a23FF8f1b23551f3"
 
 // Inner vault list for filtering
 const innerVaultList = [
   "0x57134a64B7cD9F9eb72F8255A671F5Bf2fe3E2d0",
   "0xfa27f172e0b6ebcEF9c51ABf817E2cb142FbE627",
-];
+].map((address) => address.toLowerCase())
 
 // Event ABIs
-const vaultFeeClaimedEvent =
-  "event VaultFeeClaimed(address vault, address token, uint256 assets, uint256 shares)";
-const marketFeeClaimedEvent =
-  "event MarketFeeClaimed(bytes32 id, address token, uint256 assets, uint256 shares)";
+const vaultFeeClaimedEvent = "event VaultFeeClaimed(address vault, address token, uint256 assets, uint256 shares)"
+const marketFeeClaimedEvent = "event MarketFeeClaimed(bytes32 id, address token, uint256 assets, uint256 shares)"
 
 const fetch = async (options: FetchOptions) => {
-  const dailyFees = options.createBalances();
+  const dailyFees = options.createBalances()
 
   // Get VaultFeeClaimed events
-  const vaultFeeLogs = await options.getLogs({
-    target: eventContract,
-    eventAbi: vaultFeeClaimedEvent,
-  });
+  const vaultFeeLogs = await options.getLogs({ target: eventContract, eventAbi: vaultFeeClaimedEvent, })
 
   //   Get MarketFeeClaimed events
-  const marketFeeLogs = await options.getLogs({
-    target: eventContract,
-    eventAbi: marketFeeClaimedEvent,
-  });
+  const marketFeeLogs = await options.getLogs({ target: eventContract, eventAbi: marketFeeClaimedEvent, })
 
   // Process vault fees - only for specified inner vaults
   vaultFeeLogs.forEach((log) => {
-    if (innerVaultList.includes(log.vault)) {
-      dailyFees.add(log.token, log.assets);
-    }
-  });
+    if (!innerVaultList.includes(log.vault.toLowerCase())) return;
+    dailyFees.add(log.token, log.assets)
+  })
 
   //   Process market fees
-  marketFeeLogs.forEach((log) => {
-    dailyFees.add(log.token, log.assets);
-  });
+  marketFeeLogs.forEach((log) => dailyFees.add(log.token, log.assets))
 
   return {
     dailyFees,
     dailyRevenue: dailyFees,
-  };
-};
+  }
+}
 
 const adapter: SimpleAdapter = {
   version: 2,
@@ -62,6 +51,6 @@ const adapter: SimpleAdapter = {
       start: "2025-04-16",
     },
   },
-};
+}
 
-export default adapter;
+export default adapter
