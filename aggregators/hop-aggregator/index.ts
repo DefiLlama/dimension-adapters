d426764bc827518ce9ac119d4d906bc4dbb9aa7c
Eden <noat.eth@gmail.com>
2025-04-21 22:42:56 +0700

    Fix some adapters errors (#2979)
    * return null on api call failed
    
    * add starbase dead date because web is down
    
    * add plexus dead date because web is down
    
    * add hop-agg dead date

--- aggregators/hop-aggregator/index.ts
+++ aggregators/hop-aggregator/index.ts
@@ -1,6 +1,6 @@
 import { CHAIN } from "../../helpers/chains";
 import { httpPost } from "../../utils/fetchURL";
-import { FetchOptions } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { getTimestampAtStartOfDayUTC } from "../../utils/date"
 
 const fetchVolume = async (_t: any, _b: any ,options: FetchOptions) => {
@@ -16,7 +16,8 @@ const fetchVolume = async (_t: any, _b: any ,options: FetchOptions) => {
   };
 };
 
-const adapter: any = {
+const adapter: SimpleAdapter = {
+  deadFrom: '2025-01-25',
   adapter: {
     [CHAIN.SUI]: {
       fetch: fetchVolume,
