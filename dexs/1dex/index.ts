bd4d656f2c46bde3d46cf7d2d41c0989b08931fd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-21 19:58:39 +0200

    Update index.ts

--- dexs/1dex/index.ts
+++ dexs/1dex/index.ts
@@ -1,38 +1,22 @@
-import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import fetchURL from "../../utils/fetchURL";
+import { SimpleAdapter } from "../../adapters/types"
+import { CHAIN } from "../../helpers/chains"
+import { httpGet } from "../../utils/fetchURL"
 
-const endpoint = `https:///api.1dex.com/24h-trade-info`;
+const endpoint = `https:///api.1dex.com/24h-trade-info`
 
-interface IVolume {
-  volume_usdt: string;
+async function fetch() {
+  const res = await httpGet(endpoint);
+  return { dailyVolume: res.data.volume_usdt }
 }
-const graph = (chain: string) => {
-  return async (timestamp: number): Promise<FetchResultVolume> => {
-    const dayTimestamp = getUniqStartOfTodayTimestamp(
-      new Date(timestamp * 1000)
-    );
-    let volume = 0;
-    if (chain === CHAIN.EOS) {
-      const response: IVolume = (await fetchURL(endpoint))?.data;
-      volume = response?.volume_usdt ? Number(response.volume_usdt) : 0;
-    }
-    return {
-      dailyVolume: volume,
-      timestamp: dayTimestamp,
-    };
-  };
-};
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.EOS]: {
-      fetch: graph(CHAIN.EOS),
+      fetch,
       start: "2025-04-15",
       runAtCurrTime: true,
-    },
-  },
-};
+    }
+  }
+}
 
-export default adapter;
+export default adapter
