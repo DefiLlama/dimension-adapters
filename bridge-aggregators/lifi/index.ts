a5200ef470f513d193b350102c11e1aed8ef90de
treeoflife2 <sol.analyzoor@gmail.com>
2025-04-22 17:24:45 +0530

    lifi: correct starttime for each chain (#2995)

--- bridge-aggregators/lifi/index.ts
+++ bridge-aggregators/lifi/index.ts
@@ -4,7 +4,10 @@ import { CHAIN } from "../../helpers/chains";
 import fetchURL from "../../utils/fetchURL";
 
 type IContract = {
-  [c: string | Chain]: string;
+  [c: string | Chain]: {
+    id: string;
+    startTime: string;
+  }
 }
 
 interface LifiTransferV2 {
@@ -27,56 +30,162 @@ interface LifiResponseV2 {
 }
 
 const contract: IContract = {
-  [CHAIN.BITCOIN]: '20000000000001',
-  [CHAIN.SOLANA]: '1151111081099710',
-  [CHAIN.MOONBEAM]: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
-  [CHAIN.MOONRIVER]: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
-  [CHAIN.FRAXTAL]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.CELO]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.LISK]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.ABSTRACT]: '0x4f8C9056bb8A3616693a76922FA35d53C056E5b3',
-  [CHAIN.SONIC]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.UNICHAIN]: '0x864b314D4C5a0399368609581d3E8933a63b9232',
-  [CHAIN.VELAS]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.APECHAIN]: '0x2dea447e7dc6cd2f10b31bF10dCB30F87E838417',
-  [CHAIN.BERACHAIN]: '0xf909c4Ae16622898b885B89d7F839E0244851c66',
-  [CHAIN.INK]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.OP_BNB]: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
-  [CHAIN.SONEIUM]: '0x864b314D4C5a0399368609581d3E8933a63b9232',
-  [CHAIN.AURORA]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.ARBITRUM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.OPTIMISM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.BASE]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.ETHEREUM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.AVAX]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.BSC]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.LINEA]: '0xDE1E598b81620773454588B85D6b5D4eEC32573e',
-  [CHAIN.MANTLE]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.POLYGON]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.POLYGON_ZKEVM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.FANTOM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.MODE]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.SCROLL]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.ERA]: '0x341e94069f53234fe6dabef707ad424830525715',
-  [CHAIN.METIS]: '0x24ca98fB6972F5eE05f0dB00595c7f68D9FaFd68',
-  [CHAIN.XDAI]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.TAIKO]: '0x3A9A5dBa8FE1C4Da98187cE4755701BCA182f63b',
-  [CHAIN.BLAST]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.BOBA]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.FUSE]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.CRONOS]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  [CHAIN.GRAVITY]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-
-  // [CHAIN.ZKSYNC]: '0x341e94069f53234fE6DabeF707aD424830525715',
-  // [CHAIN.SEI]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  // [CHAIN.ROOTSTOCK]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  // [CHAIN.EVMOS]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  // [CHAIN.KAIA]: '0x1255d17c1BC2f764d087536410879F2d0D8772fD',
-  // [CHAIN.OKEXCHAIN]: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
-  // [CHAIN.IMMUTABLEZKEVM]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  // [CHAIN.WORLDCHAIN]: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
-  // [CHAIN.LENS]: '0xF3B20515d9B193531c48E47c18aF16d1e5d28f9a',
-
+  [CHAIN.BITCOIN]: {
+    id: '20000000000001',
+    startTime: '2023-03-11'
+  },
+  [CHAIN.SOLANA]: {
+    id: '1151111081099710',
+    startTime: '2024-01-01'
+  },
+  [CHAIN.MOONBEAM]: {
+    id: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
+    startTime: '2022-10-18'
+  },
+  [CHAIN.MOONRIVER]: {
+    id: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
+    startTime: '2023-10-18'
+  },
+  [CHAIN.FRAXTAL]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2024-06-27'
+  },
+  [CHAIN.CELO]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2022-10-18'
+  },
+  [CHAIN.LISK]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2024-12-09'
+  },
+  [CHAIN.ABSTRACT]: {
+    id: '0x4f8C9056bb8A3616693a76922FA35d53C056E5b3',
+    startTime: '2025-01-15'
+  },
+  [CHAIN.SONIC]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2025-01-22'
+  },
+  [CHAIN.UNICHAIN]: {
+    id: '0x864b314D4C5a0399368609581d3E8933a63b9232',
+    startTime: '2025-02-12'
+  },
+  [CHAIN.VELAS]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2022-10-20'
+  },
+  [CHAIN.APECHAIN]: {
+    id: '0x2dea447e7dc6cd2f10b31bF10dCB30F87E838417',
+    startTime: '2025-01-20'
+  },
+  [CHAIN.BERACHAIN]: {
+    id: '0xf909c4Ae16622898b885B89d7F839E0244851c66',
+    startTime: '2025-02-12'
+  },
+  [CHAIN.INK]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2025-01-22'
+  },
+  [CHAIN.OP_BNB]: {
+    id: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
+    startTime: '2023-10-24'
+  },
+  [CHAIN.SONEIUM]: {
+    id: '0x864b314D4C5a0399368609581d3E8933a63b9232',
+    startTime: '2025-02-17'
+  },
+  [CHAIN.AURORA]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2022-10-21'
+  },
+  [CHAIN.ARBITRUM]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-08-21'
+  },
+  [CHAIN.OPTIMISM]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-07-25'
+  },
+  [CHAIN.BASE]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-08-15'
+  },
+  [CHAIN.ETHEREUM]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-07-27'
+  },
+  [CHAIN.AVAX]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2022-10-18'
+  },
+  [CHAIN.BSC]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-07-21'
+  },
+  [CHAIN.LINEA]: {
+    id: '0xDE1E598b81620773454588B85D6b5D4eEC32573e',
+    startTime: '2023-08-28'
+  },
+  [CHAIN.MANTLE]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2024-05-13'
+  },
+  [CHAIN.POLYGON]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-07-20'
+  },
+  [CHAIN.POLYGON_ZKEVM]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-06-01'
+  },
+  [CHAIN.FANTOM]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2022-10-18'
+  },
+  [CHAIN.MODE]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2024-04-15'
+  },
+  [CHAIN.SCROLL]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2024-02-06'
+  },
+  [CHAIN.ERA]: {
+    id: '0x341e94069f53234fe6dabef707ad424830525715',
+    startTime: '2023-07-13'
+  },
+  [CHAIN.METIS]: {
+    id: '0x24ca98fB6972F5eE05f0dB00595c7f68D9FaFd68',
+    startTime: '2024-02-03'
+  },
+  [CHAIN.XDAI]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-07-24'
+  },
+  [CHAIN.TAIKO]: {
+    id: '0x3A9A5dBa8FE1C4Da98187cE4755701BCA182f63b',
+    startTime: '2024-08-15'
+  },
+  [CHAIN.BLAST]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2024-05-17'
+  },
+  [CHAIN.BOBA]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2022-10-21'
+  },
+  [CHAIN.FUSE]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-10-19'
+  },
+  [CHAIN.CRONOS]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2023-10-19'
+  },
+  [CHAIN.GRAVITY]: {
+    id: '0x1231deb6f5749ef6ce6943a275a1d3e7486f4eae',
+    startTime: '2024-07-30'
+  }
 }
 
 const LifiBridgeEvent = "event LiFiTransferStarted((bytes32 transactionId, string bridge, string integrator, address referrer, address sendingAssetId, address receiver, uint256 minAmount, uint256 destinationChainId, bool hasSourceSwaps, bool hasDestinationCall) bridgeData)"
@@ -88,7 +197,7 @@ const fetchFromAPI = async (chain: Chain, startTime: number, endTime: number): P
 
   while (hasMore) {
     const params = new URLSearchParams({
-      fromChain: contract[chain],
+      fromChain: contract[chain].id,
       fromTimestamp: startTime.toString(),
       toTimestamp: endTime.toString(),
       status: 'DONE',
@@ -111,12 +220,13 @@ const fetchFromAPI = async (chain: Chain, startTime: number, endTime: number): P
     transfers.forEach((tx) => {
       if (
         tx.status === 'DONE' &&
-        tx.receiving.chainId !== Number(contract[chain]) // Ensure it's a cross-chain transfer
+        tx.receiving.chainId !== Number(contract[chain].id) // Ensure it's a cross-chain transfer
       ) {
         const value = parseFloat(tx.sending.amountUSD) || 0;
         totalValue += value;
       }
     });
+
     nextCursor = response.next;
     hasMore = response.hasNext;
   }
@@ -134,7 +244,7 @@ const fetch = async (options: FetchOptions): Promise<FetchResultVolume> => {
 
   const dailyVolume = options.createBalances();
   const data: any[] = await options.getLogs({
-    target: contract[options.chain],
+    target: contract[options.chain].id,
     topic: '0xcba69f43792f9f399347222505213b55af8e0b0b54b893085c2e27ecbe1644f1',
     eventAbi: LifiBridgeEvent,
   });
@@ -150,7 +260,10 @@ const adapter: SimpleAdapter = {
   adapter: Object.keys(contract).reduce((acc, chain) => {
     return {
       ...acc,
-      [chain]: { fetch, start: '2023-08-10', }
+      [chain]: {
+        fetch,
+        start: contract[chain].startTime
+      }
     }
   }, {})
 };
