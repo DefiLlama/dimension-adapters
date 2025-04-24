ca39cbbb8e142ba3525928c07ace287bf127a35a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-24 16:10:33 +0200

    fix superchain

--- fees/superchain.ts
+++ fees/superchain.ts
@@ -1,42 +1,40 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types"
 import { CHAIN } from "../helpers/chains"
-import ADDRESSES from '../helpers/coreAssets.json'
 
 const fees: any = {
   [CHAIN.BASE]: ['0x9c3631dDE5c8316bE5B7554B0CcD2631C15a9A05'],
   [CHAIN.ETHEREUM]: [
     '0xa3d596eafab6b13ab18d40fae1a962700c84adea',
-    '0x793e01dCf6F9759Bf26dd7869b03129e64217537',
-    '0x4a4962275DF8C60a80d3a25faEc5AA7De116A746',
-    '0xe900b3Edc1BA0430CFa9a204A1027B90825ac951',
-    '0xed4811010a86f7c39134fbc20206d906ad1176b6',
-    '0x13f37e6b638ca83e7090bb3722b3ae04bf884019',
-    '0x99c9fc46f92e8a1c0dec1b1747d010903e884be1',
-    '0xC2eC5Dd60CBD5242b8cdeB9d0d37Ff6024584631', 
+    // '0xC2eC5Dd60CBD5242b8cdeB9d0d37Ff6024584631',
   ]
 }
-const fetchFees = async (options: FetchOptions) => {
-  const { api, fromApi, createBalances } = options;
-  await api.sumTokens({ owners: fees[options.chain], tokens: [ADDRESSES.null] })
-  await fromApi.sumTokens({ owners: fees[options.chain], tokens: [ADDRESSES.null] })
-  const balances = createBalances()
-  balances.addBalances(api.getBalancesV2())
-  balances.subtract(fromApi.getBalancesV2())
-  const dailyFees = balances.clone()
+const fetchFees = async (_: any, _1: any, options: FetchOptions) => {
+  const logs = await options.getLogs({
+    targets: fees[options.chain],
+    eventAbi: 'event SafeReceived (address indexed sender, uint256 value)',
+  })
+  const logs2 = await options.getLogs({
+    targets: fees[options.chain],
+    eventAbi: 'event Deposited (address from, uint256 value, bytes data)',
+  })
+  const dailyFees = options.createBalances()
+  for (const log of logs) dailyFees.addGasToken(log.value)
+  for (const log of logs2) dailyFees.addGasToken(log.value)
+
   return {
     dailyFees,
+    dailyRevenue: dailyFees,
   }
 }
 
 const adapter: SimpleAdapter = {
-  version: 2,
   adapter: {
     [CHAIN.BASE]: {
       fetch: fetchFees,
-          },
+    },
     [CHAIN.ETHEREUM]: {
       fetch: fetchFees,
-          },
+    },
   }
 }
 
