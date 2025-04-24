23fd73057f9730eb5b78fdc240b01afdb3141ec8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-24 14:01:32 +0200

    code refactor

--- fees/bucket-protocol/index.ts
+++ fees/bucket-protocol/index.ts
@@ -1,25 +1,20 @@
-import {
-  Adapter,
-  FetchResultFees,
-  FetchResultV2,
-  FetchV2,
-} from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import fetchURL from "../../utils/fetchURL";
+import { Adapter, FetchOptions, } from "../../adapters/types"
+import { CHAIN } from "../../helpers/chains"
+import fetchURL from "../../utils/fetchURL"
 
-const bucketApiURL = "https://open-api.bucketprotocol.io/api/";
+const bucketApiURL = "https://open-api.bucketprotocol.io/api/"
 
 interface DailyStats {
-  date: string;
-  total_records: number;
-  total_fee_value: string;
-  average_fee_value: string;
-  min_fee_value: string;
-  max_fee_value: string;
-  first_record_time: string;
-  last_record_time: string;
-  first_record_time_ms: number;
-  last_record_time_ms: number;
+  date: string
+  total_records: number
+  total_fee_value: string
+  average_fee_value: string
+  min_fee_value: string
+  max_fee_value: string
+  first_record_time: string
+  last_record_time: string
+  first_record_time_ms: number
+  last_record_time_ms: number
 }
 
 const methodology = {
@@ -27,27 +22,24 @@ const methodology = {
     "All the services fees paid by users, including borrow, PSM, liquidation, redeem, flashLoan and interest",
   dailyRevenue:
     "All the services fees paid by users, including borrow, PSM, liquidation, redeem, flashLoan and interest earned by Bucket",
-};
+}
 
-const fetchBucketStats: FetchV2 = async ({
-  startTimestamp,
-}): Promise<FetchResultV2> => {
-  const url = `${bucketApiURL}fees?timestamp_ms=${startTimestamp * 1000}`;
-  const stats: DailyStats = (await fetchURL(url)).data;
+const fetchBucketStats = async (_: any, _1: any, { startTimestamp, }: FetchOptions) => {
+  const url = `${bucketApiURL}fees?timestamp_ms=${startTimestamp * 1000}`
+  const stats: DailyStats = (await fetchURL(url)).data
 
-  const dailyFees = stats.total_fee_value;
-  const dailyRevenue = stats.total_fee_value;
+  const dailyFees = stats.total_fee_value
+  const dailyRevenue = stats.total_fee_value
 
   return {
     dailyFees,
     dailyUserFees: dailyFees,
     dailyRevenue,
     dailyProtocolRevenue: dailyRevenue,
-  };
-};
+  }
+}
 
 const adapter: Adapter = {
-  version: 2,
   adapter: {
     [CHAIN.SUI]: {
       fetch: fetchBucketStats,
@@ -57,6 +49,6 @@ const adapter: Adapter = {
       },
     },
   },
-};
+}
 
-export default adapter;
+export default adapter
