3f8bb5e4fbe3611fe1578484d2fed55a6efb145a
Bruno Wu <wubruno@outlook.com>
2025-04-24 11:52:07 +0200

    Add revenue for oogabooga

--- aggregators/oogabooga/index.ts
+++ aggregators/oogabooga/index.ts
@@ -4,18 +4,44 @@ import { CHAIN } from "../../helpers/chains";
 const swapEvent =
 	"event Swap(address indexed sender,uint256 inputAmount,address indexed inputToken,uint256 amountOut,address indexed outputToken,int256 slippage,uint32 referralCode,address to)";
 
+const quoteEvent =
+	"event Fee(address feeToken, uint256 feeAmount, uint256 obQuote, uint256 bexQuote, uint256 kodiakQuote)";
+
+const OBEXECUTOR_FEE_MODULE = "0xF83ECD5511cf190764Be32D2F9eCeD57a8676cdc";
+const OBROUTER = "0xFd88aD4849BA0F729D6fF4bC27Ff948Ab1Ac3dE7";
+
 async function fetch({ getLogs, createBalances }: FetchOptions) {
 	const dailyVolume = createBalances();
-	const logs = await getLogs({
-		targets: ["0xFd88aD4849BA0F729D6fF4bC27Ff948Ab1Ac3dE7"],
+	const dailyFees = createBalances();
+
+	const swapEvents = await getLogs({
+		targets: [OBROUTER],
 		eventAbi: swapEvent,
 	});
-
-	for (const l of logs) {
+	for (const l of swapEvents) {
 		dailyVolume.add(l.outputToken, l.amountOut);
 	}
 
-	return { dailyVolume };
+	// Positive slippage
+	for (const l of swapEvents) {
+		if (l.slippage > 0n) {
+			dailyFees.add(l.outputToken, l.slippage);
+		}
+	}
+
+	// Swap fees
+	const feeEvents = await getLogs({
+		targets: [OBEXECUTOR_FEE_MODULE],
+		eventAbi: quoteEvent,
+	});
+
+	for (const l of feeEvents) {
+		if (l.feeAmount > 0n) {
+			dailyFees.add(l.feeToken, l.feeAmount);
+		}
+	}
+
+	return { dailyVolume, dailyRevenue: dailyFees };
 }
 
 export default {
