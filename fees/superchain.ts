ca03796568693f5de90937f1898b791c5f3bd365
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-24 16:45:55 +0200

    superchain: add link to source of wallets

--- fees/superchain.ts
+++ fees/superchain.ts
@@ -1,13 +1,15 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types"
 import { CHAIN } from "../helpers/chains"
 
+// https://github.com/ethereum-optimism/op-analytics/blob/main/op_collective_economics/opcollective_feesplit/inputs/op_collective_feesplit_config.csv
 const fees: any = {
   [CHAIN.BASE]: ['0x9c3631dDE5c8316bE5B7554B0CcD2631C15a9A05'],
   [CHAIN.ETHEREUM]: [
     '0xa3d596eafab6b13ab18d40fae1a962700c84adea',
-    // '0xC2eC5Dd60CBD5242b8cdeB9d0d37Ff6024584631',
+    '0xC2eC5Dd60CBD5242b8cdeB9d0d37Ff6024584631',
   ]
 }
+
 const fetchFees = async (_: any, _1: any, options: FetchOptions) => {
   const logs = await options.getLogs({
     targets: fees[options.chain],
