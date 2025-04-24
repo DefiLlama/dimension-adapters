e00850ee472cb8735bc35f0495f5bc5edfab817a
HoldStation <develop@holdstation.com>
2025-04-24 18:45:18 +0700

    feat(berachain): add dimensions berachain (#3012)
    Co-authored-by: CuongNT <cuongnt@holdstation.com>

--- fees/holdstation-defutures.ts
+++ fees/holdstation-defutures.ts
@@ -8,6 +8,11 @@ const historicalVolumeEndpoint = (from: string, to: string) =>
 const dailyVolumeEndpoint = (from: string, to: string) =>
   `https://api-trading.holdstation.com/api/trading-history/volume-by-day?fromDate=${from}&toDate=${to}`;
 
+const historicalVolumeBerachainEndpoint = (from: string, to: string) =>
+  `https://api-trading-bera.holdstation.com/api/fees/summary/internal?fromDate=${from}&toDate=${to}`;
+const dailyVolumeBerachainEndpoint = (from: string, to: string) =>
+  `https://api-trading-bera.holdstation.com/api/trading-history/volume-by-day?fromDate=${from}&toDate=${to}`;
+
 interface IFees {
   totalFee: string;
   govFee: string;
@@ -20,32 +25,54 @@ interface DailyVolume {
   totalVolume: string;
 }
 
-const fetch = async (timestamp: number): Promise<FetchResult> => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  const fromTimestamp = new Date(dayTimestamp * 1000).toISOString().split("T")[0];
-  const toTimestamp = new Date((dayTimestamp + 60 * 60 * 24) * 1000).toISOString().split("T")[0];
-  const data: IFees = (await fetchURL(historicalVolumeEndpoint(fromTimestamp, toTimestamp))).result;
-  const dailyVolume: DailyVolume[] = (await fetchURL(dailyVolumeEndpoint(fromTimestamp, fromTimestamp)));
+type URLBuilder = (from: string, to: string) => string;
 
-  const dailyFees = data.totalFee;
-  const dailyRevenue = data.govFee;
-  const dailySupplySideRevenue = data.vaultFee;
-
-  return {
-    dailyFees,
-    dailyRevenue,
-    dailyVolume: dailyVolume.length > 0 ? dailyVolume[0].volume : "0",
-    totalVolume: dailyVolume.length > 0 ? dailyVolume[0].totalVolume : "0",
-    dailySupplySideRevenue: dailySupplySideRevenue,
-    timestamp: dayTimestamp,
-  };
+const endpointMap: { [chain: string]: {historical: URLBuilder, daily: URLBuilder} } = {
+  [CHAIN.ERA]: {
+    historical: historicalVolumeEndpoint,
+    daily: dailyVolumeEndpoint,
+  },
+  [CHAIN.BERACHAIN]: {
+    historical: historicalVolumeBerachainEndpoint,
+    daily: dailyVolumeBerachainEndpoint,
+  }
 };
+
+const fetch =
+  (chain: string) =>
+  async (timestamp: number): Promise<FetchResult> => {
+
+    const { historical, daily } = endpointMap[chain];
+
+    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
+    const fromTimestamp = new Date(dayTimestamp * 1000).toISOString().split("T")[0];
+    const toTimestamp = new Date((dayTimestamp + 60 * 60 * 24) * 1000).toISOString().split("T")[0];
+    const data: IFees = (await fetchURL(historical(fromTimestamp, toTimestamp))).result;
+    const dailyVolume: DailyVolume[] = (await fetchURL(daily(fromTimestamp, fromTimestamp)));
+
+    const dailyFees = data.totalFee;
+    const dailyRevenue = data.govFee;
+    const dailySupplySideRevenue = data.vaultFee;
+
+    return {
+      dailyFees,
+      dailyRevenue,
+      dailyVolume: dailyVolume.length > 0 ? dailyVolume[0].volume : "0",
+      totalVolume: dailyVolume.length > 0 ? dailyVolume[0].totalVolume : "0",
+      dailySupplySideRevenue: dailySupplySideRevenue,
+      timestamp: dayTimestamp,
+    };
+  }
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.ERA]: {
-      fetch,
+      fetch: fetch(CHAIN.ERA),
       start: '2023-05-09',
+    },
+    [CHAIN.BERACHAIN]: {
+      fetch: fetch(CHAIN.BERACHAIN),
+      start: '2025-02-07',
     },
   },
 };
