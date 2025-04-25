c716a1942383acedfabea18013965e23dab0e5e9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-25 09:48:38 +0200

    fix cetus

--- dexs/cetus/index.ts
+++ dexs/cetus/index.ts
@@ -1,8 +1,6 @@
 import fetchURL from "../../utils/fetchURL";
-import { Chain } from "@defillama/sdk/build/general";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
 type IUrl = {
   [s: string]: {
@@ -10,7 +8,6 @@ type IUrl = {
     histogramUrl: string,
   };
 }
-
 
 const url: IUrl = {
   [CHAIN.APTOS]: {
@@ -29,45 +26,34 @@ interface IVolumeall {
   date: string;
 }
 
-const fetch = (chain: Chain) => {
-  return async ({ startTimestamp, endTimestamp }: FetchOptions) => {
-    if (chain === CHAIN.SUI) {
-      const totalVolume =(await fetchURL(`https://api-sui.cetus.zone/v2/sui/vol/time_range?date_type=hour&start_time=0&end_time=${endTimestamp}`)).data.vol_in_usd;
-      const dailyVolume =(await fetchURL(`https://api-sui.cetus.zone/v2/sui/vol/time_range?date_type=hour&start_time=${startTimestamp}&end_time=${endTimestamp}`)).data.vol_in_usd;
-      const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(endTimestamp * 1000))
-      return {
-        totalVolume: totalVolume,
-        dailyVolume: dailyVolume,
-        timestamp: dayTimestamp,
-      };
-    }
-    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(endTimestamp * 1000))
-    const historicalVolume: IVolumeall[] = (await fetchURL(url[chain].histogramUrl)).data.list;
-    const totalVolume = (await fetchURL(url[chain].countUrl)).data.vol_in_usd
-    const dailyVolume = historicalVolume
-      .find(dayItem => (new Date(dayItem.date.split('T')[0]).getTime() / 1000) === dayTimestamp)?.num
-    return {
-      totalVolume: totalVolume,
-      dailyVolume: dailyVolume,
-      timestamp: dayTimestamp,
-    };
+async function fetch(_: any, _1: any, { startOfDay, endTimestamp, chain, }: FetchOptions) {
+  const historicalVolume: IVolumeall[] = (await fetchURL(url[chain].histogramUrl)).data.list;
+  const dailyVolume = historicalVolume
+    .find(dayItem => (new Date(dayItem.date.split('T')[0]).getTime() / 1000) === startOfDay)?.num
+  return {
+    dailyVolume: dailyVolume,
+    timestamp: startOfDay,
+  };
+}
+
+async function fetchSUI(_: any, _1: any, { startTimestamp, endTimestamp }: FetchOptions) {
+  const dailyVolume = (await fetchURL(`https://api-sui.cetus.zone/v2/sui/vol/time_range?date_type=hour&start_time=${startTimestamp}&end_time=${endTimestamp}`)).data.vol_in_usd;
+  return {
+    dailyVolume: dailyVolume,
   };
 }
 
 
-
-
 const adapter: SimpleAdapter = {
-  version: 2,
   adapter: {
-      [CHAIN.APTOS]: {
-        fetch: fetch(CHAIN.APTOS),
-        start: '2022-10-20',
-      },
-      [CHAIN.SUI]: {
-        fetch: fetch(CHAIN.SUI),
-        start: '2023-05-02',
-      }
+    [CHAIN.APTOS]: {
+      fetch,
+      start: '2022-10-20',
+    },
+    [CHAIN.SUI]: {
+      fetch: fetchSUI,
+      start: '2023-05-02',
+    }
   }
 };
 
