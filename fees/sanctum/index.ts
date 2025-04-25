fd93f12d689c5b7d97f6d87cb36632ffa3220526
alkinemk <mazatkelian@yahoo.fr>
2025-04-25 21:25:31 +0300

    address comments

--- fees/sanctum/index.ts
+++ fees/sanctum/index.ts
@@ -1,16 +1,11 @@
-// Documentation for Sanctum Reserve
-//
-// Fees: There is a dynamic fee charged for utilisation of the Reserve Pool - based on the percentage of SOL left in the Reserve pool. This allows for low fees most of the time, and ensures efficient usage of SOL in times of great liquidity demand.
-// Source: https://learn.sanctum.so/docs/protocol/the-reserve/fees
-//
-// Are deposits open for The Reserve?: Public deposits are not open for The Reserve. Depositors will need to deposit SOL for a long time frame, and cannot remove their SOL at any given time. Returns rely on periods of demand for SOL, which fluctuate and cannot be predicted.
-// Source: https://learn.sanctum.so/docs/protocol/the-reserve/are-deposits-open-for-the-reserve
-//
-// What does the Reserve do?: Sanctum Reserve Pool provides deep liquidity for all liquid staking tokens on Solana. It accepts staked SOL and gives SOL in return, unstaking the staked SOL at the end of the epoch to replenish its reserves.
-// Source: https://learn.sanctum.so/docs/protocol/the-reserve/what-does-the-reserve-do
-//
-// When do I use the Reserve Pool?: You can unstake a stake account instantly to receive SOL. If the validator your stake account is staked to does not have its own LST, you can still unstake to receive SOL instantly.
-// Source: https://learn.sanctum.so/docs/protocol/the-reserve/when-do-i-use-the-reserve-pool
+/*
+
+Fees: There is a dynamic fee charged for utilisation of the Reserve Pool - based on the percentage of SOL left in the Reserve pool. This allows for low fees most of the time, and ensures efficient usage of SOL in times of great liquidity demand.
+Source: https://learn.sanctum.so/docs/protocol/the-reserve/fees
+
+For the Reserve and the Router, fees = revenue because there is no stakeholder other than Sanctum
+
+*/
 
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
