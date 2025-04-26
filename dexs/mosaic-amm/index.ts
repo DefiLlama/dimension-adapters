c7484581098bc0da15be6775cb410c57f4b03c28
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-26 20:52:12 +0200

    minor fix

--- dexs/mosaic-amm/index.ts
+++ dexs/mosaic-amm/index.ts
@@ -1,27 +1,18 @@
-import fetchURL from "../../utils/fetchURL";
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
+import fetchURL from "../../utils/fetchURL"
+import { CHAIN } from "../../helpers/chains"
 
-async function fetch(_: any, _1: any, { startOfDay }: FetchOptions) {
-  const pools = (await fetchURL("https://stats.mosaic.ag/v1/public/pools")).data.pools;
+async function fetch() {
+  const pools = (await fetchURL("https://stats.mosaic.ag/v1/public/pools")).data.pools
+  const dailyVolume = pools.reduce((volume: number, pool: any) => volume + pool.stats.volume_24h_usd, 0,)
 
-  const dailyVolume = pools.reduce(
-    (volume: number, pool: any) => volume + pool.stats.volume_24h_usd,
-     0,
-  );
-
-  return {
-    dailyVolume: dailyVolume,
-    timestamp: startOfDay,
-  };
+  return { dailyVolume: dailyVolume, }
 }
 
-const adapter: SimpleAdapter = {
+export default {
   adapter: {
     [CHAIN.MOVE]: {
+      runAtCurrTime: true,
       fetch,
     },
   },
-};
-
-export default adapter;
+}
--- fees/mosaic-amm.ts
+++ fees/mosaic-amm.ts
@@ -1,26 +1,18 @@
-import fetchURL from "../utils/fetchURL";
-import { FetchOptions, SimpleAdapter } from "../adapters/types";
-import { CHAIN } from "../helpers/chains";
+import fetchURL from "../utils/fetchURL"
+import { CHAIN } from "../helpers/chains"
 
 async function fetch() {
-  const pools = (await fetchURL("https://stats.mosaic.ag/v1/public/pools")).data.pools;
+  const pools = (await fetchURL("https://stats.mosaic.ag/v1/public/pools")).data.pools
+  const dailyFees = pools.reduce((fees: number, pool: any) => fees + pool.stats.fee_24h_usd, 0,)
 
-  const dailyFees = pools.reduce(
-    (fees: number, pool: any) => fees + pool.stats.fee_24h_usd,
-     0,
-  );
-
-  return {
-    dailyFees,
-  };
+  return { dailyFees, }
 }
 
-const adapter: SimpleAdapter = {
+export default {
   adapter: {
     [CHAIN.MOVE]: {
       fetch,
+      runAtCurrTime: true,
     },
   },
-};
-
-export default adapter;
+}
