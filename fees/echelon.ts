69b1f33d969ca07295db30088ff8ebb3181dc4b4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-28 11:28:43 +0200

    refactor

--- fees/echelon.ts
+++ fees/echelon.ts
@@ -17,8 +17,8 @@ const feesEndpoint = (endTimestamp: number, timeframe: string) =>
 
 const movementFeesEndpoint = (endTimestamp: number, timeframe: string) =>
   endTimestamp
-      ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
-      : feesQueryURL + timeframe + "&network=movement_mainnet";
+    ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
+    : feesQueryURL + timeframe + "&network=movement_mainnet";
 
 const fetch = async (timestamp: number) => {
   const dayFeesQuery = (await fetchURL(feesEndpoint(timestamp, "1D")))?.data;
@@ -27,28 +27,36 @@ const fetch = async (timestamp: number) => {
     0
   );
 
-  const dayFeesMovementQuery = (await fetchURL(movementFeesEndpoint(timestamp, "1D")))?.data;
-  const dailyMovementFees = dayFeesMovementQuery.reduce(
-      (partialSum: number, a: IVolumeall) => partialSum + a.value,
-      0
-  );
-
   const totalFeesQuery = (await fetchURL(feesEndpoint(0, "ALL")))?.data;
   const totalFees = totalFeesQuery.reduce(
     (partialSum: number, a: IVolumeall) => partialSum + a.value,
     0
   );
 
+  return {
+    totalFees: totalFees,
+    dailyFees: dailyFees,
+  };
+};
+
+
+const fetchMovement = async (timestamp: number) => {
+
+  const dayFeesMovementQuery = (await fetchURL(movementFeesEndpoint(timestamp, "1D")))?.data;
+  const dailyMovementFees = dayFeesMovementQuery.reduce(
+    (partialSum: number, a: IVolumeall) => partialSum + a.value,
+    0
+  );
+
   const totalFeesMovementQuery = (await fetchURL(movementFeesEndpoint(0, "ALL")))?.data;
   const totalMovementFees = totalFeesMovementQuery.reduce(
-      (partialSum: number, a: IVolumeall) => partialSum + a.value,
-      0
+    (partialSum: number, a: IVolumeall) => partialSum + a.value,
+    0
   );
 
   return {
-    totalFees: totalFees + totalMovementFees,
-    dailyFees: dailyFees + dailyMovementFees,
-    timestamp,
+    totalFees: totalMovementFees,
+    dailyFees: dailyMovementFees,
   };
 };
 
@@ -56,7 +64,11 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.APTOS]: {
       fetch,
-      start: '2023-04-03',
+      start: '2024-04-25',
+    },
+    [CHAIN.MOVE]: {
+      fetch: fetchMovement,
+      start: '2025-03-15',
     },
   },
 };
