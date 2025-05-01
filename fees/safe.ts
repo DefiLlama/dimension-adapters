ae2ae0c521208eefe685c9395191d93da95cd80d
Eden <noat.eth@gmail.com>
2025-05-01 14:23:56 +0700

    add new fee recipient address from cowswap fees

--- fees/safe.ts
+++ fees/safe.ts
@@ -2,6 +2,17 @@ import { Adapter, FetchOptions, } from '../adapters/types';
 import { CHAIN } from '../helpers/chains';
 import { queryIndexer } from '../helpers/indexer';
 
+function getFeeRecipient(timestamp: number) {
+  // new recipient migration
+  // https://etherscan.io/tx/0x69c53420e8d37122a5513896c7953e6963eea000f45f3698d50d891f56b48ab9
+  if (timestamp <= 1743465600) {
+    return '63695Eee2c3141BDE314C5a6f89B98E62808d716'
+  } else {
+    return 'E344241493D573428076c022835856a221dB3E26'
+  }
+}
+
+// this address hold CowSwap fees for partner: https://etherscan.io/address/0xa03be496e67ec29bc62f01a428683d7f9c204930
 // order example https://explorer.cow.fi/orders/0x6926d210ec9c5a9acb36db99842b10cd9ad328d391fcc42d23cf49bfb1b17720d7f876620bdff1c9aba5b444128d1722ddd678b367083dfb?tab=overview
 const fetch: any = async (options: FetchOptions) => {
     const dailyFees = options.createBalances()
@@ -12,7 +23,7 @@ const fetch: any = async (options: FetchOptions) => {
   FROM
     ethereum.traces
   WHERE
-    to_address = '\\x63695Eee2c3141BDE314C5a6f89B98E62808d716'
+    to_address = '\\x${getFeeRecipient(options.fromTimestamp)}'
     AND block_time BETWEEN llama_replace_date_range;
     `, options);
     eth_transfer_logs.map((e: any) => dailyFees.addGasToken(e.eth_value ?? 0))
