d59ec8dbbda39ea8d4bbe5ff098419818086a1ca
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-05-02 15:08:29 +0200

    optmize velodrome and aerodrome slipstream

--- dexs/aerodrome-slipstream/index.ts
+++ dexs/aerodrome-slipstream/index.ts
@@ -3,6 +3,9 @@ import { FetchOptions, FetchResult, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { addOneToken } from '../../helpers/prices';
 import { filterPools2 } from "../../helpers/uniswap";
+import { sliceIntoChunks } from "@defillama/sdk/build/util";
+
+
 
 const CONFIG = {
   factory: '0x5e7BB104d84c7CB9B682AaC2F3d509f5F406809A'
@@ -29,14 +32,14 @@ type PoolInfo = {
   logs: any
 };
 
-const fetch = async (_:any, _1:any, fetchOptions: FetchOptions): Promise<FetchResult> => {
+const fetch = async (_: any, _1: any, fetchOptions: FetchOptions): Promise<FetchResult> => {
   // const startTime = Date.now();
   const { api, createBalances, getToBlock, getFromBlock, chain, getLogs } = fetchOptions
   const dailyVolume = createBalances()
   const dailyFees = createBalances()
   const [toBlock, fromBlock] = await Promise.all([getToBlock(), getFromBlock()])
 
-  const rawPools = await getLogs({ target: CONFIG.factory, fromBlock: 13843704, toBlock, topics: [topics.event_poolCreated], eventAbi: eventAbis.event_poolCreated }) 
+  const rawPools = await getLogs({ target: CONFIG.factory, fromBlock: 13843704, toBlock, topics: [topics.event_poolCreated], eventAbi: eventAbis.event_poolCreated })
   const pools = rawPools.map(([token0, token1, _tick, pair]) => ({ pair, token0, token1 }))
 
   const { pairs, token0s, token1s } = await filterPools2({
@@ -44,7 +47,7 @@ const fetch = async (_:any, _1:any, fetchOptions: FetchOptions): Promise<FetchRe
     pairs: pools.map(p => p.pair),
     token0s: pools.map(p => p.token0),
     token1s: pools.map(p => p.token1),
-    minUSDValue: 2000,
+    minUSDValue: 5000,
     maxPairSize: 1000
   })
 
@@ -52,62 +55,41 @@ const fetch = async (_:any, _1:any, fetchOptions: FetchOptions): Promise<FetchRe
   const fees = await api.multiCall({ calls: filteredPools.map(({ pair }) => ({ target: pair })), abi: abis.fee });
   const poolsAndFees = filteredPools.map((pool, index) => ({ ...pool, fees: fees[index] / 1e6, logs: [] as any[] }));
 
-  const blockStep = 5000;
+  const blockStep = 10000;
   const poolChunkSize = 10;
+  const poolChunks = sliceIntoChunks(poolsAndFees, poolChunkSize);
+  let chunkCount = poolChunks.length;
+  let chunkIndex = 0;
 
-  for (let i = 0; i < poolsAndFees.length; i += poolChunkSize) {
-    const chunkPools = poolsAndFees.slice(i, i + poolChunkSize);
+  for (const chunk of poolChunks) {
 
     for (let startBlock = fromBlock; startBlock <= toBlock; startBlock += blockStep) {
       const endBlock = Math.min(startBlock + blockStep - 1, toBlock);
 
-      const logsChunk = await Promise.all(
-        chunkPools.map(async (pool) => {
-          const logs = await sdk.indexer.getLogs({
-            chain,
-            target: pool.pair,
-            fromBlock: startBlock,
-            toBlock: endBlock,
-            topics: [topics.event_swap],
-            eventAbi: eventAbis.event_swap,
-            all: true
-          });
-          return { pool, logs };
+      const logs = await sdk.getEventLogs({
+        chain,
+        targets: chunk.map(p => p.pair),
+        fromBlock: startBlock,
+        toBlock: endBlock,
+        eventAbi: eventAbis.event_swap,
+        flatten: false,
+      })
+
+      logs.forEach((logs: any, index: number) => {
+        const { token0, token1, fees = 0 } = chunk[index]
+        logs.forEach((log: any) => {
+          const amount0 = Number(log[2])
+          const amount1 = Number(log[3])
+          const fee0 = amount0 * fees
+          const fee1 = amount1 * fees
+    
+          addOneToken({ chain, balances: dailyVolume, token0, token1, amount0, amount1 })
+          addOneToken({ chain, balances: dailyFees, token0, token1, amount0: fee0, amount1: fee1 })
         })
-      );
-
-      for (const { pool, logs } of logsChunk) {
-        pool.logs.push(...logs);
-      }
+      })
     }
-  }
-
-  // const totalPools = poolsAndFees.length;
-  // let totalLogs = 0;
-  // const logsByPool = poolsAndFees.map(pool => {
-  //   const logCount = pool.logs.length;
-  //   totalLogs += logCount;
-  //   return { pool: pool.pair, logCount };
-  // });
-
-  // const runtime = Date.now() - startTime;
-  // console.log(`Script execution time: ${runtime} ms`);
-  // console.log(`Total number of poolsAndFees: ${totalPools}`);
-  // console.log(`Total number of logs: ${totalLogs}`);
-  // console.table(logsByPool);
-
-  for (const pool of poolsAndFees) {
-    const { token0, token1, fees = 0, logs = [] } = pool;
-
-    logs.forEach((log: any) => {
-      const amount0 = Number(log[2])
-      const amount1 = Number(log[3])
-      const fee0 = amount0 * fees
-      const fee1 = amount1 * fees
-
-      addOneToken({ chain, balances: dailyVolume, token0, token1, amount0, amount1 })
-      addOneToken({ chain, balances: dailyFees, token0, token1, amount0: fee0, amount1: fee1 })
-    })
+    chunkIndex++;
+    sdk.log('aerodrome Processed chunk', chunkIndex, 'of', chunkCount, 'for chain', chain)
   }
 
   return { dailyVolume, dailyFees, dailyRevenue: dailyFees, dailyHoldersRevenue: dailyFees }
@@ -115,7 +97,6 @@ const fetch = async (_:any, _1:any, fetchOptions: FetchOptions): Promise<FetchRe
 
 const adapters: SimpleAdapter = {
   version: 1,
-  isExpensiveAdapter: true,
   adapter: {
     [CHAIN.BASE]: {
       fetch: fetch as any,
