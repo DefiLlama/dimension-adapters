3e80136cd03e4dae9191ceeff75a43167a8c5413
Eden <noat.eth@gmail.com>
2025-05-06 00:02:26 +0700

    fix: query history data

--- fees/beraborrow/index.ts
+++ fees/beraborrow/index.ts
@@ -45,6 +45,33 @@ const Contracts = {
   ],
 }
 
+const FallbackCollateralVaults: Array<string> = [
+  '0x9158d1b0c9Cc4EC7640EAeF0522f710dADeE9a1B',
+  '0xAA2cBDe9f11f09ee9774D6d6C98dbB4792d9549a',
+  '0x3dfeBfc084dA9C8749Ef06839F399867C3448FdD',
+  '0x7B3487fFE71110a636e318f9E7a330f15Fd5436F',
+  '0x33aD23316A0C3dd999d4AeDe4fe99074DE42324b',
+  '0x933bD39CeDb5947523110c33E98bEa8480978adB',
+  '0xE59AB0C3788217e48399Dae3CD11929789e4d3b2',
+  '0xebB8152a4E5Bd5DB71EACEbC5239A414D46E9C59',
+  '0xCd6e82Dc0A62489688f8aB5dd4414d085C9bA4F7',
+  '0xD60c309053d75bcf939Bb0762F9F011096c929De',
+  '0x0430f05228E3a471C8C79B62835B967CF707060F',
+  '0x86d662DAD49D7584Caee6839E01D15B05Fa01204',
+  '0xc837E31a5440A294a8296237986aFeC64C43dA75',
+  '0xD08BE9db61798FbE8a5F10C01e31C39956Ad545B',
+  '0x93e39028f76b572702A29E43c9996A5BE0Cb7bd8',
+  '0x60AfF0Ff28F54eaD6B2cc676206fE329b645b1CD',
+  '0x4069A3f38d76190bC2af5324a2A0EFa81Bc52d3c',
+  '0xfC3111435C6d4CD1431862346Ac9646d21752Bb0',
+  '0xe62954577718CB92D59236Da8c483A8b44F83eEd',
+  '0xA8a3Dc9A75ec56C99287ad2Fd3b5E5f236d40DeE',
+  '0x5Aa4f0fecd81B94A6A7d85c3797bf64864C938c0',
+  '0x3bc3028e88341F8b319bD3178BA0b10706787b75',
+  '0x9b089d29dD48B5c1B20B2867372a2fDE16875080',
+  '0x32Ff3CEd25b916d52641D4081753F8951Faa7249',
+]
+
 interface CollateralVault {
   vault: string;
   asset: string;
@@ -59,30 +86,42 @@ interface CollateralVaultAmount extends CollateralVault {
 async function getCollateralVaults(options: FetchOptions): Promise<{[key: string]: CollateralVault}> {
   const vaults: {[key: string]: CollateralVault} = {}
 
-  const collateralVaults = (await options.api.call({
-    abi: Abis.getCollateralVaults,
-    target: Contracts.CollateralVaultRegistry,
-  })).map((vault: any) => vault.vault)
-  const collateralVaultsAssets = await options.api.multiCall({
+  // CollateralVaultRegistry contract was deployed on 2025-04-03
+  // before that, we use FallbackCollateralVaults
+  // after that, we get vaults list from CollateralVaultRegistry
+  let collateralVaults: Array<string> = FallbackCollateralVaults
+  if (options.api.timestamp && options.api.timestamp >= 1743724800) {
+    collateralVaults = (await options.api.call({
+      abi: Abis.getCollateralVaults,
+      target: Contracts.CollateralVaultRegistry,
+    })).map((vault: any) => vault.vault)
+  }
+
+  const collateralVaultsAssets = (await options.api.multiCall({
     abi: Abis.asset,
     calls: collateralVaults,
-  })
+    permitFailure: true,
+  })).map(item => item !== null ? item : ZeroAddress) // use ZeroAddress for non-existed vaults
   const collateralVaultsDecimals = await options.api.multiCall({
     abi: Abis.decimals,
     calls: collateralVaults,
+    permitFailure: true,
   })
   const collateralVaultsAssetsDecimals = await options.api.multiCall({
     abi: Abis.decimals,
     calls: collateralVaultsAssets,
+    permitFailure: true,
   })
 
   for (let i = 0; i < collateralVaults.length; i++) {
-    const vault = normalizeAddress(collateralVaults[i])
-    vaults[vault] = {
-      vault,
-      asset: collateralVaultsAssets[i],
-      vaultDecimals: Number(collateralVaultsDecimals[i]),
-      assetDecimals: Number(collateralVaultsAssetsDecimals[i])
+    if (collateralVaultsAssets[i] && collateralVaultsAssets[i] !== ZeroAddress) {
+      const vault = normalizeAddress(collateralVaults[i])
+      vaults[vault] = {
+        vault,
+        asset: collateralVaultsAssets[i],
+        vaultDecimals: Number(collateralVaultsDecimals[i]),
+        assetDecimals: Number(collateralVaultsAssetsDecimals[i])
+      }
     }
   }
 
@@ -199,10 +238,11 @@ const fetchFees = async (options: FetchOptions) => {
   const tuples = await options.api.multiCall({
     abi: Abis.getAllCollateralsAndDenManagers,
     calls: Contracts.DenManagerGettersContracts,
+    permitFailure: true,
   });
 
   // flat the tuples into a single array
-  const flatTuples = tuples.reduce((acc: any, tuple: any) => {
+  const flatTuples = tuples.filter(item => item !== null).reduce((acc: any, tuple: any) => {
     return acc.concat(tuple);
   }, []);
 
@@ -214,24 +254,22 @@ const fetchFees = async (options: FetchOptions) => {
     denManagers.forEach((denManager: any) => denManagersSet.add(denManager.toLowerCase()));
   });
   const collaterals = Array.from(collateralSet) as string[];
-  const liquidataionLogs = await options.getLogs({
-    targets: collaterals,
-    flatten: false,
-    eventAbi: Abis.TransferEvent,
-    topics: [
-      TRANSFER_TOPIC,
-      getAddressParam(ZeroAddress),
-      getAddressParam(Contracts.ProtocolFeeRecipient),
-    ],
-  });
-  liquidataionLogs.forEach((logs: any, index: any) => {
-    const collateral = collaterals[index]
-    logs
-      .filter((log: any) => denManagersSet.has(log.from.toLowerCase()))  // filter for transfers from denManagers
-      .forEach((log: any) => {
-        dailyFees.add(collateral, log.value);
-      });
-  })
+  if (collaterals.length > 0) {
+    const liquidataionLogs = await options.getLogs({
+      targets: collaterals,
+      flatten: false,
+      eventAbi: Abis.TransferEvent,
+    });
+    liquidataionLogs.forEach((logs: any, index: any) => {
+      const collateral = collaterals[index]
+      logs
+        // // filter for transfers from denManagers and to ProtocolFeeRecipient
+        .filter((log: any) => denManagersSet.has(log.from.toLowerCase()) && String(log.to).toLowerCase() === Contracts.ProtocolFeeRecipient)
+        .forEach((log: any) => {
+          dailyFees.add(collateral, log.value);
+        });
+    })
+  }
 
   // PermissionlessPSM deposit fee (in NECT)
   const psmDepositLogs = await options.getLogs({
