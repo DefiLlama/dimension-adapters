11b6ae0c4df72eb8f9fe88d872712540658172af
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-05 13:36:34 +0530

    aerodrome v1 Fix Bribe logic and add in slipstream (#3101)

--- dexs/aerodrome/index.ts
+++ dexs/aerodrome/index.ts
@@ -5,8 +5,9 @@ import { addOneToken } from '../../helpers/prices';
 import { ethers } from "ethers";
 
 const CONFIG = {
-  factory: '0x420DD381b31aEf6683db6B902084cB0FFECe40Da',
-  voter: '0x16613524e02ad97eDfeF371bC883F2F5d6C480A5'
+  PoolFactory: '0x420DD381b31aEf6683db6B902084cB0FFECe40Da',
+  voter: '0x16613524e02ad97eDfeF371bC883F2F5d6C480A5',
+  GaugeFactory: '0x35f35ca5b132cadf2916bab57639128eac5bbcb5'
 }
 
 const eventAbis = {
@@ -14,6 +15,7 @@ const eventAbis = {
   event_swap: 'event Swap(address indexed sender, address indexed to, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out)',
   event_gaugeCreated: 'event GaugeCreated(address indexed poolFactory, address indexed votingRewardsFactory, address indexed gaugeFactory, address pool, address bribeVotingReward, address feeVotingReward, address gauge, address creator)',
   event_notify_reward: 'event NotifyReward(address indexed from, address indexed reward, uint256 indexed epoch, uint256 amount)',
+  event_claim_rewards: 'event ClaimRewards(address indexed from, address indexed reward, uint256 amount)'
 }
 
 const abis = {
@@ -22,16 +24,18 @@ const abis = {
 
 const getBribes = async (fetchOptions: FetchOptions): Promise<{ dailyBribesRevenue: sdk.Balances }> => {
   const { createBalances, } = fetchOptions
-  const iface = new ethers.Interface([eventAbis.event_notify_reward]);
+  const iface = new ethers.Interface([eventAbis.event_claim_rewards]);
 
   const dailyBribesRevenue = createBalances()
   const logs_gauge_created = await fetchOptions.getLogs({ target: CONFIG.voter, fromBlock: 3200601, eventAbi: eventAbis.event_gaugeCreated, skipIndexer: true, })
   if (!logs_gauge_created?.length) return { dailyBribesRevenue };
 
-  const bribes_contract: string[] = logs_gauge_created.map((log) => log.bribeVotingReward.toLowerCase())
+  const bribes_contract: string[] = logs_gauge_created
+    .filter((log) => log[2].toLowerCase() === CONFIG.GaugeFactory.toLowerCase())
+    .map((log) => log[4].toLowerCase())
   const bribeSet = new Set(bribes_contract)
   // need to manually parse logs, auto parsing fails for some reason
-  const logs = await fetchOptions.getLogs({ noTarget: true, eventAbi: eventAbis.event_notify_reward, entireLog: true,  })
+  const logs = await fetchOptions.getLogs({ noTarget: true, eventAbi: eventAbis.event_claim_rewards, entireLog: true,  })
   logs.forEach((log: any) => {
     const contract = (log.address || log.source).toLowerCase()
     if (!bribeSet.has(contract)) return;
@@ -46,10 +50,10 @@ const getVolumeAndFees = async (fromBlock: number, toBlock: number, fetchOptions
   const dailyVolume = createBalances()
   const dailyFees = createBalances()
 
-  const rawPools = await fetchOptions.getLogs({ target: CONFIG.factory, fromBlock: 3200668, eventAbi: eventAbis.event_pool_created, onlyArgs: true, cacheInCloud: true, skipIndexer: true, })
+  const rawPools = await fetchOptions.getLogs({ target: CONFIG.PoolFactory, fromBlock: 3200668, eventAbi: eventAbis.event_pool_created, onlyArgs: true, cacheInCloud: true, skipIndexer: true, })
 
   const fees = await api.multiCall({
-    abi: abis.fees, target: CONFIG.factory, calls: rawPools.map(i => ({ params: [i.pool, i.stable] }))
+    abi: abis.fees, target: CONFIG.PoolFactory, calls: rawPools.map(i => ({ params: [i.pool, i.stable] }))
   })
   const poolInfoMap = {} as any
   const aeroPoolSet = new Set()
