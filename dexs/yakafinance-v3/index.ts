e9efbe33a723417075fb20285ffd4f86d5d68d9e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-05-08 12:55:17 +0200

    refactor

--- dexs/yakafinance-v3/index.ts
+++ dexs/yakafinance-v3/index.ts
@@ -1,36 +1,33 @@
-import request, { gql } from "graphql-request";
+import request from "graphql-request";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const fetch = async (timestamp: number, _:any, options: FetchOptions): Promise<any> => {
-    const dayID = Math.floor(options.startOfDay / 86400);
-    console.log("dayID :" + dayID)
-    const query =gql`
+const fetch = async (_timestamp: number, _: any, options: FetchOptions): Promise<any> => {
+  const dayID = Math.floor(options.startOfDay / 86400);
+  const query = `
     {
         algebraDayData(id:${dayID}) {
             id
             volumeUSD
-        }
-        factories {
-            totalVolumeUSD
+            feesUSD
         }
     }`;
-    const url = "https://api.studio.thegraph.com/query/50593/yaka-analytics/v0.0.3";
-    const req = await request(url, query);
-    return {
-        dailyVolume: req.algebraDayData.volumeUSD,
-        totalVolume: req.factories[0].totalVolumeUSD,
-        timestamp: timestamp,
-    }
+  const url = "https://api.studio.thegraph.com/query/50593/yaka-analytics/v0.0.3";
+  const req = await request(url, query);
+  return {
+    dailyVolume: req.algebraDayData.volumeUSD,
+    dailyFees: req.algebraDayData.feesUSD,
+    dailyRevenue: req.algebraDayData.feesUSD* 0.12,
+  }
 }
 
 const adapter: SimpleAdapter = {
-    adapter: {
-        [CHAIN.SEI]: {
-            fetch,
-            start: '2024-10-01',
-        },
-    }
+  adapter: {
+    [CHAIN.SEI]: {
+      fetch,
+      start: '2024-10-01',
+    },
+  }
 }
 
 export default adapter;
--- fees/yakafinance/index.ts
+++ fees/yakafinance/index.ts
@@ -1,25 +1,13 @@
 import { Adapter, FetchOptions } from '../../adapters/types';
 import { CHAIN } from '../../helpers/chains';
-import request, { gql } from "graphql-request";
+import request from "graphql-request";
 import BigNumber from "bignumber.js";
-
-const getFees = async (options: FetchOptions) => {
-    const [feeV1] = await Promise.all([fetchV1()(options)]);
-    const dailyFees = Number(feeV1.dailyFees);
-    const dailyRevenue = Number(feeV1.dailyRevenue);
-    const dailyHoldersRevenue = Number(feeV1.dailyHoldersRevenue);
-    return {
-        dailyFees: `${dailyFees}`,
-        dailyRevenue: `${dailyRevenue}`,
-        dailyHoldersRevenue: `${dailyHoldersRevenue}`,
-    }
-}
 
 const adapter: Adapter = {
     version: 2,
     adapter: {
         [CHAIN.SEI]: {
-            fetch: getFees,
+            fetch: getFees as any,
             start: 1719432193,
         },
     },
@@ -33,11 +21,7 @@ const PROTOCOL_FEE_RATE = 0.12;
 const endpoint = "https://api.goldsky.com/api/public/project_cltwdng5fw97s01x16mntew1i/subgraphs/yaka-finance/1.0.0/gn"
 const blocksEndpoint = "https://api.studio.thegraph.com/query/82132/sei-blocks/version/latest"
 
-function getUnixTimeNow() {
-    return Math.floor(Date.now() / 1000)
-}
-
-function getBlocks(timestamps) {
+async function getBlocks(timestamps) {
     let queryString = 'query blocks {'
     queryString += timestamps.map((timestamp) => {
         return `t${timestamp}:blocks(first: 1, orderBy: timestamp, orderDirection: desc, where: { timestamp_gt: ${timestamp}, timestamp_lt: ${timestamp + 600} }) {
@@ -45,61 +29,50 @@ function getBlocks(timestamps) {
         }`
     })
     queryString += '}'
-    return gql`${queryString}`
+    const blocksRes = await request(blocksEndpoint, queryString)
+    return timestamps.map((timestamp) => {
+        return blocksRes[`t${timestamp}`][0].number
+    })
 }
 
-
-
-const fetchV1 = () => {
-    return async ({ }: FetchOptions) => {
-        const utcCurrentTime = getUnixTimeNow()
-        const utcOneDayBack = utcCurrentTime - 86400
-
-        const blocksQuery = getBlocks([utcOneDayBack])
-        const blocksRes = await request(blocksEndpoint, blocksQuery)
-        console.log(blocksRes)
-        let blocks = []
-        for (const block in blocksRes) {
-            blocks.push(blocksRes[block][0].number)
-        }
-        const fromBlock = blocks[0]
-        const query = gql`
+async function getFees({ startTimestamp, endTimestamp }: FetchOptions) {
+    const [fromBlock, toBlock] = await getBlocks([startTimestamp, endTimestamp])
+    const query = `
       query fees {
         yesterday: pairs(block: {number: ${fromBlock}}, where: {volumeUSD_gt: "0"}, first: 1000) {
           id
           isStable
           volumeUSD
         }
-        today: pairs(where: {volumeUSD_gt: "0"}, first: 1000) {
+        today: pairs(block: {number: ${toBlock}}, where: {volumeUSD_gt: "0"}, first: 1000) {
           id
           isStable
           volumeUSD
         }
       }
     `;
-        const todayVolume: { [id: string]: BigNumber } = {};
-        const graphRes = await request(endpoint, query);
-        let dailyFee = new BigNumber(0);
-        for (const pool of graphRes["today"]) {
-            todayVolume[pool.id] = new BigNumber(pool.volumeUSD);
-        }
+    const todayVolume: { [id: string]: BigNumber } = {};
+    const graphRes = await request(endpoint, query);
+    let dailyFee = new BigNumber(0);
+    for (const pool of graphRes["today"]) {
+        todayVolume[pool.id] = new BigNumber(pool.volumeUSD);
+    }
 
-        for (const pool of graphRes["yesterday"]) {
-            if (!todayVolume[pool.id]) continue;
-            const dailyVolume = BigNumber(todayVolume[pool.id]).minus(
-                pool.volumeUSD
-            );
-            if (pool.isStable) {
-                dailyFee = dailyFee.plus(dailyVolume.times(STABLE_FEES));
-            } else {
-                dailyFee = dailyFee.plus(dailyVolume.times(VOLATILE_FEES));
-            }
+    for (const pool of graphRes["yesterday"]) {
+        if (!todayVolume[pool.id]) continue;
+        const dailyVolume = BigNumber(todayVolume[pool.id]).minus(
+            pool.volumeUSD
+        );
+        if (pool.isStable) {
+            dailyFee = dailyFee.plus(dailyVolume.times(STABLE_FEES));
+        } else {
+            dailyFee = dailyFee.plus(dailyVolume.times(VOLATILE_FEES));
         }
+    }
 
-        return {
-            dailyFees: dailyFee.toString(),
-            dailyRevenue: dailyFee.times(PROTOCOL_FEE_RATE).toString(),
-            dailyHoldersRevenue: dailyFee.minus(dailyFee.times(PROTOCOL_FEE_RATE)).toString(),
-        };
+    return {
+        dailyFees: dailyFee,
+        dailyRevenue: dailyFee.times(PROTOCOL_FEE_RATE),
+        dailyHoldersRevenue: dailyFee.minus(dailyFee.times(PROTOCOL_FEE_RATE)),
     };
 };
