c2339e31f6abc62552913c2cb1732677119f47c5
violet <violet@violets-MacBook-Pro.local>
2025-05-13 15:57:59 +0700

    replace dune by blockchain log

--- dexs/uniderp/index.ts
+++ dexs/uniderp/index.ts
@@ -1,43 +1,78 @@
 import { Adapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { queryDuneSql } from "../../helpers/dune";
 
-const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-    const volumeRes = await queryDuneSql(options, `
-        select
-            SUM(amount_usd) as volume
-        from dex.trades
-        where blockchain = 'unichain'
-            and project = 'uniswap'
-            and version = '4'
-            and tx_hash in (
-                select
-                    evt_tx_hash
-                from uniswap_v4_unichain.poolmanager_evt_swap
-                where id in (
-                        select
-                            id
-                        from uniswap_v4_unichain.poolmanager_evt_initialize
-                        where hooks = 0xb4960cd4f9147f9e37a7aa9005df7156f61e4444))
-            and block_time >= from_unixtime(${options.startTimestamp})
-                        AND block_time < from_unixtime(${options.endTimestamp})
-    `);
-    const dailyVolume = volumeRes[0].volume;
+const SWAP_TOPIC = "0x40e9cecb9f5f1f1c5b9c97dec2917b7ee92e57ba5563708daca94dd84ad7112f";
 
+const poolV4Abi = 'event Initialize(bytes32 indexed id, address indexed currency0, address indexed currency1, uint24 fee, int24 tickSpacing, address hooks, uint160 sqrtPriceX96, int24 tick)'
+const swapAbi = "event Swap(bytes32 indexed id, address indexed sender, int128 amount0, int128 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint24 fee)"
+
+const getAbsoluteBigInt = (value: bigint): bigint => {
+    return value < BigInt(0) ? value * BigInt(-1) : value;
+};
+
+const config = {
+    [CHAIN.UNICHAIN]: {
+      poolManager: "0x1f98400000000000000000000000000000000004", 
+      uniderpHook: "0xb4960cd4f9147f9e37a7aa9005df7156f61e4444", 
+      start: "2025-04-23",
+      fromBlock: 14569072
+    },
+}
+
+const fetch = async ( {createBalances, getLogs, chain }: FetchOptions) => {
+    const { poolManager, uniderpHook, fromBlock } = config[chain]
+
+    const dailyVolume = createBalances()
+
+    // Get list pools created by uniderp
+    const logs = await getLogs({
+        target: poolManager,
+        skipIndexer: true,
+        eventAbi: poolV4Abi,
+        fromBlock
+    })
+    const poolIds = logs.filter((log: any) => log.hooks.toLowerCase() === uniderpHook).map((log: any) => log.id);
+    for (const poolId of poolIds) {
+        // Filter all swap events from pools created by uniderp
+        const swapLogs = await getLogs({
+          target: poolManager,
+          skipIndexer: true,
+          eventAbi: swapAbi,
+          topics: [
+            SWAP_TOPIC,
+            poolId
+          ],
+        })
+        for (const log of swapLogs) {
+            dailyVolume.addGasToken(getAbsoluteBigInt(log.amount0));
+        }
+    }
+    
     return {
         dailyVolume
     }
 }
 
+const methodology = {
+    UserFees: "User pays 1% fees on each swap.",
+    ProtocolRevenue: "Treasury receives 0.5% of each swap. (0.2% from swap + 0.3% from LPs)",
+    Revenue: "All revenue generated comes from user fees.",
+    Fees: "All fees comes from the user."
+  }
+
 const adapter: Adapter = {
-    version: 1,
-    adapter: {
-        [CHAIN.UNICHAIN]: {
-            fetch: fetch as any,
-            start: '2025-05-01'
-        },
-    },
-    isExpensiveAdapter: true
+    version: 2,
+    adapter: Object.keys(config).reduce((acc, chain) => {
+        const { start } = config[chain];
+        acc[chain] = {
+          fetch,
+          start: start,
+          meta: {
+            methodology
+          },
+        };
+        return acc;
+    }, {}),
 }
 
 export default adapter;
--- fees/uniderp/index.ts
+++ fees/uniderp/index.ts
@@ -1,41 +1,109 @@
 import { FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { ZeroAddress } from "ethers";
 
+const LAUNCH_FEE = 0.00069; // 0.00069 ETH for each token created
 const config = {
     [CHAIN.UNICHAIN]: {
-      uniderpHook: "0xb4960cd4f9147f9e37a7aa9005df7156f61e4444", start: "2025-05-01",
+      poolManager: "0x1f98400000000000000000000000000000000004", 
+      uniderpLauncher: "0x239584404983804085c8Fd69C1e1651ea99680b0", 
+      uniderpHook: "0xb4960cd4f9147f9e37a7aa9005df7156f61e4444", 
+      start: "2025-04-23",
+      fromBlock: 14569072
     },
 }
 
+const SWAP_TOPIC = "0x40e9cecb9f5f1f1c5b9c97dec2917b7ee92e57ba5563708daca94dd84ad7112f";
 
-const eventAbi = "event FeeTaken(uint8 indexed feeType, address indexed token, address indexed receiver, uint256 amount)"
+const hookEventAbi = "event FeeTaken(uint8 indexed feeType, address indexed token, address indexed receiver, uint256 amount)"
+const launcherEventAbi = "event TokenCreated(uint256 lpTokenId, address tokenAddress, address indexed creatorAddress, string symbol, int24 startingTickIfToken0IsNewToken, uint256 amountTokensBought)"
+const swapAbi = "event Swap(bytes32 indexed id, address indexed sender, int128 amount0, int128 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint24 fee)"
+const poolV4Abi = 'event Initialize(bytes32 indexed id, address indexed currency0, address indexed currency1, uint24 fee, int24 tickSpacing, address hooks, uint160 sqrtPriceX96, int24 tick)'
 
-const fetchFees = async (uniderpHook:string, options: FetchOptions): Promise<FetchResultV2> => {
+const getAbsoluteBigInt = (value: bigint): bigint => {
+  return value < BigInt(0) ? value * BigInt(-1) : value;
+};
+
+const fetchFees = async (options: FetchOptions): Promise<FetchResultV2> => {
   const {getLogs, createBalances} = options;
+
+  const { poolManager,uniderpLauncher, fromBlock, uniderpHook } = config[options.chain]
+
   const dailyFees = createBalances();
   const dailyRevenue = createBalances();
-  const logs = await getLogs({
+
+  // events from hook
+  const hookLogs = await getLogs({
     target: uniderpHook,
-    eventAbi,
+    skipIndexer: true,
+    eventAbi: hookEventAbi,
   });
-  for (const log of logs) {
+  
+  for (const log of hookLogs) {
+    // Fee taken from hook: platform (20%) + token creator (40%) + referrer fee (10%)
     dailyFees.addToken(log.token, log.amount);
     if (log.feeType === 0n) {
+      // platform (20%)
       dailyRevenue.addToken(log.token, log.amount);
     }
   }
+
+  // events from launching new token
+  const launcherLogs = await getLogs({
+    target: uniderpLauncher,
+    skipIndexer: true,
+    eventAbi: launcherEventAbi,
+  });
+    // 0.00069 for each token created
+  dailyFees.addGasToken(launcherLogs.length * LAUNCH_FEE * 1e18);
+
+  // 0.3% liquidity
+  // Get list pools created by uniderp
+  const logs = await getLogs({
+    target: poolManager,
+    skipIndexer: true,
+    fromBlock,
+    eventAbi: poolV4Abi,
+  })
+  const poolIds = logs.filter((log: any) => log.hooks.toLowerCase() === uniderpHook).map((log: any) => log.id);
+  for (const poolId of poolIds) {
+    // Filter all swap events from pools created by uniderp
+    const swapLogs = await getLogs({
+      target: poolManager,
+      skipIndexer: true,
+      eventAbi: swapAbi,
+      topics: [
+        SWAP_TOPIC,
+        poolId
+      ],
+    })
+    for (const log of swapLogs) {
+      const gasFeeAmount = getAbsoluteBigInt(log.amount0) * 300n / 10000n;
+      dailyFees.addGasToken(gasFeeAmount);
+      dailyRevenue.addGasToken(gasFeeAmount);
+    }
+  }
+
   return { dailyFees, dailyRevenue };
 }
 
-
+const methodology = {
+  UserFees: "User pays 1% fees on each swap.",
+  ProtocolRevenue: "Treasury receives 0.5% of each swap. (0.2% from swap + 0.3% from LPs)",
+  Revenue: "All revenue generated comes from user fees.",
+  Fees: "All fees comes from the user."
+}
 
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: Object.keys(config).reduce((acc, chain) => {
-    const { uniderpHook, start } = config[chain];
+    const { start } = config[chain];
     acc[chain] = {
-      fetch: (options: FetchOptions) => fetchFees(uniderpHook, options),
+      fetch: fetchFees,
       start: start,
+      meta: {
+        methodology
+      }
     };
     return acc;
   }, {}),
