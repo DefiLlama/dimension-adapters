cebb60185546e0425f3a9b17f30bf34433dafa9e
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-13 14:16:49 +0530

    Revert "temp rug dune sql (#3152)" (#3154)
    This reverts commit b2f3397857a97d5e54cf4a0474dc1cf1be9abbfe.

--- helpers/dune.ts
+++ helpers/dune.ts
@@ -123,8 +123,6 @@ const tableName = {
 } as any
 
 export const queryDuneSql = (options: any, query: string) => {
-  throw new Error('Dune adapter is temp stopped')
-
   checkCanRunDuneQuery()
 
   return queryDune("3996608", {
