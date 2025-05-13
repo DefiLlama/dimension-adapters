1a0e1a915fdbc8d7655df6ddbbac80d7e1868909
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-14 00:23:08 +0530

    separate jito mev tips and jito liquid staking adapter (#3160)

---
+++ fees/jito-mev-tips/index.ts
@@ -0,0 +1,47 @@
+/*
+  Source:
+  - dailyFees: Represents MEV rewards/tips paid by users/searchers.
+    Collected from transfers to Jito MEV-related program addresses.
+*/
+
+import { FetchOptions, SimpleAdapter } from "../../adapters/types"
+import { CHAIN } from "../../helpers/chains"
+import { getSolanaReceived } from "../../helpers/token"
+
+const fetchFees = async (_a:any, _b:any, options: FetchOptions) => {
+
+  const targets = [
+    '96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5',
+    'HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe',
+    'Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY',
+    'ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49',
+    'DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh',
+    'ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt',
+    'DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL',
+    '3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT',
+  ]
+
+  const dailyFees = await getSolanaReceived({ options, targets, })
+
+  return {
+    dailyFees
+  }
+}
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.SOLANA]: {
+      fetch: fetchFees,
+      start: '2024-04-30',
+      meta:{
+        methodology: {
+          fees: 'MEV/tips paid by users/searchers',
+        }
+      }
+    }
+  },
+  isExpensiveAdapter: true
+}
+
+export default adapter
--- fees/jito/index.ts
+++ fees/jito/index.ts
@@ -1,7 +1,6 @@
 /*
   Source:
-  - dailyFees: Represents MEV rewards/tips paid by users/searchers to Jito infrastructure (Relayer, Block Engine, Validators via Tip Distribution).
-    Collected from transfers to Jito MEV-related program addresses.
+  - dailyFees: fee accured to the jito DAO (Withdrawal Fees, Interceptor Fees, Tip Router Fees).
   - dailyRevenue/dailyProtocolRevenue: Represents fees accruing specifically to the Jito DAO Treasury.
     This includes:
       - Withdrawal Fees (0.1% on unstake) from the JitoSOL stake pool.
@@ -13,36 +12,25 @@
 
 import { FetchOptions, SimpleAdapter } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains"
-import { getSolanaReceived } from "../../helpers/token"
 import { getSqlFromFile, queryDuneSql } from "../../helpers/dune"
 
 const fetchFees = async (_a:any, _b:any, options: FetchOptions) => {
 
-  const targets = [
-    '96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5',
-    'HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe',
-    'Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY',
-    'ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49',
-    'DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh',
-    'ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt',
-    'DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL',
-    '3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT',
-  ]
   const sql = getSqlFromFile("helpers/queries/jito.sql", {
     start: options.startTimestamp,
     end: options.endTimestamp
   });
 
   const fees: any[] = (await queryDuneSql(options, sql));
-  const dailyFees = await getSolanaReceived({ options, targets, })
 
   const dailyProtocolRevenue = options.createBalances();
   dailyProtocolRevenue.addCGToken('usd', fees[0].total_usd_amt)
 
   return {
-    dailyFees,
+    dailyFees: dailyProtocolRevenue,
     dailyRevenue: dailyProtocolRevenue,
-    dailyProtocolRevenue
+    dailyProtocolRevenue,
+    dailyHoldersRevenue: "0",
   }
 }
 
@@ -54,8 +42,8 @@ const adapter: SimpleAdapter = {
       start: '2024-04-30',
       meta:{
         methodology: {
-          fees: 'MEV rewards/tips paid by users/searchers to jito infra',
-          protocolrevenue: 'fee accured to the jito DAO'
+          fees: 'fee accured to the jito DAO (Withdrawal Fees, Interceptor Fees, Tip Router Fees)',
+          protocolRevenue: 'fee accured to the jito DAO (Withdrawal Fees, Interceptor Fees, Tip Router Fees)'
         }
       }
     }
