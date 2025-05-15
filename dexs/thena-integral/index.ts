fce33b8b855a012d9cd828a44e6afcf01eb9ad80
lamptzz <lam.pt@zinza.com.vn>
2025-05-15 18:49:49 +0700

    add thena integral

---
+++ dexs/thena-integral/index.ts
@@ -0,0 +1,16 @@
+import * as sdk from "@defillama/sdk";
+import { CHAIN } from "../../helpers/chains";
+import { univ2Adapter2 } from "../../helpers/getUniSubgraphVolume";
+
+const adapters = univ2Adapter2({
+  [CHAIN.BSC]: sdk.graph.modifyEndpoint('BoHp9H2rGzVFPiqc56PJ1Gw7EPDaiHMcupsUuksMGp2K')
+}, {
+  factoriesName: "factories",
+  dayData: "fusionDayData",
+  dailyVolume: "volumeUSD",
+  totalVolume: "totalVolumeUSD",
+});
+
+adapters.version = 2;
+adapters.adapter.bsc.start = 1731888000;
+export default adapters;
