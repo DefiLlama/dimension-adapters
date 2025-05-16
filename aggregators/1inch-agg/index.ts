c01e6e01c60ed0e82b63afdab2ce14eaad512cdb
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-16 23:20:35 +0530

    virtual protocol optimized dune and formatting

--- aggregators/1inch-agg/index.ts
+++ aggregators/1inch-agg/index.ts
@@ -14,7 +14,17 @@ const chainsMap: Record<string, string> = {
 };
 
 const prefetch = async (options: FetchOptions) => {
-  const sql_query = getSqlFromFile('helpers/queries/1inch_agg.sql', {})
+  const sql_query = `
+    SELECT
+        split_part(upper(blockchain), '_', 1) as blockchain,
+        sum(amount_usd) as volume_24h
+    FROM oneinch.swaps
+    WHERE
+        (protocol = 'AR' OR flags['second_side'])
+        AND TIME_RANGE
+    GROUP BY 1
+    ORDER BY volume_24h DESC
+  `;
   return await queryDuneSql(options, sql_query);
 }
 
--- helpers/queries/1inch_agg.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    split_part(upper(blockchain), '_', 1) as blockchain,
-    sum(amount_usd) as volume_24h
-FROM oneinch.swaps
-WHERE
-    (protocol = 'AR' OR flags['second_side'])
-    AND TIME_RANGE
-GROUP BY 1
-ORDER BY volume_24h DESC
