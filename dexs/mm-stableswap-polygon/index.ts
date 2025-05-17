7230554c8dbedb299c051af112d4fa8d2e997902
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-05-17 19:12:24 +0200

    fix broken adapters

--- dexs/mm-stableswap-polygon/index.ts
+++ dexs/mm-stableswap-polygon/index.ts
@@ -1,10 +1,25 @@
-import * as sdk from "@defillama/sdk";
-import { univ2Adapter2 } from "../../helpers/getUniSubgraphVolume";
+import { FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-export default univ2Adapter2({
-  [CHAIN.POLYGON]: sdk.graph.modifyEndpoint('HTJcrXUUtrVFKyNHZH99ywRx3TQm5ChSFVbn3oBiqGq6')
-}, {
-  factoriesName: "pancakeFactories",
-  dayData: "pancakeDayData"
-});
+export default {
+  version: 2,
+  adapter: {
+    [CHAIN.POLYGON]: { fetch }
+  }
+}
+
+async function fetch({ createBalances, api, getLogs, }: FetchOptions) {
+  const dailyVolume = createBalances()
+  const dailyFees = createBalances()
+  const logs = await getLogs({
+    target: '0x690BBaa9EDBb762542FD198763092eaB2B2A5350',
+    eventAbi: 'event TokenSwap (address indexed buyer, uint256 tokensSold, uint256 tokensBought, uint128 soldId, uint128 boughtId)'
+  })
+  const tokens  =await api.multiCall({ target: '0x690BBaa9EDBb762542FD198763092eaB2B2A5350', abi: 'function getToken(uint8) view returns (address)' , calls: Array.from({length: 3}, (_, i) => i)})
+  logs.forEach((log: any) => {
+    dailyVolume.add(tokens[log.boughtId], log.tokensBought)
+    dailyFees.add(tokens[log.boughtId], log.tokensBought * 0.0002)
+  })
+
+  return { dailyFees, dailyVolume }
+}
