3cf8ff7ce7de6529ac00f1cfcea9a24081430301
FlowX Dev <development@flowx.finance>
2025-05-19 18:14:01 +0700

    Change url to calculate data

--- aggregators/flowx-aggregator/index.ts
+++ aggregators/flowx-aggregator/index.ts
@@ -3,11 +3,13 @@ import { httpGet } from "../../utils/fetchURL";
 import { FetchOptions } from "../../adapters/types";
 
 const fetchVolume = async ({ fromTimestamp, toTimestamp }: FetchOptions) => {
-  const url = `https://flowx-finance-mono.vercel.app/api/defillama/aggregator-vol?startTimestamp=${fromTimestamp}&endTimestamp=${toTimestamp}`;
+  const url = `https://flowx-finance-mono.vercel.app/api/defillama/aggregator-with-date?startTimestamp=${
+    fromTimestamp * 1000
+  }&endTimestamp=${toTimestamp * 1000}`;
+
   const res = await httpGet(url);
-  const record = res[0];
   return {
-    dailyVolume: record.totalUSD,
+    dailyVolume: res.totalUSD,
   };
 };
 
