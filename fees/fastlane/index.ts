dfb126c992fdea469b39f89f57a1a25e7d13cf42
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-19 22:10:58 +0530

    Feat: add old event logs for fastlane for refilling

--- fees/fastlane/index.ts
+++ fees/fastlane/index.ts
@@ -1,17 +1,60 @@
+import { ethers } from "ethers";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const FASTLANE_AUCTION_HANDLER = '0xCACe8D78269ba00f1C4D5Fc3B1228C7DF0a7C8BA';
+const FASTLANE_AUCTION_HANDLER_V1 = '0xf5DF545113DeE4DF10f8149090Aa737dDC05070a';
+const FASTLANE_AUCTION_HANDLER_V2 = '0xCACe8D78269ba00f1C4D5Fc3B1228C7DF0a7C8BA';
+
+const eventAbis = {
+    v1: 'event RelayFlashBid(address indexed sender, uint256 amount, bytes32 indexed oppTxHash, address indexed validator, address searcherContractAddress)',
+    v21: 'event RelayFlashBid(address indexed sender, bytes32 indexed oppTxHash, address indexed validator, uint256 bidAmount, uint256 amountPaid, address searcherContractAddress)',
+    v22: 'event RelayFastBid(address indexed sender, address indexed validator, bool success, uint256 bidAmount, address searcherContractAddress)',
+    v23: 'event RelayFeeCollected(address indexed payor, address indexed payee, uint256 amount)',
+}
 
 const fetch = async (options: FetchOptions) => {
-    const logs = await options.getLogs({
-        target: FASTLANE_AUCTION_HANDLER,
-        eventAbi: 'event RelayFeeCollected(address indexed payor, address indexed payee, uint256 amount)',
-    });
     const dailyFees = options.createBalances();
-    for (const log of logs) {
-        dailyFees.addGasToken(log.amount);
+
+    const v1_logs = await options.getLogs({
+        target: FASTLANE_AUCTION_HANDLER_V1,
+        eventAbi: eventAbis.v1,
+        entireLog: true,
+    });
+    const v1_iface = new ethers.Interface([eventAbis.v1]);
+    const v21_iface = new ethers.Interface([eventAbis.v21]);
+    const v22_iface = new ethers.Interface([eventAbis.v22]);
+
+    const v21_logs = await options.getLogs({
+        target: FASTLANE_AUCTION_HANDLER_V2,
+        eventAbi: eventAbis.v21,
+        entireLog: true,
+    });
+    const v22_logs = await options.getLogs({
+        target: FASTLANE_AUCTION_HANDLER_V2,
+        eventAbi: eventAbis.v22,
+        entireLog: true,
+    });
+    const v23_logs = await options.getLogs({
+        target: FASTLANE_AUCTION_HANDLER_V2,
+        eventAbi: eventAbis.v23,
+    });
+
+    for (const log of v1_logs) {
+        const parsedLog = v1_iface.parseLog(log)
+        dailyFees.addGasToken(parsedLog!.args.amount);
     }
+    for (const log of v21_logs) {
+        const parsedLog = v21_iface.parseLog(log)
+        dailyFees.addGasToken(parsedLog!.args.bidAmount);
+    }
+    for (const log of v22_logs) {
+        const parsedLog = v22_iface.parseLog(log)
+        dailyFees.addGasToken(parsedLog!.args.bidAmount);
+    }
+    for (const log of v23_logs) {
+        dailyFees.addGasToken(log.args.amount);
+    }
+
     return { dailyFees }
 }
 
@@ -20,7 +63,7 @@ const adapter: SimpleAdapter = {
     adapter: {
         [CHAIN.POLYGON]: {
             fetch,
-            start: '2023-11-17',
+            start: '2022-12-08',
             meta: {
                 methodology: {
                     fees: "MEV fees paid by searchers to validators for priority transaction inclusion (bundles) on the Polygon network.",
