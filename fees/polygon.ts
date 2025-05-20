d63d6e0c0eb92402ecc6ed005fc5b680d8949440
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-21 01:20:46 +0530

    use dune adapter for polygon chain fee for more accurate data

--- fees/polygon.ts
+++ fees/polygon.ts
@@ -1,4 +1,30 @@
-import { blockscoutFeeAdapter2 } from "../helpers/blockscoutFees";
 import { CHAIN } from "../helpers/chains";
+import { Adapter, ProtocolType, FetchOptions } from "../adapters/types";
+import { fetchL2FeesWithDune } from "../helpers/ethereum-l2";
 
-export default blockscoutFeeAdapter2(CHAIN.POLYGON)
+const ethereumWallets = ['null']
+
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+    const { dailyFees, dailyRevenue } = await fetchL2FeesWithDune(options, {
+        chainName: 'polygon',
+        ethereumWallets,
+        blobSubmitterLabel: 'Polygon'
+    });
+    return {
+        dailyFees
+    }
+}
+
+const adapter: Adapter = {
+    version: 1,
+    adapter: {
+        [CHAIN.POLYGON]: {
+            fetch,
+            start: '2020-05-30'
+        },
+    },
+    protocolType: ProtocolType.CHAIN,
+    allowNegativeValue: true, // calldata and blob costs
+}
+
+export default adapter;
