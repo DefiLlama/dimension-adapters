24abe1bf093a66e88ed422e81c0a6c2e9fe89d34
vlbhtcno1 <datlilkayz11@gmail.com>
2025-05-23 01:36:25 +0700

    Update stbot.ts fee wallets
    Update stbot.ts fee wallets

--- fees/stbot.ts
+++ fees/stbot.ts
@@ -6,7 +6,11 @@ const fetch: any = async (options: FetchOptions) => {
   const targets = [
     'F34kcgMgCF7mYWkwLN3WN7KrFprr2NbwxuLvXx4fbztj',
     '96aFQc9qyqpjMfqdUeurZVYRrrwPJG2uPV6pceu4B1yb',
-
+    'BTQyUXhxiLrFPD5JUANCwg4ViibmNY39McmWk4bVNxLA',
+    '4vfFG2xGZsjXQgA6ZCTzA1PgUGLppFHY9eGnh3ZVGUuz',
+    'A7XTexV13EPnhtH55qhT7qmFkgYCMAMnfXk89VWu9PCJ',
+    'GreGavLfh5sK1BeQ2WYvmk352wbyNNzQdCmqWCV8QSib
+    
     // older addresses I think
     // 'HEPL5rTb6n1Ax6jt9z2XMPFJcDe9bSWvWQpsK7AMcbZg',
     // 'K1LRSA1DSoKBtC5DkcvnermRQ62YxogWSCZZPWQrdG5',
