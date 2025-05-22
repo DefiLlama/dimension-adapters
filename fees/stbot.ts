000befb71143ae942433b6141bc2f92d1abc93c8
vlbhtcno1 <datlilkayz11@gmail.com>
2025-05-23 01:44:29 +0700

    format code

--- fees/stbot.ts
+++ fees/stbot.ts
@@ -9,8 +9,8 @@ const fetch: any = async (options: FetchOptions) => {
     'BTQyUXhxiLrFPD5JUANCwg4ViibmNY39McmWk4bVNxLA',
     '4vfFG2xGZsjXQgA6ZCTzA1PgUGLppFHY9eGnh3ZVGUuz',
     'A7XTexV13EPnhtH55qhT7qmFkgYCMAMnfXk89VWu9PCJ',
-    'GreGavLfh5sK1BeQ2WYvmk352wbyNNzQdCmqWCV8QSib
-    
+    'GreGavLfh5sK1BeQ2WYvmk352wbyNNzQdCmqWCV8QSib'
+
     // older addresses I think
     // 'HEPL5rTb6n1Ax6jt9z2XMPFJcDe9bSWvWQpsK7AMcbZg',
     // 'K1LRSA1DSoKBtC5DkcvnermRQ62YxogWSCZZPWQrdG5',
@@ -24,7 +24,7 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.SOLANA]: {
       fetch: fetch,
-          },
+    },
   },
   isExpensiveAdapter: true
 };
