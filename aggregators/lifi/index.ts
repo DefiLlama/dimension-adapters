8f91f885deca8a89cd0cfaf1dc1d9f70d51d6155
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-23 15:58:31 +0530

    Fix Lifi logs and remove btc and solana for aggregator swaps

--- aggregators/lifi/index.ts
+++ aggregators/lifi/index.ts
@@ -1,10 +1,17 @@
+import { ethers } from "ethers";
 import { FetchOptions, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import { LifiDiamonds } from "../../helpers/aggregators/lifi";
 import { CHAIN } from "../../helpers/chains";
-import { fetchVolumeFromLIFIAPI, LifiDiamonds } from "../../helpers/aggregators/lifi";
 
 
 const LifiSwapEvent = "event LiFiGenericSwapCompleted(bytes32 indexed transactionId, string integrator, string referrer, address receiver, address fromAssetId, address toAssetId, uint256 fromAmount, uint256 toAmount)"
-const integrators != ['jumper.exchange', 'transferto.xyz', 'jumper.exchange.gas']
+const integrators = ['jumper.exchange', 'transferto.xyz', 'jumper.exchange.gas']
+
+const EVMLifIDiamonds = Object.keys(LifiDiamonds).filter(
+  (chain) => chain !== CHAIN.BITCOIN && chain !== CHAIN.SOLANA
+);
+
+const iface = new ethers.Interface([LifiSwapEvent]);
 
 const fetch: any = async (options: FetchOptions): Promise<FetchResultVolume> => {
   const dailyVolume = options.createBalances();
@@ -12,21 +19,22 @@ const fetch: any = async (options: FetchOptions): Promise<FetchResultVolume> => 
     target: LifiDiamonds[options.chain].id,
     topic: '0x38eee76fd911eabac79da7af16053e809be0e12c8637f156e77e1af309b99537',
     eventAbi: LifiSwapEvent,
+    entireLog: true
   });
 
   logs.forEach((e: any) => {
-    const { bridgeData: { integrator, sendingAssetId, minAmount } } = e;
-    if (integrators.includes(integrator)) {
-      dailyVolume.add(sendingAssetId, minAmount);
+    const parsedLog = iface.parseLog(e);
+    if (!integrators.includes(parsedLog?.args.integrator)) {
+      dailyVolume.add(parsedLog?.args.fromAssetId, parsedLog?.args.fromAmount);
     }
   });
 
-  return { dailySwapVolume: dailyVolume } as any;
+  return { dailyVolume } as any;
 };
 
 const adapter: SimpleAdapter = {
   version: 2,
-  adapter: Object.keys(LifiDiamonds).reduce((acc, chain) => {
+  adapter: EVMLifIDiamonds.reduce((acc, chain) => {
     return {
       ...acc,
       [chain]: { fetch, start: LifiDiamonds[chain].startTime, }
