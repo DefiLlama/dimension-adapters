3c890bda2b9c3a57c7968683e632867f18862ca3
Amped Finance <150587280+AmpedFinance@users.noreply.github.com>
2025-05-26 01:48:32 +1000

    update sonic subgraph with new assets (#3265)

--- fees/amped/index.ts
+++ fees/amped/index.ts
@@ -7,7 +7,7 @@ const endpoints: { [key: string]: string } = {
   [CHAIN.LIGHTLINK_PHOENIX]:
     "https://graph.phoenix.lightlink.io/query/subgraphs/name/amped-finance/trades",
   [CHAIN.SONIC]:
-    "https://api.studio.thegraph.com/query/91379/trades-sonic/version/latest",
+    "https://api.goldsky.com/api/public/project_cm9j641qy0e0w01tzh6s6c8ek/subgraphs/sonic-trades/1.0.1/gn",
   // [CHAIN.BSC]: "https://api.studio.thegraph.com/query/91379/amped-trades-bsc/version/latest"
   [CHAIN.BERACHAIN]: "https://api.studio.thegraph.com/query/91379/amped-trades-bera/version/latest",
   [CHAIN.BASE]: "https://api.studio.thegraph.com/query/91379/trades-base/version/latest"
