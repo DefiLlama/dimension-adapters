aee3adf528cfe372bc596206a2fa3778ac1535cf
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-28 23:13:21 +0530

    add gate.io deposit wallet in the blacklist

--- fees/makenow-meme.ts
+++ fees/makenow-meme.ts
@@ -24,7 +24,13 @@ const fetchFees = async (_a:any, _b:any, options: FetchOptions) => {
     // Blacklist the old transfer fee receiver to prevent
     // wallet change tx from being counted
     // (5R48wJazTurDMHjERWW3ZTQ6nMdXegD6QH3sE5FsV89UjRCHbBN4n3Pt8y4ngTxi5P5CCt5jx83mRbG6GaPw9rY3)
+    '8tA49tvPiTCkeVfuTms1F2nwVg6FWpQsQ8eNZ4g9vVQF',
+    'AEBoqzQU3fDYzhVmaRedcNeVcQQSMEqCAuQ2A7pYNEd7',
+    '4KRS8BPCgDZHBTXkugCHuh2ZsZQhmAbdx6ASjMQYNdXd',
+    'CJFY81Zom7BpZ66xieAHk3hW43Jru9KmgCBe1eKnWUMi',
     '76Mk7UH3nSjJXKLi7CVaKurUSywo6xXqhu1k1tJMFUSi',
+    '2ViaoccYRm7gRewuPyW4Rp5WvxVJzNoKxxAMBUiii4rp',
+    'u6PJ8DtQuPFnfmwHbGFULQ4u4EgjDiyYKjVEsynXq2w'
   ]
   const blacklist_txn_ids = [
     'yNic4CejTw3UVqqpuxgva2Jo238F9WHj1W3ZBH9ExGM2gZQnK9nmcnN2xYe7gjD2YDdCZS7vLA34V1JoYd9ewgL',
@@ -33,7 +39,7 @@ const fetchFees = async (_a:any, _b:any, options: FetchOptions) => {
 
   // Format addresses for IN clause
   const formattedAddresses = targets.map(addr => `'${addr}'`).join(', ');
-  const formattedBlacklist = targets.map(addr => `'${addr}'`).join(', ');
+  const formattedBlacklist = blacklists.map(addr => `'${addr}'`).join(', ');
   const formattedBlacklistTxnIds = blacklist_txn_ids.map(id => `'${id}'`).join(', ');
 
   // Convert Allium query to Dune query
