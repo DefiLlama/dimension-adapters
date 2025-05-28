0606dad4752cd3be2ba4a409a10ddec7baa0ef7c
nathan-bui-2908 <hanh@grvt.io>
2025-05-28 12:06:05 +0700

    Grvt adapter (#1)
    * add dex adapter
    
    * update timestamp
    
    * add fee
    
    * fix endpoint
    
    * update package name

---
+++ dexs/grvt-perps/index.ts
@@ -0,0 +1,40 @@
+import {FetchOptions, FetchResult, SimpleAdapter} from "../../adapters/types";
+import fetchURL from "../../utils/fetchURL";
+import { CHAIN } from "../../helpers/chains";
+import { getTimestampAtStartOfNextDayUTC, getTimestampAtStartOfDayUTC } from "../../utils/date";
+
+// endTime is in seconds
+const endpoint = (startTime: number, endTime: number) => {
+  return `https://openview.grvt.io/api/v1/defillama/stats?start_timestamp=${startTime}&end_timestamp=${endTime}`;
+};
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.ZKSYNC]: {
+      fetch: fetchGRVTDex,
+      start: '2024-12-01',
+    },
+  },
+};
+
+export async function fetchGRVTDex(fetchOptions: FetchOptions) {
+  const startOfDayUTC = getTimestampAtStartOfDayUTC(fetchOptions.startTimestamp);
+  const endOfDayUTC = getTimestampAtStartOfNextDayUTC(fetchOptions.endTimestamp);
+  const url = endpoint(startOfDayUTC,endOfDayUTC);
+  const resp = await getGrvtData(url);
+  const dailyVolume = Number(resp.dailyVolume).toFixed(5);
+
+  return {
+    timestamp: startOfDayUTC,
+    dailyVolume,
+  };
+}
+
+async function getGrvtData(
+  endpoint: string
+): Promise<FetchResult> {
+  return await fetchURL(endpoint);
+}
+
+export default adapter;
---
+++ fees/grvt-perps/index.ts
@@ -0,0 +1,42 @@
+import {FetchOptions, FetchResult, SimpleAdapter} from "../../adapters/types";
+import fetchURL from "../../utils/fetchURL";
+import { CHAIN } from "../../helpers/chains";
+import { getTimestampAtStartOfNextDayUTC, getTimestampAtStartOfDayUTC } from "../../utils/date";
+
+// endTime is in seconds
+const endpoint = (startTime: number, endTime: number) => {
+  return `https://openview.grvt.io/api/v1/defillama/stats?start_timestamp=${startTime}&end_timestamp=${endTime}`;
+};
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.ZKSYNC]: {
+      fetch: fetchGRVTFees,
+      start: '2024-12-01',
+    },
+  },
+};
+
+export async function fetchGRVTFees(fetchOptions: FetchOptions) {
+  const startOfDayUTC = getTimestampAtStartOfDayUTC(fetchOptions.startTimestamp);
+  const endOfDayUTC = getTimestampAtStartOfNextDayUTC(fetchOptions.endTimestamp);
+  const url = endpoint(startOfDayUTC,endOfDayUTC);
+  const resp = await getGrvtData(url);
+  const dailyFees = Number(resp.dailyFees).toFixed(5);
+  const dailyRevenue = Number(resp.dailyRevenue).toFixed(5);
+
+  return {
+    timestamp: startOfDayUTC,
+    dailyFees,
+    dailyRevenue
+  };
+}
+
+async function getGrvtData(
+  endpoint: string
+): Promise<FetchResult> {
+  return await fetchURL(endpoint);
+}
+
+export default adapter;
