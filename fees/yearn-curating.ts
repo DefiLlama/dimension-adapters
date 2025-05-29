d93f6eb68d97f86957c1c5ce4f85cb5a37afab0b
Eden <noat.eth@gmail.com>
2025-05-29 22:08:22 +0700

    add yearn curatoed vaults

---
+++ fees/yearn-curating.ts
@@ -0,0 +1,25 @@
+import { SimpleAdapter } from "../adapters/types";
+import { CuratorConfig, getCuratorExport } from "../helpers/curators";
+
+const curatorConfig: CuratorConfig = {
+  vaults: {
+    ethereum: {
+      morphoVaultOwners: [
+        '0xFc5F89d29CCaa86e5410a7ad9D9d280d4455C12B',
+      ],
+    },
+    base: {
+      morphoVaultOwners: [
+        '0xFc5F89d29CCaa86e5410a7ad9D9d280d4455C12B',
+        '0x50b75d586929ab2f75dc15f07e1b921b7c4ba8fa',
+      ],
+    },
+  }
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: getCuratorExport(curatorConfig),
+}
+
+export default adapter
