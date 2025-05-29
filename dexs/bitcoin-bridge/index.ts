5a634bb063a49b0a16b76e49e8e5e0b06a02d9ad
utkarsh-21st <utkarshdhing@gmail.com>
2025-05-29 10:48:01 +0000

    add fee and dex adapter for BitcoinBridge.app

---
+++ dexs/bitcoin-bridge/index.ts
@@ -0,0 +1,32 @@
+import { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { httpGet } from "../../utils/fetchURL";
+
+const photonBridgeEndpoint = "https://bridge.superproof.ai/bridge"
+
+const fetch = async (timestamp: number) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+  const volumeForDay = (await httpGet(photonBridgeEndpoint.concat(`/dashboard/vol/day?timestamp=${dayTimestamp}`),
+    { headers: { "x-client-id": "defillama" } }));
+
+  const totalVolume = volumeForDay.total_volume
+  const dailyVolume = volumeForDay.day_vol
+
+  return {
+    totalVolume: totalVolume,
+    dailyVolume: dailyVolume,
+    timestamp: dayTimestamp,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.ETHEREUM]: {
+      fetch,
+      start: '2025-05-28',
+    },
+  },
+};
+
+export default adapter;
