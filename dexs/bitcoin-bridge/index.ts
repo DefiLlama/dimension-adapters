eab3f703361721a5db748a522d68cb36f0744a36
utkarsh-21st <utkarshdhing@gmail.com>
2025-05-30 08:35:08 +0000

    get volume by chain

--- dexs/bitcoin-bridge/index.ts
+++ dexs/bitcoin-bridge/index.ts
@@ -1,37 +1,47 @@
-import { SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types"
+import { CHAIN } from "../../helpers/chains"
 import { httpGet } from "../../utils/fetchURL";
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 
 const photonBridgeEndpoint = "https://bridge.superproof.ai/bridge"
 
-const fetch = async (timestamp: number) => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const volumeForDay = (await httpGet(photonBridgeEndpoint.concat(`/dashboard/vol/day?timestamp=${dayTimestamp}`),
-    { headers: { "x-client-id": "defillama" } }));
+const chainMapping = {
+  ETH: CHAIN.ETHEREUM,
+  BTC: CHAIN.BITCOIN,
+}
 
-  const totalVolume = volumeForDay.total_volume
-  const dailyVolume = volumeForDay.day_vol
+const CHAINS = ['BTC', 'ETH']
 
-  return {
-    totalVolume: totalVolume,
-    dailyVolume: dailyVolume,
-    timestamp: dayTimestamp,
+const getFetchForChain = (chainShortName: string) => {
+  return async (_a: any, _b: any, options: FetchOptions) => {
+    const startOfDay = getTimestampAtStartOfDayUTC(options.startOfDay);
+    const volumeForDay = await httpGet(photonBridgeEndpoint.concat(`/dashboard/vol/chain/day?chain=${chainShortName}&timestamp=${startOfDay}`))
+
+    const totalVolume = volumeForDay.total_volume
+    const dailyVolume = volumeForDay.day_vol
+
+    return {
+      totalVolume: totalVolume,
+      dailyVolume: dailyVolume,
+      timestamp: startOfDay,
+    };
   };
 };
 
+
 const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.ETHEREUM]: {
-      fetch,
+  adapter: CHAINS.reduce((acc, chainKey) => {
+    acc[chainMapping[chainKey]] = {
+      fetch: getFetchForChain(chainKey) as any,
       start: '2025-05-28',
       meta: {
         methodology: {
           Volume: "This represents the total value of assets bridged over the period.",
         }
       }
-    },
-  },
+    };
+    return acc;
+  }, {}),
 };
 
 export default adapter;
