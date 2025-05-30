52a857434b0325abcf0626a0f3937fdc80aa9672
treeoflife2 <sol.analyzoor@gmail.com>
2025-05-30 17:53:59 +0530

    Fix: liquity add balance bug

--- fees/liquity-v2.ts
+++ fees/liquity-v2.ts
@@ -3,10 +3,13 @@ import { getLiquityV2LogAdapter } from "../helpers/liquity"
 
 async function fetch(options: FetchOptions) {
   const v0DeploymentRes = await getLiquityV2LogAdapter({ collateralRegistry: '0xd99dE73b95236F69A559117ECD6F519Af780F3f7' })(options)
-  const v1DeploymentRes = await getLiquityV2LogAdapter({ collateralRegistry: '0xf949982b91c8c61e952b3ba942cbbfaef5386684' })(options)
-  const dailyFees = v0DeploymentRes.dailyFees
-  dailyFees.addBalances(v1DeploymentRes.dailyFees)
-  return { dailyFees }
+  const dailyFees = v0DeploymentRes.dailyFees.clone()
+  let v1DeploymentRes = null
+  if (options.startTimestamp >= 1747612799){
+    v1DeploymentRes = await getLiquityV2LogAdapter({ collateralRegistry: '0xf949982b91c8c61e952b3ba942cbbfaef5386684' })(options)
+    dailyFees.addBalances(v1DeploymentRes.dailyFees);
+  }
+  return { dailyFees, dailyRevenue: dailyFees }
 }
 
 export default {
