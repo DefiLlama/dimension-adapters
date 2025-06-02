54f70e04cb7c8025b5fc6f2e1bb01689e348187e
treeoflife2 <sol.analyzoor@gmail.com>
2025-06-02 19:38:25 +0530

    iface to parse event

--- fees/reachme/index.ts
+++ fees/reachme/index.ts
@@ -1,15 +1,18 @@
+import { ethers } from "ethers";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const REACH_V1 = '0x3ff200940a172AbB1c70646d500cA22cdBCEA915'
 const REACH_V2 = '0x3479E83c089Bb42DC43C6FEfe5F89Ea0e5bA47F9'
 
-const ReachmePaymentDepositedEvent = "event PaymentDeposited(uint256 indexed depositId, string identifier, address requester, address kol, uint256 totalAmount, uint256 instantAmount, uint256 escrowAmount)"
+const PaymentDepositedEvent = "event PaymentDeposited(uint256 indexed depositId, string identifier, address requester, address kol, uint256 totalAmount, uint256 instantAmount, uint256 escrowAmount)"
 
-const RefundedEvent = "event RefundIssued(uint256 indexed depositId, string identifier, address requester, uint256 amount)"
+const RefundIssuedEvent = "event RefundIssued(uint256 indexed depositId, string identifier, address requester, uint256 amount)"
 
 const ReachmeProtocolFeeWallet = '0x0c602fFfe55727BbFa46289E7b2aF7440672F5C4'
 
+const payment_iface = new ethers.Interface([PaymentDepositedEvent])
+const refund_iface = new ethers.Interface([RefundIssuedEvent])
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
@@ -17,45 +20,49 @@ const fetch = async (options: FetchOptions) => {
 
   const payment_logs_v1: any[] = await options.getLogs({
     target: REACH_V1,
-    eventAbi: ReachmePaymentDepositedEvent,
+    eventAbi: PaymentDepositedEvent,
     entireLog: true
   });
 
   const payment_logs_v2: any[] = await options.getLogs({
     target: REACH_V2,
-    eventAbi: ReachmePaymentDepositedEvent,
+    eventAbi: PaymentDepositedEvent,
     entireLog: true
   });
 
   payment_logs_v1.forEach((log: any) => {
-    dailyFees.addGasToken(log.args.totalAmount);
-    dailyProtocolRevenue.addGasToken(Number(log.args.totalAmount) - Number(log.args.instantAmount) - Number(log.args.escrowAmount));
+    const parsed = payment_iface.parseLog(log)
+    dailyFees.addGasToken(parsed?.args.totalAmount);
+    dailyProtocolRevenue.addGasToken(Number(parsed?.args.totalAmount) - Number(parsed?.args.instantAmount) - Number(parsed?.args.escrowAmount));
   });
 
   payment_logs_v2.forEach((log: any) => {
-    dailyFees.addGasToken(log.args.totalAmount);
-    dailyProtocolRevenue.addGasToken(Number(log.args.totalAmount) - Number(log.args.instantAmount) - Number(log.args.escrowAmount));
+    const parsed = payment_iface.parseLog(log)
+    dailyFees.addGasToken(parsed?.args.totalAmount);
+    dailyProtocolRevenue.addGasToken(Number(parsed?.args.totalAmount) - Number(parsed?.args.instantAmount) - Number(parsed?.args.escrowAmount));
   });
 
   const dailyRefunds = options.createBalances();
   const refund_logs_v1: any[] = await options.getLogs({
     target: REACH_V1,
-    eventAbi: RefundedEvent,
+    eventAbi: RefundIssuedEvent,
     entireLog: true
   });
 
   const refund_logs_v2: any[] = await options.getLogs({
     target: REACH_V2,
-    eventAbi: RefundedEvent,
+    eventAbi: RefundIssuedEvent,
     entireLog: true
   });
 
   refund_logs_v1.forEach((log: any) => {
-    dailyRefunds.addGasToken(log.args.amount);
+    const parsed = refund_iface.parseLog(log)
+    dailyRefunds.addGasToken(parsed?.args.amount);
   });
 
   refund_logs_v2.forEach((log: any) => {
-    dailyRefunds.addGasToken(log.args.amount);
+    const parsed = refund_iface.parseLog(log)
+    dailyRefunds.addGasToken(parsed?.args.amount);
   });
 
   dailyFees.subtract(dailyRefunds);
