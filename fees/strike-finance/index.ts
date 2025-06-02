a6e3f976b96ae89b829934c642fd462563e1b9ed
Will Hankins <will@tidelabs.io>
2025-06-02 16:39:15 -0700

    Remove daily volume
    Volume will be moved to dex directory

--- fees/strike-finance/index.ts
+++ fees/strike-finance/index.ts
@@ -6,19 +6,16 @@ const url: string = "https://tidelabs.io:2121/defillama/strike-finance/fees";
 
 const fetch = async (_:number, _t: any, options: FetchOptions): Promise<FetchResult> => {
   const {
-    data: { dailyFees, dailyRevenue, dailyVolume },
+    data: { dailyFees, dailyRevenue },
   } = await axios.get(url);
   const dailyFeesUSD = options.createBalances();
   const dailyRevenueUSD = options.createBalances();
-  const dailyVolumeUSD = options.createBalances();
   dailyFeesUSD.addCGToken('cardano', Number(dailyFees));
   dailyRevenueUSD.addCGToken('cardano', Number(dailyRevenue));
-  dailyVolumeUSD.addCGToken('cardano', Number(dailyVolume));
   return {
     timestamp: options.startOfDay,
     dailyFees: dailyFeesUSD,
     dailyRevenue: dailyRevenueUSD,
-    dailyVolume: dailyVolumeUSD,
   };
 };
 
