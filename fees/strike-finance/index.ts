2be8d720cdd6ed536ab14d83f644f8702b9f6ab7
Will Hankins <will@tidelabs.io>
2025-06-02 17:05:30 -0700

    Add Strike Finance fee and revenue adapter

---
+++ fees/strike-finance/index.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { Adapter, FetchOptions, FetchResult } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+
+const fetch = async (options: FetchOptions): Promise<FetchResult> => {
+  const {
+    data: { dailyFees, dailyRevenue },
+  } = await axios.get(
+    `https://tidelabs.io:2121/defillama/strike-finance/fees?from=${options.startTimestamp}&to=${options.endTimestamp}`,
+  );
+  const dailyFeesUSD = options.createBalances();
+  const dailyRevenueUSD = options.createBalances();
+  dailyFeesUSD.addCGToken('cardano', Number(dailyFees));
+  dailyRevenueUSD.addCGToken('cardano', Number(dailyRevenue));
+  return {
+    timestamp: options.startOfDay,
+    dailyFees: dailyFeesUSD,
+    dailyRevenue: dailyRevenueUSD,
+  };
+};
+
+const adapter: Adapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.CARDANO]: {
+      fetch,
+      start: '2024-05-16',
+      runAtCurrTime: true,
+    },
+  },
+  allowNegativeValue: true,
+};
+
+export default adapter;
