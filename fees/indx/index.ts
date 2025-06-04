08b9f7d81a488c1504de30cbcc127caf5b95b04d
dave-brewlabs <87349828+mav-brewlabs@users.noreply.github.com>
2025-06-04 18:09:30 +1000

    Update index.ts

--- fees/indx/index.ts
+++ fees/indx/index.ts
@@ -1,39 +1,28 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { getETHReceived } from '../../helpers/token';
 
-const feeAddress = '0xD04086A2E18f4B1BB565A703EBeC56eaee2ACCA0'.toLowerCase();
+const feeAddress = '0xD04086A2E18f4B1BB565A703EBeC56eaee2ACCA0';
 
 const fetch = async (options: FetchOptions) => {
-    const dailyFees = options.createBalances();
-    
-    const startBalance = await options.fromApi.call({
-        target: feeAddress,
-        abi: 'function balanceOf() view returns (uint256)',
-        params: [],
-    });
-    
-    const endBalance = await options.toApi.call({
-        target: feeAddress,
-        abi: 'function balanceOf() view returns (uint256)',
-        params: [],
-    });
-    
-    const received = Number(endBalance) - Number(startBalance);
-    if (received > 0) {
-        dailyFees.addGasToken(received);
-    }
-    
-    return { dailyFees, dailyRevenue: dailyFees };
+  const balances = options.createBalances();
+  await getETHReceived({
+    options,
+    balances,
+    target: feeAddress
+  });
+  
+  return { dailyFees: balances, dailyRevenue: balances };
 };
 
 const adapter: SimpleAdapter = {
-    version: 2,
-    adapter: {
-        [CHAIN.BASE]: {
-            fetch,
-            start: '2025-05-17'
-        }
+  version: 2,
+  adapter: {
+    [CHAIN.BASE]: {
+      fetch,
+      start: '2025-05-17'
     }
+  }
 };
 
 export default adapter;
