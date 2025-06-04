2791c74bd0332ae053a979c9f336d687548aa670
dave-brewlabs <87349828+mav-brewlabs@users.noreply.github.com>
2025-06-04 18:29:46 +1000

    Update index.ts

--- fees/indx/index.ts
+++ fees/indx/index.ts
@@ -1,30 +1,39 @@
-import { Adapter, FetchOptions } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getETHReceived } from '../../helpers/token';
 
 const feeAddress = '0xD04086A2E18f4B1BB565A703EBeC56eaee2ACCA0';
 
-const fetchFees = async (options: FetchOptions) => {
-    const dailyFees = options.createBalances();
-    await getETHReceived({ options, balances: dailyFees, targets: [feeAddress] })
-    return { dailyFees, dailyRevenue: dailyFees }
-}
+const fetch = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances();
+  
+  const endBalance = await options.api.call({
+    target: feeAddress,
+    abi: 'uint256:0x',
+    block: options.toBlock,
+  });
+  
+  const startBalance = await options.api.call({
+    target: feeAddress,
+    abi: 'uint256:0x',
+    block: options.fromBlock,
+  });
+  
+  const received = Number(endBalance) - Number(startBalance);
+  if (received > 0) {
+    dailyFees.addGasToken(received);
+  }
+  
+  return { dailyFees, dailyRevenue: dailyFees };
+};
 
-const adapter: Adapter = {
-    version: 2,
-    isExpensiveAdapter: true,
-    adapter: {
-        [CHAIN.BASE]: {
-            fetch: fetchFees,
-            start: '2025-05-17',
-            meta: {
-                methodology: {
-                    Fees: 'ETH fees collected from INDX protocol operations.',
-                    Revenue: 'All fees collected are protocol revenue.',
-                }
-            }
-        },
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.BASE]: {
+      fetch,
+      start: '2025-05-17'
     }
-}
+  }
+};
 
 export default adapter;
