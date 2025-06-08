8e5d081f8fded6de9631034e9d03c7427c009f29
bajajanirudh <aniruddhbajaj@gmail.com>
2025-06-08 09:06:51 +0530

    refactor topics

--- fees/stars-arena.ts
+++ fees/stars-arena.ts
@@ -4,20 +4,25 @@ import { CHAIN } from "../helpers/chains";
 const abi = {
   "Trade": "event Trade (address trader, address subject, bool isBuy, uint256 shareAmount, uint256 amount, uint256 protocolAmount, uint256 subjectAmount, uint256 referralAmount, uint256 supply, uint256 buyPrice, uint256 myShares)",
   "TradeFractionalShares": "event TradeFractionalShares (address trader, address subject, bool isBuy, uint256 shareAmount, uint256 amount, uint256 protocolAmount, uint256 subjectAmount, uint256 referralAmount, uint256 fractionalSupply, uint256 buyPrice, uint256 myFractionalShares)",
-}
+};
+
+const topics = {
+  Trade: "0xc9d4f93ded9b42fa24561e02b2a40f720f71601eb1b3f7b3fd4eff20877639ee",
+  TradeFractionalShares: "0x7d26fca21642884249fe04718e734992f6e00b24a015ddfbd8018e2639417b56",
+};
 
 const fetch = async ({createBalances, getLogs}: FetchOptions) => {
   const dailyFees = createBalances();
   const dailyRevenue = createBalances();
 
   const tradeLogs = await getLogs({
-    topic: '0xc9d4f93ded9b42fa24561e02b2a40f720f71601eb1b3f7b3fd4eff20877639ee',
+    topic: topics.Trade,
     targets: ['0xC605C2cf66ee98eA925B1bb4FeA584b71C00cC4C', '0x69B7F08B2952e2EE3CA4222190BCF07831f1096f'],
     eventAbi: abi.Trade,
   });
 
   const tradeFractionalShareLogs = await getLogs({
-    topic: '0x7d26fca21642884249fe04718e734992f6e00b24a015ddfbd8018e2639417b56',
+    topic: topics.TradeFractionalShares,
     targets: ['0xC605C2cf66ee98eA925B1bb4FeA584b71C00cC4C'],
     eventAbi: abi.TradeFractionalShares,
   });
