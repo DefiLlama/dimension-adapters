1bad1d72702badeeabe2ed64b5fe0038fa66334c
bajajanirudh <aniruddhbajaj@gmail.com>
2025-06-08 09:03:26 +0530

    update revenue calc

--- fees/stars-arena.ts
+++ fees/stars-arena.ts
@@ -8,16 +8,30 @@ const abi = {
 
 const fetch = async ({createBalances, getLogs}: FetchOptions) => {
   const dailyFees = createBalances();
-  const dailyRevenue = createBalances()
-  const logs = await getLogs({
+  const dailyRevenue = createBalances();
+
+  const tradeLogs = await getLogs({
     topic: '0xc9d4f93ded9b42fa24561e02b2a40f720f71601eb1b3f7b3fd4eff20877639ee',
     targets: ['0xC605C2cf66ee98eA925B1bb4FeA584b71C00cC4C', '0x69B7F08B2952e2EE3CA4222190BCF07831f1096f'],
     eventAbi: abi.Trade,
   });
-  logs.map((log: any) => {
+
+  const tradeFractionalShareLogs = await getLogs({
+    topic: '0x7d26fca21642884249fe04718e734992f6e00b24a015ddfbd8018e2639417b56',
+    targets: ['0xC605C2cf66ee98eA925B1bb4FeA584b71C00cC4C'],
+    eventAbi: abi.TradeFractionalShares,
+  });
+
+  tradeLogs.map((log: any) => {
     dailyFees.addGasToken(log.protocolAmount+log.subjectAmount+log.referralAmount);
     dailyRevenue.addGasToken(log.protocolAmount)
   });
+
+  tradeFractionalShareLogs.map((log: any) => {
+    dailyFees.addGasToken(log.protocolAmount+log.subjectAmount+log.referralAmount);
+    dailyRevenue.addGasToken(log.protocolAmount)
+  });
+
   return {
     dailyFees,
     dailyRevenue
