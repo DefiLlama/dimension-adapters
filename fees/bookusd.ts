d350d1eacbfaa3687deec4b48a4e4ea0afd8265d
Eden <noat.eth@gmail.com>
2025-06-09 02:45:26 +0700

    bookusd use wrong collateral token (#3393)

--- fees/bookusd.ts
+++ fees/bookusd.ts
@@ -6,5 +6,6 @@ export default liquityV1Exports({
     troveManager: "0xFe5D0aBb0C4Addbb57186133b6FDb7E1FAD1aC15",
     stableCoin: "0xc28957E946AC244612BcB205C899844Cbbcb093D",
     holderRevenuePercentage: 100,
+    collateralCoin: '0xc9ad421f96579ace066ec188a7bba472fb83017f', // BOOK
   },
 });
