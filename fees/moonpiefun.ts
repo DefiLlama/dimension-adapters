cf5b14b56c52ddc2554eeb4d2df2cb677c5936f1
EBUBE <53199415+theiceeman@users.noreply.github.com>
2025-06-09 14:02:19 +0100

    Feat/suport moonpie (#3402)
    * feat: adapter for assetchain swap
    
    * minor fix
    
    * feat: fees for assetchain swap
    
    * Update assetchain-swap.ts
    
    * Update assetchain-swap.ts
    
    * feat: support moonpie on assetchain
    
    * remove yarn.lock
    
    * added protocol methology
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ fees/moonpiefun.ts
@@ -0,0 +1,41 @@
+import { Adapter, FetchOptions } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { addTokensReceived } from "../helpers/token";
+import ADDRESSES from "../helpers/coreAssets.json";
+
+const TREASURY = "0x31995B7ea0D0ec85e9c72C903AF0F29acF3622F2".toLowerCase();
+const USDT = '0x26E490d30e73c36800788DC6d6315946C4BbEa24'; // or the USDT address for your chain
+
+
+const methodology = {
+  Fees: "Protocol collects fees from trading in USDT.",
+  Revenue: "All fees collected are considered revenue."
+}
+
+const adapter: Adapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.ASSETCHAIN]: {
+      fetch: async (_t, _a, options: FetchOptions) => {
+        const dailyFees = options.createBalances();
+        await addTokensReceived({
+          options,
+          tokens: [USDT],
+          target: TREASURY,
+          balances: dailyFees,
+        });
+        return {
+          dailyFees,
+          dailyRevenue: dailyFees,
+          timestamp: options.startOfDay,
+        };
+      },
+      start: 1747025482,
+      meta: {
+        methodology: methodology,
+      },
+    },
+  },
+};
+
+export default adapter;
