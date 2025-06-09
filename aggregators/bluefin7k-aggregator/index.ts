78dce167a835c9acb7977839f15863af8ab6b240
abdullahabro <kamario.abdullah@gmail.com>
2025-06-09 15:14:43 +0500

    [BFS-1567]: Add script for bluefin 7k metrics

---
+++ aggregators/bluefin7k-aggregator/index.ts
@@ -0,0 +1,23 @@
+import fetchURL from '../../utils/fetchURL';
+import { FetchV2, SimpleAdapter } from '../../adapters/types';
+import { CHAIN } from '../../helpers/chains';
+
+const URL = 'https://statistic.7k.ag';
+
+const fetch: FetchV2 = async ({ fromTimestamp, toTimestamp }) => {
+	const dailyVolume = await fetchURL(
+		`${URL}/volume-with-ts?from_timestamp=${fromTimestamp}&to_timestamp=${toTimestamp}`,
+	);
+	return { dailyVolume };
+};
+
+const adapter: SimpleAdapter = {
+	version: 2,
+	adapter: {
+		[CHAIN.SUI]: {
+			fetch,
+			start: '2025-06-02',
+		},
+	},
+};
+export default adapter;
