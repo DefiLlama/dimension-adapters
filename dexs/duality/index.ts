6c52675a8cfd784aadb6831acf014f60aaf7f77c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-06-10 13:16:19 +0200

    refactor

--- dexs/duality/index.ts
+++ dexs/duality/index.ts
@@ -1,7 +1,7 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getTimestampAtStartOfDayUTC } from "../../utils/date";
-import fetchURL, { httpGet } from "../../utils/fetchURL"
+import { httpGet } from "../../utils/fetchURL"
 import { getEnv } from "../../helpers/env";
 
 const historicalVolumeEndpoint = "https://neutron.numia.xyz/trading/volume"
@@ -11,8 +11,8 @@ interface IChartItem {
   value: number
 }
 
-const fetchVolumeWithAuth = async ()=>{
-  const headers =  {"Authorization": `Bearer ${getEnv('NUMIA_API_KEY')}`, "Content-Type": "application/json"};
+const fetchVolumeWithAuth = async () => {
+  const headers = { "Authorization": `Bearer ${getEnv('NUMIA_API_KEY')}`, "Content-Type": "application/json" };
   const historicalVolume: IChartItem[] = await httpGet(historicalVolumeEndpoint, { headers: headers });
   return historicalVolume
 }
@@ -22,31 +22,19 @@ const fetch = async (_timestamp: number, _at: any, options: FetchOptions) => {
   const historicalVolume = await fetchVolumeWithAuth()
   const dayTimestamp = getTimestampAtStartOfDayUTC(options.startOfDay)
 
-  const totalVolume = historicalVolume
-    .filter(volItem => (new Date(volItem.day).getTime() / 1000) <= dayTimestamp)
-    .reduce((acc, { value }) => acc + value, 0)
-
-  const dailyVolume = historicalVolume
-    .find(dayItem =>  (new Date(dayItem.day).getTime() / 1000) == dayTimestamp)?.value
+  const dailyVolume = historicalVolume.find(dayItem => (new Date(dayItem.day).getTime() / 1000) == dayTimestamp)?.value
 
 
   return {
-    totalVolume: totalVolume,
     dailyVolume: dailyVolume,
-    timestamp: dayTimestamp,
   };
-};
-
-const getStartTimestamp = async () => {
-  const historicalVolume = await fetchVolumeWithAuth()
-  return (new Date(historicalVolume[0].day).getTime()) / 1000
 }
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.NEUTRON]: {
       fetch,
-      start: getStartTimestamp,
+      start: 1721606400,
     },
   },
 };
