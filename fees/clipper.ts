169a8ff97d38ebec3c38d6fb213aafd632e8a0be
0xgnek <0xgnek@gmail.com>
2025-06-11 15:55:19 +0000

    fix import

--- fees/clipper.ts
+++ fees/clipper.ts
@@ -1,5 +1,5 @@
 import * as sdk from "@defillama/sdk";
-import { Chain } from "../adapters/types";
+import { Chain, FetchResult } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import request, { gql } from "graphql-request";
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
