2fd3aeee962fff5f6b1731eb4162c6bdeee6d80f
Will Hankins <will@tidelabs.io>
2025-06-12 23:40:28 -0700

    Add Bodega Market dailyHoldersRevenue (#3431)
    * Add Bodega Market dailyHoldersRevenue
    
    * refactor
    
    ---------
    
    Co-authored-by: Eden <noat.eth@gmail.com>

--- fees/bodega-market/index.ts
+++ fees/bodega-market/index.ts
@@ -4,18 +4,24 @@ import { CHAIN } from "../../helpers/chains";
 
 const fetch = async (options: FetchOptions): Promise<FetchResult> => {
   const {
-    data: { dailyFees, dailyRevenue },
+    data: { dailyFees, dailyRevenue, dailyHoldersRevenue },
   } = await axios.get(
     `https://tidelabs.io:2121/defillama/bodega-market/fees?from=${options.startTimestamp}&to=${options.endTimestamp}`
   );
   const dailyFeesUSD = options.createBalances();
   const dailyRevenueUSD = options.createBalances();
+  const dailyHoldersRevenueUSD = options.createBalances();
   dailyFeesUSD.addCGToken("cardano", Number(dailyFees));
   dailyRevenueUSD.addCGToken("cardano", Number(dailyRevenue));
+  dailyHoldersRevenueUSD.addCGToken("cardano", Number(dailyHoldersRevenue));
   return {
     timestamp: options.startOfDay,
     dailyFees: dailyFeesUSD,
     dailyRevenue: dailyRevenueUSD,
+    dailyHoldersRevenue: dailyHoldersRevenueUSD,
+
+    // no protocol revenue
+    dailyProtocolRevenue: 0,
   };
 };
 
@@ -27,8 +33,10 @@ const adapter: Adapter = {
       start: "2024-06-4",
       meta: {
         methodology: {
-          Fees: "All betting fees collected, which are distributed to market creators and staking users.",
-          Revenue: "All betting fees distributed specifically to staking users.",
+          Fees: "All betting fees (4% of total protocol volume) paid by users.",
+          Revenue: "All betting fees (4% of total protocol volume) paid by users.",
+          HoldersRevenue: "All revenue distributed to BODEGA holders.",
+          ProtocolRevenue: "No revenue for Bodega protocol.",
         },
       },
     },
