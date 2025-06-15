cf7ee231faff71e183bf3fe09140ff68c9c34fd6
treeoflife2 <sol.analyzoor@gmail.com>
2025-06-15 16:36:30 +0530

    refactor

--- fees/gliquid/index.ts
+++ fees/gliquid/index.ts
@@ -1,55 +1,43 @@
-import { gql, GraphQLClient } from "graphql-request";
+import request, { gql } from "graphql-request";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const methodology = {
-  Fees: "Swap fees paid by users.",
-};
+const endpoint = "https://api.goldsky.com/api/public/project_cmb20ryy424yb01wy7zwd7xd1/subgraphs/analytics/v1.0.0/gn"
 
-const graphQLClient = new GraphQLClient(
-  "https://api.goldsky.com/api/public/project_cmb20ryy424yb01wy7zwd7xd1/subgraphs/analytics/v1.0.0/gn"
-);
 
-const getFees = () => {
-  return gql`
+const fetch = async ({ startOfDay }: FetchOptions) => {
+  const query = gql`
     query GetFees($date: Int!) {
       algebraDayDatas(where: { date: $date }) {
         feesUSD
       }
-      factories {
-        totalFeesUSD
-      }
     }
   `;
-};
-
-const getGQLClient = () => {
-  return graphQLClient;
-};
-
-const fetch = async ({ startOfDay }: FetchOptions) => {
-  const feesRes = await getGQLClient().request(getFees(), {
+  const feesRes = await request(endpoint, query, {
     date: startOfDay,
   });
+
   const dailyFees = feesRes.algebraDayDatas[0].feesUSD;
-  const totalFees = feesRes.factories[0].totalFeesUSD;
+
   return {
     dailyFees: dailyFees,
-    totalFees: totalFees,
   };
 };
 
+
 const adapter: SimpleAdapter = {
+  version: 2,
   adapter: {
     [CHAIN.HYPERLIQUID]: {
       fetch: fetch,
       start: "2025-05-29",
       meta: {
-        methodology,
+        methodology: {
+          Fees: "Swap fees paid by users.",
+        },
       },
     },
   },
-  version: 2,
 };
 
 export default adapter;
