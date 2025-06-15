b5cf5b3e21dc660e35bcb1900e61021f8c79f93c
CLAM <>
2025-06-15 12:36:28 +0200

    add fee fetch and methodology

---
+++ fees/gliquid/index.ts
@@ -0,0 +1,55 @@
+import { gql, GraphQLClient } from "graphql-request";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+
+const methodology = {
+  Fees: "Swap fees paid by users.",
+};
+
+const graphQLClient = new GraphQLClient(
+  "https://api.goldsky.com/api/public/project_cmb20ryy424yb01wy7zwd7xd1/subgraphs/analytics/v1.0.0/gn"
+);
+
+const getFees = () => {
+  return gql`
+    query GetFees($date: Int!) {
+      algebraDayDatas(where: { date: $date }) {
+        feesUSD
+      }
+      factories {
+        totalFeesUSD
+      }
+    }
+  `;
+};
+
+const getGQLClient = () => {
+  return graphQLClient;
+};
+
+const fetch = async ({ startOfDay }: FetchOptions) => {
+  const statsRes = await getGQLClient().request(getFees(), {
+    date: startOfDay,
+  });
+  const dailyFees = statsRes.algebraDayDatas[0].feesUSD;
+  const totalFees = statsRes.factories[0].totalFeesUSD;
+  return {
+    dailyFees: dailyFees,
+    totalFees: totalFees,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.HYPERLIQUID]: {
+      fetch: fetch,
+      start: "2025-05-29",
+      meta: {
+        methodology,
+      },
+    },
+  },
+  version: 2,
+};
+
+export default adapter;
