1a6fcf1ed632431ee1d8671c8771b4c3bfd1a91b
Jake Loranger <Jloranger0@gmail.com>
2025-06-16 19:35:45 -0400

    Calculate Alpha Arcade volume

---
+++ dexs/alpha-arcade/index.ts
@@ -0,0 +1,68 @@
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import fetchURL from "../../utils/fetchURL";
+
+const fetch = async (options: FetchOptions) => {
+    let dailyVolume = 0;
+    const { startTimestamp, endTimestamp } = options;
+    const TARGET_APP_CALL_NAMES = [
+      'uh3u9Q==', // MATCH
+      'gyGzvQ==', // SPLIT
+      'jF2wVg==', // MERGE
+      'MgBiOw=='  // CLAIM
+    ];
+
+    // Convert UNIX timestamps to RFC 3339 format
+    const toRFC3339 = (timestamp: number) => new Date(timestamp * 1000).toISOString();
+    const startRFC3339 = toRFC3339(startTimestamp);
+    const endRFC3339 = toRFC3339(endTimestamp);
+    const baseURL = `https://mainnet-idx.4160.nodely.dev/v2/transactions`;
+    let nextToken: string | undefined = undefined;
+
+    do {
+      let url = `${baseURL}?min-round=1&max-round=999999999&start-time=${startRFC3339}&end-time=${endRFC3339}`;
+      if (nextToken) {
+        url += `&next=${nextToken}`;
+      }
+
+      const response = await fetchURL(url);
+      const txns = response.transactions || [];
+
+      const alphaArcadeTxns = txns.filter((txn) => {
+        const appCall = txn['application-transaction'];
+        const appArgs = appCall?.['application-args'];
+        if (!Array.isArray(appArgs)) return false;
+
+        return (
+          TARGET_APP_CALL_NAMES.includes(appArgs[0])
+        );
+      });
+
+      for (const txn of alphaArcadeTxns) {
+        const appArgs = txn['application-transaction']?.['application-args'];
+
+        const innerTxn = txn['inner-txns']?.[0];
+        const amount = innerTxn?.['asset-transfer-transaction']?.amount;
+
+        dailyVolume += amount;
+      }
+
+      nextToken = response['next-token'];
+    } while (nextToken);
+
+    return {
+      dailyVolume,
+    };
+};
+
+const adapter: SimpleAdapter = {
+    version: 2,
+    adapter:{
+      [CHAIN.ALGORAND]: {
+          fetch: fetch,
+          start: '2025-03-30',
+      }
+    }
+};
+
+export default adapter;
