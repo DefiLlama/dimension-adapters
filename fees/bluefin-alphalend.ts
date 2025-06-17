967eea18f0971509bdeb77de38a427c31e906950
abdullahabro <kamario.abdullah@gmail.com>
2025-06-17 23:47:07 +0500

    Add Bluefin Alphalend Fees

---
+++ fees/bluefin-alphalend.ts
@@ -0,0 +1,28 @@
+import fetchURL from "../utils/fetchURL"
+import { FetchResultFees, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+
+
+const fetch = async (_: number): Promise<FetchResultFees> => {
+    const data = await fetchURL("https://lend.api.sui-staging.bluefin.io/api/v1/fees/daily");
+    const dailyFees = Number(data.fees)
+    const dailyRevenue = Number(data.fees)
+
+    return {
+        dailyFees,
+        dailyRevenue,
+    };
+};
+
+const adapter: SimpleAdapter = {
+    version: 1,
+    adapter: {
+        [CHAIN.SUI]: {
+            fetch,
+            start: '2025-06-17',
+            runAtCurrTime: true,
+        },
+    },
+};
+
+export default adapter;
