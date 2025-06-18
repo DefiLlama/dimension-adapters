9c54cc5b4f6d7c78401df104e090c1db8cc26fd6
CuongNT <cuongnt@holdstation.com>
2025-06-18 10:27:34 +0700

    feat(wld): update daily volumn holdstation agg -- fix cmt using uniswapv2

--- aggregators/holdstation-agg/index.ts
+++ aggregators/holdstation-agg/index.ts
@@ -1,12 +1,10 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { addOneToken } from "../../helpers/prices";
-import { getProvider } from "@defillama/sdk";
-import { ethers } from "ethers";
+import { uniV2Exports } from "../../helpers/uniswap";
 
 const contractAddress = "0x43222f934ea5c593a060a6d46772fdbdc2e2cff0";
-const ROUTER_ADDRESS = "0x96E0fE5927172a66c2d947981802dd048eC65fB7";
-const WLD_ADDRESS = "0x2cfc85d8e48f8eab294be644d9e25c3030863003";
+const factory = "0x02E72c4edf877FBacfD1fC594422004D9746E14D";
 
 const event_fillQuoteEthToToken =
   "event FillQuoteEthToToken(address indexed buyToken,address indexed user,address target,uint256 amountSold,uint256 amountBought,uint256 feeAmount)";
@@ -15,9 +13,8 @@ const event_fillQuoteTokenToEth =
 const event_fillQuoteTokenToToken =
   "event FillQuoteTokenToToken(address indexed sellToken,address indexed buyToken,address indexed user,address target,uint256 amountSold,uint256 amountBought,uint8 feeToken,uint256 feeAmount)";
 
-const fetch = async (options: FetchOptions) => {
-  const dailyVolume = options.createBalances()
-  const dailyFees = options.createBalances()
+const customLogic = async ({ dailyVolume, dailyFees, fetchOptions }: any) => {
+  const options: FetchOptions = fetchOptions;
 
   const [
     log_fillQuoteEthToToken,
@@ -45,56 +42,9 @@ const fetch = async (options: FetchOptions) => {
     dailyFees.addToken(feeToken, log.feeAmount)
   })
 
-  const provider = getProvider(CHAIN.WC);
-  const iface = new ethers.Interface(['function swapTokensForExactTokens(uint256 amountOut,uint256 amountInMax,address[] path,address to,uint256 deadline)'])
-  const { getToBlock, getFromBlock } = options;
-  const [fromBlock, toBlock] = await Promise.all([getFromBlock(), getToBlock()]);
-
-  for (let i = fromBlock; i <= toBlock; i++) {
-    const block = await provider.getBlock(i);
-    if (!block) continue;
-    for (const transactionHash of block.transactions) {
-      const tx = await provider.getTransaction(transactionHash) as any;
-      if (!tx || !tx.to || !tx.data) continue;
-      // check if the transaction is a swap on the router contract
-      // and decode the input data to extract swap details
-      if (tx.to?.toLowerCase() === ROUTER_ADDRESS.toLowerCase() &&
-        tx.data.startsWith(iface.getFunction('swapTokensForExactTokens'))) {
-          const data = tx!.input
-          const decodedInput = iface.decodeFunctionData('swapTokensForExactTokens', data)
-          const { amountOut, amountInMax, path, to, deadline } = decodedInput
-          if (!path || path.length < 2) continue
-
-          const isTokenAtStart = path[0].toLowerCase() === WLD_ADDRESS.toLowerCase();
-          const isTokenAtEnd = path[path.length - 1].toLowerCase() === WLD_ADDRESS.toLowerCase();
-
-          if (!isTokenAtStart && !isTokenAtEnd) continue;
-
-          const volume = isTokenAtStart ? amountInMax : amountOut;
-          dailyVolume.addToken(WLD_ADDRESS, volume);
-      }
-    }
-  }
-
-  return {
-    dailyVolume, dailyFees,
-  };
+  return { dailyVolume, dailyFees };
 };
 
-const adapter: SimpleAdapter = {
-  version: 2,
-  adapter: {
-    [CHAIN.WC]: {
-      fetch,
-      start: "2025-04-16",
-      meta: {
-        methodology: {
-          dailyVolume:
-            "Volume is calculated by summing the token volume of all trades settled on the protocol that day.",
-        },
-      },
-    },
-  },
-};
-
-export default adapter;
+export default uniV2Exports({
+  [CHAIN.WC]: { factory, customLogic, start: "2025-04-16" },
+})
