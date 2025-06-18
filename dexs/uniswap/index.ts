9e9de209e430406b0ad871705b62b9013a7993ab
Eden <noat.eth@gmail.com>
2025-06-18 18:26:59 +0700

    track volume univ3 on wc

--- dexs/uniswap/index.ts
+++ dexs/uniswap/index.ts
@@ -60,14 +60,14 @@ const blacklisted = {
 
 const v3Endpoints = {
   // [CHAIN.ETHEREUM]: sdk.graph.modifyEndpoint('5AXe97hGLfjgFAc6Xvg6uDpsD5hqpxrxcma9MoxG7j7h'),
-  [CHAIN.OPTIMISM]: sdk.graph.modifyEndpoint('Jhu62RoQqrrWoxUUhWFkiMHDrqsTe7hTGb3NGiHPuf9'),
+  // [CHAIN.OPTIMISM]: sdk.graph.modifyEndpoint('Jhu62RoQqrrWoxUUhWFkiMHDrqsTe7hTGb3NGiHPuf9'),
   // [CHAIN.ARBITRUM]: "https://api.thegraph.com/subgraphs/id/QmZ5uwhnwsJXAQGYEF8qKPQ85iVhYAcVZcZAPfrF7ZNb9z",
-  [CHAIN.POLYGON]: sdk.graph.modifyEndpoint('3hCPRGf4z88VC5rsBKU5AA9FBBq5nF3jbKJG7VZCbhjm'),
+  // [CHAIN.POLYGON]: sdk.graph.modifyEndpoint('3hCPRGf4z88VC5rsBKU5AA9FBBq5nF3jbKJG7VZCbhjm'),
   // [CHAIN.CELO]: sdk.graph.modifyEndpoint('ESdrTJ3twMwWVoQ1hUE2u7PugEHX3QkenudD6aXCkDQ4'),
   // [CHAIN.BSC]: sdk.graph.modifyEndpoint('F85MNzUGYqgSHSHRGgeVMNsdnW1KtZSVgFULumXRZTw2'), // use oku
   // [CHAIN.AVAX]: sdk.graph.modifyEndpoint('9EAxYE17Cc478uzFXRbM7PVnMUSsgb99XZiGxodbtpbk'),
-  [CHAIN.BASE]: sdk.graph.modifyEndpoint('HMuAwufqZ1YCRmzL2SfHTVkzZovC9VL2UAKhjvRqKiR1'),
-  [CHAIN.ERA]: "https://api.thegraph.com/subgraphs/name/freakyfractal/uniswap-v3-zksync-era",
+  // [CHAIN.BASE]: sdk.graph.modifyEndpoint('HMuAwufqZ1YCRmzL2SfHTVkzZovC9VL2UAKhjvRqKiR1'),
+  // [CHAIN.ERA]: "https://api.thegraph.com/subgraphs/name/freakyfractal/uniswap-v3-zksync-era",
   // [CHAIN.UNICHAIN]: sdk.graph.modifyEndpoint('BCfy6Vw9No3weqVq9NhyGo4FkVCJep1ZN9RMJj5S32fX')
 };
 
@@ -253,7 +253,7 @@ interface IOkuResponse {
 }
 const fetchFromOku = async (options: FetchOptions) => {
   try {
-    const url = `https://omni.icarus.tools/${mappingChain(options.chain)}/cush/analyticsProtocolHistoric`;
+    const url = `https://omni.icarus.tools/${mappingChain(options.chain === 'era' ? 'zksync' : options.chain)}/cush/analyticsProtocolHistoric`;
     const body = {
       "params": [
         options.startTimestamp * 1000, //start
@@ -282,17 +282,16 @@ const mappingChain = (chain: string) => {
   return chain
 }
 
-adapter.breakdown.v3[CHAIN.UNICHAIN] = {
-  fetch: async (_t:any, _tb: any , options: FetchOptions) => {
-    const adapter = getUniV3LogAdapter({ factory: "0x1F98400000000000000000000000000000000003" })
-    const response = await adapter(options)
-    return response;
-  },
-  meta: {
-    methodology
-  }
-}
-
+// adapter.breakdown.v3[CHAIN.UNICHAIN] = {
+//   fetch: async (_t:any, _tb: any , options: FetchOptions) => {
+//     const adapter = getUniV3LogAdapter({ factory: "0x1F98400000000000000000000000000000000003" })
+//     const response = await adapter(options)
+//     return response;
+//   },
+//   meta: {
+//     methodology
+//   }
+// }
 adapter.breakdown.v3[CHAIN.AVAX] = {
   fetch: async (_t:any, _tb: any , options: FetchOptions) => {
     const adapter = getUniV3LogAdapter({ factory: "0x740b1c1de25031C31FF4fC9A62f554A55cdC1baD" })
@@ -304,9 +303,24 @@ adapter.breakdown.v3[CHAIN.AVAX] = {
   }
 }
 
+adapter.breakdown.v3[CHAIN.WC] = {
+  fetch: async (_t:any, _tb: any , options: FetchOptions) => {
+    const adapter = getUniV3LogAdapter({ factory: "0x7a5028BDa40e7B173C278C5342087826455ea25a" })
+    const response = await adapter(options)
+    return response;
+  },
+  meta: {
+    methodology
+  }
+}
+
 
 const okuChains = [
   CHAIN.ETHEREUM,
+  CHAIN.OPTIMISM,
+  CHAIN.POLYGON,
+  CHAIN.ERA,
+  CHAIN.UNICHAIN,
   CHAIN.SEI,
   CHAIN.ERA,
   CHAIN.TAIKO,
