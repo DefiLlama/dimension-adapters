e54a774ebc3affc2b11d7057d989598d023b75fa
Dre Derp 💵 <andre@dre.finance>
2025-06-19 05:58:07 +0300

    Fixed the revenue adapter for Rezerve Money (#3490)
    * wrote up trading fees for rezerve
    
    * formatted fees by decimals
    
    * added bond sales into the revenue calculations and also fixed the decimals

--- fees/rezerve-money/index.ts
+++ fees/rezerve-money/index.ts
@@ -1,27 +1,24 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getFees as getFeesFromShadow } from "./shadow";
+import { fetchBond } from "./bonds";
+import { fetchFeesFromShadow } from "./shadow";
 
 const fetch = async (options: FetchOptions) => {
-  const fromBlock = await options.getFromBlock();
-  const toBlock = await options.getToBlock();
   const dailyFees = options.createBalances();
+  const dailyRevenue = options.createBalances();
 
-  const usdc_rzr_shadowlp = "0x08c5e3b7533ee819a4d1f66e839d0e8f04ae3d0c"; // Replace with your LP token address
-
-  const fees = await getFeesFromShadow(fromBlock, toBlock, usdc_rzr_shadowlp);
-  dailyFees.add(fees.token0.token, fees.token0.fees);
-  dailyFees.add(fees.token1.token, fees.token1.fees);
+  await fetchFeesFromShadow(dailyFees, options);
+  await fetchBond(dailyFees, dailyRevenue, options);
 
   return {
     dailyFees,
-    dailyRevenue: dailyFees,
+    dailyRevenue,
   };
 };
 
 const methodology = {
   Fees: "Total fees accumulated by protocol-owned liquidity measured by k value growth",
-  Revenue: "All fees go to protocol-owned liquidity",
+  Revenue: "10% of all bond sales go to treasury",
 };
 
 const adapter: SimpleAdapter = {
