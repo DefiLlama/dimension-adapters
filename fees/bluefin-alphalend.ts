82a4240dd9cb65781571a8b99366d9efaef64cb3
Abdullah Abro <kamario.abdullah@gmail.com>
2025-06-23 08:54:42 +0500

    Add Bluefin Alphalend Fees Metrics (#3469)
    * Add Bluefin Alphalend Fees
    
    * use production URL
    
    * [BFS-1455]: Add metdata about explanations of the fields

---
+++ fees/bluefin-alphalend.ts
@@ -0,0 +1,37 @@
+import fetchURL from "../utils/fetchURL";
+import { FetchResultFees, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+
+const fetch = async (_: number): Promise<FetchResultFees> => {
+  const data = await fetchURL(
+    "https://lend.api.sui-prod.bluefin.io/api/v1/fees/daily"
+  );
+  const dailyFees = Number(data.fees);
+  const dailyRevenue = Number(data.fees);
+
+  return {
+    dailyFees,
+    dailyRevenue,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.SUI]: {
+      fetch,
+      start: "2025-06-17",
+      runAtCurrTime: true,
+      meta: {
+        methodology: {
+          Fees:
+            "All fees paid/earned while using lending/borrowing and liquidation.",
+          Revenue: "Fees collected by protocol native markets.",
+          ProtocolRevenue: "Fees/liquidation collected by protocol.",
+        },
+      },
+    },
+  },
+};
+
+export default adapter;
