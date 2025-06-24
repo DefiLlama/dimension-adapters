8cdecdc7a99f6204a73ded732bed17d4626fd352
treeoflife2 <sol.analyzoor@gmail.com>
2025-06-24 18:39:39 +0530

    Flashtrades endpoint change, and holder revenue as token stacker rewards and response changes (#3526)
    * add token holder revenue
    
    * add holder revenue and updated protocol revenue and endpoint for flashtrades

--- fees/flashtrade.ts
+++ fees/flashtrade.ts
@@ -1,45 +1,58 @@
-import { Adapter, FetchResultFees } from "../adapters/types";
+import { Adapter, FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import fetchURL from "../utils/fetchURL";
 
+const USDC_MINT = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+
 interface Pool {
-    pool: string;
-    accured: string;
-    paid: string;
+    poolName: string;
+    date: string;
+    totalRevenue: string;
+    totalProtocolFee: string;
 }
 
-const methodology = {
-    Fees: 'Sum of all fees accrued from LP pools.',
-    ProtocolReveneue: '30% of all the fees accrued excluding Community pool.'
-}
-
-const urlDailyStats = "https://api.prod.flash.trade/market-stat/revenue-24hr";
+const urlDailyStats = "https://api.prod.flash.trade/protocol-fees/daily";
 
 const calculateProtocolRevenue = (stats: Pool[]) => {
     return stats
-        .filter(item => item.pool !== "Community.1")
-        .reduce((sum, item) => sum + 0.3 * parseFloat(item.accured), 0);
+        .filter(item => item.poolName !== "Community.1")
+        .reduce((sum, item) => sum + 0.3 * parseFloat(item.totalProtocolFee), 0);
 }
 
-const fetchFlashStats = async (_: number): Promise<FetchResultFees> => {
-    const dailyStatsResponse = await fetchURL(urlDailyStats);
-    const dailyStats: Pool[] = dailyStatsResponse;
-    const dailyAccrued = dailyStats.reduce((sum, item) => sum + parseFloat(item.accured), 0);
-    const dailyProtocolRevenue = calculateProtocolRevenue(dailyStats);
+const fetch = async (options: FetchOptions) => {
+    const dailyFees = options.createBalances();
+    const dailyRevenue = options.createBalances();
+    const dailyProtocolRevenue = options.createBalances();
+
+    const dailyStats: Pool[] = await fetchURL(urlDailyStats);
+    const dailyAccrued = dailyStats.reduce((sum, item) => sum + parseFloat(item.totalProtocolFee), 0);
+    const protocol_revenue = calculateProtocolRevenue(dailyStats);
+
+    dailyFees.add(USDC_MINT, dailyAccrued);
+    dailyRevenue.add(USDC_MINT, protocol_revenue * 2);
+    dailyProtocolRevenue.add(USDC_MINT, protocol_revenue);
 
     return {
-        dailyFees: dailyAccrued.toString(),
-        dailyRevenue: dailyProtocolRevenue.toString(),
-        dailyProtocolRevenue: dailyProtocolRevenue.toString(),
+        dailyFees,
+        dailyRevenue,
+        dailyProtocolRevenue,
+        dailyHoldersRevenue: dailyProtocolRevenue,
     };
 };
+
+const methodology = {
+    Fees: 'Sum of all fees accrued from LP pools.',
+    Revenue: 'Sum of protocol revenue and holder revenue.',
+    ProtocolRevenue: '30% of all the fees accrued excluding Community pool.',
+    HolderRevenue: '50% of revenue goes to token stakers.',
+}
 
 const adapter: Adapter = {
     version: 2,
     adapter: {
         [CHAIN.SOLANA]: {
+            fetch,
             runAtCurrTime: true,
-            fetch: fetchFlashStats,
             meta: { methodology },
         },
     },
