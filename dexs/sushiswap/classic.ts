8af0c3fed425b1fc8a6565dddd2d98db0e01dfa1
Eden <noat.eth@gmail.com>
2025-07-02 01:16:09 +0700

    fix lint

--- dexs/sushiswap/classic.ts
+++ dexs/sushiswap/classic.ts
@@ -115,7 +115,7 @@ const classic = Object.keys(endpointsClassic).reduce(
     [chain]: {
       fetch: async (options: FetchOptions) => {
         try {
-          const call = chain === CHAIN.BOBA ? graphsClassicBoba : [CHAIN.ETHEREUM, CHAIN.KATANA].includes(chain) ? graphsClassicETH : graphsClassic;
+          const call = chain === CHAIN.BOBA ? graphsClassicBoba : [CHAIN.ETHEREUM, CHAIN.KATANA].includes(chain as CHAIN) ? graphsClassicETH : graphsClassic;
           const values = (await call(chain as Chain)(options));
           const result = {
             dailyVolume: values?.dailyVolume || 0,
