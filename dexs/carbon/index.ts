a5dd6dafda3a71ec18375569c71d84cccb6760e8
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-02 23:43:12 +0530

    refactor

--- dexs/carbon/index.ts
+++ dexs/carbon/index.ts
@@ -1,22 +1,14 @@
-import BigNumber from "bignumber.js";
 import request, { gql } from "graphql-request";
-import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
-const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
-
-const baseEndpoint =
-  "https://api.goldsky.com/api/public/project_cm0bho0j0ji6001t8e26s0wv8/subgraphs/intentx-base-analytics-083/latest/gn";
+const baseEndpoint = "https://api.goldsky.com/api/public/project_cm0bho0j0ji6001t8e26s0wv8/subgraphs/intentx-base-analytics-083/latest/gn";
 
 const queryBase = gql`
   query stats($from: String!, $to: String!) {
     dailyHistories(
       where: { timestamp_gte: $from, timestamp_lte: $to, accountSource: "0x8Ab178C07184ffD44F0ADfF4eA2ce6cFc33F3b86" }
     ) {
-      timestamp
-      platformFee
-      accountSource
       tradeVolume
     }
   }
@@ -24,48 +16,32 @@ const queryBase = gql`
 
 interface IGraphResponse {
   dailyHistories: Array<{
-    tiemstamp: string;
-    platformFee: string;
-    accountSource: string;
     tradeVolume: string;
   }>;
 }
 
-const toString = (x: BigNumber) => {
-  if (x.isEqualTo(0)) return undefined;
-  return x.toString();
-};
-
-const fetchVolumeBase = async (timestamp: number): Promise<FetchResultVolume> => {
+const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultVolume> => {
   const response: IGraphResponse = await request(baseEndpoint, queryBase, {
-    from: String(timestamp - ONE_DAY_IN_SECONDS),
-    to: String(timestamp),
+    from: String(options.startTimestamp),
+    to: String(options.endTimestamp),
   });
 
-  let dailyMakerVolume = new BigNumber(0);
-  let dailyTakerVolume = new BigNumber(0);
+  let dailyVolume = 0;
 
   response.dailyHistories.forEach((data) => {
-    dailyMakerVolume = dailyMakerVolume.plus(new BigNumber(data.tradeVolume));
-    dailyTakerVolume = dailyTakerVolume.plus(new BigNumber(data.tradeVolume));
+    dailyVolume += Number(data.tradeVolume) / 1e18;
   });
 
-  const dailyVolume = dailyMakerVolume.plus(dailyTakerVolume).dividedBy(new BigNumber(1e18));
-
-  const _dailyVolume = toString(dailyVolume);
-
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-
   return {
-    timestamp: dayTimestamp,
-    dailyVolume: _dailyVolume ?? "0",
+    dailyVolume,
   };
 };
 
 const adapter: SimpleAdapter = {
+  version: 1,
   adapter: {
     [CHAIN.BASE]: {
-      fetch: fetchVolumeBase,
+      fetch,
       start: "2023-11-01",
     },
   },
--- fees/carbon/index.ts
+++ fees/carbon/index.ts
@@ -1,107 +1,60 @@
-import BigNumber from "bignumber.js";
 import request, { gql } from "graphql-request";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const endpoint =
-  "https://api.goldsky.com/api/public/project_cm0bho0j0ji6001t8e26s0wv8/subgraphs/intentx-base-analytics-083/latest/gn";
+const endpoint = "https://api.goldsky.com/api/public/project_cm0bho0j0ji6001t8e26s0wv8/subgraphs/intentx-base-analytics-083/latest/gn";
 
 const query = gql`
   query stats($from: String!, $to: String!) {
     dailyHistories(
       where: { timestamp_gte: $from, timestamp_lte: $to, accountSource: "0x8Ab178C07184ffD44F0ADfF4eA2ce6cFc33F3b86" }
     ) {
-      timestamp
       platformFee
-      accountSource
-      tradeVolume
-    }
-    totalHistories(where: { accountSource: "0x8Ab178C07184ffD44F0ADfF4eA2ce6cFc33F3b86" }) {
-      timestamp
-      platformFee
-      accountSource
-      tradeVolume
     }
   }
 `;
 
 interface IGraphResponse {
   dailyHistories: Array<{
-    tiemstamp: string;
     platformFee: string;
-    accountSource: string;
-    tradeVolume: string;
-  }>;
-  totalHistories: Array<{
-    tiemstamp: string;
-    platformFee: string;
-    accountSource: string;
-    tradeVolume: BigNumber;
   }>;
 }
 
-const toString = (x: BigNumber) => {
-  if (x.isEqualTo(0)) return undefined;
-  return x.toString();
-};
-
-const fetchVolume = async ({ endTimestamp, startTimestamp }: FetchOptions) => {
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const response: IGraphResponse = await request(endpoint, query, {
-    from: String(startTimestamp),
-    to: String(endTimestamp),
+    from: String(options.startTimestamp),
+    to: String(options.endTimestamp),
   });
 
-  let dailyFees = new BigNumber(0);
+  let dailyFees = 0;
 
   response.dailyHistories.forEach((data) => {
-    dailyFees = dailyFees.plus(new BigNumber(data.platformFee));
+    dailyFees += Number(data.platformFee) / 1e18;
   });
-
-  let totalFees = new BigNumber(0);
-
-  response.totalHistories.forEach((data) => {
-    totalFees = totalFees.plus(new BigNumber(data.platformFee));
-  });
-
-  dailyFees = dailyFees.dividedBy(new BigNumber(1e18));
-  totalFees = totalFees.dividedBy(new BigNumber(1e18));
-
-  const _dailyFees = toString(dailyFees);
-  const _totalFees = toString(totalFees);
-
-  const dailyUserFees = _dailyFees;
-  const dailyRevenue = _dailyFees;
-  const dailyProtocolRevenue = "0";
-  const dailyHoldersRevenue = _dailyFees;
-  const dailySupplySideRevenue = "0";
-
-  const totalUserFees = _totalFees;
-  const totalRevenue = _totalFees;
-  const totalProtocolRevenue = "0";
-  const totalSupplySideRevenue = "0";
 
   return {
-    dailyFees: _dailyFees,
-    totalFees: _totalFees,
-
-    dailyUserFees: dailyUserFees,
-    dailyRevenue,
-    dailyProtocolRevenue: dailyProtocolRevenue,
-    dailyHoldersRevenue: dailyHoldersRevenue,
-    dailySupplySideRevenue: dailySupplySideRevenue,
-    totalUserFees: totalUserFees,
-    totalRevenue: totalRevenue,
-    totalProtocolRevenue: totalProtocolRevenue,
-    totalSupplySideRevenue: totalSupplySideRevenue,
+    dailyFees,
+    dailyUserFees: dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: "0",
+    dailyHoldersRevenue: dailyFees,
+    dailySupplySideRevenue: "0",
   };
 };
 
+const methodology = {
+  Fees: "Fees are calculated as the platform fee from the Carbon protocol.",
+  Revenue: "Revenue is calculated as the platform fee from the Carbon protocol.",
+  HoldersRevenue: "Holders Revenue is calculated as the platform fee from the Carbon protocol.",
+}
+
 const adapter: SimpleAdapter = {
-  version: 2,
+  version: 1,
   adapter: {
     [CHAIN.BASE]: {
-      fetch: fetchVolume,
+      fetch,
       start: "2023-11-01",
+      meta: { methodology },
     },
   },
 };
