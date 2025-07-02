64917665903b45c2fb89581ebe282a7c358c80aa
jasmin-avalon <122794657+jasmin-avalon@users.noreply.github.com>
2025-07-02 12:06:37 -0700

    set price for USDa

---
+++ fees/avalon-finance-usda/constants.ts
@@ -0,0 +1,28 @@
+import ADDRESSES from '../../helpers/coreAssets.json'
+
+export const TOKEN_DETAILS = {
+	[ADDRESSES.ethereum.USDa]: {
+		coingeckoId: 'usda-2',
+		decimals: 18,
+	},
+	[ADDRESSES.bsc.USDa]: {
+		coingeckoId: 'usda-2',
+		decimals: 18,
+	},
+	[ADDRESSES.mantle.USDa]: {
+		coingeckoId: 'usda-2',
+		decimals: 18,
+	},
+	[ADDRESSES.sonic.USDa]: {
+		coingeckoId: 'usda-2',
+		decimals: 18,
+	},
+	[ADDRESSES.berachain.USDa]: {
+		coingeckoId: 'usda-2',
+		decimals: 18,
+	},
+	[ADDRESSES.klaytn.USDa]: {
+		coingeckoId: 'usda-2',
+		decimals: 18,
+	},
+}
