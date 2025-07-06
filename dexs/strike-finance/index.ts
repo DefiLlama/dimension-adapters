7079ae1ad1db1870a42b2d91ae7aa23860c7f320
Will Hankins <will@tidelabs.io>
2025-07-06 11:17:20 -0700

    Update index.ts

--- dexs/strike-finance/index.ts
+++ dexs/strike-finance/index.ts
@@ -2,28 +2,26 @@ import axios from "axios";
 import { CHAIN } from "../../helpers/chains";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 
-
 export async function fetch(options: FetchOptions) {
   const {
-    data: { dailyVolume },
+    data: { totalVolume },
   } = await axios.get(
-    `https://tidelabs.io:2121/defillama/strike-finance/fees?from=${options.startTimestamp}&to=${options.endTimestamp}`,
+    `https://beta.strikefinance.org/api/analytics/volume?from=${options.startTimestamp}&to=${options.endTimestamp}`
   );
   const dailyVolumeUSD = options.createBalances();
-  dailyVolumeUSD.addCGToken('cardano', Number(dailyVolume));
+  dailyVolumeUSD.addCGToken("cardano", Number(totalVolume));
 
   return {
-    dailyVolume
+    totalVolume,
   };
 }
-
 
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
     [CHAIN.CARDANO]: {
       fetch,
-      start: '2025-05-16',
+      start: "2025-05-16",
     },
   },
 };
