050bdf6e1f9e8dfab312b2e58e8639866a17321d
0xwml <wesley@thalalabs.xyz>
2025-07-07 11:51:17 -0600

    [echelon] add daily revenue

--- fees/echelon.ts
+++ fees/echelon.ts
@@ -3,6 +3,7 @@ import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
 const feesQueryURL = "https://app.echelon.market/api/defillama/fees?timeframe=";
+const revenueQueryURL = "https://app.echelon.market/api/defillama/revenue?timeframe=";
 
 interface IVolumeall {
   value: number;
@@ -19,9 +20,24 @@ const movementFeesEndpoint = (endTimestamp: number, timeframe: string) =>
     ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
     : feesQueryURL + timeframe + "&network=movement_mainnet";
 
+const revenueEndpoint = (endTimestamp: number, timeframe: string) =>
+    endTimestamp
+        ? revenueQueryURL + timeframe + `&endTimestamp=${endTimestamp}`
+        : revenueQueryURL + timeframe;
+
+const movementRevenueEndpoint = (endTimestamp: number, timeframe: string) =>
+    endTimestamp
+        ? revenueQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
+        : revenueQueryURL + timeframe + "&network=movement_mainnet";
+
 const config: Record<string, (endTimestamp: number, timeframe: string) => string> = {
   [CHAIN.APTOS]: feesEndpoint,
   [CHAIN.MOVE]: movementFeesEndpoint,
+}
+
+const revenueConfig: Record<string, (endTimestamp: number, timeframe: string) => string> = {
+  [CHAIN.APTOS]: revenueEndpoint,
+  [CHAIN.MOVE]: movementRevenueEndpoint,
 }
     
 const fetch = async (timestamp: number, _:any, options: FetchOptions) => {
@@ -31,8 +47,12 @@ const fetch = async (timestamp: number, _:any, options: FetchOptions) => {
     0
   );
 
+  const dayRevenueQuery = (await fetchURL(revenueConfig[options.chain](timestamp, "1D")))?.data;
+  const dailyRevenue = dayRevenueQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+
   return {
     dailyFees: dailyFees,
+    dailyRevenue: dailyRevenue,
   };
 };
 
