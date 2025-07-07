c92edd821a77b74ee3e5f0c4c9b1d32d638ae403
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2025-07-07 19:13:39 +0200

    fix contango fee and volume/oi capture

--- dexs/contango/index.ts
+++ dexs/contango/index.ts
@@ -6,16 +6,23 @@ import { wrapGraphError } from "../../helpers/getUniSubgraph";
 
 type IEndpoint = {
   [chain: string]: string;
-}
+};
+
+const alchemyGraphUrl = (chain) =>
+  `https://subgraph.satsuma-prod.com/773bd6dfe1c6/egills-team/v2-${chain}/api`;
 
 const endpoint: IEndpoint = {
-  [CHAIN.ARBITRUM]: sdk.graph.modifyEndpoint('BmHqxUxxLuMoDYgbbXU6YR8VHUTGPBf9ghD7XH6RYyTQ'),
-  [CHAIN.OPTIMISM]: sdk.graph.modifyEndpoint('PT2TcgYqhQmx713U3KVkdbdh7dJevgoDvmMwhDR29d5'),
-  [CHAIN.ETHEREUM]: sdk.graph.modifyEndpoint('FSn2gMoBKcDXEHPvshaXLPC1EJN7YsfCP78swEkXcntY'),
-  [CHAIN.POLYGON]: sdk.graph.modifyEndpoint('5t3rhrAYt79iyjm929hgwyiaPLk9uGxQRMiKEasGgeSP'),
-  [CHAIN.BASE]: "https://graph.contango.xyz:18000/subgraphs/name/contango-xyz/v2-base",
-  [CHAIN.XDAI]: sdk.graph.modifyEndpoint('9h1rHUKJK9CGqztdaBptbj4Q9e2zL9jABuu9LpRQ1XkC'),
-}
+  [CHAIN.ARBITRUM]: alchemyGraphUrl("arbitrum"),
+  [CHAIN.OPTIMISM]: alchemyGraphUrl("optimism"),
+  [CHAIN.ETHEREUM]: alchemyGraphUrl("ethereum"),
+  [CHAIN.POLYGON]: alchemyGraphUrl("polygon"),
+  [CHAIN.BASE]: alchemyGraphUrl("base"),
+  [CHAIN.XDAI]: alchemyGraphUrl("gnosis"),
+  [CHAIN.AVAX]: alchemyGraphUrl("avalanche"),
+  [CHAIN.LINEA]: alchemyGraphUrl("linea"),
+  [CHAIN.BSC]: alchemyGraphUrl("bsc"),
+  [CHAIN.SCROLL]: alchemyGraphUrl("scroll"),
+};
 
 interface IAssetTotals {
   id: string;
@@ -54,11 +61,14 @@ const fetch = async (timestamp: number, _: any, options: FetchOptions) => {
       }
     }
     `;
-  let response: IResponse
+  let response: IResponse;
   try {
-    response = await request(endpoint[options.chain], query)
+    response = await request(endpoint[options.chain], query);
   } catch (error) {
-    console.error('Error fetching contango data', wrapGraphError(error as Error).message);
+    console.error(
+      "Error fetching contango data",
+      wrapGraphError(error as Error).message
+    );
     return { timestamp };
   }
 
@@ -67,12 +77,19 @@ const fetch = async (timestamp: number, _: any, options: FetchOptions) => {
   const dailyVolume = createBalances();
 
   const tokens = response.today.map((asset) => asset.id);
-  const decimals = await api.multiCall({ abi: 'erc20:decimals', calls: tokens })
+  const decimals = await api.multiCall({
+    abi: "erc20:decimals",
+    calls: tokens,
+  });
 
   const data: IAsset[] = response.today.map((asset, index: number) => {
-    const yesterday = response.yesterday.find((e: IAssetTotals) => e.id === asset.id);
-    const totalVolume = Number(asset.totalVolume) - Number(yesterday?.totalVolume || 0);
-    const totalFees = Number(asset.totalFees) - Number(yesterday?.totalFees || 0);
+    const yesterday = response.yesterday.find(
+      (e: IAssetTotals) => e.id === asset.id
+    );
+    const totalVolume =
+      Number(asset.totalVolume) - Number(yesterday?.totalVolume || 0);
+    const totalFees =
+      Number(asset.totalFees) - Number(yesterday?.totalFees || 0);
     const openInterest = Math.abs(Number(asset.openInterest));
     const multipliedBy = 10 ** Number(decimals[index]);
     return {
@@ -80,45 +97,63 @@ const fetch = async (timestamp: number, _: any, options: FetchOptions) => {
       openInterest: openInterest * multipliedBy,
       fees: totalFees * multipliedBy,
       volume: totalVolume * multipliedBy,
-    } as IAsset
-  })
+    } as IAsset;
+  });
   data.map(({ volume, id, openInterest, fees }) => {
-    dailyVolume.add(id, +volume)
-    dailyOpenInterest.add(id, +openInterest)
-    dailyFees.add(id, +fees)
+    dailyVolume.add(id, +volume);
+    dailyOpenInterest.add(id, +openInterest);
+    dailyFees.add(id, +fees);
   });
 
   return {
-    dailyOpenInterest, dailyFees, dailyVolume,
+    dailyOpenInterest,
+    dailyFees,
+    dailyVolume,
   };
-}
+};
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
       fetch,
-      start: '2023-10-03',
+      start: "2023-10-03",
     },
     [CHAIN.OPTIMISM]: {
       fetch,
-      start: '2023-10-02',
+      start: "2023-10-02",
     },
     [CHAIN.ETHEREUM]: {
       fetch,
-      start: '2023-10-03',
+      start: "2023-10-03",
     },
     [CHAIN.POLYGON]: {
       fetch,
-      start: '2023-10-13',
+      start: "2023-10-13",
     },
     [CHAIN.BASE]: {
       fetch,
-      start: '2023-10-09',
+      start: "2023-10-09",
     },
     [CHAIN.XDAI]: {
       fetch,
-      start: '2023-10-06',
+      start: "2023-10-06",
     },
-  }
+    [CHAIN.AVAX]: {
+      fetch,
+      start: "2024-08-11",
+    },
+    [CHAIN.LINEA]: {
+      fetch,
+      start: "2024-08-11",
+    },
+    [CHAIN.BSC]: {
+      fetch,
+      start: "2024-06-07",
+    },
+    [CHAIN.SCROLL]: {
+      fetch,
+      start: "2024-08-11",
+    },
+  },
 };
 export default adapter;
