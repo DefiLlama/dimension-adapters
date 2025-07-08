458174dcfe2501cc04ed8b544d98b6f562bed9fb
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-08 15:23:06 +0530

    refactor

--- fees/echelon.ts
+++ fees/echelon.ts
@@ -10,53 +10,51 @@ interface IVolumeall {
   timestamp: string;
 }
 
-const feesEndpoint = (endTimestamp: number, timeframe: string) =>
-  endTimestamp
-    ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}`
-    : feesQueryURL + timeframe;
+const buildURL = (baseURL: string, timeframe: string, endTimestamp: number, networkParam?: string) => {
+  let url = baseURL + timeframe;
+  if (endTimestamp) {
+    url += `&endTimestamp=${endTimestamp}`;
+  }
+  if (networkParam) {
+    url += networkParam;
+  }
+  return url;
+};
 
-const movementFeesEndpoint = (endTimestamp: number, timeframe: string) =>
-  endTimestamp
-    ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
-    : feesQueryURL + timeframe + "&network=movement_mainnet";
+const fees_url = (endTimestamp: number, timeframe: string) => buildURL(feesQueryURL, timeframe, endTimestamp);
+const movementFees_url = (endTimestamp: number, timeframe: string) => buildURL(feesQueryURL, timeframe, endTimestamp, "&network=movement_mainnet");
+const revenue_url = (endTimestamp: number, timeframe: string) => buildURL(revenueQueryURL, timeframe, endTimestamp);
+const movementRevenue_url = (endTimestamp: number, timeframe: string) => buildURL(revenueQueryURL, timeframe, endTimestamp, "&network=movement_mainnet");
 
-const revenueEndpoint = (endTimestamp: number, timeframe: string) =>
-    endTimestamp
-        ? revenueQueryURL + timeframe + `&endTimestamp=${endTimestamp}`
-        : revenueQueryURL + timeframe;
-
-const movementRevenueEndpoint = (endTimestamp: number, timeframe: string) =>
-    endTimestamp
-        ? revenueQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
-        : revenueQueryURL + timeframe + "&network=movement_mainnet";
-
-const config: Record<string, (endTimestamp: number, timeframe: string) => string> = {
-  [CHAIN.APTOS]: feesEndpoint,
-  [CHAIN.MOVE]: movementFeesEndpoint,
+const config: Record<string, { fees: (endTimestamp: number, timeframe: string) => string, revenue: (endTimestamp: number, timeframe: string) => string }> = {
+  [CHAIN.APTOS]: {
+    fees: fees_url,
+    revenue: revenue_url,
+  },
+  [CHAIN.MOVE]: {
+    fees: movementFees_url,
+    revenue: movementRevenue_url,
+  },
 }
 
-const revenueConfig: Record<string, (endTimestamp: number, timeframe: string) => string> = {
-  [CHAIN.APTOS]: revenueEndpoint,
-  [CHAIN.MOVE]: movementRevenueEndpoint,
-}
+const sumValues = (data: IVolumeall[]) => 
+  data.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
     
 const fetch = async (timestamp: number, _:any, options: FetchOptions) => {
-  const dayFeesQuery = (await fetchURL(config[options.chain](timestamp, "1D")))?.data;
-  const dailyFees = dayFeesQuery.reduce(
-    (partialSum: number, a: IVolumeall) => partialSum + a.value,
-    0
-  );
+  const dayFeesQuery = (await fetchURL(config[options.chain].fees(timestamp, "1D")))?.data;
+  const dailyFees = sumValues(dayFeesQuery);
 
-  const dayRevenueQuery = (await fetchURL(revenueConfig[options.chain](timestamp, "1D")))?.data;
-  const dailyRevenue = dayRevenueQuery.reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+  const dayRevenueQuery = (await fetchURL(config[options.chain].revenue(timestamp, "1D")))?.data;
+  const dailyRevenue = sumValues(dayRevenueQuery);
 
   return {
-    dailyFees: dailyFees,
-    dailyRevenue: dailyRevenue,
+    dailyFees,
+    dailyRevenue,
   };
 };
 
 const adapter: SimpleAdapter = {
+  version: 1,
   adapter: {
     [CHAIN.APTOS]: {
       fetch,
