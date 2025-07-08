058572b896ee1528c0bea574fbf40569c7fd3a0b
deepakpraka <131285971+deepakpraka@users.noreply.github.com>
2025-07-08 17:10:42 +0530

    Added perps revenue  (#3624)
    * Added kanalabs perps volumes in dexs
    
    * Added kana-perps-revenue
    
    * rename kana-perps adapter: rename fetch function and update fetch logic; add new fees adapter for kana-perps with methodology documentation.
    
    ---------
    
    Co-authored-by: treeoflife2 <sol.analyzoor@gmail.com>

--- fees/kanalabs-perp/index.ts
+++ fees/kanalabs-perp/index.ts
@@ -10,24 +10,35 @@ export enum KanaChainID {
 
 const fetch = async (timestamp: number, t: any, options: FetchOptions) => {
   const dayTimestamp = options.startOfDay + 86400;
+
   const query = gql`
-    query getDefillamaVolumeForPerps($timestamp: Float!, $chainId: Float!) {
-      getDefillamaVolumeForPerps(timestamp: $timestamp, chainId: $chainId)
+    query getPerpsRevenueSummary($day_ts: Float!, $chainId: Float!) {
+      getPerpsRevenueSummary(day_ts: $day_ts, chainId: $chainId)
     }
   `;
+
   const variables = {
-    timestamp: dayTimestamp - 1,
+    day_ts: dayTimestamp - 1,
     chainId: KanaChainID.aptos,
   };
+
   const data = await request(GRAPHQL_URL, query, variables);
-  const result = data.getDefillamaVolumeForPerps;
+  const result = data.getPerpsRevenueSummary;
+  const dailyFees = result.today;
 
   return {
-    dailyVolume: result.today.volume,
+    dailyFees,
+    dailyUserFees: dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
   };
 };
 
-const startTimeBlock = 1695897800;
+const methodology = {
+  Fees: "Fees are collected from the users when they trade on Kana Perps.",
+  Revenue: "Revenue is the sum of fees collected from the users.",
+  ProtocolRevenue: "Protocol revenue is the sum of fees collected from the users.",
+}
 
 const adapter: SimpleAdapter = {
   version: 1,
@@ -35,6 +46,9 @@ const adapter: SimpleAdapter = {
     [CHAIN.APTOS]: {
       fetch,
       start: '2024-09-12',
+      meta: {
+        methodology,
+      },
     },
   },
 };
