7e19c453d5181be670724486f1cc3bb49a15b20e
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-08 16:22:41 +0530

    refactor

--- aggregators/apstation/index.ts
+++ aggregators/apstation/index.ts
@@ -4,34 +4,35 @@ import { CHAIN } from "../../helpers/chains";
 
 const SwapEvent = "event Swap(address tokenIn, address tokenOut, uint256 amountIn, address referer, address sender)";
 
-const Contract = '0x4f4b84b42059E8CaabB211Aa8F02A9cab53A6c4e';
+const factory_contract = "0x4f4b84b42059E8CaabB211Aa8F02A9cab53A6c4e";
 
 const fetch = async (options: FetchOptions) => {
-    const dailyVolume = options.createBalances();
-    const data: any[] = await options.getLogs({
-        target: Contract,
-        eventAbi: SwapEvent,
-    });
-    data.forEach((log: any) => {
-        dailyVolume.add(log.tokenIn, log.amountIn);
-    });
-    return { dailyVolume };
+  const dailyVolume = options.createBalances();
+
+  const data: any[] = await options.getLogs({
+    target: factory_contract,
+    eventAbi: SwapEvent,
+  });
+  data.forEach((log: any) => {
+    dailyVolume.add(log.tokenIn, log.amountIn);
+  });
+
+  return { dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
-    version: 2,
-    adapter: {
-      [CHAIN.HYPERLIQUID]: {
-        fetch: fetch,
-        start: "2025-07-05",
-        meta: {
-          methodology: {
-            Volume:
-              "Volume from Apstation",
-          },
+  version: 2,
+  adapter: {
+    [CHAIN.HYPERLIQUID]: {
+      fetch,
+      start: "2025-07-05",
+      meta: {
+        methodology: {
+          Volume: "Apstation volume",
         },
       },
     },
-  };
-  
+  },
+};
+
 export default adapter;
