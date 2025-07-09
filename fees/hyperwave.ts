334e348cfb36d09ca8ff53d92514dbd2f01a3b81
darvin <todarvinrio@gmail.com>
2025-07-09 14:07:27 +0530

    adding daily aggregated pnl

--- fees/hyperwave.ts
+++ fees/hyperwave.ts
@@ -1,6 +1,7 @@
 import { Adapter, FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains"
 import { httpPost } from "../utils/fetchURL";
+import jmespath from "jmespath";
 
 // Each entry in the history is a tuple: [timestamp, value]
 type HistoryEntry = [number, string];
@@ -32,23 +33,51 @@ const MS_4 = "0xc8f969ef6b51a428859f3a606e6b103dc1fb92e9";
 const MS_5 = "0x2cd4aa47e778fe8fa27cdcd4ce2bc99b6bf90f61";
 const MS_ALL = [MS_1, MS_2, MS_3, MS_4, MS_5];
 
+async function fetchHyperliquidInfo<T>(
+    input: any,
+    path: string
+): Promise<T> {
+    const response = await httpPost("https://api.hyperliquid.xyz/info", input);
+    const data = jmespath.search(response, path);
+    return data as T;
+}
+
 const fetch = async (options: FetchOptions) => {
-    const eoa = MS_5
-    const dailyFees = options.createBalances();
-    const data:PortfolioData = await httpPost("https://api.hyperliquid.xyz/info", { type: "portfolio", user: eoa })
-    const dailyData = data[0]
-    // const perpDailyData = data[4]
-    const dailyPnl = dailyData[1].pnlHistory
-    // const dailyAccountVault = dailyData[1].accountValueHistory
-    const dailyFeesData = dailyPnl.map((t: any) => ({ pnl: t[1]*1e6,time: t[0] * 1e3 }))
-    const todayFees = dailyFeesData[dailyFeesData.length - 1].pnl
-    dailyFees.addCGToken("usd-coin", todayFees/1e6)
+    
+    const dailyRevenue = options.createBalances();
+    const JMES_TO_PNL = "[0][1].pnlHistory"
+    const DELAY = 200 // ms
+    // const delay = 10000 // ms
 
+    // Fetch pnlHistory for all MS addresses and concatenate them with DELAY delay between calls
+    const allPnlHistories: HistoryEntry[][] = [];
+    for (const ms of MS_ALL) {
+        const pnlHistory = await fetchHyperliquidInfo<HistoryEntry[]>({ type: "portfolio", user: ms }, JMES_TO_PNL);
+        allPnlHistories.push(pnlHistory);
+        await new Promise(res => setTimeout(res, DELAY));
+    }
+    // Flatten all histories into a single array
+    const dailyPnlHistory = allPnlHistories.flat();
 
-    console.log(dailyPnl)
+    // Aggregate PnL by day (UTC)
+    const dailyPnlMap = new Map<number, number>();
+    for (const [timestamp, pnlStr] of dailyPnlHistory) {
+        const date = new Date(Number(timestamp));
+        // Get UTC date string (YYYY-MM-DD)
+        const day = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+        const pnl = Number(pnlStr);
+        dailyPnlMap.set(day, (dailyPnlMap.get(day) ?? 0) + pnl);
+    }
+    // Get the latest day and its fees
+    const sortedDays = Array.from(dailyPnlMap.keys()).sort((a, b) => a - b);
+    const todayDay = sortedDays[sortedDays.length - 1];
+    const todayFees = dailyPnlMap.get(todayDay) ?? 0;
+    dailyRevenue.addCGToken("usd-coin", todayFees)
+
+    // console.log(dailyPnlMap)
 
     return {
-        dailyFees: dailyFees
+        dailyRevenue: dailyRevenue
     }
 }
 
