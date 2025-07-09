b5628b876fc055ba456cebb1129fd8b7f621ffa4
darvin <todarvinrio@gmail.com>
2025-07-09 14:25:55 +0530

    format

--- fees/hyperwave.ts
+++ fees/hyperwave.ts
@@ -1,5 +1,5 @@
 import { Adapter, FetchOptions } from "../adapters/types";
-import { CHAIN } from "../helpers/chains"
+import { CHAIN } from "../helpers/chains";
 import { httpPost } from "../utils/fetchURL";
 import jmespath from "jmespath";
 
@@ -7,23 +7,22 @@ import jmespath from "jmespath";
 type HistoryEntry = [number, string];
 
 interface HistoryData {
-  accountValueHistory: HistoryEntry[];
-  pnlHistory: HistoryEntry[];
-  vlm: string;
+    accountValueHistory: HistoryEntry[];
+    pnlHistory: HistoryEntry[];
+    vlm: string;
 }
 
 // The main data structure is a tuple of string key and HistoryData
 type PortfolioData = [
-  ["day", HistoryData],
-  ["week", HistoryData],
-  ["month", HistoryData],
-  ["allTime", HistoryData],
-  ["perpDay", HistoryData],
-  ["perpWeek", HistoryData],
-  ["perpMonth", HistoryData],
-  ["perpAllTime", HistoryData]
+    ["day", HistoryData],
+    ["week", HistoryData],
+    ["month", HistoryData],
+    ["allTime", HistoryData],
+    ["perpDay", HistoryData],
+    ["perpWeek", HistoryData],
+    ["perpMonth", HistoryData],
+    ["perpAllTime", HistoryData]
 ];
-
 
 // Multi-Sigs
 const MS_1 = "0x128Cc5830214aBAF05A0aC178469a28de56C0BA9";
@@ -33,27 +32,27 @@ const MS_4 = "0xc8f969ef6b51a428859f3a606e6b103dc1fb92e9";
 const MS_5 = "0x2cd4aa47e778fe8fa27cdcd4ce2bc99b6bf90f61";
 const MS_ALL = [MS_1, MS_2, MS_3, MS_4, MS_5];
 
-async function fetchHyperliquidInfo<T>(
-    input: any,
-    path: string
-): Promise<T> {
+async function fetchHyperliquidInfo<T>(input: any, path: string): Promise<T> {
     const response = await httpPost("https://api.hyperliquid.xyz/info", input);
     const data = jmespath.search(response, path);
     return data as T;
 }
 
 const fetch = async (options: FetchOptions) => {
-    
     const dailyRevenue = options.createBalances();
-    let JMES_TO_PNL = "[0][1].pnlHistory"
-    const DELAY = 200 // ms
+    let JMES_TO_PNL = "[0][1].pnlHistory";
+    const DELAY = 200; // ms
     // const delay = 10000 // ms
-    const START_TIMESTAMP = options.startTimestamp * 1e3
-    const END_TIMESTAMP = options.endTimestamp * 1e3
+    const START_TIMESTAMP = options.startTimestamp * 1e3;
+    const END_TIMESTAMP = options.endTimestamp * 1e3;
 
     // Determine if START_TIMESTAMP is today or yesterday
     const now = new Date();
-    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
+    const today = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate()
+    ).getTime();
     const yesterday = today - 24 * 60 * 60 * 1000;
 
     if (START_TIMESTAMP !== today && START_TIMESTAMP !== yesterday) {
@@ -64,13 +63,16 @@ const fetch = async (options: FetchOptions) => {
     // Fetch pnlHistory for all MS addresses and concatenate them with DELAY delay between calls
     const allPnlHistories: HistoryEntry[][] = [];
     for (const ms of MS_ALL) {
-        const pnlHistory = await fetchHyperliquidInfo<HistoryEntry[]>({ type: "portfolio", user: ms }, JMES_TO_PNL);
+        const pnlHistory = await fetchHyperliquidInfo<HistoryEntry[]>(
+            { type: "portfolio", user: ms },
+            JMES_TO_PNL
+        );
         allPnlHistories.push(pnlHistory);
-        await new Promise(res => setTimeout(res, DELAY));
+        await new Promise((res) => setTimeout(res, DELAY));
     }
     // Flatten all histories into a single array
     const dailyPnlHistory = allPnlHistories.flat();
-    console.log(dailyPnlHistory[0])
+    console.log(dailyPnlHistory[0]);
 
     // Aggregate total PnL between START_TIMESTAMP and END_TIMESTAMP (inclusive)
     let totalPnl = 0;
@@ -83,9 +85,9 @@ const fetch = async (options: FetchOptions) => {
     dailyRevenue.addCGToken("usd-coin", totalPnl);
 
     return {
-        dailyRevenue: dailyRevenue
-    }
-}
+        dailyRevenue: dailyRevenue,
+    };
+};
 
 const adapter: Adapter = {
     version: 2,
@@ -96,10 +98,10 @@ const adapter: Adapter = {
             meta: {
                 methodology: {
                     Revenue: "Yield generated from HLP vault",
-                }
-            }
+                },
+            },
         },
     },
-}
+};
 
 export default adapter;
