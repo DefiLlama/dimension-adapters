08112346f3aa8ccba500a83c60859d465bdc6ef6
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-11 22:03:16 +0530

    refactor and fix bug

--- fees/meso-finance.ts
+++ fees/meso-finance.ts
@@ -2,37 +2,26 @@ import fetchURL from '../utils/fetchURL';
 import { FetchOptions, SimpleAdapter } from '../adapters/types';
 import { CHAIN } from '../helpers/chains';
 
-const feesQueryURL =
-  'https://api.meso.finance/api/v1/Tool/defillama/fees?timeframe=';
+const feesQueryURL = 'https://api.meso.finance/api/v1/Tool/defillama/fees?timeframe=';
 
 interface IVolumeall {
   value: number;
   timestamp: number;
 }
 
-const feesEndpoint = (timestamp: number, timeframe: string) =>
-  timestamp
-    ? feesQueryURL + timeframe + `&timestamp=${timestamp}`
-    : feesQueryURL + timeframe;
-
-const config: Record<
-  string,
-  (endTimestamp: number, timeframe: string) => string
-> = {
-  [CHAIN.APTOS]: feesEndpoint,
-};
-
 const fetch = async (timestamp: number, _: any, options: FetchOptions) => {
-  const dayFeesData = await fetchURL(config[options.chain](timestamp, '1D'));
+  const url = feesQueryURL + '1D' + `&timestamp=${timestamp}`
+  const dayFeesData = await fetchURL(url);
 
-  const dailyFees = dayFeesData.filter((a: IVolumeall) => a.timestamp >= timestamp).reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
+  const dailyFees = dayFeesData.filter((a: IVolumeall) => a.timestamp >= options.startTimestamp && a.timestamp <= options.endTimestamp).reduce((partialSum: number, a: IVolumeall) => partialSum + a.value, 0);
 
   return {
-    dailyFees: dailyFees
+    dailyFees
   };
 };
 
 const adapter: SimpleAdapter = {
+  version: 1,
   adapter: {
     [CHAIN.APTOS]: {
       fetch,
