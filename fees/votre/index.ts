c19f1e14304b45614104833dd9244fb4b25164ba
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-15 11:56:38 +0530

    refactor: streamline revenue fetching and remove unused loans function

--- fees/votre/index.ts
+++ fees/votre/index.ts
@@ -4,8 +4,6 @@ import { request, gql } from "graphql-request";
 const BASE_MAINNET_SUBGRAPH_URL = 'https://api.goldsky.com/api/public/project_cm3exke617zqh01074tulgtx0/subgraphs/collar-base-mainnet/0.1.3/gn'
 
 
-
-
 async function revenue(startTime: number, endTime: number) {
   const query = gql`
     query getProtocolFees($startTime: Int!, $endTime: Int!) {
@@ -24,46 +22,31 @@ async function revenue(startTime: number, endTime: number) {
   return data.providerPositions;
 }
 
-async function loans(startTime: number, endTime: number) {
-  const query = gql`
-    query getLoans($startTime: Int!, $endTime: Int!) {
-      loans(first: 1000, where: { openedAt_gte: $startTime, openedAt_lt: $endTime}) {
-        underlyingAmount
-        loansNFT {
-          underlying
-        }
-      }
-    }
-  `;
-  const data = await request(BASE_MAINNET_SUBGRAPH_URL, query, {
-    startTime,
-    endTime
-  });
-  return data.loans;
-}
-
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
-  const dailyVolume = options.createBalances();
-  const { fromTimestamp, toTimestamp } = options;
+  const { startTimestamp, endTimestamp } = options;
 
-  const providerPositions = await revenue(fromTimestamp, toTimestamp);
+  const providerPositions = await revenue(startTimestamp, endTimestamp);
   providerPositions.forEach((log: any) => {
     dailyFees.add(log.collarProviderNFT.cashAsset, log.protocolFeeAmount);
   });
-  const loansData = await loans(fromTimestamp, toTimestamp);
-  loansData.forEach((log: any) => {
-    dailyVolume.add(log.loansNFT.underlying, log.underlyingAmount);
-  });
-  return { dailyFees, dailyProtocolRevenue: dailyFees, dailyVolume };
+
+  return { dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees };
 };
+
+const methodology = {
+  Fees: '',
+  Revenue: '',
+  ProtocolRevenue: ''
+} 
 
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
     base: {
       fetch,
-      start: '2025-04-16'
+      start: '2025-04-16',
+      meta: { methodology }
     }
   }
 };
