78b30f3aeafe5b5b58859993b1dd2a2621f165d5
99adarsh <itsadarshkumar@gmail.com>
2025-07-16 17:56:36 +0530

    add perpetual volume in total volume for elys chain

--- dexs/elys-dex.ts
+++ dexs/elys-dex.ts
@@ -4,10 +4,14 @@ import { httpGet } from "../utils/fetchURL";
 
 const fetch = async (_: any, _1: any, { dateString }: FetchOptions) => {
   const data = await  httpGet('https://supply.elys.network/stats/daily-volume')
+  const perpData = await  httpGet('https://supply.elys.network/stats/daily-perp-volume')
 
   let dailyVolume = 0
   data.forEach((item: any) => {
     if (item.date.slice(0, 10) === dateString) dailyVolume += item.volume
+  })
+  perpData.forEach((item: any) => {
+    if (item.date.slice(0, 10) === dateString) dailyVolume += item.total_volume
   })
 
   return { dailyVolume, }
