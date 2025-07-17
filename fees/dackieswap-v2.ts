dab9fbe6dfe332ed8691e89126fa7410ad58342c
Eden <noat.eth@gmail.com>
2025-07-17 15:27:55 +0700

    fix broken fees (#3740)
    * fix broken fees
    
    * add hyperswap methods

--- fees/dackieswap-v2.ts
+++ fees/dackieswap-v2.ts
@@ -1,26 +1,3 @@
-// 0.25% swap fee, 0.17% to LP, 0.08% to protocol
-// https://docs.dackieswap.xyz/products/product-features/traders/trading-fee
-
-import { getDexChainFees } from "../helpers/getUniSubgraphFees";
-import volumeAdapter from "../dexs/dackieswap-v2";
-import { Adapter, BaseAdapter } from "../adapters/types";
-
-const TOTAL_FEES = 0.0025;
-const LP_FEES = 0.0017;
-const PROTOCOL_FEES = 0.0008;
-
-const feeAdapter: BaseAdapter = getDexChainFees({
-  totalFees: TOTAL_FEES,
-  protocolFees: PROTOCOL_FEES,
-  supplySideRevenue: LP_FEES,
-  revenue: PROTOCOL_FEES,
-  userFees: TOTAL_FEES,
-  volumeAdapter
-});
-
-const adapter: Adapter = {
-  version: 2,
-  adapter: feeAdapter
-};
+import adapter from '../dexs/dackieswap-v2';
 
 export default adapter;
