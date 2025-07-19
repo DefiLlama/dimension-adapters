4b6d059c177a8ac99630878475d1f973040a42e9
tree <sol.analyzoor@gmail.com>
2025-07-19 15:59:33 +0530

    refactor and add methodology (#3766)

--- fees/jupiter-perpetual/index.ts
+++ fees/jupiter-perpetual/index.ts
@@ -3,6 +3,7 @@ import { CHAIN } from "../../helpers/chains";
 import { getSqlFromFile, queryDuneSql } from "../../helpers/dune";
 
 const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultV2> => {
+
   // https://dune.com/queries/4751411
   const sql = getSqlFromFile("helpers/queries/jupiter-perpetual.sql", {
     start: options.startTimestamp,
@@ -10,6 +11,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResu
   });
   const data: any[] = (await queryDuneSql(options, sql));
   const dailyFees = data[0].total_fees;
+
   return {
     dailyFees,
     dailyRevenue: `${dailyFees * (25 / 100)}`,
@@ -19,16 +21,26 @@ const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResu
   }
 };
 
+const meta = {
+  methodology: {
+    Fees: "Fees paid by users to open/close positions for perps",
+    Revenue: "25% of total fees goes to protocol tresuary + JLP holders",
+    ProtocolRevenue: "50% of revenue (12.5% of total fees) goes to protocol treasury",
+    HoldersRevenue: "50% of revenue (12.5% of total fees) goes to JUP holders", 
+    SupplySideRevenue: "75% of total fees goes to liquidity providers",
+  }
+}
+
 const adapter = {
   version: 1,
   adapter: {
     [CHAIN.SOLANA]: {
       fetch,
-      runAtCurrTime: true,
       start: '2024-01-23',
+      meta
     },
   },
   isExpensiveAdapter: true,
 };
-export default adapter;
 
+export default adapter;
