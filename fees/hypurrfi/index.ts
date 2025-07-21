b926167ef833d43e6cbc328de32b66806ed5b494
Eden <noat.eth@gmail.com>
2025-07-21 19:17:59 +0700

    add methodology for hypurrfi

--- fees/hypurrfi/index.ts
+++ fees/hypurrfi/index.ts
@@ -2,6 +2,41 @@ import { CHAIN } from "../../helpers/chains";
 import type { SimpleAdapter } from "../../adapters/types";
 import { aaveExport } from "../../helpers/aave";
 
+const meta = {
+  methodology: {
+    Fees: 'Include borrow interest, flashloan fee, liquidation fee and penalty paid by borrowers.',
+    Revenue: 'Amount of fees go to HypurrFi treasury.',
+    SupplySideRevenue: 'Amount of fees distributed to suppliers.',
+    ProtocolRevenue: 'Amount of fees go to HypurrFi treasury.',
+  },
+  breakdownMethodology: {
+    Fees: {
+      'BorrowInterest': 'All interest paid by borrowers from all markets (excluding USDXL).',
+      'BorrowInterestUSDXL': 'All interest paid by borrowers from USDXL only.',
+      'LiquidationFees': 'Fees from liquidation penalty and bonuses.',
+      'FlashloanFees': 'Flashloan fees paid by flashloan borrowers and executors.',
+    },
+    Revenue: {
+      'BorrowInterest': 'A portion of interest paid by borrowers from all markets (excluding USDXL).',
+      'BorrowInterestUSDXL': 'All 100% interest paid by USDXL borrowers.',
+      'LiquidationFees': 'A portion of fees from liquidation penalty and bonuses.',
+      'FlashloanFees': 'A portion of fees paid by flashloan borrowers and executors.',
+    },
+    SupplySideRevenue: {
+      'BorrowInterest': 'Amount of interest distributed to lenders from all markets (excluding USDXL).',
+      'BorrowInterestUSDXL': 'No supply side revenue for lenders on USDXL market.',
+      'LiquidationFees': 'Fees from liquidation penalty and bonuses are distributed to lenders.',
+      'FlashloanFees': 'Flashloan fees paid by flashloan borrowers and executors are distributed to lenders.',
+    },
+    ProtocolRevenue: {
+      'BorrowInterest': 'Amount of interest distributed to lenders from all markets (excluding USDXL) are collected by HypurrFi treasury.',
+      'BorrowInterestUSDXL': 'All interest paid on USDXL market are collected by HypurrFi treasury.',
+      'LiquidationFees': 'A portion of fees from liquidation penalty and bonuses are colected by HypurrFi treasury.',
+      'FlashloanFees': 'A portion of fees paid by flashloan borrowers and executors are collected by HypurrFi treasury.',
+    },
+  }
+}
+
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
@@ -18,6 +53,7 @@ const adapter: SimpleAdapter = {
             }
           },
         ],
+        meta,
       },
     })
   }
