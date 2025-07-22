4fb7bf8edaa1d9ef22eb09261a03be35de3f9b28
Eden <noat.eth@gmail.com>
2025-07-22 16:01:25 +0700

    using indexer to query logs

--- dexs/uniswap-v4.ts
+++ dexs/uniswap-v4.ts
@@ -52,14 +52,14 @@
 
 // }
 
+import * as sdk from "@defillama/sdk";
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { queryDune } from '../helpers/dune';
 
 interface IUniswapConfig {
   poolManager: string;
   positionManager: string;
-  source: 'DUNE' | 'LOGS';
+  source: 'LOGS' | 'INDEXER';
   start: string;
 }
 
@@ -70,208 +70,207 @@ interface IPool {
   currency1: string;
 }
 
+const SwapEvent = 'event Swap(bytes32 indexed id, address indexed sender, int128 amount0, int128 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint24 fee)';
+const FunctionPoolKeys = 'function poolKeys(bytes25) view returns(address currency0, address currency1, uint24 fee, int24 tickSpacing, address hooks)';
+const NativeCoin = '0x0000000000000000000000000000000000000000';
+
 const Configs: Record<string, IUniswapConfig> = {
   [CHAIN.ETHEREUM]: {
     poolManager: '0x000000000004444c5dc75cB358380D2e3dE08A90',
     positionManager: '0xbd216513d74c8cf14cf4747e6aaa6420ff64ee9e',
-    source: 'DUNE',
+    source: 'INDEXER',
     start: '2025-01-24',
   },
   [CHAIN.UNICHAIN]: {
     poolManager: '0x1f98400000000000000000000000000000000004',
     positionManager: '0x4529a01c7a0410167c5740c487a8de60232617bf',
-    source: 'DUNE',
+    source: 'INDEXER',
     start: '2025-01-24',
   },
   [CHAIN.OPTIMISM]: {
     poolManager: '0x9a13f98cb987694c9f086b1f5eb990eea8264ec3',
     positionManager: '0x3c3ea4b57a46241e54610e5f022e5c45859a1017',
-    source: 'DUNE',
+    source: 'INDEXER',
     start: '2025-01-24',
   },
   [CHAIN.BASE]: {
     poolManager: '0x498581ff718922c3f8e6a244956af099b2652b2b',
     positionManager: '0x7c5f5a4bbd8fd63184577525326123b519429bdc',
-    source: 'DUNE',
+    source: 'INDEXER',
     start: '2025-01-24',
   },
   [CHAIN.ARBITRUM]: {
     poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
     positionManager: '0xd88f38f930b7952f2db2432cb002e7abbf3dd869',
-    source: 'DUNE',
+    source: 'INDEXER',
     start: '2025-01-24',
   },
   [CHAIN.POLYGON]: {
     poolManager: '0x67366782805870060151383f4bbff9dab53e5cd6',
     positionManager: '0x1ec2ebf4f37e7363fdfe3551602425af0b3ceef9',
-    source: 'DUNE',
+    source: 'INDEXER',
     start: '2025-01-24',
   },
   [CHAIN.BLAST]: {
     poolManager: '0x1631559198a9e474033433b2958dabc135ab6446',
-    source: 'DUNE',
+    source: 'INDEXER',
     positionManager: '0x4ad2f4cca2682cbb5b950d660dd458a1d3f1baad',
     start: '2025-01-24',
   },
   [CHAIN.ZORA]: {
     poolManager: '0x0575338e4c17006ae181b47900a84404247ca30f',
-    source: 'DUNE',
+    source: 'LOGS',
     positionManager: '0xf66c7b99e2040f0d9b326b3b7c152e9663543d63',
     start: '2025-01-24',
   },
   [CHAIN.WC]: {
     poolManager: '0xb1860d529182ac3bc1f51fa2abd56662b7d13f33',
-    source: 'DUNE',
+    source: 'LOGS',
     positionManager: '0xc585e0f504613b5fbf874f21af14c65260fb41fa',
     start: '2025-01-24',
   },
   [CHAIN.INK]: {
     poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
-    source: 'DUNE',
+    source: 'LOGS',
     positionManager: '0x1b35d13a2e2528f192637f14b05f0dc0e7deb566',
     start: '2025-01-29',
   },
   [CHAIN.SONEIUM]: {
     poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
-    source: 'LOGS',
+    source: 'INDEXER',
     positionManager: '0x1b35d13a2e2528f192637f14b05f0dc0e7deb566',
     start: '2025-01-29',
   },
   [CHAIN.AVAX]: {
     poolManager: '0x06380c0e0912312b5150364b9dc4542ba0dbbc85',
-    source: 'DUNE',
+    source: 'INDEXER',
     positionManager: '0xb74b1f14d2754acfcbbe1a221023a5cf50ab8acd',
     start: '2025-01-24',
   },
   [CHAIN.BSC]: {
     poolManager: '0x28e2ea090877bf75740558f6bfb36a5ffee9e9df',
-    source: 'DUNE',
+    source: 'INDEXER',
     positionManager: '0x7a4a5c919ae2541aed11041a1aeee68f1287f95b',
     start: '2025-01-24',
   },
 }
 
-export const UNISWAP_V4_DUNE_QUERY = (fromTime: number, toTime: number) => {
-  return `
-    WITH transactions AS (
-      SELECT
-        swaps.chain AS chain,
-        pools.currency0 AS token,
-        ABS(swaps.amount0) AS amount,
-        ABS(swaps.amount0) * swaps.fee / 1000000 AS feeAmount
-      FROM uniswap_v4_multichain.poolmanager_evt_swap AS swaps
-      INNER JOIN uniswap_v4_multichain.poolmanager_evt_initialize AS pools
-        ON swaps.chain = pools.chain AND swaps.id = pools.id
-      WHERE
-        swaps.evt_block_time <= from_unixtime(${toTime}) AND swaps.evt_block_time >= from_unixtime(${fromTime})
-        AND (
-          (swaps.chain = 'ethereum' AND swaps.contract_address = 0x000000000004444c5dc75cb358380d2e3de08a90)
-          OR (swaps.chain = 'base' AND swaps.contract_address = 0x498581ff718922c3f8e6a244956af099b2652b2b)
-          OR (swaps.chain = 'unichain' AND swaps.contract_address = 0x1f98400000000000000000000000000000000004)
-          OR (swaps.chain = 'optimism' AND swaps.contract_address = 0x9a13f98cb987694c9f086b1f5eb990eea8264ec3)
-          OR (swaps.chain = 'arbitrum' AND swaps.contract_address = 0x360e68faccca8ca495c1b759fd9eee466db9fb32)
-          OR (swaps.chain = 'polygon' AND swaps.contract_address = 0x67366782805870060151383f4bbff9dab53e5cd6)
-          OR (swaps.chain = 'blast' AND swaps.contract_address = 0x1631559198a9e474033433b2958dabc135ab6446)
-          OR (swaps.chain = 'zora' AND swaps.contract_address = 0x0575338e4c17006ae181b47900a84404247ca30f)
-          OR (swaps.chain = 'worldchain' AND swaps.contract_address = 0xb1860d529182ac3bc1f51fa2abd56662b7d13f33)
-          OR (swaps.chain = 'ink' AND swaps.contract_address = 0x360e68faccca8ca495c1b759fd9eee466db9fb32)
-          OR (swaps.chain = 'avalanche_c' AND swaps.contract_address = 0x06380c0e0912312b5150364b9dc4542ba0dbbc85)
-          OR (swaps.chain = 'bnb' AND swaps.contract_address = 0x28e2ea090877bf75740558f6bfb36a5ffee9e9df)
-        )
-    )
-    SELECT
-      chain,
-      token,
-      SUM(amount) AS totalSwapAmount,
-      SUM(feeAmount) AS totalSwapFee
-    FROM transactions
-    GROUP BY chain, token
-  `;
-}
+// export const UNISWAP_V4_DUNE_QUERY = (fromTime: number, toTime: number) => {
+//   return `
+//     WITH transactions AS (
+//       SELECT
+//         swaps.chain AS chain,
+//         pools.currency0 AS token,
+//         ABS(swaps.amount0) AS amount,
+//         ABS(swaps.amount0) * swaps.fee / 1000000 AS feeAmount
+//       FROM uniswap_v4_multichain.poolmanager_evt_swap AS swaps
+//       INNER JOIN uniswap_v4_multichain.poolmanager_evt_initialize AS pools
+//         ON swaps.chain = pools.chain AND swaps.id = pools.id
+//       WHERE
+//         swaps.evt_block_time <= from_unixtime(${toTime}) AND swaps.evt_block_time >= from_unixtime(${fromTime})
+//         AND (
+//           (swaps.chain = 'ethereum' AND swaps.contract_address = 0x000000000004444c5dc75cb358380d2e3de08a90)
+//           OR (swaps.chain = 'base' AND swaps.contract_address = 0x498581ff718922c3f8e6a244956af099b2652b2b)
+//           OR (swaps.chain = 'unichain' AND swaps.contract_address = 0x1f98400000000000000000000000000000000004)
+//           OR (swaps.chain = 'optimism' AND swaps.contract_address = 0x9a13f98cb987694c9f086b1f5eb990eea8264ec3)
+//           OR (swaps.chain = 'arbitrum' AND swaps.contract_address = 0x360e68faccca8ca495c1b759fd9eee466db9fb32)
+//           OR (swaps.chain = 'polygon' AND swaps.contract_address = 0x67366782805870060151383f4bbff9dab53e5cd6)
+//           OR (swaps.chain = 'blast' AND swaps.contract_address = 0x1631559198a9e474033433b2958dabc135ab6446)
+//           OR (swaps.chain = 'zora' AND swaps.contract_address = 0x0575338e4c17006ae181b47900a84404247ca30f)
+//           OR (swaps.chain = 'worldchain' AND swaps.contract_address = 0xb1860d529182ac3bc1f51fa2abd56662b7d13f33)
+//           OR (swaps.chain = 'ink' AND swaps.contract_address = 0x360e68faccca8ca495c1b759fd9eee466db9fb32)
+//           OR (swaps.chain = 'avalanche_c' AND swaps.contract_address = 0x06380c0e0912312b5150364b9dc4542ba0dbbc85)
+//           OR (swaps.chain = 'bnb' AND swaps.contract_address = 0x28e2ea090877bf75740558f6bfb36a5ffee9e9df)
+//         )
+//     )
+//     SELECT
+//       chain,
+//       token,
+//       SUM(amount) AS totalSwapAmount,
+//       SUM(feeAmount) AS totalSwapFee
+//     FROM transactions
+//     GROUP BY chain, token
+//   `;
+// }
 
-async function prefetchWithDune(options: FetchOptions) {
-  return await queryDune('3996608',{
-    fullQuery: UNISWAP_V4_DUNE_QUERY(options.fromTimestamp, options.toTimestamp),
-  });
-}
+// async function prefetchWithDune(options: FetchOptions) {
+//   return await queryDune('3996608',{
+//     fullQuery: UNISWAP_V4_DUNE_QUERY(options.fromTimestamp, options.toTimestamp),
+//   });
+// }
 
 function getPoolKey(poolId: string): string {
   return poolId.slice(0, 52);
-}
-
-function fromDuneChain(duneChainId: string): string {
-  if (duneChainId === 'bnb') return CHAIN.BSC;
-  if (duneChainId === 'avalanche_c') return CHAIN.AVAX;
-  if (duneChainId === 'worldchain') return CHAIN.WC;
-
-  return duneChainId;
 }
 
 async function fetch(options: FetchOptions) {
   const dailyFees = options.createBalances()
   const dailyVolume = options.createBalances()
 
-  if (Configs[options.chain].source === "LOGS") {
-    const events = await options.getLogs({
-      target: Configs[options.chain].poolManager,
-      eventAbi: 'event Swap(bytes32 indexed id, address indexed sender, int128 amount0, int128 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint24 fee)',
+  const config = Configs[options.chain];
+  if (!config) {
+    throw Error(`config not found for chain ${options.chain}`);
+  }
+
+  let events: Array<any> = [];
+
+  if (config.source === 'LOGS') {
+    events = await options.getLogs({
+      target: config.poolManager,
+      eventAbi: SwapEvent,
     });
+  } else if (config.source === 'INDEXER') {
+    events = (await sdk.indexer.getLogs({
+      chain: options.chain,
+      onlyArgs: true,
+      fromBlock: options.fromApi.block as number,
+      toBlock: options.toApi.block as number,
+      eventAbi: SwapEvent,
+      target: config.poolManager,
+    })).map(item => item);
+  }
 
-    const pools: {[key: string]: IPool | null} = {}
-    for (const event of events) {
-      pools[event.id] = null
-    }
+  const pools: {[key: string]: IPool | null} = {}
+  for (const event of events) {
+    pools[event.id] = null
+  }
 
-    // query pools info
-    const poolIds = Object.keys(pools)
-    const poolKeys = await options.api.multiCall({
-      abi: 'function poolKeys(bytes25) view returns(address currency0, address currency1, uint24 fee, int24 tickSpacing, address hooks)',
-      calls: poolIds.map(poolId => {
-        return {
-          target: Configs[options.chain].positionManager,
-          params: [getPoolKey(poolId)],
+  // query pools info
+  const poolIds = Object.keys(pools)
+  const poolKeys = await options.api.multiCall({
+    abi: FunctionPoolKeys,
+    calls: poolIds.map(poolId => {
+      return {
+        target: config.positionManager,
+        params: [getPoolKey(poolId)],
+      }
+    }),
+    permitFailure: true,
+  })
+
+  for (let i = 0; i < poolIds.length; i++) {
+    if (poolKeys[i]) {
+      if (String(poolKeys[i].currency0) !== NativeCoin || String(poolKeys[i].currency1) !== NativeCoin) {
+        pools[poolIds[i]] = {
+          poolId: poolIds[i],
+          poolKey: getPoolKey(poolIds[i]),
+          currency0: String(poolKeys[i].currency0),
+          currency1: String(poolKeys[i].currency1),
         }
-      })
-    })
-
-    for (let i = 0; i < poolIds.length; i++) {
-      pools[poolIds[i]] = {
-        poolId: poolIds[i],
-        poolKey: getPoolKey(poolIds[i]),
-        currency0: String(poolKeys[i].currency0),
-        currency1: String(poolKeys[i].currency1),
       }
     }
-    
-    for (const event of events) {
-      const poolId = String(event.id)
+  }
+  
+  for (const event of events) {
+    const poolId = String(event.id)
+    if (pools[poolId] as IPool) {
       const token = (pools[poolId] as IPool).currency0
-      if (token === '0x0000000000000000000000000000000000000000') {
+      if (token === NativeCoin) {
         dailyFees.addGasToken(Math.abs(Number(event.amount0)) * (Number(event.fee) / 1e6))
         dailyVolume.addGasToken(Math.abs(Number(event.amount0)))
       } else {
         dailyFees.add(token, Math.abs(Number(event.amount0)) * (Number(event.fee) / 1e6))
         dailyVolume.add(token, Math.abs(Number(event.amount0)))
-      }
-    }
-  } else {
-    const tokens = options.preFetchedResults ? options.preFetchedResults
-      .map((item: any) => {
-        return {
-          ...item,
-          chain: fromDuneChain(item.chain),
-        }
-      })
-      .filter((item: any) => item.chain === options.chain)
-    : [];
-
-    for (const token of tokens) {
-      if (token === '0x0000000000000000000000000000000000000000') {
-        dailyFees.addGasToken(token.totalSwapFee)
-        dailyVolume.addGasToken(token.totalSwapAmount)
-      } else {
-        dailyFees.add(token.token, token.totalSwapFee)
-        dailyVolume.add(token.token, token.totalSwapAmount)
       }
     }
   }
@@ -290,7 +289,7 @@ async function fetch(options: FetchOptions) {
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: {},
-  prefetch: prefetchWithDune,
+  // prefetch: prefetchWithDune,
   isExpensiveAdapter: true,
 };
 
