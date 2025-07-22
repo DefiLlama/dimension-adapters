8e919ea2ea757a973b79746123867f678ffd9534
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-22 11:56:52 +0530

    refactor

--- dexs/renegade-fi/index.ts
+++ dexs/renegade-fi/index.ts
@@ -1,5 +1,5 @@
 import fetchURL from "../../utils/fetchURL";
-import { Chain, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import { Chain, FetchOptions, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 
@@ -15,12 +15,6 @@ interface IApiResponse {
   totalPoints: number;
 }
 
-const META = { 
-  methodology: {
-    dailyVolume: "Volume data is fetched from Renegade.fi's API endpoints for each supported chain.",
-  }
-};
-
 const historicalVolumeEndpoint = (chain: Chain) => {
   const chainId = {
     [CHAIN.ARBITRUM]: "42161",
@@ -30,37 +24,31 @@ const historicalVolumeEndpoint = (chain: Chain) => {
   return `https://trade.renegade.fi/api/stats/historical-volume-kv?chainId=${chainId}`;
 };
 
-const fetchData = (chain: Chain) => {
-  return async (timestamp: number): Promise<FetchResultVolume> => {
-    const startOfDay = getTimestampAtStartOfDayUTC(timestamp);
-    const historicalVolume: IApiResponse = await fetchURL(historicalVolumeEndpoint(chain));
-    let dailyVolume = 0, totalVolume = 0;
-    
-    for (const record of historicalVolume.data) {
-      if (record.timestamp === startOfDay) {
-        dailyVolume = record.volume;
-      }
-      totalVolume += record.volume;
+const fetch = async (timestamp: number, _b:any, options: FetchOptions): Promise<FetchResultVolume> => {
+  const startOfDay = getTimestampAtStartOfDayUTC(timestamp);
+  const historicalVolume: IApiResponse = await fetchURL(historicalVolumeEndpoint(options.chain));
+  let dailyVolume = 0;
+  
+  for (const record of historicalVolume.data) {
+    if (record.timestamp === startOfDay) {
+      dailyVolume = record.volume;
     }
-    
-    return {
-      dailyVolume,
-      totalVolume,
-    };
+  }
+  
+  return {
+    dailyVolume,
   };
 };
 
 const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.ARBITRUM]: {
-      fetch: fetchData(CHAIN.ARBITRUM),
+      fetch,
       start: "2024-09-03",
-      meta: META,
     },
     [CHAIN.BASE]: {
-      fetch: fetchData(CHAIN.BASE),
+      fetch,
       start: "2025-05-29",
-      meta: META,
     },
   },
   version: 1,
