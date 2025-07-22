9710dd31b49f952662acde809f2fdc901e3a8ce7
tree <sol.analyzoor@gmail.com>
2025-07-22 23:45:26 +0530

    Pangolin v3 Fees (#3796)
    * pangolin-v3 dex volume
    
    * pangolin v3 fees

--- dexs/pangolin-v3/index.ts
+++ dexs/pangolin-v3/index.ts
@@ -15,23 +15,47 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
       pangolinDayDatas(orderBy: date, orderDirection: desc, first: 1000) {
         date
         volumeUSD
+        feesUSD
       }
     }
   `
   const response = await request(GRAPH_URL, query)
   const dayData = response.pangolinDayDatas.find((day: any) => day.date === unixTimestamp)
+  const dailyVolume = dayData.volumeUSD
+  const dailyFees = dayData.feesUSD
+  const dailySupplySideRevenue = dailyFees * 0.8
+  const dailyRevenue = dailyFees * 0.2
+  const dailyProtocolRevenue = dailyFees * 0.1
+
 
   return {
-    dailyVolume: dayData.volumeUSD
+    dailyVolume,
+    dailyFees,
+    dailyRevenue,
+    dailyUserFees: dailyFees,
+    dailySupplySideRevenue,
+    dailyProtocolRevenue,
+    dailyHoldersRevenue: dailyProtocolRevenue
   }
 }
 
+const meta = {
+  methodology: {
+    Fees: 'Fees paid by users for swaps',
+    UserFees: 'Fees paid by users for swaps',
+    Revenue: '10% Fees to Pangolin Protocol treasury, 10% to PNG stakers',
+    ProtocolRevenue: '10% Fees to Pangolin Protocol treasury',
+    SupplySideRevenue: '80% Fees to liquidity providers',
+    HoldersRevenue: '10% Fees to PNG stakers'
+  }
+}
 
 const adapter = {
   adapter: {
     [CHAIN.AVAX]: {
       fetch,
-      start: '2025-04-04'
+      start: '2025-04-04',
+      meta
     }
   }
 }
