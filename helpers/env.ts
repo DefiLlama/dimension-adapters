8612dfae4003f8b3b15165af527c1872e8d885a8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-07-23 15:35:27 +0200

    add support for chain: tac

--- helpers/env.ts
+++ helpers/env.ts
@@ -20,6 +20,8 @@ const DEFAULTS: any = {
   CANTO_RPC: 'https://tuber.build/api/eth-rpc',
   APTOS_PRC: 'https://aptos-mainnet.pontem.network',
   HYDRAGON_RPC: "https://rpc-mainnet.hydrachain.org",
+  TAC_RPC: "https://rpc.tac.build",
+  TAC_RPC_MULTICALL: "0xcA11bde05977b3631167028862bE2a173976CA11",
 }
 
 export const ENV_KEYS = new Set([
