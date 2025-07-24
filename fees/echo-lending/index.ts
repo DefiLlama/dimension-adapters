e75e523e6b611657fbcae746abd6f99929f7f57f
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-24 22:25:42 +0530

    refactor

--- fees/echo-lending/index.ts
+++ fees/echo-lending/index.ts
@@ -1,4 +1,4 @@
-import { Adapter, FetchResultV2, FetchV2 } from '../../adapters/types';
+import { Adapter, FetchOptions } from '../../adapters/types';
 import { CHAIN } from '../../helpers/chains';
 import fetchURL from '../../utils/fetchURL';
 
@@ -12,17 +12,10 @@ interface EchoLendingStats {
   }
 }
 
-const methodology = {
-  Fees: 'Interest paid by borrowers',
-  ProtocolRevenue: 'Percentage of interest going to treasury',
-};
-
-const fetchEchoLendingStats: FetchV2 = async ({
-  startTimestamp,
-  endTimestamp,
-}): Promise<FetchResultV2> => {
-  const url = `${echoLendingApiURL}?type=lending&startTimestamp=${startTimestamp}&endTimestamp=${endTimestamp}`;
+const fetch = async (options: FetchOptions) => {
+  const url = `${echoLendingApiURL}?type=lending&startTimestamp=${options.startTimestamp}&endTimestamp=${options.endTimestamp}`;
   const { data }: EchoLendingStats = await fetchURL(url);
+
   return {
     dailyFees: data.total_revenue,
     dailyRevenue: data.echo_revenue,
@@ -31,11 +24,19 @@ const fetchEchoLendingStats: FetchV2 = async ({
   };
 };
 
+const methodology = {
+  Fees: 'Interest paid by borrowers',
+  Revenue: 'Protocol share from the interest paid and fees',
+  ProtocolRevenue: 'Protocol share from the interest paid and fees',
+  SupplySideRevenue: 'Interest paid to lenders',
+};
+
+
 const adapter: Adapter = {
   version: 2,
   adapter: {
     [CHAIN.APTOS]: {
-      fetch: fetchEchoLendingStats,
+      fetch,
       start: '2025-07-23',
       meta: {
         methodology,
