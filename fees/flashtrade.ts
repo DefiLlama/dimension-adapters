752f3744dbcfd0c77a2687ddba1249558e30dffd
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-25 23:10:05 +0530

    FlashTrade: use pool fees API

--- fees/flashtrade.ts
+++ fees/flashtrade.ts
@@ -1,6 +1,7 @@
 import { Adapter, FetchOptions, FetchResultFees } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import fetchURL from "../utils/fetchURL";
+import ADDRESSES from "../helpers/coreAssets.json";
 
 interface Pool {
     poolName: string;
@@ -18,7 +19,7 @@ interface Fees {
 }
 
 const urlRevStats = "https://api.prod.flash.trade/protocol-fees/daily";
-const urlFeeesStats = "https://api.prod.flash.trade/market-stat/revenue-24hr";
+// const urlFeeesStats = "https://api.prod.flash.trade/market-stat/revenue-24hr";
 
 const calculateProtocolRevenue = (stats: Pool[]) => {
     return stats
@@ -26,22 +27,36 @@ const calculateProtocolRevenue = (stats: Pool[]) => {
         .reduce((sum, item) => sum + 0.3 * Number(item.totalRevenue) / 1e6, 0);
 }
 
-const fetch = async (options: FetchOptions): Promise<FetchResultFees> => {
+const pools = [
+    "Crypto.1",
+    "Virtual.1",
+    "Governance.1",
+    "Community.1",
+    "Community.2",
+    "Community.3",
+    "Trump.1",
+]
+
+const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultFees> => {
     const timestamp = options.startOfDay;
+    console.log(timestamp);
+    const targetDate = new Date(timestamp * 1000).toISOString().split('T')[0];
+    console.log(targetDate);
+
+    let dailyFees = 0;
+    for (const pool of pools) {
+        const url = `https://api.prod.flash.trade/pnl-info/cumulative-pnl-per-day?poolName=${pool}&startDate=2023-01-01%2000:00:00&endDate=${targetDate}%2023:59:59`;
+        const res = await fetchURL(url);
+        dailyFees += (res[targetDate]?.totalFees / 1e6) || 0;
+    }
 
     const dailyRevStatsResponse = await fetchURL(urlRevStats);
-    const dailtyFeesStatsResponse = await fetchURL(urlFeeesStats);
     const dailyStats: Pool[] = dailyRevStatsResponse;
-    const dailyFeesStats: Fees[] = dailtyFeesStatsResponse;
-
-    const targetDate = new Date(timestamp * 1000).toISOString().split('T')[0];
 
     const todayStats = dailyStats.filter(item => {
         const itemDate = new Date(item.date).toISOString().split('T')[0];
         return itemDate === targetDate;
     });
-
-    const dailyFees = dailyFeesStats.reduce((sum, item) => sum + parseFloat(item.accured), 0);
 
     // Token stakers revenue is 0 before 2025-06-15
     const dailyRevenue = calculateProtocolRevenue(todayStats);
@@ -68,12 +83,11 @@ const methodology = {
 }
 
 const adapter: Adapter = {
-    version: 2,
+    version: 1,
     adapter: {
         [CHAIN.SOLANA]: {
             fetch,
-            runAtCurrTime: true,
-            start: '2024-03-12',
+            start: '2023-12-29',
             meta: { methodology },
         },
     },
