7d635851931e8d0307b0e087562e8eb01dc5e3cb
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-26 17:32:44 +0530

    refactor aggregator-derivatives

--- aggregator-derivatives/defiapp/index.ts
+++ aggregator-derivatives/defiapp/index.ts
@@ -4,42 +4,20 @@ import { CHAIN } from "../../helpers/chains";
 
 const tsToISO = (ts: number) => new Date(ts * 1e3).toISOString();
 
-interface IDefiAppResponse {
-  startTime: string;
-  endTime: string;
-  totalPerpsVolumeUsd: string;
-}
-
-type IRequest = {
-  [key: string]: Promise<any>;
-};
-const requests: IRequest = {};
-
-export async function fetchCacheURL(url: string) {
-  const key = url;
-  if (!requests[key]) {
-    requests[key] = httpGet(url, {
-      headers: {
-        "Content-Type": "application/json",
-        // DefiLlama team to configure
-        "X-API-KEY": process.env.DEFIAPP_API_KEY,
-        User: "defillama",
-      },
-    });
-  }
-  return requests[key];
-}
-
 const fetch = async (_: any, _b: any, options: any): Promise<FetchResult> => {
-  const { endTimestamp, startTimestamp } = options;
-  const dayResponse = <IDefiAppResponse>(
-    await fetchCacheURL(
-      `https://api.defi.app/api/stats/volume-perps/between?startTime=${tsToISO(
-        startTimestamp
-      )}&endTime=${tsToISO(endTimestamp)}`
-    )
-  );
-  const dailyVolume = dayResponse.totalPerpsVolumeUsd;
+  const startDate = options.startOfDay - (24 * 3600);
+  const endDate = options.startOfDay;
+  console.log(startDate, endDate, options.startOfDay, options.startTimestamp);
+
+  const response = await httpGet(`https://api.defi.app/api/stats/volume-perps/between?startTime=${tsToISO(startDate)}&endTime=${tsToISO(endDate)}`, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": process.env.DEFIAPP_API_KEY,
+      User: "defillama",
+    },
+  });
+  const dailyVolume = response.totalPerpsVolumeUsd;
+
   return {
     dailyVolume,
   };
@@ -52,6 +30,7 @@ const adapter: SimpleAdapter = {
       start: "2025-05-01", // May 1st, 2025
     },
   },
+  doublecounted: true,
 };
 
 export default adapter;
