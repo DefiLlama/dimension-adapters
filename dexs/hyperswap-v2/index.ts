7f5cb871318dd8fee2535e495d1978d3d2ef1363
tree <sol.analyzoor@gmail.com>
2025-07-26 12:16:59 +0530

    use the hyperswap API as volume/fees are lower due to pricing issue in hl (#3835)

--- dexs/hyperswap-v2/index.ts
+++ dexs/hyperswap-v2/index.ts
@@ -1,7 +1,7 @@
-import { FetchOptions, FetchResultV2 } from "../../adapters/types";
+import { FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
 import { httpGet } from "../../utils/fetchURL";
 import { CHAIN } from "../../helpers/chains";
-import { getUniV2LogAdapter } from "../../helpers/uniswap";
+// import { getUniV2LogAdapter } from "../../helpers/uniswap";
 
 interface HyperswapPair {
     version: string;
@@ -14,52 +14,14 @@ interface HyperswapResponse {
     pageCount: number;
 }
 
-const encodeToken = (input: string) => {
-    const keystreamHexAscii = 
-      "cdc49b16573644bbe4cb47c809a6d387" +
-      "873331e665d622b1337a4e19593c5a18" +
-      "cdc49b16573";
-    const encoder = new TextEncoder();
-    const pt = encoder.encode(input);
-    const ks = encoder.encode(keystreamHexAscii);
-    const ct = new Uint8Array(pt.length);
-    for (let i = 0; i < pt.length; i++) {
-      ct[i] = pt[i] ^ ks[i];
-    }
-    let bin = "";
-    for (let b of ct) {
-      bin += String.fromCharCode(b);
-    }
-    return btoa(bin);
-}
-
 const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
-    const clientToken = await httpGet('https://proxy.hyperswapx.workers.dev/get-token')
-    const url = (page: number) =>  `https://proxy.hyperswapx.workers.dev/api/pairs?page=${page}&maxPerPage=50`
+    const url = (page: number) =>  `https://api-partner.hyperswap.exchange/api/pairs?minTvl=50000&maxPerPage=50&page=${page}`
+    const headers = { 'x-internal-access': 1 }
     let page = 0;
     const data: HyperswapPair[] = []
     while(true) {
-        const res = (await httpGet(url(page), {
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json,text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Cache-Control": "no-cache",
-                "Sec-Ch-Ua": '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
-                "Sec-Ch-Ua-Mobile": "?0",
-                "Sec-Ch-Ua-Platform": '"macOS"',
-                "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-origin",
-                "x-client-token": `${encodeToken(clientToken)}`,
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-                "Origin": "https://hyperswap.exchange",
-                "Referer": "https://hyperswap.exchange/"
-            }
-        })) as { data: HyperswapResponse };
+        const res = (await httpGet(url(page), { headers })) as { data: HyperswapResponse };
         page++
-
         if (res.data.pageCount < page) break
 
         data.push(...res.data.pairs)   
@@ -67,6 +29,7 @@ const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
 
     const dailyVolume = options.createBalances()
     const dailyFees = options.createBalances()
+
     data.filter((e) => e.version === "v2").forEach((pair) => {
         if (pair.h24) {
             dailyVolume.addUSDValue(Number(pair.h24))
@@ -74,47 +37,68 @@ const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
         }
     })
 
+    const dailyRevenue = dailyFees.clone(0.4)
+    const dailyProtocolRevenue = dailyFees.clone(0.08)
+    const dailySupplySideRevenue = dailyFees.clone(0.6)
+    const dailyHoldersRevenue = dailyFees.clone(0.32)
+
     return {
         dailyVolume,
         dailyFees,
+        dailyRevenue,
+        dailyProtocolRevenue,
+        dailySupplySideRevenue,
+        dailyHoldersRevenue,
+        dailyUserFees: dailyFees,
     }
 }
 
-// const adapter: SimpleAdapter = {
-//     version: 2,
-//     adapter: {
-//         [CHAIN.HYPERLIQUID]: {
-//             fetch,
-//             runAtCurrTime: true,
-//         }
-//     }
-// }
+const methodology = {
+    Fees: "Total swap fees paided by users.",
+    Revenue: "Revenue collected from 40% swap fees.",
+    ProtocolRevenue: "Revenue for HyperSwap from 8% swap fees.",
+    SupplySideRevenue: "Amount of 60% swap fees distributed to LPs.",
+    HoldersRevenue: "Amount of 32% swap fees distributed to Swap stakers and buy-back and burn.",
+    UserFees: "Total swap fees paided by users."
+}
 
-// export default adapter
-
-export default {
+const adapter: SimpleAdapter = {
     version: 2,
     adapter: {
         [CHAIN.HYPERLIQUID]: {
-            fetch: getUniV2LogAdapter({
-                factory: '0x724412C00059bf7d6ee7d4a1d0D5cd4de3ea1C48',
-
-                // https://docs.hyperswap.exchange/hyperswap/token-design/or-protocol-earnings
-                userFeesRatio: 1,
-                revenueRatio: 0.4, // 40% swap fees
-                protocolRevenueRatio: 0.08, // 8% swap fees
-                holdersRevenueRatio: 0.32, // 32% swap fees
-            }),
-            meta: {
-                methodology: {
-                    Fees: "Total swap fees paided by users.",
-                    Revenue: "Revenue collected from 40% swap fees.",
-                    ProtocolRevenue: "Revenue for HyperSwap from 8% swap fees.",
-                    SupplySideRevenue: "Amount of 60% swap fees distributed to LPs.",
-                    HoldersRevenue: "Amount of 32% swap fees distributed to Swap stakers and buy-back and burn.",
-                    UserFees: "Total swap fees paided by users."
-                }
-            }
+            fetch,
+            runAtCurrTime: true,
+            start: '2025-02-18',
+            meta: { methodology }
         }
     }
 }
+
+export default adapter
+
+// export default {
+//     version: 2,
+//     adapter: {
+//         [CHAIN.HYPERLIQUID]: {
+//             fetch: getUniV2LogAdapter({
+//                 factory: '0x724412C00059bf7d6ee7d4a1d0D5cd4de3ea1C48',
+
+//                 // https://docs.hyperswap.exchange/hyperswap/token-design/or-protocol-earnings
+//                 userFeesRatio: 1,
+//                 revenueRatio: 0.4, // 40% swap fees
+//                 protocolRevenueRatio: 0.08, // 8% swap fees
+//                 holdersRevenueRatio: 0.32, // 32% swap fees
+//             }),
+//             meta: {
+//                 methodology: {
+//                     Fees: "Total swap fees paided by users.",
+//                     Revenue: "Revenue collected from 40% swap fees.",
+//                     ProtocolRevenue: "Revenue for HyperSwap from 8% swap fees.",
+//                     SupplySideRevenue: "Amount of 60% swap fees distributed to LPs.",
+//                     HoldersRevenue: "Amount of 32% swap fees distributed to Swap stakers and buy-back and burn.",
+//                     UserFees: "Total swap fees paided by users."
+//                 }
+//             }
+//         }
+//     }
+// }
