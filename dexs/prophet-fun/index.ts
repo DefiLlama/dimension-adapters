03855fa4c8dc0fead20b4ef9a7c1b1d2764cd636
MrJeleika <den932005@gmail.com>
2025-07-28 16:30:28 +0300

    add prophet.fun

---
+++ dexs/prophet-fun/index.ts
@@ -0,0 +1,27 @@
+import fetchURL from "../../utils/fetchURL"
+import { type SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+
+const URL = "https://backend.prophet.fun/business-metrics/daily-volume"
+
+
+const fetch = async (timestamp: any) => {
+  const data = await fetchURL(URL + "?timestamp=" + timestamp.startOfDay);
+
+  return {
+    dailyVolume: data.dailyVolume,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  methodology: 'Volume of all trades that that go through the protocol',
+  adapter: {
+    [CHAIN.SOLANA]: {
+      fetch,
+      start: '2025-07-24'
+    },
+  }
+};
+
+export default adapter;
