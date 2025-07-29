ad0ac372f71f4a11b95a235d926e9363838c2ee5
treeoflife2 <sol.analyzoor@gmail.com>
2025-07-29 23:06:37 +0530

    refactor

--- dexs/betterswap/index.ts
+++ dexs/betterswap/index.ts
@@ -1,42 +1,30 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { httpGet } from "../../utils/fetchURL";
+import fetchURL from "../../utils/fetchURL";
 
 interface BetterSwapResponse {
-    volumeVET: string;
     volumeUSD: string;
-    totalTransactions: number;
-    vetPriceUSD: string;
-    startDate: string;
-    endDate: string;
-    factoryAddress: string;
 }
 
 // BetterSwap factoryAddress
 const factoryAddress = "0x5970dcbebac33e75eff315c675f1d2654f7bf1f5";
 
-const fetch = async (options: FetchOptions) => {
-    const startDate = new Date(options.startTimestamp * 1000)
-        .toISOString()
-        .split("T")[0];
+const fetch = async (_a:any, _b:any, options: FetchOptions) => {
+    const startDate = new Date(options.startOfDay * 1000).toISOString().split("T")[0];
     const url = `https://www.betterswap.io/api/volume?startDate=${startDate}&factoryAddress=${factoryAddress}`;
 
-    const response: BetterSwapResponse = await httpGet(url);
+    const response: BetterSwapResponse = await fetchURL(url);
 
     return {
         dailyVolume: parseFloat(response.volumeUSD),
-        timestamp: options.startTimestamp,
     };
 };
 
 const adapter: SimpleAdapter = {
-    version: 2,
-    adapter: {
-        [CHAIN.VECHAIN]: {
-            fetch,
-            start: 1745971200, // 2025-04-30
-        },
-    },
+    version: 1,
+    fetch,
+    chains: [CHAIN.VECHAIN],
+    start: '2025-04-30',
 };
 
 export default adapter;
