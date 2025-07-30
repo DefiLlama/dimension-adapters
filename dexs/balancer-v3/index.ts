e71a6353dd8e117f7a27e94e1c8469cdcf36fb37
mendesfabio <ms.fabiomendes@gmail.com>
2025-07-30 15:27:45 -0300

    expand balancer v3

--- dexs/balancer-v3/index.ts
+++ dexs/balancer-v3/index.ts
@@ -3,9 +3,14 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const v3ChainMapping: any = {
-  [CHAIN.ETHEREUM]: 'MAINNET',
-  [CHAIN.XDAI]: 'GNOSIS',
-}
+  [CHAIN.ETHEREUM]: "MAINNET",
+  [CHAIN.XDAI]: "GNOSIS",
+  [CHAIN.ARBITRUM]: "ARBITRUM",
+  [CHAIN.OPTIMISM]: "OPTIMISM",
+  [CHAIN.AVAX]: "AVALANCHE",
+  [CHAIN.BASE]: "BASE",
+  [CHAIN.HYPERLIQUID]: "HYPERLIQUID",
+};
 
 const adapter: SimpleAdapter = {
   version: 2,
@@ -13,18 +18,19 @@ const adapter: SimpleAdapter = {
   runAtCurrTime: true,
   chains: Object.keys(v3ChainMapping),
   methodology: {
-    Volume: 'Total volume is the sum of all trades in the last 24 hours.',
-    Fees: 'Total fees earned from all the trades and yield in the last 24 hours.',
-    Revenue: 'Total revenue earned by the protocol in the last 24 hours, which is 50% of the trade fees and 10% of the yield capture.',
-  }
+    Volume: "Total volume is the sum of all trades in the last 24 hours.",
+    Fees: "Total fees earned from all the trades and yield in the last 24 hours.",
+    Revenue:
+      "Total revenue earned by the protocol in the last 24 hours, which is 50% of the trade fees and 10% of the yield capture.",
+  },
 };
 
 // chains = ["MAINNET", "ARBITRUM", "AVALANCHE", "BASE", "GNOSIS", "POLYGON", "ZKEVM", "OPTIMISM", "MODE", "FRAXTAL"]
 
 async function fetch({ createBalances, chain }: FetchOptions) {
-  const dailyVolume = createBalances()
-  const dailyFees = createBalances()
-  const dailyRevenue = createBalances()
+  const dailyVolume = createBalances();
+  const dailyFees = createBalances();
+  const dailyRevenue = createBalances();
   const query = `query {
   pools: poolGetPools(
     orderBy: volume24h
@@ -46,17 +52,16 @@ async function fetch({ createBalances, chain }: FetchOptions) {
       yieldCapture24h
     }
   }
-}`
-  const { pools } = await request('https://api-v3.balancer.fi/graphql', query);
+}`;
+  const { pools } = await request("https://api-v3.balancer.fi/graphql", query);
   pools.forEach((pool: any) => {
-    dailyFees.addUSDValue(+pool.dynamicData.fees24h)
-    dailyFees.addUSDValue(+pool.dynamicData.yieldCapture24h)
-    dailyVolume.addUSDValue(+pool.dynamicData.volume24h)
-    dailyRevenue.addUSDValue(+(pool.dynamicData.fees24h * 0.5)) // 50% of fees go to the protocol
-    dailyRevenue.addUSDValue(+(pool.dynamicData.yieldCapture24h * 0.1)) // 10% of yield capture goes to the protocol
-  })
-  return { dailyFees, dailyVolume, dailyRevenue }
+    dailyFees.addUSDValue(+pool.dynamicData.fees24h);
+    dailyFees.addUSDValue(+pool.dynamicData.yieldCapture24h);
+    dailyVolume.addUSDValue(+pool.dynamicData.volume24h);
+    dailyRevenue.addUSDValue(+(pool.dynamicData.fees24h * 0.5)); // 50% of fees go to the protocol
+    dailyRevenue.addUSDValue(+(pool.dynamicData.yieldCapture24h * 0.1)); // 10% of yield capture goes to the protocol
+  });
+  return { dailyFees, dailyVolume, dailyRevenue };
 }
-
 
 export default adapter;
