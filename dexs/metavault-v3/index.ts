c3269afbd8f7181be443042d5098382032034122
tree <sol.analyzoor@gmail.com>
2025-07-30 22:07:45 +0530

    Fix subgraph errors (#3884)

--- dexs/metavault-v3/index.ts
+++ dexs/metavault-v3/index.ts
@@ -1,7 +1,6 @@
-import { Chain } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getGraphDimensions2 } from "../../helpers/getUniSubgraph";
-import { BreakdownAdapter, SimpleAdapter } from "../../adapters/types";
+import { SimpleAdapter } from "../../adapters/types";
 
 const endpointsV3 = {
   // [CHAIN.LINEA]: "https://linea-graph-node.metavault.trade/subgraphs/name/metavault/v3",
