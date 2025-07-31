90ec6f65d35f9b535793a11d6b208714d5ee3fa2
MoonlanderDev <dev@moonlander.trade>
2025-07-31 16:43:28 +0800

    feat: adding fees daily api and data fetching

--- dexs/moonlander/index.ts
+++ dexs/moonlander/index.ts
@@ -2,15 +2,6 @@ import { httpGet } from "../../utils/fetchURL";
 import { FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const pairsAbi =
-  "function pairsV4() view returns ((string name, address base, uint16 basePosition, uint8 pairType, uint8 status, uint256 maxLongOiUsd, uint256 maxShortOiUsd, uint256 fundingFeePerSecondP, uint256 minFundingFeeR, uint256 maxFundingFeeR, (uint256 notionalUsd, uint16 tier, uint16 maxLeverage, uint16 initialLostP, uint16 liqLostP)[] leverageMargins, uint16 slippageConfigIndex, uint16 slippagePosition, (string name, uint256 onePercentDepthAboveUsd, uint256 onePercentDepthBelowUsd, uint16 slippageLongP, uint16 slippageShortP, uint16 index, uint8 slippageType, bool enable, uint256 longThresholdUsd, uint256 shortThresholdUsd) slippageConfig, uint16 feeConfigIndex, uint16 feePosition, (string name, uint16 index, uint16 openFeeP, uint16 closeFeeP, bool enable, uint24 shareP, uint24 minCloseFeeP) feeConfig, uint40 longHoldingFeeRate, uint40 shortHoldingFeeRate)[])";
-const marketInfoAbi =
-  "function getMarketInfos(address[] pairBases) view returns ((address pairBase, uint256 longQty, uint256 shortQty, uint128 lpLongAvgPrice, uint128 lpShortAvgPrice, int256 fundingFeeRate)[])";
-const priceAbi = "function getPrice(address token) view returns (uint256)";
-
-const dailyEndpoint =
-  "https://api.moonlander.trade/v1/trading-volumes/sum-by-date";
-
 const chains: { [key: string]: string } = {
   [CHAIN.CRONOS]: "CRONOS",
   [CHAIN.CRONOS_ZKEVM]: "CRONOS_ZKEVM",
@@ -26,10 +17,26 @@ const startTimestamps: { [chain: string]: string } = {
   [CHAIN.CRONOS_ZKEVM]: "2024-12-17",
 };
 
+const pairsAbi =
+  "function pairsV4() view returns ((string name, address base, uint16 basePosition, uint8 pairType, uint8 status, uint256 maxLongOiUsd, uint256 maxShortOiUsd, uint256 fundingFeePerSecondP, uint256 minFundingFeeR, uint256 maxFundingFeeR, (uint256 notionalUsd, uint16 tier, uint16 maxLeverage, uint16 initialLostP, uint16 liqLostP)[] leverageMargins, uint16 slippageConfigIndex, uint16 slippagePosition, (string name, uint256 onePercentDepthAboveUsd, uint256 onePercentDepthBelowUsd, uint16 slippageLongP, uint16 slippageShortP, uint16 index, uint8 slippageType, bool enable, uint256 longThresholdUsd, uint256 shortThresholdUsd) slippageConfig, uint16 feeConfigIndex, uint16 feePosition, (string name, uint16 index, uint16 openFeeP, uint16 closeFeeP, bool enable, uint24 shareP, uint24 minCloseFeeP) feeConfig, uint40 longHoldingFeeRate, uint40 shortHoldingFeeRate)[])";
+const marketInfoAbi =
+  "function getMarketInfos(address[] pairBases) view returns ((address pairBase, uint256 longQty, uint256 shortQty, uint128 lpLongAvgPrice, uint128 lpShortAvgPrice, int256 fundingFeeRate)[])";
+const priceAbi = "function getPrice(address token) view returns (uint256)";
+
+const dailyEndpoint =
+  "https://api.moonlander.trade/v1/trading-volumes/sum-by-date";
+const feesEndPoint = "https://api.moonlander.trade/v1/defillama/fee";
+
 const getDailyUri = ({ chain, startTime, endTime }: any) => {
   return `${dailyEndpoint}?chains=${
     chains[chain]
   }&startTime=${startTime.toISOString()}&endTime=${endTime.toISOString()}`;
+};
+
+const getFeesUri = ({ chain, startTime, endTime }: any) => {
+  return `${feesEndPoint}?block_chain=${
+    chains[chain]
+  }&startDate=${startTime.toISOString()}&endDate=${endTime.toISOString()}`;
 };
 
 const getOpenInterest = async ({
@@ -78,9 +85,13 @@ const getOpenInterest = async ({
   };
 };
 
-interface APIResponse {
+interface DailyDateAPIResponse {
   vol: string;
   usdVol: string;
+}
+
+interface FeesAPIResponse {
+  dailyFeeAmountUsd: number;
 }
 
 async function fetch({
@@ -89,8 +100,16 @@ async function fetch({
   chain,
   api,
 }: FetchOptions) {
-  const dailyData: APIResponse = await httpGet(
+  const dailyData: DailyDateAPIResponse = await httpGet(
     getDailyUri({
+      chain,
+      startTime: new Date(startTimestamp * 1000),
+      endTime: new Date(endTimestamp * 1000),
+    })
+  );
+
+  const dailyFeesData: FeesAPIResponse = await httpGet(
+    getFeesUri({
       chain,
       startTime: new Date(startTimestamp * 1000),
       endTime: new Date(endTimestamp * 1000),
@@ -105,6 +124,7 @@ async function fetch({
     shortOpenInterestAtEnd,
     longOpenInterestAtEnd,
     openInterestAtEnd,
+    dailyFees: dailyFeesData.dailyFeeAmountUsd,
   };
 }
 
