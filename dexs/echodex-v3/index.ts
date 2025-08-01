a3297d5da4e221f25aa1eefdb821ae31d35a8558
Eden <noat.eth@gmail.com>
2025-08-01 17:56:17 +0700

    fix ampleswap, echodex, spartacus-exchange

--- dexs/echodex-v3/index.ts
+++ dexs/echodex-v3/index.ts
@@ -1,21 +1,14 @@
 import { CHAIN } from "../../helpers/chains";
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 import { SimpleAdapter } from "../../adapters/types";
+import { getUniV3LogAdapter } from "../../helpers/uniswap";
 
-const fetch = univ2Adapter({
-  endpoints: {
-    [CHAIN.LINEA]: "https://graph-query.linea.build/subgraphs/name/echodex/exchange-v3"
-  },
-  factoriesName: "factories",
-  dayData: "echodexDayData",
-  dailyVolume: "volumeUSD"
-});
+const swapEvent = 'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint128 protocolFeesToken0, uint128 protocolFeesToken1)'
 
 const adapter: SimpleAdapter = {
-  version: 1,
-  adapter: {
-    [CHAIN.LINEA]: { fetch, start: 1691107200 },
-  },
+  version: 2,
+  fetch: getUniV3LogAdapter({ factory: '0x559Fa53Be355835a038aC303A750E8788668636B', swapEvent }),
+  chains: [CHAIN.LINEA],
+  start: '2023-04-09',
 }
 
 export default adapter;
