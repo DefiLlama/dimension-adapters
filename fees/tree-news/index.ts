bf92dd96e2226576c6e1cacc129f3d23460c5bfd
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-01 21:22:07 +0530

    replace solana token mint with cg token, as lack of liquidity pools, add 0 protocol revenue, refactor

--- fees/tree-news/index.ts
+++ fees/tree-news/index.ts
@@ -7,41 +7,35 @@ interface IData {
   burned_tokens: string;
 }
 
-const fetchEVM = async (options: FetchOptions) => {
-  // Determine token address based on chain
+const fetch = async (options: FetchOptions) => {
+  const tokenAddress = options.chain === CHAIN.BASE
+    ? '0x52c2b317eb0bb61e650683d2f287f56c413e4cf6'
+    : '0xba25b2281214300e4e649fead9a6d6acd25f1c0a';
 
-  const tokenAddress = options.chain === CHAIN.BASE 
-    ? '0x52c2b317eb0bb61e650683d2f287f56c413e4cf6'  // Base token
-    : '0xba25b2281214300e4e649fead9a6d6acd25f1c0a'; // Ethereum token
-    console.log(options.chain, CHAIN.BASE, tokenAddress);
   const data: IData[] = await queryDuneSql(options, `
     SELECT 
       CAST(SUM(value) AS VARCHAR) AS burned_tokens
     FROM erc20_${options.chain}.evt_Transfer
     WHERE contract_address = ${tokenAddress}
-      AND to = 0x000000000000000000000000000000000000dEaD  -- Dead address
+      AND to = 0x000000000000000000000000000000000000dEaD
       AND evt_block_time >= FROM_UNIXTIME(${options.startTimestamp})
       AND evt_block_time < FROM_UNIXTIME(${options.endTimestamp})
       AND value <= CAST(100000 * POWER(10, 18) AS UINT256)  -- 100k cap to remove treasury burns
   `);
 
   const dailyFees = options.createBalances();
-  const dailyRevenue = options.createBalances();
-  const dailyHoldersRevenue = options.createBalances();
 
-  // Process the results
   if (data && data.length > 0 && data[0].burned_tokens !== '0') {
     const burnedAmount = data[0].burned_tokens;
-    
     dailyFees.add(tokenAddress, burnedAmount);
-    dailyRevenue.add(tokenAddress, burnedAmount);
-    dailyHoldersRevenue.add(tokenAddress, burnedAmount);
   }
 
   return {
     dailyFees,
-    dailyRevenue,
-    dailyHoldersRevenue,
+    dailyUserFees: dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: '0',
+    dailyHoldersRevenue: dailyFees,
   };
 };
 
@@ -57,60 +51,37 @@ const fetchSolana = async (options: FetchOptions) => {
   `);
 
   const dailyFees = options.createBalances();
-  const dailyRevenue = options.createBalances();
-  const dailyHoldersRevenue = options.createBalances();
-  // Process the results
+
   if (data && data.length > 0 && data[0].burned_tokens !== '0') {
     const burnedAmount = data[0].burned_tokens;
-    
-    dailyFees.add('9gaCDFUN1Kvz1YfocbcowtzZq8PCebdfgT5AmJz5yEVY', burnedAmount);
-    dailyRevenue.add('9gaCDFUN1Kvz1YfocbcowtzZq8PCebdfgT5AmJz5yEVY', burnedAmount);
-    dailyHoldersRevenue.add('9gaCDFUN1Kvz1YfocbcowtzZq8PCebdfgT5AmJz5yEVY', burnedAmount);
+    dailyFees.addCGToken('tree-capital', Number(burnedAmount) / 1e8);
   }
+
   return {
     dailyFees,
-    dailyRevenue,
-    dailyHoldersRevenue,
+    dailyUserFees: dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: '0',
+    dailyHoldersRevenue: dailyFees,
   };
 };
 
+const methodology = {
+  Fees: "All funds spent by users to subscribe. Includes direct token burns, and USDC used to buy and burn.",
+  UserFees: "All funds spent by users to subscribe. Includes direct token burns, and USDC used to buy and burn.",
+  Revenue: "All tokens burned by users for subscriptions.",
+  ProtocolRevenue: "Treasury doesn't earn any revenue as everything is burned.",
+  HoldersRevenue: "100% of subscriptions are burned, benefitting all holders of the token.",
+}
+
 const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.ETHEREUM]: {
-      fetch: fetchEVM,
-      start: '2024-11-01',
-      meta: {
-        methodology: {
-          Fees: "All funds spent by users to subscribe. Includes direct token burns, and USDC used to buy and burn.",
-          Revenue: "All tokens burned by users for subscriptions.",
-          HoldersRevenue: "100% of subscriptions are burned, benefitting all holders of the token.",
-        }
-      }
-    },
-    [CHAIN.BASE]: {
-      fetch: fetchEVM,
-      start: '2024-11-01',
-      meta: {
-        methodology: {
-          Fees: "All funds spent by users to subscribe. Includes direct token burns, and USDC used to buy and burn.",
-          Revenue: "All tokens burned by users for subscriptions.",
-          HoldersRevenue: "100% of subscriptions are burned, benefitting all holders of the token.",
-        }
-      }
-    },
-    [CHAIN.SOLANA]: {
-      fetch: fetchSolana,
-      start: '2025-07-31',
-      meta: {
-        methodology: {
-          Fees: "All funds spent by users to subscribe. Includes direct token burns, and USDC used to buy and burn.",
-          Revenue: "All tokens burned by users for subscriptions.",
-          HoldersRevenue: "100% of subscriptions are burned, benefitting all holders of the token.",
-        }
-      }
-    }
-  },
   version: 2,
+  adapter: {
+    [CHAIN.ETHEREUM]: {fetch, start: '2024-11-01'},
+    [CHAIN.BASE]: { fetch, start: '2024-11-01' },
+    [CHAIN.SOLANA]: { fetch: fetchSolana, start: '2025-07-30' }
+  },
+  methodology,
   isExpensiveAdapter: true
 };
 
