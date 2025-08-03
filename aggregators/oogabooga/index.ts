108e326ceb6295ab5e7a14e9b93e028516355a11
Bruno Wu <wubruno@outlook.com>
2025-08-03 15:21:27 +0200

    feat: add hyperevm ooga contracts

--- aggregators/oogabooga/index.ts
+++ aggregators/oogabooga/index.ts
@@ -7,49 +7,65 @@ const swapEvent =
 const quoteEvent =
 	"event Fee(address feeToken, uint256 feeAmount, uint256 obQuote, uint256 bexQuote, uint256 kodiakQuote)";
 
-const OBEXECUTOR_FEE_MODULE = "0xF83ECD5511cf190764Be32D2F9eCeD57a8676cdc";
-const OBROUTER = "0xFd88aD4849BA0F729D6fF4bC27Ff948Ab1Ac3dE7";
+const OBEXECUTOR_FEE_MODULE = {
+	[CHAIN.BERACHAIN]: [
+		"0xF83ECD5511cf190764Be32D2F9eCeD57a8676cdc",
+		"0xCE33Ec5E1BA85EB9485dDB2DF3610186cA3b6a35",
+	],
+	[CHAIN.HYPERLIQUID]: ["0x53A8EC5a42106FC8B2AB1468c3DA363F1bA49266"],
+} as const;
 
-async function fetch({ getLogs, createBalances }: FetchOptions) {
-	const dailyVolume = createBalances();
-	const dailyFees = createBalances();
+const OBROUTER = {
+	[CHAIN.BERACHAIN]: "0xFd88aD4849BA0F729D6fF4bC27Ff948Ab1Ac3dE7",
+	[CHAIN.HYPERLIQUID]: "0x5fbD1B5AA82d09359C05428647871fe9aDd3F411",
+} as const;
 
-	const swapEvents = await getLogs({
-		targets: [OBROUTER],
-		eventAbi: swapEvent,
-	});
-	for (const l of swapEvents) {
-		dailyVolume.add(l.outputToken, l.amountOut);
-	}
+const fetch =
+	(chainId: CHAIN) =>
+	async ({ getLogs, createBalances }: FetchOptions) => {
+		const dailyVolume = createBalances();
+		const dailyFees = createBalances();
 
-	// Positive slippage
-	for (const l of swapEvents) {
-		if (l.slippage > 0n) {
-			dailyFees.add(l.outputToken, l.slippage);
+		const swapEvents = await getLogs({
+			targets: [OBROUTER[chainId]],
+			eventAbi: swapEvent,
+		});
+		for (const l of swapEvents) {
+			dailyVolume.add(l.outputToken, l.amountOut);
 		}
-	}
 
-	// Swap fees
-	const feeEvents = await getLogs({
-		targets: [OBEXECUTOR_FEE_MODULE],
-		eventAbi: quoteEvent,
-	});
-
-	for (const l of feeEvents) {
-		if (l.feeAmount > 0n) {
-			dailyFees.add(l.feeToken, l.feeAmount);
+		// Positive slippage
+		for (const l of swapEvents) {
+			if (l.slippage > 0n) {
+				dailyFees.add(l.outputToken, l.slippage);
+			}
 		}
-	}
 
-	return { dailyVolume, dailyRevenue: dailyFees };
-}
+		// Swap fees
+		const feeEvents = await getLogs({
+			targets: OBEXECUTOR_FEE_MODULE[chainId],
+			eventAbi: quoteEvent,
+		});
+
+		for (const l of feeEvents) {
+			if (l.feeAmount > 0n) {
+				dailyFees.add(l.feeToken, l.feeAmount);
+			}
+		}
+
+		return { dailyVolume, dailyRevenue: dailyFees };
+	};
 
 export default {
 	version: 2,
 	adapter: {
 		[CHAIN.BERACHAIN]: {
-			fetch: fetch,
+			fetch: fetch(CHAIN.BERACHAIN),
 			start: 324008,
+		},
+		[CHAIN.HYPERLIQUID]: {
+			fetch: fetch(CHAIN.HYPERLIQUID),
+			start: 9974083,
 		},
 	},
 };
