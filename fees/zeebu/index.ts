4bfbb406ebb7febe77c50499f309098ad92e9fd3
dsarvaiya <114975126+dsarvaiya@users.noreply.github.com>
2025-08-05 11:51:54 +0530

    Update index.ts
    added dailyProtocolRevenue and update methodology.

--- fees/zeebu/index.ts
+++ fees/zeebu/index.ts
@@ -1,6 +1,12 @@
+import * as sdk from "@defillama/sdk";
 import { request, gql } from "graphql-request";
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { Adapter, FetchV2, ChainEndpoints } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+//import { getTokenPrice } from "../../helpers/prices"; // Fetch Zeebu price in USD
+import { getTimestampAtStartOfDayUTC } from "../../utils/date";
+import BigNumber from "bignumber.js";
+//import { ETHEREUM } from "../../helpers/chains";
+//import { Chain } from '@defillama/sdk/build/general';
 
 // Define target contracts and chains
 const CONTRACTS = {
@@ -19,55 +25,116 @@ const endpoints = {
   [CHAIN.BASE]: 'https://api.studio.thegraph.com/query/89152/fees_reward_base/version/latest',
 }
 
-interface GraphResponse {
-  dayVolumeFeesAggregates: {
-    contract: string;
-    dailyFees: string;
-    dailyVolume: string;
-    dayID: string;
-  }[];
-}
-const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-  const dayID = (Math.floor(options.startOfDay / 86400) ).toString();
-  const graphQuery = gql`
-    query ($dayID: String!) {
-      dayVolumeFeesAggregates(
-        orderBy: dayID
-        orderDirection: desc
-        where: { dayID: $dayID }
-      ) {
-        contract
-        dailyFees
-        dailyVolume
-        dayID
+const graphsDaily = (graphUrls: Record<string, string>) => {
+  return (chain: CHAIN) => {
+    return async (timestamp: number) => {
+      const dayID = (Math.floor(timestamp.startOfDay / 86400) ).toString(); // Ensure this aligns with your subgraph's dayID logic
+      //console.log("dayID",dayID, chain);
+      const graphQuery = gql`
+        query ($dayID: String!) {
+          dayVolumeFeesAggregates(
+            orderBy: dayID
+            orderDirection: desc
+            where: { dayID: $dayID }
+          ) {
+            contract
+            dailyFees
+            dailyVolume
+            dayID
+          }
+        }
+      `;
+
+      const totalFeesQuery = gql`
+        query {
+          overallVolumeFeesAggregates{
+            totalFees
+            totalVolume
+            chain
+            contract
+          }
+        }
+      `;
+
+      try {
+        // Fetch total fees
+        const totalFeesResponse = await request(graphUrls[chain], totalFeesQuery, { });
+        //console.log("totalFeesResponse",totalFeesResponse);
+        const totalFees = totalFeesResponse.overallVolumeFeesAggregates.reduce(
+          (sum, item) => sum + parseFloat(((item.totalFees * 2)/1e18) || 0),
+          0
+        );
+        //console.log("totalFees",totalFees);
+
+        // Fetch daily fees
+        const graphRes = await request(graphUrls[chain], graphQuery, { dayID: dayID });
+        //console.log("graphRes",graphRes);
+        const aggregates = graphRes.dayVolumeFeesAggregates;
+        //console.log("aggregates",aggregates);
+
+        // Aggregate daily fees and daily volume
+        const dailyFees = aggregates.reduce((sum, agg) => sum + parseFloat(((agg.dailyFees * 2)/1e18) || 0), 0);
+        //console.log("dailyFees",dailyFees);
+        const dailyUserFees = dailyFees;
+        const totalUserFees = totalFees;
+
+        const dailyRevenue = dailyFees;
+        const totalRevenue = totalFees;
+
+        const dailyHoldersRevenue = dailyFees * 0.6 / 100;
+        const totalHoldersRevenue = totalFees * 0.6 / 100;
+
+        const dailySupplySideRevenue = dailyFees * 0.6 / 100;
+        const totalSupplySideRevenue = totalFees * 0.6 / 100;
+
+        const dailyProtocolRevenue = dailyFees - dailyHoldersRevenue;
+
+        return {dailyFees, totalFees, dailyUserFees, totalUserFees, dailyRevenue, totalRevenue, dailyHoldersRevenue, totalHoldersRevenue, dailyProtocolRevenue };
+      } catch (error) {
+        console.error(`Error fetching data for chain ${chain}:`, error.message);
+        return {
+          dailyFees: 0,
+          totalFees: 0,
+          dailyUserFees : 0, 
+          totalUserFees : 0, 
+          dailyRevenue : 0, 
+          totalRevenue : 0, 
+          dailyHoldersRevenue : 0, 
+          totalHoldersRevenue : 0,
+          dailyProtocolRevenue : 0
+        };
       }
-    }
-  `;
-  const url = endpoints[options.chain];
-  const graphRes: GraphResponse = await request(url, graphQuery, { dayID: dayID });
-  const aggregates = graphRes.dayVolumeFeesAggregates;
-
-  const dailyFees = aggregates.reduce((sum, agg) => {
-    const fee = agg.dailyFees ? (Number(agg.dailyFees) * 2) / 1e18 : 0;
-    return sum + fee;
-  }, 0);
-  const dailyHoldersRevenue = dailyFees * 0.6 / 100;
-
-  return { dailyFees, dailyUserFees: dailyFees, dailyHoldersRevenue };
+    };
+  };
 };
 
-const methodology = {
-  Fees: "2% collectively paid by merchant and customer",
-  Revenue: "Invoice fees",
-  HoldersRevenue: "Staking rewards earned by veZBU holders, 0.6% of collected fees "
-}
-
-const adapter: SimpleAdapter = {
-  methodology,
+export default {
   adapter: {
-    [CHAIN.BASE]: { fetch, start: 1728518400 },
-    [CHAIN.BSC]: { fetch, start: 1688083200 },
-  },
-}
+    // Define for each chain
+    [CHAIN.BASE]: {
+      fetch : graphsDaily(endpoints)(CHAIN.BASE),
+      start: 1728518400,
+      meta: {
+        methodology: {
+          Fees: "1% collectively paid by merchant and customer",
+          UserFees : "Daily fees",
+          Revenue: "Invoice fees",
+          HoldersRevenue: "Staking rewards earned by veZBU holders, 0.6% of collected fees "
+        }
+      }
+    },
+    [CHAIN.BSC]: {
+      fetch : graphsDaily(endpoints)(CHAIN.BSC),
+      start: 1688083200,
+      meta: {
+        methodology: {
+          Fees: "1% collectively paid by merchant and customer",
+          Revenue: "Invoice fees",
+          HoldersRevenue: "Staking rewards earned by veZBU holders, 0.6% of collected fees "
+        }
+      }
+    },
 
-export default adapter;
+  },
+  version: 2,
+} as Adapter;
