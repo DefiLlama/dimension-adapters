9b6ffda7dabe627a3215b408275238205dc3d655
grigorevPavel <>
2025-08-06 00:46:06 +0300

    added prod api

--- bridge-aggregators/babydogeSwapAxelar/index.ts
+++ bridge-aggregators/babydogeSwapAxelar/index.ts
@@ -5,49 +5,38 @@ import { httpGet } from "../../utils/fetchURL"
 
 const BNB_ADDRESS = "0xc748673057861a797275cd8a068abb95a902e8de"
 const BASE_ADDRESS = "0x58ecEF26335Af7b04A998105a6603B0Dc475aF33"
-const apiBaseURL = "" // todo
+const apiBaseURLDev = "https://baby-axelar-bridge-connector-backend-dev.dev.babyparrot.xyz/lama-api"
+const apiBaseURLProd = "https://axelar-bridge-connector-backend-main.babybulldog.xyz/lama-api"
 const volumeMethod = "/volume"
 const feesMethod = "/fees"
 
+const url = apiBaseURLProd
+
+const CHAIN_MAP = {
+    [CHAIN.BSC]: 56,
+    [CHAIN.BASE]: 8453,
+}
+
 const fetch = async (options: FetchOptions) => {
-    const params = { chain: options.chain }
-    const volumeResponse = await httpGet(apiBaseURL + volumeMethod, { params })
-    const feesResponse = await httpGet(apiBaseURL + feesMethod, { params })
+    const params = { chain_id: CHAIN_MAP[options.chain] }
+    const volumeResponse = await httpGet(url + volumeMethod, { params })
+    const feesResponse = await httpGet(url + feesMethod, { params })
 
-    let tokenAddress: string = ADDRESSES.null
-
-    if (options.chain === CHAIN.BSC) tokenAddress = BNB_ADDRESS
-    if (options.chain === CHAIN.BASE) tokenAddress = BASE_ADDRESS
-
-    const volume = options.createBalances()
-    const dailyVolume = options.createBalances()
-    volume.add(tokenAddress, volumeResponse.volume)
-    dailyVolume.add(tokenAddress, volumeResponse.dailyVolume)
-
-    const fees = options.createBalances()
-    const dailyFees = options.createBalances()
-    const revenue = options.createBalances()
-    const dailyRevenue = options.createBalances()
-
-    fees.add(tokenAddress, feesResponse.totalFee)
-    dailyFees.add(tokenAddress, feesResponse.dailyTotalFee)
-
-    // Fees <=> Revenue for the project
-    revenue.add(tokenAddress, feesResponse.totalFee)
-    dailyRevenue.add(tokenAddress, feesResponse.dailyTotalFee)
+    console.log(`Fetched volume and fees for ${options.chain} chain:`, volumeResponse, feesResponse, params)
 
     return {
-        totalVolume: volume,
-        dailyVolume,
-        totalFees: fees,
-        dailyFees,
-        totalRevenue: revenue,
-        dailyRevenue,
+        totalVolume: volumeResponse.totalVolume,
+        dailyVolume: volumeResponse.dailyVolume,
+        totalFees: feesResponse.totalFees,
+        dailyFees: feesResponse.dailyFees,
+        totalRevenue: feesResponse.totalFees, // revenue = fees
+        dailyRevenue: feesResponse.dailyFees, // revenue = fees
     }
 }
 
 export default {
     version: 2,
+    start: '2025-08-05',
     adapter: {
         [CHAIN.BSC]: {
             fetch: fetch,
