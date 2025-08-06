822573f32b117faf561e6923ea810057db286cbf
vladi-coti <vladi@coti.io>
2025-08-06 10:49:33 +0300

    privex fixed volume and fees

--- fees/privex/index.ts
+++ fees/privex/index.ts
@@ -1,60 +1,63 @@
+import { formatEther } from "ethers";
 import { request, gql } from "graphql-request";
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, FetchResultFees, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const endpoints = {
-  [CHAIN.BASE]: "https://api.goldsky.com/api/public/project_cm1hfr4527p0f01u85mz499u8/subgraphs/base_analytics/latest/gn",
+  [CHAIN.BASE]: "https://api.goldsky.com/api/public/project_cmae5a5bs72to01xmbkb04v80/subgraphs/privex-analytics/1.0.0/gn",
   [CHAIN.COTI]: "https://subgraph.prvx.aegas.it/subgraphs/name/coti-analytics"
 };
 
-const fetchCoti = async (_a: any, _b: any, options: FetchOptions) => {
-  const [fromBlock, toBlock] = await Promise.all([options.getFromBlock(), options.getToBlock()])
-  const query = gql`
-    query fees {
-      yesterday: totalHistories(block: {number: ${fromBlock}}) {
-        timestamp
-        platformFee
-      }
-      today: totalHistories(block: {number: ${toBlock}}) {
-        timestamp
-        platformFee
-      }
-    }
-  `;
-  const graphRes = await request(endpoints[options.chain], query);
-  const todayFees = graphRes['today'].reduce((p: any, c: any) => p + Number(c.platformFee), 0)
-  const yesterdayFees = graphRes['yesterday'].reduce((p: any, c: any) => p + Number(c.platformFee), 0)
-  const fees24H = todayFees - yesterdayFees;
-  const dailyFees = fees24H / 1e18; // convert from wei
+interface IGraphResponse {
+  dailyHistories: Array<{
+    tiemstamp: string;
+    platformFee: string;
+    accountSource: string;
+    tradeVolume: string;
+  }>;
+}
 
-  return {
-    dailyFees,
-    dailyRevenue: dailyFees,
-    dailyProtocolRevenue: dailyFees,
-  };
-};
-
-const fetchBase = async (_a: any, _b: any, options: FetchOptions) => {
-  const endpoint = endpoints[options.chain];
-  const day = Math.floor(options.endTimestamp / 86400);
-  const query = gql`
-    query fees {
-      dailyHistories(
-        where: { 
-          day: ${day}, 
-        }
-        orderBy: day
-        orderDirection: desc
-        first: 100
-      ) {
-        day
-        platformFee
-      }
+const queryBase = gql`
+  query stats($from: String!, $to: String!) {
+    dailyHistories(
+      where: { timestamp_gte: $from, timestamp_lte: $to, accountSource: "0x921dd892d67aed3d492f9ad77b30b60160b53fe1" }
+    ) {
+      timestamp
+      platformFee
+      accountSource
+      tradeVolume
     }
-  `;
-  let data = await request(endpoint, query);
-  const recordWithFees = data?.dailyHistories?.find(d => parseFloat(d.platformFee || "0") > 0);
-  const dailyFees = parseFloat(recordWithFees?.platformFee || "0") / 1e18;
+  }
+`;
+
+const queryCoti = gql`
+  query stats($from: String!, $to: String!) {
+    dailyHistories(
+      where: { timestamp_gte: $from, timestamp_lte: $to, accountSource: "0xbf318724218ced9a3ff7cfc642c71a0ca1952b0f" }
+    ) {
+      timestamp
+      platformFee
+      accountSource
+      tradeVolume
+    }
+  }
+`;
+
+const queries = {
+  [CHAIN.BASE]: queryBase,
+  [CHAIN.COTI]: queryCoti,
+}
+
+const fetchFees = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultFees> => {
+  const from = options.fromTimestamp;
+  const to = options.toTimestamp;
+  const response: IGraphResponse = await request(endpoints[options.chain], queries[options.chain], {
+    from: String(from),
+    to: String(to),
+  });
+
+  const dailyFeesBigInt = response.dailyHistories.reduce((sum, data) => sum + BigInt(data.platformFee), BigInt(0));
+  const dailyFees = formatEther(dailyFeesBigInt);
 
   return {
     dailyFees,
@@ -73,12 +76,12 @@ const adapter: SimpleAdapter = {
   version: 1,
   adapter: {
     [CHAIN.BASE]: {
-      fetch: fetchBase,
+      fetch: fetchFees,
       start: "2024-09-08",
       meta: { methodology },
     },
     [CHAIN.COTI]: {
-      fetch: fetchCoti,
+      fetch: fetchFees,
       start: "2025-01-01",
       meta: { methodology },
     },
