136e0bdcd15b9302f7e4270d7be750505582540f
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-06 18:35:11 +0530

    keep builder fees delay, and separate phantom-perps and axiom-perps fees adapter

--- dexs/phantom-perps/index.ts
+++ dexs/phantom-perps/index.ts
@@ -1,20 +1,2 @@
-import { CHAIN } from "../../helpers/chains";
-import { fetchBuilderCodeRevenue } from "../../helpers/hyperliquid";
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
-
-const HL_BUILDER_ADDRESS = '0xb84168cf3be63c6b8dad05ff5d755e97432ff80b';
-
-const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-  const { dailyVolume } = await fetchBuilderCodeRevenue({ options, builder_address: HL_BUILDER_ADDRESS });
-  return { dailyVolume };
-};
-
-const adapter: SimpleAdapter = {
-  version: 1,
-  fetch,
-  chains: [CHAIN.HYPERLIQUID],
-  start: '2025-07-01',
-  isExpensiveAdapter: true
-};
-
+import adapter from "../../fees/phantom-perps";
 export default adapter;
