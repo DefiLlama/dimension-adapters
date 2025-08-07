a63206e361ffd868d7e64da621d054fa9178425f
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-07 23:57:00 +0530

    refactor

--- fees/zeebu/index.ts
+++ fees/zeebu/index.ts
@@ -20,89 +20,6 @@ const endpoints = {
   [CHAIN.BASE]: 'https://api.studio.thegraph.com/query/89152/fees_reward_base/version/latest',
 }
 
-const graphsDaily = (graphUrls: Record<string, string>) => {
-  return (chain: CHAIN) => {
-    return async (timestamp: number) => {
-      const dayID = (Math.floor(timestamp.startOfDay / 86400) ).toString(); // Ensure this aligns with your subgraph's dayID logic
-      //console.log("dayID",dayID, chain);
-      const graphQuery = gql`
-        query ($dayID: String!) {
-          dayVolumeFeesAggregates(
-            orderBy: dayID
-            orderDirection: desc
-            where: { dayID: $dayID }
-          ) {
-            contract
-            dailyFees
-            dailyVolume
-            dayID
-          }
-        }
-      `;
-
-      const totalFeesQuery = gql`
-        query {
-          overallVolumeFeesAggregates{
-            totalFees
-            totalVolume
-            chain
-            contract
-          }
-        }
-      `;
-
-      try {
-        // Fetch total fees
-        const totalFeesResponse = await request(graphUrls[chain], totalFeesQuery, { });
-        //console.log("totalFeesResponse",totalFeesResponse);
-        const totalFees = totalFeesResponse.overallVolumeFeesAggregates.reduce(
-          (sum, item) => sum + parseFloat(((item.totalFees * 2)/1e18) || 0),
-          0
-        );
-        //console.log("totalFees",totalFees);
-
-        // Fetch daily fees
-        const graphRes = await request(graphUrls[chain], graphQuery, { dayID: dayID });
-        //console.log("graphRes",graphRes);
-        const aggregates = graphRes.dayVolumeFeesAggregates;
-        //console.log("aggregates",aggregates);
-
-        // Aggregate daily fees and daily volume
-        const dailyFees = aggregates.reduce((sum, agg) => sum + parseFloat(((agg.dailyFees * 2)/1e18) || 0), 0);
-        //console.log("dailyFees",dailyFees);
-        const dailyUserFees = dailyFees;
-        const totalUserFees = totalFees;
-
-        const dailyRevenue = dailyFees;
-        const totalRevenue = totalFees;
-
-        const dailyHoldersRevenue = dailyFees * 0.6 / 100;
-        const totalHoldersRevenue = totalFees * 0.6 / 100;
-
-        const dailySupplySideRevenue = dailyFees * 0.6 / 100;
-        const totalSupplySideRevenue = totalFees * 0.6 / 100;
-
-        const dailyProtocolRevenue = dailyFees - dailyHoldersRevenue;
-
-        return {dailyFees, totalFees, dailyUserFees, totalUserFees, dailyRevenue, totalRevenue, dailyHoldersRevenue, totalHoldersRevenue, dailyProtocolRevenue };
-      } catch (error) {
-        console.error(`Error fetching data for chain ${chain}:`, error.message);
-        return {
-          dailyFees: 0,
-          totalFees: 0,
-          dailyUserFees : 0, 
-          totalUserFees : 0, 
-          dailyRevenue : 0, 
-          totalRevenue : 0, 
-          dailyHoldersRevenue : 0, 
-          totalHoldersRevenue : 0,
-          dailyProtocolRevenue : 0
-        };
-      }
-    };
-  };
-};
-
 interface GraphResponse {
   dayVolumeFeesAggregates: {
     contract: string;
@@ -111,8 +28,9 @@ interface GraphResponse {
     dayID: string;
   }[];
 }
+
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-  const dayID = (Math.floor(options.startOfDay / 86400) ).toString();
+  const dayID = (Math.floor(options.startOfDay / 86400)).toString();
   const graphQuery = gql`
     query ($dayID: String!) {
       dayVolumeFeesAggregates(
@@ -128,17 +46,6 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     }
   `;
 
-  const totalFeesQuery = gql`
-        query {
-          overallVolumeFeesAggregates{
-            totalFees
-            totalVolume
-            chain
-            contract
-          }
-        }
-      `;
-
   const url = endpoints[options.chain];
   const graphRes: GraphResponse = await request(url, graphQuery, { dayID: dayID });
   const aggregates = graphRes.dayVolumeFeesAggregates;
@@ -149,26 +56,20 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const dailyHoldersRevenue = dailyFees * 0.6 / 100;
   const dailyProtocolRevenue = dailyFees - dailyHoldersRevenue;
 
-  const totalFeesResponse = await request(url, totalFeesQuery, { });
-  const totalFees = totalFeesResponse.overallVolumeFeesAggregates.reduce(
-    (sum, item) => sum + parseFloat(((item.totalFees * 2)/1e18) || 0),
-    0
-  );
-
-  const totalUserFees = totalFees;
-  const totalRevenue = totalFees;
-  const totalHoldersRevenue = totalFees * 0.6 / 100;
-  const totalSupplySideRevenue = totalFees * 0.6 / 100;
-
-
-  return { dailyFees, dailyUserFees: dailyFees, dailyHoldersRevenue, dailyProtocolRevenue, totalUserFees, totalRevenue, totalHoldersRevenue, totalSupplySideRevenue};
-
+  return { 
+    dailyFees, 
+    dailyUserFees: dailyFees, 
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue,
+    dailyHoldersRevenue,
+  };
 };
 
 const methodology = {
   Fees: "2% collectively paid by merchant and customer",
   Revenue: "Invoice fees",
-  HoldersRevenue: "Staking rewards earned by veZBU holders, 0.6% of collected fees "
+  ProtocolRevenue: "Protocol share from fees",
+  HoldersRevenue: "Staking rewards earned by veZBU holders, 0.6% of collected fees ",
 }
 
 const adapter: SimpleAdapter = {
