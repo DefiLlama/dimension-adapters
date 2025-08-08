f00d1a18d6f82f29d40be568b0fddd6ee6b1f81e
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-08 16:06:39 +0530

    refactor beefy revenue

--- fees/beefy/index.ts
+++ fees/beefy/index.ts
@@ -1,4 +1,4 @@
-import { FetchOptions } from '../../adapters/types';
+import { FetchOptions, SimpleAdapter } from '../../adapters/types';
 import { CHAIN } from '../../helpers/chains';
 import ADDRESSES from '../../helpers/coreAssets.json';
 import { addTokensReceived } from '../../helpers/token';
@@ -10,18 +10,10 @@ const revenueFee = totalFee - strategistFee - callFee;
 const holderShare = 36;
 const protocolShare = 64;
 
-const methodology = {
-  Fees: `${totalFee}% of each harvest is charged as a performance fee`,
-  Revenue: `All fees except for ${strategistFee}% to strategist and variable harvest() call fee are revenue`,
-  HoldersRevenue: `${holderShare}% of revenue is distributed to holders who stake`,
-  ProtocolRevenue: `${protocolShare}% of revenue is distributed to the treasury`,
-};
-
 const beefyRevenueBridgeAddress = '0x02ae4716b9d5d48db1445814b0ede39f5c28264b'; // on L2s
 const beefyFeeBatch = '0x65f2145693bE3E75B8cfB2E318A3a74D057e6c7B'; // on ethereum
 
-type ChainConfig = {
-  chainId: CHAIN;
+type ChainConfigType = {
   start: string;
   contract: string;
   stables: string[];
@@ -30,205 +22,150 @@ type ChainConfig = {
 
 type ERC20TransferLogResult = {
   from: string;
+  from_address?: string;
   to: string;
   value: bigint;
+}
+
+function makeErc20BlacklistFromFilter(blacklistedFromAddresses: string[],): (log: ERC20TransferLogResult) => boolean {
+  const blacklistedFromAddressSet = new Set(blacklistedFromAddresses.map((address: string) => address.toLowerCase()));
+  return (log: ERC20TransferLogResult) => {
+    const from = log?.from?.toLowerCase() || log?.from_address?.toLowerCase();
+    return !blacklistedFromAddressSet.has(from || '');
+  };
 }
 
 /**
  * Each contract collects fees in WNATIVE, then swaps to a stablecoin, which is bridged (possibly via other chains) to Ethereum.
  * Rather than tracking all the WNATIVE transfers, we track the incoming stablecoin transfers to the contract address from the swap transactions.
  * To ensure bridged funds are not double-counted, we try to exclude transfers/swaps originating from other chains.
+ * we can comment out real, scroll, fantom, zkevm, kava, canto etc. after backfill
  */
-const configs: ChainConfig[] = [
-  {
-    /**
-     * real
-     * @dev EOL, can comment out after backfill
-     */
-    chainId: CHAIN.REAL,
-    start: '2024-07-17', // 0xea3bd6b2f389c0c0880469d46c05472ab78deb6eec577788c53b719da24d1b6c
+const chainConfig: Partial<Record<CHAIN, ChainConfigType>> = {
+  [CHAIN.REAL]: {
+    start: '2024-07-17',
     contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.real.USDC]
+    stables: [ADDRESSES.real.USDC],
   },
-  {
-    /**
-     * scroll
-     * @dev EOL, can comment out after backfill
-     */
-    chainId: CHAIN.SCROLL,
-    start: '2024-10-16', // 0x9ddb499f4b0644cfc68bd5d4e6ee0269fa798582e776faefac19d7deab49abbf
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.scroll.USDC]
-  },
-  {
-    /**
-     * fantom
-     * @dev EOL, can comment out after backfill
-     */
-    chainId: CHAIN.FANTOM,
-    start: '2023-08-23', // 0x556eca0d3d2d58a6692b0e3f3c98d36d3ed45939e926e05c6fcfa711ad966322
-    contract: beefyRevenueBridgeAddress,
-    stables: [
-      '0x1B6382DBDEa11d97f24495C9A90b7c88469134a4', // axlUSDC
-    ],
-  },
-  {
-    /**
-     * zkevm
-     * @dev EOL, can comment out after backfill
-     */
-    chainId: CHAIN.POLYGON_ZKEVM,
-    start: '2023-08-23', // 0xcb2295351d8be1127ef73262ef17126287ad29af4aa1502e1e1a1eb7df6b9815
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.polygon_zkevm.USDC]
-  },
-  {
-    /**
-     * kava
-     * @dev EOL, can comment out after backfill
-     */
-    chainId: CHAIN.KAVA,
-    start: '2023-08-23', // 0xce30683b7b268011841cf608876dd76275fe8c2397f53da3828f7d45d30a2ad1
-    contract: beefyRevenueBridgeAddress,
-    stables: [
-      '0xEB466342C4d449BC9f53A865D5Cb90586f405215', // axlUSDC
-    ]
-  },
-  {
-    /**
-     * canto
-     * @dev EOL, can comment out after backfill
-     */
-    chainId: CHAIN.CANTO,
-    start: '2023-08-23', // 0x6b557a3dee371ca82da607a09bb2db2f12d0c53ab07df45fe83e52c17d6c11fd
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.canto.USDC]
-  },
-  {
-    /**
-     * rootstock
-     * @dev Test would not run locally with RootStock enabled, may need to be commented out
-     */
-    chainId: CHAIN.ROOTSTOCK,
-    start: '2024-09-30', // 0x0f6f952f1c100c5b641090dc7149ebd347dae5373c72da537aa399fd349454f9
+  [CHAIN.ROOTSTOCK]: {
+    start: '2024-09-30',
     contract: beefyRevenueBridgeAddress,
     stables: [
       '0xAf368c91793CB22739386DFCbBb2F1A9e4bCBeBf', // USDT
     ],
   },
-  {
-    /** sei */
-    chainId: CHAIN.SEI,
-    start: '2024-08-01', // 0x1e0b6a11d1cb84cb6e208d0208b2463c7d197ca45a80e355ce1855b51ebfd12d
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.sei.USDC]
-  },
-  {
-    /** optimism */
-    chainId: CHAIN.OPTIMISM,
-    start: '2023-08-25',
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.optimism.USDC_CIRCLE]
-  },
-  {
-    /** cronos */
-    chainId: CHAIN.CRONOS,
-    start: '2023-08-23', // 0xe42503b9613a913773dffe0bd45ed8f77d92793bfdcf9ab1b28c688ab56f395a
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.cronos.USDC]
-  },
-  {
-    /** bsc */
-    chainId: CHAIN.BSC,
-    start: '2023-08-25',
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.bsc.USDT],
-    excludeFrom: [
-      '0x138eb30f73bc423c6455c53df6d89cb01d9ebc63', // stargate USDT pool (result of bridges from: rootstock)
-    ],
-  },
-  {
-    /** gnosis */
-    chainId: CHAIN.XDAI,
-    start: '2023-08-23', // 0xc0b9485adaaf7efc7c3f82269557dcf1ed9ee534a1acf7111dcb55b2030b9b8b
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.xdai.USDC]
-  },
-  {
-    /** sonic */
-    chainId: CHAIN.SONIC,
-    start: '2024-12-17',
-    contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.sonic.USDC_e]
-  },
-  {
-    /** fraxtal */
-    chainId: CHAIN.FRAXTAL,
-    start: '2024-03-27', // 0xe022f13135874da015ea9e8ea880b31634155538197d6169a13784a614b4f66d
+  [CHAIN.FRAXTAL]: {
+    start: '2024-03-27',
     contract: beefyRevenueBridgeAddress,
     stables: [
       '0xFc00000000000000000000000000000000000001', // frxUSD
     ]
   },
-  {
-    /** metis */
-    chainId: CHAIN.METIS,
-    start: '2023-08-23', // 0xd3cb0e059789807793142311e0dee0eb872735acdff841b5a47cd0b317069139
+  [CHAIN.SCROLL]: {
+    start: '2024-10-16',
     contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.metis.m_USDC]
+    stables: [ADDRESSES.scroll.USDC],
   },
-  {
-    /** moonbeam */
-    chainId: CHAIN.MOONBEAM,
-    start: '2024-06-14', // 0x64d3b0e857e8e342f7581b7ba161f23d5790c6afabe193501a2e4603f8e2ed1c
+  [CHAIN.FANTOM]: {
+    start: '2023-08-23',
+    contract: beefyRevenueBridgeAddress,
+    stables: [
+      '0x1B6382DBDEa11d97f24495C9A90b7c88469134a4', // axlUSDC
+    ],
+  },
+  [CHAIN.POLYGON_ZKEVM]: {
+    start: '2023-08-23',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.polygon_zkevm.USDC],
+  },
+  [CHAIN.KAVA]: {
+    start: '2023-08-23',
+    contract: beefyRevenueBridgeAddress,
+    stables: [
+      '0xEB466342C4d449BC9f53A865D5Cb90586f405215', // axlUSDC
+    ],
+  },
+  [CHAIN.CANTO]: {
+    start: '2023-08-23',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.canto.USDC],
+  },
+  [CHAIN.SEI]: {
+    start: '2024-08-01',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.sei.USDC],
+  },
+  [CHAIN.OPTIMISM]: {
+    start: '2023-08-25',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.optimism.USDC_CIRCLE],
+  },
+  [CHAIN.CRONOS]: {
+    start: '2023-08-23',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.cronos.USDC],
+  },
+  [CHAIN.BSC]: {
+    start: '2023-08-25',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.bsc.USDT],
+    excludeFrom: [
+      '0x138eb30f73bc423c6455c53df6d89cb01d9ebc63', // stargate USDT pool (result of bridges from: rootstock)
+    ]
+  },
+  [CHAIN.XDAI]: {
+    start: '2023-08-23',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.xdai.USDC],
+  },
+  [CHAIN.SONIC]: {
+    start: '2024-12-17',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.sonic.USDC_e],
+  },
+  [CHAIN.METIS]: {
+    start: '2023-08-23',
+    contract: beefyRevenueBridgeAddress,
+    stables: [ADDRESSES.metis.m_USDC],
+  },
+  [CHAIN.MOONBEAM]: {
+    start: '2024-06-14',
     contract: beefyRevenueBridgeAddress,
     stables: [
       '0xCa01a1D0993565291051daFF390892518ACfAD3A', // axlUSDC
     ]
   },
-  {
-    /** mantle */
-    chainId: CHAIN.MANTLE,
-    start: '2024-01-05', // 0x66934644292573d4b8c47b8c9d2ce4ae204ab45c79a4222944556f0abf9f6ffd
+  [CHAIN.MANTLE]: {
+    start: '2024-01-05',
     contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.mantle.USDC]
+    stables: [ADDRESSES.mantle.USDC],
   },
-  {
-    /** base */
-    chainId: CHAIN.BASE,
-    start: '2023-08-23', // 0x007eeb770b6ff211ec373e4486417358bd0e8d786c74c9c6dbf6e32f2e23ee51
+  [CHAIN.BASE]: {
+    start: '2023-08-23',
     contract: beefyRevenueBridgeAddress,
     stables: [ADDRESSES.base.USDbC],
   },
-  {
-    /** mode */
-    chainId: CHAIN.MODE,
-    start: '2023-08-23', // 0x3c2936e0c0e43f3ec692c8001dcbaec942895ce0b13c01dbe8f6642ac585c59c
+  [CHAIN.MODE]: {
+    start: '2023-08-23',
     contract: beefyRevenueBridgeAddress,
     stables: [ADDRESSES.mode.USDC],
   },
-  {
-    /** avax */
-    chainId: CHAIN.AVAX,
-    start: '2023-08-23', // 0x5338286bfcde6ed927a639f128f02dfd8347eb5aacd561ea0f6169c529568971
+  [CHAIN.AVAX]: {
+    start: '2023-08-23',
     contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.avax.USDC]
+    stables: [ADDRESSES.avax.USDC],
   },
-  {
-    /** linea */
-    chainId: CHAIN.LINEA,
-    start: '2023-12-07', // 0x4c71e178794787169c69367cc8fa69f54dc4736c8c088710f9e33d814ef4fae6
+  [CHAIN.LINEA]: {
+    start: '2023-12-07',
     contract: beefyRevenueBridgeAddress,
-    stables: [ADDRESSES.linea.USDC]
+    stables: [ADDRESSES.linea.USDC],
   },
-  {
-    /** berachain */
-    chainId: CHAIN.BERACHAIN,
-    start: '2025-02-06', // 0xa06dba08b797f30a5cf810c15bbb42eb60379ca0a4ea3ebe263620611c8da0f9
+  [CHAIN.BERACHAIN]: {
+    start: '2025-02-06',
     contract: beefyRevenueBridgeAddress,
     stables: [ADDRESSES.berachain.USDC]
   },
-  {
+  [CHAIN.POLYGON]: {
     /**
      * polygon
      * exclude:
@@ -238,24 +175,22 @@ const configs: ChainConfig[] = [
      *   from fantom via axelar (axlUSDC was received to BeefySwapper 0xc0d1)
      *   from moonbeam via axelar (axlUSDC was received to BeefySwapper 0xc0d1)
      *   from kava via axelar (axlUSDC was received to BeefySwapper 0xc0d1)
-     */
-    chainId: CHAIN.POLYGON,
+    */
     start: '2023-09-06',
     contract: beefyRevenueBridgeAddress,
     stables: [
-      // ADDRESSES.polygon.USDT, // never received
-      ADDRESSES.polygon.USDC, // switch tx  0x15aad861f5c8bd79d7c68b060a4b173d9e45523af4b02f57b8ff93bc8b3da9bc
+      ADDRESSES.polygon.USDC,
     ],
     excludeFrom: [
-      // '0xDd27227Dba7Ea8F5869466A10A8E36Bb2D709b35', // BeefySwapper, used to swap WNATIVE to USDC
-      // '0xA374094527e1673A86dE625aa59517c5dE346d32', // Uniswap V3 WNATIVE/USDC pool, previously used to swap WNATIVE to USDC
       '0x8f5bbb2bb8c2ee94639e55d5f41de9b4839c1280', // synapse bridge (already counted, result of bridges from: cronos, metis, canto)
       '0xc0d173e3486f7c3d57e8a38a003500fd27e7d055', // BeefySwapper, used to swap axlUSDC to USDC
       '0x4fed5491693007f0cd49f4614ffc38ab6a04b619', // BeefyDeployer, historical manual swaps of nUSD (Synapse USD)
       '0x161d61e30284a33ab1ed227bedcac6014877b3de', // BeefyDev, historical manual swaps of nUSD (Synapse USD)
+      // '0xDd27227Dba7Ea8F5869466A10A8E36Bb2D709b35', // BeefySwapper, used to swap WNATIVE to USDC
+      // '0xA374094527e1673A86dE625aa59517c5dE346d32', // Uniswap V3 WNATIVE/USDC pool, previously used to swap WNATIVE to USDC
     ],
   },
-  {
+  [CHAIN.ARBITRUM]: {
     /**
      * arbitrum
      * exclude:
@@ -272,8 +207,7 @@ const configs: ChainConfig[] = [
      *   from real via real
      *   from avax via circle
      */
-    chainId: CHAIN.ARBITRUM,
-    start: '2023-09-06', // 0xab8bad3336438d336804bed884bdec61f874d650f427a04b7d7fbb05e814396d
+    start: '2023-09-06',
     contract: beefyRevenueBridgeAddress,
     stables: [
       // ADDRESSES.arbitrum.USDT, // never received
@@ -289,7 +223,7 @@ const configs: ChainConfig[] = [
       '0xBAa850bc2eCC6A4F356445f4A853281A42bD2fBb', // real bridge (already counted, result of bridges from: real)
     ],
   },
-  {
+  [CHAIN.ETHEREUM]: {
     /**
      * ethereum
      * usually only swaps weth->usdc once per week
@@ -301,8 +235,7 @@ const configs: ChainConfig[] = [
      *   from arbitrum via circle
      *   from linea via linea
      */
-    chainId: CHAIN.ETHEREUM,
-    start: '2024-07-11', // 0x25b2dc9a1f268b2e894138e4f0d445615579f298ba553c08951230ee5d224689
+    start: '2024-07-11',
     contract: beefyFeeBatch,
     stables: [ADDRESSES.ethereum.USDC],
     excludeFrom: [
@@ -314,64 +247,51 @@ const configs: ChainConfig[] = [
       '0x504A330327A089d8364C4ab3811Ee26976d388ce', // L1USDCBridge (already counted, result of bridges from: linea)
     ],
   },
-];
+}
 
-const seen = new Set<CHAIN>();
-const adapter = Object.fromEntries(configs.map(config => {
-  const {
-    chainId,
-    start,
-    contract,
-    stables,
-    excludeFrom
-  } = config;
-  if (seen.has(chainId)) {
-    throw new Error(`Duplicate chainId found: ${chainId}`);
+const fetch = async (options: FetchOptions) => {
+  const { chain } = options;
+  const config = chainConfig[chain];
+  const { excludeFrom, contract, stables } = config;
+
+  const logFilter = excludeFrom && excludeFrom.length > 0 ? makeErc20BlacklistFromFilter(excludeFrom) : undefined;
+  let dailyRevenue = await addTokensReceived({
+    options,
+    target: contract,
+    tokens: stables,
+    logFilter
+  });
+
+  // Ethereum feeBatch only swaps the protocol share of the weth to usdc, not the holder share
+  if (chain === CHAIN.ETHEREUM) {
+    // Scale it up from the protocol share to the total revenue
+    dailyRevenue = dailyRevenue.clone(100 / protocolShare);
   }
-  seen.add(chainId);
 
-  return [
-    chainId, {
-      start,
-      meta: {methodology},
-      fetch: async (options: FetchOptions) => {
-        const logFilter = excludeFrom && excludeFrom.length > 0 ? makeErc20BlacklistFromFilter(excludeFrom) : undefined;
-        let dailyRevenue = await addTokensReceived({
-          options,
-          target: contract,
-          tokens: stables,
-          logFilter
-        });
-
-        // Ethereum feeBatch only swaps the protocol share of the weth to usdc, not the holder share
-        if (chainId === CHAIN.ETHEREUM) {
-          // Scale it up from the protocol share to the total revenue
-          dailyRevenue = dailyRevenue.clone(100 / protocolShare);
-        }
-
-        // scale revenue up to include strategist and call fees
-        const dailyFees = dailyRevenue.clone(totalFee / revenueFee);
-        const dailyProtocolRevenue = dailyRevenue.clone(protocolShare / 100);
-        const dailyHoldersRevenue = dailyRevenue.clone(holderShare / 100);
-        return {
-          dailyFees,
-          dailyRevenue,
-          dailyProtocolRevenue,
-          dailyHoldersRevenue
-        };
-      },
-    }
-  ];
-}));
-
-export default {
-  version: 2,
-  adapter,
+  // scale revenue up to include strategist and call fees
+  const dailyFees = dailyRevenue.clone(totalFee / revenueFee);
+  const dailyProtocolRevenue = dailyRevenue.clone(protocolShare / 100);
+  const dailyHoldersRevenue = dailyRevenue.clone(holderShare / 100);
+  return {
+    dailyFees,
+    dailyRevenue,
+    dailyProtocolRevenue,
+    dailyHoldersRevenue
+  };
 };
 
-function makeErc20BlacklistFromFilter(blacklistedFromAddresses: string[],): (log: ERC20TransferLogResult) => boolean {
-  const blacklistedFromAddressSet = new Set(blacklistedFromAddresses.map((address: string) => address.toLowerCase()));
-  return (log: ERC20TransferLogResult) => {
-    return !blacklistedFromAddressSet.has(log.from.toLowerCase());
-  };
-}
+const methodology = {
+  Fees: `${totalFee}% of each harvest is charged as a performance fee`,
+  Revenue: `All fees except for ${strategistFee}% to strategist and variable harvest() call fee are revenue`,
+  HoldersRevenue: `${holderShare}% of revenue is distributed to holders who stake`,
+  ProtocolRevenue: `${protocolShare}% of revenue is distributed to the treasury`,
+};
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  fetch,
+  adapter: chainConfig,
+  methodology,
+};
+
+export default adapter;
