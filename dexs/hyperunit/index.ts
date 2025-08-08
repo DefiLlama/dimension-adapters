4ed671160fb53c83c4a5c3e560164f672f64746a
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-08 22:43:41 +0530

    correct usd volume

--- dexs/hyperunit/index.ts
+++ dexs/hyperunit/index.ts
@@ -16,28 +16,20 @@ const CG_UNIT_DEPLOYED_TOKENS = {
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     const spotTradeFeesQuery = `
-    WITH base_trades AS (
-      SELECT 
-        token_a_symbol,
-        sum(usd_amount) as volume_usd
-      FROM hyperliquid.dex.trades
-      WHERE timestamp >= TO_TIMESTAMP_NTZ('${options.startTimestamp}')
-        AND timestamp <= TO_TIMESTAMP_NTZ('${options.endTimestamp}')
-        AND market_type = 'spot'
-    )
     SELECT 
-      token_a_symbol,
-      sum(volume_usd) as volume_usd
-    FROM base_trades
-    WHERE token_a_symbol IN (${Object.keys(CG_UNIT_DEPLOYED_TOKENS).map(token => `'${token}'`).join(',')})
+      sum(usd_amount) as volume_usd
+    FROM hyperliquid.dex.trades
+    WHERE timestamp >= TO_TIMESTAMP_NTZ('${options.startTimestamp}')
+      AND timestamp <= TO_TIMESTAMP_NTZ('${options.endTimestamp}')
+      AND market_type = 'spot'
+      AND token_a_symbol IN (${Object.keys(CG_UNIT_DEPLOYED_TOKENS).map(token => `'${token}'`).join(',')})
   `;
 
   const data = await queryAllium(spotTradeFeesQuery);
   const dailyVolume = options.createBalances();
 
   for(const row of data) {
-    const cgToken = CG_UNIT_DEPLOYED_TOKENS[row.token_a_symbol];
-    dailyVolume.addCGToken(cgToken, Number(row.volume_usd));
+    dailyVolume.addUSDValue(Number(row.volume_usd));
   }
 
   return {
