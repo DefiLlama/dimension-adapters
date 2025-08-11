1d88b0fc068df593a60af558f48bdd3fd08cc07d
hyeonLewis <lewis.kim@kaia.io>
2025-08-11 15:48:53 +0900

    Add CL DEX on Kaia

---
+++ dexs/cl-dex/index.ts
@@ -0,0 +1,8 @@
+import { CHAIN } from "../../helpers/chains";
+import { uniV2Exports } from "../../helpers/uniswap";
+
+export default uniV2Exports({
+  [CHAIN.KLAYTN]: {
+    factory: "0x2A4C5eCaafB26460F17d509EA4d15741AF5F5f0a",
+  },
+});
