30e90e245e238fd5298f1ed1226179900b2ad601
tree <sol.analyzoor@gmail.com>
2025-08-11 23:16:31 +0530

    add starknet for extended (#3998)

--- dexs/extended/index.ts
+++ dexs/extended/index.ts
@@ -1,10 +1,9 @@
 import fetchURL from "../../utils/fetchURL"
-import { FetchResultVolume, SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import {getUniqStartOfTodayTimestamp} from "../../helpers/getUniSubgraphVolume";
 
-const historicalVolumeEndpoint = (dateTime: string) => `https://api.extended.exchange/api/v1/exchange/stats/trading?fromDate=${dateTime}&toDate=${dateTime}`
-const marketsEndpoint = 'https://api.extended.exchange/api/v1/info/markets'
+const historicalVolumeEth = (dateTime: string) => `https://api.extended.exchange/api/v1/exchange/stats/trading?fromDate=${dateTime}&toDate=${dateTime}`
+const historicalVolumeStarknet = (dateTime: string) => `https://api.starknet.extended.exchange/api/v1/exchange/stats/trading?fromDate=${dateTime}&toDate=${dateTime}`
 
 interface IVolumeall {
   tradingVolume: string;
@@ -15,29 +14,40 @@ interface IResponse {
   data: IVolumeall[];
 }
 
-const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const timestampISO = new Date(dayTimestamp * 1000).toISOString().split('T')[0];
-  const historical: IResponse= await fetchURL(historicalVolumeEndpoint(timestampISO))
-  const dailyVolume = historical.data.reduce((a: number, b: IVolumeall) => a+Number(b.tradingVolume), 0)
-  const res = (await fetchURL(marketsEndpoint)).data
-  const openInterestAtEnd = res.reduce((a: number, b: any) => a+Number(b.marketStats.openInterest || 0), 0)
+const chainConfig = {
+  [CHAIN.ETHEREUM]: {
+    start: '2025-03-11',
+    endpoints: {
+      historicalVolume: historicalVolumeEth,
+      markets: 'https://api.extended.exchange/api/v1/info/markets',
+    },
+  },
+  [CHAIN.STARKNET]: {
+    start: '2025-08-10',
+    endpoints: {
+      historicalVolume: historicalVolumeStarknet,
+      markets: 'https://api.starknet.extended.exchange/api/v1/info/markets',
+    },
+  }
+}
 
-  return { 
-      dailyVolume, 
-      openInterestAtEnd
+const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultVolume> => {
+  const timestampISO = new Date(options.startOfDay * 1000).toISOString().split('T')[0];
+  const historical: IResponse = await fetchURL(chainConfig[options.chain].endpoints.historicalVolume(timestampISO))
+  const dailyVolume = historical.data.reduce((a: number, b: IVolumeall) => a + Number(b.tradingVolume), 0)
+  const res = (await fetchURL(chainConfig[options.chain].endpoints.markets)).data
+  const openInterestAtEnd = res.reduce((a: number, b: any) => a + Number(b.marketStats.openInterest || 0), 0)
+
+  return {
+    dailyVolume,
+    openInterestAtEnd
   };
 };
 
 const adapter: SimpleAdapter = {
   version: 1,
-  adapter: {
-    [CHAIN.ETHEREUM]: {
-      fetch,
-      runAtCurrTime: true,
-      start: '2025-03-11',
-    },
-  },
+  fetch,
+  adapter: chainConfig
 };
 
 export default adapter; 
