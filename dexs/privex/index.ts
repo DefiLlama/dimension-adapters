307d913ea4f9418879c9d332d536d8e09aa0fa6a
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-11 23:07:37 +0530

    refactor

--- dexs/privex/index.ts
+++ dexs/privex/index.ts
@@ -12,10 +12,18 @@ interface IGraphResponse {
   }>;
 }
 
-const endpoints = {
-  [CHAIN.BASE]: "https://api.goldsky.com/api/public/project_cmae5a5bs72to01xmbkb04v80/subgraphs/privex-analytics/1.0.0/gn",
-  [CHAIN.COTI]: "https://subgraph.prvx.aegas.it/subgraphs/name/coti-analytics"
-};
+const chainConfig = {
+  [CHAIN.BASE]: {
+    start: '2024-09-08', // October 8, 2024
+    accountSource: '0x921dd892d67aed3d492f9ad77b30b60160b53fe1',
+    endpoint: 'https://api.goldsky.com/api/public/project_cmae5a5bs72to01xmbkb04v80/subgraphs/privex-analytics/1.0.0/gn',
+  },
+  [CHAIN.COTI]: {
+    start: '2025-01-01', // January 1, 2025
+    accountSource: '0xbf318724218ced9a3ff7cfc642c71a0ca1952b0f',
+    endpoint: 'https://subgraph.prvx.aegas.it/subgraphs/name/coti-analytics',
+  },
+}
 
 const queryBase = gql`
   query stats($from: String!, $to: String!) {
@@ -30,50 +38,45 @@ const queryBase = gql`
   }
 `;
 
-const queryCoti = gql`
-  query stats($from: String!, $to: String!) {
-    dailyHistories(
-      where: { timestamp_gte: $from, timestamp_lte: $to, accountSource: "0xbf318724218ced9a3ff7cfc642c71a0ca1952b0f" }
-    ) {
-      timestamp
-      platformFee
-      accountSource
-      tradeVolume
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+  const query = ` 
+    query stats {
+      dailyHistories(
+        where: { timestamp_gte: "${options.fromTimestamp}", timestamp_lte: "${options.toTimestamp}", accountSource: "${chainConfig[options.chain].accountSource}" }
+      ) {
+        timestamp
+        platformFee
+        accountSource
+        tradeVolume
+      }
     }
-  }
-`;
-
-const queries = {
-  [CHAIN.BASE]: queryBase,
-  [CHAIN.COTI]: queryCoti,
-}
-
-const fetchVolume = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultVolume> => {
-  const from = options.fromTimestamp;
-  const to = options.toTimestamp;
-  const response: IGraphResponse = await request(endpoints[options.chain], queries[options.chain], {
-    from: String(from),
-    to: String(to),
-  });
+  `
+  const response: IGraphResponse = await request(chainConfig[options.chain].endpoint, query);
 
   const dailyVolumeBigInt = response.dailyHistories.reduce((sum, data) => sum + BigInt(data.tradeVolume), BigInt(0));
+  const dailyFeesBigInt = response.dailyHistories.reduce((sum, data) => sum + BigInt(data.platformFee), BigInt(0));
+  const dailyFees = formatEther(dailyFeesBigInt);
   const dailyVolume = formatEther(dailyVolumeBigInt * 2n);
 
-  return { dailyVolume };
+  return { 
+    dailyVolume,
+    dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
+  };
+};
+
+const methodology = {
+  Fees: "Platform fees collected by PriveX from derivatives trading activities",
+  Revenue: "All platform fees collected represent protocol revenue",
+  ProtocolRevenue: "All platform fees collected represent protocol revenue",
 };
 
 const adapter: SimpleAdapter = {
   version: 1,
-  adapter: {
-    [CHAIN.BASE]: {
-      fetch: fetchVolume,
-      start: '2024-09-08', // October 8, 2024
-    },
-    [CHAIN.COTI]: {
-      fetch: fetchVolume,
-      start: '2025-01-01', // January 1, 2025
-    },
-  },
+  fetch,
+  methodology,
+  adapter: chainConfig
 };
 
 export default adapter;
