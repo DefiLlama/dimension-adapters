903ae90017c0faaddfdc2f41fa2eb224b55639c7
eden <noat.eth@gmail.com>
2025-08-12 14:26:25 +0700

    fix rain.fi fees (#4004)

--- fees/rain/index.ts
+++ fees/rain/index.ts
@@ -3,39 +3,42 @@ import { CHAIN } from "../../helpers/chains";
 import { getSolanaReceived } from "../../helpers/token";
 import fetchURL from "../../utils/fetchURL";
 
-interface Statistics {
-  all: PeriodStats;
-  pastYear: PeriodStats;
-  pastMonth: PeriodStats;
-  pastWeek: PeriodStats;
-  pastDay: PeriodStats;
-}
-
-interface PeriodStats {
-  totalInterest: number;
-  totalProfit: number;
-  totalLoss: number;
-}
-
-const rainFeesUrl = "https://api-v3.rain.fi/api/dirty/all-lender-profit"
+const rainHistoricalFeesUrl = 'https://api-v3.rain.fi/api/dirty/historical-apys?days=30';
 
 const fethcFeesSolana = async (options: FetchOptions) => {
   const dailyRevenue = await getSolanaReceived({ options, target: 'H3RFN3GbDfwGhZc5QPqzW6U4cwhuk9vgPhEfFbcPDrm5' })
-  const stats: Statistics = (await fetchURL(rainFeesUrl));
+  const stats: any = (await fetchURL(rainHistoricalFeesUrl));
 
-  const dailyFees = stats.pastDay.totalInterest + stats.pastDay.totalProfit + stats.pastDay.totalLoss
-  const totalFees = stats.all.totalInterest + stats.all.totalProfit + stats.all.totalLoss
+  const dateString = new Date(options.startOfDay * 1000).toISOString().split('T')[0]
 
-  return { dailyFees, totalFees, dailyRevenue }
+  let dailyFees = dailyRevenue.clone()
+  for (const market of stats.result) {
+    for (const dateData of market.data) {
+      if (dateData.date === dateString) {
+        dailyFees.addUSDValue(Number(dateData.interest))
+      }
+    }
+  }
+
+  const dailySupplySideRevenue = dailyFees.clone()
+  dailySupplySideRevenue.subtract(dailyRevenue)
+
+  return { dailyFees, dailyRevenue, dailySupplySideRevenue, dailyProtocolRevenue: dailyRevenue }
 }
 
 
 const adapter: SimpleAdapter = {
   version: 2,
   isExpensiveAdapter: true,
+  methodology: {
+    Fees: 'Interest paid by borrowers.',
+    Revenue: 'Amount of intertest collected by Rain protocol.',
+    SupplySideRevenue: 'Amount of intertest distributed to lenders.',
+    ProtocolRevenue: 'Amount of intertest collected by Rain protocol.',
+  },
   adapter: {
     [CHAIN.SOLANA]: {
-      runAtCurrTime: true,
+      // runAtCurrTime: true,
       fetch: fethcFeesSolana,
       start: '2025-01-01',
     },
