97327cff0d5a5eb0e19385f98d1ddb692a7b0ddd
eden <noat.eth@gmail.com>
2025-08-14 14:36:25 +0700

    update uni v2, v3 forks (#4030)

--- dexs/swapsicle-v2/index.ts
+++ dexs/swapsicle-v2/index.ts
@@ -1,25 +1,35 @@
 import { CHAIN } from "../../helpers/chains";
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
 import { SimpleAdapter } from "../../adapters/types";
+import { getUniV3LogAdapter } from "../../helpers/uniswap";
 
-const fetch = univ2Adapter({
-  endpoints: {
-    [CHAIN.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/swapsicle/analytics/prod/gn",
-    [CHAIN.TELOS]: "https://test.telos.subgraph.swapsicle.io/subgraphs/name/swapsicle/analytics",
-    [CHAIN.TAIKO]: "https://api.goldsky.com/api/public/project_clr6mlufzbtuy01vd012wgt5k/subgraphs/swapsicle-analytics-taiko/prod/gn"
-  },
-  factoriesName: "factories",
-  dayData: "algebraDayData",
-  dailyVolume: "volumeUSD",
-  totalVolume: "totalVolumeUSD",
-});
+const poolCreatedEvent = 'event Pool (address indexed token0, address indexed token1, address pool)'
+const swapEvent = 'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 price, uint128 liquidity, int24 tick)'
 
+const config = {
+  isAlgebraV3: true,
+  swapEvent,
+  poolCreatedEvent,
+  userFeesRatio: 1,
+  revenueRatio: 0.455,
+  protocolRevenueRatio: 0.13,
+  holdersRevenueRatio: 0.325,
+}
+
+// https://docs.swapsicle.io/guide/Tokenomics.html#fee-distribution
 const adapter: SimpleAdapter = {
-  version: 1,
+  version: 2,
+  methodology: {
+    Fees: 'Users pay 0.25% per swap.',
+    UserFees: 'Users pay 0.25% per swap.',
+    Revenue: 'Swappi collects 32% swap fees for protocol treasury and PPI buy back.',
+    ProtocolRevenue: 'Swappi collects 12% swap fees for protocol treasury.',
+    HoldersRevenue: 'Swappi collects 20% swap fees for PPI buy back.',
+    SupplySideRevenue: 'Swappi distributes 68% swap fees to LPs.',
+  },
   adapter: {
-    [CHAIN.MANTLE]: { fetch, start: 1697155200 },
-    [CHAIN.TELOS]: { fetch, start: 1698105600 },
-    [CHAIN.TAIKO]: { fetch, start: 1724943360 },
+    [CHAIN.MANTLE]: { fetch: getUniV3LogAdapter({ factory: '0xC848bc597903B4200b9427a3d7F61e3FF0553913', ...config }), start: 1697155200 },
+    [CHAIN.TELOS]: { fetch: getUniV3LogAdapter({ factory: '0xA09BAbf9A48003ae9b9333966a8Bda94d820D0d9', ...config }), start: 1698105600 },
+    [CHAIN.TAIKO]: { fetch: getUniV3LogAdapter({ factory: '0xBa90FC740a95A6997306255853959Bb284cb748a', ...config }), start: 1724943360 },
   },
 }
 
