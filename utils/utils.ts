d416c51966c0bd8c93bdd86e9e53cdeff2d93bd5
Eden <noat.eth@gmail.com>
2025-08-14 19:05:42 +0700

    setup gearbox fees adapter using on-chain logs

--- utils/utils.ts
+++ utils/utils.ts
@@ -5,3 +5,11 @@ export async function sleep(time: number) {
 export function formatAddress(address: any): string {
   return String(address).toLowerCase();
 }
+
+export function evmAddressToEventTopic(address: string): string {
+  if (address[0] === '0' && address[1] === 'x') {
+    return `0x000000000000000000000000${formatAddress(address).slice(2)}`
+  } else {
+    return `0x000000000000000000000000${formatAddress(address)}`
+  }
+}
