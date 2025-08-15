900ca62e2e2289c16c13c0eb5b26b743a60e2962
Eden <noat.eth@gmail.com>
2025-08-16 02:24:13 +0700

    fix broken subgraphs, using logs

--- dexs/datadex/index.ts
+++ dexs/datadex/index.ts
@@ -1,29 +1,11 @@
-// vana
+import { CHAIN } from '../../helpers/chains'
+import { uniV2Exports } from '../../helpers/uniswap'
 
-import { SimpleAdapter } from "../../adapters/types";
-import { DEFAULT_TOTAL_VOLUME_FIELD, getChainVolume2 } from "../../helpers/getUniSubgraphVolume";
-import { CHAIN } from "../../helpers/chains";
-
-const endpoints = {
-  [CHAIN.VANA]: "https://api.goldsky.com/api/public/project_clnbo3e3c16lj33xva5r2aqk7/subgraphs/data-dex-vana/prod/gn",
-};
-
-const graphs = getChainVolume2({
-  graphUrls: endpoints,
-  totalVolume: {
-    factory: "factories",
-    field: DEFAULT_TOTAL_VOLUME_FIELD,
-  },
-});
-
-const adapter: SimpleAdapter = {
-  version: 2,
-  adapter: {
-    [CHAIN.VANA]: {
-      fetch: graphs(CHAIN.VANA),
-      start: '2024-12-03',
+export default {
+  ...uniV2Exports({
+    [CHAIN.VANA]: { 
+      factory: '0xc2a0d530e57B1275fbce908031DA636f95EA1E38', 
+      fees: 0.003, // 0.3% fee on swaps
     },
-  },
-};
-
-export default adapter;
+  }),
+}
