7dbef7ce4f8ffaf62f2f194552a48d3b096216a2
Zdeadex <51711900+zdeadex@users.noreply.github.com>
2025-08-15 14:52:05 +0200

    WinnieSwap dexs - TBC
    winnieswap dexs folder and index.ts
    To be completed

---
+++ dexs/winnieswap/index.ts
@@ -0,0 +1,62 @@
+import { FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
+import request, { gql } from "graphql-request";
+import { CHAIN } from "../../helpers/chains";
+
+const GRAPH_URL = ''
+
+const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
+    const query = gql`
+        query q{
+            tokenDayDatas(where: {date: ${options.startOfDay}}, first: 1000, orderBy: volumeUSD, orderDirection: desc) {
+                volumeUSD
+                feesUSD
+            }
+        }
+    `
+
+    const data = await request(GRAPH_URL, query)
+    const dailyVolume = options.createBalances()
+    const dailyFees = options.createBalances()
+
+    data.tokenDayDatas.forEach((e: any) => {
+        dailyVolume.addUSDValue(Number(e.volumeUSD))
+        dailyFees.addUSDValue(Number(e.feesUSD))
+    })
+
+    const dailyRevenue = dailyFees.clone(0)
+    const dailyProtocolRevenue = dailyFees.clone(0)
+    const dailySupplySideRevenue = dailyFees.clone(1)
+    const dailyHoldersRevenue = dailyFees.clone(0)
+
+    return {
+        dailyVolume,
+        dailyFees,
+        dailyUserFees: dailyFees,
+        dailyRevenue,
+        dailyProtocolRevenue,
+        dailySupplySideRevenue,
+        dailyHoldersRevenue,
+    }
+}
+
+const methodology = {
+    Fees: "Total swap fees paid by users.",
+    Revenue: "0% protocol revenue share and 0% holders revenue share.",
+    ProtocolRevenue: "0% of fees collected by the protocol.",
+    SupplySideRevenue: "100% of fees distributed to LPs.",
+    HoldersRevenue: "0% of fees used for buy-back and burn.",
+    UserFees: "Total swap fees paid by users."
+}
+
+const adapter: SimpleAdapter = {
+    version: 2,
+    adapter: {
+        [CHAIN.BERACHAIN]: {
+            fetch,
+            start: '2025-07-07',
+            meta: { methodology }
+        }
+    }
+}
+
+export default adapter
