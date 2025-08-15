97bc6b81916bdcd9ec112a1733fb6e619b61d44f
Eden <noat.eth@gmail.com>
2025-08-15 17:35:09 +0700

    add fees

--- dexs/quickswap-v4.ts
+++ dexs/quickswap-v4.ts
@@ -1,15 +1,27 @@
+import { SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { uniV3Exports } from "../helpers/uniswap";
+import { getUniV3LogAdapter } from "../helpers/uniswap";
 
-const algebraV3PoolCreatedEvent = 'event Pool (address indexed token0, address indexed token1, address pool)'
-const algebraV3SwapEvent = 'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 price, uint128 liquidity, int24 tick, uint24 overrideFee, uint24 pluginFee)'
+const poolCreatedEvent = 'event Pool (address indexed token0, address indexed token1, address pool)'
+const swapEvent = 'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 price, uint128 liquidity, int24 tick, uint24 overrideFee, uint24 pluginFee)'
 
-export default uniV3Exports({
-  [CHAIN.BASE]: {
-    factory: "0xC5396866754799B9720125B104AE01d935Ab9C7b",
-    poolCreatedEvent: algebraV3PoolCreatedEvent,
-    swapEvent: algebraV3SwapEvent,
-    isAlgebraV3: true,
-    start: '2025-08-12',
-  },
-})
+const config = {
+  isAlgebraV3: true,
+  poolCreatedEvent,
+  swapEvent,
+  userFeesRatio: 1,
+  revenueRatio: 0, // 100% fees to LPs
+  protocolRevenueRatio: 0, // 100% fees to LPs
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.BASE]: {
+      fetch: getUniV3LogAdapter({ factory: '0xC5396866754799B9720125B104AE01d935Ab9C7b', ...config }),
+      start: '2025-08-12',
+    },
+  }
+}
+
+export default adapter;
