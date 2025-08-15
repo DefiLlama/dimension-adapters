2ddd9a7d09a7d24680ac895bde32c15af1a77fad
Eden <noat.eth@gmail.com>
2025-08-15 23:59:57 +0700

    refactor codes

--- fees/zapfi/index.ts
+++ fees/zapfi/index.ts
@@ -3,70 +3,59 @@ import { FetchOptions } from "../../adapters/types";
 import ADDRESSES from '../../helpers/coreAssets.json'
 import { CHAIN } from "../../helpers/chains";
 
-const withdrawABI = 'event Withdrawal(address to,bytes32 nullifierHash,address indexed relayer,uint256 fee)'
-
-const ZAPFI_CONTRACTS = {
-    [CHAIN.ARBITRUM]: {
-      ETH: {
-        pools: ['0x12A9C918008686b5dA394D127d57eC188729BA82'],
-        token: ADDRESSES.null // Native ETH 
-      },
-      USDT: {
-        pools: ['0xB3bE18D9264C0F51c1aed3f8823387f3F2Eb15d1'],
-        token: ADDRESSES.arbitrum.USDT
-      },
-    }
+interface IPool {
+  poolAddress: string;
+  token: string;
 }
 
-interface ZapfiPoolData {
-    pools: string[];
-    token: string;
+const ZAPFI_CONTRACTS: {[key: string]: Array<IPool>} = {
+  [CHAIN.ARBITRUM]: [
+    {
+      poolAddress: '0x12A9C918008686b5dA394D127d57eC188729BA82',
+      token: ADDRESSES.null,
+    },
+    {
+      poolAddress: '0xB3bE18D9264C0F51c1aed3f8823387f3F2Eb15d1',
+      token: ADDRESSES.arbitrum.USDT,
+    },
+  ],
 }
 
-const getFees = async ({getLogs, chain, createBalances}: FetchOptions) => {
-    const fees: { [token: string]: number } = {};
+const WithdrawalEvent = 'event Withdrawal(address to,bytes32 nullifierHash,address indexed relayer,uint256 fee)'
 
-    for (const [token, data] of Object.entries(ZAPFI_CONTRACTS[chain]) as [string, ZapfiPoolData][]) {
-        const feesPaid = await getLogs({
-            targets: data.pools,
-            eventAbi: withdrawABI,
-        });
-        fees[data.token] = feesPaid.reduce((sum, log) => sum + Number(log.fee), 0);
-    }
+const fetch = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances()
 
-    const dailyFees = createBalances();
-    for (const [token, fee] of Object.entries(fees)) {
-        dailyFees.add(token, fee);
+  for (const pool of ZAPFI_CONTRACTS[options.chain]) {
+    const events = await options.getLogs({
+      eventAbi: WithdrawalEvent,
+      target: pool.poolAddress,
+    });
+
+    for (const event of events) {
+      dailyFees.add(pool.token, event.fee)
     }
-    
-    return {dailyFees};
+  }
+
+  return {
+    dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
+  }
 }
 
 const methodology = {
-    Fees: "All fees that are paid by users",
+  Fees: "All fees that are paid by users by depositing in privacy pools",
+  Revenue: "All fees are collected as revenue",
+  ProtocolRevenue: "All fees are collected by protocol",
 }
 
 const adapter: Adapter = {
   version: 2,
-  adapter: {
-    [CHAIN.ARBITRUM]: {
-      fetch: getFees,
-      start: '2025-08-12',
-      meta: {
-        methodology
-      }
-    },
-    // [CHAIN.ARBITRUM]: {
-    //   fetch: getFeesExport(ArbitrumAddress, [event_withdrawal]),
-    //   start: '2025-08-12',
-    //   meta: {
-    //     methodology: {
-    //       Fees: "Fees paid by users when transfer out from protocol.",
-    //       Revenue: "Fees earn by protocol.",
-    //     }
-    //   }
-    // },
-  },
+  methodology,
+  fetch,
+  chains: [CHAIN.ARBITRUM],
+  start: '2025-08-12',
 }
 
 export default adapter;
