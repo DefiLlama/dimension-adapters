8ebc2a7a22c33453ed559fd459a6d8590eb37cb4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-16 13:44:09 +0200

    fix osmosis

--- fees/osmosis.ts
+++ fees/osmosis.ts
@@ -1,7 +1,6 @@
 import { Adapter, FetchOptions } from "../adapters/types";
 import fetchURL from "../utils/fetchURL";
 import { CHAIN } from "../helpers/chains";
-import { getTimestampAtStartOfDayUTC } from "../utils/date";
 
 interface IChartItem {
   timestamp: string;
@@ -9,18 +8,15 @@ interface IChartItem {
   dailyRevenue: number;
 }
 
-const fetch = async (_a:any, _b:any, options: FetchOptions) => {
+const fetch = async (_a:any, _b:any, { dateString }: FetchOptions) => {
   const feeEndpoint = `https://public-osmosis-api.numia.xyz/external/defillama/chain_fees_and_revenue`;
   const historicalFees: IChartItem[] = await fetchURL(feeEndpoint);
 
-  const dayTimestamp = getTimestampAtStartOfDayUTC(options.startOfDay);
-  const dateStr = new Date(dayTimestamp * 1000).toISOString().split('T')[0];
-
   const dayData = historicalFees.find(feeItem => 
-    feeItem.timestamp.split(' ')[0] === dateStr
+    feeItem.timestamp.split(' ')[0] === dateString
   );
   if (!dayData) {
-    throw new Error(`No data found for ${dateStr}`);
+    throw new Error(`No data found for ${dateString}`);
   }
 
   return {
