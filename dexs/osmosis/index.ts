80d902cb0fdd4ba254f1d7e3d06fd1797e8c6ab0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-16 13:44:09 +0200

    fix osmosis

--- dexs/osmosis/index.ts
+++ dexs/osmosis/index.ts
@@ -1,6 +1,5 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 import fetchURL from "../../utils/fetchURL"
 
 const historicalVolumeEndpoint = "https://public-osmosis-api.numia.xyz/volume/historical/chart"
@@ -10,36 +9,22 @@ interface IChartItem {
   value: number
 }
 
-const fetch = async (timestamp: number, _at: any, options: FetchOptions) => {
-  const dayTimestamp = getTimestampAtStartOfDayUTC(options.startOfDay)
+const fetch = async (timestamp: number, _at: any, { dateString }: FetchOptions) => {
   const historicalVolume: IChartItem[] = (await fetchURL(historicalVolumeEndpoint));
 
-  const dateStr = new Date(timestamp * 1000).toISOString().split('T')[0];
-
-  const totalVolume = historicalVolume
-    .filter(volItem => (new Date(volItem.time).getTime() / 1000) <= dayTimestamp)
-    .reduce((acc, { value }) => acc + value, 0)
-
   const dailyVolume = historicalVolume
-    .find(dayItem => dayItem.time.split('T')[0] === dateStr)?.value
+    .find(dayItem => dayItem.time.split('T')[0] === dateString)?.value
 
     return {
-      totalVolume: totalVolume,
       dailyVolume: dailyVolume,
   };
 };
-
-const getStartTimestamp = async () => {
-  const historicalVolume: IChartItem[] = (await fetchURL(historicalVolumeEndpoint))
-  return (new Date(historicalVolume[0].time).getTime()) / 1000
-}
 
 const adapter: SimpleAdapter = {
   version: 1,
   adapter: {
     [CHAIN.OSMOSIS]: {
       fetch,
-      // runAtCurrTime: true,
       start: "2022-04-15",
     },
   },
