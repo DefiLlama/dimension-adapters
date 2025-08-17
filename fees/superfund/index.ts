05308f3c4e27ca080ab67383abfdb2beb46be10e
Eden <noat.eth@gmail.com>
2025-08-17 23:18:08 +0700

    refactor codes

--- fees/superfund/index.ts
+++ fees/superfund/index.ts
@@ -1,44 +1,35 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
+// https://docs.superlend.xyz/superlend-vaults/superfunds
+const PROTOCOL_FEE = 0.1; // 10%
+
 const SUPERLEND_USD = "0x10076ed296571cE4Fde5b1FDF0eB9014a880e47B";
-const PRICE_ABI =
-  "function convertToAssets(uint256 _shares) external view returns (uint256)";
+const USDC = "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913";
+const PRICE_ABI = "function convertToAssets(uint256 _shares) external view returns (uint256)";
 
 const fetch = async (options: FetchOptions) => {
-  const totalSupply =
-    (await options.api.call({
-      abi: "uint256:totalSupply",
-      target: SUPERLEND_USD,
-    })) / 1e6;
-
-  const slUsdPriceYesterday =
-    (await options.fromApi.call({
-      abi: PRICE_ABI,
-      target: SUPERLEND_USD,
-      params: ["1000000000000000000"],
-    })) / 1e18;
-
-  const slUsdPriceToday =
-    (await options.toApi.call({
-      abi: PRICE_ABI,
-      target: SUPERLEND_USD,
-      params: ["1000000000000000000"],
-    })) / 1e18;
-
-  const dailyYield = (slUsdPriceToday - slUsdPriceYesterday) * totalSupply;
-
   const dailyFees = options.createBalances();
-  dailyFees.addUSDValue(dailyYield / 0.9);
-
   const dailyRevenue = options.createBalances();
-  dailyRevenue.add(dailyFees.clone(0.1));
 
+  const totalAssets = await options.api.call({ abi: "uint256:totalAssets", target: SUPERLEND_USD })
+  const slUsdPriceYesterday = await options.fromApi.call({ abi: PRICE_ABI, target: SUPERLEND_USD, params: ["1000000000000000000"] })
+  const slUsdPriceToday = await options.toApi.call({ abi: PRICE_ABI, target: SUPERLEND_USD, params: ["1000000000000000000"] })
+
+  const totalYieldIncludeProtocolFees = (slUsdPriceToday - slUsdPriceYesterday) * totalAssets / (1 - PROTOCOL_FEE) / 1e18;
+  const protocolFees = totalYieldIncludeProtocolFees * PROTOCOL_FEE;
+
+  dailyFees.add(USDC, totalYieldIncludeProtocolFees)
+  dailyRevenue.add(USDC, protocolFees)
+
+  const dailySupplySideRevenue = dailyFees.clone()
+  dailySupplySideRevenue.subtract(dailyRevenue)
+  
   return {
     dailyFees,
     dailyRevenue,
     dailyProtocolRevenue: dailyRevenue,
-    dailySupplySideRevenue: dailyFees.clone(0.9),
+    dailySupplySideRevenue,
   };
 };
 
