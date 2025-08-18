4b14c380eeeaf4cdc7d590be6387b1291c64819f
okorieebube <okorieebube1@gmail.com>
2025-08-18 19:41:30 +0100

    update moonpiefun treasury

--- fees/moonpiefun.ts
+++ fees/moonpiefun.ts
@@ -3,7 +3,7 @@ import { CHAIN } from "../helpers/chains";
 import { addTokensReceived } from "../helpers/token";
 import ADDRESSES from "../helpers/coreAssets.json";
 
-const TREASURY = "0x31995B7ea0D0ec85e9c72C903AF0F29acF3622F2".toLowerCase();
+const TREASURY = "0x86039dc5084358863d3D69C0c24C40b0b6Cf9130".toLowerCase();
 const USDT = '0x26E490d30e73c36800788DC6d6315946C4BbEa24'; // or the USDT address for your chain
 
 
