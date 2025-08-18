354ae164772d971bd61a26d3b1ba18832690b089
Eden <noat.eth@gmail.com>
2025-08-18 16:51:37 +0700

    refactor codes

--- dexs/bullbit-ai/index.ts
+++ dexs/bullbit-ai/index.ts
@@ -2,6 +2,8 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getETHReceived } from "../../helpers/token";
 
+const FIXED_PLATFORM_FEE = 1 // 1%
+
 const feeReceiverMultisig = [
   "0x87D30c1a5a79b060d7F6FBEa7791c381a2aFc7Ad",
 ]
@@ -15,21 +17,26 @@ const fetch: any = async (options: FetchOptions) => {
     options
   });
 
-  const dailyVolume = dailyFees.resizeBy(100) // because of 1% fixed platform fee as per docs
+  const dailyVolume = dailyFees.clone(100 / FIXED_PLATFORM_FEE)
 
   return {
-    dailyVolume: dailyVolume
+    dailyVolume: dailyVolume,
+    dailyFees: dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
   }
 }
 
 const adapter: SimpleAdapter = {
   version: 2,
-  adapter: {
-    [CHAIN.BSC]: {
-      fetch,
-      start: '2025-16-08',
-    },
+  methodology: {
+    Fees: 'All fees paid by users for launching, trading tokens.',
+    Revenue: 'All fees collected by bullbit.ai protocol.',
+    ProtocolRevenue: 'All fees collected by bullbit.ai protocol.',
   },
+  fetch,
+  chains: [CHAIN.BSC],
+  start: '2025-08-16',
 }
 
 export default adapter
