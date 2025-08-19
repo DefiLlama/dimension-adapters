dab9548c82b649d9991a6768feda63a110805b7c
Abdullah Abro <kamario.abdullah@gmail.com>
2025-08-19 15:42:38 +0500

    Add revenue from API for Bluefin Alphalend (#4061)
    * Add revenue from API for Bluefin Alphalend
    
    * alphalend missing metrics
    
    ---------
    
    Co-authored-by: treeoflife2 <sol.analyzoor@gmail.com>

--- fees/bluefin-alphalend.ts
+++ fees/bluefin-alphalend.ts
@@ -1,35 +1,38 @@
 import fetchURL from "../utils/fetchURL";
-import { FetchResultFees, SimpleAdapter } from "../adapters/types";
+import { SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
-const fetch = async (_: number): Promise<FetchResultFees> => {
-  const data = await fetchURL(
-    "https://lend.api.sui-prod.bluefin.io/api/v1/fees/daily"
-  );
-  const dailyFees = Number(data.fees);
-  const dailyRevenue = Number(data.fees);
+const fetch = async (_: any) => {
+  const data = await fetchURL("https://lend.api.sui-prod.bluefin.io/api/v1/fees/daily");
+  const dailyFees = Number(data.fees || 0);
+  const dailyRevenue = Number(data.revenue || 0);
+  const dailySupplySideRevenue = Number(dailyFees - dailyRevenue);
 
   return {
     dailyFees,
+    dailyUserFees: dailyFees,
     dailyRevenue,
+    dailyHoldersRevenue: '0',
+    dailyProtocolRevenue: dailyRevenue,
+    dailySupplySideRevenue
   };
 };
 
+const methodology = {
+  Fees: "All fees paid/earned while using lending/borrowing and liquidation.",
+  Revenue: "Fees collected by protocol native markets.",
+  ProtocolRevenue: "Fees/liquidation collected by protocol.",
+  SupplySideRevenue: "Fees going to lenders.",
+  HoldersRevenue: "No holders revenue.",
+};
+
 const adapter: SimpleAdapter = {
-  methodology: {
-    Fees:
-      "All fees paid/earned while using lending/borrowing and liquidation.",
-    Revenue: "Fees collected by protocol native markets.",
-    ProtocolRevenue: "Fees/liquidation collected by protocol.",
-  },
-  version: 1,
-  adapter: {
-    [CHAIN.SUI]: {
-      fetch,
-      start: "2025-06-17",
-      runAtCurrTime: true,
-    },
-  },
+  version: 2,
+  fetch,
+  methodology,
+  chains: [CHAIN.SUI],
+  start: '2025-06-17',
+  runAtCurrTime: true,
 };
 
 export default adapter;
