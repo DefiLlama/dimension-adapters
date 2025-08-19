206c409a84f44fc38a7f89af726265c3837bf5f0
prevostc <998369+prevostc@users.noreply.github.com>
2025-08-19 18:09:54 +0200

    Use events to infer fees

--- fees/cap-money/index.ts
+++ fees/cap-money/index.ts
@@ -1,74 +1,120 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
-import { addTokensReceived } from '../../helpers/token';
+import { addTokensReceived } from "../../helpers/token";
 import { CHAIN } from "../../helpers/chains";
-import { capConfig } from "./config";
-import { fetchAssetAddresses, fetchVaultConfigs } from "./helpers";
+import { capABI, capConfig } from "./lib/config";
+import { fetchAssetAddresses, fetchVaultConfigs } from "./lib/helpers";
 
 const fetch = async (options: FetchOptions) => {
     const infra = capConfig[options.chain].infra;
-    const tokens = capConfig[options.chain].tokens;
-
     const assetAddresses = await fetchAssetAddresses(options, options.chain);
     const vaultConfigs = await fetchVaultConfigs(options);
 
-    const senders = [tokens.cUSD.address, tokens.stcUSD.address, infra.lender.address, infra.delegation.address];
-    const watchedTokens = [tokens.cUSD.address, tokens.stcUSD.address, ...assetAddresses];
+    const feesDistributedLogs = (
+        await Promise.all(
+            vaultConfigs
+                .map((vaultConfig) =>
+                    vaultConfig.interestReceivers.map(async (interestReceiver) => {
+                        const logs = await options.getLogs({
+                            target: interestReceiver,
+                            eventAbi: capABI.FeeReceiver.FeesDistributedEvent,
+                        });
 
-    const borrowFees = await addTokensReceived({
-        options,
-        fromAdddesses: senders,
-        tokens: watchedTokens,
-        targets: vaultConfigs.map(i => i.interestReceivers).flat()
-    })
+                        return logs.map((log) => ({
+                            feeAsset: vaultConfig.vault, // fee is collected in vault assets
+                            amount: log.amount,
+                        }));
+                    }),
+                )
+                .flat(),
+        )
+    ).flat();
+    const minterFees = options.createBalances();
+    for (const { feeAsset, amount } of feesDistributedLogs) {
+        minterFees.add(feeAsset, amount);
+    }
 
-    const restakerFees = await addTokensReceived({
-        options,
-        fromAdddesses: senders,
-        tokens: watchedTokens,
+    const protocolFeeClaimedLogs = (
+        await Promise.all(
+            vaultConfigs
+                .map((vaultConfig) =>
+                    vaultConfig.interestReceivers.map(async (interestReceiver) => {
+                        const logs = await options.getLogs({
+                            target: interestReceiver,
+                            eventAbi: capABI.FeeReceiver.ProtocolFeeClaimed,
+                        });
+
+                        return logs.map((log) => ({
+                            feeAsset: vaultConfig.vault, // fee is collected in vault assets
+                            amount: log.amount,
+                        }));
+                    }),
+                )
+                .flat(),
+        )
+    ).flat();
+    const protocolFees = options.createBalances();
+    for (const { feeAsset, amount } of protocolFeeClaimedLogs) {
+        protocolFees.add(feeAsset, amount);
+    }
+
+    const restakerFeesLogs = await options.getLogs({
         target: infra.delegation.address,
-    })
+        eventAbi: capABI.Delegation.DistributeReward,
+    });
+    const restakerFees = options.createBalances();
+    for (const log of restakerFeesLogs) {
+        restakerFees.add(log.asset, log.amount);
+    }
 
-    const insuranceFunds = vaultConfigs.map(i => i.insuranceFund).filter(i => i !== null);
-    const insuranceFundFees = insuranceFunds.length > 0
-        ? await addTokensReceived({
-            options,
-            fromAdddesses: senders,
-            tokens: watchedTokens,
-            targets: insuranceFunds,
-        })
-        : options.createBalances();
+    const insuranceFunds = vaultConfigs
+        .map((i) => i.insuranceFund)
+        .filter((i) => i !== null);
+    const minters = vaultConfigs.map((i) => i.vault);
+    const insuranceFundFees =
+        insuranceFunds.length > 0
+            ? await addTokensReceived({
+                options,
+                fromAdddesses: minters,
+                tokens: assetAddresses,
+                targets: insuranceFunds,
+            })
+            : options.createBalances();
 
     const dailyFees = options.createBalances();
-    dailyFees.addBalances(borrowFees);
+    dailyFees.addBalances(minterFees);
+    dailyFees.addBalances(protocolFees);
     dailyFees.addBalances(restakerFees);
     dailyFees.addBalances(insuranceFundFees);
 
     // no revenue for now
     const dailyRevenue = options.createBalances();
+    dailyRevenue.addBalances(protocolFees);
 
     const dailySupplySideRevenue = options.createBalances();
-    dailySupplySideRevenue.addBalances(borrowFees);
+    dailySupplySideRevenue.addBalances(minterFees);
     dailySupplySideRevenue.addBalances(restakerFees);
+    dailySupplySideRevenue.addBalances(insuranceFundFees);
 
     return {
         dailyFees,
         dailyRevenue,
         dailySupplySideRevenue,
-    }
+    };
 };
 
 const methodology = {
-    Fees: 'All fees paid by users for either borrowing (borrow fees + restaker fees) or minting (insurance fund fees).',
-    Revenue: 'Revenue is not collected yet.',
-    SupplySideRevenue: 'Borrow fees distributed to stakers and restaker fees are distributed to delegators.',
-}
+    Fees: "All fees paid by users for either borrowing (borrow fees + restaker fees) or minting (insurance fund fees).",
+    Revenue: "Revenue is not collected yet.",
+    SupplySideRevenue:
+        "Borrow fees distributed to stakers and restaker fees are distributed to delegators.",
+};
 
 const adapter: SimpleAdapter = {
     version: 2,
     fetch,
     chains: [CHAIN.ETHEREUM],
     start: capConfig[CHAIN.ETHEREUM].fromDate,
-    methodology
-}
+    methodology,
+};
 
 export default adapter;
