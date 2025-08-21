9160b47122e9b76ddf213c9f5b00f1ffcc6eb30e
Eden <noat.eth@gmail.com>
2025-08-21 15:45:03 +0700

    fix datadex export

--- dexs/datadex/index.ts
+++ dexs/datadex/index.ts
@@ -1,11 +1,8 @@
 import { CHAIN } from '../../helpers/chains'
-import { uniV2Exports } from '../../helpers/uniswap'
+import { uniV3Exports } from '../../helpers/uniswap'
 
 export default {
-  ...uniV2Exports({
-    [CHAIN.VANA]: { 
-      factory: '0xc2a0d530e57B1275fbce908031DA636f95EA1E38', 
-      fees: 0.003, // 0.3% fee on swaps
-    },
+  ...uniV3Exports({
+    [CHAIN.VANA]: { factory: '0xc2a0d530e57B1275fbce908031DA636f95EA1E38' },
   }),
 }
