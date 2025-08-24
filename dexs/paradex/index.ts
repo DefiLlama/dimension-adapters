23a7f165f57fae3f5a13b4012949e1aaff52f87c
Eden <noat.eth@gmail.com>
2025-08-24 18:31:25 +0700

    fix paradex-perp-volume

--- dexs/paradex/index.ts
+++ dexs/paradex/index.ts
@@ -4,7 +4,7 @@ import { CHAIN } from "../../helpers/chains";
 
 // const historicalVolumeEndpoint = (market: string, start: number, end: number) => `https://api.prod.paradex.trade/v1/markets/summary?market=${market}&start=${start}&end=${end}`
 // const marketsEndpoint = "https://api.prod.paradex.trade/v1/markets"
-const volumeEndpoint = 'https://tradeparadigm.metabaseapp.com/api/public/dashboard/e4d7b84d-f95f-48eb-b7a6-141b3dcef4e2/dashcard/7734/card/6988?parameters=%5B%5D'
+const volumeEndpoint = 'https://tradeparadigm.metabaseapp.com/api/public/dashboard/e4d7b84d-f95f-48eb-b7a6-141b3dcef4e2/dashcard/10655/card/9655?parameters=%5B%5D'
 
 
 interface IVolumeData {
@@ -18,7 +18,7 @@ const fetch = async (timestamp: number): Promise<FetchResultVolume> => {
 
   const volumesData = await fetchURL(volumeEndpoint) as IVolumeData
   const timestampStr = new Date(timestamp * 1000).toISOString().split('T')[0] + "T00:00:00Z"
-  const dailyVolume = volumesData.data.rows.find(row => ((row[0] === timestampStr) && (row[1] === 'Perp')))?.[2]
+  const dailyVolume = volumesData.data.rows.find(row => ((row[0] === timestampStr)))?.[2]
   if (!dailyVolume) throw new Error('record missing!')
 
     return { 
