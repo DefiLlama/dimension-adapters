4dc616ce41e3a502669c43a4e60e1a4a687a30d8
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-08-24 23:31:26 +0530

    remove dune dependency for boros, remove redundant event calls

--- dexs/boros/abi.ts
+++ dexs/boros/abi.ts
@@ -1,12 +1,6 @@
 export const abi = {
     MARKET_CREATION_EVENT: 'event MarketCreated(address market,tuple(string name,string symbol,bool k_isIsolatedOnly, uint32 k_maturity,uint16 k_tokenId,uint24 k_marketId,uint8 k_tickStep, uint16 k_iTickThresh) immData, tuple(uint16 maxOpenOrders, address markRateOracle, address fIndexOracle, uint128 hardOICap, uint64 takerFee, uint64 otcFee, tuple(uint64 base,uint64 slope,uint64 feeRate)liqSettings,uint64 kIM, uint64 kMM,uint32 tThresh, uint16 maxRateDeviationFactorBase1e4, uint16 closingOrderBoundBase1e4, int16 loUpperConstBase1e4, int16 loUpperSlopeBase1e4, int16 loLowerConstBase1e4, int16 loLowerSlopeBase1e4, uint8 status, bool useImpliedAsMarkRate) config)',
 
-    SWAP_EVENT: 'event Swap (int256 sizeOut, int256 costOut, uint256 fee)',
-
-    ORDER_FILLED_EVENT: 'event LimitOrderFilled (uint64 from, uint64 to)',
-
-    AMM_CREATION_EVENT: 'event AMMCreated(address amm,bool isPositive,tuple(uint24 ammId, string name,string symbol,address router, address market,uint32 oracleImpliedRateWindow, uint64 feeRate, uint256 totalSupplyCap,bytes26 seeder,address permissionController) createParams,tuple(uint256 minAbsRate,uint256 maxAbsRate, uint256 cutOffTimestamp,uint256 initialAbsRate,int256 initialSize,uint256 flipLiquidity, uint256 initialCash) seedParams)',
-
     MARKET_ORDERS_FILLED_EVENT: 'event MarketOrdersFilled (bytes26 user, uint256 totalTrade, uint256 totalFees)',
 
     OTC_SWAP_EVENT: 'event OtcSwap (bytes26 user, bytes26 counterParty, uint256 trade, int256 cashToCounter, uint256 otcFee)',
--- dexs/boros/index.ts
+++ dexs/boros/index.ts
@@ -1,16 +1,11 @@
 import { SimpleAdapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getSqlFromFile, queryDuneSql } from "../../helpers/dune";
 import { abi } from "./abi";
 
 const BOROS_FACTORY = '0x3080808080Ee6a795c1a6Ff388195Aa5F11ECeE0';
-const AMM_CREATION_FACTORY = '0x3205e972714B52512c837AE6f5FCFDeB07f0f23C';
-const TOPICS = {
-    LIMIT_ORDER_PLACED: '0x7a1823ff8473ae353f7ac7587b085e7544b1e3cc8f87c33c504af60fe5111471',
-    PARTIALLY_FILLED: '0x48dc6d310fa6f4ef65aacba36e1aad2df2296c7024eb71845d8e2b7c76c6e852',
-    LIMIT_ORDER_CANCELED: '0x2c85ce2db412cbd774172130804b8f3851259af67fb84249f5d0254031be8116'
-};
 const firstMarketCreationBlock = 362039621; // 2025-07-27
+const TWO_128 = 1n << 128n;
+const TWO_127 = 1n << 127n;
 
 interface BorosMarket {
     address: string;
@@ -18,18 +13,15 @@ interface BorosMarket {
     maturity: number;
 };
 
-const marketsRecord: Record<string, BorosMarket> = {};
-
 const SYMBOL_TO_CGID = {
     'ETH': 'ethereum',
     'BTC': 'bitcoin',
 };
 
-const getCgId = (marketSymbol: string, marketType: string) => {
-    const symbolPosition = marketType === 'orderbook' ? 1 : 2;
+const getCgId = (marketSymbol: string) => {
     let symbol = "";
     try {
-        symbol = marketSymbol.split("-")[symbolPosition].replace("USDT", "");
+        symbol = marketSymbol.split("-")[1].replace("USDT", "");
     } catch (error) {
         console.error("Failed to extract base asset from symbol:", symbol, error);
         return null;
@@ -59,46 +51,35 @@ const fetch = async (options: FetchOptions) => {
             symbol: marketLog.immData.symbol,
             maturity: Number(marketLog.immData.k_maturity),
         };
-        marketsRecord[market.address] = market;
         if (market.maturity < options.fromTimestamp) continue;
-        const cgId = getCgId(market.symbol, 'orderbook');
+        const cgId = getCgId(market.symbol);
         if (!cgId) continue;
-
-        const orderFilledLogs = await options.getLogs({
-            target: market.address,
-            eventAbi: abi.ORDER_FILLED_EVENT,
-        });
-        let filledOrders: bigint[] = [];
-        orderFilledLogs.forEach((order: [bigint, bigint]) => {
-            for (let i = order[0]; i <= order[1]; i++) {
-                filledOrders.push(i);
-            }
-        });
-        if (filledOrders.length == 0) continue;
-        const decimalOrderIds = filledOrders.map(id => `CAST('${id}' AS DECIMAL(38,0))`).join(', ');
-
-        const duneQuery = getSqlFromFile('helpers/queries/boros.sql', {
-            initialBlock: firstMarketCreationBlock, address: market.address, decimalOrderIds, orderPlacedTopic: TOPICS.LIMIT_ORDER_PLACED, partiallyFilledTopic: TOPICS.PARTIALLY_FILLED, orderCanceledTopic: TOPICS.LIMIT_ORDER_CANCELED, startTimestamp: options.fromTimestamp, endTimestamp: options.toTimestamp
-        });
-        const res = await queryDuneSql(options, duneQuery);
-        const executedOrderSize = res.length > 0 ? res[0].executed_order_size : 0;
-        dailyVolume.addCGToken(cgId, executedOrderSize);
 
         const marketOrderFilledLogs = await options.getLogs({
             target: market.address,
             eventAbi: abi.MARKET_ORDERS_FILLED_EVENT,
         });
+
         marketOrderFilledLogs.forEach(trade => {
+            let tradeAmount = trade.totalTrade >> 128n;
+            if (tradeAmount > TWO_127)
+                tradeAmount = TWO_128 - tradeAmount;
+            dailyVolume.addCGToken(cgId, Number(tradeAmount) / 1e18);
             dailyFees.addCGToken(cgId, Number(trade.totalFees) / 1e18);
-        })
+        });
 
         const otcSwapLogs = await options.getLogs({
             target: market.address,
             eventAbi: abi.OTC_SWAP_EVENT,
         });
-        otcSwapLogs.forEach(trade => {
-            dailyFees.addCGToken(cgId, Number(trade.otcFee) / 1e18);
-            dailySupplySideRevenue.addCGToken(cgId, Number(trade.otcFee) / 1e18)
+
+        otcSwapLogs.forEach(swap => {
+            let tradeAmount = swap.trade >> 128n;
+            if (tradeAmount > TWO_127)
+                tradeAmount = TWO_128 - tradeAmount;
+            dailyVolume.addCGToken(cgId, Number(tradeAmount) / 1e18);
+            dailyFees.addCGToken(cgId, Number(swap.otcFee) / 1e18);
+            dailySupplySideRevenue.addCGToken(cgId, Number(swap.otcFee) / 1e18)
         })
 
         const paymentSettlementLogs = await options.getLogs({
@@ -109,31 +90,6 @@ const fetch = async (options: FetchOptions) => {
             dailyFees.addCGToken(cgId, Number(settlement.fees) / 1e18);
         });
     };
-    const ammCreationLogs = await options.getLogs({
-        target: AMM_CREATION_FACTORY,
-        eventAbi: abi.AMM_CREATION_EVENT,
-        fromBlock: firstMarketCreationBlock
-    });
-
-    for (const ammLog of ammCreationLogs) {
-        const parentMarket = ammLog.createParams.market;
-        const maturity = marketsRecord[parentMarket]?.maturity;
-        if (!maturity || maturity < options.fromTimestamp) continue;
-        const address = ammLog.amm;
-        const ammTradeLogs = await options.getLogs({
-            target: address,
-            eventAbi: abi.SWAP_EVENT,
-        });
-
-        if (ammTradeLogs.length == 0) continue;
-        const cgId = getCgId(ammLog.createParams.symbol, 'amm');
-        if (!cgId) continue;
-        ammTradeLogs.forEach(trade => {
-            const swapAmount = trade[0] > 0 ? trade[0] : -trade[0];
-            dailyVolume.addCGToken(cgId, swapAmount / BigInt(1e18));
-        });
-    }
-
     let dailyRevenue = dailyFees.clone();
     dailyRevenue.subtract(dailySupplySideRevenue);
 
