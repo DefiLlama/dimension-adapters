eedbe6ea424da181c3b9f95c2f071543c5810d62
buchaoqun <bu736554084@163.com>
2025-08-26 09:53:53 +0800

    Modify the logic.

--- fees/solv-finance/index.ts
+++ fees/solv-finance/index.ts
@@ -1,14 +1,13 @@
 import { Chain } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { FetchOptions, FetchV2, SimpleAdapter } from "../../adapters/types";
-import { addGasTokensReceived, addTokensReceived, getSolanaReceived } from "../../helpers/token";
+import { addGasTokensReceived, addTokensReceived } from "../../helpers/token";
 import { Balances } from "@defillama/sdk";
 import { getConfig } from "../../helpers/cache";
 import BigNumber from "bignumber.js";
 
 const feesConfig =
-  "https://raw.githubusercontent.com/solv-finance/solv-protocol-defillama/main/solv-fees-new.json";
-const yields = 0.2;
+  "https://raw.githubusercontent.com/solv-finance/solv-protocol-defillama/main/solv-fees-v2.json";
 
 const chains: { [chain: Chain]: { deployedAt: number } } = {
   [CHAIN.ETHEREUM]: { deployedAt: 1726531200 },
@@ -16,7 +15,7 @@ const chains: { [chain: Chain]: { deployedAt: number } } = {
   [CHAIN.ARBITRUM]: { deployedAt: 1726531200 },
   [CHAIN.MANTLE]: { deployedAt: 1726531200 },
   [CHAIN.MERLIN]: { deployedAt: 1726531200 },
-  // [CHAIN.CORE]: { deployedAt: 1726531200 },
+  [CHAIN.CORE]: { deployedAt: 1726531200 },
   [CHAIN.SCROLL]: { deployedAt: 1726531200 },
   [CHAIN.SOLANA]: { deployedAt: 1726531200 },
   [CHAIN.AVAX]: { deployedAt: 1726531200 },
@@ -25,6 +24,8 @@ const chains: { [chain: Chain]: { deployedAt: number } } = {
   [CHAIN.LINEA]: { deployedAt: 1726531200 },
   [CHAIN.ROOTSTOCK]: { deployedAt: 1726531200 },
   [CHAIN.SONEIUM]: { deployedAt: 1742169600 },
+  [CHAIN.INK]: { deployedAt: 1742169600 },
+  [CHAIN.BERACHAIN]: { deployedAt: 1742169600 },
 };
 
 const fetch: FetchV2 = async (options) => {
@@ -33,7 +34,7 @@ const fetch: FetchV2 = async (options) => {
       name: string;
       marketAddress: string;
       poolId: string;
-      contractAddress: string;
+      openFundShareAddress: string;
       navOracle: string;
       openFundShareSlot: string;
       revenueRatio: string;
@@ -90,15 +91,15 @@ async function fees(options: FetchOptions, contracts: any): Promise<{ dailyFees:
     }),
     options.api.multiCall({
       abi: `function slotBaseInfo(uint256 slot_) view returns (tuple(address issuer, address currency, uint64 valueDate, uint64 maturity, uint64 createTime, bool transferable, bool isValid))`,
-      calls: pools.map((pool: { contractAddress: string; openFundShareSlot: string }) => ({
-        target: shareConcretes[pool.contractAddress],
+      calls: pools.map((pool: { OpenFundShareAddress: string; openFundShareSlot: string }) => ({
+        target: shareConcretes[pool.OpenFundShareAddress],
         params: [pool.openFundShareSlot],
       })),
     }),
     options.toApi.multiCall({
       abi: "function slotTotalValue(uint256) view returns (uint256)",
-      calls: pools.map((pool: { contractAddress: string; openFundShareSlot: string }) => ({
-        target: shareConcretes[pool.contractAddress],
+      calls: pools.map((pool: { OpenFundShareAddress: string; openFundShareSlot: string }) => ({
+        target: shareConcretes[pool.OpenFundShareAddress],
         params: [pool.openFundShareSlot],
       })),
     })
@@ -113,13 +114,14 @@ async function fees(options: FetchOptions, contracts: any): Promise<{ dailyFees:
     })),
   });
 
+  let redemptionFees: { [key: string]: { address: string, token: string } } = {};
   for (let i = 0; i < pools.length; i++) {
     const pool = pools[i];
     const revenueRatio = BigNumber(pool.revenueRatio);
     const subscriptionFee = pool.subscriptionFee;
     const redemptionFee = pool.redemptionFee;
     const receivedFee = pool.receivedFee;
-    const gasTokens = pool.gasTokens;
+    const receivedFeeGasTokens = pool.receivedFeeGasTokens;
 
     const yesterdayNav = BigNumber(yesterdayNavs[i].nav_);
     const todayShares = BigNumber(todayShareTotalValues[i]);
@@ -138,19 +140,25 @@ async function fees(options: FetchOptions, contracts: any): Promise<{ dailyFees:
     }
 
     if (redemptionFee) {
-      let redemptionFees = await received(options, redemptionFee);
-      dailyRevenue.addBalances(redemptionFees);
+      for (const address of redemptionFee.address) {
+        for (const token of redemptionFee.token) {
+          redemptionFees[`${address.toLowerCase()}-${token.toLowerCase()}`] = {
+            address: address,
+            token: token,
+          };
+        }
+      }
     }
 
     if (receivedFee) {
       const receivedFees = await received(options, receivedFee);
       dailyFees.addBalances(receivedFees);
-      dailyRevenue.addBalances(receivedFees.clone(yields));
+      dailyRevenue.addBalances(receivedFees.clone(revenueRatio.toNumber()));
     }
 
-    if (gasTokens) {
-      const nativeTokenFees = await gasTokensReceived(options, gasTokens);
-      dailyRevenue.addBalances(nativeTokenFees.clone(yields));
+    if (receivedFeeGasTokens) {
+      const nativeTokenFees = await gasTokensReceived(options, receivedFeeGasTokens);
+      dailyRevenue.addBalances(nativeTokenFees.clone(revenueRatio.toNumber()));
     }
 
     // fee = net value increase after on-chain deduction * today's shares / (1 - corresponding fund's revenue_ratio)
@@ -163,6 +171,17 @@ async function fees(options: FetchOptions, contracts: any): Promise<{ dailyFees:
     }
 
     dailyFees.add(poolBaseInfo.currency, fee.toNumber());
+  }
+
+  if (Object.keys(redemptionFees).length > 0) {
+    const redemptionFeeAddresses = Object.values(redemptionFees).map(fee => fee.address);
+    const redemptionFeeTokens = Object.values(redemptionFees).map(fee => fee.token);
+
+    const redemptionFeeAmount = await received(options, {
+      address: redemptionFeeAddresses,
+      token: redemptionFeeTokens
+    });
+    dailyRevenue.addBalances(redemptionFeeAmount);
   }
 
   return { dailyFees, dailyRevenue };
@@ -187,35 +206,15 @@ async function subscriptionFees(
   const fromBlock = await options.getFromBlock();
   const toBlock = await options.getToBlock();
 
-  const [subscribeLogs, requestRedeemLogs, revokeRedeemLogs] = await Promise.all([
-    options.getLogs({
-      target: marketAddress,
-      topics: [
-        "0xc51cca244fc8e01ee10b07c39991abc0fcb99dd8650fa53b0797d3e8446451f6", // Subscribe
-        poolId,
-      ],
-      fromBlock,
-      toBlock,
-    }),
-    options.getLogs({
-      target: marketAddress,
-      topics: [
-        "0x7b643ca1af436c1321536413ea4e5d302421a825be5ca49aaaba4583c09bd906", // RequestRedeem
-        poolId,
-      ],
-      fromBlock,
-      toBlock,
-    }),
-    options.getLogs({
-      target: marketAddress,
-      topics: [
-        "0x1d1d9dc1cf42a6e6d9c7cd77a670b1ca2af37ce238872f36e794f163a45cc313", // RevokeRedeem
-        poolId,
-      ],
-      fromBlock,
-      toBlock,
-    })
-  ]);
+  const subscribeLogs = await options.getLogs({
+    target: marketAddress,
+    topics: [
+      "0xc51cca244fc8e01ee10b07c39991abc0fcb99dd8650fa53b0797d3e8446451f6", // Subscribe
+      poolId,
+    ],
+    fromBlock,
+    toBlock,
+  })
 
   let subscribeTotal = BigNumber(0);
   for (const log of subscribeLogs) {
@@ -223,24 +222,7 @@ async function subscriptionFees(
     subscribeTotal = subscribeTotal.plus(subscribeAmount);
   }
 
-  let requestRedeemTotal = BigNumber(0);
-  for (const log of requestRedeemLogs) {
-    const redeemAmount = parseRequestRedeemEvent(log.data);
-    requestRedeemTotal = requestRedeemTotal.plus(redeemAmount);
-  }
-
-  let revokeRedeemTotal = BigNumber(0);
-  for (const log of revokeRedeemLogs) {
-    const revokeAmount = await getRevokeRedeemAmount(log, requestRedeemLogs);
-    revokeRedeemTotal = revokeRedeemTotal.plus(revokeAmount);
-  }
-
-  const netSubscription = subscribeTotal.minus(requestRedeemTotal).plus(revokeRedeemTotal);
-
-  if (netSubscription.lte(0)) {
-    return 0;
-  }
-  return netSubscription.toNumber();
+  return subscribeTotal.toNumber();
 }
 
 function parseSubscribeEvent(data: string): BigNumber {
@@ -254,63 +236,24 @@ function parseSubscribeEvent(data: string): BigNumber {
   return BigNumber(0);
 }
 
-function parseRequestRedeemEvent(data: string): BigNumber {
-  const hexData = data.slice(2);
-
-  if (hexData.length >= 128) {
-    const redeemValueHex = hexData.slice(96, 128);
-    return BigNumber(`0x${redeemValueHex}`);
-  }
-
-  return BigNumber(0);
-}
-
-async function getRevokeRedeemAmount(
-  revokeLog: any,
-  requestRedeemLogs: any[]
-): Promise<BigNumber> {
-  const hexData = revokeLog.data.slice(2);
-  if (hexData.length < 128) {
-    return BigNumber(0);
-  }
-
-  const redemptionIdHex = hexData.slice(64, 96);
-  const redemptionId = BigNumber(`0x${redemptionIdHex}`);
-
-  const matchingLog = requestRedeemLogs.find(log => {
-    const logHexData = log.data.slice(2);
-    if (logHexData.length >= 128) {
-      const logRedemptionId = BigNumber(`0x${logHexData.slice(64, 96)}`);
-      return logRedemptionId.eq(redemptionId);
-    }
-    return false;
-  });
-
-  if (matchingLog) {
-    return parseRequestRedeemEvent(matchingLog.data);
-  }
-
-  return BigNumber(0);
-}
-
 async function concrete(pools: any[], options: FetchOptions): Promise<any> {
   var contracts: any[] = [];
   var only = {};
   for (var i = 0; i < pools.length; i++) {
-    if (!only[pools[i].contractAddress]) {
+    if (!only[pools[i].openFundShareAddress]) {
       contracts.push(pools[i]);
-      only[pools[i].contractAddress] = true;
+      only[pools[i].openFundShareAddress] = true;
     }
   }
 
   const concreteLists = await options.api.multiCall({
-    calls: contracts.map((contract) => contract.contractAddress),
+    calls: contracts.map((contract) => contract.openFundShareAddress),
     abi: "address:concrete",
   });
 
   let concretes = {};
   for (var k = 0; k < concreteLists.length; k++) {
-    concretes[contracts[k].contractAddress] = concreteLists[k];
+    concretes[contracts[k].openFundShareAddress] = concreteLists[k];
   }
 
   return concretes;
