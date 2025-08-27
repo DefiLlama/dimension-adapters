99c67dda0f95ef4f5f9eef2dc8de4d9d7c298b81
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-27 19:39:14 +0200

    remove log

--- dexs/saros/index.ts
+++ dexs/saros/index.ts
@@ -7,7 +7,6 @@ async function fetch(_: any, _1: any, { startTimestamp, dateString }: FetchOptio
   const { data } = await httpPost('https://api.saros.xyz/api/saros/pool/total', {
     "from": (startTimestamp - 86400) * 1000
   })
-  console.log(dateString, data.length)
   const item = data.find((dayItem: any) => dayItem.from.startsWith(dateString))
   if (!item) throw new Error(`No data for date: ${dateString}`)
   return { dailyVolume: item.volume }
