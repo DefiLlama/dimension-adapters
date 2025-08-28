88597442fe1f1fd66ca58ed25a184b8898fc09b2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-28 20:41:21 +0200

    aave: track horizon rwa

--- fees/aave/index.ts
+++ fees/aave/index.ts
@@ -61,6 +61,13 @@ export const AaveMarkets: {[key: string]: Array<AaveLendingPoolConfig>} = {
       lendingPoolProxy: '0x0AA97c284e98396202b6A04024F5E2c65026F3c0',
       dataProvider: '0x8Cb4b66f7B13F2Ae4D3c91338fC007dbF8C14208',
     },
+
+    // horizon market 
+    {
+      version: 3,
+      lendingPoolProxy: '0xAe05Cd22df81871bc7cC2a04BeCfb516bFe332C8',
+      dataProvider: '0x53519c32f73fE1797d10210c4950fFeBa3b21504',
+    },
   ],
   [CHAIN.OPTIMISM]: [
     {
