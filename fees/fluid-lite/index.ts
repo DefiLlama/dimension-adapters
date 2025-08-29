5eeb3dd93c1827cd818127980620471e80717a0a
Eden <noat.eth@gmail.com>
2025-08-29 20:37:06 +0700

    refactor codes

--- fees/fluid-lite/index.ts
+++ fees/fluid-lite/index.ts
@@ -1,23 +1,69 @@
-import { CONFIG_FLUID_LITE } from "./config";
-import { getFluidLiteDailyRevenue } from "./revenue";
 import { CHAIN } from "../../helpers/chains";
-import { Adapter, Fetch, FetchOptions } from "../../adapters/types";
+import { Adapter, FetchOptions } from "../../adapters/types";
+import BigNumber from 'bignumber.js';
 
-const fetch: Fetch = async (_t: any, _a: any, options: FetchOptions) => {
-  const dailyRevenue = await getFluidLiteDailyRevenue(options);
+const iETHv2_VAULT = "0xA0D3707c569ff8C87FA923d3823eC5D81c98Be78";
+const stETHAddress = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84";
+const EventLogCollectRevenue = 'event LogCollectRevenue(uint256 amount, address indexed to)';
 
-  return { dailyRevenue, timestamp: options.startOfDay };
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+  const dailyRevenue = options.createBalances();
+  const [currentRevenueValue, startRevenueValue] = await Promise.all([
+    options.api.call({
+      abi: 'function revenue() view returns (uint256)',
+      target: iETHv2_VAULT,
+    }),
+
+    options.fromApi.call({
+      abi: 'function revenue() view returns (uint256)',
+      target: iETHv2_VAULT,
+    }),
+  ]);
+
+  // Add revenue delta to daily revenue
+  const revenueDelta = Number(currentRevenueValue) - Number(startRevenueValue)
+  dailyRevenue.add(
+    stETHAddress,
+    revenueDelta
+  );
+
+  const collectRevenueLogs = await options.getLogs({
+    target: iETHv2_VAULT,
+    onlyArgs: true,
+    eventAbi: EventLogCollectRevenue,
+    fromBlock: Number(options.fromApi.block),
+    toBlock: Number(options.api.block),
+    skipCacheRead: true,
+    skipIndexer: true,
+    // More resource-intensive but prevents logs from being cached.
+    // Currently, the adapter is updated every hour.
+    // In case of an error within a given time range for some reasons, the next sequence
+    // can likely fix the issue naturally if it retries fetching all the logs
+  });
+
+  // If revenue is collected in this timeframe, add the collected amount to daily fees
+  const collectedRevenueAmount: BigNumber = collectRevenueLogs.reduce(
+    (acc, log) => acc.plus(new BigNumber(log[0])),
+    new BigNumber(0)
+  );
+
+  dailyRevenue.add(
+    stETHAddress,
+    collectedRevenueAmount.toFixed()
+  );
+
+  return { dailyFees: dailyRevenue, dailyRevenue }
 };
 
 const adapter: Adapter = {
   version: 1,
-  adapter: {
-    [CHAIN.ETHEREUM]: {
-      start: CONFIG_FLUID_LITE.startBlockNumber,
-      fetch,
-    },
+  methodology: {
+    Fees: 'Lite Vault charges a 20% performance fee on vaults and an additional 0.05% exit fee. Revenue is collected and transferred to the Instadapp treasury.',
+    Revenue: 'Lite Vault charges a 20% performance fee on vaults and an additional 0.05% exit fee. Revenue is collected and transferred to the Instadapp treasury.',
   },
-  methodology: CONFIG_FLUID_LITE.methodology,
+  fetch,
+  chains: [CHAIN.ETHEREUM],
+  start: '2023-02-13',
 };
 
 export default adapter;
