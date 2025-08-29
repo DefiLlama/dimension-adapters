9ae9cbbac267da1bf03e7b86e8afe36ef3713035
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-29 12:11:11 +0200

    minor fix

--- fees/cattos.ts
+++ fees/cattos.ts
@@ -14,7 +14,6 @@ const fetch = async (_1: any, _2: any, options: FetchOptions): Promise<FetchResu
   `
   const chainData = await queryDuneSql(options, query)
   const dailyFees = options.createBalances()
-  console.log(chainData)
   dailyFees.addCGToken('aptos', chainData[0]["total_rev"])
 
   return { dailyFees, dailyRevenue: dailyFees, };
@@ -25,7 +24,7 @@ const adapter: any = {
   adapter: {
     [CHAIN.APTOS]: {
       fetch: fetch,
-      start: '2025-05-21',
+      start: '2025-07-21',
     }
   },
   isExpensiveAdapter: true,
