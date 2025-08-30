002605392be62284a092f4023e3a86d486430004
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-30 11:27:43 +0530

    refactor nemo-vault

--- fees/nemo-vault/index.ts
+++ fees/nemo-vault/index.ts
@@ -1,42 +1,39 @@
-import { Adapter, FetchResultV2, FetchV2 } from '../../adapters/types';
+import { Adapter, FetchOptions } from '../../adapters/types';
 import { CHAIN } from '../../helpers/chains';
 import fetchURL from '../../utils/fetchURL';
 
 const nemoApiURL = 'https://api.nemoprotocol.com/api/v1/market/fee';
 
 interface NemoStats {
-  data:{
+  data: {
     fee: string;
   }
 }
 
-const methodology = {
-  Fees: 'The performance fee is the fee that curators charge in regular vault operations.',
-};
-
-const fetchNemoStats: FetchV2 = async ({
-  startTimestamp,
-  endTimestamp,
-}): Promise<FetchResultV2> => {
+const fetch = async ({ startTimestamp, endTimestamp }: FetchOptions) => {
   const url = `${nemoApiURL}?starttime=${startTimestamp}&endtime=${endTimestamp}&tradeType=2`;
   const { data }: NemoStats = await fetchURL(url);
   const dailyFees = data.fee;
   return {
     dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
+    dailyHoldersRevenue: 0,
   };
+};
+
+const methodology = {
+  Fees: 'The performance fee is the fee that curators charge in regular vault operations.',
+  Revenue: 'The performance fee is the fee that curators charge in regular vault operations.',
+  ProtocolRevenue: 'The performance fee is the fee that curators charge in regular vault operations.',
 };
 
 const adapter: Adapter = {
   version: 2,
-  adapter: {
-    [CHAIN.SUI]: {
-      fetch: fetchNemoStats,
-      start: '2025-08-20',
-      meta: {
-        methodology,
-      },
-    },
-  },
+  fetch,
+  chains: [CHAIN.SUI],
+  methodology,
+  start: '2025-08-20',
 };
 
 export default adapter;
