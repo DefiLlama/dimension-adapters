6589f51f754d0a65574271fe6fa854ca432ea017
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-01 15:24:55 +0200

    code refactor

--- dexs/mintiq-market/index.ts
+++ dexs/mintiq-market/index.ts
@@ -1,28 +1,41 @@
-import axios from "axios";
 
-const API_URL = "https://lwrrxstrfnvgsreqoujq.supabase.co/functions/v1/dynamic-api";
+import { FetchOptions } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
 
-interface Collection {
-  nftAddress: string;
-  floor: number;
-  volume_24h: number;
-  volume_24h_delta: number;
-  floor_delta: number;
+const abi = {
+  "NFTListed": "event NFTListed(address indexed nft, uint256 indexed tokenId, address seller, uint256 price)",
+  "NFTSold": "event NFTSold(address indexed nft, uint256 indexed tokenId, address buyer, uint256 price)",
+  "NFTUnlisted": "event NFTUnlisted(address indexed nft, uint256 indexed tokenId)",
+  "FEE_PERCENT": "uint256:FEE_PERCENT",
+  "activeListings": "function activeListings(uint256) view returns (address nft, uint256 tokenId)",
+  "buyNFT": "function buyNFT(address nft, uint256 tokenId) payable",
+  "feeReceiver": "address:feeReceiver",
+  "getActiveListingsLength": "uint256:getActiveListingsLength",
+  "getAllActiveListingsFiltered": "function getAllActiveListingsFiltered() view returns (address[] nftAddresses, uint256[] tokenIds, address[] sellers, uint256[] prices)",
+  "listNFT": "function listNFT(address nft, uint256 tokenId, uint256 price)",
+  "listingIndex": "function listingIndex(address, uint256) view returns (uint256)",
+  "listings": "function listings(address, uint256) view returns (address seller, uint256 price, bool isActive)",
+  "owner": "address:owner",
+  "unlistNFT": "function unlistNFT(address nft, uint256 tokenId)"
 }
 
-export const adapter = {
-  fetch: async () => {
-    const { data } = await axios.get<Collection[]>(API_URL);
+const contract = '0xb97cDe183139A434291F2C185c2a072c3b8EaE80'
 
-    // Приведение к формату dexs
-    return data.map(col => ({
-      pair: col.nftAddress,
-      chain: "xrpl-evm",
-      base: col.nftAddress,
-      quote: "XRP",
-      volumeUsd: col.volume_24h,
-      timestamp: new Date(col.timestamp).getTime() / 1000,
-    }));
+async function fetch({ getLogs, api, createBalances, }: FetchOptions) {
+  const fees = (await api.call({ target: contract, abi: abi.FEE_PERCENT })) / 100
+  const logs = await getLogs({ target: contract, eventAbi: abi.NFTSold, })
+  const dailyVolume = createBalances()
+  dailyVolume.addGasToken(logs.map(i => i.price))
+  return { dailyVolume, dailyFees: dailyVolume.clone(fees) }
+}
+
+export default {
+  fetch,
+  version: 2,
+  start: '2025-06-27',
+  chains: [CHAIN.XRPL_EVM],
+  methodology: {
+    Fees: "5% fee charged on all NFT marketplace sales",
+    Volume: "Total volume of NFT sales on the marketplace"
   },
-  version: "1.0",
 };
