3f0b68caade195c9086b869777fe08acd9ffc4c6
Zdeadex <51711900+zdeadex@users.noreply.github.com>
2025-09-01 18:42:12 +0200

    Update index.ts

--- dexs/winnieswap/index.ts
+++ dexs/winnieswap/index.ts
@@ -1,62 +1,44 @@
-import { FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
-import request, { gql } from "graphql-request";
+export default adapter
+
+import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { DEFAULT_TOTAL_VOLUME_FIELD, getGraphDimensions2 } from "../../helpers/getUniSubgraph";
 
-const GRAPH_URL = 'https://api.goldsky.com/api/public/project_cmesjqx64lbfh01wc6z2q9tb0/subgraphs/winnieswap3/3.0.0/gn'
-
-const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
-    const query = gql`
-        query q{
-            tokenDayDatas(where: {date: ${options.startOfDay}}, first: 1000, orderBy: volumeUSD, orderDirection: desc) {
-                volumeUSD
-                feesUSD
-            }
-        }
-    `
-
-    const data = await request(GRAPH_URL, query)
-    const dailyVolume = options.createBalances()
-    const dailyFees = options.createBalances()
-
-    data.tokenDayDatas.forEach((e: any) => {
-        dailyVolume.addUSDValue(Number(e.volumeUSD))
-        dailyFees.addUSDValue(Number(e.feesUSD))
-    })
-
-    const dailyRevenue = dailyFees.clone(0)
-    const dailyProtocolRevenue = dailyFees.clone(0)
-    const dailySupplySideRevenue = dailyFees.clone(1)
-    const dailyHoldersRevenue = dailyFees.clone(0)
-
-    return {
-        dailyVolume,
-        dailyFees,
-        dailyUserFees: dailyFees,
-        dailyRevenue,
-        dailyProtocolRevenue,
-        dailySupplySideRevenue,
-        dailyHoldersRevenue,
-    }
+const v3Endpoints: { [key: string]: string } = {
+  [CHAIN.BERACHAIN]: "https://api.goldsky.com/api/public/project_cmesjqx64lbfh01wc6z2q9tb0/subgraphs/winnieswap3/3.0.0/gn"
 }
+const fetch = getGraphDimensions2({
+  graphUrls: v3Endpoints,
+  totalVolume: {
+    factory: "factories",
+    field: DEFAULT_TOTAL_VOLUME_FIELD,
+  },
+  feesPercent: {
+    type: "fees",
+    ProtocolRevenue: 25,
+    HoldersRevenue: 0,
+    UserFees: 100, // User fees are 100% of collected fees
+    SupplySideRevenue: 75, // 75% of fees are going to LPs
+    Revenue: 25 // Revenue is 100% of collected fees
+  }
+});
 
 const methodology = {
-    Fees: "Total swap fees paid by users.",
-    Revenue: "0% protocol revenue share and 0% holders revenue share.",
-    ProtocolRevenue: "0% of fees collected by the protocol.",
-    SupplySideRevenue: "100% of fees distributed to LPs.",
-    HoldersRevenue: "0% of fees used for buy-back and burn.",
-    UserFees: "Total swap fees paid by users."
+  Fees: "Total swap fees paid by users.",
+  Revenue: "25% protocol revenue share and 75% holders revenue share.",
+  ProtocolRevenue: "25% of fees collected by the protocol.",
+  SupplySideRevenue: "75% of fees distributed to LPs.",
+  HoldersRevenue: "0% of fees used for buy-back and burn.",
+  UserFees: "Total swap fees paid by users."
 }
 
+// https://docs.henlo-winnie.dev/winnieswap/introduction#trading-fees-split
 const adapter: SimpleAdapter = {
-    version: 2,
-    adapter: {
-        [CHAIN.BERACHAIN]: {
-            fetch,
-            start: '2025-07-07',
-            meta: { methodology }
-        }
-    }
+  version: 2,
+  fetch,
+  start: '2025-07-07',
+  chains: [CHAIN.BERACHAIN],
+  methodology
 }
 
-export default adapter
+export default adapter;
