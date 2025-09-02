e221b3165d42170c8d89c560e1f650b2a5e8c5f5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-03 01:00:40 +0200

    refactor: dynamic fees

--- fees/myriadmarkets/index.ts
+++ fees/myriadmarkets/index.ts
@@ -2,82 +2,99 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const MARKETS = {
-    'abstract': '0x3e0F5F8F5Fb043aBFA475C0308417Bf72c463289',
-    'linea': '0x39e66ee6b2ddaf4defded3038e0162180dbef340',
-};
+  [CHAIN.ABSTRACT]: '0x3e0F5F8F5Fb043aBFA475C0308417Bf72c463289',
+  [CHAIN.LINEA]: '0x39e66ee6b2ddaf4defded3038e0162180dbef340',
+}
 
-const MARKET_ACTION_EVENT = 'event MarketActionTx (address indexed user,uint8 indexed action, uint256 indexed marketId, uint256 outcomeId, uint256 shares, uint256 value, uint256 timestamp)';
-const MARKET_DATA_FUNCTION = 'function getMarketAltData(uint256 marketId) external view returns(uint256 buyFee, bytes32 questionId ,uint256 questionIdUint,address token,uint256 buyTreasuryFee, address treasury ,address realitio ,uint256 realitioTimeout ,address manager)';
+const abi = {
+  MarketActionTx: 'event MarketActionTx (address indexed user,uint8 indexed action, uint256 indexed marketId, uint256 outcomeId, uint256 shares, uint256 value, uint256 timestamp)',
+  getMarketAltData: 'function getMarketAltData(uint256 marketId) external view returns(uint256 buyFee, bytes32 questionId ,uint256 questionIdUint,address token,uint256 buyTreasuryFee, address treasury ,address realitio ,uint256 realitioTimeout ,address manager)',
+  getMarketFees: "function getMarketFees(uint256 marketId) view returns ((uint256 fee, uint256 treasuryFee, uint256 distributorFee) buyFees, (uint256 fee, uint256 treasuryFee, uint256 distributorFee) sellFees, address treasury, address distributor)",
+}
 
-type CallObject = {
-    abi: string;
-    target: string;
-    params: string[];
-};
+async function fetch({ createBalances, chain, api, getLogs }: FetchOptions) {
+  const market = MARKETS[chain]
+  const dailyVolume = createBalances();
+  const dailyFees = createBalances();
+  const dailySupplySideRevenue = createBalances();
+  const dailyRevenue = createBalances();
 
-const feeRate = 0.03; //3%
+  const markets = await api.call({ abi: 'uint256[]:getMarkets', target: market });
+  const marketData = await api.multiCall({ target: market, abi: abi.getMarketAltData, calls: markets })
+  const marketFees = (await api.multiCall({ target: market, abi: abi.getMarketFees, calls: markets }))
+  const marketMapping: any = {}
+  markets.forEach((val, idx) => marketMapping[val] = {
+    token: marketData[idx].token,
+    fees: marketFees[idx]
+  })
 
-async function fetch(options: FetchOptions) {
-    const dailyVolume = options.createBalances();
-    const dailyFees = options.createBalances();
+  marketFees.forEach(i => {
+    i.buyFees = i.buyFees.map(j => Number(j) / 1e18)
+  })
+  const tradeLogs = await getLogs({ target: market, eventAbi: abi.MarketActionTx, });
 
-    const markets = await options.api.call({
-        abi: 'uint256[]:getMarkets',
-        target: MARKETS[options.chain]
-    });
+  tradeLogs.forEach(({ action, marketId, value }) => {
+    value = Number(value)
+    action = Number(action)
+    const { fees, token } = marketMapping[marketId]
+    const isBuy = action === 0
+    const feeKey = isBuy ? 'buyFees' : 'sellFees'
+    const [fee, treasuryFee, distributorFee] = fees[feeKey]
+    const totalFee = fee + treasuryFee + distributorFee
 
-    const calls: CallObject[] = [];
+    switch (action) {
+      case 0: // buy
+      case 1: // sell
+        dailyVolume.add(token, value);
+        dailyFees.add(token, value * totalFee, isBuy ? 'BuyFee' : 'SellFee')
+        dailySupplySideRevenue.add(token, value * distributorFee, 'DistributorFee')
+        dailySupplySideRevenue.add(token, value * fee, 'LPFee')
+        dailyRevenue.add(token, value * fee, 'TreasuryFee')
+        break;
+    }
+  });
 
-    for (const marketId of markets) {
-        calls.push({
-            abi: MARKET_DATA_FUNCTION,
-            target: MARKETS[options.chain],
-            params: [marketId]
-        });
-    };
-    const marketData = await options.api.batchCall(calls);
 
-    const tradeLogs = await options.getLogs({
-        target: MARKETS[options.chain],
-        eventAbi: MARKET_ACTION_EVENT,
-    });
-
-    tradeLogs.forEach(trade => {
-        const action = trade.action;
-        const tradeValue = trade.value;
-        const marketId = trade.marketId;
-        const marketToken = marketData[marketId].token;
-
-        if (action == 0 || action == 1)
-            dailyVolume.add(marketToken, tradeValue);
-        if (action == 0)
-            dailyFees.add(marketToken, Number(tradeValue) * feeRate);
-    });
-
-    const dailyRevenue = dailyFees.clone(1 / 3);
-
-    return {
-        dailyVolume,
-        dailyFees,
-        dailyRevenue,
-        dailyProtocolRevenue: dailyRevenue,
-        dailySupplySideRevenue: dailyRevenue
-    };
+  return {
+    dailyVolume,
+    dailyFees,
+    dailyRevenue,
+    dailyProtocolRevenue: dailyRevenue.clone(),
+    dailyHoldersRevenue: 0,
+  };
 }
 
 const methodology = {
-    Fees: "3% fee charged on buy trades",
-    Revenue: "1% fee to fund further development of Myriad Markets",
-    ProtocolRevenue: "1% fee to fund further development of Myriad Markets",
-    SupplySideRevenue: "1% fee to reward liquidity providers",
+  Fees: "fees charged on buys/sells (usually 3%)",
+  Revenue: "1% fee to fund further development of Myriad Markets",
+  ProtocolRevenue: "All revenue go to the protocol",
+  SupplySideRevenue: "1% fee to reward liquidity providers & 1% fee to the distributors",
 };
 
+const breakdownMethodology = {
+  Fees: {
+    'BuyFee': 'Fee charged while buying',
+    'SellFee': 'Fee charged while selling',
+  },
+  Revenue: {
+    'TreasuryFee': 'Part of trading fee that goes to the protocol treasury',
+  },
+  SupplySideRevenue: {
+    'DistributorFee': 'Cut from trading fees to the distributors',
+    'LPFee': 'Cut from trading fees to the Liquidity providers',
+  },
+}
+
+
 const adapter: SimpleAdapter = {
-    version: 2,
-    fetch,
-    chains: [CHAIN.LINEA, CHAIN.ABSTRACT],
-    start: "2025-07-04",
-    methodology
+  version: 2,
+  fetch,
+  breakdownMethodology,
+  adapter: {
+    [CHAIN.ABSTRACT]: { start: '2025-07-06', },
+    [CHAIN.LINEA]: { start: '2025-08-01', },
+  },
+  methodology
 };
 
 export default adapter;
