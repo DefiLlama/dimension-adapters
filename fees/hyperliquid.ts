4dfb29632b561f82c3e8b438e9392cc7ee577356
Eden <noat.eth@gmail.com>
2025-09-02 20:53:01 +0700

    fix hyperliquid fees and revenue

--- fees/hyperliquid.ts
+++ fees/hyperliquid.ts
@@ -3,6 +3,7 @@ import { Adapter, FetchOptions, } from '../adapters/types';
 import { findClosest } from "../helpers/utils/findClosest";
 import { httpGet } from "../utils/fetchURL";
 
+// fees source: https://hyperdash.info/statistics
 const fetch = async (options: FetchOptions) => {
     const dailyFees = options.createBalances();
     const data: any[] = (await httpGet(`https://api.hypurrscan.io/fees`)).map((t: any) => ({ ...t, time: t.time * 1e3 }))
@@ -11,11 +12,12 @@ const fetch = async (options: FetchOptions) => {
     const endCumFees: any = findClosest(options.endTimestamp, data, 3600)
     dailyFees.addCGToken("usd-coin", (endCumFees.total_fees - startCumFees.total_fees)/1e6)
 
-    // https://hyperdash.info/statistics
-    // 93% of fees go to Assistance Fund for burning tokens, remaining 7% go to HLP Vault
 
-    const dailyHoldersRevenue = dailyFees.clone(0.93);
-    const dailySupplySideRevenue = dailyFees.clone(0.07);
+    // confirm from hyperliquid team
+    // before 30 Aug, 97% of fees go to Assistance Fund for burning tokens, remaining 3% go to HLP Vault
+    // after 30 Aug, 99% of fees go to Assistance Fund for burning tokens, remaining 1% go to HLP Vault
+    const dailyHoldersRevenue = dailyFees.clone(options.startTimestamp >= 1756512000 ? 0.99 : 0.97);
+    const dailySupplySideRevenue = dailyFees.clone(options.startTimestamp >= 1756512000 ? 0.01 : 0.03);
 
     return {
         dailyFees,
@@ -36,9 +38,9 @@ const adapter: Adapter = {
     },
     methodology: {
         Fees: "Trade fees and Ticker auction proceeds. Note this excludes the HLP vault and HyperEVM fees.",
-        Revenue: "93% of fees go to Assistance Fund for burning tokens",
-        SupplySideRevenue: "7% of fees go to HLP Vault suppliers",
-        HoldersRevenue: "93% of fees go to Assistance Fund for burning tokens",
+        Revenue: "97% of fees go to Assistance Fund for burning tokens, it was 99% after network upgrade on 30 Aug, 2025",
+        SupplySideRevenue: "3% of fees go to HLP Vault suppliers, it was 1% after network upgrade on 30 Aug, 2025",
+        HoldersRevenue: "97% of fees go to Assistance Fund for burning tokens, it was 99% after network upgrade on 30 Aug, 2025",
         ProtocolRevenue: "No protocol revenue",
     }
 }
