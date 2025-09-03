9866f46f4ee36651a3a3b029ea06be8e2d59910a
dry-cake <174254844+dry-cake@users.noreply.github.com>
2025-09-03 21:40:11 +0400

    add: more data to adapter v2

--- dexs/aqua-network/index.ts
+++ dexs/aqua-network/index.ts
@@ -5,20 +5,27 @@ import { CHAIN } from "../../helpers/chains";
 const AQUA_VOLUME_ENDPOINT = "https://amm-api.aqua.network/api/external/v1/statistics/totals/?size=all"
 
 interface IVolumeAll {
-  volume: number;
-  tvl: number;
-  date: string;
-  protocol_fees: number;
-  lp_fees: number;
-  external_rewards: number;
+    volume: number;
+    tvl: number;
+    date: string;
+    protocol_fees: number;
+    lp_fees: number;
+    external_rewards: number;
+    timestamp_date_from: number;
+    timestamp_date_to: number;
 }
 
-const fetch = async (_: any, _1: any, { dateString, }: FetchOptions) => {
+const fetch = async ({ fromTimestamp, toTimestamp }: FetchOptions) => {
   const historicalVolume: IVolumeAll[] = (await fetchURL(AQUA_VOLUME_ENDPOINT));
 
+  // Seems like we have here gap about 3.5 hours in to-timestamps, can u maybe explain that diff? 
+  // Finding day period from our api, that matches llama toTimestamp (current time)
   const day = historicalVolume
-    .find(i => i.date === dateString)
-  if (!day) throw new Error('No data for timestamp: ' + dateString);
+    .find(i =>  toTimestamp >= i.timestamp_date_from && toTimestamp <= i.timestamp_date_to);
+
+  if (!day) {
+    throw new Error('No data for timestamp: ' + fromTimestamp);
+  }
 
   const ProtocolFees = day.protocol_fees / 1e7
   const LPFees = day.lp_fees / 1e7
@@ -27,14 +34,28 @@ const fetch = async (_: any, _1: any, { dateString, }: FetchOptions) => {
   return {
     dailyVolume: day.volume / 1e7,
     dailyFees: ProtocolFees + LPFees + ExternalRewards,
+    dailyUserFees: ProtocolFees + LPFees,
     dailySupplySideRevenue: LPFees,
-    dailyRevenue: LPFees + ExternalRewards,
-    dailyHoldersRevenue: LPFees + ExternalRewards,
+    dailyRevenue: ProtocolFees,
+    dailyHoldersRevenue: ProtocolFees,
+    dailyBribesRevenue: ExternalRewards,
     dailyProtocolRevenue: 0,
   }
 };
 
+const methodology = {
+    dailyFees: "All fees including 100% of the swap fees and external rewards for AQUA holders.",
+    dailyUserFees: "100% of the swap fees",
+    dailyRevenue: "50% of the swap fees that are received by the protocol and then distributed between AQUA holders that voted for the markets where these fees have been collected",
+    dailyProtocolRevenue: "Share of the fees kept by Aquarius. Currently equals 0.",
+    dailyHoldersRevenue: "50% of the swap fees that are received by the protocol and then distributed between AQUA holders that voted for the markets where these fees have been collected.",
+    dailySupplySideRevenue: "50% of the swap fees that are shared with the Aquarius liquidity providers",
+    dailyBribesRevenue: "Amount of external incentives for AQUA holders voting for specific markets on Aquarius.",
+}
+
 const adapter: SimpleAdapter = {
+  version: 2,
+  methodology,
   adapter: {
     [CHAIN.STELLAR]: { fetch, start: '2024-07-01' },
   },
