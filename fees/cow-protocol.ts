aac9fbc2995fc83f7c506305fb8b374fbf5c2256
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-04 16:13:04 +0200

    cow: track lens

--- fees/cow-protocol.ts
+++ fees/cow-protocol.ts
@@ -35,11 +35,11 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     let protocolRevenue = protocolFee + partnerFeeCow + mevBlockerFee; // Excluding partner fees
 
     // Sanity check for Gnosis chain
-    if(options.chain === CHAIN.XDAI && totalFees > 5) {
+    if (options.chain === CHAIN.XDAI && totalFees > 5) {
       throw new Error(`Total fees ${totalFees} ETH very high for gnosis. Protocol: ${protocolFee}, Partner(Partner): ${partnerFeePartner}, Partner(COW): ${partnerFeeCow}, MEV: ${mevBlockerFee}`);
     }
 
-    if(options.chain === CHAIN.ETHEREUM && totalFees > 1000) {
+    if (options.chain === CHAIN.ETHEREUM && totalFees > 1000) {
       totalFees = 0;
       protocolRevenue = 0;
       // throw new Error(`Total fees ${totalFees} ETH very high for ethereum. Protocol: ${protocolFee}, Partner: ${partnerFee}, MEV: ${mevBlockerFee}`);
@@ -47,7 +47,8 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
 
     dailyFees.addCGToken('ethereum', totalFees);
     dailyProtocolRevenue.addCGToken('ethereum', protocolRevenue);
-  } else { 
+  } else {
+    if (options.chain === CHAIN.LENS) return {}
     throw new Error(`No data found for chain ${options.chain} on ${options.startOfDay}`);
   }
 
@@ -61,7 +62,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
 
 const methodology = {
   UserFees: "All trading fees including protocol fees, partner fees, and MEV blocker fees",
-  Fees: "All trading fees including protocol fees, partner fees, and MEV blocker fees", 
+  Fees: "All trading fees including protocol fees, partner fees, and MEV blocker fees",
   Revenue: "Trading fees (protocol fees + cow's MEV blocker fees + partner fee share)",
   ProtocolRevenue: "Trading fees (protocol fees + cow's MEV blocker fees + partner fee share)",
 }
@@ -73,6 +74,7 @@ const chainConfig = {
   [CHAIN.XDAI]: { start: '2023-02-03' },
   [CHAIN.AVAX]: { start: '2025-06-30' },
   [CHAIN.POLYGON]: { start: '2025-06-30' },
+  [CHAIN.LENS]: { start: '2025-06-16', },
 }
 
 const adapter: Adapter = {
