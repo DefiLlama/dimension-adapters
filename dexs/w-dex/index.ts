c7e2f4c6464e93aca26cd726b2fad8e81cc79c9a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-04 14:18:59 +0200

    fix adapter

--- dexs/w-dex/index.ts
+++ dexs/w-dex/index.ts
@@ -3,6 +3,7 @@ import { FetchOptions, IJSON, SimpleAdapter } from "../../adapters/types";
 import { filterPools } from "../../helpers/uniswap";
 import { ethers } from "ethers";
 import { addOneToken } from "../../helpers/prices";
+import { all } from "axios";
 
 const poolEvent = "event Pool(address indexed token0, address indexed token1, address pool)";
 const poolSwapEvent = "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 price, uint128 liquidity, int24 tick, uint24 overrideFee, uint24 pluginFee)";
@@ -44,77 +45,81 @@ const fetch = async (options: FetchOptions) => {
   });
   const dailyVolume = createBalances();
   const dailyFees = createBalances();
+  const allFees = createBalances();
   const dailyRevenue = createBalances();
   const dailySupplySideRevenue = createBalances();
 
-  if (!Object.keys(filteredPairs).length)
-    return {
-      dailyVolume,
-      dailyFees,
-      dailyUserFees: dailyFees,
-      dailyRevenue,
-      dailySupplySideRevenue,
-    };
-
-  const allLogs = await getLogs({
-    targets: Object.keys(filteredPairs),
-    eventAbi: poolSwapEvent,
-    flatten: false,
-  });
+  const filteredPairs2 = Object.keys(filteredPairs)
+  const allLogs = await getLogs({ targets: filteredPairs2, eventAbi: poolSwapEvent, flatten: false, });
 
   allLogs.map((logs: any, index) => {
     if (!logs.length) return;
-    const pair = Object.keys(filteredPairs)[index];
+    const pair = filteredPairs2[index];
     const [token0, token1] = pairObject[pair];
     const fee = fees[pair];
     logs.forEach((log: any) => {
-      addOneToken({
-        chain,
-        balances: dailyVolume,
-        token0,
-        token1,
-        amount0: log.amount0,
-        amount1: log.amount1,
-      });
-      addOneToken({
-        chain,
-        balances: dailyFees,
-        token0,
-        token1,
-        amount0: log.amount0.toString() * fee,
-        amount1: log.amount1.toString() * fee,
-      });
-      addOneToken({
-        chain,
-        balances: dailyRevenue,
-        token0,
-        token1,
-        amount0: log.amount0.toString() * fee * 0.85,
-        amount1: log.amount1.toString() * fee * 0.85,
-      });
+      addOneToken({ chain, balances: dailyVolume, token0, token1, amount0: log.amount0, amount1: log.amount1, });
+      addOneToken({ chain, balances: allFees, token0, token1, amount0: log.amount0.toString() * fee, amount1: log.amount1.toString() * fee, });
     });
   });
+
+  const protocolFees = allFees.clone(0.135, 'ProtocolFee');
+  const infraFees = allFees.clone(0.015, 'InfraFee');
+  const lpFees = allFees.clone(0.85, 'LPFee');
+
+  dailyFees.addBalances(infraFees, 'InfraFee');
+  dailyFees.addBalances(protocolFees, 'ProtocolFee');
+  dailyRevenue.addBalances(protocolFees, 'ProtocolFee');
+
+  dailyFees.addBalances(lpFees, 'LPFee');
+  dailySupplySideRevenue.addBalances(lpFees, 'LPFee');
+
+  dailyFees.add(protocolFees, 'ProtocolFee');
+  dailyRevenue.add(protocolFees, 'ProtocolFee');
+
+  dailyFees.add(infraFees, 'InfraFee');
+  dailySupplySideRevenue.add(infraFees, 'InfraFee');
 
   return {
     dailyVolume,
     dailyFees,
-    dailyUserFees: dailyFees,
+    dailyUserFees: dailyFees.clone(),
+    dailyProtocolRevenue: dailyRevenue,
     dailyRevenue,
-    dailyProtocolRevenue: 0,
     dailySupplySideRevenue,
-    dailyHoldersRevenue: dailyRevenue,
+    dailyHoldersRevenue: 0,
   };
 };
+
+
+const breakdownMethodology = {
+  Fees: {
+    'InfraFee': '1.5% of swap fees go to Algebra.',
+    'LPFee': '85% of swap fees go to liquidity providers.',
+    'ProtocolFee': '13.5% of swap fees go to the protocol.',
+  },
+  Revenue: {
+    'ProtocolFee': '13.5% of swap fees go to the protocol.',
+  },
+  SupplySideRevenue: {
+    'InfraFee': '1.5% of swap fees go to Algebra.',
+    'LPFee': '85% of swap fees go to liquidity providers.',
+  },
+  ProtocolRevenue: {
+    'ProtocolFee': '13.5% of swap fees go to the protocol.',
+  },
+}
 
 const methodology = {
   Fees: "All swap fees paid by users.",
   UserFees: "All swap fees paid by users.",
-  SupplySideRevenue: "No fees distributed to LPs.",
-  Revenue: "85% of swap fees are revenue. 1.5% are distributed to Algebra.",
+  SupplySideRevenue: "LPs get 85% of swap fees, Algebra 1.5%",
+  Revenue: "13.5% of swap fees go to the protocol.",
   ProtocolRevenue: "Protocol get 13.5%",
 }
 
 const adapter: SimpleAdapter = {
+  breakdownMethodology,
   version: 2,
   fetch,
   chains: [CHAIN.POLYGON],
--- fees/w-dex.ts
+++
@@ -1,2 +0,0 @@
-import adapter from "../dexs/w-dex";
-export default adapter;
