3c7e96b1fb3a3e7c52d501ea81b7eafa64e025ee
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-06 11:28:01 +0200

    bugfix

--- dexs/galaswap-v3.ts
+++ dexs/galaswap-v3.ts
@@ -2,17 +2,27 @@ import { FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { httpGet } from "../utils/fetchURL";
 
-async function fetch({ createBalances }:FetchOptions) {
+const baseCGMap: any = {
+  "GALA": 'gala',
+  "GUSDC": 'usd-coin', "GUSDT": 'tether', "GSOL": 'solana', "$GMUSIC": 'gala-music', "GFARTCOIN": 'fartcoin',
+}
+
+async function fetch({ createBalances }: FetchOptions) {
   const dailyVolume = createBalances();
   const dailyFees = createBalances();
   const data = await httpGet('https://dex-backend-prod1.defi.gala.com/coin-gecko/tickers')
   data.forEach((item: any) => {
+    const cgToken = baseCGMap[item.base_currency]
+    if (!cgToken) {
+      console.log('No CG mapping for', item.base_currency);
+      return;
+    }
+    if (!item.base_volume) return;
     const fee = item.ticker_id.split('/')[2] / 1e6
-    const vol = item.base_volume	+ item.target_volume
-    dailyVolume.addUSDValue(vol)
-    dailyFees.addUSDValue(vol * fee)
-  })
 
+    dailyVolume.addCGToken(cgToken, item.base_volume)
+    dailyFees.addCGToken(cgToken, item.base_volume * fee)
+  })
 
   return { dailyVolume, dailyFees }
 }
