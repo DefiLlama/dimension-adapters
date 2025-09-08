9b12a44e9a193df57b75c0c6186f0aa90df7a073
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-08 16:22:05 +0200

    refactor klashi

--- dexs/kalshi.ts
+++ dexs/kalshi.ts
@@ -1,36 +1,21 @@
 import { FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import axios from "axios";
+import { httpGet } from "../utils/fetchURL";
+
+async function historicFetch(options: FetchOptions) {
+  const data = await httpGet(`https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_${options.dateString}.json`)
+  let dailyVolume = 0
+  let openInterestAtEnd = 0
+  for (const market of data) {
+    dailyVolume += market.daily_volume
+    openInterestAtEnd += market.open_interest
+  }
+  return { dailyVolume, openInterestAtEnd }
+}
 
 export default {
-  runAtCurrTime: true,
-  version: 2,
   chains: [CHAIN.KALSHI],
-  fetch: async (options: FetchOptions) => {
-    const url = "https://api.elections.kalshi.com/trade-api/v2/markets"
-    let cursor: any = undefined
-    let dailyVolume = 0
-    let openInterestAtEnd = 0
-
-
-    do {
-      const { data } = await axios.get(url, {
-        params: {
-          cursor,
-          limit: 1000,
-          status: 'open,closed,settled', // https://docs.kalshi.com/api-reference/market/get-markets#parameter-status
-          min_close_ts: options.fromTimestamp // filter for only markets that close only after our start time
-        }
-      })
-      options.api.log(`fetched ${data.markets.length} markets from kalshi with cursor ${data.cursor}`)
-      cursor = data.cursor
-
-      for (const market of data.markets) {
-        dailyVolume += market.volume_24h
-        openInterestAtEnd += market.open_interest
-      }
-    } while (cursor);
-
-    return { dailyVolume, openInterestAtEnd }
+  fetch: async (_: any, _1: any, options: FetchOptions) => {
+    return historicFetch(options)
   }
 }
