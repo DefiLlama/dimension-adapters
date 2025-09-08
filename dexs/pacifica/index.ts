2a4dc98b3283ac093d4e34425e4b265a04dd9a2d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-08 14:17:29 +0200

    refactor

--- dexs/pacifica/index.ts
+++ dexs/pacifica/index.ts
@@ -1,83 +1,18 @@
-// @ts-ignore
-const { sumTokens2 } = require('../helper/solana');
-// @ts-ignore
-const { getConfig } = require('../helper/cache');
-// @ts-ignore
-const { calculateVolume } = require('./volume');
+import { CHAIN } from "../../helpers/chains";
+import { httpGet } from "../../utils/fetchURL";
 
-// Pacifica Exchange - Solana only
-const PACIFICA_SOLANA_CONTRACT = '72R843XwZxqWhsJceARQQTTbYtWy6Zw9et2YV4FpRHTa';
-
-// Pacifica API Endpoints (confirmed)
-const PACIFICA_INFO_API = 'https://api.pacifica.fi/api/v1/info';
-
-const OWNER_MAP: { [chain: string]: string } = {
-  'solana': '72R843XwZxqWhsJceARQQTTbYtWy6Zw9et2YV4FpRHTa',
-};
-
-// TypeScript interfaces for API responses
-interface PacificaMarketInfo {
-  symbol: string;
-  tick_size: string;
-  min_tick: string;
-  max_tick: string;
-  lot_size: string;
-  max_leverage: number;
-  isolated_only: boolean;
-  min_order_size: string;
-  max_order_size: string;
-  funding_rate: string;
-  next_funding_rate: string;
-}
-
-
-interface PacificaInfoApiResponse {
-  success: boolean;
-  data: PacificaMarketInfo[];
-  error: string | null;
-  code: string | null;
-}
-
-interface ApiContext {
-  chain: string;
-  chainId: number;
-}
-
-
-async function fetchTokenData(api: ApiContext): Promise<string[]> {
-  // Pacifica is a perps exchange that only accepts USDC as collateral
-  // USDC on Solana mint address
-  return ['EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'];
-}
-
-
-async function fetchMarketInfo(): Promise<PacificaMarketInfo[]> {
-  try {
-    const response: PacificaInfoApiResponse = await getConfig('pacifica/market-info', PACIFICA_INFO_API);
-    return response.data || [];
-  } catch (error) {
-    console.error('Error fetching Pacifica market info:', error);
-    return [];
+export default {
+  chains: [CHAIN.SOLANA],
+  runAtCurrTime: true,
+  version: 2,
+  fetch: async () => {
+    const { data } = await httpGet('https://api.pacifica.fi/api/v1/info/prices')
+    const volume = data.reduce((a: number, b: { volume_24h: string }) => a + Number(b.volume_24h), 0)
+    const oi = data.reduce((a: number, b: { open_interest	: string }) => a + Number(b.open_interest	), 0)
+    return {
+      dailyVolume: volume,
+      openInterestAtEnd: oi,
+    };
   }
-}
 
-// @ts-ignore
-module.exports = {
-  timetravel: false,
-  hallmarks: [
-    [Math.floor(new Date('2025-06-10').getTime() / 1000), 'Pacifica Exchange Launch']
-  ],
-  category: 'Derivatives' as const,
-  website: 'https://pacifica.fi/',
-  github: 'https://github.com/pacifica-fi',
-  twitter: 'https://x.com/pacifica_fi',
-  solana: {
-    tvl: async (api: ApiContext) => {
-      return sumTokens2({ owner: OWNER_MAP.solana, tokens: await fetchTokenData(api) });
-    },
-    volume: async (api: ApiContext) => {
-      return await calculateVolume(api);
-    }
-  },
-  methodology: 'USDC collateral deposited into Pacifica Exchange for perpetual futures trading. Volume data fetched from Pacifica API.'
-};
+}
