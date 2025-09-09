b80690aeeb677f39dc8191878752bd688019ebcc
Stone <stone@skatechain.org>
2025-09-09 20:48:13 +0800

    Feat: volume and fee data (#4183)
    * feat: revamp to daily volume and fees
    
    * fix: methodology
    
    * chore: rename
    
    ---------
    
    Co-authored-by: stone <Lau.Stone@vce.com>

--- dexs/skate-amm/index.ts
+++ dexs/skate-amm/index.ts
@@ -1,4 +1,3 @@
-import { encodeBase58, dataSlice, getAddress } from "ethers";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { httpGet } from "../../utils/fetchURL";
@@ -16,65 +15,36 @@ const skateChainIds = {
     [CHAIN.SUI]: 1001
 }
 
-const skateDataApi = "https://data.skatechain.org/pools/stats";
-const tokenDetailsApi = "https://api.amm.skatechain.org/config/deployment/pools"
+const skateDataApi = "https://revamp-data.skatechain.org/pools/stats";
 
 const fetch = async (options: FetchOptions) => {
-    // Get token details
-    const token_details_response = await httpGet(tokenDetailsApi);
-    const token_details = Object.values(token_details_response.data);
 
-    // Get the swaps 
-    let dailyVolume = options.createBalances();
+    const dailyVolume = options.createBalances();
+    const dailyFees = options.createBalances();
 
     const tokenVolume_options = {
         params: {
-            metric: "volume",
-            chainIds: skateChainIds[options.api.chain],
+            chainId: skateChainIds[options.api.chain],
             startTime: options.startTimestamp,
             endTime: options.endTimestamp,
-            settlementTerms: "token"
         }
     }
-    let tokenVolume = await httpGet(skateDataApi, tokenVolume_options);
+    const tokenVolumeInfo = await httpGet(skateDataApi, tokenVolume_options);
 
-    tokenVolume = tokenVolume.data.map((pool) => {
-        return {
-            kernel_pool_address: pool.kernel_pool_address,
-            description: pool.description,
-            periphery_token0_address: (options.api.chain === CHAIN.SOLANA || options.api.chain === CHAIN.ECLIPSE)
-                ? encodeBase58(pool.periphery_token0_address)
-                : getAddress(dataSlice(pool.periphery_token0_address, 12)), // Convert bytes32 Address to bytes20 Address
-            periphery_token1_address: (options.api.chain === CHAIN.SOLANA || options.api.chain === CHAIN.ECLIPSE)
-                ? encodeBase58(pool.periphery_token1_address)
-                : getAddress(dataSlice(pool.periphery_token1_address, 12)), // Convert bytes32 Address to bytes20 Address
-            total_token0_volume_in: parseFloat(pool.total_token0_volume_in),
-            total_token1_volume_in: parseFloat(pool.total_token1_volume_in),
-        }
-    })
-
-    for (const pair of tokenVolume) {
-        try {
-            // Look for the matching pair by kernelPool
-            const matching_pool = token_details.find(pool => pool.kernelPool.toLowerCase() === pair.kernel_pool_address.toLowerCase());
-            // Retrieve by periphery chainId
-            const matching_pool_details = matching_pool.peripheryInfo[skateChainIds[options.api.chain]];
-
-            dailyVolume.add(pair.periphery_token0_address, pair.total_token0_volume_in * 10 ** (matching_pool_details['token0'].decimal));
-            dailyVolume.add(pair.periphery_token1_address, pair.total_token1_volume_in * 10 ** (matching_pool_details['token1'].decimal));
-        } catch (e) {
-            // Silently fail
-            // console.log("Error: ", e);
-        }
+    for (const tokenInfo of tokenVolumeInfo.data) {
+        dailyVolume.add(tokenInfo.token, tokenInfo.volume);
+        dailyFees.add(tokenInfo.token, tokenInfo.fees);
     }
 
     return {
-        dailyVolume
+        dailyVolume,
+        dailyFees
     }
 };
 
 const methodology = {
-    "Volume": "Describes the amount of tokens swapped into the pool."
+    Volume: "Total token swap volumes by users.",
+    Fees: 'All fees paid by users for trading.'
 }
 
 const adapter: SimpleAdapter = {
