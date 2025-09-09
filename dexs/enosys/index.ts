2c7982b0e54dd956b2e4181ef00eeb7ef1e80475
Eden <noat.eth@gmail.com>
2025-09-10 03:30:02 +0700

    fix enosys amm v3 volume

--- dexs/enosys/index.ts
+++ dexs/enosys/index.ts
@@ -1,21 +1,12 @@
-import { FetchOptions, FetchV2, SimpleAdapter } from "../../adapters/types";
+import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import fetchURL from "../../utils/fetchURL";
-
-const URL_FLARE = "http://flare.index.enosys.global/v1/public/24hourV3Volume";
-
-const fetchVolume: FetchV2 = async () => {
-  const dailyVolume = await fetchURL(URL_FLARE);
-
-  return { dailyVolume };
-};
+import { getUniV3LogAdapter } from "../../helpers/uniswap";
 
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
     [CHAIN.FLARE]: {
-      runAtCurrTime: true,
-      fetch: fetchVolume,
+      fetch: getUniV3LogAdapter({ factory: '0x17AA157AC8C54034381b840Cb8f6bf7Fc355f0de'}),
       start: '2025-03-03',
     },
   },
