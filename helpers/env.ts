2dd628ca69df77a3648b76153f9d10b2b908edfd
cto-virtus <ceo@virtus-protocol.com>
2025-09-09 16:12:04 +0300

    feat: Add Virtus aggregator adapter using backend daily volume endpoint (#4177)
    * Add Virtus aggregator adapter using backend daily volume endpoint
    
    * minor refactor

--- helpers/env.ts
+++ helpers/env.ts
@@ -29,6 +29,7 @@ const DEFAULTS: any = {
   NIBIRU_RPC: "https://evm-rpc.archive.nibiru.fi",
   TAC_RPC_MULTICALL: "0xcA11bde05977b3631167028862bE2a173976CA11",
   SOLANA_RPC: "https://api.mainnet-beta.solana.com",
+  VIRTUS_BACKEND_BASE: 'https://back.virtus-protocol.com/api',
 }
 
 export const ENV_KEYS = new Set([
@@ -64,6 +65,7 @@ export const ENV_KEYS = new Set([
   'METAPLEX_API_KEY',
   'DEFIAPP_API_KEY',
   'SMARDEX_SUBGRAPH_API_KEY',
+  'VIRTUS_BACKEND_BASE',
 ])
 
 // This is done to support both ZEROx_API_KEY and ZEROX_API_KEY
