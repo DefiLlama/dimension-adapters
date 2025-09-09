a9b43a0b193e8b71a772282f7b367167cf93bf33
Eden <noat.eth@gmail.com>
2025-09-10 03:36:22 +0700

    track fees enosys v2, v3

--- dexs/enosys/index.ts
+++ dexs/enosys/index.ts
@@ -6,7 +6,7 @@ const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
     [CHAIN.FLARE]: {
-      fetch: getUniV3LogAdapter({ factory: '0x17AA157AC8C54034381b840Cb8f6bf7Fc355f0de'}),
+      fetch: getUniV3LogAdapter({ factory: '0x17AA157AC8C54034381b840Cb8f6bf7Fc355f0de', userFeesRatio: 1, revenueRatio: 0.1, protocolRevenueRatio: 0.1 }),
       start: '2025-03-03',
     },
   },
