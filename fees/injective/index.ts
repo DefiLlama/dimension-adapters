a3fdac1480fd7d8e070a984d3430d039670ea929
manualy <danielswietytrue@gmail.com>
2025-09-15 16:18:22 +0200

    holdersRevenue calculation change

--- fees/injective/index.ts
+++ fees/injective/index.ts
@@ -1,11 +1,34 @@
 import { ChainBlocks, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { httpGet } from "../../utils/fetchURL";
 
-interface IResponse {
+interface IFeesResponse {
   exchange_fees_usd: number;
   gas_fees: number;
   total_fees_usd: number;
 }
+
+interface IRewardsResponse {
+  total_reward_usd: number;
+}
+
+interface IAuctionsDailyData {
+  amount_inj: string;
+  amount_stablecoin: string;
+  date: string;
+  is_auction_day: boolean;
+  price_inj: string;
+  round: number;
+  usd_value: string;
+  winner: string;
+  winning_bid: string;
+}
+
+interface IAuctionsResponse {
+  days: IAuctionsDailyData[];
+  total_usd_value: number;
+}
+
+const BASE_URL = "https://bigquery-api-636134865280.europe-west1.run.app";
 
 const fetchFees = async (
   timestamp: number,
@@ -15,16 +38,30 @@ const fetchFees = async (
   const dateStr = new Date(options.startOfDay * 1000)
     .toISOString()
     .split("T")[0];
-  const url = `https://bigquery-api-636134865280.europe-west1.run.app/fees?start_date=${dateStr}`;
-  const res: IResponse = await httpGet(url);
 
-  const totalDailyFees = res.total_fees_usd;
+  const feesUrl = `${BASE_URL}/fees?start_date=${dateStr}`;
+  const feesRes: IFeesResponse = await httpGet(feesUrl);
+
+  const rewardsUrl = `${BASE_URL}/rewards?start_date=${dateStr}`;
+  const rewardsRes: IRewardsResponse = await httpGet(rewardsUrl);
+
+  const auctionUrl = `${BASE_URL}/auction?start_date=${dateStr}`;
+  const auctionRes: IAuctionsResponse = await httpGet(auctionUrl);
+
+  const totalDailyFees = feesRes.total_fees_usd;
+  const totalRewards = rewardsRes.total_reward_usd;
+
+  const totalBurn = auctionRes.days.reduce((sum, day) => {
+    const amountBurned = parseFloat(day.winning_bid);
+    const priceInj = parseFloat(day.price_inj);
+    return sum + amountBurned * priceInj;
+  }, 0);
 
   return {
     dailyFees: totalDailyFees,
     dailyRevenue: totalDailyFees,
-    dailyHoldersRevenue: totalDailyFees,
-    timestamp: timestamp,
+    dailyHoldersRevenue: totalRewards + totalBurn,
+    timestamp,
   };
 };
 
