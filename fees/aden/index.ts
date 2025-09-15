d7ad9017becdb55580504f6a2aba4458b69a85d2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-15 16:03:43 +0200

    aden: track aster data #4131

--- fees/aden/index.ts
+++ fees/aden/index.ts
@@ -1,9 +1,43 @@
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
 import { getBuilderExports } from "../../helpers/orderly";
+import { httpGet } from "../../utils/fetchURL";
+
+let asterBuilderData: any = null
+async function asterFetch(_: any, _1: any, { dateString }: FetchOptions) {
+  const asterVolumeEndpoint = "https://www.asterdex.com/bapi/futures/v1/public/future/volume/builder/daily_stats/aden";
+  if (!asterBuilderData) asterBuilderData = httpGet(asterVolumeEndpoint).then(({ data }) => {
+    const dateDataMap: any = {}
+    data.forEach((i: any) => {
+      dateDataMap[i.date.slice(0, 10)] = i
+    })
+    return dateDataMap
+  })
+
+  const data = (await asterBuilderData)[dateString]
+
+  if (!data)
+    throw new Error('Data missing for date: ' + dateString)
+
+  const dailyVolume = +data.takerVolume + +data.makerVolume
+  const dailyFees = +data.builderFee
+
+  const response: any = { dailyVolume, dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, dailyHoldersRevenue: 0 }
+
+  return response
+}
 
 const methodology = {
-  Fees: "Builder Fees collected from Orderly Network(0.3 bps on taker volume).",
-  Revenue: "0.3 bps trading fees on taker volume, 0 on maker volume",
+  Fees: "Builder Fees collected from Orderly Network(0.3 bps on taker volume) and Aster Exchange(0.4 bps on taker volume)",
+  Revenue: "All the fees collected",
   ProtocolRevenue: "All the revenue go to the protocol",
 }
 
-export default getBuilderExports({ broker_id: 'aden', start: '2025-07-14', methodology });
+const adapter = getBuilderExports({ broker_id: 'aden', start: '2025-07-14', methodology }) as SimpleAdapter
+
+adapter.adapter = {
+  [CHAIN.ORDERLY]: { start: '2025-07-14', fetch: adapter.fetch, },
+  [CHAIN.OFF_CHAIN]: { start: '2025-07-19', fetch: asterFetch },
+}
+
+export default adapter
