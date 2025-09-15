f2591fddd4139ab5105ed1c3f6a76fba6f78b0f7
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-09-15 12:03:05 +0530

    add tAvax

--- fees/treehouse-protocol/index.ts
+++ fees/treehouse-protocol/index.ts
@@ -9,44 +9,52 @@ const EVENT_ABI = {
 };
 
 const ADDRESSES = {
-    TREEHOUSE_ACCOUNTING: "0xb7Ce3cb5Bc5c00cd2f9B39d9b0580f5355535709",
-    TREEHOUSE_ETHEREUM: "0xD11c452fc99cF405034ee446803b6F6c1F6d5ED8",
-    TREEHOUSE_REDEMPTION: "0xcd63a29FAfF07130d3Af89bB4f40778938AaBB85",
-    TREEHOUSE_FASTLANE_REDEMPTION: "0x829525417Cd78CBa0f99A8736426fC299506C0d6",
-    LIDO_WST_ETHEREUM: "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"
+    [CHAIN.ETHEREUM]: {
+        TREEHOUSE_ACCOUNTING: "0xb7Ce3cb5Bc5c00cd2f9B39d9b0580f5355535709",
+        TREEHOUSE_TOKEN: "0xD11c452fc99cF405034ee446803b6F6c1F6d5ED8", //tEth
+        TREEHOUSE_REDEMPTION: "0xcd63a29FAfF07130d3Af89bB4f40778938AaBB85",
+        TREEHOUSE_FASTLANE_REDEMPTION: "0x829525417Cd78CBa0f99A8736426fC299506C0d6",
+        STAKED_TOKEN: "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0" // Lido wstEth
+    },
+    [CHAIN.AVAX]: {
+        TREEHOUSE_ACCOUNTING: "0x6f5D00a263dE6d40B4b2342996D2682E34f8A454",
+        TREEHOUSE_TOKEN: "0x14a84f1a61ccd7d1be596a6cc11fe33a36bc1646", //tAvax
+        TREEHOUSE_REDEMPTION: "0x765f6dc8496ca7EF1e4a391bE10185229AACf04b",
+        TREEHOUSE_FASTLANE_REDEMPTION: "0x3D00a639183B07e35EFEF044eE6cC14e8598A01c",
+        STAKED_TOKEN: "0x2b2c81e08f1af8835a78bb2a90ae924ace0ea4be" //benqi sAvax
+    }
 };
 
 async function fetch(_a: any, _b: any, options: FetchOptions) {
     let dailyFees = options.createBalances();
     const dailySupplySideRevenue = options.createBalances();
     const dailyProtocolRevenue = options.createBalances();
-
     const markedLogs = await options.getLogs({
-        target: ADDRESSES.TREEHOUSE_ACCOUNTING,
+        target: ADDRESSES[options.chain as keyof typeof ADDRESSES].TREEHOUSE_ACCOUNTING,
         eventAbi: EVENT_ABI.MARKED,
     });
 
     markedLogs.forEach(log => {
-        dailySupplySideRevenue.add(ADDRESSES.TREEHOUSE_ETHEREUM, log.amount);
-        dailyProtocolRevenue.add(ADDRESSES.TREEHOUSE_ETHEREUM, log.fees);
+        dailySupplySideRevenue.add(ADDRESSES[options.chain as keyof typeof ADDRESSES].TREEHOUSE_TOKEN, log.amount);
+        dailyProtocolRevenue.add(ADDRESSES[options.chain as keyof typeof ADDRESSES].TREEHOUSE_TOKEN, log.fees);
     });
 
     const standardRedemptionLogs = await options.getLogs({
-        target: ADDRESSES.TREEHOUSE_REDEMPTION,
+        target: ADDRESSES[options.chain as keyof typeof ADDRESSES].TREEHOUSE_REDEMPTION,
         eventAbi: EVENT_ABI.STANDARD_REDEMPTION
     });
 
     standardRedemptionLogs.forEach(log => {
-        dailyProtocolRevenue.add(ADDRESSES.TREEHOUSE_ETHEREUM, log.fee)
+        dailyProtocolRevenue.add(ADDRESSES[options.chain as keyof typeof ADDRESSES].TREEHOUSE_TOKEN, log.fee)
     });
 
     const fastlaneRedemptionLogs = await options.getLogs({
-        target: ADDRESSES.TREEHOUSE_FASTLANE_REDEMPTION,
+        target: ADDRESSES[options.chain as keyof typeof ADDRESSES].TREEHOUSE_FASTLANE_REDEMPTION,
         eventAbi: EVENT_ABI.FASTLANE_REDEMPTION
     });
 
     fastlaneRedemptionLogs.forEach(log => {
-        dailyProtocolRevenue.add(ADDRESSES.LIDO_WST_ETHEREUM, log.fee)
+        dailyProtocolRevenue.add(ADDRESSES[options.chain as keyof typeof ADDRESSES].STAKED_TOKEN, log.fee)
     });
 
     dailyFees = dailySupplySideRevenue.clone();
@@ -63,7 +71,7 @@ async function fetch(_a: any, _b: any, options: FetchOptions) {
 
 const methodology = {
     Fees: "Includes Market Effective Yield(MEY) earned by treehouse assets and redemption fee",
-    Revenue: "Standard Redemption(7 days waiting) fee of 0.05%,Fastlane redemption fee of 2% and 20% performance fee on MEY",
+    Revenue: "Standard Redemption(7 days waiting(tEth), 17 days waiting(tAvax)) fee of 0.05%,Fastlane redemption fee of 2%(tEth), 4%(tAvax) and 20% performance fee on MEY",
     ProtocolRevenue: "All the revenue goes to protocol treasury",
     HoldersRevenue: "No fee sharing to holders yet",
     SupplySideRevenue: "MEY earned by treehouse asset holders post performance fee",
@@ -86,10 +94,12 @@ const breakdownMethodology = {
 
 const adapter: SimpleAdapter = {
     fetch,
-    chains: [CHAIN.ETHEREUM],
     methodology,
     breakdownMethodology,
-    start: '2024-09-10'
+    adapter: {
+        [CHAIN.ETHEREUM]: { start: '2024-09-10' },
+        [CHAIN.AVAX]: { start: '2025-08-28' }
+    }
 };
 
 export default adapter;
