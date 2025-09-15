12cf5a9f1d6a2b2af69047d49a387877785f5a77
Kalidas <Kalidas@alkimiexchange.com>
2025-09-15 15:30:38 +0100

    feat(alkimi): add fees adapter using media spend revenue API

---
+++ fees/alkimi/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  methodology: "Alkimi collects fees on media spend; these are later distributed as ALKIMI tokens to stakers.",
+  timetravel: false,
+  fees: require("./fees"),
+};
