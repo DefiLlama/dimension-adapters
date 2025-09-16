9066689413b1a6955cdd02da4ca365d0a704169e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-16 11:21:10 +0200

    fix flamingo dex

--- dexs/flamingo-finance/index.ts
+++ dexs/flamingo-finance/index.ts
@@ -1,23 +1,27 @@
-import { SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import fetchURL from "../../utils/fetchURL";
-const { getUniqStartOfTodayTimestamp } = require("../../helpers/getUniSubgraphVolume");
 
-const fetch = async (timestamp: number) => {
+const fetch = async (_: any, _1: any, { dateString }: FetchOptions) => {
 
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const data = (await fetchURL('https://api.flamingo.finance/project-info/defillama-volume?timestamp=' + dayTimestamp));
+  const data = (await fetchURL('https://flamingo-us-1.b-cdn.net/flamingo/analytics/rolling-30-days/pool_data'))
+  const dayData = data.find((day: any) => day.date.slice(0, 10) === dateString)
+  if (!dayData) throw new Error(`No data for date ${dateString}`)
+  let dailyVolume = 0;
+  let dailyFees = 0
 
-  return {
-    dailyVolume: data.volume,
-    timestamp: data.timestamp,
-  };
+  Object.values(dayData.pool_data).forEach((pool: any) => {
+    if (pool.fees_usd_total) dailyFees += +pool.fees_usd_total
+    if (pool.volume_usd_total) dailyVolume += +pool.volume_usd_total
+  })
+
+  return { dailyVolume, dailyFees, };
 };
 
 const adapter: SimpleAdapter = {
   adapter: {
     neo: {
       fetch,
-      start: '2021-12-10',
+      start: '2025-08-18',
     },
   },
 };
