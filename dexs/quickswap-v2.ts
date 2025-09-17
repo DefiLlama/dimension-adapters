05989628c1c375915eefee629eaa074388043d37
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-17 14:10:28 +0200

    fix quickswap dex

--- dexs/quickswap-v2.ts
+++ dexs/quickswap-v2.ts
@@ -1,45 +1,43 @@
 import * as sdk from "@defillama/sdk";
 import { CHAIN } from "../helpers/chains";
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
-import { getUniV2LogAdapter } from "../helpers/uniswap";
-import { DEFAULT_DAILY_VOLUME_FACTORY, DEFAULT_DAILY_VOLUME_FIELD, DEFAULT_TOTAL_VOLUME_FACTORY, DEFAULT_TOTAL_VOLUME_FIELD } from "../helpers/getUniSubgraphVolume";
-import { getGraphDimensions2 } from "../helpers/getUniSubgraph";
+import { uniV2Exports } from "../helpers/uniswap";
+import request from "graphql-request"
 
 const endpoints = {
   [CHAIN.POLYGON]: sdk.graph.modifyEndpoint("FUWdkXWpi8JyhAnhKL5pZcVshpxuaUQG8JHMDqNCxjPd"),
 };
 
-const graphs = getGraphDimensions2({
-  graphUrls: endpoints,
-  totalVolume: {
-    factory: DEFAULT_TOTAL_VOLUME_FACTORY,
-    field: DEFAULT_TOTAL_VOLUME_FIELD,
-  },
-  dailyVolume: {
-    factory: DEFAULT_DAILY_VOLUME_FACTORY,
-    field: DEFAULT_DAILY_VOLUME_FIELD,
-    dateField: "date",
-  },
-  feesPercent: {
-    type: "volume" as const,
-    Fees: 0.3,
-    UserFees: 0.3,
-    Revenue: 0.05 / 0.3,
-    ProtocolRevenue: 0.01/ 0.3,
-    SupplySideRevenue: 0.25 / 0.3,
-    HoldersRevenue: 0.04 / 0.3,
-  }
-});
+const onChainAdapter: any = uniV2Exports({
+  [CHAIN.BASE]: {
+    factory: '0xEC6540261aaaE13F236A032d454dc9287E52e56A',
+    start: '2020-10-08',
+    userFeesRatio: 1,
+    revenueRatio: 0.05 / 0.3,
+    protocolRevenueRatio: 0.05 / 0.3,
+    holdersRevenueRatio: 0,
 
-const univ2LogConfig = {
-  userFeesRatio: 1,
-  revenueRatio: 0.05 / 0.3,
-  protocolRevenueRatio: 0.05 / 0.3,
-  holdersRevenueRatio: 0,
+  },
+}, { runAsV1: true })
+
+async function fetch(_: any, _1: any, { startOfDay }: FetchOptions) {
+  const dayId = Math.floor(startOfDay / 86400)
+
+  const query = `{    uniswapDayData(id: ${dayId}) {      dailyVolumeUSD    }  }`
+  const { uniswapDayData: { dailyVolumeUSD } } = await request(endpoints[CHAIN.POLYGON], query)
+  const dailyFees = dailyVolumeUSD * 0.003
+  return {
+    dailyVolume: dailyVolumeUSD,
+    dailyFees,
+    dailyRevenue: dailyFees * 0.05 / 0.3,
+    dailyProtocolRevenue: dailyFees * 0.05 / 0.3,
+    dailyHoldersRevenue: 0,
+    dailyUserFees: dailyFees,
+  }
 }
 
 const adapter: SimpleAdapter = {
-  version: 2,
+  version: 1,
   methodology: {
     UserFees: "User pays 0.3% fees on each swap.",
     Fees: "0.3% of each swap is collected as trading fees",
@@ -49,8 +47,8 @@ const adapter: SimpleAdapter = {
     HoldersRevenue: "Community receives 13.33% of collected fees for buybacks (0.04% of swap volume).",
   },
   adapter: {
-    [CHAIN.POLYGON]: { fetch: async (options: FetchOptions) => { return await graphs(options) }, start: '2020-10-08' },
-    [CHAIN.BASE]: { fetch: getUniV2LogAdapter({ factory: '0xEC6540261aaaE13F236A032d454dc9287E52e56A', ...univ2LogConfig }), start: '2025-06-14' },
+    [CHAIN.POLYGON]: { fetch, },
+    [CHAIN.BASE]: onChainAdapter!.adapter.base,
     // [CHAIN.DOGECHAIN]: { fetch: getUniV2LogAdapter({ factory: '0xC3550497E591Ac6ed7a7E03ffC711CfB7412E57F', ...univ2LogConfig }), start: '2023-04-11' },
   },
 }
--- fees/quickswap-v2.ts
+++
@@ -1,4 +0,0 @@
-
-import adapter from '../dexs/quickswap-v2'
-
-export default adapter;
