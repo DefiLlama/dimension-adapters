135c99e67cf20b38806c58d91f3dab4f760abb25
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-20 11:53:58 +0200

    revert: inj: skip OI data

--- dexs/injective-derivatives.ts
+++ dexs/injective-derivatives.ts
@@ -7,13 +7,13 @@ const OPEN_INTEREST_URL = `https://bigquery-api-636134865280.europe-west1.run.ap
 
 const fetch = async (_: number, _t: any, options: FetchOptions) => {
   const derivativeRes: any = await httpGet(`${DERIVATIVE_URL}?start_date=${options.dateString}`);
-  // const openInterestRes: any = await httpGet(`${OPEN_INTEREST_URL}?start_date=${options.dateString}`);
+  const openInterestRes: any = await httpGet(`${OPEN_INTEREST_URL}?start_date=${options.dateString}`);
   if (derivativeRes.days.length !== 1) throw new Error("No data found for the given date: " + options.dateString);
-  // if (openInterestRes.days.length !== 1) throw new Error("No OI data found for the given date: " + options.dateString);
+  if (openInterestRes.days.length !== 1) throw new Error("No OI data found for the given date: " + options.dateString);
 
   return {
     dailyVolume: derivativeRes.total_volume_usd,
-    // openInterestAtEnd: openInterestRes.total_open_interest,
+    openInterestAtEnd: openInterestRes.total_open_interest,
   };
 };
 
