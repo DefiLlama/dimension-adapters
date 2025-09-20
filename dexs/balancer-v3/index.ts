eec3eb19247fc1bcad078ea9d99d8181029dc680
Eden <noat.eth@gmail.com>
2025-09-20 09:52:05 +0700

    update balancer v3 fees

--- dexs/balancer-v3/index.ts
+++ dexs/balancer-v3/index.ts
@@ -1,6 +1,7 @@
 import request from "graphql-request";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { METRIC } from "../../helpers/metrics";
 
 const v3ChainMapping: any = {
   [CHAIN.ETHEREUM]: "MAINNET",
@@ -15,6 +16,7 @@ const v3ChainMapping: any = {
 async function fetch(options: FetchOptions) {
   const dailyVolume = options.createBalances();
   const dailyFees = options.createBalances();
+  const dailyUserFees = options.createBalances();
   const dailyRevenue = options.createBalances();
   const dailySupplySideRevenue = options.createBalances();
 
@@ -42,15 +44,22 @@ async function fetch(options: FetchOptions) {
 }`;
   const { pools } = await request("https://api-v3.balancer.fi/graphql", query);
   pools.forEach((pool: any) => {
-    dailyFees.addUSDValue(+pool.dynamicData.fees24h);
-    dailyFees.addUSDValue(+pool.dynamicData.yieldCapture24h);
     dailyVolume.addUSDValue(+pool.dynamicData.volume24h);
-    dailyRevenue.addUSDValue(+(pool.dynamicData.fees24h * 0.5)); // 50% of fees go to the protocol
-    dailyRevenue.addUSDValue(+(pool.dynamicData.yieldCapture24h * 0.1)); // 10% of yield capture goes to the protocol
-    dailySupplySideRevenue.addUSDValue(+pool.dynamicData.yieldCapture24h * 0.9); // 90% of yield capture goes to the supply side
-    dailySupplySideRevenue.addUSDValue(+pool.dynamicData.fees24h * 0.5); // 50% of fees goes to the supply side
+
+    dailyFees.addUSDValue(+pool.dynamicData.fees24h, METRIC.SWAP_FEES);
+    dailyUserFees.addUSDValue(+pool.dynamicData.fees24h, METRIC.SWAP_FEES);
+    dailyRevenue.addUSDValue(+(pool.dynamicData.fees24h * 0.5), METRIC.SWAP_FEES); // 50% of fees go to the protocol
+    dailySupplySideRevenue.addUSDValue(+pool.dynamicData.fees24h * 0.5, METRIC.SWAP_FEES); // 50% of fees goes to the supply side
+    
+    // subgraph error on hyperlqiuid yields
+    if (options.chain !== CHAIN.HYPERLIQUID) {
+      dailyFees.addUSDValue(+pool.dynamicData.yieldCapture24h, METRIC.ASSETS_YIELDS);
+      dailyRevenue.addUSDValue(+(pool.dynamicData.yieldCapture24h * 0.1), METRIC.ASSETS_YIELDS); // 10% of yield capture goes to the protocol
+      dailySupplySideRevenue.addUSDValue(+pool.dynamicData.yieldCapture24h * 0.9, METRIC.ASSETS_YIELDS); // 90% of yield capture goes to the supply side
+    }
   });
-  return { dailyFees, dailyVolume, dailyRevenue, dailyProtocolRevenue: dailyRevenue, dailySupplySideRevenue };
+
+  return { dailyFees, dailyUserFees, dailyVolume, dailyRevenue, dailyProtocolRevenue: dailyRevenue, dailySupplySideRevenue };
 }
 
 const adapter: SimpleAdapter = {
@@ -59,11 +68,33 @@ const adapter: SimpleAdapter = {
   runAtCurrTime: true,
   chains: Object.keys(v3ChainMapping),
   methodology: {
-    Fees: "Fees earned from all the trades and yield in the last 24 hours.",
-    Revenue: "Revenue earned by the protocol in the last 24 hours, which is 50% of the trade fees and 10% of the yield capture.",
-    ProtocolRevenue: "Revenue earned by the protocol in the last 24 hours, which is 50% of the trade fees and 10% of the yield capture.",
-    SupplySideRevenue: "Revenue earned by the supply side in the last 24 hours, which is 90% of the yield capture and 50% of the fees.",
+    Fees: "Fees earned from all the trades and yields.",
+    UserFees: "Fees earned from all the trades.",
+    Revenue: "Revenue earned by the protocol, which is 50% of the trade fees and 10% of the yield capture.",
+    ProtocolRevenue: "Revenue earned by the protocol, which is 50% of the trade fees and 10% of the yield capture.",
+    SupplySideRevenue: "Revenue earned by the supply side, which is 90% of the yield capture and 50% of the fees.",
   },
+  breakdownMethodology: {
+    Fees: {
+      [METRIC.SWAP_FEES]: 'Swap fees paid by users from all trades.',
+      [METRIC.ASSETS_YIELDS]: 'Yields captured from all assets in liquity pools.',
+    },
+    UserFees: {
+      [METRIC.SWAP_FEES]: 'Swap fees paid by users from all trades.',
+    },
+    Revenue: {
+      [METRIC.SWAP_FEES]: '50% of swap fees paid by users from all trades.',
+      [METRIC.ASSETS_YIELDS]: '10% of yields captured from all assets in liquity pools.',
+    },
+    ProtocolRevenue: {
+      [METRIC.SWAP_FEES]: '50% of swap fees paid by users from all trades.',
+      [METRIC.ASSETS_YIELDS]: '10% of yields captured from all assets in liquity pools.',
+    },
+    SupplySideRevenue: {
+      [METRIC.SWAP_FEES]: '50% of swap fees paid by users from all trades.',
+      [METRIC.ASSETS_YIELDS]: '90% of yields captured from all assets in liquity pools.',
+    },
+  }
 };
 
 export default adapter;
