4973a6f5140998648c5400f50ed94a92a990426a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-22 10:34:54 +0200

    bugfix

--- options/dopex/clamm.ts
+++ options/dopex/clamm.ts
@@ -66,7 +66,6 @@ async function getChainStats({ graphUrl, timestamp }: IGetChainStatsParams) {
   );
 
   return {
-    timestamp: dayStart.toString(),
     ...daily,
     dailyFees: daily.dailyRevenue,
   };
