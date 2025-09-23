f200520dd7f735513d750152d14dc02a54daee39
small-debug <mathkindom@gmail.com>
2025-09-23 10:58:07 +0900

    Added Qubic chain revenue for Mining

---
+++ fees/qubic/index.ts
@@ -0,0 +1,27 @@
+import { SimpleAdapter, FetchOptions } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { httpGet } from "../../utils/fetchURL";
+const RPC_ENDPOINT = "https://rpc.qubic.org"
+
+const fetchfunc = async (_: any, _1: any, _options: FetchOptions) => {
+    const res = await fetch(`${RPC_ENDPOINT}/v1/latest-stats`).then((res) => res.json());
+    const dailyFees = res.data.burnedQus * res.data.price;
+    return {dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, dailyHoldersRevenue: 0}
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  methodology: {
+    Fees: 'All fees paid by users for swap and bridge tokens via Qubic.',
+    Revenue: 'Fees are distributed to Qubic.',
+    ProtocolRevenue: 'Fees are distributed to Qubic.',
+  },
+  adapter: {
+  [CHAIN.QUBIC]: {
+    fetch: fetchfunc,
+    start: '2025-05-14',
+  },
+}
+}
+
+export default adapter;
--- helpers/chains.ts
+++ helpers/chains.ts
@@ -266,4 +266,5 @@ export enum CHAIN {
   EDGEX = "edgex",
   AKASH = "akash",
   DOGE = "doge",
+  QUBIC = "qubic",
 }
