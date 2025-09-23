436e95230730fd3d86b8c158c93aa7b424cf6c2f
maybeyonas <todarvinrio@gmail.com>
2025-09-23 11:56:53 +0530

    Add hwhype to Hyperwave revenue (#4264)
    * create folder
    
    * hwhlp restructure
    
    * change structure to appending fees
    
    * append hwhype
    
    * add hwhype historical
    
    * refactor
    
    ---------
    
    Co-authored-by: Eden <noat.eth@gmail.com>

---
+++ fees/hyperwave/index.ts
@@ -0,0 +1,39 @@
+import { Adapter, FetchOptions } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getHwhlpFees } from "./hwhlp";
+import { getHwhypeFees } from "./hwhype";
+
+const fetch = async (options: FetchOptions) => {
+    const hwhlpFees = await getHwhlpFees(options);
+    const { dailyFees: hwhypeFees, dailyRevenue} = await getHwhypeFees(options);
+    
+    const dailyFees = options.createBalances();
+    dailyFees.addBalances(hwhlpFees)
+    dailyFees.addBalances(hwhypeFees)
+
+    return {        
+        dailyFees,
+        dailySupplySideRevenue: dailyFees,
+        dailyRevenue: dailyRevenue,
+        dailyProtocolRevenue: dailyRevenue,
+    };
+};
+
+const adapter: Adapter = {
+    version: 2,
+    adapter: {
+        [CHAIN.HYPERLIQUID]: {
+            fetch,
+            start: '2025-06-06',
+        },
+    },
+    allowNegativeValue: true, // PnL can be negative
+    methodology: {
+        Fees: "Yields generated from HLP and hwHYPE vaults.",
+        Revenue: "Yields collected by protocol as revenue, currently, no revenue.",
+        ProtocolRevenue: "Revenue share for protocol, currently no revenue share for Hyperwave protocol.",
+        SupplySideRevenue: "Currewntly, 100% of yields paid to hwHLP and hwHYPE holders, suppliers."
+    },
+};
+
+export default adapter;
