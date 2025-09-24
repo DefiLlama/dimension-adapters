eae5e8c80f48953e49fe3a753140ec07ed1fbbeb
tree <sol.analyzoor@gmail.com>
2025-09-24 12:36:54 +0530

    Add JIP-24 DAO fees update and breakdownmethodology (#4276)
    * Add JIP-24 DAO fees update and breakdownmethodology
    
    * update breakdownmethodology
    
    * fix dune syntax

--- helpers/queries/jito.sql
+++ helpers/queries/jito.sql
@@ -44,15 +44,28 @@ WITH
             AND block_date >= FROM_UNIXTIME({{start}})
             AND block_date < FROM_UNIXTIME({{end}})
     ),
-    revenue AS (
-        SELECT block_date, jitoSOL_amt, usd_amt FROM jitostake_pool_fees
-        UNION ALL
-        SELECT block_date, jitoSOL_amt, usd_amt FROM interceptor_fees
-        UNION ALL
-        SELECT block_date, jitoSOL_amt, usd_amt FROM tip_router_fees
+    bam_mev_tips AS (
+        -- Mev Tips JIP24 (SOL) from query_5725652 logic
+        SELECT
+            a.block_date,
+            a.balance_change / 1e9 AS jitoSOL_amt,
+            p_sol.price * a.balance_change / 1e9 AS usd_amt
+        FROM solana.account_activity a
+        LEFT JOIN solana.transactions t
+            ON a.tx_id = t.id
+            AND a.block_date = t.block_date
+        LEFT JOIN prices.minute AS p_sol
+            ON DATE_TRUNC('minute', a.block_time) = p_sol.timestamp
+            AND p_sol.blockchain = 'solana'
+            AND p_sol.contract_address = FROM_BASE58('So11111111111111111111111111111111111111112')
+        WHERE a.block_date >= date('2025-08-01')
+            AND a.block_date >= FROM_UNIXTIME({{start}})
+            AND a.block_date < FROM_UNIXTIME({{end}})
+            AND a.address = '5eosrve6LktMZgVNszYzebgmmC7BjLK8NoWyRQtcmGTF'
+            AND any_match(account_keys, x -> x = 'T1pyyaTNZsKv2WcRAB8oVnk93mLJw2XzjtVYqCsaHqt')
     )
 SELECT
-    COALESCE(SUM(jitoSOL_amt), 0) AS total_jitoSOL_amt,
-    COALESCE(SUM(usd_amt), 0) AS total_usd_amt
-FROM
-    revenue;
+    (SELECT COALESCE(SUM(usd_amt), 0) FROM jitostake_pool_fees) AS jitostake_pool_fees,
+    (SELECT COALESCE(SUM(usd_amt), 0) FROM interceptor_fees) AS interceptor_fees,
+    (SELECT COALESCE(SUM(usd_amt), 0) FROM tip_router_fees) AS tip_router_fees,
+    (SELECT COALESCE(SUM(usd_amt), 0) FROM bam_mev_tips) AS bam_mev_tips;
