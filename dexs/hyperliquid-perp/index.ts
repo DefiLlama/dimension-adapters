a366c0dea8656eac0d4368192fc88aabc5de340b
eden <noat.eth@gmail.com>
2025-09-26 19:19:16 +0700

    get hl perps data from indexer (#4306)

--- dexs/hyperliquid-perp/index.ts
+++ dexs/hyperliquid-perp/index.ts
@@ -1,29 +1,55 @@
-import type { SimpleAdapter } from "../../adapters/types";
+import type { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { httpGet } from "../../utils/fetchURL";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
-import { httpPost } from "../../utils/fetchURL";
+import { getEnv } from "../../helpers/env";
 
-const URL = "https://api.hyperliquid.xyz/info";
+// const URL = "https://api.hyperliquid.xyz/info";
 
-interface Response {
-  dayNtlVlm: string;
-  openInterest: string;
-  markPx: string;
-}
+// interface Response {
+//   dayNtlVlm: string;
+//   openInterest: string;
+//   markPx: string;
+// }
 
-const fetch = async (timestamp: number) => {
-  const response: Response[] = (await httpPost(URL, {"type": "metaAndAssetCtxs"}))[1];
+// hl indexer only supports data from this date
+const FROM_TIME = 1758499200
+
+const fetch = async (timestamp: number, _: any, options: FetchOptions) => {
+  if (timestamp < FROM_TIME) {
+    throw Error('request data too old, unsupported by LLAMA_HL_INDEXER');
+  }
+
+  // 20250925
+  const endpoint = getEnv('LLAMA_HL_INDEXER')
+  if (!endpoint) {
+    throw Error('missing LLAMA_HL_INDEXER env');
+  }
+
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  const dailyVolume = response.reduce((acc, item) => {
-    return acc + Number(item.dayNtlVlm);
-  },0);
-  const openInterestAtEnd = response.reduce((acc, item) => {
-    return acc +( Number(item.openInterest) * Number(item.markPx));
-  },0);
+  const dateString = new Date(timestamp * 1000).toISOString().split('T')[0].replaceAll('-', '');
+  const response = await httpGet(`${endpoint}/v1/data/hourly?date=${dateString}`);
+  
+  const dailyVolume = options.createBalances()
+  const dailyFees = options.createBalances()
+
+  for (const item of response.data) {
+    dailyVolume.addCGToken('usd-coin', item.volumeUsd);
+    dailyFees.addCGToken('usd-coin', item.perpsFeeByTokens.USDC);
+  }
+
+  // const response: Response[] = (await httpPost(URL, {"type": "metaAndAssetCtxs"}))[1];
+  // const dailyVolume = response.reduce((acc, item) => {
+  //   return acc + Number(item.dayNtlVlm);
+  // },0);
+  // const openInterestAtEnd = response.reduce((acc, item) => {
+  //   return acc +( Number(item.openInterest) * Number(item.markPx));
+  // },0);
 
   return {
-    dailyVolume: dailyVolume?.toString(),
-    openInterestAtEnd: openInterestAtEnd?.toString(),
+    dailyVolume,
+    dailyFees,
+    // openInterestAtEnd: openInterestAtEnd?.toString(),
     timestamp: dayTimestamp,
   };
 };
@@ -33,7 +59,7 @@ const adapter: SimpleAdapter = {
   adapter: {
     [CHAIN.HYPERLIQUID]: {
       fetch,
-      runAtCurrTime: true,
+      // runAtCurrTime: true,
       start: '2023-02-25',
     },
   }
