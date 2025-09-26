e0fe76b54680619d6d15b5f20c5cd8f25ccb270c
Eden <noat.eth@gmail.com>
2025-09-26 12:34:28 +0700

    fix uniswap v4

--- dexs/uniswap-v4.ts
+++ dexs/uniswap-v4.ts
@@ -77,87 +77,87 @@ const SwapEvent = 'event Swap(bytes32 indexed id, address indexed sender, int128
 const FunctionPoolKeys = 'function poolKeys(bytes25) view returns(address currency0, address currency1, uint24 fee, int24 tickSpacing, address hooks)';
 
 const Configs: Record<string, IUniswapConfig> = {
-  [CHAIN.ETHEREUM]: {
-    poolManager: '0x000000000004444c5dc75cB358380D2e3dE08A90',
-    positionManager: '0xbd216513d74c8cf14cf4747e6aaa6420ff64ee9e',
-    source: 'LOGS',
-    start: '2025-01-24',
-    blacklistPoolIds: [
-      '0x78f394840909614a7a1213503e4207d7e62f4a07af85561fc420e7ee6d22d6ce',
-    ],
-  },
-  [CHAIN.UNICHAIN]: {
-    poolManager: '0x1f98400000000000000000000000000000000004',
-    positionManager: '0x4529a01c7a0410167c5740c487a8de60232617bf',
-    source: 'LOGS',
-    start: '2025-01-24',
-  },
-  [CHAIN.OPTIMISM]: {
-    poolManager: '0x9a13f98cb987694c9f086b1f5eb990eea8264ec3',
-    positionManager: '0x3c3ea4b57a46241e54610e5f022e5c45859a1017',
-    source: 'LOGS',
-    start: '2025-01-24',
-  },
-  [CHAIN.BASE]: {
-    poolManager: '0x498581ff718922c3f8e6a244956af099b2652b2b',
-    positionManager: '0x7c5f5a4bbd8fd63184577525326123b519429bdc',
-    source: 'LOGS',
-    start: '2025-01-24',
-  },
-  [CHAIN.ARBITRUM]: {
-    poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
-    positionManager: '0xd88f38f930b7952f2db2432cb002e7abbf3dd869',
-    source: 'LOGS',
-    start: '2025-01-24',
-  },
-  [CHAIN.POLYGON]: {
-    poolManager: '0x67366782805870060151383f4bbff9dab53e5cd6',
-    positionManager: '0x1ec2ebf4f37e7363fdfe3551602425af0b3ceef9',
-    source: 'LOGS',
-    start: '2025-01-24',
-  },
-  [CHAIN.BLAST]: {
-    poolManager: '0x1631559198a9e474033433b2958dabc135ab6446',
-    source: 'LOGS',
-    positionManager: '0x4ad2f4cca2682cbb5b950d660dd458a1d3f1baad',
-    start: '2025-01-24',
-  },
+  // [CHAIN.ETHEREUM]: {
+  //   poolManager: '0x000000000004444c5dc75cB358380D2e3dE08A90',
+  //   positionManager: '0xbd216513d74c8cf14cf4747e6aaa6420ff64ee9e',
+  //   source: 'LOGS',
+  //   start: '2025-01-24',
+  //   blacklistPoolIds: [
+  //     '0x78f394840909614a7a1213503e4207d7e62f4a07af85561fc420e7ee6d22d6ce',
+  //   ],
+  // },
+  // [CHAIN.UNICHAIN]: {
+  //   poolManager: '0x1f98400000000000000000000000000000000004',
+  //   positionManager: '0x4529a01c7a0410167c5740c487a8de60232617bf',
+  //   source: 'LOGS',
+  //   start: '2025-01-24',
+  // },
+  // [CHAIN.OPTIMISM]: {
+  //   poolManager: '0x9a13f98cb987694c9f086b1f5eb990eea8264ec3',
+  //   positionManager: '0x3c3ea4b57a46241e54610e5f022e5c45859a1017',
+  //   source: 'LOGS',
+  //   start: '2025-01-24',
+  // },
+  // [CHAIN.BASE]: {
+  //   poolManager: '0x498581ff718922c3f8e6a244956af099b2652b2b',
+  //   positionManager: '0x7c5f5a4bbd8fd63184577525326123b519429bdc',
+  //   source: 'LOGS',
+  //   start: '2025-01-24',
+  // },
+  // [CHAIN.ARBITRUM]: {
+  //   poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
+  //   positionManager: '0xd88f38f930b7952f2db2432cb002e7abbf3dd869',
+  //   source: 'LOGS',
+  //   start: '2025-01-24',
+  // },
+  // [CHAIN.POLYGON]: {
+  //   poolManager: '0x67366782805870060151383f4bbff9dab53e5cd6',
+  //   positionManager: '0x1ec2ebf4f37e7363fdfe3551602425af0b3ceef9',
+  //   source: 'LOGS',
+  //   start: '2025-01-24',
+  // },
+  // [CHAIN.BLAST]: {
+  //   poolManager: '0x1631559198a9e474033433b2958dabc135ab6446',
+  //   source: 'LOGS',
+  //   positionManager: '0x4ad2f4cca2682cbb5b950d660dd458a1d3f1baad',
+  //   start: '2025-01-24',
+  // },
   [CHAIN.ZORA]: {
     poolManager: '0x0575338e4c17006ae181b47900a84404247ca30f',
     source: 'LOGS',
     positionManager: '0xf66c7b99e2040f0d9b326b3b7c152e9663543d63',
     start: '2025-01-24',
   },
-  [CHAIN.WC]: {
-    poolManager: '0xb1860d529182ac3bc1f51fa2abd56662b7d13f33',
-    source: 'LOGS',
-    positionManager: '0xc585e0f504613b5fbf874f21af14c65260fb41fa',
-    start: '2025-01-24',
-  },
-  [CHAIN.INK]: {
-    poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
-    source: 'LOGS',
-    positionManager: '0x1b35d13a2e2528f192637f14b05f0dc0e7deb566',
-    start: '2025-01-29',
-  },
-  [CHAIN.SONEIUM]: {
-    poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
-    source: 'LOGS',
-    positionManager: '0x1b35d13a2e2528f192637f14b05f0dc0e7deb566',
-    start: '2025-01-29',
-  },
-  [CHAIN.AVAX]: {
-    poolManager: '0x06380c0e0912312b5150364b9dc4542ba0dbbc85',
-    source: 'LOGS',
-    positionManager: '0xb74b1f14d2754acfcbbe1a221023a5cf50ab8acd',
-    start: '2025-01-24',
-  },
-  [CHAIN.BSC]: {
-    poolManager: '0x28e2ea090877bf75740558f6bfb36a5ffee9e9df',
-    source: 'LOGS',
-    positionManager: '0x7a4a5c919ae2541aed11041a1aeee68f1287f95b',
-    start: '2025-01-24',
-  },
+  // [CHAIN.WC]: {
+  //   poolManager: '0xb1860d529182ac3bc1f51fa2abd56662b7d13f33',
+  //   source: 'LOGS',
+  //   positionManager: '0xc585e0f504613b5fbf874f21af14c65260fb41fa',
+  //   start: '2025-01-24',
+  // },
+  // [CHAIN.INK]: {
+  //   poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
+  //   source: 'LOGS',
+  //   positionManager: '0x1b35d13a2e2528f192637f14b05f0dc0e7deb566',
+  //   start: '2025-01-29',
+  // },
+  // [CHAIN.SONEIUM]: {
+  //   poolManager: '0x360e68faccca8ca495c1b759fd9eee466db9fb32',
+  //   source: 'LOGS',
+  //   positionManager: '0x1b35d13a2e2528f192637f14b05f0dc0e7deb566',
+  //   start: '2025-01-29',
+  // },
+  // [CHAIN.AVAX]: {
+  //   poolManager: '0x06380c0e0912312b5150364b9dc4542ba0dbbc85',
+  //   source: 'LOGS',
+  //   positionManager: '0xb74b1f14d2754acfcbbe1a221023a5cf50ab8acd',
+  //   start: '2025-01-24',
+  // },
+  // [CHAIN.BSC]: {
+  //   poolManager: '0x28e2ea090877bf75740558f6bfb36a5ffee9e9df',
+  //   source: 'LOGS',
+  //   positionManager: '0x7a4a5c919ae2541aed11041a1aeee68f1287f95b',
+  //   start: '2025-01-24',
+  // },
 }
 
 // export const UNISWAP_V4_DUNE_QUERY = (fromTime: number, toTime: number) => {
@@ -228,57 +228,59 @@ async function fetch(options: FetchOptions) {
       onlyArgs: true,
     });
 
-    const pools: {[key: string]: IPool | null} = {}
-    for (const event of events) {
-      if (config.blacklistPoolIds && config.blacklistPoolIds.includes(event.id)) {
-        // ignore blacklist pools
-        continue;
-      }
-      pools[event.id] = null
-    }
-
-    // query pools info
-    const poolIds = Object.keys(pools)
-    const poolKeys = await options.api.multiCall({
-      abi: FunctionPoolKeys,
-      calls: poolIds.map(poolId => {
-        return {
-          target: config.positionManager,
-          params: [getPoolKey(poolId)],
+    if (events.length > 0) {
+      const pools: {[key: string]: IPool | null} = {}
+      for (const event of events) {
+        if (config.blacklistPoolIds && config.blacklistPoolIds.includes(event.id)) {
+          // ignore blacklist pools
+          continue;
         }
-      }),
-      permitFailure: true,
-    })
+        pools[event.id] = null
+      }
 
-    for (let i = 0; i < poolIds.length; i++) {
-      if (poolKeys[i]) {
-        // uniswap v4 supports hooks execute before and after swap
-        // so poolManager may be emit Swap event without the liquidity pool was even existed
-        // these logics are likely can be ignored because it didn't work as LP or swap from users
-        // to check a valid liquidity pool, we need atleast one token is not null address
-        if (poolKeys[i].currency0 !== ADDRESSES.null || poolKeys[i].currency1 !== ADDRESSES.null) {
-          pools[poolIds[i]] = {
-            poolId: poolIds[i],
-            poolKey: getPoolKey(poolIds[i]),
-            currency0: String(poolKeys[i].currency0),
-            currency1: String(poolKeys[i].currency1),
+      // query pools info
+      const poolIds = Object.keys(pools)
+      const poolKeys = await options.api.multiCall({
+        abi: FunctionPoolKeys,
+        calls: poolIds.map(poolId => {
+          return {
+            target: config.positionManager,
+            params: [getPoolKey(poolId)],
+          }
+        }),
+        permitFailure: true,
+      })
+
+      for (let i = 0; i < poolIds.length; i++) {
+        if (poolKeys[i]) {
+          // uniswap v4 supports hooks execute before and after swap
+          // so poolManager may be emit Swap event without the liquidity pool was even existed
+          // these logics are likely can be ignored because it didn't work as LP or swap from users
+          // to check a valid liquidity pool, we need atleast one token is not null address
+          if (poolKeys[i].currency0 !== ADDRESSES.null || poolKeys[i].currency1 !== ADDRESSES.null) {
+            pools[poolIds[i]] = {
+              poolId: poolIds[i],
+              poolKey: getPoolKey(poolIds[i]),
+              currency0: String(poolKeys[i].currency0),
+              currency1: String(poolKeys[i].currency1),
+            }
           }
         }
       }
-    }
-    
-    for (const event of events) {
-      const poolId = String(event.id)
-      if (pools[poolId] as IPool) {
-        const blacklistTokens = new Set(getDefaultDexTokensBlacklisted(options.chain))
-        if (blacklistTokens.has((pools[poolId] as IPool).currency0) || blacklistTokens.has((pools[poolId] as IPool).currency1)) {
-          continue;
-        }
+      
+      for (const event of events) {
+        const poolId = String(event.id)
+        if (pools[poolId] as IPool) {
+          const blacklistTokens = new Set(getDefaultDexTokensBlacklisted(options.chain))
+          if (blacklistTokens.has((pools[poolId] as IPool).currency0) || blacklistTokens.has((pools[poolId] as IPool).currency1)) {
+            continue;
+          }
 
-        const token = (pools[poolId] as IPool).currency0
-        dailyFees.add(token, Math.abs(Number(event.amount0)) * (Number(event.fee) / 1e6))
-        dailyVolume.add(token, Math.abs(Number(event.amount0)))
-      }
+          const token = (pools[poolId] as IPool).currency0
+          dailyFees.add(token, Math.abs(Number(event.amount0)) * (Number(event.fee) / 1e6))
+          dailyVolume.add(token, Math.abs(Number(event.amount0)))
+        }
+      }      
     }
   }
 
