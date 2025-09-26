dcaf782ac5cfb00f17bc2bd4b0a3e7cefcf8dbdc
Eden <noat.eth@gmail.com>
2025-09-27 00:31:34 +0700

    refactor

--- fees/xdock-meme.ts
+++ fees/xdock-meme.ts
@@ -9,28 +9,40 @@ const XdockFeeCollector = '0xe6A5f4b8257BbAd4F033D3831ebF23E0F833961F';
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
+  const dailyRevenue = options.createBalances();
+  const dailySupplySideRevenue = options.createBalances();
+
   const data: any[] = await options.getLogs({
     target: XdockFeeCollector,
     eventAbi: FeeCollectedEvent,
   });
   data.forEach((log: any) => {
     dailyFees.add(WOKB, log.feeAmount);
+    dailyRevenue.add(WOKB, log.feeAmount);
   });
-  const dailyCreatorFees = options.createBalances();
+  
   const data1: any[] = await options.getLogs({
     target: XdockFeeCollector,
     eventAbi: FeeToCreatorEvent,
   });
   data1.forEach((log: any) => {
-    dailyCreatorFees.add(WOKB, log.feeAmount);
+    dailyFees.add(WOKB, log.feeAmount);
+    dailySupplySideRevenue.add(WOKB, log.feeAmount);
   });
-  return { dailyFees, dailyRevenue: dailyFees, dailyCreatorRevenue: dailyCreatorFees };
+
+  return {
+    dailyFees,
+    dailyRevenue,
+    dailyProtocolRevenue: dailyRevenue,
+    dailySupplySideRevenue,
+  };
 };
 
 const methodology = {
   Fees: 'All fees paid by users for launching, trading tokens.',
   Revenue: 'Fees collected by xdock.meme protocol.',
-  dailyCreatorRevenue: 'Fees paid to the creator after token graduation',
+  ProtocolRevenue: 'Fees collected by xdock.meme protocol.',
+  SupplySideRevenue: 'Fees paid to the creator after token graduation.',
 }
 
 const adapter: SimpleAdapter = {
@@ -38,7 +50,7 @@ const adapter: SimpleAdapter = {
   fetch,
   chains: [CHAIN.XLAYER],
   start: '2025-09-08',
-  methodology
+  methodology,
 }
 
 export default adapter;
