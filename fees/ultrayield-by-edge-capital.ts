d1ae7008154a12823015e62636502966fd8f3602
Artem Trifonov <artem@ultrayield.app>
2025-10-01 17:20:43 +0300

    rename edge-capital.ts -> ultrayield-by-edge-capital.ts remove unused address

--- fees/ultrayield-by-edge-capital.ts
+++ fees/ultrayield-by-edge-capital.ts
@@ -4,7 +4,6 @@ const curatorConfig: CuratorConfig = {
   vaults: {
     tac: {
       eulerVaultOwners: [
-        '0xB2b9a27a6160Bf9ffbD1a8d245f5de75541b1DDD',
         '0x1280e86Cd7787FfA55d37759C0342F8CD3c7594a',
       ],
     },
