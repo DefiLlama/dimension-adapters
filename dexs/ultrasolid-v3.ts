b78c8c9e75aaee487064c08ac1e7398fd7163348
treeoflife2 <sol.analyzoor@gmail.com>
2025-10-02 16:01:49 +0530

    ultrasolid-v3 fees adapter and use uni-v3 helper

--- dexs/ultrasolid-v3.ts
+++ dexs/ultrasolid-v3.ts
@@ -1,81 +1,15 @@
-import { sliceIntoChunks } from "@defillama/sdk/build/util";
-import { FetchOptions, FetchResult, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
+import { uniV3Exports } from "../helpers/uniswap";
 
-const factory = '0xD883a0B7889475d362CEA8fDf588266a3da554A1';
-const factory_block = 10741969;
-
-const eventAbis = {
-  event_poolCreated: 'event PoolCreated(address indexed token0, address indexed token1, uint24 indexed fee, int24 tickSpacing, address pool)',
-  event_swap: 'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)'
-};
-
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-const BLOCK_STEP = 10_000;
-
-const fetch = async (_: any, _1: any, fetchOptions: FetchOptions): Promise<FetchResult> => {
-  const { api, createBalances, getFromBlock, getLogs } = fetchOptions;
-  const dailyVolume = createBalances();
-  const dailyFees = createBalances();
-  const [fromBlock, toBlock] = await Promise.all([getFromBlock(), (await api.getBlock()) - 100]);
-
-  const rawPairs = await getLogs({ target: factory, fromBlock: factory_block, toBlock, eventAbi: eventAbis.event_poolCreated });
-  const pairs = rawPairs.map(({ token0, token1, fee, tickSpacing, pool }) => ({ token0, token1, pool_fees: fee, tickSpacing, pool }));
-
-  const pairInfoMap: Record<string, any> = {};
-  pairs.forEach((p) => {
-    pairInfoMap[p.pool] = p;
-  });
-
-  const targetChunkSize = 50;
-  const pairChunks = sliceIntoChunks(pairs, targetChunkSize);
-
-  for (let i = 0; i < pairChunks.length; i++) {
-    const chunk = pairChunks[i];
-    const targets = chunk.map(({ pool }) => pool);
-
-    for (let start = fromBlock; start <= toBlock; start += BLOCK_STEP) {
-      const end = Math.min(start + BLOCK_STEP - 1, toBlock);
-      await sleep(500);
-
-      const rawLogs = await getLogs({
-        targets,
-        eventAbi: eventAbis.event_swap,
-        flatten: false,
-        fromBlock: start,
-        toBlock: end,
-        onlyArgs: true,
-        skipCache: true,
-        skipCacheRead: true
-      });
-
-      rawLogs.forEach((logs: any[], idx: number) => {
-        const pool = targets[idx];
-        const info = pairInfoMap[pool];
-        if (!info) return;
-
-        const { token1, pool_fees } = info;
-        logs.forEach(({ amount1 }: any) => {
-          const absAmount = amount1 < 0n ? -amount1 : amount1;
-          const fee = Math.round((Number(absAmount) * Number(pool_fees)) / 1_000_000);
-          dailyVolume.add(token1, absAmount);
-          dailyFees.add(token1, fee);
-        });
-      });
-    }
+export default uniV3Exports({
+  [CHAIN.HYPERLIQUID]: {
+    factory: '0xD883a0B7889475d362CEA8fDf588266a3da554A1',
+    swapEvent: 'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)',
+    poolCreatedEvent: 'event PoolCreated(address indexed token0, address indexed token1, uint24 indexed fee, int24 tickSpacing, address pool)',
+    start: '2025-08-10',
+    revenueRatio: 0,
+    protocolRevenueRatio: 0,
+    holdersRevenueRatio: 0,
+    userFeesRatio: 1,
   }
-
-  return { dailyVolume, dailyFees };
-};
-
-const adapters: SimpleAdapter = {
-  version: 1,
-  adapter: {
-    [CHAIN.HYPERLIQUID]: {
-      fetch,
-      start: '2025-08-10'
-    }
-  }
-};
-
-export default adapters;
+})
