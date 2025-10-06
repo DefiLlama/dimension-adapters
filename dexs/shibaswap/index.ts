631fffab4810f67999297a3a916e2ca8e978b525
rohannero <rohanzarathustra@gmail.com>
2025-10-06 16:48:42 -0400

    add shibarium tracking for shibaswap

--- dexs/shibaswap/index.ts
+++ dexs/shibaswap/index.ts
@@ -4,5 +4,9 @@ import { uniV2Exports } from "../../helpers/uniswap"
 export default uniV2Exports({
   [CHAIN.ETHEREUM]: {
     factory: '0x115934131916c8b277dd010ee02de363c09d037c',
+  },
+  [CHAIN.SHIBARIUM]: {
+    factory: '0xc2b4218F137e3A5A9B98ab3AE804108F0D312CBC',
+    start: '5-15-24'
   }
 })
