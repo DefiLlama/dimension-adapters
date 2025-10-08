1582494123e046e62cfcca6416e71f53ad994b5d
0x4r45h <0x4r45h@gmail.com>
2025-10-08 19:25:39 +0330

    workaround for days without any buy backs

--- fees/apex-omni.ts
+++ fees/apex-omni.ts
@@ -4,7 +4,6 @@ import { getTimestampAtStartOfDayUTC } from "../utils/date"
 import { httpGet } from "../utils/fetchURL"
 import { METRIC } from "../helpers/metrics";
 import ADDRESSES from '../helpers/coreAssets.json'
-import { ethers } from "ethers";
 
 const BUYBACK_VAULT_ADDR = '0x18A45C46840CF830e43049C8fe205CA05B43527B';
 const TOKEN_APEX = ADDRESSES.arbitrum.APEX;
@@ -28,20 +27,16 @@ const fetchFees = async (_: any, _b: any, options: FetchOptions): Promise<FetchR
 const fetchRevenue = async (_: any, _b: any, options: FetchOptions): Promise<FetchResultFees> => {
   const dailyHoldersRevenue = options.createBalances();
 
-  // Buybacks are not automated, so we have to tack this address for any inflows
+  // Buybacks are not automated, so we have to track this address for any inflows
   const transferEvents = await options.getLogs({
     target: TOKEN_APEX,
     eventAbi: 'event Transfer(address indexed from, address indexed to, uint256 value)',
-    topics: [
-      '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef', // Transfer event signature
-      '',
-      ethers.zeroPadValue(BUYBACK_VAULT_ADDR,32)
-    ]
   });
   transferEvents.forEach((log: any) => {
-    dailyHoldersRevenue.add(TOKEN_APEX, log.value, METRIC.TOKEN_BUY_BACK);
+    if (log.to.toLowerCase() === BUYBACK_VAULT_ADDR.toLowerCase()) {
+        dailyHoldersRevenue.add(TOKEN_APEX, log.value, METRIC.TOKEN_BUY_BACK);
+    }
   });
-
   return {
     dailyHoldersRevenue
   }
