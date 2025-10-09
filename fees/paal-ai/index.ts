2a849751e8d1b1a794f0e75ac2b5706158cca625
0x4r45h <0x4r45h@gmail.com>
2025-10-09 19:50:16 +0330

    paal-ai revenue

--- fees/paal-ai/index.ts
+++ fees/paal-ai/index.ts
@@ -1,13 +1,16 @@
 import { Adapter, FetchOptions, } from "../../adapters/types";
 import { queryIndexer } from "../../helpers/indexer";
 import { getTokenDiff } from "../../helpers/token";
+import {METRIC} from "../../helpers/metrics";
 
 /** Address to check = paalecosystemfund.eth */
-const CONTRACT_ADDRESS = "0x54821d1B461aa887D37c449F3ace8dddDFCb8C0a";
+const CONTRACT_ECOSYSTEM_FUND = "0x54821d1B461aa887D37c449F3ace8dddDFCb8C0a";
+const CONTRACT_STAKING = "0x85e253162C7e97275b703980F6b6fA8c0469D624";
 
 const fetch = async (options: FetchOptions) => {
-  const dailyFees = options.createBalances();
-  await getTokenDiff({ target: CONTRACT_ADDRESS, options, includeGasToken: true, balances: dailyFees, })
+  // any funds on the CONTRACT_ECOSYSTEM_FUND is revenue
+  const dailyRevenue = options.createBalances();
+  await getTokenDiff({ target: CONTRACT_ECOSYSTEM_FUND, options, includeGasToken: true, balances: dailyRevenue, })
   const transactions = await queryIndexer(`
     SELECT
       block_number,
@@ -24,16 +27,35 @@ const fetch = async (options: FetchOptions) => {
       AND block_time BETWEEN llama_replace_date_range;
       `, options);
 
-  transactions.map((transaction: any) => dailyFees.addGasToken(transaction.eth_value))
-  return { dailyFees, }
+  transactions.map((transaction: any) => dailyRevenue.addGasToken(transaction.eth_value))
+
+  const dailyFees = dailyRevenue.clone();
+  const dailyHoldersRevenue = options.createBalances();
+  // track Eth distribution to stakers
+  const transferEvents = await options.getLogs({
+      target: CONTRACT_STAKING,
+      eventAbi: 'event DistributeReward(address indexed user, uint256 amount, bool _wasCompounded)',
+  });
+    transferEvents.forEach((log: any) => {
+        dailyHoldersRevenue.addGasToken(log.amount, METRIC.TRADING_FEES);
+    });
+    return {
+      dailyFees,
+      dailyRevenue,
+      dailyHoldersRevenue
+    }
 }
 
+const info = {
+  methodology: {
+    Fees: "Fees paid by users for using PAAL AI services.",
+    HoldersRevenue: "50% of certain earnings are allocated to stakers",
+  }
+}
 /** Adapter */
 const adapter: Adapter = {
-  methodology: {
-    Fees: 'Fees paid by users for using PAAL AI services.',
-  },
   version: 2,
+  methodology: info.methodology,
   adapter: {
     ethereum: {
       fetch,
