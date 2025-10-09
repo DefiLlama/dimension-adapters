f98633179f371bab7614a1ad676940425900ac8b
tree <sol.analyzoor@gmail.com>
2025-10-09 21:12:23 +0530

    add correct revenue for token-works (#4400)

--- fees/token-works/index.ts
+++ fees/token-works/index.ts
@@ -3,6 +3,84 @@ import { CHAIN } from '../../helpers/chains';
 import { METRIC } from '../../helpers/metrics';
 import { getETHReceived } from '../../helpers/token';
 import fetchURL from '../../utils/fetchURL';
+
+const PKSTR_TOKEN = '0xc50673EDb3A7b94E8CAD8a7d4E0cD68864E33eDF'
+
+async function fetch(options: FetchOptions): Promise<FetchResultV2> {
+  const commonTokenTreasuryRevenue = options.createBalances();
+  const pkstrTokenTreasuryRevenue = options.createBalances();
+
+  const strategies: { name: string, id: string, hook: string, poolId: string, tokenAddress: string, collection: string }[] = [];
+  const strategiesres = await fetchURL('https://www.nftstrategy.fun/api/strategies');
+
+  for (const strategy of strategiesres) {
+    strategies.push({
+      id: strategy.id,
+      name: strategy.collectionName,
+      hook: strategy.hook,
+      poolId: strategy.poolId,
+      tokenAddress: strategy.tokenAddress,
+      collection: strategy.collection
+    });
+  }
+
+  const tokens = new Set(strategies.map(strategy => strategy.tokenAddress));
+  const commonTokens = Array.from(tokens).filter(token => token != PKSTR_TOKEN)
+
+  // Get ETH received by common tokens (10% tax on swaps)
+  await getETHReceived({
+    targets: commonTokens,
+    balances: commonTokenTreasuryRevenue,
+    options
+  });
+
+  // Get ETH received by PKSTR token (10% tax on swaps)
+  await getETHReceived({
+    targets: [PKSTR_TOKEN],
+    balances: pkstrTokenTreasuryRevenue,
+    options
+  });
+
+  // Total fees = all ETH received by all token contracts (80% fee share)
+  const dailyFees = options.createBalances();
+  dailyFees.addBalances(commonTokenTreasuryRevenue);
+  dailyFees.addBalances(pkstrTokenTreasuryRevenue);
+  dailyFees.resizeBy(1.2); // to adjust for 80% fee receieved in contract address
+
+  // PKSTR: 20% of tax goes to token-works treasury (protocol revenue)
+  const dailyRevenue = await pkstrTokenTreasuryRevenue.clone(0.2);
+
+  // royalty fees(10% of token tax)
+  const dailySupplySideRevenue = await commonTokenTreasuryRevenue.clone(0.1);
+
+  return {
+    dailyFees,
+    dailyRevenue,
+    dailyProtocolRevenue: dailyRevenue,
+    dailySupplySideRevenue,
+    dailyHoldersRevenue: 0,
+  }
+}
+
+export default {
+  version: 2,
+  fetch,
+  chains: [CHAIN.ETHEREUM],
+  methodology: {
+    Fees: 'buy/sell tax collected from strategy tokens trading (not including swap fees on dexes).',
+    Revenue: '20% of PKSTR token tax goes to token-works team.',
+    ProtocolRevenue: '20% of PKSTR token tax goes to token-works team.',
+    SupplySideRevenue: '10% of token tax will be distributed to NFT creators as royalty.',
+    HoldersRevenue: 'No holders revenue at the moment.',
+  },
+  breakdownMethodology: {
+    Fees: {
+      [METRIC.CREATOR_FEES]: '10% token buy/sell tax distributed to NFT creators.',
+    }
+  },
+}
+
+
 // import ADDRESSES from '../../helpers/coreAssets.json';
 // import { ethers } from 'ethers';
 
@@ -23,122 +101,54 @@ import fetchURL from '../../utils/fetchURL';
 //   return poolId.slice(0, 52);
 // }
 
-async function fetch(options: FetchOptions): Promise<FetchResultV2> {
-  const tokenTreasuryRevenue = options.createBalances();
+// Not using below code because of token-works hook bug
+// https://x.com/0xSLK/status/1975602712185114978
 
-  const strategies: { name: string, id: string, hook: string, poolId: string, tokenAddress: string, collection: string }[] = [];
-  const strategiesres = await fetchURL('https://www.nftstrategy.fun/api/strategies');
+// for (const strategy of STRATEGIES) {
+//   const tradeFeeLogs = await options.getLogs({
+//     target: UNISWAP_POOL_MANAGER,
+//     topics: [
+//       SWAP_TOPIC,
+//       strategy.poolId,
+//       ethers.zeroPadValue(strategy.hook, 32)
+//     ],
+//     eventAbi: ABI.SWAP_EVENT
+//   });
 
-  for (const strategy of strategiesres) {
-    strategies.push({
-      id: strategy.id,
-      name: strategy.collectionName,
-      hook: strategy.hook,
-      poolId: strategy.poolId,
-      tokenAddress: strategy.tokenAddress,
-      collection: strategy.collection
-    });
-  }
+//   tradeFeeLogs.forEach((trade: any) => {
+//     dailyFees.add(ADDRESSES.ethereum.WETH, trade.amount0)
+//   });
+// }
 
-  const tokens = new Set(strategies.map(strategy => strategy.tokenAddress));
-  await getETHReceived({
-    targets: Array.from(tokens),
-    balances: tokenTreasuryRevenue,
-    options
-  });
+// const hook_set = new Set(STRATEGIES.map(strategy => strategy.hook));
+// const feeEvents = await options.getLogs({
+//   targets: Array.from(hook_set),
+//   eventAbi: ABI.HookFee,
+//   flatten: true,
+// });
 
-  const dailyFees = tokenTreasuryRevenue.clone(1/0.8, METRIC.TOKEN_BUY_BACK);
-  const dailyRevenue = dailyFees.clone(0.9, METRIC.TOKEN_BUY_BACK);
-  const dailySupplySideRevenue = dailyFees.clone(0.1, METRIC.CREATOR_FEES);
+// const uniswapV4Pools: {[key: string]: any} = {}
+// for (const strategy of STRATEGIES)   {
+//   uniswapV4Pools[strategy.poolId] = {
+//     currency0: '0x0000000000000000000000000000000000000000',
+//     currency1: strategy.tokenAddress,
+//   }
+// }
 
-  // Not using below code because of token-works hook bug
-  // https://x.com/0xSLK/status/1975602712185114978
+// for (const feeEvent of feeEvents) {
+//   const totalFee0 = Math.abs(Number(feeEvent.feeAmount0));
+//   const totalFee1 = Math.abs(Number(feeEvent.feeAmount1));
+//   // if (totalFee0 > 0) {
+//   //   continue;
+//   // }
+//   dailyFees.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.9, METRIC.TOKEN_BUY_BACK);
+//   dailyFees.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.9, METRIC.TOKEN_BUY_BACK);
+//   dailyFees.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.1, METRIC.CREATOR_FEES);
+//   dailyFees.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.1, METRIC.CREATOR_FEES);
 
-  // for (const strategy of STRATEGIES) {
-  //   const tradeFeeLogs = await options.getLogs({
-  //     target: UNISWAP_POOL_MANAGER,
-  //     topics: [
-  //       SWAP_TOPIC,
-  //       strategy.poolId,
-  //       ethers.zeroPadValue(strategy.hook, 32)
-  //     ],
-  //     eventAbi: ABI.SWAP_EVENT
-  //   });
+//   dailyRevenue.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.9, METRIC.TOKEN_BUY_BACK);
+//   dailyRevenue.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.9, METRIC.TOKEN_BUY_BACK);
 
-  //   tradeFeeLogs.forEach((trade: any) => {
-  //     dailyFees.add(ADDRESSES.ethereum.WETH, trade.amount0)
-  //   });
-  // }
-
-  // const hook_set = new Set(STRATEGIES.map(strategy => strategy.hook));
-  // const feeEvents = await options.getLogs({
-  //   targets: Array.from(hook_set),
-  //   eventAbi: ABI.HookFee,
-  //   flatten: true,
-  // });
-
-  // const uniswapV4Pools: {[key: string]: any} = {}
-  // for (const strategy of STRATEGIES)   {
-  //   uniswapV4Pools[strategy.poolId] = {
-  //     currency0: '0x0000000000000000000000000000000000000000',
-  //     currency1: strategy.tokenAddress,
-  //   }
-  // }
-
-  // for (const feeEvent of feeEvents) {
-  //   const totalFee0 = Math.abs(Number(feeEvent.feeAmount0));
-  //   const totalFee1 = Math.abs(Number(feeEvent.feeAmount1));
-  //   // if (totalFee0 > 0) {
-  //   //   continue;
-  //   // }
-  //   dailyFees.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.9, METRIC.TOKEN_BUY_BACK);
-  //   dailyFees.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.9, METRIC.TOKEN_BUY_BACK);
-  //   dailyFees.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.1, METRIC.CREATOR_FEES);
-  //   dailyFees.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.1, METRIC.CREATOR_FEES);
-
-  //   dailyRevenue.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.9, METRIC.TOKEN_BUY_BACK);
-  //   dailyRevenue.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.9, METRIC.TOKEN_BUY_BACK);
-
-  //   dailySupplySideRevenue.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.1, METRIC.CREATOR_FEES);
-  //   dailySupplySideRevenue.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.1, METRIC.CREATOR_FEES);
-  // }
-
-  return {
-    dailyFees,
-    dailyRevenue,
-    dailyProtocolRevenue: 0,
-    dailySupplySideRevenue,
-    dailyHoldersRevenue: dailyRevenue,
-  }
-}
-
-export default {
-  version: 2,
-  fetch,
-  chains: [CHAIN.ETHEREUM],
-  methodology: {
-    Fees: 'Total buy/sell tax collected from strategy tokens trading (not including swap fees on dexes).',
-    Revenue: '90% of token tax will be used for buybacks and burns.',
-    ProtocolRevenue: 'No launch fees.',
-    SupplySideRevenue: '10% of token tax will be distributed to NFT creators.',
-    HoldersRevenue: '90% of token tax and all NFT sold will be used to buy back strategy tokens.',
-  },
-  breakdownMethodology: {
-    Fees: {
-      [METRIC.TOKEN_BUY_BACK]: '90% token buy/sell tax will be used to buy back strategy tokens.',
-      [METRIC.CREATOR_FEES]: '10% token buy/sell tax distributed to NFT creators.',
-    },
-    Revenue: {
-      [METRIC.TOKEN_BUY_BACK]: '90% token buy/sell tax will be used to buy back strategy tokens.',
-    },
-    SupplySideRevenue: {
-      [METRIC.CREATOR_FEES]: '10% token buy/sell tax distributed to NFT creators.',
-    },
-    ProtocolRevenue: {
-      [METRIC.TOKEN_BUY_BACK]: '90% token buy/sell tax will be used to buy back strategy tokens.',
-    },
-    HoldersRevenue: {
-      [METRIC.TOKEN_BUY_BACK]: '90% token buy/sell tax will be used to buy back strategy tokens.',
-    },
-  },
-}
+//   dailySupplySideRevenue.add(uniswapV4Pools[feeEvent.id].currency0, totalFee0 * 0.1, METRIC.CREATOR_FEES);
+//   dailySupplySideRevenue.add(uniswapV4Pools[feeEvent.id].currency1, totalFee1 * 0.1, METRIC.CREATOR_FEES);
+// }
