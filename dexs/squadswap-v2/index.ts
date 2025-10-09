746be35bc9cb3fccc65076eb502b96e2e264e7ff
Eden <noat.eth@gmail.com>
2025-10-09 20:15:48 +0700

    breakdown fees squadswap

--- dexs/squadswap-v2/index.ts
+++ dexs/squadswap-v2/index.ts
@@ -4,5 +4,8 @@ import { uniV2Exports } from "../../helpers/uniswap"
 export default uniV2Exports({
   [CHAIN.BSC]: {
     factory: '0x1D9F43a6195054313ac1aE423B1f810f593b6ac1',
+    userFeesRatio: 1,
+    revenueRatio: 0.1,
+    protocolRevenueRatio: 0.1,
   }
 })
