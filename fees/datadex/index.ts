b0073edd092590e543319d93d1a9a6b0448bd9f1
Eden <noat.eth@gmail.com>
2025-10-09 19:37:46 +0700

    refactor datadex

--- fees/datadex/index.ts
+++ fees/datadex/index.ts
@@ -1,16 +1,3 @@
-import { CHAIN } from '../../helpers/chains';
-import { uniV3Exports } from '../../helpers/uniswap';
-import { SimpleAdapter } from '../../adapters/types';
+import adapter from "../../dexs/datadex";
 
-const methodology = {
-    Fees: "Swap fees collected from users on each trade.",
-    Revenue: "Configurable portion of the swap fees collected from users.",
-    ProtocolRevenue: "When set, the protocol receives a portion of trade fees.",
-};
-
-const adapters: SimpleAdapter = uniV3Exports({
-    [CHAIN.VANA]: { factory: "0xc2a0d530e57B1275fbce908031DA636f95EA1E38", protocolRevenueRatio: 1 / 10 },
-})
-
-adapters.methodology = methodology;
-export default adapters;
+export default adapter;
