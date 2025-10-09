a37c1d9bf3cb9d0b172e58b26cb74debcdca6c60
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-10-09 11:39:25 +0200

    Feat: Add Symmio and builders (#4377)
    * Add Symmio and builders
    
    * Fix: Version 2 → 1
    
    * v2 → v1

---
+++ dexs/cloverfield-perps/index.ts
@@ -0,0 +1,4 @@
+import { CHAIN } from '../../helpers/chains';
+import { symmioAffiliateAdapter } from '../../helpers/symmio';
+
+export default symmioAffiliateAdapter('Cloverfield', [CHAIN.BASE, CHAIN.BSC]);
