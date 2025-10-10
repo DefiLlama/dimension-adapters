33f5417083cd600a5fe4e1e1871237245a19b170
Eden <noat.eth@gmail.com>
2025-10-11 01:13:30 +0700

    filter spam vol

--- aggregators/liquidmesh/index.ts
+++ aggregators/liquidmesh/index.ts
@@ -1,149 +1,122 @@
-
-import ADDRESSES from '../../helpers/coreAssets.json'
 import { Dependencies, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { queryDuneSql } from "../../helpers/dune";
 import { FetchOptions } from "../../adapters/types";
+import { getDefaultDexTokensBlacklisted, getDefaultDexTokensWhitelisted } from "../../helpers/lists";
+import { formatAddress } from "../../utils/utils";
 
 interface IData {
-    trade_date: string;
-    chain: string;
-    daily_trades: number;
-    daily_usd_volume: number;
+	chain: string;
+	token: string;
+	amount: number;
 }
 
 const chainsMap: Record<string, string> = {
-    "ethereum": CHAIN.ETHEREUM,
-    "base": CHAIN.BASE,
-    "bnb": CHAIN.BSC,
-    "sonic": CHAIN.SONIC,
-    "tron": CHAIN.TRON,
-    "sol": CHAIN.SOLANA
+	"ethereum": CHAIN.ETHEREUM,
+	"base": CHAIN.BASE,
+	"bnb": CHAIN.BSC,
+	"sonic": CHAIN.SONIC,
+	"tron": CHAIN.TRON,
 };
 
-const prefetch = async (options: FetchOptions) => {
-    const data: IData[] = await queryDuneSql(options, `
-        WITH recent_evm_orders AS (
-            SELECT
-                evt_tx_hash,
-                evt_block_time,
-                DATE(evt_block_time) as trade_date,
-                chain
-            FROM liquidmesh_multichain.liquidmeshrouter_evt_orderrecord
-            WHERE
-                 evt_block_time >= from_unixtime(${options.startTimestamp})
-                 AND evt_block_time < from_unixtime(${options.endTimestamp})
-        ),
+const prefetch = async (options: FetchOptions): Promise<any> => {
+	// must exclude all blacklisted tokens
+	const ethereumBlacklistedTokens = getDefaultDexTokensBlacklisted(CHAIN.ETHEREUM)
+	const bscBlacklistedTokens = getDefaultDexTokensBlacklisted(CHAIN.BSC)
 
-        recent_lm_sol_tx AS (
-             
-            select
-               evt_tx_id as evt_tx_hash,
-               evt_block_time as evt_block_time,
-               date(evt_block_time)  as trade_date
-            from
-                liquidmesh_solana.liquid_mesh_router_evt_liquidmeshswapevent
-            where date(evt_block_time) >= date(from_unixtime(${options.startTimestamp}))
-                  AND date(evt_block_time) < date(from_unixtime(${options.endTimestamp}))
-                  
-      ),
+	const allBlacklistedTokens = ethereumBlacklistedTokens.concat(bscBlacklistedTokens)
 
-         recent_evm_trade AS (
-           SELECT
-              tx_hash,
-              blockchain,
-              sum(amount_usd) as amount_usd
-              FROM dex.trades 
-              where block_time >= from_unixtime(${options.startTimestamp})
-                    AND block_time < from_unixtime(${options.endTimestamp})
-              group by tx_hash,blockchain
-        ),
-        recent_sol_trade AS (
-             select
-                  tx_id as tx_hash,
-                  sum(amount_usd) as amount_usd
-              from dex_solana.trades
-                  where date(block_time) >= date(from_unixtime(${options.startTimestamp}))
-                        AND date(block_time) < date(from_unixtime(${options.endTimestamp}))
-                          and block_month >= date '2025-08-01'
-                      group by
-                          tx_id
-      ),
+	const data: IData[] = await queryDuneSql(options, `
+		SELECT
+			chain,
+			fromToken as token,
+			SUM(fromAmount) as amount
+		FROM liquidmesh_multichain.liquidmeshrouter_evt_orderrecord
+		WHERE
+			evt_block_time >= from_unixtime(${options.startTimestamp})
+			AND evt_block_time < from_unixtime(${options.endTimestamp})
+			AND fromToken NOT IN (${allBlacklistedTokens.toString()})
+			AND toToken NOT IN (${allBlacklistedTokens.toString()})
+		GROUP BY chain, fromToken
+	`)
 
-         evm_orders_with_usd AS (
-            SELECT
-                o.trade_date,
-                o.evt_tx_hash,
-                o.evt_block_time,
-                o.chain,
-                rt.amount_usd
-            FROM recent_evm_orders o
-            LEFT JOIN recent_evm_trade rt ON
-                 rt.tx_hash=o.evt_tx_hash
-                 and o.chain=rt.blockchain
-        ),
-        sol_orders_with_usd AS (
-              SELECT
-                o.trade_date,
-                o.evt_tx_hash,
-                o.evt_block_time,
-                'sol' as chain,
-                rt.amount_usd
-            FROM recent_lm_sol_tx o
-            LEFT JOIN recent_sol_trade rt ON
-                 rt.tx_hash=o.evt_tx_hash
-        )
-
-        SELECT
-            trade_date,
-            chain,
-            COUNT(*) as daily_trades,
-            SUM(amount_usd) as daily_usd_volume
-        FROM evm_orders_with_usd
-        GROUP BY trade_date,chain
-
-        UNION ALL
-        SELECT
-            trade_date,
-            chain,
-            COUNT(*) as daily_trades,
-            SUM(amount_usd) as daily_usd_volume
-        FROM sol_orders_with_usd
-        GROUP BY trade_date,chain
-    `);
-    
-    return data;
+	return data;
 };
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-    const results: IData[] = options.preFetchedResults || [];
-    const chainData = results.find(
-        (item) => chainsMap[item.chain.toLowerCase()] === options.chain && 
-                 item.trade_date === options.dateString
-    );
-    
-    return {
-        dailyVolume: chainData?.daily_usd_volume || 0,
-    };
+	const dailyVolume = options.createBalances()
+
+	const tokensAndAmounts: Array<IData> = options.preFetchedResults || []
+
+	const blacklistedTokens = getDefaultDexTokensBlacklisted(options.chain)
+	const whitesliedTokens = await getDefaultDexTokensWhitelisted({ chain: options.chain })
+	for (const token of tokensAndAmounts.filter(item => options.chain === chainsMap[item.chain])) {
+		if (options.chain === CHAIN.BSC) {
+			if (whitesliedTokens.includes(formatAddress(token.token))) {
+				dailyVolume.add(token.token, token.amount);
+			}
+		} else {
+			if (!blacklistedTokens.includes(formatAddress(token.token))) {
+				dailyVolume.add(token.token, token.amount);
+			}
+		}
+	}
+
+	await dailyVolume.getUSDJSONs({ debug: true })
+
+	return {
+		dailyVolume,
+	};
+};
+
+const fetchSolana = async (_a: any, _b: any, options: FetchOptions) => {
+	const dailyVolume = options.createBalances()
+
+	const tokensAndAmounts: Array<IData> = await queryDuneSql(options, `
+		SELECT
+			'solana' AS chain,
+			(CASE
+				WHEN source_token_mint = '11111111111111111111111111111111' THEN 'So11111111111111111111111111111111111111112'
+				ELSE source_token_mint
+			END) AS token,
+			SUM(amount_in) as amount
+		FROM liquidmesh_solana.liquid_mesh_router_evt_liquidmeshswapevent
+		WHERE
+			evt_block_time >= from_unixtime(${options.startTimestamp})
+			AND evt_block_time < from_unixtime(${options.endTimestamp})
+		GROUP BY source_token_mint
+	`)
+
+	for (const item of tokensAndAmounts) {
+		dailyVolume.add(item.token, item.amount);
+	}
+
+	return {
+		dailyVolume,
+	};
 };
 
 const adapter: SimpleAdapter = {
-    version: 1,
-    dependencies: [Dependencies.DUNE],
-    adapter: Object.values(chainsMap).reduce((acc, chain) => {
-        return {
-            ...acc,
-            [chain]: {
-                fetch: fetch,
-                start: '2025-08-01',
-            },
-        };
-    }, {}),
-    prefetch: prefetch,
-    methodology: {
-        Volume: "Tracks the trading volume across all supported chains through LiquidMesh aggregator",
-    },
-    isExpensiveAdapter: true
+	version: 1,
+	dependencies: [Dependencies.DUNE],
+	start: '2025-08-01',
+	adapter: Object.values(chainsMap).reduce((acc, chain) => {
+		return {
+			...acc,
+			[chain]: {
+				fetch: fetch,
+			},
+		};
+	}, {
+		solana: {
+			fetch: fetchSolana,
+		}
+	}),
+	prefetch: prefetch,
+	methodology: {
+		Volume: "Tracks the trading volume across all supported chains through LiquidMesh aggregator",
+	},
+	isExpensiveAdapter: true,
 }
 
 export default adapter
