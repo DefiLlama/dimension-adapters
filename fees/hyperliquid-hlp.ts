4a95c774b230482c45a242ffe17e8c2d5bfc3515
eden <noat.eth@gmail.com>
2025-10-12 03:22:56 +0700

    fetch HLP pnl from hl api (#4421)
    * fetch HLP pnl from hl api
    
    * add notes
    
    * allow negative PnL

--- fees/hyperliquid-hlp.ts
+++ fees/hyperliquid-hlp.ts
@@ -1,21 +1,69 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { getRevenueRatioShares, LLAMA_HL_INDEXER_FROM_TIME, queryHyperliquidIndexer, queryHypurrscanApi } from "../helpers/hyperliquid";
+import { httpPost } from "../utils/fetchURL";
+import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume"
+
+// hl api return data in random days, not every days
+// so we will cal avg pnl from lower timestamp to upper timestamp
+// for example, hl api return data for 9 Oct, 11 Oct but missing 10 Oct
+// so the fees for both 9 Oct and 10 Oct are: (Fees of 11 Oct - Fees of 1 Oct) / 2
+function getAvgPnlForDay({ historyPnls, timestamp }: { historyPnls: {[key: number]: number}, timestamp: number }): number {
+  const ONE_DAY = 24 * 3600
+
+  let upperTimestamp = timestamp
+  let lowerTimestamp = timestamp - ONE_DAY
+
+  while(!historyPnls[upperTimestamp]) {
+    upperTimestamp += ONE_DAY;
+  }
+  while(!historyPnls[lowerTimestamp]) {
+    lowerTimestamp -= ONE_DAY;
+  }
+
+  const days = (upperTimestamp - lowerTimestamp) / ONE_DAY
+  const pnl = historyPnls[upperTimestamp] - historyPnls[lowerTimestamp]
+
+  return pnl / days
+}
 
 async function fetch(_1: number, _: any,  options: FetchOptions) {
   const dailyFees = options.createBalances()
-  const { hlpShare } = getRevenueRatioShares(options.startOfDay)
 
+  const vaultData = await httpPost('https://api-ui.hyperliquid.xyz/info', {
+    type: 'vaultDetails',
+    vaultAddress: '0xdfc24b077bc1425ad1dea75bcb6f8158e10df303',
+  })
+
+  const historyPnls: {[key: number]: number} = {}
+  for (const chartItem of vaultData.portfolio) {
+    if (chartItem[0] === 'allTime') {
+      for (const pnlItem of chartItem[1].pnlHistory) {
+        const timestamp = getUniqStartOfTodayTimestamp(new Date(pnlItem[0]))
+        historyPnls[timestamp] = Number(pnlItem[1])
+      }
+    }
+  }
+
+  let dailyPnl = getAvgPnlForDay({ timestamp: options.startOfDay, historyPnls })
+
+  let perpFees = options.createBalances()
+  let spotFees = options.createBalances()
+  const { hlpShare } = getRevenueRatioShares(options.startOfDay)
   if (options.startOfDay < LLAMA_HL_INDEXER_FROM_TIME) {
     // get fees from hypurrscan, no volume
     const result = await queryHypurrscanApi(options);
-    dailyFees.add(result.dailyPerpFees.clone(hlpShare), 'Perp Fees')
-    dailyFees.add(result.dailySpotFees.clone(hlpShare), 'Spot Fees')
+    perpFees = result.dailyPerpFees.clone(hlpShare)
+    spotFees = result.dailySpotFees.clone(hlpShare)
   } else {
     const result = await queryHyperliquidIndexer(options);
-    dailyFees.add(result.dailyPerpRevenue.clone(hlpShare), 'Perp Fees')
-    dailyFees.add(result.dailyPerpRevenue.clone(hlpShare), 'Spot Fees')
+    perpFees = result.dailyPerpRevenue.clone(hlpShare)
+    spotFees = result.dailySpotRevenue.clone(hlpShare)
   }
+
+  dailyFees.add(perpFees, 'Perp Fees')
+  dailyFees.add(perpFees, 'Spot Fees')
+  dailyFees.addUSDValue(dailyPnl, 'Market Making & Liquidation Fees')
 
   return {
     dailyFees,
@@ -34,10 +82,12 @@ const breakdownMethodology = {
   Fees: {
     'Perp Fees': 'Share of 1% perp trading revenue.',
     'Spot Fees': 'Share of 1% spot trading revenue',
+    'Market Making & Liquidation Fees': 'All market marking and liquidation profits.',
   },
   SupplySideRevenue: {
     'Perp Fees': 'Share of 1% perp trading revenue.',
     'Spot Fees': 'Share of 1% spot trading revenue',
+    'Market Making & Liquidation Fees': 'All market marking and liquidation profits.',
   },
 }
 
@@ -51,6 +101,7 @@ const adapter: SimpleAdapter = {
     },
   },
   doublecounted: true, // we have already counted to supplySideRevenue on perps and spot
+  allowNegativeValue: true, // HLP vault PnL can be negative
 };
 
 export default adapter;
