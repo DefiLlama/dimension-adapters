7873fb96e671fb7f20b7b0cf748daa68d093babc
Eden <noat.eth@gmail.com>
2025-10-12 05:47:12 +0700

    disable hlp mm pnl

--- fees/hyperliquid-hlp.ts
+++ fees/hyperliquid-hlp.ts
@@ -1,56 +1,54 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { getRevenueRatioShares, LLAMA_HL_INDEXER_FROM_TIME, queryHyperliquidIndexer, queryHypurrscanApi } from "../helpers/hyperliquid";
-import { httpPost } from "../utils/fetchURL";
-import { getUniqStartOfTodayTimestamp } from "../helpers/getUniSubgraphVolume"
 
 // hl api return data in random days, not every days
 // so we will cal avg pnl from lower timestamp to upper timestamp
 // for example, hl api return data for 9 Oct, 11 Oct but missing 10 Oct
 // so the fees for both 9 Oct and 10 Oct are: (Fees of 11 Oct - Fees of 1 Oct) / 2
-function getAvgPnlForDay({ historyPnls, timestamp }: { historyPnls: {[key: number]: number}, timestamp: number }): number {
-  const ONE_DAY = 24 * 3600
+// function getAvgPnlForDay({ historyPnls, timestamp }: { historyPnls: {[key: number]: number}, timestamp: number }): number {
+//   const ONE_DAY = 24 * 3600
 
-  const currentTimestamp = Math.floor(new Date().getTime() / 1000)
-  let upperTimestamp = timestamp + ONE_DAY
-  if (upperTimestamp > currentTimestamp) {
-    throw Error(`can not found data for day ${timestamp}`)
-  }
+//   const currentTimestamp = Math.floor(new Date().getTime() / 1000)
+//   let upperTimestamp = timestamp + ONE_DAY
+//   if (upperTimestamp > currentTimestamp) {
+//     throw Error(`can not found data for day ${timestamp}`)
+//   }
 
-  let lowerTimestamp = timestamp
+//   let lowerTimestamp = timestamp
 
-  while(!historyPnls[upperTimestamp]) {
-    upperTimestamp += ONE_DAY;
-  }
-  while(!historyPnls[lowerTimestamp]) {
-    lowerTimestamp -= ONE_DAY;
-  }
+//   while(!historyPnls[upperTimestamp]) {
+//     upperTimestamp += ONE_DAY;
+//   }
+//   while(!historyPnls[lowerTimestamp]) {
+//     lowerTimestamp -= ONE_DAY;
+//   }
 
-  const days = (upperTimestamp - lowerTimestamp) / ONE_DAY
-  const pnl = historyPnls[upperTimestamp] - historyPnls[lowerTimestamp]
+//   const days = (upperTimestamp - lowerTimestamp) / ONE_DAY
+//   const pnl = historyPnls[upperTimestamp] - historyPnls[lowerTimestamp]
 
-  return pnl / days
-}
+//   return pnl / days
+// }
 
 async function fetch(_1: number, _: any,  options: FetchOptions) {
   const dailyFees = options.createBalances()
 
-  const vaultData = await httpPost('https://api-ui.hyperliquid.xyz/info', {
-    type: 'vaultDetails',
-    vaultAddress: '0xdfc24b077bc1425ad1dea75bcb6f8158e10df303',
-  })
+  // const vaultData = await httpPost('https://api-ui.hyperliquid.xyz/info', {
+  //   type: 'vaultDetails',
+  //   vaultAddress: '0xdfc24b077bc1425ad1dea75bcb6f8158e10df303',
+  // })
 
-  const historyPnls: {[key: number]: number} = {}
-  for (const chartItem of vaultData.portfolio) {
-    if (chartItem[0] === 'allTime') {
-      for (const pnlItem of chartItem[1].pnlHistory) {
-        const timestamp = getUniqStartOfTodayTimestamp(new Date(pnlItem[0]))
-        historyPnls[timestamp] = Number(pnlItem[1])
-      }
-    }
-  }
+  // const historyPnls: {[key: number]: number} = {}
+  // for (const chartItem of vaultData.portfolio) {
+  //   if (chartItem[0] === 'allTime') {
+  //     for (const pnlItem of chartItem[1].pnlHistory) {
+  //       const timestamp = getUniqStartOfTodayTimestamp(new Date(pnlItem[0]))
+  //       historyPnls[timestamp] = Number(pnlItem[1])
+  //     }
+  //   }
+  // }
 
-  const dailyPnl = getAvgPnlForDay({ timestamp: options.startOfDay, historyPnls })
+  // const dailyPnl = getAvgPnlForDay({ timestamp: options.startOfDay, historyPnls })
 
   let perpFees = options.createBalances()
   let spotFees = options.createBalances()
@@ -68,7 +66,7 @@ async function fetch(_1: number, _: any,  options: FetchOptions) {
 
   dailyFees.add(perpFees, 'Perp Fees')
   dailyFees.add(perpFees, 'Spot Fees')
-  dailyFees.addUSDValue(dailyPnl, 'Market Making & Liquidation Fees')
+  // dailyFees.addUSDValue(dailyPnl, 'Market Making & Liquidation Fees')
 
   return {
     dailyFees,
@@ -87,12 +85,12 @@ const breakdownMethodology = {
   Fees: {
     'Perp Fees': 'Share of 1% perp trading revenue.',
     'Spot Fees': 'Share of 1% spot trading revenue',
-    'Market Making & Liquidation Fees': 'All market marking and liquidation profits.',
+    // 'Market Making & Liquidation Fees': 'All market marking and liquidation profits.',
   },
   SupplySideRevenue: {
     'Perp Fees': 'Share of 1% perp trading revenue.',
     'Spot Fees': 'Share of 1% spot trading revenue',
-    'Market Making & Liquidation Fees': 'All market marking and liquidation profits.',
+    // 'Market Making & Liquidation Fees': 'All market marking and liquidation profits.',
   },
 }
 
@@ -106,7 +104,7 @@ const adapter: SimpleAdapter = {
     },
   },
   doublecounted: true, // we have already counted to supplySideRevenue on perps and spot
-  allowNegativeValue: true, // HLP vault PnL can be negative
+  // allowNegativeValue: true, // HLP vault PnL can be negative
 };
 
 export default adapter;
