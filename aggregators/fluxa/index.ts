91f8b55d4eecfba3df403f76625135bce79b6dd9
treeoflife2 <sol.analyzoor@gmail.com>
2025-10-13 19:58:30 +0530

    refactor fluxa

--- aggregators/fluxa/index.ts
+++ aggregators/fluxa/index.ts
@@ -1,29 +1,24 @@
 import { CHAIN } from "../../helpers/chains";
-import { httpGet } from "../../utils/fetchURL";
-import { FetchOptions } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import fetchURL from "../../utils/fetchURL";
 
 const BASE_URL = "https://api-fluxa.capylabs.io/api/v1";
 
-const fetch = async ({ fromTimestamp, toTimestamp }: FetchOptions) => {
-  const url = `${BASE_URL}/transactions/daily-total-volume?startTimestamp=${
-    fromTimestamp * 1000
-  }&endTimestamp=${toTimestamp * 1000}`;
+const fetch = async (options: FetchOptions) => {
+  const url = `${BASE_URL}/transactions/daily-total-volume?startTimestamp=${options.startTimestamp * 1000 }&endTimestamp=${options.endTimestamp * 1000}`;
 
-  const res = await httpGet(url);
+  const res = await fetchURL(url);
+
   return {
     dailyVolume: res.totalDailyVolumeUsd,
   };
 };
 
-const adapter: any = {
+const adapter: SimpleAdapter = {
   version: 2,
-  deadFrom: "2025-10-12",
-  adapter: {
-    [CHAIN.SEI]: {
-      fetch: fetch,
-      start: "2025-10-13",
-    },
-  },
+  fetch,
+  chains: [CHAIN.SEI],
+  start: "2025-10-12",
 };
 
 export default adapter;
