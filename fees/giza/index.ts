79143b74168729dec45e55c9d6eb8da73065ead3
Eden <noat.eth@gmail.com>
2025-10-13 22:53:04 +0700

    refactor

--- fees/giza/index.ts
+++ fees/giza/index.ts
@@ -1,40 +1,37 @@
 import { SimpleAdapter, FetchOptions } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains"
 
-const methodology = {
-    Fees: "A linearly decaying fee starting at 5% is charged on withdrawals made within 30 days of deposit.",
-    Revenue: "All early withdrawal fees are collected by the protocol."
-}
-
-const stakeWithdrawABI = "event StakeWithdrawn(address indexed user, uint256 amount, uint256 fee)";
+const Staking = '0xE576638a9f2AD99EE9dD6F4AcBb83217566D8e18'
+const GIZA = '0x590830dFDf9A3F68aFCDdE2694773dEBDF267774'
+const StakeWithdrawnEvent = "event StakeWithdrawn(address indexed user, uint256 amount, uint256 fee)"
 
 const fetch = async (options: FetchOptions) => {
 
     const dailyFees = options.createBalances();
     const withdrawLogs = await options.getLogs({
-        target: "0xE576638a9f2AD99EE9dD6F4AcBb83217566D8e18",
-        eventAbi: stakeWithdrawABI,
-        fromBlock: await options.getFromBlock(),
-        toBlock: await options.getToBlock(),
-        onlyArgs: true,
+        target: Staking,
+        eventAbi: StakeWithdrawnEvent,
     })
 
     for (const log of withdrawLogs) {
-        dailyFees._add("0x590830dFDf9A3F68aFCDdE2694773dEBDF267774", log.fee)
+        dailyFees.add(GIZA, log.fee)
     }
 
-    return { dailyFees, dailyRevenue: dailyFees }
+    return {
+        dailyFees,
+        dailyRevenue: dailyFees,
+    }
 }
 
 const adapter: SimpleAdapter = {
     version: 2,
-    adapter: {
-        [CHAIN.BASE]: {
-            fetch: fetch,
-            start: "4-14-25",
-        }
+    fetch,
+    chains: [CHAIN.BASE],
+    start: "2025-04-14",
+    methodology: {
+        Fees: "A linearly decaying fee starting at 5% is charged on withdrawals made within 30 days of deposit.",
+        Revenue: "All early withdrawal fees are collected by the protocol."
     },
-    methodology: methodology,
 }
 
 export default adapter 
