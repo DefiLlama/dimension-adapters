d41cc8a995c7868186131502b4a9381228d34079
Eden <noat.eth@gmail.com>
2025-10-15 05:02:18 +0700

    track moonwell vaults fees

--- helpers/curators/configs.ts
+++ helpers/curators/configs.ts
@@ -92,6 +92,14 @@ export const MorphoConfigs: any = {
       },
     ],
   },
+  [CHAIN.OPTIMISM]: {
+    vaultFactories: [
+      {
+        address: '0x3Bb6A6A0Bc85b367EFE0A5bAc81c5E52C892839a',
+        fromBlock: 130770189,
+      },
+    ],
+  },
 }
 
 export const EulerConfigs: any = {
