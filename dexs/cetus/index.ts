a916ec9fcb1a271142c382518364fb68f30e8fbc
treeoflife2 <sol.analyzoor@gmail.com>
2025-10-15 12:50:47 +0530

    keep aptos chain for historical data

--- dexs/cetus/index.ts
+++ dexs/cetus/index.ts
@@ -2,7 +2,11 @@ import fetchURL from "../../utils/fetchURL";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-async function fetch(_a: any, _b: any, { startTimestamp, endTimestamp }: FetchOptions) {
+async function fetch(_a: any, _b: any, { startTimestamp, endTimestamp, chain }: FetchOptions) {
+  if (chain === CHAIN.APTOS && endTimestamp > 1747958400){
+    return { dailyVolume: 0 }
+  }
+
   // const hackDay = (+new Date('2025-05-22')) / 1e3
   // if (endTimestamp > hackDay) return { dailyVolume: 0 }  // dex is paused
   const dailyVolume = (await fetchURL(`https://api-sui.cetus.zone/v3/sui/vol/time_range?date_type=hour&start_time=${startTimestamp}&end_time=${endTimestamp}`)).data.vol_in_usd;
@@ -13,7 +17,7 @@ async function fetch(_a: any, _b: any, { startTimestamp, endTimestamp }: FetchOp
 const adapter: SimpleAdapter = {
   version: 1,
   fetch,
-  chains: [CHAIN.SUI],
+  chains: [CHAIN.SUI, CHAIN.APTOS],
   start: '2023-05-02',
 };
 
