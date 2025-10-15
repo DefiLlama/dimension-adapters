8005ea2a45c01651600d8f08afa128000f5b3cc3
treeoflife2 <sol.analyzoor@gmail.com>
2025-10-15 12:42:58 +0530

    refactor

--- dexs/cetus-dlmm/index.ts
+++ dexs/cetus-dlmm/index.ts
@@ -2,25 +2,18 @@ import fetchURL from "../../utils/fetchURL";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
+const fetch = async ({ startTimestamp, endTimestamp }: FetchOptions) => {
+  const volList = (await fetchURL(`https://api-sui.cetus.zone/v3/sui/dlmm/histogram?dateType=hour&dataType=vol&beginTimestamp=${startTimestamp}&endTimestamp=${endTimestamp}`)).data.list;
+  let dailyVolume = volList.reduce((p, c) => p + Number(c.value), 0);
 
-const fetch = async (_: any, _1: any, { fromTimestamp, toTimestamp }: FetchOptions) => {
-  const volList = (await fetchURL(`https://api-sui.cetus.zone/v3/sui/dlmm/histogram?dateType=hour&dataType=vol&beginTimestamp=${fromTimestamp}&endTimestamp=${toTimestamp}`)).data.list;
-  var Volume = 0;
-  for (const item of volList) {
-    Volume += Number(item.value);
-  }
-  return {
-    dailyVolume: Volume,
-  };
+  return { dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.SUI]: {
-      fetch: fetch,
-      start: '2025-09-30',
-    }
-  }
+  version: 2,
+  fetch,
+  chains: [CHAIN.SUI],
+  start: '2025-09-30',
 };
 
 export default adapter;
--- dexs/cetus/index.ts
+++ dexs/cetus/index.ts
@@ -2,24 +2,19 @@ import fetchURL from "../../utils/fetchURL";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-
-async function fetchSUI(_: any, _1: any, { startTimestamp, endTimestamp }: FetchOptions) {
-
+async function fetch(_a: any, _b: any, { startTimestamp, endTimestamp }: FetchOptions) {
   // const hackDay = (+new Date('2025-05-22')) / 1e3
   // if (endTimestamp > hackDay) return { dailyVolume: 0 }  // dex is paused
   const dailyVolume = (await fetchURL(`https://api-sui.cetus.zone/v3/sui/vol/time_range?date_type=hour&start_time=${startTimestamp}&end_time=${endTimestamp}`)).data.vol_in_usd;
-  return {
-    dailyVolume: dailyVolume,
-  };
+
+  return { dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.SUI]: {
-      fetch: fetchSUI,
-      start: '2023-05-02',
-    }
-  }
+  version: 1,
+  fetch,
+  chains: [CHAIN.SUI],
+  start: '2023-05-02',
 };
 
 export default adapter;
