b6cb64287f135ca8991737db0f65683a4893e8da
treeoflife2 <sol.analyzoor@gmail.com>
2025-10-17 22:04:37 +0530

    flowx perp change chain to hyperliquid

--- dexs/flowx-perps/index.ts
+++ dexs/flowx-perps/index.ts
@@ -3,15 +3,15 @@ import { CHAIN } from "../../helpers/chains";
 import fetchURL from "../../utils/fetchURL";
 
 const fetch = async ({ fromTimestamp, toTimestamp }: FetchOptions) => {
-  const volume = await fetchURL(`https://api.flowx.finance/flowx-be/api/perp-tracking/volume-in-timerange?startTime=${fromTimestamp * 1000}&endTime=${toTimestamp * 1000}`);
+  const dailyVolume = await fetchURL(`https://api.flowx.finance/flowx-be/api/perp-tracking/volume-in-timerange?startTime=${fromTimestamp * 1000}&endTime=${toTimestamp * 1000}`);
 
-  return { dailyVolume: volume };
+  return { dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
   version: 2,
   fetch,
-  chains: [CHAIN.SUI],
+  chains: [CHAIN.HYPERLIQUID],
   start: "2025-10-01",
 };
 
