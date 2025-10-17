b222b13bbda7745d6634368daa84f5404fe6a545
treeoflife2 <sol.analyzoor@gmail.com>
2025-10-17 22:33:47 +0530

    refactor aark fees/volume adapter

--- dexs/aark/index.ts
+++ dexs/aark/index.ts
@@ -2,12 +2,9 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const usdcAddress = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'
-
-
 const MoonOrderOpenedV2 = "event MoonOrderOpenedV2(address user, uint32 moonIndex, uint32 marketId, uint32 timestamp, uint64 entryPrice, int64 qty, uint16 leverage, int64 lastAccFundingFactor, uint64 takeProfit, uint48 initMargin, uint48 openFee, uint16 executionFee)";
 const MoonOrderClosedV2 = "event MoonOrderClosedV2(address user, uint256 moonIndex, uint32 marketId, uint64 indexPrice, int48 pnl, uint48 closeFee, int48 fundingFee, uint48 userPayback, uint256 timestamp)";
-                           
+
 const FuturesManager = '0x0b848a8A5eC8950E67d19E7a21A6Be29F44F685e';
 
 const fetch = async (options: FetchOptions) => {
@@ -19,12 +16,10 @@ const fetch = async (options: FetchOptions) => {
   const positionQty = new Map<string, number>();
   openData.forEach((log: any) => {
     const entryPrice = Number(log.entryPrice) / 1e8;
-    const qty = ((log.qty < 0) ? -Number(log.qty) : Number(log.qty)) / (1e10); // short position to positive value
-    const openVolume = entryPrice * qty * 1e6;
-    //    console.log('moonIndex:', log.moonIndex, log.entryPrice, 'entryPrice:', entryPrice, 'qty:', qty, 'openVolume:', openVolume);
-
+    const qty = Math.abs(Number(log.qty)) / (1e10);
+    const openVolume = entryPrice * qty;
     positionQty.set(log.moonIndex, qty);
-    dailyVolume.add(usdcAddress, openVolume);
+    dailyVolume.addUSDValue(openVolume);
   });
 
   const closeData: any[] = await options.getLogs({
@@ -32,30 +27,23 @@ const fetch = async (options: FetchOptions) => {
     eventAbi: MoonOrderClosedV2,
   });
   closeData.forEach((log: any) => {
-    if(positionQty.has(log.moonIndex)){
+    if (positionQty.has(log.moonIndex)) {
       const qty = positionQty.get(log.moonIndex)!;
       const indexPrice = Number(log.indexPrice) / 1e8;
-      const closeVolume = indexPrice * qty * 1e6;
-      dailyVolume.add(usdcAddress, closeVolume);
+      const closeVolume = indexPrice * qty;
+      dailyVolume.addUSDValue(closeVolume);
       positionQty.delete(log.moonIndex);
-    }else{
-      //console.log(`No matching open position for close event with moonIndex: ${log.moonIndex}`);
     }
   });
 
   return { dailyVolume };
 };
 
-const methodology = {
-  dailyVolume: 'Perp Volume: Notional volume of all trades in the protocol, includes leverage',
-}
-
 const adapter: SimpleAdapter = {
   version: 2,
   fetch,
   chains: [CHAIN.ARBITRUM],
   start: '2024-11-01',
-  methodology
 }
 
 export default adapter;
--- fees/aark/index.ts
+++ fees/aark/index.ts
@@ -3,7 +3,6 @@ import { CHAIN } from "../../helpers/chains";
 
 const usdcAddress = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'
 
-
 const MoonOrderOpenedV2 = "event MoonOrderOpenedV2(address user, uint32 moonIndex, uint32 marketId, uint32 timestamp, uint64 entryPrice, int64 qty, uint16 leverage, int64 lastAccFundingFactor, uint64 takeProfit, uint48 initMargin, uint48 openFee, uint16 executionFee)";
 const MoonOrderClosedV2 = "event MoonOrderClosedV2(address user, uint256 moonIndex, uint32 marketId, uint64 indexPrice, int48 pnl, uint48 closeFee, int48 fundingFee, uint48 userPayback, uint256 timestamp)";
                            
@@ -29,11 +28,13 @@ const fetch = async (options: FetchOptions) => {
   closeData.forEach((log: any) => {
     dailyFees.add(usdcAddress, log.closeFee);
   });
-  return { dailyFees };
+  return { dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, dailyHoldersRevenue: 0 };
 };
 
 const methodology = {
   Fees: 'All fees paid by users for executionFee, openFee, closeFee.',
+  Revenue: 'trade open/close/execution fees to protocol.',
+  ProtocolRevenue: 'trade open/close/execution fees to protocol.',
 }
 
 const adapter: SimpleAdapter = {
