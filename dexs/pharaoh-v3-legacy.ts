03cc2488ce2161f6c3becc93644468464018f740
0xshad0w <sd0w@protonmail.com>
2025-10-17 09:28:31 +0100

    add pharaoh-v3-cl and legacy

---
+++ dexs/pharaoh-v3-legacy.ts
@@ -0,0 +1,43 @@
+import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { fetchStats } from "./pharaoh-v3";
+
+const fetch = async (_: any, _1: any, options: FetchOptions) => {
+  const stats = await fetchStats(options);
+
+  const dailyFees = stats.legacyFeesUSD;
+  const dailyVolume = stats.legacyVolumeUSD;
+  const dailyHoldersRevenue = stats.legacyUserFeesRevenueUSD;
+  const dailyProtocolRevenue = stats.legacyProtocolRevenueUSD;
+  const dailyBribesRevenue = stats.legacyBribeRevenueUSD;
+
+  return {
+    dailyVolume,
+    dailyFees,
+    dailyUserFees: dailyFees,
+    dailyHoldersRevenue,
+    dailyProtocolRevenue,
+    dailyRevenue: dailyProtocolRevenue + dailyHoldersRevenue,
+    dailySupplySideRevenue: dailyFees - dailyHoldersRevenue - dailyProtocolRevenue,
+    dailyBribesRevenue,
+  };
+};
+
+const methodology = {
+  Fees: "Fees are collected from users on each swap.",
+  Revenue: "Revenue going to the protocol + Token holder Revenue.",
+  UserFees: "User pays fees on each swap.",
+  ProtocolRevenue: "Revenue going to the protocol.",
+  HoldersRevenue: "User fees are distributed among holders.",
+  SupplySideRevenue: "Fees distributed to LPs.",
+  BribesRevenue: "Bribes are distributed among holders.",
+};
+
+const adapter: SimpleAdapter = {
+  fetch,
+  chains: [CHAIN.AVAX],
+  start: '2025-10-08',
+  methodology,
+};
+
+export default adapter;
