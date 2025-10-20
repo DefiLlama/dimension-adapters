abee96fcc3c211e40cad29a1017714100964ccce
iocozmo <tmiwia@gmail.com>
2025-10-19 22:54:48 -0400

    Cleanup

--- fees/ore/index.ts
+++ fees/ore/index.ts
@@ -1,4 +1,3 @@
-// Dune Query ID: 5982386
 import { Dependencies, FetchOptions, SimpleAdapter } from '../../adapters/types';
 import { CHAIN } from '../../helpers/chains';
 import { queryDuneSql } from '../../helpers/dune';
