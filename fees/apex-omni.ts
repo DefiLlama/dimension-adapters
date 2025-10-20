4b2ceee1e384c752607d85ef7052ff74ae8d810d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-10-20 09:36:43 +0200

    throw error if fee data is missing #4390

--- fees/apex-omni.ts
+++ fees/apex-omni.ts
@@ -1,9 +1,9 @@
 import { FetchOptions, FetchResultFees, SimpleAdapter } from "../adapters/types"
 import { CHAIN } from "../helpers/chains";
-import { getTimestampAtStartOfDayUTC } from "../utils/date"
 import { httpGet } from "../utils/fetchURL"
 import { METRIC } from "../helpers/metrics";
 import ADDRESSES from '../helpers/coreAssets.json'
+import { addTokensReceived } from "../helpers/token";
 
 const BUYBACK_VAULT_ADDR = '0x18A45C46840CF830e43049C8fe205CA05B43527B';
 const TOKEN_APEX = ADDRESSES.arbitrum.APEX;
@@ -13,35 +13,20 @@ interface IFees {
 }
 
 const fetch = async (_: any, _b: any, options: FetchOptions): Promise<FetchResultFees> => {
-  const todaysTimestamp = getTimestampAtStartOfDayUTC(options.startOfDay) * 1000;
-  const url = `https://omni.apex.exchange/api/v3/data/fee-by-date?time=${todaysTimestamp}`;
-  const feesData: IFees = (await httpGet(url, { timeout: 10000 })).data;
-  const dailyFees = feesData?.feeOfDate || '0';
+  const url = `https://omni.apex.exchange/api/v3/data/fee-by-date?time=${options.startOfDay * 1000}`;
+  const feesData: IFees = (await httpGet(url)).data;
+  if (typeof feesData?.feeOfDate !== "string") throw new Error("No fee data");
   return {
-    dailyFees,
-    dailyUserFees: dailyFees,
-    dailyRevenue: 0,
-    dailyHoldersRevenue: 0,
+    dailyFees: feesData?.feeOfDate,
   }
 }
 
 // tracks APEX token buybacks
 const fetchRevenue = async (_: any, _b: any, options: FetchOptions): Promise<FetchResultFees> => {
-  const dailyHoldersRevenue = options.createBalances();
-
   // Buybacks are not automated, so we have to track this address for any inflows
-  const transferEvents = await options.getLogs({
-    target: TOKEN_APEX,
-    eventAbi: 'event Transfer(address indexed from, address indexed to, uint256 value)',
-  });
-  transferEvents.forEach((log: any) => {
-    if (log.to.toLowerCase() === BUYBACK_VAULT_ADDR.toLowerCase()) {
-        dailyHoldersRevenue.add(TOKEN_APEX, log.value, METRIC.TOKEN_BUY_BACK);
-    }
-  });
+  const dailyHoldersRevenue = await addTokensReceived({ options, token: TOKEN_APEX, target: BUYBACK_VAULT_ADDR})
+
   return {
-    dailyFees: 0,
-    dailyUserFees: 0,
     dailyRevenue: dailyHoldersRevenue,
     dailyHoldersRevenue
   }
