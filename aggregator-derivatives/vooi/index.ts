c9b8be4ea0166160a2998c7c5de5ca2c213c19a3
mikalai-vooi <mikalai@vooi.io>
2025-10-21 18:01:45 +0300

    Vooi add Gains protocol

--- aggregator-derivatives/vooi/index.ts
+++ aggregator-derivatives/vooi/index.ts
@@ -1,83 +1,130 @@
 import fetchURL from "../../utils/fetchURL";
 import { FetchResult, SimpleAdapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import asyncRetry from "async-retry";
 
-const URL = "https://vooi-rebates.fly.dev/defillama/volumes";
-
-const fetchArbitrum = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResult> => {
-    const fetchData = await fetchURL(`${URL}?ts=${options.startOfDay}`)
-    let synfuturesItem = fetchData.filter(((item) => item.protocol == "synfutures"))
-    let ostiumItem = fetchData.find(((item) => item.protocol == "ostium"))
-    let gmxItem = fetchData.find(((item) => item.protocol == "gmx" && item.network == "arbitrum"))
-
-    let dailyVolume = Number(ostiumItem?.dailyVolume || 0) + Number(gmxItem?.dailyVolume || 0)
-
-    for (let i in synfuturesItem){
-        dailyVolume = Number(dailyVolume) + Number(synfuturesItem[i].dailyVolume)
+async function fetchStatistics(startOfDay: number) {
+  const data = await asyncRetry(
+    async () =>
+      fetchURL(
+        `https://vooi-rebates.fly.dev/defillama/volumes?ts=${startOfDay}`
+      ),
+    {
+      retries: 3,
+      minTimeout: 1000,
+      maxTimeout: 5000,
+      factor: 2,
     }
+  );
+  return data.map((item) => ({
+    ...item,
+    dailyVolume: Number(item.dailyVolume),
+  }));
+}
 
-    return {
-        dailyVolume
-    };
+const fetchArbitrum = async (
+  _a: number,
+  _t: any,
+  options: FetchOptions
+): Promise<FetchResult> => {
+  const data = await fetchStatistics(options.startOfDay);
+  const dailyVolume = data.reduce((acc, item) => {
+    if (item.protocol == "ostium") {
+      acc += item.dailyVolume;
+    }
+    if (item.protocol == "gmx" && item.network == "arbitrum") {
+      acc += item.dailyVolume;
+    }
+    if (item.protocol == "gains" && item.network == "arbitrum") {
+      acc += item.dailyVolume;
+    }
+    if (item.protocol == "synfutures") {
+      acc += item.dailyVolume;
+    }
+    return acc;
+  }, 0);
+  return { dailyVolume };
 };
-
-const fetchOptimism = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResult> => {
-    const fetchData = await fetchURL(`${URL}?ts=${options.startOfDay}`)
-    let orderlyItem = fetchData.find(((item) => item.protocol == "orderly"))
-
-    return {
-        dailyVolume: Number(orderlyItem?.dailyVolume || 0)
-    };
+const fetchOptimism = async (
+  _a: number,
+  _t: any,
+  options: FetchOptions
+): Promise<FetchResult> => {
+  const data = await fetchStatistics(options.startOfDay);
+  const dailyVolume = data.reduce((acc, item) => {
+    if (item.protocol == "orderly") {
+      acc += item.dailyVolume;
+    }
+    return acc;
+  }, 0);
+  return { dailyVolume };
 };
-
-const fetchHyperliquid = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResult> => {
-    const fetchData = await fetchURL(`${URL}?ts=${options.startOfDay}`)
-    let hyperliquidItem = fetchData.find(((item) => item.protocol == "hyperliquid"))
-    return {
-        dailyVolume: Number(hyperliquidItem?.dailyVolume || 0)
-    };
+const fetchHyperliquid = async (
+  _a: number,
+  _t: any,
+  options: FetchOptions
+): Promise<FetchResult> => {
+  const data = await fetchStatistics(options.startOfDay);
+  const dailyVolume = data.reduce((acc, item) => {
+    if (item.protocol == "hyperliquid") {
+      acc += item.dailyVolume;
+    }
+    return acc;
+  }, 0);
+  return { dailyVolume };
 };
-
-const fetchBsc = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResult> => {
-    const fetchData = await fetchURL(`${URL}?ts=${options.startOfDay}`)
-    let kiloexItem = fetchData.filter(((item) => item.protocol == "kiloex" && item.network != "base"))
-    return {
-        dailyVolume: kiloexItem.reduce((acc, item) => acc + Number(item.dailyVolume), 0)
-    };
+const fetchBsc = async (
+  _a: number,
+  _t: any,
+  options: FetchOptions
+): Promise<FetchResult> => {
+  const data = await fetchStatistics(options.startOfDay);
+  const dailyVolume = data.reduce((acc, item) => {
+    if (item.protocol == "kiloex" && item.network != "base") {
+      acc += item.dailyVolume;
+    }
+    return acc;
+  }, 0);
+  return { dailyVolume };
 };
-
-const fetchBase = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResult> => {
-    const fetchData = await fetchURL(`${URL}?ts=${options.startOfDay}`)
-    let kiloexItem = fetchData.filter(((item) => item.protocol == "kiloex" && item.network == "base"))[0]
-    return {
-        dailyVolume: Number(kiloexItem?.dailyVolume || 0)
-    };
+const fetchBase = async (
+  _a: number,
+  _t: any,
+  options: FetchOptions
+): Promise<FetchResult> => {
+  const data = await fetchStatistics(options.startOfDay);
+  const dailyVolume = data.reduce((acc, item) => {
+    if (item.protocol == "kiloex" && item.network === "base") {
+      acc += item.dailyVolume;
+    }
+    return acc;
+  }, 0);
+  return { dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
-    adapter: {
-        [CHAIN.ARBITRUM]: {
-            fetch: fetchArbitrum,
-            start: '2024-05-02'
-        },
-        [CHAIN.OPTIMISM]: {
-            fetch: fetchOptimism,
-            start: '2024-05-02'
-        },
-        [CHAIN.BSC]: {
-            fetch: fetchBsc,
-            start: '2024-06-01'
-        },
-        [CHAIN.BASE]: {
-            fetch: fetchBase,
-            start: '2024-08-01'
-        },
-        [CHAIN.HYPERLIQUID]: {
-            fetch: fetchHyperliquid,
-            start: '2024-11-04'
-        }
+  adapter: {
+    [CHAIN.ARBITRUM]: {
+      fetch: fetchArbitrum,
+      start: "2024-05-02",
     },
-    doublecounted: true
-}
-
-export default adapter
+    [CHAIN.OPTIMISM]: {
+      fetch: fetchOptimism,
+      start: "2024-05-02",
+    },
+    [CHAIN.BSC]: {
+      fetch: fetchBsc,
+      start: "2024-06-01",
+    },
+    [CHAIN.BASE]: {
+      fetch: fetchBase,
+      start: "2024-08-01",
+    },
+    [CHAIN.HYPERLIQUID]: {
+      fetch: fetchHyperliquid,
+      start: "2024-11-04",
+    },
+  },
+  doublecounted: true,
+};
+export default adapter;
