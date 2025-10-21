54e45be3aff9d82e8b1cd429bf7d11151b057f14
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-10-21 17:38:35 +0530

    track cowswap bsc

--- aggregators/cowswap/index.ts
+++ aggregators/cowswap/index.ts
@@ -4,7 +4,6 @@ import { CHAIN } from "../../helpers/chains";
 const abis = {
   "Trade": "event Trade(address indexed owner, address sellToken, address buyToken, uint256 sellAmount, uint256 buyAmount, uint256 feeAmount, bytes orderUid)", // gnosis
 }
-
 const fetch = async (_:any, _1: any, { createBalances, getLogs, }: FetchOptions) => {
   const dailyVolume = createBalances()
 
@@ -25,6 +24,7 @@ const adapter: any = {
     polygon: { start: '2023-12-10', },
     avax: { start: '2025-03-10', },
     [CHAIN.LENS]: { start: '2025-06-16', },
+    [CHAIN.BSC]: { start: '2025-09-04', }
   },
 };
 
