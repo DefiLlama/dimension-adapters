9833eab8713c8450d01d40277612f57cb45ab1b9
Eden <noat.eth@gmail.com>
2025-10-22 23:56:54 +0700

    use version 1

--- dexs/rho-x/index.ts
+++ dexs/rho-x/index.ts
@@ -2,9 +2,9 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import fetchURL from "../../utils/fetchURL";
 
-const fetch = async ({ fromTimestamp, toTimestamp }: FetchOptions) => {
-  const startTime = new Date(fromTimestamp * 1000).toISOString();
-  const endTime = new Date(toTimestamp * 1000).toISOString();
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+  const startTime = new Date(options.fromTimestamp * 1000).toISOString();
+  const endTime = new Date(options.toTimestamp * 1000).toISOString();
   const url = `https://x.rho.trading/api/v1/stats/volume?startTime=${startTime}&endTime=${endTime}`
   const dailyVolume = await fetchURL(url);
 
@@ -12,7 +12,6 @@ const fetch = async ({ fromTimestamp, toTimestamp }: FetchOptions) => {
 };
 
 const adapter: SimpleAdapter = {
-  version: 2,
   fetch,
   chains: [CHAIN.ETHEREUM],
   start: "2025-09-22",
