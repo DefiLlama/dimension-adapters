67008e2cde398d283fafdb00bc78ba1a34e3ef16
Eden <noat.eth@gmail.com>
2025-10-22 15:24:10 +0700

    refactor

--- fees/meta-pool-eth/index.ts
+++ fees/meta-pool-eth/index.ts
@@ -1,37 +1,40 @@
 import { SimpleAdapter, FetchOptions } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains"
+import { METRIC } from "../../helpers/metrics"
+import { getERC4626VaultsYield } from "../../helpers/erc4626"
+import ADDRESSES from "../../helpers/coreAssets.json";
 
-const swapABI = "event Swap (address indexed user, uint256 amountIn, uint256 amountOut, uint256 fees, uint256 treasuryFees)";
-const sharesToAssetsABI = "function convertToAssets (uint256 shares) external view returns (uint256)";
-const unstakePool = "0xdF261F967E87B2aa44e18a22f4aCE5d7f74f03Cc";
+const vaults = [
+    '0x48afbbd342f64ef8a9ab1c143719b63c2ad81710', // old
+    '0xd06f6a56c5f599cb375b616df306f32b7f6f4a0e', // new
+];
+
+// https://docs.metapool.app/master/faq-frequently-asked-questions/faq-by-chain/ethereum#does-meta-pool-charge-for-the-service
+const PROTOCOL_FEE = 0.1; // 10%
+
+// https://docs.metapool.app/master/faq-frequently-asked-questions/faq-by-chain/ethereum#deposite-fee
+const DEPOSIT_FEE = 0.01; // 1%
 
 const fetch = async (options: FetchOptions) => {
-    const dailyFees = options.createBalances();
-    const dailyRevenue = options.createBalances();
+    const dailyYield = await getERC4626VaultsYield({options, vaults })
 
-    const unstakeLogs = await options.getLogs({
-        target: unstakePool,
-        eventAbi: swapABI,
-        fromBlock: await options.getFromBlock(),
-        toBlock: await options.getToBlock(),
-        entireLog: true,
+    const dailyFees = dailyYield.clone(1 / (1 - PROTOCOL_FEE), METRIC.STAKING_REWARDS);
+    const dailyRevenue = dailyFees.clone(PROTOCOL_FEE, METRIC.STAKING_REWARDS);
+    const dailySupplySideRevenue = dailyFees.clone(1, METRIC.STAKING_REWARDS);
+    dailySupplySideRevenue.subtract(dailyRevenue, METRIC.STAKING_REWARDS);
+
+    const dailyUserFees = options.createBalances()
+
+    const depositEvents = await options.getLogs({
+        target: '0xD06f6a56c5f599cB375B616DF306f32B7F6f4A0E',
+        eventAbi: 'event Deposit (address indexed sender, address indexed owner, uint256 assets, uint256 shares)',
     })
-
-    for (const log of unstakeLogs) {
-        const assetsConversion = await options.api.multiCall({
-            target: unstakePool,
-            abi: sharesToAssetsABI,
-            calls: [
-                { params: [log.args.fees] },
-                { params: [log.args.treasuryFees] }
-            ],
-            block: log.blockNumber
-        })
-        const totalFees = BigInt(assetsConversion[0]) + BigInt(assetsConversion[1]);
-        dailyFees.addGasToken(totalFees);
-        dailyRevenue.addGasToken(assetsConversion[1]);
+    for (const event of depositEvents) {
+        dailyFees.add(ADDRESSES.ethereum.WETH, Number(event.assets) * DEPOSIT_FEE, METRIC.DEPOSIT_WITHDRAW_FEES)
+        dailyRevenue.add(ADDRESSES.ethereum.WETH, Number(event.assets) * DEPOSIT_FEE, METRIC.DEPOSIT_WITHDRAW_FEES)
     }
-    return { dailyFees, dailyRevenue }
+
+    return { dailyFees, dailyRevenue, dailyUserFees, dailySupplySideRevenue }
 }
 
 const adapter: SimpleAdapter = {
@@ -39,7 +42,34 @@ const adapter: SimpleAdapter = {
     adapter: {
         [CHAIN.ETHEREUM]: {
             fetch: fetch,
-            start: "Jul-13-2023"
+            start: '2023-07-14',
+        },
+    },
+    methodology: {
+        Fees: 'Total ETH staking rewards.',
+        UserFees: 'Users pay 1% fee on every deposit.',
+        Revenue: 'Share of 10% from ETH staking rewards + 1% deposit fee.',
+        ProtocolRevenue: 'Share of 10% from ETH staking rewards + 1% deposit fee.',
+        SupplySideRevenue: 'Share of 90% from ETH staking rewards.',
+    },
+    breakdownMethodology: {
+        Fees: {
+            [METRIC.STAKING_REWARDS]: 'ETH staking rewards',
+            [METRIC.DEPOSIT_WITHDRAW_FEES]: '1% fees charged on every deposit.',
+        },
+        UserFees: {
+            [METRIC.DEPOSIT_WITHDRAW_FEES]: '1% fees charged on every deposit.',
+        },
+        Revenue: {
+            [METRIC.STAKING_REWARDS]: 'Share of 10% ETH staking rewards',
+            [METRIC.DEPOSIT_WITHDRAW_FEES]: '1% fees charged on every deposit.',
+        },
+        ProtocolRevenue: {
+            [METRIC.STAKING_REWARDS]: 'Share of 10% ETH staking rewards',
+            [METRIC.DEPOSIT_WITHDRAW_FEES]: '1% fees charged on every deposit.',
+        },
+        SupplySideRevenue: {
+            [METRIC.STAKING_REWARDS]: 'Share of 90% ETH staking rewards',
         },
     }
 }
