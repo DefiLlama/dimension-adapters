44d64a645c8ee734671e23c0bd5d809de1ea64b8
Bearetta Bera <bearetta@berachain.com>
2025-10-22 15:33:36 +0200

    add performance fee

--- fees/berachain-bend/index.ts
+++ fees/berachain-bend/index.ts
@@ -10,7 +10,7 @@ const CONFIG = {
 const methodology = {
   Fees: "Total borrow interest paid by borrowers + liquidation bonuses earned by liquidators.",
   SupplySideRevenue: "Total interests are distributed to suppliers/lenders + liquidation bonuses to liquidators.",
-  Revenue: "Total fees paid by borrowers from all markets.",
+  Revenue: "Total fees paid by borrowers from all markets and part of performance fee retained from all vaults.",
   ProtocolRevenue: "No revenue for Bend protocol.",
 }
 
@@ -18,15 +18,21 @@ const methodology = {
 const abi = {
   morphoBlueFunctions: {
     idToMarketParams: "function idToMarketParams(bytes32 Id) returns (address loanToken, address collateralToken, address oracle, address irm, uint256 lltv)",
-    market: "function market(bytes32 input) returns (uint128 totalSupplyAssets, uint128 totalSupplyShares, uint128 totalBorrowAssets, uint128 totalBorrowShares, uint128 lastUpdate, uint128 fee)"
+    market: "function market(bytes32 input) returns (uint128 totalSupplyAssets, uint128 totalSupplyShares, uint128 totalBorrowAssets, uint128 totalBorrowShares, uint128 lastUpdate, uint128 fee)",
+    feeRecipient: "function feeRecipient() returns(address feeRecipient)"
   },
   metaMorphoFunctions: {
     withdrawQueueLength: "function withdrawQueueLength() view returns (uint256)",
-    withdrawQueue: "function withdrawQueue(uint256 index) view returns (bytes32)"
+    withdrawQueue: "function withdrawQueue(uint256 index) view returns (bytes32)",
+    asset: "function asset() view returns (address)",
+    convertToAssets: "function convertToAssets(uint256 shares) view returns (uint256)"
   },
   morphoBlueEvents: {
     AccrueInterest: "event AccrueInterest(bytes32 indexed id, uint256 prevBorrowRate, uint256 interest, uint256 feeShares)",
     Liquidate: "event Liquidate(bytes32 indexed id,address indexed caller,address indexed borrower,uint256 repaidAssets,uint256 repaidShares,uint256 seizedAssets,uint256 badDebtAssets,uint256 badDebtShares)",
+  },
+  metaMorphoEvents: {
+    Transfer: "event Transfer(address indexed from, address indexed to, uint256 value)"
   }
 }
 
@@ -51,6 +57,11 @@ type MorphoBlueLiquidateEvent = {
   seizedAmount: bigint;
 };
 
+type PerformanceFee = {
+  token: string,
+  amount: bigint
+}
+
 type RewardVault = {
   stakingTokenAddress: string
 }
@@ -62,6 +73,9 @@ const toLowerKey = (id: string) => id.toLowerCase();
 const ONE = 10n ** 18n;
 
 const wMul = (x: bigint, y: bigint): bigint => (x * y / ONE);
+
+const ZERO_ADDRESS = '0x' + '0'.repeat(40);
+
 
 
 // https://docs.morpho.org/learn/concepts/liquidation/#liquidation-incentive-factor-lif
@@ -149,6 +163,7 @@ const fetchEvents = async (
     };
   });
 
+
   const liquidations: Array<MorphoBlueLiquidateEvent> = (
     await options.getLogs({
       eventAbi: abi.morphoBlueEvents.Liquidate,
@@ -171,11 +186,51 @@ const fetchEvents = async (
 };
 
 
+const fetchPerformanceFees = async (options: FetchOptions): Promise<Array<PerformanceFee>> => {
+  const vaults = await _getWhitelistedVaults();
+
+  const [feeRecipient, underlyingAssets] = await Promise.all(
+    [
+      options.api.call({
+        abi: abi.morphoBlueFunctions.feeRecipient,
+        target: CONFIG.blue,
+      }),
+      options.api.multiCall({
+        abi: abi.metaMorphoFunctions.asset,
+        calls: vaults,
+      }
+      )
+    ])
+
+  return Promise.all(vaults.map(async (v: string, idx: number) => {
+    const sharesAmount = (await options.getLogs({
+      target: v,
+      eventAbi: abi.metaMorphoEvents.Transfer,
+    }))
+    .filter(log => log[0] == ZERO_ADDRESS && log[1] == feeRecipient)
+    .map(log => log[2])
+    .reduce((totalAmount: bigint, value: bigint) => totalAmount + value, 0n);
+
+    const assetAmount = await options.api.call({
+      target: v, 
+      abi: abi.metaMorphoFunctions.convertToAssets,
+      params: [sharesAmount]
+    })
+
+    return {
+      token: underlyingAssets[idx],
+      amount: assetAmount
+    } as PerformanceFee
+  }))
+}
+
 const fetch: FetchV2 = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
   const dailyRevenuesFee = options.createBalances();
 
   const { interests, liquidations } = await fetchEvents(options);
+
+  const performanceFees = await fetchPerformanceFees(options)
 
   for (const event of interests) {
     if (!event.token) continue;
@@ -190,6 +245,10 @@ const fetch: FetchV2 = async (options: FetchOptions) => {
     const repaid = (BigInt(event.seizedAmount) * ONE) / event.lif;
     const bonus = BigInt(event.seizedAmount) - repaid;
     dailyFees.add(event.token, bonus);
+  }
+
+  for (const performanceFee of performanceFees ) {
+    dailyRevenuesFee.add(performanceFee.token, performanceFee.amount)
   }
 
   return {
