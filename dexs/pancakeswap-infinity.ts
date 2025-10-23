216095a838ab9e8e799cc8cb102e6c84ff2fe2d8
Eden <noat.eth@gmail.com>
2025-10-24 01:53:40 +0700

    optimize to reduce mem use

--- dexs/pancakeswap-infinity.ts
+++ dexs/pancakeswap-infinity.ts
@@ -1,3 +1,4 @@
+import * as sdk from "@defillama/sdk";
 import { BaseAdapter, FetchOptions, SimpleAdapter } from "../adapters/types"
 import { CHAIN } from "../helpers/chains"
 import { getDefaultDexTokensBlacklisted } from "../helpers/lists"
@@ -13,7 +14,7 @@ const adapter: SimpleAdapter = {
   adapter: {}
 }
 
-async function fetch({ getLogs, createBalances, chain }: FetchOptions) {
+async function fetch({ getLogs, createBalances, chain, fromApi, toApi }: FetchOptions) {
   const { clPoolManager, fromBlock, blacklistTokens } = config[chain]
   const dailyVolume = createBalances()
   const dailyFees = createBalances()
@@ -25,39 +26,51 @@ async function fetch({ getLogs, createBalances, chain }: FetchOptions) {
     eventAbi: 'event Initialize(bytes32 indexed id, address indexed currency0, address indexed currency1, address hooks, uint24 fee, bytes32 parameters, uint160 sqrtPriceX96, int24 tick)',
   })
 
-  const swapLogs = await getLogs({
-    target: clPoolManager,
-    eventAbi: "event Swap(bytes32 indexed id, address indexed sender, int128 amount0, int128 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint24 fee, uint16 protocolFee)",
-  })
-  const poolMap: any = {}
-  logs.forEach((log: any) => {
-    const { id, currency0, currency1 } = log
-    poolMap[id.toLowerCase()] = {
-      currency0,
-      currency1,
-    }
-  })
-  const BigIntE6 = BigInt(1e6)
-  swapLogs.forEach((log: any) => {
-    const { id, amount0, amount1, protocolFee, fee } = log
-    const pool = poolMap[id.toLowerCase()]
-    if (!pool) {
-      return;
-    }
-    const { currency0, currency1 } = pool
+  // use loop to reduce memory while run adapter
+  const maxBlockRange = 1000
+  const getFromBlock = Number(fromApi.block)
+  const getToBlock = Number(toApi.block)
+  for (let i = getFromBlock; i <= getToBlock; i += maxBlockRange + 1) {
+    const swapLogs = await sdk.getEventLogs({
+      chain: chain,
+      target: clPoolManager,
+      eventAbi: 'event Swap(bytes32 indexed id, address indexed sender, int128 amount0, int128 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint24 fee, uint16 protocolFee)',
+      fromBlock: i,
+      toBlock: i + maxBlockRange,
+      onlyArgs: true,
+    });
 
-    if (blacklistTokens && (blacklistTokens.includes(currency0.toLowerCase()) || blacklistTokens.includes(currency1.toLowerCase()))) {
-      return;
-    }
+    const poolMap: any = {}
+    logs.forEach((log: any) => {
+      const { id, currency0, currency1 } = log
+      poolMap[id.toLowerCase()] = {
+        currency0,
+        currency1,
+      }
+    })
+    const BigIntE6 = BigInt(1e6)
+    swapLogs.forEach((log: any) => {
+      const { id, amount0, amount1, protocolFee, fee } = log
+      const pool = poolMap[id.toLowerCase()]
+      if (!pool) {
+        return;
+      }
+      const { currency0, currency1 } = pool
 
-    const amoun0Fees = (amount0 * BigInt(fee)) / BigIntE6
-    const amoun1Fees = (amount1 * BigInt(fee)) / BigIntE6
-    const amount0ProtocolFees = (amount0 * BigInt(protocolFee)) / BigIntE6
-    const amount1ProtocolFees = (amount1 * BigInt(protocolFee)) / BigIntE6
-    addOneToken({ chain, balances: dailyVolume, token0: currency0, amount0: amount0, token1: currency1, amount1: amount1 })
-    addOneToken({ chain, balances: dailyFees, token0: currency0, amount0: amoun0Fees, token1: currency1, amount1: amoun1Fees })
-    addOneToken({ chain, balances: dailyRevenue, token0: currency0, amount0: amount0ProtocolFees, token1: currency1, amount1: amount1ProtocolFees })
-  })
+      if (blacklistTokens && (blacklistTokens.includes(currency0.toLowerCase()) || blacklistTokens.includes(currency1.toLowerCase()))) {
+        return;
+      }
+
+      const amoun0Fees = (amount0 * BigInt(fee)) / BigIntE6
+      const amoun1Fees = (amount1 * BigInt(fee)) / BigIntE6
+      const amount0ProtocolFees = (amount0 * BigInt(protocolFee)) / BigIntE6
+      const amount1ProtocolFees = (amount1 * BigInt(protocolFee)) / BigIntE6
+      addOneToken({ chain, balances: dailyVolume, token0: currency0, amount0: amount0, token1: currency1, amount1: amount1 })
+      addOneToken({ chain, balances: dailyFees, token0: currency0, amount0: amoun0Fees, token1: currency1, amount1: amoun1Fees })
+      addOneToken({ chain, balances: dailyRevenue, token0: currency0, amount0: amount0ProtocolFees, token1: currency1, amount1: amount1ProtocolFees })
+    })
+  }
+  
   return { dailyVolume, dailyFees, dailyRevenue }
 }
 
