65940034ef86bb29190207688550bf7e7ad7fa1d
jay20972 <jay@mesolabs.io>
2025-10-23 13:58:27 +0800

    fix clmm (#4515)
    * update to v3
    
    * add cetus dlmm
    
    * refactor
    
    * keep aptos chain for historical data
    
    * fix clmm
    
    ---------
    
    Co-authored-by: treeoflife2 <sol.analyzoor@gmail.com>

--- dexs/cetus/index.ts
+++ dexs/cetus/index.ts
@@ -7,9 +7,15 @@ async function fetch(_a: any, _b: any, { startTimestamp, endTimestamp, chain }: 
     return { dailyVolume: 0 }
   }
 
+  let list  = (await fetchURL(`https://api-sui.cetus.zone/v3/sui/clmm/histogram?beginTimestamp=${startTimestamp}&endTimestamp=${endTimestamp}&dateType=hour`)).data.list;
+
+  let dailyVolume = 0;
+  for (const item of list) {
+    dailyVolume += Number(item.value);
+  }
   // const hackDay = (+new Date('2025-05-22')) / 1e3
   // if (endTimestamp > hackDay) return { dailyVolume: 0 }  // dex is paused
-  const dailyVolume = (await fetchURL(`https://api-sui.cetus.zone/v3/sui/vol/time_range?date_type=hour&start_time=${startTimestamp}&end_time=${endTimestamp}`)).data.vol_in_usd;
+  //const dailyVolume = (await fetchURL(`https://api-sui.cetus.zone/v3/sui/vol/time_range?date_type=hour&start_time=${startTimestamp}&end_time=${endTimestamp}`)).data.vol_in_usd;
 
   return { dailyVolume };
 };
