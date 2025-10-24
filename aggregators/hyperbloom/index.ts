9e6f887f62ca88deb18d100d909d538511a47e80
Eden <noat.eth@gmail.com>
2025-10-25 04:25:04 +0700

    refactor codes - count input token

--- aggregators/hyperbloom/index.ts
+++ aggregators/hyperbloom/index.ts
@@ -1,27 +1,34 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
-const TransformedERC20Event =
-  "event TransformedERC20(address indexed taker, address inputToken, address outputToken, uint256 inputTokenAmount, uint256 outputTokenAmount)";
+const TransformedERC20Event = "event TransformedERC20(address indexed taker, address inputToken, address outputToken, uint256 inputTokenAmount, uint256 outputTokenAmount)";
 
 const HYPERBLOOM_ADDRESSES = [
-  "0x4212a77e4533eca49643d7b731f5fb1b2782fe94", //new
-  "0x74cddb25b3f230200b28d79ce85c43991648954a", //old
+  "0x4212a77e4533eca49643d7b731f5fb1b2782fe94", // new
+  "0x74cddb25b3f230200b28d79ce85c43991648954a", // old
 ];
 
 const fetch = async (options: FetchOptions) => {
   const dailyVolume = options.createBalances();
+  const dailyFees = options.createBalances();
 
   const logs: any[] = await options.getLogs({
     targets: HYPERBLOOM_ADDRESSES,
     eventAbi: TransformedERC20Event,
+    flatten: true,
   });
 
-  logs.forEach((log: any) => {
-    dailyVolume.add(log.outputToken, log.outputTokenAmount);
-  });
+  for (const log of logs) {
+    dailyVolume.add(log.inputToken, log.inputTokenAmount);
+    dailyFees.add(log.inputToken, Number(log.inputTokenAmount) * 0.00025); // 0.025%
+  }
 
-  return { dailyVolume };
+  return {
+    dailyVolume,
+    dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
+   };
 };
 
 const adapter: SimpleAdapter = {
@@ -29,7 +36,10 @@ const adapter: SimpleAdapter = {
   fetch,
   start: "2025-05-31",
   methodology: {
-    Volume: "Volume from Hyperbloom",
+    Volume: "Total trading volume aggregated via Hyperbloom routers.",
+    Fees: "Flat 0.025% amount of  trading fees on all trades.",
+    Revenue: "Flat 0.025% amount of  trading fees on all trades are revenue.",
+    ProtocolRevenue: "Flat 0.025% amount of  trading fees on all trades are revenue.",
   },
   chains: [CHAIN.HYPERLIQUID],
 };
