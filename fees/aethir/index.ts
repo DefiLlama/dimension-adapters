0d29cf69a0f0e235395baaa0df6ffe723d284615
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-10-30 12:25:59 +0530

    switch to api call

--- fees/aethir/index.ts
+++ fees/aethir/index.ts
@@ -1,37 +1,21 @@
 import { FetchOptions, FetchResult, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import fetchURL from "../../utils/fetchURL";
 
-const AETHIR_CORE = "0x226DC7D2AA1F9a565e82faf04772FDbBaF2da42d";
-const AETHIR_TOKEN = "0xc87B37a581ec3257B734886d9d3a581F5A9d056c";
+const AETHIR_ENDPOINT = "https://dashboard-api.aethir.com/protocol/demand-metrics/historical-network-revenue";
 
-const DEPOSIT_SERVICE_FEE_EVENT = "event DepositServiceFee (address indexed developer, uint64 nonce, uint256 amount)";
+async function fetch(_a: any, _b: any, options: FetchOptions): Promise<FetchResult> {
+    const today = new Date(options.startOfDay * 1000).toISOString().split('T')[0];
+    const result = (await fetchURL(AETHIR_ENDPOINT)).dailyRevenue;
+    const dailyFees = result.find((entry: any) => entry.date === today).usdValue;
 
-const WITHDRAW_SERVICE_FEE_EVENT = "event WithdrawServiceFee (address indexed developer, uint64 nonce, uint256 amount)";
-
-async function fetch(options: FetchOptions): Promise<FetchResult> {
-    const dailyFees = options.createBalances();
-
-    const serviceFeeDepositLogs = await options.getLogs({
-        eventAbi: DEPOSIT_SERVICE_FEE_EVENT,
-        target: AETHIR_CORE
-    });
-
-    const serviceFeeWithdrawalLogs = await options.getLogs({
-        eventAbi: WITHDRAW_SERVICE_FEE_EVENT,
-        target: AETHIR_CORE
-    });
-
-    serviceFeeDepositLogs.forEach((deposit: any) => dailyFees.add(AETHIR_TOKEN, deposit.amount));
-
-    serviceFeeWithdrawalLogs.forEach((withdraw: any) => dailyFees.subtractToken(AETHIR_TOKEN, withdraw.amount));
-
-    const dailyRevenue = dailyFees.clone(0.2);
+    const dailyRevenue = dailyFees * 0.2;
 
     return {
         dailyFees,
         dailyRevenue,
         dailyProtocolRevenue: dailyRevenue,
-        dailySupplySideRevenue: dailyFees.clone(0.8),
+        dailySupplySideRevenue: dailyFees * 0.8,
     }
 }
 
@@ -44,11 +28,9 @@ const methodology = {
 
 const adapter: SimpleAdapter = {
     fetch,
-    version: 2,
     chains: [CHAIN.ARBITRUM],
     start: '2024-07-22',
     methodology,
-    allowNegativeValue: true //withdrawals could exceed deposits on a particular day
 }
 
 export default adapter;
