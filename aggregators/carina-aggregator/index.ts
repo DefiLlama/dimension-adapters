39512a2aa906534c4f16b31b6611c0bf25ca5be9
Chien Nguyen Minh <125812782+incognito243@users.noreply.github.com>
2025-11-03 20:46:05 +0700

    feat: update carina aggregator (#4576)

---
+++ aggregators/carina-aggregator/index.ts
@@ -0,0 +1,19 @@
+import fetchURL from "../../utils/fetchURL";
+import { CHAIN } from "../../helpers/chains";
+
+const URL = "https://core.carina.finance/v1/orders/volume";
+
+const fetch = async ({ fromTimestamp, toTimestamp }) => {
+  const response = await fetchURL(`${URL}?startTimestamp=${fromTimestamp}&endTimestamp=${toTimestamp}`);
+  return { dailyVolume: response.volume };
+};
+
+export default {
+  version: 2,
+  adapter: {
+    [CHAIN.SEI]: {
+      fetch,
+      start: "2025-11-03",
+    },
+  },
+}
