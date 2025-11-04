7bc64372148956333f7a3406ba9e84c1bfd3060e
Leoni Mella <leoni.mella@gmail.com>
2025-11-04 15:00:59 -0300

    feat: add FlowSwap V3

---
+++ dexs/flowswap-v3.ts
@@ -0,0 +1,8 @@
+import { CHAIN } from '../helpers/chains';
+import { uniV3Exports } from '../helpers/uniswap';
+
+export default uniV3Exports({
+  [CHAIN.FLOW]: {
+    factory: '0xca6d7Bb03334bBf135902e1d919a5feccb461632',
+  },
+});
