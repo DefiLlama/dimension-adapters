4eee457f38793a9d06179ba92b084126990b11a8
Eden <noat.eth@gmail.com>
2025-11-06 21:29:55 +0700

    add ssr and revenue

--- fees/ember-protocol.ts
+++ fees/ember-protocol.ts
@@ -4,29 +4,35 @@ import { CHAIN } from "../helpers/chains";
 
 const ember_fees_url="https://vaults.api.sui-prod.bluefin.io/api/v1/vaults/fees"
 
-const fetch = async ({ startTimestamp, endTimestamp }: FetchOptions): Promise<FetchResultFees> => {
-
-  console.log(startTimestamp, endTimestamp);
-  console.log(`${ember_fees_url}?startTimestampInMs=${startTimestamp*1000}&endTimestampInMs=${endTimestamp*1000}`);
-  const result= await fetchURL(`${ember_fees_url}?startTimestampInMs=${startTimestamp*1000}&endTimestampInMs=${endTimestamp*1000}`);
+const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultFees> => {
+  const result= await fetchURL(`${ember_fees_url}?startTimestampInMs=${options.startTimestamp*1000}&endTimestampInMs=${options.endTimestamp*1000}`);
   const feesUsdE9=result.feesUsdE9;
   const revenueUsdE9=result.revenueUsdE9;
 
+  const fees = Number(feesUsdE9) / 1e9;
+  const revenue = Number(revenueUsdE9) / 1e9;
+  
   return {
-    dailyFees: Number(feesUsdE9)/1e9,
-    dailyRevenue: Number(revenueUsdE9)/1e9,
+    dailyFees: fees,
+    dailyRevenue: revenue,
+    dailyProtocolRevenue: revenue,
+    dailySupplySideRevenue: fees - revenue,
   };
 };
 
 const adapter: SimpleAdapter = {
-  version: 2,
   adapter: {
     [CHAIN.SUI]: {
-        fetch,
-        start: '2025-09-01',
-        runAtCurrTime: true,
-      },
+      fetch,
+      start: '2025-09-01',
+    },
   },
+  methodology: {
+    Fees: 'Total yields collected from Ember protocol vaults.',
+    Revenue: 'Share of yields to Ember protocol.',
+    ProtocolRevenue: 'Share of yields to Ember protocol.',
+    SupplySideRevenue: 'Share of yields to vaults depositors.',
+  }
 };
 
 export default adapter;
