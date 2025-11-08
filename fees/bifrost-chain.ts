e66b597ff51fcc34e68d47387c21a81bb270f8c3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-08 14:07:53 +0100

    track bifrost chain tx fees & devl volume

---
+++ fees/bifrost-chain.ts
@@ -0,0 +1,25 @@
+import { CHAIN } from "../helpers/chains";
+import { FetchOptions } from "../adapters/types";
+import fetchURL from "../utils/fetchURL";
+let res: any;
+
+const fetch = async (_: any, _1: any, options: FetchOptions) => {
+  const startTime = new Date(options.startTimestamp * 1000).toISOString().split("T")[0]
+  if (!res)
+    res = fetchURL('https://dapi.bifrost.io/api/dapp/stats/overview')
+  const v = (await res).find(v => v.date === startTime)
+
+  return { dailyFees: +v.txFee };
+};
+
+
+const adapter: any = {
+  adapter: {
+    [CHAIN.BIFROST]: {
+      fetch,
+      start: '2024-11-08',
+    },
+  },
+};
+
+export default adapter;
