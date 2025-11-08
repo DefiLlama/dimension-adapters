56482a1edf7a0eec4063033b4fa5011edfa94c9c
Eden <noat.eth@gmail.com>
2025-11-09 04:37:19 +0700

    refactor

--- fees/baumz/index.ts
+++ fees/baumz/index.ts
@@ -11,7 +11,15 @@ const methodology = {
 const adapter = getBuilderExports({ broker_id: 'baumz-1024', start: '2025-11-08', methodology }) as SimpleAdapter
 
 adapter.adapter = {
-  [CHAIN.ORDERLY]: { start: '2025-11-08', fetch: async function(_: any, _1: any, options: FetchOptions) { return { ...(await (adapter.fetch as any)(_, _1, options)), dailyHoldersRevenue: 0 } }, },
+  [CHAIN.ORDERLY]: { 
+    start: '2025-11-08',
+    fetch: async function(_: any, _1: any, options: FetchOptions) {
+      return {
+        ...(await (adapter.fetch as any)(_, _1, options)),
+        dailyHoldersRevenue: 0,
+      }
+    },
+  },
 }
 
 export default adapter
