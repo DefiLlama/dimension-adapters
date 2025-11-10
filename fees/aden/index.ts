15f63bf0b30658b9b5d4226c80cb8c9b547ba7b5
silence-fe <silence-fe@gate.me>
2025-11-10 22:14:46 +0800

    feat: add gate for aden

--- fees/aden/index.ts
+++ fees/aden/index.ts
@@ -4,54 +4,139 @@ import { getBuilderExports } from "../../helpers/orderly";
 import { httpGet } from "../../utils/fetchURL";
 
 const VOLUME_ENDPOINT_MAP = {
-  [CHAIN.OFF_CHAIN]: 'https://fapi.asterdex.com/fapi/v1/statisticsData/adenTradingInfo?period=DAILy',
-  [CHAIN.GATE]: 'https://mock-server-beta-nine.vercel.app/list',
-}
+  [CHAIN.OFF_CHAIN]:
+    "https://fapi.asterdex.com/fapi/v1/statisticsData/adenTradingInfo?period=DAILy",
+  [CHAIN.GATE]:
+    "https://api.gateperps.com/api/v4/dex_futures/usdt/contract_stats/defillama",
+};
 
-const asterBuilderDataMap: Map<string, Promise<any>> = new Map()
+const asterBuilderDataMap: Map<string, Promise<any>> = new Map();
 
-async function commonFetch(type: keyof typeof VOLUME_ENDPOINT_MAP, _: any, _1: any, { dateString }: FetchOptions) {
-  const asterVolumeEndpoint = VOLUME_ENDPOINT_MAP[type];
-  
-  if (!asterBuilderDataMap.has(asterVolumeEndpoint)) {
+/**
+ * Fetch data for CHAIN.GATE
+ * This endpoint requires a date parameter to request data for a single date
+ */
+async function fetchGateData(
+  endpoint: string,
+  dateString: string
+): Promise<any> {
+  const endpointWithDate = `${endpoint}?date=${dateString}`;
+  const cacheKey = `${endpoint}_${dateString}`;
+
+  if (!asterBuilderDataMap.has(cacheKey)) {
     asterBuilderDataMap.set(
-      asterVolumeEndpoint,
-      httpGet(asterVolumeEndpoint).then(({ perps: data }) => {
-        const dateDataMap: any = {}
-        data.forEach((i: any) => {
-          dateDataMap[i.dateString] = i
-        })
-        return dateDataMap
-      })
-    )
+      cacheKey,
+      httpGet(endpointWithDate).then((res: any) => res)
+    );
   }
 
-  const asterBuilderData = asterBuilderDataMap.get(asterVolumeEndpoint)!
-  const data = (await asterBuilderData)[dateString]
+  const data = await asterBuilderDataMap.get(cacheKey)!;
+  if (!data) {
+    throw new Error("Data missing for date: " + dateString);
+  }
 
-  if (!data)
-    throw new Error('Data missing for date: ' + dateString)
+  return {
+    dailyVolume: data.volume,
+    dailyFees: data.fees,
+    dailyRevenue: data.fees,
+    dailyProtocolRevenue: data.fees,
+    dailyHoldersRevenue: 0,
+  };
+}
 
-  const dailyVolume = +data.takerVolume + +data.makerVolume
-  const dailyFees = +data.builderFee
+/**
+ * Fetch data for CHAIN.OFF_CHAIN
+ * This endpoint returns data for multiple dates at once, need to find the specified date from the returned data
+ */
+async function fetchOffChainData(
+  endpoint: string,
+  dateString: string
+): Promise<any> {
+  if (!asterBuilderDataMap.has(endpoint)) {
+    asterBuilderDataMap.set(
+      endpoint,
+      httpGet(endpoint).then((res: any) => {
+        const dateDataMap: any = {};
+        if (res.perps) {
+          res.perps.forEach((item: any) => {
+            dateDataMap[item.dateString] = item;
+          });
+        }
+        return dateDataMap;
+      })
+    );
+  }
 
-  const response: any = { dailyVolume, dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, dailyHoldersRevenue: 0 }
+  const dateDataMap = await asterBuilderDataMap.get(endpoint)!;
+  const data = dateDataMap[dateString];
 
-  return response
+  if (!data) {
+    throw new Error("Data missing for date: " + dateString);
+  }
+
+  return {
+    dailyVolume: +data.takerVolume + +data.makerVolume,
+    dailyFees: +data.builderFee,
+    dailyRevenue: +data.builderFee,
+    dailyProtocolRevenue: +data.builderFee,
+    dailyHoldersRevenue: 0,
+  };
+}
+
+/**
+ * Unified data fetching entry point, routes to the appropriate handler function based on type
+ */
+async function commonFetch(
+  type: keyof typeof VOLUME_ENDPOINT_MAP,
+  _: any,
+  _1: any,
+  { dateString }: FetchOptions
+) {
+  const endpoint = VOLUME_ENDPOINT_MAP[type];
+
+  if (type === CHAIN.GATE) {
+    return fetchGateData(endpoint, dateString);
+  }
+
+  if (type === CHAIN.OFF_CHAIN) {
+    return fetchOffChainData(endpoint, dateString);
+  }
+
+  throw new Error(`Unsupported chain type: ${type}`);
 }
 
 const methodology = {
   Fees: "Builder Fees collected from Orderly Network(0.4 bps on taker volume) and Aster Exchange(0.4 bps on taker volume)",
   Revenue: "All the fees collected",
   ProtocolRevenue: "All the revenue go to the protocol",
-}
+};
 
-const adapter = getBuilderExports({ broker_id: 'aden', start: '2025-07-14', methodology }) as SimpleAdapter
+const adapter = getBuilderExports({
+  broker_id: "aden",
+  start: "2025-07-14",
+  methodology,
+}) as SimpleAdapter;
 
 adapter.adapter = {
-  [CHAIN.ORDERLY]: { start: '2025-07-14', fetch: async function(_: any, _1: any, options: FetchOptions) { return { ...(await (adapter.fetch as any)(_, _1, options)), dailyHoldersRevenue: 0 } }, },
-  [CHAIN.OFF_CHAIN]: { start: '2025-07-19', fetch: (_: any, _1: any, options: FetchOptions) => commonFetch(CHAIN.OFF_CHAIN, _, _1, options) },
-  [CHAIN.GATE]: { start: '2025-10-23', fetch: (_: any, _1: any, options: FetchOptions) => commonFetch(CHAIN.GATE, _, _1, options) },
-}
-  
-export default adapter
+  [CHAIN.ORDERLY]: {
+    start: "2025-07-14",
+    fetch: async function (_: any, _1: any, options: FetchOptions) {
+      return {
+        ...(await (adapter.fetch as any)(_, _1, options)),
+        dailyHoldersRevenue: 0,
+      };
+    },
+  },
+  [CHAIN.OFF_CHAIN]: {
+    start: "2025-07-19",
+    fetch: (_: any, _1: any, options: FetchOptions) =>
+      commonFetch(CHAIN.OFF_CHAIN, _, _1, options),
+  },
+  [CHAIN.GATE]: {
+    start: "2025-11-01",
+    fetch: (_: any, _1: any, options: FetchOptions) =>
+      commonFetch(CHAIN.GATE, _, _1, options),
+  },
+};
+
+export default adapter;
