54bf81c76ca6b80e7986c35f0d14ae1e1fbd8468
jane930284 <jerry919119@gmail.com>
2025-11-12 14:44:23 +0900

    Create fees/pandora-speed-trading/index.ts

---
+++ fees/pandora-speed-trading/index.ts
@@ -0,0 +1,43 @@
+
+import { Balances } from "@defillama/sdk";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+
+const usdcAddress = '0x858f0a7454462de49a135d9b63da941eea6f5899'
+const OpenEvent = "event TradeOpened(uint256 tradeId, address trader, uint256 entryPrice, int256 positionSize, uint256 openFee, uint256 executionFee)";
+const CloseEvent = "event TradeClosed(uint256 tradeId, address trader, uint256 exitPrice, int256 pnl, uint256 closeFee, uint256 executionFee)";
+
+const PositionContract = '0x05eE95faFe92Af6EA619514E07C90844071c6a7d';
+
+const fetch = async (options: FetchOptions) => {
+  
+  const dailyFees = options.createBalances();
+  const openData: any[] = await options.getLogs({
+    target: PositionContract,
+    eventAbi: OpenEvent,
+  });
+
+  openData.forEach((log: any) => {
+    dailyFees.add(usdcAddress, log.executionFee);
+    dailyFees.add(usdcAddress, log.openFee);
+  });
+
+  const closeData: any[] = await options.getLogs({
+    target: PositionContract,
+    eventAbi: CloseEvent,
+  });
+
+  closeData.forEach((log: any) => {
+    dailyFees.add(usdcAddress, log.closeFee);
+  });
+  return { dailyFees };
+};
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  fetch,
+  chains: [CHAIN.ABSTRACT],
+  start: '2025-11-01',
+}
+
+export default adapter;
