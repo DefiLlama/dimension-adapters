025fc7fa64e983925d70a033030755d7e48b69d3
silence-fe <silence-fe@gate.me>
2025-11-13 12:53:01 +0800

    feat: Add GATE_LAYER for aden

--- fees/aden/index.ts
+++ fees/aden/index.ts
@@ -1,43 +1,46 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getBuilderExports } from "../../helpers/orderly";
 import { httpGet } from "../../utils/fetchURL";
 
-let asterBuilderData: any = null
-async function asterFetch(_: any, _1: any, { dateString }: FetchOptions) {
-  const asterVolumeEndpoint = "https://fapi.asterdex.com/fapi/v1/statisticsData/adenTradingInfo?period=DAILy";
-  if (!asterBuilderData) asterBuilderData = httpGet(asterVolumeEndpoint).then(({ perps: data }) => {
-    const dateDataMap: any = {}
-    data.forEach((i: any) => {
-      dateDataMap[i.dateString] = i
-    })
-    return dateDataMap
-  })
+/**
+ * Fetch data for CHAIN.GATE_LAYER
+ * This endpoint requires a date parameter to request data for a single date
+ */
+async function fetchGateData(dateString: string): Promise<any> {
+  const endpointWithDate = `https://api.gateperps.com/api/v4/dex_futures/usdt/contract_stats/defillama?date=${dateString}`;
 
-  const data = (await asterBuilderData)[dateString]
+  const data = await httpGet(endpointWithDate);
 
-  if (!data)
-    throw new Error('Data missing for date: ' + dateString)
+  if (!data) {
+    throw new Error("Data missing for date: " + dateString);
+  }
 
-  const dailyVolume = +data.takerVolume + +data.makerVolume
-  const dailyFees = +data.builderFee
-
-  const response: any = { dailyVolume, dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, dailyHoldersRevenue: 0 }
-
-  return response
+  return {
+    dailyVolume: data.volume,
+    dailyFees: data.fees,
+    dailyRevenue: data.fees,
+    dailyProtocolRevenue: data.fees,
+    dailyHoldersRevenue: 0,
+  };
 }
 
 const methodology = {
-  Fees: "Builder Fees collected from Orderly Network(0.4 bps on taker volume) and Aster Exchange(0.4 bps on taker volume)",
+  Fees: "Builder Fees collected from Gate Layer Network(0.4 bps on taker volume)",
   Revenue: "All the fees collected",
   ProtocolRevenue: "All the revenue go to the protocol",
-}
+};
 
-const adapter = getBuilderExports({ broker_id: 'aden', start: '2025-07-14', methodology }) as SimpleAdapter
+const adapter: SimpleAdapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.GATE_LAYER]: {
+      start: "2025-10-15",
+      fetch: async (_: any, _1: any, { dateString }: FetchOptions) => {
+        return fetchGateData(dateString);
+      },
+    },
+  },
+  methodology,
+};
 
-adapter.adapter = {
-  [CHAIN.ORDERLY]: { start: '2025-07-14', fetch: async function(_: any, _1: any, options: FetchOptions) { return { ...(await (adapter.fetch as any)(_, _1, options)), dailyHoldersRevenue: 0 } }, },
-  [CHAIN.OFF_CHAIN]: { start: '2025-07-19', fetch: asterFetch },
-}
-
-export default adapter
+export default adapter;
