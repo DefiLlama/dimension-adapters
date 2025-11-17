d803ca8e9356e862cdf366f39f64c570ddeb99ee
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-11-17 10:25:26 +0530

    fix galaswap volume

--- dexs/galaswap-v3.ts
+++ dexs/galaswap-v3.ts
@@ -1,14 +1,39 @@
 import { FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { httpGet } from "../utils/fetchURL";
+import fetchURL from "../utils/fetchURL";
 
-async function fetch(_: any, _2: any, { dateString }: FetchOptions) {
-  const { volumeUSD: dailyVolume, feesUSD: dailyFees } = await httpGet('https://dex-backend-prod1.defi.gala.com/dex/pairs/volume-fees?date=' + dateString);
-  return { dailyVolume, dailyFees }
+const GALA_SWAP_API = "https://dex-backend-prod1.defi.gala.com/explore/pools?limit=20";
+
+async function fetch(_: any, _2: any, _3: FetchOptions) {
+  const { count } = (await fetchURL(`${GALA_SWAP_API}&page=1`)).data;
+  const totalPages = Math.ceil(count / 20);
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const poolData = (await Promise.all(pages.map((page: number) =>
+    fetchURL(`${GALA_SWAP_API}&page=${page}`)
+  )));
+
+  const data = poolData.flatMap(pages => pages.data.pools).reduce((acc: { volume: number, fees: number }, { fee24h, volume1d }: { fee24h: number, volume1d: number }) => {
+    acc.fees += fee24h;
+    acc.volume += volume1d;
+    return acc;
+  }, { volume: 0, fees: 0 });
+
+  return {
+    dailyVolume: data.volume,
+    dailyFees: data.fees,
+    dailySupplySideRevenue: data.fees
+  }
 }
+
+const methodology = {
+  Fees: "Swap fees paid by users",
+  Volume: "Galaswap trade volume",
+  SupplySideRevenue: "All the fees goes to liquidity providers"
+};
 
 export default {
   fetch,
   start: '2025-09-03',
   chains: [CHAIN.GALA],
+  methodology,
 }
