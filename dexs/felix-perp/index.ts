ebd801a33edae899049df863deff46d3d4839ee5
eden <noat.eth@gmail.com>
2025-11-18 16:08:09 +0700

    track hl HIP-3 deployers (#4688)

---
+++ dexs/felix-perp/index.ts
@@ -0,0 +1,29 @@
+import { FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { fetchHIP3DeployerData } from "../../helpers/hyperliquid";
+
+async function fetch(_1: number, _: any,  options: FetchOptions): Promise<FetchResultV2> {
+  const result = await fetchHIP3DeployerData({ options, hip3DeployerId: 'flx' });
+  
+  return {
+    dailyVolume: result.dailyPerpVolume,
+    dailyFees: result.dailyPerpFee,
+    dailyRevenue: result.dailyPerpFee,
+  }
+}
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.HYPERLIQUID]: {
+      fetch,
+      start: '2025-11-13',
+    },
+  },
+  doublecounted: true,
+  methodology: {
+    Fees: 'Trading fees paid by users on Hyperliquid markets deployed by Felix protocol.',
+    Revenue: 'Trading fees paid by users on Hyperliquid markets deployed by Felix protocol.',
+  }
+};
+
+export default adapter;
