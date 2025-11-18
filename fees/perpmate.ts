1f6257c6fd4142e33bd7c6863e17708c4518e727
dev-uri <uri@rollspace.com>
2025-11-18 23:02:33 +0300

    Update builder address

--- fees/perpmate.ts
+++ fees/perpmate.ts
@@ -1,33 +1,36 @@
-import { CHAIN } from '../helpers/chains'
-import { FetchOptions, SimpleAdapter } from '../adapters/types'
-import { fetchBuilderCodeRevenue } from '../helpers/hyperliquid'
+import { CHAIN } from "../helpers/chains";
+import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { fetchBuilderCodeRevenue } from "../helpers/hyperliquid";
 
-const BUILDER_CODE_ADDRESS = "0xE4FEa748ECa48F44b1e042775F0C2363be1A2d80";
+const PERPMATE_BUILDER_ADDRESS = "0xe4fea748eca48f44b1e042775f0c2363be1a2d80";
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-  const { dailyVolume, dailyFees, dailyRevenue, dailyProtocolRevenue } = await fetchBuilderCodeRevenue({ options, builder_address: BUILDER_CODE_ADDRESS });
+  const { dailyVolume, dailyFees, dailyRevenue, dailyProtocolRevenue } =
+    await fetchBuilderCodeRevenue({
+      options,
+      builder_address: PERPMATE_BUILDER_ADDRESS,
+    });
 
   return {
     dailyVolume,
     dailyFees,
     dailyRevenue,
     dailyProtocolRevenue,
-  }
-}
+  };
+};
 
 const methodology = {
-  Fees: 'builder code revenue from Hyperliquid Perps Trades.',
-  Revenue: 'builder code revenue from Hyperliquid Perps Trades.',
-  ProtocolRevenue: 'builder code revenue from Hyperliquid Perps Trades.',
-}
+  Fees: "builder code revenue from Hyperliquid Perps Trades.",
+  Revenue: "builder code revenue from Hyperliquid Perps Trades.",
+  ProtocolRevenue: "builder code revenue from Hyperliquid Perps Trades.",
+};
 
 const adapter: SimpleAdapter = {
   fetch,
   chains: [CHAIN.HYPERLIQUID],
-  start: '2025-09-04',
+  start: "2025-09-04",
   methodology,
   doublecounted: true,
-}
+};
 
 export default adapter;
-
