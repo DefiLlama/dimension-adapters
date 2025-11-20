ac0866de947cbbc20de8e9f7e5711799a257b3cb
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-11-20 11:46:28 -0800

    Honeypot finance (#4698)
    * add honeypot-finance
    
    * return volume and fees
    
    ---------
    
    Co-authored-by: Eden <noat.eth@gmail.com>

---
+++ fees/honeypot-finance/index.ts
@@ -0,0 +1,26 @@
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getBuilderExports } from "../../helpers/orderly";
+
+const methodology = {
+  Fees: "Builder Fees collected from Orderly Network",
+  Revenue: "All the fees collected",
+  ProtocolRevenue: "All the revenue goes to the protocol",
+};
+
+const adapter = getBuilderExports({
+  broker_id: "honeypot",
+  start: "2025-11-01",
+  methodology,
+}) as SimpleAdapter;
+
+adapter.adapter = {
+  [CHAIN.ORDERLY]: {
+    start: "2025-11-01",
+    fetch: async function (_: any, _1: any, options: FetchOptions) {
+      return await (adapter.fetch as any)(_, _1, options);
+    },
+  },
+};
+
+export default adapter;
