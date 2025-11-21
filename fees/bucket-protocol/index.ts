f49b963b8d359a901b8651ad1e351f92eddbf722
allenhsu <allenhsu.taiwan@gmail.com>
2025-11-21 11:55:14 +0800

    feat: modify the bucketprotocol fee

--- fees/bucket-protocol/index.ts
+++ fees/bucket-protocol/index.ts
@@ -1,20 +1,13 @@
-import { Adapter, FetchOptions, } from "../../adapters/types"
-import { CHAIN } from "../../helpers/chains"
-import fetchURL from "../../utils/fetchURL"
+import { Adapter, FetchOptions } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import fetchURL from "../../utils/fetchURL";
 
-const bucketApiURL = "https://open-api.bucketprotocol.io/api/"
-
+const bucketApiURL = "https://backend.bucketprotocol.io/api";
 interface DailyStats {
-  date: string
-  total_records: number
-  total_fee_value: string
-  average_fee_value: string
-  min_fee_value: string
-  max_fee_value: string
-  first_record_time: string
-  last_record_time: string
-  first_record_time_ms: number
-  last_record_time_ms: number
+  date: string;
+  dailyFee: string;
+  startTimestampMs: number;
+  endTimestampMs: number;
 }
 
 const methodology = {
@@ -22,31 +15,37 @@ const methodology = {
     "All the services fees paid by users, including borrow, PSM, liquidation, redeem, flashLoan and interest",
   dailyRevenue:
     "All the services fees paid by users, including borrow, PSM, liquidation, redeem, flashLoan and interest earned by Bucket",
-}
+};
 
-const fetchBucketStats = async (_: any, _1: any, { startTimestamp, }: FetchOptions) => {
-  const url = `${bucketApiURL}fees?timestamp_ms=${startTimestamp * 1000}`
-  const stats: DailyStats = (await fetchURL(url)).data
+const fetchBucketStats = async (
+  _: any,
+  _1: any,
+  { startTimestamp }: FetchOptions
+) => {
+  const url = `${bucketApiURL}/fee/dailystatus?timestamp_ms=${
+    startTimestamp * 1000
+  }`;
+  const stats: DailyStats = (await fetchURL(url)).data;
 
-  const dailyFees = stats.total_fee_value
-  const dailyRevenue = stats.total_fee_value
+  const dailyFees = stats.dailyFee;
+  const dailyRevenue = stats.dailyFee;
 
   return {
     dailyFees,
     dailyUserFees: dailyFees,
     dailyRevenue,
     dailyProtocolRevenue: dailyRevenue,
-  }
-}
+  };
+};
 
 const adapter: Adapter = {
   methodology,
   adapter: {
     [CHAIN.SUI]: {
       fetch: fetchBucketStats,
-      start: "2024-02-29",
+      start: "2025-09-01",
     },
   },
-}
+};
 
-export default adapter
+export default adapter;
