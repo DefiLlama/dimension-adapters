f03b2ff5bd666fd2f704f6a20d86affec45f8e03
Eden <noat.eth@gmail.com>
2025-11-22 04:34:23 +0700

    count rerferral fees for ssr

--- aggregators/kuru/index.ts
+++ aggregators/kuru/index.ts
@@ -1,18 +1,18 @@
 import { FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import * as sdk from "@defillama/sdk";
 
 const abis = {
   "KuruFlowSwap": "event KuruFlowSwap(address indexed user,address indexed referrer,address tokenIn,address tokenOut,bool isFeeInInput,uint256 amountIn,uint256 amountOut,uint256 referrerFeeBps,uint256 totalFeeBps)",
   "FeeCollected": "FeeCollected(address feeCollector, uint256 amount, address referrer, uint256 referrerAmount, address user, address token)",
 }
 
+const KuruFlowEntrypoint = '0xb3e6778480b2E488385E8205eA05E20060B813cb'
+  
 const fetch = async (_:any, _1: any, { createBalances, getLogs, }: FetchOptions) => {
   const dailyVolume = createBalances()
   const dailyFees = createBalances()
-  const KuruFlowEntrypoint = '0xb3e6778480b2E488385E8205eA05E20060B813cb'
-
-  sdk.log('[KURU] Fetching swap events...');
+  const dailyRevenue = createBalances()
+  const dailySupplySideRevenue = createBalances()
 
   // Get swap events for volume
   const swapLogs = await getLogs({ 
@@ -32,30 +32,35 @@ const fetch = async (_:any, _1: any, { createBalances, getLogs, }: FetchOptions)
   
   // Calculate total fees = amount + referrerAmount
   feeLogs.forEach((log: any) => {
-    const totalFee = BigInt(log.amount) + BigInt(log.referrerAmount)
-    dailyFees.add(log.token, totalFee)
+    dailyFees.add(log.token, log.amount)
+    dailyFees.add(log.token, log.referrerAmount)
+    
+    dailyRevenue.add(log.token, log.amount)
+    dailySupplySideRevenue.add(log.token, log.referrerAmount)
   })
 
   return { 
     dailyVolume,
     dailyFees,
     dailyUserFees: dailyFees,
-    dailyRevenue: dailyFees,
-    dailyProtocolRevenue: dailyFees,
+    dailyRevenue,
+    dailyProtocolRevenue: dailyRevenue,
+    dailySupplySideRevenue,
   }
 };
 
 const adapter: any = {
   fetch,
   adapter: {
-    [CHAIN.MONAD]: { start: '2025-11-17', },
+    [CHAIN.MONAD]: { start: '2025-11-17' },
   },
   methodology: {
     Volume: "Sum of all token swaps routed through KURU Aggregator",
     Fees: "Total fees collected from users (protocol fee + referrer fee)",
     UserFees: "All fees are paid by users",
-    Revenue: "All fees collected go to protocol and referrers",
-    ProtocolRevenue: "All fees collected by protocol (includes referrer portion)",
+    Revenue: "All fees collected go to protocol.",
+    ProtocolRevenue: "All fees collected by protocol (exclude referrer portion).",
+    SupplySideRevenue: "All fees collected by referrers.",
   }
 };
 
