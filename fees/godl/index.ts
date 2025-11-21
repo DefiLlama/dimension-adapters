e3a7ee1838f5ab30f273c89ee8e8df3618d764bf
Eden <noat.eth@gmail.com>
2025-11-22 04:11:56 +0700

    use dune query

--- fees/godl/index.ts
+++ fees/godl/index.ts
@@ -4,10 +4,27 @@ import {
   SimpleAdapter,
 } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getSolanaReceivedDune } from "../../helpers/token";
+import { queryDuneSql } from "../../helpers/dune";
 
 const fetch: any = async (_a: any, _b: any, options: FetchOptions) => {
-  const dailyFees = await getSolanaReceivedDune({options, target: '5epGzdW6veQwLQiQs1L45uUQ8jdSLQHWL8RbC7uTWVY3'})
+  const dailyFees = options.createBalances()
+  
+  const duneQueryString = `
+    SELECT
+      SUM(CASE WHEN post_balance > pre_balance THEN (post_balance - pre_balance) / 1e9 ELSE 0 END) AS total_sol_inbound
+    FROM solana.account_activity
+    WHERE
+      address = '5epGzdW6veQwLQiQs1L45uUQ8jdSLQHWL8RbC7uTWVY3'
+      AND block_time >= from_unixtime(${options.startTimestamp})
+      AND block_time < from_unixtime(${options.endTimestamp})
+      AND tx_success = true
+  `;
+
+  const results = await queryDuneSql(options, duneQueryString);
+  if (results.length > 0) {
+    const revenue = results[0].total_sol_inbound || 0;
+    dailyFees.addCGToken("solana",revenue);
+  }
 
   const dailyProtocolRevenue = dailyFees.clone(0.01);
   const dailyHoldersRevenue = dailyFees.clone(0.99);
