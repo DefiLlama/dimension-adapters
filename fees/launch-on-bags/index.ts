52f3c24389a76ff9c74a252e62b3b5139d03624d
Eden <noat.eth@gmail.com>
2025-11-22 07:01:41 +0700

    add referrals fees to ssr

--- fees/launch-on-bags/index.ts
+++ fees/launch-on-bags/index.ts
@@ -20,12 +20,14 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     const data: IData[] = await queryDuneSql(options, query)
 
     const dailyFees = options.createBalances();
+    const dailySupplySideRevenue = options.createBalances();
     const dailyProtocolRevenue = options.createBalances();
 
     data.forEach(row => {
         const totalFees = Number(row.total_protocol_fees) + Number(row.total_referral_fees) + Number(row.total_trading_fees);
         dailyFees.add(row.quote_mint, Number(totalFees));
         dailyProtocolRevenue.add(row.quote_mint, Number(row.total_trading_fees));
+        dailySupplySideRevenue.add(row.quote_mint, Number(row.total_referral_fees));
     });
 
     return {
@@ -33,6 +35,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
         dailyUserFees: dailyFees,
         dailyRevenue: dailyProtocolRevenue,
         dailyProtocolRevenue,
+        dailySupplySideRevenue,
     };
 };
 
@@ -47,7 +50,8 @@ const adapter: SimpleAdapter = {
     methodology: {
         Fees: "Trading fees paid by users.",
         Revenue: "Fees collected by Bags App protocol.",
-        ProtocolRevenue: "Fees collected by Bags App protocol."
+        ProtocolRevenue: "Fees collected by Bags App protocol.",
+        SupplySideRevenue: 'Fees collected by referrals',
     },
 }
 
