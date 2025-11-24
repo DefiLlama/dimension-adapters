dd818d844b9cd0640867852946c3244c49e13bb4
Eden <noat.eth@gmail.com>
2025-11-24 21:18:11 +0700

    track monad launch

--- dexs/curve/index.ts
+++ dexs/curve/index.ts
@@ -431,6 +431,19 @@ const CurveDexConfigs: {[key: string]: ICurveDexConfig} = {
     ],
     customPools: {},
   },
+  [CHAIN.MONAD]: {
+    start: '2025-11-23',
+    factory_stable_ng: [
+      '0x8271e06E5887FE5ba05234f5315c19f3Ec90E8aD',
+    ],
+    factory_twocrypto: [
+      '0xe7FBd704B938cB8fe26313C3464D4b7B7348c88C',
+    ],
+    factory_tricrypto: [
+      '0x6E28493348446503db04A49621d8e6C9A40015FB',
+    ],
+    customPools: {},
+  },
 
   // [CHAIN.TAC]: {
   //   start: '2025-06-25',
