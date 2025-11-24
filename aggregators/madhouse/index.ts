de9ef66eadf571a2429bec543c0a8a6621f67a7e
Eden <noat.eth@gmail.com>
2025-11-24 20:39:33 +0700

    using event logs

--- aggregators/madhouse/index.ts
+++ aggregators/madhouse/index.ts
@@ -1,14 +1,21 @@
 import { CHAIN } from "../../helpers/chains";
 import { FetchOptions } from "../../adapters/types";
-import fetchURL from "../../utils/fetchURL";
+
+const CONTRACTS: Record<string, string> = {
+  [CHAIN.MONAD]: '0x6017684Bea9Cb6e9874fC6DBA4438271eBF9F5DA'
+}
 
 const fetch = async (options: FetchOptions) => {
-  const data = await fetchURL(`https://stats-api.madhouse.ag/volume?chainId=143`);
-
-  // Data format: { "2025-11-24": 60.32 }
-  // Get the date string for the current day
-  const dateStr = new Date(options.startOfDay * 1000).toISOString().split('T')[0];
-  const dailyVolume = data?.[dateStr] || 0;
+  const dailyVolume = options.createBalances()
+  
+  const swapLogs = await options.getLogs({
+    target: CONTRACTS[options.chain],
+    eventAbi: 'event SwapExecuted(address tokenIn, uint256 amountIn, address tokenOut, uint256 amountOut)',
+  })
+  
+  for (const log of swapLogs) {
+    dailyVolume.add(log.tokenIn, log.amountIn);
+  }
 
   return {
     dailyVolume,
