0c5f0e4b536c87312475628be12d1517dcf3afe3
James Jia <jia.james1@gmail.com>
2025-11-26 15:48:09 -0500

    Add adapter for capricorn (#4759)

---
+++ dexs/capricorn/index.ts
@@ -0,0 +1,8 @@
+import { CHAIN } from "../../helpers/chains";
+import { uniV3Exports } from "../../helpers/uniswap";
+
+export default uniV3Exports({
+  [CHAIN.MONAD]: {
+    factory: "0x6B5F564339DbAD6b780249827f2198a841FEB7F3",
+  },
+});
