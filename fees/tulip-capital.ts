da08fc513d68d3a715fb70dd72d1aa2ed63d6ed7
TadejPolajnar <tadej.polajnar@gmail.com>
2025-11-27 12:11:45 +0800

    chore: Use constants as chain name in tulip capital config

--- fees/tulip-capital.ts
+++ fees/tulip-capital.ts
@@ -1,8 +1,9 @@
+import { CHAIN } from "../helpers/chains";
 import { CuratorConfig, getCuratorExport } from "../helpers/curators";
 
 const curatorConfig: CuratorConfig = {
   vaults: {
-    ethereum: {
+    [CHAIN.ETHEREUM]: {
       morphoVaultOwners: [
         '0x59e608E4842162480591032f3c8b0aE55C98d104',
       ],
@@ -14,28 +15,28 @@ const curatorConfig: CuratorConfig = {
         "0xb09f761cb13baca8ec087ac476647361b6314f98", // Flagship cbBTC
       ],
     },
-    berachain: {
+    [CHAIN.BERACHAIN]: {
       eulerVaultOwners: [
         '0x18d23B961b11079EcD499c0EAD8E4F347e4d3A66',
       ],
     },
-    bob: {
+    [CHAIN.BOB]: {
       eulerVaultOwners: [
         '0x7c615e12D1163fc0DdDAA01B51922587034F5C93',
       ],
     },
-    bsc: {
+    [CHAIN.BSC]: {
       eulerVaultOwners: [
         '0x7c615e12D1163fc0DdDAA01B51922587034F5C93',
       ],
     },
-    avax: {
+    [CHAIN.AVAX]: {
       lagoon: [
         "0x3048925b3ea5a8c12eecccb8810f5f7544db54af", // Turtle Avalanche USDC
         "0xb893c8d7000e0408eb7d168152ec7fefdd0d25e3", // Turtle Avalanche BTC.b
       ],
     },
-    tac: {
+    [CHAIN.TAC]: {
       lagoon:[
         '0x279385c180f5d01c4a4bdff040f17b8957304762' // Noon USN TAC
       ]
