25a2d4d457128cbfd395f932173f5f56f1de78e8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-28 18:09:44 +0100

    update dydx v4 endpoint

--- fees/dydx-v4/index.ts
+++ fees/dydx-v4/index.ts
@@ -5,20 +5,18 @@ interface IFees {
   day: string
   sum_tradingfeecollection: string
 }
-const fetchFees  = async (timestamp: number, _t: ChainBlocks ,options: FetchOptions) => {
-  const url = 'https://api.lacertalabs.xyz/data/tradingfeecollection'
-  const dateStr = new Date(options.startOfDay * 1000).toISOString().split('T')[0]
+const fetchFees  = async (_: number, _t: ChainBlocks ,options: FetchOptions) => {
+  const url = 'https://public-dydx-api.numia.xyz/dydx/transparency/trading-fees'
   const res = await httpGet(url)
   delete res['latestTen']
   const item: IFees[] = Object.values(res)
-  const dailyFees = item.find((i) => i.day.split('T')[0] === dateStr)?.sum_tradingfeecollection
+  const dailyFees = item.find((i) => i.day.split(' ')[0] === options.dateString)?.sum_tradingfeecollection
   const dailyFeesNum = dailyFees ? parseFloat(dailyFees) : undefined
 
   return {
     dailyFees: dailyFeesNum,
     dailyRevenue: dailyFeesNum,
     dailyHoldersRevenue: dailyFeesNum,
-    timestamp: timestamp,
   }
 }
 
