da2d236e0582adb0ffdabc8cb0b559906360c7dd
Sudzpel <114531512+Sudzpel@users.noreply.github.com>
2025-12-02 15:58:48 +0300

    add octoswap dimension adapters (#4778)
    * add OctoSwap adapters
    
    * track revenue
    
    ---------
    
    Co-authored-by: Eden <noat.eth@gmail.com>

---
+++ dexs/octoswap-classic.ts
@@ -0,0 +1,10 @@
+import { uniV2Exports } from "../helpers/uniswap";
+
+// https://docs.octo.exchange/resources-faq#6-what-are-the-trading-fees
+export default uniV2Exports({ 
+  monad: {
+    factory: '0xCe104732685B9D7b2F07A09d828F6b19786cdA32',
+    revenueRatio: 1/3,
+    protocolRevenueRatio: 1/3,
+  }
+})
