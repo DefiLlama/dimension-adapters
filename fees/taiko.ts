7a4c20cc648a856a6761f151b55444f31270f8ba
treeoflife2 <sol.analyzoor@gmail.com>
2025-12-04 15:24:13 +0530

    taiko refactor

--- fees/taiko.ts
+++ fees/taiko.ts
@@ -1,4 +1,21 @@
+import { FetchOptions, ProtocolType, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { etherscanFeeAdapter } from "../helpers/etherscanFees";
+import { getEtherscanFees } from "../helpers/etherscanFees";
 
-export default etherscanFeeAdapter(CHAIN.TAIKO, "https://taikoscan.io/chart/transactionfee?output=csv", "ethereum");
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+  const amount = await getEtherscanFees(options, 'https://taikoscan.io/chart/transactionfee?output=csv')
+  const dailyFees = options.createBalances()
+  dailyFees.addCGToken('ethereum', amount / 1e18)
+
+  return { dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees };
+}
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  fetch,
+  chains: [CHAIN.TAIKO],
+  start: '2024-05-25',
+  protocolType: ProtocolType.CHAIN,
+}
+
+export default adapter;
