9da9c26ae4f2f561a1908a066b8d91b4dacdabbf
treeoflife2 <sol.analyzoor@gmail.com>
2025-12-04 14:49:17 +0530

    refactor

--- dexs/jupiter-prediction/index.ts
+++ dexs/jupiter-prediction/index.ts
@@ -4,31 +4,33 @@ import { queryDuneSql } from "../../helpers/dune";
 
 async function fetch(_a: any, _b: any, options: FetchOptions): Promise<FetchResult> {
 
-    const query = `with trade_data as (
-    select
+    const query = `
+        with trade_data as (
+            select
                 BYTEARRAY_TO_BIGINT (BYTEARRAY_REVERSE (BYTEARRAY_SUBSTRING (data, 1 + 24, 8))) / 1e6 as fees,
-        case
-            when bytearray_substring (data, 1, 1) = from_hex('b3') --buy
-            then 
-                    BYTEARRAY_TO_BIGINT (BYTEARRAY_REVERSE (BYTEARRAY_SUBSTRING (data, 1 + 16, 8))) / 1e6 + 
-                    BYTEARRAY_TO_BIGINT (BYTEARRAY_REVERSE (BYTEARRAY_SUBSTRING (data, 1 + 24, 8))) / 1e6
-            when bytearray_substring (data, 1, 1) = from_hex('fb') --sell
-            then 
-                    BYTEARRAY_TO_BIGINT (BYTEARRAY_REVERSE (BYTEARRAY_SUBSTRING (data, 1 + 16, 8))) / 1e6
-            end as volume
-        from solana.instruction_calls
-        where block_time >= from_unixtime(${options.fromTimestamp})
-            and block_time< from_unixtime(${options.toTimestamp})
-            and executing_account = '3ZZuTbwC6aJbvteyVxXUS7gtFYdf7AuXeitx6VyvjvUp'
-        and (bytearray_substring (data, 1, 1) = from_hex('fb')
-            or bytearray_substring (data, 1, 1) = from_hex('b3'))
-        and tx_success
-        and cardinality(log_messages) > 10)
-
+                case
+                    when bytearray_substring (data, 1, 1) = from_hex('b3') --buy
+                    then 
+                        BYTEARRAY_TO_BIGINT (BYTEARRAY_REVERSE (BYTEARRAY_SUBSTRING (data, 1 + 16, 8))) / 1e6 + 
+                        BYTEARRAY_TO_BIGINT (BYTEARRAY_REVERSE (BYTEARRAY_SUBSTRING (data, 1 + 24, 8))) / 1e6
+                    when bytearray_substring (data, 1, 1) = from_hex('fb') --sell
+                    then 
+                            BYTEARRAY_TO_BIGINT (BYTEARRAY_REVERSE (BYTEARRAY_SUBSTRING (data, 1 + 16, 8))) / 1e6
+                end as volume
+            from solana.instruction_calls
+            where block_time >= from_unixtime(${options.fromTimestamp})
+                and block_time< from_unixtime(${options.toTimestamp})
+                and executing_account = '3ZZuTbwC6aJbvteyVxXUS7gtFYdf7AuXeitx6VyvjvUp'
+            and (bytearray_substring (data, 1, 1) = from_hex('fb')
+                or bytearray_substring (data, 1, 1) = from_hex('b3'))
+            and tx_success
+            and cardinality(log_messages) > 10
+        )
         select
             sum(volume) as total_volume,
             sum(fees) as total_fees
-            from trade_data`;
+        from trade_data
+    `;
 
     const queryResult = await queryDuneSql(options, query);
 
@@ -56,12 +58,14 @@ const methodology = {
 }
 
 const adapter: SimpleAdapter = {
+    version: 1,
     fetch,
+    start: '2025-10-21',
+    chains: [CHAIN.SOLANA],
     methodology,
     isExpensiveAdapter: true,
+    doublecounted: true,
     dependencies: [Dependencies.DUNE],
-    start: '2025-10-21',
-    chains:[CHAIN.SOLANA]
 }
 
 export default adapter;
