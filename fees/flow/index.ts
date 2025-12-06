b9d0a8a6accd887e0ba7b83fb43f08f53bf13855
treeoflife2 <sol.analyzoor@gmail.com>
2025-12-06 16:55:45 +0530

    refactor

--- fees/flow/index.ts
+++ fees/flow/index.ts
@@ -3,34 +3,34 @@ import { CHAIN } from "../../helpers/chains";
 import fetchURL from "../../utils/fetchURL";
 
 async function fetch(_a: any, _b: any, options: FetchOptions): Promise<FetchResult> {
-    const start = new Date(options.startOfDay * 1000).toISOString();
-    const end = new Date(options.endTimestamp * 1000).toISOString();
+  const start = new Date(options.startOfDay * 1000).toISOString();
+  const end = new Date(options.endTimestamp * 1000).toISOString();
 
-    const response = await fetchURL(`https://api.find.xyz/flowscan/v1/stats?from=${start}&metric=fees&timescale=daily&to=${end}`);
-    
-    if(!response || !response.data || !response.data[0])
-        throw new Error('Flow fees not found');
+  const response = await fetchURL(`https://api.find.xyz/flowscan/v1/stats?from=${start}&metric=fees&timescale=daily&to=${end}`);
 
-    const dailyFees = options.createBalances();
-    dailyFees.addCGToken('flow',response.data[0].number);
+  if (!response || !response.data || !response.data[0])
+    throw new Error('Flow fees not found');
 
-    return {
-        dailyFees,
-        dailyRevenue:0,
-    };
+  const dailyFees = options.createBalances();
+  dailyFees.addCGToken('flow', response.data[0].number);
+
+  return {
+    dailyFees,
+    dailyRevenue: dailyFees,
+  };
 }
 
 const methodology = {
-    Fees: 'Transaction and storage fees paid by users',
-    Revenue: 'No revenue'
+  Fees: 'Transaction and storage fees paid by users',
+  Revenue: 'Fees goes to validators'
 }
 
 const adapter: SimpleAdapter = {
-    fetch,
-    chains: [CHAIN.FLOW],
-    start: '2018-12-19',
-    methodology,
-    protocolType:ProtocolType.CHAIN
+  fetch,
+  chains: [CHAIN.FLOW],
+  start: '2018-12-19',
+  methodology,
+  protocolType: ProtocolType.CHAIN
 }
 
 export default adapter
