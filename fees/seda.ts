5e964f1fa74b09f77aa51b2e43986d04386a6252
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-12-07 08:47:17 +0530

    last smol fix

--- fees/seda.ts
+++ fees/seda.ts
@@ -24,7 +24,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const yesterday = options.startOfDay * 1000 - 86400000;
 
   const extractResult = (data: any, dataFrequencyInDays: number = 1) => {
-    const dataToday = data.result.data.data.find((entry: [number, number]) => entry[0] === today) ?? [0, 0];
+    const dataToday = data.result.data.data.find((entry: [number, number | string]) => entry[0] === today) ?? [0, 0];
     const dataYesterday = data.result.data.data.find((entry: [number, number | string]) => entry[0] === yesterday) ?? [0, 0];
 
     const result = dataFrequencyInDays === 1 ? dataToday[1] - dataYesterday[1] : +(dataToday[1] || dataYesterday[1]);
