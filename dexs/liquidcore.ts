fb9c1010370ff648f328ddbe9ea7bb2b1b44147e
Shisho <shishohf@protonmail.com>
2025-12-07 19:04:08 -0500

    add new whype/usdc pool

--- dexs/liquidcore.ts
+++ dexs/liquidcore.ts
@@ -3,7 +3,10 @@ import { CHAIN } from "../helpers/chains";
 
 const SwapEvent = "event Swap(address indexed user, address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut, uint256 fee, uint256 reserve0, uint256 reserve1)";
 
-const LIQUIDCORE_POOL = "0xA7478A5ff7cB27A8008D6D90785db10223bc6087";
+const LIQUIDCORE_POOLS = [
+  "0xA7478A5ff7cB27A8008D6D90785db10223bc6087",
+  "0xD3994A6CF46cA91536376f89aCDadf92eD289a9F"
+];
 
 // Protocol takes 10% of fees, LPs get 90%
 const PROTOCOL_FEE_RATIO = 0.1;
@@ -16,10 +19,14 @@ const fetch = async (options: FetchOptions): Promise<FetchResult> => {
   const dailyProtocolRevenue = options.createBalances();
   const dailySupplySideRevenue = options.createBalances();
 
-  const logs = await options.getLogs({
-    target: LIQUIDCORE_POOL,
-    eventAbi: SwapEvent,
-  });
+  const logs = (await Promise.all(
+    LIQUIDCORE_POOLS.map(pool => 
+      options.getLogs({
+        target: pool,
+        eventAbi: SwapEvent,
+      })
+    )
+  )).flat();
 
   for (const log of logs) {
     // Track volume using the input token amount
