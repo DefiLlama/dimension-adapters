d5087c66f0410b05f38ea558cf8c4700a8606fed
treeoflife2 <sol.analyzoor@gmail.com>
2025-12-10 19:15:44 +0530

    Add meteora buyback holders revenue

--- dexs/meteora/index.ts
+++ dexs/meteora/index.ts
@@ -1,7 +1,11 @@
 import { CHAIN } from '../../helpers/chains';
 import { httpGet } from '../../utils/fetchURL';
+import { getSolanaReceived } from '../../helpers/token';
+import { FetchOptions } from '../../adapters/types';
+import CoreAddresses from '../../helpers/coreAssets.json';
 
 const meteoraStatsEndpoint = 'https://amm-v2.meteora.ag/pools/v2';
+const BUYBACK_WALLET = 'FzULv8pR9Rd7cyVKjVkzmJ1eqEmgwDnzjYyNUcEJtoG9';
 
 interface Pool {
   total_count: number
@@ -11,20 +15,30 @@ interface Pool {
   }>
 }
 
-async function fetch() {
+async function fetch(options: FetchOptions) {
   let dailyVolume = 0;
   let dailyFees = 0;
+
   let page = 0;
   const url = `${meteoraStatsEndpoint}?page=${page}&size=100000`;
   const response: Pool = (await httpGet(url));
+
   response.data.forEach(pool => {
     dailyVolume += pool.trading_volume
     dailyFees += pool.fee_volume
   })
   if (isNaN(dailyVolume) || isNaN(dailyFees)) throw new Error('Invalid daily volume')
+
+  const dailyHoldersRevenue = await getSolanaReceived({
+    options,
+    target: BUYBACK_WALLET,
+    mints: [CoreAddresses.solana.MET],
+  })
+
   return {
     dailyVolume,
     dailyFees,
+    dailyHoldersRevenue,
   }
 }
 
