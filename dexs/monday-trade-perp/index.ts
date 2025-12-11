fc05285e2159a2fe36f4347f7379989b873c6de2
treeoflife2 <sol.analyzoor@gmail.com>
2025-12-11 18:54:28 +0530

    refactor

--- dexs/monday-trade-perp/index.ts
+++ dexs/monday-trade-perp/index.ts
@@ -1,40 +1,36 @@
-import {CHAIN} from "../../helpers/chains";
-import {FetchOptions, SimpleAdapter} from "../../adapters/types";
-import axios from "axios";
+import { CHAIN } from "../../helpers/chains";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { httpGet } from "../../utils/fetchURL";
 
-const info: { [key: string]: any } = {
-    [CHAIN.MONAD]: {
-        api: "https://mainnet-api.monday.trade/v4/public/thirdPart/defillama/volumeAndFees",
-        chainId: 143,
-    },
+const url = "https://mainnet-api.monday.trade/v4/public/thirdPart/defillama/volumeAndFees"
+
+const chainConfig: { [key: string]: any } = {
+  [CHAIN.MONAD]: { chainId: 143, start: '2025-11-25' }
 };
 
 const fetch = async (_t: number, _: any, options: FetchOptions) => {
-    const dailyVolume = options.createBalances();
-    const chainInfo = info[CHAIN.MONAD]
-    const resData = await axios.get(chainInfo.api, {
-        params: {
-            chainId: chainInfo.chainId,
-            startTime: options.startTimestamp,
-            endTime: options.endTimestamp,
-        }
-    })
-    const data = resData.data
-    data.data.forEach((item: any) => {
-        dailyVolume.addToken(item.tokenAddress, item.volume);
-    })
-    return {
-        dailyVolume,
-    };
+  const dailyVolume = options.createBalances();
+  const chainInfo = chainConfig[options.chain]
+
+  const resData = await httpGet(url, {
+    params: {
+      chainId: chainInfo.chainId,
+      startTime: options.startTimestamp,
+      endTime: options.endTimestamp,
+    }
+  })
+
+  resData.data.forEach((item: any) => {
+    dailyVolume.addToken(item.tokenAddress, Number(item.volume));
+  })
+
+  return { dailyVolume };
 };
+
 const adapter: SimpleAdapter = {
-    version: 1,
-    adapter: {
-        [CHAIN.MONAD]: {
-            fetch,
-            start: '2025-11-25',
-        },
-    },
+  version: 1,
+  fetch,
+  adapter: chainConfig,
 };
 
 export default adapter;
