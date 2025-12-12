f508169a49b7bb4ef08566b88d1e91b0c9e5fa1c
0x4r45h <0x4r45h@gmail.com>
2025-12-12 21:46:17 +0330

    fix indents

--- fees/nexus-mutual.ts
+++ fees/nexus-mutual.ts
@@ -8,39 +8,43 @@ interface IData {
 
 const fetch = async ( options: FetchOptions) => {
   const query = `
-      WITH daily_avg_prices AS (SELECT block_date,
-                                       avg_eth_usd_price,
-                                       avg_dai_usd_price,
-                                       avg_nxm_eth_price,
-                                       avg_nxm_usd_price
-                                FROM nexusmutual_ethereum.capital_pool_prices),
-           covers AS (SELECT c.cover_id,
-                             cover_start_date,
-                             cover_end_date,
-                             c.premium_asset,
-                             c.premium *
-                             IF(c.cover_asset = 'DAI', p.avg_dai_usd_price, p.avg_eth_usd_price) AS premium_usd,
-                             c.premium * IF(c.cover_asset = 'DAI', p.avg_dai_usd_price, p.avg_eth_usd_price) /
-                             p.avg_eth_usd_price                                                 AS premium_eth
-                      FROM nexusmutual_ethereum.covers_v1 c
-                               INNER JOIN daily_avg_prices p ON c.block_date = p.block_date
+      WITH daily_avg_prices AS (
+          SELECT
+              block_date,
+              avg_eth_usd_price,
+              avg_dai_usd_price,
+              avg_nxm_eth_price,
+              avg_nxm_usd_price
+          FROM nexusmutual_ethereum.capital_pool_prices
+      ),
+           covers AS (
+               SELECT
+                   c.cover_id,
+                   cover_start_date,
+                   cover_end_date,
+                   c.premium_asset,
+                   c.premium * IF(c.cover_asset = 'DAI', p.avg_dai_usd_price, p.avg_eth_usd_price) AS premium_usd,
+                   c.premium * IF(c.cover_asset = 'DAI', p.avg_dai_usd_price, p.avg_eth_usd_price) / p.avg_eth_usd_price AS premium_eth
+               FROM nexusmutual_ethereum.covers_v1 c
+                        INNER JOIN daily_avg_prices p ON c.block_date = p.block_date
 
-                      UNION ALL
-
-                      SELECT c.cover_id,
-                             cover_start_date,
-                             cover_end_date,
-                             c.premium_asset,
-                             c.premium_incl_commission * p.avg_nxm_usd_price AS premium_usd,
-                             c.premium_incl_commission * p.avg_nxm_eth_price AS premium_eth
-                      FROM query_4599092 c
-                               INNER JOIN daily_avg_prices p ON c.block_date = p.block_date
-                      WHERE c.is_migrated = FALSE)
-
+               UNION ALL
+               
+               SELECT
+                   c.cover_id,
+                   cover_start_date,
+                   cover_end_date,
+                   c.premium_asset,
+                   c.premium_incl_commission * p.avg_nxm_usd_price AS premium_usd,
+                   c.premium_incl_commission * p.avg_nxm_eth_price AS premium_eth
+               FROM query_4599092 c
+                        INNER JOIN daily_avg_prices p ON c.block_date = p.block_date
+               WHERE c.is_migrated = FALSE
+           )
       SELECT SUM(premium_usd) AS premium_usd
       FROM covers
       WHERE cover_start_date >= FROM_UNIXTIME(${options.startTimestamp})
-        AND cover_start_date < FROM_UNIXTIME(${options.endTimestamp})
+        AND cover_start_date < FROM_UNIXTIME(${options.endTimestamp});
   `
   const res: IData[] = await queryDuneSql(options, query);
   const dailyFees = options.createBalances();
