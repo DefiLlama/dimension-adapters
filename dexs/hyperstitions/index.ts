da0d86c79f81ced9fe3ed1d5b9f88145788d0dc8
@aaronjmars <61592645+aaronjmars@users.noreply.github.com>
2025-12-13 19:32:11 +0100

    Add Hyperstitions volume adapter (Monad) (#4996)

---
+++ dexs/hyperstitions/index.ts
@@ -0,0 +1,38 @@
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+
+const HYPERSTITIONS_CONTRACT = "0x97b4a6b501C55cCC7A597E259266E7E28A2d0BE0";
+
+const BOUGHT_EVENT = "event Bought(address indexed buyer, uint256 indexed id, uint256 sharesOut, uint256 sttIn)";
+const SOLD_EVENT = "event Sold(address indexed seller, uint256 indexed id, uint256 sharesIn, uint256 sttOut)";
+
+const fetch = async (options: FetchOptions) => {
+  const dailyVolume = options.createBalances();
+  
+  const [buyLogs, sellLogs] = await Promise.all([
+    options.getLogs({ target: HYPERSTITIONS_CONTRACT, eventAbi: BOUGHT_EVENT }),
+    options.getLogs({ target: HYPERSTITIONS_CONTRACT, eventAbi: SOLD_EVENT }),
+  ]);
+
+  buyLogs.forEach((log: any) => {
+    dailyVolume.addCGToken("hyperstitions", Number(log.sttIn) / 1e18);
+  });
+
+  sellLogs.forEach((log: any) => {
+    dailyVolume.addCGToken("hyperstitions", Number(log.sttOut) / 1e18);
+  });
+
+  return { dailyVolume };
+};
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  adapter: {
+    [CHAIN.MONAD]: {
+      fetch,
+      start: "2025-12-03",
+    },
+  },
+};
+
+export default adapter;
