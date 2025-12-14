b0c3189651b922d9e836cf7eda5f71e51ec7db8c
Felix Bruguera <felixbruguera0@gmail.com>
2025-12-14 15:40:02 -0400

    add source and use v2 adapter

--- fees/tether-gold/index.ts
+++ fees/tether-gold/index.ts
@@ -1,12 +1,14 @@
-import { FetchOptions } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+
+// https://gold.tether.to/legal/feeschedule
 
 const xaut = '0x68749665FF8D2d112Fa859AA293F07A622782F38'
 
 const MINT_EVENT = 'event Mint (address indexed _destination, uint256 _amount)'
 const REDEEM_EVENT = 'event Redeem (uint256 _amount)'
 
-const fetch = async (_t: number, _c: any, options: FetchOptions) => {
+const fetch = async (options: FetchOptions) => {
     const dailyFees = options.createBalances();
     const [mintLogs, redeemLogs] = await Promise.all([
         options.getLogs({
@@ -28,17 +30,15 @@ const fetch = async (_t: number, _c: any, options: FetchOptions) => {
         dailyUserFees: dailyFees,
     }
 }
-const adapters = {
-  adapter: {
-    [CHAIN.ETHEREUM]: {
-      fetch: fetch,
-      start: '2021-10-31',
+const adapters : SimpleAdapter = {
+    version: 2,
+    fetch,
+    chains: [CHAIN.ETHEREUM],
+    start: '2021-10-31',
+    methodology: {
+        Fees: "0.25% fee on purchase or redemption of XAUT tokens",
+        Revenue: "Purchase and Redeem fees from users",
+        UserFees: "Purchase and Redeem fees",
     }
-  },
-  methodology: {
-    Fees: "0.25% fee on purchase or redemption of XAUT tokens",
-    Revenue: "Purchase and Redeem fees from users",
-    UserFees: "Purchase and Redeem fees",
-  }
 };
 export default adapters;
