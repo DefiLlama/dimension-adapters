3f62bcf319c21c616a5cbb610957cfec26e2671c
solbricklayer <67789073+solbricklayer@users.noreply.github.com>
2025-12-14 18:05:37 +0100

    Collector Crypt - Update wallets to match official query
    update to match official query https://dune.com/queries/5490579/8946009

--- fees/collector-crypt/index.ts
+++ fees/collector-crypt/index.ts
@@ -10,19 +10,22 @@ const fetch = async (_a:any, _b:any, options: FetchOptions) => {
             SELECT 
                 SUM(amount / POWER(10, 6)) AS inflow
             FROM tokens_solana.transfers
-            WHERE to_owner = 'GachazZscHZ5bn3vnq1yEC4zpYdhAYJBzuKJwSJksc9z'
-                AND from_owner NOT IN (
-                    'BAxTk97HsaJqbnbFmTiQTaL4KSRvJ8Y65ArZCsP6vA5M',
-                    '8373hLiAEXxaJ3oV7SRzx4KHwurEg9rEG98tUPj1sdtX',
-                    '21KhtC7y2JGYvwc8dcGqTdbrudbM8fgMPJsVwxRQqdY8',
-                    'DFEstpYN3fsz93AC9v2ujzPPngPgodqH2xxopuyfSsAE',
-                    'HW2HRqN1pXQGH9GfP9xet4XwqtLqFyYGDNRKjUAVgh9u',
-                    'HighJBfnAaqH9cKkeMErQFJZ4ATxQJwxqFupX6zaKTns',
-                    'LGNDXqcm6U57QQ6Ad7icZ6oizkAVKRWrw97KwZy5nVf',
-                    'EpicWWZspT1trKndbDDr29ULViN56rN5vofWSKZp8ePF',
-                    'Mid9NeCpPNxP59fAdsLgMLy7BYexxXFw52ZP58Jrney',
-                    'Lowq9dkpY43VpjfYeRjtKfGA6JtB7HaMmwQgXkjHLvN'
-                )
+            WHERE to_owner IN ('GachazZscHZ5bn3vnq1yEC4zpYdhAYJBzuKJwSJksc9z','GachaNgyXTU3zFogQ8Z5jR2BLXs8215X2AtEH18VxJq3','96DULv1BqYfe5wyMr6pVUNC6Uyrtj6yr3tNi6VtfwW9s')
+                AND from_owner NOT IN ('BAxTk97HsaJqbnbFmTiQTaL4KSRvJ8Y65ArZCsP6vA5M',
+                                        '21KhtC7y2JGYvwc8dcGqTdbrudbM8fgMPJsVwxRQqdY8',
+                                        'DFEstpYN3fsz93AC9v2ujzPPngPgodqH2xxopuyfSsAE',
+                                        'HW2HRqN1pXQGH9GfP9xet4XwqtLqFyYGDNRKjUAVgh9u',
+                                        'HighJBfnAaqH9cKkeMErQFJZ4ATxQJwxqFupX6zaKTns',
+                                        'LGNDXqcm6U57QQ6Ad7icZ6oizkAVKRWrw97KwZy5nVf',
+                                        'EpicWWZspT1trKndbDDr29ULViN56rN5vofWSKZp8ePF',
+                                        'Mid9NeCpPNxP59fAdsLgMLy7BYexxXFw52ZP58Jrney',
+                                        'Lowq9dkpY43VpjfYeRjtKfGA6JtB7HaMmwQgXkjHLvN',
+                                        'Low6UekJP3QrFVMfNRTL8CPK2SiGFhvp57sgF2pkmVu',
+                                        'miDtj3vgdxVykHzRyFwyG8MXpvK8eQqamSLVdBr7WPt',
+                                        'HiGHqwYddP5N2waqUmXPdaASpMpUEvfqPr2fSawctEb',
+                                        'epiC3zkqa1RfcPMMM1Kc8m3GZGDwF2RmjbfA3g1BBjn',
+                                        'LGNDfXQFMiRMz3qqTNAREmRFQutMvazqqRrzn5i98uj',
+                                        'SPrT7eFrCM9UJ4j7Xf9iktKCoBwJjfykFbiNbRsKQm8')
                 AND amount / power(10, 6) IN (50, 250)
                 AND token_mint_address = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
                 AND TIME_RANGE
@@ -39,19 +42,24 @@ const fetch = async (_a:any, _b:any, options: FetchOptions) => {
             SELECT 
                 SUM(amount / POWER(10, 6)) AS buyback
             FROM tokens_solana.transfers
-            WHERE from_owner = 'GachazZscHZ5bn3vnq1yEC4zpYdhAYJBzuKJwSJksc9z'
-                AND to_owner NOT IN (
-                    'BAxTk97HsaJqbnbFmTiQTaL4KSRvJ8Y65ArZCsP6vA5M',
-                    '8373hLiAEXxaJ3oV7SRzx4KHwurEg9rEG98tUPj1sdtX',
-                    '21KhtC7y2JGYvwc8dcGqTdbrudbM8fgMPJsVwxRQqdY8',
-                    'DFEstpYN3fsz93AC9v2ujzPPngPgodqH2xxopuyfSsAE',
-                    'HW2HRqN1pXQGH9GfP9xet4XwqtLqFyYGDNRKjUAVgh9u',
-                    'HighJBfnAaqH9cKkeMErQFJZ4ATxQJwxqFupX6zaKTns',
-                    'LGNDXqcm6U57QQ6Ad7icZ6oizkAVKRWrw97KwZy5nVf',
-                    'EpicWWZspT1trKndbDDr29ULViN56rN5vofWSKZp8ePF',
-                    'Mid9NeCpPNxP59fAdsLgMLy7BYexxXFw52ZP58Jrney',
-                    'Lowq9dkpY43VpjfYeRjtKfGA6JtB7HaMmwQgXkjHLvN'
-                )
+            WHERE from_owner IN ('GachazZscHZ5bn3vnq1yEC4zpYdhAYJBzuKJwSJksc9z','GachaNgyXTU3zFogQ8Z5jR2BLXs8215X2AtEH18VxJq3')
+                AND to_owner NOT IN ('BAxTk97HsaJqbnbFmTiQTaL4KSRvJ8Y65ArZCsP6vA5M',
+                                    '21KhtC7y2JGYvwc8dcGqTdbrudbM8fgMPJsVwxRQqdY8',
+                                    'DFEstpYN3fsz93AC9v2ujzPPngPgodqH2xxopuyfSsAE',
+                                    'HW2HRqN1pXQGH9GfP9xet4XwqtLqFyYGDNRKjUAVgh9u',
+                                    'HighJBfnAaqH9cKkeMErQFJZ4ATxQJwxqFupX6zaKTns',
+                                    'LGNDXqcm6U57QQ6Ad7icZ6oizkAVKRWrw97KwZy5nVf',
+                                    'EpicWWZspT1trKndbDDr29ULViN56rN5vofWSKZp8ePF',
+                                    'Mid9NeCpPNxP59fAdsLgMLy7BYexxXFw52ZP58Jrney',
+                                    'Lowq9dkpY43VpjfYeRjtKfGA6JtB7HaMmwQgXkjHLvN',
+                                    'Low6UekJP3QrFVMfNRTL8CPK2SiGFhvp57sgF2pkmVu',
+                                    'miDtj3vgdxVykHzRyFwyG8MXpvK8eQqamSLVdBr7WPt',
+                                    'HiGHqwYddP5N2waqUmXPdaASpMpUEvfqPr2fSawctEb',
+                                    'epiC3zkqa1RfcPMMM1Kc8m3GZGDwF2RmjbfA3g1BBjn',
+                                    'LGNDfXQFMiRMz3qqTNAREmRFQutMvazqqRrzn5i98uj',
+                                    'SPrT7eFrCM9UJ4j7Xf9iktKCoBwJjfykFbiNbRsKQm8',
+                                    'Cc4pHGnoaRWL1WnHsV517T3YvQn5gLDBMiuVXkF9rZhK',
+                                    '8373hLiAEXxaJ3oV7SRzx4KHwurEg9rEG98tUPj1sdtX')
                 AND token_mint_address = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
                 AND TIME_RANGE
             )
