e8a9b77ea28380eca35e4ebdfe5215a7bdc69207
Breno Polanski <breno.polanski@gmail.com>
2025-12-15 15:06:36 -0300

    fix: migrate Tokenlon adapter from GraphQL to event logs (#5097)
    * fix: migrate Tokenlon adapter from GraphQL to event logs
    
    * fix(tokenlon): update start dates for all chains

--- dexs/tokenlon/index.ts
+++ dexs/tokenlon/index.ts
@@ -1,25 +1,5 @@
-import * as sdk from "@defillama/sdk";
-import { BreakdownAdapter, ChainBlocks, FetchOptions, FetchResultVolume, SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import { request } from "graphql-request";
-import { Chain } from "../../adapters/types";
-
-interface IGraph {
-  makerAssetAddr: string;
-  makerAssetAmount: string;
-  makerToken: string;
-  makerTokenAmount: string;
-}
-
-interface IData {
-  fillOrders: IGraph[];
-  swappeds: IGraph[];
-  filledRFQs: IGraph[];
-}
-
-type TEndpoint = {
-  [s: string | Chain]: string;
-};
+import { FetchOptions, BreakdownAdapter } from '../../adapters/types';
+import { CHAIN } from '../../helpers/chains';
 
 const config: any = {
   [CHAIN.POLYGON]: { exchange: '0xac4F3817a726016fC8254119FC48bE838a21f17F' },
@@ -28,79 +8,46 @@ const config: any = {
   [CHAIN.BSC]: { exchange: '0xac4F3817a726016fC8254119FC48bE838a21f17F' },
   [CHAIN.OPTIMISM]: { exchange: '0xac4F3817a726016fC8254119FC48bE838a21f17F' },
   [CHAIN.ERA]: { exchange: '0xC63c379Ae456af9C73Bf81A7D745fAF1d9e180e0' },
-}
-
-const endpoints: TEndpoint = {
-  [CHAIN.ETHEREUM]: sdk.graph.modifyEndpoint('5JhweAV1Y3k3GbbEssfetBaoyDNWz1Y72zscRrYsAgVT'),
-};
-const rfqV2Endpoint = "https://subgraph.satsuma-prod.com/61c3dea518e9/imtoken-labs--349710/rfq-v2-subgraph/version/v0.0.1-test-version/api"
-
-const fetchVolume = (chain: Chain) => {
-  return async (
-    __timestamp: number,
-    _: ChainBlocks,
-    { createBalances, fromTimestamp, toTimestamp }: FetchOptions
-  ): Promise<FetchResultVolume> => {
-    const dailyVolume = createBalances();
-    const query = `
-    {
-      swappeds(first:1000, where:{timestamp_gte:${fromTimestamp}, timestamp_lte:${toTimestamp}}) {
-        makerAssetAddr
-        makerAssetAmount
-      }
-      fillOrders(first:1000, where:{timestamp_gte:${fromTimestamp}, timestamp_lte:${toTimestamp}}) {
-        makerAssetAddr
-        makerAssetAmount
-      }
-    }
-    `;
-    const response: IData = await request(endpoints[chain], query);
-    const historicalData: IGraph[] = [...response.fillOrders, ...response.swappeds];
-    historicalData.map((e: IGraph) => {
-      dailyVolume.add(e.makerAssetAddr, e.makerAssetAmount);
-    });
-
-    const rfqv2Query = `{
-      filledRFQs(
-      first: 1000
-      where: {blockTimestamp_gte: ${fromTimestamp}, blockTimestamp_lte: ${toTimestamp}}
-    ) {
-      makerToken
-      makerTokenAmount
-    }}`
-    const rfqV2Response = await request(rfqV2Endpoint, rfqv2Query)
-    const rfqv2Record: IGraph[] = [...rfqV2Response.filledRFQs]
-    rfqv2Record.map((e: IGraph) => {
-      dailyVolume.add(e.makerToken, e.makerTokenAmount);
-    });
-
-    return { dailyVolume, timestamp: toTimestamp };
-  };
 };
 
 const abis = {
   FillOrder:
-    "event FillOrder(string source, bytes32 indexed transactionHash, bytes32 indexed orderHash, address indexed userAddr, address takerAssetAddr, uint256 takerAssetAmount, address makerAddr, address makerAssetAddr, uint256 makerAssetAmount, address receiverAddr, uint256 settleAmount, uint16 feeFactor)",
+    'event FillOrder(string source, bytes32 indexed transactionHash, bytes32 indexed orderHash, address indexed userAddr, address takerAssetAddr, uint256 takerAssetAmount, address makerAddr, address makerAssetAddr, uint256 makerAssetAmount, address receiverAddr, uint256 settleAmount, uint16 feeFactor)',
   Swapped:
-    "event Swapped(string source, bytes32 indexed transactionHash, address indexed userAddr, address takerAssetAddr, uint256 takerAssetAmount, address makerAddr, address makerAssetAddr, uint256 makerAssetAmount, address receiverAddr, uint256 settleAmount, uint256 receivedAmount, uint16 feeFactor, uint16 subsidyFactor)",
+    'event Swapped(string source, bytes32 indexed transactionHash, address indexed userAddr, address takerAssetAddr, uint256 takerAssetAmount, address makerAddr, address makerAssetAddr, uint256 makerAssetAmount, address receiverAddr, uint256 settleAmount, uint256 receivedAmount, uint16 feeFactor, uint16 subsidyFactor)',
   FilledRFQ:
-    "event FilledRFQ(bytes32 indexed offerHash,address indexed user,address indexed maker,address takerToken,uint256 takerTokenAmount,address makerToken,uint256 makerTokenAmount,address recipient,uint256 settleAmount,uint256 feeFactor)",
+    'event FilledRFQ(bytes32 indexed offerHash,address indexed user,address indexed maker,address takerToken,uint256 takerTokenAmount,address makerToken,uint256 makerTokenAmount,address recipient,uint256 settleAmount,uint256 feeFactor)',
   FillOrderByRFQ:
-    "event FillOrder( string source,bytes32 indexed transactionHash,bytes32 indexed orderHash,address indexed userAddr,address takerAssetAddr,uint256 takerAssetAmount,address makerAddr,address makerAssetAddr,uint256 makerAssetAmount,address receiverAddr,uint256 settleAmount,uint16 feeFactor)",
-
-  Swap:
-    "event Swap(bytes32 indexed swapHash,address indexed maker, address indexed taker,address recipient,address inputToken,uint256 inputAmount,address outputToken,uint256 outputAmount)",
-  SwappedV2: "event Swapped((string source, bytes32 transactionHash, uint256 settleAmount, uint256 receivedAmount, uint16 feeFactor, uint16 subsidyFactor), (address makerAddr, address takerAssetAddr, address makerAssetAddr, uint256 takerAssetAmount, uint256 makerAssetAmount, address userAddr, address receiverAddr, uint256 salt, uint256 deadline) order)",
+    'event FillOrder( string source,bytes32 indexed transactionHash,bytes32 indexed orderHash,address indexed userAddr,address takerAssetAddr,uint256 takerAssetAmount,address makerAddr,address makerAssetAddr,uint256 makerAssetAmount,address receiverAddr,uint256 settleAmount,uint16 feeFactor)',
+  Swap: 'event Swap(bytes32 indexed swapHash,address indexed maker, address indexed taker,address recipient,address inputToken,uint256 inputAmount,address outputToken,uint256 outputAmount)',
+  SwappedV2:
+    'event Swapped((string source, bytes32 transactionHash, uint256 settleAmount, uint256 receivedAmount, uint16 feeFactor, uint16 subsidyFactor), (address makerAddr, address takerAssetAddr, address makerAssetAddr, uint256 takerAssetAmount, uint256 makerAssetAmount, address userAddr, address receiverAddr, uint256 salt, uint256 deadline) order)',
 };
 
-const fetch = async (timestamp: number, _: ChainBlocks, { createBalances, getLogs, }: FetchOptions) => {
+const fetchEthereum = async (options: FetchOptions) => {
+  const { createBalances, getLogs } = options;
   const dailyVolume = createBalances();
 
-  const pmmLogs = await getLogs({ target: "0x8D90113A1e286a5aB3e496fbD1853F265e5913c6", eventAbi: abis.FillOrder });
-  const rfqv1Logs = await getLogs({ target: "0xfD6C2d2499b1331101726A8AC68CCc9Da3fAB54F", eventAbi: abis.FillOrderByRFQ });
-  const rfqv2Logs = await getLogs({ target: "0x91c986709bb4fe0763edf8e2690ee9d5019bea4a", eventAbi: abis.FilledRFQ });
-  const ammV1Logs = await getLogs({ target: "0x4a14347083B80E5216cA31350a2D21702aC3650d", eventAbi: abis.Swapped });
-  const ammV2Logs = await getLogs({ target: "0x4a14347083B80E5216cA31350a2D21702aC3650d", eventAbi: abis.SwappedV2 });
+  const pmmLogs = await getLogs({
+    target: '0x8D90113A1e286a5aB3e496fbD1853F265e5913c6',
+    eventAbi: abis.FillOrder,
+  });
+  const rfqv1Logs = await getLogs({
+    target: '0xfD6C2d2499b1331101726A8AC68CCc9Da3fAB54F',
+    eventAbi: abis.FillOrderByRFQ,
+  });
+  const rfqv2Logs = await getLogs({
+    target: '0x91c986709bb4fe0763edf8e2690ee9d5019bea4a',
+    eventAbi: abis.FilledRFQ,
+  });
+  const ammV1Logs = await getLogs({
+    target: '0x4a14347083B80E5216cA31350a2D21702aC3650d',
+    eventAbi: abis.Swapped,
+  });
+  const ammV2Logs = await getLogs({
+    target: '0x4a14347083B80E5216cA31350a2D21702aC3650d',
+    eventAbi: abis.SwappedV2,
+  });
 
   [ammV1Logs, rfqv1Logs, pmmLogs].flat().forEach((log: any) => {
     dailyVolume.add(log.makerAssetAddr, log.makerAssetAmount);
@@ -113,34 +60,64 @@ const fetch = async (timestamp: number, _: ChainBlocks, { createBalances, getLog
   [rfqv2Logs].flat().forEach((log: any) => {
     dailyVolume.add(log.makerToken, log.makerTokenAmount);
   });
-  return { timestamp, dailyVolume };
+
+  return { dailyVolume };
 };
 
-const fetchL2 = async (timestamp: number, _: ChainBlocks, { createBalances, getLogs, chain, }: FetchOptions) => {
+const fetchL2 = async (options: FetchOptions) => {
+  const { createBalances, getLogs, chain } = options;
   const dailyVolume = createBalances();
-  const ammLogs = await getLogs({ target: config[chain].exchange, eventAbi: abis.Swap });
+  const ammLogs = await getLogs({
+    target: config[chain].exchange,
+    eventAbi: abis.Swap,
+  });
 
   [ammLogs].flat().forEach((log: any) => {
     dailyVolume.add(log.inputToken, log.inputAmount);
   });
-  return { timestamp, dailyVolume };
+
+  return { dailyVolume };
 };
 
-const adaptersMultiChain: any = {}
-adaptersMultiChain[CHAIN.ETHEREUM] = { fetch, start: '2020-12-15' }
-Object.keys(config).forEach(chain => {
-  adaptersMultiChain[chain] = { fetch: fetchL2 }
-})
-
 const adapter: BreakdownAdapter = {
+  version: 2,
   breakdown: {
     tokenlon: {
       [CHAIN.ETHEREUM]: {
-        fetch: fetchVolume(CHAIN.ETHEREUM),
-        start: '2020-12-17',
+        fetch: fetchEthereum,
+        start: '2020-12-15',
       },
     },
-    "tokenlon-agg": adaptersMultiChain,
+    'tokenlon-agg': {
+      [CHAIN.ETHEREUM]: {
+        fetch: fetchEthereum,
+        start: '2020-12-15',
+      },
+      [CHAIN.POLYGON]: {
+        fetch: fetchL2,
+        start: '2023-11-06',
+      },
+      [CHAIN.ARBITRUM]: {
+        fetch: fetchL2,
+        start: '2023-11-22',
+      },
+      [CHAIN.BASE]: {
+        fetch: fetchL2,
+        start: '2024-03-13',
+      },
+      [CHAIN.BSC]: {
+        fetch: fetchL2,
+        start: '2024-03-13',
+      },
+      [CHAIN.OPTIMISM]: {
+        fetch: fetchL2,
+        start: '2023-11-22',
+      },
+      [CHAIN.ERA]: {
+        fetch: fetchL2,
+        start: '2024-04-16',
+      },
+    },
   },
 };
 
