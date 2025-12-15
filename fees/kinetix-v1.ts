e7cf65849b3baa01d4f17533a5e8e8f1450e2d9b
kanerep <kanerep@icloud.com>
2025-12-15 20:58:25 +0000

    fix: remove the deadFrom property from v1 which uses logs

--- fees/kinetix-v1.ts
+++ fees/kinetix-v1.ts
@@ -1,12 +1,9 @@
 import { CHAIN } from "../helpers/chains";
 import { gmxV1Exports } from "../helpers/gmx";
 
-export default {
-  ...gmxV1Exports({
-    [CHAIN.KAVA]: {
-      vault: "0xa721f9f61CECf902B2BCBDDbd83E71c191dEcd8b",
-      start: "2023-12-12",
-    },
-  }),
-  deadFrom: "2025-08-19", // Kinetix Perpetuals V1 & V2 officially terminated
-};
+export default gmxV1Exports({
+  [CHAIN.KAVA]: {
+    vault: "0xa721f9f61CECf902B2BCBDDbd83E71c191dEcd8b",
+    start: "2023-12-12",
+  },
+});
