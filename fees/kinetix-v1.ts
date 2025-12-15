fd650a1cdd05251b2416731f745534a4acacf452
kanerep <kanerep@icloud.com>
2025-12-15 17:23:43 +0000

    fix(kinetix): comment out history, add deadFrom and a stub fetch function

--- fees/kinetix-v1.ts
+++ fees/kinetix-v1.ts
@@ -1,9 +1,17 @@
 import { CHAIN } from "../helpers/chains";
-import { gmxV1Exports } from "../helpers/gmx";
+import { Adapter } from "../adapters/types";
 
-export default gmxV1Exports({
-  [CHAIN.KAVA]: {
-    vault: "0xa721f9f61CECf902B2BCBDDbd83E71c191dEcd8b",
-    start: '2023-12-12',
+// Original used gmxV1Exports with vault: "0xa721f9f61CECf902B2BCBDDbd83E71c191dEcd8b"
+
+const adapter: Adapter = {
+  version: 2,
+  deadFrom: "2025-08-19", // Kinetix Perpetuals V1 & V2 officially terminated
+  adapter: {
+    [CHAIN.KAVA]: {
+      fetch: async () => ({}),
+      start: "2023-12-12",
+    },
   },
-})
+};
+
+export default adapter;
