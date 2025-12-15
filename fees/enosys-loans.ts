62d48a3175a04349e5ada4ce19f1916ba2525928
TJakubek <39831436+TJakubek@users.noreply.github.com>
2025-12-15 21:21:27 +0100

    feat: adding enosys loans for fee tracking (#5096)

---
+++ fees/enosys-loans.ts
@@ -0,0 +1,8 @@
+import { CHAIN } from "../helpers/chains";
+import { liquityV2Exports } from "../helpers/liquity";
+
+export default liquityV2Exports({
+  [CHAIN.FLARE]: {
+    collateralRegistry: "0x9474206bc035D03d142264fd9913d1D51246d3AC",
+  },
+});
