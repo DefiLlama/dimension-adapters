311524451c7dc1b97fd6d07f9615daec6db0a048
kanerep <kanerep@icloud.com>
2025-12-15 16:48:17 +0000

    fix(kinetix-v3): comment out kava chain and add base with simple daily queries

--- fees/kinetix-v3/index.ts
+++ fees/kinetix-v3/index.ts
@@ -5,8 +5,7 @@ import { CHAIN } from "../../helpers/chains";
 import { getTimestampAtStartOfDayUTC } from "../../utils/date";
 
 const endpoints = {
-  [CHAIN.KAVA]:
-    "https://kava-graph-node.metavault.trade/subgraphs/name/kinetixfi/v3-subgraph",
+  // [CHAIN.KAVA]: "https://the-graph.kava.io/subgraphs/name/kinetixfi/v3-subgraph", // subgraph stale since April 2024, protocol winding down
   [CHAIN.BASE]:
     "https://api.studio.thegraph.com/query/55804/kinetixfi-base-v3/version/latest",
 };
@@ -55,10 +54,10 @@ const adapter: Adapter = {
   methodology: "Fees collected from user trading fees",
   version: 1,
   adapter: {
-    [CHAIN.KAVA]: {
-      fetch: fetch(endpoints[CHAIN.KAVA]),
-      start: '2023-08-15', // Tuesday, August 15, 2023 12:00:00 AM
-    },
+    // [CHAIN.KAVA]: {
+    //   fetch: fetch(endpoints[CHAIN.KAVA]),
+    //   start: '2023-08-15', // Tuesday, August 15, 2023 12:00:00 AM
+    // },
     [CHAIN.BASE]: {
       fetch: fetch(endpoints[CHAIN.BASE]),
       start: '2024-05-08', //  Wednesday, May 8, 2024 12:00:00 AM
