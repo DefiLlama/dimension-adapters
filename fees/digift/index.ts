69fc9485a61274053a4a580339d4733323c47abc
Felix Bruguera <felixbruguera0@gmail.com>
2025-12-15 17:09:48 -0400

    apply changes

--- fees/digift/index.ts
+++ fees/digift/index.ts
@@ -4,13 +4,15 @@ import { addTokensReceived } from "../../helpers/token"
 import CoreAssets from "../../helpers/coreAssets.json";
 import { METRIC } from "../../helpers/metrics";
 
-// This is the platform fee address set in the management contract: https://etherscan.io/address/0x96ABd1C04882BCe372203d43649be525F8AE87ba#readContract#F36
-const feeAddress = '0x348826f471b63c4C9C393B885E595BA1e2AFf3F5'
-
+const management = '0x96ABd1C04882BCe372203d43649be525F8AE87ba'
 const usdc = CoreAssets.ethereum.USDC
 
 const fetch = async (options: FetchOptions) => {
     const dailyFees = options.createBalances();
+    const feeAddress = await options.api.call({
+        abi: "address:platformFeeAddress",
+        target: management
+    });
     const transfers = await addTokensReceived({
         options,
         tokens: [usdc],
@@ -30,8 +32,8 @@ const adapters : SimpleAdapter = {
     start: '2024-01-08',
     methodology: {
         Fees: "Platform fees on subscription and redemption of tokens",
-        Revenue: "Platform fees from users",
-        UserFees: "Platform fees",
+        Revenue: "Platform fees paid by users on subscription and redemption",
+        UserFees: "Platform fees paid by users on subscription and redemption",
     }
 };
 export default adapters;
