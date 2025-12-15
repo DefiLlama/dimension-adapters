f76ad81778c06d315bdfa98d33c03200b312a72c
kanerep <kanerep@icloud.com>
2025-12-15 12:00:44 +0000

    fix: update chainlink keepers to use chainlink automation v2.1 contracts

--- fees/chainlink-keepers.ts
+++ fees/chainlink-keepers.ts
@@ -2,34 +2,46 @@ import { ChainApi } from "@defillama/sdk";
 import pLimit from "p-limit";
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import ADDRESSES from '../helpers/coreAssets.json';
+import ADDRESSES from "../helpers/coreAssets.json";
 
 const topics = {
-  upkeepPerformed: '0xcaacad83e47cc45c280d487ec84184eee2fa3b54ebaa393bda7549f13da228f6',
-  success: '0x0000000000000000000000000000000000000000000000000000000000000001'
-}
+  upkeepPerformed:
+    "0xad8cc9579b21dfe2c2f6ea35ba15b656e46b4f5b0cb424f52739b8ce5cac9c5b",
+  success: "0x0000000000000000000000000000000000000000000000000000000000000001",
+};
 
 const eventAbis = {
-    upkeepPerformed: "event UpkeepPerformed(uint256 indexed id, bool indexed success, address indexed from, uint96 payment, bytes performData)",
-}
+  upkeepPerformed:
+    "event UpkeepPerformed(uint256 indexed id, bool indexed success, uint96 totalPayment, uint256 gasUsed, uint256 gasOverhead, bytes trigger)",
+};
 
-type TAddrress = {
+type TAddress = {
   [l: string | CHAIN]: string;
-}
+};
 
 const LINK = ADDRESSES.ethereum.LINK;
 
-const address: TAddrress = {
-  [CHAIN.ETHEREUM]: '0x02777053d6764996e594c3E88AF1D58D5363a2e6',
-  [CHAIN.BSC]: '0x02777053d6764996e594c3E88AF1D58D5363a2e6',
-  [CHAIN.POLYGON]: '0x02777053d6764996e594c3E88AF1D58D5363a2e6',
-  [CHAIN.FANTOM]: '0x02777053d6764996e594c3E88AF1D58D5363a2e6',
-  [CHAIN.AVAX]: '0x02777053d6764996e594c3E88AF1D58D5363a2e6',
-  [CHAIN.ARBITRUM]: '0x75c0530885F385721fddA23C539AF3701d6183D4',
-  [CHAIN.OPTIMISM]: '0x75c0530885F385721fddA23C539AF3701d6183D4'
-}
+// Chainlink Automation v2.1+ Registry addresses
+// Source: https://docs.chain.link/chainlink-automation/overview/supported-networks
+const address: TAddress = {
+  [CHAIN.ETHEREUM]: "0x6593c7De001fC8542bB1703532EE1E5aA0D458fD",
+  [CHAIN.BSC]: "0xDc21E279934fF6721CaDfDD112DAfb3261f09A2C",
+  [CHAIN.POLYGON]: "0x08a8eea76D2395807Ce7D1FC942382515469cCA1",
+  [CHAIN.AVAX]: "0x7f00a3Cd4590009C349192510D51F8e6312E08CB",
+  [CHAIN.ARBITRUM]: "0x37D9dC70bfcd8BC77Ec2858836B923c560E891D1",
+  [CHAIN.OPTIMISM]: "0x4F70c323b8B72AeffAF633Aa4D5e8B6Be5df4AEf",
+  [CHAIN.BASE]: "0xf4bAb6A129164aBa9B113cB96BA4266dF49f8743",
+  [CHAIN.XDAI]: "0x299c92a219F61a82E91d2062A262f7157F155AC1",
+  [CHAIN.POLYGON_ZKEVM]: "0x0F7E163446AAb41DB5375AbdeE2c3eCC56D9aA32",
+  [CHAIN.SCROLL]: "0xBe55E7eb27Cd69Be0883E0284632A91bB7AdC272",
+  [CHAIN.ERA]: "0x8D405a2252fe4bd50dF29835e621986E59A81D74",
+};
 
-const getTransactions = async (fromBlock: number, toBlock: number, api: ChainApi): Promise<{ transactions: any[]; totalPayment: number }> => {
+const getTransactions = async (
+  fromBlock: number,
+  toBlock: number,
+  api: ChainApi
+): Promise<{ transactions: any[]; totalPayment: number }> => {
   const target = address[api.chain];
   const TX_HASH_BATCH = 50;
   const MAX_PARALLEL = 3;
@@ -38,17 +50,17 @@ const getTransactions = async (fromBlock: number, toBlock: number, api: ChainApi
     target,
     fromBlock,
     toBlock,
-    topics: [topics.upkeepPerformed, '', topics.success],
+    topics: [topics.upkeepPerformed, null, topics.success],
     eventAbi: eventAbis.upkeepPerformed,
-    entireLog: true
+    entireLog: true,
   });
 
   let totalPayment = 0;
   const seenHashes = new Set<string>();
 
   for (const e of logs) {
-    const { transactionHash, args } = e
-    if (args.payment) totalPayment += Number(args.payment)
+    const { transactionHash, args } = e;
+    if (args.totalPayment) totalPayment += Number(args.totalPayment);
     if (transactionHash) seenHashes.add(transactionHash);
   }
 
@@ -70,17 +82,19 @@ const getTransactions = async (fromBlock: number, toBlock: number, api: ChainApi
   const results = await Promise.all(
     txHashBatches.map((hashChunk) =>
       limit(() =>
-        api.getTransactions({
-          chain: api.chain,
-          addresses: [target],
-          from_block: fromBlock,
-          to_block: toBlock,
-          transaction_hashes: hashChunk,
-          transactionType: "to"
-        }).catch((err) => {
-          console.error(`Failed to fetch transactions on ${api.chain}:`, err);
-          return [];
-        })
+        api
+          .getTransactions({
+            chain: api.chain,
+            addresses: [target],
+            from_block: fromBlock,
+            to_block: toBlock,
+            transaction_hashes: hashChunk,
+            transactionType: "to",
+          })
+          .catch((err) => {
+            console.error(`Failed to fetch transactions on ${api.chain}:`, err);
+            return [];
+          })
       )
     )
   );
@@ -94,12 +108,23 @@ const getTransactions = async (fromBlock: number, toBlock: number, api: ChainApi
   return { transactions: allTransactions, totalPayment };
 };
 
-const fetch = async (_: any, _1: any, { getFromBlock, getToBlock, createBalances, api }: FetchOptions) => {
-  const [fromBlock, toBlock] = await Promise.all([getFromBlock(), getToBlock()])
-  const dailyRevenue = createBalances()
-  const dailyGas = createBalances()
-  const dailyPayment = createBalances()
-  const { transactions, totalPayment } =  await getTransactions(fromBlock, toBlock, api)
+const fetch = async (
+  _: any,
+  _1: any,
+  { getFromBlock, getToBlock, createBalances, api }: FetchOptions
+) => {
+  const [fromBlock, toBlock] = await Promise.all([
+    getFromBlock(),
+    getToBlock(),
+  ]);
+  const dailyRevenue = createBalances();
+  const dailyGas = createBalances();
+  const dailyPayment = createBalances();
+  const { transactions, totalPayment } = await getTransactions(
+    fromBlock,
+    toBlock,
+    api
+  );
 
   const dailyGasUsed = transactions.reduce((acc, tx) => {
     const gasUsed = Number(tx.gasUsed ?? 0);
@@ -107,44 +132,63 @@ const fetch = async (_: any, _1: any, { getFromBlock, getToBlock, createBalances
     return acc + gasUsed * effectiveGasPrice;
   }, 0);
 
-  dailyGas.add(ADDRESSES.null, dailyGasUsed)
-  dailyPayment.add(LINK, totalPayment, { skipChain: true })
-  dailyRevenue.addUSDValue(await dailyPayment.getUSDValue() - await dailyGas.getUSDValue())
+  dailyGas.add(ADDRESSES.null, dailyGasUsed);
+  dailyPayment.add(LINK, totalPayment, { skipChain: true });
+  dailyRevenue.addUSDValue(
+    (await dailyPayment.getUSDValue()) - (await dailyGas.getUSDValue())
+  );
 
-  return { dailyFees: dailyPayment, dailyRevenue }
-}
+  return { dailyFees: dailyPayment, dailyRevenue };
+};
 
 const adapter: SimpleAdapter = {
   version: 1,
   adapter: {
+    // Start dates based on first deployed registry contract
     [CHAIN.ETHEREUM]: {
       fetch,
-      start: '2023-02-03',
+      start: "2023-09-14",
     },
     [CHAIN.BSC]: {
       fetch,
-      start: '2023-02-03',
+      start: "2023-09-13",
     },
     [CHAIN.POLYGON]: {
       fetch,
-      start: '2023-02-03',
-    },
-    [CHAIN.FANTOM]: {
-      fetch,
-      start: '2023-02-03',
+      start: "2023-09-13",
     },
     [CHAIN.AVAX]: {
       fetch,
-      start: '2023-02-03',
+      start: "2023-09-13",
     },
     [CHAIN.ARBITRUM]: {
       fetch,
-      start: '2023-02-03',
+      start: "2023-09-13",
     },
     [CHAIN.OPTIMISM]: {
       fetch,
-      start: '2023-02-03'
-    }
-  }
-}
+      start: "2025-06-13",
+    },
+    [CHAIN.BASE]: {
+      fetch,
+      start: "2024-09-05",
+    },
+    [CHAIN.XDAI]: {
+      fetch,
+      start: "2024-02-28",
+    },
+    [CHAIN.POLYGON_ZKEVM]: {
+      fetch,
+      start: "2024-11-05",
+    },
+    [CHAIN.SCROLL]: {
+      fetch,
+      start: "2024-11-01",
+    },
+    [CHAIN.ERA]: {
+      fetch,
+      start: "2024-11-12",
+    },
+  },
+};
 export default adapter;
