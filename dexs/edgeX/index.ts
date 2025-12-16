5087dafac6f683c353f72f0dd768000d584d7aec
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-12-16 10:11:48 +0530

    add runAtCurrTime flag

--- dexs/edgeX/index.ts
+++ dexs/edgeX/index.ts
@@ -4,13 +4,13 @@ import { CHAIN } from "../../helpers/chains";
 
 const summaryEndpoint = "https://pro.edgex.exchange/api/v1/public/quote/getTicketSummary?period=LAST_DAY_1";
 
-const fetch = async (_a:any,_b:any,_c:any): Promise<FetchResultVolume> => {
+const fetch = async (_a: any, _b: any, _c: any): Promise<FetchResultVolume> => {
   const previousDayTradeSummary = await fetchURL(summaryEndpoint);
-  
+
   const openInterestAtEnd = previousDayTradeSummary.data.tickerSummary.openInterest;
   const dailyVolume = previousDayTradeSummary.data.tickerSummary.value;
 
-  return{
+  return {
     dailyVolume,
     openInterestAtEnd
   }
@@ -21,6 +21,7 @@ const adapter: SimpleAdapter = {
     [CHAIN.EDGEX]: {
       fetch,
       start: '2024-08-06',
+      runAtCurrTime: true,
     },
   },
 };
