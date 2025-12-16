8f07b361c637c6c9265c0bab505ddfcf37664b23
Smriti-08 <smriti@lampros.tech>
2025-12-16 18:02:49 +0530

    Added fees for Edu-Chain, Ethereal Mainnet, Eventum, Evercelar and Flynet

--- helpers/chains.ts
+++ helpers/chains.ts
@@ -295,4 +295,9 @@ export enum CHAIN {
   DERI_CHAIN = "deri_chain",
   BABYLON = "babylon",
   DUCK_CHAIN = "duckchain",
+  EDU_CHAIN = "edu_chain",
+  ETHEREAL_MAINNET = "ethereal_mainnet",
+  EVENTUM = "eventum",
+  EVERCLEAR = "everclear",
+  FLYNET = "flynet",
 }
