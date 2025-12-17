a59c1482a0eb3f0f55c9ea68de9f35f66723c2db
tree <sol.analyzoor@gmail.com>
2025-12-17 22:41:36 +0530

    Fix: trojan fees calc from solana activity table (#5190)

--- fees/primordium.ts
+++ fees/primordium.ts
@@ -1,31 +1,46 @@
+import ADDRESSES from "../helpers/coreAssets.json";
 import { Dependencies, FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { queryDuneSql } from "../helpers/dune";
 
 // Trojan Fee Wallet: 9yMwSPk9mrXSN7yDHUuZurAh1sjbJsfpUqjZ7SvVtdco
 
-const fetch = async (_a:any, _b:any, options: FetchOptions) => {  
+const fetch = async (_a:any, _b:any, options: FetchOptions) => {
   const query = `
-    WITH botTrades AS (
-        SELECT
-            block_time,
-            amount_usd,
-            fee_usd
-        FROM
-            trojan_solana.bot_trades
-        WHERE
-            blockchain = 'solana'
-            AND is_last_trade_in_transaction = true
-            AND TIME_RANGE
+    WITH
+    allFeePayments AS (
+      SELECT
+        tx_id,
+        balance_change AS fee_token_amount
+      FROM
+        solana.account_activity
+      WHERE
+        TIME_RANGE
+        AND tx_success
+        AND address = '9yMwSPk9mrXSN7yDHUuZurAh1sjbJsfpUqjZ7SvVtdco'
+        AND balance_change > 0 
+    ),
+    botTrades AS (
+      SELECT
+        trades.tx_id,
+        MAX(fee_token_amount) AS fee
+      FROM
+        dex_solana.trades AS trades
+        JOIN allFeePayments AS feePayments ON trades.tx_id = feePayments.tx_id
+      WHERE
+        TIME_RANGE
+        AND trades.trader_id != '7LCZckF6XXGQ1hDY6HFXBKWAtiUgL9QY5vj1C4Bn1Qjj'
+      GROUP BY trades.tx_id
     )
     SELECT
-        SUM(fee_usd) AS dailyFees
+      SUM(fee) AS fee
     FROM
-        botTrades
+      botTrades
   `;
-  const data = await queryDuneSql(options, query);
+
+  const fees = await queryDuneSql(options, query);
   const dailyFees = options.createBalances();
-  dailyFees.addUSDValue(data[0].dailyFees);
+  dailyFees.add(ADDRESSES.solana.SOL, fees[0].fee);
 
   return { dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees }
 }
