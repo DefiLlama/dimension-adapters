e5be715205d4b16740ef2f042c6b053f5057dc47
0x4r45h <0x4r45h@gmail.com>
2025-12-18 18:12:24 +0330

    combine queries into a single one add COALESCE to handle nulls remove unused selected columns

--- fees/nexus-mutual.ts
+++ fees/nexus-mutual.ts
@@ -2,73 +2,72 @@ import {Adapter, Dependencies, FetchOptions } from "../adapters/types";
 import {CHAIN} from "../helpers/chains";
 import {queryDuneSql} from "../helpers/dune";
 
-interface ICoversData {
+interface ICombinedData {
   premium_usd: number;
-}
-interface IClaimsData {
   claim_total: number;
 }
+
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-  const queryCoversData = `
+  const mergedQuery = `
       WITH daily_avg_prices AS (
-          SELECT
-              block_date,
-              avg_eth_usd_price,
-              avg_dai_usd_price,
-              avg_nxm_eth_price,
-              avg_nxm_usd_price
-          FROM nexusmutual_ethereum.capital_pool_prices
+            SELECT
+                block_date,
+                avg_eth_usd_price,
+                avg_dai_usd_price,
+                avg_nxm_eth_price,
+                avg_nxm_usd_price
+            FROM nexusmutual_ethereum.capital_pool_prices
       ),
            covers AS (
                SELECT
-                   c.cover_id,
-                   cover_start_date,
-                   cover_end_date,
-                   c.premium_asset,
-                   c.premium * IF(c.cover_asset = 'DAI', p.avg_dai_usd_price, p.avg_eth_usd_price) AS premium_usd,
-                   c.premium * IF(c.cover_asset = 'DAI', p.avg_dai_usd_price, p.avg_eth_usd_price) / p.avg_eth_usd_price AS premium_eth
+                   c.premium * IF(c.cover_asset = 'DAI', p.avg_dai_usd_price, p.avg_eth_usd_price) AS premium_usd
                FROM nexusmutual_ethereum.covers_v1 c
                         INNER JOIN daily_avg_prices p ON c.block_date = p.block_date
+               WHERE c.cover_start_date >= FROM_UNIXTIME(${options.startTimestamp})
+                 AND c.cover_start_date < FROM_UNIXTIME(${options.endTimestamp})
 
                UNION ALL
                
                SELECT
-                   c.cover_id,
-                   cover_start_date,
-                   cover_end_date,
-                   c.premium_asset,
-                   c.premium_incl_commission * p.avg_nxm_usd_price AS premium_usd,
-                   c.premium_incl_commission * p.avg_nxm_eth_price AS premium_eth
+                   c.premium_incl_commission * p.avg_nxm_usd_price AS premium_usd
                FROM query_4599092 c
                         INNER JOIN daily_avg_prices p ON c.block_date = p.block_date
                WHERE c.is_migrated = FALSE
+                 AND c.cover_start_date >= FROM_UNIXTIME(${options.startTimestamp})
+                 AND c.cover_start_date < FROM_UNIXTIME(${options.endTimestamp})
+           ),
+           total_premiums AS (
+               SELECT SUM(premium_usd) AS premium_usd
+               FROM covers
+           ),
+           total_claims AS (
+               SELECT
+                   SUM(
+                           eth_usd_claim_amount +
+                           dai_usd_claim_amount +
+                           usdc_usd_claim_amount +
+                           cbbtc_usd_claim_amount
+                   ) AS claim_total
+               FROM query_5785588  -- claims paid - base root
+               WHERE claim_date >= FROM_UNIXTIME(${options.startTimestamp})
+                 AND claim_date < FROM_UNIXTIME(${options.endTimestamp})
            )
-      SELECT SUM(premium_usd) AS premium_usd
-      FROM covers
-      WHERE cover_start_date >= FROM_UNIXTIME(${options.startTimestamp})
-        AND cover_start_date < FROM_UNIXTIME(${options.endTimestamp});
-  `
 
-  const queryClaimsData = `
       SELECT
-          SUM(
-               eth_usd_claim_amount +
-               dai_usd_claim_amount +
-               usdc_usd_claim_amount +
-               cbbtc_usd_claim_amount
-          ) AS claim_total
-      FROM query_5785588  -- claims paid - base root
-      WHERE claim_date >= FROM_UNIXTIME(${options.startTimestamp})
-        AND claim_date < FROM_UNIXTIME(${options.endTimestamp});
+          COALESCE(total_premiums.premium_usd, 0) AS premium_usd,
+          COALESCE(total_claims.claim_total, 0) AS claim_total
+      FROM total_premiums
+               CROSS JOIN total_claims;
   `;
-  const coversPromise: Promise<ICoversData[]> = queryDuneSql(options, queryCoversData);
-  const claimsPromise: Promise<IClaimsData[]> = queryDuneSql(options, queryClaimsData);
 
-  const [coversData,claimsData] : [ICoversData[], IClaimsData[]] = await Promise.all([coversPromise,claimsPromise]);
+  const combinedData: ICombinedData[] = await queryDuneSql(options, mergedQuery);
+
+  const data = combinedData[0];
 
   const dailyFees = options.createBalances();
-  dailyFees.addUSDValue(coversData[0].premium_usd - claimsData[0].claim_total)
-  const dailyRevenue = dailyFees.clone(0.5)
+  dailyFees.addUSDValue(data.premium_usd - data.claim_total);
+
+  const dailyRevenue = dailyFees.clone(0.5);
 
   return {
     dailyFees,
