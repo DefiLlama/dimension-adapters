c285937813ac36f8293ab962c1661ec72efdda56
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-12-20 11:42:50 +0100

    Refactor fee fetching logic and update exports
    Updated the fetch function to use logs from subManagement for daily fees calculation. Removed unused imports and adjusted the export structure.

--- fees/digift/index.ts
+++ fees/digift/index.ts
@@ -1,39 +1,32 @@
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { addTokensReceived } from "../../helpers/token"
-import CoreAssets from "../../helpers/coreAssets.json";
-import { METRIC } from "../../helpers/metrics";
 
-const management = '0x96ABd1C04882BCe372203d43649be525F8AE87ba'
-const usdc = CoreAssets.ethereum.USDC
+const subManagement = '0x3797C46db697c24a983222c335F17Ba28e8c5b69'
 
 const fetch = async (options: FetchOptions) => {
-    const dailyFees = options.createBalances();
-    const feeAddress = await options.api.call({
-        abi: "address:platformFeeAddress",
-        target: management
-    });
-    const transfers = await addTokensReceived({
-        options,
-        tokens: [usdc],
-        target: feeAddress
-    })
-    dailyFees.addBalances(transfers, METRIC.MINT_REDEEM_FEES)
-    return {
-        dailyFees,
-        dailyRevenue: dailyFees,
-        dailyUserFees: dailyFees,
-    }
+  const dailyFees = options.createBalances()
+  const logs = await options.getLogs({
+    target: subManagement,
+    eventAbi: "event TransferPlatformFee(address indexed from, address indexed token, uint256 amount)",
+  })
+
+  logs.forEach(log => {
+    dailyFees.add(log.token, log.amount);
+  })
+
+  return {
+    dailyFees,
+    dailyRevenue: dailyFees,
+  }
 }
-const adapters : SimpleAdapter = {
-    version: 2,
-    fetch,
-    chains: [CHAIN.ETHEREUM],
-    start: '2024-01-08',
-    methodology: {
-        Fees: "Platform fees on subscription and redemption of tokens",
-        Revenue: "Platform fees paid by users on subscription and redemption",
-        UserFees: "Platform fees paid by users on subscription and redemption",
-    }
+
+export default {
+  version: 2,
+  fetch,
+  chains: [CHAIN.ETHEREUM],
+  start: '2024-05-24',
+  methodology: {
+    Fees: "Platform fees on subscription and redemption of tokens",
+    Revenue: "Platform fees paid by users on subscription and redemption",
+  }
 };
-export default adapters;
