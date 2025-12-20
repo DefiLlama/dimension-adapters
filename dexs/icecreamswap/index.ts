aa3937e8c6813cb86d44657a580f35b012415b71
Arash <0x4r45h@gmail.com>
2025-12-20 13:14:45 +0330

    IceCreamSwap fees & revenue (#5284)

--- dexs/icecreamswap/index.ts
+++ dexs/icecreamswap/index.ts
@@ -1,7 +1,31 @@
-import { CHAIN } from "../../helpers/chains";
-import { uniV2Exports } from "../../helpers/uniswap";
+import {CHAIN} from "../../helpers/chains";
+import {uniV2Exports} from "../../helpers/uniswap";
+import {SimpleAdapter} from "../../adapters/types";
 
-export default uniV2Exports({
-  [CHAIN.BITGERT]: { factory: '0x9E6d21E759A7A288b80eef94E4737D313D31c13f'},
-  [CHAIN.CORE]: { factory: '0x9E6d21E759A7A288b80eef94E4737D313D31c13f'},
-});
+const FACTORY_ADDRESS = '0x9E6d21E759A7A288b80eef94E4737D313D31c13f';
+
+const methodology = {
+  UserFees: "Users pays 0.3% of each swap",
+  Fees: "A 0.3% trading fee is collected",
+  Revenue: "A 1/6 fees goes to the protocol",
+  SupplySideRevenue: "5/6 of trading fees are distributed among liquidity providers."
+}
+
+const adapters: SimpleAdapter = uniV2Exports({
+  [CHAIN.BITGERT]: {
+    factory: FACTORY_ADDRESS,
+    fees: 0.003,
+    revenueRatio: 1 / 6,
+    userFeesRatio: 1
+  },
+  [CHAIN.CORE]: {
+    factory: FACTORY_ADDRESS,
+    fees: 0.003,
+    revenueRatio: 1 / 6,
+    userFeesRatio: 1,
+  },
+})
+
+adapters.methodology = methodology;
+
+export default adapters;
