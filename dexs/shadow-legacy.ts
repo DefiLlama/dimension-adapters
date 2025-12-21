2eee8dbc4f3e94a23ee73876a59cd6a95250fa47
Felix Bruguera <felixbruguera0@gmail.com>
2025-12-21 08:57:00 -0400

    refactor shadow

--- dexs/shadow-legacy.ts
+++ dexs/shadow-legacy.ts
@@ -1,48 +1,154 @@
-import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import * as sdk from '@defillama/sdk';
+import { FetchOptions, FetchResult, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { fetchStats } from "./shadow-exchange";
+import { addOneToken } from '../helpers/prices';
+import { ethers } from "ethers";
+import PromisePool from "@supercharge/promise-pool";
 
-type TStartTime = {
-  [key: string]: number;
-};
+// Fee split source: https://docs.shadow.so/pages/x-33#fee-split
 
-const startTimeV2: TStartTime = {
-  [CHAIN.SONIC]: 1735129946,
-};
+const CONFIG = {
+  factory: '0x2dA25E7446A70D7be65fd4c053948BEcAA6374c8',
+  voter: '0x9f59398d0a397b2eeb8a6123a6c7295cb0b0062d',
+  treasury: '0xE25E95F75432A79D31256CC3026E24AAA5540882'
+}
 
-const fetch = async (_: any, _1: any, options: FetchOptions) => {
-  const stats = await fetchStats(options);
 
-  const dailyFees = stats.legacyFeesUSD;
-  const dailyVolume = stats.legacyVolumeUSD;
-  const dailyHoldersRevenue = stats.legacyUserFeesRevenueUSD;
-  const dailyProtocolRevenue = stats.legacyProtocolRevenueUSD;
-  const dailyBribesRevenue = stats.legacyBribeRevenueUSD;
+const eventAbis = {
+  event_poolCreated: 'event PairCreated(address indexed token0, address indexed token1, address pair, uint256)',
+  event_swap: 'event Swap(address indexed sender, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out, address indexed to)',
+  event_gaugeCreated: 'event GaugeCreated(address indexed gauge, address creator, address feeDistributor, address indexed pool)',
+  event_notify_reward: 'event NotifyReward(address indexed from, address indexed reward, uint256 amount, uint256 period)',
+}
 
-  return {
-    dailyVolume,
-    dailyFees,
-    dailyUserFees: dailyFees,
-    dailyHoldersRevenue,
-    dailyProtocolRevenue,
-    dailyRevenue: dailyProtocolRevenue + dailyHoldersRevenue,
-    dailySupplySideRevenue: dailyFees - dailyHoldersRevenue - dailyProtocolRevenue,
-    dailyBribesRevenue,
-  };
-};
+const abis = {
+  fee: 'uint256:fee'
+}
+const firstBlock = 4028276
 
+
+export const getBribes = async (fetchOptions: FetchOptions, gaugeCreatedEvent: string, voter: string, factory: string): Promise<{ dailyBribesRevenue: sdk.Balances }> => {
+  const { createBalances, getLogs } = fetchOptions
+  const iface = new ethers.Interface([eventAbis.event_notify_reward]);
+  const dailyBribesRevenue = createBalances()
+  const logs_gauge_created = await getLogs({ target: voter, fromBlock: firstBlock, eventAbi: gaugeCreatedEvent, skipIndexer: true, entireLog: true })
+  if (!logs_gauge_created?.length) return { dailyBribesRevenue };
+  const bribes_contract: string[] = logs_gauge_created
+    .filter((log) => (log.address || log.source).toLowerCase() === voter.toLowerCase())
+  const pools = bribes_contract.map(log => log.args.pool.toLowerCase())
+  const poolsFactories = (await fetchOptions.api.multiCall({ abi:'address:factory', calls: pools}))
+  const bribes_contracts_v2 = bribes_contract.filter((_, index) => poolsFactories[index].toLowerCase() === factory.toLowerCase()).map((log) => log.args.feeDistributor.toLowerCase())
+  const bribeSet = new Set(bribes_contracts_v2)
+
+  const logs = await getLogs({ noTarget: true, topic: '0x52977ea98a2220a03ee9ba5cb003ada08d394ea10155483c95dc2dc77a7eb24b', entireLog: true })
+  logs.forEach((log: any) => {
+    const contract = (log.address || log.source).toLowerCase()
+    if (!bribeSet.has(contract)) return;
+    const parsedLog = iface.parseLog(log)
+    const token = parsedLog!.args.reward
+    const amount = parsedLog!.args.amount
+    dailyBribesRevenue.add(token, amount)
+  })
+  return { dailyBribesRevenue }
+}
+
+const fetch = async (_: any, _1: any, fetchOptions: FetchOptions): Promise<FetchResult> => {
+  const { api, createBalances, getToBlock, getFromBlock, chain, getLogs } = fetchOptions
+  const dailyVolume = createBalances()
+  const dailyFees = createBalances()
+  const dailyHoldersRevenue = createBalances()
+  const dailySupplySideRevenue = createBalances()
+  const dailyProtocolRevenue = createBalances() 
+  const [toBlock, fromBlock] = await Promise.all([getToBlock(), getFromBlock()])
+
+  const rawPools = await getLogs({ target: CONFIG.factory, fromBlock: firstBlock, toBlock, eventAbi: eventAbis.event_poolCreated, skipIndexer: true})
+  const _pools = rawPools.map((i: any) => i.pair.toLowerCase())
+  const fees = await api.multiCall({ abi: abis.fee,  calls: _pools })
+  const feeRecipients = await api.multiCall({ abi: 'address:feeRecipient', calls: _pools })
+  const aeroPoolSet = new Set()
+  const poolInfoMap = {} as any
+  rawPools.forEach(({ token0, token1, pair }, index) => {
+    let pool = pair.toLowerCase()
+    const fee = fees[index] / 1e6
+    const hasGauge = feeRecipients[index] !== CONFIG.treasury
+    poolInfoMap[pool] = { token0, token1, fee, hasGauge }
+    aeroPoolSet.add(pool)
+  })
+
+  const blockStep = 1000;
+  let i = 0;
+  let startBlock = fromBlock;
+  let ranges: any = []
+  const iface = new ethers.Interface([eventAbis.event_swap]);
+
+
+  while (startBlock < toBlock) {
+    const endBlock = Math.min(startBlock + blockStep - 1, toBlock)
+    ranges.push([startBlock, endBlock])
+    startBlock += blockStep
+  }
+
+  let errorFound = false
+
+
+  await PromisePool
+    .withConcurrency(5)
+    .for(ranges)
+    .process(async ([startBlock, endBlock]: any) => {
+      if (errorFound) return;
+      try {
+        const logs = await fetchOptions.getLogs({
+          noTarget: true,
+          fromBlock: startBlock,
+          toBlock: endBlock,
+          eventAbi: eventAbis.event_swap,
+          entireLog: true,
+          skipCache: true,
+        })
+        logs.forEach((log: any) => {
+          const pool = (log.address || log.source).toLowerCase()
+          if (!aeroPoolSet.has(pool)) return;
+          const { token0, token1, fee, hasGauge } = poolInfoMap[pool]
+          const parsedLog = iface.parseLog(log)
+          const amount0 = Number(parsedLog!.args.amount0In) + Number(parsedLog!.args.amount0Out)
+          const amount1 = Number(parsedLog!.args.amount1In) + Number(parsedLog!.args.amount1Out)
+          const fee0 = amount0 * fee
+          const fee1 = amount1 * fee
+          addOneToken({ chain, balances: dailyVolume, token0, token1, amount0, amount1 })
+          addOneToken({ chain, balances: dailyFees, token0, token1, amount0: fee0, amount1: fee1 })
+          if (hasGauge) {
+            addOneToken({ chain, balances: dailyHoldersRevenue, token0, token1, amount0: fee0, amount1: fee1 })
+          }
+          else {
+            addOneToken({ chain, balances: dailySupplySideRevenue, token0, token1, amount0: fee0 * 0.95, amount1: fee1 * 0.95 })
+            addOneToken({ chain, balances: dailyProtocolRevenue, token0, token1, amount0: fee0 * 0.05, amount1: fee1 * 0.05 })
+          }
+        })
+      } catch (e) {
+        errorFound = e
+        throw e
+      }
+    })
+
+  if (errorFound) throw errorFound
+
+  const { dailyBribesRevenue } = await getBribes(fetchOptions, eventAbis.event_gaugeCreated, CONFIG.voter, CONFIG.factory)
+
+  return { dailyVolume, dailyFees, dailyUserFees: dailyFees, dailyRevenue: dailyFees, dailyHoldersRevenue, dailySupplySideRevenue, dailyProtocolRevenue, dailyBribesRevenue }
+}
 const methodology = {
   UserFees: "User pays fees on each swap.",
   ProtocolRevenue: "Revenue going to the protocol.",
   HoldersRevenue: "User fees are distributed among holders.",
   BribesRevenue: "Bribes are distributed among holders.",
+  SupplySideRevenue: "Fees distributed to LPs (from gauged pools).",
 };
 
 const adapter: SimpleAdapter = {
-  methodology,
+  version: 1,
   fetch,
+  methodology,
   chains: [CHAIN.SONIC],
-  start: startTimeV2[CHAIN.SONIC],
-};
-
+  start: '2025-02-02',
+}
 export default adapter;
