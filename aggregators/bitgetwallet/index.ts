d36d9c6a2c1fbc0ccd7e8052534feaa6c12f6afb
treeoflife2 <sol.analyzoor@gmail.com>
2025-12-24 18:41:29 +0530

    refactor

--- aggregators/bitgetwallet/index.ts
+++ aggregators/bitgetwallet/index.ts
@@ -5,77 +5,61 @@ import fetchURL from "../../utils/fetchURL";
 const historicalVolumeEndpoint = "https://api-3rd.bitkeep.com/swap-go/open/getOrderDayVolume"
 
 interface IVolumeall {
-    volume: string;
-    date: string;
+  volume: string;
+  date: string;
 }
 
 // to compute volume on chain: https://github.com/DefiLlama/dimension-adapters/pull/2059#issuecomment-2469986758
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-    const chain = options.chain;
-    const startOfDay = options.startOfDay;
-    const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint + `?chain=${chain}`))?.data?.list;
-    const dailyVolume = historicalVolume?.find(dayItem => (new Date(dayItem.date).getTime() / 1000) === startOfDay)?.volume
-    return {
-        dailyVolume
-    };
+
+  const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint + `?chain=${options.chain}`))?.data?.list;
+  const dailyVolume = historicalVolume?.find(dayItem => (new Date(dayItem.date).getTime() / 1000) === options.startOfDay)?.volume
+
+  return { dailyVolume };
 }
 
 const CHAINS: Array<CHAIN> = [
-    CHAIN.APTOS,
-    CHAIN.HYPERLIQUID,
-    CHAIN.SOLANA,
-    CHAIN.BLAST,
-    CHAIN.BITCOIN,
-    CHAIN.ARBITRUM,
-    CHAIN.KLAYTN,
-    CHAIN.SONIC,
-    CHAIN.MANTLE,
-    CHAIN.RIPPLE,
-    CHAIN.AVAX,
-    CHAIN.LINEA,
-    CHAIN.SUI,
-    CHAIN.SCROLL,
-    CHAIN.BASE,
-    CHAIN.POLYGON,
-    CHAIN.TON,
-    CHAIN.CRONOS,
-    CHAIN.DOGECHAIN,
-    CHAIN.BERACHAIN,
-    CHAIN.MONAD,
-    CHAIN.TRON,
-    CHAIN.CELO,
-    CHAIN.BSC,
-    CHAIN.MORPH,
-    CHAIN.XLAYER,
-    CHAIN.CORE,
-    CHAIN.OP_BNB,
-    CHAIN.ZKSYNC,
-    CHAIN.ETHEREUM,
-    CHAIN.OPTIMISM,
-    CHAIN.FANTOM,
-    CHAIN.PLASMA,
-    CHAIN.SEI
+  CHAIN.APTOS,
+  CHAIN.HYPERLIQUID,
+  CHAIN.SOLANA,
+  CHAIN.BLAST,
+  CHAIN.BITCOIN,
+  CHAIN.ARBITRUM,
+  CHAIN.KLAYTN,
+  CHAIN.SONIC,
+  CHAIN.MANTLE,
+  CHAIN.RIPPLE,
+  CHAIN.AVAX,
+  CHAIN.LINEA,
+  CHAIN.SUI,
+  CHAIN.SCROLL,
+  CHAIN.BASE,
+  CHAIN.POLYGON,
+  CHAIN.TON,
+  CHAIN.CRONOS,
+  CHAIN.DOGECHAIN,
+  CHAIN.BERACHAIN,
+  CHAIN.MONAD,
+  CHAIN.TRON,
+  CHAIN.CELO,
+  CHAIN.BSC,
+  CHAIN.MORPH,
+  CHAIN.XLAYER,
+  CHAIN.CORE,
+  CHAIN.OP_BNB,
+  CHAIN.ZKSYNC,
+  CHAIN.ETHEREUM,
+  CHAIN.OPTIMISM,
+  CHAIN.FANTOM,
+  CHAIN.PLASMA,
+  CHAIN.SEI
 ];
 
-
-
 const adapter: SimpleAdapter = {
-    version: 1,
-    adapter: {
-        ...CHAINS.map(chain => {
-            return {
-                [chain]: {
-                    fetch,
-                    start: '2025-09-01'
-                }
-            }
-        }).reduce((acc, item) => {
-            return {
-                ...acc,
-                ...item
-            }
-        })
-    }
+  version: 1,
+  fetch,
+  chains: CHAINS,
+  start: '2025-09-01',
 };
 
 export default adapter;
--- bridge-aggregators/bitgetwallet/index.ts
+++ bridge-aggregators/bitgetwallet/index.ts
@@ -3,83 +3,70 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import fetchURL from "../../utils/fetchURL";
 
 const CHAINS: Array<CHAIN> = [
-    CHAIN.APTOS,
-    CHAIN.HYPERLIQUID,
-    CHAIN.SOLANA,
-    CHAIN.BLAST,
-    CHAIN.BITCOIN,
-    CHAIN.ARBITRUM,
-    CHAIN.KLAYTN,
-    CHAIN.SONIC,
-    CHAIN.MANTLE,
-    CHAIN.RIPPLE,
-    CHAIN.AVAX,
-    CHAIN.LINEA,
-    CHAIN.SUI,
-    CHAIN.SCROLL,
-    CHAIN.BASE,
-    CHAIN.POLYGON,
-    CHAIN.TON,
-    CHAIN.CRONOS,
-    CHAIN.DOGECHAIN,
-    CHAIN.BERACHAIN,
-    CHAIN.MONAD,
-    CHAIN.TRON,
-    CHAIN.CELO,
-    CHAIN.BSC,
-    CHAIN.MORPH,
-    CHAIN.XLAYER,
-    CHAIN.CORE,
-    CHAIN.OP_BNB,
-    CHAIN.ZKSYNC,
-    CHAIN.ETHEREUM,
-    CHAIN.OPTIMISM,
-    CHAIN.FANTOM,
-    CHAIN.PLASMA,
-    CHAIN.SEI
+  CHAIN.APTOS,
+  CHAIN.HYPERLIQUID,
+  CHAIN.SOLANA,
+  CHAIN.BLAST,
+  CHAIN.BITCOIN,
+  CHAIN.ARBITRUM,
+  CHAIN.KLAYTN,
+  CHAIN.SONIC,
+  CHAIN.MANTLE,
+  CHAIN.RIPPLE,
+  CHAIN.AVAX,
+  CHAIN.LINEA,
+  CHAIN.SUI,
+  CHAIN.SCROLL,
+  CHAIN.BASE,
+  CHAIN.POLYGON,
+  CHAIN.TON,
+  CHAIN.CRONOS,
+  CHAIN.DOGECHAIN,
+  CHAIN.BERACHAIN,
+  CHAIN.MONAD,
+  CHAIN.TRON,
+  CHAIN.CELO,
+  CHAIN.BSC,
+  CHAIN.MORPH,
+  CHAIN.XLAYER,
+  CHAIN.CORE,
+  CHAIN.OP_BNB,
+  CHAIN.ZKSYNC,
+  CHAIN.ETHEREUM,
+  CHAIN.OPTIMISM,
+  CHAIN.FANTOM,
+  CHAIN.PLASMA,
+  CHAIN.SEI
 ];
 
 interface IVolumeBridge {
-    volume: string;
-    date: string;
+  volume: string;
+  date: string;
 }
 
 async function queryDataByApi(path: string) {
-    const historicalVolumeEndpoint = "https://api-3rd.bitkeep.com/swap-go/open";
-    let info = await fetchURL(`${historicalVolumeEndpoint}${path}`);
-    const data: IVolumeBridge[] = (info)?.data?.list || [];
-    return data
+  const historicalVolumeEndpoint = "https://api-3rd.bitkeep.com/swap-go/open";
+  let info = await fetchURL(`${historicalVolumeEndpoint}${path}`);
+  const data: IVolumeBridge[] = (info)?.data?.list || [];
+  return data
 }
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-    const startOfDay = options.startOfDay;
-    const path = `/getOrderDayVolume?bridge=1&chain=${options.chain}&timestamp=${startOfDay}`
-    const data = await queryDataByApi(path)
-    const dateString = new Date(startOfDay * 1000).toISOString().split("T")[0];
-    const dailyVolume = data.find(dayItem => dayItem.date === dateString)?.volume
+  const path = `/getOrderDayVolume?bridge=1&chain=${options.chain}&timestamp=${options.startOfDay}`
+  const data = await queryDataByApi(path)
+  const dateString = new Date(options.startOfDay * 1000).toISOString().split("T")[0];
+  const dailyVolume = data.find(dayItem => dayItem.date === dateString)?.volume
 
-    return {
-        dailyBridgeVolume: dailyVolume || 0,
-    };
+  return {
+    dailyBridgeVolume: dailyVolume || 0,
+  };
 };
 
 const adapter: SimpleAdapter = {
-    version: 1,
-    adapter: {
-        ...CHAINS.map(chain => {
-            return {
-                [chain]: {
-                    fetch,
-                    start: '2025-08-01'
-                }
-            }
-        }).reduce((acc, item) => {
-            return {
-                ...acc,
-                ...item
-            }
-        })
-    },
+  version: 1,
+  fetch,
+  chains: CHAINS,
+  start: '2025-08-01',
 };
 
 export default adapter;
