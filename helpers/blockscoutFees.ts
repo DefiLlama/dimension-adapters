6a795dc31a56a38b627871cfb0db2802eb9b7f03
Smriti-08 <smriti@lampros.tech>
2025-12-24 10:11:56 +0530

    Update XChain explorer URL in blockscoutFees.ts

--- helpers/blockscoutFees.ts
+++ helpers/blockscoutFees.ts
@@ -95,7 +95,7 @@ export const chainConfigMap: any = {
   [CHAIN.MIND_NETWORK]: { CGToken: 'ethereum', explorer: 'https://explorer.mindnetwork.xyz/' },
   [CHAIN.MOLTEN_NETWORK]: { CGToken: 'molten-2', explorer: 'https://molten.calderaexplorer.xyz/' },
   [CHAIN.SYNDICATE]: { CGToken: 'syndicate-3', explorer: 'https://explorer.syndicate.io/' },
-  [CHAIN.XCHAIN]: { CGToken: 'ethereum', explorer: 'https://xchain-explorer.idex.io/' },
+  [CHAIN.XCHAIN]: { CGToken: 'ethereum', explorer: 'https://xchain-explorer.kuma.bid/' },
  
 }
 
