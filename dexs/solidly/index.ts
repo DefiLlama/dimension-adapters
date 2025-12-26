82b552c3e577ad72b9c6de6dc220b4b3df9350b3
Smriti-08 <smriti@lampros.tech>
2025-12-26 16:22:10 +0530

    Removed fees/solidly/index.ts and updated dexs/solidly/index.ts

--- dexs/solidly/index.ts
+++ dexs/solidly/index.ts
@@ -2,5 +2,9 @@ import { CHAIN } from "../../helpers/chains";
 import { uniV2Exports } from "../../helpers/uniswap";
 
 export default uniV2Exports({
-  [CHAIN.FANTOM]: { factory: '0x3fAaB499b519fdC5819e3D7ed0C26111904cbc28', },
-})
+  [CHAIN.FANTOM]: {
+    factory: "0x3fAaB499b519fdC5819e3D7ed0C26111904cbc28",
+    fees: 0.0025,    // 0.25% for volatile pairs
+    stableFees: 0.0001, // 0.01% for stable pairs
+  },
+});
