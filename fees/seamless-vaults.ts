b059684a0880948569a5fbbba1d550b16315a113
Eden <noat.eth@gmail.com>
2025-12-27 23:03:11 +0700

    use vaults

--- fees/seamless-vaults.ts
+++ fees/seamless-vaults.ts
@@ -4,7 +4,11 @@ import { CuratorConfig, getCuratorExport } from '../helpers/curators';
 const curatorConfig: CuratorConfig = {
   vaults: {
     [CHAIN.BASE]: {
-      morphoVaultOwners: ['0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee'],
+      morpho: [
+        '0x616a4E1db48e22028f6bbf20444Cd3b8e3273738',
+        '0x27D8c7273fd3fcC6956a0B370cE5Fd4A7fc65c18',
+        '0x5a47C803488FE2BB0A0EAaf346b420e4dF22F3C7',
+      ],
       start: '2025-01-21',
     },
   },
