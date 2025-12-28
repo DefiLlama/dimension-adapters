b5b450f129b95d846cc9ca0875e6ba56c068d974
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-12-28 22:12:19 +0100

    throw error

--- dexs/fibonacci-dex.ts
+++ dexs/fibonacci-dex.ts
@@ -1,32 +1,4 @@
-// import { CHAIN } from "../helpers/chains";
-// import { graphDimensionFetch } from "../helpers/getUniSubgraph";
-// import { Adapter } from "../adapters/types";
 
-
-// const fetch = graphDimensionFetch({
-//     graphUrls: {
-//       [CHAIN.FORMNETWORK]: "https://formapi.0xgraph.xyz/api/public/f96b70ac-d704-4e09-b300-ff0fb4992df2/subgraphs/analytics/v0.0.1/gn",
-//     },
-//     dailyVolume: {
-//       factory: "algebraDayData",
-//       field: "volumeUSD",
-//     },
-//     dailyFees: {
-//       factory: "algebraDayData",
-//       field: "feesUSD",
-//     },
-//   });
-  
-//   const adapters: Adapter = {
-//     adapter: {
-//       [CHAIN.FORMNETWORK]: {
-//         fetch,
-//         start: '2024-10-29',
-//       },
-//     },
-//   };
-  
-//   export default adapters;
 
 import { CHAIN } from "../helpers/chains";
 import { Adapter } from "../adapters/types";
@@ -35,10 +7,7 @@ const adapters: Adapter = {
   deadFrom: "2025-08-15", // Form Network has been sunsetted and Fibonacci Dex no longer exists
   adapter: {
     [CHAIN.FORMNETWORK]: {
-      fetch: async () => ({
-        dailyVolume: 0,
-        dailyFees: 0,
-      }),
+      fetch: async () => { throw new Error("Form Network has been sunsetted and Fibonacci Dex no longer exists"); },
       start: "2024-10-29",
     },
   },
