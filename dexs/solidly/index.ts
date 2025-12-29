a608c4a88e316001a8d25d1eccb7615218748ee6
Smriti-08 <smriti@lampros.tech>
2025-12-29 16:32:03 +0530

    Added fees for solidly (#5383)
    * Added fees for solidly
    
    * Removed fees/solidly/index.ts and updated dexs/solidly/index.ts
    
    * Refactor FANTOM chain configuration in index.ts
    
    Updated fees and added additional parameters for the FANTOM chain.
    
    * Remove revenue ratio properties
    
    * Added revenue ratio
    
    Added revenue and fees ratio properties for CHAIN.FANTOM.

--- dexs/solidly/index.ts
+++ dexs/solidly/index.ts
@@ -1,6 +1,15 @@
 import { CHAIN } from "../../helpers/chains";
 import { uniV2Exports } from "../../helpers/uniswap";
 
-export default uniV2Exports({
-  [CHAIN.FANTOM]: { factory: '0x3fAaB499b519fdC5819e3D7ed0C26111904cbc28', },
-})
+export default uniV2Exports({ 
+  [CHAIN.FANTOM]: {
+    factory: '0x3fAaB499b519fdC5819e3D7ed0C26111904cbc28',
+    fees: 0.002,     
+    stableFees: 0.0001, 
+    start: '2022-02-10',  
+    revenueRatio: 1,              
+    protocolRevenueRatio: 0,     
+    holdersRevenueRatio: 1,      
+    userFeesRatio: 1,       
+  }
+});
