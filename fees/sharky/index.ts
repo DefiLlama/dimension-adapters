bd57099f6ca941287a93d0d5f9c03504743c81bb
bheluga <anonymous_cat_llama@protonmail.com>
2025-12-31 06:49:17 +0000

    refactor

--- fees/sharky/index.ts
+++ fees/sharky/index.ts
@@ -1,20 +1,20 @@
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter, Dependencies } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getSolanaReceived } from "../../helpers/token";
 
 /**
- * Shartky Protocol Fee Wallet Address
+ * Sharky Protocol Fee Wallet Address
  * Source: https://dune.com/queries/1939620
  * Official Sharkify Dune dashboard containing the protocol fee collection address
  */
 const FEE_WALLET = "feegKBq3GAfqs9G6muPjdn8xEEZhALLTr2xsigDyxnV";
 
-const fetch = async (options: FetchOptions) => {
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const dailyFees = await getSolanaReceived({
     options,
     target: FEE_WALLET,
   });
-  
+
   return {
     dailyFees,
     dailyRevenue: dailyFees,
@@ -22,18 +22,20 @@ const fetch = async (options: FetchOptions) => {
   };
 };
 
+const methodology = {
+  Fees: "Tracks SOL received by the protocol fee wallet (feegKBq3GAfqs9G6muPjdn8xEEZhALLTr2xsigDyxnV)",
+  Revenue: "All SOL received is considered revenue",
+  ProtocolRevenue: "All the revenue goes to protocol",
+}
+
 const adapter: SimpleAdapter = {
-  version: 2,
-  adapter: {
-    [CHAIN.SOLANA]: {
-      fetch,
-      start: 1704067200, // January 1, 2024
-    },
-  },
-  methodology: {
-    Fees: "Tracks SOL received by the protocol fee wallet (feegKBq3GAfqs9G6muPjdn8xEEZhALLTr2xsigDyxnV). Fee wallet source: https://dune.com/queries/1939620",
-    Revenue: "All SOL received is considered protocol revenue",
-  },
+  version: 1,
+  fetch,
+  chains: [CHAIN.SOLANA],
+  start: '2022-12-01',
+  methodology,
+  isExpensiveAdapter: true,
+  dependencies: [Dependencies.ALLIUM],
 };
 
 export default adapter;
