e0ec822df7a564656d997c85fc8f72ce6a59f37a
treeoflife2 <sol.analyzoor@gmail.com>
2026-01-01 13:14:09 +0530

    improved jup-lend query

--- fees/jupiter-lend/index.ts
+++ fees/jupiter-lend/index.ts
@@ -11,7 +11,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResu
   });
 
   const data: any[] = await queryDuneSql(options, sql);
-
+  console.log(data);
   const df = data.reduce((sum, row) => sum + (row.daily_fees_usd || 0), 0);
   const dssr = data.reduce((sum, row) => sum + (row.daily_supply_side_revenue_usd || 0), 0);
   const dr = data.reduce((sum, row) => sum + (row.daily_revenue_usd || 0), 0);
