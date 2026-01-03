ce7ae445eb157ee336eadc5b0846b6d0232d4e25
Eden <noat.eth@gmail.com>
2026-01-03 22:44:45 +0700

    rename

--- dexs/probable.ts
+++ dexs/probable.ts
@@ -1,13 +1,11 @@
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
+import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
 
 const CONTRACT_ADDRESS = "0xf99f5367ce708c66f0860b77b4331301a5597c86";
 const USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955"; 
 
 const fetch = async (options: FetchOptions) => {
   const dailyVolume = options.createBalances();
-  const dailyFees = options.createBalances();
-  const dailyRevenue = options.createBalances();
 
   const logs = await options.getLogs({
     target: CONTRACT_ADDRESS,
@@ -27,8 +25,7 @@ const fetch = async (options: FetchOptions) => {
 
   return {
     dailyVolume,
-    dailyFees,
-    dailyRevenue,
+    dailyFees: 0, // no fees
   };
 };
 
