14aec1fbdabddea8219b59881e29d95aa15b9619
Smriti-08 <smriti@lampros.tech>
2026-01-06 14:38:49 +0530

    Updated unidex.ts

--- fees/unidex.ts
+++ fees/unidex.ts
@@ -1,106 +1,134 @@
-import { Adapter, FetchResultFees } from "../adapters/types";
-import { CHAIN } from "../helpers/chains";
-import { getTimestampAtStartOfDayUTC } from "../utils/date";
-import { Chain } from "../adapters/types";
-import request, { gql } from "graphql-request";
+import { Adapter, Dependencies, FetchOptions } from "../adapters/types"
+import { CHAIN } from '../helpers/chains'
+import { queryDuneSql } from "../helpers/dune"
 
-type TChainIDs = { [key in Chain]?: number };
-const chainIDs: TChainIDs = {
-  [CHAIN.FANTOM]: 250,
-  [CHAIN.ARBITRUM]: 42161,
-  [CHAIN.OPTIMISM]: 10,
-  [CHAIN.ERA]: 324,
-  [CHAIN.BASE]: 8453,
-  [CHAIN.EVMOS]: 9001,
-};
+const fetch = async (timestamp: number, _a: any, options: FetchOptions) => {
+  const query = `
+    WITH trades AS (
+      SELECT
+        block_time,
+        DATE_TRUNC('day', block_time) AS block_date,
+        user,
+        tx_hash,
+        COALESCE(fee, 0) AS fee,
+        sizeDelta
+      FROM query_4086080
+      WHERE blockchain = 'Arbitrum'
+    ), vaultInteractions AS (
+      SELECT
+        block_time,
+        DATE_TRUNC('day', block_time) AS block_date,
+        user,
+        tx_hash,
+        0 AS fee,
+        0 AS sizeDelta
+      FROM query_4086117
+      WHERE blockchain = 'Arbitrum'
+    ), integrator_volume AS (
+      SELECT
+        DATE_TRUNC('day', "Date") AS block_date,
+        "volumeUSD" AS integrator_volume
+      FROM query_4060867
+    ), gains_trade_referral_volume_daily AS (
+      SELECT
+        DATE_TRUNC('day', ref_orders.block_time) AS block_date,
+        SUM(
+          ref_orders.position_size_dai * ref_orders.leverage * ref_orders.collateralPriceUsd
+        ) AS gains_volume_usd
+      FROM gains_network_arbitrum.GNSMultiCollatDiamond_evt_ReferrerRewardDistributed AS ref_events
+      JOIN query_3388490 AS ref_orders
+        ON ref_events.evt_tx_hash = ref_orders.tx_hash
+      WHERE
+        ref_events.referrer = 0x8c128f336b479b142429a5f351af225457a987fa
+      GROUP BY 1
+    ), hyperliquid_referral_volume_daily AS (
+      SELECT
+        DATE_TRUNC('day', TRY_CAST(date AS DATE)) AS block_date,
+        SUM(volume) AS hyperliquid_volume_usd,
+        SUM(fees) AS hyperliquid_fees_usd
+      FROM query_5808723
+      GROUP BY 1
+    ), tradesAndVaultInteractions AS (
+      SELECT *
+      FROM trades
+      UNION ALL
+      SELECT *
+      FROM vaultInteractions
+    ), activityByDay AS (
+      SELECT
+        tavi.block_date,
+        SUM(tavi.sizeDelta) AS platform_volumeUSD,
+        COALESCE(MAX(iv.integrator_volume), 0) AS integrator_volumeUSD,
+        SUM(tavi.fee) AS totalFeesUSD
+      FROM tradesAndVaultInteractions AS tavi
+      LEFT JOIN integrator_volume AS iv
+        ON tavi.block_date = iv.block_date
+      GROUP BY tavi.block_date
+    ), combined_data AS (
+      SELECT
+        abd.block_date,
+        abd.platform_volumeUSD,
+        abd.integrator_volumeUSD,
+        COALESCE(gtv.gains_volume_usd, 0) AS gains_volumeUSD,
+        COALESCE(hlv.hyperliquid_volume_usd, 0) AS hyperliquid_volumeUSD,
+        (
+          abd.platform_volumeUSD + abd.integrator_volumeUSD + COALESCE(gtv.gains_volume_usd, 0) + COALESCE(hlv.hyperliquid_volume_usd, 0)
+        ) AS volumeUSD,
+        abd.totalFeesUSD + COALESCE(hlv.hyperliquid_fees_usd, 0) AS totalFeesUSD
+      FROM activityByDay AS abd
+      LEFT JOIN gains_trade_referral_volume_daily AS gtv
+        ON abd.block_date = gtv.block_date
+      LEFT JOIN hyperliquid_referral_volume_daily AS hlv
+        ON abd.block_date = hlv.block_date
+    )
+    SELECT
+      block_date,
+      volumeUSD,
+      totalFeesUSD
+    FROM combined_data
+    WHERE DATE_TRUNC('day', block_date) = DATE_TRUNC('day', FROM_UNIXTIME(${timestamp}))
+    ORDER BY block_date DESC
+  `;
 
-interface IDayProduct {
-  cumulativeFeesUsd: number;
-  chainId: number;
+  const result = await queryDuneSql(options, query);
+
+  if (!result || result.length === 0) {
+    return {
+      dailyVolume: '0',
+      dailyFees: '0',
+      dailyUserFees: '0',
+      dailyRevenue: '0',
+      dailyProtocolRevenue: '0',
+      dailySupplySideRevenue: '0',
+    };
+  }
+
+  const dailyVolume = result[0].volumeUSD || 0;
+  const dailyFees = result[0].totalFeesUSD || 0;
+
+  return {
+    dailyVolume: dailyVolume.toString(),
+    dailyFees: dailyFees.toString(),
+    dailyUserFees: dailyFees.toString(),
+    dailyHoldersRevenue: (dailyFees * 0.65).toString(),
+    dailyProtocolRevenue: dailyFees.toString(),
+    dailySupplySideRevenue: (dailyFees * 0.20).toString(),
+  };
 }
 
-const fetch = (chain: Chain) => {
-  return async (timestamp: number): Promise<FetchResultFees> => {
-    const todaysTimestamp = getTimestampAtStartOfDayUTC(timestamp);
-
-    const graphQuery = gql`
-      query MyQuery {
-        DayProducts(filter: {date: ${todaysTimestamp}}) {
-          cumulativeFeesUsd
-          chainId
-        }
-      }
-    `;
-
-    const endpoint = "https://arkiver.moltennetwork.com/graphql";
-    const response = await request(endpoint, graphQuery);
-    const dayProducts: IDayProduct[] = response.DayProducts;
-
-    const feesByChain: { [chainId: number]: number } = {};
-    dayProducts.forEach((product) => {
-      const chainId = product.chainId;
-      if (chainId === 360) {
-        feesByChain[42161] = (feesByChain[42161] || 0) + product.cumulativeFeesUsd;
-      } else {
-        feesByChain[chainId] = (feesByChain[chainId] || 0) + product.cumulativeFeesUsd;
-      }
-    });
-
-    const chainID = chainIDs[chain];
-    const dailyFeeUSD = chainID !== undefined ? feesByChain[chainID] || 0 : 0;
-
-    const dailyHoldersRevenue = dailyFeeUSD * 0.65;
-    const dailyProtocolRevenue = dailyFeeUSD;
-    const dailySupplySideRevenue = dailyFeeUSD * 0.20;
-
-    return {
-      dailyFees: dailyFeeUSD.toString(),
-      dailyHoldersRevenue: dailyHoldersRevenue.toString(),
-      dailyProtocolRevenue: dailyProtocolRevenue.toString(),
-      dailySupplySideRevenue: dailySupplySideRevenue.toString(),
-      timestamp,
-    };
-  };
-};
-
-const methodology = {
-  Fees: "Fees collected from user trading fees",
-  Revenue: "Fees going to the treasury + holders",
-  SupplySideFees: "Fees going to liquidity providers of the protocol",
-};
-
 const adapter: Adapter = {
-  methodology,
   adapter: {
-    [CHAIN.OPTIMISM]: {
-      fetch: fetch(CHAIN.OPTIMISM),
-      start: '2023-06-22',
-    },
-    [CHAIN.ERA]: {
-      fetch: fetch(CHAIN.ERA),
-      start: '2023-06-22',
-    },
     [CHAIN.ARBITRUM]: {
-      fetch: fetch(CHAIN.ARBITRUM),
-      start: '2023-06-22',
-    },
-    [CHAIN.BASE]: {
-      fetch: fetch(CHAIN.BASE),
-      start: '2023-06-22',
-    },
-    [CHAIN.FANTOM]: {
-      fetch: fetch(CHAIN.FANTOM),
-      start: '2023-06-22',
-    },
-    [CHAIN.METIS]: {
-      fetch: fetch(CHAIN.METIS),
-      start: '2023-06-27',
-    },
-    [CHAIN.EVMOS]: {
-      fetch: fetch(CHAIN.EVMOS),
-      start: '2023-11-16',
+      fetch,
+      start: '2023-10-09',
     },
   },
-};
+  dependencies: [Dependencies.DUNE],
+  methodology: {
+    Fees: "Fees collected from user trading fees",
+    Revenue: "Fees going to the treasury + holders",
+    SupplySideFees: "Fees going to liquidity providers of the protocol",
+  },
+}
 
-export default adapter;
+export default adapter
