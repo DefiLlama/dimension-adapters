496a3a58fbbacdaa57ad163e790a5335dc735d19
bheluga <anonymous_cat_llama@protonmail.com>
2026-01-06 14:32:26 +0530

    refactor

--- fees/groypfi/index.ts
+++ fees/groypfi/index.ts
@@ -1,46 +1,38 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import fetchURL from "../../utils/fetchURL";
 
-const FEE_RECIPIENT = "0:eee00893fff24abaa4f466789ed11a172103cf723e2e206619999edd42b8845944";
+const FEE_RECIPIENT = "0:eee00893fff24abaa4f46678ded11a1721030f723e2e20661999edd42b884594";
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
-  
-  const response = await fetch(
+
+  const data = await fetchURL(
     `https://tonapi.io/v2/blockchain/accounts/${FEE_RECIPIENT}/transactions?limit=1000&start_date=${options.startTimestamp}&end_date=${options.endTimestamp}`
   );
-  
-  const data = await response.json();
-  
-  let totalFees = 0;
-  for (const tx of data.transactions || []) {
-    if (tx.in_msg?.value) {
-      totalFees += tx.in_msg.value;
-    }
-  }
-  
+
+  const totalFees = data.transactions.reduce((acc: number, currTxn: any) => acc + currTxn.in_msg.value, 0);
   dailyFees.addGasToken(totalFees);
-  
+
   return {
     dailyFees,
     dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
   };
 };
 
+const methodology = {
+  Fees: "1% fee on lending, DEX swaps, cross-chain swaps, perps, staking, and NFT marketplace",
+  Revenue: "All the fees are revenue",
+  ProtocolRevenue: "All the revnue goes to protocol"
+}
+
 const adapter: SimpleAdapter = {
   version: 2,
-  adapter: {
-    [CHAIN.TON]: {
-      fetch,
-      start: '2025-01-04',
-      meta: {
-        methodology: {
-          Fees: "1% fee on lending, DEX swaps, cross-chain swaps, perps, staking, and NFT marketplace",
-          Revenue: "100% of fees go to protocol",
-        },
-      },
-    },
-  },
+  fetch,
+  chains: [CHAIN.TON],
+  start: '2025-01-04',
+  methodology,
 };
 
 export default adapter;
