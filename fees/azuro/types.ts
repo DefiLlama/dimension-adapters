a3fb247fe3e63eb05895a10591c78a8b2c5d5293
Javokhir Artykov <55207018+cxxnm@users.noreply.github.com>
2026-01-09 17:51:46 +0500

    migrate to v3 (#5575)
    Co-authored-by: Javokhir Artykov <javokhior@gmail.com>

--- fees/azuro/types.ts
+++ fees/azuro/types.ts
@@ -1,23 +1,10 @@
-export type Maybe<T> = T | null;
-
-export type Scalars = {
-    ID: string;
-    String: string;
-    Boolean: boolean;
-    Int: number;
-    Float: number;
-    BigDecimal: string;
-    BigInt: string;
-    Bytes: string;
-};
-
 export enum BetResult {
     Lost = 'Lost',
     Won = 'Won'
 }
 
 export type Bet = {
-    amount: Scalars['BigDecimal'];
-    odds: Scalars['BigDecimal'];
-    result?: Maybe<BetResult>;
+    amount: number;
+    odds: number;
+    result?: BetResult;
 };
