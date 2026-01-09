7fc86a20e22bfbbe49d73333926f1bafd119f570
treeoflife2 <sol.analyzoor@gmail.com>
2026-01-09 16:33:33 +0530

    refactor

--- fees/openledger/index.ts
+++ fees/openledger/index.ts
@@ -1,11 +1,11 @@
 import { SimpleAdapter, FetchOptions } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import PromisePool from "@supercharge/promise-pool";
 
 const CREDIT_MANAGER = "0x6a2dbd72ad8441A57f6a98521d1fEe199Be22D15";
 const BLOCK_CHUNK_SIZE = 900; // OpenLedger RPC has 1000 block limit
 
-const CreditPurchasedEvent =
-  "event CreditPurchased(address indexed user, uint256 amountPaid, uint256 creditAmount)";
+const CreditPurchasedEvent = "event CreditPurchased(address indexed user, uint256 amountPaid, uint256 creditAmount)";
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
@@ -13,27 +13,33 @@ const fetch = async (options: FetchOptions) => {
   const fromBlock = await options.getFromBlock();
   const toBlock = await options.getToBlock();
 
+  const chunks: { start: number; end: number }[] = [];
   for (let start = fromBlock; start <= toBlock; start += BLOCK_CHUNK_SIZE) {
     const end = Math.min(start + BLOCK_CHUNK_SIZE - 1, toBlock);
-    const logs = await options.getLogs({
-      target: CREDIT_MANAGER,
-      eventAbi: CreditPurchasedEvent,
-      fromBlock: start,
-      toBlock: end,
-    });
-
-    logs.forEach((log: any) => {
-      dailyFees.addCGToken(
-        'openledger-2',
-        Number(log.amountPaid) / 1e18
-      );
-    });
+    chunks.push({ start, end });
   }
+
+  await PromisePool.withConcurrency(5)
+    .for(chunks)
+    .process(async ({ start, end }) => {
+      const logs = await options.getLogs({
+        target: CREDIT_MANAGER,
+        eventAbi: CreditPurchasedEvent,
+        fromBlock: start,
+        toBlock: end,
+      });
+
+      logs.forEach((log: any) => {
+        dailyFees.addCGToken('openledger-2', Number(log.amountPaid) / 1e18);
+      });
+    }
+  );
 
   return {
     dailyFees,
     dailyUserFees: dailyFees,
     dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
   };
 };
 
@@ -48,7 +54,7 @@ const adapter: SimpleAdapter = {
   version: 2,
   fetch,
   chains: [CHAIN.OPENLEDGER],
-  start: "2026-01-01", // must be <= first real tx
+  start: "2026-01-01",
   methodology,
 };
 export default adapter;
