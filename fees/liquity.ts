e69e883bfec7c1860b32208ed84fafda64131fca
eden <noat.eth@gmail.com>
2026-01-12 02:54:49 +0700

    update liquity v1

--- fees/liquity.ts
+++ fees/liquity.ts
@@ -7,7 +7,8 @@ export default {
     [CHAIN.ETHEREUM]: { 
       troveManager: '0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2', 
       stableCoin: ADDRESSES.ethereum.LUSD,
-      holderRevenuePercentage: 100
+      holderRevenuePercentage: 100,
+      protocolRevenuePercentage: 0, // no protocol revenue
     }
   }),
   methodology: {
@@ -22,6 +23,7 @@ export default {
       'Borrow Fees': 'One-time paid borrow fees paid by borrowers.',
       'Redemption Fees': 'Redemption fees paid by borrowers.',
       'Gas Compensation': 'Gas compensations paid to liquidator when trigger liquidations.',
+      'Liquidation Profit': 'On liquidations, there are an amount of profit from ETH collaterals are distributed to stability pool stakers.',
     },
     Revenue: {
       'Borrow Fees': 'One-time paid borrow fees paid by borrowers.',
@@ -33,6 +35,7 @@ export default {
     },
     SupplySideRevenue: {
       'Gas Compensation': 'Gas compensations paid to liquidator when trigger liquidations.',
+      'Liquidation Profit': 'On liquidations, there are an amount of profit from ETH collaterals are distributed to stability pool stakers.',
     },
     ProtocolRevenue: 'No revenue for Liquity protocol.',
   },
