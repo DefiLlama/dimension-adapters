4e9799568fb26fc7aa7fb889f337e309a9d6fb03
eden <noat.eth@gmail.com>
2026-01-12 19:02:33 +0700

    refactor

--- aggregators/tephra/index.ts
+++ aggregators/tephra/index.ts
@@ -3,22 +3,25 @@ import { CHAIN } from "../../helpers/chains";
 
 const TransformedERC20Event = "event TransformedERC20(address indexed taker, address inputToken, address outputToken, uint256 inputTokenAmount, uint256 outputTokenAmount)";
 
-const TEPHRA_AGGREGATOR = "0xde3102F480dE10385680DCBaFA1834945a63273E"
+const FLAT_FEE_RATE = 0.0005; // 0.05%
+const TEPHRA_AGGREGATOR_CONTRACTS = [
+  '0xde3102F480dE10385680DCBaFA1834945a63273E',
+];
 
 const fetch = async (options: FetchOptions) => {
   const dailyVolume = options.createBalances();
-  const dailyFees = options.createBalances();
 
   const logs: any[] = await options.getLogs({
-    targets: [TEPHRA_AGGREGATOR],
+    targets: TEPHRA_AGGREGATOR_CONTRACTS,
     eventAbi: TransformedERC20Event,
     flatten: true,
   });
 
   for (const log of logs) {
     dailyVolume.add(log.inputToken, log.inputTokenAmount);
-    dailyFees.add(log.inputToken, Number(log.inputTokenAmount) * 0.0005); // 0.05%
   }
+  
+  const dailyFees = dailyVolume.clone(FLAT_FEE_RATE);
 
   return {
     dailyVolume,
