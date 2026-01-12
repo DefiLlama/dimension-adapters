8e17df30d50eaa7c2e0c77f016f6b17ef5f0c4b2
bheluga <anonymous_cat_llama@protonmail.com>
2026-01-12 15:50:53 +0530

    fix lifi spike (#5599)
    * fix lifi spike
    
    * fix ts check errors

--- fees/lifi/index.ts
+++ fees/lifi/index.ts
@@ -1,5 +1,6 @@
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { LifiFeeCollectors } from "../../helpers/aggregators/lifi";
+import {DefaultDexTokensBlacklisted} from "../../helpers/lists"
 
 const FeeCollectedEvent = "event FeesCollected(address indexed _token, address indexed _integrator, uint256 _integratorFee, uint256 _lifiFee)"
 
@@ -16,7 +17,9 @@ const fetch = async (options: FetchOptions) => {
         eventAbi: FeeCollectedEvent,
     });
     // 0x0000000000000000000000000000000000000000 is the gas token for all chains, we already handle it in the Balances
+    const blacklistForChain = new Set(DefaultDexTokensBlacklisted[options.chain]);
     data.forEach((log: any) => {
+        if(blacklistForChain.has((log._token).toLowerCase())) return;
         dailyFees.add(log._token, log._integratorFee, IntegratorFee);
         dailyFees.add(log._token, log._lifiFee, LifiProtocolFee);
         
