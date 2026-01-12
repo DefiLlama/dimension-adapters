117bd640614eb1d963f1f124d8a66b4ff36e5794
urvi-bala <urvibala@lampros.tech>
2026-01-12 16:33:23 +0530

    replaced data tables with logs table

--- fees/xena-finance.ts
+++ fees/xena-finance.ts
@@ -5,64 +5,66 @@ import { queryDuneSql } from "../helpers/dune"
 
 const fetch = async (timestamp: number, _a: any, options: FetchOptions) => {
   const query = `
-    WITH prices AS (
+    WITH raw_fees AS (
+
       SELECT
-        token,
-        call_block_number AS block_number,
-        MAX(output_0) AS price
-      FROM xena_base.Oracle_call_getPrice
-      GROUP BY token, call_block_number
-    ),
-    fees AS (
-      SELECT
-        CAST(feeValue AS DECIMAL) AS feeValue,
-        evt_block_time
-      FROM xena_base.Pool_evt_IncreasePosition
+          varbinary_to_uint256(varbinary_substring(data, 1, 32)) / 1e30 AS fee,
+          block_date
+      FROM base.logs
+      WHERE contract_address = 0x22787c26bb0ab0d331eb840ff010855a70a0dca6
+        AND topic0 = 0x8f1a004341b7c2e1e0799b80c6b849e04431c20757ba9b8c9064d5132405465d
 
       UNION ALL
+
       SELECT
-        CAST(feeValue AS DECIMAL) AS feeValue,
-        evt_block_time
-      FROM xena_base.Pool_evt_DecreasePosition
+          varbinary_to_uint256(varbinary_substring(data, 1, 32)) / 1e30 AS fee,
+          block_date
+      FROM base.logs
+      WHERE contract_address = 0x22787c26bb0ab0d331eb840ff010855a70a0dca6
+        AND topic0 = 0x8b8cf2b995650a0e5239d131bc9ace3606d59971f1c0370675babdbc1fc48e5f
 
       UNION ALL
+
       SELECT
-        CAST(feeValue AS DECIMAL) AS feeValue,
-        evt_block_time
-      FROM xena_base.Pool_evt_LiquidatePosition
+          varbinary_to_uint256(varbinary_substring(data, 65, 32)) / 1e30 AS fee,
+          block_date
+      FROM base.logs
+      WHERE contract_address = 0x22787c26bb0ab0d331eb840ff010855a70a0dca6
+        AND topic0 = 0x136cbd19b29e7d7cbbb67178581f238ef5029382a513cd55f0096e974441a6fb
 
       UNION ALL
+
       SELECT
-        CAST(fee * price AS DECIMAL) AS feeValue,
-        evt_block_time
-      FROM xena_base.Pool_evt_Swap
-      JOIN prices
-        ON prices.block_number = evt_block_number
-       AND prices.token = tokenIn
+          varbinary_to_uint256(varbinary_substring(data, 33, 32)) / 1e30 AS fee,
+          block_date
+      FROM base.logs
+      WHERE contract_address = 0x22787c26bb0ab0d331eb840ff010855a70a0dca6
+        AND topic0 = 0xb24b74123b08b3e5d2af6b47e948b1c8eed24d9f717f27a4b2fc3aa82699696e
 
       UNION ALL
+
       SELECT
-        CAST(fee * price AS DECIMAL) AS feeValue,
-        evt_block_time
-      FROM xena_base.Pool_evt_LiquidityAdded
-      JOIN prices
-        ON prices.block_number = evt_block_number
-       AND prices.token = xena_base.Pool_evt_LiquidityAdded.token
+          varbinary_to_uint256(varbinary_substring(data, 65, 32)) / 1e30 AS fee,
+          block_date
+      FROM base.logs
+      WHERE contract_address = 0x22787c26bb0ab0d331eb840ff010855a70a0dca6
+        AND topic0 = 0x43c967b388d3a4ccad3f7ab80167852e322e5a3fde9893f530252281b2ae8b70
 
       UNION ALL
+
       SELECT
-        CAST(fee * price AS DECIMAL) AS feeValue,
-        evt_block_time
-      FROM xena_base.Pool_evt_LiquidityRemoved
-      JOIN prices
-        ON prices.block_number = evt_block_number
-       AND prices.token = xena_base.Pool_evt_LiquidityRemoved.token
+          varbinary_to_uint256(varbinary_substring(data, 65, 32)) / 1e30 AS fee,
+          block_date
+      FROM base.logs
+      WHERE contract_address = 0x22787c26bb0ab0d331eb840ff010855a70a0dca6
+        AND topic0 = 0xd765e08eef31c0983ecca03ecd166297ac485ecd5dd69e291c848f0a020333c1
     )
 
     SELECT
-      SUM(feeValue) / 1e30 AS daily_fees
-    FROM fees
-    WHERE DATE_TRUNC('day', evt_block_time) = DATE_TRUNC('day', FROM_UNIXTIME(${timestamp}))
+        SUM(fee) AS daily_fees
+    FROM raw_fees
+    WHERE date(block_date) = date(FROM_UNIXTIME(${timestamp}));
+
   `;
 
   const result = await queryDuneSql(options, query);
@@ -77,7 +79,7 @@ const fetch = async (timestamp: number, _a: any, options: FetchOptions) => {
     };
   }
 
-  const dailyFee = result[0].daily_fees || 0;
+  const dailyFee = Number(result[0].daily_fees || 0) / 1e18;
 
   return {
     dailyFees: dailyFee.toString(),
