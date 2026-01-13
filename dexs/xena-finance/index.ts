ebe70e171e78b88a5def9e6de6f6422621c255c2
urvi-bala <urvibala@lampros.tech>
2026-01-13 15:59:54 +0530

    replaced graphql with Dune query

--- dexs/xena-finance/index.ts
+++ dexs/xena-finance/index.ts
@@ -1,55 +1,46 @@
-import request, { gql } from "graphql-request";
-import { Fetch, SimpleAdapter } from "../../adapters/types";
-import { CHAIN } from "../../helpers/chains";
-import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
+import { Adapter, Dependencies, FetchOptions } from "../../adapters/types"
+import { CHAIN } from '../../helpers/chains'
+import { queryDuneSql } from "../../helpers/dune"
 
-const endpoints: { [key: string]: string } = {
-  [CHAIN.BASE]: "https://subgraph.xena.finance/subgraphs/name/analyticsv2"
-}
+const fetch = async (timestamp: number, _a: any, options: FetchOptions) => {
+  const query = `
+    WITH swap_volumes AS (
+        SELECT
+            BYTEARRAY_TO_UINT256(SUBSTR(l.data, 65, 32)) / 1e30 AS amountIn,
+            l.block_time AS time
+        FROM base.logs l
+        WHERE l.contract_address = 0x22787c26bb0ab0d331eb840ff010855a70a0dca6
+          AND l.topic0 = 0xb24b74123b08b3e5d2af6b47e948b1c8eed24d9f717f27a4b2fc3aa82699696e
+    )
+    SELECT
+        SUM(amountIn) AS swap
+    FROM swap_volumes
+    WHERE DATE_TRUNC('day', time) = DATE(FROM_UNIXTIME(${timestamp}));
+  `;
 
-const historicalDataSwap = gql`
-  query get_volume($period: String!, $id: String!) {
-    volumeStats(where: {period: $period, id: $id}) {
-        swap
-    }
+  const result = await queryDuneSql(options, query);
+
+  if (!result || result.length === 0) {
+    return {
+      dailyVolume: '0',
+    };
   }
-`
 
-interface IGraphResponse {
-  volumeStats: Array<{
-    swap: string,
-  }>
-}
-
-const getFetch = (query: string)=> (chain: string): Fetch => async (timestamp: number) => {
-  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date((timestamp * 1000)))
-  const dailyData: IGraphResponse = await request(endpoints[chain], query, {
-    id: `day-${String(dayTimestamp)}`,
-    period: 'daily',
-  })
+  const dailyVolume = Number(result[0].swap || 0);
 
   return {
-    timestamp: dayTimestamp,
-    dailyVolume:
-      dailyData.volumeStats.length == 1
-        ? String(Number(Object.values(dailyData.volumeStats[0]).reduce((sum, element) => String(Number(sum) + Number(element)))))
-        : undefined,
-
-  }
+    dailyVolume: dailyVolume.toString(),
+  };
 }
 
-const startTimestamps: { [chain: string]: number } = {
-  [CHAIN.BASE]: 1696856400,
-}
-
-const adapter: SimpleAdapter = {
-  deadFrom: '2025-01-01',
+const adapter: Adapter = {
   adapter: {
     [CHAIN.BASE]: {
-      fetch: getFetch(historicalDataSwap)(CHAIN.BASE),
-      start: startTimestamps[CHAIN.BASE],
-    }
+      fetch,
+      start: '2023-10-09',
+    },
   },
-};
+  dependencies: [Dependencies.DUNE],
+}
 
 export default adapter;
