47cae431fc409813121688851d1470a09fa572e5
datadashboards <dunedatadash@gmail.com>
2026-01-13 09:19:51 +0100

    feat: o1 exchange volume base

--- dexs/o1-exchange/index.ts
+++ dexs/o1-exchange/index.ts
@@ -2,52 +2,61 @@
 
 import { Dependencies, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { addTokensReceived, getSolanaReceived } from '../../helpers/token';
+import { addTokensReceived, getETHReceived, getSolanaReceived } from '../../helpers/token';
 
-const chainConfig: any = {
-  [CHAIN.SOLANA]: {
-    start: '2025-07-01',
-    treasuryAddress: 'FUzZ2SPwLPAKaHubxQzRsk9K8dXb4YBMR6hTrYEMFFZc',
-  },
-  [CHAIN.BASE]: {
-    start: '2025-07-01',
-    treasuryAddress: '0x1E493E7CF969FD7607A8ACe7198f6C02e5eF85A4',
-  },
+const fetch: any = async (_a: any, _b: any, options: FetchOptions) => {
+  const targets = [
+    '0x1E493E7CF969FD7607A8ACe7198f6C02e5eF85A4',
+    '0xc98218Df72975EE1472919d2685e5BD215Baaad4'
+  ];
+
+  const dailyFees = await addTokensReceived({ options, targets });
+  await getETHReceived({ options, targets, balances: dailyFees, notFromSenders: ['0x4200000000000000000000000000000000000006'] });
+  const dailyVolume = dailyFees.clone(100);
+
+  return { dailyFees, dailyUserFees: dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, dailyVolume };
 }
 
-const fetch = async (_: any, _1: any, options: FetchOptions) => {
-  if (options.chain === CHAIN.SOLANA) {
-    const solanaFees = await getSolanaReceived({
-      options,
-      targets: [chainConfig[options.chain].treasuryAddress],
-      blacklists: [],
-    });
-    const dailyVolume = solanaFees.clone(100);
-    return { dailyFees: solanaFees, dailyUserFees: solanaFees, dailyRevenue: solanaFees, dailyProtocolRevenue: solanaFees, dailyVolume }
-  }
-  const baseFees = await addTokensReceived({
+  
+const fetchSol: any = async (_a: any, _b: any, options: FetchOptions) => {
+  const targets = [
+    'FUzZ2SPwLPAKaHubxQzRsk9K8dXb4YBMR6hTrYEMFFZc',
+    'HG73jy6opRQwgTaynUeT6MxX6h3mshNWLPGHme4HdiYy'
+  ];
+
+  const dailyFees = await getSolanaReceived({
+    blacklists: targets,
     options,
-    targets: [chainConfig[options.chain].treasuryAddress],
+    targets,
   });
-  const dailyVolume = baseFees.clone(100);
+  
+  const dailyVolume = dailyFees.clone(100);
 
-  return { dailyFees: baseFees, dailyUserFees: baseFees, dailyRevenue: baseFees, dailyProtocolRevenue: baseFees, dailyVolume }
-}
-
-const methodology = {
-  Fees: "1% Trading fees paid by users while using o1.exchange.",
-  UserFees: "1% Trading fees paid by users while using o1.exchange.",
-  Revenue: "All fees are collected by o1.exchange.",
-  ProtocolRevenue: "All fees are collected by o1.exchange.",
+  return { dailyFees, dailyUserFees: dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees, dailyVolume }; 
 }
 
 const adapter: SimpleAdapter = {
   version: 1,
   fetch,
-  adapter: chainConfig,
-  methodology,
+  methodology: {
+    Volume: 'Total trading volume is calculated as fees multiplied by 100, since trading fees are 1% of the volume.',
+    Fees: 'User pays 1% fee on each trade',
+    UserFees: 'User pays 1% fee on each trade',
+    Revenue: 'All trading fees are revenue.',
+    ProtocolRevenue: 'All trading fees are revenue collected by o1 exchange.',
+  },
+  adapter: {
+    [CHAIN.SOLANA]: {
+      fetch: fetchSol,
+      start: '2025-07-01',
+    },
+    [CHAIN.BASE]: {
+      fetch: fetch,
+      start: '2025-07-01',
+    },
+  },
+  isExpensiveAdapter: true,
   dependencies: [Dependencies.ALLIUM],
-  isExpensiveAdapter: true
 };
 
 export default adapter;
