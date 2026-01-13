311957c4c6952d5e97ceb039ab3b2d98c7d51090
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-01-13 20:46:01 +0100

    minor fix

--- fees/quai-network/index.ts
+++ fees/quai-network/index.ts
@@ -1,19 +1,22 @@
 import { Balances } from "@defillama/sdk";
-import { Adapter, FetchOptions, ProtocolType } from "../../adapters/types";
+import { Adapter, FetchOptions, } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import fetchURL, { httpGet } from "../../utils/fetchURL";
+import fetchURL from "../../utils/fetchURL";
 
 const getRavenMiningRewards = async (options: FetchOptions, dailyFees: Balances) => {
   const address = "RXtpH2yp6AA6VvPVTuhCrqxYG7vCGEPMB5";
-  let pageNum = 0, dailyRavenMined = 0, lastTxnTime = options.startOfDay;
+  let pageNum = 0, dailyRavenMined = 0, lastTxnTime = options.startTimestamp
 
-  while (lastTxnTime >= options.startOfDay) {
+  while (lastTxnTime >= options.startTimestamp) {
     const response = await fetchURL(`https://explorer.rvn.zelcore.io/api/txs?address=${address}&pageNum=${pageNum}`);
     for (const { isCoinBase, time, valueOut } of response.txs) {
-      if (isCoinBase && time >= options.startOfDay && time < options.endTimestamp)
-       dailyRavenMined += valueOut;
+      if (isCoinBase && time >= options.startTimestamp && time < options.endTimestamp)
+        dailyRavenMined += valueOut;
       lastTxnTime = time;
     }
+
+    if (response.txs.length === 0) break;
+
     pageNum++;
   }
   dailyFees.addCGToken("ravencoin", dailyRavenMined);
@@ -21,32 +24,36 @@ const getRavenMiningRewards = async (options: FetchOptions, dailyFees: Balances)
 
 const getLitecoinMiningRewards = async (options: FetchOptions, dailyFees: Balances) => {
   const address = "ltc1qlg96gqruz4vu5w86z6rpqxt6ugqxrljxzpvcu8";
-  let dailyLitecoinMined = 0, lastTxnTime = options.startOfDay, lastSeenTxn = "";
+  let dailyLitecoinMined = 0, lastTxnTime = options.startTimestamp, lastSeenTxn = "";
 
-  while (lastTxnTime >= options.startOfDay) {
+  while (lastTxnTime >= options.startTimestamp) {
     const response = await fetchURL(`https://litecoinspace.org/api/address/${address}/txs/chain/${lastSeenTxn}`);
+    if (response.length === 0) break;
 
     for (const { txid, vin, vout, status } of response) {
-      if (vin && vin.length > 0 && vin[0].is_coinbase && status.block_time >= options.startOfDay && status.block_time < options.endTimestamp) {
+      if (vin && vin.length > 0 && vin[0].is_coinbase && status.block_time >= options.startTimestamp && status.block_time < options.endTimestamp) {
         dailyLitecoinMined += (vout?.[0]?.value ?? 0) / 1e8;
       }
       lastSeenTxn = txid;
       lastTxnTime = status.block_time;
     }
+
   }
   dailyFees.addCGToken("litecoin", dailyLitecoinMined);
 }
 
 const getBchMiningRewards = async (options: FetchOptions, dailyFees: Balances) => {
   const address = "qqqea0a7ryny69sskvx857apy3r6wt6w35524nm4xw";
-  let dailyBchMined = 0, lastTxnTime = options.startOfDay, offset = 0;
+  let dailyBchMined = 0, lastTxnTime = options.startTimestamp, offset = 0;
 
-  while (lastTxnTime >= options.startOfDay) {
+  while (lastTxnTime >= options.startTimestamp) {
     const txs = await fetchURL(`https://api.blockchain.info/haskoin-store/bch/address/${address}/transactions?limit=100&offset=${offset}`);
+    if (txs.length === 0) break;
+
     const txnDetails = await fetchURL(`https://api.blockchain.info/haskoin-store/bch/transactions?txids=${txs.map((tx: any) => tx.txid).join(',')}`);
 
     for (const { inputs, outputs, time } of txnDetails) {
-      if (inputs && inputs.length > 0 && inputs[0].coinbase && time >= options.startOfDay && time < options.endTimestamp) {
+      if (inputs && inputs.length > 0 && inputs[0].coinbase && time >= options.startTimestamp && time < options.endTimestamp) {
         dailyBchMined += (outputs?.[0]?.value ?? 0) / 1e8;
       }
       lastTxnTime = time;
@@ -59,10 +66,13 @@ const getBchMiningRewards = async (options: FetchOptions, dailyFees: Balances) =
 
 const getDogeMiningRewards = async (options: FetchOptions, dailyFees: Balances) => {
   const address = "D6rnVyuuHB3x8et741kmXzFuZSTVnJQDke"
-  let pageNum = 1, dailyDogeMined = 0, lastTxnTime = options.startOfDay;
+  let pageNum = 1, dailyDogeMined = 0, lastTxnTime = options.startTimestamp;
 
-  while (lastTxnTime >= options.startOfDay) {
+  while (lastTxnTime >= options.startTimestamp) {
     const { transactionSummaries } = await fetchURL(`https://doge.firesat.io/api/v1/address/${address}?page=${pageNum}&limit=100`);
+
+    if (transactionSummaries.length === 0) break;
+
     for (const { coinbase, time, received } of transactionSummaries) {
       if (coinbase && time >= options.startOfDay && time < options.endTimestamp)
         dailyDogeMined += +received;
@@ -82,23 +92,15 @@ const chainDetails = {
 };
 
 const getQuaiBurnDetails = async (options: FetchOptions, dailyHoldersRevenue: Balances) => {
+  const { fromApi, api } = options;
   const quaiBurnAddress = "0x0050AF0000000000000000000000000000000000";
-  let lastTxnTime = options.startOfDay, next_page_params = {}, dailyQuaiBurnt = 0;
+  const tokens = ["0x0000000000000000000000000000000000000000",]; // Quai Native
 
-  while (next_page_params != null && lastTxnTime >= options.startOfDay) {
-    const response = await httpGet(`https://quaiscan.io/api/v2/addresses/${quaiBurnAddress}/transactions?${Object.entries(next_page_params).map(([k, v]: any) => `${k}=${encodeURIComponent(v === null ? "null" : v)}`).join("&")}`, {
-      headers: {
-        "Accept": "application/json",
-      }
-    });
-    for (const item of response.items) {
-      lastTxnTime = Math.floor(+new Date(item.timestamp) / 1000);
-      if (lastTxnTime >= options.startOfDay && lastTxnTime < options.endTimestamp)
-        dailyQuaiBurnt += +item.value / 1e18;
-    }
-    next_page_params = response.next_page_params;
-  }
-  dailyHoldersRevenue.addCGToken("quai-network", dailyQuaiBurnt);
+  await api.sumTokens({ tokens, owner: quaiBurnAddress })
+  await fromApi.sumTokens({ tokens, owner: quaiBurnAddress })
+
+  dailyHoldersRevenue.addBalances(api.getBalancesV2())
+  dailyHoldersRevenue.subtract(fromApi.getBalancesV2())
 }
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
@@ -129,7 +131,6 @@ const adapter: Adapter = {
   fetch,
   chains: [CHAIN.QUAI],
   start: '2025-12-17',
-  protocolType: ProtocolType.CHAIN,
   methodology,
 };
 
