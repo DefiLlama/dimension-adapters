c9d70ed63b879ce2ffd661e644ea0038346fffec
eden <noat.eth@gmail.com>
2026-01-17 03:18:06 +0700

    clean codes using export helper (#5641)

--- open-interest/ventuals-oi.ts
+++ open-interest/ventuals-oi.ts
@@ -1,24 +1,3 @@
-import { FetchOptions, FetchResultV2, SimpleAdapter } from "../adapters/types";
-import { CHAIN } from "../helpers/chains";
-import { fetchHIP3DeployerData } from "../helpers/hyperliquid";
+import { exportHIP3DeployerAdapter } from "../helpers/hyperliquid";
 
-async function fetch(_1: number, _: any,  options: FetchOptions): Promise<FetchResultV2> {
-  const result = await fetchHIP3DeployerData({ options, hip3DeployerId: 'vntl' });
-  
-  return {
-    openInterestAtEnd: result.currentPerpOpenInterest,
-  }
-}
-
-const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.HYPERLIQUID]: {
-      fetch,
-      // start: '2025-11-13',
-      runAtCurrTime: true,
-    },
-  },
-  doublecounted: true,
-};
-
-export default adapter;
+export default exportHIP3DeployerAdapter('vntl', { type: 'oi' });
