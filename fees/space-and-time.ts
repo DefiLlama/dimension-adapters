9d57a095357097114d2dd54ba1a00d9c59445a59
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-01-16 18:02:00 +0100

    add support for proxied fetch

--- fees/space-and-time.ts
+++ fees/space-and-time.ts
@@ -1,16 +1,14 @@
 import * as sdk from "@defillama/sdk";
 import { FetchOptions, ProtocolType, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { httpGet } from "../utils/fetchURL";
+import { proxiedFetch } from "../utils/fetchURL";
 import { getBlock } from "../helpers/getBlock";
 
+// curl -x dc.oxylabs.io:8000 -U "user-test1_x8kti-country-US:Ww1234567890_" https://ip.oxylabs.io/location
+
 const getGasBurned = async (startTimestamp: number, endTimestamp: number) => {
-  const data = await httpGet(
-    "https://metrics.spaceandtime.dev/defillama/gas-burned",
-    {
-      params: { start: startTimestamp, end: endTimestamp },
-    },
-  );
+  const data = await proxiedFetch(`https://metrics.spaceandtime.dev/defillama/gas-burned?start=${startTimestamp}&end=${endTimestamp}`)
+
   return data.gasBurned;
 };
 
@@ -22,7 +20,7 @@ const getPayPortalFees = async (startTimestamp: number, endTimestamp: number) =>
     fromBlock: Number(fromBlock),
     toBlock: Number(toBlock),
     target: "0x84C276C3EC3Dd3F67F51B775a53001c9d5017964",
-    eventAbi:"event TransferWithFee(address indexed sender, address indexed recipient, uint256 netAmount, uint256 fee)",
+    eventAbi: "event TransferWithFee(address indexed sender, address indexed recipient, uint256 netAmount, uint256 fee)",
   });
   return Number(logs.reduce((sum, log) => sum + log.args.fee ? BigInt(log.args.fee) : 0n, 0n));
 };
@@ -30,7 +28,7 @@ const getPayPortalFees = async (startTimestamp: number, endTimestamp: number) =>
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const dailyFees = options.createBalances();
   const dailyRevenue = options.createBalances();
-  
+
   // gas fees paid on space and time blockchain
   const gasBurned = await getGasBurned(options.startTimestamp, options.endTimestamp);
 
@@ -40,7 +38,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   dailyFees.addCGToken('space-and-time', Number(gasBurned) / 1e18);
   dailyFees.addCGToken('space-and-time', Number(payPortalFees) / 1e18);
   dailyRevenue.addCGToken('space-and-time', Number(payPortalFees) / 1e18);
-  
+
   return { dailyFees, dailyRevenue, dailyProtocolRevenue: dailyRevenue };
 };
 
