835f430f737bba188767bb4ed240cdb33b181d12
FelixBruguera <felixbruguera0@gmail.com>
2026-01-16 12:50:54 -0400

    add revenue to sparkdex v2 and v3 (#5617)

--- dexs/sparkdex-v2/index.ts
+++ dexs/sparkdex-v2/index.ts
@@ -1,6 +1,19 @@
+import { SimpleAdapter } from '../../adapters/types'
 import { CHAIN } from '../../helpers/chains'
-import { uniV2Exports } from '../../helpers/uniswap'
+import { getUniV2LogAdapter } from '../../helpers/uniswap'
 
-export default uniV2Exports({
-  [CHAIN.FLARE]: { factory: '0x16b619B04c961E8f4F06C10B42FDAbb328980A89',},
-})
+const adapter: SimpleAdapter = {
+	version: 2,
+	methodology: {
+		Volume: 'Total swap volume',
+		Fees: 'Users pay 0.3% per swap.',
+		UserFees: 'Users pay 0.3% per swap.',
+		Revenue: 'No revenue.',
+		SupplySideRevenue: 'All swap fees are distributed to LPs.',
+	},
+	start: '2024-06-27',
+	chains: [CHAIN.FLARE],
+	fetch: getUniV2LogAdapter({ factory: '0x16b619B04c961E8f4F06C10B42FDAbb328980A89', userFeesRatio: 1, revenueRatio: 0 }),
+}
+
+export default adapter
