a1f1eeccaa0f8cb1b450f395db55918751b94f81
treeoflife2 <sol.analyzoor@gmail.com>
2026-01-16 18:46:08 +0530

    refactor

--- fees/rhea-cross-chain/index.ts
+++ fees/rhea-cross-chain/index.ts
@@ -1,21 +1,26 @@
-import { SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
 import { httpGet } from "../../utils/fetchURL";
+
 const api_fee = "https://api.ref.finance/get-total-fee"
 const api_revenue = "https://api.ref.finance/get-total-revenue"
+
+const fetch = async (options: FetchOptions) => {
+  const fee_result = await httpGet(api_fee);
+  const revenue_result = await httpGet(api_revenue);
+
+  return {
+    dailyFees: fee_result.fee_data.cross_chain_fee,
+    dailyRevenue: revenue_result.revenue_data.cross_chain_revenue,
+  }
+}
+
 const adapter: SimpleAdapter = {
-    adapter: {
-        "near": {
-          start: '2026-01-12',
-          fetch: async () => {
-            const fee_result = await httpGet(api_fee);
-            const revenue_result = await httpGet(api_revenue);
-            return {
-                dailyFees: fee_result?.fee_data?.cross_chain_fee || '0',
-                dailyRevenue: revenue_result?.revenue_data?.cross_chain_revenue || '0',
-            }
-          }
-        }
-    }
+  version: 2,
+  fetch,
+  chains: [CHAIN.NEAR],
+  start: '2026-01-12',
+  runAtCurrTime: true,
 };
 
 export default adapter;
