c649a0a8dfd60c3d3a8b444b7607c56961db02ae
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2026-01-19 09:37:56 +0100

    fix injective spot

--- dexs/injective-spot.ts
+++ dexs/injective-spot.ts
@@ -2,12 +2,16 @@ import { httpGet } from "../utils/fetchURL";
 import { FetchOptions, } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
-
 const SPOT_URL = `https://bigquery-api-636134865280.europe-west1.run.app/injective_spot_volume`;
 
 const fetch = async (_: number, _t: any, options: FetchOptions) => {
   const spotRes: any = await httpGet(`${SPOT_URL}?start_date=${options.dateString}`);
-  if (spotRes.days.length !== 1) throw new Error("No data found for the given date: " + options.dateString);
+  
+  if (!spotRes?.days?.length) {
+    return {
+      dailyVolume: "0",
+    };
+  }
 
   return { dailyVolume: spotRes.total_volume_usd, };
 };
