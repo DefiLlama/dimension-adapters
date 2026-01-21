9425b6249f3950e873dc8deae6591b6c0bb70587
Felix Bruguera <felixbruguera0@gmail.com>
2026-01-21 11:08:53 -0400

    replace salfish subgraph with logs

--- dexs/sailfish/index.ts
+++ dexs/sailfish/index.ts
@@ -1,36 +1,19 @@
-// https://api.goldsky.com/api/public/project_cm5nst0b7iiqy01t6hxww7gao/subgraphs/sailfish-v3-occ-mainnet/1.0.0/gn
+import { SimpleAdapter } from "../../adapters/types";
+import { getUniV3LogAdapter } from '../../helpers/uniswap'
 
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
-import { DEFAULT_TOTAL_VOLUME_FIELD, getGraphDimensions2 } from "../../helpers/getUniSubgraph";
-
-const v3Endpoints: { [key: string]: string } = {
-  occ: "https://api.goldsky.com/api/public/project_cm5nst0b7iiqy01t6hxww7gao/subgraphs/sailfish-v3-occ-mainnet/1.0.0/gn"
-}
-const v3Graphs = getGraphDimensions2({
-  graphUrls: v3Endpoints,
-  totalVolume: {
-    factory: "factories",
-    field: DEFAULT_TOTAL_VOLUME_FIELD,
-  },
-  feesPercent: {
-    type: "fees",
-    ProtocolRevenue: 0,
-    HoldersRevenue: 0,
-    UserFees: 100, // User fees are 100% of collected fees
-    SupplySideRevenue: 100, // 100% of fees are going to LPs
-    Revenue: 0 // Revenue is 100% of collected fees
-  }
-});
-
-
-const adapters: SimpleAdapter = {
+const swapEvent = "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 price, uint128 liquidity, int24 tick)"
+const adapter: SimpleAdapter = {
   version: 2,
-  adapter: {
-    occ: {
-      fetch: v3Graphs
-    },
-  }
+  methodology: {
+    Volume: 'Total swap volume',
+    Fees: 'Swap fees paid by users.',
+    UserFees: 'Swap fees paid by users.',
+    Revenue: 'No revenue.',
+    SupplySideRevenue: 'All the swap fees are distributed to LPs',
+  },
+  start: '2025-1-17',
+  chains: ["occ"],
+  fetch: getUniV3LogAdapter({ factory: '0x963A7f4eB46967A9fd3dFbabD354fC294FA2BF5C', userFeesRatio: 1, revenueRatio: 0, swapEvent: swapEvent }),
 }
 
-
-export default adapters;
+export default adapter
