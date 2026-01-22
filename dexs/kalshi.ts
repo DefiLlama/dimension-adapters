4d65b157cb4b735043fc610cec1c47e1eaf2cc4a
treeoflife2 <sol.analyzoor@gmail.com>
2026-01-22 18:14:06 +0530

    Fix: kalshi cash volume

--- dexs/kalshi.ts
+++ dexs/kalshi.ts
@@ -1,27 +1,42 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import fetchURL from "../utils/fetchURL";
+import { queryDuneSql } from "../helpers/dune";
 
-async function fetch(_a:number, _b:any, options: FetchOptions) {
-  const startDate = options.startOfDay - (24 * 3600);
-  const dateString = new Date(startDate * 1000).toISOString().split('T')[0]
-  const url = `https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_${dateString}.json`
-  const data = await fetchURL(url)
-
-  let dailyVolume = 0
-  let openInterestAtEnd = 0
-
-  for (const market of data) {
-    dailyVolume += Number(market.daily_volume)
-    if (market.status !== 'active') continue // the market might have settled in the past24 hours
-    openInterestAtEnd += Number(market.open_interest)
-  }
+async function fetch(_a: any, _b: any, options: FetchOptions) {
+  const dateString = new Date((options.startOfDay - (24 * 3600)) * 1000).toISOString().split('T')[0]
+  
+  const query = `
+  WITH trade_report_agg AS (
+    SELECT 
+      SUM(price * contracts_traded / 100) AS cash_volume
+    FROM kalshi.trade_report
+    WHERE date = date('${dateString}')
+  ),
+  market_report_agg AS (
+    SELECT 
+      SUM(CASE WHEN status = 'active' THEN open_interest ELSE 0 END) AS open_interest
+    FROM kalshi.market_report 
+    WHERE date = '${dateString}'
+  )
+  SELECT 
+    tr.cash_volume,
+    mr.open_interest
+  FROM trade_report_agg tr
+  CROSS JOIN market_report_agg mr
+  `
+  const data: { 
+    cash_volume: string,
+    open_interest: string
+  }[] = await queryDuneSql(options, query)
+  
+  const dailyVolume = Number(data[0]?.cash_volume) || 0
+  const openInterestAtEnd = Number(data[0]?.open_interest) || 0
 
   return { dailyVolume, openInterestAtEnd }
 }
 
 const adapter: SimpleAdapter = {
-  fetch,
+  fetch,  
   chains: [CHAIN.OFF_CHAIN],
   start: '2021-06-30',
 }
