a77b4d329bc6e5e28066a07d07788a62030c6ce8
Joeh <joeh@sobal.fi>
2026-01-23 17:53:09 +0800

    feat: add kumbaya on MegaETH

---
+++ dexs/kumbaya/index.ts
@@ -0,0 +1,9 @@
+import { CHAIN } from "../../helpers/chains";
+import { uniV3Exports } from "../../helpers/uniswap";
+
+export default uniV3Exports({
+  [CHAIN.MEGAETH]: {
+    factory: "0x68b34591f662508076927803c567Cc8006988a09",
+    start: "2025-12-21",
+  },
+});
