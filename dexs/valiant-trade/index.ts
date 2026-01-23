3167a80645713513b0bab3f086c0db52a781b7e8
eden <noat.eth@gmail.com>
2026-01-24 05:29:12 +0700

    fix

--- dexs/valiant-trade/index.ts
+++ dexs/valiant-trade/index.ts
@@ -9,11 +9,9 @@ const CONFIG: Record<string, { url: string, start: string }> = {
   },
 }
 
-async function fetch(timestamp: number, _b: any, options: FetchOptions) {
+async function fetch(_a: number, _b: any, options: FetchOptions) {
   const baseUrl = CONFIG[options.chain].url;
-  const startTimestamp = timestamp - 86400;
-  const endTimestamp = timestamp;
-  const url = `${baseUrl}?start=${startTimestamp}&end=${endTimestamp}`;
+  const url = `${baseUrl}?start=${options.fromTimestamp}&end=${options.toTimestamp}`;
   const data = await httpGet(url);
 
   return {
@@ -38,6 +36,6 @@ export default {
   fetch,
   chains: [CHAIN.FOGO],
   start: '2025-12-20',
-  runAtCurrTime: true,
+  // runAtCurrTime: true,
   methodology,
 }
