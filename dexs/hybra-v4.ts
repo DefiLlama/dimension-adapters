f1b2817eeaed6a32a432f52e9fb5c0930e68eb28
eden <noat.eth@gmail.com>
2026-01-27 02:23:19 +0700

    fix hybra-v4 adapter

--- dexs/hybra-v4.ts
+++ dexs/hybra-v4.ts
@@ -1,13 +1,13 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { addOneToken } from "../helpers/prices";
-import { request } from "graphql-request";
+import { formatAddress } from "../utils/utils";
 
 const swapEvent = 'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)'
+const SetCustomFeeEvent = 'event SetCustomFee(address indexed pool, uint24 indexed fee)'
 
 const FACTORY = '0x32b9dA73215255d50D84FeB51540B75acC1324c2';
-
-const FEE_SUBGRAPH_URL = 'https://api.goldsky.com/api/public/project_cmbj707z4cd9901sib1f6cu0c/subgraphs/fee-setting/fee-setting/gn';
+const CUSTOM_FEE_CONTRACT = '0x4992d95c26297ae7b08544f4d59bc870e3134ab7';
 
 // Default fee mapping based on tickSpacing (from Hybra V4 factory initialization)
 // enableTickSpacing(tickSpacing, fee) where fee is in basis points (1e6 = 100%)
@@ -22,15 +22,7 @@ const TICK_SPACING_TO_FEE: {[key: number]: number} = {
 
 interface FeeEvent {
 	blockNumber: number;
-	timestamp: number;
 	fee: number;
-}
-
-interface SetCustomFee {
-	pool: string;
-	fee: string;
-	timestamp_: string;
-	block_number: string;
 }
 
 async function fetch(options: FetchOptions) {
@@ -88,44 +80,28 @@ async function fetch(options: FetchOptions) {
 
 const customLogic = async ({ pairObject, dailyVolume, allLogs, fetchOptions }: any) => {
 	const { api, chain, createBalances } = fetchOptions;
-	const poolAddresses = Object.keys(pairObject);
-
-	// 1. Get all SetCustomFee events from GraphQL to track fee history
-	const query = `
-		query {
-			setCustomFees(orderBy: timestamp_, orderDirection: asc) {
-				fee
-				pool
-				timestamp_
-				block_number
-			}
-		}
-	`;
-
-	const data = await request(FEE_SUBGRAPH_URL, query);
-	const customFees: SetCustomFee[] = data?.setCustomFees || [];
-
-	// 2. Build fee history mapping: pool -> [(blockNumber, timestamp, fee)]
-	const feeHistory: {[pool: string]: FeeEvent[]} = {};
-	customFees.forEach((event: SetCustomFee) => {
-		const pool = event.pool.toLowerCase();
-		if (!feeHistory[pool]) {
-			feeHistory[pool] = [];
-		}
-		feeHistory[pool].push({
-			blockNumber: parseInt(event.block_number),
-			timestamp: parseInt(event.timestamp_),
-			fee: parseFloat(event.fee) / 1e6
+  const poolAddresses = Object.keys(pairObject);
+	
+  const feeHistory: {[pool: string]: FeeEvent[]} = {};
+  const setCustomFeesEvents = await fetchOptions.getLogs({
+    target: CUSTOM_FEE_CONTRACT,
+    eventAbi: SetCustomFeeEvent,
+    fromBlock: 16716093,
+    cacheInCloud: true,
+  });
+  for (const log of setCustomFeesEvents) {
+    const pool = formatAddress(log.pool)
+    feeHistory[pool] = feeHistory[pool] || [];
+    feeHistory[pool].push({
+			blockNumber: parseInt(log.block_number),
+			fee: parseFloat(log.fee) / 1e6
 		});
-	});
+  }
 
 	// Sort fee events by blockNumber and timestamp (already sorted by query, but ensure consistency)
 	Object.keys(feeHistory).forEach(pool => {
 		feeHistory[pool].sort((a, b) => {
-			if (a.blockNumber !== b.blockNumber) {
-				return a.blockNumber - b.blockNumber;
-			}
-			return a.timestamp - b.timestamp;
+		  return a.blockNumber - b.blockNumber;
 		});
 	});
 
