dd98bfbda75e7092f2b5f7d7eb458431edd98221
eden <noat.eth@gmail.com>
2026-01-28 01:33:01 +0700

    split flowbot

---
+++ dexs/flowbot-perps.ts
@@ -0,0 +1,5 @@
+import { exportBuilderAdapter } from '../helpers/hyperliquid'
+
+const hlBuilderCode = '0xb5d19a1f92fcd5bfdd154d16793bb394f246cb36'
+
+export default exportBuilderAdapter([hlBuilderCode], { start: '2025-11-27' });
