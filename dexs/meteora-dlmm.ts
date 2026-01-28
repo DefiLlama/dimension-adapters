6c089b866c605f73c3a45c7baa29ff8d5922db32
bheluga <anonymous_cat_llama@protonmail.com>
2026-01-28 16:31:15 +0530

    filter out meteora wash trade fees

--- dexs/meteora-dlmm.ts
+++ dexs/meteora-dlmm.ts
@@ -1,13 +1,51 @@
 import { CHAIN } from '../helpers/chains';
 import fetchURL from '../utils/fetchURL';
+import { sleep } from '../utils/utils';
 
-const meteoraStatsEndpoint = 'https://dlmm-api.meteora.ag/info/protocol_metrics';
+const meteoraStatsEndpoint = 'https://dlmm-api.meteora.ag/pair/all_with_pagination';
 
 async function fetch() {
-  const i = await fetchURL(meteoraStatsEndpoint)
+  let page = 1;
+  let dailyVolume = 0;
+  let dailyRevenue = 0;
+  let dailySupplySideRevenue = 0;
+  const limit = 100;
+
+  while (true) {
+    const response = await fetchURL(`${meteoraStatsEndpoint}?page=${page}&limit=${limit}`);
+
+    const pools = response.pairs || [];
+    if (pools.length === 0) break;
+
+    for (const pool of pools) {
+      const tvl = pool.liquidity || 0;
+      const volume = pool.trade_volume_24h || 0;
+      const protocolFeeRatio = +pool.protocol_fee_percentage / 100 || 0;
+      const fees = pool.fees_24h;
+
+      // Ignore if TVL < 1M and volume > 10x TVL
+      if (pool.is_blacklisted || (tvl < 1_000_000 && volume > tvl * 10))
+        continue;
+
+      dailyVolume += volume;
+      dailyRevenue += fees * protocolFeeRatio;
+      dailySupplySideRevenue += fees * (1 - protocolFeeRatio);
+    }
+
+    const lastPool = pools[pools.length - 1];
+    if (lastPool.trade_volume_24h < 1000) break;
+
+    await sleep(100)
+
+    page++;
+  }
+  const dailyFees = dailySupplySideRevenue + dailyRevenue;
+
   return {
-    dailyVolume: i.daily_trade_volume,
-    dailyFees: i.daily_fee,
+    dailyVolume,
+    dailyFees,
+    dailyRevenue,
+    dailySupplySideRevenue,
   }
 }
 
