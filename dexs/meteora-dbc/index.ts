130d0aeb2da884279ab310de0f2f2bc697261559
bheluga <bheluga@defillama.com>
2026-01-28 20:55:24 +0530

    remove meteora wash trading fees and add historic support for holders revenue (#5754)
    * fix reservoir tools
    
    * filter out meteora wash trade fees
    
    * filter out meteora wash trading fee
    
    * smol fix
    
    * move meteora holders revenue to dbc for historic support

--- dexs/meteora-dbc/index.ts
+++ dexs/meteora-dbc/index.ts
@@ -2,6 +2,7 @@ import { Dependencies, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { queryDuneSql } from "../../helpers/dune";
 import { FetchOptions } from "../../adapters/types";
+import { getSolanaReceived } from '../../helpers/token';
 
 interface IData {
     quote_mint: string;
@@ -10,6 +11,8 @@ interface IData {
     total_protocol_fees: number;
     total_referral_fees: number;
 }
+
+const BUYBACK_WALLET = 'FzULv8pR9Rd7cyVKjVkzmJ1eqEmgwDnzjYyNUcEJtoG9';
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     const query = `
@@ -92,12 +95,20 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
         dailySupplySideRevenue.add(row.quote_mint, Number(row.total_trading_fees) + Number(row.total_referral_fees))
     });
 
+    const dailyHoldersRevenue = await getSolanaReceived({
+        options,
+        target: BUYBACK_WALLET,
+        mints: ["METvsvVRapdj9cFLzq4Tr43xK4tAjQfwX76z3n6mWQL"],  // MET token
+    })
+
+
     return {
         dailyVolume,
         dailyFees,
         dailyUserFees: dailyFees,
         dailyRevenue: dailyProtocolRevenue,
         dailyProtocolRevenue,
+        dailyHoldersRevenue,
         dailySupplySideRevenue
     };
 };
@@ -106,13 +117,14 @@ const adapter: SimpleAdapter = {
     version: 1,
     fetch,
     chains: [CHAIN.SOLANA],
-    dependencies: [Dependencies.DUNE],
+    dependencies: [Dependencies.DUNE,Dependencies.ALLIUM],
     start: '2025-04-23',
     isExpensiveAdapter: true,
     methodology: {
         Fees: "Trading fees paid by users.",
         Revenue: "Protocol fees collected by Meteora DBC protocol.",
         ProtocolRevenue: "Protocol fees collected by Meteora DBC protocol.",
+        HoldersRevenue: "Part of revenue going to MET token buybacks."
         SupplySideRevenue: "The portion of the trading fees paid to LPs and referrals."
     }
 }
