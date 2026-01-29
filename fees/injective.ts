c9f1c1868cd18a649e040d943700138d6149bfb7
manualy <danielswietytrue@gmail.com>
2026-01-29 10:26:05 +0100

    inj fees changes after refactor

--- fees/injective.ts
+++ fees/injective.ts
@@ -2,7 +2,14 @@ import { FetchOptions, ProtocolType } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { httpGet } from "../utils/fetchURL";
 
+interface IFeesDailyData {
+  exchange_fees_usd: number;
+  gas_fees_usd: number;
+  total_fees_usd: number;
+}
+
 interface IFeesResponse {
+  days: IFeesDailyData[];
   exchange_fees_usd: number;
   gas_fees: number;
   total_fees_usd: number;
@@ -30,21 +37,28 @@ const BASE_URL = "https://bigquery-api-636134865280.europe-west1.run.app";
 
 
 const fetch = async (_: number, _t: any, options: FetchOptions) => {
-  const feesRes: any = await httpGet(`${BASE_URL}/fees?start_date=${options.dateString}`);
+  const feesRes: IFeesResponse = await httpGet(`${BASE_URL}/fees?start_date=${options.dateString}`);
   const auctionRes: IAuctionsResponse = await httpGet(`${BASE_URL}/auction?start_date=${options.dateString}`);
-  if (feesRes.days.length !== 1) throw new Error("No data found for the given date: " + options.dateString);
+  if (feesRes.days.length !== 1 || auctionRes.days.length !== 1) throw new Error("No data found for the given date: " + options.dateString);
 
-  const totalDailyFees = feesRes.total_fees_usd;
-  const totalBurn = auctionRes.total_auctions > 0 ? auctionRes.total_usd_value : 0
+  const totalBurn = auctionRes.total_usd_value
+
+  const dailyFees = options.createBalances()
   const dailyRevenue = options.createBalances()
-  dailyRevenue.addUSDValue(totalDailyFees, 'Transaction Fees')
-  dailyRevenue.addUSDValue(totalBurn, 'Auction Fees')
+  const dailyHoldersRevenue = options.createBalances()
 
+  dailyFees.addUSDValue(feesRes.total_fees_usd);
+
+  dailyRevenue.addBalances(dailyFees, 'Transaction Fees')
+  dailyRevenue.addUSDValue(totalBurn, 'Auction Fees')
+  
+  dailyHoldersRevenue.addBalances(dailyFees, 'Transaction Fees')
+  dailyHoldersRevenue.addUSDValue(totalBurn, 'Auction Fees')
 
   return {
-    dailyFees: totalDailyFees,
-    dailyRevenue: totalDailyFees + totalBurn,
-    dailyHoldersRevenue: totalDailyFees + totalBurn,
+    dailyFees,
+    dailyRevenue,
+    dailyHoldersRevenue,
   };
 };
 
@@ -57,7 +71,7 @@ export default {
   breakdownMethodology: {
     Revenue: {
       'Transaction Fees': 'Gas fees paid by users on each transaction, 100% is burned',
-      'Auction Fees': 'Exchange fees is auctioned off to the highest bidder for INJ, 100% of auction fees are burned',
+      'Auction Fees': 'Exchange fees are auctioned off to the highest bidder for INJ, 100% of auction fees are burned. Auction fees are spread evenly across days in between auctions.',
     },
   },
   fetch,
