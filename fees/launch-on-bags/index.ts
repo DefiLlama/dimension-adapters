df9d1a80c538581ed832d2a6065b351f2f579020
yew jun <yewjun@raccoons.dev>
2026-01-29 19:13:58 +0800

    feat: Add Bags dammv2 fee

--- fees/launch-on-bags/index.ts
+++ fees/launch-on-bags/index.ts
@@ -1,32 +1,68 @@
-import { Dependencies, SimpleAdapter } from "../../adapters/types";
+import { Dependencies, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getSqlFromFile, queryDuneSql } from "../../helpers/dune";
-import { FetchOptions } from "../../adapters/types";
 import { METRIC } from "../../helpers/metrics";
+import { httpGet } from "../../utils/fetchURL";
 
-interface IData {
+const DAMMV2_API_URL = "https://bags-adapter-api.datapi.meteora.ag/v1/fees";
+
+interface IDbcData {
   quote_mint: string;
   daily_fees: string;
   daily_protocol_revenue: string;
 }
 
+// DAMMv2 data from API (USD values)
+interface IDammv2FeeResult {
+  timestamp: number;
+  timestamp_str: string;
+  fees: number;
+  protocol_revenue: number;
+}
+
+interface IDammv2FeeResponse {
+  start_time: number;
+  end_time: number;
+  results: IDammv2FeeResult[];
+}
+
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-  const query = getSqlFromFile('helpers/queries/bags.sql', {
+  const dbcQuery = getSqlFromFile('helpers/queries/bags.sql', {
     tx_signer: 'BAGSB9TpGrZxQbEsrEznv5jXXdwyP6AXerN8aVRiAmcv',
     start: options.startTimestamp,
     end: options.endTimestamp
   })
 
-  const data: IData[] = await queryDuneSql(options, query)
+  const dbcData: IDbcData[] = await queryDuneSql(options, dbcQuery);
   const dailyFees = options.createBalances();
   const dailyProtocolRevenue = options.createBalances();
 
-  data.forEach(row => {
-    const creatorFees = Number(row.daily_fees) - Number(row.daily_protocol_revenue);
-    dailyFees.add(row.quote_mint, row.daily_protocol_revenue, METRIC.PROTOCOL_FEES);
-    dailyProtocolRevenue.add(row.quote_mint, row.daily_protocol_revenue, METRIC.PROTOCOL_FEES);
+  dbcData.forEach(row => {
+    const protocolFees = Number(row.daily_protocol_revenue);
+    const creatorFees = Number(row.daily_fees) - protocolFees;
+
+    dailyFees.add(row.quote_mint, protocolFees, METRIC.PROTOCOL_FEES);
+    dailyProtocolRevenue.add(row.quote_mint, protocolFees, METRIC.PROTOCOL_FEES);
     dailyFees.add(row.quote_mint, creatorFees, METRIC.CREATOR_FEES);
   });
+
+  // Fetch DAMMv2 data from API (post-migration)
+  // DAMMV2_API_URL return fee in USD value
+  // startTimestamp need to add 1 seconds to match the start of the day 
+  const dammv2Url = `${DAMMV2_API_URL}?start_time=${options.startTimestamp + 1}&end_time=${options.endTimestamp}`;
+  const dammv2Response: IDammv2FeeResponse = await httpGet(dammv2Url);
+
+  dammv2Response.results.forEach(r => {
+    const totalFees = r.fees ?? 0;
+    const protocolRevenue = r.protocol_revenue ?? 0;
+    const creatorFees = totalFees - protocolRevenue;
+
+    dailyFees.addUSDValue(protocolRevenue, METRIC.PROTOCOL_FEES);
+    dailyProtocolRevenue.addUSDValue(protocolRevenue, METRIC.PROTOCOL_FEES);
+
+    dailyFees.addUSDValue(creatorFees, METRIC.CREATOR_FEES);
+  })
+
 
   return {
     dailyFees,
