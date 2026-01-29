f52e25698fe8b05209acb5d40264de9f7228963a
MiguelBits <mcv.cooper@gmail.com>
2026-01-29 16:22:04 +0000

    Add Satsuma DEX volume/fees (Citrea)

---
+++ dexs/satsuma/index.ts
@@ -0,0 +1,33 @@
+import { SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getChainVolume } from "../../helpers/getUniSubgraphVolume";
+
+const endpoints = {
+  [CHAIN.CITREA]: "https://api.goldsky.com/api/public/project_cmamb6kkls0v2010932jjhxj4/subgraphs/analytics-mainnet/v1.0.3/gn"
+};
+
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: "factories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "algebraDayData",
+    field: "volumeUSD",
+    dateField: "date"
+  },
+  hasDailyVolume: true,
+});
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  adapter: {
+    [CHAIN.CITREA]: {
+      fetch: graphs(CHAIN.CITREA),
+      start: "2024-11-01",
+    },
+  },
+};
+
+export default adapter;
