a5107de96303da6ed2d9c8b59b23b8fb47e38f73
manualy <danielswietytrue@gmail.com>
2026-01-29 14:46:09 +0100

    go back to old date for backfill compatibility

--- open-interest/injective-derivatives.ts
+++ open-interest/injective-derivatives.ts
@@ -1,12 +1,17 @@
 import { httpGet } from "../utils/fetchURL";
-import { FetchOptions, } from "../adapters/types";
+import { FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
 const OPEN_INTEREST_URL = `https://bigquery-api-636134865280.europe-west1.run.app/open_interest`;
 
 const fetch = async (_: number, _t: any, options: FetchOptions) => {
-  const openInterestRes: any = await httpGet(`${OPEN_INTEREST_URL}?start_date=${options.dateString}`);
-  if (openInterestRes.days.length !== 1) throw new Error("No OI data found for the given date: " + options.dateString);
+  const openInterestRes: any = await httpGet(
+    `${OPEN_INTEREST_URL}?start_date=${options.dateString}`,
+  );
+  if (openInterestRes.days.length !== 1)
+    throw new Error(
+      "No OI data found for the given date: " + options.dateString,
+    );
 
   return {
     openInterestAtEnd: openInterestRes.total_open_interest,
@@ -15,6 +20,6 @@ const fetch = async (_: number, _t: any, options: FetchOptions) => {
 
 export default {
   fetch,
-  start: "2025-09-12",
-  chains: [CHAIN.INJECTIVE]
+  start: "2021-07-17",
+  chains: [CHAIN.INJECTIVE],
 };
