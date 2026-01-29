840172607a760cc0772a1a3955c84151e59c8541
Felix Bruguera <felixbruguera0@gmail.com>
2026-01-29 15:05:17 -0400

    add fees

--- dexs/saber/index.ts
+++ dexs/saber/index.ts
@@ -14,36 +14,76 @@ import { queryDuneSql } from "../../helpers/dune"
 
 async function fetch(_t: any, _a: any, options: FetchOptions) {
   const dailyVolume = options.createBalances()
+  const dailyFees = options.createBalances()
+
   const query = `
-  WITH amounts AS (
+  WITH transactions AS (
     SELECT 
-      SUM(varbinary_to_bigint(reverse(varbinary_substring(data, 2, 8)))) as inAmount,
-      account_arguments[5] AS poolSource
+      varbinary_to_bigint(reverse(varbinary_substring(data, 2, 8))) as inAmount,
+      account_arguments[5] AS poolSource,
+      account_arguments[6] as poolDestination,
+      account_arguments[8] as adminAddress,
+      tx_id
     FROM solana.instruction_calls
-    WHERE inner_executing_account = 'SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ'
+    WHERE executing_account = 'SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ'
     AND TIME_RANGE
-    AND is_inner = true
+    AND varbinary_substring(data, 1, 1) = 0x01
     AND tx_success = true
-    GROUP BY 2
+  ),
+  fees AS (
+    SELECT SUM(amount_usd) as fees, token_mint_address as mint
+    FROM tokens_solana.transfers t
+    INNER JOIN transactions
+    ON transactions.poolDestination = t.from_token_account 
+    AND transactions.adminAddress = t.to_token_account 
+    AND transactions.tx_id = t.tx_id
+    WHERE TIME_RANGE
+    GROUP BY token_mint_address
+  ), 
+  volume AS (
+    SELECT SUM(transactions.inAmount) AS volume, ta.token_mint_address AS mint
+    FROM transactions
+    INNER JOIN solana_utils.token_accounts ta
+    ON transactions.poolSource = ta.address
+    GROUP BY ta.token_mint_address
   )
-  SELECT amounts.inAmount AS amount, ta.token_mint_address AS mint
-  FROM amounts
-  INNER JOIN solana_utils.token_accounts ta
-  ON amounts.poolSource = ta.address`
+  SELECT 
+    COALESCE(f.mint, v.mint) as mint,
+    COALESCE(f.fees, 0) as fees,
+    COALESCE(v.volume, 0) as volume
+  FROM fees f
+  FULL OUTER JOIN volume v 
+  ON f.mint = v.mint`
 
- const result = await queryDuneSql(options, query)
- result.forEach((result: Record<string, any>) => dailyVolume.add(result.mint, result.amount))
-
- return {
-  dailyVolume
- }
+  const result = await queryDuneSql(options, query)
+  result.forEach((row: Record<string, any>) => {
+    dailyVolume.add(row.mint, row.volume)
+    dailyFees.addUSDValue(row.fees)
+  })
+  const dailySupplySideRevenue = dailyFees.clone(0.5)
+  const dailyRevenue = dailyFees.clone(0.5)  
+  return {
+    dailyVolume,
+    dailyFees,
+    dailyUserFees: dailyFees,
+    dailySupplySideRevenue,
+    dailyRevenue,
+    dailyProtocolRevenue: dailyRevenue
+  }
 }
 
 const adapter : SimpleAdapter = {
   version: 1,
   fetch,
+  methodology: {
+    Fees: 'Swap fees paid by users',
+    UserFees: 'Swap fees paid by users',
+    SupplySideRevenue: 'Half of the swap fees go to LPs',
+    Revenue: 'Half ot the swap fees go to the protocol',
+    ProtocolRevenue: 'Half of the swap fees go to the protocol'
+  },
   chains: [CHAIN.SOLANA],
-  start: "2021-06-26",
+  start: "2021-05-28",
   dependencies: [Dependencies.DUNE],
   isExpensiveAdapter: true
 }
