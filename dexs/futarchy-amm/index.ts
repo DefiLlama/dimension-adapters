42c35d78f9c4dac35c6897b6636e2d2d3ed5c590
treeoflife2 <sol.analyzoor@gmail.com>
2026-02-03 13:06:41 +0530

    Refactor metadao and add breakdownmethodology

--- dexs/futarchy-amm/index.ts
+++ dexs/futarchy-amm/index.ts
@@ -1,15 +1,50 @@
+/*
+Futarchy AMM Volume
+
+Calculates total USDC-equivalent swap volume via the Futarchy AMM.
+- Buy swaps: volume = USDC input
+- Sell swaps: volume = USDC output
+
+Parameters:
+  {{start}} - Unix timestamp for start of period
+  {{end}} - Unix timestamp for end of period
+*/
+
 import { Dependencies, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { getSqlFromFile, queryDuneSql } from "../../helpers/dune";
+import { queryDuneSql } from "../../helpers/dune";
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const dailyVolume = options.createBalances();
-
-  const sql = getSqlFromFile('helpers/queries/futarchy-volume.sql', {
-    start: options.startTimestamp,
-    end: options.endTimestamp,
-  });
-
+  const sql = `
+    WITH futswap AS (
+        SELECT
+            CASE
+                WHEN to_hex(SUBSTR(data, 105, 1)) = '00' THEN 'buy'
+                WHEN to_hex(SUBSTR(data, 105, 1)) = '01' THEN 'sell'
+            END AS swap_type,
+            from_big_endian_64(reverse(SUBSTR(data, 106, 8))) / 1e6 AS input_amount,
+            from_big_endian_64(reverse(SUBSTR(data, 114, 8))) / 1e6 AS output_amount
+        FROM solana.instruction_calls
+        WHERE TIME_RANGE
+          AND executing_account = 'FUTARELBfJfQ8RDGhg1wdhddq1odMAJUePHFuBYfUxKq'
+          AND inner_executing_account = 'FUTARELBfJfQ8RDGhg1wdhddq1odMAJUePHFuBYfUxKq'
+          AND account_arguments[1] = 'DGEympSS4qLvdr9r3uGHTfACdN8snShk4iGdJtZPxuBC'
+          AND cardinality(account_arguments) = 1
+          AND is_inner = true
+          AND tx_success = true
+          AND CAST(data AS VARCHAR) LIKE '0xe445a52e51cb9a1d%'
+          AND LENGTH(data) >= 300
+          AND array_join(log_messages, ' ') LIKE '%SpotSwap%'
+    )
+    SELECT 
+      SUM(CASE 
+        WHEN swap_type = 'buy' THEN input_amount 
+        ELSE output_amount 
+      END) AS volume
+    FROM futswap
+    WHERE swap_type IN ('buy', 'sell')
+  `
   const result = await queryDuneSql(options, sql);
   const volume = result[0]?.volume ?? 0;
   dailyVolume.addUSDValue(volume);
@@ -18,17 +53,12 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
 };
 
 const adapter: SimpleAdapter = {
-  adapter: {
-    [CHAIN.SOLANA]: {
-      fetch,
-      start: '2025-10-09',
-    },
-  },
+  version: 1,
+  fetch,
+  chains: [CHAIN.SOLANA],
+  start: '2025-10-09',
   dependencies: [Dependencies.DUNE],
   isExpensiveAdapter: true,
-  methodology: {
-    Volume: "Volume represents total USDC-equivalent value swapped via Futarchy AMM SpotSwap events. For buys, volume = input_amount; for sells, volume = output_amount.",
-  },
 };
 
 export default adapter;
--- fees/futarchy-amm/index.ts
+++ fees/futarchy-amm/index.ts
@@ -14,36 +14,24 @@ import { getSqlFromFile, queryDuneSql } from '../../helpers/dune'
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const dailyFees = options.createBalances()
-  const dailyRevenue = options.createBalances()
 
-  const ammSql = getSqlFromFile('helpers/queries/futarchy-amm.sql', {
+  const query = await getSqlFromFile('helpers/queries/futarchy.sql', {
     start: options.startTimestamp,
     end: options.endTimestamp,
   })
 
-  const meteoraSql = getSqlFromFile('helpers/queries/futarchy-meteora.sql', {
-    start: options.startTimestamp,
-    end: options.endTimestamp,
+  const result = await queryDuneSql(options, query)
+  
+  result.forEach((row: any) => {
+    const fees = row.total_fees_usd ?? 0
+    dailyFees.addUSDValue(fees, row.source)
   })
-
-  const [ammResults, meteoraResults] = await Promise.all([
-    queryDuneSql(options, ammSql, { extraUIDKey: 'amm' }),
-    queryDuneSql(options, meteoraSql, { extraUIDKey: 'meteora' }),
-  ])
-
-  const ammFees = ammResults[0]?.total_fees_usd ?? 0
-  const meteoraFees = meteoraResults[0]?.total_fees_usd ?? 0
-
-  dailyFees.addUSDValue(ammFees)
-  dailyFees.addUSDValue(meteoraFees)
-  dailyRevenue.addUSDValue(ammFees)
-  dailyRevenue.addUSDValue(meteoraFees)
 
   return {
     dailyFees,
     dailyUserFees: dailyFees,
-    dailyRevenue,
-    dailyProtocolRevenue: dailyRevenue,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
   }
 }
 
@@ -54,17 +42,22 @@ const methodology = {
   ProtocolRevenue: 'All fees are protocol revenue - 100% of AMM fees and 100% of Meteora LP fees.',
 }
 
+const breakdownMethodology = {
+  Fees: {
+    'meteora_damm': '0.4% LP fees from Meteora DAMM pools where Futarchy DAOs own 100% liquidity',
+    'futarchy_amm': '0.5% fees from Futarchy AMM swaps',
+  },
+}
+
 const adapter: SimpleAdapter = {
   version: 1,
-  adapter: {
-    [CHAIN.SOLANA]: {
-      fetch,
-      start: '2025-10-09',
-    },
-  },
+  fetch,
+  chains: [CHAIN.SOLANA],
+  start: '2025-10-09',
   dependencies: [Dependencies.DUNE],
   isExpensiveAdapter: true,
   methodology,
+  breakdownMethodology
 }
 
 export default adapter
