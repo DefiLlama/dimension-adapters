833561fabe1887763c7e94e59dc24b5446236ca0
FelixBruguera <felixbruguera0@gmail.com>
2026-02-04 04:49:58 -0400

    fix frax revenue (#5810)

--- fees/frax-amo.ts
+++ fees/frax-amo.ts
@@ -47,7 +47,7 @@ const fetch = async (timestamp: number, _: any, options: FetchOptions): Promise<
   return {
     timestamp,
     dailyFees,
-    dailyRevenue: dailyFees,
+    dailyRevenue: dailyProtocolRevenue,
     dailyProtocolRevenue: dailyProtocolRevenue,
     dailySupplySideRevenue: dailySupplySideRevenue,
   };
@@ -90,7 +90,7 @@ const adapter: SimpleAdapter = {
   version: 1,
   methodology: {
     Fees: 'Total interest paid to users by borrowing FRAX.',
-    Revenue: 'Total interest paid to users by borrowing FRAX.',
+    Revenue: 'Amount of interest collected by Frax Finance.',
     ProtocolRevenue: 'Amount of interest collected by Frax Finance.',
     SupplySideRevenue: 'Amount of interest paid to lenders.',
   }
