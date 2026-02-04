a7e16985dfb713a3381b5abe73b77ace36022957
treeoflife2 <sol.analyzoor@gmail.com>
2026-02-04 21:06:33 +0530

    refactor dune query

--- fees/fomo/index.ts
+++ fees/fomo/index.ts
@@ -1,5 +1,3 @@
-// source: https://dune.com/queries/5673933/9216113
-
 import { Dependencies, FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { queryDuneSql } from "../../helpers/dune";
@@ -22,6 +20,8 @@ const fetch = async (_: any, _1: any, options: FetchOptions) => {
       FROM solana.account_activity
       WHERE
         tx_success
+        AND block_time >= from_unixtime(${options.startTimestamp})
+        AND block_time < from_unixtime(${options.endTimestamp})
         AND address = 'HrTf9CzXR1dRH4Sof5QrpmGWwpwAf3qZzwCsEjQpXcSq'
         AND token_balance_change > 0 
         AND token_mint_address = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
@@ -30,60 +30,42 @@ const fetch = async (_: any, _1: any, options: FetchOptions) => {
     -- Filter to only bot trades (exclude market maker)
     onchain_bot_fees AS (
       SELECT 
-        DATE(trades.block_time) AS fee_date,
         trades.tx_id,
         MAX(fee_token_amount) AS fee
       FROM dex_solana.trades AS trades
       JOIN onchain_fee_payments AS fp ON trades.tx_id = fp.tx_id
       WHERE
         trades.trader_id != 'R4rNJHaffSUotNmqSKNEfDcJE8A7zJUkaoM5Jkd7cYX'
-      GROUP BY DATE(trades.block_time), trades.tx_id
+      GROUP BY trades.tx_id
     ),
 
-    -- Aggregate on-chain fees by day
-    onchain_daily AS (
+    -- Aggregate on-chain fees
+    onchain_total AS (
       SELECT
-        fee_date,
         SUM(fee) AS fee_usd
       FROM onchain_bot_fees
-      GROUP BY fee_date
     ),
 
     -- Off-chain relay fees (deduplicated)
     offchain_ranked AS (
       SELECT
-        CAST(fee_period AS DATE) AS fee_date,
         platform_fees + referral_fees AS fee_usd,
         ROW_NUMBER() OVER (PARTITION BY fee_period ORDER BY synced_at DESC) AS rn
       FROM dune.tryfomo.fomo_relay_fees
+      WHERE fee_period >= from_unixtime(${options.startTimestamp})
+        AND fee_period < from_unixtime(${options.endTimestamp})
     ),
 
-    offchain_daily AS (
-      SELECT fee_date, fee_usd
+    offchain_total AS (
+      SELECT SUM(fee_usd) AS fee_usd
       FROM offchain_ranked
       WHERE rn = 1
-    ),
-
-    -- Combine both sources
-    combined AS (
-      SELECT fee_date, fee_usd FROM onchain_daily
-      UNION ALL
-      SELECT fee_date, fee_usd FROM offchain_daily
     )
 
     SELECT
-      fee_date,
-      SUM(fee_usd) AS fee_usd
-    FROM (
-      SELECT
-        fee_date,
-        fee_usd,
-        MAX(fee_date) OVER () AS max_fee_date
-      FROM combined
-    )
-    WHERE fee_date < max_fee_date
-    GROUP BY fee_date
-    ORDER BY fee_date DESC
+      COALESCE(onchain_total.fee_usd, 0) + COALESCE(offchain_total.fee_usd, 0) AS fee_usd
+    FROM onchain_total
+    CROSS JOIN offchain_total
   `;
 
   const fees = await queryDuneSql(options, query);
