00c19ac27df84fbe883d95c7e4c52cbeaae71f75
bheluga <bheluga@defillama.com>
2026-02-07 16:49:04 +0530

    refactor

--- dexs/denaria.ts
+++ dexs/denaria.ts
@@ -2,61 +2,65 @@ import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
 const DENARIA_PERP_PAIR = "0xd07822ee341c11a193869034d7e5f583c4a94872";
-const LINEA_USDC = "0x176211869cA2b568f2A7D4EE941E073a821EE1ff";
 
 const EXECUTED_TRADE_EVENT =
-  "event ExecutedTrade(address indexed user, bool direction, uint256 tradeSize, uint256 tradeReturn, uint256 currentPrice, uint256 leverage)";
+    "event ExecutedTrade(address indexed user, bool direction, uint256 tradeSize, uint256 tradeReturn, uint256 currentPrice, uint256 leverage)";
 
 async function fetch(options: FetchOptions) {
-  const dailyVolume = options.createBalances();
+    const dailyVolume = options.createBalances();
+    const dailyFees = options.createBalances();
 
-  const fromBlock = await options.getFromBlock();
-  const toBlock = await options.getToBlock();
+    const tradeLogs = await options.getLogs({
+        target: DENARIA_PERP_PAIR,
+        eventAbi: EXECUTED_TRADE_EVENT,
+    });
 
-  const tradeLogs: any[] = await options.getLogs({
-    target: DENARIA_PERP_PAIR,
-    eventAbi: EXECUTED_TRADE_EVENT,
-    fromBlock,
-    toBlock,
-  });
+    const totalTraderExposure = await options.api.call({
+        target: DENARIA_PERP_PAIR,
+        abi: "uint256:totalTraderExposure",
+    });
 
-  // console.log("[Denaria][Volume]", { fromBlock, toBlock, logs: tradeLogs.length });
+    for (const log of tradeLogs) {
+        const isLong = Boolean(log.direction);
 
-  for (const log of tradeLogs) {
-    const isLong = Boolean(log.direction);
+        const tradeSize = Number(log.tradeSize) / 1e18;
+        const price = Number(log.currentPrice) / 1e8;
+        const tradeReturn = Number(log.tradeReturn) / 1e18;
 
-    const tradeSize18 = BigInt(log.tradeSize);     // 1e18
-    const price1e8 = BigInt(log.currentPrice);     // 1e8
-
-    // Volume in USD(1e18)
-    let volumeUsd18 = 0n;
-
-    if (isLong === false) {
-      // SHORT: volume = tradeSize(BTC 1e18) * price(1e8) / 1e8
-      volumeUsd18 = (tradeSize18 * price1e8) / BigInt(1e8);
-    } else {
-      // LONG: volume = tradeSize(USD 1e18)
-      volumeUsd18 = tradeSize18;
+        if (isLong) {
+            dailyVolume.addUSDValue(tradeSize);
+            dailyFees.addUSDValue(tradeSize - (tradeReturn * price));
+        } else {
+            dailyVolume.addUSDValue(tradeSize * price);
+            dailyFees.addUSDValue(tradeSize * price - tradeReturn);
+        }
     }
 
-    // USD(1e18) -> USDC raw(1e6) dividendo per 1e12
-    const volumeUsdc6 = volumeUsd18 / BigInt(1e12);
-    if (volumeUsdc6 > 0n) dailyVolume.add(LINEA_USDC, volumeUsdc6.toString());
-  }
+    const openInterestAtEnd = options.createBalances();
+    openInterestAtEnd.addCGToken("bitcoin", totalTraderExposure / 1e18);
 
-  return { dailyVolume };
+    return {
+        dailyVolume,
+        dailyFees,
+        dailyRevenue: dailyFees,
+        dailyProtocolRevenue: dailyFees,
+        openInterestAtEnd,
+    };
 }
 
 const methodology = {
-  Volume: "All Denaria Perp volume on Linea, computed from ExecutedTrade notional in virtual USD.",
+    Volume: "All Denaria Perp volume on Linea, computed from ExecutedTrade notional in virtual USD.",
+    Fees: "Deneria Perp trading fees",
+    Revenue: "All the fees are revenue",
+    ProtocolRevenue: "All the revenue goes to the protocol",
 };
 
 const adapter: SimpleAdapter = {
-  version: 2,
-  chains: [CHAIN.LINEA],
-  fetch,
-  start: "2025-12-14",
-  methodology,
+    version: 2,
+    chains: [CHAIN.LINEA],
+    fetch,
+    start: "2025-12-15",
+    methodology,
 };
 
 export default adapter;
