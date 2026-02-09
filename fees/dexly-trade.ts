98df72fb00d66daa9296ad785ef6679eba31d783
ue <ugurerdal93@gmail.com>
2026-02-09 17:15:30 +0300

    Create dexly-trade.ts

---
+++ fees/dexly-trade.ts
@@ -0,0 +1,36 @@
+import { CHAIN } from "../helpers/chains";
+import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { fetchBuilderCodeRevenue } from "../helpers/hyperliquid";
+
+const BASED_APP_BUILDER_ADD = "0x22047776933bC123D0602ed17aaF0D2f5647DF0C";
+
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+  const { dailyVolume, dailyFees, dailyRevenue, dailyProtocolRevenue } =
+    await fetchBuilderCodeRevenue({
+      options,
+      builder_address: BASED_APP_BUILDER_ADD,
+    });
+
+  return {
+    dailyVolume,
+    dailyFees,
+    dailyRevenue,
+    dailyProtocolRevenue,
+  };
+};
+
+const methodology = {
+  Fees: "builder code revenue from Hyperliquid Perps Trades.",
+  Revenue: "builder code revenue from Hyperliquid Perps Trades.",
+  ProtocolRevenue: "builder code revenue from Hyperliquid Perps Trades.",
+};
+
+const adapter: SimpleAdapter = {
+  fetch,
+  chains: [CHAIN.HYPERLIQUID],
+  start: "2026-02-09",
+  methodology,
+  doublecounted: true,
+};
+
+export default adapter;
