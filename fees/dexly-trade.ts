4150bab8c4ec72c6fcfdfa32417455193babad97
ue <ugurerdal93@gmail.com>
2026-02-10 00:30:03 +0300

    Update dexly-trade.ts

--- fees/dexly-trade.ts
+++ fees/dexly-trade.ts
@@ -2,13 +2,13 @@ import { CHAIN } from "../helpers/chains";
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { fetchBuilderCodeRevenue } from "../helpers/hyperliquid";
 
-const BASED_APP_BUILDER_ADD = "0x22047776933bC123D0602ed17aaF0D2f5647DF0C";
+const DEXLY_TRADE_APP_BUILDER_ADD = "0x22047776933bC123D0602ed17aaF0D2f5647DF0C";
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const { dailyVolume, dailyFees, dailyRevenue, dailyProtocolRevenue } =
     await fetchBuilderCodeRevenue({
       options,
-      builder_address: BASED_APP_BUILDER_ADD,
+      builder_address: DEXLY_TRADE_APP_BUILDER_ADD,
     });
 
   return {
