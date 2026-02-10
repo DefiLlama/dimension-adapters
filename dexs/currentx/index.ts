26370a6b3f939feca0cbca57131d72dbb51f3de0
DocPis <docpis@tutamail.com>
2026-02-10 03:23:17 +0100

    Update index.ts

--- dexs/currentx/index.ts
+++ dexs/currentx/index.ts
@@ -4,18 +4,15 @@ import { getUniV3LogAdapter } from "../../helpers/uniswap";
 
 // https://docs.currentx.app
 const feesConfig = {
-  userFeesRatio: 0.75,  // 75% Users
-  revenueRatio: 0.25,   // 25% Protocol
-};
+  userFeesRatio: 0.75,
+  revenueRatio: 0.25,
+}
 
 const adapter: SimpleAdapter = {
   version: 2,
   adapter: {
-    [CHAIN.MEGAETH]: {
-      fetch: getUniV3LogAdapter({
-        factory: "0x09cF8A0b9e8C89bff6d1ACbe1467e8E335Bdd03E",
-        ...feesConfig,
-      }),
+    [CHAIN.CITREA]: {
+      fetch: getUniV3LogAdapter({ factory: '0x09cF8A0b9e8C89bff6d1ACbe1467e8E335Bdd03E', ...feesConfig }),
       start: "2026-02-05",
     },
   },
