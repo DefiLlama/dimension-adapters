4fcf32b06a2767564a7aa54e03773420c0a7fdbe
DocPis <docpis@tutamail.com>
2026-02-10 03:19:45 +0100

    Refactor adapter to use getUniV3LogAdapter

--- dexs/currentx/index.ts
+++ dexs/currentx/index.ts
@@ -1,34 +1,24 @@
-import request, { gql } from "graphql-request";
-import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { getUniV3LogAdapter } from "../../helpers/uniswap";
 
-const fetch = async (timestamp: number, _:any, options: FetchOptions): Promise<any> => {
-    const dayID = Math.floor(options.startOfDay / 86400);
-    const query =gql`
-    {
-        uniswapDayData(id:${dayID}) {
-            id
-            dailyVolumeUSD
-        }
-    }`;
-    const url = "https://api.goldsky.com/api/public/project_cmlbj5xkhtfha01z0caladt37/subgraphs/currentx-v2/1.0.0/gn";
-    const req = await request(url, query);
-    if (!req.uniswapDayData) {
-         return { dailyVolume: "0" };
-    }
-    return {
-        dailyVolume: req.uniswapDayData.dailyVolumeUSD,
-    }
-}
+// https://docs.currentx.app
+const feesConfig = {
+  userFeesRatio: 0.75,  // 75% Users
+  revenueRatio: 0.25,   // 25% Protocol
+};
 
 const adapter: SimpleAdapter = {
-    adapter: {
-        [CHAIN.MEGAETH]: {
-            fetch,
-            start: '2026-02-05',
-        },
-    }
-}
+  version: 2,
+  adapter: {
+    [CHAIN.MEGAETH]: {
+      fetch: getUniV3LogAdapter({
+        factory: "0x09cF8A0b9e8C89bff6d1ACbe1467e8E335Bdd03E",
+        ...feesConfig,
+      }),
+      start: "2026-02-05",
+    },
+  },
+};
 
 export default adapter;
-
