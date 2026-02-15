d702af7a4622452b18049e981fe773381cef02c0
Kermo <100954684+0xKermo@users.noreply.github.com>
2026-02-15 11:35:43 +0300

    Remove chain configuration for SCROLL (#5908)

--- aggregators/fibrous-finance/index.ts
+++ aggregators/fibrous-finance/index.ts
@@ -3,7 +3,6 @@ import { FetchOptions, FetchResult } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 
 const chainConfig: Record<string, { id: number, start: string }> = {
-  [CHAIN.SCROLL]: {id: 534352, start: '2024-10-09'},
   [CHAIN.BASE]: { id: 8453, start: '2025-04-04' },
   [CHAIN.HYPERLIQUID]: { id: 999, start: '2025-09-08' },
   [CHAIN.STARKNET]: {id:23448594291968336, start: '2024-03-02'},
