24d89648f5537fe38c1ef4b0aa81ade4b5f6ae7a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-18 11:10:18 +0100

    consolidate hl adapters

--- dexs/echosync-perps.ts
+++
@@ -1,8 +0,0 @@
-import { exportBuilderAdapter } from '../helpers/hyperliquid'
-
-// https://www.echosync.io/en
-
-const START = '2025-11-07';
-const HL_BUILDER_ADDRESS = '0x831ad7eb3e600a3ab8df851ce27df8d8dd6b5d9c';
-
-export default exportBuilderAdapter([HL_BUILDER_ADDRESS], { start: START })
