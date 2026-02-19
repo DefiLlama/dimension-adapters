cbfa1d22e2fc941dafb894d4eea3c36afb90a4fe
parity-dex <parity.manager@gmail.com>
2026-02-19 10:54:44 +0900

    add parity dex

---
+++ dexs/parity-dex-cl/index.ts
@@ -0,0 +1,9 @@
+import { CHAIN } from "../../helpers/chains";
+import { uniV3Exports } from "../../helpers/uniswap";
+
+export default uniV3Exports({
+  [CHAIN.MONAD]: {
+    factory: '0x2A6CE23C5017aF1b07B9c4E4014442aDE18Bd404',
+    start: '2026-02-11',
+  },
+})
