89e9829aac818eedb3e2d9c4c9d2c7e8c23095e9
bheluga <anonymous_cat_llama@protonmail.com>
2026-02-20 22:54:10 +0530

    move volume to kline, oi to new folder

--- dexs/pacifica/index.ts
+++ dexs/pacifica/index.ts
@@ -1,21 +1,28 @@
+import { PromisePool } from "@supercharge/promise-pool";
 import { CHAIN } from "../../helpers/chains";
-import fetchURL from "../../utils/fetchURL";
+import { FetchOptions } from "../../adapters/types";
+import fetchURL, { fetchURLAutoHandleRateLimit } from "../../utils/fetchURL";
 
-const fetch = async (_a: any) => {
-  const { data } = await fetchURL('https://api.pacifica.fi/api/v1/info/prices')
-
-  const volume = data.reduce((a: number, b: { volume_24h: string }) => a + Number(b.volume_24h), 0)
-  const oi = data.reduce((a: number, b: { open_interest: string, mark: string }) => a + (Number(b.open_interest) * Number(b.mark)), 0)
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+  const data = await fetchURL('https://api.pacifica.fi/api/v1/info')
+  const tickers = data.data.map((tradeSummary: any) => tradeSummary.symbol)
+  let volume = 0;
+  await PromisePool.withConcurrency(1)
+    .for(tickers)
+    .process(async (ticker) => {
+      const data = await fetchURLAutoHandleRateLimit(`https://api.pacifica.fi/api/v1/kline?symbol=${ticker}&interval=1d&start_time=${(options.startTimestamp) * 1000}`)
+      const todaysData = data.data.filter((kline: any) => kline.t >= options.startTimestamp * 1000 && kline.T <= options.endTimestamp * 1000);
+      if (todaysData.length === 0) return;
+      volume += +todaysData[0].v * +todaysData[0].c;
+    })
 
   return {
-    dailyVolume: volume/2,
-    openInterestAtEnd: oi,
-  };
+    dailyVolume: volume / 2, //Volumes in both directions are counted in klines
+  }
 }
 
 export default {
-  version: 2,
   fetch,
   chains: [CHAIN.SOLANA],
-  runAtCurrTime: true
+  start: '2025-06-09'
 }
