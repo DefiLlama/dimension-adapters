cad381f19b841ffa4ba0f9b324c839624cc75e93
bheluga <bheluga@defillama.com>
2026-02-24 19:00:48 +0530

    refactor

--- fees/mixoor/index.ts
+++ fees/mixoor/index.ts
@@ -6,43 +6,36 @@ import {
 import { CHAIN } from "../../helpers/chains";
 import { queryDuneSql } from "../../helpers/dune";
 
-const FEE_WALLET = "9qX97Bd8dvHAknHVjCxz4uEJcPSE3NGjjgniMVdDBu6d";
-const SOL_MINT = "So11111111111111111111111111111111111111112";
-
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+
+  const FEE_WALLET = '9qX97Bd8dvHAknHVjCxz4uEJcPSE3NGjjgniMVdDBu6d'
+  const query = `
+  SELECT
+    'So11111111111111111111111111111111111111112' AS token,
+    COALESCE(SUM(balance_change), 0) AS total_fees
+  FROM solana.account_activity
+  WHERE address = '${FEE_WALLET}'
+    AND balance_change > 0
+    AND TIME_RANGE
+
+  UNION ALL
+
+  SELECT
+    token_mint_address AS token,
+    COALESCE(SUM(amount), 0) AS total_fees
+  FROM tokens_solana.transfers
+  WHERE to_owner = '${FEE_WALLET}'
+    AND from_owner != '${FEE_WALLET}'
+    AND TIME_RANGE
+  GROUP BY token_mint_address
+`
+  const result = await queryDuneSql(options, query);
   const dailyFees = options.createBalances();
 
-  // SOL fees: use account_activity to capture in-program SOL transfers
-  // (system program transfers AND CPI-based deductions from program accounts)
-  const solQuery = `
-    SELECT SUM(balance_change) AS sol_amount
-    FROM solana.account_activity
-    WHERE address = '${FEE_WALLET}'
-      AND balance_change > 0
-      AND token_mint_address IS NULL
-      AND TIME_RANGE
-  `;
-  const solResult = await queryDuneSql(options, solQuery);
-  if (solResult?.[0]?.sol_amount) {
-    dailyFees.add(SOL_MINT, solResult[0].sol_amount);
-  }
-
-  // SPL token fees: use token transfers table
-  const splQuery = `
-    SELECT token_mint_address AS mint, SUM(amount) AS amount
-    FROM tokens_solana.transfers
-    WHERE to_owner = '${FEE_WALLET}'
-      AND token_mint_address != '${SOL_MINT}'
-      AND TIME_RANGE
-    GROUP BY token_mint_address
-  `;
-  const splResult = await queryDuneSql(options, splQuery, {
-    extraUIDKey: "spl",
+  result.forEach((row: any) => {
+    dailyFees.add(row.token, row.total_fees);
   });
-  splResult?.forEach((row: any) => {
-    dailyFees.add(row.mint, row.amount);
-  });
-
+  
   return {
     dailyFees,
     dailyRevenue: dailyFees,
