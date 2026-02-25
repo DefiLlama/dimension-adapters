fe19d9d7220a93484aff343741d4d5908ba7a152
0xjosaphat <jo.saphat.metr@proton.me>
2026-02-25 13:21:38 +0100

    Metropolis DEX: adjustments URL's for subgraphs

--- fees/metropolis-dlmm/index.ts
+++ fees/metropolis-dlmm/index.ts
@@ -1,19 +1,19 @@
 import { CHAIN } from "../../helpers/chains";
 import { FetchOptions } from "../../adapters/types";
-import { httpPost } from "../../utils/fetchURL";
+import {httpGet} from "../../utils/fetchURL";
 
-const fetch = async (_: number, _block: any, { startOfDayId }: FetchOptions) => {
-  const query = `query { traderJoeDayData(id: ${startOfDayId}) { volumeUSD feesUSD } }`;
-  const { data: { traderJoeDayData: { volumeUSD, feesUSD } } } = await httpPost('https://sonic-graph-b.metropolis.exchange/subgraphs/name/metropolis/sonic-lb-v22-2-w-v', { query })
+const fetch = async (_: number, _block: any, { startOfDayId, chain }: FetchOptions) => {
+  const { volumeUSD, feesUSD } = await httpGet(`https://api-b.metropolis.exchange/api/v1/defilama/daily-stats/${chain}/${startOfDayId}`)
+
   const dailyVolume = +volumeUSD
   const dailyFees = +feesUSD
   return {
     dailyVolume,
     dailyFees,
-    dailySupplySideRevenue: dailyFees * 0.8,
     dailyRevenue: dailyFees * 0.2,
-    dailyProtocolRevenue: dailyFees * 0.05,
-    dailyHoldersRevenue: dailyFees * 0.15,
+    dailyProtocolRevenue: 0,
+    dailySupplySideRevenue: dailyFees * 0.8,
+    dailyHoldersRevenue: dailyFees * 0.2,
   }
 };
 
@@ -21,9 +21,9 @@ const fetch = async (_: number, _block: any, { startOfDayId }: FetchOptions) => 
 const methodology = {
   Fees: "Swap fees",
   Revenue: "20% of the swap fees",
-  ProtocolRevenue: "5% of the swap fees",
+  ProtocolRevenue: "0% of the swap fees",
   SupplySideRevenue: "80% of the swap fees",
-  HoldersRevenue: "15% of the swap fees",
+  HoldersRevenue: "20% of the swap fees",
 };
 
 export default {
