b7d4844739a71fd78cfc6c801061b162ab99ba1b
treeoflife2 <sol.analyzoor@gmail.com>
2026-02-26 17:51:22 +0530

    refactor

--- dexs/pacifica/index.ts
+++ dexs/pacifica/index.ts
@@ -1,28 +1,38 @@
 import { PromisePool } from "@supercharge/promise-pool";
 import { CHAIN } from "../../helpers/chains";
-import { FetchOptions } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import fetchURL, { fetchURLAutoHandleRateLimit } from "../../utils/fetchURL";
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const data = await fetchURL('https://api.pacifica.fi/api/v1/info')
+  if (!data.data){
+    throw new Error('Tickers are unavailable, please try again later');
+  }
+
   const tickers = data.data.map((tradeSummary: any) => tradeSummary.symbol)
-  let volume = 0;
-  await PromisePool.withConcurrency(1)
+  let dailyVolume = 0;
+
+  const { errors } = await PromisePool.withConcurrency(1)
     .for(tickers)
     .process(async (ticker) => {
-      const data = await fetchURLAutoHandleRateLimit(`https://api.pacifica.fi/api/v1/kline?symbol=${ticker}&interval=1d&start_time=${(options.startTimestamp) * 1000}`)
+      const data = await fetchURLAutoHandleRateLimit(`https://api.pacifica.fi/api/v1/kline?symbol=${ticker}&interval=1d&start_time=${(options.startOfDay) * 1000}`)
       const todaysData = data.data.filter((kline: any) => kline.t >= options.startTimestamp * 1000 && kline.T <= options.endTimestamp * 1000);
       if (todaysData.length === 0) return;
-      volume += +todaysData[0].v * +todaysData[0].c;
+      dailyVolume += (todaysData[0].v * +todaysData[0].c) / 2; // They include taker + maker in ohlcv candles
     })
 
-  return {
-    dailyVolume: volume / 2, //Volumes in both directions are counted in klines
+  if (errors.length > 0) {
+    throw new Error(`Failed to fetch data for ${errors.length} ticker(s): ${errors.map(e => e.message).join(', ')}`);
   }
+
+  return { dailyVolume }
 }
 
-export default {
+const adapter: SimpleAdapter = {
+  version: 1,
   fetch,
   chains: [CHAIN.SOLANA],
   start: '2025-06-09'
 }
+
+export default adapter;
