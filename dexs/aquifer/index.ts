82849f55dbbe5f9fdee267f16f874e23387675a8
TOTLSOTA <chris@ergonia.io>
2026-02-26 15:26:24 +0000

    move aquifer data parsing to proper dune table, it was very under-represented before

--- dexs/aquifer/index.ts
+++ dexs/aquifer/index.ts
@@ -5,40 +5,29 @@ import { CHAIN } from "../../helpers/chains"
 import { queryDuneSql } from "../../helpers/dune"
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
-    const query = `
-        with swaps as (
-            select
-                tx_id
-                , outer_instruction_index
-                , inner_instruction_index
-            from solana.instruction_calls
-            where executing_account = 'AQU1FRd7papthgdrwPTTq5JacJh8YtwEXaBfKU3bTz45'
-                and TIME_RANGE
-                and tx_success = true
-        )
-        select
-            SUM(amount_usd) as daily_volume
-        from tokens_solana.transfers t
-            inner join swaps s on t.tx_id = s.tx_id
-            and t.outer_instruction_index = s.outer_instruction_index
-            and t.inner_instruction_index = s.inner_instruction_index + 1
-        where t.block_time >= from_unixtime(${options.startTimestamp})
-        and t.block_time <= from_unixtime(${options.endTimestamp})
-    `
-    const data = await queryDuneSql(options, query)
+  const query = `
+    select
+      sum(amount_usd) as daily_volume
+    from dex_solana.trades
+    where TIME_RANGE
+      and block_time >= from_unixtime(${options.startTimestamp})
+      and block_time <= from_unixtime(${options.endTimestamp})
+      and project = 'aquifer'
+  `
+  const data = await queryDuneSql(options, query)
 
-    return {
-        dailyVolume: Number(data[0].daily_volume) || 0
-    }
+  return {
+    dailyVolume: Number(data?.[0]?.daily_volume) || 0,
+  }
 }
 
 const adapter: SimpleAdapter = {
-    version: 1,
-    fetch,
-    chains: [CHAIN.SOLANA],
-    dependencies: [Dependencies.DUNE],
-    isExpensiveAdapter: true,
-    start: '2025-09-07',
+  version: 1,
+  fetch,
+  chains: [CHAIN.SOLANA],
+  dependencies: [Dependencies.DUNE],
+  isExpensiveAdapter: true,
+  start: '2025-09-07',
 }
 
 export default adapter
