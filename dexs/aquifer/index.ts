237e5469188907e155101324fb9deaa6fa0513bb
totlsota <chris@ergonia.io>
2026-02-26 17:36:17 +0000

    update aquifer data parsing to proper dune table, it was very under-represented before  (#6010)
    * move aquifer data parsing to proper dune table, it was very under-represented before
    
    * pr feedback, remove redundant filter
    
    * smol refactor
    
    ---------
    
    Co-authored-by: bheluga <bheluga@defillama.com>

--- dexs/aquifer/index.ts
+++ dexs/aquifer/index.ts
@@ -6,29 +6,16 @@ import { queryDuneSql } from "../../helpers/dune"
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     const query = `
-        with swaps as (
-            select
-                tx_id
-                , outer_instruction_index
-                , inner_instruction_index
-            from solana.instruction_calls
-            where executing_account = 'AQU1FRd7papthgdrwPTTq5JacJh8YtwEXaBfKU3bTz45'
-                and TIME_RANGE
-                and tx_success = true
-        )
-        select
-            SUM(amount_usd) as daily_volume
-        from tokens_solana.transfers t
-            inner join swaps s on t.tx_id = s.tx_id
-            and t.outer_instruction_index = s.outer_instruction_index
-            and t.inner_instruction_index = s.inner_instruction_index + 1
-        where t.block_time >= from_unixtime(${options.startTimestamp})
-        and t.block_time <= from_unixtime(${options.endTimestamp})
-    `
+    select
+      coalesce(sum(amount_usd), 0) as daily_volume
+    from dex_solana.trades
+    where TIME_RANGE
+      and project = 'aquifer'
+  `
     const data = await queryDuneSql(options, query)
 
     return {
-        dailyVolume: Number(data[0].daily_volume) || 0
+        dailyVolume: data[0].daily_volume,
     }
 }
 
