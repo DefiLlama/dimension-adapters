38356f825a2e25b7761c2712516698d3ffc65736
0xjosaphat <jo.saphat.metr@proton.me>
2026-02-26 14:36:23 +0100

    Metropolis DEX: added switch for old and new fees

--- fees/metropolis-dlmm/index.ts
+++ fees/metropolis-dlmm/index.ts
@@ -1,34 +1,41 @@
-import { CHAIN } from "../../helpers/chains";
-import { FetchOptions } from "../../adapters/types";
+import {CHAIN} from "../../helpers/chains";
+import {FetchOptions} from "../../adapters/types";
 import {httpGet} from "../../utils/fetchURL";
 
-const fetch = async (_: number, _block: any, { startOfDayId, chain }: FetchOptions) => {
-  const { volumeUSD, feesUSD } = await httpGet(`https://api-b.metropolis.exchange/api/v1/defilama/daily-stats/${chain}/${startOfDayId}`)
+const NEW_FEES_DATE_1 = 20487
 
-  const dailyVolume = +volumeUSD
-  const dailyFees = +feesUSD
-  return {
-    dailyVolume,
-    dailyFees,
-    dailyRevenue: dailyFees * 0.2,
-    dailyProtocolRevenue: 0,
-    dailySupplySideRevenue: dailyFees * 0.8,
-    dailyHoldersRevenue: dailyFees * 0.2,
-  }
+const fetch = async (_: number, _block: any, {startOfDayId, chain}: FetchOptions) => {
+    const {
+        volumeUSD,
+        feesUSD
+    } = await httpGet(`https://api-b.metropolis.exchange/api/v1/defilama/daily-stats/${chain}/${startOfDayId}`)
+    const feeDailyProtocolRevenue: number = Number.parseInt(startOfDayId || "0") >= NEW_FEES_DATE_1 ? 0 : 0.05
+    const feeDailyHoldersRevenue: number = Number.parseInt(startOfDayId || "0") >= NEW_FEES_DATE_1 ? 0.2 : 0.15
+
+    const dailyVolume = +volumeUSD
+    const dailyFees = +feesUSD
+    return {
+        dailyVolume,
+        dailyFees,
+        dailyRevenue: dailyFees * 0.2,
+        dailyProtocolRevenue: dailyFees * feeDailyProtocolRevenue,
+        dailySupplySideRevenue: dailyFees * 0.8,
+        dailyHoldersRevenue: dailyFees * feeDailyHoldersRevenue,
+    }
 };
 
 // https://docs.metropolis.exchange/protocol/or-pools-and-farms/or-dlmm-lb
 const methodology = {
-  Fees: "Swap fees",
-  Revenue: "20% of the swap fees",
-  ProtocolRevenue: "0% of the swap fees",
-  SupplySideRevenue: "80% of the swap fees",
-  HoldersRevenue: "20% of the swap fees",
+    Fees: "Swap fees",
+    Revenue: "20% of the swap fees",
+    ProtocolRevenue: "0% of the swap fees",
+    SupplySideRevenue: "80% of the swap fees",
+    HoldersRevenue: "20% of the swap fees",
 };
 
 export default {
-  fetch,
-  start: "2024-12-16",
-  chains: [CHAIN.SONIC],
-  methodology,
+    fetch,
+    start: "2024-12-16",
+    chains: [CHAIN.SONIC],
+    methodology,
 }
