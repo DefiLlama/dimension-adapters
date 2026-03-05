42da7afb95906bb823d4ad18590a13bd6020a7f2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-03-05 19:52:49 +0100

    add workaround for dune indexing issue in aquifer adapter

--- dexs/aquifer/index.ts
+++ dexs/aquifer/index.ts
@@ -8,7 +8,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     // Workaround for dune indexing issue
     const now = Date.now()
     const tenHoursAgo = now - (10 * 60 * 60 * 1000)
-    if (options.endTimestamp && options.endTimestamp * 1000 < tenHoursAgo) {
+    if (options.endTimestamp && options.endTimestamp * 1000 > tenHoursAgo) {
         throw new Error("End timestamp is less than 10 hours ago, skipping due to dune indexing delay")
     }
     
