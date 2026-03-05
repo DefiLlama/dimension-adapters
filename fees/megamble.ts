23cb317b93708cb04843566badd428cdc4530d0d
Isonips Legendz <128399923+isonips@users.noreply.github.com>
2026-03-05 17:58:06 +0100

    Revise fee distribution structure in megamble.ts

--- fees/megamble.ts
+++ fees/megamble.ts
@@ -1,21 +1,17 @@
-import { Adapter, FetchOptions } from "../adapters/types"; import { CHAIN } from "../helpers/chains";
+import { Adapter, FetchOptions } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
 
 /**
  * Megamble - Fully on-chain competitive game on MegaETH
  *
  * Fee structure per click (0.001 ETH):
- *   - 85% goes to the pot (returned to winner)
- *   - 15% is protocol fee (accumulatedFees)
- *
- * When game ends, pot distribution:
- *   - 85% to the winner
- *   - 15% to protocol treasury
+ *   - 85% goes to the pot (winner takes all)
+ *   - 10% goes to treasury (protocol revenue)
+ *   - 5% goes to referrer (supply side revenue)
  *
  * Events used:
  *   Clicked(uint256 indexed round, address indexed player, string username,
  *           uint256 clickNumber, uint256 potAfter, bool usedCredit)
- *   GameEnded(uint256 indexed round, address indexed winner, string winnerName,
- *             uint256 prize, uint256 totalClicks, uint256 totalPot)
  */
 
 const GAME_CONTRACT = "0x051B5a8B20F3e49E073Cf7A37F4fE2e5117Af3b6";
@@ -24,6 +20,7 @@ const CLICK_PRICE = "1000000000000000"; // 0.001 ETH in wei
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
   const dailyRevenue = options.createBalances();
+  const dailySupplySideRevenue = options.createBalances();
   const dailyUserFees = options.createBalances();
 
   const clickLogs = await options.getLogs({
@@ -32,13 +29,6 @@ const fetch = async (options: FetchOptions) => {
       "event Clicked(uint256 indexed round, address indexed player, string username, uint256 clickNumber, uint256 potAfter, bool usedCredit)",
   });
 
-  const gameEndedLogs = await options.getLogs({
-    target: GAME_CONTRACT,
-    eventAbi:
-      "event GameEnded(uint256 indexed round, address indexed winner, string winnerName, uint256 prize, uint256 totalClicks, uint256 totalPot)",
-  });
-
-  // All clicks generate fees (both direct and credit clicks)
   const totalClicks = clickLogs.length;
   const totalSpend = BigInt(totalClicks) * BigInt(CLICK_PRICE);
 
@@ -47,23 +37,22 @@ const fetch = async (options: FetchOptions) => {
   const directSpend = BigInt(directClicks.length) * BigInt(CLICK_PRICE);
   dailyUserFees.addGasToken(directSpend);
 
-  // dailyFees = 15% of ALL clicks + 15% of each pot at game end
-  let totalFees = BigInt(0);
+  // dailyFees = 15% of all clicks (10% treasury + 5% referral)
+  const totalFeesAmount = (totalSpend * BigInt(15)) / BigInt(100);
+  dailyFees.addGasToken(totalFeesAmount);
 
-  const clickFees = (totalSpend * BigInt(15)) / BigInt(100);
-  totalFees += clickFees;
+  // dailyRevenue = 10% treasury (protocol revenue)
+  const treasuryAmount = (totalSpend * BigInt(10)) / BigInt(100);
+  dailyRevenue.addGasToken(treasuryAmount);
 
-  for (const log of gameEndedLogs) {
-    const protocolCut = (BigInt(log.totalPot) * BigInt(15)) / BigInt(100);
-    totalFees += protocolCut;
-  }
-
-  dailyFees.addGasToken(totalFees);
-  dailyRevenue.addGasToken(totalFees);
+  // dailySupplySideRevenue = 5% referral earnings
+  const referralAmount = (totalSpend * BigInt(5)) / BigInt(100);
+  dailySupplySideRevenue.addGasToken(referralAmount);
 
   return {
     dailyFees,
     dailyRevenue,
+    dailySupplySideRevenue,
     dailyUserFees,
   };
 };
@@ -79,4 +68,3 @@ const adapter: Adapter = {
 };
 
 export default adapter;
-
