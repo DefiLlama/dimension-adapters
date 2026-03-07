8fc7252c76214c1df79eb011b24cc05ae6c2be04
eden <noat.eth@gmail.com>
2026-03-07 17:53:38 +0700

    track harmonic and nozomi fees

--- fees/jito-mev-tips/index.ts
+++ fees/jito-mev-tips/index.ts
@@ -9,18 +9,19 @@ import { CHAIN } from "../../helpers/chains"
 import { getSolanaReceived } from "../../helpers/token"
 import { METRIC } from "../../helpers/metrics"
 
+export const JitoTipPaymentAddresses = [
+  '96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5',
+  'HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe',
+  'Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY',
+  'ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49',
+  'DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh',
+  'ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt',
+  'DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL',
+  '3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT',
+]
+
 const fetch = async (options: FetchOptions) => {
-  const targets = [
-    '96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5',
-    'HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe',
-    'Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY',
-    'ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49',
-    'DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh',
-    'ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt',
-    'DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL',
-    '3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT',
-  ]
-  const receivedBalances = await getSolanaReceived({ options, targets, })
+  const receivedBalances = await getSolanaReceived({ options, targets: JitoTipPaymentAddresses })
   const dailyFees = options.createBalances()
   dailyFees.addBalances(receivedBalances, METRIC.MEV_REWARDS)
   const dailySupplySideRevenue = dailyFees.clone(0.96);
