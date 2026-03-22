e0aaa0f5c9c0f6389632c029b97d396db64313bd
bheluga <anonymous_cat_llama@protonmail.com>
2026-03-22 23:02:27 +0530

    fix: katana fees

--- helpers/blockscoutFees.ts
+++ helpers/blockscoutFees.ts
@@ -135,6 +135,7 @@ export const chainConfigMap: any = {
   [CHAIN.VINUCHAIN]: { CGToken: 'vinuchain', explorer: 'https://vinuexplorer.org/' },
   [CHAIN.ENI]: { CGToken: 'wrapped-egas', explorer: 'https://scan.eniac.network/' },
   [CHAIN.MEGAETH]: { CGToken: 'ethereum', explorer: 'https://megaeth.blockscout.com/' },
+  [CHAIN.KATANA]: { CGToken: 'ethereum', explorer: 'https://explorer.katanarpc.com/' },
 }
 
 function getTimeString(timestamp: number) {
