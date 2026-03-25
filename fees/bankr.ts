a48fbe610747034a5e13be8c62daa4cadab6773e
treeoflife2 <sol.analyzoor@gmail.com>
2026-03-25 14:47:30 +0530

    keep pullhourly flag

--- fees/bankr.ts
+++ fees/bankr.ts
@@ -96,7 +96,7 @@ const fetch = async (options: FetchOptions) => {
 
 const adapter: SimpleAdapter = {
   version: 2,
-  // pullHourly: true,
+  pullHourly: true,
   fetch,
   start: '2025-08-11',
   chains: [CHAIN.BASE],
