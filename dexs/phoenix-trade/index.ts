274a65adb7b564f14e15b5d2039ce45ff8e66c08
bheluga <bheluga@defillama.com>
2026-03-28 14:12:35 +0530

    smol fix

--- dexs/phoenix-trade/index.ts
+++ dexs/phoenix-trade/index.ts
@@ -13,11 +13,14 @@ async function fetch(_: any, __: any, options: FetchOptions) {
     const marketsData = await fetchURL(`${PERP_API_URL}/view/markets`);
     const markets = marketsData.markets.map((market: any) => market.symbol);
 
+    const startTime = options.startOfDay * 1000;
+    const endTime = (options.endTimestamp) * 1000;
+
     await PromisePool.withConcurrency(1)
         .for(markets)
         .process(async (market) => {
-            const ohlcvData = await fetchURLAutoHandleRateLimit(`${PERP_API_URL}/candles/${market}?timeframe=1d&limit=300&startTime=${options.startOfDay * 1000}&endTime=${(options.endTimestamp) * 1000}`);
-            const todaysData = ohlcvData.filter((data: any) => data.time >= options.startOfDay * 1000 && data.time < (options.endTimestamp) * 1000);
+            const ohlcvData = await fetchURLAutoHandleRateLimit(`${PERP_API_URL}/candles/${market}?timeframe=1d&limit=300&startTime=${startTime}&endTime=${endTime}`);
+            const todaysData = ohlcvData.filter((data: any) => data.time >= startTime && data.time < endTime);
             dailyVolume.addUSDValue(todaysData[0]?.volume ?? 0);
             await sleep(1000);
         });
