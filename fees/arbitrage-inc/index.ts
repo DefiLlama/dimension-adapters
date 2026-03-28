15b428fc291a9af78fe921a278b8602cf4c8194a
Lukecele <luca@users.noreply.github.com>
2026-03-28 09:49:59 +0100

    feat: remove TOKENS filter from arbitrage-inc adapter since exchange supports all tokens

--- fees/arbitrage-inc/index.ts
+++ fees/arbitrage-inc/index.ts
@@ -4,18 +4,9 @@ import { addTokensReceived } from "../../helpers/token";
 
 const FEE_RECEIVER = '0xafF5340ECFaf7ce049261cff193f5FED6BDF04E7'.toLowerCase();
 
-const TOKENS = [
-  '0xbb4CdB9CBd36B01bD1cBaEBf2E08E7023b076de6', // WBNB
-  '0xe9e7CEA3DedcA698478E4cbC3F78dF2E8C6E2F8B', // BUSD
-  '0x55d398326f99059fF775485246999027B3197955', // USDT
-  '0x7130d2A12B9BCbFAe4f2634d864A1BCe1767a8D0', // BTCB
-  '0x2170Ed0880ac9A755fd29B2688956BD959F933F8', // ETH
-];
-
 const fetch = async (options: FetchOptions) => {
   const dailyFees = await addTokensReceived({
     options,
-    tokens: TOKENS,
     targets: [FEE_RECEIVER],
     skipIndexer: true,
   });
