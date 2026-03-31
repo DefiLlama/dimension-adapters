e9589b77e3c72a5dabb90e3282d808c026cf798a
Felix Bruguera <felixbruguera0@gmail.com>
2026-03-31 12:33:11 -0400

    add more chains

--- fees/airnft/index.ts
+++ fees/airnft/index.ts
@@ -3,23 +3,34 @@ import { CHAIN } from "../../helpers/chains";
 import { METRIC } from "../../helpers/metrics";
 import coreAssets from "../../helpers/coreAssets.json";
 
-const AIRNFT_CONTRACT = '0xF5db804101d8600c26598A1Ba465166c33CdAA4b'
+interface chainData {
+    contract: string;
+    asset: string;
+}
+
+const chainConfig : Record<string, chainData> = {
+    [CHAIN.BSC]: {contract: '0xF5db804101d8600c26598A1Ba465166c33CdAA4b', asset: coreAssets.bsc.WBNB},
+    [CHAIN.POLYGON]: {contract: '0xCd494673999194365033D7A287af9f0a3b163874', asset: coreAssets.polygon.WMATIC},
+    [CHAIN.FANTOM]: {contract: '0x94e22c14118353651636f9af43cd0a5a08b93da3', asset: coreAssets.fantom.WFTM},
+
+}
 const PURCHASE = 'event Purchase(address indexed previousOwner, address indexed newOwner, uint price, uint nftID, string uri)'
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
   const dailyRevenue = options.createBalances();
   const dailySupplySideRevenue = options.createBalances();
+  const { contract, asset } = chainConfig[options.chain]
 
   const logs = await options.getLogs({
     eventAbi: PURCHASE,
-    target: AIRNFT_CONTRACT,
+    target: contract,
   })
   logs.forEach((log) => {
     const protocolsCut = Number(log.price) / 40
-    dailyFees.add(coreAssets.bsc.WBNB, log.price, "NFT Trading Fees")
-    dailyRevenue.add(coreAssets.bsc.WBNB, protocolsCut, METRIC.SERVICE_FEES)
-    dailySupplySideRevenue.add(coreAssets.bsc.WBNB, Number(log.price) - protocolsCut, METRIC.CREATOR_FEES)
+    dailyFees.add(asset, log.price, "NFT Trading Fees")
+    dailyRevenue.add(asset, protocolsCut, METRIC.SERVICE_FEES)
+    dailySupplySideRevenue.add(asset, Number(log.price) - protocolsCut, METRIC.CREATOR_FEES)
   });
 
   return {
@@ -34,8 +45,11 @@ const adapters: SimpleAdapter = {
   version: 2,
   pullHourly: true,
   fetch,
-  chains: [CHAIN.BSC],
-  start: '2021-04-12',
+  adapter: {
+    [CHAIN.BSC]: { start: '2021-04-12' },
+    [CHAIN.POLYGON]: { start: '2022-01-11' },
+    [CHAIN.FANTOM]: { start: '2022-02-17'}
+  },
   methodology: {
     Fees: "NFT trading fees on all marketplace transactions",
     Revenue: "The protocol takes a 2.5% cut from the seller on all transactions",
