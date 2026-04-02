80cd5869da07aca5bb831b411958e150faa4acb7
ruddhks552 <ruddhks552@gmail.com>
2026-04-02 22:20:22 +0900

    Add GameClub BSC prediction market fees adapter

---
+++ fees/gameclub.ts
@@ -0,0 +1,45 @@
+import { FetchOptions, FetchResultV2, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { addTokensReceived } from "../helpers/token";
+import ADDRESSES from "../helpers/coreAssets.json";
+
+const MainContract = "0xC590Ab41e94F801c41c96Fa595f67D59B8C6A176";
+const FeeCollector = "0xAa1E6F6f628F15C57a675B1f38cDcE8e26c58E15";
+
+const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
+    const dailyFees = options.createBalances();
+    const dailyRevenue = options.createBalances();
+
+    await addTokensReceived({
+          options,
+          fromAdddesses: [MainContract],
+          targets: [FeeCollector],
+          token: ADDRESSES.bsc.USDT,
+          balances: dailyFees,
+    });
+
+    dailyRevenue.add(dailyFees);
+
+    return {
+          dailyFees,
+          dailyRevenue,
+          dailyProtocolRevenue: dailyRevenue,
+    };
+};
+
+const adapter: SimpleAdapter = {
+    version: 2,
+    methodology: {
+          Fees: "Users pay fees when they trade on GameClub prediction markets",
+          Revenue: "All fees go to the protocol",
+          ProtocolRevenue: "All fees go to the protocol",
+    },
+    adapter: {
+          [CHAIN.BSC]: {
+                  fetch: fetch,
+                  start: "2025-04-01",
+          },
+    },
+};
+
+export default adapter;
