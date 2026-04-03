f4053683fe8a5b6cac92faee8d4022977c5143b2
bheluga <bheluga@defillama.com>
2026-04-03 16:28:02 +0530

    add pullhourly

--- fees/gameclub.ts
+++ fees/gameclub.ts
@@ -8,38 +8,35 @@ const FeeCollector = "0xAa1E6F6f628F15C57a675B1f38cDcE8e26c58E15";
 
 const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
     const dailyFees = options.createBalances();
-    const dailyRevenue = options.createBalances();
 
     await addTokensReceived({
-          options,
-          fromAdddesses: [MainContract],
-          targets: [FeeCollector],
-          token: ADDRESSES.bsc.USDT,
-          balances: dailyFees,
+        options,
+        fromAdddesses: [MainContract],
+        targets: [FeeCollector],
+        token: ADDRESSES.bsc.USDT,
+        balances: dailyFees,
     });
 
-    dailyRevenue.add(dailyFees);
-
     return {
-          dailyFees,
-          dailyRevenue,
-          dailyProtocolRevenue: dailyRevenue,
+        dailyFees,
+        dailyRevenue: dailyFees,
+        dailyProtocolRevenue: dailyFees,
     };
+};
+
+const methodology = {
+    Fees: "Users pay fees when they trade on GameClub prediction markets",
+    Revenue: "All fees go to the protocol",
+    ProtocolRevenue: "All fees go to the protocol",
 };
 
 const adapter: SimpleAdapter = {
     version: 2,
-    methodology: {
-          Fees: "Users pay fees when they trade on GameClub prediction markets",
-          Revenue: "All fees go to the protocol",
-          ProtocolRevenue: "All fees go to the protocol",
-    },
-    adapter: {
-          [CHAIN.BSC]: {
-                  fetch: fetch,
-                  start: "2025-04-01",
-          },
-    },
+    pullHourly: true,
+    methodology,
+    chains: [CHAIN.BSC],
+    fetch,
+    start: "2026-04-01",
 };
 
 export default adapter;
