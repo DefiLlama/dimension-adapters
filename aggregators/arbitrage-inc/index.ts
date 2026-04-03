b915a976a635731a0ed53b25dacaecf6c87126e4
bheluga <anonymous_cat_llama@protonmail.com>
2026-04-03 16:00:22 +0530

    track arbitrage inc agg volume

--- aggregators/arbitrage-inc/index.ts
+++ aggregators/arbitrage-inc/index.ts
@@ -3,6 +3,7 @@ import { CHAIN } from "../../helpers/chains";
 import { addTokensReceived } from "../../helpers/token";
 
 const FEE_RECEIVER = '0xafF5340ECFaf7ce049261cff193f5FED6BDF04E7';
+const FEE_RATE = 0.001;
 
 const fetch = async (options: FetchOptions) => {
     const dailyFees = await addTokensReceived({
@@ -10,10 +11,13 @@ const fetch = async (options: FetchOptions) => {
         targets: [FEE_RECEIVER],
     });
 
+    const dailyVolume = dailyFees.clone(1 / FEE_RATE);
+
     return {
         dailyFees,
         dailyRevenue: dailyFees,
         dailyProtocolRevenue: dailyFees,
+        dailyVolume,
     };
 };
 
@@ -23,6 +27,7 @@ const methodology = {
     Fees: "We track fees sent to the fee receiver address which represents the developer commission for every swap executed via our frontend integration.",
     Revenue: DEV_FEE_DESC,
     ProtocolRevenue: DEV_FEE_DESC,
+    Volume: "Amount of trades executed on the Arbitrage Inc platform",
 };
 
 const breakdownMethodology = {
