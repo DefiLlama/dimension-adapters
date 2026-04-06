14252e172dca81b1960e77ff02663a11faf66388
jonathan <lodarat@gmail.com>
2026-04-06 13:24:28 +0700

    add adapter bullbit-perp-dex

---
+++ dexs/bullbit-ai/index.ts
@@ -0,0 +1,43 @@
+import { Dependencies, FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getETHReceived } from "../../helpers/token";
+
+const FIXED_PLATFORM_FEE = 1 // 1%
+
+const feeReceiverMultisig = [
+  "0x87D30c1a5a79b060d7F6FBEa7791c381a2aFc7Ad",
+]
+
+const fetch: any = async (options: FetchOptions) => {
+  const dailyFees = options.createBalances();
+
+  await getETHReceived({
+    targets: feeReceiverMultisig,
+    balances: dailyFees,
+    options
+  });
+
+  const dailyVolume = dailyFees.clone(100 / FIXED_PLATFORM_FEE)
+
+  return {
+    dailyVolume: dailyVolume,
+    dailyFees: dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
+  }
+}
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  fetch,
+  chains: [CHAIN.BSC],
+  start: '2025-08-16',
+  dependencies: [Dependencies.ALLIUM],
+  methodology: {
+    Fees: 'All fees paid by users for launching, trading tokens.',
+    Revenue: 'All fees collected by bullbit.ai protocol.',
+    ProtocolRevenue: 'All fees collected by bullbit.ai protocol.',
+  }
+}
+
+export default adapter
