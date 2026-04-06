bc159766f92145c9371889614680713bb0b9e464
ruddhks552 <ruddhks552@gmail.com>
2026-04-06 14:07:05 +0900

    Refactor fetch function and clean up codeFix GameClub fees adapter - track all USDT to FeeCollector

--- fees/gameclub.ts
+++ fees/gameclub.ts
@@ -3,40 +3,38 @@ import { CHAIN } from "../helpers/chains";
 import { addTokensReceived } from "../helpers/token";
 import ADDRESSES from "../helpers/coreAssets.json";
 
-const MainContract = "0xC590Ab41e94F801c41c96Fa595f67D59B8C6A176";
 const FeeCollector = "0xAa1E6F6f628F15C57a675B1f38cDcE8e26c58E15";
 
 const fetch = async (options: FetchOptions): Promise<FetchResultV2> => {
-    const dailyFees = options.createBalances();
+  const dailyFees = options.createBalances();
 
-    await addTokensReceived({
-        options,
-        fromAdddesses: [MainContract],
-        targets: [FeeCollector],
-        token: ADDRESSES.bsc.USDT,
-        balances: dailyFees,
-    });
+  await addTokensReceived({
+    options,
+    targets: [FeeCollector],
+    token: ADDRESSES.bsc.USDT,
+    balances: dailyFees,
+  });
 
-    return {
-        dailyFees,
-        dailyRevenue: dailyFees,
-        dailyProtocolRevenue: dailyFees,
-    };
+  return {
+    dailyFees,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
+  };
 };
 
 const methodology = {
-    Fees: "Users pay fees when they trade on GameClub prediction markets",
-    Revenue: "All fees go to the protocol",
-    ProtocolRevenue: "All fees go to the protocol",
+  Fees: "Users pay fees when they trade on GameClub prediction markets",
+  Revenue: "All fees go to the protocol",
+  ProtocolRevenue: "All fees go to the protocol",
 };
 
 const adapter: SimpleAdapter = {
-    version: 2,
-    pullHourly: true,
-    methodology,
-    chains: [CHAIN.BSC],
-    fetch,
-    start: "2026-04-01",
+  version: 2,
+  pullHourly: true,
+  methodology,
+  chains: [CHAIN.BSC],
+  fetch,
+  start: "2026-04-01",
 };
 
 export default adapter;
