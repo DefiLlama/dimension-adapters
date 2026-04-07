9d8d7cb09fba86c2c40adf44f461b7838195a3e4
Kollan House <kollanh@gmail.com>
2026-04-07 11:40:43 -0700

    revert split and adopt previous design

--- fees/futarchy-amm/index.ts
+++ fees/futarchy-amm/index.ts
@@ -3,7 +3,7 @@
   
   Aggregates fees from two sources:
   1. Futarchy AMM - 0.5% fee on SpotSwap transactions
-  2. Meteora DAMM Pools - 0.4% LP fees from pools where Futarchy DAOs own 100% liquidity
+  2. Meteora DAMM Pools - LP fees from pools where Futarchy DAOs own 100% liquidity
   
   All fees are protocol revenue (100% to Futarchy).
 */
@@ -14,36 +14,24 @@ import { getSqlFromFile, queryDuneSql } from '../../helpers/dune'
 
 const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const dailyFees = options.createBalances()
-  const dailyRevenue = options.createBalances()
 
-  const ammSql = getSqlFromFile('helpers/queries/futarchy-amm.sql', {
+  const query = getSqlFromFile('helpers/queries/futarchy.sql', {
     start: options.startTimestamp,
     end: options.endTimestamp,
   })
 
-  const meteoraSql = getSqlFromFile('helpers/queries/futarchy-meteora.sql', {
-    start: options.startTimestamp,
-    end: options.endTimestamp,
+  const result = await queryDuneSql(options, query)
+  
+  result.forEach((row: any) => {
+    const fees = row.total_fees_usd ?? 0
+    dailyFees.addUSDValue(fees, row.source)
   })
-
-  const [ammResults, meteoraResults] = await Promise.all([
-    queryDuneSql(options, ammSql, { extraUIDKey: 'amm' }),
-    queryDuneSql(options, meteoraSql, { extraUIDKey: 'meteora' }),
-  ])
-
-  const ammFees = ammResults[0]?.total_fees_usd ?? 0
-  const meteoraFees = meteoraResults[0]?.total_fees_usd ?? 0
-
-  dailyFees.addUSDValue(ammFees)
-  dailyFees.addUSDValue(meteoraFees)
-  dailyRevenue.addUSDValue(ammFees)
-  dailyRevenue.addUSDValue(meteoraFees)
 
   return {
     dailyFees,
     dailyUserFees: dailyFees,
-    dailyRevenue,
-    dailyProtocolRevenue: dailyRevenue,
+    dailyRevenue: dailyFees,
+    dailyProtocolRevenue: dailyFees,
   }
 }
 
@@ -54,17 +42,22 @@ const methodology = {
   ProtocolRevenue: 'All fees are protocol revenue - 100% of AMM fees and 100% of Meteora LP fees.',
 }
 
+const breakdownMethodology = {
+  Fees: {
+    'futarchy_amm': '0.5% fee on Futarchy AMM SpotSwap transactions.',
+    'meteora_damm': '0.4% LP fees from Meteora DAMM pools where Futarchy DAOs own 100% liquidity.',
+  },
+}
+
 const adapter: SimpleAdapter = {
   version: 1,
-  adapter: {
-    [CHAIN.SOLANA]: {
-      fetch,
-      start: '2025-10-09',
-    },
-  },
+  fetch,
+  chains: [CHAIN.SOLANA],
+  start: '2025-10-09',
   dependencies: [Dependencies.DUNE],
   isExpensiveAdapter: true,
   methodology,
+  breakdownMethodology,
 }
 
 export default adapter
