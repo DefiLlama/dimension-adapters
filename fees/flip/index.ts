4f7b163264ac00798ea3621c5ae24d3547939f96
bheluga <bheluga@defillama.com>
2026-04-09 16:00:26 +0530

    add pullhourly

--- fees/flip/index.ts
+++ fees/flip/index.ts
@@ -3,42 +3,34 @@ import { CHAIN } from "../../helpers/chains";
 import { getETHReceived } from "../../helpers/token";
 
 const defaultFeeCollectors = [
-  '0x98114De4823484313d56b8a8D90c55224CE571AC',
-  '0xab726237d912909c1b99a31d7194a30be84286ce',
+    '0x98114De4823484313d56b8a8D90c55224CE571AC',
+    '0xab726237d912909c1b99a31d7194a30be84286ce',
 ];
 
+const fetch = async (options: FetchOptions) => {
+    const dailyFees = await getETHReceived({ options, targets: defaultFeeCollectors })
 
-export const config: any = {
-  base: {
-    fees_collectors: [...defaultFeeCollectors],
-  },  
-}
-
-export const fetch = async (options: FetchOptions) => {
-  const { chain } = options
-  const { fees_collectors = defaultFeeCollectors } = config[chain] ?? {}
-  const dailyRevenue = await getETHReceived({ options, targets: fees_collectors })
-
-  return {    
-    dailyRevenue,
-    dailyProtocolRevenue: dailyRevenue,
-    dailyVolume: dailyRevenue
-  }
+    return {
+        dailyFees,
+        dailyRevenue: dailyFees,
+        dailyProtocolRevenue: dailyFees,
+    }
 
 }
 
 const methodology = {
-  Fees: 'Total native ETH received by fee collector wallets on Base',
-  Revenue: 'Total native ETH received by fee collector wallets on Base',
-  ProtocolRevenue: 'Total native ETH received by fee collector wallets on Base',
+    Fees: 'Total native ETH received by fee collector wallets on Base',
+    Revenue: 'All the fees are revenue',
+    ProtocolRevenue: 'All the fees goes to the protocol',
 }
 
 const adapter: SimpleAdapter = {
-  version: 2,
-  fetch,
-  chains: [CHAIN.BASE],
-  start: '2026-04-01',
-  methodology
+    version: 2,
+    pullHourly: true,
+    fetch,
+    chains: [CHAIN.BASE],
+    start: '2026-04-01',
+    methodology
 }
 
 export default adapter;
