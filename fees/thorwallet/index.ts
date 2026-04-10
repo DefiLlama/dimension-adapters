fb9a69fb184569f15d8b52ab8b13436d09083aff
bheluga <bheluga@defillama.com>
2026-04-10 23:55:09 +0530

    fix breakdown

--- fees/thorwallet/index.ts
+++ fees/thorwallet/index.ts
@@ -1,6 +1,8 @@
+
 import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { httpGet } from "../../utils/fetchURL";
+import { METRIC } from "../../helpers/metrics";
 
 const fetch = async (options: FetchOptions) => {
     const data = await httpGet('https://api-v2-prod.thorwallet.org/defillama/fees');
@@ -10,10 +12,11 @@ const fetch = async (options: FetchOptions) => {
     dailyFees.addUSDValue(parseFloat(breakdown.swapAffiliateFees), 'Swap Affiliate Fees');
     dailyFees.addUSDValue(parseFloat(breakdown.perpCloseFees), 'Perp Close Fees');
 
-    const dailyRevenue = dailyFees.clone(0.95);
-    const dailyProtocolRevenue = dailyFees.clone(0.45);
-    const dailyHoldersRevenue = dailyFees.clone(0.5);
-    const dailySupplySideRevenue = dailyFees.clone(0.05);
+    const dailyRevenue = dailyFees.clone(0.45, METRIC.PROTOCOL_FEES);
+    dailyRevenue.add(dailyFees.clone(0.5, METRIC.STAKING_REWARDS), METRIC.STAKING_REWARDS);
+    const dailyProtocolRevenue = dailyFees.clone(0.45, METRIC.PROTOCOL_FEES);
+    const dailyHoldersRevenue = dailyFees.clone(0.5, METRIC.STAKING_REWARDS);
+    const dailySupplySideRevenue = dailyFees.clone(0.05, 'Raffle Pot Rewards');
 
     return {
         dailyFees,
@@ -40,21 +43,18 @@ const breakdownMethodology = {
         'Perp Close Fees': 'Fees from closing perpetual trading positions.',
     },
     Revenue: {
-        'Swap Affiliate Fees': '95% of Affiliate fees from swaps across THORChain, Maya, and Chainflip.',
-        'Perp Close Fees': '95% of Fees from closing perpetual trading positions.',
+        [METRIC.PROTOCOL_FEES]: '45% of fees allocated to protocol treasury.',
+        [METRIC.STAKING_REWARDS]: '50% of fees distributed to token holders.',
     },
     ProtocolRevenue: {
-        'Swap Affiliate Fees': '45% of Affiliate fees from swaps across THORChain, Maya, and Chainflip.',
-        'Perp Close Fees': '45% of Fees from closing perpetual trading positions.',
+        [METRIC.PROTOCOL_FEES]: '45% of fees allocated to protocol treasury.',
     },
     HoldersRevenue: {
-        'Swap Affiliate Fees': '50% of Affiliate fees from swaps across THORChain, Maya, and Chainflip.',
-        'Perp Close Fees': '50% of Fees from closing perpetual trading positions.',
+        [METRIC.STAKING_REWARDS]: '50% of fees distributed to token holders.',
     },
     SupplySideRevenue: {
-        'Swap Affiliate Fees': '5% of Affiliate fees from swaps across THORChain, Maya, and Chainflip.',
-        'Perp Close Fees': '5% of Fees from closing perpetual trading positions.',
-    },
+        'Raffle Pot Rewards': '5% of fees distributed to in game raffle pot players',
+    }
 };
 
 const adapter: SimpleAdapter = {
