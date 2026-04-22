140472b883f6fc15d009c5b072665b72439e30ab
bheluga <bheluga@defillama.com>
2026-04-22 23:42:14 +0530

    Update ramsesx-arb-legacy.ts

--- dexs/ramsesx-arb-legacy.ts
+++ dexs/ramsesx-arb-legacy.ts
@@ -1,30 +1,4 @@
-import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { fetchProtocolDayStats } from "./ramses-hl-cl";
+import { createLegacyAdapter } from "./ramses-hl-cl";
 
-const fetch = async (_: any, _1: any, options: FetchOptions) => {
-  const stats = await fetchProtocolDayStats(options);
-
-  const dailyFees = stats.legacyFeesUSD;
-  const dailyVolume = stats.legacyVolumeUSD;
-
-  return {
-    dailyVolume,
-    dailyFees,
-    dailyUserFees: dailyFees,
-  };
-};
-
-const methodology = {
-  Fees: "Fees are collected from users on each swap.",
-  UserFees: "User pays fees on each swap.",
-};
-
-const adapter: SimpleAdapter = {
-  fetch,
-  chains: [CHAIN.ARBITRUM],
-  start: "2026-01-28",
-  methodology,
-};
-
-export default adapter;
+export default createLegacyAdapter(CHAIN.ARBITRUM, "2026-01-28");
