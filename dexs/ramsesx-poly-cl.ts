8c76cb283b718fd67cbfd239ae52223d8e45378f
bheluga <bheluga@defillama.com>
2026-04-22 23:40:17 +0530

    Update ramsesx-poly-cl.ts

--- dexs/ramsesx-poly-cl.ts
+++ dexs/ramsesx-poly-cl.ts
@@ -1,29 +1,4 @@
-import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { fetchProtocolDayStats } from "./ramses-hl-cl";
+import { createClAdapter } from "./ramses-hl-cl";
 
-const fetch = async (_: any, _1: any, options: FetchOptions) => {
-  const stats = await fetchProtocolDayStats(options);
-  const dailyFees = stats.clFeesUSD;
-  const dailyVolume = stats.clVolumeUSD;
-
-  return {
-    dailyVolume,
-    dailyFees,
-    dailyUserFees: dailyFees,
-  };
-};
-
-const methodology = {
-  Fees: "Fees are collected from users on each swap.",
-  UserFees: "User pays fees on each swap.",
-};
-
-const adapter: SimpleAdapter = {
-  fetch,
-  chains: [CHAIN.POLYGON],
-  start: "2026-01-28",
-  methodology,
-};
-
-export default adapter;
+export default createClAdapter(CHAIN.POLYGON, "2026-01-28");
