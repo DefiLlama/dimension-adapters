84b7afaaab1380c943d7e3b04db936dcb2dd5d45
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2026-04-22 16:02:53 -0300

    paradex: add assistance fund buybacks

--- fees/paradex/index.ts
+++ fees/paradex/index.ts
@@ -1,7 +1,8 @@
 // source for fees: https://www.paradex.trade/stats
 import fetchURL from "../../utils/fetchURL"
-import { FetchResult, SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { addTokensReceived } from "../../helpers/token";
 
 const feesEndpoint = "https://tradeparadigm.metabaseapp.com/api/public/dashboard/e4d7b84d-f95f-48eb-b7a6-141b3dcef4e2/dashcard/5913/card/5760?parameters=%5B%5D"
 
@@ -11,30 +12,46 @@ interface IFeesData {
   }
 }
 
-const fetch = async (timestamp: number): Promise<FetchResult> => {
+// https://app.paradex.trade/dime/overview
+const ASSISTANCE_FUND = "0xe80c1286a424B09fB9FC1d82afedAf9d4CE8e5f6";
+const DIME_TOKEN = "0xb32e10022ffbedfe10bc818a1c7e67d9d87e0fa7";
+
+const fetchParadex = async (options: FetchOptions): Promise<FetchResultV2> => {
   // Fetch fees data
   const feesData = await fetchURL(feesEndpoint) as IFeesData
-  const timestampStr = new Date(timestamp * 1000).toISOString().split('T')[0] + "T00:00:00Z"
+  const timestampStr = new Date(options.toTimestamp * 1000).toISOString().split('T')[0] + "T00:00:00Z"
   const dailyFees = feesData.data.rows.find(row => row[0] === timestampStr)?.[1]
   if (!dailyFees) throw new Error('record missing!')
 
   return { 
-    timestamp, 
     dailyFees,
     dailyUserFees: dailyFees
   };
 };
 
+const fetchEth = async (options: FetchOptions): Promise<FetchResultV2> => {
+  const buybacks = await addTokensReceived({ options, tokens: [DIME_TOKEN], targets: [ASSISTANCE_FUND] });
+
+  return {
+    dailyHoldersRevenue: buybacks
+  };
+};
+
 const adapter: SimpleAdapter = {
-  version: 1,
+  version: 2,
   adapter: {
     [CHAIN.PARADEX]: {
-      fetch,
+      fetch: fetchParadex,
       start: '2023-09-01',
     },
+    [CHAIN.ETHEREUM]: {
+      fetch: fetchEth,
+      start: '2026-04-01',
+    }
   },
   methodology: {
 		Fees: "Tracks total fees paid by traders on Paradex.",
+    HoldersRevenue: "DIME purchased with net protocol revenue."
 	},
   skipBreakdownValidation: true, // skipping breakdown validation as we dont have the revenue breakdown
 };
