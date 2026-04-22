2520b63889e8a30020af54fddb1755f6df87a18b
bheluga <bheluga@defillama.com>
2026-04-22 23:25:00 +0530

    Update ramsesx-arb-cl.ts

--- dexs/ramsesx-arb-cl.ts
+++ dexs/ramsesx-arb-cl.ts
@@ -1,29 +1,46 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { fetchProtocolDayStats } from "./ramses-hl-cl";
+import { fetchStats, methodology, breakdownMethodology } from "./ramses-hl-cl";
+import { METRIC } from "../helpers/metrics";
 
 const fetch = async (_: any, _1: any, options: FetchOptions) => {
-  const stats = await fetchProtocolDayStats(options);
-  const dailyFees = stats.clFeesUSD;
-  const dailyVolume = stats.clVolumeUSD;
+    const stats = await fetchStats(options);
+    const dailyVolume = stats.clVolumeUSD;
 
-  return {
-    dailyVolume,
-    dailyFees,
-    dailyUserFees: dailyFees,
-  };
-};
+    const dailyFees = options.createBalances();
+    const dailyHoldersRevenue = options.createBalances();
+    const dailyProtocolRevenue = options.createBalances();
+    const dailySupplySideRevenue = options.createBalances();
 
-const methodology = {
-  Fees: "Fees are collected from users on each swap.",
-  UserFees: "User pays fees on each swap.",
+    dailyFees.addUSDValue(stats.clFeesUSD, METRIC.SWAP_FEES);
+    dailyHoldersRevenue.addUSDValue(stats.clUserFeesRevenueUSD, 'Swap Fees to holders');
+    dailyProtocolRevenue.addUSDValue(stats.clProtocolRevenueUSD, 'Swap Fees to protocol');
+
+    dailyFees.addUSDValue(stats.clBribeRevenueUSD, 'Bribes');
+    dailyHoldersRevenue.addUSDValue(stats.clBribeRevenueUSD, 'Bribes to holders');
+
+    const dailyRevenue = dailyProtocolRevenue.clone();
+    dailyRevenue.add(dailyHoldersRevenue);
+
+    dailySupplySideRevenue.addUSDValue(stats.clFeesUSD - stats.clUserFeesRevenueUSD - stats.clProtocolRevenueUSD, 'Swap Fees to LPs');
+
+    return {
+        dailyVolume,
+        dailyFees,
+        dailyUserFees: dailyFees,
+        dailyHoldersRevenue,
+        dailyProtocolRevenue,
+        dailyRevenue,
+        dailySupplySideRevenue,
+    };
 };
 
 const adapter: SimpleAdapter = {
-  fetch,
-  chains: [CHAIN.ARBITRUM],
-  start: "2026-01-13",
-  methodology,
+    fetch,
+    chains: [CHAIN.ARBITRUM],
+    start: "2026-01-13",
+    methodology,
+    breakdownMethodology,
 };
 
 export default adapter;
