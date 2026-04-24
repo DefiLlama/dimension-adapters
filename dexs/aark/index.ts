5f43a93389fe8d00c907310b84290a44f6e986cd
bheluga <anonymous_cat_llama@protonmail.com>
2026-04-24 11:18:59 +0530

    smol fix

--- dexs/aark/index.ts
+++ dexs/aark/index.ts
@@ -7,6 +7,7 @@ const MoonOrderClosedV2 = "event MoonOrderClosedV2(address user, uint256 moonInd
 const FuturesManager = '0x0b848a8A5eC8950E67d19E7a21A6Be29F44F685e';
 
 export const inflatedMarkets = {
+  "2026-04-23": [62n],
   "2026-04-24": [62n] //1000PEPE actual price 0.003 , but showing as 5$ due to glitch
 }
 
