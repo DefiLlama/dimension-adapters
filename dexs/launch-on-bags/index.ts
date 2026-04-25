bccd3027a926fd8c4e3e54b7e98fdf79919ea8b3
ramy-zemo <ramy-abdalla@hotmail.com>
2026-04-25 14:31:44 +0200

    feat: Add Bags Dex Volume

---
+++ dexs/launch-on-bags/index.ts
@@ -0,0 +1,43 @@
+import { Dependencies, FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { getSqlFromFile, queryDuneSql } from "../../helpers/dune";
+
+interface IData {
+    quote_mint: string;
+    daily_volume: string;
+}
+
+const fetch = async (options: FetchOptions) => {
+    const query = getSqlFromFile('helpers/queries/bags_volume.sql', {
+        tx_signer: 'BAGSB9TpGrZxQbEsrEznv5jXXdwyP6AXerN8aVRiAmcv',
+        start: options.startTimestamp,
+        end: options.endTimestamp,
+    });
+
+    const data: IData[] = await queryDuneSql(options, query);
+    
+    const dailyVolume = options.createBalances();
+
+    data.forEach((row) => {
+        dailyVolume.add(row.quote_mint, row.daily_volume);
+    });
+
+    return {
+        dailyVolume
+    };
+};
+
+const adapter: SimpleAdapter = {
+    version: 2,
+    pullHourly: true,
+    fetch,
+    chains: [CHAIN.SOLANA],
+    dependencies: [Dependencies.DUNE],
+    isExpensiveAdapter: true,
+    start: '2025-05-10',
+    methodology: {
+        Volume: "Trading volume is measured as the raw SOL amount across all DEX trades where a Bags-launched token trades against SOL.",
+    },
+};
+
+export default adapter;
