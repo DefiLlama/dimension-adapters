e352d778a7cdd2a519dd18a79ec071b25ebda26d
bheluga <anonymous_cat_llama@protonmail.com>
2026-04-26 23:27:22 +0530

    fix bouncetech-oi

--- open-interest/bounce-tech-oi.ts
+++ open-interest/bounce-tech-oi.ts
@@ -11,6 +11,7 @@
 
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
+import { ethers } from "ethers";
 
 const GLOBAL_STORAGE = '0xa07d06383c1863c8A54d427aC890643d76cc03ff';
 const USDC = '0xb88339CB7199b77E23DB6E890353E22632Ba630f'; // notionalUsdc precompile always returns 6-decimal USDC
@@ -21,7 +22,9 @@ const fetch = async (options: FetchOptions) => {
 
     const lts: string[] = await options.api.call({ abi: 'address[]:lts', target: factory });
 
-    const notionals = await options.api.multiCall({ abi: 'function notionalUsdc(address user) returns (uint256)', calls: lts.map(lt => ({ target: handler, params: [lt] })) });
+    //using ethers as alchemy fails to precompile on hyperevm
+    const handlerContract = new ethers.Contract(handler, ['function notionalUsdc(address user) view returns (uint256)'], options.api.provider);
+    const notionals = await Promise.all(lts.map(lt => handlerContract.notionalUsdc(lt)));
     const isLongs = await options.api.multiCall({ abi: 'bool:isLong', calls: lts });
 
     const openInterestAtEnd = options.createBalances();
