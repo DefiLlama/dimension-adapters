bfd90cc563266154660b88195e719ac452350190
bheluga <anonymous_cat_llama@protonmail.com>
2026-04-27 17:08:32 +0530

    smol fix

--- fees/centrifuge/index.ts
+++ fees/centrifuge/index.ts
@@ -148,7 +148,7 @@ const breakdownMethodology = {
 
 const adapter: SimpleAdapter = {
     version: 2,
-    //pullHourly: true,
+    pullHourly: true,
     fetch,
     adapter: chainConfig,
     methodology,
