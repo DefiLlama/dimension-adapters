dd4f8df22796f8222d640b805354076c9dfeee87
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2026-04-28 16:20:39 -0300

    track unidex-v4 open interest

---
+++ open-interest/unidex-v4-oi.ts
@@ -0,0 +1,45 @@
+import { Dependencies, FetchOptions, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { queryDuneSql } from "../helpers/dune";
+
+const fetch = async (options: FetchOptions) => {
+  const rows = await queryDuneSql(options, `
+    WITH openInterestChanges AS (
+      SELECT *
+      FROM query_4086137
+      WHERE blockchain = 'Arbitrum'
+    ),
+    openInterestByDay AS (
+      SELECT
+        date_trunc('day', block_time) AS block_date,
+        SUM(SUM(amount)) OVER (ORDER BY date_trunc('day', block_time) ASC) AS openInterestUSD,
+        SUM(SUM(IF(direction = 'Long', amount, NULL))) OVER (ORDER BY date_trunc('day', block_time) ASC) AS longOpenInterestUSD,
+        SUM(SUM(IF(direction = 'Short', amount, NULL))) OVER (ORDER BY date_trunc('day', block_time) ASC) AS shortOpenInterestUSD
+      FROM openInterestChanges
+      GROUP BY 1
+    )
+    SELECT openInterestUSD, longOpenInterestUSD, shortOpenInterestUSD
+    FROM openInterestByDay
+    WHERE block_date = DATE '${options.dateString}'
+  `);
+
+  const row = rows?.[0];
+  if (!row) return {};
+
+  return {
+    openInterestAtEnd: row.openInterestUSD,
+    longOpenInterestAtEnd: row.longOpenInterestUSD,
+    shortOpenInterestAtEnd: row.shortOpenInterestUSD,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  version: 2,
+  chains: [CHAIN.ARBITRUM],
+  fetch,
+  start: '2024-09-20',
+  dependencies: [Dependencies.DUNE],
+  isExpensiveAdapter: true,
+};
+
+export default adapter;
