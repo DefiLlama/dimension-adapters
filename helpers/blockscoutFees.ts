85d6d6199d61668909bc28fded2e0c745356251a
Chibuzor Daniel Uwezukwe <uwezukwechibuzor@gmail.com>
2026-04-28 13:43:44 +0100

    Fix taiko chain fees (#6563)

--- helpers/blockscoutFees.ts
+++ helpers/blockscoutFees.ts
@@ -94,6 +94,7 @@ export const chainConfigMap: any = {
   [CHAIN.MOLTEN_NETWORK]: { CGToken: 'molten-2', explorer: 'https://molten.calderaexplorer.xyz/' },
   [CHAIN.SYNDICATE]: { CGToken: 'syndicate-3', explorer: 'https://explorer.syndicate.io/' },
   [CHAIN.XCHAIN]: { CGToken: 'ethereum', explorer: 'https://xchain-explorer.kuma.bid/' },
+  [CHAIN.TAIKO]: { CGToken: 'ethereum', explorer: 'https://blockscoutapi.mainnet.taiko.xyz', start: '2024-05-25' },
   [CHAIN.SHIBARIUM]: { CGToken: 'bone-shibaswap', explorer: 'https://shibariumscan.io/' },
   [CHAIN.VANA]: { CGToken: 'vana', explorer: 'https://vanascan.io/' },
   [CHAIN.NEO_X_MAINNET]: { CGToken: "gas", explorer: "https://xexplorer.neo.org/" },
