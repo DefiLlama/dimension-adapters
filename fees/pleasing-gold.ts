008e392990357742713e7e297b6e2f48a5f52ca1
pdubey7 <rdpushp1@gmail.com>
2026-05-03 03:15:55 +0530

    fix: use USDT for fee pricing instead of PUSD (incorrect CoinGecko price)

--- fees/pleasing-gold.ts
+++ fees/pleasing-gold.ts
@@ -4,7 +4,7 @@ import { CHAIN } from "../helpers/chains";
 const PSWAP_CONTRACT = "0x3D084Fc4Cc4D5A0B8d6B6517341f359505b35336";
 const PGOLD = "0x3e76BB02286BFeAA89DD35f11253f2CbCE634F91";
 // PUSD is pegged 1:1 to USDT on Arbitrum
-const PUSD = "0xc8fb643d18f1e53698cfda5c8fdf0cdc03c1dbec";
+const USDT = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9"; // PUSD is 1:1 redeemable for USDT
 
 const swapPGOLDToPUSD = "event SwapPGOLDToPUSD(address indexed user, uint256 inAmount, uint256 outAmount, uint256 fee)";
 const swapPUSDToPGOLD = "event SwapPUSDToPGOLD(address indexed user, uint256 inAmount, uint256 outAmount, uint256 fee)";
@@ -19,7 +19,7 @@ const fetch = async (options: FetchOptions) => {
 
   // SwapPGOLDToPUSD: fee is in PUSD (18 decimals, USD-pegged)
   for (const log of logs1) {
-    dailyFees.add(PUSD, log.fee);
+    dailyFees.add(USDT, log.fee);
   }
 
   // SwapPUSDToPGOLD: fee is in PGOLD
