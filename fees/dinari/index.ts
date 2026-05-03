569b51701bd9fd4df66a94f8e61150c4a6043a82
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2026-05-03 04:10:33 +0100

    update start date

--- fees/dinari/index.ts
+++ fees/dinari/index.ts
@@ -5,10 +5,10 @@ import { CHAIN } from "../../helpers/chains";
 // https://github.com/dinaricrypto/sbt-contracts/blob/50f7cb5f0613c03fad42e7ece78e56d2992d03ba/releases/v0.4.2/order_processor.json#L6
 // https://github.com/dinaricrypto/sbt-contracts/blob/50f7cb5f0613c03fad42e7ece78e56d2992d03ba/src/orders/OrderProcessor.sol#L652
 const config: Record<string, { processor: string; start: string }> = {
-  [CHAIN.ETHEREUM]: { processor: "0xA8a48C202AF4E73ad19513D37158A872A4ac79Cb", start: "2024-01-01" },
-  [CHAIN.ARBITRUM]: { processor: "0xFA922457873F750244D93679df0d810881E4131D", start: "2024-01-01" },
-  [CHAIN.BASE]: { processor: "0x63FF43009f9ba3584aF2Ddfc3D5FE2cb8AE539c0", start: "2024-01-01" },
-  [CHAIN.PLUME]: { processor: "0xFB0C1fF92C4EDCCC00DABFC2ddaC8338E416786e", start: "2024-01-01" },
+  [CHAIN.ETHEREUM]: { processor: "0xA8a48C202AF4E73ad19513D37158A872A4ac79Cb", start: "2024-05-22" },
+  [CHAIN.ARBITRUM]: { processor: "0xFA922457873F750244D93679df0d810881E4131D", start: "2024-05-22" },
+  [CHAIN.BASE]: { processor: "0x63FF43009f9ba3584aF2Ddfc3D5FE2cb8AE539c0", start: "2024-06-07" },
+  [CHAIN.PLUME]: { processor: "0xFB0C1fF92C4EDCCC00DABFC2ddaC8338E416786e", start: "2025-10-22" },
 };
 
 const events = {
