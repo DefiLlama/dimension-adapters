0c4f848fb072e6d03b23a7524e88e82ef48d8d2d
bheluga <anonymous_cat_llama@protonmail.com>
2026-05-05 14:26:19 +0530

    throw error on sundaeswap spike

--- fees/sundaeswap/index.ts
+++ fees/sundaeswap/index.ts
@@ -36,11 +36,11 @@ const historicalVolumeEndpoint = "https://stats.sundaeswap.finance/api/defillama
 const MAX_FEE_TIER = 1 / 100;
 
 const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResult> => {
-  let dailyFees = options.createBalances();
-  let dailyRevenue = options.createBalances();
-  let dailyProtocolRevenue = options.createBalances();
-  let dailyHoldersRevenue = options.createBalances();
-  let dailySupplySideRevenue = options.createBalances();
+  const dailyFees = options.createBalances();
+  const dailyRevenue = options.createBalances();
+  const dailyProtocolRevenue = options.createBalances();
+  const dailyHoldersRevenue = options.createBalances();
+  const dailySupplySideRevenue = options.createBalances();
 
   const start = formatDate(options.startTimestamp);
   const end = formatDate(options.endTimestamp);
@@ -102,12 +102,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResu
   const isBadSpike = feesInUsd > volumeInUsd * MAX_FEE_TIER;
 
   if (isBadSpike) {
-    //throw new Error(`Bad spike in fees for ${options.dateString}, volume: ${volumeInUsd}, fees: ${feesInUsd}`);
-    dailyFees = options.createBalances();
-    dailyRevenue = options.createBalances();
-    dailySupplySideRevenue = options.createBalances();
-    dailyProtocolRevenue = options.createBalances();
-    dailyHoldersRevenue = options.createBalances();
+    throw new Error(`Bad spike in fees for ${options.dateString}, volume: ${volumeInUsd}, fees: ${feesInUsd}`);
   }
 
   return {
