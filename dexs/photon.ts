165bb2cda2341e7801e52d3ea102787f35868310
bheluga <bheluga@defillama.com>
2026-05-06 22:09:29 +0530

    remove console log

--- dexs/photon.ts
+++ dexs/photon.ts
@@ -8,8 +8,7 @@ const fetch = async (_a: any, _b: any, options: FetchOptions) => {
     const now = Date.now()
     const tenHoursAgo = now - (10 * 60 * 60 * 1000)
     if ((options.toTimestamp * 1000) > tenHoursAgo) {
-        console.log("End timestamp is less than 10 hours ago, skipping fetch due to dune indexing delay", new Date(options.toTimestamp * 1000).toISOString(), new Date(tenHoursAgo).toISOString())
-        throw new Error("End timestamp is less than 10 hours ago, skipping due to dune indexing delay")
+        throw new Error("End timestamp is less than 10 hours ago, skipping fetch due to dune indexing delay")
     }
 
     const data = await queryDuneSql(options, `
