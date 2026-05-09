340c503bfc99b6ae4a9745b2fbc72a4d62c1f753
Shailesh K <shaileshkhote@gmail.com>
2026-05-09 18:28:10 +0530

    refactor(active-users): align dune stats factory naming

--- factory/duneEvmStats.ts
+++ factory/duneEvmStats.ts
@@ -2,18 +2,24 @@ import { CHAIN } from "../helpers/chains";
 import { duneChainStats, DuneChainStatsMode } from "../helpers/duneChainStats";
 import { createFactoryExports } from "./registry";
 
-const configs: Record<string, { start: string; table: string }> = {
-  [CHAIN.ABSTRACT]: { start: "2024-10-25", table: "abstract.transactions" },
-  [CHAIN.LINEA]: { start: "2023-07-06", table: "linea.transactions" },
-  [CHAIN.OP_BNB]: { start: "2023-08-11", table: "opbnb.transactions" },
-  [CHAIN.RONIN]: { start: "2021-01-25", table: "ronin.transactions" },
-  [CHAIN.SONIC]: { start: "2024-12-01", table: "sonic.transactions" },
-  [CHAIN.SOPHON]: { start: "2024-10-21", table: "sophon.transactions" },
+type ProtocolConfig = {
+  chain: string;
+  start: string;
+  table: string;
+};
+
+const protocolConfigMap: Record<string, ProtocolConfig> = {
+  "abstract": { chain: CHAIN.ABSTRACT, start: "2024-10-25", table: "abstract.transactions" },
+  "linea": { chain: CHAIN.LINEA, start: "2023-07-06", table: "linea.transactions" },
+  "op_bnb": { chain: CHAIN.OP_BNB, start: "2023-08-11", table: "opbnb.transactions" },
+  "ronin": { chain: CHAIN.RONIN, start: "2021-01-25", table: "ronin.transactions" },
+  "sonic": { chain: CHAIN.SONIC, start: "2024-12-01", table: "sonic.transactions" },
+  "sophon": { chain: CHAIN.SOPHON, start: "2024-10-21", table: "sophon.transactions" },
 };
 
 const buildProtocols = (mode?: DuneChainStatsMode) => Object.fromEntries(
-  Object.entries(configs).map(([chain, { start, table }]) => [
-    chain,
+  Object.entries(protocolConfigMap).map(([name, { chain, start, table }]) => [
+    name,
     duneChainStats(chain, start, table, mode),
   ])
 );
