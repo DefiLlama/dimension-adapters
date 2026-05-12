f707b97099920a6f07a5738f2da162cddb5d6d52
bheluga <bheluga@defillama.com>
2026-05-12 18:41:51 +0530

    add breakdown methodology

--- fees/swell.ts
+++ fees/swell.ts
@@ -1,7 +1,7 @@
 import { CHAIN } from "../helpers/chains";
-import { METRIC } from "../helpers/metrics";
 import { Adapter, FetchOptions, FetchResultV2 } from "../adapters/types";
 import { addTokensReceived } from "../helpers/token";
+import { METRIC } from "../helpers/metrics";
 
 // https://docs.swellnetwork.io/swell-staking/sweth-liquid-staking/sweth-v1.0-system-design/rewards-and-distribution/liquid-staking-rewards-and-fees
 
@@ -36,19 +36,21 @@ async function fetch(options: FetchOptions): Promise<FetchResultV2> {
     target: swETH,
     abi: 'uint256:totalSupply',
   })
-  
+
   const totalDeposited = BigInt(totalSupply) * BigInt(exchangeRateBefore) / BigInt(1e18)
-  
+
   // swell distribute 90% rewards to stakers post protocol revenue and node operators cut
   // 90% to stakers, 5% to node operators, 5% to Swell treasury
   const df = Number(totalDeposited) * (exchangeRateAfter - exchangeRateBefore) / 0.9 / 1e18
 
   const swellTreasuryRewards = df * 0.05
-  const supplySideRewards = df - swellTreasuryRewards
-  
-  dailyFees.addGasToken(df)
-  dailyRevenue.addGasToken(swellTreasuryRewards)
-  dailySupplySideRevenue.addGasToken(supplySideRewards)
+  const nodeOperatorsRewards = df * 0.05
+  const stakersRewards = df - swellTreasuryRewards - nodeOperatorsRewards
+
+  dailyFees.addGasToken(df, METRIC.STAKING_REWARDS)
+  dailyRevenue.addGasToken(swellTreasuryRewards, 'Staking rewards to swell')
+  dailySupplySideRevenue.addGasToken(stakersRewards, 'Staking rewards to stakers')
+  dailySupplySideRevenue.addGasToken(nodeOperatorsRewards, 'Staking rewards to node operators')
 
   // ALL SWELL burned to dead address
   await addTokensReceived({
@@ -68,47 +70,64 @@ async function fetch(options: FetchOptions): Promise<FetchResultV2> {
   })
 
   // holders revenue = buyback burns only
-  dailyHoldersRevenue.add(totalBurns)
-  dailyHoldersRevenue.subtract(programmaticBurns)
+  dailyHoldersRevenue.add(totalBurns, METRIC.TOKEN_BUY_BACK)
+  dailyHoldersRevenue.subtract(programmaticBurns, METRIC.TOKEN_BUY_BACK)
+
+  const stakingRevenueToProtocol = await dailyRevenue.clone();
+  stakingRevenueToProtocol.subtract(dailyHoldersRevenue)
+
+  const stakingRevenueRetained = await stakingRevenueToProtocol.getUSDValue();
+  const dailyProtocolRevenue = options.createBalances();
+  dailyProtocolRevenue.addUSDValue(stakingRevenueRetained, 'Staking rewards retained by protocol')
 
   return {
     dailyFees,
     dailyRevenue,
     dailySupplySideRevenue,
-    dailyProtocolRevenue: dailyRevenue,
+    dailyProtocolRevenue,
     dailyHoldersRevenue,
   }
 }
 
+const methodology = {
+  Fees: 'Total staking rewards and fees paid by protocol.',
+  Revenue: '5% staking rewards are charged by swell protocol.',
+  SupplySideRevenue: '90% staking rewards are distributed to ETH stakers and 5% to node operators.',
+  ProtocolRevenue: 'Revenue retained by protocol after buybacks and burns.',
+  HoldersRevenue: 'Executed SWELL buybacks tracked as SWELL transferred to the burn address excluding programmatic burns.',
+}
+
+const breakdownMethodology = {
+  Fees: {
+    [METRIC.STAKING_REWARDS]: 'Total staking rewards and fees paid by protocol.',
+  },
+  Revenue: {
+    'Staking rewards to swell': '5% staking rewards are charged by swell protocol.',
+  },
+  SupplySideRevenue: {
+    'Staking rewards to stakers': '90% staking rewards are distributed to ETH stakers.',
+    'Staking rewards to node operators': '5% staking rewards are distributed to node operators.',
+  },
+  ProtocolRevenue: {
+    'Staking rewards retained by protocol': 'Revenue retained by protocol after buybacks and burns.',
+  },
+  HoldersRevenue: {
+    [METRIC.TOKEN_BUY_BACK]: 'Executed SWELL buybacks tracked as SWELL transferred to the burn address excluding programmatic burns.',
+  },
+}
+
 const adapter: Adapter = {
   version: 2,
+  pullHourly: true,
   adapter: {
     [CHAIN.ETHEREUM]: {
       fetch,
       start: '2023-10-14',
     },
   },
-  methodology: {
-    Fees: 'Total validators fees and rewards from staked ETH.',
-    Revenue: '5% staking rewards are charged by Swell Protocol Treasury.',
-    SupplySideRevenue: '90% staking rewards are distributed to ETH stakers and 5% to node operators.',
-    ProtocolRevenue: '5% staking rewards are charged by Swell Protocol Treasury.',
-    HoldersRevenue: 'Executed SWELL buybacks tracked as SWELL transferred to the burn address excluding programmatic burns.',
-  },
-  breakdownMethodology: {
-    dailyFees: {
-      fees: 'Total validator rewards'
-    },
-    dailyRevenue: {
-      protocol_revenue: '5% staking yield taken as protocol revenue'
-    },
-    dailySupplySideRevenue: {
-      supply_side: 'Rewards distributed to stakers and node operators'
-    },
-    dailyHoldersRevenue: {
-      holders_buyback: 'SWELL transferred to the burn address excluding programmatic burns'
-    }
-  },
+  methodology,
+  breakdownMethodology,
+  allowNegativeValue: true,
 };
 
 export default adapter;
