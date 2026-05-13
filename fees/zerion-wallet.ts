505eb173701f99b6a95f0831669a7f6a14b68e34
bheluga <bheluga@defillama.com>
2026-05-13 17:48:32 +0530

    Update zerion-wallet.ts

--- fees/zerion-wallet.ts
+++ fees/zerion-wallet.ts
@@ -34,7 +34,7 @@ const adapter: Adapter = {
   methodology,
   version: 2,
   pullHourly: true,
-  chains: [CHAIN.ETHEREUM, CHAIN.OPTIMISM, CHAIN.ARBITRUM, CHAIN.BASE, CHAIN.POLYGON, CHAIN.BSC,],
+  chains: Object.keys(multisigs),
 }
 
 export default adapter;
