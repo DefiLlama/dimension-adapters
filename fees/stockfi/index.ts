761aa376188eb68b6ba8681604b8e731471f48f7
bheluga <bheluga@defillama.com>
2026-05-15 22:42:52 +0530

    add methodology

--- fees/stockfi/index.ts
+++ fees/stockfi/index.ts
@@ -1,6 +1,7 @@
 import BigNumber from "bignumber.js";
 import { FetchOptions, SimpleAdapter } from "../../adapters/types"
 import { CHAIN } from "../../helpers/chains"
+import { METRIC } from "../../helpers/metrics";
 
 const VAULT_REGISTRY = "0x9732A52eB6BAc678BbC95F6C06Ba70a5b2071379";
 const VAULT_REGISTERED_EVENT = "event VaultRegistered(address indexed vault, address indexed baseToken)";
@@ -24,26 +25,33 @@ const fetch = async (options: FetchOptions) => {
     });
 
     for (const fee of feeData) {
-        dailyFees.addUSDValue(new BigNumber(fee.amount.toString()).div(1e18).toNumber(), 'Borrowing Fees');
+        dailyFees.addUSDValue(new BigNumber(fee.amount.toString()).div(1e18).toNumber(), METRIC.MINT_REDEEM_FEES);
     };
 
     return { dailyFees, dailyRevenue: dailyFees, dailyProtocolRevenue: dailyFees }
 };
 
+const methodology = {
+    Fees: "A one-time 1% fee charged on stUSD minted from each vault.",
+    Revenue: "A one-time 1% fee charged on stUSD minted from each vault.",
+    ProtocolRevenue: "A one-time 1% fee charged on stUSD minted from each vault.",
+}
+
 const breakdownMethodology = {
   Fees: {
-    "Borrowing Fees": "A one-time 1% fee charged on stUSD minted from each vault.",
+    [METRIC.MINT_REDEEM_FEES]: "A one-time 1% fee charged on stUSD minted from each vault.",
   },
   Revenue: {
-    "Borrowing Fees": "A one-time 1% fee charged on stUSD minted from each vault.",
+    [METRIC.MINT_REDEEM_FEES]: "A one-time 1% fee charged on stUSD minted from each vault.",
   },
   ProtocolRevenue: {
-    "Borrowing Fees": "A one-time 1% fee charged on stUSD minted from each vault."
+    [METRIC.MINT_REDEEM_FEES]: "A one-time 1% fee charged on stUSD minted from each vault."
   }
 };
 
 const adapter: SimpleAdapter = {
   version: 2,
+  methodology,
   breakdownMethodology,
   pullHourly: true,
   adapter: {
