8c39ec0212c6b9f575f8c32d6c059130afb12677
Vukašin Manojlović <vmanojlovic@monad.foundation>
2026-05-15 12:05:08 +0200

    Move Euler DAO's Euler vaults to K3

--- factory/curators.ts
+++ factory/curators.ts
@@ -121,7 +121,7 @@ const configs: Record<string, CuratorConfig> = {
       [CHAIN.SWELLCHAIN]: { eulerVaultOwners: ['0xC798cA555e4C7e6Fa04A23e1a727c12884F40B69'], start: '2025-01-01' },
       [CHAIN.LINEA]: { eulerVaultOwners: ['0x624DC899774EEf1cD9c17ED10d19c9483Fa9eb0A'], start: '2025-10-01' },
       [CHAIN.ARBITRUM]: { eulerVaultOwners: ['0xAeE4e2E8024C1B58f4686d1CB1646a6d5755F05C'], start: '2025-10-01' },
-      [CHAIN.MONAD]: { eulerVaultOwners: ['0x5D42F8aCd567810D57D60f90bB9C6d194207a6e1'], start: '2025-11-28' },
+      // [CHAIN.MONAD]: { eulerVaultOwners: ['0x5D42F8aCd567810D57D60f90bB9C6d194207a6e1'], start: '2025-11-28' }, operated by k3 now: https://forum.euler.finance/t/sunsetting-of-dao-managed-market-and-vaults/1828
     },
   },
   "feather": {
@@ -174,6 +174,7 @@ const configs: Record<string, CuratorConfig> = {
       [CHAIN.ARBITRUM]: { eulerVaultOwners: ['0xAeE4e2E8024C1B58f4686d1CB1646a6d5755F05C'], start: '2025-07-01' },
       [CHAIN.UNICHAIN]: { eulerVaultOwners: ['0xAeE4e2E8024C1B58f4686d1CB1646a6d5755F05C'], start: '2025-10-01' },
       [CHAIN.ETHEREUM]: { morphoVaultOwners: ['0xdD84A24eeddE63F10Ec3e928f1c8302A47538b6B'], eulerVaultOwners: ['0xdD84A24eeddE63F10Ec3e928f1c8302A47538b6B'], start: '2025-07-01' },
+      [CHAIN.MONAD]: { eulerVaultOwners: ['0x5D42F8aCd567810D57D60f90bB9C6d194207a6e1'], start: '2025-11-28' },
     },
   },
   "keyring": {
