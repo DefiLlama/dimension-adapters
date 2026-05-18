149c73659e98849aa681aea4778af213bdfd186d
pdubey7 <rdpushp1@gmail.com>
2026-05-18 20:17:57 +0530

    feat: add Jupiter Lend isolated Ethena (USDe) market fees adapter
    Tracks fees/revenue for the Jupiter Lend x Ethena isolated USDe
    lending market launched May 13, 2026 with Bitwise as curator.
    
    - New SQL: filters jupiter-lend Dune query to USDe mint only
      (DEkqHyPN7GMRJ5cArtQFAWefqbZb33Hyf6s5iCwjEonT)
    - Same fee split as jupiter-lend: 50% Fluid, 50% Jupiter/JUP holders
    - doublecounted: true (subset of jupiter-lend which tracks all markets)
    - Market filled 200M+ in first days at ~20% APY
    
    Closes #7045

---
+++ fees/jupiter-lend-ethena/index.ts
@@ -0,0 +1,79 @@
+import { Dependencies, FetchOptions, FetchResultV2, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { JUPITER_METRICS, jupBuybackRatioFromRevenue } from "../jupiter";
+import { getSqlFromFile, queryDuneSql } from "../../helpers/dune";
+
+const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResultV2> => {
+  const sql = getSqlFromFile("helpers/queries/jupiter-lend-ethena.sql", {
+    start: options.startTimestamp,
+    end: options.endTimestamp
+  });
+
+  const data: any[] = await queryDuneSql(options, sql);
+
+  const df = data.reduce((sum, row) => sum + (row.daily_fees_usd || 0), 0);
+  const dssrToLenders = data.reduce((sum, row) => sum + (row.daily_supply_side_revenue_usd || 0), 0);
+  const drBeforeFluidShare = data.reduce((sum, row) => sum + (row.daily_revenue_usd || 0), 0);
+  const drFluidShare = drBeforeFluidShare * 0.5;
+  const dr = drBeforeFluidShare - drFluidShare;
+
+  const dailyFees = options.createBalances();
+  const dailySupplySideRevenue = options.createBalances();
+  const dailyRevenue = options.createBalances();
+  const dailyProtocolRevenue = options.createBalances();
+  const dailyHoldersRevenue = options.createBalances();
+
+  dailyFees.addUSDValue(df, JUPITER_METRICS.BorrowInterest);
+  dailySupplySideRevenue.addUSDValue(dssrToLenders, JUPITER_METRICS.InterestToLenders);
+  dailySupplySideRevenue.addUSDValue(drFluidShare, JUPITER_METRICS.InterestToFluid);
+  dailyRevenue.addUSDValue(dr, JUPITER_METRICS.InterestToJupiter);
+
+  const buybackRatio = jupBuybackRatioFromRevenue(options.startOfDay);
+  dailyProtocolRevenue.addUSDValue(dr * (1 - buybackRatio), JUPITER_METRICS.InterestToJupiter);
+  dailyHoldersRevenue.addUSDValue(dr * buybackRatio, JUPITER_METRICS.TokenBuyBack);
+
+  return {
+    dailyFees,
+    dailyRevenue,
+    dailyProtocolRevenue,
+    dailySupplySideRevenue,
+    dailyHoldersRevenue,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  fetch,
+  chains: [CHAIN.SOLANA],
+  start: '2026-05-13',
+  dependencies: [Dependencies.DUNE],
+  methodology: {
+    Fees: 'All interest paid by borrowers in the Jupiter Lend Ethena (USDe) isolated market.',
+    Revenue: 'Amount of interest share to Jupiter and JUP token holders.',
+    ProtocolRevenue: '50% of the revenue goes to Jupiter.',
+    HoldersRevenue: '50% of the revenue goes to JUP token holders via buy back.',
+    SupplySideRevenue: 'Amount of interest distributed to lenders and Fluid from the Ethena market.',
+  },
+  breakdownMethodology: {
+    Fees: {
+      [JUPITER_METRICS.BorrowInterest]: 'All interest paid by borrowers in the Ethena USDe market.',
+    },
+    Revenue: {
+      [JUPITER_METRICS.InterestToJupiter]: 'Jupiter share of interest (50% of protocol cut, other 50% to Fluid).',
+    },
+    SupplySideRevenue: {
+      [JUPITER_METRICS.InterestToLenders]: 'Interest distributed to USDe lenders.',
+      [JUPITER_METRICS.InterestToFluid]: 'Interest distributed to Fluid.',
+    },
+    ProtocolRevenue: {
+      [JUPITER_METRICS.InterestToJupiter]: '50% of the revenue goes to Jupiter.',
+    },
+    HoldersRevenue: {
+      [JUPITER_METRICS.TokenBuyBack]: '50% of the revenue used to buy back JUP.',
+    },
+  },
+  isExpensiveAdapter: true,
+  doublecounted: true,
+};
+
+export default adapter;
