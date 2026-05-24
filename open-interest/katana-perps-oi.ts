c25a7280efe1444e83dc33fe6c92d67bc70a9c30
bheluga <bheluga@defillama.com>
2026-05-24 23:33:06 +0530

    rename

--- open-interest/katana-perps-oi.ts
+++ open-interest/katana-perps-oi.ts
@@ -1,11 +1,11 @@
 import type { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { fetchURLAutoHandleRateLimit } from "../utils/fetchURL";
+import fetchURL from "../utils/fetchURL";
 
 const MARKETS_API_URL = "https://api-perps.katana.network/v1/markets";
 
 const fetch = async (_: FetchOptions) => {
-  const markets = await fetchURLAutoHandleRateLimit(MARKETS_API_URL);
+  const markets = await fetchURL(MARKETS_API_URL);
   const openInterestAtEnd = markets.reduce(
     (sum: number, market: any) => sum + Number(market.openInterest) * Number(market.indexPrice),
     0
@@ -21,7 +21,6 @@ const adapter: SimpleAdapter = {
   fetch,
   chains: [CHAIN.KATANA],
   runAtCurrTime: true,
-  start: "2026-01-12",
 };
 
 export default adapter;
