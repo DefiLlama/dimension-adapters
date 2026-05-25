b0d3df27c2d9e6ec4da00a74462e24ccaf0f875e
Chibuzor Daniel Uwezukwe <uwezukwechibuzor@gmail.com>
2026-05-25 08:00:13 +0100

    Track Scopuly fees and revenue (#7180)
    * Track Scopuly fees and revenue
    
    * dont swallow eroos
    
    * smol fix
    
    ---------
    
    Co-authored-by: bheluga <bheluga@defillama.com>

--- dexs/Scopuly/index.ts
+++ dexs/Scopuly/index.ts
@@ -1,24 +1,35 @@
 import fetchURL from "../../utils/fetchURL";
-import { SimpleAdapter } from "../../adapters/types";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
-const historicalVolumeEndpoint = "https://api.scopuly.com/api/liquidity_pools_volume"
+const historicalDataEndpoint = "https://api.scopuly.com/api/liquidity_pools_volume"
 
-interface IVolumeall {
+interface IChartItem {
   vol: number;
   time: number;
+  fees: number;
 }
 
-const fetch = async (timestamp: number) => {
+const fetch = async (timestamp: number, _: any, options: FetchOptions) => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
-  const historicalVolume: IVolumeall[] = (await fetchURL(historicalVolumeEndpoint));
+  const historicalData: IChartItem[] = await fetchURL(historicalDataEndpoint)
 
-  const dailyVolume = historicalVolume
-    .find(dayItem => getUniqStartOfTodayTimestamp(new Date(Number(dayItem.time))) === dayTimestamp)?.vol
+  const findDay = (items: IChartItem[]) =>
+    items.find(item => getUniqStartOfTodayTimestamp(new Date(Number(item.time))) === dayTimestamp)
+
+  const item = findDay(historicalData)
+
+  if (!item)
+    throw new Error(`No data found for date ${options.dateString}`)
 
   return {
-    dailyVolume: dailyVolume,
+    dailyVolume: item.vol,
+    dailyFees: item.fees,
+    dailyUserFees: item.fees,
+    dailyRevenue: "0",
+    dailyProtocolRevenue: "0",
+    dailySupplySideRevenue: item.fees,
     timestamp: dayTimestamp,
   };
 };
@@ -30,6 +41,7 @@ const adapter: SimpleAdapter = {
       start: '2024-01-30',
     },
   },
+  doublecounted: true, //stellar dex
 };
 
 export default adapter;
