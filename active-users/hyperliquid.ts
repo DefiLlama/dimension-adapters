ec51d7b2733693e5790a70c63848c8b0288ea56d
Shailesh K <56791956+Shaileshkhote@users.noreply.github.com>
2026-05-29 14:13:20 +0530

    feat: track active users and txcount for hypeprliquid

---
+++ active-users/hyperliquid.ts
@@ -0,0 +1,30 @@
+import { Dependencies, FetchOptions, ProtocolType, SimpleAdapter } from "../adapters/types";
+import { queryAllium } from "../helpers/allium";
+import { CHAIN } from "../helpers/chains";
+
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
+  const alliumQuery = `
+    SELECT
+      COALESCE(active_users, 0) AS user_count,
+      COALESCE(success_transactions, 0) AS transaction_count
+    FROM hyperliquid.metrics.overview
+    WHERE activity_date = TO_DATE(TO_TIMESTAMP_NTZ(${options.startTimestamp}))
+  `;
+
+  const alliumResult = await queryAllium(alliumQuery);
+
+  return {
+    dailyActiveUsers: alliumResult[0]?.user_count ?? 0,
+    dailyTransactionsCount: alliumResult[0]?.transaction_count ?? 0,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  fetch,
+  chains: [CHAIN.HYPERLIQUID],
+  dependencies: [Dependencies.ALLIUM],
+  start: "2023-11-24",
+};
+
+export default adapter;
