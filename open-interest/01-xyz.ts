5eff60e3fb99a801017a88d1523fb3ace00a4f20
bheluga <bheluga@defillama.com>
2026-05-29 18:27:22 +0530

    revert 01-xyz changes

--- open-interest/01-xyz.ts
+++ open-interest/01-xyz.ts
@@ -1,3 +1,4 @@
+const endpoint = 'https://exchange-api.evedex.com/api/external/cmc/v1/contracts'
 import type { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import fetchURL from "../utils/fetchURL";
@@ -7,14 +8,16 @@ const fetch = async (_options: FetchOptions) => {
   const { markets } = await fetchURL('https://zo-mainnet.n1.xyz/info');
 
   let openInterestAtEnd = 0
+  let dailyVolume = 0
   for (const market of markets) {
     if (!market.symbol.endsWith('USD')) continue;
     await sleep(200); // to avoid rate limits
-    const { indexPrice, perpStats: { open_interest }} = await fetchURL(`https://zo-mainnet.n1.xyz/market/${market.marketId}/stats`);
+    const { volumeQuote24h, indexPrice, perpStats: { open_interest }} = await fetchURL(`https://zo-mainnet.n1.xyz/market/${market.marketId}/stats`);
     openInterestAtEnd += open_interest * indexPrice;
+    dailyVolume += volumeQuote24h;
   }
 
-  return { openInterestAtEnd };
+  return { openInterestAtEnd, dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
