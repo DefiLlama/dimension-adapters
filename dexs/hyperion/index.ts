17b0dc52443f04f38ca57fcb4acfdc1fba97b934
Flex <flex@Flexs-MBA.local>
2026-05-29 20:18:18 +0800

    fix: use rest defillama stats endpoints

--- dexs/hyperion/index.ts
+++ dexs/hyperion/index.ts
@@ -1,41 +1,17 @@
-import request, { gql } from "graphql-request";
 import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraph/utils";
 import fetchURL from "../../utils/fetchURL";
 
-const BASE_URL = "https://api.hyperion.xyz/v1/graphql";
+const BASE_URL =
+  "https://api.hyperion.xyz/base/data/public/defillama/volume-fee-stat";
 
 const fetch = async (timestamp: number) => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
 
-  const query = gql`
-    query defillamaStats($timestamp: Float!) {
-      api {
-        getVolumeFeeStat(timestamp: $timestamp) {
-          dailyFees
-        }
-      }
-    }
-  `;
-
-  const poolList: Array<{ dailyVolumeUSD: string }> = (
-    await fetchURL(
-      `https://assets.hyperion.xyz/files/pool-list.json?t=${dayTimestamp}`
-    )
-  ).data;
-
-  const dailyVolume = poolList.reduce(
-    (acc, { dailyVolumeUSD }) => acc + Number(dailyVolumeUSD),
-    0
+  const { dailyVolume, dailyFees } = await fetchURL(
+    `${BASE_URL}?timestamp=${dayTimestamp}`,
   );
-
-  const variables = {
-    timestamp: dayTimestamp,
-  };
-
-  const data = await request(BASE_URL, query, variables);
-  const dailyFees = data.api.getVolumeFeeStat.dailyFees;
   const dailyRevenue = Number(dailyFees) * 0.2;
 
   return {
