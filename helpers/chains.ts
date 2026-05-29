de0f556282d5b0c1762d807aef51741600907dcd
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2026-05-29 01:27:48 +0100

    feat: Track dash chain fees

--- helpers/chains.ts
+++ helpers/chains.ts
@@ -47,6 +47,7 @@ export enum CHAIN {
   LITECOIN = "litecoin",
   BITCOIN = "bitcoin",
   BITCOIN_CASH = "bitcoincash",
+  DASH = "dash",
   DOGECHAIN = "dogechain",
   ENERGI = "energi",
   VELAS = "velas",
