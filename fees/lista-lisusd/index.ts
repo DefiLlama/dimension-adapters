97586d12be151f9a249d480360f9834d286ed6b3
eden <noat.eth@gmail.com>
2026-05-31 00:15:20 +0700

    allow pullHourly

--- fees/lista-lisusd/index.ts
+++ fees/lista-lisusd/index.ts
@@ -248,7 +248,7 @@ const fetch = async (options: FetchOptions) => {
 
 const adapter: SimpleAdapter = {
   version: 2,
-  // pullHourly: true,
+  pullHourly: true,
   adapter: {
     [CHAIN.BSC]: {
       fetch,
