b64f8dcd1f43f6d63daa21de7d5c4f6d8948a546
Pavel Emdin <zanziver@gmail.com>
2026-06-01 11:19:35 +0200

    add igra chain transaction fees via blockscout

--- factory/blockscout.ts
+++ factory/blockscout.ts
@@ -125,6 +125,7 @@ const protocolChainMap: Record<string, string> = {
   "lukso": CHAIN.LUKSO,
   "kasplex": CHAIN.KASPLEX,
   "gatelayer": CHAIN.GATE_LAYER,
+  "igra": CHAIN.IGRA,
 }
 
 const deadFromMap: Record<string, string> = {
