e3a06bfa3a7344d99944df667c207b5862565e3e
lkhoshnafs <l.khoshnafs@gmail.com>
2026-06-01 21:35:52 +0400

    refactor(akka): switch to on-chain volume tracking via Swap events

--- aggregators/akka/index.ts
+++ aggregators/akka/index.ts
@@ -1,53 +1,47 @@
-import fetchURL from "../../utils/fetchURL";
-import { FetchOptions, FetchResult, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 
-const URL = 'https://routerv2.akka.finance';
+const swapEvent =
+  "event Swap(address indexed _from, address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut, uint256 fee)";
 
-interface IAPIResponse {
-  dailyVolume: string;
-}
-
-const chainIds = {
-  [CHAIN.CORE]: 1116,
-  [CHAIN.XDC]: 50,
-  [CHAIN.BITLAYER]: 200901,
-  [CHAIN.BSQUARED]: 223,
+const config: Record<string, { targets: string[]; start: string; end?: string }> = {
+  [CHAIN.HYPERLIQUID]: {
+    targets: ["0xcce7452db4392b40aa0e1592a7c486e13bf69654"],
+    start: "2026-01-20",
+  },
+  [CHAIN.CORE]: {
+    targets: [
+      "0x7C5Af181D9e9e91B15660830B52f7B7076Be0d64",
+      "0x5f3B7b49c5763045a6571dEe9A2b13ccd2407daA",
+      "0x0cfedb22dbda450077dc64d21f58ad8ff3646036",
+    ],
+    start: "2023-11-19",
+    end: "2026-05-14",
+  },
 };
 
-const startTimestamps = {
-  [CHAIN.CORE]: '2024-06-01',
-  [CHAIN.XDC]: '2024-10-29',
-  [CHAIN.BITLAYER]: '2024-10-29',
-  [CHAIN.BSQUARED]: '2024-10-29',
-};
+const fetch = async (options: FetchOptions) => {
+  const dailyVolume = options.createBalances();
+  const { targets } = config[options.chain];
 
-const fetch = async (_a: any, _b: any, options: FetchOptions): Promise<FetchResult> => {
-  const chainId = chainIds[options.chain];
+  const logs = await options.getLogs({
+    targets,
+    eventAbi: swapEvent,
+  });
 
-  const endpoint = `/v2/${chainId}/statistics/dappradar`;
-  const response = await fetchURL(`${URL}${endpoint}`);
+  logs.forEach((log: any) => {
+    dailyVolume.add(log.tokenIn, log.amountIn);
+  });
 
-  const { dailyVolume }: IAPIResponse = response;
-
-  return {
-    dailyVolume
-  };
+  return { dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
-  version: 1,
-  adapter: Object.keys(chainIds).reduce((acc, chain) => {
-    const startTimestamp = startTimestamps[chain];
-    return {
-      ...acc,
-      [chain]: {
-        fetch,
-        runAtCurrTime: true,
-        start: startTimestamp,
-      },
-    };
-  }, {}),
+  version: 2,
+  adapter: Object.entries(config).reduce((acc, [chain, { start, end }]) => {
+    acc[chain] = { fetch, start, ...(end ? { end } : {}) };
+    return acc;
+  }, {} as any),
 };
 
 export default adapter;
