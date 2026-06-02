bb8793cf1183e2c52373a4406678f714772a1188
Felix Bruguera <felixbruguera0@gmail.com>
2026-06-02 14:46:33 -0400

    add pharos and usdpm market to pleasing gold

--- fees/pleasing-gold.ts
+++ fees/pleasing-gold.ts
@@ -1,29 +1,48 @@
 import { SimpleAdapter, FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 
-const PSWAP_CONTRACT = "0x3D084Fc4Cc4D5A0B8d6B6517341f359505b35336";
-const PGOLD = "0x3e76BB02286BFeAA89DD35f11253f2CbCE634F91";
+const PSWAP_CONTRACTS: Record<string, string[]>  = {
+    [CHAIN.ARBITRUM]: ["0x3D084Fc4Cc4D5A0B8d6B6517341f359505b35336"],
+    [CHAIN.PHAROS]: ["0xee860417fc843a7191d88c62b2937bb891b24bc6"],
+}
+const PGOLD: Record<string, string> = {
+    [CHAIN.ARBITRUM]: "0x3e76BB02286BFeAA89DD35f11253f2CbCE634F91",
+    [CHAIN.PHAROS]: "0x531f1e4a3ca96b9f42467659d8088b07fe8d2839",
+}
 const PUSD = "0xC8Fb643D18F1e53698CFDa5c8Fdf0cdC03C1dBec"
+const USDPM: Record<string, string> = {
+    [CHAIN.ARBITRUM]: "0x0122947b771672e4ed24c6b8959202fc5171ac77",
+    [CHAIN.PHAROS]: "0x16a7228ac1e772c5029d7069f3a6eca66f894218",
+}
 
 const swapPGOLDToPUSD = "event SwapPGOLDToPUSD(address user, uint256 inAmount, uint256 outAmount, uint256 fee)";
 const swapPUSDToPGOLD = "event SwapPUSDToPGOLD(address user, uint256 inAmount, uint256 outAmount, uint256 fee)";
+const swapPGOLDToUSDPM = "event SwapPGOLDToUSDpm(address user , uint256 inAmount , uint256 outAmount , uint256 fee)";
+const swapUSDPMToPGOLD = "event SwapUSDpmToPGOLD(address user , uint256 inAmount , uint256 outAmount , uint256 fee)"
 
 const fetch = async (options: FetchOptions) => {
     const dailyFees = options.createBalances();
+    const contracts = PSWAP_CONTRACTS[options.chain]
 
-    const [pGoldToPUSDLogs, pUSDToPGOLDLogs] = await Promise.all([
-        options.getLogs({ target: PSWAP_CONTRACT, eventAbi: swapPGOLDToPUSD }),
-        options.getLogs({ target: PSWAP_CONTRACT, eventAbi: swapPUSDToPGOLD }),
+    const [pGoldToPUSDLogs, pUSDToPGOLDLogs, PGOLDToUSDPMLogs, USDPMToPGOLDLogs] = await Promise.all([
+        options.getLogs({ targets: contracts, eventAbi: swapPGOLDToPUSD }),
+        options.getLogs({ targets: contracts, eventAbi: swapPUSDToPGOLD }),
+        options.getLogs({ targets: contracts, eventAbi: swapPGOLDToUSDPM }),
+        options.getLogs({ targets: contracts, eventAbi: swapUSDPMToPGOLD }),
     ]);
 
     // SwapPGOLDToPUSD: fee is in PUSD (18 decimals, USD-pegged)
     for (const log of pGoldToPUSDLogs) {
         dailyFees.add(PUSD, log.fee);
     }
+    // SwapPGOLDToUSDPM: fee is in USDPM (18 decimals, USD-pegged)
+    for (const log of PGOLDToUSDPMLogs) {
+        dailyFees.add(USDPM[options.chain], log.fee);
+    }
 
     // SwapPUSDToPGOLD: fee is in PGOLD
-    for (const log of pUSDToPGOLDLogs) {
-        dailyFees.add(PGOLD, log.fee);
+    for (const log of pUSDToPGOLDLogs.concat(USDPMToPGOLDLogs)) {
+        dailyFees.add(PGOLD[options.chain], log.fee);
     }
 
     return { dailyFees, dailyRevenue: 0, dailySupplySideRevenue: dailyFees };
@@ -34,6 +53,10 @@ const adapter: SimpleAdapter = {
     pullHourly: true,
     adapter: {
         [CHAIN.ARBITRUM]: {
+            fetch,
+            start: '2025-10-29',
+        },
+        [CHAIN.PHAROS]: {
             fetch,
             start: '2025-10-29',
         }
