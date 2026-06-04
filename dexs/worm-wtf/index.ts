56c2fcf26103fa1ce94f5544e1a52b5342011dc4
treeoflife2 <sol.analyzoor@gmail.com>
2026-06-04 18:56:49 +0530

    version 1 fix for wom-wtf

--- dexs/worm-wtf/index.ts
+++ dexs/worm-wtf/index.ts
@@ -5,7 +5,7 @@ import { queryDuneSql } from "../../helpers/dune";
 const WORM_PROGRAM = "WrgN8d3Xe7qTzZw59kiXaf3fAagHHWg78Mbhkn2dTPD";
 const CREATOR_PROGRAM = "SormXyTMQ69ux8yhn9CBQ8v7UuqepefMHbM5TcNDtkf";
 
-const fetch = async (options: FetchOptions) => {
+const fetch = async (_a: any, _b: any, options: FetchOptions) => {
   const rows = await queryDuneSql(options, `
     WITH calls AS (
       SELECT
