733a40fca55a67df22b724196c7d0f73772e836f
DaniiRix <danielrix01@protonmail.com>
2026-06-05 00:21:12 +0530

    manta: track users via allium

--- users/chains.ts
+++ users/chains.ts
@@ -94,6 +94,7 @@ const alliumChainMap: Record<string, string> = {
     katana: CHAIN.KATANA,
     abstract: CHAIN.ABSTRACT,
     linea: CHAIN.LINEA,
+    manta_pacific: CHAIN.MANTA,
     ronin: CHAIN.RONIN,
     sonic: CHAIN.SONIC,
     mantle: CHAIN.MANTLE,
