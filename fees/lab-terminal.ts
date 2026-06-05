cf4369a5415a9bfe5e80f6ef6dfc377df5f53aab
Muhammad Saif <saifsaddiquee786@gmail.com>
2026-06-05 11:35:04 +0500

    chores: Added lab-terminal adapter via Dune

--- fees/lab-terminal.ts
+++ fees/lab-terminal.ts
@@ -1,15 +1,15 @@
 import ADDRESSES from "../helpers/coreAssets.json";
-import { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { Dependencies, FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { queryDuneSql } from "../helpers/dune";
 
-const fetch: any = async (_: any, _1: any, options: FetchOptions) => {
+const fetch: any = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
   const dailyVolume = options.createBalances();
 
   const query = `
   SELECT
-    COALESCE(daily_fees_sol, 0)          AS daily_fees_sol,
+    COALESCE(daily_fees_usd_sol, 0)          AS daily_fees_usd_sol,
     COALESCE(daily_solana_volume_usd, 0) AS daily_solana_volume_usd
   FROM
     dune.lab_terminal.historical_aggregates_v3
@@ -22,16 +22,18 @@ const fetch: any = async (_: any, _1: any, options: FetchOptions) => {
   const res = await queryDuneSql(options, query);
   if (!res?.length) return { dailyFees, dailyVolume };
 
-  dailyFees.add(ADDRESSES.solana.SOL, res[0].daily_fees_sol * 1e9);
-  dailyVolume.add(ADDRESSES.solana.USDC, res[0].daily_solana_volume_usd * 1e6);
+  dailyFees.add(ADDRESSES.solana.SOL, res[0].daily_fees_usd_sol * 1e9);
+  dailyVolume.add(ADDRESSES.solana.SOL, res[0].daily_solana_volume_usd * 1e6);
 
   return { dailyFees, dailyVolume };
 };
 
 const adapter: SimpleAdapter = {
-  chains: [CHAIN.SOLANA],
+  version: 2,
   fetch,
+  chains: [CHAIN.SOLANA],
   start: "2025-06-29",
+  dependencies: [Dependencies.DUNE],
   isExpensiveAdapter: true,
   methodology: {
     Fees: "Trading fees paid by users in SOL",
