48dfc74259e6a9386f2ef1e7afef588cba7facf7
eden <noat.eth@gmail.com>
2026-06-09 04:16:18 +0700

    broken lab-termial fees

--- fees/lab-terminal.ts
+++ fees/lab-terminal.ts
@@ -4,30 +4,32 @@ import { CHAIN } from "../helpers/chains";
 import { queryDuneSql } from "../helpers/dune";
 
 const fetch: any = async (options: FetchOptions) => {
+  throw Error('made it broken while verying numbers')
+  
   const dailyFees = options.createBalances();
-  const dailyVolume = options.createBalances();
+  // const dailyVolume = options.createBalances();
 
   const query = `
-  SELECT
-    COALESCE(daily_fees_usd_sol, 0)          AS daily_fees_usd_sol,
-    COALESCE(daily_solana_volume_usd, 0) AS daily_solana_volume_usd
-  FROM
-    dune.lab_terminal.historical_aggregates_v3
-  WHERE
-    snapshot_date >= CAST(from_unixtime(${options.startTimestamp}) AS DATE)
-    AND snapshot_date <= CAST(from_unixtime(${options.endTimestamp}) AS DATE)
-  LIMIT 1
-`;
+    SELECT
+      COALESCE(daily_fees_usd_sol, 0)          AS daily_fees_usd_sol,
+      COALESCE(daily_solana_volume_usd, 0) AS daily_solana_volume_usd
+    FROM
+      dune.lab_terminal.historical_aggregates_v3
+    WHERE
+      snapshot_date >= CAST(from_unixtime(${options.startTimestamp}) AS DATE)
+      AND snapshot_date <= CAST(from_unixtime(${options.endTimestamp}) AS DATE)
+    LIMIT 1
+  `;
 
   const res = await queryDuneSql(options, query);
-  if (!res?.length) return { dailyFees, dailyVolume };
+  if (!res?.length) return { dailyFees };
 
   dailyFees.add(ADDRESSES.solana.SOL, res[0].daily_fees_usd_sol * 1e9);
-  dailyVolume.add(ADDRESSES.solana.SOL, res[0].daily_solana_volume_usd * 1e6);
+  // dailyVolume.add(ADDRESSES.solana.SOL, res[0].daily_solana_volume_usd * 1e6);
 
   return {
     dailyFees,
-    dailyVolume,
+    // dailyVolume,
     // dailyRevenue: dailyFees,  // skipping these for now as we are not excluding amount for referrals
     // dailyProtocolRevenue: dailyFees,
   };
@@ -39,7 +41,6 @@ const adapter: SimpleAdapter = {
   chains: [CHAIN.SOLANA],
   start: "2025-06-29",
   dependencies: [Dependencies.DUNE],
-  isExpensiveAdapter: true,
   methodology: {
     Fees: "Trading fees paid by users in SOL",
     Volume: "Total USD volume of trades on Solana",
