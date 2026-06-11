338f1c37805be5eb0f98b482cfe4fba8aceaf3e1
Piteas <134698667+piteasio@users.noreply.github.com>
2026-06-11 14:18:36 +0300

    Add files via upload

--- aggregators/piteas/index.ts
+++ aggregators/piteas/index.ts
@@ -2,11 +2,7 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { formatAddress } from "../../utils/utils";
 
-// PiteasRouter — deployed 2023-07-07 21:35:45 UTC
-// https://otter-pulsechain.g4mm4.io/tx/0xd48a2cf58b231ea285e05a365ab71c9fd6f539d30033513a34e512b56790d02c
 const PITEAS_ROUTER = "0x6BF228eb7F8ad948d37deD07E595EfddfaAF88A6";
-
-// Wrapped PLS — used to price native PLS swaps (srcToken/destToken may be zero address)
 const WPLS = "0xA1077a294dDE1B09bB078844df40758a5D0f9a27";
 const NATIVE_TOKEN = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 
