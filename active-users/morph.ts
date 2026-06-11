34ea88650ff6041789b2ba4ae714d3ca290be289
bheluga <bheluga@defillama.com>
2026-06-11 17:14:25 +0530

    combine in single req

--- active-users/morph.ts
+++ active-users/morph.ts
@@ -5,16 +5,9 @@ import fetchURL from "../utils/fetchURL";
 const BASE_URL = "https://api.socialscan.io/morphl2-mainnet/v1/explorer/chart-data/daily";
 
 const fetch = async (options: FetchOptions) => {
-  const activeRows = await fetchURL(`${BASE_URL}?metrics=address.active_address_cnt`);
-  const txRows = await fetchURL(`${BASE_URL}?metrics=transaction.cnt`);
-
-  const active = activeRows.data?.find((item: any) => item.date === options.dateString);
-  const tx = txRows.data?.find((item: any) => item.date === options.dateString);
-  if (!active || active["address.active_address_cnt"] == null || !tx || tx["transaction.cnt"] == null) {
-    throw new Error(`No Morph daily user/transaction data found for ${options.dateString}`);
-  }
-  const dailyActiveUsers = Number(active["address.active_address_cnt"]);
-  const dailyTransactionsCount = Number(tx["transaction.cnt"]);
+  const activeRows = await fetchURL(`${BASE_URL}?metrics=address.active_address_cnt,transaction.cnt`);
+  const dailyActiveUsers = Number(activeRows.data.find((item: any) => item.date === options.dateString)["address.active_address_cnt"]);
+  const dailyTransactionsCount = Number(activeRows.data.find((item: any) => item.date === options.dateString)["transaction.cnt"]);
 
   return {
     dailyActiveUsers,
