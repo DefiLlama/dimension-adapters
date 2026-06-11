2deed011a5534ca38bfb45f222258f0bbdf5ece5
bheluga <bheluga@defillama.com>
2026-06-11 12:55:37 +0530

    update collector crypt addresses (#7564)

--- fees/collector-crypt/index.ts
+++ fees/collector-crypt/index.ts
@@ -4,6 +4,34 @@ import { queryDuneSql } from "../../helpers/dune";
 
 const GACHA_TIERS = [25, 50, 75, 80, 100, 250, 1000];
 
+//https://dune.com/queries/7450765
+const TEAM_ADDRESSES = [
+  'BAxTk97HsaJqbnbFmTiQTaL4KSRvJ8Y65ArZCsP6vA5M',
+  '21KhtC7y2JGYvwc8dcGqTdbrudbM8fgMPJsVwxRQqdY8',
+  'DFEstpYN3fsz93AC9v2ujzPPngPgodqH2xxopuyfSsAE',
+  'HW2HRqN1pXQGH9GfP9xet4XwqtLqFyYGDNRKjUAVgh9u',
+  'HighJBfnAaqH9cKkeMErQFJZ4ATxQJwxqFupX6zaKTns',
+  'LGNDXqcm6U57QQ6Ad7icZ6oizkAVKRWrw97KwZy5nVf',
+  'EpicWWZspT1trKndbDDr29ULViN56rN5vofWSKZp8ePF',
+  'Mid9NeCpPNxP59fAdsLgMLy7BYexxXFw52ZP58Jrney',
+  'Lowq9dkpY43VpjfYeRjtKfGA6JtB7HaMmwQgXkjHLvN',
+  'Low6UekJP3QrFVMfNRTL8CPK2SiGFhvp57sgF2pkmVu',
+  'miDtj3vgdxVykHzRyFwyG8MXpvK8eQqamSLVdBr7WPt',
+  'HiGHqwYddP5N2waqUmXPdaASpMpUEvfqPr2fSawctEb',
+  'epiC3zkqa1RfcPMMM1Kc8m3GZGDwF2RmjbfA3g1BBjn',
+  'LGNDfXQFMiRMz3qqTNAREmRFQutMvazqqRrzn5i98uj',
+  'SPrT7eFrCM9UJ4j7Xf9iktKCoBwJjfykFbiNbRsKQm8',
+  'Cc4pHGnoaRWL1WnHsV517T3YvQn5gLDBMiuVXkF9rZhK',
+  '8373hLiAEXxaJ3oV7SRzx4KHwurEg9rEG98tUPj1sdtX',
+  'onePMfirJs2Rx3eixoPnjY6NHiaC74pkQ2k313K2Lxs',
+  'SportGmqffp9zC3VZV7Wwz6s2nCkEB5Q3nVwKGU4esD',
+  'DQPERZ9e86pNJ4mhUnCEP8V75yxZofsipoVrRWT5Wdxd',
+  'cc3novbXuNSe292qKH2gGhxToaWjuBvJbA7zQf8NVxi',
+  'GachaNgyXTU3zFogQ8Z5jR2BLXs8215X2AtEH18VxJq3',
+  'GachazZscHZ5bn3vnq1yEC4zpYdhAYJBzuKJwSJksc9z',
+  '96DULv1BqYfe5wyMr6pVUNC6Uyrtj6yr3tNi6VtfwW9s'
+]
+
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
   const dailyVolume = options.createBalances();
@@ -20,23 +48,7 @@ const fetch = async (options: FetchOptions) => {
         SUM(CASE WHEN amount / POWER(10, 6) = 1000 THEN amount / POWER(10, 6) ELSE 0 END) AS gacha_spend_1000
       FROM tokens_solana.transfers
       WHERE to_owner IN ('GachazZscHZ5bn3vnq1yEC4zpYdhAYJBzuKJwSJksc9z','GachaNgyXTU3zFogQ8Z5jR2BLXs8215X2AtEH18VxJq3','96DULv1BqYfe5wyMr6pVUNC6Uyrtj6yr3tNi6VtfwW9s')
-        AND from_owner NOT IN (
-          'BAxTk97HsaJqbnbFmTiQTaL4KSRvJ8Y65ArZCsP6vA5M',
-          '21KhtC7y2JGYvwc8dcGqTdbrudbM8fgMPJsVwxRQqdY8',
-          'DFEstpYN3fsz93AC9v2ujzPPngPgodqH2xxopuyfSsAE',
-          'HW2HRqN1pXQGH9GfP9xet4XwqtLqFyYGDNRKjUAVgh9u',
-          'HighJBfnAaqH9cKkeMErQFJZ4ATxQJwxqFupX6zaKTns',
-          'LGNDXqcm6U57QQ6Ad7icZ6oizkAVKRWrw97KwZy5nVf',
-          'EpicWWZspT1trKndbDDr29ULViN56rN5vofWSKZp8ePF',
-          'Mid9NeCpPNxP59fAdsLgMLy7BYexxXFw52ZP58Jrney',
-          'Lowq9dkpY43VpjfYeRjtKfGA6JtB7HaMmwQgXkjHLvN',
-          'Low6UekJP3QrFVMfNRTL8CPK2SiGFhvp57sgF2pkmVu',
-          'miDtj3vgdxVykHzRyFwyG8MXpvK8eQqamSLVdBr7WPt',
-          'HiGHqwYddP5N2waqUmXPdaASpMpUEvfqPr2fSawctEb',
-          'epiC3zkqa1RfcPMMM1Kc8m3GZGDwF2RmjbfA3g1BBjn',
-          'LGNDfXQFMiRMz3qqTNAREmRFQutMvazqqRrzn5i98uj',
-          'SPrT7eFrCM9UJ4j7Xf9iktKCoBwJjfykFbiNbRsKQm8'
-        )
+        AND from_owner NOT IN (${TEAM_ADDRESSES.map(addr => `'${addr}'`).join(', ')})
         AND amount / power(10, 6) IN (25, 50, 75, 80, 100, 250, 1000)
         AND token_mint_address = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
         AND TIME_RANGE
@@ -54,25 +66,7 @@ const fetch = async (options: FetchOptions) => {
         SUM(amount / POWER(10, 6)) AS buyback
       FROM tokens_solana.transfers
       WHERE from_owner IN ('GachazZscHZ5bn3vnq1yEC4zpYdhAYJBzuKJwSJksc9z','GachaNgyXTU3zFogQ8Z5jR2BLXs8215X2AtEH18VxJq3')
-        AND to_owner NOT IN (
-          'BAxTk97HsaJqbnbFmTiQTaL4KSRvJ8Y65ArZCsP6vA5M',
-          '21KhtC7y2JGYvwc8dcGqTdbrudbM8fgMPJsVwxRQqdY8',
-          'DFEstpYN3fsz93AC9v2ujzPPngPgodqH2xxopuyfSsAE',
-          'HW2HRqN1pXQGH9GfP9xet4XwqtLqFyYGDNRKjUAVgh9u',
-          'HighJBfnAaqH9cKkeMErQFJZ4ATxQJwxqFupX6zaKTns',
-          'LGNDXqcm6U57QQ6Ad7icZ6oizkAVKRWrw97KwZy5nVf',
-          'EpicWWZspT1trKndbDDr29ULViN56rN5vofWSKZp8ePF',
-          'Mid9NeCpPNxP59fAdsLgMLy7BYexxXFw52ZP58Jrney',
-          'Lowq9dkpY43VpjfYeRjtKfGA6JtB7HaMmwQgXkjHLvN',
-          'Low6UekJP3QrFVMfNRTL8CPK2SiGFhvp57sgF2pkmVu',
-          'miDtj3vgdxVykHzRyFwyG8MXpvK8eQqamSLVdBr7WPt',
-          'HiGHqwYddP5N2waqUmXPdaASpMpUEvfqPr2fSawctEb',
-          'epiC3zkqa1RfcPMMM1Kc8m3GZGDwF2RmjbfA3g1BBjn',
-          'LGNDfXQFMiRMz3qqTNAREmRFQutMvazqqRrzn5i98uj',
-          'SPrT7eFrCM9UJ4j7Xf9iktKCoBwJjfykFbiNbRsKQm8',
-          'Cc4pHGnoaRWL1WnHsV517T3YvQn5gLDBMiuVXkF9rZhK',
-          '8373hLiAEXxaJ3oV7SRzx4KHwurEg9rEG98tUPj1sdtX'
-        )
+        AND to_owner NOT IN (${TEAM_ADDRESSES.map(addr => `'${addr}'`).join(', ')})
         AND token_mint_address = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
         AND TIME_RANGE
     )
