55544999aedf99e5c6106a4d23c60e61760c1c4a
afx-dex <afx-dev@afx.xyz>
2026-06-16 14:15:28 +0800

    Create afx.ts

---
+++ active-users/afx.ts
@@ -0,0 +1,20 @@
+import type { FetchOptions, SimpleAdapter } from "../adapters/types";
+import { CHAIN } from "../helpers/chains";
+import { AFX_START, fetchAfxDailyStats } from "../helpers/afx";
+
+const fetch = async (options: FetchOptions) => {
+  const stats = await fetchAfxDailyStats(options);
+
+  return {
+    dailyActiveUsers: stats?.activeOrderUsers ?? 0,
+  };
+};
+
+const adapter: SimpleAdapter = {
+  version: 1,
+  fetch,
+  chains: [CHAIN.ARBITRUM],
+  start: AFX_START,
+};
+
+export default adapter;
