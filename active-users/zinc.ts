aacf3b7433ebd0f9be8021a85914aaeccc867130
Felix Bruguera <felixbruguera0@gmail.com>
2026-06-26 17:00:35 -0400

    count distinct txs

--- active-users/zinc.ts
+++ active-users/zinc.ts
@@ -5,7 +5,7 @@ import { queryDuneSql } from "../helpers/dune";
 // source: https://dune.com/queries/7638215
 const query = `
   WITH ix AS (
-    SELECT block_date AS day, tx_signer,
+    SELECT block_date AS day, tx_signer, tx_id,
           lower(to_hex(bytearray_substring(data, 1, 8))) AS disc
     FROM solana.instruction_calls
     WHERE executing_account = 'zincUFpnqYwdYMc1KfH6rKcBvbcdVtHKckKhvrHLDsV'
@@ -17,7 +17,7 @@ const query = `
         'ceb0ca12c8d1b36c','5a5f6b2acd7c32e1','69165acc9d9111e7','3ed9522b034f39f2','f37a0dc84f8fb991',
         'f56ab02904a7e15c','fcdfc8d1fc05f341','4111767b3cf731a5','b5f8d719a050304f','0c4f9d925cc55f12','577963b87e3f67d9'
       ) THEN tx_signer END) AS active_players,
-      count_if(disc IN ('1e01a07f27c78600','477a16bf7134f551')) AS rounds_deployed
+      count(DISTINCT CASE WHEN disc IN ('1e01a07f27c78600','477a16bf7134f551') THEN tx_id END) AS rounds_deployed
     FROM ix
   )
   SELECT active_players, rounds_deployed
