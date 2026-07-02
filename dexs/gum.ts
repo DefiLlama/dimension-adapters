8902cf3c4bfe7c813902ed908484c99c81fe118a
bheluga <anonymous_cat_llama@protonmail.com>
2026-07-02 23:14:40 +0530

    respect rate limit

--- dexs/gum.ts
+++ dexs/gum.ts
@@ -2,6 +2,7 @@ import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import fetchURL from "../utils/fetchURL";
 import { PromisePool } from "@supercharge/promise-pool";
+import { sleep } from "../utils/utils";
 
 const GUM_MARKETS_URL = "https://gum-api.jup.net/fe/mainnet-beta/perps/markets";
 
@@ -11,13 +12,14 @@ async function fetch(options: FetchOptions) {
   const marketsResponse = await fetchURL(GUM_MARKETS_URL);
   const symbols = marketsResponse.map((market: any) => market.symbol);
 
-  await PromisePool.withConcurrency(5)
+  await PromisePool.withConcurrency(3)
     .for(symbols).process(async (symbol) => {
       const ohlcvData = await fetchURL(`https://gum-api.jup.net/fe/mainnet-beta/perps/ohlcv/${symbol}?interval=1D&from=${options.startOfDay}&to=${options.endTimestamp}`);
       const todaysData = ohlcvData.find((data: any) => data.t === options.startOfDay * 1000);
       if (todaysData) {
         dailyVolume.addUSDValue(todaysData.v);
       }
+      await sleep(500);
     });
 
   return {
