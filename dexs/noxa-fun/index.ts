cc9d08f1bc333c54283d127378b532cc2128c9f3
bheluga <anonymous_cat_llama@protonmail.com>
2026-07-14 22:55:07 +0530

    fix noxa fun methodology

--- dexs/noxa-fun/index.ts
+++ dexs/noxa-fun/index.ts
@@ -213,7 +213,7 @@ async function fetch(options: FetchOptions) {
 }
 
 const methodology = {
-  Fees: "Includes uniswap swap fees for tokens graduated from noxa fun (filtered by pools with >$5k TVL)",
+  Fees: "Includes uniswap swap fees for tokens graduated from noxa fun (filtered by pools with >$1000 TVL)",
   Revenue: "Revenue is disabled for now",
   SupplySideRevenue: "All fees collected from swaps are distributed to the creator of the token",
 }
