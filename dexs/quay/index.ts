174056131b5671ffd3b4f2c2fa08fc4ea4bc7692
Pawel Rainer <pawel.rainer@gmail.com>
2026-07-23 14:48:28 +0200

    quay: version 1 + TIME_RANGE bounds

--- dexs/quay/index.ts
+++ dexs/quay/index.ts
@@ -28,8 +28,7 @@ const fetch = async (options: FetchOptions) => {
             inner join swaps s on t.tx_id = s.tx_id
             and t.outer_instruction_index = s.outer_instruction_index
             and t.inner_instruction_index = coalesce(s.inner_instruction_index, -1) + 1
-        where t.block_time >= from_unixtime(${options.startTimestamp})
-        and t.block_time <= from_unixtime(${options.endTimestamp})
+        where TIME_RANGE
     `;
     const data = await queryDuneSql(options, query);
 
@@ -39,6 +38,7 @@ const fetch = async (options: FetchOptions) => {
 };
 
 const adapter: SimpleAdapter = {
+    version: 1,
     fetch,
     dependencies: [Dependencies.DUNE],
     chains: [CHAIN.SOLANA],
