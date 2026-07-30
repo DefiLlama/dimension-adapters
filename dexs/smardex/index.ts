ff941f5d43389a183789949c14520684eb72a3a8
0xshubhs.eth <shubht3303@gmail.com>
2026-07-30 18:36:19 +0530

    smardex: rebuild volume and fees onchain, subgraph gateway is dead (#8493)
    * smardex: rebuild volume and fees onchain, subgraph gateway is dead
    
    * pullHourly + labeled fee breakdown
    
    * filter pairs by pooled value before pulling logs, same as univ2 helper

---
+++ dexs/smardex/index.ts
@@ -0,0 +1,51 @@
+import { cache } from "@defillama/sdk";
+import { FetchOptions, SimpleAdapter } from "../../adapters/types";
+import { CHAIN } from "../../helpers/chains";
+import { addOneToken } from "../../helpers/prices";
+import { filterPools2 } from "../../helpers/uniswap";
+import { CHAIN_CONFIG } from "./config";
+import { addUsdnVolume } from "./usdn-volume";
+
+const SWAP_EVENT = "event Swap(address indexed sender, address indexed to, int256 amount0, int256 amount1)";
+
+export async function getAmmSwapVolume(options: FetchOptions) {
+  const factory = CHAIN_CONFIG.FACTORIES[options.chain].toLowerCase();
+  const cacheKey = `tvl-adapter-cache/cache/uniswap-forks/${factory}-${options.chain}.json`;
+  let { pairs, token0s, token1s } = await cache.readCache(cacheKey, { readFromR2Cache: true });
+  if (!pairs?.length) {
+    const pairsLength = await options.api.call({ target: factory, abi: "uint256:allPairsLength" });
+    const calls = [];
+    for (let i = 0; i < Number(pairsLength); i++) calls.push({ target: factory, params: [i] });
+    pairs = await options.api.multiCall({ abi: "function allPairs(uint256) view returns (address)", calls });
+    token0s = await options.api.multiCall({ abi: "address:token0", calls: pairs });
+    token1s = await options.api.multiCall({ abi: "address:token1", calls: pairs });
+  }
+
+  ({ pairs, token0s, token1s } = await filterPools2({ fetchOptions: options, pairs, token0s, token1s }));
+
+  const dailyVolume = options.createBalances();
+  const allLogs = await options.getLogs({ targets: pairs, eventAbi: SWAP_EVENT, flatten: false });
+  allLogs.forEach((logs: any[], i: number) => {
+    for (const log of logs) {
+      addOneToken({ chain: options.chain, balances: dailyVolume, token0: token0s[i], token1: token1s[i], amount0: log.amount0, amount1: log.amount1 });
+    }
+  });
+  return dailyVolume;
+}
+
+async function fetch(options: FetchOptions) {
+  const dailyVolume = await getAmmSwapVolume(options);
+  if (options.chain === CHAIN.ETHEREUM) await addUsdnVolume(options, dailyVolume);
+  return { dailyVolume };
+}
+
+const adapter: SimpleAdapter = { adapter: {}, version: 2, pullHourly: true };
+
+Object.keys(CHAIN_CONFIG.FACTORIES).forEach((chain: string) => {
+  adapter.adapter![chain] = {
+    fetch,
+    start: CHAIN_CONFIG.START_TIMES[chain],
+  };
+});
+
+export default adapter;
