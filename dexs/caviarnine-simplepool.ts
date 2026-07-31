3183d7a69e360eacfd36618b96cfed69a8c590fb
bheluga <bheluga@defillama.com>
2026-07-31 18:45:20 +0530

    fix caviarnine spike (#8520)

--- dexs/caviarnine-simplepool.ts
+++ dexs/caviarnine-simplepool.ts
@@ -4,13 +4,15 @@ import { CHAIN } from "../helpers/chains";
 
 const URL = "https://api-core.caviarnine.com/v1.0/stats/product/simplepools";
 
+const volumeThreshold = 50_000_000;
+
 const fetch = async (): Promise<FetchResult> => {
   const response = await fetchURL(URL);
   const dailyVolume = response.summary.volume.interval_1d.usd;
   const dailyFees = response.summary.protocol_fees.interval_1d.usd;
   return {
-    dailyVolume,
-    dailyFees,
+    dailyVolume: dailyVolume > volumeThreshold ? 0 : dailyVolume,
+    dailyFees: dailyVolume > volumeThreshold ? 0 : dailyFees,
   };
 };
 
