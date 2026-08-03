23d5ff32bec68577989150cc580561013014a07a
DaniiRix <danielrix01@protonmail.com>
2026-08-03 18:21:58 +0200

    active-users: add 64 dex protocols from allium

--- users/dexUsers.ts
+++ users/dexUsers.ts
@@ -15,6 +15,7 @@ const dexUsers: { id: string; project: string; chains: string[] | Record<string,
       [CHAIN.OPTIMISM]: "2022-01-17",
       [CHAIN.BASE]: "2023-08-28",
       [CHAIN.FRAXTAL]: "2024-03-12",
+      [CHAIN.XLAYER]: "2024-05-22",
       [CHAIN.XDAI]: "2025-01-07",
       [CHAIN.HYPERLIQUID]: "2025-02-21",
       [CHAIN.CELO]: "2025-04-10",
@@ -105,6 +106,114 @@ const dexUsers: { id: string; project: string; chains: string[] | Record<string,
   { id: "5349", project: "manifest", chains: [CHAIN.SOLANA], start: "2024-10-12" },
   { id: "6884", project: "aquifer", chains: [CHAIN.SOLANA], start: "2025-07-03" },
   { id: "7666", project: "scorch", chains: [CHAIN.SOLANA], start: "2026-02-19" },
+
+  { id: "4449", project: "pumpfun", chains: [CHAIN.SOLANA], start: "2024-01-14" }, // pump.fun
+  {
+    id: "parent#dodo", // DODO AMM
+    project: "dodo",
+    chains: {
+      [CHAIN.ETHEREUM]: "2020-07-22",
+      [CHAIN.BSC]: "2021-04-27",
+      [CHAIN.POLYGON]: "2021-05-24",
+    },
+    start: "2020-07-22",
+  },
+  {
+    id: "7260", // ElfomoFi
+    project: "elfomofi",
+    chains: {
+      [CHAIN.BASE]: "2025-12-28",
+      [CHAIN.BSC]: "2026-03-03",
+    },
+    start: "2025-12-28",
+  },
+  { id: "7599", project: "listadao", chains: [CHAIN.BSC], start: "2025-11-17" }, // Lista DEX
+  { id: "parent#potatoswap", project: "potatoswap", chains: [CHAIN.XLAYER], start: "2024-04-16" }, // PotatoSwap V2
+  { id: "6368", project: "byreal", chains: [CHAIN.SOLANA], start: "2025-06-26" }, // Byreal
+  { id: "7667", project: "whalestreet", chains: [CHAIN.SOLANA], start: "2025-11-20" }, // Whalestreet
+  { id: "parent#defituna", project: "defituna", chains: [CHAIN.SOLANA], start: "2026-06-25" }, // DefiTuna AMM
+  { id: "3499", project: "ekubo", chains: [CHAIN.ETHEREUM], start: "2025-03-15" }, // Ekubo
+  { id: "parent#hydrex", project: "hydrex", chains: [CHAIN.BASE], start: "2026-06-25" }, // Hydrex Integral
+  { id: "parent#stabble", project: "stabble", chains: [CHAIN.SOLANA], start: "2024-06-05" }, // stabble Stableswap
+  { id: "1788", project: "invariant", chains: [CHAIN.SOLANA], start: "2022-03-22" }, // Invariant
+  { id: "parent#saros", project: "saros", chains: [CHAIN.SOLANA], start: "2021-12-16" }, // Saros DLMM
+  {
+    id: "parent#hyperswap", // HyperSwap V3
+    project: "hyperswap",
+    chains: {
+      [CHAIN.BSC]: "2022-07-01",
+      [CHAIN.HYPERLIQUID]: "2025-02-18",
+    },
+    start: "2022-07-01",
+  },
+  { id: "5640", project: "sonex", chains: [CHAIN.SONEIUM], start: "2025-01-14" }, // Sonex
+  { id: "parent#blackhole", project: "blackhole", chains: [CHAIN.AVAX], start: "2025-07-10" }, // Blackhole CLMM
+  {
+    id: "parent#dyorswap", // DyorSwap AMM
+    project: "dyorswap",
+    chains: {
+      [CHAIN.XLAYER]: "2024-04-16",
+      [CHAIN.INK]: "2024-12-17",
+      [CHAIN.SONEIUM]: "2024-12-21",
+      [CHAIN.PLASMA]: "2025-09-25",
+    },
+    start: "2024-04-16",
+  },
+  { id: "419", project: "saber", chains: [CHAIN.SOLANA], start: "2021-05-28" }, // Saber
+  { id: "1155", project: "sheepdex", chains: [CHAIN.BSC], start: "2021-10-10" }, // SheepDex
+  { id: "parent#shadow-exchange", project: "shadow", chains: [CHAIN.SONIC], start: "2024-12-27" }, // Shadow Exchange CLMM
+  {
+    id: "parent#spookyswap", // SpookySwap V2
+    project: "spookyswap",
+    chains: {
+      [CHAIN.FANTOM]: "2021-04-17",
+      [CHAIN.SONIC]: "2024-12-11",
+    },
+    start: "2021-04-17",
+  },
+  { id: "parent#etherex", project: "etherex", chains: [CHAIN.LINEA], start: "2025-07-26" }, // Etherex CL
+  { id: "3053", project: "fluxbeam", chains: [CHAIN.SOLANA], start: "2023-05-29" }, // FluxBeam
+  { id: "4294", project: "infusion", chains: [CHAIN.BASE], start: "2024-03-12" }, // Infusion
+  { id: "parent#guru-network-dao", project: "thick", chains: [CHAIN.BASE], start: "2024-05-21" }, // Thick
+  { id: "parent#swapx", project: "swapx", chains: [CHAIN.SONIC], start: "2024-12-30" }, // SwapX Algebra
+  { id: "parent#phoenix", project: "phoenix", chains: [CHAIN.SOLANA], start: "2023-02-15" }, // Phoenix Spot
+  { id: "2837", project: "wagmi", chains: [CHAIN.SONIC], start: "2024-12-19" }, // WAGMI
+  { id: "parent#equalizer", project: "equalizer", chains: [CHAIN.SONIC], start: "2024-12-10" }, // Equalizer Exchange
+  { id: "parent#lynex", project: "lynex", chains: [CHAIN.LINEA], start: "2023-08-14" }, // Lynex V2
+  { id: "5816", project: "defive", chains: [CHAIN.SONIC], start: "2025-01-05" }, // DeFive
+  { id: "3311", project: "retro", chains: [CHAIN.POLYGON], start: "2023-06-26" }, // Retro
+  { id: "parent#ubeswap", project: "ubeswap", chains: [CHAIN.CELO], start: "2021-02-23" }, // Ubeswap V2
+  { id: "5593", project: "inkyswap", chains: [CHAIN.INK], start: "2024-12-26" }, // InkySwap
+  { id: "4510", project: "revoswap", chains: [CHAIN.XLAYER], start: "2024-04-16" }, // Revoswap
+  { id: "3714", project: "sharkswap", chains: [CHAIN.BASE], start: "2023-10-24" }, // SharkSwap DEX
+  { id: "parent#metropolis-exchange", project: "metropolis", chains: [CHAIN.SONIC], start: "2024-12-12" }, // Metropolis DLMM
+  { id: "873", project: "bscswap", chains: [CHAIN.BSC], start: "2020-09-14" }, // BSCSwap
+  { id: "1447", project: "hashflow", chains: [CHAIN.ETHEREUM], start: "2023-10-03" }, // Hashflow
+  { id: "parent#thruster", project: "thruster", chains: [CHAIN.BLAST], start: "2024-02-28" }, // Thruster V3
+  { id: "3265", project: "agni", chains: [CHAIN.MANTLE], start: "2025-05-21" }, // Agni Finance
+  { id: "6798", project: "lithos", chains: [CHAIN.PLASMA], start: "2025-09-29" }, // Lithos
+  { id: "parent#merchant-moe", project: "merchant_moe", chains: [CHAIN.MANTLE], start: "2024-03-15" }, // Merchant Moe Liquidity Book
+  { id: "parent#synthswap", project: "synthswap", chains: [CHAIN.BASE], start: "2023-07-31" }, // Synthswap V2
+  { id: "2745", project: "leetswap", chains: [CHAIN.BASE], start: "2023-07-26" }, // LeetSwap
+  { id: "327", project: "youswap", chains: [CHAIN.BSC], start: "2022-07-04" }, // YouSwap
+  { id: "parent#9mm", project: "9mm", chains: [CHAIN.BASE], start: "2024-06-14" }, // 9mm V3
+  { id: "parent#zkswap-finance", project: "zkswap", chains: [CHAIN.ERA], start: "2023-05-30" }, // zkSwap V2
+  { id: "parent#sonus", project: "sonus_exchange", chains: [CHAIN.SONEIUM], start: "2025-01-14" }, // Sonus Exchange AMM
+  { id: "5766", project: "bulla", chains: [CHAIN.BERACHAIN], start: "2025-02-06" }, // Bulla Exchange
+  { id: "911", project: "sumswap", chains: [CHAIN.ETHEREUM], start: "2021-06-01" }, // SumSwap
+  { id: "6791", project: "ionex", chains: [CHAIN.PLASMA], start: "2025-09-27" }, // Ionex
+  { id: "parent#octoswap", project: "octoswap", chains: [CHAIN.MONAD], start: "2025-11-12" }, // OctoSwap CL
+  { id: "parent#kittenswap", project: "kittenswap", chains: [CHAIN.HYPERLIQUID], start: "2025-02-18" }, // Kittenswap Algebra
+  { id: "3255", project: "horizondex", chains: [CHAIN.BASE], start: "2023-10-13" }, // HorizonDEX
+  { id: "parent#spiritswap", project: "spiritswap", chains: [CHAIN.FANTOM], start: "2021-04-25" }, // SpiritSwap AMM
+  { id: "parent#sonefi", project: "sonefi", chains: [CHAIN.SONEIUM], start: "2025-01-09" }, // SoneFi AMM
+  { id: "6045", project: "laminar", chains: [CHAIN.HYPERLIQUID], start: "2025-03-05" }, // Laminar
+  { id: "6774", project: "fuseon", chains: [CHAIN.PLASMA], start: "2025-09-25" }, // Fuseon
+  { id: "parent#kim-exchange", project: "kim_exchange", chains: [CHAIN.MODE], start: "2024-01-15" }, // KIM Exchange V3
+  { id: "5040", project: "xtrade", chains: [CHAIN.XLAYER], start: "2025-10-28" }, // XTrade
+  { id: "parent#monoswap", project: "monoswap", chains: [CHAIN.BLAST], start: "2024-02-28" }, // MonoSwap V3
+  { id: "1007", project: "safeswap", chains: [CHAIN.ETHEREUM], start: "2021-08-28" }, // SafeSwap
+  { id: "5629", project: "squidswap", chains: [CHAIN.INK], start: "2024-12-18" }, // SquidSwap
 ];
 
 export default dexUsers.map(({ id, project, chains, start }) => ({
