86ca02952ed38da7afee5a6a3ae78177a55af24f
bheluga <bheluga@defillama.com>
2026-08-12 14:22:10 +0530

    update end date

--- dexs/justbet/index.ts
+++ dexs/justbet/index.ts
@@ -94,5 +94,5 @@ export default {
   },
   version: 2,
   pullHourly: true,
-  deadFrom: '2025-11-01', // WINR Chain was shut down
+  deadFrom: '2026-01-02', // WINR Chain was shut down
 } as Adapter;
