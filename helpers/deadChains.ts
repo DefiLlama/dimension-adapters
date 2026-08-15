34b7701b7bb46263a79d8e0b0291b9515c12f23f
0xshubhs <shubht3303@gmail.com>
2026-08-15 12:51:36 +0530

    mark moonbeam and moonriver dead, both stopped producing blocks on 2026-08-10

--- helpers/deadChains.ts
+++ helpers/deadChains.ts
@@ -1,7 +1,9 @@
 import * as sdk from "@defillama/sdk"
 
 export const deadChains = [
-  ...sdk.chainUtils.getDeadChains()
+  ...sdk.chainUtils.getDeadChains(),
+  'moonbeam', // parachain wind-down announced 2026-07-03, GLMR migrated to Base, last block 16796699 on 2026-08-10: https://forum.moonbeam.network/t/multisig-maintenance-wind-down-moonbeam-moonriver-and-moonbase-july-2026/2512
+  'moonriver', // same wind-down as moonbeam, MOVR migrated to Base, last block 17381654 on 2026-08-10
 ]
 
 
