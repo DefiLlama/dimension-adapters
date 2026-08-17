70d73b7e89eb04000a0a1ef30b7d4ddf10570481
FelixBruguera <felixbruguera0@gmail.com>
2026-08-17 12:03:52 -0400

    migrate adapters from indexa

--- fees/aura.ts
+++ fees/aura.ts
@@ -1,72 +1,30 @@
 import { Adapter, FetchOptions } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { queryIndexer } from "../helpers/indexer";
 import { METRIC } from "../helpers/metrics";
 
 const BAL_TOKEN = '0xba100000625a3754423978a60c9317c58a424e3D';
+const AURA_COLLECTOR = '0xaF52695E1bB01A16D33D7194C28C42b10e0Dbec2';
+const TRANSFER_TOPIC = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
+
+const VOTE_INCENTIVE_SOURCES = new Set([
+  '0x26743984e3357efc59f2fd6c1afdc310335a61c9',
+  '0xd3cf852898b21fc233251427c2dc93d3d604f3bb',
+]);
+
+const padAddress = (address: string) => '0x' + address.slice(2).toLowerCase().padStart(64, '0');
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
 
-  // bal vote
-  const bal_transfer_logs = await queryIndexer(`
-        SELECT
-          substr(encode(topic_1, 'hex'), 25) AS origin,
-          substr(encode(topic_2, 'hex'), 25) AS destination,
-          encode(transaction_hash, 'hex') AS HASH,
-          encode(data, 'hex') AS data
-        FROM
-          ethereum.event_logs
-        WHERE
-          block_number > 14932175
-          AND contract_address = '\\xba100000625a3754423978a60c9317c58a424e3D'
-          AND topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-          AND topic_1 in('\\x00000000000000000000000026743984e3357eFC59f2fd6C1aFDC310335a61c9', '\\x000000000000000000000000d3cf852898b21fc233251427c2dc93d3d604f3bb')
-          AND topic_2 = '\\x000000000000000000000000aF52695E1bB01A16D33D7194C28C42b10e0Dbec2'
-          AND block_time BETWEEN llama_replace_date_range;
-      `, options);
-
-  // bal yield
-  const bal_bal_yield_logs = await queryIndexer(`
-        SELECT
-          substr(encode(topic_1, 'hex'), 25) AS origin,
-          substr(encode(topic_2, 'hex'), 25) AS destination,
-          encode(transaction_hash, 'hex') AS HASH,
-          encode(data, 'hex') AS data
-        FROM
-          ethereum.event_logs
-        WHERE
-          block_number > 14932175
-          AND contract_address = '\\xba100000625a3754423978a60c9317c58a424e3D'
-          AND topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-          AND topic_1 not in('\\x00000000000000000000000026743984e3357eFC59f2fd6C1aFDC310335a61c9', '\\x000000000000000000000000d3cf852898b21fc233251427c2dc93d3d604f3bb')
-          AND topic_2 = '\\x000000000000000000000000aF52695E1bB01A16D33D7194C28C42b10e0Dbec2'
-          AND block_time BETWEEN llama_replace_date_range;
-          `, options);
-
-  // const bbusd_transfer_logs = await queryIndexer(`
-  //       SELECT
-  //         substr(encode(topic_1, 'hex'), 25) AS origin,
-  //         substr(encode(topic_2, 'hex'), 25) AS destination,
-  //         encode(transaction_hash, 'hex') AS HASH,
-  //         encode(data, 'hex') AS data
-  //       FROM
-  //         ethereum.event_logs
-  //       WHERE
-  //         block_number > 14932175
-  //         AND contract_address in ('\\xA13a9247ea42D743238089903570127DdA72fE44','\\x7b50775383d3d6f0215a8f290f2c9e2eebbeceb2', '\\xfebb0bbf162e64fb9d0dfe186e517d84c395f016')
-  //         AND topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-  //         AND topic_1 in('\\x00000000000000000000000026743984e3357eFC59f2fd6C1aFDC310335a61c9', '\\x000000000000000000000000d3cf852898b21fc233251427c2dc93d3d604f3bb')
-  //         AND topic_2 = '\\x000000000000000000000000aF52695E1bB01A16D33D7194C28C42b10e0Dbec2'
-  //         AND block_time BETWEEN llama_replace_date_range;
-  //         `, options);
-
-  bal_transfer_logs.map((e: any) => {
-    dailyFees.add(BAL_TOKEN, '0x' + e.data, METRIC.STAKING_REWARDS)
+  const logs = await options.getLogs({
+    target: BAL_TOKEN,
+    eventAbi: 'event Transfer(address indexed from, address indexed to, uint256 value)',
+    topics: [TRANSFER_TOPIC, null as any, padAddress(AURA_COLLECTOR)],
   });
 
-  bal_bal_yield_logs.map((e: any) => {
-    dailyFees.add(BAL_TOKEN, '0x' + e.data, METRIC.ASSETS_YIELDS)
+  logs.forEach((log: any) => {
+    const metric = VOTE_INCENTIVE_SOURCES.has(log.from.toLowerCase()) ? METRIC.STAKING_REWARDS : METRIC.ASSETS_YIELDS;
+    dailyFees.add(BAL_TOKEN, log.value, metric)
   });
 
 
--- fees/unibot.ts
+++ fees/unibot.ts
@@ -1,72 +1,43 @@
 import { FetchOptions, SimpleAdapter } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
-import { queryIndexer, toByteaArray } from "../helpers/indexer";
+import { queryAllium } from "../helpers/allium";
+
+const V2_ROUTER = '0x27b9c20f64920eb7fbf64491423a54df9594188c';
+const V1_ROUTER = '0x3999d2c5207c06bbc5cf8a6bea52966cabb76d41';
+const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+const UNIBOT_TOKEN = '0xf819d9cb1c2a819fd991781a822de3ca8607c3c9';
 
 const fetch: any = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
   const dailyTokenTaxes = options.createBalances();
 
-  const to_address = ["0x27B9c20f64920EB7fBF64491423a54DF9594188C"]
-  const transactions_v2 = await queryIndexer(`
-      SELECT
-        sum ("value") as value
-      FROM
-        ethereum.traces
-      WHERE
-        block_number > 17341451
-        and to_address in ${toByteaArray(to_address, { skipBytea: true })}
-        AND block_time BETWEEN llama_replace_date_range
-        `, options);
+  // native ETH transfers (including internal ones) split into the three
+  // buckets the old ethereum.traces queries tracked:
+  // - v2 fees: any ETH received by the v2 router
+  // - v1 flow: ETH moved between the v1 router and WETH (fee is 1% of it)
+  // - token tax: any ETH sent out by the UNIBOT token contract
+  const [row] = await queryAllium(`
+    SELECT
+      SUM(CASE WHEN to_address = '${V2_ROUTER}' THEN raw_amount ELSE 0 END) AS v2_fees,
+      SUM(CASE WHEN (from_address = '${WETH}' AND to_address = '${V1_ROUTER}')
+            OR (from_address = '${V1_ROUTER}' AND to_address = '${WETH}') THEN raw_amount ELSE 0 END) AS v1_flow,
+      SUM(CASE WHEN from_address = '${UNIBOT_TOKEN}' THEN raw_amount ELSE 0 END) AS token_tax
+    FROM ethereum.assets.native_token_transfers
+    WHERE transfer_type = 'value_transfer'
+      AND (
+        to_address = '${V2_ROUTER}'
+        OR (from_address = '${WETH}' AND to_address = '${V1_ROUTER}')
+        OR (from_address = '${V1_ROUTER}' AND to_address = '${WETH}')
+        OR from_address = '${UNIBOT_TOKEN}'
+      )
+      AND block_timestamp >= TO_TIMESTAMP_NTZ(${options.startTimestamp})
+      AND block_timestamp < TO_TIMESTAMP_NTZ(${options.endTimestamp})
+  `);
 
-  const transactions = await queryIndexer(`
-        SELECT
-          block_number,
-          block_time,
-          "value",
-          encode(transaction_hash, 'hex') AS HASH,
-          encode(to_address, 'hex') AS to_address
-        FROM
-          ethereum.traces
-        WHERE
-            block_number > 17447804
-            and to_address = '\\x3999D2c5207C06BBC5cf8A6bEa52966cabB76d41'
-            AND from_address = '\\xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-            AND block_time BETWEEN llama_replace_date_range
-      UNION ALL
-        SELECT
-          block_number,
-          block_time,
-          "value",
-          encode(transaction_hash, 'hex') AS HASH,
-          encode(to_address, 'hex') AS to_address
-        FROM
-          ethereum.traces
-        WHERE
-            block_number > 17447804
-            and from_address = '\\x3999D2c5207C06BBC5cf8A6bEa52966cabB76d41'
-            AND to_address = '\\xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-            AND block_time BETWEEN llama_replace_date_range
-            `, options);
-
-  transactions.map((p: any) => dailyFees.addGasToken((p.value || 0) * 0.01))
-  transactions_v2.map((p: any) => dailyFees.addGasToken(p.value || 0))
-
-
-  const revFromToken = await queryIndexer(`
-        SELECT
-          block_number,
-          block_time,
-          "value",
-          encode(transaction_hash, 'hex') AS HASH,
-          encode(to_address, 'hex') AS to_address
-        FROM
-          ethereum.traces
-        WHERE
-          block_number > 17277183
-          AND from_address = '\\xf819d9cb1c2a819fd991781a822de3ca8607c3c9'
-          AND block_time BETWEEN llama_replace_date_range
-          `, options);
-  revFromToken.concat(transactions_v2).map((p: any) => dailyTokenTaxes.addGasToken(p.value || 0))
+  dailyFees.addGasToken(Number(row.v1_flow || 0) * 0.01);
+  dailyFees.addGasToken(row.v2_fees || 0);
+  dailyTokenTaxes.addGasToken(row.token_tax || 0);
+  dailyTokenTaxes.addGasToken(row.v2_fees || 0);
 
   // ref https://dune.com/queries/2621049/4349967
   const totalFees = options.createBalances()
