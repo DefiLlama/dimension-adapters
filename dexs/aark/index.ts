2acb0d53ddc7df0d6e8c78584e72365c473f8092
bheluga <bheluga@defillama.com>
2026-09-15 23:40:05 +0530

    fix aark fees spike (#9466)

--- dexs/aark/index.ts
+++ dexs/aark/index.ts
@@ -6,9 +6,10 @@ const MoonOrderClosedV2 = "event MoonOrderClosedV2(address user, uint256 moonInd
 
 const FuturesManager = '0x0b848a8A5eC8950E67d19E7a21A6Be29F44F685e';
 
-export const inflatedMarkets = {
+export const inflatedMarkets: Record<string, bigint[]> = {
   "2026-04-23": [62n],
-  "2026-04-24": [62n] //1000PEPE actual price 0.003 , but showing as 5$ due to glitch
+  "2026-04-24": [62n], //1000PEPE actual price 0.003 , but showing as 5$ due to glitch
+  "2026-09-15": [21n],
 }
 
 const fetch = async (options: FetchOptions) => {
