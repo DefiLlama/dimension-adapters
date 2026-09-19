43eb7bf653ec942ce1cc1ef00b8862a149e9242b
treeoflife2 <sol.analyzoor@gmail.com>
2026-09-19 15:15:48 +0530

    Fix: gum ag oi

--- open-interest/gum-oi.ts
+++ open-interest/gum-oi.ts
@@ -5,21 +5,18 @@ import fetchURL from "../utils/fetchURL";
 const GUM_API_URL = "https://gum-api.jup.net/fe/mainnet-beta/perps/markets";
 
 export async function fetch(_options: FetchOptions) {
-    const response = await fetchURL(GUM_API_URL);
+  const response = await fetchURL(GUM_API_URL);
 
-    // openInterest is USD notional and double-sided (long+short), so halve it
-    const openInterestAtEnd = response.reduce((acc, curr) => acc + curr.openInterest, 0) / 2;
+  const openInterestAtEnd = response.reduce((acc: number, curr: any) => acc + curr.openInterest, 0);
 
-    return {
-        openInterestAtEnd,
-    };
+  return { openInterestAtEnd };
 }
 
 const adapter: SimpleAdapter = {
-    version: 2,
-    chains: [CHAIN.JUPNET],
-    fetch,
-    runAtCurrTime: true,
+  version: 2,
+  chains: [CHAIN.JUPNET],
+  fetch,
+  runAtCurrTime: true,
 };
 
 export default adapter;
