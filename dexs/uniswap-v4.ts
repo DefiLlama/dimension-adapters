5426bd248093167aec9b67da63dc0bd44b2088d7
bheluga <bheluga@defillama.com>
2026-09-22 23:19:19 +0530

    Revert "uniswap: disable pull hourly for refill (#9616)" (#9618)
    This reverts commit ba486657f810edde8738740b4a1d205fe95a1d82.

--- dexs/uniswap-v4.ts
+++ dexs/uniswap-v4.ts
@@ -459,7 +459,7 @@ async function fetch(options: FetchOptions) {
 
 const adapter: SimpleAdapter = {
   version: 2,
-  //pullHourly: true,
+  pullHourly: true,
   adapter: {},
   prefetch,
   methodology: {
